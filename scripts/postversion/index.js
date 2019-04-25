7d5f4ef5e6d2677849f9b17ff0c02cefe410e638
Marcus Notheis <marcus.notheis@sap.com>
2019-04-25 18:10:33 +0200

    Cleanup postversion

--- scripts/postversion/index.js
+++ scripts/postversion/index.js
@@ -1,18 +1,9 @@
-const shell = require('shelljs');
 const { highlightLog } = require('../utils');
 
 const postversion = async () => {
   highlightLog('Postversion task');
 
   await require('./release-storybook');
-  highlightLog('Update Changelog');
-  shell.exec('node_modules/.bin/lerna-changelog --from=v4.0.0 --write-to-file=CHANGELOG.md');
-  shell.exec('git add CHANGELOG.md');
-  shell.exec('git add README.md');
-  shell.exec('git commit -m "Updated Readme"');
-  shell.exec('git push');
-  highlightLog('Create GitHub Release');
-  require('./create-github-release');
 };
 
 postversion();
