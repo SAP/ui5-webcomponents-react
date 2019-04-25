0f60f162e39ca3228f86bf02b245de21435df87a
Marcus Notheis <marcus.notheis@sap.com>
2019-04-25 18:04:47 +0200

    docs: Fixed automatic storybook deployment

--- scripts/postversion/release-storybook.js
+++ scripts/postversion/release-storybook.js
@@ -5,14 +5,15 @@ const { highlightLog } = require('../utils');
 module.exports = new Promise((resolve) => {
   highlightLog('Prepare Storybook');
 
-  const storybooksGenerated = shell.exec('cd docs && ../node_modules/.bin/build-storybook -o .out').code === 0;
+  const storybooksGenerated =
+    shell.exec('node_modules/.bin/build-storybook -c packages/docs/.storybook -o .out').code === 0;
   if (!storybooksGenerated) {
     console.error('Generating storybook failed!');
     process.exit(1);
   }
   highlightLog('Deploy Storybook');
 
-  shell.exec('node_modules/.bin/storybook-to-ghpages --existing-output-dir=docs/.out');
+  shell.exec('node_modules/.bin/storybook-to-ghpages --existing-output-dir .out');
 
   highlightLog('Storybooks deployed successfully');
   resolve();
