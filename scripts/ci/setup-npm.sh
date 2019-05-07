bb1ebb8bfe81d0672627a5f043168755f13bff02
Marcus Notheis <marcus.notheis@sap.com>
2019-05-07 08:44:45 +0200

    ci(travis): Fixed script variable

--- scripts/ci/setup-npm.sh
+++ scripts/ci/setup-npm.sh
@@ -5,6 +5,6 @@ set -o errexit
 
 echo "//registry.npmjs.org/:username=${NPM_USERNAME}" > ~/.npmrc
 echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ~/.npmrc
-echo "//registry.npmjs.org/:_authToken=${$NPM_AUTH_TOKEN}" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
 
 npm whoami
