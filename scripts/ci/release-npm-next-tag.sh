b34808066aea075e854d3041c31bf10026bebc1d
Marcus Notheis <marcus.notheis@sap.com>
2019-05-08 15:24:54 +0200

    ci(pull-requests): Don't release pull requests

--- scripts/ci/release-npm-next-tag.sh
+++ scripts/ci/release-npm-next-tag.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
-RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/core && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+if [ "$TRAVIS_PULL_REQUEST" != "false" ];
+then
+    RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
+    cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+    cd ${TRAVIS_BUILD_DIR}/build/node_modules/core && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+    cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+    cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+    cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+fi
--- scripts/ci/setup-npm.sh
+++ scripts/ci/setup-npm.sh
@@ -3,8 +3,10 @@
 set -o nounset
 set -o errexit
 
-echo "//registry.npmjs.org/:username=${NPM_USERNAME}" > ~/.npmrc
-echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ~/.npmrc
-echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
-
-npm whoami
+if [ "$TRAVIS_PULL_REQUEST" != "false" ];
+then
+    echo "//registry.npmjs.org/:username=${NPM_USERNAME}" > ~/.npmrc
+    echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ~/.npmrc
+    echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
+    npm whoami
+fi
