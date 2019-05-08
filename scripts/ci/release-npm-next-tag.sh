23b4a10a9da957d2f5c6f43fed113f72fe494f09
Marcus Notheis <marcus.notheis@sap.com>
2019-05-08 12:46:46 +0200

    ci(travis): Updated Release Script

--- scripts/ci/release-npm-next-tag.sh
+++ scripts/ci/release-npm-next-tag.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
-cd build/node_modules/charts && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd build/node_modules/core && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd build/node_modules/fiori3 && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd build/node_modules/styles && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd build/node_modules/utils && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/core && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
