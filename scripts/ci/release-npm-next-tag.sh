8d4bf0a33a3a013706a4a7283e8838a7d499161d
Marcus Notheis <marcus.notheis@sap.com>
2019-05-09 09:27:03 +0200

    ci(release): Only trigger an npm release if packages changed
    Only trigger a npm release if the npm relevant packages changed. Docs and other configs are skipped

--- scripts/ci/release-npm-next-tag.sh
+++ scripts/ci/release-npm-next-tag.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
-if [ "$TRAVIS_PULL_REQUEST" != "false" ];
+CHANGED_PACKAGES=$(git diff-tree --no-commit-id --name-only -r -m  ${TRAVIS_COMMIT} | \
+    grep -e "^packages/" | \
+    grep -v -e "packages/docs")
+
+if [ -n ${CHANGED_PACKAGES} ]
 then
-    RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
-    cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-    cd ${TRAVIS_BUILD_DIR}/build/node_modules/core && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-    cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-    cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-    cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+    echo "This npm relevant packages changed - skip npm release"
+    exit 0
 fi
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+    echo "This is a pull request build - skip npm release"
+    exit 0
+fi
+
+
+RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/core && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
