83515a374f072679c64ebd8cd87d33e82dac2fc3
MarcusNotheis <marcus.notheis@sap.com>
2019-05-16 15:51:38 +0200

    WIP: Checkout branch before lerna version

--- scripts/ci/version-bump.sh
+++ scripts/ci/version-bump.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 export FIORI_FOR_REACT_SNAPSHOT_BUILD='true'
+git checkout ${TRAVIS_BRANCH}
+
 RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
 # trigger lerna release
 ${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version ${RELEASE_VERSION} \
