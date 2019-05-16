509bd75ef9ce9cdf8ff0da9490544783e7c7ce5b
MarcusNotheis <marcus.notheis@sap.com>
2019-05-16 14:58:19 +0200

    ci(travis): Create snapshot build version with lerna

--- scripts/ci/release-npm-next-tag.sh
+++ scripts/ci/release-npm-next-tag.sh
@@ -1,24 +1,34 @@
 #! /bin/bash
 
-CHANGED_PACKAGES=$(git diff-tree --no-commit-id --name-only -r -m  ${TRAVIS_COMMIT} | \
-    grep -e "^packages/" | \
-    grep -v -e "packages/docs")
+#CHANGED_PACKAGES=$(git diff-tree --no-commit-id --name-only -r -m  ${TRAVIS_COMMIT} | \
+#    grep -e "^packages/" | \
+#    grep -v -e "packages/docs")
+#
+#if [ -n ${CHANGED_PACKAGES} ]
+#then
+#    echo "No npm relevant packages changed - skip npm release"
+#    exit 0
+#fi
+#
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+#then
+#    echo "This is a pull request build - skip npm release"
+#    exit 0
+#fi
 
-if [ -n ${CHANGED_PACKAGES} ]
-then
-    echo "No npm relevant packages changed - skip npm release"
-    exit 0
-fi
+TRAVIS_BUILD_DIR=$(pwd)
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-    echo "This is a pull request build - skip npm release"
-    exit 0
-fi
-
-
+export FIORI_FOR_REACT_SNAPSHOT_BUILD='true'
 RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
-cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm version ${RELEASE_VERSION} && npm publish --access public --tag next
+# trigger lerna release and create new storybook
+./node_modules/.bin/lerna version ${RELEASE_VERSION} \
+		--exact \
+		--no-push \
+		--yes
+
+exit 0
+
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm publish --access public --tag next
+cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm publish --access public --tag next
