a15f3ddb0cf39ba9a9076c4b730f697b172585b3
MarcusNotheis <marcus.notheis@sap.com>
2019-05-17 15:19:32 +0200

    WIP: Added no verify flag to bump version

--- scripts/ci/version-bump-rc.sh
+++ scripts/ci/version-bump-rc.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-#if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
-#then
-#    echo "This is a pull request build - skip version bump"
-#    exit 0
-#fi
-#
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
+then
+    echo "This is a pull request build - skip version bump"
+    exit 0
+fi
+
 #if [ "${TRAVIS_BRANCH}" != "master" ]
 #then
 #    echo "This is a not on the master branch - skip version bump"
@@ -23,7 +23,7 @@ git checkout ${TRAVIS_BRANCH}
 
 # create a new version using standard version
 echo "Version Bump with standard version"
-${TRAVIS_BUILD_DIR}/node_modules/.bin/standard-version --prerelease rc
+${TRAVIS_BUILD_DIR}/node_modules/.bin/standard-version --prerelease rc --no-verify
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 echo "New Version after bump ${PACKAGE_VERSION}"
 
