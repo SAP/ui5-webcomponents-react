3935d93451411fef1fdb833d3e2bbd36a7f7d031
MarcusNotheis <marcus.notheis@sap.com>
2019-05-17 08:31:33 +0200

    WIP: Use Standard Version to create release canditate

--- scripts/ci/version-bump.sh
+++ scripts/ci/version-bump.sh
@@ -3,9 +3,16 @@
 export FIORI_FOR_REACT_SNAPSHOT_BUILD='true'
 git checkout ${TRAVIS_BRANCH}
 
-RELEASE_VERSION=$(node -p "require('./lerna.json').version")-$(git rev-parse --short HEAD)
+# create a new version using standard version
+${TRAVIS_BUILD_DIR}/node_modules/.bin/standard-version --prerelease rc
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+
+git tag --delete "v${PACKAGE_VERSION}"
+git reset --hard HEAD~1
+
 # trigger lerna release
-${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version ${RELEASE_VERSION} \
+${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version ${PACKAGE_VERSION} \
+        --conventional-commits \
         --allow-branch ${TRAVIS_BRANCH} \
 		--exact \
 		--no-push \
