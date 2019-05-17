76805202183c7a6662c1535bc287267a82e6522a
MarcusNotheis <marcus.notheis@sap.com>
2019-05-17 09:52:31 +0200

    WIP: Reset Change in Package.json after bump

--- scripts/ci/version-bump-rc.sh
+++ scripts/ci/version-bump-rc.sh
@@ -7,11 +7,13 @@ git checkout ${TRAVIS_BRANCH}
 ${TRAVIS_BUILD_DIR}/node_modules/.bin/standard-version --prerelease rc
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
+# discard new version in package.json again. Lerna will take care of incrementing it again.
+git checkout HEAD -- package.json
+
 # trigger lerna release
 ${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version ${PACKAGE_VERSION} \
         --conventional-commits \
         --allow-branch ${TRAVIS_BRANCH} \
-        --ignore-changes 'package.json' \
 		--exact \
 		--no-push \
 		--yes
