bfcf4ea71207be827343a7b49b85c8e805418112
MarcusNotheis <marcus.notheis@sap.com>
2019-05-20 10:11:12 +0200

    WIP: Use Lerna Prerelease Flag

--- scripts/ci/version-bump-rc.sh
+++ scripts/ci/version-bump-rc.sh
@@ -14,13 +14,14 @@ export FIORI_FOR_REACT_SNAPSHOT_BUILD='true'
 git status
 
 # create a new version using standard version
-echo "Version Bump with standard version"
+#echo "Version Bump with standard version"
 #npm run std-version -- --prerelease rc --no-verify
-PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-echo "New Version after bump ${PACKAGE_VERSION}"
+#PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+#echo "New Version after bump ${PACKAGE_VERSION}"
 
 # trigger lerna release
-${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version \
+${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version prerelease \
         --conventional-prerelease \
         --no-git-tag-version \
-        --allow-branch ${TRAVIS_BRANCH}
+        --allow-branch ${TRAVIS_BRANCH} \
+        --no-push
