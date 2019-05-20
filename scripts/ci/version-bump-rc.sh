982020230764aaf2107db0ab88a1b4443a232046
MarcusNotheis <marcus.notheis@sap.com>
2019-05-20 12:03:52 +0200

    WIP: Removed unused parts of release script
    BREAKING CHANGE Test

--- scripts/ci/version-bump-rc.sh
+++ scripts/ci/version-bump-rc.sh
@@ -6,18 +6,9 @@ then
     exit 0
 fi
 
-echo "Set Git User Name"
 git config --global user.name "ui5-webcomponents-react-bot"
 
 export FIORI_FOR_REACT_SNAPSHOT_BUILD='true'
-
-git status
-
-# create a new version using standard version
-#echo "Version Bump with standard version"
-#npm run std-version -- --prerelease rc --no-verify
-#PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-#echo "New Version after bump ${PACKAGE_VERSION}"
 
 # trigger lerna release
 ${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version prerelease \
