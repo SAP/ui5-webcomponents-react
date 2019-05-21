28757add457e2cd0765f05ae42f0e4521fc16508
MarcusNotheis <marcus.notheis@sap.com>
2019-05-21 15:15:57 +0200

    ci(travis): Push RC-Builds directly to master

--- scripts/ci/release-npm-next-tag.sh
+++ scripts/ci/release-npm-next-tag.sh
@@ -19,22 +19,21 @@ then
 fi
 
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-# TODO Add this line as soon as we have bot with admin privileges
-#git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
-git checkout -b "release/${PACKAGE_VERSION}"
-git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "release/${PACKAGE_VERSION}" > /dev/null 2>&1;
+git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
+#git checkout -b "release/${PACKAGE_VERSION}"
+#git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "release/${PACKAGE_VERSION}" > /dev/null 2>&1;
 
 cd ${TRAVIS_BUILD_DIR}/build/node_modules/charts && npm publish --access public --tag next
 cd ${TRAVIS_BUILD_DIR}/build/node_modules/fiori3 && npm publish --access public --tag next
 cd ${TRAVIS_BUILD_DIR}/build/node_modules/styles && npm publish --access public --tag next
 cd ${TRAVIS_BUILD_DIR}/build/node_modules/utils && npm publish --access public --tag next
 
-curl --request POST \
-  --url https://api.github.com/repos/SAP/fiori-for-react/pulls \
-  --header 'authorization: Bearer ${GH_TOKEN}' \
-  --header 'content-type: application/json' \
-  --data '{
-	"title": "Release v${PACKAGE_VERSION}",
-	"head": "release/${PACKAGE_VERSION}",
-	"base": "master"
-}'
+#curl --request POST \
+#  --url https://api.github.com/repos/SAP/fiori-for-react/pulls \
+#  --header 'authorization: Bearer ${GH_TOKEN}' \
+#  --header 'content-type: application/json' \
+#  --data '{
+#	"title": "Release v${PACKAGE_VERSION}",
+#	"head": "release/${PACKAGE_VERSION}",
+#	"base": "master"
+#}'
