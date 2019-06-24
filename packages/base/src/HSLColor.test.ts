44223b89d263dc943437f16447851ac6cca69bf0
MarcusNotheis <marcus.notheis@sap.com>
2019-06-24 10:10:57 +0200

    refactor(base): Deleted obsolete Themes
    BREAKING CHANGE Deleted Themes `sap_belize`, `sap_belize_plus` and `sap_belize_hcb`
    BREAKING CHANGE Removed `createThemeMap` from base
    BREAKING CHANGE Removed `getThemeClassFor` from base
    BREAKING CHANGE Removed `getThemeOverwrite` from base
    BREAKING CHANGE Deleted `Themes` from base

--- packages/base/src/HSLColor.test.ts
+++ packages/base/src/HSLColor.test.ts
@@ -1,30 +1,19 @@
 import { HSLColor } from './HSLColor';
-import { sapUiBaseText, sapAccentColor7, sapColorSemanticNegative, sapUiHighlight } from './sap_belize';
+import { sapColorSemanticNegative, sapUiBrand, sapUiHighlight } from './sap_fiori_3';
 
 describe('HSL Color', () => {
   test('lighten', () => {
-    const color = HSLColor.of(sapUiBaseText).lighten(20).hsl;
-    expect(color).toEqual('hsl(0, 0%, 40%)');
+    const color = HSLColor.of(sapUiHighlight).lighten(63).hsl;
+    expect(color).toEqual('hsl(209.8, 90.9%, 95%)'); //hsl(208.4, 90.5%, 95.9%)
   });
 
   test('darken', () => {
-    const color = HSLColor.of(sapAccentColor7).darken(8).hsl;
-    expect(color).toEqual('hsl(180, 70.8%, 26.9%)');
-  });
-
-  test('border colors', () => {
-    const color = HSLColor.of(sapColorSemanticNegative).lighten(10).hsl;
-    expect(color).toEqual('hsl(0, 100%, 46.7%)');
+    const color = HSLColor.of(sapUiBrand).darken(10).hsl;
+    expect(color).toEqual('hsl(209.8, 90.9%, 32.9%)'); //hsl(210, 90.5%, 32.9%)
   });
 
   test('border bg colors', () => {
-    const color = HSLColor.of(sapColorSemanticNegative).lighten(58).hsl;
-    expect(color).toEqual('hsl(0, 100%, 94.7%)');
-  });
-
-  test('Combined Saturate and Lighten', () => {
-    const saturated = HSLColor.of(sapUiHighlight).saturate(10).hsl;
-    const color = HSLColor.of(saturated).lighten(10).hsl;
-    expect(color).toEqual('hsl(207.2, 54.5%, 56%)'); // hsl(207.3, 54.8%, 56.7%)
+    const color = HSLColor.of(sapColorSemanticNegative).lighten(59.5).hsl;
+    expect(color).toEqual('hsl(0, 100%, 95.5%)'); //hsl(0, 100%, 96.1%)
   });
 });
--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -1,43 +1,29 @@
 /**
  * Created by d059190 at 16.03.18
  */
+import font72 from './72';
+import { bootstrap } from './bootstrap';
+import * as fonts from './fonts';
+import { HSLColor } from './HSLColor';
+import * as sap_fiori_3 from './sap_fiori_3';
+import * as spacing from './spacing';
+import { withStyles } from './withStyles';
+import { Device } from './Device';
 import { Event } from './Event';
+import { isNumeric } from './isNumeric';
 import KeyCodes from './KeyCodes';
 import { LOG_LEVEL, Logger } from './Logger';
 import Optional from './Optional';
 import StyleClassHelper from './StyleClassHelper';
-import Themes from './Themes';
-import { isNumeric } from './isNumeric';
-import { Device } from './Device';
-import * as fonts from '../../base/src/fonts';
-import font72 from '../../base/src/72';
-import * as sap_belize from '../../base/src/sap_belize';
-import * as sap_belize_plus from '../../base/src/sap_belize_plus';
-import * as sap_belize_hcb from '../../base/src/sap_belize_hcb';
-import * as sap_fiori_3 from '../../base/src/sap_fiori_3';
-import * as spacing from '../../base/src/spacing';
-import { HSLColor } from '../../base/src/HSLColor';
-import { bootstrap } from '../../base/src/bootstrap';
-import { withStyles } from '../../base/src/withStyles';
 
-import {
-  createThemeMap,
-  deprecationNotice,
-  getThemeClassFor,
-  getThemeOverwrite,
-  pushElementBackInScreen
-} from './Util';
+import { deprecationNotice, pushElementBackInScreen } from './Util';
 
 export {
   StyleClassHelper,
   KeyCodes,
   Optional,
-  getThemeClassFor,
-  createThemeMap,
-  getThemeOverwrite,
   deprecationNotice,
   pushElementBackInScreen,
-  Themes,
   Event,
   Logger,
   LOG_LEVEL,
@@ -45,9 +31,6 @@ export {
   Device,
   fonts,
   font72,
-  sap_belize,
-  sap_belize_plus,
-  sap_belize_hcb,
   spacing,
   HSLColor,
   sap_fiori_3,
