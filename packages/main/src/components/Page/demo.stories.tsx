8a3cb32017f0fa6a89335cae6f1cd54a1aea0365
MarcusNotheis <marcus.notheis@sap.com>
2019-07-23 16:11:19 +0200

    refactor(Page): Replace withStyles HOC by forwardRef implementation

--- packages/main/src/components/Page/demo.stories.tsx
+++ packages/main/src/components/Page/demo.stories.tsx
@@ -1,5 +1,6 @@
-import { boolean, select } from '@storybook/addon-knobs';
+import { boolean, select, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import React from 'react';
 import { Bar } from '../../lib/Bar';
 import { Button } from '../../lib/Button';
@@ -10,12 +11,13 @@ import { PageBackgroundDesign } from '../../lib/PageBackgroundDesign';
 const renderPage = () => (
   <div style={{ height: '400px', width: '100%' }}>
     <Page
-      title="Page Demo"
+      title={text('title', 'Page Demo')}
       showFooter={boolean('showFooter', true)}
       showHeader={boolean('showHeader', true)}
       showBackButton={boolean('showBackButton', true)}
       backgroundDesign={select('backgroundDesign', PageBackgroundDesign, PageBackgroundDesign.Standard)}
       renderCustomFooter={() => <Bar renderContentRight={() => <Button>Button</Button>} />}
+      onNavButtonPress={action('onNavButtonPress')}
     >
       <Label>Page Content</Label>
     </Page>
