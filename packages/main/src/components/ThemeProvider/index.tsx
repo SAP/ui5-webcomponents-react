7eccab9a6786f1bdda80d56a9958e8de1cf57c2b
MarcusNotheis <marcus.notheis@sap.com>
2019-08-12 12:09:03 +0200

    refactor(ThemeProvider): Replace Class Component with functional component

--- packages/main/src/components/ThemeProvider/index.tsx
+++ packages/main/src/components/ThemeProvider/index.tsx
@@ -3,10 +3,9 @@ import { getCompactSize, getTheme } from '@ui5/webcomponents-base/src/Configurat
 import { injectThemeProperties } from '@ui5/webcomponents-base/src/theming/StyleInjection';
 import { createGenerateClassName, sap_fiori_3 } from '@ui5/webcomponents-react-base';
 import fiori3ThemeProperties from '@ui5/webcomponents/dist/themes/sap_fiori_3/parameters-bundle.css.json';
-import React, { Fragment, PureComponent, ReactNode } from 'react';
+import React, { FC, Fragment, ReactNode, useEffect, useMemo, lazy, Suspense } from 'react';
 import { JssProvider, ThemeProvider as ReactJssThemeProvider } from 'react-jss';
 import { ContentDensity } from '../../lib/ContentDensity';
-import { MessageToast } from '../../lib/MessageToast';
 import { Themes } from '../../lib/Themes';
 
 export interface ThemeProviderProps {
@@ -14,49 +13,62 @@ export interface ThemeProviderProps {
   children: ReactNode;
 }
 
+const EmptyComponent = () => null;
+
 const generateClassName = createGenerateClassName();
 
-export class ThemeProvider extends PureComponent<ThemeProviderProps> {
-  static defaultProps = {
-    withToastContainer: false
-  };
+const MessageToast = lazy(() => import('../../lib/MessageToast'));
 
-  constructor(props) {
-    super(props);
+const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+  useEffect(() => {
     boot().then((_) => {
       let existingThemingProperties = document.querySelector('head style[data-ui5-webcomponents-theme-properties]');
       if (!existingThemingProperties || !existingThemingProperties.innerHTML) {
         injectThemeProperties(fiori3ThemeProperties._);
       }
     });
-  }
+  }, []);
+  const { withToastContainer, children } = props;
 
-  private static getTheme = (theme: Themes) => {
+  const theme = getTheme();
+  const isCompactSize = getCompactSize();
+
+  const contentDensity = useMemo(() => {
+    return isCompactSize ? ContentDensity.Compact : ContentDensity.Cozy;
+  }, [isCompactSize]);
+
+  const parameters = useMemo(() => {
     if (theme === Themes.sap_fiori_3) return sap_fiori_3;
     return null;
-  };
+  }, [theme]);
 
-  private static getContentDensity = (compactSize: boolean) => {
-    return compactSize ? ContentDensity.Compact : ContentDensity.Cozy;
-  };
+  const themeContext = useMemo(() => {
+    return {
+      theme,
+      contentDensity,
+      parameters
+    };
+  }, [theme, contentDensity, parameters]);
 
-  render() {
-    const { withToastContainer } = this.props;
-    return (
-      <JssProvider generateId={generateClassName}>
-        <ReactJssThemeProvider
-          theme={{
-            theme: getTheme(),
-            contentDensity: ThemeProvider.getContentDensity(getCompactSize()),
-            parameters: ThemeProvider.getTheme(getTheme())
-          }}
-        >
-          <Fragment>
-            {this.props.children}
-            {withToastContainer && <MessageToast />}
-          </Fragment>
-        </ReactJssThemeProvider>
-      </JssProvider>
-    );
-  }
-}
+  return (
+    <JssProvider generateId={generateClassName}>
+      <ReactJssThemeProvider theme={themeContext}>
+        <Fragment>
+          {children}
+          {withToastContainer && (
+            <Suspense fallback={<EmptyComponent />}>
+              <MessageToast />
+            </Suspense>
+          )}
+        </Fragment>
+      </ReactJssThemeProvider>
+    </JssProvider>
+  );
+};
+
+ThemeProvider.displayName = 'ThemeProvider';
+ThemeProvider.defaultProps = {
+  withToastContainer: false
+};
+
+export { ThemeProvider };
--- packages/main/src/lib/MessageToast.ts
+++ packages/main/src/lib/MessageToast.ts
@@ -1,3 +1,6 @@
 import { MessageToast } from '../components/MessageToast';
 
 export { MessageToast };
+
+// Required for React lazy
+export default MessageToast;
