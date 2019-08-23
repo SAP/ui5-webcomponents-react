8b6327efe8893ec8b747a44ded6958de8009de05
MarcusNotheis <marcus.notheis@sap.com>
2019-08-23 09:30:48 +0200

    WIP: Updated Carousel Test

--- packages/main/src/components/Carousel/Carousel.test.tsx
+++ packages/main/src/components/Carousel/Carousel.test.tsx
@@ -1,14 +1,10 @@
 import { getEventFromCallback, mountThemedComponent } from '@shared/tests/utils';
-import { expect, use } from 'chai';
-import { matchSnapshot } from 'chai-karma-snapshot';
 import React, { cloneElement } from 'react';
 import * as sinon from 'sinon';
 import { Carousel } from '../../lib/Carousel';
 import { CarouselArrowsPlacement } from '../../lib/CarouselArrowsPlacement';
 import { Icon } from '../../lib/Icon';
 import { PlacementType } from '../../lib/PlacementType';
-
-use(matchSnapshot);
 
 const renderCarousel = (props) => {
   return (
@@ -25,12 +21,12 @@ const renderCarousel = (props) => {
 };
 
 describe('Carousel', () => {
-  it('Renders without crashing', () => {
+  test('Renders without crashing', () => {
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 0 }));
-    expect(wrapper.debug()).to.matchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
-  it('Should render a text indicator', () => {
+  test('Should render a text indicator', () => {
     const wrapper = mountThemedComponent(
       <Carousel>
         <p>Carousel 1</p>
@@ -45,129 +41,129 @@ describe('Carousel', () => {
         <p>Carousel 10</p>
       </Carousel>
     );
-    expect(wrapper.debug()).to.matchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
-  it('CarouselArrowsPlacement: Content', () => {
+  test('CarouselArrowsPlacement: Content', () => {
     const wrapper = mountThemedComponent(renderCarousel({ arrowsPlacement: CarouselArrowsPlacement.Content }));
-    expect(wrapper.debug()).to.matchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
-  it('CarouselArrowsPlacement: PageIndicator', () => {
+  test('CarouselArrowsPlacement: PageIndicator', () => {
     const wrapper = mountThemedComponent(renderCarousel({ arrowsPlacement: CarouselArrowsPlacement.PageIndicator }));
-    expect(wrapper.debug()).to.matchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
-  it('Page Indicator Placement: Top', () => {
+  test('Page Indicator Placement: Top', () => {
     const wrapper = mountThemedComponent(
       renderCarousel({
         arrowsPlacement: CarouselArrowsPlacement.PageIndicator,
         pageIndicatorPlacement: PlacementType.Top
       })
     );
-    expect(wrapper.debug()).to.matchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
     wrapper.setProps({
       children: cloneElement(wrapper.props().children, { activePage: 1 })
     });
     wrapper.update();
-    expect(wrapper.debug()).to.matchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
-  it('Update activePage via prop', () => {
+  test('Update activePage via prop', () => {
     const wrapper = mountThemedComponent(
       renderCarousel({
         activePage: 0
       })
     );
-    expect(wrapper.debug()).to.matchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 
-  it('Navigation to next page', () => {
+  test('Navigation to next page', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 0, onPageChanged: callback }));
     wrapper
       .find(Icon)
       .last()
       .simulate('click');
-    expect(getEventFromCallback(callback).getParameter('selectedIndex')).to.equal(1);
+    expect(getEventFromCallback(callback).getParameter('selectedIndex')).toEqual(1);
   });
 
-  it('Navigation to previous page', () => {
+  test('Navigation to previous page', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 1, onPageChanged: callback }));
     wrapper
       .find(Icon)
       .first()
       .simulate('click');
-    expect(getEventFromCallback(callback).getParameter('selectedIndex')).to.equal(0);
+    expect(getEventFromCallback(callback).getParameter('selectedIndex')).toEqual(0);
   });
 
-  it('Navigation to previous page - w/o Loop', () => {
+  test('Navigation to previous page - w/o Loop', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 0, onPageChanged: callback }));
     wrapper
       .find(Icon)
       .first()
       .simulate('click');
-    expect(callback.called).to.equal(false);
+    expect(callback.called).toEqual(false);
   });
 
-  it('Navigation to previous page - w/ Loop', () => {
+  test('Navigation to previous page - w/ Loop', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 0, loop: true, onPageChanged: callback }));
     wrapper
       .find(Icon)
       .first()
       .simulate('click');
-    expect(getEventFromCallback(callback).getParameter('selectedIndex')).to.equal(6);
+    expect(getEventFromCallback(callback).getParameter('selectedIndex')).toEqual(6);
   });
 
-  it('Navigation to next page - w/o Loop', () => {
+  test('Navigation to next page - w/o Loop', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 6, onPageChanged: callback }));
     wrapper
       .find(Icon)
       .last()
       .simulate('click');
-    expect(callback.called).to.equal(false);
+    expect(callback.called).toEqual(false);
   });
 
-  it('Navigation to next page - w/ Loop', () => {
+  test('Navigation to next page - w/ Loop', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 6, loop: true, onPageChanged: callback }));
     wrapper
       .find(Icon)
       .last()
       .simulate('click');
-    expect(getEventFromCallback(callback).getParameter('selectedIndex')).to.equal(0);
+    expect(getEventFromCallback(callback).getParameter('selectedIndex')).toEqual(0);
   });
 
-  it('Carousel with 1 child', () => {
+  test('Carousel with 1 child', () => {
     const wrapper = mountThemedComponent(
       <Carousel arrowsPlacement={CarouselArrowsPlacement.Content}>
         <p>Carousel 1</p>
       </Carousel>
     );
-    expect(wrapper.render().find('[data-value="paginationArrow"]')).to.have.length(0);
+    expect(wrapper.render().find('[data-value="paginationArrow"]')).toHaveLength(0);
   });
 
-  it('Navigation to next page with Keyboard', () => {
+  test('Navigation to next page with Keyboard', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 0, onPageChanged: callback }));
     wrapper
       .find('div[role="list"]')
       .last()
       .simulate('keydown', { key: 'ArrowRight' });
-    expect(getEventFromCallback(callback).getParameter('selectedIndex')).to.equal(1);
+    expect(getEventFromCallback(callback).getParameter('selectedIndex')).toEqual(1);
   });
 
-  it('Navigation to previous page with Keyboard', () => {
+  test('Navigation to previous page with Keyboard', () => {
     const callback = sinon.spy();
     const wrapper = mountThemedComponent(renderCarousel({ activePage: 1, onPageChanged: callback }));
     wrapper
       .find('div[role="list"]')
       .first()
       .simulate('keydown', { key: 'ArrowLeft' });
-    expect(getEventFromCallback(callback).getParameter('selectedIndex')).to.equal(0);
+    expect(getEventFromCallback(callback).getParameter('selectedIndex')).toEqual(0);
   });
 });
