68520164f89b89ed66c4e26ce77322e90e053377
MarcusNotheis <marcus.notheis@sap.com>
2019-08-23 09:46:05 +0200

    WIP: WithWebComponent

--- packages/main/src/internal/WithWebComponent.test.tsx
+++ packages/main/src/internal/WithWebComponent.test.tsx
@@ -1,12 +1,11 @@
 import { mountThemedComponent } from '@shared/tests/utils';
 import UI5Button from '@ui5/webcomponents/dist/Button';
-import { expect } from 'chai';
 import React, { cloneElement, FC } from 'react';
 import { spy } from 'sinon';
 import { withWebComponent } from './withWebComponent';
 
 describe('withWebComponent', () => {
-  it('Unmount Event Handlers correctly after prop update', () => {
+  test('Unmount Event Handlers correctly after prop update', () => {
     let Button: FC<any> = withWebComponent(UI5Button);
     const callback = spy();
     const wrapper = mountThemedComponent(<Button onClick={(...args) => callback(...args)} />);
@@ -16,13 +15,13 @@ describe('withWebComponent', () => {
       .instance();
     // @ts-ignore
     component.fireEvent('click');
-    expect(callback.callCount).to.equal(1, 'onPress handler has not been called');
+    expect(callback.callCount).toEqual(1);
     wrapper.setProps({
       children: cloneElement(wrapper.prop('children'), { onClick: (...args) => callback(...args) })
     });
     wrapper.update();
     // @ts-ignore
     component.fireEvent('click');
-    expect(callback.callCount).to.equal(2, 'onPress handler has not been called after update');
+    expect(callback.callCount).toEqual(2);
   });
 });
