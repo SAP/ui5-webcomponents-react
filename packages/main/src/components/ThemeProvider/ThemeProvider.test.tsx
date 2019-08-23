e6b5de6fd9d3081ccf64907b4691853bfc4fac4f
MarcusNotheis <marcus.notheis@sap.com>
2019-08-23 09:44:11 +0200

    WIP: ThemeProvider

--- packages/main/src/components/ThemeProvider/ThemeProvider.test.tsx
+++ packages/main/src/components/ThemeProvider/ThemeProvider.test.tsx
@@ -1,5 +1,4 @@
 import { withStyles } from '@ui5/webcomponents-react-base';
-import { expect } from 'chai';
 import { mount } from 'enzyme';
 import React from 'react';
 import { ThemeProvider } from '../../lib/ThemeProvider';
@@ -7,8 +6,8 @@ import { ThemeProvider } from '../../lib/ThemeProvider';
 describe('ThemeProvider', () => {
   it('Provides Correct Context', (done) => {
     const InnerComponent = withStyles(() => {})(({ theme }) => {
-      expect(theme.theme).to.equal('sap_fiori_3');
-      expect(theme.contentDensity).to.equal('Cozy');
+      expect(theme.theme).toEqual('sap_fiori_3');
+      expect(theme.contentDensity).toEqual('Cozy');
       done();
       return null;
     });
