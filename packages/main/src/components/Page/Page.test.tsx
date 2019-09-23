587e1a11830513db3d7106910e2cad721aa402b6
MarcusNotheis <marcus.notheis@sap.com>
2019-09-23 15:57:41 +0200

    refactor(Bar): Functional Component with forwardRef

--- packages/main/src/components/Page/Page.test.tsx
+++ packages/main/src/components/Page/Page.test.tsx
@@ -1,26 +1,28 @@
-import { renderThemedComponent, ThemedComponent } from '@shared/tests/utils';
-import { shallow } from 'enzyme';
-import React from 'react';
+import { renderThemedComponent } from '@shared/tests/utils';
 import { Page } from '@ui5/webcomponents-react/lib/Page';
-
-const renderPage = (props = {}) =>
-  ThemedComponent(
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <Page title="Page Demo" showFooter showHeader {...props}>
-        Page Content
-      </Page>
-    </div>
-  );
+import React from 'react';
 
 describe('Page', () => {
   test('Basic Page', () => {
-    const wrapper = shallow(renderPage()).render();
-    expect(wrapper.html()).toMatchSnapshot();
+    const wrapper = renderThemedComponent(
+      <div style={{ height: '100vh', width: '100vw' }}>
+        <Page title="Page Demo" showFooter showHeader>
+          Page Content
+        </Page>
+      </div>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 
   test('Basic Page w/o back button', () => {
-    const wrapper = shallow(renderPage({ showBackButton: false })).render();
-    expect(wrapper.html()).toMatchSnapshot();
+    const wrapper = renderThemedComponent(
+      <div style={{ height: '100vh', width: '100vw' }}>
+        <Page title="Page Demo" showFooter showHeader showBackButton={false}>
+          Page Content
+        </Page>
+      </div>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 
   test('Without footer and Header', () => {
@@ -29,6 +31,6 @@ describe('Page', () => {
         Page Content
       </Page>
     );
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
