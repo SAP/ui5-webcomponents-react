738596fdcdc6a557f561df5c7414713b7dafebda
Lukas <lukas742@gmx.net>
2019-10-07 13:52:34 +0200

    feat(Notification): added defaultProps, updated Snapshots

--- packages/main/src/components/NotificationGroup/index.tsx
+++ packages/main/src/components/NotificationGroup/index.tsx
@@ -1,10 +1,21 @@
-import React, { forwardRef, RefObject } from 'react';
+import React, { FC, forwardRef, RefObject } from 'react';
 
 import { NotificationProptypes } from '../Notification';
 import { Notification } from '@ui5/webcomponents-react/lib/Notification';
 
-const NotificationGroup = forwardRef((props: NotificationProptypes, ref: RefObject<HTMLDivElement>) => {
-  return <Notification {...props} />;
-});
+const NotificationGroup: FC<NotificationProptypes> = forwardRef(
+  (props: NotificationProptypes, ref: RefObject<HTMLDivElement>) => {
+    return <Notification {...props} />;
+  }
+);
+
+NotificationGroup.defaultProps = {
+  visible: true,
+  autoPriority: true,
+  truncate: true,
+  showCloseButton: true
+};
+
+NotificationGroup.displayName = 'Notification Group';
 
 export { NotificationGroup };
