4a1c25a7117ac0eaddcf6f764e4b657add0f2fed
vbersch <viktor.bersch@sap.com>
2019-10-15 11:58:49 +0200

    feat(AnalyticalTable): offer scrollTo and scrollToItem methods on table ref (#185)
    * fix(AnalyticalTable): add key to Cell component
    
    * feat(AnalyticalTable): expose scrollTo and scrollToItem methods on analytical table ref

--- packages/main/src/components/AnalyticalTable/index.tsx
+++ packages/main/src/components/AnalyticalTable/index.tsx
@@ -35,6 +35,7 @@ import { makeTemplateColumns } from './hooks/utils';
 import { LoadingComponent } from './LoadingComponent';
 import { TitleBar } from './TitleBar';
 import { VirtualTableBody } from './virtualization/VirtualTableBody';
+import { useTableScrollHandles } from './hooks/useTableScrollHandles';
 
 export interface ColumnConfiguration {
   accessor?: string;
@@ -143,6 +144,8 @@ const AnalyticalTable: FC<TableProps> = forwardRef((props: TableProps, ref: Ref<
   const [selectedRowPath, onRowClicked] = useRowSelection(onRowSelected);
   const [resizedColumns, onColumnSizeChanged] = useResizeColumns();
 
+  const [analyticalTableRef, reactWindowRef] = useTableScrollHandles(ref);
+
   const internalGroupBy = useRef(groupBy);
 
   let tableState = useMemo(() => {
@@ -232,9 +235,8 @@ const AnalyticalTable: FC<TableProps> = forwardRef((props: TableProps, ref: Ref<
     [internalGroupBy.current, onGroup]
   );
 
-  // Render the UI for your table
   return (
-    <div className={className} style={style} title={tooltip} ref={ref}>
+    <div className={className} style={style} title={tooltip} ref={analyticalTableRef}>
       {title && <TitleBar>{title}</TitleBar>}
       {typeof renderExtension === 'function' && <div>{renderExtension()}</div>}
       <div className={tableContainerClasses.valueOf()}>
@@ -273,6 +275,7 @@ const AnalyticalTable: FC<TableProps> = forwardRef((props: TableProps, ref: Ref<
             columns={columns}
             selectedRow={selectedRowPath}
             rowHeight={rowHeight}
+            reactWindowRef={reactWindowRef}
           />
           {loading && busyIndicatorEnabled && <LoadingComponent />}
         </div>
