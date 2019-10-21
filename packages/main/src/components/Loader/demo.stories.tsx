de2310d857ebc95f8529f3bfc1abb33ba1be7618
Marcus Notheis <MarcusNotheis@users.noreply.github.com>
2019-10-21 14:26:19 +0200

    feat(Loader/Spinner): Add `delay` prop (#197)
    Only show the component after the `delay` is over
    
    Closes #67

--- packages/main/src/components/Loader/demo.stories.tsx
+++ packages/main/src/components/Loader/demo.stories.tsx
@@ -1,10 +1,14 @@
-import { select, text } from '@storybook/addon-knobs';
-import React from 'react';
+import { number, select, text } from '@storybook/addon-knobs';
 import { Loader } from '@ui5/webcomponents-react/lib/Loader';
 import { LoaderType } from '@ui5/webcomponents-react/lib/LoaderType';
+import React from 'react';
 
 export const renderLoader = () => (
-  <Loader type={select('type', LoaderType, LoaderType.Indeterminate)} progress={text('progress', '40%')} />
+  <Loader
+    type={select('type', LoaderType, LoaderType.Indeterminate)}
+    progress={text('progress', '40%')}
+    delay={number('delay', 0)}
+  />
 );
 renderLoader.story = {
   name: 'Default'
--- packages/main/src/components/Spinner/demo.stories.tsx
+++ packages/main/src/components/Spinner/demo.stories.tsx
@@ -1,9 +1,22 @@
-import { select } from '@storybook/addon-knobs';
-import React from 'react';
+import { number, select } from '@storybook/addon-knobs';
 import { Size } from '@ui5/webcomponents-react/lib/Size';
 import { Spinner } from '@ui5/webcomponents-react/lib/Spinner';
+import React, { useMemo } from 'react';
 
-export const renderSpinner = () => <Spinner size={select('size', Size, Size.Medium)} />;
+let spinnerKey = 0;
+
+const Demo = (props) => {
+  const { delay } = props;
+
+  const spinner = useMemo(() => {
+    spinnerKey++;
+    return <Spinner key={`${spinnerKey}`} size={select('size', Size, Size.Medium)} delay={delay} />;
+  }, [delay]);
+
+  return spinner;
+};
+
+export const renderSpinner = () => <Demo delay={number('delay', 2000)} />;
 renderSpinner.story = {
   name: 'Default'
 };
