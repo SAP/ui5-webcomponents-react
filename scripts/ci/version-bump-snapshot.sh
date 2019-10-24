91f2bd071168e69ee50086d9619ccc1972d62a64
MarcusNotheis <marcus.notheis@sap.com>
2019-10-24 10:00:36 +0200

    fix(ci): Use lerna.json instead of package.json

--- scripts/ci/version-bump-snapshot.sh
+++ scripts/ci/version-bump-snapshot.sh
@@ -11,6 +11,7 @@ git config --global user.name "ui5-webcomponents-react-bot"
 export FIORI_FOR_REACT_SNAPSHOT_BUILD='true'
 
 # trigger lerna release
-PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+PACKAGE_VERSION=$(node -p -e "require('./lerna.json').version")
 ${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna version "${PACKAGE_VERSION}-${TRAVIS_COMMIT}" \
+        --no-git-tag-version \
         --no-push
