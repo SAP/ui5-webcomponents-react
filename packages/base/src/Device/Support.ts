e151cbadad6a97b3de684526b43c889acccd83e9
MarcusNotheis <marcus.notheis@sap.com>
2019-11-26 08:36:22 +0100

    refactor(Device): Reuse UI5 Device and remove custom code

--- packages/base/src/Device/Support.ts
+++ packages/base/src/Device/Support.ts
@@ -1,32 +1,23 @@
-import './lib/modernizr';
+import { supportTouch } from '@ui5/webcomponents-core/dist/sap/ui/Device';
+import '@ui5/webcomponents-react-base/third-party/modernizr';
+
+declare const Modernizr: {
+  pointerevents: boolean;
+  matchmedia: boolean;
+  deviceorientation: boolean;
+  websockets: boolean;
+  placeholder: boolean;
+};
 
 export class Support {
-  public touch: boolean;
-  public pointer: boolean;
-  public matchmedia: boolean;
-  public matchmedialistener: boolean;
-  public orientation: boolean;
-  public retina: boolean;
-  public websocket: boolean;
+  public touch: boolean = supportTouch();
+  public pointer: boolean = Modernizr.pointerevents;
+  public matchmedia: boolean = Modernizr.matchmedia;
+  public matchmedialistener: boolean = !!(this.matchmedia && window.matchMedia('all and (max-width:0px)'));
+  public orientation: boolean = Modernizr.deviceorientation;
+  public retina: boolean = (window as any).retina || window.devicePixelRatio >= 2;
+  public websocket: boolean = Modernizr.websockets;
   public input = {
-    placeholder: false
+    placeholder: Modernizr.placeholder
   };
-
-  constructor(browser) {
-    this.touch = Modernizr.touchevents;
-    this.pointer = Modernizr.pointerevents;
-    this.matchmedia = Modernizr.matchmedia;
-    let matchmedialistener = !!(this.matchmedia && window.matchMedia('all and (max-width:0px)'));
-    if (browser.safari && browser.version < 6 && !browser.fullscreen && !browser.webview) {
-      // Safari seems to have addListener but no events are fired ?!
-      matchmedialistener = false;
-    }
-    this.matchmedialistener = matchmedialistener;
-    this.orientation = Modernizr.deviceorientation;
-    this.retina = (window as any).retina || window.devicePixelRatio >= 2;
-    this.websocket = Modernizr.websockets;
-    this.input.placeholder = Modernizr.placeholder;
-
-    return this;
-  }
 }
