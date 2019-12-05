17daa550ed387f35ce760e3cdb2cc4ffe79f3e2b
MarcusNotheis <marcus.notheis@sap.com>
2019-12-05 15:50:45 +0100

    Update AnalyticalTable.test.tsx

--- packages/main/src/components/AnalyticalTable/AnalyticalTable.test.tsx
+++ packages/main/src/components/AnalyticalTable/AnalyticalTable.test.tsx
@@ -229,7 +229,7 @@ describe('AnalyticalTable', () => {
     let componentDrag = wrapper.find('div[role="columnheader"] div[draggable]').at(0);
     let inst = componentDrag.instance();
     // @ts-ignore
-    let dragColumnId = inst.id;
+    let dragColumnId = inst.dataset.columnId;
 
     // @ts-ignore
     expect(inst.draggable).toBeDefined();
@@ -244,7 +244,7 @@ describe('AnalyticalTable', () => {
     dataTransfer.getData = () => {
       return dragColumnId;
     };
-    let componentDrop = wrapper.find({ role: 'columnheader' }).at(1);
+    let componentDrop = wrapper.find('div[role="columnheader"] div[draggable]').at(1);
     // @ts-ignore
     componentDrop.simulate('drop', { dataTransfer: dataTransfer });
 
