64353f0d61dc51ec08d00312e410de159c9fab9a
MarcusNotheis <marcus.notheis@sap.com>
2019-12-13 14:43:08 +0100

    WIP: Test github action paths

--- scripts/ci/release-snapshot.sh
+++ scripts/ci/release-snapshot.sh
@@ -1,15 +1,11 @@
 #! /bin/bash
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
-then
-    echo "This is a pull request build - skip npm release"
-    exit 0
-fi
+ls -l ${GITHUB_WORKSPACE}
 
-# make sure we are on the master branch
-git checkout master
 
-${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna publish prerelease \
+exit 1;
+
+${GITHUB_WORKSPACE}/node_modules/.bin/lerna publish prerelease \
   --canary \
   --conventional-prerelease \
   --dist-tag dev \
