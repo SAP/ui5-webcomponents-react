b91081e1f61cd0a3f62952db4106c6b23d06645d
Marcus Notheis <MarcusNotheis@users.noreply.github.com>
2019-12-13 15:02:04 +0100

    ci: Build npm snapshot with github actions (#251)

--- scripts/ci/release-snapshot.sh
+++ scripts/ci/release-snapshot.sh
@@ -1,15 +1,6 @@
 #! /bin/bash
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]
-then
-    echo "This is a pull request build - skip npm release"
-    exit 0
-fi
-
-# make sure we are on the master branch
-git checkout master
-
-${TRAVIS_BUILD_DIR}/node_modules/.bin/lerna publish prerelease \
+./node_modules/.bin/lerna publish prerelease \
   --canary \
   --conventional-prerelease \
   --dist-tag dev \
