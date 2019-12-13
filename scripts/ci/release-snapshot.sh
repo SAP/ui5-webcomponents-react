fab5ed13c2c03912bf068b35d23d9538c9833867
MarcusNotheis <marcus.notheis@sap.com>
2019-12-13 14:49:29 +0100

    WIP: Checkout package

--- scripts/ci/release-snapshot.sh
+++ scripts/ci/release-snapshot.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 
-ls -l ${GITHUB_WORKSPACE}
-
-
-exit 1;
-
-${GITHUB_WORKSPACE}/node_modules/.bin/lerna publish prerelease \
+./node_modules/.bin/lerna publish prerelease \
   --canary \
   --conventional-prerelease \
   --dist-tag dev \
