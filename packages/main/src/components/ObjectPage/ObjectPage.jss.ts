bdf2c955266e67c8d6225b4bd2419cab98892533
vbersch <viktor.bersch@sap.com>
2020-01-22 10:17:46 +0100

    fix(ObjectPage): fix empty section content when dynamically adding sections (#282)

--- packages/main/src/components/ObjectPage/ObjectPage.jss.ts
+++ packages/main/src/components/ObjectPage/ObjectPage.jss.ts
@@ -21,6 +21,7 @@ const styles = ({ parameters }: JSSTheme) => ({
   },
   outerContentContainer: {
     width: '100%',
+    height: '100%',
     overflow: 'hidden'
   },
   contentScrollContainer: {
