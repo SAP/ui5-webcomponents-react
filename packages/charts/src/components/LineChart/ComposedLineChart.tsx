ebd650589aa627a17d056f8cd01c1652013ff91e
klju1017 <julian.klein@sap.com>
2020-02-10 16:13:17 +0100

    Update ComposedLineChart.tsx

--- packages/charts/src/components/LineChart/ComposedLineChart.tsx
+++ packages/charts/src/components/LineChart/ComposedLineChart.tsx
@@ -5,70 +5,60 @@ import { useTheme } from 'react-jss';
 import { JSSTheme } from '@ui5/webcomponents-react/src/interfaces/JSSTheme';
 import { LineChartPlaceholder } from './Placeholder';
 import { ChartBaseDefaultProps } from '../../util/ChartBaseDefaultProps';
-import { Line } from '../../elements/Line';
+import { Line as LineRe } from 'recharts';
 import { ComposedLineProps } from '../../interfaces/ComposedLineProps';
+import { LineRechart } from './RechartLine';
+import { ComposedChartContainer } from '../../internal/ComposedChartContainer';
 
 export interface ComposedLineChartProps extends ComposedLineProps {}
 
-const CustomDataLabel = (props) => {
-  const { x, y, stroke, value, formatter } = props;
-  const { parameters } = useTheme() as JSSTheme;
-  return (
-    <text
-      x={x}
-      y={y}
-      dy={-8}
-      textAnchor="middle"
-      style={{
-        fontFamily: parameters.sapUiFontFamily,
-        fill: parameters.sapUiContentLabelColor,
-        fontSize: parameters.sapMFontSmallSize
-      }}
-    >
-      {value}
-    </text>
-  );
-};
+// const CustomDataLabel = (props) => {
+//   const { x, y, stroke, value, formatter } = props;
+//   const { parameters } = useTheme() as JSSTheme;
+//   return (
+//     <text
+//       x={x}
+//       y={y}
+//       dy={-8}
+//       textAnchor="middle"
+//       style={{
+//         fontFamily: parameters.sapUiFontFamily,
+//         fill: parameters.sapUiContentLabelColor,
+//         fontSize: parameters.sapMFontSmallSize
+//       }}
+//     >
+//       {value}
+//     </text>
+//   );
+// };
 
 const ComposedLineChart = forwardRef((props: ComposedLineChartProps, ref: Ref<any>) => {
-  const { dataKey, color, colors, type, onDataClickHandler } = props as ComposedLineChartProps;
+  const { dataKey, color, colors, type, stroke, strokeWidth } = props as ComposedLineChartProps;
 
   useInitialize();
 
   const chartRef = useConsolidatedRef<any>(ref);
 
-  const onDataPointClick = useCallback(
-    (e) => {
-      console.log(e);
-      if (e) {
-        console.log(e);
-        onDataClickHandler({
-          e,
-          index: e.activeTooltipIndex,
-          label: e.activeLabel,
-          values: e.activePayload
-        });
-      }
-      console.log(e);
-    },
-    [dataKey]
-  );
+  // const onDataPointClick = useCallback(
+  //   (e) => {
+  //     if (e) {
+  //       onDataClickHandler({
+  //         e,
+  //         index: e.activeTooltipIndex,
+  //         label: e.activeLabel,
+  //         values: e.activePayload
+  //       });
+  //     }
+  //   },
+  //   [dataKey]
+  // );
+  //
+  // let currentDataKey;
+  // const setDataKey = (e) => {
+  //   currentDataKey = e.dataKey;
+  // };
 
-  let currentDataKey;
-  const setDataKey = (e) => {
-    currentDataKey = e.dataKey;
-    console.log(currentDataKey);
-  };
-
-  return (
-    <Line
-      onClick={onDataPointClick}
-      type={type ? type : 'monotone'}
-      dataKey={dataKey}
-      stroke={color ? color : colors[0]}
-      activeDot={{ onMouseOver: setDataKey }}
-    />
-  );
+  return <LineRe dataKey={dataKey} stroke={stroke} strokeWidth={strokeWidth} />;
 });
 
 // @ts-ignore
