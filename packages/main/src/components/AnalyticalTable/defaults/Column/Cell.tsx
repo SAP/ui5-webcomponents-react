720da31c5f65f73c00db028003cd0f4b082e5c12
MarcusNotheis <marcus.notheis@sap.com>
2020-03-18 08:42:24 +0100

    fix(AnalyticalTable): Always render Cell content

--- packages/main/src/components/AnalyticalTable/defaults/Column/Cell.tsx
+++ packages/main/src/components/AnalyticalTable/defaults/Column/Cell.tsx
@@ -2,7 +2,7 @@ import { Text } from '@ui5/webcomponents-react/lib/Text';
 import React from 'react';
 
 export const Cell = ({ cell: { value = '', isGrouped }, row }) => {
-  let cellContent = `${value}`;
+  let cellContent = `${value ?? ''}`;
   if (isGrouped) {
     cellContent += ` (${row.subRows.length})`;
   }
