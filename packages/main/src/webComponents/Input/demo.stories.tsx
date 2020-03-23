e4789e06a6409576a0e9f0ae436db89706cf2cd7
MarcusNotheis <marcus.notheis@sap.com>
2020-03-23 08:35:29 +0100

    docs(Input): Updated Demo [ci skip]

--- packages/main/src/webComponents/Input/demo.stories.tsx
+++ packages/main/src/webComponents/Input/demo.stories.tsx
@@ -1,8 +1,9 @@
 import { action } from '@storybook/addon-actions';
-import { boolean, select } from '@storybook/addon-knobs';
+import { boolean, select, text } from '@storybook/addon-knobs';
 import { Input } from '@ui5/webcomponents-react/lib/Input';
 import { InputType } from '@ui5/webcomponents-react/lib/InputType';
 import { ValueState } from '@ui5/webcomponents-react/lib/ValueState';
+import { Icon } from '@ui5/webcomponents-react/lib/Icon';
 import React from 'react';
 
 export default {
@@ -13,17 +14,18 @@ export default {
 export const generatedDefaultStory = () => (
   <Input
     disabled={boolean('disabled', false)}
-    placeholder={'generatedString'}
+    placeholder={'A placeholder text...'}
     readonly={boolean('readonly', false)}
-    type={select('type', InputType, null)}
-    value={'generatedString'}
+    type={select('type', InputType, InputType.Text)}
+    value={text('value', '')}
     valueState={select('valueState', ValueState, null)}
     showSuggestions={boolean('showSuggestions', false)}
     onChange={action('onChange')}
     onInput={action('onInput')}
     onSubmit={action('onSubmit')}
     onSuggestionItemSelect={action('onSuggestionItemSelect')}
-    icon={null}
+    icon={<Icon name="search" />}
+    name={text('name', 'html-input-name')}
   >
     Input
   </Input>
