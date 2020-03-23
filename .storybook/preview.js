891f2351ad9fa420823db0e461b02d1a281e4312
MarcusNotheis <marcus.notheis@sap.com>
2020-03-23 10:11:35 +0100

    fix(ObjectPage): RTL support for Header
    Closes #366

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,21 +1,21 @@
 import { select, withKnobs } from '@storybook/addon-knobs';
 import { makeDecorator } from '@storybook/addons';
 import { addDecorator, addParameters } from '@storybook/react';
+import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme';
 import '@ui5/webcomponents-base/dist/features/browsersupport/IE11';
+import '@ui5/webcomponents-fiori/dist/json-imports/Themes';
 import '@ui5/webcomponents-react-base/polyfill/IE11';
 import { ContentDensity } from '@ui5/webcomponents-react/lib/ContentDensity';
 import { ThemeProvider } from '@ui5/webcomponents-react/lib/ThemeProvider';
 import { Themes } from '@ui5/webcomponents-react/lib/Themes';
 import '@ui5/webcomponents/dist/json-imports/i18n';
+import '@ui5/webcomponents/dist/json-imports/Themes';
 import '@webcomponents/webcomponentsjs/webcomponents-bundle';
 import { window } from 'global';
 import 'highlight.js/styles/solarized-dark.css';
 import qs from 'qs';
 import React, { useEffect } from 'react';
 import 'react-app-polyfill/ie11';
-import '@ui5/webcomponents/dist/json-imports/Themes';
-import '@ui5/webcomponents-fiori/dist/json-imports/Themes';
-import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme';
 
 addParameters({
   options: {
@@ -30,7 +30,7 @@ addParameters({
 
 addDecorator(withKnobs);
 
-const ThemeContainer = ({ theme, contentDensity, children, setQueryParam }) => {
+const ThemeContainer = ({ theme, contentDensity, children, setQueryParam, direction }) => {
   useEffect(() => {
     if (contentDensity === ContentDensity.Compact) {
       document.body.classList.add('ui5-content-density-compact');
@@ -38,6 +38,10 @@ const ThemeContainer = ({ theme, contentDensity, children, setQueryParam }) => {
       document.body.classList.remove('ui5-content-density-compact');
     }
   }, [contentDensity, setQueryParam]);
+
+  useEffect(() => {
+    document.querySelector('html').setAttribute('dir', direction.toLowerCase());
+  }, [direction]);
 
   useEffect(() => {
     setTheme(theme);
@@ -73,6 +77,7 @@ const withQuery = makeDecorator({
       <ThemeContainer
         theme={select('Theme', [Themes.sap_fiori_3, Themes.sap_fiori_3_dark], Themes.sap_fiori_3)}
         contentDensity={select('ContentDensity', ContentDensity, contentDensity)}
+        direction={select('Text Direction', ['LTR', 'RTL'], 'LTR')}
         setQueryParam={setQueryParam}
       >
         {getStory(context)}
