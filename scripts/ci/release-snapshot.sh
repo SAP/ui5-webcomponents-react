32a647a4b0ed5b33ee983c58334227580988fb2b
Marcus Notheis <MarcusNotheis@users.noreply.github.com>
2020-03-26 15:21:33 +0100

    ci: Don't release snapshots when in release-candidate stage

--- scripts/ci/release-snapshot.sh
+++ scripts/ci/release-snapshot.sh
@@ -6,7 +6,7 @@ git checkout scripts/rollup/results.json
 CURRENT_LERNA_VERSION=$(node -p -e "require('./lerna.json').version")
 IS_IN_RC_STATUS=$(echo "${CURRENT_LERNA_VERSION}" | awk '/rc\.[0-9]+$/')
 
-if [ -z "${IS_IN_RC_STATUS}" ]
+if [ ! -z "${IS_IN_RC_STATUS}" ]
 then
   echo "Current Version is already in release candidate status. Skipping snapshot release."
   exit 0
