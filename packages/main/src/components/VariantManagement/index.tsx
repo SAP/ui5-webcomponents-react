4bc61ec7feb7150495129f6abfced688467a8002
MarcusNotheis <marcus.notheis@sap.com>
2020-04-09 13:22:57 +0200

    fix(VariantManagement): fix invalid hook calls
    BREAKING CHANGE: renamed prop `initialSelectedKey` to `selectedKey`

--- packages/main/src/components/VariantManagement/index.tsx
+++ packages/main/src/components/VariantManagement/index.tsx
@@ -1,4 +1,5 @@
 import '@ui5/webcomponents-icons/dist/icons/navigation-down-arrow';
+import { StyleClassHelper } from '@ui5/webcomponents-react-base/lib/StyleClassHelper';
 import { createComponentStyles } from '@ui5/webcomponents-react-base/lib/createComponentStyles';
 import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
 import { usePassThroughHtmlProps } from '@ui5/webcomponents-react-base/lib/usePassThroughHtmlProps';
@@ -25,7 +26,7 @@ export interface VariantItem {
 export interface VariantManagementPropTypes extends CommonProps {
   placement?: PlacementType;
   popupTitle?: string;
-  initialSelectedKey?: string;
+  selectedKey?: string;
   closeOnItemSelect?: boolean;
   variantItems: VariantItem[];
   onSelect?: (event: CustomEvent<{ item: HTMLElement; selectedItem: VariantItem }>) => void;
@@ -34,30 +35,26 @@ export interface VariantManagementPropTypes extends CommonProps {
 }
 
 const styles = {
-  VariantManagement: {
+  container: {
     display: 'flex',
     alignItems: 'center',
     textAlign: 'center',
     cursor: 'pointer'
   },
-  VariantManagementText: {
+  title: {
     cursor: 'pointer',
     color: ThemingParameters.sapButton_TextColor,
     '&:hover': {
-      color: ThemingParameters.sapHighlightColor
-    },
-    '&:active': {
-      color: ThemingParameters.sapHighlightColor
+      color: ThemingParameters.sapButton_Hover_TextColor
     }
   },
   disabled: {
-    color: ThemingParameters.sapGroup_TitleTextColor,
-    cursor: 'default',
-    '&:hover': {
-      color: ThemingParameters.sapGroup_TitleTextColor
-    },
-    '&:active': {
-      color: ThemingParameters.sapGroup_TitleTextColor
+    '& $title': {
+      color: ThemingParameters.sapGroup_TitleTextColor,
+      cursor: 'default',
+      '&:hover': {
+        color: 'ThemingParameters.sapGroup_TitleTextColor'
+      }
     }
   },
   footer: {
@@ -71,7 +68,7 @@ const useStyles = createComponentStyles(styles, { name: 'VariantManagement' });
  * <code>import { VariantManagement } from '@ui5/webcomponents-react/lib/VariantManagement';</code>
  */
 const VariantManagement: FC<VariantManagementPropTypes> = forwardRef(
-  (props: VariantManagementPropTypes, ref: Ref<any>) => {
+  (props: VariantManagementPropTypes, ref: Ref<HTMLDivElement>) => {
     const {
       variantItems,
       popupTitle,
@@ -80,7 +77,6 @@ const VariantManagement: FC<VariantManagementPropTypes> = forwardRef(
       tooltip,
       placement,
       level,
-      initialSelectedKey,
       onSelect,
       closeOnItemSelect,
       disabled
@@ -89,19 +85,13 @@ const VariantManagement: FC<VariantManagementPropTypes> = forwardRef(
     const classes = useStyles();
     const popoverRef = useRef<Ui5ResponsivePopoverDomRef>(null);
 
-    const [selectedKey, setSelectedKey] = useState(
-      initialSelectedKey ? initialSelectedKey : variantItems?.[0]?.key ?? null
-    );
-
-    if (!variantItems || variantItems.length < 1) {
-      return null;
-    }
+    const [selectedKey, setSelectedKey] = useState(props.selectedKey ?? variantItems?.[0]?.key ?? null);
 
     useEffect(() => {
-      if (initialSelectedKey) {
-        setSelectedKey(initialSelectedKey);
+      if (props.selectedKey) {
+        setSelectedKey(props.selectedKey);
       }
-    }, [initialSelectedKey, setSelectedKey]);
+    }, [props.selectedKey, setSelectedKey]);
 
     const handleCancelButtonClick = useCallback(() => {
       popoverRef.current.close();
@@ -120,56 +110,59 @@ const VariantManagement: FC<VariantManagementPropTypes> = forwardRef(
           Cancel
         </Button>
       );
-    }, [classes.footer]);
+    }, [classes.footer, handleCancelButtonClick]);
 
     const getItemByKey = (key) => {
-      return variantItems.find((item) => item.key === key);
+      return variantItems?.find((item) => item.key === key);
     };
 
     const selectedItem = getItemByKey(selectedKey) || variantItems[0];
 
-    let textClasses = classes.VariantManagementText;
+    const variantManagementClasses = StyleClassHelper.of(classes.container);
+
     if (disabled) {
-      textClasses += ` ${classes.disabled}`;
+      variantManagementClasses.put(classes.disabled);
+    }
+
+    if (className) {
+      variantManagementClasses.put(className);
     }
 
     const handleVariantItemSelect = useCallback(
       (event) => {
         const newSelectedKey = event.detail.item.dataset.key;
-        const selectedItem = getItemByKey(newSelectedKey) || variantItems[0];
         setSelectedKey(newSelectedKey);
-        onSelect(enrichEventWithDetails(event, { ...event.details, selectedItem }));
+        onSelect(
+          enrichEventWithDetails(event, {
+            ...event.details,
+            selectedItem: getItemByKey(newSelectedKey) || variantItems[0]
+          })
+        );
         if (closeOnItemSelect) {
           handleCancelButtonClick();
         }
       },
       [handleCancelButtonClick, closeOnItemSelect, selectedKey, variantItems, setSelectedKey]
     );
+
     const passThroughProps = usePassThroughHtmlProps(props, ['onSelect']);
 
+    if (!variantItems || variantItems.length < 1) {
+      return null;
+    }
+
     return (
-      <>
-        <div className={classes.VariantManagement}>
-          <Title level={level} className={textClasses}>
-            {selectedItem.label}
-          </Title>
-          <Button
-            onClick={handleOpenVariantManagement}
-            design={ButtonDesign.Transparent}
-            icon="navigation-down-arrow"
-            disabled={disabled}
-          />
-        </div>
-        <ResponsivePopover
-          ref={popoverRef}
-          headerText={popupTitle}
-          placementType={placement}
-          footer={footerButtons}
-          className={className}
-          style={style}
-          tooltip={tooltip}
-          {...passThroughProps}
-        >
+      <div className={variantManagementClasses.valueOf()} style={style} title={tooltip} {...passThroughProps} ref={ref}>
+        <Title level={level} className={classes.title}>
+          {selectedItem.label}
+        </Title>
+        <Button
+          onClick={handleOpenVariantManagement}
+          design={ButtonDesign.Transparent}
+          icon="navigation-down-arrow"
+          disabled={disabled}
+        />
+        <ResponsivePopover ref={popoverRef} headerText={popupTitle} placementType={placement} footer={footerButtons}>
           <List onItemClick={handleVariantItemSelect} mode={ListMode.SingleSelect}>
             {variantItems.map((item) => (
               <StandardListItem
@@ -184,19 +177,20 @@ const VariantManagement: FC<VariantManagementPropTypes> = forwardRef(
             ))}
           </List>
         </ResponsivePopover>
-      </>
+      </div>
     );
   }
 );
 
 VariantManagement.defaultProps = {
   popupTitle: 'Variants',
-  initialSelectedKey: null,
+  selectedKey: null,
   onSelect: () => {},
   closeOnItemSelect: true,
   placement: PlacementType.Bottom,
   level: TitleLevel.H4,
-  disabled: false
+  disabled: false,
+  variantItems: []
 };
 VariantManagement.displayName = 'Variant Management';
 
