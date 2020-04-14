f25d2d12c9aff4e2237f1d8eb9f5867c03d0bedd
MarcusNotheis <marcus.notheis@sap.com>
2020-04-14 14:49:57 +0200

    ci(snapshots): make sure master is checked out

--- scripts/ci/release-snapshot.sh
+++ scripts/ci/release-snapshot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# make sure we are on the master branch
+git checkout master
+
 git_hash=$(git rev-parse --short "$GITHUB_SHA")
 
 ${GITHUB_WORKSPACE}/node_modules/.bin/lerna publish "0.0.0-${git_hash}" \
