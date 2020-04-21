a7ffcd67b4cca7da3c6f1ad450e1e4673e41c55f
Lukas <lukas742@gmx.net>
2020-04-21 10:30:04 +0200

    fix(Charts): Add hcb and hcw support for charts. (#441)

--- packages/charts/src/themes/themeMap.ts
+++ packages/charts/src/themes/themeMap.ts
@@ -2,13 +2,22 @@ import * as sapBelize from './sap_belize';
 import * as sapBelizePlus from './sap_belize_plus';
 import * as sapFiori3 from './sap_fiori_3';
 import * as sapFiori3Dark from './sap_fiori_3_dark';
+import * as sapBelizeHCB from './sap_belize_hcb';
+import * as sapBelizeHCW from './sap_belize_hcw';
 
 const themeMap = new Map();
 themeMap.set('sap_belize', sapBelize);
 themeMap.set('sap_belize_plus', sapBelizePlus);
 themeMap.set('sap_fiori_3', sapFiori3);
 themeMap.set('sap_fiori_3_dark', sapFiori3Dark);
+themeMap.set('sap_belize_hcw', sapBelizeHCW);
+themeMap.set('sap_belize_hcb', sapBelizeHCB);
 
 export const getOrLoadTheme = (theme) => {
-  return themeMap.get(theme);
+  if (themeMap.has(theme)) {
+    return themeMap.get(theme);
+  } else {
+    console.warn(`${theme}-theme not found. Falling back to default theme 'sap_fiori_3'.`);
+    return themeMap.get('sap_fiori_3');
+  }
 };
