62753cc4bab056758e3c7f7d1f3ee3c6518a1b0d
Harbarth, Lukas <lukas.harbarth@sap.com>
2020-04-22 12:23:32 +0200

    if a fragment is passed as slot, return the corresponding children

--- packages/main/src/internal/withWebComponent.tsx
+++ packages/main/src/internal/withWebComponent.tsx
@@ -70,6 +70,14 @@ export const withWebComponent = <T extends {}>(
         return Object.entries(rest)
           .filter(([slotName]) => slotProperties.includes(slotName))
           .map(([slotName, slotValue]) => {
+            if (slotValue?.type === React.Fragment) {
+              return Children.map(slotValue.props.children, (item: ReactElement, index) =>
+                cloneElement(item, {
+                  key: `${slotName}-${index}`,
+                  slot: slotName
+                })
+              );
+            }
             return Children.map(slotValue, (item: ReactElement, index) =>
               cloneElement(item, {
                 key: `${slotName}-${index}`,
