da57c4b14ae24e2bee03b6b218d9de4f017944bb
klju1017 <julian.klein@sap.com>
2020-04-27 15:28:55 +0200

    Revert "Merge branch 'fix/charts' of https://github.com/SAP/ui5-webcomponents-react into fix/charts"
    This reverts commit d9055cc6cc383c4a5af51bd49e56c360fbfee07f, reversing
    changes made to 754c9f8786ca4a599281ba6b131e1d075b1bf1e8.

--- packages/charts/src/components/ComposedChart/index.tsx
+++ packages/charts/src/components/ComposedChart/index.tsx
@@ -245,66 +245,34 @@ const ComposedChart: FC<ComposedChartProps> = forwardRef((props: ComposedChartPr
         {(chartConfig.xAxisVisible ?? true) &&
           dimensions.map((dimension, index) => {
             const XAxisLabel = useAxisLabel(dimension.formatter);
-            let AxisComponent;
-            const axisProps = {
-              key: dimension.accessor,
-              dataKey: dimension.accessor,
-              interval: dimension?.interval ?? (isBigDataSet ? 'preserveStart' : 0),
-              tick: index === 0 ? XAxisLabel : SecondaryDimensionLabel,
-              tickLine: index < 1,
-              axisLine: index < 1,
-              padding: { left: paddingCharts, right: paddingCharts }
-            };
-
-            if (layout === 'vertical') {
-              axisProps.type = 'category';
-              AxisComponent = YAxis;
-            } else {
-              axisProps.dataKey = dimension.accessor;
-              AxisComponent = XAxis;
-            }
-
-            return <AxisComponent {...axisProps} />;
+            return (
+              <XAxis
+                key={dimension.accessor}
+                dataKey={dimension.accessor}
+                xAxisId={index}
+                interval={dimension?.interval ?? (isBigDataSet ? 'preserveStart' : 0)}
+                tick={index === 0 ? XAxisLabel : SecondaryDimensionLabel}
+                tickLine={index < 1}
+                axisLine={index < 1}
+                padding={{ left: paddingCharts / 2, right: paddingCharts / 2 }}
+              />
+            );
           })}
-        {layout === 'horizontal' && (
-          <YAxis
-            axisLine={chartConfig.yAxisVisible ?? false}
-            tickLine={false}
-            yAxisId="primary"
-            tickFormatter={primaryMeasure?.formatter}
-            interval={0}
-          />
-        )}
-        {layout === 'vertical' && (
-          <XAxis
-            axisLine={chartConfig.yAxisVisible ?? false}
-            tickLine={false}
-            xAxisId="primary"
-            tickFormatter={primaryMeasure?.formatter}
-            interval={0}
-            type="number"
-          />
-        )}
-
-        {chartConfig.secondYAxis && chartConfig.secondYAxis.dataKey && layout === 'horizontal' && (
+        <YAxis
+          axisLine={chartConfig.yAxisVisible ?? false}
+          tickLine={false}
+          yAxisId="left"
+          tickFormatter={primaryMeasure?.formatter}
+          interval={0}
+        />
+        {chartConfig.secondYAxis && chartConfig.secondYAxis.dataKey && (
           <YAxis
             dataKey={chartConfig.secondYAxis.dataKey}
             stroke={chartConfig.secondYAxis.color ?? `var(--sapChart_OrderedColor_${(colorSecondY % 11) + 1})`}
             label={{ value: chartConfig.secondYAxis.name, offset: 2, angle: +90, position: 'center' }}
             orientation="right"
-            yAxisId="secondary"
+            yAxisId="right"
             interval={0}
-          />
-        )}
-        {chartConfig.secondYAxis && chartConfig.secondYAxis.dataKey && layout === 'vertical' && (
-          <XAxis
-            dataKey={chartConfig.secondYAxis.dataKey}
-            stroke={chartConfig.secondYAxis.color ?? `var(--sapChart_OrderedColor_${(colorSecondY % 11) + 1})`}
-            label={{ value: chartConfig.secondYAxis.name, offset: 2, angle: +90, position: 'center' }}
-            orientation="top"
-            xAxisId="secondary"
-            interval={0}
-            type="number"
           />
         )}
         {chartConfig.referenceLine && (
@@ -344,9 +312,7 @@ const ComposedChart: FC<ComposedChartProps> = forwardRef((props: ComposedChartPr
               chartElementProps.onClick = onDataPointClickInternal;
               chartElementProps.stackId = element.stackId ?? undefined;
               chartElementProps.labelPosition = element.stackId ? 'insideTop' : 'top';
-              if (layout === 'vertical') {
-                labelPosition = element.stackId ? 'insideRight' : 'right';
-              }
+              chartElementProps.maxBarSize = 40;
               break;
             case 'area':
               chartElementProps.dot = !isBigDataSet;
@@ -361,16 +327,9 @@ const ComposedChart: FC<ComposedChartProps> = forwardRef((props: ComposedChartPr
               );
               break;
           }
-
-          if (layout === 'vertical') {
-            chartElementProps.xAxisId =
-              chartConfig?.secondYAxis?.dataKey === element.accessor ? 'secondary' : 'primary';
-          } else {
-            chartElementProps.yAxisId =
-              chartConfig?.secondYAxis?.dataKey === element.accessor ? 'secondary' : 'primary';
-          }
           return (
             <ChartElement
+              yAxisId={chartConfig?.secondYAxis?.dataKey === element.accessor ? 'right' : 'left'}
               key={element.accessor}
               name={element.label ?? element.accessor}
               label={<CustomDataLabel config={element} chartType={element.type} position={labelPosition} />}
