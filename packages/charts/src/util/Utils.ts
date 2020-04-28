7954d8c8e58ee91a98d5b39bc8e931b947ea0ffb
MarcusNotheis <marcus.notheis@sap.com>
2020-04-28 10:11:26 +0200

    adjust size of loading placeholders

--- packages/charts/src/util/Utils.ts
+++ packages/charts/src/util/Utils.ts
@@ -1,56 +1,8 @@
-import merge from 'lodash.merge';
-import { useMemo } from 'react';
-import { defaultFont } from '../config';
-
-export const useMergedConfig = (x, y) => {
-  return useMemo(() => {
-    return merge(x, y);
-  }, [x, y]);
-};
-
-// this needs to be a function as we need the `this` of the chart;
-export const formatAxisCallback = (formatter) =>
-  function (value) {
-    // @ts-ignore
-    const currentDataset = this.chart.data.datasets[0];
-    return formatter(value, currentDataset);
-  };
-
-export const formatDataLabel = (formatter) => (val, context) => {
-  return formatter(val, context.dataset, context);
-};
-
-export const formatTooltipLabel = (categoryFormatter, valueFormatter, valueAccessor = 'yLabel') => (
-  tooltipItem,
-  data
-): string => {
-  const currentDataset = data.datasets[tooltipItem.datasetIndex];
-  let label = categoryFormatter(currentDataset.label, currentDataset, tooltipItem) || '';
-
-  if (label) {
-    label += ': ';
-  }
-
-  label += valueFormatter(tooltipItem[valueAccessor], currentDataset, tooltipItem);
-  return label;
-};
-
-export const formatTooltipLabelForPieCharts = (categoryFormatter, valueFormatter) => (tooltipItem, data): string => {
-  const currentDataset = data.datasets[tooltipItem.datasetIndex];
-  let label = categoryFormatter(data.labels[tooltipItem.index], currentDataset, tooltipItem) || '';
-
-  if (label) {
-    label += ': ';
-  }
-  label += valueFormatter(data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index], currentDataset, tooltipItem);
-  return label;
-};
-
 let canvas;
 let textHeight;
 
 export const getTextWidth = (text) => {
-  const font = `normal ${defaultFont.size}pt ${defaultFont.family}`;
+  const font = `normal 12pt "72","72full",Arial,Helvetica,sans-serif`;
   // re-use canvas object for better performance
   canvas = canvas || (canvas = document.createElement('canvas'));
   const context = canvas.getContext('2d');
@@ -59,7 +11,7 @@ export const getTextWidth = (text) => {
   return metrics.width;
 };
 
-export const getTextHeight = (text = 'M', font = `normal ${defaultFont.size}pt ${defaultFont.family}`) => {
+export const getTextHeight = (text = 'M', font = `normal 12pt "72","72full",Arial,Helvetica,sans-serif`) => {
   // re-use canvas object for better performance
   canvas = canvas || (canvas = document.createElement('canvas'));
   const context = canvas.getContext('2d');
