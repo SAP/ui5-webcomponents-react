14c7ea53389f6ff65e813f799978734ceb671142
MarcusNotheis <marcus.notheis@sap.com>
2020-04-30 10:12:23 +0200

    update Panel

--- packages/main/src/webComponents/Panel/Panel.stories.tsx
+++ packages/main/src/webComponents/Panel/Panel.stories.tsx
@@ -1,6 +1,7 @@
 import { boolean, select, text } from '@storybook/addon-knobs';
 import { Panel } from '@ui5/webcomponents-react/lib/Panel';
 import { PanelAccessibleRoles } from '@ui5/webcomponents-react/lib/PanelAccessibleRoles';
+import { TitleLevel } from '@ui5/webcomponents-react/lib/TitleLevel';
 import React from 'react';
 
 export default {
@@ -10,11 +11,12 @@ export default {
 
 export const generatedDefaultStory = () => (
   <Panel
-    accessibleRole={select('accessibleRole', PanelAccessibleRoles, PanelAccessibleRoles['Form'])}
+    accessibleRole={select('accessibleRole', PanelAccessibleRoles, PanelAccessibleRoles.Form)}
     collapsed={boolean('collapsed', false)}
     fixed={boolean('fixed', false)}
     headerText={text('headerText', 'Panel Header Text')}
     header={null}
+    headerLevel={select('headerLevel', TitleLevel, TitleLevel.H2)}
   >
     Some Content
   </Panel>
