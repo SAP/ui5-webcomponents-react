f8aa46156cbb4ebbda5f45de810a4f0eb1b2ba53
MarcusNotheis <marcus.notheis@sap.com>
2020-04-30 09:59:37 +0200

    update carousel
    BREAKING CHANGE: *Carousel*: rename prop `cycling` to `cyclic`
    BREAKING CHANGE: *Carousel*: replace prop `itemsPerPage` with `itemsPerPageL`, `itemsPerPageM`, `itemsPerPageS`

--- packages/main/src/webComponents/Carousel/Carousel.stories.tsx
+++ packages/main/src/webComponents/Carousel/Carousel.stories.tsx
@@ -1,3 +1,4 @@
+import { action } from '@storybook/addon-actions';
 import { boolean, number, select } from '@storybook/addon-knobs';
 import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
 import { Carousel } from '@ui5/webcomponents-react/lib/Carousel';
@@ -12,9 +13,12 @@ export default {
 export const generatedDefaultStory = () => (
   <Carousel
     arrowsPlacement={select('arrowsPlacement', CarouselArrowsPlacement, CarouselArrowsPlacement['Content'])}
-    cycling={boolean('cycling', false)}
+    cyclic={boolean('cyclic', false)}
     hideNavigation={boolean('hideNavigation', false)}
-    itemsPerPage={number('itemsPerPage', 1)}
+    itemsPerPageL={number('itemsPerPageL', 1)}
+    itemsPerPageM={number('itemsPerPageM', 1)}
+    itemsPerPageS={number('itemsPerPageS', 1)}
+    onNavigate={action('onNavigate')}
     selectedIndex={number('selectedIndex', 0)}
   >
     <div style={{ height: '300px', width: '100%', backgroundColor: ThemingParameters.sapInformationBackground }}>
