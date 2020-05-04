8acd7d1e5f237e680bef22d38cce75a9a143ac19
MarcusNotheis <marcus.notheis@sap.com>
2020-05-04 16:18:59 +0200

    Spinner

--- packages/main/src/components/Spinner/index.tsx
+++ packages/main/src/components/Spinner/index.tsx
@@ -1,8 +1,10 @@
+import { createComponentStyles } from '@ui5/webcomponents-react-base/lib/createComponentStyles';
+import { useI18nBundle } from '@ui5/webcomponents-react-base/lib/hooks';
 import { StyleClassHelper } from '@ui5/webcomponents-react-base/lib/StyleClassHelper';
 import { usePassThroughHtmlProps } from '@ui5/webcomponents-react-base/lib/usePassThroughHtmlProps';
+import { PLEASE_WAIT } from '@ui5/webcomponents-react/assets/i18n/i18n-defaults';
 import { Size } from '@ui5/webcomponents-react/lib/Size';
 import React, { FC, forwardRef, RefObject, useEffect, useState } from 'react';
-import { createComponentStyles } from '@ui5/webcomponents-react-base/lib/createComponentStyles';
 import { CommonProps } from '../../interfaces/CommonProps';
 import { styles } from './Spinner.jss';
 
@@ -45,6 +47,8 @@ const Spinner: FC<SpinnerProps> = forwardRef((props: SpinnerProps, ref: RefObjec
 
   const passThroughProps = usePassThroughHtmlProps(props);
 
+  const i18nBundle = useI18nBundle('@ui5/webcomponents-react');
+
   if (!isVisible) {
     return null;
   }
@@ -59,7 +63,7 @@ const Spinner: FC<SpinnerProps> = forwardRef((props: SpinnerProps, ref: RefObjec
       tabIndex={0}
       aria-valuemin={0}
       aria-valuemax={100}
-      title={tooltip || 'Please wait'}
+      title={tooltip || i18nBundle.getText(PLEASE_WAIT)}
       slot={slot}
       style={style}
       {...passThroughProps}
