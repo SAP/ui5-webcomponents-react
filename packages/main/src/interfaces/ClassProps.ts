4700c9819fc36e2617c7261710339c2d4d339686
MarcusNotheis <marcus.notheis@sap.com>
2020-05-08 08:36:01 +0200

    chore: clean up old, unused inferfaces

--- packages/main/src/interfaces/ClassProps.ts
+++
@@ -1,7 +0,0 @@
-import { RefObject } from 'react';
-import { ObjectWithVariableKeys } from './ObjectWithVariableKeys';
-
-export interface ClassProps {
-  classes?: ObjectWithVariableKeys;
-  innerRef?: RefObject<any>;
-}
