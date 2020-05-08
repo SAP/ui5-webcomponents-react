44a44e47f25ac1008ed50ba6458de2dae08abfdf
MarcusNotheis <marcus.notheis@sap.com>
2020-05-08 12:10:14 +0200

    never use babel runtime helper esm

--- scripts/rollup/configFactory.js
+++ scripts/rollup/configFactory.js
@@ -29,7 +29,6 @@ const rollupConfigFactory = (pkgName, externals = []) => {
     json(),
     babel({
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      envName: 'esm',
       babelHelpers: 'runtime',
       configFile: path.resolve(PATHS.root, 'babel.config.js')
     })
