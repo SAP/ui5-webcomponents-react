6518aa05d6b8f80bcfd4e7fd6fb36b1589f18b1f
MarcusNotheis <marcus.notheis@sap.com>
2020-05-08 13:36:46 +0200

    fix(cra-template): fix test to be successful

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,22 +1,30 @@
-import { ThemeProvider } from '@ui5/webcomponents-react/lib/ThemeProvider';
+import {
+  FlexBox,
+  FlexBoxAlignItems,
+  FlexBoxDirection,
+  FlexBoxJustifyContent,
+  Link,
+  LinkDesign,
+  ShellBar,
+  ThemeProvider
+} from '@ui5/webcomponents-react';
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ShellBar primaryTitle="UI5 Web Components for React Template" />
+      <FlexBox
+        style={{ width: '100%', height: '100vh' }}
+        direction={FlexBoxDirection.Column}
+        justifyContent={FlexBoxJustifyContent.Center}
+        alignItems={FlexBoxAlignItems.Center}
+      >
+        <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
+          Getting Started with UI5 Web Component for React
+        </Link>
+      </FlexBox>
     </ThemeProvider>
   );
 }
