030473c9a38d2bcaa3069925c9efd5ec75d0f909
Marcus Notheis <MarcusNotheis@users.noreply.github.com>
2020-05-15 08:44:17 +0200

    chore(main): remove deprecated render methods (#511)
    BREAKING CHANGE: **AnalyticalTable**: remove deprecated prop `renderExtension`, please use `extension` instead
    BREAKING CHANGE: **Bar**: remove deprecated prop `renderContentLeft`, `renderContentMiddle`, `renderContentRight`, please use `contentLeft`, `contentMiddle`, `contentRight` instead
    BREAKING CHANGE: **FormItem**: remove deprecated prop `labelText`, please use `label` instead
    BREAKING CHANGE: **FilterBar**: remove deprecated prop `renderSearch`, `renderVariants`, please use `search`, `variants` instead
    BREAKING CHANGE: **ObjectPage**: remove deprecated prop `renderHeaderContent`, `renderBreadcrumbs`, `renderKeyInfos`, please use `headerContent`, `breadcrumbs`, `keyInfos` instead
    BREAKING CHANGE: **Page**: remove deprecated prop `renderCustomHeader`, `renderCustomFooter`, please use `customHeader`, `customFooter` instead

--- packages/main/src/components/FilterItem/index.tsx
+++ packages/main/src/components/FilterItem/index.tsx
@@ -17,7 +17,6 @@ import styles from './FilterItem.jss';
 
 export interface FilterItemPropTypes extends CommonProps {
   placeholder?: string;
-  renderText?: (item?: any) => JSX.Element;
   type?: FilterType;
   label?: string;
   filterItems?: any[];
@@ -143,7 +142,6 @@ const FilterItem: FC<FilterItemPropTypes> = forwardRef((props: FilterItemPropTyp
 
 FilterItem.defaultProps = {
   placeholder: '',
-  renderText: (item) => (item && item.text) || '',
   type: FilterType.Default,
   filterItems: [],
   label: '',
