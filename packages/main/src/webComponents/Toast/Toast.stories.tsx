0aeecf07d8b51c1ceb9e25c972d417fc86b1178f
Marcus Notheis <marcus.notheis@sap.com>
2020-05-19 15:51:54 +0200

    docs(Toast): explain how a toast can be shown
    Closes #519

--- packages/main/src/webComponents/Toast/Toast.stories.tsx
+++ packages/main/src/webComponents/Toast/Toast.stories.tsx
@@ -2,11 +2,17 @@ import { number, select } from '@storybook/addon-knobs';
 import { Button } from '@ui5/webcomponents-react/lib/Button';
 import { Toast } from '@ui5/webcomponents-react/lib/Toast';
 import { ToastPlacement } from '@ui5/webcomponents-react/lib/ToastPlacement';
-import React from 'react';
+import React, { useRef } from 'react';
+import mdx from './Toast.mdx';
 
 export default {
   title: 'UI5 Web Components / Toast',
-  component: Toast
+  component: Toast,
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
 };
 
 const showToast = () => {
@@ -14,20 +20,26 @@ const showToast = () => {
   document.querySelector('#web_components_react_toast_demo').show();
 };
 
-export const generatedDefaultStory = () => (
-  <>
-    <Toast
-      // @ts-ignore
-      id="web_components_react_toast_demo"
-      duration={number('duration', 3000)}
-      placement={select('placement', ToastPlacement, ToastPlacement.BottomCenter)}
-    >
-      Some Content
-    </Toast>
-    <Button onClick={showToast}>Show Toast</Button>
-  </>
-);
+export const generatedDefaultStory = () => {
+  const toast = useRef();
+
+  const showToast = () => {
+    toast.current.show();
+  };
+  return (
+    <>
+      <Toast
+        ref={toast}
+        duration={number('duration', 3000)}
+        placement={select('placement', ToastPlacement, ToastPlacement.BottomCenter)}
+      >
+        Some Content
+      </Toast>
+      <Button onClick={showToast}>Show Toast</Button>
+    </>
+  );
+};
 
 generatedDefaultStory.story = {
-  name: 'Generated default story'
+  name: 'Default story'
 };
