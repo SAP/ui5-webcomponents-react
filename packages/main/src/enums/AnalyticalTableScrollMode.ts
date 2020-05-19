64f7e6481a7a6164bce8973e8c3337cc52b45831
Lukas Harbarth <lukas742@gmx.net>
2020-05-19 10:45:07 +0200

    fix(AnalyticalTable): remove deprecated AnalyticalTableScrollMode smart (#526)

--- packages/main/src/enums/AnalyticalTableScrollMode.ts
+++ packages/main/src/enums/AnalyticalTableScrollMode.ts
@@ -2,8 +2,6 @@ export enum AnalyticalTableScrollMode {
   // Scroll as little as possible to ensure the item is visible. (If the item is already visible, it won't scroll at all.)
   auto = 'auto',
   // If the item is already visible, don't scroll at all. If it is less than one viewport away, scroll as little as possible so that it becomes visible. If it is more than one viewport away, scroll so that it is centered within the list.
-  smart = 'smart',
-  // Center align the item within the list.
   center = 'center',
   // Align the item to the end of the list (the bottom for vertical lists or the right for horizontal lists).
   end = 'end',
