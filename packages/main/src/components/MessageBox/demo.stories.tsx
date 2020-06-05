f20d8614bfe241b5e7ea8c87ac047c2eb7e92df4
Marcus Notheis <marcus.notheis@sap.com>
2020-06-05 16:42:31 +0200

    fix(MessageBox): allow custom action texts (#561)
    Closes #551

--- packages/main/src/components/MessageBox/demo.stories.tsx
+++ packages/main/src/components/MessageBox/demo.stories.tsx
@@ -2,6 +2,7 @@ import { action } from '@storybook/addon-actions';
 import { boolean, select, text } from '@storybook/addon-knobs';
 import { MessageBox } from '@ui5/webcomponents-react/lib/MessageBox';
 import { MessageBoxTypes } from '@ui5/webcomponents-react/lib/MessageBoxTypes';
+import { MessageBoxActions } from '@ui5/webcomponents-react/lib/MessageBoxActions';
 import React from 'react';
 
 export const defaultStory = () => (
@@ -18,6 +19,18 @@ export const defaultStory = () => (
 defaultStory.story = {
   name: 'Default'
 };
+
+export const withCustomActions = () => (
+  <MessageBox
+    type={select('type', Object.values(MessageBoxTypes), MessageBoxTypes.CONFIRM)}
+    open={boolean('open', true)}
+    onClose={action('onClose')}
+    title={text('title', 'A Custom Message Box Title')}
+    actions={[MessageBoxActions.OK, 'A custom action', MessageBoxActions.CANCEL]}
+  >
+    Message Box Content
+  </MessageBox>
+);
 
 export default {
   title: 'Components / MessageBox',
