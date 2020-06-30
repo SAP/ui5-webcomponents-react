0f86f3ec17b96f6780200b9cd8fd8fc5f724418d
Harbarth, Lukas <lukas.harbarth@sap.com>
2020-06-30 16:35:46 +0200

    fix import

--- packages/main/src/components/AnalyticalTable/hooks/useStyling.ts
+++ packages/main/src/components/AnalyticalTable/hooks/useStyling.ts
@@ -1,12 +1,12 @@
-import { StyleClassHelper } from '@ui5/webcomponents-react-base';
+import { getRTL } from '@ui5/webcomponents-base/dist/config/RTL';
+import { StyleClassHelper } from '@ui5/webcomponents-react-base/lib/StyleClassHelper';
 import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
+import { GlobalStyleClasses } from '@ui5/webcomponents-react/lib/GlobalStyleClasses';
 import { TableSelectionBehavior } from '@ui5/webcomponents-react/lib/TableSelectionBehavior';
 import { TableSelectionMode } from '@ui5/webcomponents-react/lib/TableSelectionMode';
 import { TextAlign } from '@ui5/webcomponents-react/lib/TextAlign';
 import { VerticalAlign } from '@ui5/webcomponents-react/lib/VerticalAlign';
-import { getRTL } from '@ui5/webcomponents-base/dist/config/RTL';
 import { CSSProperties } from 'react';
-import { GlobalStyleClasses } from '@ui5/webcomponents-react/lib/GlobalStyleClasses';
 
 const getTableProps = (tableProps, { instance }) => {
   const { classes } = instance.webComponentsReactProperties;
