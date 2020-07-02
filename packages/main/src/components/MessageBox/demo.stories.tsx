659e3206064e83c24bbb8917f000bad597de0730
D071203 <julian.klein@sap.com>
2020-07-02 21:26:09 +0200

    fix(Components Message Box Story): added button for displaying and hiding of message box

--- packages/main/src/components/MessageBox/demo.stories.tsx
+++ packages/main/src/components/MessageBox/demo.stories.tsx
@@ -1,14 +1,41 @@
 import { MessageBox } from '@ui5/webcomponents-react/lib/MessageBox';
 import { MessageBoxTypes } from '@ui5/webcomponents-react/lib/MessageBoxTypes';
 import { MessageBoxActions } from '@ui5/webcomponents-react/lib/MessageBoxActions';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { createSelectArgTypes } from '@shared/stories/createSelectArgTypes';
+import { Button } from '@ui5/webcomponents-react/lib/Button';
 
-export const defaultStory = (props) => (
-  <MessageBox type={props.type} open={props.open} onClose={props.onClose} title={props.title}>
-    Message Box Content
-  </MessageBox>
-);
+export const defaultStory = (props) => {
+  const messageBoxRef = useRef();
+  const onButtonClick = useCallback(
+    (e) => {
+      messageBoxRef.current.open(true);
+    },
+    [messageBoxRef]
+  );
+
+  const onCloseMessageBox = useCallback(
+    (e) => {
+      messageBoxRef.current.close(true);
+    },
+    [messageBoxRef]
+  );
+
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      <MessageBox
+        ref={messageBoxRef}
+        type={props.type}
+        open={props.open}
+        onClose={onCloseMessageBox}
+        title={props.title}
+      >
+        Message Box Content
+      </MessageBox>
+    </>
+  );
+};
 
 defaultStory.storyName = 'Default';
 
@@ -38,7 +65,7 @@ export default {
     }
   },
   args: {
-    open: true,
+    open: false,
     type: MessageBoxTypes.CONFIRM
   }
 };
