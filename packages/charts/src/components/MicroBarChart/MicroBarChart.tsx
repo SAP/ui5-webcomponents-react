feb847070dfe9614a907ad52c6026488c603103f
Marcus Notheis <marcus.notheis@sap.com>
2020-07-09 16:08:46 +0200

    allow accessor to be a deep accessor or a function

--- packages/charts/src/components/MicroBarChart/MicroBarChart.tsx
+++ packages/charts/src/components/MicroBarChart/MicroBarChart.tsx
@@ -1,21 +1,17 @@
-import { StyleClassHelper } from '@ui5/webcomponents-react-base/lib/StyleClassHelper';
 import { createComponentStyles } from '@ui5/webcomponents-react-base/lib/createComponentStyles';
+import { StyleClassHelper } from '@ui5/webcomponents-react-base/lib/StyleClassHelper';
 import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
 import { useConsolidatedRef } from '@ui5/webcomponents-react-base/lib/useConsolidatedRef';
 import { enrichEventWithDetails } from '@ui5/webcomponents-react-base/lib/Utils';
+import { BarChartPlaceholder } from '@ui5/webcomponents-react-charts/lib/BarChartPlaceholder';
 import { ChartContainer } from '@ui5/webcomponents-react-charts/lib/components/ChartContainer';
 import React, { CSSProperties, FC, forwardRef, Ref, useCallback, useMemo } from 'react';
+import { getValueByDataKey } from 'recharts/lib/util/ChartUtils';
 import { IChartBaseProps } from '../../interfaces/IChartBaseProps';
 import { IChartMeasure } from '../../interfaces/IChartMeasure';
 import { defaultFormatter } from '../../internal/defaults';
-import { BarChartPlaceholder } from '@ui5/webcomponents-react-charts/lib/BarChartPlaceholder';
 
-interface MeasureConfig extends Omit<IChartMeasure, 'accessor' | 'color'> {
-  /**
-   * A string containing the path to the dataset key this bar should display.
-   * Supports object structures by using `'parent.child'`. Can also be a getter.
-   */
-  accessor: string;
+interface MeasureConfig extends Omit<IChartMeasure, 'color'> {
   /**
    * array of any valid CSS Color or CSS Variable. Defaults to the `sapChart_OrderedColor_` colors
    */
@@ -37,20 +33,22 @@ interface DimensionConfig {
    * A string containing the path to the dataset key this bar should display.
    * Supports object structures by using `'parent.child'`. Can also be a getter.
    */
-  accessor: string;
+  accessor: string | Function;
   /**
    * function will be called for each data label and allows you to format it according to your needs
    */
   formatter?: (value: any) => string;
 }
 
-export interface MicroBarChartProps extends IChartBaseProps {
+export interface MicroBarChartProps
+  extends Omit<IChartBaseProps, 'noLegend' | 'onLegendClick' | 'noAnimation' | 'chartConfig'> {
   dimension: DimensionConfig;
   /**
    * An array of config objects. Each object is defining one bar in the chart.
    *
    * <h4>Required properties</h4>
    * - `accessor`: string containing the path to the dataset key this bar should display. Supports object structures by using <code>'parent.child'</code>.
+   * Can also be a getter.
    *
    * <h4>Optional properties</h4>
    *
@@ -71,7 +69,12 @@ const MicroBarChartStyles = {
   container: {
     display: 'flex',
     flexDirection: 'column',
-    overflow: 'hidden'
+    overflow: 'hidden',
+    fontFamily: ThemingParameters.sapFontFamily,
+    fontWeight: 'normal',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'space-around'
   },
   barContainer: {
     cursor: 'auto'
@@ -85,20 +88,15 @@ const MicroBarChartStyles = {
     justifyContent: 'space-between'
   },
   valueContainer: {
-    display: 'flex'
+    display: 'flex',
+    height: '4px',
+    backgroundColor: ThemingParameters.sapContent_Placeholderloading_Background
   },
   valueBar: { height: '4px' },
-  fillUp: {
-    height: '4px',
-    backgroundColor: '#f1f2f3',
-    flexGrow: 1
-  },
   label: {
-    color: 'hsl(0, 0%, 40%)',
+    color: ThemingParameters.sapContent_LabelColor,
     display: 'block',
     overflow: 'hidden',
-    fontFamily: "'72', Arial, Helvetica, sans-serif",
-    fontWeight: 'normal',
     whiteSpace: 'nowrap',
     textOverflow: 'ellipsis',
     fontSize: ThemingParameters.sapFontSmallSize,
@@ -110,11 +108,10 @@ const MicroBarChartStyles = {
     overflow: 'hidden',
     fontSize: ThemingParameters.sapFontSmallSize,
     boxSizing: 'border-box',
-    fontFamily: "'72', Arial, Helvetica, sans-serif",
-    fontWeight: 'normal',
+
     whiteSpace: 'nowrap',
     textOverflow: 'ellipsis',
-    color: '#333333',
+    color: ThemingParameters.sapTextColor,
     textAlign: 'right'
   }
 };
@@ -125,7 +122,7 @@ const useStyles = createComponentStyles(MicroBarChartStyles, { name: 'MicroBarCh
  * <code>import { MicroBarChart } from '@ui5/webcomponents-react-charts/lib/MicroBarChart';</code>
  */
 const MicroBarChart: FC<MicroBarChartProps> = forwardRef((props: MicroBarChartProps, ref: Ref<HTMLDivElement>) => {
-  const { loading, dataset, onDataPointClick, style, className, tooltip, slot, chartConfig } = props;
+  const { loading, dataset, onDataPointClick, style, className, tooltip, slot } = props;
   const classes = useStyles();
 
   const dimension: DimensionConfig = useMemo(
@@ -143,14 +140,8 @@ const MicroBarChart: FC<MicroBarChartProps> = forwardRef((props: MicroBarChartPr
     [props.measure]
   );
 
-  const maxValue = dataset ? Math.max(...dataset?.map((item) => item[measure.accessor])) : 0;
+  const maxValue = dataset ? Math.max(...dataset?.map((item) => getValueByDataKey(item, measure.accessor))) : 0;
   const chartRef = useConsolidatedRef<any>(ref);
-
-  const marginWithChartConfig = chartConfig?.margin
-    ? {
-        margin: `${chartConfig.margin.top}px ${chartConfig.margin.right}px ${chartConfig.margin.bottom}px ${chartConfig.margin.left}px`
-      }
-    : {};
 
   const barWidth = measure?.width ? `${measure.width}px` : 'auto';
 
@@ -160,7 +151,7 @@ const MicroBarChart: FC<MicroBarChartProps> = forwardRef((props: MicroBarChartPr
         onDataPointClick(
           enrichEventWithDetails(e, {
             dataKey: measure.accessor,
-            value: item[measure.accessor],
+            value: getValueByDataKey(item, measure.accessor),
             payload: item,
             dataIndex: index
           })
@@ -186,16 +177,14 @@ const MicroBarChart: FC<MicroBarChartProps> = forwardRef((props: MicroBarChartPr
       slot={slot}
       resizeDebounce={250}
     >
-      <div className={classes.container} style={marginWithChartConfig}>
+      <div className={classes.container}>
         {dataset?.map((item, index) => {
-          const formattedDimension = dimension.formatter(item[dimension.accessor]);
-          const formattedMeasure = measure.formatter(item[measure.accessor]);
+          const dimensionValue = getValueByDataKey(item, dimension.accessor);
+          const measureValue = getValueByDataKey(item, measure.accessor);
+          const formattedDimension = dimension.formatter(dimensionValue);
+          const formattedMeasure = measure.formatter(measureValue);
           return (
-            <div
-              key={item[dimension.accessor]}
-              className={barContainerClasses.toString()}
-              onClick={onBarClick(item, index)}
-            >
+            <div key={dimensionValue} className={barContainerClasses.className} onClick={onBarClick(item, index)}>
               <div className={classes.labelContainer}>
                 <span className={classes.label} title={formattedDimension}>
                   {formattedDimension}
@@ -213,13 +202,11 @@ const MicroBarChart: FC<MicroBarChartProps> = forwardRef((props: MicroBarChartPr
               >
                 <div
                   className={classes.valueBar}
-
                   style={{
-                    width: `${(item[measure.accessor] / maxValue) * 100}%`,
+                    width: `${(measureValue / maxValue) * 100}%`,
                     backgroundColor: resolveColor(index, measure?.colors?.[index])
                   }}
                 />
-                <div className={classes.fillUp} />
               </div>
             </div>
           );
