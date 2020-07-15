e2cea381916e3997d726244ca2c780364aa8b21a
Marcus Notheis <marcus.notheis@sap.com>
2020-07-15 14:32:31 +0200

    fix: remove useMemo from usePassThroughHtmlProps
    Remove `useMemo` here as it had no effect because the returned object is always spreaded.

--- packages/base/src/hooks/usePassThroughHtmlProps.ts
+++ packages/base/src/hooks/usePassThroughHtmlProps.ts
@@ -1,23 +1,14 @@
-import { useMemo } from 'react';
-
 const PROP_WHITELIST = /^(aria-|data-|id$|on[A-Z])/;
 
 export const usePassThroughHtmlProps = (props, propBlackList: string[] = []) => {
   const componentPropBlacklist = new Set(propBlackList);
-  const passThroughPropNames = Object.keys(props).filter(
-    (name) => PROP_WHITELIST.test(name) && !componentPropBlacklist.has(name)
-  );
 
-  return useMemo(
-    () => {
-      return passThroughPropNames.reduce(
-        (acc, val) => ({
-          ...acc,
-          [val]: props[val]
-        }),
-        {}
-      );
-    },
-    passThroughPropNames.map((name) => props[name])
-  );
+  const returnVal = {};
+  for (const name in props) {
+    if (PROP_WHITELIST.test(name) && !componentPropBlacklist.has(name)) {
+      returnVal[name] = props[name];
+    }
+  }
+
+  return returnVal;
 };
