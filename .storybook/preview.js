059bcbac5eb9081557ba6830712f5161421e6715
Marcus Notheis <marcus.notheis@sap.com>
2020-07-16 13:02:47 +0200

    docs(i18n): document how i18n works

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -15,13 +15,6 @@ import React, { useEffect } from 'react';
 import 'react-app-polyfill/ie11';
 
 addParameters({
-  options: {
-    storySort: (a, b) => {
-      return a[1].kind === b[1].kind
-        ? 0
-        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true, caseFirst: 'upper' });
-    }
-  },
   passArgsFirst: true,
   viewMode: 'docs',
   docs: { forceExtractedArgTypes: true },
