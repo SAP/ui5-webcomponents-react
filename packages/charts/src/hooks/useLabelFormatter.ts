8b2c1d0f5cfd26cf91aba61a9f367dac78809231
Harbarth, Lukas <lukas.harbarth@sap.com>
2020-07-22 12:11:06 +0200

    fix(Charts): tooltip respects dimension formatter

---
+++ packages/charts/src/hooks/useLabelFormatter.ts
@@ -0,0 +1,12 @@
+import { useCallback } from 'react';
+
+export const useLabelFormatter = (primaryDimension) => {
+  return useCallback(
+    (name) => {
+      if (primaryDimension && typeof primaryDimension?.formatter === 'function') {
+        return primaryDimension.formatter(name);
+      }
+    },
+    [primaryDimension]
+  );
+};
