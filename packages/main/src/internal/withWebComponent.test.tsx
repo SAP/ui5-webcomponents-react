1a0c7fe20e400a44b4e270944fb48c7006b88237
Marcus Notheis <marcus.notheis@sap.com>
2020-07-30 12:11:28 +0200

    simplify withWebComponent

---
+++ packages/main/src/internal/withWebComponent.test.tsx
@@ -0,0 +1,70 @@
+import { render, fireEvent, screen } from '@shared/tests';
+import { Button } from '@ui5/webcomponents-react/lib/Button';
+import { Dialog } from '@ui5/webcomponents-react/lib/Dialog';
+import React from 'react';
+
+describe('withWebComponent', () => {
+  test('Unmount Event Handlers correctly after prop update', () => {
+    const callback = jest.fn();
+
+    const { rerender, unmount } = render(<Button onClick={callback}>Click Me</Button>);
+    fireEvent.click(screen.getByText('Click Me'));
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    const newCallback = jest.fn();
+    rerender(<Button onClick={newCallback}>Click Me</Button>);
+    fireEvent.click(screen.getByText('Click Me'));
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(newCallback).toHaveBeenCalledTimes(1);
+
+    fireEvent.click(screen.getByText('Click Me'));
+    expect(newCallback).toHaveBeenCalledTimes(2);
+
+    rerender(<Button>Click Me</Button>);
+    fireEvent.click(screen.getByText('Click Me'));
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(newCallback).toHaveBeenCalledTimes(2);
+
+    unmount();
+  });
+
+  test('Slots accept both Fragment and regular nodes', () => {
+    const callbackFooter1 = jest.fn();
+    const callbackFooter2 = jest.fn();
+
+    const { rerender } = render(
+      <Dialog footer={<Button onClick={callbackFooter1}>Footer Button 1</Button>}>My Dialog Content</Dialog>
+    );
+
+    fireEvent.click(screen.getByText('Footer Button 1'));
+    expect(callbackFooter1).toHaveBeenCalledTimes(1);
+
+    rerender(
+      <Dialog
+        footer={
+          <>
+            <Button onClick={callbackFooter1}>Footer Button 1</Button>
+            <Button onClick={callbackFooter2}>Footer Button 2</Button>
+          </>
+        }
+      >
+        My Dialog Content
+      </Dialog>
+    );
+
+    fireEvent.click(screen.getByText('Footer Button 1'));
+    expect(callbackFooter1).toHaveBeenCalledTimes(2);
+
+    fireEvent.click(screen.getByText('Footer Button 2'));
+    expect(callbackFooter2).toHaveBeenCalledTimes(1);
+  });
+
+  test('correct handling of boolean props', () => {
+    const { rerender } = render(<Button disabled>Click Me</Button>);
+
+    expect(screen.getByText('Click Me')).toHaveAttribute('disabled');
+
+    rerender(<Button>Click Me</Button>);
+    expect(screen.getByText('Click Me')).not.toHaveAttribute('disabled');
+  });
+});
--- packages/main/src/internal/withWebComponent.tsx
+++ packages/main/src/internal/withWebComponent.tsx
@@ -9,7 +9,6 @@ import React, {
   Ref,
   RefObject,
   useEffect,
-  useMemo,
   useRef
 } from 'react';
 import { CommonProps } from '../interfaces/CommonProps';
@@ -17,8 +16,10 @@ import { Ui5DomRef } from '../interfaces/Ui5DomRef';
 
 const capitalizeFirstLetter = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
-const toKebabCase = (s: string) => s.replace(/([A-Z])/g, (a, b: string) => `-${b.toLowerCase()}`);
-const snakeToCamel = (str: string) => str.replace(/([-_]\w)/g, (g) => g[1].toUpperCase());
+const camelToKebabCase = (s: string) => s.replace(/([A-Z])/g, (a, b: string) => `-${b.toLowerCase()}`);
+const kebabToCamelCase = (str: string) => str.replace(/([-_]\w)/g, (g) => g[1].toUpperCase());
+
+const createEventPropName = (eventName) => `on${capitalizeFirstLetter(kebabToCamelCase(eventName))}`;
 
 type EventHandler = (event: CustomEvent<unknown>) => void;
 
@@ -41,66 +42,58 @@ export const withWebComponent = <T extends Record<string, unknown>>(
     const eventRegistry = useRef<Record<string, EventHandler>>({});
 
     // regular props (no booleans, no slots and no events)
-    const regularProps = useMemo(
-      () => {
-        return regularProperties.reduce((acc, val) => {
-          if (rest.hasOwnProperty(val)) {
-            return { ...acc, [toKebabCase(val)]: rest[val] };
-          }
-          return acc;
-        }, {});
-      },
-      regularProperties.map((name) => rest[name])
-    );
+    const regularProps = regularProperties.reduce((acc, name) => {
+      if (rest.hasOwnProperty(name)) {
+        return { ...acc, [camelToKebabCase(name)]: rest[name] };
+      }
+      return acc;
+    }, {});
 
     // boolean properties - only attach if they are truthy
-    const booleanProps = useMemo(
-      () => {
-        return booleanProperties.reduce((acc, val) => {
-          if (rest[val] === true || rest[val] === 'true') {
-            return { ...acc, [toKebabCase(val)]: true };
-          }
-          return acc;
-        }, {});
-      },
-      booleanProperties.map((name) => rest[name])
-    );
+    const booleanProps = booleanProperties.reduce((acc, name) => {
+      if (rest[name] === true || rest[name] === 'true') {
+        return { ...acc, [camelToKebabCase(name)]: true };
+      }
+      return acc;
+    }, {});
 
-    const slots = useMemo(
-      () => {
-        return Object.entries(rest)
-          .filter(([slotName]) => slotProperties.includes(slotName))
-          .map(([slotName, slotValue]) => {
-            return Children.map(
-              slotValue?.type === React.Fragment ? slotValue.props.children : slotValue,
-              (item: ReactElement, index) =>
-                cloneElement(item, {
-                  key: `${slotName}-${index}`,
-                  slot: slotName
-                })
-            );
-          });
-      },
-      slotProperties.map((name) => rest[name])
-    );
+    const slots = slotProperties.reduce((acc, name) => {
+      const slotValue = rest[name] as ReactElement;
+      if (slotValue) {
+        return [
+          ...acc,
+          ...Children.map(
+            slotValue?.type === React.Fragment ? slotValue.props?.children : slotValue,
+            (item: ReactElement, index) =>
+              cloneElement(item, {
+                key: `${name}-${index}`,
+                slot: name
+              })
+          )
+        ];
+      }
+      return acc;
+    }, []);
 
     // event binding
     useEffect(
       () => {
         eventProperties.forEach((eventName) => {
-          const eventHandler = rest[`on${capitalizeFirstLetter(snakeToCamel(eventName))}`] as EventHandler;
-          if (typeof eventHandler === 'function' && eventRegistry.current[eventName] !== eventHandler) {
-            if (eventRegistry.current[eventName]) {
-              ref.current.removeEventListener(eventName, eventRegistry.current[eventName]);
-            }
+          const eventHandler = rest[createEventPropName(eventName)] as EventHandler;
+          if (typeof eventHandler === 'function') {
             eventRegistry.current[eventName] = eventHandler;
             ref.current.addEventListener(eventName, eventRegistry.current[eventName]);
-          } else if (eventRegistry.current[eventName] && !eventHandler) {
-            ref.current.removeEventListener(eventName, eventRegistry.current[eventName]);
           }
         });
+
+        return () => {
+          // eslint-disable-next-line guard-for-in
+          for (const eventName in eventRegistry.current) {
+            ref.current?.removeEventListener(eventName, eventRegistry.current[eventName]);
+          }
+        };
       },
-      eventProperties.map((eventName) => rest[`on${capitalizeFirstLetter(eventName)}`])
+      eventProperties.map((eventName) => rest[createEventPropName(eventName)])
     );
 
     // non web component related props, just pass them
@@ -108,7 +101,7 @@ export const withWebComponent = <T extends Record<string, unknown>>(
       .filter(([key]) => !regularProperties.includes(key))
       .filter(([key]) => !slotProperties.includes(key))
       .filter(([key]) => !booleanProperties.includes(key))
-      .filter(([key]) => !eventProperties.map((val) => `on${capitalizeFirstLetter(val)}`).includes(key))
+      .filter(([key]) => !eventProperties.map((eventName) => createEventPropName(eventName)).includes(key))
       .reduce((acc, [key, val]) => ({ ...acc, [key]: val }), {});
 
     return (
