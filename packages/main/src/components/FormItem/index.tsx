3109a28c1ff709634e2976eba51afcce635e018f
Marcus Notheis <marcus.notheis@sap.com>
2020-08-21 16:11:29 +0200

    fix(Form): fix tab order
    Closes #645

--- packages/main/src/components/FormItem/index.tsx
+++ packages/main/src/components/FormItem/index.tsx
@@ -13,6 +13,12 @@ export interface FormItemProps {
   children: ReactNode | ReactNodeArray;
 }
 
+interface InternalProps extends FormItemProps {
+  columnIndex?: number;
+  labelSpan?: number;
+  rowIndex?: number;
+}
+
 const useStyles = createComponentStyles(
   {
     label: {
@@ -28,17 +34,20 @@ const useStyles = createComponentStyles(
 );
 
 const FormItem: FC<FormItemProps> = (props: FormItemProps) => {
-  const { label, children } = props;
+  const { label, children, columnIndex, rowIndex, labelSpan } = props as InternalProps;
 
   const classes = useStyles();
 
-  // @ts-ignore
-  const gridColumnStart = (props.columnIndex ?? 0) * 12 + 1;
+  const gridColumnStart = (columnIndex ?? 0) * 12 + 1;
+  const gridRowStart = rowIndex ?? 0;
+
+  const contentGridColumnStart =
+    columnIndex != null ? (labelSpan === 12 ? gridColumnStart : gridColumnStart + (labelSpan ?? 0)) : undefined;
 
   return (
     <>
       {typeof label === 'string' && (
-        <Label className={classes.label} style={{ gridColumnStart }} wrap>
+        <Label className={classes.label} style={{ gridColumnStart, gridRowStart }} wrap>
           {label ? `${label}:` : ''}
         </Label>
       )}
@@ -48,12 +57,14 @@ const FormItem: FC<FormItemProps> = (props: FormItemProps) => {
           {
             wrap: label.props.wrap ?? true,
             className: `${classes.label} ${label.props.className ?? ''}`,
-            style: { gridColumnStart, ...(label.props.style || {}) }
+            style: { gridColumnStart, gridRowStart, ...(label.props.style || {}) }
           },
           label.props.children ? `${label.props.children}:` : ''
         )}
 
-      <div className={classes.content}>{children}</div>
+      <div className={classes.content} style={{ gridColumnStart: contentGridColumnStart, gridRowStart }}>
+        {children}
+      </div>
     </>
   );
 };
