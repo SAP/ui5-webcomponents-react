aea156614989c75c9e5f7b1dc55beafc9cdb28cb
Luis Valgoi <luis.valgoi@sap.com>
2020-08-25 15:41:20 -0300

    feat: added assets imports

--- packages/ui5-webcomponents-react-seed/src/index.js
+++ packages/ui5-webcomponents-react-seed/src/index.js
@@ -1,3 +1,16 @@
+
+// browser support
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import '@ui5/webcomponents-base/dist/features/browsersupport/IE11.js';
+import '@ui5/webcomponents-react-base/polyfill/IE11';
+import '@ui5/webcomponents-icons/dist/json-imports/Icons.js';
+
+import '@ui5/webcomponents-icons/dist/Assets.js';
+import "@ui5/webcomponents-react/dist/Assets.js";
+import "@ui5/webcomponents-fiori/dist/Assets.js";
+import "@ui5/webcomponents/dist/Assets.js";
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
