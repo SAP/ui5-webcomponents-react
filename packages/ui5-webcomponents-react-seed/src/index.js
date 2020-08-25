037515bb180d95ccec5b208d780cc835ad5112e1
Luis Valgoi <luis.valgoi@sap.com>
2020-08-25 16:16:29 -0300

    feat: added coverage script

--- packages/ui5-webcomponents-react-seed/src/index.js
+++ packages/ui5-webcomponents-react-seed/src/index.js
@@ -6,10 +6,9 @@ import '@ui5/webcomponents-base/dist/features/browsersupport/IE11.js';
 import '@ui5/webcomponents-react-base/polyfill/IE11';
 import '@ui5/webcomponents-icons/dist/json-imports/Icons.js';
 
-import '@ui5/webcomponents-icons/dist/Assets.js';
-import "@ui5/webcomponents-react/dist/Assets.js";
-import "@ui5/webcomponents-fiori/dist/Assets.js";
-import "@ui5/webcomponents/dist/Assets.js";
+import '@ui5/webcomponents/dist/Assets.js';
+import '@ui5/webcomponents-fiori/dist/Assets.js';
+import '@ui5/webcomponents-react/dist/Assets';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
