2d374c64306ad4432c90e12d4c39dc7a1019859f
Luis Valgoi <luis.valgoi@sap.com>
2020-08-25 13:26:21 -0300

    Initialize project using Create React App

---
+++ packages/ui5-webcomponents-react-seed/src/App.js
@@ -0,0 +1,32 @@
+import {
+  FlexBox,
+  FlexBoxAlignItems,
+  FlexBoxDirection,
+  FlexBoxJustifyContent,
+  Link,
+  LinkDesign,
+  ShellBar,
+  ThemeProvider
+} from '@ui5/webcomponents-react';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <ShellBar primaryTitle="UI5 Web Components for React Template" />
+      <FlexBox
+        style={{ width: '100%', height: '100vh' }}
+        direction={FlexBoxDirection.Column}
+        justifyContent={FlexBoxJustifyContent.Center}
+        alignItems={FlexBoxAlignItems.Center}
+      >
+        <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
+          Getting Started with UI5 Web Component for React
+        </Link>
+      </FlexBox>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ packages/ui5-webcomponents-react-seed/src/App.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Getting Started with UI5 Web Component for React/i);
+  expect(linkElement).toBeInTheDocument();
+});
