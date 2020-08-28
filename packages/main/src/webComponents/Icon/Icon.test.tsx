845a77a64b519ceacba286b7a00129370dc1c529
Marcus Notheis <marcus.notheis@sap.com>
2020-08-28 16:51:36 +0200

    fix tests

--- packages/main/src/webComponents/Icon/Icon.test.tsx
+++ packages/main/src/webComponents/Icon/Icon.test.tsx
@@ -1,10 +1,11 @@
+import '@ui5/webcomponents-icons/dist/icons/add';
 import { render } from '@shared/tests';
 import { Icon } from '@ui5/webcomponents-react/lib/Icon';
 import React from 'react';
 
 describe('Icon', () => {
   test('Basic Test (generated)', () => {
-    const { asFragment } = render(<Icon />);
+    const { asFragment } = render(<Icon name="add" />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- packages/main/src/webComponents/SegmentedButton/SegmentedButton.test.tsx
+++ packages/main/src/webComponents/SegmentedButton/SegmentedButton.test.tsx
@@ -1,10 +1,17 @@
 import { render } from '@shared/tests';
+import { Button } from '@ui5/webcomponents-react/lib/Button';
 import { SegmentedButton } from '@ui5/webcomponents-react/lib/SegmentedButton';
 import React from 'react';
 
 describe('SegmentedButton', () => {
   test('Basic Test (generated)', () => {
-    const { asFragment } = render(<SegmentedButton />);
+    const { asFragment } = render(
+      <SegmentedButton>
+        <Button>Segment 1</Button>
+        <Button>Segment 2</Button>
+        <Button>Segment 3</Button>
+      </SegmentedButton>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
