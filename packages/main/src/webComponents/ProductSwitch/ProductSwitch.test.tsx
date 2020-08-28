bd3cc1b6b5c5d89ed0654f930198a24af509b8b1
Marcus Notheis <marcus.notheis@sap.com>
2020-08-28 14:54:53 +0200

    chore: update snapshots

--- packages/main/src/webComponents/ProductSwitch/ProductSwitch.test.tsx
+++ packages/main/src/webComponents/ProductSwitch/ProductSwitch.test.tsx
@@ -1,10 +1,15 @@
 import { render } from '@shared/tests';
 import { ProductSwitch } from '@ui5/webcomponents-react/lib/ProductSwitch';
+import { ProductSwitchItem } from '@ui5/webcomponents-react/lib/ProductSwitchItem';
 import React from 'react';
 
 describe('ProductSwitch', () => {
   test('Basic Test (generated)', () => {
-    const { asFragment } = render(<ProductSwitch />);
+    const { asFragment } = render(
+      <ProductSwitch>
+        <ProductSwitchItem heading="Product Switch Item" />
+      </ProductSwitch>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
