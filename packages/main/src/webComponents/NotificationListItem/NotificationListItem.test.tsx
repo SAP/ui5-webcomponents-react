a5d5b46375070f4f83922306c6080f7397d891a2
Marcus Notheis <marcus.notheis@sap.com>
2020-08-30 09:45:53 +0200

    update NotificationListItem test

--- packages/main/src/webComponents/NotificationListItem/NotificationListItem.test.tsx
+++ packages/main/src/webComponents/NotificationListItem/NotificationListItem.test.tsx
@@ -1,10 +1,19 @@
 import { render } from '@shared/tests';
 import { NotificationListItem } from '@ui5/webcomponents-react/lib/NotificationListItem';
+import { NotificationOverflowAction } from '@ui5/webcomponents-react/lib/NotificationOverflowAction';
+import { Priority } from '@ui5/webcomponents-react/lib/Priority';
 import React from 'react';
 
 describe('NotificationListItem', () => {
   test('Basic Test (generated)', () => {
-    const { asFragment } = render(<NotificationListItem />);
+    const { asFragment } = render(
+      <NotificationListItem
+        heading="My Notification"
+        showClose
+        priority={Priority.High}
+        actions={<NotificationOverflowAction text="My Action" />}
+      />
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
