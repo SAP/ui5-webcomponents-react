b84108e2c56a48fab3171f7680a5a05bd64577c1
Marcus Notheis <marcus.notheis@sap.com>
2020-09-02 09:36:48 +0200

    ThemeProvider: useIsomorphicLayoutEffect

--- packages/main/src/components/ThemeProvider/index.tsx
+++ packages/main/src/components/ThemeProvider/index.tsx
@@ -3,8 +3,9 @@ import { getTheme } from '@ui5/webcomponents-base/dist/config/Theme';
 import { createComponentStyles } from '@ui5/webcomponents-react-base/lib/createComponentStyles';
 import { cssVariablesStyles } from '@ui5/webcomponents-react-base/lib/CssSizeVariables';
 import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
+import { useIsomorphicLayoutEffect } from '@ui5/webcomponents-react-base/lib/useIsomorphicLayoutEffect';
 import { ContentDensity } from '@ui5/webcomponents-react/lib/ContentDensity';
-import React, { FC, ReactNode, useEffect, useMemo } from 'react';
+import React, { FC, ReactNode, useMemo } from 'react';
 import { ThemeProvider as ReactJssThemeProvider } from 'react-jss';
 import { JSSTheme } from '../../interfaces/JSSTheme';
 import { GlobalStyleClassesStyles } from './GlobalStyleClasses.jss';
@@ -17,18 +18,28 @@ export interface ThemeProviderProps {
   children: ReactNode;
 }
 
-// inject the size variables first before the ThemeProvider Component is mounted, otherwise there will be some flickering
-if (!document.querySelector('style[data-ui5-webcomponents-react-sizes]')) {
-  const variables = document.createElement('style');
-  variables.setAttribute('data-ui5-webcomponents-react-sizes', '');
-  variables.innerHTML = cssVariablesStyles;
-  document.head.appendChild(variables);
-}
-
 const ThemeProvider: FC<ThemeProviderProps> = (props: ThemeProviderProps) => {
   const { children } = props;
   const isCompactSize = document.body.classList.contains('ui5-content-density-compact');
   useStyles();
+
+  useIsomorphicLayoutEffect(() => {
+    if (!document.querySelector('style[data-ui5-webcomponents-react-sizes]')) {
+      const variables = document.createElement('style');
+      variables.setAttribute('data-ui5-webcomponents-react-sizes', '');
+      variables.innerHTML = cssVariablesStyles;
+      document.head.appendChild(variables);
+    }
+
+    if (cssVarsPonyfillNeeded()) {
+      window.CSSVarsPonyfill.cssVars({
+        rootElement: document.head,
+        include: 'style[data-ui5-webcomponents-react-sizes],style[data-jss]',
+        watch: true,
+        silent: true
+      });
+    }
+  }, []);
 
   const theme = getTheme();
 
@@ -40,17 +51,6 @@ const ThemeProvider: FC<ThemeProviderProps> = (props: ThemeProviderProps) => {
       rtl: getRTL()
     };
   }, [theme, isCompactSize]);
-
-  useEffect(() => {
-    if (cssVarsPonyfillNeeded()) {
-      window.CSSVarsPonyfill.cssVars({
-        rootElement: document.head,
-        include: 'style[data-ui5-webcomponents-react-sizes],style[data-jss]',
-        watch: true,
-        silent: true
-      });
-    }
-  }, []);
 
   return <ReactJssThemeProvider theme={themeContext}>{children}</ReactJssThemeProvider>;
 };
