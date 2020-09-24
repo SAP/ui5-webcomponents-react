bcb187f2e23848a0f896ae54c40f5263e770b9ce
Marcus Notheis <marcus.notheis@sap.com>
2020-09-24 17:01:00 +0200

    refactor(VariantManagement): use mdx for docs and RTL for tests (#687)
    Co-authored-by: Harbarth, Lukas <lukas.harbarth@sap.com>

--- packages/main/src/components/VariantManagement/VariantManagement.test.tsx
+++ packages/main/src/components/VariantManagement/VariantManagement.test.tsx
@@ -1,9 +1,7 @@
-import { createPassThroughPropsTest, getEventFromCallback } from '@shared/tests/utils';
-import { mount } from 'enzyme';
+import { fireEvent, render, screen, waitFor, getByText } from '@shared/tests';
+import { createPassThroughPropsTest } from '@shared/tests/utils';
 import { VariantManagement } from '@ui5/webcomponents-react/lib/VariantManagement';
 import React from 'react';
-import sinon from 'sinon';
-import { act } from 'react-dom/test-utils';
 
 const variantItems = [
   { label: 'Variant 1', key: '1' },
@@ -12,29 +10,30 @@ const variantItems = [
 
 describe('VariantManagement', () => {
   test('Render without crashing', () => {
-    const wrapper = mount(<VariantManagement variantItems={variantItems} selectedKey="1" />);
-    expect(wrapper.render()).toMatchSnapshot();
+    const wrapper = render(<VariantManagement variantItems={variantItems} selectedKey="1" />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
   test('Render without crashing - disabled', () => {
-    const wrapper = mount(<VariantManagement variantItems={variantItems} disabled />);
-    expect(wrapper.render()).toMatchSnapshot();
+    const wrapper = render(<VariantManagement variantItems={variantItems} disabled />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
 
-  test.skip('With suggestions', () => {
-    const callback = sinon.spy();
-    const wrapper = mount(<VariantManagement onSelect={callback} variantItems={variantItems} />, {
-      attachTo: document.body.appendChild(document.createElement('div'))
+  test.skip('With suggestions', async () => {
+    const callback = jest.fn();
+    const wrapper = render(<VariantManagement onSelect={callback} variantItems={variantItems} />);
+
+    fireEvent.click(wrapper.container.querySelector('ui5-button[icon="navigation-down-arrow"]'));
+
+    // wait for appearance
+    await waitFor(() => {
+      expect(screen.getByText(variantItems[1].label)).toBeVisible();
     });
 
-    wrapper.find('ui5-button').first().instance().fireEvent('click');
+    fireEvent.click(screen.getByText(variantItems[1].label));
 
-    act(() => {
-      wrapper.find('ui5-li').last().instance().fireItemPress({});
-    });
-
-    expect(wrapper.render()).toMatchSnapshot();
-    expect(getEventFromCallback(callback).detail.selectedItem).toEqual(variantItems[1]);
+    expect(wrapper.asFragment()).toMatchSnapshot();
+    expect(callback.mock.calls[0][0].detail.selectedItem).toEqual(variantItems[1]);
     wrapper.unmount();
   });
 
