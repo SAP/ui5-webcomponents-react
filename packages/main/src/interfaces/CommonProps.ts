d3dd3848d32ff9aca1de9580897a516e6682628b
Marcus Notheis <marcus.notheis@sap.com>
2020-09-25 14:13:14 +0200

    chore: remove HTMLAttributes from Web Components

--- packages/main/src/interfaces/CommonProps.ts
+++ packages/main/src/interfaces/CommonProps.ts
@@ -1,6 +1,6 @@
 import { CSSProperties, Ref, HTMLAttributes } from 'react';
 
-export interface CommonProps extends HTMLAttributes<HTMLDivElement> {
+export interface CommonProps extends HTMLAttributes<HTMLElement> {
   /**
    * Element style which will be appended to the most outer element of a component.
    * Use this prop carefully, some css properties might break the component.
