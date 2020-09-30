e16396030bb33f1eb2329c70eb3f1cb02f9e63ea
Marcus Notheis <marcus.notheis@sap.com>
2020-09-30 08:48:13 +0200

    refactor(Device): replace modernizr with custom implementation

--- .storybook/main.js
+++ .storybook/main.js
@@ -81,7 +81,6 @@ module.exports = {
       '@ui5/webcomponents-react/dist': path.join(PATHS.root, 'packages', 'main', 'dist'),
       '@ui5/webcomponents-react': path.join(PATHS.root, 'packages', 'main', 'src'),
       '@ui5/webcomponents-react-charts': path.join(PATHS.root, 'packages', 'charts', 'src'),
-      '@ui5/webcomponents-react-base/third-party': path.join(PATHS.root, 'packages', 'base', 'third-party'),
       '@ui5/webcomponents-react-base': path.join(PATHS.root, 'packages', 'base', 'src')
     };
 
--- packages/base/src/Device/Support.ts
+++ packages/base/src/Device/Support.ts
@@ -1,23 +1,29 @@
 import { supportTouch } from '@ui5/webcomponents-base/dist/Device';
-import '@ui5/webcomponents-react-base/third-party/modernizr';
 
-declare const Modernizr: {
-  pointerevents: boolean;
-  matchmedia: boolean;
-  deviceorientation: boolean;
-  websockets: boolean;
-  placeholder: boolean;
+const testIfBrowserSupportsPlaceholders = () => {
+  return 'placeholder' in document.createElement('input') && 'placeholder' in document.createElement('textarea');
+};
+
+const testIfBrowserSupportsWebSockets = () => {
+  let isSupported = false;
+  try {
+    isSupported = 'WebSocket' in window && window.WebSocket.CLOSING === 2;
+  } catch (e) {}
+  return isSupported;
 };
 
 export class Support {
   public touch: boolean = supportTouch();
-  public pointer: boolean = Modernizr.pointerevents;
-  public matchmedia: boolean = Modernizr.matchmedia;
+  // prettier-ignore
+  public pointer: boolean = ('onpointerdown' in window);
+  // prettier-ignore
+  public matchmedia = ('matchMedia' in window);
   public matchmedialistener = !!(this.matchmedia && window.matchMedia('all and (max-width:0px)'));
-  public orientation: boolean = Modernizr.deviceorientation;
+  // prettier-ignore
+  public orientation = ('DeviceOrientationEvent' in window);
   public retina: boolean = (window as any).retina || window.devicePixelRatio >= 2;
-  public websocket: boolean = Modernizr.websockets;
+  public websocket: boolean = testIfBrowserSupportsWebSockets();
   public input = {
-    placeholder: Modernizr.placeholder
+    placeholder: testIfBrowserSupportsPlaceholders()
   };
 }
