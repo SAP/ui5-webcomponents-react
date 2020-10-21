2b2d01fabd98f5404380dd40622c0661ce8bace3
Luis Valgoi <luis.valgoi@sap.com>
2020-10-21 14:26:04 -0300

    refactor: fixed mixture of imports

--- packages/cra-template-seed/template/src/components/Popover/Info/PopoverInfo.js
+++ packages/cra-template-seed/template/src/components/Popover/Info/PopoverInfo.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { Popover } from '@ui5/webcomponents-react/lib/Popover';
 import { PlacementType } from '@ui5/webcomponents-react/lib/PlacementType';
-import { FlexBoxDirection, Title } from '@ui5/webcomponents-react';
+import { FlexBoxDirection } from '@ui5/webcomponents-react/lib/FlexBoxDirection';
+import { Title } from '@ui5/webcomponents-react/lib/Title';
 import { FlexBox } from '@ui5/webcomponents-react/lib/FlexBox';
 import { spacing } from '@ui5/webcomponents-react-base';
 
