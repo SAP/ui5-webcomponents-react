2ece1a81f853a23a13457017463d3bf91732dc2f
Luis Valgoi <luis.valgoi@sap.com>
2020-10-21 15:44:50 -0300

    refactor: removed testid and fixed tests

--- packages/cra-template-seed/template/src/components/HyperLink/HyperLink.js
+++ packages/cra-template-seed/template/src/components/HyperLink/HyperLink.js
@@ -5,7 +5,7 @@ import { LinkDesign } from '@ui5/webcomponents-react/lib/LinkDesign';
 
 const HyperLink = ({ text, onClick }) => {
   return (
-    <Link data-testid="hyperlink-wrapper" onClick={onClick} design={LinkDesign.Emphasized}>
+    <Link onClick={onClick} design={LinkDesign.Emphasized}>
       {text}
     </Link>
   );
