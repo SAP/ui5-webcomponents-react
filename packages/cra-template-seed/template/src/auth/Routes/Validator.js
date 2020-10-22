96ada8010d2a6b3356f83651c4bf1ac595d086a3
Luis Valgoi <luis.valgoi@sap.com>
2020-10-22 12:32:46 -0300

    refactor: removed pendeable

--- packages/cra-template-seed/template/src/auth/Routes/Validator.js
+++ packages/cra-template-seed/template/src/auth/Routes/Validator.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { useHasPendableAccess } from '../../hooks/useAuthority';
+import { useHasAccess } from '../../hooks/useAuthority';
 
 import NotFound from '../../pages/Fallback/NotFound';
 import Empty from '../../pages/Fallback/Empty';
 import Constants from '../../util/Constants';
 
 const RouteValidator = ({ allowedAuthorities, authorityKey, path, component, ...props }) => {
-  const hasAccess = useHasPendableAccess(allowedAuthorities, authorityKey);
-  if (hasAccess === Constants.REACT_QUERY.CODES.LOADING) {
+  const hasAccess = useHasAccess(allowedAuthorities, authorityKey);
+  if (hasAccess === null) {
     return <Route path={path} {...props} component={Empty} />;
   }
 
--- packages/cra-template-seed/template/src/hooks/useAuthority.js
+++ packages/cra-template-seed/template/src/hooks/useAuthority.js
@@ -3,23 +3,13 @@ import Constants from '../util/Constants';
 import APIProvider from '../util/api/url/APIProvider';
 
 export function useHasAccess(allowedAuthorities, authorityKey) {
-  const { data, isSuccess } = useGet(Constants.REACT_QUERY.KEYS.RQ_GET_USER_LOGGED, APIProvider.getUrl('GET_USER_LOGGED'), null);
+  const { data, isError, isLoading } = useGet(Constants.REACT_QUERY.KEYS.RQ_GET_USER_LOGGED, APIProvider.getUrl('GET_USER_LOGGED'), null);
 
-  if (!isSuccess) {
+  if (isLoading) {
     return null;
   }
 
-  return data[authorityKey].some((permission) => allowedAuthorities.includes(permission));
-}
-
-export function useHasPendableAccess(allowedAuthorities, authorityKey) {
-  const { data, status } = useGet(Constants.REACT_QUERY.KEYS.RQ_GET_USER_LOGGED, APIProvider.getUrl('GET_USER_LOGGED'), null);
-
-  if (status === Constants.REACT_QUERY.CODES.LOADING) {
-    return status;
-  }
-
-  if (status === Constants.REACT_QUERY.CODES.ERROR) {
+  if (isError) {
     return false;
   }
 
