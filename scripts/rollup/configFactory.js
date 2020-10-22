18482a1c8639f1171d1d64b5a21079ab1d32b207
Kol Crooks <kolcrooks10@gmail.com>
2020-10-22 05:46:06 -0400

    chore: convert dev scripts to ESM modules (#720)
    [ci skip]

--- scripts/rollup/configFactory.js
+++ scripts/rollup/configFactory.js
@@ -1,16 +1,17 @@
-const { nodeResolve } = require('@rollup/plugin-node-resolve');
-const commonjs = require('@rollup/plugin-commonjs');
-const { babel } = require('@rollup/plugin-babel');
-const path = require('path');
-const fs = require('fs');
-const json = require('@rollup/plugin-json');
-const micromatch = require('micromatch');
-const PATHS = require('../../config/paths');
-const { asyncCopyTo, highlightLog } = require('../utils');
-const replace = require('@rollup/plugin-replace');
-const glob = require('glob');
-const { terser } = require('rollup-plugin-terser');
-const { spawnSync } = require('child_process');
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import Babel from '@rollup/plugin-babel';
+import path from 'path';
+import fs from 'fs';
+import json from '@rollup/plugin-json';
+import micromatch from 'micromatch';
+import PATHS from '../../config/paths.js';
+import { asyncCopyTo, highlightLog } from '../utils.js';
+import replace from '@rollup/plugin-replace';
+import glob from 'glob';
+import { terser } from 'rollup-plugin-terser';
+
+const { babel } = Babel;
 
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
@@ -21,30 +22,33 @@ const rollupConfigFactory = (pkgName, externals = []) => {
 
   const allFilesAndFolders = glob.sync(`${LIB_BASE_PATH}/**/*`);
 
-  const allLibFiles = allFilesAndFolders.filter((file) => fs.statSync(file).isFile());
-
+  const allLibFiles = allFilesAndFolders.filter((file) =>
+    fs.statSync(file).isFile()
+  );
   const plugins = [
     nodeResolve({
-      extensions: ['.mjs', '.js', '.json', '.node', '.jsx', '.ts', '.tsx']
+      extensions: ['.mjs', '.js', '.json', '.node', '.jsx', '.ts', '.tsx'],
     }),
     commonjs(),
     json(),
     babel({
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
       babelHelpers: 'runtime',
-      configFile: path.resolve(PATHS.root, 'babel.config.js')
+      configFile: path.resolve(PATHS.root, 'babel.config.cjs'),
     }),
     // Turn __DEV__ and process.env checks into constants.
     replace({
       exclude: 'node_modules/**',
       values: {
         __DEV__: 'false',
-        'process.env.NODE_ENV': "'production'"
-      }
-    })
+        'process.env.NODE_ENV': "'production'",
+      },
+    }),
   ];
 
-  const packageJson = require(path.resolve(PKG_BASE_PATH, 'package.json'));
+  const packageJson = JSON.parse(
+    fs.readFileSync(path.resolve(PKG_BASE_PATH, 'package.json'), 'utf8')
+  );
   const externalModules = Array.from(
     new Set([
       'react',
@@ -53,17 +57,23 @@ const rollupConfigFactory = (pkgName, externals = []) => {
       packageJson.name,
       ...Object.keys(packageJson.dependencies || {}),
       ...Object.keys(packageJson.peerDependencies || {}),
-      ...externals
+      ...externals,
     ])
   );
 
   highlightLog(`Build lib folder for ${pkgName}`);
 
   console.info('Copy License');
-  fs.copyFileSync(path.resolve(PATHS.root, 'LICENSE'), path.resolve(PKG_BASE_PATH, `LICENSE`));
+  fs.copyFileSync(
+    path.resolve(PATHS.root, 'LICENSE'),
+    path.resolve(PKG_BASE_PATH, `LICENSE`)
+  );
 
   console.info('Copy index file');
-  asyncCopyTo(path.resolve(PKG_BASE_PATH, 'src', 'index.ts'), path.resolve(PKG_BASE_PATH, `index.esm.js`));
+  asyncCopyTo(
+    path.resolve(PKG_BASE_PATH, 'src', 'index.ts'),
+    path.resolve(PKG_BASE_PATH, `index.esm.js`)
+  );
 
   const external = (id) => {
     const containsThisModule = (pkg) => id === pkg || id.startsWith(pkg + '/');
@@ -71,7 +81,9 @@ const rollupConfigFactory = (pkgName, externals = []) => {
   };
   const treeshake = {
     moduleSideEffects:
-      packageJson.sideEffects === false ? false : (id) => micromatch.isMatch(id, packageJson.sideEffects)
+      packageJson.sideEffects === false
+        ? false
+        : (id) => micromatch.isMatch(id, packageJson.sideEffects),
   };
   return [
     ...allLibFiles.map((file) => ({
@@ -83,13 +95,15 @@ const rollupConfigFactory = (pkgName, externals = []) => {
           file: path.resolve(
             PKG_BASE_PATH,
             'lib',
-            file.replace(`${LIB_BASE_PATH}${path.sep}`, '').replace(/\.ts$/, '.js')
+            file
+              .replace(`${LIB_BASE_PATH}${path.sep}`, '')
+              .replace(/\.ts$/, '.js')
           ),
           format: 'es',
-          sourcemap: true
-        }
+          sourcemap: true,
+        },
       ],
-      plugins
+      plugins,
     })),
     {
       input: path.resolve(PKG_BASE_PATH, 'src', 'index.ts'),
@@ -99,16 +113,20 @@ const rollupConfigFactory = (pkgName, externals = []) => {
       output: [
         {
           file: path.resolve(PKG_BASE_PATH, 'cjs', `${pkgName}.development.js`),
-          format: 'cjs'
+          format: 'cjs',
         },
         {
-          file: path.resolve(PKG_BASE_PATH, 'cjs', `${pkgName}.production.min.js`),
+          file: path.resolve(
+            PKG_BASE_PATH,
+            'cjs',
+            `${pkgName}.production.min.js`
+          ),
           format: 'cjs',
-          plugins: [terser()]
-        }
-      ]
-    }
+          plugins: [terser()],
+        },
+      ],
+    },
   ];
 };
 
-module.exports = rollupConfigFactory;
+export default rollupConfigFactory;
