a67bd2a49ae92a2df1d5e4fbfc8067448f6bf690
Luis Valgoi <luis.valgoi@sap.com>
2020-10-22 12:47:44 -0300

    refactor: removed constants

--- packages/cra-template-seed/template/src/util/Constants.js
+++ packages/cra-template-seed/template/src/util/Constants.js
@@ -1,10 +1,5 @@
 export default {
   REACT_QUERY: {
-    CODES: {
-      SUCCESS: 'success',
-      LOADING: 'loading',
-      ERROR: 'error',
-    },
     KEYS: {
       RQ_GET_USER_LOGGED: 'RQ_GET_USER_LOGGED',
       RQ_GET_TODO_LIST: 'RQ_GET_TODO_LIST',
