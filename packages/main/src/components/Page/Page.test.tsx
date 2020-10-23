fa1fd81786f17402792997c0e908ab1cbdb71501
sidram05 <38928563+sidram05@users.noreply.github.com>
2020-10-23 02:37:46 -0500

    test(Page): rewrite test using react-testing-library (#818)
    [ci skip]

--- packages/main/src/components/Page/Page.test.tsx
+++ packages/main/src/components/Page/Page.test.tsx
@@ -1,38 +1,38 @@
+import { render } from '@shared/tests';
 import { createPassThroughPropsTest } from '@shared/tests/utils';
-import { mount } from 'enzyme';
 import { Page } from '@ui5/webcomponents-react/lib/Page';
 import React from 'react';
 
 describe('Page', () => {
   test('Basic Page', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <div style={{ height: '100vh', width: '100vw' }}>
         <Page title="Page Demo" showFooter showHeader>
           Page Content
         </Page>
       </div>
     );
-    expect(wrapper.render()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Basic Page w/o back button', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <div style={{ height: '100vh', width: '100vw' }}>
         <Page title="Page Demo" showFooter showHeader showBackButton={false}>
           Page Content
         </Page>
       </div>
     );
-    expect(wrapper.render()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Without footer and Header', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <Page title="Page Demo" showFooter={false} showHeader={false}>
         Page Content
       </Page>
     );
-    expect(wrapper.render()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   createPassThroughPropsTest(Page);
