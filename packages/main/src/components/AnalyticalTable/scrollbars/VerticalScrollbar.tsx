429c1ff6670d2deca528332406547523d3d786b3
Marcus Notheis <marcus.notheis@sap.com>
2020-10-23 11:43:12 +0200

    styling update

--- packages/main/src/components/AnalyticalTable/scrollbars/VerticalScrollbar.tsx
+++ packages/main/src/components/AnalyticalTable/scrollbars/VerticalScrollbar.tsx
@@ -12,76 +12,54 @@ interface VerticalScrollbarProps {
   handleVerticalScrollBarScroll: any;
 }
 
-export const VerticalScrollbar = forwardRef(
-  (props: VerticalScrollbarProps, ref: Ref<HTMLDivElement>) => {
-    const {
-      internalRowHeight,
-      tableRef,
-      minRows,
-      rows,
-      handleVerticalScrollBarScroll,
-    } = props;
+export const VerticalScrollbar = forwardRef((props: VerticalScrollbarProps, ref: Ref<HTMLDivElement>) => {
+  const { internalRowHeight, tableRef, minRows, rows, handleVerticalScrollBarScroll } = props;
 
-    const hasHorizontalScrollbar =
-      tableRef?.current?.clientWidth !== tableRef?.current?.scrollWidth;
+  const hasHorizontalScrollbar = tableRef?.current?.clientWidth !== tableRef?.current?.scrollWidth;
 
-    return (
-      <FlexBox
-        direction={FlexBoxDirection.Column}
-        style={{ position: 'relative' }}
+  return (
+    <FlexBox direction={FlexBoxDirection.Column} style={{ position: 'relative' }}>
+      <div
+        style={{
+          height: `${internalRowHeight}px`,
+          boxSizing: 'border-box',
+          borderTop: `1px solid ${ThemingParameters.sapList_BorderColor}`,
+          borderRight: `1px solid ${ThemingParameters.sapList_BorderColor}`,
+          borderBottom: `1px solid ${ThemingParameters.sapList_BorderColor}`,
+          backgroundColor: ThemingParameters.sapList_HeaderBackground
+        }}
+      />
+      <div
+        ref={ref}
+        style={{
+          overflowY: 'auto',
+          height: tableRef.current ? `${tableRef.current.clientHeight - internalRowHeight}px` : '0',
+          borderRight: `1px solid ${ThemingParameters.sapList_BorderColor}`,
+          borderBottom: `1px solid ${ThemingParameters.sapList_BorderColor}`,
+          boxSizing: 'border-box'
+        }}
+        onScroll={handleVerticalScrollBarScroll}
+        className={GlobalStyleClasses.sapScrollBar}
       >
         <div
           style={{
-            height: `${internalRowHeight}px`,
-            boxSizing: 'border-box',
-            borderWidth: '1px',
-            borderTopStyle: 'solid',
-            borderBottomStyle: 'solid',
-            borderRightStyle: 'solid',
-            borderLeftStyle: 'none',
-            borderColor: ThemingParameters.sapList_BorderColor,
-            backgroundColor: ThemingParameters.sapList_HeaderBackground,
+            height: `${Math.max(minRows, rows.length) * internalRowHeight}px`,
+            width: '1px'
           }}
         />
-        <div
-          ref={ref}
-          style={{
-            overflowY: 'auto',
-            height: tableRef.current
-              ? `${tableRef.current.clientHeight - internalRowHeight}px`
-              : '0',
-            borderWidth: '1px',
-            borderRightStyle: 'solid',
-            borderBottomStyle: 'solid',
-            borderTopStyle: 'none',
-            borderLeftStyle: 'none',
-            borderColor: ThemingParameters.sapList_BorderColor,
-            boxSizing: 'border-box',
-          }}
-          onScroll={handleVerticalScrollBarScroll}
-          className={GlobalStyleClasses.sapScrollBar}
-        >
-          <div
-            style={{
-              height: `${Math.max(minRows, rows.length) * internalRowHeight}px`,
-              width: '1px',
-            }}
-          />
-        </div>
-        <div
-          style={{
-            flexGrow: 1,
-            backgroundColor: ThemingParameters.sapList_HeaderBackground,
-            borderWidth: '1px',
-            borderRightStyle: hasHorizontalScrollbar ? 'solid' : 'none',
-            borderBottomStyle: hasHorizontalScrollbar ? 'solid' : 'none',
-            borderTopStyle: 'none',
-            borderLeftStyle: 'none',
-            borderColor: ThemingParameters.sapList_BorderColor,
-            boxSizing: 'border-box',
-          }}
-        />
-      </FlexBox>
-    );
-  }
-);
+      </div>
+      <div
+        style={{
+          flexGrow: 1,
+          backgroundColor: ThemingParameters.sapList_HeaderBackground,
+          borderWidth: '1px',
+          borderRight: hasHorizontalScrollbar ? `1px solid ${ThemingParameters.sapList_BorderColor}` : 'none',
+          borderBottom: hasHorizontalScrollbar ? `1px solid ${ThemingParameters.sapList_BorderColor}` : 'none',
+          boxSizing: 'border-box'
+        }}
+      />
+    </FlexBox>
+  );
+});
+
+VerticalScrollbar.displayName = 'VerticalScrollbar';
