16993f22284642e1d715993428f3ba56b044ec5b
Luis Valgoi <luis.valgoi@sap.com>
2020-10-23 11:28:41 -0300

    refactor: buggy

--- packages/cra-template-seed/template/src/pages/Fallback/Buggy.js
+++ packages/cra-template-seed/template/src/pages/Fallback/Buggy.js
@@ -1,15 +1,17 @@
+// WARNING: This is only for demonstration. Do not use it directly in your application. */
+
 import React from 'react';
 
 /* eslint-disable no-unused-vars */
 class Buggy extends React.Component {
-  state = { greeting: 'Welcome' };
   componentDidMount() {
     throw new Error('An error has occured in Buggy component!');
   }
+
   render() {
     return (
       <>
-        <h2>{this.state.greeting}</h2>
+        <h2>Hey, I'm buggy</h2>
       </>
     );
   }
