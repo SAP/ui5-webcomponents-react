062c08acfe09a6475ef27ae86dc38c9787ba83a3
Luis Valgoi <luis.valgoi@sap.com>
2020-10-27 15:55:55 -0300

    refactor: removed hyperlink component

--- packages/cra-template-seed/template/src/pages/Todo/List/TodoList.js
+++ packages/cra-template-seed/template/src/pages/Todo/List/TodoList.js
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 import { useHistory } from 'react-router-dom';
 import { isChrome, isMobile, isTablet, isDesktop, isIE } from '@ui5/webcomponents-base/dist/Device';
 
-import HyperLink from '../../../components/HyperLink/HyperLink';
+import { Link } from '@ui5/webcomponents-react/lib/Link';
 import BrowserProvider from '../../../util/browser/BrowserProvider';
 import ComponentWithAuthorizationRestriction from '../../../auth/ComponentWithAuthorizationRestriction';
 import TodoListPaginatedItems from './TodoListPaginatedItems';
@@ -19,25 +19,21 @@ const TodoList = () => {
        App"
       />
       <h1>Routing</h1>
-      <HyperLink onClick={() => history.push('/dontexist')} text="Test NotFound Page" />
+      <Link onClick={() => history.push('/dontexist')}>Test NotFound Page</Link>
       <br />
-      <HyperLink onClick={() => history.push(BrowserProvider.getUrl('BUGGY'))} text="Test Error Page" />
+      <Link onClick={() => history.push(BrowserProvider.getUrl('BUGGY'))}>Test Error Page</Link>
       <br />
-
       <ComponentWithAuthorizationRestriction allowedAuthorities={['canAccessDropApplication']} authorityKey="permissions">
         <h1>Component Validator</h1>
         <p>Drop Application (this is a restricted text and you should not see unless you have access)</p>
       </ComponentWithAuthorizationRestriction>
-
       <h1>Device Detect</h1>
       <p>{isMobile() ? 'This text appears when is MOBILE' : 'This text appears when is not MOBILE'}</p>
       <p>{isTablet() ? 'This text appears when is TABLET' : 'This text appears when is not TABLET'}</p>
       <p>{isDesktop() ? 'This text appears when is DESKTOP' : 'This text appears when is not DESKTOP'}</p>
-
       <h1>Browser Detect</h1>
       <p>{isChrome() ? 'This Text is rendered only for CHROME' : 'This Text is rendered only when is NOT CHROME'}</p>
       <p>{isIE() ? 'This Text is rendered only for IE' : 'This Text is rendered only when is NOT IE'}</p>
-
       <h1>Pagination + Edition (Formik and Yup)</h1>
       <TodoListPaginatedItems history={history} />
     </CenteredContent>
