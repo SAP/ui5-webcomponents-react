7aab1bb8157bd8119e3b540a3e18ef12245dc45b
Harbarth, Lukas <lukas.harbarth@sap.com>
2020-10-29 09:43:14 +0100

    docs(Token): enhance story

--- packages/main/src/webComponents/Token/index.tsx
+++ packages/main/src/webComponents/Token/index.tsx
@@ -22,12 +22,13 @@ export interface TokenPropTypes extends Omit<WithWebComponentPropTypes, 'onSelec
   /**
    * Fired when the a <code>Token</code> is selected by user interaction with mouse or clicking space.
    */
-  onSelect?: (event: CustomEvent<{}>) => void;
+  onSelect?: (event: CustomEvent) => void;
 }
 
 /**
- * Tokens are small items of information (similar to tags) that mainly serve to visualize previously selected items.
-
+ * Tokens are small items of information (similar to tags) that mainly serve to visualize previously selected items.<br />
+ * __Note:__ Tokens shouldn't be implemented as a standalone component. Use the `Tokenizer` to achieve the intended functionality.
+ *
  * <a href="https://sap.github.io/ui5-webcomponents/playground/components/Token" target="_blank">UI5 Web Components Playground</a>
  */
 const Token: FC<TokenPropTypes> = withWebComponent<TokenPropTypes>(
