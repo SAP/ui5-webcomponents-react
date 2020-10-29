59a9cdcb7f31582ac6d4e3d847c55441e76ad4ae
Harbarth, Lukas <lukas.harbarth@sap.com>
2020-10-29 12:30:20 +0100

    docs(Table): enhance story

--- packages/main/src/webComponents/TableColumn/index.tsx
+++ packages/main/src/webComponents/TableColumn/index.tsx
@@ -22,7 +22,7 @@ export interface TableColumnPropTypes extends WithWebComponentPropTypes {
 }
 
 /**
- * The <code>TableColumn</code> component allows to define column specific properties that are applied when rendering the
+ * The <code>TableColumn</code> component allows defining column specific properties that are applied when rendering the
  <code>Table</code> component
 
  * <a href="https://sap.github.io/ui5-webcomponents/playground/components/TableColumn" target="_blank">UI5 Web Components Playground</a>
