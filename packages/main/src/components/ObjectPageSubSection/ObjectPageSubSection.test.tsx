3fe006cce6102c75e081757f782e6376518dec9b
kajal1811 <73377704+kajal1811@users.noreply.github.com>
2020-11-02 13:50:13 +0530

    Update ObjectPageSubSection.test.tsx

--- packages/main/src/components/ObjectPageSubSection/ObjectPageSubSection.test.tsx
+++ packages/main/src/components/ObjectPageSubSection/ObjectPageSubSection.test.tsx
@@ -1,13 +1,13 @@
 import { createPassThroughPropsTest } from '@shared/tests/utils';
 import { ObjectPageSubSection } from '@ui5/webcomponents-react/lib/ObjectPageSubSection';
-import { mount, shallow } from 'enzyme';
+import { render } from '@shared/tests';
 import React from 'react';
 
 describe('ObjectPageSubSection', () => {
   test('Render without Crashing', () => {
     // @ts-ignore
-    const wrapper = mount(<ObjectPageSubSection id="test" />);
-    expect(wrapper.render()).toMatchSnapshot();
+    const { asFragment } = render(<ObjectPageSubSection id="test" />);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('No ID should throw', () => {
