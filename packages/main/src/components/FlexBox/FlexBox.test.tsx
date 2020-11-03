63fdec242db8993b6f04a165809386403a32b957
sidram05 <38928563+sidram05@users.noreply.github.com>
2020-11-02 23:44:17 -0600

    rewrite of FlexBox test
    transition from enzyme to react-testing-library

--- packages/main/src/components/FlexBox/FlexBox.test.tsx
+++ packages/main/src/components/FlexBox/FlexBox.test.tsx
@@ -1,48 +1,48 @@
 import { createPassThroughPropsTest } from '@shared/tests/utils';
-import { mount } from 'enzyme';
+import { render } from '@shared/tests';
 import { FlexBox } from '@ui5/webcomponents-react/lib/FlexBox';
 import { FlexBoxJustifyContent } from '@ui5/webcomponents-react/lib/FlexBoxJustifyContent';
 import * as React from 'react';
 
 describe('FlexBox', () => {
   test('JustifyContent: End', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <FlexBox justifyContent={FlexBoxJustifyContent.End}>
         <span>Test 1</span>
       </FlexBox>
     );
-    const node = wrapper.getDOMNode();
+    const node = asFragment.getDOMNode();
     expect(window.getComputedStyle(node).justifyContent).toEqual('flex-end');
   });
 
   test('Height and Width', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <FlexBox style={{ height: '1337px', width: '42px' }}>
         <span>Test 1</span>
       </FlexBox>
     );
-    const node = wrapper.getDOMNode();
+    const node = asFragment.getDOMNode();
     expect(window.getComputedStyle(node).height).toEqual('1337px');
     expect(window.getComputedStyle(node).width).toEqual('42px');
   });
 
   test('Display: Inline', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <FlexBox displayInline>
         <span>Test 1</span>
       </FlexBox>
     );
-    const node = wrapper.getDOMNode();
+    const node = asFragment.getDOMNode();
     expect(window.getComputedStyle(node).display).toEqual('inline-flex');
   });
 
   test('with Custom Class Names and Style', () => {
-    const wrapper = mount(
+    const { asFragment } = render(
       <FlexBox className="testClass" style={{ backgroundColor: '#000' }}>
         <span>Test 1</span>
       </FlexBox>
     );
-    expect(wrapper.render()).toMatchSnapshot();
+    expect(asFragment.render()).toMatchSnapshot();
   });
 
   createPassThroughPropsTest(FlexBox);
