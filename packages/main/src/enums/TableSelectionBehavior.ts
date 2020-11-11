5a840131f194960b473b27a5cb7547c0175722b9
Lukas Harbarth <lukas742@gmx.net>
2020-11-11 14:45:10 +0100

    docs(AnalyticalTable): add JSDoc comment, enhance story (#934)

--- packages/main/src/enums/TableSelectionBehavior.ts
+++ packages/main/src/enums/TableSelectionBehavior.ts
@@ -1,5 +1,14 @@
 export enum TableSelectionBehavior {
+  /**
+   * A selection column is rendered along with the normal columns. The whole row is selectable.
+   */
   ROW = 'Row',
+  /**
+   * No selection column is rendered along with the normal columns. The whole row is selectable.
+   */
   ROW_ONLY = 'RowOnly',
+  /**
+   * The row is only selectable by clicking on the corresponding field in the selection column.
+   */
   ROW_SELECTOR = 'RowSelector'
 }
