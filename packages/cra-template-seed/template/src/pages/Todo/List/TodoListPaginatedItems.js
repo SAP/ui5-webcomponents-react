671e5c19b94e27f26071658ed8ed4b1d6fb40b5c
Luis Valgoi <luis.valgoi@sap.com>
2020-11-11 16:41:40 -0300

    refactor: extracted logic from components and added hooks

--- packages/cra-template-seed/template/src/pages/Todo/List/TodoListPaginatedItems.js
+++ packages/cra-template-seed/template/src/pages/Todo/List/TodoListPaginatedItems.js
@@ -1,26 +1,23 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { usePaginatedGet } from '../../../hooks/useRequest';
+import { useTodos } from '../../../hooks/services/useTodos';
 
 import { List } from '@ui5/webcomponents-react/lib/List';
 import { StandardListItem } from '@ui5/webcomponents-react/lib/StandardListItem';
 import { Spinner } from '@ui5/webcomponents-react/lib/Spinner';
 import { Title } from '@ui5/webcomponents-react/lib/Title';
 import { TitleLevel } from '@ui5/webcomponents-react/lib/TitleLevel';
-
 import { Pagination } from '../../../components/Pagination/Pagination';
 
-import Constants from '../../../util/Constants';
-import { getUrl as getUrlBrowser } from '../../../util/browser/BrowserProvider';
-import { getUrl as getUrlAPI } from '../../../util/api/url/APIProvider';
+import { getUrl } from '../../../util/browser/BrowserProvider';
 
 const TodoListPaginatedItems = () => {
   const history = useHistory();
   const [page, setPage] = useState(0);
-  const { resolvedData, isLoading } = usePaginatedGet(Constants.REACT_QUERY.KEYS.RQ_GET_TODO_LIST, page, getUrlAPI('GET_TODO_LIST'));
+  const { resolvedData, isLoading } = useTodos(page);
 
   const redirectToEditPage = (e) => {
-    history.push(getUrlBrowser('TODO_EDIT', [{ value: e.detail.item.dataset.id }]));
+    history.push(getUrl('TODO_EDIT', [{ value: e.detail.item.dataset.id }]));
   };
 
   return (
