eeec229e58d4beb9134e13b9b2aedfd2c2f5f278
Lukas Harbarth <lukas742@gmx.net>
2020-11-24 08:45:28 +0100

    Update packages/main/src/components/AnalyticalTable/TableBody/VirtualTableBody.tsx
    Co-authored-by: Marcus Notheis <marcus.notheis@sap.com>

--- packages/main/src/components/AnalyticalTable/TableBody/VirtualTableBody.tsx
+++ packages/main/src/components/AnalyticalTable/TableBody/VirtualTableBody.tsx
@@ -54,7 +54,7 @@ export const VirtualTableBody = (props: VirtualTableBodyProps) => {
     estimateSize: React.useCallback(
       (index) => {
         if (renderRowSubComponent && rows[index].isExpanded && rowSubComponentsHeight.current?.[index]) {
-          return internalRowHeight + rowSubComponentsHeight.current?.[index];
+          return internalRowHeight + rowSubComponentsHeight.current?.[index] ?? 0;
         }
         return internalRowHeight;
       },
