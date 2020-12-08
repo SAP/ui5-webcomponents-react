629598469702d794d839a4bf0111645312895665
Lukas Harbarth <lukas742@gmx.net>
2020-12-08 09:21:27 +0100

    feat(AnalyticalTable): add "Interactive" visibleRowCountMode to allow vertical resizing of the table (#1056)

--- shared/tests/index.tsx
+++ shared/tests/index.tsx
@@ -18,3 +18,36 @@ export * from '@testing-library/react';
 
 // override render method
 export { customRender as render };
+
+//copied from https://github.com/testing-library/react-testing-library/issues/268#issuecomment-454872934 on 4.Dec 2020
+class FakeMouseEvent extends MouseEvent {
+  offsetX: number;
+  offsetY: number;
+  pageX: number;
+  pageY: number;
+  x: number;
+  y: number;
+
+  constructor(type: string, values) {
+    const { pageX, pageY, offsetX, offsetY, x, y, ...mouseValues } = values;
+    super(type, mouseValues);
+
+    Object.assign(this, {
+      offsetX: offsetX || 0,
+      offsetY: offsetY || 0,
+      pageX: pageX || 0,
+      pageY: pageY || 0,
+      x: x || 0,
+      y: y || 0
+    });
+  }
+}
+
+export function getMouseEvent(type: string, values = {}): FakeMouseEvent {
+  values = {
+    bubbles: true,
+    cancelable: true,
+    ...values
+  };
+  return new FakeMouseEvent(type, values);
+}
