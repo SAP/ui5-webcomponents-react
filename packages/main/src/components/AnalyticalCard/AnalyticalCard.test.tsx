af3872749d92a9a857c6ce770985cbcfe2a63dbd
Lukas Harbarth <lukas742@gmx.net>
2020-12-08 08:14:48 +0100

    chore(AnalyticalCard): add tests, use correct CSSVar for bottom border, fix props docs (#1063)
    Co-authored-by: Marcus Notheis <marcus.notheis@sap.com>

--- packages/main/src/components/AnalyticalCard/AnalyticalCard.test.tsx
+++ packages/main/src/components/AnalyticalCard/AnalyticalCard.test.tsx
@@ -1,36 +1,42 @@
+import { render, screen } from '@shared/tests';
 import { createPassThroughPropsTest } from '@shared/tests/utils';
-import { render } from '@shared/tests';
 import { AnalyticalCard } from '@ui5/webcomponents-react/lib/AnalyticalCard';
 import { AnalyticalCardHeader } from '@ui5/webcomponents-react/lib/AnalyticalCardHeader';
-import { DeviationIndicator } from '@ui5/webcomponents-react/lib/DeviationIndicator';
 import { Text } from '@ui5/webcomponents-react/lib/Text';
-import { ValueState } from '@ui5/webcomponents-react/lib/ValueState';
 import React from 'react';
 
-const Header = (
-  <AnalyticalCardHeader
-    title="Title"
-    subTitle="Subtitle"
-    arrowIndicator={DeviationIndicator.Down}
-    indicatorState={ValueState.Success}
-    value="Value"
-    valueState={ValueState.Success}
-    unit="Unit"
-    target="Target"
-    deviation="Deviation"
-    showIndicator
-    description="Description"
-  />
-);
+const Header = <AnalyticalCardHeader title="Header Title" />;
 
-// TODO Add more tests
 describe('Analytical Card', () => {
   test('Render without Crashing', () => {
+    const { asFragment } = render(
+      <AnalyticalCard>
+        <Text>I'm a content!</Text>
+      </AnalyticalCard>
+    );
+    expect(screen.getByText("I'm a content!")).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('with custom header', () => {
+    const { asFragment } = render(
+      <AnalyticalCard header={<div>I'm a header!</div>}>
+        <Text>I'm a content!</Text>
+      </AnalyticalCard>
+    );
+    expect(screen.getByText("I'm a content!")).toBeInTheDocument();
+    expect(screen.getByText("I'm a header!")).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('with AnalyticalCardHeader', () => {
     const { asFragment } = render(
       <AnalyticalCard header={Header}>
         <Text>I'm a content!</Text>
       </AnalyticalCard>
     );
+    expect(screen.getByText('Header Title')).toBeInTheDocument();
+    expect(screen.getByText("I'm a content!")).toBeInTheDocument();
     expect(asFragment()).toMatchSnapshot();
   });
 
---
+++ packages/main/src/components/AnalyticalCardHeader/AnalyticalCardHeader.test.tsx
@@ -0,0 +1,92 @@
+import { fireEvent, render, screen } from '@shared/tests';
+import { createPassThroughPropsTest } from '@shared/tests/utils';
+import { AnalyticalCard } from '@ui5/webcomponents-react/lib/AnalyticalCard';
+import { AnalyticalCardHeader } from '@ui5/webcomponents-react/lib/AnalyticalCardHeader';
+import { DeviationIndicator } from '@ui5/webcomponents-react/lib/DeviationIndicator';
+import { ValueState } from '@ui5/webcomponents-react/lib/ValueState';
+import React from 'react';
+
+const valueStateMapping = [
+  { state: 'Error', className: 'AnalyticalCardHeader-error' },
+  { state: 'Warning', className: 'AnalyticalCardHeader-critical' },
+  { state: 'Success', className: 'AnalyticalCardHeader-good' },
+  { state: 'None', className: 'N.A.' }
+];
+const testFactory = () => {
+  valueStateMapping.forEach((item) => {
+    test(`with value state: ${item.state}`, () => {
+      render(
+        <AnalyticalCardHeader
+          unit="EUR"
+          arrowIndicator={DeviationIndicator.Down}
+          showIndicator
+          indicatorState={ValueState[item.state]}
+          value="123"
+          valueState={ValueState[item.state]}
+        />
+      );
+      if (item.state !== 'None') {
+        expect(screen.getByText('EUR').parentElement.firstChild).toHaveClass(item.className);
+        expect(screen.getByText('123').parentElement).toHaveClass(item.className);
+      } else {
+        valueStateMapping.forEach(({ className }) => {
+          expect(screen.getByText('EUR').parentElement.firstChild).not.toHaveClass(className);
+          expect(screen.getByText('123').parentElement).not.toHaveClass(className);
+        });
+      }
+    });
+  });
+};
+
+describe('AnalyticalCardHeader', () => {
+  test('click event', () => {
+    const cb = jest.fn();
+
+    const { rerender, container } = render(<AnalyticalCardHeader onHeaderPress={cb} title="header" />);
+
+    expect(container.firstChild).toHaveClass('AnalyticalCardHeader-cardHeaderClickable');
+    fireEvent.click(screen.getByText('header'));
+    expect(cb).toHaveBeenCalledTimes(1);
+
+    rerender(<AnalyticalCardHeader title="header" />);
+    expect(container.firstChild).not.toHaveClass('AnalyticalCardHeader-cardHeaderClickable');
+  });
+
+  test('with all string props', () => {
+    const { asFragment } = render(
+      <AnalyticalCardHeader
+        title="Title"
+        subTitle="Subtitle"
+        value="Value"
+        unit="Unit"
+        target="TargetDesc"
+        deviation="DeviationDesc"
+        description="Description"
+      />
+    );
+    expect(screen.getByText('Title')).toBeInTheDocument();
+    expect(screen.getByText('Subtitle')).toBeInTheDocument();
+    expect(screen.getByText('Value')).toBeInTheDocument();
+    expect(screen.getByText('Unit')).toBeInTheDocument();
+    expect(screen.getByText('TargetDesc')).toBeInTheDocument();
+    expect(screen.getByText('DeviationDesc')).toBeInTheDocument();
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+  test('with arrow indicator', () => {
+    const { rerender } = render(
+      <AnalyticalCardHeader unit="EUR" arrowIndicator={DeviationIndicator.Down} showIndicator />
+    );
+    expect(screen.getByText('EUR').parentElement.firstChild).toHaveClass('AnalyticalCardHeader-arrowDown');
+
+    rerender(<AnalyticalCardHeader unit="EUR" arrowIndicator={DeviationIndicator.Up} showIndicator />);
+    expect(screen.getByText('EUR').parentElement.firstChild).toHaveClass('AnalyticalCardHeader-arrowUp');
+
+    rerender(<AnalyticalCardHeader unit="EUR" arrowIndicator={DeviationIndicator.None} showIndicator />);
+    expect(screen.getByText('EUR').parentElement.firstChild).toHaveClass('AnalyticalCardHeader-arrowRight');
+  });
+
+  testFactory();
+
+  createPassThroughPropsTest(AnalyticalCard);
+});
