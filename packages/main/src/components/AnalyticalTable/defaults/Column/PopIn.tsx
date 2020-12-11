c43368c90a835f77270f5e3b3a93d929ec752620
Harbarth, Lukas <lukas.harbarth@sap.com>
2020-12-11 13:35:26 +0100

    popIn column adapts content

--- packages/main/src/components/AnalyticalTable/defaults/Column/PopIn.tsx
+++ packages/main/src/components/AnalyticalTable/defaults/Column/PopIn.tsx
@@ -1,8 +1,12 @@
+import { FlexBox } from '@ui5/webcomponents-react/lib/FlexBox';
+import { FlexBoxAlignItems } from '@ui5/webcomponents-react/lib/FlexBoxAlignItems';
+import { FlexBoxDirection } from '@ui5/webcomponents-react/lib/FlexBoxDirection';
+import { FlexBoxWrap } from '@ui5/webcomponents-react/lib/FlexBoxWrap';
+import { Text } from '@ui5/webcomponents-react/lib/Text';
 import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { makeRenderer } from 'react-table';
-import { FlexBox, FlexBoxAlignItems, FlexBoxDirection, FlexBoxWrap, Text } from '@ui5/webcomponents-react';
 
 const PopInStyles = {
   container: {
@@ -20,14 +24,18 @@ const PopInStyles = {
 };
 
 const useStyles = createUseStyles(PopInStyles, { name: 'PopIn' });
-//todo different contentToRender status (grouped, aggegeted, etc), hAlign
+
 export const PopIn = (instance) => {
   const { state, contentToRender, cell, row, internalRowHeight } = instance;
   const classes = useStyles();
   return (
     <FlexBox direction={FlexBoxDirection.Column} className={classes.container}>
       <FlexBox
-        alignItems={FlexBoxAlignItems.Start}
+        alignItems={
+          contentToRender !== 'Grouped' && contentToRender !== 'Expandable'
+            ? FlexBoxAlignItems.Start
+            : FlexBoxAlignItems.Center
+        }
         wrap={FlexBoxWrap.NoWrap}
         className={classes.defaultCell}
         style={{
@@ -36,30 +44,31 @@ export const PopIn = (instance) => {
       >
         {cell.render(contentToRender)}
       </FlexBox>
-      {state.popInColumns?.map((item) => {
-        const popInInstanceProps = row.allCells.find((cell) => cell.column.id === item.id);
-        const renderHeader = () => {
-          if (item.column.PopInHeader) {
-            return typeof item.column.PopInHeader === 'function'
-              ? item.column.PopInHeader({ ...instance, ...popInInstanceProps })
-              : item.column.PopInHeader;
-          }
-          return typeof item.column.Header === 'function'
-            ? makeRenderer({ ...instance, ...popInInstanceProps }, item.column)(item.column.Header)
-            : item.column.Header;
-        };
-        return (
-          <FlexBox direction={FlexBoxDirection.Column} key={item.id}>
-            {item.column?.Header && <div className={classes.header}>{renderHeader()}:</div>}
-            <div style={{ height: internalRowHeight }}>
-              {popInInstanceProps &&
-                (item.column?.Cell
-                  ? makeRenderer({ ...instance, ...popInInstanceProps, isPopIn: true }, item.column)(item.column.Cell)
-                  : <Text wrapping={false}>{popInInstanceProps.value}</Text> ?? null)}
-            </div>
-          </FlexBox>
-        );
-      })}
+      {contentToRender !== 'Grouped' &&
+        state.popInColumns?.map((item) => {
+          const popInInstanceProps = row.allCells.find((cell) => cell.column.id === item.id);
+          const renderHeader = () => {
+            if (item.column.PopInHeader) {
+              return typeof item.column.PopInHeader === 'function'
+                ? item.column.PopInHeader({ ...instance, ...popInInstanceProps })
+                : item.column.PopInHeader;
+            }
+            return typeof item.column.Header === 'function'
+              ? makeRenderer({ ...instance, ...popInInstanceProps }, item.column)(item.column.Header)
+              : item.column.Header;
+          };
+          return (
+            <FlexBox direction={FlexBoxDirection.Column} key={item.id}>
+              {item.column?.Header && <div className={classes.header}>{renderHeader()}:</div>}
+              <div style={{ height: internalRowHeight }}>
+                {popInInstanceProps &&
+                  (item.column?.Cell
+                    ? makeRenderer({ ...instance, ...popInInstanceProps, isPopIn: true }, item.column)(item.column.Cell)
+                    : <Text wrapping={false}>{popInInstanceProps.value}</Text> ?? null)}
+              </div>
+            </FlexBox>
+          );
+        })}
     </FlexBox>
   );
 };
