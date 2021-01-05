a6140f1675e4f730638e98dcf3730fd1d994612a
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-01-05 12:24:28 +0100

    Calendar: add missing props and usage description

--- packages/main/src/webComponents/Calendar/index.tsx
+++ packages/main/src/webComponents/Calendar/index.tsx
@@ -1,9 +1,42 @@
 import { CalendarSelection } from '@ui5/webcomponents-react/lib/CalendarSelection';
+import { CalendarType } from '@ui5/webcomponents-react/lib/CalendarType';
 import { withWebComponent, WithWebComponentPropTypes } from '@ui5/webcomponents-react/lib/withWebComponent';
 import '@ui5/webcomponents/dist/Calendar';
 import { FC } from 'react';
 
 export interface CalendarPropTypes extends WithWebComponentPropTypes {
+  /**
+   * Defines the selected dates as UTC timestamps.
+   */
+  selectedDates?: unknown[];
+  /**
+   * Defines the UNIX timestamp - seconds since 00:00:00 UTC on Jan 1, 1970.
+   */
+  timestamp?: number;
+  /**
+   * Determines the calendar type. The input value is formated according to the calendar type and the picker shows the months and years from the specified calendar.
+   *
+   * Available options are:
+   *
+   * *   `Gregorian`
+   * *   `Islamic`
+   * *   `Japanese`
+   * *   `Buddhist`
+   * *   `Persian`<br/>__Note:__ Calendar types other than Gregorian must be imported manually:<br />`import "@ui5/webcomponents-localization/dist/features/calendar/{primaryCalendarType}.js";`
+   */
+  primaryCalendarType?: CalendarType;
+  /**
+   * Determines the minimum date available for selection.
+   */
+  minDate?: string;
+  /**
+   * Determines the maximum date available for selection.
+   */
+  maxDate?: string;
+  /**
+   * Determines the format, displayed in the input field.
+   */
+  formatPattern?: string;
   /**
    * Defines the visibility of the week numbers column.
    *
@@ -31,7 +64,7 @@ export interface CalendarPropTypes extends WithWebComponentPropTypes {
  */
 const Calendar: FC<CalendarPropTypes> = withWebComponent<CalendarPropTypes>(
   'ui5-calendar',
-  ['selection'],
+  ['primaryCalendarType', 'formatPattern', 'maxDate', 'minDate', 'selection', 'selectedDates', 'timestamp'],
   ['hideWeekNumbers'],
   [],
   ['selected-dates-change']
@@ -41,7 +74,8 @@ Calendar.displayName = 'Calendar';
 
 Calendar.defaultProps = {
   hideWeekNumbers: false,
-  selection: CalendarSelection.Single
+  selection: CalendarSelection.Single,
+  primaryCalendarType: CalendarType.Gregorian
 };
 
 export { Calendar };
