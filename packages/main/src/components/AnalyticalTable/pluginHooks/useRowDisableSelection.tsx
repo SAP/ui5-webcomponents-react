cf7241a7c261c68ee63847dae3284874f4ee9618
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-01-12 17:20:03 +0100

    add story

--- packages/main/src/components/AnalyticalTable/pluginHooks/useRowDisableSelection.tsx
+++ packages/main/src/components/AnalyticalTable/pluginHooks/useRowDisableSelection.tsx
@@ -68,3 +68,5 @@ export const useRowDisableSelection = (disableRowSelection: DisableRowSelectionT
 
   return useDisableSelectionRow;
 };
+
+useRowDisableSelection.pluginName = 'useRowDisableSelection';
