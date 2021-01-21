616db1973daf56fa58ffb2088f9e4b05b26defad
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-01-21 08:40:37 +0100

    chore: drop support for Edge Legacy - remove Edge Legacy polyfill

--- packages/base/src/polyfill/Edge.ts
+++
@@ -1,8 +0,0 @@
-import ResizeObserver from 'resize-observer-polyfill';
-import { polyfill as scrollToPolyfill } from 'smoothscroll-polyfill';
-
-// @ts-ignore
-window.ResizeObserver = ResizeObserver;
-
-// required for scrollTo methods
-scrollToPolyfill();
