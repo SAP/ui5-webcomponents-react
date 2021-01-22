13ce55ee404aa9fd9ebd5ebde8450f7e10d39d88
Marcus Notheis <marcus.notheis@sap.com>
2021-01-22 08:33:12 +0100

    delete unused method

--- packages/charts/src/lib/Utils.ts
+++ packages/charts/src/lib/Utils.ts
@@ -1,3 +1,3 @@
-import { getTextHeight, getTextWidth, truncateLongLabel } from '../util/Utils';
+import { getTextWidth, truncateLongLabel } from '../util/Utils';
 
-export { truncateLongLabel, getTextHeight, getTextWidth };
+export { truncateLongLabel, getTextWidth };
--- packages/charts/src/util/Utils.ts
+++ packages/charts/src/util/Utils.ts
@@ -11,15 +11,6 @@ export const getTextWidth = (text) => {
   return metrics.width;
 };
 
-export const getTextHeight = (text = 'M', font = `normal 12pt "72","72full",Arial,Helvetica,sans-serif`) => {
-  // re-use canvas object for better performance
-  canvas = canvas || (canvas = document.createElement('canvas'));
-  const context = canvas.getContext('2d');
-  context.font = font;
-  const metrics = context.measureText(text); // should be around 90% accurate...
-  return textHeight || (textHeight = metrics.width);
-};
-
 export const truncateLongLabel = (value: string, length = 13) => {
   if (value?.length > length) {
     return `${value.slice(0, length - 2)}...`;
