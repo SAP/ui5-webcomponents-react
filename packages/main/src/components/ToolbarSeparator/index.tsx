7835cc54518b0c39c3d0d250c84f2753868f859e
Lukas Harbarth <lukas742@gmx.net>
2021-02-04 15:08:42 +0100

    test(Toolbar): add tests for toolbar and aria-label to ToolbarSeparator (#1254)

--- packages/main/src/components/ToolbarSeparator/index.tsx
+++ packages/main/src/components/ToolbarSeparator/index.tsx
@@ -1,8 +1,10 @@
+import { useI18nBundle } from '@ui5/webcomponents-react-base/hooks/useI18nBundle';
 import { createUseStyles } from 'react-jss';
 import { CssSizeVariables } from '@ui5/webcomponents-react-base/lib/CssSizeVariables';
 import { StyleClassHelper } from '@ui5/webcomponents-react-base/lib/StyleClassHelper';
 import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
 import React from 'react';
+import { SEPARATOR } from '@ui5/webcomponents-react/dist/assets/i18n/i18n-defaults';
 
 const styles = {
   separator: {
@@ -18,10 +20,11 @@ const ToolbarSeparator = (props) => {
   const { style, className } = props;
   const classes = useStyles(styles);
   const separatorClasses = StyleClassHelper.of(classes.separator);
+  const i18nBundle = useI18nBundle('@ui5/webcomponents-react');
   if (className) {
     separatorClasses.put(className);
   }
-  return <div style={style} className={separatorClasses.valueOf()} />;
+  return <div style={style} className={separatorClasses.valueOf()} aria-label={i18nBundle.getText(SEPARATOR)} />;
 };
 ToolbarSeparator.displayName = 'ToolbarSeparator';
 export { ToolbarSeparator };
