c7df58477b629e328c12097b4c2f535569be5363
Marcus Notheis <marcus.notheis@sap.com>
2021-02-18 13:59:38 +0100

    fix: fix tests in both create-react-app templates

--- packages/cra-template-seed/template/src/setupTests.js
+++ packages/cra-template-seed/template/src/setupTests.js
@@ -3,3 +3,25 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import ResiveObserverPolyfill from 'resize-observer-polyfill';
+
+const setupMatchMedia = () => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+};
+
+beforeEach(() => {
+  setupMatchMedia();
+  window.ResizeObserver = ResiveObserverPolyfill;
+});
