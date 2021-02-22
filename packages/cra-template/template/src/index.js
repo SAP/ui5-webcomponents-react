0dc453f12d6df0f2c39f77bffea1838f69cfb59c
Marcus Notheis <marcus.notheis@sap.com>
2021-02-22 13:39:04 +0100

    fix(cra-template): add asset imports (#1322)
    Asset imports are now required for running the app in production mode

--- packages/cra-template/template/src/index.js
+++ packages/cra-template/template/src/index.js
@@ -1,3 +1,6 @@
+import '@ui5/webcomponents/dist/Assets.js';
+import '@ui5/webcomponents-fiori/dist/Assets.js';
+import '@ui5/webcomponents-react/dist/Assets';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
