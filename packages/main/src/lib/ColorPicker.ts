5aa9eacba5d6ae53796e66f0843f128cd3a8ad0d
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-02-23 14:32:05 +0100

    feat(ColorPicker): add ColorPicker component

---
+++ packages/main/src/lib/ColorPicker.ts
@@ -0,0 +1,5 @@
+import { ColorPicker } from '../webComponents/ColorPicker';
+import type { ColorPickerPropTypes } from '../webComponents/ColorPicker';
+
+export { ColorPicker };
+export type { ColorPickerPropTypes };
