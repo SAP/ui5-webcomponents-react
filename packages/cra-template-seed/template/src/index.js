2cb3e74b8205ab5480fd4e2a9d2d768ea463d155
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-03-03 16:20:16 +0100

    fix(cra-template-seed): add InputElementsFormSupport import

--- packages/cra-template-seed/template/src/index.js
+++ packages/cra-template-seed/template/src/index.js
@@ -1,6 +1,7 @@
 import '@ui5/webcomponents/dist/Assets.js';
 import '@ui5/webcomponents-fiori/dist/Assets.js';
 import '@ui5/webcomponents-react/dist/Assets';
+import '@ui5/webcomponents/dist/features/InputElementsFormSupport';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
