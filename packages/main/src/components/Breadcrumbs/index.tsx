d68f23617c2b2be1193e18a38246c7f12faaf54e
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-04-13 11:47:50 +0200

    focus only first or last focused breadcrumb with tab, add arrow left/right navigation between breadcrumbs

--- packages/main/src/components/Breadcrumbs/index.tsx
+++ packages/main/src/components/Breadcrumbs/index.tsx
@@ -1,7 +1,20 @@
+import { ThemingParameters } from '@ui5/webcomponents-react-base/dist/ThemingParameters';
 import { usePassThroughHtmlProps } from '@ui5/webcomponents-react-base/dist/usePassThroughHtmlProps';
 import { BreadcrumbsSeparatorStyle } from '@ui5/webcomponents-react/dist/BreadcrumbsSeparatorStyle';
 import { Label } from '@ui5/webcomponents-react/dist/Label';
-import React, { Children, FC, forwardRef, Fragment, ReactNode, ReactNodeArray, Ref } from 'react';
+import React, {
+  Children,
+  cloneElement,
+  FC,
+  forwardRef,
+  ReactElement,
+  ReactNode,
+  ReactNodeArray,
+  Ref,
+  useRef,
+  useState
+} from 'react';
+import { createUseStyles } from 'react-jss';
 import { CommonProps } from '../../interfaces/CommonProps';
 
 const SeparatorStyles = {
@@ -12,8 +25,6 @@ const SeparatorStyles = {
   GreaterThan: '>',
   Slash: '/'
 };
-
-const separatorInlineStyles = { margin: '0 0.25rem' };
 
 export interface BreadcrumbsPropTypes extends CommonProps {
   /**
@@ -30,14 +41,67 @@ export interface BreadcrumbsPropTypes extends CommonProps {
   currentLocationText?: string;
 }
 
+const useStyles = createUseStyles(
+  {
+    list: {
+      listStyleType: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      marginBlock: 0,
+      paddingInline: 0,
+      flexWrap: 'wrap',
+      '& li': {
+        display: 'flex',
+        overflow: 'hidden'
+      }
+    },
+    separatorInline: {
+      margin: '0 0.25rem'
+    },
+    currentLocation: {
+      fontFamily: ThemingParameters.sapFontFamily,
+      fontSize: ThemingParameters.sapFontSize,
+      color: ThemingParameters.sapContent_LabelColor,
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      '&:focus': {
+        outline: `${ThemingParameters.sapContent_FocusWidth} ${ThemingParameters.sapContent_FocusStyle} ${ThemingParameters.sapContent_FocusColor}`,
+        outlineOffset: '-0.0625rem'
+      }
+    }
+  },
+  { name: 'Breadcrumbs' }
+);
+
 /**
  * Enables users to navigate between items by providing a list of links to previous steps in the user's navigation path.
  */
 const Breadcrumbs: FC<BreadcrumbsPropTypes> = forwardRef((props: BreadcrumbsPropTypes, ref: Ref<HTMLDivElement>) => {
   const { children, separatorStyle, currentLocationText, tooltip, style, className, slot } = props;
+  const classes = useStyles();
   const childrenArray = Children.toArray(children).filter(Boolean);
+  const [focusedItem, setFocusedItem] = useState(0);
+
+  const handleItemFocus = (e) => {
+    const currentId = parseInt(e.target.dataset.id);
+    setFocusedItem(currentId);
+  };
+
+  const handleKeyDownInList = (e) => {
+    const target = e.target as HTMLElement;
+    const currentId = parseInt(target.parentElement.id);
+    if (e.key === 'ArrowRight' && currentId + 1 <= childrenArray.length) {
+      breadcrumbsRef.current.children[currentId + 1].children[0].focus();
+    }
+    if (e.key === 'ArrowLeft' && currentId > 0) {
+      breadcrumbsRef.current.children[currentId - 1].children[0].focus();
+    }
+  };
 
   const passThroughProps = usePassThroughHtmlProps(props);
+
+  const breadcrumbsRef = useRef(null);
 
   return (
     <nav
@@ -49,35 +113,48 @@ const Breadcrumbs: FC<BreadcrumbsPropTypes> = forwardRef((props: BreadcrumbsProp
       aria-label="Breadcrumb Trail"
       {...passThroughProps}
     >
-      <ol
-        style={{
-          listStyleType: 'none',
-          display: 'flex',
-          alignItems: 'center',
-          marginBlock: 0,
-          paddingInline: 0,
-          flexWrap: 'wrap'
-        }}
-      >
+      <ol className={classes.list} ref={breadcrumbsRef} onKeyDown={handleKeyDownInList}>
         {childrenArray.map((item, index) => {
           if (index === childrenArray.length - 1) {
-            return <li key={`bc-${index}`}>{item}</li>;
+            return (
+              <li key={`bc-${index}`} id={`${index}`}>
+                {cloneElement(item as ReactElement, {
+                  tabIndex: focusedItem === index ? 0 : -1,
+                  onFocus: handleItemFocus,
+                  'data-id': index
+                })}
+                {currentLocationText && (
+                  <Label className={classes.separatorInline} aria-hidden="true">
+                    {SeparatorStyles[separatorStyle]}
+                  </Label>
+                )}
+              </li>
+            );
           }
           return (
-            <li key={`bc-${index}`} style={{ display: 'flex' }}>
-              {item}
-              <Label style={separatorInlineStyles} aria-hidden="true">
+            <li key={`bc-${index}`} id={`${index}`}>
+              {cloneElement(item as ReactElement, {
+                tabIndex: focusedItem === index ? 0 : -1,
+                onFocus: handleItemFocus,
+                'data-id': index
+              })}
+              <Label className={classes.separatorInline} aria-hidden="true">
                 {SeparatorStyles[separatorStyle]}
               </Label>
             </li>
           );
         })}
         {currentLocationText && (
-          <li key={`bc-${currentLocationText}`} style={{ display: 'flex' }}>
-            <Label style={separatorInlineStyles} aria-hidden="true">
-              {SeparatorStyles[separatorStyle]}
-            </Label>
-            <Label aria-current="page">{currentLocationText}</Label>
+          <li key={`bc-${currentLocationText}`} id={`${childrenArray.length}`}>
+            <div
+              aria-current="page"
+              tabIndex={focusedItem === childrenArray.length ? 0 : -1}
+              className={classes.currentLocation}
+              onFocus={handleItemFocus}
+              data-id={childrenArray.length}
+            >
+              {currentLocationText}
+            </div>
           </li>
         )}
       </ol>
