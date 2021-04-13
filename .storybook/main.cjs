a8a90c13da8a2cc7e26098f2571c6cd97315524b
Marcus Notheis <marcus.notheis@sap.com>
2021-04-13 16:59:31 +0200

    chore(deps): bump storybook to 6.2.7

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,13 +1,7 @@
 const path = require('path');
-
 const root = path.resolve(__dirname, '..');
-require('dotenv').config({
-  path: path.join(root, '.env')
-});
 
-const DEPENDENCY_REGEX = /node_modules\/(@ui5\/webcomponents(-(base|core|fiori|icons|theme-base))?|lit-html)\//;
-
-let addons = [
+const addons = [
   '@storybook/addon-toolbars',
   '@storybook/addon-docs',
   '@storybook/addon-controls',
@@ -21,56 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 module.exports = {
   stories: ['../docs/**/*.stories.mdx', '../packages/**/*.stories.@(tsx|jsx|mdx)'],
   addons: addons,
-  webpack: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-    if (configType === 'PRODUCTION') {
-      config.module.rules.push({
-        test: /\.(js|mjs)$/,
-        include: DEPENDENCY_REGEX,
-        loader: 'babel-loader',
-        options: {
-          sourceType: 'unambiguous',
-          babelrc: false,
-          configFile: false,
-          compact: false,
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                // Allow importing core-js in entrypoint and use browserlist to select polyfills
-                useBuiltIns: 'entry',
-                // Set the corejs version we are using to avoid warnings in console
-                // This will need to change once we upgrade to corejs@3
-                corejs: 3,
-                // Do not transform modules to CJS
-                modules: false,
-                // Exclude transforms that make all code slower
-                exclude: ['transform-typeof-symbol']
-              }
-            ]
-          ],
-          plugins: [
-            [
-              '@babel/plugin-transform-runtime',
-              {
-                version: require('@babel/runtime/package.json').version,
-                useESModules: true
-              }
-            ]
-          ],
-          cacheDirectory: true,
-          cacheCompression: false,
-
-          // If an error happens in a package, it's possible to be
-          // because it was compiled. Thus, we don't want the browser
-          // debugger to show the original code. Instead, the code
-          // being evaluated would be much more helpful.
-          sourceMaps: false
-        }
-      });
-    }
+  webpack: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@shared': path.join(root, 'shared'),
@@ -81,7 +26,9 @@ module.exports = {
       '@ui5/webcomponents-react-base/types': path.join(root, 'packages', 'base', 'types'),
       '@ui5/webcomponents-react-base': path.join(root, 'packages', 'base', 'src')
     };
-
     return config;
-  }
+  },
+  features: {
+    postcss: false,
+  },
 };
--- shared/stories/createSelectArgTypes.ts
+++ shared/stories/createSelectArgTypes.ts
@@ -7,9 +7,9 @@ export const createSelectArgTypes = (definition: ArgDefinition) => {
 
   for (const arg in definition) {
     argTypes[arg] = {
+      options: Array.isArray(definition[arg]) ? definition[arg] : Object.values(definition[arg]),
       control: {
-        type: 'radio',
-        options: Array.isArray(definition[arg]) ? definition[arg] : Object.values(definition[arg])
+        type: 'radio'
       }
     };
   }
