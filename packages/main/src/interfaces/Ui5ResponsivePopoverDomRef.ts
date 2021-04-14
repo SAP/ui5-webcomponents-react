0145978b1914afd2291dff7eeb7f804cd967a70e
Lukas Harbarth <lukas.harbarth@sap.com>
2021-04-14 10:37:48 +0200

    fix(MessageBox): screen reader reads out whole content of MessageBox when opened (#1504)

--- packages/main/src/interfaces/Ui5ResponsivePopoverDomRef.ts
+++ packages/main/src/interfaces/Ui5ResponsivePopoverDomRef.ts
@@ -1,6 +1,21 @@
 import { Ui5DomRef } from './Ui5DomRef';
 
 export interface Ui5ResponsivePopoverDomRef extends Ui5DomRef {
-  open: (p: HTMLElement) => void;
+  /**
+   * Opens popover on desktop and dialog on mobile.
+   * @param opener - The element that the popover is opened by.
+   */
+  open: (opener: HTMLElement) => void;
+  /**
+   * Closes the popover/dialog.
+   */
   close: () => void;
+  /**
+   * Focuses the element denoted by initialFocus, if provided, or the first focusable element otherwise.
+   */
+  applyFocus: () => void;
+  /**
+   * Override this method to provide custom logic for the popup's open/closed state. Maps to the "opened" property by default.
+   */
+  isOpen: () => void;
 }
