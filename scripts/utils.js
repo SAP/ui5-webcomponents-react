51680249c2ffb23a7268d1f98011e1ddd1221022
Marcus Notheis <marcus.notheis@sap.com>
2021-04-21 09:22:33 +0200

    chore(deps-dev): bump husky to 6.0.0 (#1518)

--- scripts/utils.js
+++ scripts/utils.js
@@ -78,9 +78,7 @@ export function highlightLog(projectName) {
   let whitespacesPerSide = (width - 6 - projectName.length) / 2;
   whitespacesPerSide = Math.max(whitespacesPerSide, 1);
   console.log(
-    `###${' '.repeat(Math.ceil(whitespacesPerSide))}${projectName}${' '.repeat(
-      Math.floor(whitespacesPerSide)
-    )}###`
+    `###${' '.repeat(Math.ceil(whitespacesPerSide))}${projectName}${' '.repeat(Math.floor(whitespacesPerSide))}###`
   );
   console.log('#'.repeat(width));
   console.log('');
