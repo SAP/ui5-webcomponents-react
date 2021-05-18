f5981b25c7b05ea155ebec6174bb6c90852149fd
Lukas Harbarth <lukas.harbarth@sap.com>
2021-05-18 09:22:03 +0200

    docs: add guide on how to use public interfaces to "Getting Started" (#1590)

--- packages/main/src/interfaces/Ui5DatePickerDomRef.ts
+++ packages/main/src/interfaces/Ui5DatePickerDomRef.ts
@@ -1,5 +1,8 @@
 import { Ui5PickerDomRef } from './Ui5PickerDomRef';
 
+/**
+ * DOM ref types for `DatePicker`, `DateRangePicker` and `DateTimePicker` component.
+ */
 export interface Ui5DatePickerDomRef extends Ui5PickerDomRef {
   /**
    * Checks if a date is between the minimum and maximum date.
