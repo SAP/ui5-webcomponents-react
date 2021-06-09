a20e9de4ffc3526ae830c2645918465a1372b194
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-06-09 09:21:04 +0200

    chore: cherry-pick action-sheet

--- packages/main/src/components/ActionSheet/index.tsx
+++ packages/main/src/components/ActionSheet/index.tsx
@@ -1,3 +1,4 @@
+import { isPhone } from '@ui5/webcomponents-base/dist/Device';
 import { addCustomCSS } from '@ui5/webcomponents-base/dist/Theming';
 import { createUseStyles } from 'react-jss';
 import { StyleClassHelper } from '@ui5/webcomponents-react-base/lib/StyleClassHelper';
@@ -5,18 +6,40 @@ import { useConsolidatedRef } from '@ui5/webcomponents-react-base/lib/useConsoli
 import { usePassThroughHtmlProps } from '@ui5/webcomponents-react-base/lib/usePassThroughHtmlProps';
 import { ButtonDesign } from '@ui5/webcomponents-react/lib/ButtonDesign';
 import { ResponsivePopover } from '@ui5/webcomponents-react/lib/ResponsivePopover';
-import React, { Children, cloneElement, FC, forwardRef, ReactElement, RefObject } from 'react';
+import React, {
+  Children,
+  cloneElement,
+  FC,
+  forwardRef,
+  ReactElement,
+  RefObject,
+  useCallback,
+  useReducer,
+  useRef
+} from 'react';
 import { createPortal } from 'react-dom';
 import { Ui5ResponsivePopoverDomRef } from '../../interfaces/Ui5ResponsivePopoverDomRef';
 import { ButtonPropTypes } from '../../webComponents/Button';
 import { ResponsivePopoverPropTypes } from '../../webComponents/ResponsivePopover';
 import styles from './ActionSheet.jss';
+import { useI18nBundle } from '@ui5/webcomponents-react-base/lib/hooks';
+import { ThemingParameters } from '@ui5/webcomponents-react-base/lib/ThemingParameters';
+import { AVAILABLE_ACTIONS, CANCEL, X_OF_Y } from '@ui5/webcomponents-react/dist/assets/i18n/i18n-defaults';
+import { Button } from '@ui5/webcomponents-react/lib/Button';
 
 export interface ActionSheetPropTypes extends Omit<ResponsivePopoverPropTypes, 'children'> {
   /**
    * Defines the actions of the <code>ActionSheet</code>. <br><b>Note:</b> Although this slot accepts all HTML Elements, it is strongly recommended that you only use `Buttons` in order to preserve the intended design.
    */
   children?: ReactElement<ButtonPropTypes> | ReactElement<ButtonPropTypes>[];
+  /**
+   * Displays a cancel button below the action buttons on mobile devices. No cancel button will be shown on desktop and tablet devices.
+   */
+  showCancelButton?: boolean;
+  /**
+   * Defines whether the `header` or `headerText` should always be displayed or only on mobile devices.
+   */
+  alwaysShowHeader?: boolean;
 }
 
 const useStyles = createUseStyles(styles, { name: 'ActionSheet' });
@@ -24,11 +47,38 @@ const useStyles = createUseStyles(styles, { name: 'ActionSheet' });
 addCustomCSS(
   'ui5-button',
   `
-  :host([data-is-action-sheet-button]) .ui5-button-root {
+  :host([data-action-btn-index]) .ui5-button-root {
     justify-content: flex-start;
   }
   `
 );
+
+if (isPhone()) {
+  addCustomCSS(
+    'ui5-responsive-popover',
+    `
+  :host([data-actionsheet]) ui5-button {
+    display: none;
+  }
+  :host([data-actionsheet]) ui5-dialog {
+    top: auto !important;
+    bottom: 0;
+    height: auto;
+    border-radius: 0;
+    background-color: transparent;
+    box-shadow: none;
+    box-sizing: border-box;
+  }
+  :host([data-actionsheet]) ui5-title {
+    color: ${ThemingParameters.sapContent_ContrastTextColor} !important;
+    text-shadow: none;
+    text-align: start !important;
+    padding-left:1rem !important;
+  }
+  `
+  );
+}
+
 /**
  * The `ActionSheet` holds a list of buttons from which the user can select to complete an action. <br />
  * The children of the action sheet should be `Button` components. Elements in the `ActionSheet` are left-aligned. Actions should be arranged in order of importance, from top to bottom.
@@ -53,14 +103,25 @@ const ActionSheet: FC<ActionSheetPropTypes> = forwardRef(
       onAfterClose,
       onAfterOpen,
       onBeforeClose,
-      onBeforeOpen
+      onBeforeOpen,
+      showCancelButton,
+      alwaysShowHeader
     } = props;
-
+    const i18nBundle = useI18nBundle('@ui5/webcomponents-react');
     const classes = useStyles();
-
     const actionSheetClasses = StyleClassHelper.of(classes.actionSheet).putIfPresent(className);
-
     const popoverRef: RefObject<Ui5ResponsivePopoverDomRef> = useConsolidatedRef(ref);
+    const actionBtnsRef = useRef(null);
+    const [focusedItem, setFocusedItem] = useReducer((_, action) => {
+      return parseInt(action.target.dataset.actionBtnIndex);
+    }, 0);
+    const childrenToRender = Children.toArray(children).filter(Boolean);
+    const childrenArrayLength = childrenToRender.length;
+    const childrenLength = isPhone() && showCancelButton ? childrenArrayLength + 1 : childrenArrayLength;
+
+    const handleCancelBtnClick = () => {
+      popoverRef.current.close();
+    };
 
     const onActionButtonClicked = (handler) => (e) => {
       popoverRef.current.close();
@@ -69,12 +130,29 @@ const ActionSheet: FC<ActionSheetPropTypes> = forwardRef(
       }
     };
 
+    const handleKeyDown = useCallback(
+      (e) => {
+        const currentIndex = parseInt(e.target.dataset.actionBtnIndex);
+        if (e.key === 'ArrowDown' && currentIndex + 1 < childrenLength) {
+          actionBtnsRef.current.querySelector(`[data-action-btn-index="${currentIndex + 1}"]`).focus();
+        }
+        if (e.key === 'ArrowUp' && currentIndex > 0) {
+          actionBtnsRef.current.querySelector(`[data-action-btn-index="${currentIndex - 1}"]`).focus();
+        }
+      },
+      [childrenLength, actionBtnsRef.current]
+    );
+
     const renderActionSheetButton = (element, index) => {
       return cloneElement(element, {
+        role: 'button',
         key: index,
         design: ButtonDesign.Transparent,
-        onClick: onActionButtonClicked(element.props.onClick),
-        'data-is-action-sheet-button': ''
+        onClick: onActionButtonClicked(element.props?.onClick),
+        'data-action-btn-index': index,
+        'aria-label': `${i18nBundle.getText(X_OF_Y, index + 1, childrenLength)} ${element.props?.children}`,
+        tabIndex: focusedItem === index ? 0 : -1,
+        onFocus: setFocusedItem
       });
     };
 
@@ -84,15 +162,27 @@ const ActionSheet: FC<ActionSheetPropTypes> = forwardRef(
       'onBeforeClose',
       'onBeforeOpen'
     ]);
+    const handleAfterOpen = useCallback(
+      (e) => {
+        if (isPhone()) {
+          actionBtnsRef.current.querySelector(`[data-action-btn-index="${focusedItem}"]`).focus();
+        }
+        if (typeof onAfterOpen === 'function') {
+          onAfterOpen(e);
+        }
+      },
+      [onAfterOpen, actionBtnsRef.current, focusedItem]
+    );
+    const displayHeader = alwaysShowHeader || isPhone();
 
     return createPortal(
       <ResponsivePopover
-        ref={popoverRef}
+        aria-modal
+        role="dialog"
         style={style}
         slot={slot}
-        className={actionSheetClasses.className}
         allowTargetOverlap={allowTargetOverlap}
-        headerText={headerText}
+        headerText={displayHeader ? headerText : undefined}
         horizontalAlign={horizontalAlign}
         initialFocus={initialFocus}
         modal={modal}
@@ -100,19 +190,47 @@ const ActionSheet: FC<ActionSheetPropTypes> = forwardRef(
         placementType={placementType}
         verticalAlign={verticalAlign}
         footer={footer}
-        header={header}
+        header={displayHeader ? header : undefined}
         onAfterClose={onAfterClose}
-        onAfterOpen={onAfterOpen}
         onBeforeClose={onBeforeClose}
         onBeforeOpen={onBeforeOpen}
         {...passThroughProps}
+        onAfterOpen={handleAfterOpen}
+        ref={popoverRef}
+        className={actionSheetClasses.className}
+        data-actionsheet
       >
-        {Children.map(children, renderActionSheetButton)}
+        <div
+          className={isPhone() ? classes.contentMobile : undefined}
+          role="presentation"
+          aria-label={i18nBundle.getText(AVAILABLE_ACTIONS)}
+          onKeyDown={handleKeyDown}
+          ref={actionBtnsRef}
+        >
+          {childrenToRender.map(renderActionSheetButton)}
+          {isPhone() && showCancelButton && (
+            <Button
+              design={ButtonDesign.Negative}
+              className={classes.closeBtn}
+              onClick={handleCancelBtnClick}
+              tabIndex={focusedItem === childrenLength - 1 ? 0 : -1}
+              data-action-btn-index={childrenLength - 1}
+              onFocus={setFocusedItem}
+            >
+              {i18nBundle.getText(CANCEL)}
+            </Button>
+          )}
+        </div>
       </ResponsivePopover>,
       document.body
     );
   }
 );
+
+ActionSheet.defaultProps = {
+  showCancelButton: true,
+  alwaysShowHeader: true
+};
 
 ActionSheet.displayName = 'ActionSheet';
 
