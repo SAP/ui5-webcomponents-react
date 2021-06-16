93f706de8453cd972b2b12f41ec6ddc77ff7e379
D071203 <julian.klein@sap.com>
2021-06-16 10:52:58 +0200

    Update index.tsx

--- packages/charts/src/components/ComposedChart/index.tsx
+++ packages/charts/src/components/ComposedChart/index.tsx
@@ -245,16 +245,20 @@ const ComposedChart: FC<ComposedChartProps> = forwardRef((props: ComposedChartPr
   useEffect(() => {
     if (lineChartPlacement === 'outside') {
       const newRechartsSurface =
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         Number(document.querySelector('g.recharts-line')?.getBBox().height) +
         Number(document.querySelector('svg.recharts-surface')?.getAttribute('height')) +
         20;
       document.querySelector('svg.recharts-surface')?.setAttribute('height', newRechartsSurface.toString());
       document.querySelector('svg.recharts-surface')?.childNodes.forEach((child) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         if (child.childNodes[0].id !== 'secondaryYAxis') {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
           child.style.transform = `translate(0, ${
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             Number(document.querySelector('g.recharts-line')?.getBBox().height) / 2 + 20
           }px)`;
@@ -266,22 +270,29 @@ const ComposedChart: FC<ComposedChartProps> = forwardRef((props: ComposedChartPr
           if (direction === '-') {
             setSecondAxisAbove(true);
           }
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
           child.style.transform = `translate(0, ${direction}${
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             Number(document.querySelector('g.recharts-line')?.getBBox().height) / 2 + 20
           }px)`;
         }
       });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       document.querySelector('g.recharts-line').style.transform = `translate(0, -${
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         Number(document.querySelector('g.recharts-line')?.getBBox().height) / 2 +
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         Number(document.querySelector('g.recharts-cartesian-axis-ticks')?.getBBox().height) / 2
       }px`;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       document.querySelector('ul.recharts-default-legend').style.transform = `translate(0, ${
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         Number(document.querySelector('g.recharts-line')?.getBBox().height) + 30
       }px)`;
