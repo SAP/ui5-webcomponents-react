5fe03d1177afc528068380ddcb8635dbd5ea74a8
D071203 <julian.klein@sap.com>
2021-07-20 13:38:36 +0200

    test(ColumnChartWithTrend): passThroughPropsTest

--- packages/charts/src/components/ColumnChartWithTrend/ColumnChartWithTrend.test.tsx
+++ packages/charts/src/components/ColumnChartWithTrend/ColumnChartWithTrend.test.tsx
@@ -2,10 +2,11 @@ import { render } from '@shared/tests';
 import * as React from 'react';
 import { complexDataSet } from '../../resources/DemoProps';
 import { ColumnChartWithTrend } from './ColumnChartWithTrend';
+import { createPassThroughPropsTest } from '@shared/tests/utils';
 
 describe('ColumnChart', () => {
   test('Renders with data', () => {
-    const utils = render(
+    const { asFragment } = render(
       <ColumnChartWithTrend
         dataset={complexDataSet}
         style={{ width: '50%' }}
@@ -36,11 +37,13 @@ describe('ColumnChart', () => {
         ]}
       />
     );
-    expect(utils.asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Loading placeholder', () => {
     const wrapper = render(<ColumnChartWithTrend style={{ width: '30%' }} dimensions={[]} measures={[]} />);
     expect(wrapper.asFragment()).toMatchSnapshot();
   });
+
+  createPassThroughPropsTest(ColumnChartWithTrend);
 });
