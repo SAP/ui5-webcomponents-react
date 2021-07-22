9d55a5921da13de5129289d8b3dad20679fde81b
D071203 <julian.klein@sap.com>
2021-07-22 13:42:18 +0200

    test(ColumnChartWithTrend): add comments, delete unused imports

--- packages/charts/src/components/ColumnChartWithTrend/ColumnChartWithTrend.test.tsx
+++ packages/charts/src/components/ColumnChartWithTrend/ColumnChartWithTrend.test.tsx
@@ -1,11 +1,11 @@
-import { fireEvent, render, screen, waitFor } from '@shared/tests';
+import { fireEvent, render } from '@shared/tests';
 import * as React from 'react';
 import { complexDataSet } from '../../resources/DemoProps';
 import { ColumnChartWithTrend } from './ColumnChartWithTrend';
 import { createPassThroughPropsTest } from '@shared/tests/utils';
 
 describe('ColumnChart', () => {
-  test('Renders with data', async () => {
+  it('Renders with data', async () => {
     const onClick = jest.fn();
     const onLegendClick = jest.fn();
     const { container, asFragment } = render(
@@ -36,15 +36,18 @@ describe('ColumnChart', () => {
       />
     );
 
+    // Check if two responsive containers are rendered
     const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
     expect(responsiveContainers.length).toBe(2);
 
+    // Check if trend line container, trend line and the path of the line is rendered correctly
     const trendLineChartContainer = container.querySelector('g.recharts-line');
     expect(trendLineChartContainer).toBeInTheDocument();
     const trendLine = trendLineChartContainer.querySelector('path');
     expect(trendLine).not.toBeNull();
     expect(trendLine.getAttribute('d')[0]).toBe('M');
 
+    // Check if column chart container, bars and single bars are rendered
     const columnChartContainer = container.querySelector('g.recharts-bar');
     expect(columnChartContainer).toBeInTheDocument();
     const barContainer = columnChartContainer.querySelectorAll('g.recharts-bar-rectangles');
@@ -52,27 +55,39 @@ describe('ColumnChart', () => {
     const singleBars = columnChartContainer.querySelectorAll('g.recharts-bar-rectangle');
     expect(singleBars.length).toBeGreaterThanOrEqual(1);
 
+    // Check if click on legend is working
     const legendContainer = responsiveContainers[1].querySelector('li.recharts-legend-item');
     fireEvent.click(legendContainer);
     expect(onLegendClick).toHaveBeenCalledTimes(1);
 
+    // Check if click in column chart container is working
     fireEvent.click(columnChartContainer);
     expect(onClick).toHaveBeenCalledTimes(2);
 
+    // Check if click in trend line container is working
     fireEvent.click(trendLineChartContainer);
     expect(onClick).toHaveBeenCalledTimes(3);
 
+    // Check if snapshot matches render
     expect(asFragment()).toMatchSnapshot();
   });
 
-  test('Loading placeholder', () => {
+  it('Loading placeholder', () => {
     const { container, asFragment } = render(<ColumnChartWithTrend dimensions={[]} measures={[]} />);
+
+    // Check if no responsive container is rendered
     const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
     expect(responsiveContainers.length).toBe(0);
 
+    // Check if no trend line container is rendered
     const trendLineContainer = container.querySelector('g.recharts-line');
     expect(trendLineContainer).toBeNull();
 
+    // Check if no column chart container is rendered
+    const columnChartContainer = container.querySelector('g.recharts-bar');
+    expect(columnChartContainer).toBeNull();
+
+    // Check if snapshot matches render
     expect(asFragment()).toMatchSnapshot();
   });
 
