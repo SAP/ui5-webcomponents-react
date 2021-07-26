b6704bec633e6c1e0ed87eaa59fd90e125337d9a
Marcus Notheis <marcus.notheis@sap.com>
2021-07-26 12:34:59 +0200

    StandardListItem

--- packages/main/src/webComponents/StandardListItem/index.tsx
+++ packages/main/src/webComponents/StandardListItem/index.tsx
@@ -8,13 +8,26 @@ import '@ui5/webcomponents/dist/StandardListItem';
 
 export interface StandardListItemPropTypes extends WithWebComponentPropTypes {
   /**
+   * Defines the text alternative of the component. Note: If not provided a default text alternative will be set, if present.
+   */
+  accessibleName?: string;
+  /**
+   * Defines the `additionalText`, displayed in the end of the list item.
+   */
+  additionalText?: string;
+  /**
+   * Defines the state of the `additionalText`.
+   * Available options are: `"None"` (by default), `"Success"`, `"Warning"`, `"Information"` and `"Erorr"`.
+   */
+  additionalTextState?: ValueState;
+  /**
    * Defines the description displayed right under the item text, if such is present.
    */
   description?: string;
   /**
    * Defines the `icon` source URI.
    *
-   * **Note:** SAP-icons font provides numerous buil-in icons. To find all the available icons, see the <ui5-link target="_blank" href="https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html" class="api-table-content-cell-link">Icon Explorer</ui5-link>.
+   * **Note:** SAP-icons font provides numerous built-in icons. To find all the available icons, see the <ui5-link target="_blank" href="https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html" class="api-table-content-cell-link">Icon Explorer</ui5-link>.
    */
   icon?: string;
   /**
@@ -30,15 +43,6 @@ export interface StandardListItemPropTypes extends WithWebComponentPropTypes {
    */
   image?: string;
   /**
-   * Defines the `info`, displayed in the end of the list item.
-   */
-  info?: string;
-  /**
-   * Defines the state of the `info`.
-   * Available options are: `"None"` (by default), `"Success"`, `"Warning"`, `"Information"` and `"Erorr"`.
-   */
-  infoState?: ValueState;
-  /**
    * Defines the visual indication and behavior of the list items. Available options are `Active` (by default), `Inactive` and `Detail`.
    *
    * **Note:** When set to `Active`, the item will provide visual response upon press and hover, while with type `Inactive` and `Detail` - will not.
@@ -49,7 +53,7 @@ export interface StandardListItemPropTypes extends WithWebComponentPropTypes {
    */
   selected?: boolean;
   /**
-   * Defines the text of the `StandardListItem`.
+   * Defines the text of the component.
    *
    * **Note:** Although this slot accepts HTML Elements, it is strongly recommended that you only use text in order to preserve the intended design.
    */
@@ -61,13 +65,13 @@ export interface StandardListItemPropTypes extends WithWebComponentPropTypes {
 }
 
 /**
- * The `StandardListItem` represents the simplest type of item for a `StandardListItemst`. This is a list item, providing the most common use cases such as `text`, `image` and `icon`
+ * The `StandardListItem` represents the simplest type of item for a `StandardListItemst`. This is a list item, providing the most common use cases such as `text`, `image` and `icon`.
  *
  * <ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/StandardListItem" target="_blank">UI5 Web Components Playground</ui5-link>
  */
 const StandardListItem: FC<StandardListItemPropTypes> = withWebComponent<StandardListItemPropTypes>(
   'ui5-li',
-  ['description', 'icon', 'image', 'info', 'infoState', 'type'],
+  ['accessibleName', 'additionalText', 'additionalTextState', 'description', 'icon', 'image', 'type'],
   ['iconEnd', 'selected'],
   [],
   ['detail-click']
@@ -76,8 +80,8 @@ const StandardListItem: FC<StandardListItemPropTypes> = withWebComponent<Standar
 StandardListItem.displayName = 'StandardListItem';
 
 StandardListItem.defaultProps = {
+  additionalTextState: ValueState.None,
   iconEnd: false,
-  infoState: ValueState.None,
   type: ListItemTypes.Active,
   selected: false
 };
