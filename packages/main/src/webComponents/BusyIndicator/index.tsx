005bbb237632f838f8c3b085fa92accdb95a5f79
Marcus Notheis <marcus.notheis@sap.com>
2021-07-26 11:25:55 +0200

    update Busy Indicator

--- packages/main/src/webComponents/BusyIndicator/index.tsx
+++ packages/main/src/webComponents/BusyIndicator/index.tsx
@@ -10,29 +10,37 @@ export interface BusyIndicatorPropTypes extends WithWebComponentPropTypes {
    */
   active?: boolean;
   /**
-   * Defines the size of the `BusyIndicator`.
+   * Defines the delay in milliseconds, after which the busy indicator will be visible on the screen.
+   */
+  delay?: number;
+  /**
+   * Defines the size of the component.
    *
-   * **Note:** Available options are "Small", "Medium", and "Large".
+   * **Note:**
+   *
+   * *   `Small`
+   * *   `Medium`
+   * *   `Large`
    */
   size?: BusyIndicatorSize;
   /**
-   * Defines text to be displayed below the busy indicator. It can be used to inform the user of the current operation.
+   * Defines text to be displayed below the component. It can be used to inform the user of the current operation.
    */
   text?: string;
   /**
-   * Determines the content over which the `BusyIndicator` will appear.
+   * Determines the content over which the component will appear.
    */
   children?: ReactNode | ReactNode[];
 }
 
 /**
- * The `BusyIndicator` signals that some operation is going on and that the user must wait. It does not block the current UI screen so other operations could be triggered in parallel.
+ * The `BusyIndicator` signals that some operation is going on and that the user must wait. It does not block the current UI screen so other operations could be triggered in parallel. It displays 3 dots and each dot expands and shrinks at a different rate, resulting in a cascading flow of animation.
  *
  * <ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/BusyIndicator" target="_blank">UI5 Web Components Playground</ui5-link>
  */
 const BusyIndicator: FC<BusyIndicatorPropTypes> = withWebComponent<BusyIndicatorPropTypes>(
-  'ui5-busyindicator',
-  ['size', 'text'],
+  'ui5-busy-indicator',
+  ['delay', 'size', 'text'],
   ['active'],
   [],
   []
@@ -42,6 +50,7 @@ BusyIndicator.displayName = 'BusyIndicator';
 
 BusyIndicator.defaultProps = {
   active: false,
+  delay: 1000,
   size: BusyIndicatorSize.Medium
 };
 
