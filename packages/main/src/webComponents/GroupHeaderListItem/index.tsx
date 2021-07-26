2dfabf847d6da6c55ffbf547bc0c3d8d11d3fe68
Marcus Notheis <marcus.notheis@sap.com>
2021-07-26 11:54:46 +0200

    GroupHeaderListItem

--- packages/main/src/webComponents/GroupHeaderListItem/index.tsx
+++ packages/main/src/webComponents/GroupHeaderListItem/index.tsx
@@ -5,11 +5,15 @@ import '@ui5/webcomponents/dist/GroupHeaderListItem';
 
 export interface GroupHeaderListItemPropTypes extends WithWebComponentPropTypes {
   /**
+   * Defines the text alternative of the component. Note: If not provided a default text alternative will be set, if present.
+   */
+  accessibleName?: string;
+  /**
    * Defines the selected state of the `ListItem`.
    */
   selected?: boolean;
   /**
-   * Defines the text of the `GroupHeaderListItem`.
+   * Defines the text of the component.
    * **Note:** Although this slot accepts HTML Elements, it is strongly recommended that you only use text in order to preserve the intended design.
    */
   children?: ReactNode | ReactNode[];
@@ -22,7 +26,7 @@ export interface GroupHeaderListItemPropTypes extends WithWebComponentPropTypes 
  */
 const GroupHeaderListItem: FC<GroupHeaderListItemPropTypes> = withWebComponent<GroupHeaderListItemPropTypes>(
   'ui5-li-groupheader',
-  [],
+  ['accessibleName'],
   ['selected'],
   [],
   []
