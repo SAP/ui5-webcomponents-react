d1fb05fd4e2292ef15a48fb0467d71607cff564a
D071203 <julian.klein@sap.com>
2021-07-26 15:42:04 +0200

    chore(LineChart): add multiple tests for onClick events and rendering

--- packages/charts/src/components/BarChart/BarChart.test.tsx
+++ packages/charts/src/components/BarChart/BarChart.test.tsx
@@ -29,14 +29,14 @@ const measures = [
 ];
 
 describe('BarChart', () => {
-  test('Renders with data', () => {
+  it('Renders with data', () => {
     const { asFragment, container } = render(
       <BarChart dataset={complexDataSet} dimensions={dimensions} measures={measures} />
     );
 
     // Check if a single responsive container is rendered
-    const responsiveContainer = container.querySelectorAll('div.recharts-responsive-container');
-    expect(responsiveContainer.length).toBe(1);
+    const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
+    expect(responsiveContainers.length).toBe(1);
 
     // Check if bar chart container, bars and single bars are rendered
     const barChartContainer = container.querySelector('g.recharts-bar');
@@ -50,7 +50,7 @@ describe('BarChart', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  test('Check onClick events', () => {
+  it('Check onClick events', () => {
     const onClick = jest.fn();
     const onLegendClick = jest.fn();
     const { asFragment, container } = render(
@@ -81,12 +81,22 @@ describe('BarChart', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  test('loading placeholder', () => {
-    const wrapper = render(<BarChart dimensions={[]} measures={[]} />);
-    expect(wrapper.asFragment()).toMatchSnapshot();
+  it('loading placeholder', () => {
+    const { container, asFragment } = render(<BarChart dimensions={[]} measures={[]} />);
+
+    // Check if no responsive container is rendered
+    const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
+    expect(responsiveContainers.length).toBe(0);
+
+    // Check if no column chart container is rendered
+    const columnChartContainer = container.querySelector('g.recharts-bar');
+    expect(columnChartContainer).toBeNull();
+
+    // Check if snapshot matches render
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  test('onLegendClick', () => {
+  it('onLegendClick', () => {
     const cb = jest.fn((e) => {
       e.persist();
     });
@@ -96,7 +106,7 @@ describe('BarChart', () => {
     expect(cb.mock.calls[0][0].detail.dataKey).toEqual('users');
   });
 
-  test('onLegendClick should not crash when invalid handler is provided', () => {
+  it('onLegendClick should not crash when invalid handler is provided', () => {
     render(
       <BarChart dataset={complexDataSet} dimensions={dimensions} measures={measures} onLegendClick={'123' as any} />
     );
@@ -108,124 +118,3 @@ describe('BarChart', () => {
 
   createPassThroughPropsTest(BarChart, { dimensions: [], measures: [] });
 });
-
-//
-// import { fireEvent, render, screen } from '@shared/tests';
-// import * as React from 'react';
-// import { complexDataSet } from '../../resources/DemoProps';
-// import { ColumnChartWithTrend } from './ColumnChartWithTrend';
-// import { createPassThroughPropsTest } from '@shared/tests/utils';
-//
-// const dimensions = [
-//   {
-//     accessor: 'name',
-//     formatter: (d) => `${d} 2019`,
-//     interval: 0
-//   }
-// ];
-//
-// const measures = [
-//   {
-//     accessor: 'users',
-//     label: 'Users',
-//     formatter: (val) => val.toLocaleString(),
-//     type: 'line'
-//   },
-//   {
-//     accessor: 'sessions',
-//     label: 'Active Sessions',
-//     type: 'column'
-//   }
-// ];
-//
-// describe('ColumnChart', () => {
-//   it('Renders with data', async () => {
-//     const onClick = jest.fn();
-//     const onLegendClick = jest.fn();
-//     const { container, asFragment } = render(
-//       <ColumnChartWithTrend
-//         onLegendClick={onLegendClick}
-//         onClick={onClick}
-//         dataset={complexDataSet}
-//         dimensions={dimensions}
-//         measures={measures}
-//       />
-//     );
-//
-//     // Check if two responsive containers are rendered
-//     const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
-//     expect(responsiveContainers.length).toBe(2);
-//
-//     // Check if trend line container, trend line and the path of the line is rendered correctly
-//     const trendLineChartContainer = container.querySelector('g.recharts-line');
-//     expect(trendLineChartContainer).toBeInTheDocument();
-//     const trendLine = trendLineChartContainer.querySelector('path');
-//     expect(trendLine).not.toBeNull();
-//     expect(trendLine.getAttribute('d')[0]).toBe('M');
-//
-//     // Check if column chart container, bars and single bars are rendered
-//     const columnChartContainer = container.querySelector('g.recharts-bar');
-//     expect(columnChartContainer).toBeInTheDocument();
-//     const barContainer = columnChartContainer.querySelectorAll('g.recharts-bar-rectangles');
-//     expect(barContainer.length).toBeGreaterThanOrEqual(1);
-//     const singleBars = columnChartContainer.querySelectorAll('g.recharts-bar-rectangle');
-//     expect(singleBars.length).toBeGreaterThanOrEqual(1);
-//
-//     // Check if click on axis label is working
-//     const firstXAxisLabel = screen.getByText(/January 2.../);
-//     fireEvent.click(firstXAxisLabel);
-//     expect(onClick).toBeCalled();
-//
-//     // Check if click on legend is working
-//     const legendContainer = screen.getByText(/Active Sessions/);
-//     fireEvent.click(legendContainer);
-//     expect(onLegendClick).toBeCalled();
-//
-//     // Check if click in column chart container is working
-//     fireEvent.click(columnChartContainer);
-//     expect(onClick).toBeCalled();
-//
-//     // Check if click in trend line container is working
-//     fireEvent.click(trendLineChartContainer);
-//     expect(onClick).toBeCalled();
-//
-//     // Check if snapshot matches render
-//     expect(asFragment()).toMatchSnapshot();
-//   });
-//
-//   it('Loading placeholder', () => {
-//     const { container, asFragment } = render(<ColumnChartWithTrend dimensions={[]} measures={[]} />);
-//
-//     // Check if no responsive container is rendered
-//     const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
-//     expect(responsiveContainers.length).toBe(0);
-//
-//     // Check if no trend line container is rendered
-//     const trendLineContainer = container.querySelector('g.recharts-line');
-//     expect(trendLineContainer).toBeNull();
-//
-//     // Check if no column chart container is rendered
-//     const columnChartContainer = container.querySelector('g.recharts-bar');
-//     expect(columnChartContainer).toBeNull();
-//
-//     // Check if snapshot matches render
-//     expect(asFragment()).toMatchSnapshot();
-//   });
-//
-//   it('onLegendClick should not crash when invalid handler is provided', () => {
-//     render(
-//       <ColumnChartWithTrend
-//         dataset={complexDataSet}
-//         dimensions={dimensions}
-//         measures={measures}
-//         onLegendClick={'123' as any}
-//       />
-//     );
-//
-//     expect(() => {
-//       fireEvent.click(screen.getByText(/Active Sessions/));
-//     }).not.toThrow();
-//   });
-//
-//   createPassThroughPropsTest(ColumnChartWithTrend, { dimensions: [], measures: [] });
-// });
--- packages/charts/src/components/LineChart/LineChart.test.tsx
+++ packages/charts/src/components/LineChart/LineChart.test.tsx
@@ -1,45 +1,109 @@
-import { render } from '@shared/tests';
+import { fireEvent, render, screen } from '@shared/tests';
 import * as React from 'react';
 import { complexDataSet } from '../../resources/DemoProps';
 import { LineChart } from './LineChart';
+import { createPassThroughPropsTest } from '@shared/tests/utils';
+
+const dimensions = [
+  {
+    accessor: 'name',
+    formatter: (d) => `${d} 2019`,
+    interval: 0
+  }
+];
+const measures = [
+  {
+    accessor: 'users',
+    label: 'Users',
+    formatter: (val) => val.toLocaleString()
+  },
+  {
+    accessor: 'sessions',
+    label: 'Active Sessions',
+    formatter: (val) => `${val} sessions`,
+    hideDataLabel: true
+  },
+  {
+    accessor: 'volume',
+    label: 'Vol.'
+  }
+];
 
 describe('LineChart', () => {
-  test('Renders with data', () => {
-    const utils = render(
-      <LineChart
-        dataset={complexDataSet}
-        style={{ width: '50%' }}
-        dimensions={[
-          {
-            accessor: 'name',
-            formatter: (d) => `${d} 2019`,
-            interval: 0
-          }
-        ]}
-        measures={[
-          {
-            accessor: 'users',
-            label: 'Users',
-            formatter: (val) => val.toLocaleString()
-          },
-          {
-            accessor: 'sessions',
-            label: 'Active Sessions',
-            formatter: (val) => `${val} sessions`,
-            hideDataLabel: true
-          },
-          {
-            accessor: 'volume',
-            label: 'Vol.'
-          }
-        ]}
-      />
+  it('Renders with data', () => {
+    const { container, asFragment } = render(
+      <LineChart dataset={complexDataSet} dimensions={dimensions} measures={measures} style={{ width: '50%' }} />
     );
-    expect(utils.asFragment()).toMatchSnapshot();
+
+    // Check if two responsive containers are rendered
+    const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
+    expect(responsiveContainers.length).toBe(1);
+
+    // Check if trend line container, trend line and the path of the line is rendered correctly
+    const lineChartContainer = container.querySelector('g.recharts-line');
+    expect(lineChartContainer).toBeInTheDocument();
+    const trendLine = lineChartContainer.querySelector('path');
+    expect(trendLine).not.toBeNull();
+    expect(trendLine.getAttribute('d')[0]).toBe('M');
+
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  test('loading placeholder', () => {
-    const wrapper = render(<LineChart style={{ width: '30%' }} dimensions={[]} measures={[]} />);
-    expect(wrapper.asFragment()).toMatchSnapshot();
+  it('Renders with data', async () => {
+    const onClick = jest.fn();
+    const onLegendClick = jest.fn();
+    const { container, asFragment } = render(
+      <LineChart
+        onLegendClick={onLegendClick}
+        onClick={onClick}
+        dataset={complexDataSet}
+        dimensions={dimensions}
+        measures={measures}
+      />
+    );
+
+    // Check if click on axis label is working
+    const firstXAxisLabel = screen.getByText(/January 2.../);
+    fireEvent.click(firstXAxisLabel);
+    expect(onClick).toBeCalled();
+
+    // Check if click on legend is working
+    const legendContainer = screen.getByText(/Active Sessions/);
+    fireEvent.click(legendContainer);
+    expect(onLegendClick).toBeCalled();
+
+    // Check if click in trend line container is working
+    fireEvent.click(container.querySelector('g.recharts-line'));
+    expect(onClick).toBeCalled();
+
+    // Check if snapshot matches render
+    expect(asFragment()).toMatchSnapshot();
   });
+
+  it('loading placeholder', () => {
+    const { container, asFragment } = render(<LineChart style={{ width: '30%' }} dimensions={[]} measures={[]} />);
+
+    // Check if no responsive container is rendered
+    const responsiveContainers = container.querySelectorAll('div.recharts-responsive-container');
+    expect(responsiveContainers.length).toBe(0);
+
+    // Check if no line container is rendered
+    const trendLineContainer = container.querySelector('g.recharts-line');
+    expect(trendLineContainer).toBeNull();
+
+    // Check if snapshot matches render
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('onLegendClick should not crash when invalid handler is provided', () => {
+    render(
+      <LineChart dataset={complexDataSet} dimensions={dimensions} measures={measures} onLegendClick={'123' as any} />
+    );
+
+    expect(() => {
+      fireEvent.click(screen.getByText('Users'));
+    }).not.toThrow();
+  });
+
+  createPassThroughPropsTest(LineChart, { dimensions: [], measures: [] });
 });
