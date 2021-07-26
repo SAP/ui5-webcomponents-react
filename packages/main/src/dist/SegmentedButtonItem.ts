60407efe469cfa795148d4d453353094ace23901
Marcus Notheis <marcus.notheis@sap.com>
2021-07-26 12:28:33 +0200

    SegmentedButton
    BREAKING CHANGE: **SegmentedButton**: The SegmentedButton no longer accepts `ToggleButton`s as children but expects `SegmentedButtonItem`s.

---
+++ packages/main/src/dist/SegmentedButtonItem.ts
@@ -0,0 +1,5 @@
+import { SegmentedButtonItem } from '../webComponents/SegmentedButtonItem';
+import type { SegmentedButtonItemPropTypes } from '../webComponents/SegmentedButtonItem';
+
+export { SegmentedButtonItem };
+export type { SegmentedButtonItemPropTypes };
---
+++ packages/main/src/webComponents/SegmentedButtonItem/index.tsx
@@ -0,0 +1,61 @@
+import { FC, MouseEventHandler, ReactNode } from 'react';
+import { withWebComponent, WithWebComponentPropTypes } from '@ui5/webcomponents-react/dist/withWebComponent';
+
+import '@ui5/webcomponents/dist/SegmentedButtonItem';
+
+export interface SegmentedButtonItemPropTypes extends Omit<WithWebComponentPropTypes, 'onClick'> {
+  /**
+   * Determines whether the component is displayed as pressed.
+   */
+  pressed?: boolean;
+  /**
+   * Sets the accessible aria name of the component.
+   */
+  accessibleName?: string;
+  /**
+   * Defines whether the component is disabled (default is set to `false`). A disabled component can't be pressed or focused, and it is not in the tab chain.
+   */
+  disabled?: boolean;
+  /**
+   * Defines the icon to be displayed as graphical element within the component. The SAP-icons font provides numerous options.
+   *
+   * Example: See all the available icons in the <ui5-link target="_blank" href="https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html" class="api-table-content-cell-link">Icon Explorer</ui5-link>.
+   */
+  icon?: string;
+  /**
+   * Defines the text of the component.
+   *
+   * **Note:** Although this slot accepts HTML Elements, it is strongly recommended that you only use text in order to preserve the intended design.
+   */
+  children?: ReactNode | ReactNode[];
+  /**
+   * Fired when the component is activated either with a mouse/tap or by using the Enter or Space key.
+   *
+   * **Note:** The event will not be fired if the `disabled` property is set to `true`.
+   */
+  onClick?: MouseEventHandler<HTMLElement>;
+}
+
+/**
+ * Users can use the `SegmentedButtonItem` as part of a `SegmentedButton`.
+ *
+ * Clicking or tapping on a `SegmentedButtonItem` changes its state to `pressed`. The item returns to its initial state when the user clicks or taps on it again. By applying additional custom CSS-styling classes, apps can give a different style to any `SegmentedButtonItem`.
+ *
+ * <ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/SegmentedButtonItem" target="_blank">UI5 Web Components Playground</ui5-link>
+ */
+const SegmentedButtonItem: FC<SegmentedButtonItemPropTypes> = withWebComponent<SegmentedButtonItemPropTypes>(
+  'ui5-segmented-button-item',
+  ['accessibleName', 'icon'],
+  ['pressed', 'disabled'],
+  [],
+  ['click']
+);
+
+SegmentedButtonItem.displayName = 'SegmentedButtonItem';
+
+SegmentedButtonItem.defaultProps = {
+  pressed: false,
+  disabled: false
+};
+
+export { SegmentedButtonItem };
