59d8afabe4093902e71deb3603cadda452c4c145
Marcus Notheis <marcus.notheis@sap.com>
2021-07-26 11:53:53 +0200

    FileUploader

--- packages/main/src/webComponents/FileUploader/index.tsx
+++ packages/main/src/webComponents/FileUploader/index.tsx
@@ -7,19 +7,19 @@ import '@ui5/webcomponents/dist/FileUploader';
 
 export interface FileUploaderPropTypes extends Omit<WithWebComponentPropTypes, 'onChange'> {
   /**
-   * Comma-separated list of file types that the `FileUploader` should accept.
+   * Comma-separated list of file types that the component should accept.
    *
    * **Note:** Please make sure you are adding the `.` in front on the file type, e.g. `.png` in case you want to accept png's only.
    */
   accept?: string;
   /**
-   * Defines whether `FileUploader` is in disabled state.
+   * Defines whether the component is in disabled state.
    *
-   * **Note:** A disabled `FileUploader` is completely noninteractive.
+   * **Note:** A disabledcomponent is completely noninteractive.
    */
   disabled?: boolean;
   /**
-   * If set to "true", the input field of `FileUploader` will not be rendered. Only the default slot that is passed will be rendered.
+   * If set to "true", the input field of component will not be rendered. Only the default slot that is passed will be rendered.
    */
   hideInput?: boolean;
   /**
@@ -27,15 +27,15 @@ export interface FileUploaderPropTypes extends Omit<WithWebComponentPropTypes, '
    */
   multiple?: boolean;
   /**
-   * Determines the name with which the `FileUploader` will be submitted in an HTML form.
+   * Determines the name with which the component will be submitted in an HTML form.
    *
    * **Important:** For the `name` property to have effect, you must add the following import to your project: `import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";`
    *
-   * **Note:** When set, a native `input` HTML element will be created inside the `FileUploader` so that it can be submitted as part of an HTML form. Do not use this property unless you need to submit a form.
+   * **Note:** When set, a native `input` HTML element will be created inside the component so that it can be submitted as part of an HTML form. Do not use this property unless you need to submit a form.
    */
   name?: string;
   /**
-   * Defines a short hint intended to aid the user with data entry when the `FileUploader` has no value.
+   * Defines a short hint intended to aid the user with data entry when the component has no value.
    */
   placeholder?: string;
   /**
@@ -43,7 +43,7 @@ export interface FileUploaderPropTypes extends Omit<WithWebComponentPropTypes, '
    */
   value?: string;
   /**
-   * Defines the value state of the `FileUploader`.
+   * Defines the value state of the component.
    *
    * Available options are:
    *
@@ -55,14 +55,14 @@ export interface FileUploaderPropTypes extends Omit<WithWebComponentPropTypes, '
    */
   valueState?: ValueState;
   /**
-   * By default the `FileUploader` contains a single input field. With this slot you can pass any content that you wish to add. See the samples for more information.
+   * By default the component contains a single input field. With this slot you can pass any content that you wish to add. See the samples for more information.
    */
   children?: ReactNode | ReactNode[];
   /**
-   * Defines the value state message that will be displayed as pop up under the `FileUploader`.
+   * Defines the value state message that will be displayed as pop up under the component.
    *
    * **Note:** If not specified, a default text (in the respective language) will be displayed.
-   * **Note:** The `valueStateMessage` would be displayed, when the `FileUploader` is in `Information`, `Warning` or `Error` value state.
+   * **Note:** The `valueStateMessage` would be displayed, when the component is in `Information`, `Warning` or `Error` value state.
    *
    * __Note:__ When passing a custom React component to this prop, you have to make sure your component reads the `slot` prop and appends it to the most outer element of your component.
    * Learn more about it [here](https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base--page#adding-custom-components-to-slots).
