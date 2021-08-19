8e318d146b5bcdcc7ddda6d6f99c64c45d675365
Marcus Notheis <marcus.notheis@sap.com>
2021-08-19 09:35:22 +0200

    MessageBox: deprecate uppercase MessageBoxActions and MessageBoxTypes

--- packages/main/src/enums/MessageBoxActions.ts
+++ packages/main/src/enums/MessageBoxActions.ts
@@ -1,11 +1,44 @@
 export enum MessageBoxActions {
-  ABORT = 'Abort',
-  CANCEL = 'Cancel',
-  CLOSE = 'Close',
-  DELETE = 'Delete',
-  IGNORE = 'Ignore',
-  NO = 'No',
+  /**
+   * @deprecated please use `MessageBoxActions.Abort` instead
+   */
+  ABORT = 'ABORT',
+  /**
+   * @deprecated please use `MessageBoxActions.Cancel` instead
+   */
+  CANCEL = 'CANCEL',
+  /**
+   * @deprecated please use `MessageBoxActions.Close` instead
+   */
+  CLOSE = 'CLOSE',
+  /**
+   * @deprecated please use `MessageBoxActions.Delete` instead
+   */
+  DELETE = 'DELETE',
+  /**
+   * @deprecated please use `MessageBoxActions.Ignore` instead
+   */
+  IGNORE = 'IGNORE',
+  /**
+   * @deprecated please use `MessageBoxActions.No` instead
+   */
+  NO = 'NO',
+  /**
+   * @deprecated please use `MessageBoxActions.Retry` instead
+   */
+  RETRY = 'RETRY',
+  /**
+   * @deprecated please use `MessageBoxActions.Yes` instead
+   */
+  YES = 'YES',
+
+  Abort = 'Abort',
+  Cancel = 'Cancel',
+  Close = 'Close',
+  Delete = 'Delete',
+  Ignore = 'Ignore',
+  No = 'No',
   OK = 'OK',
-  RETRY = 'Retry',
-  YES = 'Yes'
+  Retry = 'Retry',
+  Yes = 'Yes'
 }
