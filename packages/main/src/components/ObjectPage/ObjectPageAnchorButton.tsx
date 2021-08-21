2325dd00ffbac028447a8151239bc62868168412
Marcus Notheis <marcus.notheis@sap.com>
2021-08-21 11:26:19 +0200

    fix(ObjectPage): respect scoping suffix for custom anchor tab (#2011)

--- packages/main/src/components/ObjectPage/ObjectPageAnchorButton.tsx
+++ packages/main/src/components/ObjectPage/ObjectPageAnchorButton.tsx
@@ -3,10 +3,13 @@ import React, { FC, ReactElement, useEffect, useRef } from 'react';
 import { ObjectPageSectionPropTypes } from '../ObjectPageSection';
 import { ObjectPageAnchorTab } from './ObjectPageAnchorTab';
 import { safeGetChildrenArray } from './ObjectPageUtils';
+import { getEffectiveScopingSuffixForTag } from '@ui5/webcomponents-base/dist/CustomElementsScope';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 ObjectPageAnchorTab.define();
+
+const tagName = 'ui5-object-page-anchor-tab';
 
 interface ObjectPageAnchorPropTypes {
   section: ReactElement<ObjectPageSectionPropTypes>;
@@ -34,8 +37,11 @@ export const ObjectPageAnchorButton: FC<ObjectPageAnchorPropTypes> = (props: Obj
     };
   }, [onShowSubSectionPopover]);
 
+  const tagNameSuffix: string = getEffectiveScopingSuffixForTag(tagName);
+  const Component = (tagNameSuffix ? `${tagName}-${tagNameSuffix}` : tagName) as any;
+
   return (
-    <ui5-object-page-anchor-tab
+    <Component
       ref={ref}
       data-index={index}
       data-section-id={section.props.id}
