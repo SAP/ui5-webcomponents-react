e714f68d4b4e425ff9b3bb62d6b489a8fa49454e
Marcus Notheis <marcus.notheis@sap.com>
2021-08-23 08:09:59 +0200

    allow string as ThemeId for custom themes

--- packages/base/types/Theme.d.ts
+++ packages/base/types/Theme.d.ts
@@ -1,12 +1,17 @@
-declare module "@ui5/webcomponents-base/dist/config/Theme" {
-  export function getTheme(): ThemeString;
-  export function setTheme(theme: ThemeString): Promise<void>;
-  export type ThemeString =
-    | "sap_fiori_3"
-    | "sap_fiori_3_dark"
-    | "sap_belize"
-    | "sap_belize_hcb"
-    | "sap_belize_hcw"
-    | "sap_fiori_3_hcb"
-    | "sap_fiori_3_hcw";
+declare module '@ui5/webcomponents-base/dist/config/Theme' {
+  export type ThemeId =
+    | 'sap_fiori_3'
+    | 'sap_fiori_3_dark'
+    | 'sap_belize'
+    | 'sap_belize_hcb'
+    | 'sap_belize_hcw'
+    | 'sap_fiori_3_hcb'
+    | 'sap_fiori_3_hcw'
+    | string;
+
+  export function getTheme(): ThemeId;
+
+  export function setTheme(theme: ThemeId): Promise<void>;
+
+
 }
