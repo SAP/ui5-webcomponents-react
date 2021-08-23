193e5fd3c0d876968347cdfc182f8359a56ae98d
Marcus Notheis <marcus.notheis@sap.com>
2021-08-23 08:18:34 +0200

    add jsdoc comments

--- packages/base/types/Theme.d.ts
+++ packages/base/types/Theme.d.ts
@@ -8,10 +8,14 @@ declare module '@ui5/webcomponents-base/dist/config/Theme' {
     | 'sap_fiori_3_hcb'
     | 'sap_fiori_3_hcw'
     | string;
-
+  
+  /**
+   * Getter for the currenty applied theme.
+   */ 
   export function getTheme(): ThemeId;
 
+  /**
+   * Apply a new theme to the application.
+   */ 
   export function setTheme(theme: ThemeId): Promise<void>;
-
-
 }
