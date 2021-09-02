9a19f7971d4fbf75869b83ffd4165dafefcaf75e
Marcus Notheis <marcus.notheis@sap.com>
2021-09-02 14:01:47 +0200

    feat: allow disabling sapScrollBar (#2078)

--- packages/main/src/components/ThemeProvider/GlobalStyleClasses.jss.ts
+++ packages/main/src/components/ThemeProvider/GlobalStyleClasses.jss.ts
@@ -3,7 +3,7 @@ import { GlobalStyleClasses } from '@ui5/webcomponents-react/lib/GlobalStyleClas
 
 export const GlobalStyleClassesStyles = {
   '@global': {
-    [`.${GlobalStyleClasses.sapScrollBar}`]: {
+    [`.${GlobalStyleClasses.sapScrollBar}:not([data-native-scrollbar])`]: {
       '&::-webkit-scrollbar': {
         backgroundColor: ThemingParameters.sapScrollBar_TrackColor,
         '&:horizontal': {
