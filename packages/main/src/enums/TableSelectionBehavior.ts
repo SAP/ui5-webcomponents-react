dfdc6f2f9b57ba35c7df97220cd830c307e2a4ef
Harbarth, Lukas <lukas.harbarth@sap.com>
2021-10-21 13:57:03 +0200

    refactor: remove deprecated props and enum entries

--- packages/main/src/enums/TableSelectionBehavior.ts
+++ packages/main/src/enums/TableSelectionBehavior.ts
@@ -1,22 +1,6 @@
 export enum TableSelectionBehavior {
   /**
    * A selection column is rendered along with the normal columns. The whole row is selectable.
-   * @deprecated Please use `TableSelectionBehavior.Row` instead.
-   */
-  ROW = 'ROW',
-  /**
-   * No selection column is rendered along with the normal columns. The whole row is selectable.
-   * @deprecated Please use `TableSelectionBehavior.RowOnly` instead.
-   */
-  ROW_ONLY = 'ROW_ONLY',
-  /**
-   * The row is only selectable by clicking on the corresponding field in the selection column.
-   * @deprecated Please use `TableSelectionBehavior.RowSelector` instead.
-   */
-  ROW_SELECTOR = 'ROW_SELECTOR',
-
-  /**
-   * A selection column is rendered along with the normal columns. The whole row is selectable.
    */
   Row = 'Row',
   /**
