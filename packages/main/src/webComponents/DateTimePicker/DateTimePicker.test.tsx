f799f913144719feaedf7115eef558c03bc8c832
Marcus Notheis <marcus.notheis@sap.com>
2021-11-08 11:13:39 +0100

    update snapshots

--- packages/main/src/webComponents/DateTimePicker/DateTimePicker.test.tsx
+++ packages/main/src/webComponents/DateTimePicker/DateTimePicker.test.tsx
@@ -1,11 +1,10 @@
-import { render, waitFor } from '@shared/tests';
+import { render } from '@shared/tests';
 import { DateTimePicker } from '@ui5/webcomponents-react/dist/DateTimePicker';
 import React from 'react';
 
 describe('DateTimePicker', () => {
   test('Basic Test (generated)', async () => {
-    const { baseElement, asFragment } = render(<DateTimePicker />);
-    await waitFor(() => expect(baseElement).toHaveAttribute('ui5-datetime-picker'));
+    const { asFragment } = render(<DateTimePicker />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
