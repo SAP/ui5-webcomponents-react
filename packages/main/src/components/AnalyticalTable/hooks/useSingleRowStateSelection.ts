9ea460d71e40017f04d7262a11e3f2db7774da90
Lukas Harbarth <lukas.harbarth@sap.com>
2021-11-24 15:53:26 +0100

    feat(AnalyticalTable): introduce useIndeterminateRowSelection plugin hook (#2344)

--- packages/main/src/components/AnalyticalTable/hooks/useSingleRowStateSelection.ts
+++ packages/main/src/components/AnalyticalTable/hooks/useSingleRowStateSelection.ts
@@ -2,44 +2,13 @@ import { enrichEventWithDetails } from '@ui5/webcomponents-react-base/dist/Utils
 import { TableSelectionBehavior } from '@ui5/webcomponents-react/dist/TableSelectionBehavior';
 import { TableSelectionMode } from '@ui5/webcomponents-react/dist/TableSelectionMode';
 
-const tagNamesWhichShouldNotSelectARow = new Set([
-  'UI5-AVATAR',
-  'UI5-BUTTON',
-  'UI5-CALENDAR',
-  'UI5-CHECKBOX',
-  'UI5-COLOR-PICKER',
-  'UI5-COMBOBOX',
-  'UI5-DATE-PICKER',
-  'UI5-DATERANGE-PICKER',
-  'UI5-DATETIME-PICKER',
-  'UI5-DURATION-PICKER',
-  'UI5-FILE-UPLOADER',
-  'UI5-ICON',
-  'UI5-INPUT',
-  'UI5-LINK',
-  'UI5-MULTI-COMBOBOX',
-  'UI5-MULTI-INPUT',
-  'UI5-RADIO-BUTTON',
-  'UI5-RANGE-SLIDER',
-  'UI5-RATING-INDICATOR',
-  'UI5-SEGMENTED-BUTTON',
-  'UI5-SELECT',
-  'UI5-SLIDER',
-  'UI5-STEP-INPUT',
-  'UI5-SWITCH',
-  'UI5-TEXT-AREA',
-  'UI5-TIME-PICKER',
-  'UI5-TOGGLE-BUTTON',
-  'UI5-UPLOAD-COLLECTION'
-]);
-
 const getRowProps = (rowProps, { row, instance }) => {
   const { webComponentsReactProperties, toggleRowSelected, selectedFlatRows, flatRows } = instance;
   const handleRowSelect = (e, selectionCellClick = false) => {
     if (
       e.target?.dataset?.name !== 'internal_selection_column' &&
       !(e.markerAllowTableRowSelection === true || e.nativeEvent?.markerAllowTableRowSelection === true) &&
-      tagNamesWhichShouldNotSelectARow.has(e.target.tagName)
+      webComponentsReactProperties.tagNamesWhichShouldNotSelectARow.has(e.target.tagName)
     ) {
       return;
     }
@@ -109,7 +78,7 @@ const getRowProps = (rowProps, { row, instance }) => {
     {
       onKeyDown: (e, selectionCellClick = false) => {
         if (e.key === 'Enter' || e.code === 'Space') {
-          if (!tagNamesWhichShouldNotSelectARow.has(e.target.tagName)) {
+          if (!webComponentsReactProperties.tagNamesWhichShouldNotSelectARow.has(e.target.tagName)) {
             e.preventDefault();
           }
           handleRowSelect(e, selectionCellClick);
