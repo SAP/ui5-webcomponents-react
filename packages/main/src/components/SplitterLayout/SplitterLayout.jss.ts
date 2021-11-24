140b1ea371d1655aa62eb38e543f65eef9b9fc34
Marcus Notheis <marcus.notheis@sap.com>
2021-11-24 10:00:40 +0100

    Update SplitterLayout.jss.ts

--- packages/main/src/components/SplitterLayout/SplitterLayout.jss.ts
+++ packages/main/src/components/SplitterLayout/SplitterLayout.jss.ts
@@ -1,37 +1,22 @@
-import { ThemingParameters } from '@ui5/webcomponents-react-base/dist/ThemingParameters';
-import { FlexBoxDirection } from '@ui5/webcomponents-react/dist/FlexBoxDirection';
-
 const styles = {
-  splitterLayout: (props) => ({
-    display: 'flex',
-    overflow: 'hidden',
-    flexDirection: props?.orientation === 'vertical' ? FlexBoxDirection.Row : FlexBoxDirection.Column,
-    flex: `0 0 ${props?.orientation === 'vertical' ? props?.width : props?.height}`,
-    width: props.width,
-    height: props.height
-  }),
-  splitterElementVertical: (props) => ({
+  splitterLayout: {
     display: 'flex',
     overflow: 'hidden',
     position: 'relative',
-    backgroundColor: ThemingParameters.sapBackgroundColor,
-    height: props?.containerHeight,
-    flex: !!props?.size && props?.size !== 'auto' ? `0 0 ${props?.size}` : '1 0 auto',
-    minWidth: props?.minSize ?? 0,
-    maxWidth: props?.maxSize,
-    willChange: 'flex'
-  }),
-  splitterElementHorizontal: (props) => ({
-    display: 'flex',
-    overflow: 'hidden',
-    position: 'relative',
-    backgroundColor: ThemingParameters.sapBackgroundColor,
-    width: props?.containerWidth,
-    flex: !!props?.size || props?.size === 'auto' ? `0 0 ${props?.size}` : '1 0 auto',
-    minHeight: props?.minSize ?? 0,
-    maxHeight: props?.maxSize,
-    willChange: 'flex'
-  })
+    '&[data-splitter-orientation="vertical"]': {
+      flexDirection: 'row',
+      width: '100%',
+      '& > *': {
+        height: '100%'
+      }
+    },
+    '&[data-splitter-orientation="horizontal"]': {
+      flexDirection: 'column',
+      height: '100%',
+      '& > *': {
+        width: '100%'
+      }
+    }
+  }
 };
-
 export { styles };
