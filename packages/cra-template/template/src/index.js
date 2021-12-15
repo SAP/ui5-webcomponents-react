3ba8700f13a2dfec08178d163bf4df1928f95ef3
Marcus Notheis <marcus.notheis@sap.com>
2021-12-15 08:17:20 +0100

    fix: use fully specified paths for all web component imports

--- packages/cra-template/template/src/index.js
+++ packages/cra-template/template/src/index.js
@@ -1,6 +1,6 @@
 import '@ui5/webcomponents/dist/Assets.js';
 import '@ui5/webcomponents-fiori/dist/Assets.js';
-import '@ui5/webcomponents-react/dist/Assets';
+import '@ui5/webcomponents-react/dist/Assets.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
