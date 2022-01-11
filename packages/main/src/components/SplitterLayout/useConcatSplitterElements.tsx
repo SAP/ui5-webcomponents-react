eea05360310ce334be1d3eed77e9b879b72fd55d
Harbarth, Lukas <lukas.harbarth@sap.com>
2022-01-11 16:54:10 +0100

    cleanup

--- packages/main/src/components/SplitterLayout/useConcatSplitterElements.tsx
+++ packages/main/src/components/SplitterLayout/useConcatSplitterElements.tsx
@@ -28,13 +28,7 @@ export const useConcatSplitterElements = (
         childrenArray.splice(
           index + splitterCount + 1,
           0,
-          <Splitter
-            key={`splitter${index}`}
-            height={height}
-            width={width}
-            vertical={vertical}
-            data-splitter-index={index}
-          />
+          <Splitter key={`splitter${index}`} height={height} width={width} vertical={vertical} />
         );
         ++splitterCount;
       } else if (index > 0 && splitterElementChild?.props.resizable === false) {
