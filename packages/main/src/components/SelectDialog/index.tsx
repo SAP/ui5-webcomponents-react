ff4d2076fc2c9b3cf2c0bcfba9903ed6bef66732
Lukas Harbarth <lukas.harbarth@sap.com>
2022-01-19 10:38:42 +0100

    feat(SelectDialog): make infobar sticky (#2504)

--- packages/main/src/components/SelectDialog/index.tsx
+++ packages/main/src/components/SelectDialog/index.tsx
@@ -38,7 +38,7 @@ const useStyles = createUseStyles(
     spread: { width: '100%' },
     noShadow: { boxShadow: 'none' },
     inputIcon: { cursor: 'pointer', color: ThemingParameters.sapContent_IconColor },
-    infoBar: { padding: '0 0.5rem' }
+    infoBar: { padding: '0 0.5rem', position: 'sticky', top: 0, zIndex: 1 }
   },
   { name: 'SelectDialog' }
 );
@@ -344,20 +344,17 @@ const SelectDialog = forwardRef((props: SelectDialogPropTypes, ref: Ref<DialogDo
           }
         />
       </Bar>
+      {mode === ListMode.MultiSelect && (!!selectedItems.length || numberOfSelectedItems > 0) && (
+        <Toolbar design={ToolbarDesign.Info} className={classes.infoBar}>
+          <Text>{`${i18nBundle.getText(SELECTED)}: ${numberOfSelectedItems ?? selectedItems.length}`}</Text>
+        </Toolbar>
+      )}
       <List
         {...listProps}
         ref={listComponentRef}
         growing={growing}
         onLoadMore={onLoadMore}
         mode={mode}
-        header={
-          mode === ListMode.MultiSelect &&
-          (!!selectedItems.length || numberOfSelectedItems > 0) && (
-            <Toolbar design={ToolbarDesign.Info} className={classes.infoBar}>
-              <Text>{`${i18nBundle.getText(SELECTED)}: ${numberOfSelectedItems ?? selectedItems.length}`}</Text>
-            </Toolbar>
-          )
-        }
         onSelectionChange={handleSelectionChange}
       >
         {listMounted && children}
