8c8906b151b0b453e5443f017595f96a3e65cf50
Marcus Notheis <marcus.notheis@sap.com>
2022-01-21 20:51:24 +0100

    create or update enums

---
+++ packages/main/src/dist/MediaGalleryLayout.ts
@@ -0,0 +1,5 @@
+// Generated file - do not change manually!
+
+import { MediaGalleryLayout } from '../enums/MediaGalleryLayout';
+
+export { MediaGalleryLayout };
---
+++ packages/main/src/enums/MediaGalleryLayout.ts
@@ -0,0 +1,21 @@
+// Generated file - do not change manually!
+
+/**
+ * Defines the layout type of the thumbnails list of the <code>ui5-media-gallery</code> component.
+ */
+export enum MediaGalleryLayout {
+  /**
+   * The layout is determined automatically.
+   */
+  Auto = 'Left',
+
+  /**
+   * Displays the layout as a horizontal split between the thumbnails list and the selected image.
+   */
+  Horizontal = 'Right',
+
+  /**
+   * Displays the layout as a vertical split between the thumbnails list and the selected image.
+   */
+  Vertical = 'Left'
+}
