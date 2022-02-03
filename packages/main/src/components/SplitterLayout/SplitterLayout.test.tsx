8b336fb11814e839975fa5bab98407fb4fcc1b4b
D071203 <julian.klein@sap.com>
2022-02-03 12:43:54 +0100

    fix/SplitterLayout: tests

--- packages/main/src/components/SplitterLayout/SplitterLayout.test.tsx
+++ packages/main/src/components/SplitterLayout/SplitterLayout.test.tsx
@@ -52,7 +52,7 @@ describe('SplitterLayout', () => {
 
     expect(screen.getByTestId('SplitterLayout')).toHaveStyle('width: 800px; height: 400px');
     expect(screen.getByTestId('SplitterElement1')).toHaveStyle('flex: 0 0 auto');
-    expect(screen.getByTestId('SplitterElement2')).toHaveStyle('flex: 0 0 auto; min-width: 300px');
+    expect(screen.getByTestId('SplitterElement2')).toHaveStyle('flex: 0 0 auto; min-height: 300px');
     expect(screen.getByTestId('SplitterElement2').dataset.minSize).toBe('300');
     expect(screen.getByTestId('SplitterElement3')).toHaveStyle('flex: 1 0 auto');
     expect(onClick).toHaveBeenCalled();
@@ -103,8 +103,8 @@ describe('SplitterLayout', () => {
 
     expect(AllSplitter.length).toBe(2);
     expect(screen.getByTestId('Layout')).toHaveStyle('width: 80vw; height: 600px');
-    expect(screen.getByTestId('Layout').dataset.splitterVertical).toBe('false');
-    expect(screen.getByTestId('Element')).toHaveStyle('flex: 0 0 auto; min-height: 50px');
+    expect(screen.getByTestId('Layout').dataset.splitterVertical).toBe(undefined);
+    expect(screen.getByTestId('Element')).toHaveStyle('flex: 0 0 auto; min-width: 50px');
     expect(screen.getByTestId('Element').dataset.minSize).toBe('50');
     expect(asFragment()).toMatchSnapshot();
   });
@@ -136,14 +136,14 @@ describe('SplitterLayout', () => {
 
     fireEvent.keyDown(document.activeElement, keyDownOptions('Left'));
 
-    expect(getByTestId('SplitterElement3').style.flexBasis).toBe('180px');
-    expect(getByTestId('SplitterElement4').style.flexBasis).toBe('220px');
+    expect(getByTestId('SplitterElement3').style.flexBasis).toBe('780px');
+    expect(getByTestId('SplitterElement4').style.flexBasis).toBe('820px');
 
     fireEvent.keyDown(document.activeElement, keyDownOptions('Right'));
     fireEvent.keyDown(document.activeElement, keyDownOptions('Right'));
 
-    expect(getByTestId('SplitterElement3').style.flexBasis).toBe('220px');
-    expect(getByTestId('SplitterElement4').style.flexBasis).toBe('180px');
+    expect(getByTestId('SplitterElement3').style.flexBasis).toBe('820px');
+    expect(getByTestId('SplitterElement4').style.flexBasis).toBe('780px');
   });
 
   test('Splitter click, move, focus - horizontal', () => {
@@ -167,19 +167,19 @@ describe('SplitterLayout', () => {
 
     fireEvent.keyDown(document.activeElement, keyDownOptions('Up'));
 
-    expect(getByTestId('SplitterElement3').style.flex).toBe('0 0 780px');
-    expect(getByTestId('SplitterElement4').style.flex).toBe('0 0 820px');
+    expect(getByTestId('SplitterElement3').style.flex).toBe('0 0 180px');
+    expect(getByTestId('SplitterElement4').style.flex).toBe('0 0 220px');
 
     fireEvent.keyDown(document.activeElement, keyDownOptions('Down'));
     fireEvent.keyDown(document.activeElement, keyDownOptions('Down'));
 
-    expect(getByTestId('SplitterElement3').style.flex).toBe('0 0 820px');
-    expect(getByTestId('SplitterElement4').style.flex).toBe('0 0 780px');
+    expect(getByTestId('SplitterElement3').style.flex).toBe('0 0 220px');
+    expect(getByTestId('SplitterElement4').style.flex).toBe('0 0 180px');
   });
 
   test('Splitter click, move, focus - vertical - RTL', () => {
     const { getByTestId } = renderRtl(
-      <SplitterLayout vertical style={{ width: '800px', height: '800px' }} data-testid={'SplitterLayout'}>
+      <SplitterLayout style={{ width: '800px', height: '800px' }} data-testid={'SplitterLayout'}>
         <SplitterElement data-testid={'SplitterElement1'}>Content 1</SplitterElement>
         <SplitterElement data-testid={'SplitterElement2'}>Content 2</SplitterElement>
         <SplitterElement data-testid={'SplitterElement3'} size={'200px'}>
