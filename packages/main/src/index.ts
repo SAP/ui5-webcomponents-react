0b48e3361f39c50997e9472c2c56b7e2eb855234
Marcus Notheis <marcus.notheis@sap.com>
2022-02-08 09:19:47 +0100

    re-export all custom component proptypes

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,78 +1,41 @@
-import { ActionSheet } from './components/ActionSheet';
-import { AnalyticalCard } from './components/AnalyticalCard';
-import { AnalyticalCardHeader } from './components/AnalyticalCardHeader';
-import { AnalyticalTable } from './components/AnalyticalTable';
-import { TablePlaceholder } from './components/AnalyticalTable/defaults/LoadingComponent/TablePlaceholder';
 import * as AnalyticalTableHooks from './components/AnalyticalTable/pluginHooks/AnalyticalTableHooks';
-import { DynamicPage } from './components/DynamicPage';
-import { DynamicPageHeader } from './components/DynamicPageHeader';
-import { DynamicPageTitle } from './components/DynamicPageTitle';
-import { FilterBar } from './components/FilterBar';
-import { FilterGroupItem } from './components/FilterGroupItem';
-import { FlexBox } from './components/FlexBox';
-import { Form } from './components/Form';
-import { FormGroup } from './components/FormGroup';
-import { FormItem } from './components/FormItem';
-import { Grid } from './components/Grid';
-import { Loader } from './components/Loader';
-import { MessageBox } from './components/MessageBox';
-import { MessageView } from './components/MessageView';
-import { MessageItem } from './components/MessageView/MessageItem';
-import { ObjectPage } from './components/ObjectPage';
-import { ObjectPageSection } from './components/ObjectPageSection';
-import { ObjectPageSubSection } from './components/ObjectPageSubSection';
-import { ObjectStatus } from './components/ObjectStatus';
-import { ResponsiveGridLayout } from './components/ResponsiveGridLayout';
-import { SelectDialog } from './components/SelectDialog';
-import { SplitterElement } from './components/SplitterElement';
-import { SplitterLayout } from './components/SplitterLayout';
-import { Text } from './components/Text';
-import { ThemeProvider } from './components/ThemeProvider';
-import { Toolbar } from './components/Toolbar';
-import { ToolbarSeparator } from './components/ToolbarSeparator';
-import { ToolbarSpacer } from './components/ToolbarSpacer';
-import { VariantManagement } from './components/VariantManagement';
-import { VariantItem } from './components/VariantManagement/VariantItem';
-import { withWebComponent } from './internal/withWebComponent';
 
+export * from './components/ActionSheet';
+export * from './components/AnalyticalCard';
+export * from './components/AnalyticalCardHeader';
+export * from './components/AnalyticalTable';
+export * from './components/AnalyticalTable/defaults/LoadingComponent/TablePlaceholder';
+export * from './components/DynamicPage';
+export * from './components/DynamicPageHeader';
+export * from './components/DynamicPageTitle';
+export * from './components/FilterBar';
+export * from './components/FilterGroupItem';
+export * from './components/FlexBox';
+export * from './components/Form';
+export * from './components/FormGroup';
+export * from './components/FormItem';
+export * from './components/Grid';
+export * from './components/Loader';
+export * from './components/MessageBox';
+export * from './components/MessageView';
+export * from './components/MessageView/MessageItem';
+export * from './components/ObjectPage';
+export * from './components/ObjectPageSection';
+export * from './components/ObjectPageSubSection';
+export * from './components/ObjectStatus';
+export * from './components/ResponsiveGridLayout';
+export * from './components/SelectDialog';
+export * from './components/SplitterElement';
+export * from './components/SplitterLayout';
+export * from './components/Text';
+export * from './components/ThemeProvider';
+export * from './components/Toolbar';
+export * from './components/ToolbarSeparator';
+export * from './components/ToolbarSpacer';
+export * from './components/VariantManagement';
+export * from './components/VariantManagement/VariantItem';
+export * from './internal/withWebComponent';
 export * from './enums';
 export * from './webComponents';
 
-export {
-  ActionSheet,
-  AnalyticalCard,
-  AnalyticalCardHeader,
-  AnalyticalTable,
-  AnalyticalTableHooks,
-  DynamicPage,
-  DynamicPageHeader,
-  DynamicPageTitle,
-  FilterBar,
-  FilterGroupItem,
-  FlexBox,
-  Form,
-  FormGroup,
-  FormItem,
-  Grid,
-  Loader,
-  MessageBox,
-  MessageItem,
-  MessageView,
-  ObjectPage,
-  ObjectPageSection,
-  ObjectPageSubSection,
-  ObjectStatus,
-  ResponsiveGridLayout,
-  SelectDialog,
-  SplitterElement,
-  SplitterLayout,
-  TablePlaceholder,
-  Text,
-  ThemeProvider,
-  Toolbar,
-  ToolbarSeparator,
-  ToolbarSpacer,
-  VariantItem,
-  VariantManagement,
-  withWebComponent
-};
+export { AnalyticalTableHooks };
