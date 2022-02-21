8c5a4ca11ef64f90d16bda340a5a5f6028459c59
Marcus Notheis <marcus.notheis@sap.com>
2022-02-21 12:19:31 +0100

    create redirects for components as well

--- packages/main/scripts/build-redirects.mjs
+++ packages/main/scripts/build-redirects.mjs
@@ -4,27 +4,32 @@ import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import rimraf from 'rimraf';
 
-const dir = fileURLToPath(new URL('../webComponents', import.meta.url));
+function createRedirects(folder) {
+  const dir = fileURLToPath(new URL(`../${folder}`, import.meta.url));
 
-rimraf.sync(dir);
+  rimraf.sync(dir);
 
-// copy full src dir
-fs.cpSync(new URL('../dist/webComponents', import.meta.url), dir, { recursive: true });
+  // copy full src dir
+  fs.cpSync(new URL(`../dist/${folder}`, import.meta.url), dir, { recursive: true });
 
-rimraf.sync('webComponents/*/*.js.map');
+  rimraf.sync(`${folder}/*/*.js.map`);
 
-fs.readdirSync(dir)
-  .filter((file) => fs.statSync(resolve(dir, file)).isDirectory())
-  .forEach((component) => {
-    fs.writeFileSync(resolve(dir, component, 'index.d.ts'), `export * from '../../dist/webComponents/${component}';\n`);
-    fs.writeFileSync(
-      resolve(dir, component, 'index.js'),
-      dedent`
+  fs.readdirSync(dir)
+    .filter((file) => fs.statSync(resolve(dir, file)).isDirectory())
+    .forEach((component) => {
+      fs.writeFileSync(resolve(dir, component, 'index.d.ts'), `export * from '../../dist/${folder}/${component}';\n`);
+      fs.writeFileSync(
+        resolve(dir, component, 'index.js'),
+        dedent`
         import { deprecationNotice } from '@ui5/webcomponents-react-base';
        
-        deprecationNotice('${component}', \`importing from '@ui5/webcomponents-react/webComponents/${component}' is deprecated. Please use "import { ${component} } from '@ui5/webcomponents-react';" instead.\`);
+        deprecationNotice('${component}', \`importing from '@ui5/webcomponents-react/${folder}/${component}' is deprecated. Please use "import { ${component} } from '@ui5/webcomponents-react';" instead.\`);
         
-        export * from '../../dist/webComponents/${component}';\n
+        export * from '../../dist/${folder}/${component}';\n
         `
-    );
-  });
+      );
+    });
+}
+
+createRedirects('components');
+createRedirects('webComponents');
