310e0c9762207b713d875a9034b50af3c7638ab8
Marcus Notheis <marcus.notheis@sap.com>
2022-03-07 07:46:17 +0100

    add basic tests

---
+++ packages/main/src/components/Modals/Modals.test.tsx
@@ -0,0 +1,76 @@
+import { render, screen } from '@shared/tests';
+import React from 'react';
+import { Modals } from './index';
+
+describe('Modals', function () {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  test('showDialog', () => {
+    Modals.showDialog({
+      children: 'Dialog Content',
+      'data-testid': 'dialog'
+    });
+
+    jest.runOnlyPendingTimers();
+
+    expect(screen.getByTestId('dialog')).toHaveAttribute('open', 'true');
+    expect(screen.getByText('Dialog Content')).toBeInTheDocument();
+  });
+
+  test('showPopover', () => {
+    render(<span id="opener" />);
+    Modals.showPopover({
+      opener: 'opener',
+      children: 'Popover Content',
+      'data-testid': 'popover'
+    });
+
+    jest.runOnlyPendingTimers();
+
+    expect(screen.getByTestId('popover')).toHaveAttribute('open', 'true');
+    expect(screen.getByText('Popover Content')).toBeInTheDocument();
+  });
+
+  test('showResponsivePopover', () => {
+    render(<span id="opener" />);
+    Modals.showResponsivePopover({
+      opener: 'opener',
+      children: 'ResponsivePopover Content',
+      'data-testid': 'responsivepopover'
+    });
+
+    jest.runOnlyPendingTimers();
+
+    expect(screen.getByTestId('responsivepopover')).toHaveAttribute('open', 'true');
+    expect(screen.getByText('ResponsivePopover Content')).toBeInTheDocument();
+  });
+
+  test('showMessageBox', async () => {
+    Modals.showMessageBox({
+      children: 'MessageBox Content',
+      'data-testid': 'messagebox'
+    });
+
+    await screen.findByText('Confirmation');
+
+    jest.runOnlyPendingTimers();
+
+    expect(screen.getByTestId('messagebox')).toHaveAttribute('open', 'true');
+    expect(screen.getByText('MessageBox Content')).toBeInTheDocument();
+  });
+
+  test('showToast', () => {
+    Modals.showToast({
+      children: 'Toast Content'
+    });
+
+    jest.runOnlyPendingTimers();
+
+    expect(screen.getByText('Toast Content')).toBeInTheDocument();
+  });
+});
