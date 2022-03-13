bf632ea5a0d8b6b4e959bd1b01e5543620cd6e4a
Marcus Notheis <marcus.notheis@sap.com>
2022-03-13 08:18:12 +0100

    replace Empty component with return null

--- packages/cra-template-seed/template/src/auth/PageWithAuthorizationRestriction.js
+++ packages/cra-template-seed/template/src/auth/PageWithAuthorizationRestriction.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useHasAccess } from '../hooks/useAuthority';
-import Empty from '../pages/Fallback/Empty';
 
 import NotFound from '../pages/Fallback/NotFound';
 
 const PageWithAuthorizationRestriction = ({ allowedAuthorities, authorityKey, children, ...props }) => {
   const hasAccess = useHasAccess(allowedAuthorities, authorityKey);
   if (hasAccess === null) {
-    return <Empty />;
+    return null;
   }
 
   if (!hasAccess) {
