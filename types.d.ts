cc2fe728a47a42c5719a57f46ebdd8281d75dec7
Marcus Notheis <marcus.notheis@sap.com>
2022-04-05 09:54:05 +0200

    fix(i18nBundle): support micro front ends (#2743)

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,15 @@
-interface Window {
-  CSSVarsPonyfill: {
-    cssVars: (options: any) => void;
-  };
+import { Context } from 'react';
+
+declare global {
+  interface Window {
+    CSSVarsPonyfill: {
+      cssVars: (options: any) => void;
+    };
+
+    ['@ui5/webcomponents-react']: {
+      I18nContext?: Context<any>;
+    };
+  }
 }
 
 declare module '*.md' {
