928af55a41e87d7ddba7764f02dd50d0cb386fec
Marcus Notheis <marcus.notheis@sap.com>
2022-04-15 07:26:32 +0200

    create Menu and Menu Item

---
+++ packages/main/src/webComponents/Menu/index.tsx
@@ -0,0 +1,47 @@
+import { withWebComponent } from '../../internal/withWebComponent';
+import { CommonProps } from '../../interfaces/CommonProps';
+import { Ui5DomRef } from '../../interfaces/Ui5DomRef';
+import '@ui5/webcomponents/dist/Menu.js';
+import { ReactNode } from 'react';
+import { Ui5CustomEvent } from '../../interfaces/Ui5CustomEvent';
+
+interface MenuAttributes {
+  /**
+   * Defines the header text of the menu (displayed on mobile).
+   */
+  headerText?: string;
+}
+
+export interface MenuDomRef extends MenuAttributes, Ui5DomRef {
+  /**
+   * Closes the Menu.
+   */
+  close: () => void;
+  /**
+   * Shows the Menu near the opener element.
+   * @param {HTMLElement | EventTarget} opener - the element that the popover is shown at
+   */
+  showAt: (opener: HTMLElement | EventTarget) => void;
+}
+
+export interface MenuPropTypes extends MenuAttributes, CommonProps {
+  /**
+   * Defines the items of this component.
+   */
+  children?: ReactNode | ReactNode[];
+  /**
+   * Fired when an item is being clicked.
+   */
+  onItemClick?: (event: Ui5CustomEvent<HTMLElement, { item: Record<string, unknown>; text: string }>) => void;
+}
+
+/**
+ * `Menu` component represents a hierarchical menu structure.
+ *
+ * <ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/Menu" target="_blank">UI5 Web Components Playground</ui5-link>
+ */
+const Menu = withWebComponent<MenuPropTypes, MenuDomRef>('ui5-menu', ['headerText'], [], [], ['item-click']);
+
+Menu.displayName = 'Menu';
+
+export { Menu };
---
+++ packages/main/src/webComponents/MenuItem/index.tsx
@@ -0,0 +1,54 @@
+import { withWebComponent } from '../../internal/withWebComponent';
+import { CommonProps } from '../../interfaces/CommonProps';
+import { Ui5DomRef } from '../../interfaces/Ui5DomRef';
+import '@ui5/webcomponents/dist/MenuItem.js';
+import { ReactNode } from 'react';
+
+interface MenuItemAttributes {
+  /**
+   * Defines whether `MenuItem` is in disabled state.
+   *
+   * **Note:** A disabled `MenuItem` is noninteractive.
+   */
+  disabled?: boolean;
+  /**
+   * Defines the icon to be displayed as graphical element within the component. The SAP-icons font provides numerous options.
+   *
+   * **\* Example:** See all the available icons in the <ui5-link target="_blank" href="https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html" class="api-table-content-cell-link">Icon Explorer</ui5-link>.
+   */
+  icon?: string;
+  /**
+   * Defines whether a visual separator should be rendered before the item.
+   */
+  startsSection?: boolean;
+  /**
+   * Defines the text of the tree item.
+   */
+  text?: string;
+}
+
+export interface MenuItemDomRef extends MenuItemAttributes, Ui5DomRef {}
+
+export interface MenuItemPropTypes extends MenuItemAttributes, CommonProps {
+  /**
+   * Defines the items of this component.
+   */
+  children?: ReactNode | ReactNode[];
+}
+
+/**
+ * `MenuItem` is the item to use inside a `Menu`. An arbitrary hierarchy structure can be represented by recursively nesting menu items.
+ *
+ * <ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/MenuItem" target="_blank">UI5 Web Components Playground</ui5-link>
+ */
+const MenuItem = withWebComponent<MenuItemPropTypes, MenuItemDomRef>(
+  'ui5-menu-item',
+  ['icon', 'text'],
+  ['disabled', 'startsSection'],
+  [],
+  []
+);
+
+MenuItem.displayName = 'MenuItem';
+
+export { MenuItem };
