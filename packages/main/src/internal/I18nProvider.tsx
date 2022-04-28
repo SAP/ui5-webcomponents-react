80834a1dbdd1c1a47389b2f1d1e95ffe3f592b1d
Marcus Notheis <marcus.notheis@sap.com>
2022-04-28 09:05:09 +0200

    fix(i18nProvider): make sure I18nContext is always available

--- packages/main/src/internal/I18nProvider.tsx
+++ packages/main/src/internal/I18nProvider.tsx
@@ -19,6 +19,13 @@ const bundleReducer = (currentBundles: I18nBundleMap, action: { payload: I18nBun
 export function I18nProvider({ children }: I18nProviderPropTypes): JSX.Element {
   const [i18nBundles, setI18nBundles] = useReducer(bundleReducer, {});
   const localBundles = useRef<I18nBundleMap>({});
+  const isSyncedWithWindow = useRef(false);
+
+  if (!isSyncedWithWindow.current && typeof window !== 'undefined') {
+    window['@ui5/webcomponents-react'] ??= {};
+    window['@ui5/webcomponents-react'].I18nContext = I18nContext;
+    isSyncedWithWindow.current = true;
+  }
 
   const setI18nBundle = useCallback((name: string, bundle: I18nBundle) => {
     setI18nBundles({
@@ -30,10 +37,6 @@ export function I18nProvider({ children }: I18nProviderPropTypes): JSX.Element {
   }, []);
 
   useEffect(() => {
-    if (!window['@ui5/webcomponents-react']) {
-      window['@ui5/webcomponents-react'] = {};
-    }
-    window['@ui5/webcomponents-react'].I18nContext = I18nContext;
     let isMounted = true;
     const fetchI18n = async () => {
       const bundles = await Promise.all(
