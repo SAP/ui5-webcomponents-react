939b912d44fc887309abdf7ba46f55983e1a5717
Marcus Notheis <marcus.notheis@sap.com>
2022-05-03 08:42:55 +0200

    add custom variables for object section and sub section

--- packages/main/src/themes/CustomVariables.ts
+++ packages/main/src/themes/CustomVariables.ts
@@ -1,5 +1,8 @@
 export enum CustomVariables {
-  ObjectPageSectionTitleFontFamily = '--ui5wcr_ObjectPage_SectionTitleFontFamily'
+  ObjectPageSectionTitleFontFamily = '--ui5wcr_ObjectPage_SectionTitleFontFamily',
+  ObjectPageSectionBorderTop = '--ui5wcr_ObjectPage_SectionBorderTop',
+  ObjectPageSubSectionBackgroundColor = '--ui5wcr_ObjectPage_SubSectionBackgroundColor',
+  ObjectPageSubSectionBorderRadius = '--ui5wcr_ObjectPage_SubSectionBorderRadius'
 }
 
 export const CustomThemingParameters = new Proxy(CustomVariables, {
