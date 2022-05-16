693391ead636ec550bf22fbd9f88e9b3d4f614e5
Harbarth, Lukas <lukas.harbarth@sap.com>
2022-05-16 08:52:44 +0200

    fix(useViewPortRange): add optional chaining

--- packages/base/src/hooks/useViewportRange.ts
+++ packages/base/src/hooks/useViewportRange.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { getCurrentRange, attachMediaHandler, detachMediaHandler } from '../Device';
 
 export const useViewportRange = () => {
-  const [currentRange, setCurrentRange] = useState(getCurrentRange().name);
+  const [currentRange, setCurrentRange] = useState(getCurrentRange()?.name);
 
   useEffect(() => {
     let isMounted = true;
