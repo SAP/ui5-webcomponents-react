a43deee3b3f85fba689e1a2bfb0b5de2fc76edad
Lukas Harbarth <lukas.harbarth@sap.com>
2022-05-20 17:06:27 +0200

    add date to "copied from" note

--- packages/main/src/internal/useRect.ts
+++ packages/main/src/internal/useRect.ts
@@ -1,4 +1,4 @@
-// copied from https://github.com/TanStack/react-virtual/blob/061075751c3fad630710c0f7794e587a6cf9dda0/src/useRect.js (modified)
+// copied from https://github.com/TanStack/react-virtual/blob/061075751c3fad630710c0f7794e587a6cf9dda0/src/useRect.js (20. May 2022) (modified)
 import { useIsomorphicLayoutEffect } from '@ui5/webcomponents-react-base';
 import { useEffect, useReducer, useRef, useState } from 'react';
 
