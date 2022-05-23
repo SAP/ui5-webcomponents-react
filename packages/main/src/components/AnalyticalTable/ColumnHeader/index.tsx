fc5606c01dfd2fd9e041b4f500d5aa2e0d9d8f6d
Harbarth, Lukas <lukas.harbarth@sap.com>
2022-05-23 10:27:05 +0200

    fix(AnalyticalTable): don't wrap Header elements inside `Text`

--- packages/main/src/components/AnalyticalTable/ColumnHeader/index.tsx
+++ packages/main/src/components/AnalyticalTable/ColumnHeader/index.tsx
@@ -219,10 +219,18 @@ export const ColumnHeader: FC<ColumnHeaderProps> = (props: ColumnHeaderProps) =>
         onKeyUp={handleHeaderCellKeyUp}
       >
         <div className={classes.header} data-h-align={column.hAlign}>
-          <Text title={tooltip} wrapping={false} style={textStyle} className={classes.text}>
-            {children}
-          </Text>
-          <div className={classes.iconContainer} style={iconContainerDirectionStyles}>
+          {typeof children === 'string' ? (
+            <Text title={tooltip} wrapping={false} style={textStyle} className={classes.text}>
+              {children}
+            </Text>
+          ) : (
+            children
+          )}
+          <div
+            className={classes.iconContainer}
+            style={iconContainerDirectionStyles}
+            data-component-name={`AnalyticalTableHeaderIconsContainer-${id}`}
+          >
             {isFiltered && <Icon name="filter" />}
             {column.isSorted && <Icon name={column.isSortedDesc ? 'sort-descending' : 'sort-ascending'} />}
             {column.isGrouped && <Icon name="group-2" />}
