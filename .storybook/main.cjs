e032a6e1fbdd3fb266d91a6cd76a693f64f6ea09
Marcus Notheis <marcus.notheis@sap.com>
2022-06-20 09:22:26 +0200

    docs: add changelog to storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,7 +3,10 @@ const root = path.resolve(__dirname, '..');
 
 const isDevMode = process.env.NODE_ENV === 'development';
 
-const addons = ['@storybook/addon-essentials'];
+const addons = [
+  '@storybook/addon-essentials',
+  { name: '@storybook/addon-docs', options: { transcludeMarkdown: true } }
+];
 
 if (isDevMode) {
   addons.push('@storybook/addon-a11y');
@@ -13,7 +16,9 @@ const isChromatic = process.env.STORYBOOK_ENV === 'chromatic';
 
 module.exports = {
   framework: '@storybook/react',
-  stories: isChromatic ? ['../packages/main/src/components/**/*.stories.@(tsx|jsx|mdx)'] : ['../docs/**/*.stories.mdx', '../packages/**/*.stories.@(tsx|jsx|mdx)'],
+  stories: isChromatic
+    ? ['../packages/main/src/components/**/*.stories.@(tsx|jsx|mdx)']
+    : ['../docs/**/*.stories.mdx', '../packages/**/*.stories.@(tsx|jsx|mdx)'],
   addons: addons,
   core: {
     builder: 'webpack5'
@@ -26,7 +31,7 @@ module.exports = {
       '@ui5/webcomponents-react/dist/Assets': path.join(root, 'packages', 'main', 'dist', 'Assets'),
       '@ui5/webcomponents-react': path.join(root, 'packages', 'main', 'src'),
       '@ui5/webcomponents-react-charts': path.join(root, 'packages', 'charts', 'src'),
-      '@ui5/webcomponents-react-base$': path.join(root, 'packages', 'base', 'src', 'index.ts'),
+      '@ui5/webcomponents-react-base$': path.join(root, 'packages', 'base', 'src', 'index.ts')
     };
     return config;
   },
@@ -39,7 +44,7 @@ module.exports = {
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
       propFilter: (prop) => {
-        return (prop.parent ? !/(@types\/react|@emotion|@storybook)/.test(prop.parent.fileName) : true)
+        return prop.parent ? !/(@types\/react|@emotion|@storybook)/.test(prop.parent.fileName) : true;
       }
     }
   },
