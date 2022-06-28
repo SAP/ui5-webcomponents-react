001eb4af1b571be00ed22a354d0059b34a57b0c7
Marcus Notheis <marcus.notheis@sap.com>
2022-06-28 22:37:48 +0200

    remove deprecated entrypoints from charts

--- packages/charts/src/ComposedChartPlaceholder.ts
+++
@@ -1,8 +0,0 @@
-import { deprecationNotice } from '@ui5/webcomponents-react-base';
-import { ComposedChartPlaceholder } from './components/ComposedChart/Placeholder';
-
-deprecationNotice(
-  'ComposedChartPlaceholder',
-  `importing from '@ui5/webcomponents-react-charts/dist/ComposedChartPlaceholder' is deprecated. Please use "import { ComposedChartPlaceholder } from '@ui5/webcomponents-react-charts';" instead.`
-);
-export { ComposedChartPlaceholder };
