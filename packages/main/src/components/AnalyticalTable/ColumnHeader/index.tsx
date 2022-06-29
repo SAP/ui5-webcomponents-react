6b18a4bc094b17736e57cf54bb0525901075dfd6
Lukas Harbarth <lukas.harbarth@sap.com>
2022-06-29 14:31:13 +0200

    fix(AnalyticalTable): announce column header popover with screen readers (#3159)
    Fixes #3132

--- packages/main/src/components/AnalyticalTable/ColumnHeader/index.tsx
+++ packages/main/src/components/AnalyticalTable/ColumnHeader/index.tsx
@@ -206,6 +206,7 @@ export const ColumnHeader: FC<ColumnHeaderProps> = (props: ColumnHeaderProps) =>
           cursor: hasPopover ? 'pointer' : 'auto',
           borderLeft: dragOver ? `3px solid ${ThemingParameters.sapSelectedColor}` : undefined
         }}
+        aria-haspopup={hasPopover ? 'menu' : undefined}
         role={role}
         draggable={isDraggable}
         onDragEnter={onDragEnter}
