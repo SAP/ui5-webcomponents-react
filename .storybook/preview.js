5e64c8b36126c4040781470044aeaa6ffea5da4c
Lukas Harbarth <lukas.harbarth@sap.com>
2022-08-10 11:13:19 +0200

    docs(Storybook): implement workaround for anchors

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -46,6 +46,51 @@ const ThemeProviderDecorator = makeDecorator({
   wrapper: (Story, context) => {
     const { theme, contentDensity, direction } = context.globals;
 
+    // todo remove this once mdx anchors are working again (https://github.com/storybookjs/storybook/issues/18395)
+    useEffect(() => {
+      const onAnchorClick = (e) => {
+        const { hash } = e.currentTarget;
+        const { origin, search } = window.parent.location;
+        window.parent.location.replace(`${origin}/${search}${hash}`);
+      };
+      const toKebabCase = (str) =>
+        str &&
+        str
+          .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+          .map((x) => x.toLowerCase())
+          .join('-');
+
+      const svg = document.getElementById('temporary-svg');
+      const toc = document.getElementById('toc-container');
+
+      if (toc?.querySelector('ul')?.children.length) {
+        Array.from(toc?.querySelector('ul').querySelectorAll('a')).forEach((item) => {
+          item.setAttribute('href', `#${toKebabCase(item.textContent)}`);
+        });
+      }
+
+      const headings = [
+        ...Array.from(document.querySelectorAll('h2:not([id])')),
+        ...Array.from(document.querySelectorAll('h3:not([id])')),
+        ...Array.from(document.querySelectorAll('h4:not([id])'))
+      ];
+
+      headings.forEach((item) => {
+        const ae = document.createElement('a');
+        ae.setAttribute('target', '_self');
+        ae.setAttribute('aria-hidden', 'true');
+        ae.setAttribute('class', 'md-anchor');
+        ae.setAttribute('tab-index', '-1');
+        ae.setAttribute('href', `#${toKebabCase(item.textContent)}`);
+        ae.addEventListener('click', onAnchorClick);
+        const clonedSvg = svg.cloneNode(true);
+        clonedSvg.removeAttribute('style');
+        ae.append(clonedSvg);
+        item.setAttribute('id', toKebabCase(item.textContent));
+        item.appendChild(ae);
+      });
+    }, []);
+
     useEffect(() => {
       if (contentDensity === ContentDensity.Compact) {
         document.body.classList.add('ui5-content-density-compact');
@@ -65,6 +110,21 @@ const ThemeProviderDecorator = makeDecorator({
 
     return (
       <ThemeProvider>
+        <svg
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="16"
+          height="16"
+          aria-hidden="true"
+          fill="currentColor"
+          style={{ display: 'none' }}
+          id="temporary-svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+          />
+        </svg>
         <Story />
       </ThemeProvider>
     );
