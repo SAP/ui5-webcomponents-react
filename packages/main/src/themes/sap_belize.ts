a738c06b585088e1ac25ef087bd3caf44309a699
Etienne Wille <109946790+EtienneWille@users.noreply.github.com>
2022-08-18 08:48:45 +0200

    fix(SplitterLayout): adjust design to comply with guidelines (#3374)

--- packages/main/src/themes/sap_belize.ts
+++ packages/main/src/themes/sap_belize.ts
@@ -3,5 +3,7 @@ import { CustomVariables } from './CustomVariables';
 
 export const sapBelizeParameters: Partial<Record<CustomVariables, string>> = {
   [CustomVariables.ActionSheetTopBorderRadius]: ThemingParameters.sapElement_BorderCornerRadius,
-  [CustomVariables.ActionSheetBoxShadow]: ThemingParameters.sapContent_HeaderShadow
+  [CustomVariables.ActionSheetBoxShadow]: ThemingParameters.sapContent_HeaderShadow,
+  [CustomVariables.SplitterBarBorderFocus]: `.0625rem dotted ${ThemingParameters.sapContent_FocusColor}`,
+  [CustomVariables.SplitterBarBorderFix]: `.0625rem dotted ${ThemingParameters.sapContent_FocusColor}`
 };
--- packages/main/src/themes/sap_fiori_3.ts
+++ packages/main/src/themes/sap_fiori_3.ts
@@ -3,5 +3,7 @@ import { CustomVariables } from './CustomVariables';
 
 export const sapFiori3Parameters: Partial<Record<CustomVariables, string>> = {
   [CustomVariables.ActionSheetTopBorderRadius]: ThemingParameters.sapElement_BorderCornerRadius,
-  [CustomVariables.ActionSheetBoxShadow]: ThemingParameters.sapContent_HeaderShadow
+  [CustomVariables.ActionSheetBoxShadow]: ThemingParameters.sapContent_HeaderShadow,
+  [CustomVariables.SplitterBarBorderFocus]: `.0625rem dotted ${ThemingParameters.sapContent_FocusColor}`,
+  [CustomVariables.SplitterBarBorderFix]: `.0625rem dotted ${ThemingParameters.sapContent_FocusColor}`
 };
