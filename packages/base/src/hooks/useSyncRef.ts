6de1d2792ccde94ccfb1303db726562ab2375681
Marcus Notheis <marcus.notheis@sap.com>
2022-08-20 10:06:35 +0200

    fix(useSyncRef): add useCallback to avoid unnecessary rerenders (#3381)

--- packages/base/src/hooks/useSyncRef.ts
+++ packages/base/src/hooks/useSyncRef.ts
@@ -1,19 +1,22 @@
-import { MutableRefObject, Ref, RefCallback, useRef } from 'react';
+import { MutableRefObject, Ref, RefCallback, useCallback, useRef } from 'react';
 
 export function useSyncRef<RefType = any>(ref: Ref<RefType>): [RefCallback<RefType>, MutableRefObject<RefType>] {
   const localRef = useRef<RefType>(null);
 
-  const componentRef = (node) => {
-    if (ref) {
-      if (typeof ref === 'function') {
-        ref(node);
+  const componentRef = useCallback(
+    (node) => {
+      if (ref) {
+        if (typeof ref === 'function') {
+          ref(node);
+        }
+        if ({}.hasOwnProperty.call(ref, 'current')) {
+          (ref as MutableRefObject<RefType>).current = node;
+        }
       }
-      if ({}.hasOwnProperty.call(ref, 'current')) {
-        (ref as MutableRefObject<RefType>).current = node;
-      }
-    }
-    localRef.current = node;
-  };
+      localRef.current = node;
+    },
+    [ref]
+  );
 
   return [componentRef, localRef];
 }
