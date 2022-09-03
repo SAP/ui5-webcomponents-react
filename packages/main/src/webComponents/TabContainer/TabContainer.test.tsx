cb6fafed0316fa4b3135894d5c59e4ca1ae1769c
Marcus Notheis <marcus.notheis@sap.com>
2022-09-03 07:47:24 +0200

    adjust tab container tests

--- packages/main/src/webComponents/TabContainer/TabContainer.test.tsx
+++ packages/main/src/webComponents/TabContainer/TabContainer.test.tsx
@@ -1,13 +1,18 @@
 import { render } from '@shared/tests';
 import { createCustomPropsTest, createWaitForDefineTest } from '@shared/tests/utils';
 import React from 'react';
+import { Tab } from '../Tab';
 import { TabContainer } from './index';
 
 describe('TabContainer', () => {
   test('Basic Test (generated)', () => {
-    const { asFragment } = render(<TabContainer />);
+    const { asFragment } = render(
+      <TabContainer>
+        <Tab text={'Tab 1'} />
+      </TabContainer>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
-  createCustomPropsTest(TabContainer);
-  createWaitForDefineTest(TabContainer);
+  createCustomPropsTest(TabContainer, { children: <Tab text={'Tab 1'} /> });
+  createWaitForDefineTest(TabContainer, { children: <Tab text={'Tab 1'} /> });
 });
