371b9a13b67bb787b02bcc624138ae09c2118097
Lukas Harbarth <lukas.harbarth@sap.com>
2022-09-09 15:11:52 +0200

    fix(Loader): apply correct styles (#3502)

--- packages/main/src/components/Loader/Loader.jss.ts
+++ packages/main/src/components/Loader/Loader.jss.ts
@@ -1,30 +1,35 @@
 import { ThemingParameters } from '@ui5/webcomponents-react-base';
 
-//todo use theme colors when available
 export const styles = {
   '@keyframes scroll': {
     '0%': { backgroundPosition: '-100% 0' },
     '100%': { backgroundPosition: '200% 0' }
   },
   loader: {
+    position: 'relative',
     height: '0.25rem',
     width: '100%',
+    '&:before': {
+      content: '""',
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      left: 0,
+      backgroundColor: ThemingParameters.sapContent_BusyColor,
+      opacity: 0.15
+    },
     '&$loaderDeterminate': {
-      background: `linear-gradient(to right, ${ThemingParameters.sapContent_IconColor}, ${ThemingParameters.sapContent_IconColor})`,
-      backgroundColor: 'rgba(8, 84, 160, 0.15)',
+      background: `linear-gradient(to right, ${ThemingParameters.sapContent_BusyColor}, ${ThemingParameters.sapContent_BusyColor})`,
       backgroundRepeat: 'repeat-y'
     },
     '&$loaderIndeterminate': {
       background: `linear-gradient(
       to right,
-      rgba(8, 84, 160, 0) 0px,
-      rgba(8, 84, 160, 1) calc(50% - 2rem),
-      ${ThemingParameters.sapContent_IconColor} calc(50% - 2rem),
-      ${ThemingParameters.sapContent_IconColor} calc(50% + 2rem),
-      rgba(8, 84, 160, 1) calc(50% + 2rem),
-      rgba(8, 84, 160, 0) 100%
+      transparent 0px,
+      ${ThemingParameters.sapContent_BusyColor} calc(50% - 2rem),
+      ${ThemingParameters.sapContent_BusyColor} calc(50% + 2rem),
+      transparent 100%
     )`,
-      backgroundColor: 'rgba(8, 84, 160, 0.15)',
       backgroundSize: '40%',
       backgroundRepeat: 'repeat-y',
       animation: '$scroll 1.2s linear infinite'
