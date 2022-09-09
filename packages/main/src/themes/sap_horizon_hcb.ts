f689496e295add0d66c70202349238cfb0c9dc53
Marcus Notheis <marcus.notheis@sap.com>
2022-09-09 15:56:30 +0200

    fix(ObjectPageSection): fix styles for horizon high contrast themes (#3501)

--- packages/main/src/themes/sap_horizon_hcb.ts
+++ packages/main/src/themes/sap_horizon_hcb.ts
@@ -3,6 +3,7 @@ import { CustomVariables } from './CustomVariables';
 
 export const sapHorizonHcbParameters: Partial<Record<CustomVariables, string>> = {
   [CustomVariables.ObjectPageSectionTitleFontFamily]: ThemingParameters.sapFontBoldFamily,
+  [CustomVariables.ObjectPageSectionTitleTextDecoration]: `underline ${ThemingParameters.sapList_SelectionBorderColor}`,
   [CustomVariables.ObjectPageSectionBorderTop]: 'none',
   [CustomVariables.ObjectPageSubSectionBackgroundColor]: ThemingParameters.sapGroup_ContentBackground,
   [CustomVariables.ObjectPageSubSectionBorderRadius]: ThemingParameters.sapElement_BorderCornerRadius,
