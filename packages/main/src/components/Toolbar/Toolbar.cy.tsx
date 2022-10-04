8935a5b325d925efef87aa8118a7af3bbb99404e
Marcus Notheis <marcus.notheis@sap.com>
2022-10-04 09:20:03 +0200

    Toolbar: replace wait

--- packages/main/src/components/Toolbar/Toolbar.cy.tsx
+++ packages/main/src/components/Toolbar/Toolbar.cy.tsx
@@ -64,7 +64,7 @@ describe('Toolbar', () => {
     cy.get('[data-testid="toolbar-item3"]').should('not.be.visible');
 
     // open
-    cy.get(`[tooltip="Show more"]`).click();
+    cy.get(`[tooltip="Show more"]`).click().as('Open Overflow Popover');
 
     cy.findByText('Item1').should('be.visible');
     cy.get('[data-testid="toolbar-item2"]').should('be.visible');
@@ -73,8 +73,8 @@ describe('Toolbar', () => {
     cy.viewport(500, 500);
 
     // close
-    cy.get(`[tooltip="Show more"]`).click();
-    cy.wait(1000);
+    cy.get(`[tooltip="Show more"]`).click().as('Close Overflow Popover');
+    cy.get('[ui5-popover]').should('not.have.attr', 'open');
 
     cy.get('@overflowChangeSpy').should('have.callCount', 2);
 
