404551e7121c2f82faf35a1ebc9718cf3eb63372
Lukas Harbarth <lukas.harbarth@sap.com>
2022-10-17 16:45:27 +0200

    docs(Storybook): don't show `MDXCreateElement` in example code

---
+++ packages/main/src/webComponents/NotificationListItem/CodeGen.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Label } from '../Label';
+
+export const Img = <img src="https://sap.github.io/ui5-webcomponents/assets/images/avatars/woman_avatar_1.png" />;
+export const FootNotes = (
+  <>
+    <Label>John Smith</Label>
+    <Label>1 hour</Label>
+  </>
+);
+export const NotificationDescription = (
+  <>
+    And with a very long description that will not be ellipsed:
+    <br />
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus
+    odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc.{' '}
+    <br />
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus
+    odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc.
+  </>
+);
