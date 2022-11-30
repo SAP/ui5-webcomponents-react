754d6ec85dab57f65d55ec3afab3038577319705
Marcus Notheis <marcus.notheis@sap.com>
2022-11-30 13:15:16 +0100

    test: rewrite tests with hooks-testing-library to cypress

---
+++ packages/charts/src/hooks/usePrepareDimensionsAndMeasures.cy.tsx
@@ -0,0 +1,94 @@
+import { usePrepareDimensionsAndMeasures } from './usePrepareDimensionsAndMeasures';
+
+const dimensions = [
+  {
+    accessor: 'a'
+  }
+];
+
+const measures = [
+  {
+    accessor: 'b'
+  }
+];
+
+function TestComponent({ onHookResult, dimensionOptions = undefined, measureOptions = undefined }) {
+  const result = usePrepareDimensionsAndMeasures(dimensions, measures, dimensionOptions, measureOptions);
+  onHookResult(result);
+  return null;
+}
+
+describe('useLabelFormatter', () => {
+  it('should not throw an error when no defaults are passed', () => {
+    const result = cy.spy().as('result');
+    cy.mount(<TestComponent onHookResult={result} />);
+
+    cy.get('@result').should('have.been.calledWith', {
+      dimensions: [
+        {
+          accessor: 'a'
+        }
+      ],
+      measures: [
+        {
+          accessor: 'b'
+        }
+      ]
+    });
+  });
+
+  it('should merge defaults', () => {
+    const result = cy.spy().as('result');
+    cy.mount(
+      <TestComponent
+        onHookResult={result}
+        dimensionOptions={{ dimensionDefault: true }}
+        measureOptions={{ measureDefault: true }}
+      />
+    );
+
+    cy.get('@result').should('have.been.calledWith', {
+      dimensions: [
+        {
+          accessor: 'a',
+          dimensionDefault: true
+        }
+      ],
+      measures: [
+        {
+          accessor: 'b',
+          measureDefault: true
+        }
+      ]
+    });
+  });
+
+  it('should merge defaults but not overwrite existing properties', () => {
+    const result = cy.spy().as('result');
+    cy.mount(
+      <TestComponent
+        onHookResult={result}
+        dimensionOptions={{
+          dimensionDefault: true,
+          accessor: 'I should not be in the result'
+        }}
+        measureOptions={{ measureDefault: true, accessor: 'I should not be in the result' }}
+      />
+    );
+
+    cy.get('@result').should('have.been.calledWith', {
+      dimensions: [
+        {
+          accessor: 'a',
+          dimensionDefault: true
+        }
+      ],
+      measures: [
+        {
+          accessor: 'b',
+          measureDefault: true
+        }
+      ]
+    });
+  });
+});
---
+++ packages/charts/src/hooks/useTooltipFormatter.cy.tsx
@@ -0,0 +1,44 @@
+import { useTooltipFormatter } from './useTooltipFormatter';
+
+function TooltipFormatterComponent({ measure, value, name, options }) {
+  const val = useTooltipFormatter([measure]);
+  return <span>{val(value, name, options)}</span>;
+}
+
+describe('useTooltipFormatter', () => {
+  it('should return value when no formatter is present', () => {
+    cy.mount(
+      <TooltipFormatterComponent
+        measure={{ accessor: 'test' }}
+        value={100}
+        name={'value'}
+        options={{ dataKey: 'test' }}
+      />
+    );
+    cy.findByText('100').should('be.visible');
+  });
+
+  it('should not crash on invalid formatter', () => {
+    cy.mount(
+      <TooltipFormatterComponent
+        measure={{ accessor: 'test', formatter: 'abc' }}
+        value={100}
+        name={'value'}
+        options={{ dataKey: 'test' }}
+      />
+    );
+    cy.findByText('100').should('be.visible');
+  });
+
+  it('should format the value with a valid formatter', () => {
+    cy.mount(
+      <TooltipFormatterComponent
+        measure={{ accessor: 'test', formatter: (val) => val / 10 }}
+        value={100}
+        name={'value'}
+        options={{ dataKey: 'test' }}
+      />
+    );
+    cy.findByText('10').should('be.visible');
+  });
+});
