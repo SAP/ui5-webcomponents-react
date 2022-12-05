c7bac7e7e58185eae5a1910e9595ef879ed5989d
Lukas Harbarth <lukas.harbarth@sap.com>
2022-12-05 11:41:46 +0100

    fix(FilterBar): fix alignment of table cells in dialog

--- packages/main/src/components/FilterGroupItem/index.tsx
+++ packages/main/src/components/FilterGroupItem/index.tsx
@@ -4,11 +4,22 @@ import { createUseStyles } from 'react-jss';
 import { FlexBoxDirection } from '../../enums';
 import { BusyIndicatorSize } from '../../enums/BusyIndicatorSize';
 import { CommonProps } from '../../interfaces/CommonProps';
+import { addCustomCSSWithScoping } from '../../internal/addCustomCSSWithScoping';
 import { Icon, TableCell, TableRow } from '../../webComponents';
 import { BusyIndicator } from '../../webComponents/BusyIndicator';
 import { Label } from '../../webComponents/Label';
 import { FlexBox } from '../FlexBox';
 import styles from './FilterGroupItem.jss';
+
+addCustomCSSWithScoping(
+  'ui5-table-row',
+  `
+/* hide navigated cell */
+:host([data-component-name="FilterBarDialogTableRow"]) .ui5-table-row-navigated {
+ width: 0;
+}
+`
+);
 
 const useStyles = createUseStyles(styles, { name: 'FilterGroupItem' });
 
@@ -92,7 +103,12 @@ export const FilterGroupItem = forwardRef<HTMLDivElement, FilterGroupItemPropTyp
   if (!inFB) {
     return (
       //todo: disable selection for required fields when it's possible, or the table is fully controllable (https://github.com/SAP/ui5-webcomponents/issues/5662)
-      <TableRow data-react-key={props['data-react-key']} selected={selected} data-required={required}>
+      <TableRow
+        data-react-key={props['data-react-key']}
+        selected={selected}
+        data-required={required}
+        data-component-name="FilterBarDialogTableRow"
+      >
         <TableCell>
           <FlexBox direction={FlexBoxDirection.Column}>
             <Label className={classes.dialogCellLabel} title={labelTooltip ?? label} required={required}>
