2babb4bf2870b97ee5e68ea500ac49fef97c757e
Lukas Harbarth <lukas.harbarth@sap.com>
2022-12-09 15:34:55 +0100

    remove data attributes from CommonProps

--- packages/main/src/interfaces/CommonProps.ts
+++ packages/main/src/interfaces/CommonProps.ts
@@ -1,9 +1,6 @@
 import { CSSProperties, HTMLAttributes } from 'react';
 
-type DataAttributeKey = `data-${string}`;
-type HTMLDataAttributes = Record<DataAttributeKey, string>;
-
-export interface CommonProps extends HTMLAttributes<HTMLElement>, HTMLDataAttributes {
+export interface CommonProps extends HTMLAttributes<HTMLElement> {
   /**
    * Element style which will be appended to the most outer element of a component.
    * Use this prop carefully, some css properties might break the component.
