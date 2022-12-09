d1d339e76e698954265c00e75677d75eaf4330ee
Lukas Harbarth <lukas.harbarth@sap.com>
2022-12-09 13:39:51 +0100

    fix(TypeScript): include `data-*` attributes in `CommonProps` interface

--- packages/main/src/interfaces/CommonProps.ts
+++ packages/main/src/interfaces/CommonProps.ts
@@ -1,6 +1,9 @@
 import { CSSProperties, HTMLAttributes } from 'react';
 
-export interface CommonProps extends HTMLAttributes<HTMLElement> {
+type DataAttributeKey = `data-${string}`;
+type HTMLDataAttributes = Record<DataAttributeKey, string>;
+
+export interface CommonProps extends HTMLAttributes<HTMLElement>, HTMLDataAttributes {
   /**
    * Element style which will be appended to the most outer element of a component.
    * Use this prop carefully, some css properties might break the component.
