1280a659500bac7650eb6def61e4c4e6a1e6a6c3
Lukas Harbarth <lukas.harbarth@sap.com>
2022-12-21 16:16:27 +0100

    fix(ObjectPage): fix scrolling for ObjectPages with single section (#3901)
    This PR also improves scrolling to sub-sections
    
    Fixes #3870

--- packages/main/src/components/ObjectPage/ObjectPageUtils.ts
+++ packages/main/src/components/ObjectPage/ObjectPageUtils.ts
@@ -1,4 +1,4 @@
-import { RefObject, isValidElement } from 'react';
+import { isValidElement } from 'react';
 import { safeGetChildrenArray } from '../../internal/safeGetChildrenArray';
 
 export const getSectionById = (sections, id) => {
@@ -10,19 +10,4 @@ export const getSectionById = (sections, id) => {
 export const extractSectionIdFromHtmlId = (id: string) => {
   if (!id) return null;
   return id.replace(/^ObjectPageSection-/, '');
-};
-
-export const getLastObjectPageSection = (ref: RefObject<HTMLDivElement>, alwaysSetMargin): HTMLElement => {
-  const sections = ref.current?.querySelectorAll<HTMLElement>('[id^="ObjectPageSection"]');
-
-  if (!sections || sections.length < 1) {
-    return null;
-  }
-  if (!alwaysSetMargin && sections.length === 1) {
-    if (sections[0].querySelectorAll('[id^="ObjectPageSubSection"]').length === 0) {
-      return null;
-    }
-  }
-
-  return sections[sections.length - 1];
 };
