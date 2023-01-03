1afb4a4a6aa2fff1554bd41895c457da1fa330b5
Lukas Harbarth <lukas.harbarth@sap.com>
2023-01-03 14:46:31 +0100

    fix(FilterBar): don't fire bubbled up `selection-change` event of `Table`

--- packages/main/src/components/FilterBar/FilterDialog.tsx
+++ packages/main/src/components/FilterBar/FilterDialog.tsx
@@ -70,6 +70,14 @@ addCustomCSSWithScoping(
 }
 /* don't display border of panel table */
 :host([data-component-name="FilterBarDialogPanelTable"]) table {
+  table-layout: fixed;
+  border-collapse: unset;
+}
+
+/* don't allow table cells to grow */
+:host([data-component-name="FilterBarDialogPanelTable"]) table,
+:host([data-component-name="FilterBarDialogTable"]) table{
+  table-layout: fixed;
   border-collapse: unset;
 }
 
@@ -277,6 +285,11 @@ export const FilterDialog = (props: FilterDialogPropTypes) => {
 
   const handleCheckBoxChange = (e) => {
     e.preventDefault();
+
+    // could be unwanted behavior: https://github.com/SAP/ui5-webcomponents-react/issues/3928
+    if (!e.target.getAttribute('ui5-table')) {
+      return;
+    }
     const prevRowsByKey = e.detail.previouslySelectedRows.reduce(
       (acc, prevSelRow) => ({ ...acc, [prevSelRow.dataset.reactKey]: prevSelRow }),
       {}
