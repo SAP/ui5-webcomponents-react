73a431ad69644851c335f230e89cbe0d59983fed
Lukas Harbarth <lukas.harbarth@sap.com>
2023-01-24 13:08:40 +0100

    chore: add `@ui5/webcomponents-cypress-commands` package (#4005)

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -3,6 +3,7 @@ import '@testing-library/cypress/add-commands';
 import { ThemeProvider } from '@ui5/webcomponents-react';
 import { mount } from 'cypress/react18';
 import './commands';
+import '../../packages/cypress-commands';
 
 declare global {
   namespace Cypress {
@@ -11,6 +12,22 @@ declare global {
     }
   }
 }
+
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
+
+Cypress.on(
+  'uncaught:exception',
+  /**
+   *
+   * @param err
+   * @returns {boolean}
+   */ (err) => {
+    /* returning false here prevents Cypress from failing the test */
+    if (resizeObserverLoopErrRe.test(err.message)) {
+      return false;
+    }
+  }
+);
 
 Cypress.Commands.add('mount', (component, options) => {
   return mount(<ThemeProvider>{component}</ThemeProvider>, options);
