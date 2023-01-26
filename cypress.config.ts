82ec44a951f59b15807af28dc8b2c22ef1c34c43
Marcus Notheis <marcus.notheis@sap.com>
2023-01-26 14:30:30 +0100

    run cypress tests in parallel

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import codeCoverageTask from '@cypress/code-coverage/task';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: '3xdc6r',
   component: {
     setupNodeEvents(on, config) {
       codeCoverageTask(on, config);
