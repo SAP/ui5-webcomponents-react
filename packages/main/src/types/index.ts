480afa923b3b5d505360ae1500449958eca94563
Lukas Harbarth <lukas.harbarth@sap.com>
2023-01-30 11:43:22 +0100

    add comment

--- packages/main/src/types/index.ts
+++ packages/main/src/types/index.ts
@@ -1,6 +1,10 @@
 import { ReactElement, ReactFragment, ReactNode, ReactPortal } from 'react';
 
 type ReducedReactNode = Exclude<ReactNode, string | number | boolean | ReactPortal | ReactFragment>;
-export type UI5WCSlotsNode = ReducedReactNode | Iterable<ReducedReactNode> | false | ReactElement;
+export type UI5WCSlotsNode =
+  | ReducedReactNode
+  | Iterable<ReducedReactNode>
+  | false
+  | ReactElement /* necessary for React v16 & v17 ReactNode type*/;
 
 export type Nullable<T> = T | null;
