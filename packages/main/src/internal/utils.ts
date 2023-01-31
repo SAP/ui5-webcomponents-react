abbae2e88ae2e1b235679454f7ea4bbb0da69893
Lukas Harbarth <lukas.harbarth@sap.com>
2023-01-31 17:13:08 +0100

    test(FlexBox): migrate Jest tests to Cypress

--- packages/main/src/internal/utils.ts
+++ packages/main/src/internal/utils.ts
@@ -26,3 +26,8 @@ export function flattenFragments(children: ReactNode | ReactNode[], depth = 1): 
 
   return flatChildren;
 }
+
+export const capitalizeFirstLetter = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+export const lowercaseFirstLetter = (s: string) => s.charAt(0).toLowerCase() + s.slice(1);
+export const camelToKebabCase = (s: string) => s.replace(/([A-Z])/g, (a, b: string) => `-${b.toLowerCase()}`);
+export const kebabToCamelCase = (str: string) => str.replace(/([-_]\w)/g, (g) => g[1].toUpperCase());
