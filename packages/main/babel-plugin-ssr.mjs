aaf167f5cc78ed3ee930018240f299e743049e72
Marcus Notheis <marcus.notheis@sap.com>
2023-02-07 10:06:18 +0100

    remove import loader in client and wrapper mode

--- packages/main/babel-plugin-ssr.mjs
+++ packages/main/babel-plugin-ssr.mjs
@@ -1,3 +1,9 @@
+const Modes = {
+  CLIENT: 'client',
+  WRAPPER: 'wrapper',
+  SSR: 'ssr'
+};
+
 export default function (babel) {
   const { types: t } = babel;
 
@@ -5,13 +11,13 @@ export default function (babel) {
     name: 'ui5-webcomponents-ssr-plugin',
     visitor: {
       ImportDeclaration(path, state) {
-        if (state.opts.mode === 'ssr' || state.opts.mode === 'wrapper') {
+        if (state.opts.mode === Modes.SSR || state.opts.mode === Modes.WRAPPER) {
           // remove all static web components imports when building for ssr or wrappers
           if (/@ui5\/webcomponents(-fiori)?\/dist\/\w+(\.js)?$/.test(path.node.source.value)) {
             path.remove();
           }
         }
-        if (state.opts.mode !== 'ssr') {
+        if (state.opts.mode !== Modes.SSR) {
           // remove dynamic web component import when not building for ssr
           const [importSpecifier] = path.node?.specifiers ?? [];
           if (importSpecifier && importSpecifier.imported?.name === 'useServerSideEffect') {
@@ -20,13 +26,20 @@ export default function (babel) {
         }
       },
       ExpressionStatement(path, state) {
-        if (state.opts.mode === 'client' || state.opts.mode === 'wrapper') {
+        if (state.opts.mode !== Modes.SSR) {
           // remove dynamic web component import when not building for ssr
           if (
             path.node.expression.type === 'CallExpression' &&
             path.node.expression.callee.name === 'useServerSideEffect'
           ) {
             path.remove();
+          }
+        }
+      },
+      CallExpression(path, state) {
+        if (state.opts.mode !== Modes.SSR) {
+          if (path.node.callee.name === 'withWebComponent') {
+            path.node.arguments = path.node.arguments.filter((item) => item.type !== 'ArrowFunctionExpression');
           }
         }
       }
