b62728e35cb422609a91496d6c99b5d1f6d3470c
Marcus Notheis <marcus.notheis@sap.com>
2023-02-14 08:14:04 +0100

    use vite tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,21 @@ import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import IstanbulPlugin from 'vite-plugin-istanbul';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: '@docs',
-        replacement: fileURLToPath(new URL('./.storybook/components', import.meta.url))
-      }
-    ]
-  },
-  plugins: [
-    react(),
-    IstanbulPlugin({
-      cypress: true
-    })
-  ]
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      process.env.CYPRESS === 'true' &&
+        IstanbulPlugin({
+          cypress: true
+        }),
+      tsconfigPaths({
+        projects: [fileURLToPath(new URL('tsconfig.base.json', import.meta.url))]
+      })
+    ].filter(Boolean)
+  };
 });
