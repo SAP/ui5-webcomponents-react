054d80b9f1c734a2795aa0bd462e9a24dca51740
Marcus Notheis <marcus.notheis@sap.com>
2023-02-16 13:39:15 +0100

    Update Table of Content

--- .storybook/components/TableOfContent.tsx
+++ .storybook/components/TableOfContent.tsx
@@ -1,79 +1,8 @@
-import { createUseStyles } from 'react-jss';
-import { ThemingParameters } from '@ui5/webcomponents-react-base';
 import React, { useEffect } from 'react';
 import tocbot from 'tocbot';
+import classes from './ToC.module.css';
 
-const styles = {
-  header: {
-    width: '100%',
-    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
-    fontSize: '24px',
-    '@media(min-width:1330px)': {
-      display: 'none'
-    }
-  },
-  fixedContainer: {
-    '@media(min-width:1330px)': {
-      width: '200px',
-      position: 'fixed',
-      top: 75,
-      right: 6,
-      overflow: 'hidden'
-    }
-  },
-  toc: {
-    fontFamily: ThemingParameters.sapFontFamily,
-    fontSize: ThemingParameters.sapFontSize,
-    backgroundColor: 'white',
-    '& *': {
-      textDecorationLine: 'none',
-      listStyle: 'none'
-    },
-    '@media(max-width:1329px)': {
-      borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
-      '& .toc-link': {
-        textShadow: ThemingParameters.sapContent_TextShadow,
-        color: ThemingParameters.sapLinkColor,
-        '&:hover': {
-          color: ThemingParameters.sapLink_Hover_Color
-        },
-        '&:active': {
-          color: ThemingParameters.sapLink_Active_Color
-        }
-      },
-      '& .toc-list-item': {
-        margin: '4px 0'
-      },
-      '& .is-active-link': {
-        fontWeight: 400,
-        '&::before': {
-          content: 'none'
-        }
-      }
-    },
-    '@media(min-width:1330px)': {
-      '& > .toc-list': {
-        paddingLeft: '10px'
-      },
-      '& .toc-list-item': {
-        margin: '4px 0'
-      },
-      '& .toc-link::before': {
-        width: '4px'
-      },
-      '& .is-active-link': {
-        '&::before': {
-          backgroundColor: '#0a6ed1'
-        }
-      }
-    }
-  }
-};
-const useStyles = createUseStyles(styles, { name: 'TableOfContent' });
-
-export const TableOfContent = ({ headingSelector = 'h2.sbdocs-h2, h3.sbdocs-h3, h4.sbdocs-h4' }) => {
-  const classes = useStyles();
-
+export function TableOfContent({ headingSelector = 'h2.sbdocs-h2, h3.sbdocs-h3, h4.sbdocs-h4' }) {
   useEffect(() => {
     tocbot.init({
       tocSelector: '.js-toc',
@@ -97,6 +26,4 @@ export const TableOfContent = ({ headingSelector = 'h2.sbdocs-h2, h3.sbdocs-h3, 
       </div>
     </>
   );
-};
-
-TableOfContent.displayName = 'TableOfContent';
+}
