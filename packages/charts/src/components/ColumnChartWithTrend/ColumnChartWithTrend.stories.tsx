0aad402608d0fb5ec6626aaa28471b98b0e4affa
Marcus Notheis <marcus.notheis@sap.com>
2023-02-23 16:54:21 +0100

    migrate ColumnChartWithTrend Story

---
+++ packages/charts/src/components/ColumnChartWithTrend/ColumnChartWithTrend.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { complexDataSet } from '../../resources/DemoProps.js';
+import { ColumnChartWithTrend } from './ColumnChartWithTrend.js';
+
+const meta = {
+  title: 'ColumnChartWithTrend',
+  component: ColumnChartWithTrend,
+  args: {
+    dataset: complexDataSet,
+    dimensions: [
+      {
+        accessor: 'name',
+        formatter: (d) => `${d} 2019`
+      }
+    ],
+    measures: [
+      {
+        accessor: 'users',
+        label: 'Users',
+        formatter: (val) => val.toLocaleString(),
+        type: 'line'
+      },
+      {
+        accessor: 'sessions',
+        label: 'Active Sessions',
+        type: 'bar'
+      }
+    ]
+  }
+} satisfies Meta<typeof ColumnChartWithTrend>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const LoadingPlaceholder: Story = {
+  args: {
+    dataset: []
+  }
+};
