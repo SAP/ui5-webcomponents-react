414ddee7877403e6573d32f48945a05e5352674e
Marcus Notheis <marcus.notheis@sap.com>
2023-02-23 17:11:43 +0100

    migrate composed chart story

--- packages/charts/src/components/ComposedChart/index.tsx
+++ packages/charts/src/components/ComposedChart/index.tsx
@@ -83,11 +83,11 @@ export interface ComposedChartProps extends IChartBaseProps {
   /**
    * An array of config objects. Each object will define one dimension of the chart.
    *
-   * #### Required Properties
+   * **Required Properties**
    * - `accessor`: string containing the path to the dataset key the dimension should display. Supports object structures by using <code>'parent.child'</code>.
    *   Can also be a getter.
    *
-   * #### Optional Properties
+   * **Optional Properties**
    * - `formatter`: function will be called for each data label and allows you to format it according to your needs
    * - `interval`: number that controls how many ticks are rendered on the x axis
    *
@@ -96,12 +96,12 @@ export interface ComposedChartProps extends IChartBaseProps {
   /**
    * An array of config objects. Each object is defining one element in the chart.
    *
-   * #### Required properties
+   * **Required properties**
    * - `accessor`: string containing the path to the dataset key this element should display. Supports object structures by using <code>'parent.child'</code>.
    *   Can also be a getter.
    * - `type`: string which chart element to show. Possible values: `line`, `bar`, `area`.
    *
-   * #### Optional properties
+   * **Optional properties**
    *
    * - `label`: Label to display in legends or tooltips. Falls back to the <code>accessor</code> if not present.
    * - `color`: any valid CSS Color or CSS Variable. Defaults to the `sapChart_Ordinal` colors
