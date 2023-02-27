66c5cdcaf5645c3f2fb350d7ca348a2b55d9e768
Marcus Notheis <marcus.notheis@sap.com>
2023-02-27 14:58:14 +0100

    main: manual build

---
+++ main/assets/ColumnChart-db29e37e.js
@@ -0,0 +1,36 @@
+import{j as r,a as m,F as c}from"./jsx-runtime-670450c2.js";import{C as h}from"./DomRefTable.module-2e4f4a60.js";import{D as s}from"./DocsHeader-0be2c133.js";import{F as d}from"./Footer-1690f844.js";import"./index-f1f749bf.js";import{M as l,C as e}from"./index-edb1f51e.js";import"./chunk-4XCFV5WA-38ae7931.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{C as f,D as p,W as u,a as g,b as C,c as W,L as b,d as L,e as M}from"./ColumnChart.stories-77391e59.js";import{u as a}from"./index-4fb8b842.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./index-87ce9f78.js";import"./iframe-2e8e5950.js";import"../sb-preview/runtime.mjs";import"./UI5Element-727d48d7.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-63dd52a0.js";import"./utils-ed90fb1b.js";import"./AriaLabelHelper-dee23f38.js";import"./MarkedEvents-0e37da6f.js";import"./decline-ff534003.js";import"./Icons-5bcf1e3f.js";import"./class-map-abcca105.js";import"./Button-2e5054c2.js";import"./Keys-50a1cb5a.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-6923d62a.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-f40f884c.js";import"./TableOfContent-ceb1d690.js";import"./index-19530604.js";import"./WrappingType-b81e595a.js";import"./index-81e374bd.js";import"./Label-6707bdfc.js";import"./Media-6b270b80.js";import"./MediaRange-25b98f31.js";import"./index-0ca0b8e3.js";import"./Popover-97ca01c1.js";import"./Integer-8010a7ae.js";import"./FocusableElements-5a723910.js";import"./getActiveElement-bcae01ed.js";import"./ResizeHandler-c56abc92.js";import"./style-map-7b3eb5df.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-96c5f47c.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./index-99d317a4.js";import"./defaults-a7027b8c.js";import"./react-content-loader.es-6d54f457.js";import"./index-0d4b1695.js";import"./i18n-defaults-a62b2e2b.js";import"./I18nContext-bef5b452.js";import"./debounce-bc2bf61c.js";import"./isString-77c0f675.js";import"./useCancelAnimationFallback-19ca54a5.js";import"./generateCategoricalChart-12a761a0.js";import"./throttle-28509771.js";import"./index-4d501b15.js";import"./YAxisTicks-bb9f29a6.js";import"./debounce-5c77766a.js";import"./useTooltipFormatter-c9e2fa36.js";import"./ChartDataLabel-a761619a.js";import"./staticProps-9dd6f0f9.js";import"./useLongestYAxisLabel-a3ccbcb7.js";import"./useOnClickInternal-d27d7c1e.js";import"./useIsRTL-fcdcb328.js";import"./DemoProps-9cd0a46b.js";function Br(t={}){const{wrapper:i}=Object.assign({},a(),t.components);return i?r(i,Object.assign({},t,{children:r(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a",pre:"pre"},a(),t.components);return m(c,{children:[r(l,{of:f}),`
+`,r(s,{}),`
+`,r(o.h2,{children:"Example"}),`
+`,r(e,{of:p}),`
+`,r(o.h2,{children:"Properties"}),`
+`,r(h,{of:p}),`
+`,r("br",{}),`
+`,r(o.h2,{children:"More Examples"}),`
+`,r(o.h3,{children:"With custom color"}),`
+`,r(e,{of:u}),`
+`,r(o.h3,{children:"With Secondary Dimension"}),`
+`,r(e,{of:g}),`
+`,r(o.h3,{children:"With Stacks"}),`
+`,r(e,{of:C}),`
+`,r(o.h3,{children:"With Formatter"}),`
+`,r(e,{of:W}),`
+`,r(o.h3,{children:"Loading Placeholder"}),`
+`,r(e,{of:b}),`
+`,r(o.h3,{children:"With Reference Line"}),`
+`,m(o.p,{children:["You can set a reference line to any value by using the ",r(o.code,{children:"referenceLine"})," ",r(o.code,{children:"chartConfig"})," property. ",r(o.code,{children:"referenceLine"})," accepts all properties described ",r(o.a,{href:"https://recharts.org/en-US/api/ReferenceLine",children:"here"}),", but it's recommended to only use ",r(o.code,{children:"label"}),",",r(o.code,{children:"value"})," and ",r(o.code,{children:"color"})," to preserve the intended design."]}),`
+`,r(o.pre,{children:r(o.code,{className:"language-jsx",children:`<ColumnChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,r(e,{of:L}),`
+`,r(o.h2,{children:"With Highlighted Measures"}),`
+`,r(e,{of:M}),`
+`,r(d,{})]})}}export{Br as default};
+//# sourceMappingURL=ColumnChart-db29e37e.js.map
---
+++ main/assets/VariantManagement.stories-c993d328.js
@@ -0,0 +1,116 @@
+import{A as C}from"./DomRefTable.module-2e4f4a60.js";import{D as O}from"./DocsHeader-0be2c133.js";import{F as A}from"./Footer-1690f844.js";import{a as r,j as e,F as P}from"./jsx-runtime-670450c2.js";import{V as m,a as t}from"./VariantItem-7e76f976.js";import{r as d}from"./index-f1f749bf.js";import{M as T,C as v,i as g}from"./index-edb1f51e.js";import"./chunk-4XCFV5WA-38ae7931.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{P as I}from"./Popover-97ca01c1.js";import{T as f}from"./Title-18f6d5a1.js";import{V as S}from"./ValueState-ab6838cc.js";import{u as w}from"./index-4fb8b842.js";import{M as N}from"./index-87ce9f78.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-6923d62a.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-f40f884c.js";import"./TableOfContent-ceb1d690.js";import"./index-19530604.js";import"./iframe-2e8e5950.js";import"../sb-preview/runtime.mjs";import"./UI5Element-727d48d7.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./AriaLabelHelper-dee23f38.js";import"./withWebComponent-63dd52a0.js";import"./utils-ed90fb1b.js";import"./MarkedEvents-0e37da6f.js";import"./Keys-50a1cb5a.js";import"./WrappingType-b81e595a.js";import"./index-81e374bd.js";import"./Label-6707bdfc.js";import"./class-map-abcca105.js";import"./Media-6b270b80.js";import"./MediaRange-25b98f31.js";import"./index-0ca0b8e3.js";import"./UnableToLoad-d12353fc.js";import"./index-93e61262.js";import"./ResizeHandler-c56abc92.js";import"./i18n-defaults-b5ead0af.js";import"./parameters-bundle.css-0c974f42.js";import"./navigation-down-arrow-131293e3.js";import"./Icons-5bcf1e3f.js";import"./search-d68d0f2f.js";import"./decline-ff534003.js";import"./index-99d317a4.js";import"./index-96c5f47c.js";import"./i18n-defaults-a62b2e2b.js";import"./I18nContext-bef5b452.js";import"./stopPropagation-da74d54e.js";import"./Button-2e5054c2.js";import"./index-734b0669.js";import"./Bar-b4a14d85.js";import"./fastNavigation-ebf07f1c.js";import"./index-5a32d3f6.js";import"./index-a38c9f00.js";import"./Dialog-859ab2f5.js";import"./Integer-8010a7ae.js";import"./FocusableElements-5a723910.js";import"./getActiveElement-bcae01ed.js";import"./style-map-7b3eb5df.js";import"./ValueState-2c5e5904.js";import"./index-f1eb9255.js";import"./ItemNavigation-4b509b49.js";import"./TabbableElements-21c27087.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-59c61392.js";import"./CheckBox-2890456e.js";import"./accept-70258933.js";import"./unfavorite-55bdb929.js";import"./index-f093a610.js";import"./index-f32e93a8.js";import"./index-43db0e71.js";import"./Input-a6aac6bd.js";import"./Suggestions.css-1f63061d.js";import"./ResponsivePopoverCommon.css-bc05f183.js";import"./ValueStateMessage.css-6e86a950.js";import"./index-2493c1a7.js";import"./RadioButton-d0c7abe6.js";import"./useIsomorphicId-78860c77.js";import"./index-278126eb.js";import"./index-77340621.js";import"./ResponsivePopover-bf5faf8e.js";import"./index-268e9f56.js";import"./ListItemBase-ef18a748.js";import"./index-4fbabce6.js";import"./StandardListItem-cb5818d4.js";import"./ListItem-9550e3d3.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-a1b07349.js";import"./Avatar-b22f5314.js";import"./employee-10cd14ab.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";const p=({selectedByIndex:n=1})=>{const[o,l]=d.useState(void 0),[a,s]=d.useState("Only alphanumeric chars in Save View input"),[c,h]=d.useState(void 0),[b,D]=d.useState("Max 12 chars");return r(m,{onSaveAs:i=>{s(i.detail.children)},onSaveManageViews:i=>{const V=i.detail.updatedVariants.find(x=>x["data-custom-manage-views"]);!c&&V&&D(V.children)},children:[e(t,{"data-custom-save-view":!0,selected:n===0,saveViewInputProps:{valueState:o,valueStateMessage:o?e("div",{children:"Only alphanumeric and space characters allowed!"}):void 0,onInput:i=>{i.target.value.match(/^[a-z0-9\s]+$/i)?(i.isInvalid=!1,l(void 0)):(i.isInvalid=!0,l(S.Error))},"data-testid":"alphanumeric"},children:a}),e(t,{"data-custom-manage-views":!0,selected:n===1,manageViewsInputProps:{valueState:c,valueStateMessage:c?e("div",{children:"No more than 12 characters allowed!"}):void 0,onInput:i=>{i.target.value.length>12?(i.isInvalid=!0,h(S.Error)):(i.isInvalid=!1,h(void 0))},"data-testid":"12chars"},children:b})]})};try{p.displayName="VariantManagementWithCustomValidation",p.__docgenInfo={description:"",displayName:"VariantManagementWithCustomValidation",props:{selectedByIndex:{defaultValue:{value:"1"},description:"",name:"selectedByIndex",required:!1,type:{name:"number"}}}}}catch{}function E(n={}){const{wrapper:o}=Object.assign({},w(),n.components);return o?e(o,{...n,children:e(l,{})}):l();function l(){const a=Object.assign({h2:"h2",h1:"h1",p:"p",code:"code",strong:"strong",h3:"h3",pre:"pre"},w(),n.components);return r(P,{children:[e(T,{title:"Inputs / VariantManagement",component:m,subcomponents:{VariantItem:t},args:{placement:I.Bottom,level:f.H4}}),`
+`,e(O,{}),`
+`,e("br",{}),`
+`,e(a.h2,{children:"Example"}),`
+`,e(v,{children:e(g,{name:"Default",children:s=>r(m,{...s,children:[e(t,{inputProps:{valueState:"Error",onInput:console.log},children:"Default VariantItem"}),e(t,{selected:!0,children:"Selected"}),e(t,{author:"SAP",children:"Author"}),e(t,{favorite:!0,children:"Favorite"}),e(t,{isDefault:!0,children:"IsDefault"}),e(t,{favorite:!0,labelReadOnly:!0,children:"Favorite & labelReadOnly"}),e(t,{applyAutomatically:!0,children:"ApplyAutomatically"}),e(t,{readOnly:!0,children:"ReadOnly"}),e(t,{global:!0,children:"Global"}),e(t,{global:!0,readOnly:!0,children:"Global & readOnly"}),e(t,{global:!0,children:"Not deletable -> global"}),e(t,{hideDelete:!0,children:"Not deletable -> hideDelete"}),e(t,{hideDelete:!1,global:!0,children:"Deletable -> hideDelete: false, global: true"})]})})}),`
+`,e(a.h2,{children:"Properties"}),`
+`,e(C,{story:"Default"}),`
+`,e(a.h1,{children:"More Examples"}),`
+`,e(a.h2,{children:"VariantManagement with custom Input"}),`
+`,r(a.p,{children:['You can customize the inputs inside the configuration dialogs ("Save View" and "Manage Views") by leveraging the corresponding ',e(a.code,{children:"inputProps"})," prop (",e(a.code,{children:"saveViewInputProps"})," and ",e(a.code,{children:"manageViewsInputProps"}),") of the ",e(a.code,{children:"VariantItem"}),` component.
+These props accept props of the `,e(a.code,{children:"Input"})," component with which you are able to overwrite internal implementations. E.g. ",e(a.code,{children:"valueState"})," and ",e(a.code,{children:"valueState"})," message."]}),`
+`,r(a.p,{children:[e(a.strong,{children:"Note:"})," To prevent internal logic from being executed on save click, you can mark the ",e(a.code,{children:"onInput"})," event of the corresponding ",e(a.code,{children:"Input"})," component as invalid by setting ",e(a.code,{children:"event.isInvalid = true"})," inside the respective handler."]}),`
+`,e(N,{hideCloseButton:!0,children:"It is possible to overwrite internally used props. Please use with caution!"}),`
+`,e(v,{children:e(g,{name:"with custom validation",withSource:"none",children:s=>e(p,{...s})})}),`
+`,e(a.h3,{children:"Code"}),`
+`,r("details",{style:{marginBlockEnd:"2rem"},children:[e("summary",{children:"Show Code"}),e(a.pre,{children:e(a.code,{className:"language-jsx",children:`export const VariantManagementWithCustomValidation = () => {
+  const [valueStateSaveView, setValueStateSaveView] = useState(undefined);
+  const [customSaveViewVariantText, setCustomSaveViewVariantText] = useState(
+    'Only alphanumeric chars in Save View input'
+  );
+  const [valueStateManageViews, setValueStateManageViews] = useState(undefined);
+  const [customManageViewsVariantText, setCustomManageViewsVariantText] = useState('Max 12 chars');
+
+  const handleSaveViewInput = (e) => {
+    // only allow alphanumeric and space characters
+    if (!e.target.value.match(/^[a-z0-9\\s]+$/i)) {
+      // mark variant as invalid (prevent internal logic from being applied)
+      e.isInvalid = true;
+      setValueStateSaveView(ValueState.Error);
+    } else {
+      // mark variant as valid again
+      e.isInvalid = false;
+      setValueStateSaveView(undefined);
+    }
+  };
+  const handleSaveAs = (e) => {
+    setCustomSaveViewVariantText(e.detail.children);
+  };
+
+  const handleManageViewInput = (e) => {
+    // only 12 or fewer characters
+    if (e.target.value.length > 12) {
+      // mark variant as invalid (prevent internal logic from being applied)
+      e.isInvalid = true;
+      setValueStateManageViews(ValueState.Error);
+    } else {
+      // mark variant as valid again
+      e.isInvalid = false;
+      setValueStateManageViews(undefined);
+    }
+  };
+  const handleSaveManageViews = (e) => {
+    // if is custom manage view variant and is not in error state, set children to new value
+    const isCustomManageViewsItem = e.detail.updatedVariants.find((item) => item['data-custom-manage-views']);
+    if (!valueStateManageViews && isCustomManageViewsItem) {
+      setCustomManageViewsVariantText(isCustomManageViewsItem.children);
+    }
+  };
+  return (
+    <VariantManagement onSaveAs={handleSaveAs} onSaveManageViews={handleSaveManageViews}>
+      <VariantItem
+        data-custom-save-view
+        saveViewInputProps={{
+          valueState: valueStateSaveView,
+          valueStateMessage: valueStateSaveView ? (
+            <div>Only alphanumeric and space characters allowed!</div>
+          ) : undefined,
+          onInput: handleSaveViewInput
+        }}
+      >
+        {customSaveViewVariantText}
+      </VariantItem>
+      <VariantItem
+        data-custom-manage-views
+        selected
+        manageViewsInputProps={{
+          valueState: valueStateManageViews,
+          valueStateMessage: valueStateManageViews ? <div>No more than 12 characters allowed!</div> : undefined,
+          onInput: handleManageViewInput
+        }}
+      >
+        {customManageViewsVariantText}
+      </VariantItem>
+    </VariantManagement>
+  );
+};
+`})})]}),`
+`,e(A,{})]})}}const y=n=>r(m,{...n,children:[e(t,{inputProps:{valueState:"Error",onInput:console.log},children:"Default VariantItem"}),e(t,{selected:!0,children:"Selected"}),e(t,{author:"SAP",children:"Author"}),e(t,{favorite:!0,children:"Favorite"}),e(t,{isDefault:!0,children:"IsDefault"}),e(t,{favorite:!0,labelReadOnly:!0,children:"Favorite & labelReadOnly"}),e(t,{applyAutomatically:!0,children:"ApplyAutomatically"}),e(t,{readOnly:!0,children:"ReadOnly"}),e(t,{global:!0,children:"Global"}),e(t,{global:!0,readOnly:!0,children:"Global & readOnly"}),e(t,{global:!0,children:"Not deletable -> global"}),e(t,{hideDelete:!0,children:"Not deletable -> hideDelete"}),e(t,{hideDelete:!1,global:!0,children:"Deletable -> hideDelete: false, global: true"})]});y.storyName="Default";y.parameters={storySource:{source:`props => {
+  return <VariantManagement {...props}>
+          <VariantItem inputProps={{
+      valueState: "Error",
+      onInput: console.log
+    }}>Default VariantItem</VariantItem>
+          <VariantItem selected>Selected</VariantItem>
+          <VariantItem author="SAP">Author</VariantItem>
+          <VariantItem favorite>Favorite</VariantItem>
+          <VariantItem isDefault>IsDefault</VariantItem>
+          <VariantItem favorite labelReadOnly>
+            Favorite & labelReadOnly
+          </VariantItem>
+          <VariantItem applyAutomatically>ApplyAutomatically</VariantItem>
+          <VariantItem readOnly>ReadOnly</VariantItem>
+          <VariantItem global>Global</VariantItem>
+          <VariantItem global readOnly>
+            Global & readOnly
+          </VariantItem>
+          <VariantItem global>{\`Not deletable -> global\`}</VariantItem>
+          <VariantItem hideDelete>{\`Not deletable -> hideDelete\`}</VariantItem>
+          <VariantItem hideDelete={false} global>
+            {\`Deletable -> hideDelete: false, global: true\`}
+          </VariantItem>
+        </VariantManagement>;
+}`}};const M=n=>e(p,{...n});M.storyName="with custom validation";M.parameters={storySource:{source:`props => {
+  return <VariantManagementWithCustomValidation {...props} />;
+}`}};const u={title:"Inputs / VariantManagement",component:m,subcomponents:{VariantItem:t},args:{placement:I.Bottom,level:f.H4},tags:["stories-mdx"],includeStories:["defaultStory","withCustomValidation"]};u.parameters=u.parameters||{};u.parameters.docs={...u.parameters.docs||{},page:E};const _t=["defaultStory","withCustomValidation"];export{_t as __namedExportsOrder,u as default,y as defaultStory,M as withCustomValidation};
+//# sourceMappingURL=VariantManagement.stories-c993d328.js.map
