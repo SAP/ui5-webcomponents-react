99b8e83e596c834e49898dc08917b77a2dbad1fb
Marcus Notheis <marcus.notheis@sap.com>
2023-02-27 08:20:37 +0100

    Apply suggestions from code review
    Co-authored-by: Lukas Harbarth <lukas.harbarth@sap.com>

--- packages/charts/src/components/BarChart/BarChart.cy.tsx
+++ packages/charts/src/components/BarChart/BarChart.cy.tsx
@@ -1,6 +1,6 @@
-import { cypressPassThroughTestsFactory } from '@/cypress/support/utils';
 import { complexDataSet } from '../../resources/DemoProps.js';
 import { BarChart } from './BarChart.js';
+import { cypressPassThroughTestsFactory } from '@/cypress/support/utils';
 
 const dimensions = [
   {
--- packages/charts/src/components/BulletChart/BulletChart.cy.tsx
+++ packages/charts/src/components/BulletChart/BulletChart.cy.tsx
@@ -1,6 +1,6 @@
-import { cypressPassThroughTestsFactory } from '@/cypress/support/utils';
 import { complexDataSet } from '../../resources/DemoProps.js';
 import { BulletChart } from './BulletChart.js';
+import { cypressPassThroughTestsFactory } from '@/cypress/support/utils';
 
 const dimensions = [
   {
