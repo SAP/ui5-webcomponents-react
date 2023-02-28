2ac990d16a476fb50ee65dce87d93e5e1e37063c
Marcus Notheis <marcus.notheis@sap.com>
2023-02-28 13:49:32 +0100

    update nyc config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,22 @@ import codeCoverageTask from '@cypress/code-coverage/task';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: [
+        'cypress/**',
+        '**/src/interfaces/*',
+        '**/src/enums/*',
+        '**/*.stories.tsx',
+        '**/*.jss.ts',
+        '**/*.test.{ts,tsx}',
+        '**/node_modules/**',
+        'packages/*/src/index.ts',
+        'packages/main/src/components/AnalyticalTable/types/*',
+        'packages/main/src/webComponents/**'
+      ]
+    }
+  },
   component: {
     setupNodeEvents(on, config) {
       codeCoverageTask(on, config);
