6864da092533b71d12cc295ab866b4709545d9ed
Marcus Notheis <marcus.notheis@sap.com>
2023-03-03 09:11:22 +0100

    migrate button to CSF

---
+++ packages/main/src/webComponents/Button/Button.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ButtonDesign } from '../../enums';
+import employeeIcon from '@ui5/webcomponents-icons/dist/employee.js';
+import { Button } from './index.js';
+
+const meta = {
+  title: 'Inputs / Button',
+  component: Button,
+  argTypes: {
+    children: { control: 'text' }
+  },
+  args: {
+    design: ButtonDesign.Default,
+    icon: employeeIcon,
+    children: 'Button Text'
+  }
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
