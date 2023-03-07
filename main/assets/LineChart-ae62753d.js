59074021d0a0ea7b997f4df629ea0bf0be7d5a3c
MarcusNotheis <marcus.notheis@sap.com>
2023-03-07 14:52:28 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@81f51cb60ea777512dfbe07e38076c000634a73e 🚀

---
+++ main/assets/LineChart-ae62753d.js
@@ -0,0 +1,61 @@
+import{j as r,a as p,F as s}from"./jsx-runtime-670450c2.js";import"./chunk-QDOP3WTA-4d67ff75.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{M as c,C as o}from"./index-2eb5db95.js";import{C as h}from"./DomRefTable.module-9d048681.js";import{D as d}from"./DocsHeader-40fbd71a.js";import{F as l}from"./Footer-67049b4c.js";import"./index-f1f749bf.js";import{C as f,D as m,W as u,a as C,b as L,c as g,L as W,d as b,e as D}from"./LineChart.stories-91c9a74f.js";import{u as a}from"./index-4fb8b842.js";import"./iframe-387196f1.js";import"../sb-preview/runtime.mjs";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./react-jss.esm-fe4dc3ff.js";import"./index-6836c374.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./AriaLabelHelper-c82262ef.js";import"./MarkedEvents-8627ed01.js";import"./decline-0a03b0b0.js";import"./Icons-7b82f601.js";import"./class-map-5d8e4b2b.js";import"./Button-e27d9549.js";import"./Keys-3acbae73.js";import"./Device-208919c6.js";import"./index-1d945bd3.js";import"./clsx.m-1229b3e0.js";import"./index-c7ba7a71.js";import"./ThemingParameters-f4b4144e.js";import"./Import-b176d46e.js";import"./TableOfContent-7560364c.js";import"./index-283ce192.js";import"./WrappingType-b81e595a.js";import"./index-5c52c118.js";import"./Label-040efebd.js";import"./Footer.module-c4f3b823.js";import"./index-46eae42e.js";import"./Popover-cbbb060d.js";import"./Integer-d9976c13.js";import"./PopupsCommon.css-ab0735c6.js";import"./FocusableElements-c2f39514.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";import"./DemoProps-9cd0a46b.js";import"./LineChart-f564c5e3.js";import"./index-99d317a4.js";import"./YAxisTicks-aa51e4c5.js";import"./ChartContainer-09da8cde.js";import"./index-7a2a7ca3.js";import"./i18n-defaults-41f1487d.js";import"./I18nContext-bef5b452.js";import"./debounce-bc2bf61c.js";import"./isString-77c0f675.js";import"./generateCategoricalChart-ac557d4d.js";import"./throttle-28509771.js";import"./index-4d501b15.js";import"./debounce-5c77766a.js";import"./useTooltipFormatter-6c50ccb7.js";import"./defaults-3b7a26e0.js";import"./ChartDataLabel-fbee5b97.js";import"./staticProps-9dd6f0f9.js";import"./useLongestYAxisLabel-e9472a1e.js";import"./react-content-loader.es-eef51af2.js";import"./Line-2f8365e7.js";import"./useIsRTL-4cfef02b.js";function Kr(t={}){const{wrapper:n}=Object.assign({},a(),t.components);return n?r(n,Object.assign({},t,{children:r(i,{})})):i();function i(){const e=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a",pre:"pre",h4:"h4"},a(),t.components);return p(s,{children:[r(c,{of:f}),`
+`,r(d,{}),`
+`,r(e.h2,{children:"Example"}),`
+`,r(o,{of:m}),`
+`,r(e.h2,{children:"Properties"}),`
+`,r(h,{of:m}),`
+`,r("br",{}),`
+`,r("br",{}),`
+`,r(e.h2,{children:"More Examples"}),`
+`,r(e.h3,{children:"With Custom Color"}),`
+`,r(o,{of:u}),`
+`,r(e.h3,{children:"With Secondary Dimension"}),`
+`,r(o,{of:C}),`
+`,r(e.h3,{children:"With Data Labels"}),`
+`,r(o,{of:L}),`
+`,r(e.h3,{children:"With Formatter"}),`
+`,r(o,{of:g}),`
+`,r(e.h3,{children:"Loading Placeholder"}),`
+`,r(o,{of:W}),`
+`,r(e.h3,{children:"With Reference Line"}),`
+`,p(e.p,{children:["You can set a reference line to any value by using the ",r(e.code,{children:"referenceLine"})," ",r(e.code,{children:"chartConfig"})," property. ",r(e.code,{children:"referenceLine"})," accepts all properties described ",r(e.a,{href:"https://recharts.org/en-US/api/ReferenceLine",children:"here"}),", but it's recommended to only use ",r(e.code,{children:"label"}),",",r(e.code,{children:"value"})," and ",r(e.code,{children:"color"})," to preserve the intended design."]}),`
+`,r(e.pre,{children:r(e.code,{className:"language-jsx",children:`<LineChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,r(o,{of:b}),`
+`,r(e.h3,{children:"With Linear Gradient"}),`
+`,r(o,{of:D}),`
+`,r(e.h4,{children:"Code"}),`
+`,r(e.pre,{children:r(e.code,{className:"language-jsx",children:`export const LineChartWithLinearGradient = () => {
+  return (
+    <LineChart
+      dataset={bigDataSet}
+      dimensions={[{ accessor: 'name' }]}
+      measures={[
+        {
+          accessor: 'users',
+          width: 2,
+          color: 'url(#colorUsers)'
+        }
+      ]}
+    >
+      <defs>
+        <linearGradient id="colorUsers" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="0%" stopColor="red" />
+          <stop offset="100%" stopColor="green" />
+        </linearGradient>
+      </defs>
+    </LineChart>
+  );
+};
+`})}),`
+`,r(l,{})]})}}export{Kr as default};
+//# sourceMappingURL=LineChart-ae62753d.js.map
---
+++ main/assets/RatingIndicator.stories-1124a613.js
@@ -0,0 +1,30 @@
+import{M as h,C as c,j as u,a as d}from"./index-2eb5db95.js";import{R as r}from"./index-ea853f98.js";import{A as l}from"./DomRefTable.module-9d048681.js";import{D as g}from"./DocsHeader-40fbd71a.js";import{F as f}from"./Footer-67049b4c.js";import{j as t,a as y,F as I}from"./jsx-runtime-670450c2.js";import"./index-f1f749bf.js";import{u as s}from"./index-4fb8b842.js";import"./iframe-387196f1.js";import"../sb-preview/runtime.mjs";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./inheritsLoose-93e09647.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./MarkedEvents-8627ed01.js";import"./UI5Element-427ec721.js";import"./Keys-3acbae73.js";import"./Integer-d9976c13.js";import"./Float-180b3389.js";import"./Button-e27d9549.js";import"./AriaLabelHelper-c82262ef.js";import"./Device-208919c6.js";import"./Icons-7b82f601.js";import"./unfavorite-f2de935c.js";import"./chunk-QDOP3WTA-4d67ff75.js";import"./react-18-2bb9dade.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-fe4dc3ff.js";import"./index-6836c374.js";import"./decline-0a03b0b0.js";import"./class-map-5d8e4b2b.js";import"./index-1d945bd3.js";import"./clsx.m-1229b3e0.js";import"./index-c7ba7a71.js";import"./ThemingParameters-f4b4144e.js";import"./Import-b176d46e.js";import"./TableOfContent-7560364c.js";import"./index-283ce192.js";import"./WrappingType-b81e595a.js";import"./index-5c52c118.js";import"./Label-040efebd.js";import"./Footer.module-c4f3b823.js";import"./index-46eae42e.js";import"./Popover-cbbb060d.js";import"./PopupsCommon.css-ab0735c6.js";import"./FocusableElements-c2f39514.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";const b=`## Usage
+
+The recommended number of icons is between 5 and 7.
+
+## Responsive Behavior
+
+You can change the size of the Rating Indicator by changing its \`font-size\` CSS property.  
+Example: \`<RatingIndicator style="font-size: 3rem;"></RatingIndicator>\`
+
+## Keyboard Handling
+
+When the \`RatingIndicator\` is focused, the user can change the rating with the following keyboard shortcuts:  
+
+*   \\[RIGHT/UP\\] - Increases the value of the rating by one step. If the highest value is reached, does nothing
+*   \\[LEFT/DOWN\\] - Decreases the value of the rating by one step. If the lowest value is reached, does nothing.
+*   \\[HOME\\] - Sets the lowest value.
+*   \\[END\\] - Sets the highest value.
+*   \\[SPACE/ENTER/RETURN\\] - Increases the value of the rating by one step. If the highest value is reached, sets the rating to the lowest value.
+*   Any number - Changes value to the corresponding number. If typed number is larger than the number of values, sets the highest value.`;function v(e={}){const{wrapper:n}=Object.assign({},s(),e.components);return n?t(n,{...e,children:t(i,{})}):i();function i(){const a=Object.assign({h2:"h2"},s(),e.components);return y(I,{children:[t(h,{title:"Inputs / RatingIndicator",component:r}),`
+`,t(g,{since:"0.10.0"}),`
+`,t("br",{}),`
+`,t(a.h2,{children:"Example"}),`
+`,t(c,{children:t(u,{name:"Default",children:p=>t(r,{...p})})}),`
+`,t(a.h2,{children:"Properties"}),`
+`,t(l,{story:"Default"}),`
+`,t(d,{children:b}),`
+`,t(f,{})]})}}const m=e=>t(r,{...e});m.storyName="Default";m.parameters={storySource:{source:`args => {
+  return <RatingIndicator {...args} />;
+}`}};const o={title:"Inputs / RatingIndicator",component:r,tags:["stories-mdx"],includeStories:["defaultStory"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:v};const xt=["defaultStory"];export{xt as __namedExportsOrder,o as default,m as defaultStory};
+//# sourceMappingURL=RatingIndicator.stories-1124a613.js.map
