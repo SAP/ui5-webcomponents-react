115010ee12b1eaffd5957cadd6d883eb18d56b4c
Lukas Harbarth <lukas.harbarth@sap.com>
2023-03-08 16:16:50 +0100

    wip

--- packages/main/src/components/SplitterLayout/SplitterLayout.cy.tsx
+++ packages/main/src/components/SplitterLayout/SplitterLayout.cy.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SplitterElement, SplitterLayout, Label } from '../..';
+import { SplitterElement, SplitterLayout, Label, Button } from '../..';
 
 describe('SplitterLayout', () => {
   it('Reset on size change', () => {
@@ -38,5 +38,69 @@ describe('SplitterLayout', () => {
 
     cy.findByTestId('se1').should('have.css', 'flex', '0 0 calc(70% - 16px)');
     cy.findByTestId('se2').should('have.css', 'flex', '0 0 30%');
+  });
+
+  it('Vertical SplitterLayout w/ multiple SplitterElements', () => {
+    const click = cy.spy().as('click');
+    [true, false].forEach((vertical) => {
+      cy.mount(
+        <SplitterLayout vertical={vertical} style={{ width: '800px', height: '800px' }} data-testid={'sl'}>
+          <SplitterElement size={'200px'} data-testid={'se1'}>
+            <Button data-testid={'btn'} onClick={click}>
+              Button 1
+            </Button>
+          </SplitterElement>
+          <SplitterElement minSize={300} size={'400px'} data-testid={'se2'}>
+            <Button>Button 2</Button>
+          </SplitterElement>
+          <SplitterElement resizable={false} data-testid={'se3'}>
+            <Button>Button 3</Button>
+          </SplitterElement>
+        </SplitterLayout>
+      );
+      cy.findByTestId('btn').click();
+      cy.get('[role="separator"]').first().click();
+      // fallback click to prevent fuzzyness
+      cy.get('[role="separator"]')
+        .first()
+        .click()
+        .should('have.length', 1)
+        .should('have.css', 'border-style', 'dotted');
+      cy.findByTestId('sl').should('have.css', 'width', '800px').should('have.css', 'height', '800px');
+      cy.findByTestId('se2')
+        .should('have.css', 'flex', '0 0 400px')
+        .should('have.css', vertical ? 'min-height' : 'min-width', '300px');
+      cy.findByTestId('se3')
+        .should('have.css', 'flex', vertical ? '0 0 800px' : '0 0 200px')
+        .should('have.css', vertical ? 'min-height' : 'min-width', '0px');
+    });
+    cy.get('@click').should('have.been.calledTwice');
+  });
+
+  it('empty content', () => {
+    cy.mount(<SplitterLayout data-testid="sl" />);
+    cy.findByTestId('sl').should('not.be.visible').should('exist');
+  });
+
+  //todo wip
+  it('Splitter click, move, focus - vertical', () => {
+    cy.mount(
+      <div style={{ width: '800px', height: '800px' }}>
+        <SplitterLayout vertical data-testid={'sl'}>
+          <SplitterElement data-testid={'se1'}>
+            <div>Content 1</div>
+          </SplitterElement>
+          <SplitterElement data-testid={'se2'}>
+            <div>Content 2</div>
+          </SplitterElement>
+          <SplitterElement data-testid={'se3'}>
+            <div> Content 3</div>
+          </SplitterElement>
+          <SplitterElement data-testid={'se4'}>
+            <div> Content 4</div>
+          </SplitterElement>
+        </SplitterLayout>
+      </div>
+    );
   });
 });
--- packages/main/src/components/SplitterLayout/SplitterLayout.test.tsx
+++ packages/main/src/components/SplitterLayout/SplitterLayout.test.tsx
@@ -1,8 +1,6 @@
-import { fireEvent, render, renderRtl, screen } from '@shared/tests';
+import { fireEvent, render, renderRtl } from '@shared/tests';
 import { createCustomPropsTest } from '@shared/tests/utils';
-import { ThemingParameters } from '@ui5/webcomponents-react-base';
 import React from 'react';
-import { Button } from '../../webComponents/Button';
 import { SplitterElement } from '../SplitterElement';
 import { SplitterLayout } from './index';
 
@@ -23,99 +21,6 @@ describe('SplitterLayout', () => {
         y: 0
       };
     };
-  });
-
-  test('Render Vertical SplitterLayout with multiple SplitterElements', () => {
-    const onClick = jest.fn();
-    const { asFragment } = render(
-      <SplitterLayout vertical={true} style={{ width: '800px', height: '400px' }} data-testid={'SplitterLayout'}>
-        <SplitterElement size={'200px'} data-testid={'SplitterElement1'}>
-          <Button data-testid={'Button'} onClick={onClick}>
-            Button 1
-          </Button>
-        </SplitterElement>
-        <SplitterElement minSize={300} size={'400px'} data-testid={'SplitterElement2'}>
-          <Button>Button 2</Button>
-        </SplitterElement>
-        <SplitterElement resizable={false} data-testid={'SplitterElement3'}>
-          <Button>Button 3</Button>
-        </SplitterElement>
-      </SplitterLayout>
-    );
-    const AllSplitter = document.querySelectorAll('[role="separator"]');
-
-    fireEvent.click(screen.getByTestId('Button'));
-    fireEvent.click(AllSplitter[0]);
-
-    expect(AllSplitter.length).toBe(1);
-    expect(AllSplitter[0]).toHaveStyle(`border: 1px dotted ${ThemingParameters.sapHighlightColor}`);
-
-    expect(screen.getByTestId('SplitterLayout')).toHaveStyle('width: 800px; height: 400px');
-    // flex is not defined, not sure why
-    // expect(screen.getByTestId('SplitterElement2')).toHaveStyle('flex: 0 0 auto; min-height: 300px');
-    expect(screen.getByTestId('SplitterElement2').dataset.minSize).toBe('300');
-    // expect(screen.getByTestId('SplitterElement3')).toHaveStyle('flex: 1 0 auto');
-    expect(onClick).toHaveBeenCalled();
-
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('Render Vertical SplitterLayout with only one SplitterElement', () => {
-    const onClick = jest.fn();
-    const { asFragment } = render(
-      <SplitterLayout vertical={true} style={{ width: '700px', height: '400px' }} data-testid={'SplitterLayout'}>
-        <SplitterElement size={'200px'} data-testid={'SplitterElement'}>
-          <Button data-testid={'Button'} onClick={onClick}>
-            Button 1
-          </Button>
-        </SplitterElement>
-      </SplitterLayout>
-    );
-    const AllSplitter = document.querySelectorAll('[role="separator"]');
-
-    fireEvent.click(screen.getByTestId('Button'));
-
-    expect(AllSplitter.length).toBe(0);
-    expect(screen.getByTestId('SplitterLayout')).toHaveStyle('width: 700px; height: 400px');
-    expect(screen.getByTestId('SplitterLayout').dataset.splitterVertical).toBe('true');
-    // flex is not defined, not sure why
-    // expect(screen.getByTestId('SplitterElement')).toHaveStyle('flex: 0 0 auto');
-    expect(onClick).toHaveBeenCalled();
-
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('Render Horizontal SplitterLayout with multiple SplitterElements', () => {
-    const { asFragment } = render(
-      <SplitterLayout data-testid={'Layout'} style={{ width: '80vw', height: '600px' }}>
-        <SplitterElement>
-          <Button>Button 1</Button>
-        </SplitterElement>
-        <SplitterElement data-testid={'Element'} minSize={50} size={'100px'}>
-          <Button>Button 2</Button>
-        </SplitterElement>
-        <SplitterElement>
-          <Button>Button 3</Button>
-        </SplitterElement>
-      </SplitterLayout>
-    );
-
-    const AllSplitter = document.querySelectorAll('[role="separator"]');
-
-    expect(AllSplitter.length).toBe(2);
-    expect(screen.getByTestId('Layout')).toHaveStyle('width: 80vw; height: 600px');
-    expect(screen.getByTestId('Layout').dataset.splitterVertical).toBe(undefined);
-    // flex is not defined, not sure why
-    // expect(screen.getByTestId('Element')).toHaveStyle('flex: 0 0 auto; min-width: 50px');
-    console.log(screen.getByTestId('Element').style.flexBasis, screen.getByTestId('Element').style);
-    expect(screen.getByTestId('Element').dataset.minSize).toBe('50');
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('Render SplitterLayout without props and children', () => {
-    const { asFragment } = render(<SplitterLayout></SplitterLayout>);
-
-    expect(asFragment()).toMatchSnapshot();
   });
 
   test('Splitter click, move, focus - vertical', () => {
