afe6db344f74076074a5397a5723ca74aa2af9fe
Lukas742 <lukas.harbarth@sap.com>
2023-03-09 07:31:23 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@4b48b157eb480d121a8ff337fd28649d55661e00 🚀

---
+++ main/assets/ColumnChart-737664f6.js
@@ -0,0 +1,36 @@
+import{j as r,a as m,F as c}from"./jsx-runtime-670450c2.js";import{C as h}from"./DomRefTable.module-db373cb0.js";import{D as s}from"./DocsHeader-61ee84cb.js";import{F as d}from"./Footer-72e50a4a.js";import"./index-f1f749bf.js";import{M as l,C as t}from"./index-d7563925.js";import"./chunk-QDOP3WTA-64bdbb8f.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{C as f,D as p,W as u,a as g,b as C,c as W,L as b,d as L,e as M}from"./ColumnChart.stories-d50530f0.js";import{u as a}from"./index-4fb8b842.js";import"./react-jss.esm-fe4dc3ff.js";import"./inheritsLoose-93e09647.js";import"./index-e9bbeaa8.js";import"./iframe-0a6a9469.js";import"../sb-preview/runtime.mjs";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./AriaLabelHelper-c82262ef.js";import"./MarkedEvents-8627ed01.js";import"./decline-0a03b0b0.js";import"./Icons-7b82f601.js";import"./class-map-5d8e4b2b.js";import"./Button-e27d9549.js";import"./Keys-3acbae73.js";import"./Device-208919c6.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-cbb29537.js";import"./clsx.m-1229b3e0.js";import"./index-c7ba7a71.js";import"./ThemingParameters-f4b4144e.js";import"./Import-2685c381.js";import"./TableOfContent-2e1f96a2.js";import"./index-4a625d6d.js";import"./WrappingType-b81e595a.js";import"./index-eab84561.js";import"./Label-040efebd.js";import"./Footer.module-c4f3b823.js";import"./index-943d5a76.js";import"./Popover-cbbb060d.js";import"./Integer-d9976c13.js";import"./PopupsCommon.css-ab0735c6.js";import"./FocusableElements-c2f39514.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-96c5f47c.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./react-18-2bb9dade.js";import"./DemoProps-9cd0a46b.js";import"./index-99d317a4.js";import"./defaults-e7d6e8b8.js";import"./ChartContainer-aee70d91.js";import"./index-7a2a7ca3.js";import"./i18n-defaults-41f1487d.js";import"./I18nContext-bef5b452.js";import"./debounce-bc2bf61c.js";import"./isString-77c0f675.js";import"./useCancelAnimationFallback-e09c48c2.js";import"./generateCategoricalChart-cf059bbf.js";import"./throttle-28509771.js";import"./index-4d501b15.js";import"./YAxisTicks-38ba4500.js";import"./debounce-5c77766a.js";import"./useTooltipFormatter-d1d0ffa3.js";import"./ChartDataLabel-abd2589e.js";import"./staticProps-9dd6f0f9.js";import"./useLongestYAxisLabel-fe317632.js";import"./useOnClickInternal-d27d7c1e.js";import"./react-content-loader.es-eef51af2.js";import"./useIsRTL-4cfef02b.js";function Kr(e={}){const{wrapper:i}=Object.assign({},a(),e.components);return i?r(i,Object.assign({},e,{children:r(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a",pre:"pre"},a(),e.components);return m(c,{children:[r(l,{of:f}),`
+`,r(s,{}),`
+`,r(o.h2,{children:"Example"}),`
+`,r(t,{of:p}),`
+`,r(o.h2,{children:"Properties"}),`
+`,r(h,{of:p}),`
+`,r("br",{}),`
+`,r(o.h2,{children:"More Examples"}),`
+`,r(o.h3,{children:"With custom color"}),`
+`,r(t,{of:u}),`
+`,r(o.h3,{children:"With Secondary Dimension"}),`
+`,r(t,{of:g}),`
+`,r(o.h3,{children:"With Stacks"}),`
+`,r(t,{of:C}),`
+`,r(o.h3,{children:"With Formatter"}),`
+`,r(t,{of:W}),`
+`,r(o.h3,{children:"Loading Placeholder"}),`
+`,r(t,{of:b}),`
+`,r(o.h3,{children:"With Reference Line"}),`
+`,m(o.p,{children:["You can set a reference line to any value by using the ",r(o.code,{children:"referenceLine"})," ",r(o.code,{children:"chartConfig"})," property. ",r(o.code,{children:"referenceLine"})," accepts all properties described ",r(o.a,{href:"https://recharts.org/en-US/api/ReferenceLine",children:"here"}),", but it's recommended to only use ",r(o.code,{children:"label"}),",",r(o.code,{children:"value"})," and ",r(o.code,{children:"color"})," to preserve the intended design."]}),`
+`,r(o.pre,{children:r(o.code,{className:"language-jsx",children:`<ColumnChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,r(t,{of:L}),`
+`,r(o.h2,{children:"With Highlighted Measures"}),`
+`,r(t,{of:M}),`
+`,r(d,{})]})}}export{Kr as default};
+//# sourceMappingURL=ColumnChart-737664f6.js.map
---
+++ main/assets/CustomComponentsSlots-2e4f6455.js
@@ -0,0 +1,42 @@
+import{j as t,a as r,F as s}from"./jsx-runtime-670450c2.js";import"./chunk-QDOP3WTA-64bdbb8f.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{M as c}from"./index-d7563925.js";import{B as m}from"./index-f0ae9d9b.js";import{B as a}from"./index-82ccb188.js";import"./DomRefTable.module-db373cb0.js";import"./index-f1f749bf.js";import"./Import-2685c381.js";import{F as d}from"./Footer-72e50a4a.js";import{u as p}from"./index-4fb8b842.js";import"./iframe-0a6a9469.js";import"../sb-preview/runtime.mjs";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./Bar-2267974f.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./fastNavigation-8b41cdda.js";import"./AriaLabelHelper-c82262ef.js";import"./ResizeHandler-1fa8a094.js";import"./class-map-5d8e4b2b.js";import"./parameters-bundle.css-6b4a2ffb.js";import"./Button-e27d9549.js";import"./MarkedEvents-8627ed01.js";import"./Keys-3acbae73.js";import"./Device-208919c6.js";import"./Icons-7b82f601.js";import"./react-jss.esm-fe4dc3ff.js";import"./index-e9bbeaa8.js";import"./decline-0a03b0b0.js";import"./index-cbb29537.js";import"./clsx.m-1229b3e0.js";import"./Footer.module-c4f3b823.js";import"./index-4a625d6d.js";import"./WrappingType-b81e595a.js";import"./index-eab84561.js";import"./Label-040efebd.js";import"./index-943d5a76.js";import"./Popover-cbbb060d.js";import"./Integer-d9976c13.js";import"./PopupsCommon.css-ab0735c6.js";import"./FocusableElements-c2f39514.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";const l=()=>t("div",{children:"Start"}),h=()=>t(a,{children:"Close"}),u=()=>t(m,{startContent:t(l,{}),endContent:t(h,{}),children:t("div",{children:"I'm not a custom component"})}),B=n=>t("div",{slot:n.slot,children:"Start"}),C=n=>t(a,{slot:n.slot,children:"Close"}),v=()=>t(m,{startContent:t(B,{}),endContent:t(C,{}),children:t("div",{children:"I'm not a custom component"})});function xt(n={}){const{wrapper:e}=Object.assign({},p(),n.components);return e?t(e,Object.assign({},n,{children:t(i,{})})):i();function i(){const o=Object.assign({h1:"h1",p:"p",code:"code",pre:"pre"},p(),n.components);return r(s,{children:[t(c,{title:"Handling Slots"}),`
+`,t(o.h1,{children:"Adding custom components to slots"}),`
+`,r(o.p,{children:["With our wrapper we provide an easy way to consume the ",t(o.code,{children:"slots"})," of the UI5 Web Components by attaching them to a ",t(o.code,{children:"prop"}),`.
+In most cases you don't have to take anything into account, and the props can be used in a typical React way. However, there is an exception with custom components:`]}),`
+`,t(o.pre,{children:t(o.code,{className:"language-jsx",children:`const BarStart = () => {
+  return <div>Start</div>;
+};
+const BarEnd = () => {
+  return <Button>Close</Button>;
+};
+export const BarComponent = () => {
+  return (
+    <Bar startContent={<BarStart />} endContent={<BarEnd />}>
+      <div>I'm not a custom component</div>
+    </Bar>
+  );
+};
+`})}),`
+`,r(o.p,{children:["The ",t(o.code,{children:"BarComponent"})," would compile to this:"]}),`
+`,t(u,{}),`
+`,r(o.p,{children:[`As you can see, both custom components are not displayed.
+Our wrapper does add the `,t(o.code,{children:"slot"})," prop to the custom component, but the most outer HTML-Element/component (e.g. ",t(o.code,{children:"<div>Start</div>"}),") does not accept it and therefore cannot use it."]}),`
+`,r(o.p,{children:["To fix this the ",t(o.code,{children:"slot"})," prop must also be passed to the outer element of the component:"]}),`
+`,t(o.pre,{children:t(o.code,{className:"language-jsx",children:`const BarStart = (props) => {
+  return <div slot={props.slot}>Start</div>;
+};
+const BarEnd = (props) => {
+  return <Button slot={props.slot}>Close</Button>;
+};
+export const BarComponent = () => {
+  return (
+    <Bar startContent={<BarStart />} endContent={<BarEnd />}>
+      <div>I'm not a custom component</div>
+    </Bar>
+  );
+};
+`})}),`
+`,r(o.p,{children:["Now the ",t(o.code,{children:"BarComponent"})," would compile to this:"]}),`
+`,t(v,{}),`
+`,t("div",{style:{height:"var(--_ui5_bar_base_height)"}}),`
+`,t(d,{style:{position:"fixed",bottom:0,zIndex:2,maxHeight:"var(--_ui5_bar_base_height)"}})]})}}export{xt as default};
+//# sourceMappingURL=CustomComponentsSlots-2e4f6455.js.map
