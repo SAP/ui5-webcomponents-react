a00210ccbe27cf444b4db9464fa4041db12b3c10
Marcus Notheis <marcus.notheis@sap.com>
2023-03-13 18:05:39 +0100

    docs(SegmentedButton): rewrite story to CSF-3

---
+++ packages/main/src/webComponents/SegmentedButton/SegmentedButton.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SegmentedButtonItem } from '../SegmentedButtonItem/index.js';
+import { SegmentedButton } from './index.js';
+
+const meta = {
+  title: 'Inputs / SegmentedButton',
+  component: SegmentedButton,
+  argTypes: {
+    children: { control: { disable: true } }
+  },
+  args: {
+    children: (
+      <>
+        <SegmentedButtonItem>Button 1</SegmentedButtonItem>
+        <SegmentedButtonItem pressed>Button 2</SegmentedButtonItem>
+        <SegmentedButtonItem>Button 3</SegmentedButtonItem>
+      </>
+    )
+  }
+} satisfies Meta<typeof SegmentedButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
