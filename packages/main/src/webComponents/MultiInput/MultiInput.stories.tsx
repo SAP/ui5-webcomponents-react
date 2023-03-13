46374d5bd44e08a37f19766b1704f036664ca8a7
Marcus Notheis <marcus.notheis@sap.com>
2023-03-13 14:50:03 +0100

    MultiInput

---
+++ packages/main/src/webComponents/MultiInput/MultiInput.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { InputType, ValueState } from '../../enums/index.js';
+import { Token } from '../Token/index.js';
+import { MultiInput } from './index.js';
+
+const meta = {
+  title: 'Inputs / MultiInput',
+  component: MultiInput,
+  argTypes: {
+    tokens: { control: { disable: true } },
+    children: { control: { disable: true } },
+    icon: { control: { disable: true } },
+    valueStateMessage: { control: { disable: true } }
+  },
+  args: {
+    type: InputType.Text,
+    valueState: ValueState.None,
+    style: { width: '400px' }
+  }
+} satisfies Meta<typeof MultiInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => {
+    return (
+      <MultiInput
+        {...args}
+        tokens={
+          <>
+            <Token text="Argentina" />
+            <Token text="Bulgaria" />
+            <Token text="England" />
+            <Token text="Finland" />
+            <Token text="Germany" />
+            <Token text="Hungary" />
+            <Token text="Italy" />
+            <Token text="Luxembourg" />
+            <Token text="Mexico" />
+            <Token text="Philippines" />
+            <Token text="Sweden" />
+            <Token text="USA" />
+          </>
+        }
+      />
+    );
+  }
+};
