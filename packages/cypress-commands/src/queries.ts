41c748ac3d6e2d3b6c368ad3d0ab4472a4f65981
Lukas Harbarth <lukas.harbarth@sap.com>
2023-03-13 11:02:11 +0100

    feat: introduce new `@ui5/webcomponents-cypress-commands` package (#4289)

--- packages/cypress-commands/src/queries.ts
+++ packages/cypress-commands/src/queries.ts
@@ -2,6 +2,28 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
+      // TabContainer
+      /**
+       * Returns the tab of the `ui5-tabcontainer` by its text.
+       *
+       * __Note:__ Has to be chained to a `ui5-tabcontainer`.
+       * __Note:__ This will not work for sub-tabs.
+       *
+       * @example cy.get('[ui5-tab-container]').findUi5TabByText('Tab 1');
+       * @param {string} text The text of the tab that should be queried.
+       */
+      findUi5TabByText(text: string): Chainable<Element>;
+
+      /**
+       * Returns the open-popover button for sub-tabs by its text.
+       *
+       * __Note:__ Has to be chained to a `ui5-tabcontainer`.
+       * __Note:__ The tab only renders a button fur sub-tabs if the tab itself has content, otherwise the whole tab is responsible for opening the popover.
+       *
+       * @example cy.get('[ui5-tab-container]').findUi5TabOpenPopoverButtonByText('Tab 1.1');
+       * @param {string} text The text of the sub-tab that should be queried.
+       */
+      findUi5TabOpenPopoverButtonByText(text: string, options?: Partial<ClickOptions>): Chainable<Element>;
       // private
       /**
        * Returns the internal input element inside the shadow-root.
@@ -10,26 +32,6 @@ declare global {
        * @example cy.get('[ui5-checkbox]').findShadowInput();
        */
       findShadowInput(): Chainable<Element>;
-      // TabContainer
-      /**
-       * Returns the first tab by its position inside a `ui5-tabcontainer`.
-       *
-       * __Note:__ Has to be chained to a `ui5-tabcontainer`.
-       * __Note:__ This will not work for sub-tabs.
-       *
-       * @example cy.get('[ui5-tab-container]').findUi5TabByText('Tab 1');
-       */
-      findUi5TabByText(text: string): Chainable<Element>;
-
-      /**
-       * Returns the open-popover button for sub-tabs.
-       *
-       * __Note:__ Has to be chained to a `ui5-tabcontainer`.
-       * __Note:__ The tab only renders a button fur sub-tabs if the tab itself has content, otherwise the whole tab is responsible for opening the popover.
-       *
-       * @example cy.get('[ui5-tab-container]').findUi5TabOpenPopoverButtonByText('Tab 1');
-       */
-      findUi5TabOpenPopoverButtonByText(text: string, options?: Partial<ClickOptions>): Chainable<Element>;
     }
   }
 }
