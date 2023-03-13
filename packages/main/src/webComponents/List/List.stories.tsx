8a27e8e5d0a259fae4effb2a136899f10736c9a9
Marcus Notheis <marcus.notheis@sap.com>
2023-03-13 15:45:38 +0100

    docs: migrate List and all list items to CSF-3 (#4304)

---
+++ packages/main/src/webComponents/List/List.stories.tsx
@@ -0,0 +1,83 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemingParameters } from '@ui5/webcomponents-react-base';
+import { FlexBox } from '../../components/FlexBox';
+import { Text } from '../../components/Text';
+import { FlexBoxJustifyContent, ListGrowingMode, ListMode, ListSeparators } from '../../enums';
+import { CustomListItem } from '../CustomListItem';
+import { GroupHeaderListItem } from '../GroupHeaderListItem';
+import { ProgressIndicator } from '../ProgressIndicator';
+import { StandardListItem } from '../StandardListItem';
+import { List } from './index.js';
+
+const meta = {
+  title: 'Data Display / List',
+  component: List,
+  argTypes: {
+    children: { control: { disable: true } },
+    header: { control: { disable: true } }
+  },
+  args: {
+    headerText: 'List with StandardListItems',
+    mode: ListMode.None,
+    separators: ListSeparators.All,
+    growing: ListGrowingMode.None
+  }
+} satisfies Meta<typeof List>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => {
+    return (
+      <List {...args}>
+        <StandardListItem additionalText="3">List Item 1</StandardListItem>
+        <StandardListItem additionalText="2">List Item 2</StandardListItem>
+        <StandardListItem additionalText="1">List Item 3</StandardListItem>
+      </List>
+    );
+  }
+};
+
+export const CustomListItemStory: Story = {
+  name: 'CustomListItem',
+  args: {
+    headerText: 'List with a CustomListItem'
+  },
+  render: (args) => {
+    return (
+      <List {...args}>
+        <CustomListItem>
+          <FlexBox justifyContent={FlexBoxJustifyContent.SpaceAround} style={{ width: '100%' }}>
+            <Text style={{ fontWeight: 'bold' }}>FULLY</Text>
+            <Text style={{ color: 'red' }}>CUSTOMIZABLE</Text>
+            <Text style={{ color: 'white', backgroundColor: ThemingParameters.sapButton_Emphasized_Background }}>
+              CHILDREN
+            </Text>
+          </FlexBox>
+        </CustomListItem>
+        <CustomListItem>
+          <ProgressIndicator value={50} />
+        </CustomListItem>
+      </List>
+    );
+  }
+};
+
+export const GroupHeaderListItemStory: Story = {
+  name: 'GroupHeaderListItem',
+  args: {
+    headerText: 'List with a GroupHeaderListItem'
+  },
+  render: (args) => {
+    return (
+      <List {...args}>
+        <GroupHeaderListItem>GroupHeaderListItem 1</GroupHeaderListItem>
+        <StandardListItem>List Item</StandardListItem>
+        <StandardListItem>List Item</StandardListItem>
+        <GroupHeaderListItem>GroupHeaderListItem 2</GroupHeaderListItem>
+        <StandardListItem>List Item</StandardListItem>
+      </List>
+    );
+  }
+};
---
+++ packages/main/src/webComponents/NotificationListItem/NotificationListItem.stories.tsx
@@ -0,0 +1,112 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import employeeIcon from '@ui5/webcomponents-icons/dist/employee.js';
+import { AvatarColorScheme, AvatarSize, ButtonDesign, Priority, WrappingType } from '../../enums/index.js';
+import { Avatar } from '../Avatar';
+import { Label } from '../Label';
+import { List } from '../List';
+import { NotificationAction } from '../NotificationAction';
+import { NotificationListItem } from './index.js';
+
+const meta = {
+  title: 'Data Display / NotificationListItem',
+  component: NotificationListItem,
+  argTypes: {
+    avatar: { control: { disable: true } },
+    children: { control: 'text' },
+    footnotes: { control: { disable: true } },
+    actions: { control: { disable: true } }
+  },
+  args: {
+    titleText:
+      'New order (#2525) With a very long title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc.',
+    priority: Priority.None,
+    children:
+      'And with a very long description and long labels of the action buttons - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc.',
+    style: { width: '600px' }
+  }
+} satisfies Meta<typeof NotificationListItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    avatar: (
+      <Avatar size={AvatarSize.XS}>
+        <img src="https://sap.github.io/ui5-webcomponents/assets/images/avatars/woman_avatar_1.png" />
+      </Avatar>
+    ),
+    footnotes: (
+      <>
+        <Label>Monique Legrand</Label>
+        <Label>2 Days</Label>
+      </>
+    )
+  },
+  render: (args) => {
+    return (
+      <List>
+        <NotificationListItem {...args} />
+      </List>
+    );
+  }
+};
+
+export const MultipleNotifications: Story = {
+  render: () => {
+    return (
+      <List>
+        <NotificationListItem
+          showClose
+          avatar={<Avatar size={AvatarSize.XS} icon={employeeIcon} />}
+          footnotes={<Label>3 Days</Label>}
+          titleText="New order (#2526) with multiple NotificationAction buttons and close button"
+          actions={
+            <>
+              <NotificationAction design={ButtonDesign.Transparent} text="Accept All Requested Information" />
+              <NotificationAction design={ButtonDesign.Transparent} text="Reject All Requested Information" />
+            </>
+          }
+        >
+          Short description
+        </NotificationListItem>
+        <NotificationListItem
+          avatar={<Avatar size={AvatarSize.XS} icon={employeeIcon} colorScheme={AvatarColorScheme.Accent1} />}
+          footnotes={<Label>3 Days</Label>}
+          titleText="New order (#2527) with single NotificationAction and close button"
+          showClose
+          actions={<NotificationAction design={ButtonDesign.Transparent} text="Accept All Requested Information" />}
+        >
+          Short description
+        </NotificationListItem>
+        <NotificationListItem
+          wrappingType={WrappingType.Normal}
+          titleText="New high priority order (#2528) without close button"
+          priority={Priority.High}
+          avatar={
+            <Avatar size={AvatarSize.XS}>
+              <img src="https://sap.github.io/ui5-webcomponents/assets/images/avatars/woman_avatar_1.png" />
+            </Avatar>
+          }
+          footnotes={
+            <>
+              <Label>John Smith</Label>
+              <Label>1 hour</Label>
+            </>
+          }
+        >
+          And with a very long description that will not be ellipsed:
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra,
+          tellus odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum
+          lectus turpis at nunc. <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra,
+          tellus odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum
+          lectus turpis at nunc.
+        </NotificationListItem>
+      </List>
+    );
+  }
+};
