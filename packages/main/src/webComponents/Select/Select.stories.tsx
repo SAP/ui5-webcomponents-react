75818f8c440447cb4b4d438909dd723b0c883630
Marcus Notheis <marcus.notheis@sap.com>
2023-03-13 16:34:17 +0100

    docs: migrate ComboBox, MultiComboBox, Select, Input & MultiInput to CSF (#4308)

---
+++ packages/main/src/webComponents/Select/Select.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ValueState } from '../../enums/index.js';
+import { Option } from '../Option/index.js';
+import { Select } from './index.js';
+
+const meta = {
+  title: 'Inputs / Select',
+  component: Select,
+  argTypes: {
+    children: { control: { disable: true } },
+    valueStateMessage: { control: { disable: true } }
+  },
+  args: {
+    valueState: ValueState.None
+  }
+} satisfies Meta<typeof Select>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => {
+    return (
+      <Select {...args}>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+        <Option>Option 3</Option>
+        <Option>Option 4</Option>
+        <Option>Option 5</Option>
+      </Select>
+    );
+  }
+};
