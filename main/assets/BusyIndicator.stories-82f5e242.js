c30c3d826d99caff09e9dc99a8a63109fbdc5133
Lukas742 <lukas.harbarth@sap.com>
2023-03-14 14:13:41 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@d46bcdaca6b2dbb9e6bd310749f9802e99d23f6b 🚀

---
+++ main/assets/BusyIndicator.stories-82f5e242.js
@@ -0,0 +1,42 @@
+import{M as y,C as a,j as d,a as g}from"./index-ebc6f2a8.js";import{B as r}from"./index-907e0799.js";import{a as c}from"./BusyIndicator-46a9aff5.js";import{S as l}from"./index-82564fae.js";import{T as b}from"./index-2eaff221.js";import{b as f}from"./DomRefTable.module-ecca9f20.js";import{D as v}from"./DocsHeader-e27afa3d.js";import{F as S}from"./Footer-2c1d1d32.js";import{j as t,a as k,F as w}from"./jsx-runtime-670450c2.js";import"./index-f1f749bf.js";import{u}from"./index-4fb8b842.js";import"./iframe-922b4986.js";import"../sb-preview/runtime.mjs";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./inheritsLoose-93e09647.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./UI5Element-427ec721.js";import"./Keys-3acbae73.js";import"./Integer-f7f172c9.js";import"./Label-040efebd.js";import"./Device-208919c6.js";import"./WrappingType-b81e595a.js";import"./class-map-5d8e4b2b.js";import"./i18n-defaults-d66b3d5e.js";import"./event-97133b94.js";import"./slot-634e3e91.js";import"./InvisibleMessage-4a23efee.js";import"./AriaLabelHelper-43a261ec.js";import"./ValueState-2c5e5904.js";import"./slim-arrow-down-068fe0af.js";import"./Icons-7b82f601.js";import"./information-50e1ad0f.js";import"./i18n-defaults-80781f7e.js";import"./decline-f2b294b3.js";import"./ListItemBase-16737f88.js";import"./ResizeHandler-1fa8a094.js";import"./ItemNavigation-18222ac6.js";import"./getActiveElement-bcae01ed.js";import"./TabbableElements-8e49b367.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-615afd63.js";import"./debounce-9c2fb7dd.js";import"./style-map-77d201d2.js";import"./ResponsivePopover-36c83c4d.js";import"./PopupUtils-a2e37749.js";import"./Popover-907d427e.js";import"./PopupsCommon.css-7d9fbba1.js";import"./FocusableElements-7e25663e.js";import"./MediaRange-25b98f31.js";import"./Dialog-644759b8.js";import"./Icon-459702a7.js";import"./Button-04bad9a0.js";import"./MarkedEvents-b83081e9.js";import"./Title-b986a4bd.js";import"./StandardListItem-ed465290.js";import"./ListItem-20b1ed77.js";import"./RadioButton-28647d07.js";import"./CheckBox-90578acd.js";import"./accept-a98baca8.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-bbef5919.js";import"./Avatar-7c89587d.js";import"./employee-98ce1b38.js";import"./ResponsivePopoverCommon.css-187e38b6.js";import"./ValueStateMessage.css-37773c69.js";import"./ValueState-ab6838cc.js";import"./clsx.m-1229b3e0.js";import"./react-jss.esm-e54d2f1d.js";import"./ThemingParameters-f4b4144e.js";import"./chunk-PCJTTTQV-29a63e2d.js";import"./react-18-2bb9dade.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./index-69579fa7.js";import"./index-da505e61.js";import"./Import-b6629dfa.js";import"./TableOfContent-3c2e47f0.js";import"./index-b539d5f9.js";import"./Link-6d546ee9.js";import"./index-6172aa9c.js";import"./Footer.module-f90cad7d.js";import"./index-a68ca2ac.js";const I=`## Usage
+
+For the \`BusyIndicator\` you can define the size, the text and whether it is shown or hidden. In order to hide it, use the "active" property.  
+  
+In order to show busy state over an HTML element, simply nest the HTML element in a \`BusyIndicator\` instance.  
+**Note:** Since \`BusyIndicator\` has \`display: inline-block;\` by default and no width of its own, whenever you need to wrap a block-level element, you should set \`display: block\` to the busy indicator as well.
+
+### When to use:
+
+*   The user needs to be able to cancel the operation.
+*   Only part of the application or a particular component is affected.
+
+### When not to use:
+
+*   The operation takes less than one second.
+*   You need to block the screen and prevent the user from starting another activity.
+*   Do not show multiple busy indicators at once.`;function B(e={}){const{wrapper:s}=Object.assign({},u(),e.components);return s?t(s,{...e,children:t(p,{})}):p();function p(){const o=Object.assign({h2:"h2",h1:"h1",p:"p"},u(),e.components);return k(w,{children:[t(y,{title:"User Feedback / BusyIndicator",component:r,argTypes:{children:{control:{disable:!0}}},args:{size:c.Medium,delay:1e3,active:!0}}),`
+`,t(v,{since:"0.4.0"}),`
+`,t("br",{}),`
+`,t(o.h2,{children:"Example"}),`
+`,t(a,{children:t(d,{name:"Default",children:n=>t(r,{...n})})}),`
+`,t(o.h2,{children:"Properties"}),`
+`,t(f,{story:"Default"}),`
+`,t(g,{children:I}),`
+`,t(o.h1,{children:"More Examples"}),`
+`,t("br",{}),`
+`,t(o.h2,{children:"BusyIndicator with children"}),`
+`,t(a,{children:t(d,{name:"With children",args:{active:!0},children:n=>t(r,{...n,children:t(l,{})})})}),`
+`,t(a,{mdxSource:'<BusyIndicator active><Text><p>{"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et\\ndolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita\\nkasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur\\nsadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam\\nvoluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata\\nsanctus est Lorem ipsum dolor sit amet."}</p></Text></BusyIndicator>',children:t(r,{active:!0,children:t(b,{children:t(o.p,{children:`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+sanctus est Lorem ipsum dolor sit amet.`})})})}),`
+`,t(S,{})]})}}const h=e=>t(r,{...e});h.storyName="Default";h.parameters={storySource:{source:`args => {
+  return <BusyIndicator {...args} />;
+}`}};const m=e=>t(r,{...e,children:t(l,{})});m.storyName="With children";m.args={active:!0};m.parameters={storySource:{source:`args => {
+  return <BusyIndicator {...args}>
+          <Select />
+        </BusyIndicator>;
+}`}};const i={title:"User Feedback / BusyIndicator",component:r,args:{size:c.Medium,delay:1e3,active:!0},argTypes:{children:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory","withChildren"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:B};const de=["defaultStory","withChildren"];export{de as __namedExportsOrder,i as default,h as defaultStory,m as withChildren};
+//# sourceMappingURL=BusyIndicator.stories-82f5e242.js.map
---
+++ main/assets/ColorPalettePopover-5ffe270d.js
@@ -0,0 +1,33 @@
+import{j as o,a as r,F as s}from"./jsx-runtime-670450c2.js";import{C as l}from"./DomRefTable.module-ecca9f20.js";import{D as c}from"./DocsHeader-e27afa3d.js";import{F as h}from"./Footer-2c1d1d32.js";import"./index-f1f749bf.js";import{D as d}from"./DomRefTable-3244d9b7.js";import{M as f,C,a as n,b as P}from"./index-ebc6f2a8.js";import{C as u,D as m}from"./ColorPalettePopover.stories-f036e338.js";import{S as v}from"./SubcomponentsSection-6138f2fc.js";import{C as b}from"./ColorPaletteItemDescription-2583043f.js";import{C as g}from"./index-fc2fee47.js";import{u as a}from"./index-4fb8b842.js";import"./chunk-PCJTTTQV-29a63e2d.js";import"./iframe-922b4986.js";import"../sb-preview/runtime.mjs";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./index-69579fa7.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./event-97133b94.js";import"./decline-f2b294b3.js";import"./Icons-7b82f601.js";import"./i18n-defaults-80781f7e.js";import"./information-50e1ad0f.js";import"./class-map-5d8e4b2b.js";import"./Icon-459702a7.js";import"./Keys-3acbae73.js";import"./Button-04bad9a0.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-d66b3d5e.js";import"./index-da505e61.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-b6629dfa.js";import"./TableOfContent-3c2e47f0.js";import"./index-b539d5f9.js";import"./Link-6d546ee9.js";import"./WrappingType-b81e595a.js";import"./index-6172aa9c.js";import"./Label-040efebd.js";import"./Footer.module-f90cad7d.js";import"./index-a68ca2ac.js";import"./Popover-907d427e.js";import"./Integer-f7f172c9.js";import"./PopupUtils-a2e37749.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-7d9fbba1.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-615afd63.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./index-7b372893.js";import"./CSSColor-f91c8158.js";import"./ResponsivePopoverCommon.css-187e38b6.js";import"./Title-b986a4bd.js";import"./ResponsivePopover-36c83c4d.js";import"./Dialog-644759b8.js";import"./ValueState-2c5e5904.js";import"./ItemNavigation-18222ac6.js";const w=[{name:"openPopover",visibility:"public",since:"1.0.0-rc.16",parameters:[{name:"opener",type:"HTMLElement",optional:!1,description:"the element that the popover is shown at"}],description:"Shows the ColorPalettePopover. <b>Note:</b> The method is deprecated and will be removed in future, use <code>showAt</code> instead.",deprecated:{text:"The method is deprecated in favour of <code>showAt</code>."}},{name:"showAt",visibility:"public",since:"1.1.1",parameters:[{name:"opener",type:"HTMLElement",optional:!1,description:"the element that the popover is shown at"}],description:"Shows the ColorPalettePopover."}];function Vo(t={}){const{wrapper:i}=Object.assign({},a(),t.components);return i?o(i,Object.assign({},t,{children:o(p,{})})):p();function p(){const e=Object.assign({h2:"h2",p:"p",code:"code",strong:"strong",em:"em",pre:"pre",h3:"h3"},a(),t.components);return r(s,{children:[o(f,{of:u}),`
+`,o(c,{since:"0.20.0"}),`
+`,o("br",{}),`
+`,o(e.h2,{children:"Example"}),`
+`,o(C,{of:m}),`
+`,o(e.h2,{children:"Properties"}),`
+`,o(l,{of:m}),`
+`,o(d,{rows:w}),`
+`,o(e.h2,{children:"Opening ColorPalettePopovers"}),`
+`,r(e.p,{children:[o(e.code,{children:"ColorPalettePopover"}),"s can only be opened by attaching a ",o(e.code,{children:"ref"})," to the component and then call the corresponding ",o(e.strong,{children:o(e.code,{children:"showAt"})})," method. The method receives the target element - ",r(e.em,{children:["on which the ",o(e.code,{children:"ColorPalettePopover"})," is to be opened"]})," - as parameter."]}),`
+`,o(e.pre,{children:o(e.code,{className:"language-jsx",children:`const ColorPalettePopoverComponent = () => {
+  const popoverRef = useRef();
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open ColorPalettePopover</Button>
+      <ColorPalettePopover ref={popoverRef}>
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="darkblue" />
+      </ColorPalettePopover>
+    </>
+  );
+};
+`})}),`
+`,o(n,{children:v}),`
+`,o(e.h2,{children:"ColorPaletteItem"}),`
+`,o(n,{children:b}),`
+`,o(e.h3,{children:"Properties"}),`
+`,o(P,{of:g}),`
+`,o(h,{})]})}}export{Vo as default};
+//# sourceMappingURL=ColorPalettePopover-5ffe270d.js.map
