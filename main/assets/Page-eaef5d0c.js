c4ccb82261d8b01af0e6baa06fbf3083f6da4576
MarcusNotheis <marcus.notheis@sap.com>
2023-03-14 08:21:33 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@e683b169fd4830e0ab7f5cfbec69d25a7776ce3a 🚀

---
+++ main/assets/Page-eaef5d0c.js
@@ -0,0 +1,23 @@
+import{j as t,a as m,F as a}from"./jsx-runtime-670450c2.js";import{b as s}from"./DomRefTable.module-1d4dc1c3.js";import{D as h}from"./DocsHeader-0e55b533.js";import{F as c}from"./Footer-1eb82ec5.js";import"./index-f1f749bf.js";import{M as d,C as l,j as f,a as u}from"./index-b46f0dbd.js";import{C as b,D as p}from"./Page.stories-d109eefc.js";import{u as n}from"./index-4fb8b842.js";import"./chunk-PCJTTTQV-af8ad928.js";import"./iframe-69a0af02.js";import"../sb-preview/runtime.mjs";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-fe4dc3ff.js";import"./inheritsLoose-93e09647.js";import"./index-c684a8c1.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./event-97133b94.js";import"./decline-f2b294b3.js";import"./Icons-7b82f601.js";import"./i18n-defaults-80781f7e.js";import"./information-50e1ad0f.js";import"./class-map-5d8e4b2b.js";import"./Icon-459702a7.js";import"./Keys-3acbae73.js";import"./Button-876771f0.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-6bc67b6a.js";import"./index-cbb29537.js";import"./clsx.m-1229b3e0.js";import"./index-c7ba7a71.js";import"./ThemingParameters-f4b4144e.js";import"./Import-b54ec6b7.js";import"./TableOfContent-59485fc9.js";import"./index-2458596a.js";import"./Link-687260e5.js";import"./WrappingType-b81e595a.js";import"./index-98a1d896.js";import"./Label-040efebd.js";import"./Footer.module-f90cad7d.js";import"./index-f7fb38b9.js";import"./Popover-54e30804.js";import"./Integer-f7f172c9.js";import"./PopupsCommon.css-d3649668.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-615afd63.js";import"./getActiveElement-bcae01ed.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./languageAware-b2dd96bd.js";import"./PageBackgroundDesign-7e89cd25.js";import"./parameters-bundle.css-6b4a2ffb.js";import"./index-14d23bee.js";import"./Bar-62766cca.js";import"./fastNavigation-8b41cdda.js";import"./index-78e9955d.js";const g=`## Structure
+
+### Header
+
+The top most area of the page is occupied by the header. The standard header includes a navigation button and a title.
+
+### Content
+
+The content occupies the main part of the page. Only the content area is scrollable by default. This can be prevented by setting \`enableScrolling\` to \`false\`.
+
+### Footer
+
+The footer is optional and occupies the fixed bottom part of the page. Alternatively, the footer can be floating above the bottom part of the content. This is enabled with the \`floatingFooter\` property. **Note:** \`Page\` occipues the whole available space of its parent. In order to achieve the intended design you have to make sure that there is enough space for the \`Page\` to be rendered. **Note:** In order for the \`Page\` to be displayed, the parent element should have fixed height.
+`;function Lt(o={}){const{wrapper:e}=Object.assign({},n(),o.components);return e?t(e,Object.assign({},o,{children:t(r,{})})):r();function r(){const i=Object.assign({h2:"h2"},n(),o.components);return m(a,{children:[t(d,{of:b}),`
+`,t(h,{since:"0.14.0"}),`
+`,t("br",{}),`
+`,t(i.h2,{children:"Example"}),`
+`,t(l,{children:t(f,{of:p})}),`
+`,t(i.h2,{children:"Properties"}),`
+`,t(s,{of:p}),`
+`,t(u,{children:g}),`
+`,t(c,{})]})}}export{Lt as default};
+//# sourceMappingURL=Page-eaef5d0c.js.map
---
+++ main/assets/SelectDialog-ef185ce5.js
@@ -0,0 +1,57 @@
+import{j as o,a as r,F as c}from"./jsx-runtime-670450c2.js";import{C as l}from"./DomRefTable.module-1d4dc1c3.js";import{D as s}from"./DocsHeader-0e55b533.js";import{F as d}from"./Footer-1eb82ec5.js";import"./index-f1f749bf.js";import{D as h}from"./DomRefTable-f6d54921.js";import{M as g,C as p}from"./index-b46f0dbd.js";import{D as u}from"./DialogDomRef-ffa73df1.js";import{C as f,D as m,M as D}from"./SelectDialog.stories-a9ff5103.js";import{u as a}from"./index-4fb8b842.js";import"./chunk-PCJTTTQV-af8ad928.js";import"./iframe-69a0af02.js";import"../sb-preview/runtime.mjs";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-fe4dc3ff.js";import"./inheritsLoose-93e09647.js";import"./index-c684a8c1.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./event-97133b94.js";import"./decline-f2b294b3.js";import"./Icons-7b82f601.js";import"./i18n-defaults-80781f7e.js";import"./information-50e1ad0f.js";import"./class-map-5d8e4b2b.js";import"./Icon-459702a7.js";import"./Keys-3acbae73.js";import"./Button-876771f0.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-6bc67b6a.js";import"./index-cbb29537.js";import"./clsx.m-1229b3e0.js";import"./index-c7ba7a71.js";import"./ThemingParameters-f4b4144e.js";import"./Import-b54ec6b7.js";import"./TableOfContent-59485fc9.js";import"./index-2458596a.js";import"./Link-687260e5.js";import"./WrappingType-b81e595a.js";import"./index-98a1d896.js";import"./Label-040efebd.js";import"./Footer.module-f90cad7d.js";import"./index-f7fb38b9.js";import"./Popover-54e30804.js";import"./Integer-f7f172c9.js";import"./PopupsCommon.css-d3649668.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-615afd63.js";import"./getActiveElement-bcae01ed.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./search-708dd69e.js";import"./CssSizeVariables-bd372cde.js";import"./index-99d317a4.js";import"./i18n-defaults-9da374be.js";import"./I18nContext-bef5b452.js";import"./index-11fe5f04.js";import"./overflow-492fbaba.js";import"./stopPropagation-da74d54e.js";import"./index-f8711f2a.js";import"./ToggleButton-ea6c886a.js";import"./CustomVariables-fd831c35.js";import"./debounce-5c77766a.js";import"./index-d106df8a.js";import"./ListItemBase-4ef398c9.js";import"./ItemNavigation-18222ac6.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-69e83aec.js";import"./index-925f3032.js";import"./Dialog-7fbd90ac.js";import"./ValueState-2c5e5904.js";import"./ValueState-ab6838cc.js";import"./index-78e9955d.js";import"./index-05ed941c.js";import"./Title-b986a4bd.js";import"./index-7fd21690.js";import"./Input-e1aea171.js";import"./Suggestions.css-faab6178.js";import"./ResponsivePopoverCommon.css-187e38b6.js";import"./ValueStateMessage.css-37773c69.js";import"./index-cc319b39.js";import"./index-151d5f75.js";import"./StandardListItem-a72572c6.js";import"./ListItem-b3713808.js";import"./RadioButton-6085072b.js";import"./CheckBox-2598fb75.js";import"./accept-a98baca8.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-bbef5919.js";import"./Avatar-fa3b7d1c.js";import"./employee-98ce1b38.js";function xt(i={}){const{wrapper:e}=Object.assign({},a(),i.components);return e?o(e,Object.assign({},i,{children:o(n,{})})):n();function n(){const t=Object.assign({h2:"h2",h4:"h4",p:"p",code:"code",pre:"pre",strong:"strong",h3:"h3"},a(),i.components);return r(c,{children:[o(g,{of:f}),`
+`,o(s,{}),`
+`,o("br",{}),`
+`,o(t.h2,{children:"Example"}),`
+`,o(p,{of:m}),`
+`,o(t.h2,{children:"Properties"}),`
+`,o(l,{of:m}),`
+`,o(h,{rows:u}),`
+`,o(t.h2,{children:"Opening SelectDialogs"}),`
+`,o("br",{}),`
+`,o(t.h4,{children:"since 0.22.0"}),`
+`,r(t.p,{children:["We recommend opening and closing the ",o(t.code,{children:"SelectDialog"})," component in a declarative way using the ",o(t.code,{children:"open"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,o(t.pre,{children:o(t.code,{className:"language-jsx",children:`const MyComponentWithSelectDialog = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+      >
+        Open SelectDialog
+      </Button>
+      <SelectDialog open={dialogIsOpen} headerText="Select Product">
+        <StandardListItem image="pathToImg" description="description">
+          Product
+        </StandardListItem>
+      </SelectDialog>
+    </>
+  );
+};
+`})}),`
+`,o(t.h4,{children:"before 0.22.0"}),`
+`,r(t.p,{children:[o(t.code,{children:"SelectDialog"}),"s can be opened by attaching a ",o(t.code,{children:"ref"})," to the component and then calling the corresponding ",o(t.strong,{children:o(t.code,{children:"show"})})," method."]}),`
+`,o(t.pre,{children:o(t.code,{className:"language-jsx",children:`const SelectDialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open SelectDialog</Button>
+      <SelectDialog ref={dialogRef} headerText="Select Product">
+        <StandardListItem image="pathToImg" description="description">
+          Product
+        </StandardListItem>
+      </SelectDialog>
+    </>
+  );
+};
+`})}),`
+`,o(t.h2,{children:"More Examples"}),`
+`,o(t.h3,{children:"SelectDialog in MultiSelect mode with search"}),`
+`,o(p,{of:D}),`
+`,o(d,{})]})}}export{xt as default};
+//# sourceMappingURL=SelectDialog-ef185ce5.js.map
