d37dfd98dca0dfd1af11c371f88f5e4b67a6dd8c
MarcusNotheis <marcus.notheis@sap.com>
2023-03-14 09:00:18 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@ac678e1fab3b17517f72b697af10c4a88bc993b5 🚀

---
+++ main/assets/Page-9ddeea2d.js
@@ -0,0 +1,23 @@
+import{j as t,a as m,F as a}from"./jsx-runtime-670450c2.js";import{b as s}from"./DomRefTable.module-85f8a001.js";import{D as h}from"./DocsHeader-7072b05c.js";import{F as c}from"./Footer-ebcbebcc.js";import"./index-f1f749bf.js";import{M as d,C as l,j as f,a as u}from"./index-6c258604.js";import{C as b,D as p}from"./Page.stories-fcaf1289.js";import{u as n}from"./index-4fb8b842.js";import"./chunk-PCJTTTQV-62735de0.js";import"./iframe-4f89a2a2.js";import"../sb-preview/runtime.mjs";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./index-2674bc44.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-e4481254.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./event-97133b94.js";import"./decline-f2b294b3.js";import"./Icons-7b82f601.js";import"./i18n-defaults-80781f7e.js";import"./information-50e1ad0f.js";import"./class-map-5d8e4b2b.js";import"./Icon-459702a7.js";import"./Keys-3acbae73.js";import"./Button-876771f0.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-6bc67b6a.js";import"./index-da505e61.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-e598d848.js";import"./TableOfContent-944d8df7.js";import"./index-ee50618f.js";import"./Link-687260e5.js";import"./WrappingType-b81e595a.js";import"./index-6d5771c3.js";import"./Label-040efebd.js";import"./Footer.module-f90cad7d.js";import"./index-6ceb43f7.js";import"./Popover-54e30804.js";import"./Integer-f7f172c9.js";import"./PopupsCommon.css-d3649668.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-615afd63.js";import"./getActiveElement-bcae01ed.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-77d201d2.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_baseForOwn-c7d9bea5.js";import"./mapValues-2de54f78.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-61436d32.js";import"./languageAware-b2dd96bd.js";import"./PageBackgroundDesign-7e89cd25.js";import"./parameters-bundle.css-6b4a2ffb.js";import"./index-44d9f66c.js";import"./Bar-62766cca.js";import"./fastNavigation-8b41cdda.js";import"./index-f9085d70.js";const g=`## Structure
+
+### Header
+
+The top most area of the page is occupied by the header. The standard header includes a navigation button and a title.
+
+### Content
+
+The content occupies the main part of the page. Only the content area is scrollable by default. This can be prevented by setting \`enableScrolling\` to \`false\`.
+
+### Footer
+
+The footer is optional and occupies the fixed bottom part of the page. Alternatively, the footer can be floating above the bottom part of the content. This is enabled with the \`floatingFooter\` property. **Note:** \`Page\` occipues the whole available space of its parent. In order to achieve the intended design you have to make sure that there is enough space for the \`Page\` to be rendered. **Note:** In order for the \`Page\` to be displayed, the parent element should have fixed height.
+`;function Lt(o={}){const{wrapper:e}=Object.assign({},n(),o.components);return e?t(e,Object.assign({},o,{children:t(r,{})})):r();function r(){const i=Object.assign({h2:"h2"},n(),o.components);return m(a,{children:[t(d,{of:b}),`
+`,t(h,{since:"0.14.0"}),`
+`,t("br",{}),`
+`,t(i.h2,{children:"Example"}),`
+`,t(l,{children:t(f,{of:p})}),`
+`,t(i.h2,{children:"Properties"}),`
+`,t(s,{of:p}),`
+`,t(u,{children:g}),`
+`,t(c,{})]})}}export{Lt as default};
+//# sourceMappingURL=Page-9ddeea2d.js.map
