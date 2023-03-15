e4564f12c5c64a0282f466e9d3fafe800c5039d0
Lukas Harbarth <lukas.harbarth@sap.com>
2023-03-15 08:56:42 +0100

    feat: integrate `@ui5/webcomponents-cypress-commands` into cra template (#4341)

--- packages/cra-template/template/src/App.cy.jsx
+++ packages/cra-template/template/src/App.cy.jsx
@@ -1,8 +1,13 @@
 import App from './App';
 
 describe('Component tests', () => {
-  it('render Link', () => {
+  it('render Link component with text', () => {
     cy.mount(<App />);
-    cy.get('[ui5-link]').should('be.visible').should('have.text', 'Getting Started with UI5 Web Component for React');
+    cy.get('[ui5-link]').should('be.visible').and('have.text', 'Getting Started with UI5 Web Component for React');
+  });
+  it('type into Input component', () => {
+    cy.mount(<App />);
+    cy.get('[ui5-input]').typeIntoUi5Input('Hello there!');
+    cy.get('[ui5-label]').should('be.visible').and('have.text', 'Hello there!');
   });
 });
