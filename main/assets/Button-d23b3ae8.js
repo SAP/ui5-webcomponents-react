094fbf892411abf41f95c46349f6ea3d2e78cc9a
MarcusNotheis <marcus.notheis@sap.com>
2023-03-16 08:53:22 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@b03f5be12e9f388a1698a528a38508a5bf830e66 🚀

---
+++ main/assets/Button-d23b3ae8.js
@@ -0,0 +1,28 @@
+import{j as t,a as p,F as a}from"./jsx-runtime-670450c2.js";import{M as m,C as l,a as c}from"./chunk-PCJTTTQV-b0cc9f0f.js";import{C as d}from"./DomRefTable.module-bc862f8f.js";import{D as u}from"./DocsHeader-fef14909.js";import{F as h}from"./Footer-7633878f.js";import"./index-f1f749bf.js";import{D as f}from"./DomRefTable-7af8f108.js";import{C as b,D as n}from"./Button.stories-d91ee12b.js";import{u as s}from"./index-4fb8b842.js";import"./iframe-17b9c670.js";import"../sb-preview/runtime.mjs";import"./react-18-ecd79c7b.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-96c5f47c.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-e54d2f1d.js";import"./index-8c3492f9.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-65cd39a0.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./Icon-7987c836.js";import"./Icons-fe6e657a.js";import"./decline-6bd98a2e.js";import"./i18n-defaults-80781f7e.js";import"./information-b6c8ed8e.js";import"./class-map-95320e87.js";import"./Button-bc3a11a1.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./index-8ba7ce6e.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-dc90fd8a.js";import"./TableOfContent-1a9517f1.js";import"./index-cac2b592.js";import"./Link-018547e8.js";import"./WrappingType-b81e595a.js";import"./index-47dedd52.js";import"./Label-ccc234fe.js";import"./index-51d46439.js";import"./Popover-224d761a.js";import"./Integer-f7f172c9.js";import"./PopupUtils-a2e37749.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-9873a291.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-1da6d0d2.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-cae0a379.js";import"./employee-1bc983a2.js";import"./index-93722b1e.js";const g=`## Usage
+
+For the \`Button\` UI, you can define text, icon, or both. You can also specify whether the text or the icon is displayed first.  
+  
+You can choose from a set of predefined types that offer different styling to correspond to the triggered action.  
+  
+You can set the \`Button\` as enabled or disabled. An enabled \`Button\` can be pressed by clicking or tapping it. The button changes its style to provide visual feedback to the user that it is pressed or hovered over with the mouse cursor. A disabled \`Button\` appears inactive and cannot be pressed.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Button\` exposes the following CSS Shadow Parts:
+
+*   button - Used to style the native button element`,y=[{name:"accessibilityAttributes",visibility:"public",since:"1.2.0",type:"object",noattribute:"true",description:`An object of strings that defines several additional accessibility attribute values for customization depending on the use case.
+
+It supports the following fields:
+
+<ul> <li><code>expanded</code>: Indicates whether the button, or another grouping element it controls, is currently expanded or collapsed. Accepts the following string values: <ul> <li><code>true</code></li> <li><code>false</code></li> </ul> </li> <li><code>hasPopup</code>: Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by the button. Accepts the following string values: <ul> <li><code>Dialog</code></li> <li><code>Grid</code></li> <li><code>ListBox</code></li> <li><code>Menu</code></li> <li><code>Tree</code></li> </ul> </li> <li><code>controls</code>: Identifies the element (or elements) whose contents or presence are controlled by the button element. Accepts a string value.</li> </ul>`}];function Pt(o={}){const{wrapper:e}=Object.assign({},s(),o.components);return e?t(e,Object.assign({},o,{children:t(i,{})})):i();function i(){const r=Object.assign({h2:"h2"},s(),o.components);return p(a,{children:[t(m,{of:b}),`
+`,t(u,{}),`
+`,t("br",{}),`
+`,t(r.h2,{children:"Example"}),`
+`,t(l,{of:n}),`
+`,t(r.h2,{children:"Properties"}),`
+`,t(d,{of:n}),`
+`,t(c,{children:g}),`
+`,t(f,{rows:y}),`
+`,t(h,{})]})}}export{Pt as default};
+//# sourceMappingURL=Button-d23b3ae8.js.map
