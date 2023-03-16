4c166f8592bcc4c7b35e15ee649d869697252123
MarcusNotheis <marcus.notheis@sap.com>
2023-03-16 05:56:59 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@487e0c7f0ab33f32e09f3b54443a89f72e40d401 🚀

---
+++ main/assets/VariantManagement-f8fa3f36.js
@@ -0,0 +1,93 @@
+import{j as e,a as n,F as l}from"./jsx-runtime-670450c2.js";import{C as c}from"./DomRefTable.module-3e3a093d.js";import{D as d}from"./DocsHeader-9bd53987.js";import{F as u}from"./Footer-591ed5cf.js";import{a as o}from"./VariantItem-fdacc02e.js";import"./index-f1f749bf.js";import{M as h,C as m,a as v,e as V,b as g}from"./chunk-PCJTTTQV-a4927159.js";import{S as w}from"./SubcomponentsSection-6138f2fc.js";import{C as S,D as p,W as f}from"./VariantManagement.stories-a09a3628.js";import{u as s}from"./index-4fb8b842.js";import{M}from"./index-11a4eb66.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-da505e61.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-d2fda164.js";import"./TableOfContent-6c3ba8a1.js";import"./index-9a9eee39.js";import"./iframe-6ffcb8d9.js";import"../sb-preview/runtime.mjs";import"./Link-018547e8.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-65cd39a0.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./Icon-7987c836.js";import"./Icons-fe6e657a.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./WrappingType-b81e595a.js";import"./i18n-defaults-254d6b69.js";import"./index-16deb058.js";import"./Label-ccc234fe.js";import"./Device-208919c6.js";import"./class-map-95320e87.js";import"./index-96c5f47c.js";import"./index-b08e30b5.js";import"./Popover-224d761a.js";import"./slot-634e3e91.js";import"./Integer-f7f172c9.js";import"./PopupUtils-a2e37749.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-9873a291.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-1da6d0d2.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-cae0a379.js";import"./UnableToLoad-3bc3a0d0.js";import"./index-d51714aa.js";import"./languageAware-b2dd96bd.js";import"./Title-129aeb28.js";import"./i18n-defaults-0a7ef706.js";import"./parameters-bundle.css-6b4a2ffb.js";import"./navigation-down-arrow-b005863d.js";import"./search-a32b329a.js";import"./i18n-defaults-80781f7e.js";import"./index-99d317a4.js";import"./i18n-defaults-9da374be.js";import"./I18nContext-bef5b452.js";import"./stopPropagation-da74d54e.js";import"./Button-bc3a11a1.js";import"./index-4ce92c34.js";import"./Bar-78c6a88f.js";import"./fastNavigation-8b41cdda.js";import"./index-62bbf3be.js";import"./index-d1aac2ad.js";import"./Dialog-43ec4fb1.js";import"./ValueState-2c5e5904.js";import"./information-b6c8ed8e.js";import"./ValueState-ab6838cc.js";import"./index-3533392f.js";import"./ItemNavigation-8e291cfa.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-15ff681f.js";import"./CheckBox-80dd6e5f.js";import"./accept-d8c33d70.js";import"./decline-6bd98a2e.js";import"./favorite-f8e4777a.js";import"./unfavorite-3cd1aa2b.js";import"./index-c5b8ab76.js";import"./RadioButton-a8c23c83.js";import"./index-afcd0bf5.js";import"./index-7e648a5c.js";import"./index-3b40cc9c.js";import"./Input-4940ecdf.js";import"./Suggestions.css-154c701b.js";import"./ResponsivePopoverCommon.css-ca4e9b7c.js";import"./ValueStateMessage.css-f5f87de3.js";import"./useIsomorphicId-78860c77.js";import"./index-e2a07d7d.js";import"./index-3cb88402.js";import"./ResponsivePopover-fcaf2b05.js";import"./index-a9799cc0.js";import"./ListItemBase-a0bc6999.js";import"./index-da20975e.js";import"./StandardListItem-ac3f40c5.js";import"./ListItem-83c0fb9e.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-52a56681.js";import"./Avatar-7e1d4e21.js";import"./employee-1bc983a2.js";import"./react-18-ecd79c7b.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";function Et(a={}){const{wrapper:i}=Object.assign({},s(),a.components);return i?e(i,Object.assign({},a,{children:e(r,{})})):r();function r(){const t=Object.assign({h2:"h2",h1:"h1",p:"p",code:"code",strong:"strong",pre:"pre"},s(),a.components);return n(l,{children:[e(h,{of:S}),`
+`,e(d,{}),`
+`,e("br",{}),`
+`,e(t.h2,{children:"Example"}),`
+`,e(m,{of:p}),`
+`,e(t.h2,{children:"Properties"}),`
+`,e(c,{of:p}),`
+`,e(t.h1,{children:"More Examples"}),`
+`,e(t.h2,{children:"VariantManagement with custom Input"}),`
+`,n(t.p,{children:['You can customize the inputs inside the configuration dialogs ("Save View" and "Manage Views") by leveraging the corresponding ',e(t.code,{children:"inputProps"})," prop (",e(t.code,{children:"saveViewInputProps"})," and ",e(t.code,{children:"manageViewsInputProps"}),") of the ",e(t.code,{children:"VariantItem"}),` component.
+These props accept props of the `,e(t.code,{children:"Input"})," component with which you are able to overwrite internal implementations. E.g. ",e(t.code,{children:"valueState"})," and ",e(t.code,{children:"valueState"})," message."]}),`
+`,n(t.p,{children:[e(t.strong,{children:"Note:"})," To prevent internal logic from being executed on save click, you can mark the ",e(t.code,{children:"onInput"})," event of the corresponding ",e(t.code,{children:"Input"})," component as invalid by setting ",e(t.code,{children:"event.isInvalid = true"})," inside the respective handler."]}),`
+`,e(M,{hideCloseButton:!0,children:"It is possible to overwrite internally used props. Please use with caution!"}),`
+`,e(m,{of:f,sourceState:"none"}),`
+`,e(t.p,{children:e(t.strong,{children:"Code Example"})}),`
+`,n("details",{style:{marginBlockEnd:"2rem"},children:[e("summary",{children:"Show Code"}),e(t.pre,{children:e(t.code,{className:"language-jsx",children:`export const VariantManagementWithCustomValidation = () => {
+  const [valueStateSaveView, setValueStateSaveView] = useState(undefined);
+  const [customSaveViewVariantText, setCustomSaveViewVariantText] = useState(
+    'Only alphanumeric chars in Save View input'
+  );
+  const [valueStateManageViews, setValueStateManageViews] = useState(undefined);
+  const [customManageViewsVariantText, setCustomManageViewsVariantText] = useState('Max 12 chars');
+
+  const handleSaveViewInput = (e) => {
+    // only allow alphanumeric and space characters
+    if (!e.target.value.match(/^[a-z0-9\\s]+$/i)) {
+      // mark variant as invalid (prevent internal logic from being applied)
+      e.isInvalid = true;
+      setValueStateSaveView(ValueState.Error);
+    } else {
+      // mark variant as valid again
+      e.isInvalid = false;
+      setValueStateSaveView(undefined);
+    }
+  };
+  const handleSaveAs = (e) => {
+    setCustomSaveViewVariantText(e.detail.children);
+  };
+
+  const handleManageViewInput = (e) => {
+    // only 12 or fewer characters
+    if (e.target.value.length > 12) {
+      // mark variant as invalid (prevent internal logic from being applied)
+      e.isInvalid = true;
+      setValueStateManageViews(ValueState.Error);
+    } else {
+      // mark variant as valid again
+      e.isInvalid = false;
+      setValueStateManageViews(undefined);
+    }
+  };
+  const handleSaveManageViews = (e) => {
+    // if is custom manage view variant and is not in error state, set children to new value
+    const isCustomManageViewsItem = e.detail.updatedVariants.find((item) => item['data-custom-manage-views']);
+    if (!valueStateManageViews && isCustomManageViewsItem) {
+      setCustomManageViewsVariantText(isCustomManageViewsItem.children);
+    }
+  };
+  return (
+    <VariantManagement onSaveAs={handleSaveAs} onSaveManageViews={handleSaveManageViews}>
+      <VariantItem
+        data-custom-save-view
+        saveViewInputProps={{
+          valueState: valueStateSaveView,
+          valueStateMessage: valueStateSaveView ? (
+            <div>Only alphanumeric and space characters allowed!</div>
+          ) : undefined,
+          onInput: handleSaveViewInput
+        }}
+      >
+        {customSaveViewVariantText}
+      </VariantItem>
+      <VariantItem
+        data-custom-manage-views
+        selected
+        manageViewsInputProps={{
+          valueState: valueStateManageViews,
+          valueStateMessage: valueStateManageViews ? <div>No more than 12 characters allowed!</div> : undefined,
+          onInput: handleManageViewInput
+        }}
+      >
+        {customManageViewsVariantText}
+      </VariantItem>
+    </VariantManagement>
+  );
+};
+`})})]}),`
+`,e(v,{children:w}),`
+`,e(t.h2,{children:"VariantItem"}),`
+`,e(V,{of:o}),`
+`,e(g,{of:o}),`
+`,e(u,{})]})}}export{Et as default};
+//# sourceMappingURL=VariantManagement-f8fa3f36.js.map
