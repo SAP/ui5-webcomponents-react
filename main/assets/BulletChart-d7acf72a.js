91b81a445e337145ca06ef894e3e0e2365f4b844
Lukas742 <lukas.harbarth@sap.com>
2023-03-20 07:26:13 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c6f9b883b3bed42e066fad1c88e38ea7bf180930 🚀

---
+++ main/assets/BulletChart-d7acf72a.js
@@ -0,0 +1,45 @@
+import{j as r,a as p,F as s}from"./jsx-runtime-670450c2.js";import{C as l}from"./DomRefTable.module-65a07b39.js";import{D as h}from"./DocsHeader-987344ce.js";import{F as c}from"./Footer-d1e93cb9.js";import"./index-f1f749bf.js";import{M as d,C as o}from"./chunk-PCJTTTQV-be768986.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{C as u,D as n,W as f,a as g,L as b}from"./BulletChart.stories-53594306.js";import{u as a}from"./index-4fb8b842.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./index-e1c7afac.js";import"./iframe-9f6ad9b3.js";import"../sb-preview/runtime.mjs";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-65cd39a0.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./Icon-7987c836.js";import"./Icons-fe6e657a.js";import"./decline-6bd98a2e.js";import"./i18n-defaults-80781f7e.js";import"./information-b6c8ed8e.js";import"./class-map-95320e87.js";import"./Button-bc3a11a1.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-da505e61.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-c648e777.js";import"./TableOfContent-3d223b7a.js";import"./index-a78e99ab.js";import"./Link-018547e8.js";import"./WrappingType-b81e595a.js";import"./index-f297bef2.js";import"./Label-ccc234fe.js";import"./index-96c5f47c.js";import"./index-af775acb.js";import"./Popover-224d761a.js";import"./Integer-f7f172c9.js";import"./PopupUtils-a2e37749.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-9873a291.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-1da6d0d2.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-cae0a379.js";import"./react-18-ecd79c7b.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./DemoProps-9cd0a46b.js";import"./index-99d317a4.js";import"./YAxisTicks-ee6813cc.js";import"./ChartContainer-dcd0412d.js";import"./index-94593310.js";import"./i18n-defaults-9da374be.js";import"./I18nContext-bef5b452.js";import"./throttle-31c0810f.js";import"./isString-77c0f675.js";import"./generateCategoricalChart-cd8ae3dd.js";import"./index-4d501b15.js";import"./debounce-2c3bc435.js";import"./useTooltipFormatter-3a9cd82e.js";import"./defaults-10160311.js";import"./ChartDataLabel-fc48d365.js";import"./staticProps-9dd6f0f9.js";import"./useLongestYAxisLabel-0e0c4374.js";import"./useOnClickInternal-d27d7c1e.js";import"./react-content-loader.es-de0c8b5a.js";import"./ComposedChart-514b653f.js";import"./Line-ffef4f17.js";import"./Scatter-da8e0d3b.js";function Qr(i={}){const{wrapper:m}=Object.assign({},a(),i.components);return m?r(m,Object.assign({},i,{children:r(e,{})})):e();function e(){const t=Object.assign({h2:"h2",p:"p",code:"code",pre:"pre",h3:"h3"},a(),i.components);return p(s,{children:[r(d,{of:u}),`
+`,r(h,{}),`
+`,r(t.h2,{children:"Example"}),`
+`,r(o,{of:n}),`
+`,r(t.h2,{children:"Properties"}),`
+`,r(l,{of:n}),`
+`,r("br",{}),`
+`,r(t.h2,{children:"Usage Notes"}),`
+`,p(t.p,{children:[`In order to display the "Bullet" lines in the chart, you have to configure which measure should be rendered as bullet line.
+You can do this via the `,r(t.code,{children:"measures"})," array:"]}),`
+`,r(t.pre,{children:r(t.code,{className:"language-jsx",children:`<BulletChart
+  data={
+    [
+      /* data */
+    ]
+  }
+  dimensions={
+    [
+      /* dimensions */
+    ]
+  }
+  measures={[
+    {
+      accessor: 'actuals',
+      label: 'Actual Revenue',
+      type: 'primary' // this will be the primary bar
+    },
+    {
+      accessor: 'target',
+      label: 'Planned Target Revenue',
+      type: 'comparison' // this will be the bullet line
+    }
+  ]}
+/>
+`})}),`
+`,r("br",{}),`
+`,r(t.h2,{children:"More Examples"}),`
+`,r(t.h3,{children:"With Custom Size"}),`
+`,r(o,{of:f}),`
+`,r(t.h3,{children:"With Highlighted Measures"}),`
+`,r(o,{of:g}),`
+`,r(t.h3,{children:"Loading Placeholder"}),`
+`,r(o,{of:b}),`
+`,r(c,{})]})}}export{Qr as default};
+//# sourceMappingURL=BulletChart-d7acf72a.js.map
