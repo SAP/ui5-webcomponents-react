28075a0095e6297d86ab39cce93c3475bffdde6c
Lukas742 <lukas.harbarth@sap.com>
2023-03-21 12:40:30 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@2f0a3a0d594a093bb57e7e55ee0c8315fced5c45 🚀

---
+++ main/assets/Breadcrumbs-409c3a32.js
@@ -0,0 +1,26 @@
+import{j as o,a as s,F as a}from"./jsx-runtime-670450c2.js";import{C as c,A as d}from"./DomRefTable.module-f1e71728.js";import{D as h}from"./DocsHeader-a6c66755.js";import{F as f}from"./Footer-8a7e8400.js";import"./index-f1f749bf.js";import{M as u,C as l,a as i}from"./chunk-PCJTTTQV-f9dc4ae0.js";import{S as g}from"./SubcomponentsSection-6138f2fc.js";import{a as C}from"./index-a51ac7b5.js";import{C as b,D as p}from"./Breadcrumbs.stories-d37efa15.js";import{u as n}from"./index-4fb8b842.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./index-62e9746b.js";import"./iframe-4964b844.js";import"../sb-preview/runtime.mjs";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-65cd39a0.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./Icon-7987c836.js";import"./Icons-fe6e657a.js";import"./decline-6bd98a2e.js";import"./i18n-defaults-80781f7e.js";import"./information-b6c8ed8e.js";import"./class-map-95320e87.js";import"./Button-bc3a11a1.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-da505e61.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-0b35bca2.js";import"./TableOfContent-82db16df.js";import"./index-1ad0303a.js";import"./Link-018547e8.js";import"./WrappingType-b81e595a.js";import"./index-42676734.js";import"./Label-ccc234fe.js";import"./index-96c5f47c.js";import"./index-4cfca49a.js";import"./Popover-224d761a.js";import"./Integer-f7f172c9.js";import"./PopupUtils-a2e37749.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-9873a291.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-1da6d0d2.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-cae0a379.js";import"./react-18-ecd79c7b.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./ItemNavigation-8e291cfa.js";import"./ResponsivePopover-fcaf2b05.js";import"./Dialog-43ec4fb1.js";import"./ValueState-2c5e5904.js";import"./Title-129aeb28.js";import"./ListItemBase-a0bc6999.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-15ff681f.js";import"./StandardListItem-ac3f40c5.js";import"./ListItem-83c0fb9e.js";import"./RadioButton-a8c23c83.js";import"./CheckBox-80dd6e5f.js";import"./accept-d8c33d70.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-52a56681.js";import"./Avatar-7e1d4e21.js";import"./employee-1bc983a2.js";const v="**Since:** 0.18.0\n\nThe `BreadcrumbsItem` component defines the content of an item in `Breadcrumbs`\n",D=`## Keyboard Handling
+
+The \`Breadcrumbs\` provides advanced keyboard handling.  
+
+*   \\[F4, ALT+UP, ALT+DOWN, SPACE, ENTER\\] - If the dropdown arrow is focused - opens/closes the drop-down.
+*   \\[SPACE, ENTER\\] - Activates the focused item and triggers the \`onItemClick\` event.
+*   \\[ESC\\] - Closes the drop-down.
+*   \\[LEFT\\] - If the drop-down is closed - navigates one item to the left.
+*   \\[RIGHT\\] - If the drop-down is closed - navigates one item to the right.
+*   \\[UP\\] - If the drop-down is open - moves focus to the next item.
+*   \\[DOWN\\] - If the drop-down is open - moves focus to the previous item.
+*   \\[HOME\\] - Navigates to the first item.
+*   \\[END\\] - Navigates to the last item.`;function Zo(t={}){const{wrapper:m}=Object.assign({},n(),t.components);return m?o(m,Object.assign({},t,{children:o(e,{})})):e();function e(){const r=Object.assign({h2:"h2"},n(),t.components);return s(a,{children:[o(u,{of:b}),`
+`,o(h,{since:"0.18.0"}),`
+`,o("br",{}),`
+`,o(r.h2,{children:"Example"}),`
+`,o(l,{of:p}),`
+`,o(r.h2,{children:"Properties"}),`
+`,o(c,{of:p}),`
+`,o(i,{children:D}),`
+`,o(i,{children:g}),`
+`,o(r.h2,{children:"BreadcrumbsItem"}),`
+`,o(i,{children:v}),`
+`,o(d,{hideHTMLPropsNote:!0,of:C}),`
+`,o(f,{})]})}}export{Zo as default};
+//# sourceMappingURL=Breadcrumbs-409c3a32.js.map
