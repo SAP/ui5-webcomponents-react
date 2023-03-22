543911c0e42ba67f849b9b44b73aaedf6837da52
MarcusNotheis <marcus.notheis@sap.com>
2023-03-22 20:03:22 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c4c42ad47b2b0c0f9f77dfbb9b5bc52c64f774b5 🚀

---
+++ main/assets/Calendar-6a4dc841.js
@@ -0,0 +1,84 @@
+import{j as e,a as m,F as d}from"./jsx-runtime-670450c2.js";import{C as l,A as h}from"./DomRefTable.module-55b356de.js";import{D as c}from"./DocsHeader-41176292.js";import{F as u}from"./Footer-c5c962fb.js";import"./index-f1f749bf.js";import{M as f,C as g,a}from"./chunk-PCJTTTQV-e62404d4.js";import{S as y}from"./SubcomponentsSection-6138f2fc.js";import{_ as v}from"./iframe-11b6f620.js";import"./Calendar-bbb20fcd.js";import{w as N}from"./withWebComponent-65cd39a0.js";import{C,D as s}from"./Calendar.stories-ea952751.js";import{u as p}from"./index-4fb8b842.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-e54d2f1d.js";import"./inheritsLoose-93e09647.js";import"./index-17c50781.js";import"./UI5Element-427ec721.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./slot-634e3e91.js";import"./Icon-7987c836.js";import"./Icons-fe6e657a.js";import"./utils-ed90fb1b.js";import"./decline-6bd98a2e.js";import"./i18n-defaults-80781f7e.js";import"./information-b6c8ed8e.js";import"./class-map-95320e87.js";import"./Button-bc3a11a1.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-8ba7ce6e.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-33804abc.js";import"./TableOfContent-26b4b9bb.js";import"./index-26001d6d.js";import"./Link-018547e8.js";import"./WrappingType-b81e595a.js";import"./index-3ceac05a.js";import"./Label-ccc234fe.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./index-96c5f47c.js";import"./index-2147f6ab.js";import"./Popover-224d761a.js";import"./Integer-f7f172c9.js";import"./PopupUtils-a2e37749.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-9873a291.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-1da6d0d2.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./style-map-cae0a379.js";import"./react-18-ecd79c7b.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"../sb-preview/runtime.mjs";import"./Gregorian-034a4e91.js";import"./LocaleData-971cbc51.js";import"./slim-arrow-left-52e97856.js";import"./slim-arrow-right-52a56681.js";import"./Persian-6e5baf2b.js";const n=N("ui5-date",["value"],[],[],[],()=>v(()=>import("./Calendar-bbb20fcd.js").then(t=>t.d),["./Calendar-bbb20fcd.js","./withWebComponent-65cd39a0.js","./utils-ed90fb1b.js","./CustomElementsScopeUtils-137da8c8.js","./index-f1f749bf.js","./_commonjsHelpers-042e6b4d.js","./jsx-runtime-670450c2.js","./useIsomorphicLayoutEffect-a838da4a.js","./Icon-7987c836.js","./UI5Element-427ec721.js","./Icons-fe6e657a.js","./slot-634e3e91.js","./Gregorian-034a4e91.js","./LocaleData-971cbc51.js","./Integer-f7f172c9.js","./slim-arrow-left-52e97856.js","./slim-arrow-right-52a56681.js","./class-map-95320e87.js","./i18n-defaults-254d6b69.js","./style-map-cae0a379.js"],import.meta.url));n.displayName="CalendarDate";try{n.displayName="CalendarDate",n.__docgenInfo={description:'The `CalendarDate` component defines a calendar date to be used inside `Calendar`\n\n__Note:__ This component is a web component developed by the UI5 Web Components’ team.\n\n<ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/Calendar" target="_blank">UI5 Web Components Playground</ui5-link>',displayName:"CalendarDate",props:{value:{defaultValue:null,description:"The date formatted according to the `formatPattern` property of the `Calendar` that hosts the component.",name:"value",required:!1,type:{name:"string"}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}const w="The `CalendarDate` component defines a calendar date to be used inside `Calendar`\n",b=`## Usage
+
+The user can navigate to a particular date by:  
+
+*   Pressing over a month inside the months view
+*   Pressing over an year inside the years view
+
+  
+The user can confirm a date selection by pressing over a date inside the days view.  
+  
+
+## Keyboard Handling
+
+The \`Calendar\` provides advanced keyboard handling. When a picker is showed and focused the user can use the following keyboard shortcuts in order to perform a navigation:  
+\\- Day picker:  
+
+*   \\[F4\\] - Shows month picker
+*   \\[SHIFT\\] + \\[F4\\] - Shows year picker
+*   \\[PAGEUP\\] - Navigate to the previous month
+*   \\[PAGEDOWN\\] - Navigate to the next month
+*   \\[SHIFT\\] + \\[PAGEUP\\] - Navigate to the previous year
+*   \\[SHIFT\\] + \\[PAGEDOWN\\] - Navigate to the next year
+*   \\[CTRL\\] + \\[SHIFT\\] + \\[PAGEUP\\] - Navigate ten years backwards
+*   \\[CTRL\\] + \\[SHIFT\\] + \\[PAGEDOWN\\] - Navigate ten years forwards
+*   \\[HOME\\] - Navigate to the first day of the week
+*   \\[END\\] - Navigate to the last day of the week
+*   \\[CTRL\\] + \\[HOME\\] - Navigate to the first day of the month
+*   \\[CTRL\\] + \\[END\\] - Navigate to the last day of the month
+
+  
+\\- Month picker:  
+
+*   \\[PAGEUP\\] - Navigate to the previous year
+*   \\[PAGEDOWN\\] - Navigate to the next year
+*   \\[HOME\\] - Navigate to the first month of the current row
+*   \\[END\\] - Navigate to the last month of the current row
+*   \\[CTRL\\] + \\[HOME\\] - Navigate to the first month of the current year
+*   \\[CTRL\\] + \\[END\\] - Navigate to the last month of the year
+
+  
+\\- Year picker:  
+
+*   \\[PAGEUP\\] - Navigate to the previous year range
+*   \\[PAGEDOWN\\] - Navigate the next year range
+*   \\[HOME\\] - Navigate to the first year of the current row
+*   \\[END\\] - Navigate to the last year of the current row
+*   \\[CTRL\\] + \\[HOME\\] - Navigate to the first year of the current year range
+*   \\[CTRL\\] + \\[END\\] - Navigate to the last year of the current year range
+
+  
+
+### Fast Navigation
+
+This component provides a build in fast navigation group which can be used via \`F6 / Shift + F6\` or \`Ctrl + Alt(Option) + Down / Ctrl + Alt(Option) + Up\`. In order to use this functionality, you need to import the following module: \`import "@ui5/webcomponents-base/dist/features/F6Navigation.js"\`  
+  
+
+## Calendar types
+
+The component supports several calendar types - Gregorian, Buddhist, Islamic, Japanese and Persian. By default the Gregorian Calendar is used. In order to use the Buddhist, Islamic, Japanese or Persian calendar, you need to set the \`primaryCalendarType\` property and import one or more of the following modules:  
+  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Buddhist.js";\`  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Islamic.js";\`  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Japanese.js";\`  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Persian.js";\`  
+  
+Or, you can use the global configuration and set the \`calendarType\` key:  
+\`<script data-id="sap-ui-config" type="application/json"> { "calendarType": "Japanese" } <\/script>\``;function xe(t={}){const{wrapper:r}=Object.assign({},p(),t.components);return r?e(r,Object.assign({},t,{children:e(i,{})})):i();function i(){const o=Object.assign({h2:"h2"},p(),t.components);return m(d,{children:[e(f,{of:C}),`
+`,e(c,{since:"0.13.0"}),`
+`,e("br",{}),`
+`,e(o.h2,{children:"Example"}),`
+`,e(g,{of:s}),`
+`,e(o.h2,{children:"Properties"}),`
+`,e(l,{of:s}),`
+`,e(a,{children:b}),`
+`,e(a,{children:y}),`
+`,e(o.h2,{children:"CalendarDate"}),`
+`,e(h,{of:n}),`
+`,e(a,{children:w}),`
+`,e(u,{})]})}}export{xe as default};
+//# sourceMappingURL=Calendar-6a4dc841.js.map
---
+++ main/assets/DatePicker.stories-734dc21f.js
@@ -0,0 +1,60 @@
+import{M as l,C as u,f as h,a as y}from"./chunk-PCJTTTQV-e62404d4.js";import{D as r}from"./index-0d2bbe3a.js";import{C as p}from"./Persian-6e5baf2b.js";import"./Gregorian-034a4e91.js";import{V as m}from"./ValueState-ab6838cc.js";import{d as f}from"./DomRefTable.module-55b356de.js";import{D as g}from"./DocsHeader-41176292.js";import{F as b}from"./Footer-c5c962fb.js";import{j as e,a as D,F as k}from"./jsx-runtime-670450c2.js";import"./index-f1f749bf.js";import{D as v}from"./DomRefTable-fe86e12b.js";import{u as s}from"./index-4fb8b842.js";import"./iframe-11b6f620.js";import"../sb-preview/runtime.mjs";import"./react-18-ecd79c7b.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-96c5f47c.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./DatePicker-33bff2b9.js";import"./withWebComponent-65cd39a0.js";import"./utils-ed90fb1b.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-634e3e91.js";import"./Icon-7987c836.js";import"./UI5Element-427ec721.js";import"./Icons-fe6e657a.js";import"./Calendar-bbb20fcd.js";import"./Integer-f7f172c9.js";import"./LocaleData-971cbc51.js";import"./slim-arrow-left-52e97856.js";import"./slim-arrow-right-52a56681.js";import"./class-map-95320e87.js";import"./i18n-defaults-254d6b69.js";import"./style-map-cae0a379.js";import"./ValueState-2c5e5904.js";import"./AriaLabelHelper-43a261ec.js";import"./Device-208919c6.js";import"./decline-6bd98a2e.js";import"./i18n-defaults-80781f7e.js";import"./HasPopup-47461347.js";import"./Button-bc3a11a1.js";import"./MarkedEvents-b83081e9.js";import"./ResponsivePopover-fcaf2b05.js";import"./PopupUtils-a2e37749.js";import"./getActiveElement-bcae01ed.js";import"./Popover-224d761a.js";import"./PopupsCommon.css-9873a291.js";import"./FocusableElements-7e25663e.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-1da6d0d2.js";import"./ResizeHandler-1fa8a094.js";import"./MediaRange-25b98f31.js";import"./Dialog-43ec4fb1.js";import"./information-b6c8ed8e.js";import"./Title-129aeb28.js";import"./WrappingType-b81e595a.js";import"./Input-4940ecdf.js";import"./Suggestions.css-154c701b.js";import"./ResponsivePopoverCommon.css-ca4e9b7c.js";import"./ValueStateMessage.css-f5f87de3.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-e54d2f1d.js";import"./index-17c50781.js";import"./index-8ba7ce6e.js";import"./clsx.m-1229b3e0.js";import"./index-2eaff221.js";import"./ThemingParameters-f4b4144e.js";import"./Import-33804abc.js";import"./TableOfContent-26b4b9bb.js";import"./index-26001d6d.js";import"./Link-018547e8.js";import"./index-3ceac05a.js";import"./Label-ccc234fe.js";import"./index-2147f6ab.js";const P=`## Usage
+
+The user can enter a date by:
+
+*   Using the calendar that opens in a popup
+*   Typing it in directly in the input field
+
+  
+  
+When the user makes an entry and presses the enter key, the calendar shows the corresponding date. When the user directly triggers the calendar display, the actual date is displayed.
+
+## Formatting
+
+If a date is entered by typing it into the input field, it must fit to the used date format.  
+  
+Supported format options are pattern-based on Unicode LDML Date Format notation. For more information, see <ui5-link target="_blank" href="http://unicode.org/reports/tr35/#Date_Field_Symbol_Table" class="api-table-content-cell-link">UTS #35: Unicode Locale Data Markup Language</ui5-link>.  
+  
+For example, if the \`format-pattern\` is "yyyy-MM-dd", a valid value string is "2015-07-30" and the same is displayed in the input.
+
+## Keyboard Handling
+
+The \`DatePicker\` provides advanced keyboard handling. If the \`DatePicker\` is focused, you can open or close the drop-down by pressing \`F4\`, \`ALT+UP\` or \`ALT+DOWN\` keys. Once the drop-down is opened, you can use the \`UP\`, \`DOWN\`, \`LEFT\`, \`RIGHT\` arrow keys to navigate through the dates and select one by pressing the \`Space\` or \`Enter\` keys. Moreover you can use TAB to reach the buttons for changing month and year.  
+If the \`DatePicker\` input field is focused and its corresponding picker dialog is not opened, then users can increment or decrement the date referenced by \`dateValue\` property by using the following shortcuts:  
+
+*   \\[PAGEDOWN\\] - Decrements the corresponding day of the month by one
+*   \\[SHIFT\\] + \\[PAGEDOWN\\] - Decrements the corresponding month by one
+*   \\[SHIFT\\] + \\[CTRL\\] + \\[PAGEDOWN\\] - Decrements the corresponding year by one
+*   \\[PAGEUP\\] - Increments the corresponding day of the month by one
+*   \\[SHIFT\\] + \\[PAGEUP\\] - Increments the corresponding month by one
+*   \\[SHIFT\\] + \\[CTRL\\] + \\[PAGEUP\\] - Increments the corresponding year by one
+
+## Calendar types
+
+The component supports several calendar types - Gregorian, Buddhist, Islamic, Japanese and Persian. By default the Gregorian Calendar is used. In order to use the Buddhist, Islamic, Japanese or Persian calendar, you need to set the \`primaryCalendarType\` property and import one or more of the following modules:  
+  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Buddhist.js";\`  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Islamic.js";\`  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Japanese.js";\`  
+\`import "@ui5/webcomponents-localization/dist/features/calendar/Persian.js";\`  
+  
+Or, you can use the global configuration and set the \`calendarType\` key:  
+
+\`\`\`
+<script data-id="sap-ui-config" type="application/json">
+{ "calendarType": "Japanese"
+}
+<\/script>
+\`\`\``,T=[{name:"dateValue",readonly:"true",visibility:"public",type:"Date",description:"Currently selected date represented as a Local JavaScript Date instance."},{name:"closePicker",visibility:"public",description:"Closes the picker."},{name:"formatValue",visibility:"public",returnValue:{type:"string",description:"The date as string"},parameters:[{name:"date",type:"object",optional:!1,description:"A Java Script date object to be formatted as string"}],description:"Formats a Java Script date object into a string representing a locale date according to the <code>formatPattern</code> property of the DatePicker instance"},{name:"isInValidRange",visibility:"public",returnValue:{type:"boolean"},parameters:[{name:"value",type:"string",optional:!0,defaultValue:'""',description:"A value to be checked"}],description:"Checks if a date is between the minimum and maximum date."},{name:"isOpen",visibility:"public",returnValue:{type:"boolean",description:"true if the picker is open, false otherwise"},description:"Checks if the picker is open."},{name:"isValid",visibility:"public",returnValue:{type:"boolean"},parameters:[{name:"value",type:"string",optional:!0,defaultValue:'""',description:"A value to be tested against the current date format"}],description:"Checks if a value is valid against the current date format of the DatePicker."},{name:"openPicker",visibility:"public",returnValue:{type:"Promise",description:"Resolves when the picker is open"},description:"Opens the picker."}];function S(t={}){const{wrapper:o}=Object.assign({},s(),t.components);return o?e(o,{...t,children:e(i,{})}):i();function i(){const a=Object.assign({h2:"h2"},s(),t.components);return D(k,{children:[e(l,{title:"Inputs / DatePicker",component:r,argTypes:{valueStateMessage:{control:{disable:!0}}},args:{primaryCalendarType:p.Gregorian,valueState:m.None}}),`
+`,e(g,{}),`
+`,e("br",{}),`
+`,e(a.h2,{children:"Example"}),`
+`,e(u,{children:e(h,{name:"Default",children:d=>e(r,{...d})})}),`
+`,e(a.h2,{children:"Properties"}),`
+`,e(f,{story:"Default"}),`
+`,e(y,{children:P}),`
+`,e(v,{rows:T}),`
+`,e(b,{})]})}}const c=t=>e(r,{...t});c.storyName="Default";c.parameters={storySource:{source:`args => {
+  return <DatePicker {...args} />;
+}`}};const n={title:"Inputs / DatePicker",component:r,args:{primaryCalendarType:p.Gregorian,valueState:m.None},argTypes:{valueStateMessage:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:S};const $e=["defaultStory"];export{$e as __namedExportsOrder,n as default,c as defaultStory};
+//# sourceMappingURL=DatePicker.stories-734dc21f.js.map
