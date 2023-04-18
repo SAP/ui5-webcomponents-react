6dddf6abda8951a059bbe324ee45dc0d869d8e77
MarcusNotheis <marcus.notheis@sap.com>
2023-04-18 05:49:49 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@71032ee1660a531f8519610511b07b4c4d4a6f20 🚀

---
+++ main/assets/ColorPalette.stories-c3fae238.js
@@ -0,0 +1,27 @@
+import{a as u,j as e}from"./jsx-runtime-670450c2.js";import{r as m}from"./CustomElementsScopeUtils-137da8c8.js";import{d as c}from"./UI5Element-e8adceda.js";import p from"./Dialog-610dd06c.js";import{B as d}from"./Button-fa05cadd.js";import f from"./ColorPicker-191ec464.js";import{aU as h,aV as g,aW as C}from"./i18n-defaults-254d6b69.js";import{C as t}from"./index-5eab1d88.js";import{_}from"./iframe-9c5e5b67.js";import{w as P}from"./withWebComponent-d280b5c2.js";const l=P("ui5-color-palette",[],[],[],["item-click"],()=>_(()=>import("./index-5eab1d88.js").then(r=>r.b),["./index-5eab1d88.js","./UI5Element-e8adceda.js","./CustomElementsScopeUtils-137da8c8.js","./withWebComponent-d280b5c2.js","./utils-ed90fb1b.js","./index-f1f749bf.js","./_commonjsHelpers-042e6b4d.js","./jsx-runtime-670450c2.js","./useIsomorphicLayoutEffect-a838da4a.js","./slot-76e48863.js","./Icon-27a81412.js","./Icons-5b18f7d2.js","./ItemNavigation-01761450.js","./getActiveElement-bcae01ed.js","./CSSColor-f91c8158.js","./Integer-f7f172c9.js","./Device-208919c6.js","./class-map-6d48ebd3.js","./Button-fa05cadd.js","./AriaLabelHelper-43a261ec.js","./MarkedEvents-b83081e9.js","./i18n-defaults-254d6b69.js","./iframe-9c5e5b67.js","./style-map-51d6dd91.js"],import.meta.url));l.displayName="ColorPalette";try{l.displayName="ColorPalette",l.__docgenInfo={description:`The \`ColorPalette\` provides the users with a range of predefined colors. The colors are fixed and do not change with the theme.
+
+__Note:__ This component is a web component developed by the UI5 Web Components’ team.
+
+<ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/ColorPalette" target="_blank">UI5 Web Components Playground</ui5-link>`,displayName:"ColorPalette",props:{children:{defaultValue:null,description:"Defines the `ColorPaletteItem` elements.",name:"children",required:!1,type:{name:"ReactNode | ReactNode[]"}},onItemClick:{defaultValue:null,description:"Fired when the user selects a color.",name:"onItemClick",required:!1,type:{name:"(event: Ui5CustomEvent<ColorPaletteDomRef, { color: string; }>) => void"}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}class o{static get dependencies(){return[p,d,f]}static async init(){o.i18nBundle=await c("@ui5/webcomponents")}get colorPaletteDialogTitle(){return o.i18nBundle.getText(h)}get colorPaletteDialogOKButton(){return o.i18nBundle.getText(g)}get colorPaletteCancelButton(){return o.i18nBundle.getText(C)}}m("ColorPaletteMoreColors",o);const v={title:"Data Display / ColorPalette",component:l,argTypes:{children:{control:{disable:!0}}},args:{}},a={render:r=>u(l,{...r,children:[e(t,{value:"darkblue"}),e(t,{value:"pink"}),e(t,{value:"#444444"}),e(t,{value:"rgb(0,200,0)"}),e(t,{value:"green"}),e(t,{value:"darkred"}),e(t,{value:"yellow"}),e(t,{value:"blue"}),e(t,{value:"cyan"}),e(t,{value:"orange"}),e(t,{value:"#5480e7"}),e(t,{value:"#ff6699"})]})};var n,i,s;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: args => {
+    return <ColorPalette {...args}>
+        <ColorPaletteItem value="darkblue" />
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="#444444" />
+        <ColorPaletteItem value="rgb(0,200,0)" />
+        <ColorPaletteItem value="green" />
+        <ColorPaletteItem value="darkred" />
+        <ColorPaletteItem value="yellow" />
+        <ColorPaletteItem value="blue" />
+        <ColorPaletteItem value="cyan" />
+        <ColorPaletteItem value="orange" />
+        <ColorPaletteItem value="#5480e7" />
+        <ColorPaletteItem value="#ff6699" />
+      </ColorPalette>;
+  }
+}`,...(s=(i=a.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const y=["Default"],B=Object.freeze(Object.defineProperty({__proto__:null,Default:a,__namedExportsOrder:y,default:v},Symbol.toStringTag,{value:"Module"}));export{B as C,a as D};
+//# sourceMappingURL=ColorPalette.stories-c3fae238.js.map
---
+++ main/assets/DonutChart-21171ef2.js
@@ -0,0 +1,25 @@
+import{j as t,a,F as s}from"./jsx-runtime-670450c2.js";import{M as h,C as o}from"./chunk-PCJTTTQV-d1abe13e.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{C as d}from"./DomRefTable.module-ca3d13a8.js";import{D as l}from"./DocsHeader-20900745.js";import{F as c}from"./Footer-1103c581.js";import"./index-f1f749bf.js";import{C as f,D as e,W as g,a as u,b as C,L as W,c as b,d as x,H as D}from"./DonutChart.stories-0c47cec0.js";import{u as n}from"./index-4fb8b842.js";import"./iframe-9c5e5b67.js";import"../sb-preview/runtime.mjs";import"./react-18-718b9f73.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-96c5f47c.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./react-jss.esm-2e28eff7.js";import"./index-fa8f25f3.js";import"./UI5Element-e8adceda.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d280b5c2.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-76e48863.js";import"./Icon-27a81412.js";import"./Icons-5b18f7d2.js";import"./decline-7cb41e3a.js";import"./i18n-defaults-80781f7e.js";import"./information-2c502181.js";import"./class-map-6d48ebd3.js";import"./Button-fa05cadd.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./index-44cc9c22.js";import"./clsx.m-1229b3e0.js";import"./index-479d8097.js";import"./ThemingParameters-f4b4144e.js";import"./Import-8bdfa40a.js";import"./TableOfContent-08c84e3a.js";import"./index-e10646b1.js";import"./Link-3b8926df.js";import"./WrappingType-b81e595a.js";import"./index-2f9314bf.js";import"./Label-528b8168.js";import"./index-1fd465a8.js";import"./Popover-824d4d7c.js";import"./Integer-f7f172c9.js";import"./PopupUtils-6ec841a6.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-7f720278.js";import"./FocusableElements-5012f8d0.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-382f34a2.js";import"./ResizeHandler-9d370fcd.js";import"./MediaRange-25b98f31.js";import"./style-map-51d6dd91.js";import"./DemoProps-9cd0a46b.js";import"./PieChart-1bac9913.js";import"./index-99d317a4.js";import"./defaults-5b44467a.js";import"./ChartContainer-407a6692.js";import"./index-83f5c3ce.js";import"./i18n-defaults-57d9744b.js";import"./I18nContext-bef5b452.js";import"./throttle-31c0810f.js";import"./isString-77c0f675.js";import"./staticProps-9dd6f0f9.js";import"./useOnClickInternal-d27d7c1e.js";import"./Placeholder-5a74af79.js";import"./generateCategoricalChart-7417ded4.js";import"./index-4d501b15.js";import"./react-content-loader.es-de0c8b5a.js";import"./isPlainObject-237581f0.js";function Ut(r={}){const{wrapper:m}=Object.assign({},n(),r.components);return m?t(m,Object.assign({},r,{children:t(p,{})})):p();function p(){const i=Object.assign({h2:"h2",h3:"h3"},n(),r.components);return a(s,{children:[t(h,{of:f}),`
+`,t(l,{}),`
+`,t(i.h2,{id:"example",children:"Example"}),`
+`,t(o,{of:e}),`
+`,t(i.h2,{id:"properties",children:"Properties"}),`
+`,t(d,{of:e}),`
+`,t("br",{}),`
+`,t("br",{}),`
+`,t(i.h2,{id:"more-examples",children:"More Examples"}),`
+`,t(i.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,t(o,{of:g}),`
+`,t(i.h3,{id:"with-padding-angle",children:"With Padding Angle"}),`
+`,t(o,{of:u}),`
+`,t(i.h3,{id:"with-custom-radius",children:"With Custom Radius"}),`
+`,t(o,{of:C}),`
+`,t(i.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,t(o,{of:W}),`
+`,t(i.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,t(o,{of:b}),`
+`,t(i.h3,{id:"with-highlighted-active-segment",children:"With highlighted active segment"}),`
+`,t(o,{of:x}),`
+`,t(i.h3,{id:"hide-labels",children:"Hide labels"}),`
+`,t(o,{of:D}),`
+`,t(c,{})]})}}export{Ut as default};
+//# sourceMappingURL=DonutChart-21171ef2.js.map
