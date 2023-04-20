5b4376aafa99e49c754f1ceecfcfde024829f070
MarcusNotheis <marcus.notheis@sap.com>
2023-04-20 05:38:37 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@d52147db2d98b52867a8317138d7e68dd42e631a 🚀

---
+++ main/assets/ColorPalettePopover-6c6a7446.js
@@ -0,0 +1,33 @@
+import{j as o,a as r,F as s}from"./jsx-runtime-670450c2.js";import{C as l}from"./DomRefTable.module-3d83c57b.js";import{D as c}from"./DocsHeader-f65c67c8.js";import{F as d}from"./Footer-da37bb84.js";import"./index-f1f749bf.js";import{D as h}from"./DomRefTable-2b8db518.js";import{M as f,C,a as n,b as P}from"./chunk-PCJTTTQV-07df005d.js";import{C as u,D as m}from"./ColorPalettePopover.stories-036644fa.js";import{S as v}from"./SubcomponentsSection-6138f2fc.js";import{C as b}from"./ColorPaletteItemDescription-934d5448.js";import{C as g}from"./index-cf114ed1.js";import{u as a}from"./index-4fb8b842.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-2e28eff7.js";import"./inheritsLoose-93e09647.js";import"./index-41cf13c7.js";import"./iframe-22f8d39f.js";import"../sb-preview/runtime.mjs";import"./UI5Element-e8adceda.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d280b5c2.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-76e48863.js";import"./Icon-27a81412.js";import"./Icons-5b18f7d2.js";import"./decline-7cb41e3a.js";import"./i18n-defaults-80781f7e.js";import"./information-2c502181.js";import"./class-map-6d48ebd3.js";import"./Button-fa05cadd.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-44cc9c22.js";import"./clsx.m-1229b3e0.js";import"./index-479d8097.js";import"./ThemingParameters-f4b4144e.js";import"./Import-4c258798.js";import"./TableOfContent-8bbfd532.js";import"./index-d1663926.js";import"./Link-3b8926df.js";import"./WrappingType-b81e595a.js";import"./index-a3409209.js";import"./Label-528b8168.js";import"./index-96c5f47c.js";import"./index-cb46d963.js";import"./Popover-824d4d7c.js";import"./Integer-f7f172c9.js";import"./PopupUtils-6ec841a6.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-7f720278.js";import"./FocusableElements-5012f8d0.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-382f34a2.js";import"./ResizeHandler-9d370fcd.js";import"./MediaRange-25b98f31.js";import"./style-map-51d6dd91.js";import"./react-18-718b9f73.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./index-7acf7a6f.js";import"./CSSColor-f91c8158.js";import"./ResponsivePopoverCommon.css-0466170a.js";import"./Title-0440ec3f.js";import"./ResponsivePopover-152ab708.js";import"./Dialog-610dd06c.js";import"./ValueState-2c5e5904.js";import"./ItemNavigation-01761450.js";const w=[{name:"openPopover",visibility:"public",since:"1.0.0-rc.16",parameters:[{name:"opener",type:"HTMLElement",optional:!1,description:"the element that the popover is shown at"}],description:"Shows the ColorPalettePopover. <b>Note:</b> The method is deprecated and will be removed in future, use <code>showAt</code> instead.",deprecated:{text:"The method is deprecated in favour of <code>showAt</code>."}},{name:"showAt",visibility:"public",since:"1.1.1",parameters:[{name:"opener",type:"HTMLElement",optional:!1,description:"the element that the popover is shown at"}],description:"Shows the ColorPalettePopover."}];function Go(t={}){const{wrapper:i}=Object.assign({},a(),t.components);return i?o(i,Object.assign({},t,{children:o(p,{})})):p();function p(){const e=Object.assign({h2:"h2",p:"p",code:"code",strong:"strong",em:"em",pre:"pre",h3:"h3"},a(),t.components);return r(s,{children:[o(f,{of:u}),`
+`,o(c,{since:"0.20.0"}),`
+`,o("br",{}),`
+`,o(e.h2,{id:"example",children:"Example"}),`
+`,o(C,{of:m}),`
+`,o(e.h2,{id:"properties",children:"Properties"}),`
+`,o(l,{of:m}),`
+`,o(h,{rows:w}),`
+`,o(e.h2,{id:"opening-colorpalettepopovers",children:"Opening ColorPalettePopovers"}),`
+`,r(e.p,{children:[o(e.code,{children:"ColorPalettePopover"}),"s can only be opened by attaching a ",o(e.code,{children:"ref"})," to the component and then call the corresponding ",o(e.strong,{children:o(e.code,{children:"showAt"})})," method. The method receives the target element - ",r(e.em,{children:["on which the ",o(e.code,{children:"ColorPalettePopover"})," is to be opened"]})," - as parameter."]}),`
+`,o(e.pre,{children:o(e.code,{className:"language-jsx",children:`const ColorPalettePopoverComponent = () => {
+  const popoverRef = useRef();
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open ColorPalettePopover</Button>
+      <ColorPalettePopover ref={popoverRef}>
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="darkblue" />
+      </ColorPalettePopover>
+    </>
+  );
+};
+`})}),`
+`,o(n,{children:v}),`
+`,o(e.h2,{id:"colorpaletteitem",children:"ColorPaletteItem"}),`
+`,o(n,{children:b}),`
+`,o(e.h3,{id:"properties-1",children:"Properties"}),`
+`,o(P,{of:g}),`
+`,o(d,{})]})}}export{Go as default};
+//# sourceMappingURL=ColorPalettePopover-6c6a7446.js.map
---
+++ main/assets/List-799662ec.js
@@ -0,0 +1,41 @@
+import{j as t,a as d,F as l}from"./jsx-runtime-670450c2.js";import{C as c,A as r}from"./DomRefTable.module-3d83c57b.js";import{D as h}from"./DocsHeader-f65c67c8.js";import{F as u}from"./Footer-da37bb84.js";import"./index-f1f749bf.js";import{M as f,C as n,a as i}from"./chunk-PCJTTTQV-07df005d.js";import{S}from"./SubcomponentsSection-6138f2fc.js";import"./employee-0153a597.js";import{C as g}from"./index-a1d4d257.js";import{G as y}from"./index-23941dcb.js";import{S as L}from"./index-49fa6164.js";import{C as w,D as p,a as C,G as D}from"./List.stories-476f54a1.js";import{u as a}from"./index-4fb8b842.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./react-jss.esm-2e28eff7.js";import"./inheritsLoose-93e09647.js";import"./index-41cf13c7.js";import"./iframe-22f8d39f.js";import"../sb-preview/runtime.mjs";import"./UI5Element-e8adceda.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d280b5c2.js";import"./utils-ed90fb1b.js";import"./useIsomorphicLayoutEffect-a838da4a.js";import"./slot-76e48863.js";import"./Icon-27a81412.js";import"./Icons-5b18f7d2.js";import"./decline-7cb41e3a.js";import"./i18n-defaults-80781f7e.js";import"./information-2c502181.js";import"./class-map-6d48ebd3.js";import"./Button-fa05cadd.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-44cc9c22.js";import"./clsx.m-1229b3e0.js";import"./index-479d8097.js";import"./ThemingParameters-f4b4144e.js";import"./Import-4c258798.js";import"./TableOfContent-8bbfd532.js";import"./index-d1663926.js";import"./Link-3b8926df.js";import"./WrappingType-b81e595a.js";import"./index-a3409209.js";import"./Label-528b8168.js";import"./index-96c5f47c.js";import"./index-cb46d963.js";import"./Popover-824d4d7c.js";import"./Integer-f7f172c9.js";import"./PopupUtils-6ec841a6.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-7f720278.js";import"./FocusableElements-5012f8d0.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-382f34a2.js";import"./ResizeHandler-9d370fcd.js";import"./MediaRange-25b98f31.js";import"./style-map-51d6dd91.js";import"./react-18-718b9f73.js";import"./mapValues-2de54f78.js";import"./_baseForOwn-c7d9bea5.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-bd05e126.js";import"./_baseUniq-51545746.js";import"./index-356e4a49.js";import"./CustomListItem-2bcbcefe.js";import"./ListItem-843af2c9.js";import"./ListItemBase-780d7053.js";import"./ItemNavigation-01761450.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-c375cd19.js";import"./RadioButton-412fbbc0.js";import"./ValueState-2c5e5904.js";import"./CheckBox-f0072def.js";import"./accept-9744f30a.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-78d167a6.js";import"./GroupHeaderListItem-56788105.js";import"./StandardListItem-38cf6758.js";import"./Avatar-0d8d2b25.js";import"./ValueState-ab6838cc.js";import"./index-14a705f2.js";import"./ProgressIndicator-641e8e9a.js";import"./AnimationMode-9cec4b20.js";import"./index-9793a9c2.js";const I="A component to be used as custom list item within the `List` the same way as the standard `StandardListItem`. The component accepts arbitrary HTML content to allow full customization\n",b="The `GroupHeaderListItem` is a special list item, used only to separate other list items into logical groups\n",v="The `StandardListItem` represents the simplest type of item for a `StandardListItemst`. This is a list item, providing the most common use cases such as `text`, `image` and `icon`.\n\n### CSS Shadow Parts\n\nCSS Shadow Parts allow developers to style elements inside the Shadow DOM.  \nThe `StandardListItem` exposes the following CSS Shadow Parts:\n\n- title - Used to style the title of the list item\n- description - Used to style the description of the list item\n- additional-text - Used to style the additionalText of the list item\n- icon - Used to style the icon of the list item\n",T=`## Keyboard Handling
+
+### Basic Navigation
+
+The \`List\` provides advanced keyboard handling. When a list is focused the user can use the following keyboard shortcuts in order to perform a navigation:
+
+- \\[UP/DOWN\\] - Navigates up and down the items
+- \\[HOME\\] - Navigates to first item
+- \\[END\\] - Navigates to the last item
+
+The user can use the following keyboard shortcuts to perform actions (such as select, delete), when the \`mode\` property is in use:
+
+- \\[SPACE\\] - Select an item (if \`type\` is 'Active') when \`mode\` is selection
+- \\[DELETE\\] - Delete an item if \`mode\` property is \`Delete\`
+
+### Fast Navigation
+
+This component provides a build in fast navigation group which can be used via \`F6 / Shift + F6\` or \`Ctrl + Alt(Option) + Down / Ctrl + Alt(Option) + Up\`. In order to use this functionality, you need to import the following module: \`import "@ui5/webcomponents-base/dist/features/F6Navigation.js"\`
+`;function ao(e={}){const{wrapper:s}=Object.assign({},a(),e.components);return s?t(s,Object.assign({},e,{children:t(m,{})})):m();function m(){const o=Object.assign({h2:"h2"},a(),e.components);return d(l,{children:[t(f,{of:w}),`
+`,t(h,{}),`
+`,t("br",{}),`
+`,t(o.h2,{id:"example",children:"Example"}),`
+`,t(n,{of:p}),`
+`,t(o.h2,{id:"properties",children:"Properties"}),`
+`,t(c,{of:p}),`
+`,t(i,{children:T}),`
+`,t("br",{}),`
+`,t(i,{children:S}),`
+`,t(o.h2,{id:"standardlistitem",children:"StandardListItem"}),`
+`,t(r,{of:L}),`
+`,t(i,{children:v}),`
+`,t(o.h2,{id:"customlistitem",children:"CustomListItem"}),`
+`,t(n,{of:C}),`
+`,t(r,{of:g}),`
+`,t(i,{children:I}),`
+`,t(o.h2,{id:"groupheaderlistitem",children:"GroupHeaderListItem"}),`
+`,t(n,{of:D}),`
+`,t(r,{of:y}),`
+`,t(i,{children:b}),`
+`,t(u,{})]})}}export{ao as default};
+//# sourceMappingURL=List-799662ec.js.map
