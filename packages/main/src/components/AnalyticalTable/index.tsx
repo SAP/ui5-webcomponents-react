ce8b109dfc91350262e0524490807c727e22729f
Lukas Harbarth <lukas.harbarth@sap.com>
2023-05-03 13:59:23 +0200

    feat(AnalyticalTable): add `selectedRowIds` object to `onRowSelect` event & improve performance when selecting rows (#4534)
    Fixes #4517

--- packages/main/src/components/AnalyticalTable/index.tsx
+++ packages/main/src/components/AnalyticalTable/index.tsx
@@ -532,6 +532,7 @@ export interface AnalyticalTablePropTypes extends Omit<CommonProps, 'title'> {
       row?: Record<string, unknown>;
       isSelected?: boolean;
       selectedFlatRows: Record<string, unknown>[];
+      selectedRowIds: Record<string | number, boolean>;
     }>
   ) => void;
   /**
