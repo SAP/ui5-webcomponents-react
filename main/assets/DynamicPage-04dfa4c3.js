e2ac7f8944c3255e6b2d4f0e47d4d4c16d7b4a76
Lukas742 <lukas.harbarth@sap.com>
2023-05-04 08:24:27 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@bc5c5edc86fa1188f7d8f2fbb8c75599c960666d 🚀

---
+++ main/assets/DynamicPage-04dfa4c3.js
@@ -0,0 +1,67 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{C as h,A as p}from"./DomRefTable.module-9ae2cdfc.js";import{D as l}from"./DocsHeader-d657e53a.js";import{F as x}from"./Footer-6d157ad2.js";import{D as m,a as s}from"./index-cc9c5add.js";import"./index-ebeaab24.js";import{M as g,C as i,a as j,e as a}from"./chunk-PCJTTTQV-d5471771.js";import{C as f,D as c,A as u,W as y,a as b,b as w,S as v}from"./DynamicPage.stories-d7d56233.js";import{S as k}from"./SubcomponentsSection-6138f2fc.js";import{u as d}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-6b3a3cdb.js";import"./iframe-f95e4ac2.js";import"../sb-preview/runtime.js";import"./UI5Element-e8adceda.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-c710bf85.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-16710c43.js";import"./Icons-5b18f7d2.js";import"./decline-7cb41e3a.js";import"./i18n-defaults-80781f7e.js";import"./information-2c502181.js";import"./class-map-eee65cb7.js";import"./Button-6eea99db.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-254d6b69.js";import"./index-4c0ab015.js";import"./clsx.m-1229b3e0.js";import"./index-2495a9e4.js";import"./ThemingParameters-f4b4144e.js";import"./Import-7bf7eef5.js";import"./TableOfContent-21cce581.js";import"./index-1b320f3b.js";import"./Link-a00cf029.js";import"./WrappingType-b81e595a.js";import"./index-a29eece8.js";import"./Label-e22f23ee.js";import"./index-9c09ad76.js";import"./index-1811a350.js";import"./Popover-34a43919.js";import"./Integer-f7f172c9.js";import"./PopupUtils-6ec841a6.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-bfd93775.js";import"./FocusableElements-5012f8d0.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-72af48b2.js";import"./ResizeHandler-9d370fcd.js";import"./MediaRange-25b98f31.js";import"./style-map-eb41b602.js";import"./slim-arrow-down-09ec0732.js";import"./slim-arrow-up-7b94d03f.js";import"./index-99d317a4.js";import"./i18n-defaults-a98c4660.js";import"./I18nContext-cdb50611.js";import"./index-64b4aab8.js";import"./ToggleButton-bf938def.js";import"./index-00c88681.js";import"./stopPropagation-da74d54e.js";import"./index-6fc3a8a1.js";import"./overflow-e82f5104.js";import"./CssSizeVariables-bd372cde.js";import"./CustomVariables-fd831c35.js";import"./debounce-241dfefe.js";import"./index-f18a2bc8.js";import"./Media-6b270b80.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./index-e31f4b85.js";import"./ItemNavigation-01761450.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-8eb05e4f.js";import"./CheckBox-3275c87a.js";import"./ValueState-2c5e5904.js";import"./accept-9744f30a.js";import"./utils-badfc99a.js";import"./navigation-down-arrow-a69745d2.js";import"./index-0ee5c9d5.js";import"./GlobalStyleClasses-7209358d.js";import"./PageBackgroundDesign-7e89cd25.js";import"./index-aad2fec9.js";import"./ValueState-ab6838cc.js";import"./index-2da387a2.js";import"./VariantItem-0bb9b8a1.js";import"./UnableToLoad-be6277d6.js";import"./IllustratedMessage-939baa9e.js";import"./Title-c7a0da36.js";import"./i18n-defaults-217b10b4.js";import"./parameters-bundle.css-6b4a2ffb.js";import"./search-f47cd12e.js";import"./index-f2e4b26f.js";import"./Bar-6f655916.js";import"./index-1e3abf42.js";import"./Dialog-0fdd42ca.js";import"./favorite-213071d0.js";import"./unfavorite-9ac4036d.js";import"./index-8ca98263.js";import"./RadioButton-5262eb7a.js";import"./index-ff8ac9e5.js";import"./index-af995316.js";import"./Input-51d70ac6.js";import"./Suggestions.css-d3fc0bec.js";import"./ResponsivePopoverCommon.css-104fb5b1.js";import"./ValueStateMessage.css-af9170b8.js";import"./useIsomorphicId-4be7c5e3.js";import"./index-a6595dbc.js";import"./index-c972526d.js";import"./index-8bdceac2.js";import"./ResponsivePopover-37029899.js";import"./index-eaa94b0a.js";import"./ListItemBase-49bcf604.js";import"./index-1f4321a7.js";import"./StandardListItem-1576ed3d.js";import"./ListItem-2c0fcc20.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-78d167a6.js";import"./Avatar-f85f46ad.js";import"./employee-0153a597.js";import"./index-a20ff78c.js";import"./index-0d539296.js";import"./index-3c261a48.js";import"./index-1b1af45b.js";import"./slideUp-745debd3.js";import"./animate-1a16ba20.js";import"./AnimationMode-9cec4b20.js";function oi(r={}){const{wrapper:e}=Object.assign({},d(),r.components);return e?t.jsx(e,Object.assign({},r,{children:t.jsx(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h1:"h1",p:"p",strong:"strong",code:"code",ul:"ul",li:"li",h3:"h3",pre:"pre"},d(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(g,{of:f}),`
+`,t.jsx(l,{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(i,{of:c}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(h,{of:c}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(o.h2,{id:"always-show-header-content",children:"Always show Header Content"}),`
+`,t.jsx(i,{of:u}),`
+`,t.jsx(o.h2,{id:"variant-management-as-title",children:"Variant Management as Title"}),`
+`,t.jsxs(o.p,{children:[t.jsx(o.strong,{children:"Note:"})," To prevent the ",t.jsx(o.code,{children:"click"})," event of the ",t.jsx(o.code,{children:"VariantManagement"})," component from bubbling up and collapsing/expanding the ",t.jsx(o.code,{children:"DynamicPageHeader"}),", please call ",t.jsx(o.code,{children:"event.stopPropagation()"})," inside the ",t.jsx(o.code,{children:"onClick"})," callback of the ",t.jsx(o.code,{children:"VariantManagement"}),"."]}),`
+`,t.jsx(i,{of:y}),`
+`,t.jsx(o.h2,{id:"with-floating-footer",children:"With floating footer"}),`
+`,t.jsx(i,{of:b}),`
+`,t.jsx(o.h2,{id:"dynamicpage-with-custom-actions-and-navigationactions-overflow-button",children:"DynamicPage with custom actions and navigationActions overflow button"}),`
+`,t.jsxs(o.p,{children:["The ",t.jsx(o.code,{children:"DynamicPageTitle"})," offers two props (",t.jsx(o.code,{children:"actionsToolbarProps"})," and ",t.jsx(o.code,{children:"navigationActionsToolbarProps"}),") to configure the props of the ",t.jsx(o.code,{children:"actions"})," and ",t.jsx(o.code,{children:"navigationActions"}),` toolbars.
+With these objects it's possible to e.g. configure the overflow button displayed within each toolbar by setting the `,t.jsx(o.code,{children:"overflowButton"}),` prop.
+`,t.jsx(o.code,{children:"navigationActionsToolbarProps"})," only has an effect, if the ",t.jsx(o.code,{children:"navigationActions"})," toolbar is used. (width < 1280px)"]}),`
+`,t.jsx(i,{of:w}),`
+`,t.jsx(o.h2,{id:"with-sticky-sub-headers",children:"With sticky sub-headers"}),`
+`,t.jsxs(o.p,{children:["To display sticky sub-headers that line-up with the standard header of the ",t.jsx(o.code,{children:"DynamicPage"})," you're able to render ",t.jsx(o.code,{children:"children"}),` as function.
+There you receive an object as parameter that offers the following properties:`]}),`
+`,t.jsxs(o.ul,{children:[`
+`,t.jsxs(o.li,{children:[t.jsx(o.code,{children:"stickyHeaderHeight"}),": The height of the ",t.jsx(o.code,{children:"DynamicPage"})," header sections which will stick to the top of the page."]}),`
+`]}),`
+`,t.jsx(i,{of:v,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"code",children:"Code"}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`<DynamicPage {...props}>
+  {({ stickyHeaderHeight }) => (
+    <>
+      <div
+        style={{
+          position: 'sticky',
+          width: '100%',
+          height: '4rem',
+          background: 'lightgreen',
+          top: \`\${stickyHeaderHeight}px\`
+        }}
+      >
+        Sticky Header
+      </div>
+      <div style={{ width: '100%', background: 'orange', height: '10rem' }}>Content</div>
+      <div
+        style={{
+          position: 'sticky',
+          width: '100%',
+          height: '8rem',
+          background: 'lightgreen',
+          top: \`calc(\${stickyHeaderHeight}px + 4rem)\`
+        }}
+      >
+        Sticky Header 2
+      </div>
+      <div style={{ background: 'lightblue', height: '2000px', width: '100%' }}>Content</div>
+    </>
+  )}
+</DynamicPage>
+`})}),`
+`,t.jsx(j,{children:k}),`
+`,t.jsx(o.h2,{id:"dynamicpagetitle",children:"DynamicPageTitle"}),`
+`,t.jsx(a,{of:m}),`
+`,t.jsx(p,{of:m}),`
+`,t.jsx(o.h2,{id:"dynamicpageheader",children:"DynamicPageHeader"}),`
+`,t.jsx(a,{of:s}),`
+`,t.jsx(p,{of:s}),`
+`,t.jsx(x,{})]})}}export{oi as default};
+//# sourceMappingURL=DynamicPage-04dfa4c3.js.map
---
+++ main/assets/TabContainer.stories-f366bcd8.js
@@ -0,0 +1,61 @@
+import{j as e}from"./jsx-runtime-5926aa06.js";import{a as h,U,q as y,k as P}from"./UI5Element-e8adceda.js";import{h as f}from"./Icons-5b18f7d2.js";import{a as V}from"./add-6537f61a.js";import{e as G}from"./employee-0153a597.js";import{s as J}from"./settings-456fc51a.js";import{b as H,T as g,c as K,d as S,a}from"./index-563181b8.js";import{_ as Q}from"./iframe-f95e4ac2.js";import{e as q,l as s,s as w,a as R,b as F,c as X,d as Y,w as Z}from"./withWebComponent-c710bf85.js";import{o as x}from"./class-map-eee65cb7.js";import{s as v}from"./style-map-eb41b602.js";const ee=(t,o,n)=>q`<div id="${s(t._id)}" data-ui5-stable="${s(t.stableDomRef)}" role="separator" class="${x(t.classes.root)}"></div>`,te=(t,o,n)=>n?q`<${w("ui5-li-custom",o,n)} id="${s(t._id)}" data-ui5-stable="${s(t.stableDomRef)}" role="separator" class="${x(t.classes.root)}" disabled style="${v(t._style)}"></${w("ui5-li-custom",o,n)}>`:q`<ui5-li-custom id="${s(t._id)}" data-ui5-stable="${s(t.stableDomRef)}" role="separator" class="${x(t.classes.root)}" disabled style="${v(t._style)}"></ui5-li-custom>`;h("@ui5/webcomponents-theming","sap_fiori_3",async()=>R);h("@ui5/webcomponents","sap_fiori_3",async()=>F);const ae={packageName:"@ui5/webcomponents",fileName:"themes/TabSeparatorInStrip.css.ts",content:".ui5-tc__separator{width:0;border-left:.0625rem solid var(--sapGroup_TitleBorderColor);margin:.5rem .25rem}"};h("@ui5/webcomponents-theming","sap_fiori_3",async()=>R);h("@ui5/webcomponents","sap_fiori_3",async()=>F);const oe={packageName:"@ui5/webcomponents",fileName:"themes/TabSeparatorInOverflow.css.ts",content:".ui5-tc__separator{min-height:.25rem;border-bottom:.0625rem solid var(--sapGroup_TitleBorderColor);margin-inline-start:calc(var(--_ui5-tab-indentation-level)*0.5rem);margin-inline-end:.5rem}[ui5-list]>.ui5-tc__separator:first-child{min-height:.5rem}"};var ne=globalThis&&globalThis.__decorate||function(t,o,n,b){var T=arguments.length,r=T<3?o:b===null?b=Object.getOwnPropertyDescriptor(o,n):b,p;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")r=Reflect.decorate(t,o,n,b);else for(var _=t.length-1;_>=0;_--)(p=t[_])&&(r=(T<3?p(r):T>3?p(o,n,r):p(o,n))||r);return T>3&&r&&Object.defineProperty(o,n,r),r},d;let u=d=class extends U{static get stripTemplate(){return ee}static get overflowTemplate(){return te}get classes(){return{root:{"ui5-tc__separator":!0}}}get isSeparator(){return!0}getTabInStripDomRef(){return this._getElementInStrip?this._getElementInStrip():null}get stableDomRef(){return this.getAttribute("stable-dom-ref")||`${this._id}-stable-dom-ref`}get stripPresentation(){return y(d.stripTemplate,this)}get overflowPresentation(){return y(d.overflowTemplate,this)}};u=d=ne([X({tag:"ui5-tab-separator",renderer:Y})],u);u.define();H.registerTabStyles(ae);H.registerStaticAreaTabStyles(oe);const re=u,se=Object.freeze(Object.defineProperty({__proto__:null,default:re},Symbol.toStringTag,{value:"Module"})),m=Z("ui5-tab-separator",[],[],[],[],()=>Q(()=>Promise.resolve().then(()=>se),void 0,import.meta.url));m.displayName="TabSeparator";try{m.displayName="TabSeparator",m.__docgenInfo={description:'The `TabSeparator` represents a vertical line to separate tabs inside a `TabContainer`\n\n__Note:__ This component is a web component developed by the UI5 Web Components’ team.\n\n<ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/TabContainer" target="_blank">UI5 Web Components Playground</ui5-link>',displayName:"TabSeparator",props:{style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}const ie="activities",L="M480 32q13 0 22.5 9t9.5 23v384q0 13-9.5 22.5T480 480H32q-14 0-23-9.5T0 448V64q0-14 9-23t23-9h448zm0 32H32v384h448V64zM117 252l-53-53 17-19 36 36 71-88 18 17zm0 164l-53-54 17-18 36 36 71-89 18 18zm283-96q16 0 16 16 0 6-4.5 11t-11.5 5H272q-6 0-11-5t-5-11q0-7 5-11.5t11-4.5h128zm0-160q16 0 16 16 0 6-4.5 11t-11.5 5H272q-6 0-11-5t-5-11q0-7 5-11.5t11-4.5h128z",ce=!0,le="SAP-icons-v4",me="@ui5/webcomponents-icons";f(ie,{pathData:L,ltr:ce,collection:le,packageName:me});const be="activities",M="M179 251q9-9 20.5-9t19.5 9q8 8 8 19.5t-8 19.5L105 404q-8 9-19.5 9T66 404L9 347q-9-8-9-19.5T9 307q8-8 19.5-8t19.5 8l37 37zm0-171q9-9 20.5-9t19.5 9q8 8 8 19.5t-8 20.5L105 233q-8 9-19.5 9T66 233L9 177q-9-9-9-20.5T9 137q8-9 19.5-9t19.5 9l37 37zm134 105q-28 0-28-28 0-13 7.5-21t20.5-8h171q13 0 20.5 8t7.5 21q0 28-28 28H313zm171 114q28 0 28 28 0 13-7.5 21t-20.5 8H313q-13 0-20.5-8t-7.5-21q0-28 28-28h171z",Te=!0,pe="SAP-icons-v5",de="@ui5/webcomponents-icons";f(be,{pathData:M,ltr:Te,collection:pe,packageName:de});P();const E="activities",ue="menu",W="M32 64q14 0 23 9.5T64 96q0 14-9 23t-23 9q-13 0-22.5-9T0 96q0-13 9.5-22.5T32 64zm0 160q14 0 23 9.5t9 22.5q0 14-9 23t-23 9q-13 0-22.5-9T0 256q0-13 9.5-22.5T32 224zm0 160q14 0 23 9.5t9 22.5q0 14-9 23t-23 9q-13 0-22.5-9T0 416q0-13 9.5-22.5T32 384zm128-255q-13 0-22.5-9.5T128 97q0-14 9.5-23t22.5-9h320q14 0 23 9t9 23q0 13-9 22.5t-23 9.5H160zm0 159q-13 0-22.5-9.5T128 256q0-14 9.5-23t22.5-9h320q14 0 23 9t9 23q0 13-9 22.5t-23 9.5H160zm0 159q-13 0-22.5-9.5T128 415q0-14 9.5-23t22.5-9h320q14 0 23 9t9 23q0 13-9 22.5t-23 9.5H160z",he=!1,fe="SAP-icons-v4",ge="@ui5/webcomponents-icons";f(ue,{pathData:W,ltr:he,collection:fe,packageName:ge});const _e="menu",A="M481 384q14 0 23 9t9 23-9 23-23 9H194q-15 0-23.5-9t-8.5-23 8.5-23 23.5-9h287zM194 129q-15 0-23.5-9T162 97t8.5-23 23.5-9h287q14 0 23 9t9 23-9 23-23 9H194zM50 368q20 0 34 14t14 34-14 34-34 14-34-14-14-34 14-34 34-14zm0-159q20 0 34 13.5T98 256t-14 34-34 14-34-14-14-34 14-33.5T50 209zm0-160q20 0 34 14t14 34-14 34-34 14-34-14T2 97t14-34 34-14zm431 175q14 0 23 9t9 23q0 15-9 23.5t-23 8.5H194q-32 0-32-32 0-14 8.5-23t23.5-9h287z",qe=!1,xe="SAP-icons-v5",ye="@ui5/webcomponents-icons";f(_e,{pathData:A,ltr:qe,collection:xe,packageName:ye});P();const B="menu",Se={title:"Layouts & Floorplans / TabContainer",component:g,argTypes:{children:{control:{disable:!0}},overflowButton:{control:{disable:!0}}},args:{tabLayout:K.Standard,contentBackgroundDesign:S.Solid,headerBackgroundDesign:S.Solid}},i={render:t=>e.jsxs(g,{...t,children:[e.jsx(a,{text:"Tab One",icon:B,selected:!0,additionalText:"5",children:"Content Tab 1"}),e.jsx(a,{text:"Tab Two",icon:E,additionalText:"20",children:"Content Tab 2"}),e.jsx(a,{text:"Tab Three",icon:V,children:"Content Tab 3"}),e.jsx(a,{text:"Tab Four",icon:G,children:"Content Tab 4"}),e.jsx(a,{text:"Tab Five",icon:J,children:"Content Tab 5"})]})},c={name:"with TabSeparator",render:t=>e.jsxs(g,{...t,children:[e.jsx(a,{text:"Tab One",icon:B,selected:!0,additionalText:"5",children:"Content Tab 1"}),e.jsx(m,{}),e.jsx(a,{text:"Tab Two",icon:E,additionalText:"20",children:"Content Tab 2"})]})},l={name:"with nested tabs",render:t=>e.jsxs(g,{...t,children:[e.jsx(a,{text:"Tab One",selected:!0,subTabs:e.jsxs(e.Fragment,{children:[e.jsx(a,{text:"Tab 1.1"}),e.jsx(a,{text:"Tab 1.2"}),e.jsx(a,{text:"Tab 1.3"})]}),children:e.jsx("div",{style:{display:"none"}})}),e.jsx(m,{}),e.jsx(a,{text:"Tab Two"}),e.jsx(a,{text:"Tab Three",subTabs:e.jsxs(e.Fragment,{children:[e.jsx(a,{text:"Tab 3.1"}),e.jsx(a,{text:"Tab 3.2"}),e.jsx(a,{text:"Tab 3.3"})]})})]})};var C,$,j;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: args => {
+    return <TabContainer {...args}>
+        <Tab text="Tab One" icon={menuIcon} selected additionalText="5">
+          Content Tab 1
+        </Tab>
+        <Tab text="Tab Two" icon={activitiesIcon} additionalText="20">
+          Content Tab 2
+        </Tab>
+        <Tab text="Tab Three" icon={addIcon}>
+          Content Tab 3
+        </Tab>
+        <Tab text="Tab Four" icon={employeeIcon}>
+          Content Tab 4
+        </Tab>
+        <Tab text="Tab Five" icon={settingsIcon}>
+          Content Tab 5
+        </Tab>
+      </TabContainer>;
+  }
+}`,...(j=($=i.parameters)==null?void 0:$.docs)==null?void 0:j.source}}};var D,z,I;c.parameters={...c.parameters,docs:{...(D=c.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  name: 'with TabSeparator',
+  render: args => {
+    return <TabContainer {...args}>
+        <Tab text="Tab One" icon={menuIcon} selected additionalText="5">
+          Content Tab 1
+        </Tab>
+        <TabSeparator />
+        <Tab text="Tab Two" icon={activitiesIcon} additionalText="20">
+          Content Tab 2
+        </Tab>
+      </TabContainer>;
+  }
+}`,...(I=(z=c.parameters)==null?void 0:z.docs)==null?void 0:I.source}}};var N,k,O;l.parameters={...l.parameters,docs:{...(N=l.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  name: 'with nested tabs',
+  render: args => {
+    return <TabContainer {...args}>
+        <Tab text="Tab One" selected subTabs={<>
+              <Tab text="Tab 1.1" />
+              <Tab text="Tab 1.2" />
+              <Tab text="Tab 1.3" />
+            </>}>
+          <div style={{
+          display: 'none'
+        }} />
+        </Tab>
+        <TabSeparator />
+        <Tab text="Tab Two" />
+        <Tab text="Tab Three" subTabs={<>
+              <Tab text="Tab 3.1" />
+              <Tab text="Tab 3.2" />
+              <Tab text="Tab 3.3" />
+            </>} />
+      </TabContainer>;
+  }
+}`,...(O=(k=l.parameters)==null?void 0:k.docs)==null?void 0:O.source}}};const we=["Default","WithTabSeparator","WithNestedTabs"],Re=Object.freeze(Object.defineProperty({__proto__:null,Default:i,WithNestedTabs:l,WithTabSeparator:c,__namedExportsOrder:we,default:Se},Symbol.toStringTag,{value:"Module"}));export{Re as C,i as D,m as T,c as W,l as a};
+//# sourceMappingURL=TabContainer.stories-f366bcd8.js.map
