93a12b25751ded59ce2bf1e58655e691ba86d69d
Lukas742 <lukas.harbarth@sap.com>
2023-05-08 11:01:39 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c9912c782e5af935ef829d12d87a04a48ca72224 🚀

---
+++ main/assets/Switch.stories-9acb296e.js
@@ -0,0 +1,25 @@
+import{M as d,C as c,f as l,a as h}from"./chunk-PCJTTTQV-87b66520.js";import{S as o}from"./index-b7896d4d.js";import{d as x}from"./DomRefTable.module-526c901c.js";import{D as S}from"./DocsHeader-9960d66d.js";import{F as u}from"./Footer-514eb661.js";import{j as t}from"./jsx-runtime-5926aa06.js";import"./index-ebeaab24.js";import{u as s}from"./index-bda0bad7.js";import"./iframe-2c88fdb5.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./UI5Element-1225b968.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d61dcbbc.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./Icon-c5c4720b.js";import"./Icons-524f4c51.js";import"./Device-208919c6.js";import"./AriaLabelHelper-43a261ec.js";import"./accept-198406be.js";import"./decline-d268166e.js";import"./i18n-defaults-80781f7e.js";import"./less-c244d45b.js";import"./class-map-519e9c6d.js";import"./react-jss.esm-022ab528.js";import"./index-d0a0415f.js";import"./slot-76e48863.js";import"./information-b4bc7d32.js";import"./Button-f2c9b4f8.js";import"./MarkedEvents-b83081e9.js";import"./i18n-defaults-254d6b69.js";import"./index-4c0ab015.js";import"./clsx.m-1229b3e0.js";import"./index-562e1433.js";import"./ThemingParameters-7e2e4e30.js";import"./Import-0ba4ccf6.js";import"./TableOfContent-73381f66.js";import"./index-9ae5a6d3.js";import"./Link-c4094736.js";import"./WrappingType-b81e595a.js";import"./index-a4fec522.js";import"./Label-326b72d8.js";import"./index-af7d528b.js";import"./Popover-abd2c401.js";import"./Integer-f7f172c9.js";import"./PopupUtils-e5ce8d68.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-02b533b0.js";import"./FocusableElements-fe6ae4c8.js";import"./isElementHidden-01c07146.js";import"./BrowserScrollbar.css-401cf542.js";import"./ResizeHandler-a7d132d0.js";import"./MediaRange-25b98f31.js";import"./style-map-33b353d0.js";const f=`## Keyboard Handling
+
+The state can be changed by pressing the Space and Enter keys.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Switch\` exposes the following CSS Shadow Parts:
+
+- slider - Used to style the track, where the handle is being slid
+- text-on - Used to style the \`textOn\` property text
+- text-off - Used to style the \`textOff\` property text
+- handle - Used to style the handle of the switch
+`;function j(r={}){const{wrapper:i}=Object.assign({},s(),r.components);return i?t.jsx(i,{...r,children:t.jsx(p,{})}):p();function p(){const n=Object.assign({h2:"h2"},s(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(d,{title:"Inputs / Switch",component:o}),`
+`,t.jsx(S,{since:"0.4.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(n.h2,{id:"example",children:"Example"}),`
+`,t.jsx(c,{children:t.jsx(l,{name:"Default",children:a=>t.jsx(o,{...a})})}),`
+`,t.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(x,{story:"Default"}),`
+`,t.jsx(h,{children:f}),`
+`,t.jsx(u,{})]})}}const m=r=>t.jsx(o,{...r});m.storyName="Default";m.parameters={storySource:{source:`args => {
+  return <Switch {...args} />;
+}`}};const e={title:"Inputs / Switch",component:o,tags:["stories-mdx"],includeStories:["defaultStory"]};e.parameters=e.parameters||{};e.parameters.docs={...e.parameters.docs||{},page:j};const _t=["defaultStory"];export{_t as __namedExportsOrder,e as default,m as defaultStory};
+//# sourceMappingURL=Switch.stories-9acb296e.js.map
