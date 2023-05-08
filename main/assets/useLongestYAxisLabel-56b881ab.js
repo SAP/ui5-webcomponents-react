64c1d371e8dafc7a9a0d3e94d8ce3e6b8af45460
Lukas742 <lukas.harbarth@sap.com>
2023-05-08 14:45:17 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@51a1285727f7e52b77824d6cbcf9019391dc1f9c 🚀

--- main/assets/useLongestYAxisLabel-56b881ab.js
+++
@@ -1,2 +0,0 @@
-import{r as i}from"./index-ebeaab24.js";import{g as l,a as n}from"./defaults-0dc2ce09.js";import{b as p}from"./useTooltipFormatter-cfb87cca.js";const u=(r,t)=>i.useMemo(()=>{let a=0;const e=t[0];if(r instanceof Array&&e){const o=s=>t.map(m=>e.formatter(l(s,m.accessor,"")));a=Math.max(...r.map(o).flat().map(p)),a+=8}return a=Math.min(a,n),[a,{marginLeft:a,maxWidth:`calc(100% - ${a+10}px)`}]},[r,t]);export{u};
-//# sourceMappingURL=useLongestYAxisLabel-56b881ab.js.map
