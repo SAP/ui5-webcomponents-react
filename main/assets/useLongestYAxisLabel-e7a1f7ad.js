5be3a40961713afb3e1dfa60de72e561295ed40e
MarcusNotheis <marcus.notheis@sap.com>
2023-05-22 14:44:56 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c4143261f8d329756fff5c9deede3fc477261cce 🚀

---
+++ main/assets/useLongestYAxisLabel-e7a1f7ad.js
@@ -0,0 +1,2 @@
+import{r as i}from"./index-ebeaab24.js";import{g as l,a as n}from"./defaults-e1f5ef45.js";import{b as p}from"./useTooltipFormatter-29a98786.js";const u=(r,t)=>i.useMemo(()=>{let a=0;const e=t[0];if(r instanceof Array&&e){const o=s=>t.map(m=>e.formatter(l(s,m.accessor,"")));a=Math.max(...r.map(o).flat().map(p)),a+=8}return a=Math.min(a,n),[a,{marginLeft:a,maxWidth:`calc(100% - ${a+10}px)`}]},[r,t]);export{u};
+//# sourceMappingURL=useLongestYAxisLabel-e7a1f7ad.js.map
