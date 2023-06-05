747b6a5d2915e667f074ac99a5135961d62cb69c
Lukas742 <lukas.harbarth@sap.com>
2023-06-05 13:31:18 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@6f3673a3f56b82fcdfabfc38a3230ea1250dcb3e 🚀

---
+++ main/assets/MessageStrip.stories-a24fd28d.js
@@ -0,0 +1,26 @@
+import{M as u,C as m,f as c,a as x}from"./chunk-PCJTTTQV-1db77e98.js";import{M as o,a as d}from"./index-a0bf049f.js";import"./employee-d55343ff.js";import{I as h}from"./index-8d2d8058.js";import{e as j}from"./DomRefTable.module-e5f90a5a.js";import{D as f}from"./DocsHeader-10a6ee18.js";import{F as y}from"./Footer-cf32730a.js";import{j as t}from"./jsx-runtime-5926aa06.js";import"./index-ebeaab24.js";import{u as l}from"./index-bda0bad7.js";import"./iframe-9072166d.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./UI5Element-a8445a25.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-884f99a7.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-3e9cb840.js";import"./Icons-c55d12a5.js";import"./decline-c4793abd.js";import"./i18n-defaults-80781f7e.js";import"./information-e6fe8610.js";import"./class-map-323a92fb.js";import"./Button-f1768a08.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-fca59c5d.js";import"./react-jss.esm-022ab528.js";import"./index-b9e92427.js";import"./clsx.m-1229b3e0.js";import"./index-562e1433.js";import"./ThemingParameters-7e2e4e30.js";import"./TableOfContent-4a92eb2a.js";import"./index-bc2c7fc0.js";import"./Link-8b80096c.js";import"./WrappingType-b81e595a.js";import"./index-30ebdf86.js";import"./Label-6cefa2d2.js";import"./index-60e21eab.js";import"./Popover-2f417d64.js";import"./Integer-f7f172c9.js";import"./PopupUtils-b38c74c1.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-3239006e.js";import"./FocusableElements-69e44a9d.js";import"./isElementHidden-01c07146.js";import"./ResizeHandler-cea672cf.js";import"./MediaRange-25b98f31.js";import"./style-map-34e6b8f4.js";const M=`## Usage
+
+For the \`MessageStrip\` component, you can define whether it displays an icon in the beginning and a close button. Moreover, its size and background can be controlled with CSS.
+
+## Keyboard Handling
+
+### Fast Navigation
+
+This component provides a build in fast navigation group which can be used via \`F6 / Shift + F6\` or \`Ctrl + Alt(Option) + Down / Ctrl + Alt(Option) + Up\`. In order to use this functionality, you need to import the following module: \`import "@ui5/webcomponents-base/dist/features/F6Navigation.js"\`
+`;function S(r={}){const{wrapper:a}=Object.assign({},l(),r.components);return a?t.jsx(a,{...r,children:t.jsx(p,{})}):p();function p(){const e=Object.assign({h2:"h2",h1:"h1"},l(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(u,{title:"Data Display / MessageStrip",component:o,argTypes:{children:{control:"text"},icon:{control:{disable:!0}}},args:{design:d.Information,children:"MessageStrip Text"}}),`
+`,t.jsx(f,{since:"0.4.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(m,{children:t.jsx(c,{name:"Default",children:n=>t.jsx(o,{...n})})}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(j,{story:"Default"}),`
+`,t.jsx(x,{children:M}),`
+`,t.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(e.h2,{id:"messagestrip-with-icon",children:"MessageStrip with Icon"}),`
+`,t.jsx(m,{children:t.jsx(c,{name:"with Icon",args:{icon:t.jsx(h,{name:"employee"})},children:n=>t.jsx(o,{...n})})}),`
+`,t.jsx(y,{})]})}}const g=r=>t.jsx(o,{...r});g.storyName="Default";g.parameters={storySource:{source:`args => {
+  return <MessageStrip {...args} />;
+}`}};const s=r=>t.jsx(o,{...r});s.storyName="with Icon";s.args={icon:t.jsx(h,{name:"employee"})};s.parameters={storySource:{source:`args => {
+  return <MessageStrip {...args} />;
+}`}};const i={title:"Data Display / MessageStrip",component:o,args:{design:d.Information,children:"MessageStrip Text"},argTypes:{children:{control:"text"},icon:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory","withIcon"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:S};const Tt=["defaultStory","withIcon"];export{Tt as __namedExportsOrder,i as default,g as defaultStory,s as withIcon};
+//# sourceMappingURL=MessageStrip.stories-a24fd28d.js.map
