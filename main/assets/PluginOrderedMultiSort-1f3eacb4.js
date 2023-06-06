1bda184bf7b32afa32bfe3cc95ba4bf5ab41be11
Lukas742 <lukas.harbarth@sap.com>
2023-06-06 11:09:40 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@ddbfb4a44171831c6704db958519feee75b15bbe 🚀

---
+++ main/assets/PluginOrderedMultiSort-1f3eacb4.js
@@ -0,0 +1,63 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{d as a,C as p}from"./DomRefTable.module-bfa916b8.js";import{M as s,C as l}from"./chunk-PCJTTTQV-08f35eb0.js";import"./index-ebeaab24.js";import{F as d}from"./Footer-b7431144.js";import{f as n}from"./AnalyticalTable.stories-60520b4f.js";import{u as m}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-a1bdeb54.js";import"./iframe-529e6a99.js";import"../sb-preview/runtime.js";import"./UI5Element-a8445a25.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-884f99a7.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-3e9cb840.js";import"./Icons-c55d12a5.js";import"./decline-c4793abd.js";import"./i18n-defaults-80781f7e.js";import"./information-e6fe8610.js";import"./class-map-323a92fb.js";import"./Button-f1768a08.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-fca59c5d.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./index-4c0ab015.js";import"./clsx.m-1229b3e0.js";import"./index-4ee949ab.js";import"./Label-6cefa2d2.js";import"./WrappingType-b81e595a.js";import"./index-31193b6d.js";import"./Link-8b80096c.js";import"./index-ffa785e6.js";import"./Popover-2f417d64.js";import"./Integer-f7f172c9.js";import"./PopupUtils-b38c74c1.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-3239006e.js";import"./FocusableElements-69e44a9d.js";import"./isElementHidden-01c07146.js";import"./ResizeHandler-cea672cf.js";import"./MediaRange-25b98f31.js";import"./style-map-34e6b8f4.js";import"./delete-e30a738e.js";import"./ListItem-a92cce8b.js";import"./ListItemBase-124add92.js";import"./ItemNavigation-cb92b87f.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-8d822d0c.js";import"./RadioButton-021a694f.js";import"./ValueState-2c5e5904.js";import"./CheckBox-33235970.js";import"./accept-2ae5e2eb.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-70b47038.js";import"./settings-34131f1f.js";import"./index-562e1433.js";import"./ThemingParameters-7e2e4e30.js";import"./i18n-defaults-2b7c3fd4.js";import"./I18nContext-cdb50611.js";import"./useIsomorphicId-4be7c5e3.js";import"./debounce-c13310e6.js";import"./index-99d317a4.js";import"./CssSizeVariables-02fb2fd8.js";import"./CustomVariables-fd831c35.js";import"./filter-6cb7da5b.js";import"./group-2-f06acedb.js";import"./index-4b94c4a9.js";import"./stopPropagation-da74d54e.js";import"./index-082a1ce1.js";import"./CustomListItem-4a6187c7.js";import"./index-74fc3b36.js";import"./index-6b7d80ea.js";import"./StandardListItem-8dc8c287.js";import"./Avatar-3e4df27c.js";import"./employee-d55343ff.js";import"./ValueState-ab6838cc.js";import"./index-5c532abf.js";import"./Input-c2f4aa77.js";import"./Suggestions.css-2ef2fda5.js";import"./ResponsivePopoverCommon.css-04f065a8.js";import"./ValueStateMessage.css-111bf458.js";import"./navigation-down-arrow-1751e708.js";import"./navigation-right-arrow-b38baa43.js";import"./useCurrentTheme-54e82370.js";import"./index-87873df2.js";import"./index-1c2846c9.js";import"./index-be502015.js";import"./GlobalStyleClasses-7209358d.js";import"./index-e1589f6e.js";import"./Title-a5ea80a2.js";import"./index-97077744.js";import"./slim-arrow-down-91425847.js";import"./GroupHeaderListItem-bd8efaa6.js";import"./Tokenizer-26722e19.js";import"./ScrollEnablement-4353003c.js";import"./animate-1a16ba20.js";import"./ResponsivePopover-1eb02d4a.js";import"./Dialog-effa1776.js";import"./Token-55bee0ec.js";import"./ToggleButton-49eb273a.js";import"./ComboBoxItem-346fa075.js";import"./index-083b90c1.js";import"./index-7fa826b8.js";import"./InvisibleMessage-c74dbb24.js";import"./index-538d5363.js";import"./index-1fd6a64e.js";function Pr(e={}){const{wrapper:o}=Object.assign({},m(),e.components);return o?t.jsx(o,Object.assign({},e,{children:t.jsx(i,{})})):i();function i(){const r=Object.assign({h1:"h1",p:"p",code:"code",ul:"ul",li:"li",strong:"strong",h2:"h2",h3:"h3",pre:"pre"},m(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Data Display / AnalyticalTable / Plugin: useOrderedMultiSort"}),`
+`,t.jsx(r.h1,{id:"analyticaltable-plugin-useorderedmultisort",children:"AnalyticalTable Plugin: useOrderedMultiSort"}),`
+`,t.jsx(a,{moduleName:"AnalyticalTableHooks",packageName:"@ui5/webcomponents-react"}),`
+`,t.jsxs(r.p,{children:["With the ",t.jsx(r.code,{children:"useOrderedMultiSort"})," it's possible to define the sorting priority if multi-sort is enabled for the respective columns (",t.jsx(r.code,{children:"enableMultiSort: true"}),")."]}),`
+`,t.jsx(r.p,{children:"It receives one parameter that defines the sorting priority by the column id."}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.code,{children:"orderedIds: string[]"}),": Defines the sort priority when sorting by multiple columns, starting with the first column ID."]}),`
+`]}),`
+`,t.jsxs(r.p,{children:[t.jsx(r.strong,{children:"Note:"})," Column IDs that are not found in the array use the default priority, so the first sorted column has a higher priority than the next sorted column."]}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(l,{sourceState:"none",of:n}),`
+`,t.jsx(r.h3,{id:"current-parameter",children:"Current Parameter"}),`
+`,t.jsx(p,{of:n,hideHTMLPropsNote:!0,include:["orderedIds"]}),`
+`,t.jsx(r.h3,{id:"code",children:"Code"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`const columns = [
+  {
+    Header: 'Name',
+    accessor: 'name',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age',
+    accessor: 'age',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Name 2',
+    accessor: 'name2',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age 2',
+    accessor: 'age2',
+    enableMultiSort: true
+  }
+];
+const data = [
+  { name: 'Peter', age: 40, name2: 'Alissa', age2: 18 },
+  { name: 'Kristen', age: 40, name2: 'Randolph', age2: 21 },
+  { name: 'Peter', age: 30, name2: 'Rose', age2: 90 },
+  { name: 'Peter', age: 70, name2: 'Rose', age2: 22 },
+  { name: 'Kristen', age: 60, name2: 'Willis', age2: 80 },
+  { name: 'Kristen', age: 20, name2: 'Alissa', age2: 80 },
+  { name: 'Graham', age: 40, name2: 'Alissa', age2: 80 },
+  { name: 'Peter', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Willis', age2: 26 },
+  { name: 'Graham', age: 62, name2: 'Willis', age2: 26 }
+];
+const orderedIds = ['name', 'name2', 'age', 'age2'];
+const TableComponent = () => {
+  return (
+    <AnalyticalTable
+      columns={columns}
+      data={data}
+      sortable
+      tableHooks={[AnalyticalTableHooks.useOrderedMultiSort(orderedIds)]}
+    />
+  );
+};
+`})}),`
+`,t.jsx(d,{})]})}}export{Pr as default};
+//# sourceMappingURL=PluginOrderedMultiSort-1f3eacb4.js.map
