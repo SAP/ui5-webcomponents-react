c7302cd16f8e19385deba13644af66699340772f
MarcusNotheis <marcus.notheis@sap.com>
2023-06-07 09:31:37 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@1292212f8512fd60d9fc86cfdbff28beb042e2b3 🚀

---
+++ main/assets/Breadcrumbs-9088e4be.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{C as s,A as a}from"./DomRefTable.module-72664463.js";import{D as d}from"./DocsHeader-1a06cf9f.js";import{F as c}from"./Footer-c4d2ce40.js";import"./index-ebeaab24.js";import{M as h,C as f,a as i}from"./chunk-PCJTTTQV-cdcdb3cb.js";import{S as u}from"./SubcomponentsSection-6138f2fc.js";import{a as x}from"./index-4c9097cc.js";import{C as l,D as p}from"./Breadcrumbs.stories-e90cab67.js";import{u as n}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-0c954580.js";import"./iframe-82f7c4c0.js";import"../sb-preview/runtime.js";import"./UI5Element-a8445a25.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-884f99a7.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-3e9cb840.js";import"./Icons-c55d12a5.js";import"./decline-c4793abd.js";import"./i18n-defaults-80781f7e.js";import"./information-e6fe8610.js";import"./class-map-323a92fb.js";import"./Button-f1768a08.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-fca59c5d.js";import"./index-4c0ab015.js";import"./clsx.m-1229b3e0.js";import"./index-562e1433.js";import"./ThemingParameters-7e2e4e30.js";import"./TableOfContent-08a9245a.js";import"./index-317ab26f.js";import"./Link-8b80096c.js";import"./WrappingType-b81e595a.js";import"./index-69d14591.js";import"./Label-6cefa2d2.js";import"./index-9c09ad76.js";import"./index-99a4fec8.js";import"./Popover-2f417d64.js";import"./Integer-f7f172c9.js";import"./PopupUtils-b38c74c1.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-3239006e.js";import"./FocusableElements-69e44a9d.js";import"./isElementHidden-01c07146.js";import"./ResizeHandler-cea672cf.js";import"./MediaRange-25b98f31.js";import"./style-map-34e6b8f4.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./ItemNavigation-cb92b87f.js";import"./ResponsivePopover-1eb02d4a.js";import"./Dialog-effa1776.js";import"./ValueState-2c5e5904.js";import"./Title-a5ea80a2.js";import"./ListItemBase-124add92.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-8d822d0c.js";import"./StandardListItem-8dc8c287.js";import"./ListItem-a92cce8b.js";import"./RadioButton-021a694f.js";import"./CheckBox-33235970.js";import"./accept-2ae5e2eb.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-70b47038.js";import"./Avatar-3e4df27c.js";import"./employee-d55343ff.js";import"./slim-arrow-down-91425847.js";const j="**Since:** 0.18.0\n\nThe `BreadcrumbsItem` component defines the content of an item in `Breadcrumbs`\n",b=`## Keyboard Handling
+
+The \`Breadcrumbs\` provides advanced keyboard handling.
+
+- \\[F4, ALT+UP, ALT+DOWN, SPACE, ENTER\\] - If the dropdown arrow is focused - opens/closes the drop-down.
+- \\[SPACE, ENTER\\] - Activates the focused item and triggers the \`onItemClick\` event.
+- \\[ESC\\] - Closes the drop-down.
+- \\[LEFT\\] - If the drop-down is closed - navigates one item to the left.
+- \\[RIGHT\\] - If the drop-down is closed - navigates one item to the right.
+- \\[UP\\] - If the drop-down is open - moves focus to the next item.
+- \\[DOWN\\] - If the drop-down is open - moves focus to the previous item.
+- \\[HOME\\] - Navigates to the first item.
+- \\[END\\] - Navigates to the last item.
+`;function qt(o={}){const{wrapper:e}=Object.assign({},n(),o.components);return e?t.jsx(e,Object.assign({},o,{children:t.jsx(m,{})})):m();function m(){const r=Object.assign({h2:"h2"},n(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(h,{of:l}),`
+`,t.jsx(d,{since:"0.18.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(f,{of:p}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(i,{children:b}),`
+`,t.jsx(i,{children:u}),`
+`,t.jsx(r.h2,{id:"breadcrumbsitem",children:"BreadcrumbsItem"}),`
+`,t.jsx(i,{children:j}),`
+`,t.jsx(a,{hideHTMLPropsNote:!0,of:x}),`
+`,t.jsx(c,{})]})}}export{qt as default};
+//# sourceMappingURL=Breadcrumbs-9088e4be.js.map
---
+++ main/assets/BusyIndicator.stories-abec87a2.js
@@ -0,0 +1,43 @@
+import{M as y,C as s,f as d,a as x}from"./chunk-PCJTTTQV-cdcdb3cb.js";import{B as r}from"./index-b9194055.js";import{a as c}from"./BusyIndicator-8d822d0c.js";import{S as l}from"./index-36baeeb7.js";import{T as j}from"./index-562e1433.js";import{e as g}from"./DomRefTable.module-72664463.js";import{D as b}from"./DocsHeader-1a06cf9f.js";import{F as f}from"./Footer-c4d2ce40.js";import{j as t}from"./jsx-runtime-5926aa06.js";import"./index-ebeaab24.js";import{u}from"./index-bda0bad7.js";import"./iframe-82f7c4c0.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./withWebComponent-884f99a7.js";import"./utils-69f7a0e0.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./UI5Element-a8445a25.js";import"./Icons-c55d12a5.js";import"./Integer-f7f172c9.js";import"./Label-6cefa2d2.js";import"./WrappingType-b81e595a.js";import"./i18n-defaults-fca59c5d.js";import"./class-map-323a92fb.js";import"./Icon-3e9cb840.js";import"./slot-76e48863.js";import"./InvisibleMessage-c74dbb24.js";import"./AriaLabelHelper-43a261ec.js";import"./ValueState-2c5e5904.js";import"./slim-arrow-down-91425847.js";import"./information-e6fe8610.js";import"./i18n-defaults-80781f7e.js";import"./Device-208919c6.js";import"./decline-c4793abd.js";import"./ListItemBase-124add92.js";import"./ResizeHandler-cea672cf.js";import"./ItemNavigation-cb92b87f.js";import"./getActiveElement-bcae01ed.js";import"./TabbableElements-8e49b367.js";import"./isElementHidden-01c07146.js";import"./debounce-9c2fb7dd.js";import"./style-map-34e6b8f4.js";import"./ResponsivePopover-1eb02d4a.js";import"./PopupUtils-b38c74c1.js";import"./Popover-2f417d64.js";import"./PopupsCommon.css-3239006e.js";import"./FocusableElements-69e44a9d.js";import"./MediaRange-25b98f31.js";import"./Dialog-effa1776.js";import"./Button-f1768a08.js";import"./MarkedEvents-b83081e9.js";import"./Title-a5ea80a2.js";import"./StandardListItem-8dc8c287.js";import"./ListItem-a92cce8b.js";import"./RadioButton-021a694f.js";import"./CheckBox-33235970.js";import"./accept-2ae5e2eb.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-70b47038.js";import"./Avatar-3e4df27c.js";import"./employee-d55343ff.js";import"./ResponsivePopoverCommon.css-04f065a8.js";import"./ValueStateMessage.css-111bf458.js";import"./ValueState-ab6838cc.js";import"./clsx.m-1229b3e0.js";import"./react-jss.esm-022ab528.js";import"./ThemingParameters-7e2e4e30.js";import"./index-0c954580.js";import"./index-4c0ab015.js";import"./TableOfContent-08a9245a.js";import"./index-317ab26f.js";import"./Link-8b80096c.js";import"./index-69d14591.js";import"./index-99a4fec8.js";const v=`## Usage
+
+For the \`BusyIndicator\` you can define the size, the text and whether it is shown or hidden. In order to hide it, use the "active" property.
+
+In order to show busy state over an HTML element, simply nest the HTML element in a \`BusyIndicator\` instance.  
+**Note:** Since \`BusyIndicator\` has \`display: inline-block;\` by default and no width of its own, whenever you need to wrap a block-level element, you should set \`display: block\` to the busy indicator as well.
+
+### When to use:
+
+- The user needs to be able to cancel the operation.
+- Only part of the application or a particular component is affected.
+
+### When not to use:
+
+- The operation takes less than one second.
+- You need to block the screen and prevent the user from starting another activity.
+- Do not show multiple busy indicators at once.
+`;function S(e={}){const{wrapper:m}=Object.assign({},u(),e.components);return m?t.jsx(m,{...e,children:t.jsx(p,{})}):p();function p(){const o=Object.assign({h2:"h2",h1:"h1",p:"p"},u(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(y,{title:"User Feedback / BusyIndicator",component:r,argTypes:{children:{control:{disable:!0}}},args:{size:c.Medium,delay:1e3,active:!0}}),`
+`,t.jsx(b,{since:"0.4.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(s,{children:t.jsx(d,{name:"Default",children:n=>t.jsx(r,{...n})})}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(g,{story:"Default"}),`
+`,t.jsx(x,{children:v}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"busyindicator-with-children",children:"BusyIndicator with children"}),`
+`,t.jsx(s,{children:t.jsx(d,{name:"With children",args:{active:!0},children:n=>t.jsx(r,{...n,children:t.jsx(l,{})})})}),`
+`,t.jsx(s,{mdxSource:'<BusyIndicator active><Text><p>{"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et\\ndolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita\\nkasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur\\nsadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam\\nvoluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata\\nsanctus est Lorem ipsum dolor sit amet."}</p></Text></BusyIndicator>',children:t.jsx(r,{active:!0,children:t.jsx(j,{children:t.jsx(o.p,{children:`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+sanctus est Lorem ipsum dolor sit amet.`})})})}),`
+`,t.jsx(f,{})]})}}const h=e=>t.jsx(r,{...e});h.storyName="Default";h.parameters={storySource:{source:`args => {
+  return <BusyIndicator {...args} />;
+}`}};const a=e=>t.jsx(r,{...e,children:t.jsx(l,{})});a.storyName="With children";a.args={active:!0};a.parameters={storySource:{source:`args => {
+  return <BusyIndicator {...args}>
+          <Select />
+        </BusyIndicator>;
+}`}};const i={title:"User Feedback / BusyIndicator",component:r,args:{size:c.Medium,delay:1e3,active:!0},argTypes:{children:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory","withChildren"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:S};const $t=["defaultStory","withChildren"];export{$t as __namedExportsOrder,i as default,h as defaultStory,a as withChildren};
+//# sourceMappingURL=BusyIndicator.stories-abec87a2.js.map
