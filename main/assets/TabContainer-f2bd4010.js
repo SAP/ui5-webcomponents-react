643c97b9a18ba741390b37f03501d41bbb75043c
MarcusNotheis <marcus.notheis@sap.com>
2023-06-12 07:53:26 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@3990376fd7bc25904b61682c8dc698d164fc1f74 🚀

---
+++ main/assets/TabContainer-f2bd4010.js
@@ -0,0 +1,57 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{C as l,A as s}from"./DomRefTable.module-d4b8a2ca.js";import{D as d}from"./DocsHeader-2757f7f4.js";import{F as h}from"./Footer-fb464ec9.js";import"./index-ebeaab24.js";import{D as b}from"./DomRefTable-aaec1385.js";import{S as c}from"./SubcomponentsSection-6138f2fc.js";import{M as u,C as r,a as o}from"./chunk-PCJTTTQV-bcf31a10.js";import{a as x}from"./index-0fd63e4a.js";import{C as f,D as p,W as T,a as j,T as g}from"./TabContainer.stories-8f83f271.js";import{u as m}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-43e6f897.js";import"./iframe-3d188825.js";import"../sb-preview/runtime.js";import"./UI5Element-b1843653.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7b9bd1ee.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-aac9cdc2.js";import"./Icons-9f549e0f.js";import"./decline-d907918c.js";import"./i18n-defaults-80781f7e.js";import"./information-0f993e39.js";import"./class-map-6a9bd920.js";import"./Button-37a8c146.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-fca59c5d.js";import"./index-8330eb1d.js";import"./clsx.m-1229b3e0.js";import"./index-562e1433.js";import"./ThemingParameters-7e2e4e30.js";import"./TableOfContent-85f2fc26.js";import"./index-6a8f89f1.js";import"./Link-64c69167.js";import"./WrappingType-b81e595a.js";import"./index-afad28ed.js";import"./Label-350e96fc.js";import"./index-9c09ad76.js";import"./index-8a47ffb7.js";import"./Popover-a3a62d04.js";import"./Integer-f7f172c9.js";import"./PopupUtils-2f7e8748.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-38257fbf.js";import"./FocusableElements-72585d9b.js";import"./isElementHidden-01c07146.js";import"./ResizeHandler-8726336f.js";import"./MediaRange-25b98f31.js";import"./style-map-0c64293c.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./ListItem-c43ad95c.js";import"./ListItemBase-5d83f75f.js";import"./ItemNavigation-5f1efe0b.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-50734768.js";import"./RadioButton-8c17b531.js";import"./ValueState-2c5e5904.js";import"./CheckBox-f1563b72.js";import"./accept-31e71d55.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-82b13059.js";import"./CustomListItem-8cb4a16f.js";import"./slideUp-745debd3.js";import"./animate-1a16ba20.js";import"./AnimationMode-fb7e718c.js";import"./slim-arrow-up-fdc86d66.js";import"./slim-arrow-down-72222bb1.js";import"./ResponsivePopover-5c5386af.js";import"./Dialog-5f3c9cfb.js";import"./Title-684c2d0e.js";import"./ResponsivePopoverCommon.css-5e0f54ec.js";import"./add-7ec0d85a.js";import"./employee-2384bc11.js";import"./settings-eb949844.js";const S="The `Tab` represents a selectable item inside a `Tabcontainer`. It defines both the item in the tab strip (top part of the `Tabcontainer`) and the content that is presented to the user once the tab is selected\n",w="The `TabSeparator` represents a vertical line to separate tabs inside a `TabContainer`\n",C=`## Structure
+
+The \`TabContainer\` can hold two types of entities:
+
+- \`Tab\` - contains all the information on an item (text and icon)
+- \`TabSeparator\` - used to separate tabs with a line
+
+## Hierarchies
+
+Multiple sub tabs could be placed underneath one main tab. Nesting allows deeper hierarchies with indentations to indicate the level of each nested tab. When a tab has both sub tabs and own content its click area is split to allow the user to display the content or alternatively to expand / collapse the list of sub tabs.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`TabContainer\` exposes the following CSS Shadow Parts:
+
+- content - Used to style the content of the component
+
+## Keyboard Handling
+
+### Fast Navigation
+
+This component provides a build in fast navigation group which can be used via \`F6 / Shift + F6\` or \`Ctrl + Alt(Option) + Down / Ctrl + Alt(Option) + Up\`. In order to use this functionality, you need to import the following module: \`import "@ui5/webcomponents-base/dist/features/F6Navigation.js"\`
+`,D=[{name:"allItems",readonly:"true",visibility:"public",type:"undefined",description:`Returns all slotted tabs and their subTabs in a flattened array. The order of tabs is depth-first. For example, given the following slotted elements: <pre><code>
+	&lt;ui5-tabcontainer&gt;
+		&lt;ui5-tab id="First" text="First"&gt;
+			...
+			&lt;ui5-tab slot="subTabs" id="Nested" text="Nested"&gt;...&lt;/ui5-tab&gt;
+		&lt;/ui5-tab&gt;
+		&lt;ui5-tab id="Second" text="Second"&gt;...&lt;/ui5-tab&gt;
+		&lt;ui5-tab-separator id="sep"&gt;&lt;/ui5-tab-separator&gt;
+		&lt;ui5-tab id="Third" text="Third"&gt;...&lt;/ui5-tab&gt;
+	&lt;/ui5-tabcontainer&gt;
+</code></pre> Calling <code>allItems</code> on this TabContainer will return the instances in the following order: <code>[ ui5-tab#First, ui5-tab#Nested, ui5-tab#Second, ui5-tab-separator#sep, ui5-tab#Third ]</code>`}];function ne(i={}){const{wrapper:n}=Object.assign({},m(),i.components);return n?t.jsx(n,Object.assign({},i,{children:t.jsx(a,{})})):a();function a(){const e=Object.assign({h2:"h2",h1:"h1",p:"p"},m(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(u,{of:f}),`
+`,t.jsx(d,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{of:p}),`
+`,t.jsx(o,{children:C}),`
+`,t.jsx(b,{rows:D}),`
+`,t.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(e.h2,{id:"tabcontainer-with-tabseparator",children:"TabContainer with TabSeparator"}),`
+`,t.jsxs(e.p,{children:["The ",t.jsx("code",{children:"TabSeparator"})," represents a vertical line to separate tabs inside a ",t.jsx("code",{children:"TabContainer"})]}),`
+`,t.jsx(r,{of:T}),`
+`,t.jsx(e.h2,{id:"tabcontainer-with-nested-tabs",children:"TabContainer with nested Tabs"}),`
+`,t.jsx(r,{of:j}),`
+`,t.jsx(o,{children:c}),`
+`,t.jsx(e.h2,{id:"tab",children:"Tab"}),`
+`,t.jsx(o,{children:S}),`
+`,t.jsx(s,{of:x}),`
+`,t.jsx(e.h2,{id:"tabseparator",children:"TabSeparator"}),`
+`,t.jsx(o,{children:w}),`
+`,t.jsx(s,{of:g}),`
+`,t.jsx(h,{})]})}}export{ne as default};
+//# sourceMappingURL=TabContainer-f2bd4010.js.map
---
+++ main/assets/index-6b4a7d90.js
@@ -0,0 +1,31 @@
+import{_ as n}from"./iframe-3d188825.js";import"./CheckBox-f1563b72.js";import{w as t}from"./withWebComponent-7b9bd1ee.js";import{V as a}from"./ValueState-ab6838cc.js";const e=t("ui5-checkbox",["accessibleName","accessibleNameRef","name","text","valueState","wrappingType"],["checked","disabled","indeterminate","readonly","required"],[],["change"],()=>n(()=>import("./CheckBox-f1563b72.js"),["./CheckBox-f1563b72.js","./Device-208919c6.js","./UI5Element-b1843653.js","./CustomElementsScopeUtils-137da8c8.js","./withWebComponent-7b9bd1ee.js","./utils-69f7a0e0.js","./index-ebeaab24.js","./jsx-runtime-5926aa06.js","./useIsomorphicLayoutEffect-38a48652.js","./slot-76e48863.js","./Icon-aac9cdc2.js","./Icons-9f549e0f.js","./ValueState-2c5e5904.js","./AriaLabelHelper-43a261ec.js","./accept-31e71d55.js","./Label-350e96fc.js","./WrappingType-b81e595a.js","./i18n-defaults-fca59c5d.js","./class-map-6a9bd920.js"],import.meta.url));e.displayName="CheckBox";e.defaultProps={valueState:a.None};try{e.displayName="CheckBox",e.__docgenInfo={description:`Allows the user to set a binary value, such as true/false or yes/no for an item.
+
+The \`CheckBox\` component consists of a box and a label that describes its purpose. If it's checked, an indicator is displayed inside the box. To check/uncheck the \`CheckBox\`, the user has to click or tap the square box or its label.
+
+The \`CheckBox\` component only has 2 states - checked and unchecked. Clicking or tapping toggles the \`CheckBox\` between checked and unchecked state.
+
+__Note:__ This component is a web component developed by the UI5 Web Components’ team.
+
+<ui5-link href="https://sap.github.io/ui5-webcomponents/playground/components/CheckBox" target="_blank">UI5 Web Components Playground</ui5-link>`,displayName:"CheckBox",props:{onChange:{defaultValue:null,description:"Fired when the component checked state changes.",name:"onChange",required:!1,type:{name:"(event: Ui5CustomEvent<CheckBoxDomRef, never>) => void"}},accessibleName:{defaultValue:null,description:"Defines the accessible ARIA name of the component.",name:"accessibleName",required:!1,type:{name:"string"}},accessibleNameRef:{defaultValue:null,description:"Receives id(or many ids) of the elements that label the component",name:"accessibleNameRef",required:!1,type:{name:"string"}},checked:{defaultValue:null,description:`Defines if the component is checked.
+
+**Note:** The property can be changed with user interaction, either by cliking/tapping on the component, or by pressing the Enter or Space key.`,name:"checked",required:!1,type:{name:"boolean"}},disabled:{defaultValue:null,description:`Defines whether the component is disabled.
+
+**Note:** A disabled component is completely noninteractive.`,name:"disabled",required:!1,type:{name:"boolean"}},indeterminate:{defaultValue:null,description:`Defines whether the component is displayed as partially checked.
+
+**Note:** The indeterminate state can be set only programmatically and can’t be achieved by user interaction and the resulting visual state depends on the values of the \`indeterminate\` and \`checked\` properties:
+
+*   If the component is checked and indeterminate, it will be displayed as partially checked
+*   If the component is checked and it is not indeterminate, it will be displayed as checked
+*   If the component is not checked, it will be displayed as not checked regardless value of the indeterminate attribute`,name:"indeterminate",required:!1,type:{name:"boolean"}},name:{defaultValue:null,description:'Determines the name with which the component will be submitted in an HTML form.\n\n**Important:** For the `name` property to have effect, you must add the following import to your project: `import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";`\n\n**Note:** When set, a native `input` HTML element will be created inside the component so that it can be submitted as part of an HTML form. Do not use this property unless you need to submit a form.',name:"name",required:!1,type:{name:"string"}},readonly:{defaultValue:null,description:`Defines whether the component is read-only.
+
+**Note:** A read-only component is not editable, but still provides visual feedback upon user interaction.`,name:"readonly",required:!1,type:{name:"boolean"}},required:{defaultValue:null,description:"Defines whether the component is required.",name:"required",required:!1,type:{name:"boolean"}},text:{defaultValue:null,description:"Defines the text of the component.",name:"text",required:!1,type:{name:"string"}},valueState:{defaultValue:{value:"ValueState.None"},description:"Defines the value state of the component.\n\n**Note:**\n\n*   `Warning`\n*   `Error`\n*   `None`(default)\n*   `Success`\n*   `Information`",name:"valueState",required:!1,type:{name:"enum",value:[{value:'"None"'},{value:'"Error"'},{value:'"Information"'},{value:'"Warning"'},{value:'"Success"'},{value:'"Error"'},{value:'"None"'},{value:'"Success"'},{value:'"Warning"'},{value:'"Information"'}]}},wrappingType:{defaultValue:null,description:`Defines whether the component text wraps when there is not enough space.
+
+Available options are:
+
+*   \`None\` - The text will be truncated with an ellipsis.
+*   \`Normal\` - The text will wrap. The words will not be broken based on hyphenation.`,name:"wrappingType",required:!1,type:{name:"enum",value:[{value:'"None"'},{value:'"None"'},{value:'"Normal"'},{value:'"Normal"'}]}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}export{e as C};
+//# sourceMappingURL=index-6b4a7d90.js.map
