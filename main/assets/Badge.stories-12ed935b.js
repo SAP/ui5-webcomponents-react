61d5e91003e2b68d78580b38acccb7001330eaec
Lukas742 <lukas.harbarth@sap.com>
2023-06-13 10:19:37 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@4e51b3e352eb81b383a21b869dbc671502066412 🚀

---
+++ main/assets/Badge.stories-12ed935b.js
@@ -0,0 +1,78 @@
+import{M as S,C as c,f as l,a as B}from"./chunk-PCJTTTQV-063e2a8c.js";import{B as o}from"./index-d28a6d3e.js";import"./employee-649f60dc.js";import{I as s}from"./index-85cf3405.js";import{F as p,d as h}from"./index-b9e92427.js";import{e as j}from"./DomRefTable.module-001ee1c3.js";import{D as y}from"./DocsHeader-09494c78.js";import{F as u}from"./Footer-f0841fbb.js";import{j as e}from"./jsx-runtime-5926aa06.js";import"./index-ebeaab24.js";import{u as a}from"./index-bda0bad7.js";import"./iframe-403ac14f.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./UI5Element-2e105c8a.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7b9bd1ee.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Button-77ada2e8.js";import"./Icon-f65ad4fd.js";import"./Icons-1a294222.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-fca59c5d.js";import"./clsx.m-1229b3e0.js";import"./react-jss.esm-022ab528.js";import"./index-e33c04d0.js";import"./decline-bfe69317.js";import"./i18n-defaults-80781f7e.js";import"./information-bd54c92c.js";import"./class-map-6a9bd920.js";import"./index-562e1433.js";import"./ThemingParameters-7e2e4e30.js";import"./TableOfContent-291ab24f.js";import"./index-b1fb09ae.js";import"./Link-12d36fdb.js";import"./WrappingType-b81e595a.js";import"./index-c0c4d891.js";import"./Label-54923333.js";import"./index-7b70a3fd.js";import"./Popover-046c0918.js";import"./Integer-f7f172c9.js";import"./PopupUtils-2e0c5ac0.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-41f002e2.js";import"./FocusableElements-d8460590.js";import"./isElementHidden-01c07146.js";import"./ResizeHandler-267f9e5d.js";import"./MediaRange-25b98f31.js";import"./style-map-0c64293c.js";const R=`## Usage Guidelines
+
+- If the text is longer than the width of the component, it doesn’t wrap, it shows ellipsis.
+- When truncated, the full text is not visible, therefore, it’s recommended to make more space for longer texts to be fully displayed.
+- Colors are not semantic and have no visual representation in High Contrast Black (sap_belize_hcb) theme.
+`;function f(r={}){const{wrapper:n}=Object.assign({},a(),r.components);return n?e.jsx(n,{...r,children:e.jsx(i,{})}):i();function i(){const t=Object.assign({h2:"h2",h1:"h1"},a(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(S,{title:"Data Display / Badge",component:o,argTypes:{children:{control:"text"},icon:{control:{disable:!0}}},args:{children:"Badge Text",icon:e.jsx(s,{name:"employee"})}}),`
+`,e.jsx(y,{since:"0.4.0"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(c,{children:e.jsx(l,{name:"Default",children:d=>e.jsx(o,{...d})})}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(j,{story:"Default"}),`
+`,e.jsx(B,{children:R}),`
+`,e.jsx(t.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(t.h2,{id:"all-available-badge-colors",children:"All available Badge colors"}),`
+`,e.jsx(c,{children:e.jsx(l,{name:"All colors",children:()=>e.jsxs(p,{wrap:h.Wrap,children:[e.jsx(o,{colorScheme:"1",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "1"'}),e.jsx(o,{colorScheme:"2",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "2"'}),e.jsx(o,{colorScheme:"3",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "3"'}),e.jsx(o,{colorScheme:"4",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "4"'}),e.jsx(o,{colorScheme:"5",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "5"'}),e.jsx(o,{colorScheme:"6",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "6"'}),e.jsx(o,{colorScheme:"7",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "7"'}),e.jsx(o,{colorScheme:"8",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "8"'}),e.jsx(o,{colorScheme:"9",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "9"'}),e.jsx(o,{colorScheme:"10",children:'colorScheme = "10"'})]})})}),`
+`,e.jsx(u,{})]})}}const g=r=>e.jsx(o,{...r});g.storyName="Default";g.parameters={storySource:{source:`args => {
+  return <Badge {...args} />;
+}`}};const x=()=>e.jsxs(p,{wrap:h.Wrap,children:[e.jsx(o,{colorScheme:"1",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "1"'}),e.jsx(o,{colorScheme:"2",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "2"'}),e.jsx(o,{colorScheme:"3",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "3"'}),e.jsx(o,{colorScheme:"4",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "4"'}),e.jsx(o,{colorScheme:"5",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "5"'}),e.jsx(o,{colorScheme:"6",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "6"'}),e.jsx(o,{colorScheme:"7",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "7"'}),e.jsx(o,{colorScheme:"8",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "8"'}),e.jsx(o,{colorScheme:"9",style:{marginRight:"10px",marginBottom:"10px"},children:'colorScheme = "9"'}),e.jsx(o,{colorScheme:"10",children:'colorScheme = "10"'})]});x.storyName="All colors";x.parameters={storySource:{source:`() => {
+  return <FlexBox wrap={FlexBoxWrap.Wrap}>
+          <Badge colorScheme="1" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "1"
+          </Badge>
+          <Badge colorScheme="2" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "2"
+          </Badge>
+          <Badge colorScheme="3" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "3"
+          </Badge>
+          <Badge colorScheme="4" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "4"
+          </Badge>
+          <Badge colorScheme="5" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "5"
+          </Badge>
+          <Badge colorScheme="6" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "6"
+          </Badge>
+          <Badge colorScheme="7" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "7"
+          </Badge>
+          <Badge colorScheme="8" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "8"
+          </Badge>
+          <Badge colorScheme="9" style={{
+      marginRight: "10px",
+      marginBottom: "10px"
+    }}>
+            colorScheme = "9"
+          </Badge>
+          <Badge colorScheme="10">colorScheme = "10"</Badge>
+        </FlexBox>;
+}`}};const m={title:"Data Display / Badge",component:o,args:{children:"Badge Text",icon:e.jsx(s,{name:"employee"})},argTypes:{children:{control:"text"},icon:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory","allColors"]};m.parameters=m.parameters||{};m.parameters.docs={...m.parameters.docs||{},page:f};const _e=["defaultStory","allColors"];export{_e as __namedExportsOrder,x as allColors,m as default,g as defaultStory};
+//# sourceMappingURL=Badge.stories-12ed935b.js.map
---
+++ main/assets/MediaGallery-980e729a.js
@@ -0,0 +1,28 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{C as p,A as d}from"./DomRefTable.module-001ee1c3.js";import{D as l}from"./DocsHeader-09494c78.js";import{F as h}from"./Footer-f0841fbb.js";import"./index-ebeaab24.js";import{M as c,C as i,a as r}from"./chunk-PCJTTTQV-063e2a8c.js";import{S as u}from"./SubcomponentsSection-6138f2fc.js";import{C as x,D as a,a as f,I as j,M as g}from"./MediaGallery.stories-79c6ec62.js";import{u as s}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-e33c04d0.js";import"./iframe-403ac14f.js";import"../sb-preview/runtime.js";import"./UI5Element-2e105c8a.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7b9bd1ee.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-f65ad4fd.js";import"./Icons-1a294222.js";import"./decline-bfe69317.js";import"./i18n-defaults-80781f7e.js";import"./information-bd54c92c.js";import"./class-map-6a9bd920.js";import"./Button-77ada2e8.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-208919c6.js";import"./i18n-defaults-fca59c5d.js";import"./index-b9e92427.js";import"./clsx.m-1229b3e0.js";import"./index-562e1433.js";import"./ThemingParameters-7e2e4e30.js";import"./TableOfContent-291ab24f.js";import"./index-b1fb09ae.js";import"./Link-12d36fdb.js";import"./WrappingType-b81e595a.js";import"./index-c0c4d891.js";import"./Label-54923333.js";import"./index-9c09ad76.js";import"./index-7b70a3fd.js";import"./Popover-046c0918.js";import"./Integer-f7f172c9.js";import"./PopupUtils-2e0c5ac0.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-41f002e2.js";import"./FocusableElements-d8460590.js";import"./isElementHidden-01c07146.js";import"./ResizeHandler-267f9e5d.js";import"./MediaRange-25b98f31.js";import"./style-map-0c64293c.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./parameters-bundle.css-5626463c.js";import"./index-c76b0f43.js";import"./ItemNavigation-897757d1.js";import"./Carousel-a053d566.js";import"./ScrollEnablement-4353003c.js";import"./animate-1a16ba20.js";import"./AnimationMode-c3f8be94.js";import"./slim-arrow-left-d8dfdc4a.js";import"./slim-arrow-right-56a3c652.js";const y="**Since:** 0.21.0\n\nThe `MediaGalleryItem` web component represents the items displayed in the `MediaGallery` web component.\n\n**Note:** `MediaGalleryItem` is not supported when used outside of `MediaGallery`.\n\n### Keyboard Handling\n\nThe `MediaGallery` provides advanced keyboard handling. When focused, the user can use the following keyboard shortcuts in order to perform a navigation:\n\n- \\[SPACE/ENTER/RETURN\\] - Trigger `undefined` event\n",M=`## Keyboard Handling
+
+The \`MediaGallery\` provides advanced keyboard handling.  
+When the thumbnails menu is focused the following keyboard shortcuts allow the user to navigate through the thumbnail items:
+
+- \\[UP/DOWN\\] - Navigates up and down the items
+- \\[HOME\\] - Navigates to first item
+- \\[END\\] - Navigates to the last item
+- \\[SPACE/ENTER\\] - Select an item
+`;function Ht(e={}){const{wrapper:n}=Object.assign({},s(),e.components);return n?t.jsx(n,Object.assign({},e,{children:t.jsx(m,{})})):m();function m(){const o=Object.assign({h2:"h2",h3:"h3"},s(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(c,{of:x}),`
+`,t.jsx(l,{since:"0.21.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(i,{of:a}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{of:a}),`
+`,t.jsx(r,{children:M}),`
+`,t.jsx(o.h2,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(o.h3,{id:"custom-thumbnail-image",children:"Custom Thumbnail Image"}),`
+`,t.jsx(i,{of:f}),`
+`,t.jsx(o.h3,{id:"interactive-display-area",children:"Interactive Display Area"}),`
+`,t.jsx(i,{of:j}),`
+`,t.jsx(r,{children:u}),`
+`,t.jsx(o.h2,{id:"mediagalleryitem",children:"MediaGalleryItem"}),`
+`,t.jsx(r,{children:y}),`
+`,t.jsx(d,{of:g}),`
+`,t.jsx(h,{})]})}}export{Ht as default};
+//# sourceMappingURL=MediaGallery-980e729a.js.map
