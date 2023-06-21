fc99db93303c6d3b025fd30cf0e566b20ca2579e
Lukas Harbarth <lukas.harbarth@sap.com>
2023-06-21 15:34:52 +0200

    Update Footer.tsx

--- .storybook/components/Footer.tsx
+++ .storybook/components/Footer.tsx
@@ -35,7 +35,7 @@ export const Footer = ({ style }) => {
           <FlexBox alignItems={FlexBoxAlignItems.Center} wrap={FlexBoxWrap.Wrap}>
             <img src={BestRunLogo} alt="SAP Logo with Text 'The Best Run'" style={{ height: '1.5rem' }} />
             <Label wrappingType={WrappingType.Normal}>
-              ©&zwnj;&#8205; Copyright {new Date().getFullYear()}, SAP SE and UI5 Web Components for React Contributors
+              &copy; Copyright {new Date().getFullYear()}, SAP SE and UI5 Web Components for React Contributors
             </Label>
           </FlexBox>
           <FlexBox alignItems={FlexBoxAlignItems.Center} wrap={FlexBoxWrap.Wrap}>
