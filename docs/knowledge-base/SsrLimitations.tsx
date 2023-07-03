0d8298e5aa4dc0c0c047a72ab90f6e29cff2d937
Marcus Notheis <marcus.notheis@sap.com>
2023-07-03 18:07:24 +0200

    docs: add section for Next.js (#4823)

---
+++ docs/knowledge-base/SsrLimitations.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { MessageStrip, MessageStripDesign } from '@ui5/webcomponents-react';
+
+export function SsrLimitations() {
+  return (
+    <MessageStrip design={MessageStripDesign.Information} hideCloseButton>
+      <>
+        <strong>Known Limitations</strong>
+        <ul>
+          <li>
+            As custom elements need to be defined on the client, UI5 Web Components for React is only rendering the
+            outer markup of the UI5 Web Component on the server. The rendering of the Web Component is still happening
+            on the client.
+          </li>
+          <li>
+            The Next.js <code>app</code> directory shows some hydration warnings on the client. These warnings seem not
+            to affect the runtime.
+          </li>
+        </ul>
+      </>
+    </MessageStrip>
+  );
+}
