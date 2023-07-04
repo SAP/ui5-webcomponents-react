69235bef23173763fdf7897ea15f6ee7aa1a00c0
Lukas742 <lukas.harbarth@sap.com>
2023-07-04 16:01:09 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@bdfae8061bf010b93b00047eba5e4fe7c12f621d 🚀

---
+++ main/assets/Switch.stories-e6b5e86a.js
@@ -0,0 +1,25 @@
+import{M as c,C as d,f as l,a as h}from"./chunk-PCJTTTQV-c4898a76.js";import{S as o}from"./index-7022aa4c.js";import{e as x}from"./DomRefTable.module-47e7c9f8.js";import{D as S}from"./DocsHeader-e1b6d091.js";import{F as u}from"./Footer-b4f4233c.js";import{j as t}from"./jsx-runtime-5926aa06.js";import"./index-ebeaab24.js";import{u as n}from"./index-bda0bad7.js";import"./iframe-e00e8752.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./UI5Element-3b11b083.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./Icon-9aa64f88.js";import"./Icons-f64cf5a7.js";import"./Device-99b8bbf4.js";import"./AriaLabelHelper-43a261ec.js";import"./accept-1b3f8b1f.js";import"./decline-5f59d729.js";import"./i18n-defaults-80781f7e.js";import"./less-83fa0752.js";import"./class-map-18f572ce.js";import"./react-jss.esm-022ab528.js";import"./index-d16d0a43.js";import"./slot-76e48863.js";import"./information-73174d4b.js";import"./Button-86524f67.js";import"./MarkedEvents-b83081e9.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-cbae0ba3.js";import"./index-a6a0b27e.js";import"./Integer-f7f172c9.js";import"./index-4d202aba.js";import"./Avatar-d2efa509.js";import"./ResizeHandler-13af62ac.js";import"./employee-ef49f0ae.js";import"./index-f5b7c844.js";import"./index-e4c4dbee.js";import"./Link-d41264a5.js";import"./WrappingType-b81e595a.js";import"./index-4becd987.js";import"./TableOfContent-848ae268.js";import"./index-9d7d1161.js";import"./Label-9108dd9e.js";import"./index-50a6fdf4.js";import"./index-9dbc5624.js";import"./Popover-76648ec8.js";import"./PopupsCommon.css-7ddbcd9a.js";import"./FocusableElements-baf79594.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-cca56078.js";const f=`## Keyboard Handling
+
+The state can be changed by pressing the Space and Enter keys.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Switch\` exposes the following CSS Shadow Parts:
+
+- slider - Used to style the track, where the handle is being slid
+- text-on - Used to style the \`textOn\` property text
+- text-off - Used to style the \`textOff\` property text
+- handle - Used to style the handle of the switch
+`;function j(r={}){const{wrapper:i}=Object.assign({},n(),r.components);return i?t.jsx(i,{...r,children:t.jsx(p,{})}):p();function p(){const m=Object.assign({h2:"h2"},n(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(c,{title:"Inputs / Switch",component:o}),`
+`,t.jsx(S,{since:"0.4.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(m.h2,{id:"example",children:"Example"}),`
+`,t.jsx(d,{children:t.jsx(l,{name:"Default",children:a=>t.jsx(o,{...a})})}),`
+`,t.jsx(m.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(x,{story:"Default"}),`
+`,t.jsx(h,{children:f}),`
+`,t.jsx(u,{})]})}}const s=r=>t.jsx(o,{...r});s.storyName="Default";s.parameters={storySource:{source:`args => {
+  return <Switch {...args} />;
+}`}};const e={title:"Inputs / Switch",component:o,tags:["stories-mdx"],includeStories:["defaultStory"]};e.parameters=e.parameters||{};e.parameters.docs={...e.parameters.docs||{},page:j};const Ft=["defaultStory"];export{Ft as __namedExportsOrder,e as default,s as defaultStory};
+//# sourceMappingURL=Switch.stories-e6b5e86a.js.map
