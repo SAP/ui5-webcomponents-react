a38d806c54a8c6e7ee84682d0825064a13f09aad
Lukas742 <lukas.harbarth@sap.com>
2023-07-05 08:39:56 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@38c192fdcf8fc98eacdc49e7b8ca00a3a9977206 🚀

---
+++ main/assets/BusyIndicator.stories-25b5d3cf.js
@@ -0,0 +1,43 @@
+import{M as y,C as s,f as d,a as x}from"./chunk-PCJTTTQV-1f9e74ae.js";import{B as r}from"./index-85847133.js";import{a as c}from"./BusyIndicator-9bf9c132.js";import{S as l}from"./index-a65770b3.js";import{T as j}from"./index-562e1433.js";import{e as g}from"./DomRefTable.module-658124fb.js";import{D as b}from"./DocsHeader-d9588da2.js";import{F as f}from"./Footer-2da8922d.js";import{j as t}from"./jsx-runtime-5926aa06.js";import"./index-ebeaab24.js";import{u}from"./index-bda0bad7.js";import"./iframe-9bc9f51a.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./UI5Element-3b11b083.js";import"./Icons-f64cf5a7.js";import"./Integer-f7f172c9.js";import"./Label-9108dd9e.js";import"./WrappingType-b81e595a.js";import"./i18n-defaults-f002f496.js";import"./class-map-18f572ce.js";import"./Icon-9aa64f88.js";import"./slot-76e48863.js";import"./InvisibleMessage-941c3ff6.js";import"./AriaLabelHelper-43a261ec.js";import"./ValueState-2c5e5904.js";import"./slim-arrow-down-b9e7c653.js";import"./information-73174d4b.js";import"./i18n-defaults-80781f7e.js";import"./Device-99b8bbf4.js";import"./decline-5f59d729.js";import"./ListItemBase-9b88c288.js";import"./ResizeHandler-13af62ac.js";import"./ItemNavigation-4baedd26.js";import"./TabbableElements-8e49b367.js";import"./isElementHidden-01c07146.js";import"./debounce-9c2fb7dd.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-cca56078.js";import"./ResponsivePopover-00060bb5.js";import"./Popover-76648ec8.js";import"./PopupsCommon.css-7ddbcd9a.js";import"./FocusableElements-baf79594.js";import"./MediaRange-25b98f31.js";import"./Dialog-08b725c0.js";import"./Button-86524f67.js";import"./MarkedEvents-b83081e9.js";import"./Title-c6699a53.js";import"./StandardListItem-6c956409.js";import"./ListItem-d1dee57e.js";import"./RadioButton-c3da5c0b.js";import"./CheckBox-4c6776c5.js";import"./accept-1b3f8b1f.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-82b1b6d3.js";import"./Avatar-d2efa509.js";import"./employee-ef49f0ae.js";import"./ResponsivePopoverCommon.css-2fa8fc87.js";import"./ValueStateMessage.css-559bc063.js";import"./ValueState-ab6838cc.js";import"./clsx.m-1229b3e0.js";import"./react-jss.esm-022ab528.js";import"./ThemingParameters-7e2e4e30.js";import"./index-3a80ab34.js";import"./addCustomCSSWithScoping-cbae0ba3.js";import"./index-fab86bf3.js";import"./index-e73c5bd8.js";import"./index-a0fe43fb.js";import"./index-f40a248e.js";import"./Link-d41264a5.js";import"./index-ea625d40.js";import"./TableOfContent-23ce6271.js";import"./index-7954a632.js";import"./index-9686d3d0.js";import"./index-cf3946c3.js";const v=`## Usage
+
+For the \`BusyIndicator\` you can define the size, the text and whether it is shown or hidden. In order to hide it, use the "active" property.
+
+In order to show busy state over an HTML element, simply nest the HTML element in a \`BusyIndicator\` instance.  
+**Note:** Since \`BusyIndicator\` has \`display: inline-block;\` by default and no width of its own, whenever you need to wrap a block-level element, you should set \`display: block\` to the busy indicator as well.
+
+### When to use:
+
+- The user needs to be able to cancel the operation.
+- Only part of the application or a particular component is affected.
+
+### When not to use:
+
+- The operation takes less than one second.
+- You need to block the screen and prevent the user from starting another activity.
+- Do not show multiple busy indicators at once.
+`;function S(e={}){const{wrapper:m}=Object.assign({},u(),e.components);return m?t.jsx(m,{...e,children:t.jsx(p,{})}):p();function p(){const o=Object.assign({h2:"h2",h1:"h1",p:"p"},u(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(y,{title:"User Feedback / BusyIndicator",component:r,argTypes:{children:{control:{disable:!0}}},args:{size:c.Medium,delay:1e3,active:!0}}),`
+`,t.jsx(b,{since:"0.4.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(s,{children:t.jsx(d,{name:"Default",children:n=>t.jsx(r,{...n})})}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(g,{story:"Default"}),`
+`,t.jsx(x,{children:v}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"busyindicator-with-children",children:"BusyIndicator with children"}),`
+`,t.jsx(s,{children:t.jsx(d,{name:"With children",args:{active:!0},children:n=>t.jsx(r,{...n,children:t.jsx(l,{})})})}),`
+`,t.jsx(s,{mdxSource:'<BusyIndicator active><Text><p>{"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et\\ndolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita\\nkasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur\\nsadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam\\nvoluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata\\nsanctus est Lorem ipsum dolor sit amet."}</p></Text></BusyIndicator>',children:t.jsx(r,{active:!0,children:t.jsx(j,{children:t.jsx(o.p,{children:`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+sanctus est Lorem ipsum dolor sit amet.`})})})}),`
+`,t.jsx(f,{})]})}}const h=e=>t.jsx(r,{...e});h.storyName="Default";h.parameters={storySource:{source:`args => {
+  return <BusyIndicator {...args} />;
+}`}};const a=e=>t.jsx(r,{...e,children:t.jsx(l,{})});a.storyName="With children";a.args={active:!0};a.parameters={storySource:{source:`args => {
+  return <BusyIndicator {...args}>
+          <Select />
+        </BusyIndicator>;
+}`}};const i={title:"User Feedback / BusyIndicator",component:r,args:{size:c.Medium,delay:1e3,active:!0},argTypes:{children:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory","withChildren"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:S};const oe=["defaultStory","withChildren"];export{oe as __namedExportsOrder,i as default,h as defaultStory,a as withChildren};
+//# sourceMappingURL=BusyIndicator.stories-25b5d3cf.js.map
---
+++ main/assets/Dialog-c943e635.js
@@ -0,0 +1,103 @@
+import{j as o}from"./jsx-runtime-5926aa06.js";import{C as a}from"./DomRefTable.module-658124fb.js";import{D as l}from"./DocsHeader-d9588da2.js";import{F as c}from"./Footer-2da8922d.js";import"./index-ebeaab24.js";import{D as m}from"./DomRefTable-ae00ecbc.js";import{M as d,C as r,a as h}from"./chunk-PCJTTTQV-1f9e74ae.js";import{C as g,D as s,W as u}from"./Dialog.stories-a033def1.js";import{D as f}from"./DialogDomRef-ffa73df1.js";import{u as p}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-3a80ab34.js";import"./iframe-9bc9f51a.js";import"../sb-preview/runtime.js";import"./UI5Element-3b11b083.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-9aa64f88.js";import"./Icons-f64cf5a7.js";import"./decline-5f59d729.js";import"./i18n-defaults-80781f7e.js";import"./information-73174d4b.js";import"./class-map-18f572ce.js";import"./Button-86524f67.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-99b8bbf4.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-cbae0ba3.js";import"./index-fab86bf3.js";import"./Integer-f7f172c9.js";import"./index-e73c5bd8.js";import"./Avatar-d2efa509.js";import"./ResizeHandler-13af62ac.js";import"./employee-ef49f0ae.js";import"./index-a0fe43fb.js";import"./index-f40a248e.js";import"./Link-d41264a5.js";import"./WrappingType-b81e595a.js";import"./index-ea625d40.js";import"./TableOfContent-23ce6271.js";import"./index-7954a632.js";import"./Label-9108dd9e.js";import"./index-9c09ad76.js";import"./index-9686d3d0.js";import"./index-cf3946c3.js";import"./Popover-76648ec8.js";import"./PopupsCommon.css-7ddbcd9a.js";import"./FocusableElements-baf79594.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-cca56078.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./settings-ba792ad3.js";import"./CodeGen-0dd6906b.js";import"./index-df847ac7.js";import"./Dialog-08b725c0.js";import"./ValueState-2c5e5904.js";import"./ValueState-ab6838cc.js";import"./index-ceed68d2.js";import"./Bar-ae5ccc94.js";import"./parameters-bundle.css-f9dc8928.js";import"./index-ddbf79a3.js";import"./index-a5bdc9e2.js";import"./Title-c6699a53.js";import"./index-b198f971.js";import"./ListItemBase-9b88c288.js";import"./ItemNavigation-4baedd26.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-9bf9c132.js";import"./index-e5daf64f.js";import"./StandardListItem-6c956409.js";import"./ListItem-d1dee57e.js";import"./RadioButton-c3da5c0b.js";import"./CheckBox-4c6776c5.js";import"./accept-1b3f8b1f.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-82b1b6d3.js";const x=`## Structure
+
+A \`Dialog\` consists of a header, content, and a footer for action buttons. The \`Dialog\` is usually displayed at the center of the screen. Its position can be changed by the user. To enable this, you need to set the property \`draggable\` accordingly.
+
+## Responsive Behavior
+
+The \`stretch\` property can be used to stretch the \`Dialog\` on full screen.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Dialog\` exposes the following CSS Shadow Parts:
+
+- header - Used to style the header of the component
+- content - Used to style the content of the component
+- footer - Used to style the footer of the component
+`;function sn(e={}){const{wrapper:t}=Object.assign({},p(),e.components);return t?o.jsx(t,Object.assign({},e,{children:o.jsx(i,{})})):i();function i(){const n=Object.assign({h2:"h2",h4:"h4",p:"p",code:"code",pre:"pre",strong:"strong",a:"a",h1:"h1"},p(),e.components);return o.jsxs(o.Fragment,{children:[o.jsx(d,{of:g}),`
+`,o.jsx(l,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(n.h2,{id:"example",children:"Example"}),`
+`,o.jsx(r,{of:s}),`
+`,o.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{of:s}),`
+`,o.jsx(h,{children:x}),`
+`,o.jsx(m,{rows:f}),`
+`,o.jsx(n.h2,{id:"opening-dialogs",children:"Opening Dialogs"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(n.h4,{id:"since-0220",children:"since 0.22.0"}),`
+`,o.jsxs(n.p,{children:["We recommend opening and closing the ",o.jsx(n.code,{children:"Dialog"})," component in a declarative way using the ",o.jsx(n.code,{children:"open"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`const MyComponentWithDialog = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+      >
+        Open Dialog
+      </Button>
+      <Dialog open={dialogIsOpen} />
+    </>
+  );
+};
+`})}),`
+`,o.jsx(n.h4,{id:"before-0220",children:"before 0.22.0"}),`
+`,o.jsxs(n.p,{children:[o.jsx(n.code,{children:"Dialogs"})," can be opened by attaching a ",o.jsx(n.code,{children:"ref"})," to the component and then calling the corresponding ",o.jsx(n.strong,{children:o.jsx(n.code,{children:"show"})})," method."]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Dialog</Button>
+      <Dialog ref={dialogRef}>Some Content</Dialog>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(n.h2,{id:"using-dialogs-inside-other-components",children:"Using Dialogs inside other components"}),`
+`,o.jsxs(n.p,{children:[o.jsx(n.code,{children:"Dialogs"}),` are often used within other components, when opened this could sometimes have unwanted side effects.
+In this case, we recommend using `,o.jsx(n.a,{href:"https://reactjs.org/docs/portals.html",target:"_blank",rel:"nofollow noopener noreferrer",children:"createPortal"})," to mount the ",o.jsx(n.code,{children:"Dialog"})," outside of the DOM hierarchy of the parent component."]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Dialog</Button>
+      {createPortal(<Dialog ref={dialogRef} />, document.body)}
+    </>
+  );
+};
+`})}),`
+`,o.jsx(n.h2,{id:"closing-dialogs",children:"Closing Dialogs"}),`
+`,o.jsxs(n.p,{children:["Closing ",o.jsx(n.code,{children:"Dialogs"}),` works in the same way as opening them.
+You can either set the `,o.jsx(n.code,{children:"open"})," prop to ",o.jsx(n.code,{children:"false"})," or attaching a ",o.jsx(n.code,{children:"ref"})," on which the corresponding ",o.jsx(n.code,{children:"close"})," method is called."]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const handleOpen = (e) => {
+    dialogRef.current.show();
+  };
+  const handleClose = () => {
+    dialogRef.current.close();
+  };
+  return (
+    <>
+      <Button onClick={handleOpen}>Open Dialog</Button>
+      <Dialog ref={dialogRef}>
+        <Button onClick={handleClose}>Close Dialog</Button>
+      </Dialog>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(n.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(n.h2,{id:"dialog-with-content",children:"Dialog with content"}),`
+`,o.jsx(r,{of:u}),`
+`,o.jsx(c,{})]})}}export{sn as default};
+//# sourceMappingURL=Dialog-c943e635.js.map
