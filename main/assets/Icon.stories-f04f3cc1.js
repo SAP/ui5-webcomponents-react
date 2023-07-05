6960a18ef7f49d060a2e77a79db6dc7e23aa62b2
Lukas742 <lukas.harbarth@sap.com>
2023-07-05 13:21:07 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@2ba1688d830aa32aad767b8dd50d13c5677aa99c 🚀

---
+++ main/assets/Icon.stories-f04f3cc1.js
@@ -0,0 +1,59 @@
+import{M as a,C as l,f as u,a as d}from"./chunk-PCJTTTQV-e7ce6a9b.js";import{I as n}from"./index-796dae47.js";import"./employee-ef49f0ae.js";import{e as h}from"./DomRefTable.module-73c381bd.js";import{D as b}from"./DocsHeader-bca9f6b8.js";import{F as w}from"./Footer-c49f3619.js";import{j as e}from"./jsx-runtime-5926aa06.js";import"./index-ebeaab24.js";import{u as p}from"./index-bda0bad7.js";import"./iframe-4a3de5f2.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./Icon-9aa64f88.js";import"./UI5Element-3b11b083.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./Icons-f64cf5a7.js";import"./react-jss.esm-022ab528.js";import"./index-932af117.js";import"./slot-76e48863.js";import"./decline-5f59d729.js";import"./i18n-defaults-80781f7e.js";import"./information-73174d4b.js";import"./class-map-18f572ce.js";import"./Button-86524f67.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-99b8bbf4.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-cbae0ba3.js";import"./index-f1c4490b.js";import"./Integer-f7f172c9.js";import"./index-27db9d45.js";import"./Avatar-d2efa509.js";import"./ResizeHandler-13af62ac.js";import"./index-1c14d18a.js";import"./index-800f2281.js";import"./Link-d41264a5.js";import"./WrappingType-b81e595a.js";import"./index-2c77de22.js";import"./TableOfContent-c623b1a9.js";import"./index-3d9ee262.js";import"./Label-9108dd9e.js";import"./index-9fe19904.js";import"./index-08ca53e3.js";import"./Popover-76648ec8.js";import"./PopupsCommon.css-7ddbcd9a.js";import"./FocusableElements-baf79594.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-cca56078.js";const g=`## Usage
+
+1\\. **Get familiar with the icons collections.**  
+Before displaying an icon, you need to explore the icons collections to find and import the desired icon.  
+Currently there are 3 icons collection, available as 3 npm packages:
+
+- <ui5-link target="_blank" href="https://www.npmjs.com/package/@ui5/webcomponents-icons">@ui5/webcomponents-icons</ui5-link> represents the "SAP-icons" collection and includes the following <ui5-link target="_blank" href="https://sdk.openui5.org/test-resources/sap/m/demokit/iconExplorer/webapp/index.html#/overview/SAP-icons">icons</ui5-link>.
+- <ui5-link target="_blank" href="https://www.npmjs.com/package/@ui5/webcomponents-icons-tnt">@ui5/webcomponents-icons-tnt</ui5-link> represents the "tnt" collection and includes the following <ui5-link target="_blank" href="https://sdk.openui5.org/test-resources/sap/m/demokit/iconExplorer/webapp/index.html#/overview/SAP-icons-TNT">icons</ui5-link>.
+- <ui5-link target="_blank" href="https://www.npmjs.com/package/@ui5/webcomponents-icons-business-suite">@ui5/webcomponents-icons-icons-business-suite</ui5-link> represents the "business-suite" collection and includes the following <ui5-link target="_blank" href="https://ui5.sap.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html#/overview/BusinessSuiteInAppSymbols">icons</ui5-link>.
+
+2\\. **After exploring the icons collections, add one or more of the packages as dependencies to your project.**  
+\`npm i @ui5/webcomponents-icons\`  
+\`npm i @ui5/webcomponents-icons-tnt\`  
+\`npm i @ui5/webcomponents-icons-business-suite\`
+
+3\\. **Then, import the desired icon**.  
+\`import "@ui5/{package_name}/dist/{icon_name}.js";\`
+
+**For Example**:  
+For the standard "SAP-icons" icon collection, import an icon from the \`@ui5/webcomponents-icons\` package:  
+\`import "@ui5/webcomponents-icons/dist/employee.js";\`
+
+For the "tnt" (SAP Fiori Tools) icon collection, import an icon from the \`@ui5/webcomponents-icons-tnt\` package:  
+\`import "@ui5/webcomponents-icons-tnt/dist/antenna.js";\`
+
+For the "business-suite" (SAP Business Suite) icon collection, import an icon from the \`@ui5/webcomponents-icons-business-suite\` package:  
+\`import "@ui5/webcomponents-icons-business-suite/dist/ab-testing.js";\`
+
+4\\. **Display the icon using the \`Icon\` web component.**  
+Set the icon collection ("SAP-icons", "tnt" or "business-suite" - "SAP-icons" is the default icon collection and can be skipped)  
+and the icon name to the \`name\` property.
+
+\`<Icon name="employee"></Icon>\`  
+\`<Icon name="tnt/antenna"></Icon>\`  
+\`<Icon name="business-suite/ab-testing"></Icon>\`
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Icon\` exposes the following CSS Shadow Parts:
+
+- root - Used to style the outermost wrapper of the \`Icon\`
+
+## Keyboard Handling
+
+- \\[SPACE, ENTER, RETURN\\] - Fires the \`click\` event if the \`interactive\` property is set to true.
+- \\[SHIFT\\] - If \\[SPACE\\] or \\[ENTER\\],\\[RETURN\\] is pressed, pressing \\[SHIFT\\] releases the Icon without triggering the click event.
+`;function f(o={}){const{wrapper:i}=Object.assign({},p(),o.components);return i?e.jsx(i,{...o,children:e.jsx(s,{})}):s();function s(){const r=Object.assign({h2:"h2"},p(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Data Display / Icon",component:n,args:{name:"employee"}}),`
+`,e.jsx(b,{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(r.h2,{id:"example",children:"Example"}),`
+`,e.jsx(l,{children:e.jsx(u,{name:"Default",children:c=>e.jsx(n,{...c})})}),`
+`,e.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(h,{story:"Default"}),`
+`,e.jsx(d,{children:g}),`
+`,e.jsx(w,{})]})}}const m=o=>e.jsx(n,{...o});m.storyName="Default";m.parameters={storySource:{source:`args => {
+  return <Icon {...args} />;
+}`}};const t={title:"Data Display / Icon",component:n,args:{name:"employee"},tags:["stories-mdx"],includeStories:["defaultStory"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:f};const Ce=["defaultStory"];export{Ce as __namedExportsOrder,t as default,m as defaultStory};
+//# sourceMappingURL=Icon.stories-f04f3cc1.js.map
---
+++ main/assets/RadarChart-4d1ab0c3.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-5926aa06.js";import{C as e}from"./DomRefTable.module-73c381bd.js";import{D as a}from"./DocsHeader-bca9f6b8.js";import{F as l}from"./Footer-c49f3619.js";import"./index-ebeaab24.js";import{M as h,C as r}from"./chunk-PCJTTTQV-e7ce6a9b.js";import{C as x,D as s,W as c,a as d,P as j,L as f}from"./RadarChart.stories-4b0fb1ac.js";import{u as n}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-932af117.js";import"./iframe-4a3de5f2.js";import"../sb-preview/runtime.js";import"./UI5Element-3b11b083.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-9aa64f88.js";import"./Icons-f64cf5a7.js";import"./decline-5f59d729.js";import"./i18n-defaults-80781f7e.js";import"./information-73174d4b.js";import"./class-map-18f572ce.js";import"./Button-86524f67.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-99b8bbf4.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-cbae0ba3.js";import"./index-f1c4490b.js";import"./Integer-f7f172c9.js";import"./index-27db9d45.js";import"./Avatar-d2efa509.js";import"./ResizeHandler-13af62ac.js";import"./employee-ef49f0ae.js";import"./index-1c14d18a.js";import"./index-800f2281.js";import"./Link-d41264a5.js";import"./WrappingType-b81e595a.js";import"./index-2c77de22.js";import"./TableOfContent-c623b1a9.js";import"./index-3d9ee262.js";import"./Label-9108dd9e.js";import"./index-9c09ad76.js";import"./index-9fe19904.js";import"./index-08ca53e3.js";import"./Popover-76648ec8.js";import"./PopupsCommon.css-7ddbcd9a.js";import"./FocusableElements-baf79594.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-cca56078.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./DemoProps-9cd0a46b.js";import"./index-99d317a4.js";import"./ChartDataLabel-b8f14c25.js";import"./useTooltipFormatter-facce8a8.js";import"./defaults-573f0842.js";import"./ChartContainer-6fead8d0.js";import"./index-7e5033ff.js";import"./i18n-defaults-ac3ba251.js";import"./I18nContext-cdb50611.js";import"./throttle-6f27c407.js";import"./isString-0886f00f.js";import"./generateCategoricalChart-1361d668.js";import"./index-f50b85d6.js";import"./staticProps-411b051b.js";import"./Placeholder-40b72fbf.js";import"./react-content-loader.es-627d673d.js";function Ko(i={}){const{wrapper:m}=Object.assign({},n(),i.components);return m?o.jsx(m,Object.assign({},i,{children:o.jsx(p,{})})):p();function p(){const t=Object.assign({h2:"h2",h3:"h3"},n(),i.components);return o.jsxs(o.Fragment,{children:[o.jsx(h,{of:x}),`
+`,o.jsx(a,{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(r,{of:s}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(e,{of:s}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,o.jsx(r,{of:c}),`
+`,o.jsx(t.h3,{id:"with-data-labels",children:"With Data Labels"}),`
+`,o.jsx(r,{of:d}),`
+`,o.jsx(t.h3,{id:"polygon",children:"Polygon"}),`
+`,o.jsx(r,{of:j}),`
+`,o.jsx(t.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(r,{of:f}),`
+`,o.jsx(l,{})]})}}export{Ko as default};
+//# sourceMappingURL=RadarChart-4d1ab0c3.js.map
