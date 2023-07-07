adcd47553e12c237298e5a6da20179215d811479
Lukas742 <lukas.harbarth@sap.com>
2023-07-07 07:30:12 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@594dde18a44216491f3fd04c1ca1cfde111018d4 🚀

---
+++ main/assets/DynamicPage-2b61d574.js
@@ -0,0 +1,67 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{C as h,A as p}from"./DomRefTable.module-50d08a76.js";import{D as l}from"./DocsHeader-e406c1dd.js";import{F as x}from"./Footer-0bf9c2cc.js";import{D as m,a as s}from"./index-edeb5c4d.js";import"./index-ebeaab24.js";import{M as g,C as i,a as j,e as a}from"./chunk-PCJTTTQV-39a8bddb.js";import{C as f,D as c,A as u,W as y,a as b,b as w,S as v}from"./DynamicPage.stories-65fb4c49.js";import{S as k}from"./SubcomponentsSection-6138f2fc.js";import{u as d}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-209484c2.js";import"./iframe-234822d3.js";import"../sb-preview/runtime.js";import"./UI5Element-3b11b083.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-9aa64f88.js";import"./Icons-f64cf5a7.js";import"./decline-5f59d729.js";import"./i18n-defaults-80781f7e.js";import"./information-73174d4b.js";import"./class-map-18f572ce.js";import"./Button-86524f67.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-99b8bbf4.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-cbae0ba3.js";import"./index-34085539.js";import"./Integer-f7f172c9.js";import"./index-d22b80d6.js";import"./Avatar-d2efa509.js";import"./ResizeHandler-13af62ac.js";import"./employee-ef49f0ae.js";import"./index-dd53cc7e.js";import"./index-4df208dc.js";import"./Link-d41264a5.js";import"./WrappingType-b81e595a.js";import"./index-2c77de22.js";import"./TableOfContent-92b920c5.js";import"./index-a1e987a7.js";import"./Label-9108dd9e.js";import"./index-9c09ad76.js";import"./index-1b3bb3ec.js";import"./index-15b5bfb5.js";import"./Popover-76648ec8.js";import"./PopupsCommon.css-7ddbcd9a.js";import"./FocusableElements-baf79594.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-cca56078.js";import"./slim-arrow-down-b9e7c653.js";import"./slim-arrow-up-4398d599.js";import"./index-99d317a4.js";import"./i18n-defaults-ac3ba251.js";import"./I18nContext-cdb50611.js";import"./index-619db08e.js";import"./ToggleButton-d910effc.js";import"./stopPropagation-da74d54e.js";import"./index-5bcdbb58.js";import"./overflow-3cf98349.js";import"./CssSizeVariables-cd649ef5.js";import"./CustomVariables-d3e297a0.js";import"./debounce-925f19e3.js";import"./index-10a44e5e.js";import"./Media-6b270b80.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./index-3ede0d7d.js";import"./ItemNavigation-4baedd26.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-9bf9c132.js";import"./CheckBox-4c6776c5.js";import"./ValueState-2c5e5904.js";import"./accept-1b3f8b1f.js";import"./utils-badfc99a.js";import"./navigation-down-arrow-e2606240.js";import"./index-3b3ce1fd.js";import"./GlobalStyleClasses-7209358d.js";import"./PageBackgroundDesign-7e89cd25.js";import"./index-cc5faf74.js";import"./index-2cd2b261.js";import"./ValueState-ab6838cc.js";import"./VariantItem-7be22c10.js";import"./UnableToLoad-8c1903ca.js";import"./IllustratedMessage-e1dffafd.js";import"./Title-c6699a53.js";import"./i18n-defaults-1f5a0024.js";import"./parameters-bundle.css-f9dc8928.js";import"./search-89855739.js";import"./index-f0b32d8a.js";import"./Bar-ae5ccc94.js";import"./index-be067634.js";import"./Dialog-08b725c0.js";import"./favorite-7c1044c8.js";import"./unfavorite-d629df80.js";import"./index-6a6166d8.js";import"./RadioButton-c3da5c0b.js";import"./index-7b40ad97.js";import"./index-0a7ac926.js";import"./Input-d4382bc9.js";import"./Suggestions.css-a555ea97.js";import"./ResponsivePopoverCommon.css-2fa8fc87.js";import"./ValueStateMessage.css-559bc063.js";import"./useIsomorphicId-4be7c5e3.js";import"./index-5185c5b3.js";import"./index-011e333d.js";import"./index-ad92a60f.js";import"./ResponsivePopover-00060bb5.js";import"./index-0e8b67b8.js";import"./ListItemBase-9b88c288.js";import"./index-28588c32.js";import"./StandardListItem-6c956409.js";import"./ListItem-d1dee57e.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-82b1b6d3.js";import"./index-ea47ce53.js";import"./index-6ee8f4a9.js";import"./index-7614d96f.js";import"./index-f41aaa5c.js";import"./slideUp-745debd3.js";import"./animate-1a16ba20.js";import"./AnimationMode-65ee5c83.js";function ii(r={}){const{wrapper:e}=Object.assign({},d(),r.components);return e?t.jsx(e,Object.assign({},r,{children:t.jsx(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h1:"h1",p:"p",strong:"strong",code:"code",ul:"ul",li:"li",h3:"h3",pre:"pre"},d(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(g,{of:f}),`
+`,t.jsx(l,{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(i,{of:c}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(h,{of:c}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(o.h2,{id:"always-show-header-content",children:"Always show Header Content"}),`
+`,t.jsx(i,{of:u}),`
+`,t.jsx(o.h2,{id:"variant-management-as-title",children:"Variant Management as Title"}),`
+`,t.jsxs(o.p,{children:[t.jsx(o.strong,{children:"Note:"})," To prevent the ",t.jsx(o.code,{children:"click"})," event of the ",t.jsx(o.code,{children:"VariantManagement"})," component from bubbling up and collapsing/expanding the ",t.jsx(o.code,{children:"DynamicPageHeader"}),", please call ",t.jsx(o.code,{children:"event.stopPropagation()"})," inside the ",t.jsx(o.code,{children:"onClick"})," callback of the ",t.jsx(o.code,{children:"VariantManagement"}),"."]}),`
+`,t.jsx(i,{of:y}),`
+`,t.jsx(o.h2,{id:"with-floating-footer",children:"With floating footer"}),`
+`,t.jsx(i,{of:b}),`
+`,t.jsx(o.h2,{id:"dynamicpage-with-custom-actions-and-navigationactions-overflow-button",children:"DynamicPage with custom actions and navigationActions overflow button"}),`
+`,t.jsxs(o.p,{children:["The ",t.jsx(o.code,{children:"DynamicPageTitle"})," offers two props (",t.jsx(o.code,{children:"actionsToolbarProps"})," and ",t.jsx(o.code,{children:"navigationActionsToolbarProps"}),") to configure the props of the ",t.jsx(o.code,{children:"actions"})," and ",t.jsx(o.code,{children:"navigationActions"}),` toolbars.
+With these objects it's possible to e.g. configure the overflow button displayed within each toolbar by setting the `,t.jsx(o.code,{children:"overflowButton"}),` prop.
+`,t.jsx(o.code,{children:"navigationActionsToolbarProps"})," only has an effect, if the ",t.jsx(o.code,{children:"navigationActions"})," toolbar is used. (width < 1280px)"]}),`
+`,t.jsx(i,{of:w}),`
+`,t.jsx(o.h2,{id:"with-sticky-sub-headers",children:"With sticky sub-headers"}),`
+`,t.jsxs(o.p,{children:["To display sticky sub-headers that line-up with the standard header of the ",t.jsx(o.code,{children:"DynamicPage"})," you're able to render ",t.jsx(o.code,{children:"children"}),` as function.
+There you receive an object as parameter that offers the following properties:`]}),`
+`,t.jsxs(o.ul,{children:[`
+`,t.jsxs(o.li,{children:[t.jsx(o.code,{children:"stickyHeaderHeight"}),": The height of the ",t.jsx(o.code,{children:"DynamicPage"})," header sections which will stick to the top of the page."]}),`
+`]}),`
+`,t.jsx(i,{of:v,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"code",children:"Code"}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`<DynamicPage {...props}>
+  {({ stickyHeaderHeight }) => (
+    <>
+      <div
+        style={{
+          position: 'sticky',
+          width: '100%',
+          height: '4rem',
+          background: 'lightgreen',
+          top: \`\${stickyHeaderHeight}px\`
+        }}
+      >
+        Sticky Header
+      </div>
+      <div style={{ width: '100%', background: 'orange', height: '10rem' }}>Content</div>
+      <div
+        style={{
+          position: 'sticky',
+          width: '100%',
+          height: '8rem',
+          background: 'lightgreen',
+          top: \`calc(\${stickyHeaderHeight}px + 4rem)\`
+        }}
+      >
+        Sticky Header 2
+      </div>
+      <div style={{ background: 'lightblue', height: '2000px', width: '100%' }}>Content</div>
+    </>
+  )}
+</DynamicPage>
+`})}),`
+`,t.jsx(j,{children:k}),`
+`,t.jsx(o.h2,{id:"dynamicpagetitle",children:"DynamicPageTitle"}),`
+`,t.jsx(a,{of:m}),`
+`,t.jsx(p,{of:m}),`
+`,t.jsx(o.h2,{id:"dynamicpageheader",children:"DynamicPageHeader"}),`
+`,t.jsx(a,{of:s}),`
+`,t.jsx(p,{of:s}),`
+`,t.jsx(x,{})]})}}export{ii as default};
+//# sourceMappingURL=DynamicPage-2b61d574.js.map
---
+++ main/assets/RadarChart-d6eb3618.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-5926aa06.js";import{C as e}from"./DomRefTable.module-50d08a76.js";import{D as a}from"./DocsHeader-e406c1dd.js";import{F as l}from"./Footer-0bf9c2cc.js";import"./index-ebeaab24.js";import{M as h,C as r}from"./chunk-PCJTTTQV-39a8bddb.js";import{C as x,D as s,W as c,a as d,P as j,L as f}from"./RadarChart.stories-5516bd3e.js";import{u as n}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-209484c2.js";import"./iframe-234822d3.js";import"../sb-preview/runtime.js";import"./UI5Element-3b11b083.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-9aa64f88.js";import"./Icons-f64cf5a7.js";import"./decline-5f59d729.js";import"./i18n-defaults-80781f7e.js";import"./information-73174d4b.js";import"./class-map-18f572ce.js";import"./Button-86524f67.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-99b8bbf4.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-cbae0ba3.js";import"./index-34085539.js";import"./Integer-f7f172c9.js";import"./index-d22b80d6.js";import"./Avatar-d2efa509.js";import"./ResizeHandler-13af62ac.js";import"./employee-ef49f0ae.js";import"./index-dd53cc7e.js";import"./index-4df208dc.js";import"./Link-d41264a5.js";import"./WrappingType-b81e595a.js";import"./index-2c77de22.js";import"./TableOfContent-92b920c5.js";import"./index-a1e987a7.js";import"./Label-9108dd9e.js";import"./index-9c09ad76.js";import"./index-1b3bb3ec.js";import"./index-15b5bfb5.js";import"./Popover-76648ec8.js";import"./PopupsCommon.css-7ddbcd9a.js";import"./FocusableElements-baf79594.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-cca56078.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./DemoProps-9cd0a46b.js";import"./index-99d317a4.js";import"./ChartDataLabel-8ef3d9cd.js";import"./useTooltipFormatter-a785ebb0.js";import"./defaults-5fa25f5b.js";import"./ChartContainer-6b7a36db.js";import"./index-7e5033ff.js";import"./i18n-defaults-ac3ba251.js";import"./I18nContext-cdb50611.js";import"./throttle-6f27c407.js";import"./isString-0886f00f.js";import"./generateCategoricalChart-34682a79.js";import"./index-f50b85d6.js";import"./staticProps-411b051b.js";import"./Placeholder-f1b6e93c.js";import"./react-content-loader.es-627d673d.js";function Ko(i={}){const{wrapper:m}=Object.assign({},n(),i.components);return m?o.jsx(m,Object.assign({},i,{children:o.jsx(p,{})})):p();function p(){const t=Object.assign({h2:"h2",h3:"h3"},n(),i.components);return o.jsxs(o.Fragment,{children:[o.jsx(h,{of:x}),`
+`,o.jsx(a,{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(r,{of:s}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(e,{of:s}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,o.jsx(r,{of:c}),`
+`,o.jsx(t.h3,{id:"with-data-labels",children:"With Data Labels"}),`
+`,o.jsx(r,{of:d}),`
+`,o.jsx(t.h3,{id:"polygon",children:"Polygon"}),`
+`,o.jsx(r,{of:j}),`
+`,o.jsx(t.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(r,{of:f}),`
+`,o.jsx(l,{})]})}}export{Ko as default};
+//# sourceMappingURL=RadarChart-d6eb3618.js.map
