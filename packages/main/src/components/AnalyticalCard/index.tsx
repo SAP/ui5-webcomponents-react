81f4200ce46b4d3777a80c98103ad3fcfd39b027
Lukas Harbarth <lukas.harbarth@sap.com>
2023-07-10 15:14:41 +0200

    docs(AnalyticalCard): mark component as deprecated (#4860)

--- packages/main/src/components/AnalyticalCard/index.tsx
+++ packages/main/src/components/AnalyticalCard/index.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useStylesheet } from '@ui5/webcomponents-react-base';
+import { deprecationNotice, useStylesheet } from '@ui5/webcomponents-react-base';
 import type { ReactNode } from 'react';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import type { CommonProps } from '../../interfaces/index.js';
 import type { CardDomRef } from '../../webComponents/index.js';
 import { Card } from '../../webComponents/index.js';
@@ -21,11 +21,20 @@ export interface AnalyticalCardPropTypes extends CommonProps {
 
 /**
  * The `AnalyticalCard` is mainly used for data visualization. It consists of two areas – a header area and a chart area with a visual representation of the data.<br />
+ *
+ * @deprecated This component is deprecated and will be removed with our next major release (v2.0.0)! Please use the [Card](https://sap.github.io/ui5-webcomponents-react/?path=/docs/data-display-card) instead.
  */
 const AnalyticalCard = forwardRef<CardDomRef, AnalyticalCardPropTypes>((props, ref) => {
   const { children, header, ...rest } = props;
 
   useStylesheet(styleData, AnalyticalCard.displayName);
+
+  useEffect(() => {
+    deprecationNotice(
+      'AnalyticalCard',
+      'This component is deprecated and will be removed with our next major release (v2.0.0)! Please use the Card component instead.'
+    );
+  }, []);
 
   return (
     <Card ref={ref} {...rest}>
--- packages/main/src/webComponents/Card/Card.stories.tsx
+++ packages/main/src/webComponents/Card/Card.stories.tsx
@@ -1,5 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import personIcon from '@ui5/webcomponents-icons/dist/person-placeholder.js';
+import { LineChart } from '@ui5/webcomponents-react-charts';
+import { AnalyticalCardHeader } from '../../components/AnalyticalCardHeader';
+import { NumericSideIndicator } from '../../components/NumericSideIndicator';
+import { DeviationIndicator, ValueColor } from '../../enums';
 import { CardHeader } from '../CardHeader';
 import { Icon } from '../Icon';
 import { List } from '../List';
@@ -38,6 +42,61 @@ export const Default: Story = {
           <StandardListItem description="Visual Designer">Elena Petrova</StandardListItem>
           <StandardListItem description="Quality Specialist">John Miller</StandardListItem>
         </List>
+      </Card>
+    );
+  }
+};
+
+const simpleDataSet = [
+  {
+    name: 'January',
+    users: 76
+  },
+  {
+    name: 'February',
+    users: 230
+  },
+  {
+    name: 'March',
+    users: 240
+  },
+  {
+    name: 'April',
+    users: 280
+  },
+  {
+    name: 'May',
+    users: 100
+  }
+];
+
+export const WithAnalyticalCardHeader = {
+  render: (args) => {
+    return (
+      <Card
+        header={
+          <AnalyticalCardHeader
+            titleText="Project Cloud Transformation"
+            subtitleText="Revenue"
+            unitOfMeasurement="EUR"
+            trend={DeviationIndicator.Down}
+            value="65.34"
+            state={ValueColor.Error}
+            scale="K"
+            description="Q1, 2018"
+          >
+            <NumericSideIndicator titleText="Target" number="100" unit="k" />
+            <NumericSideIndicator titleText="Deviation" number="34.7" unit="%" state={ValueColor.Critical} />
+          </AnalyticalCardHeader>
+        }
+      >
+        <LineChart
+          className="chromatic-ignore"
+          noLegend
+          dimensions={[{ accessor: 'name' }]}
+          measures={[{ accessor: 'users', formatter: (val) => `${val}k` }]}
+          dataset={simpleDataSet}
+        />
       </Card>
     );
   }
