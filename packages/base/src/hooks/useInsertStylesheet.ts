617afe6b42234b617a2f912a56c70e961bdcc948
Marcus Notheis <marcus.notheis@sap.com>
2023-07-10 10:21:15 +0200

    base: remove inserted style on unmount

--- packages/base/src/hooks/useInsertStylesheet.ts
+++ packages/base/src/hooks/useInsertStylesheet.ts
@@ -1,13 +1,17 @@
-import type { StyleData } from '@ui5/webcomponents-base/dist/ManagedStyles.js';
-import { createOrUpdateStyle } from '@ui5/webcomponents-base/dist/ManagedStyles.js';
+import type { StyleDataCSP } from '@ui5/webcomponents-base/dist/ManagedStyles.js';
+import { createOrUpdateStyle, removeStyle } from '@ui5/webcomponents-base/dist/ManagedStyles.js';
 import * as React from 'react';
 import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect.js';
 
 const canUseInsertionEffect = 'useInsertionEffect' in React;
 const useInsertionEffect = canUseInsertionEffect ? Reflect.get(React, 'useInsertionEffect') : useIsomorphicLayoutEffect;
 
-export function useInsertStylesheet(styles: StyleData, pkgName: string) {
+export function useInsertStylesheet(styles: StyleDataCSP, componentName: string) {
   useInsertionEffect(() => {
-    createOrUpdateStyle(styles, pkgName);
+    createOrUpdateStyle(styles, styles.packageName, componentName);
+
+    return () => {
+      removeStyle(styles.packageName, componentName);
+    };
   });
 }
