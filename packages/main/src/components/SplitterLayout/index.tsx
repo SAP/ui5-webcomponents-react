1885b8f81b8d633e5b0e20b845a1dd01af680e25
Lukas Harbarth <lukas.harbarth@sap.com>
2023-07-11 11:19:00 +0200

    run prettier

--- packages/main/src/components/SplitterLayout/index.tsx
+++ packages/main/src/components/SplitterLayout/index.tsx
@@ -80,12 +80,15 @@ const SplitterLayout = forwardRef<HTMLDivElement, SplitterLayoutPropTypes>((prop
     initialChildren.current = false;
   }, [children, options?.resetOnChildrenChange]);
 
-  useEffect(() => {
-    if (!initialCustomDep.current) {
-      setReset(true);
-    }
-    initialCustomDep.current = false;
-  }, options?.resetOnCustomDepsChange ?? []);
+  useEffect(
+    () => {
+      if (!initialCustomDep.current) {
+        setReset(true);
+      }
+      initialCustomDep.current = false;
+    },
+    options?.resetOnCustomDepsChange ?? []
+  );
 
   useEffect(() => {
     if (options?.resetOnSizeChange) {
