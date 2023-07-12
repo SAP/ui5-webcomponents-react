e460a613b001c959211cd2e84c8e905f76de7069
Lukas742 <lukas.harbarth@sap.com>
2023-07-12 12:53:04 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@d995c4047774aba0d1833eb3ce25ae615eb03014 🚀

---
+++ main/assets/List-1889d832.js
@@ -0,0 +1,41 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{C as d,A as r}from"./DomRefTable.module-70c69e1c.js";import{D as l}from"./DocsHeader-1c79efa6.js";import{F as c}from"./Footer-0c721c1f.js";import"./index-ebeaab24.js";import{M as h,C as s,a as i}from"./chunk-PCJTTTQV-99d9e69c.js";import{S as u}from"./SubcomponentsSection-6138f2fc.js";import"./employee-1c1d2fc1.js";import{C as f}from"./index-73aa2d2c.js";import{G as x}from"./index-4554591f.js";import{S as j}from"./index-b5df98d2.js";import{C as S,D as p,a as g,G as y}from"./List.stories-027c8b40.js";import{u as a}from"./index-bda0bad7.js";import"./react-jss.esm-022ab528.js";import"./inheritsLoose-93e09647.js";import"./index-08e2d528.js";import"./iframe-42d8f269.js";import"../sb-preview/runtime.js";import"./UI5Element-78be0f3d.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-70a9e5b9.js";import"./Icons-26e87c01.js";import"./decline-09ce3004.js";import"./i18n-defaults-80781f7e.js";import"./information-872f55da.js";import"./class-map-18f572ce.js";import"./Button-427cd4bb.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-99b8bbf4.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-3e959ad1.js";import"./index-50232721.js";import"./Integer-f7f172c9.js";import"./index-4b8e5552.js";import"./Avatar-e1c9d8f0.js";import"./ResizeHandler-15c77712.js";import"./index-1bb0d3b6.js";import"./index-256b9be6.js";import"./Link-04c4a519.js";import"./WrappingType-b81e595a.js";import"./index-ea625d40.js";import"./TableOfContent-3555cec9.js";import"./index-abb57bfe.js";import"./Label-263c93f2.js";import"./index-9c09ad76.js";import"./index-48fa4127.js";import"./index-7e9212aa.js";import"./Popover-a03c4c52.js";import"./PopupsCommon.css-1eb36bcf.js";import"./FocusableElements-53839075.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-119d6915.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./CustomListItem-c77a30c8.js";import"./ListItem-6bbd1cb0.js";import"./ListItemBase-d5e2b9fa.js";import"./ItemNavigation-e8f3ce94.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-4ada5b66.js";import"./RadioButton-1b157057.js";import"./ValueState-2c5e5904.js";import"./CheckBox-13a91d81.js";import"./accept-18821b02.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-ccb981dd.js";import"./GroupHeaderListItem-514e58f4.js";import"./StandardListItem-4f4de76a.js";import"./ValueState-ab6838cc.js";import"./index-b9a1131e.js";import"./ProgressIndicator-97c2568e.js";import"./AnimationMode-30bec6df.js";import"./index-1a462acc.js";const L="A component to be used as custom list item within the `List` the same way as the standard `StandardListItem`. The component accepts arbitrary HTML content to allow full customization\n",w="The `GroupHeaderListItem` is a special list item, used only to separate other list items into logical groups\n",C="The `StandardListItem` represents the simplest type of item for a `StandardListItemst`. This is a list item, providing the most common use cases such as `text`, `image` and `icon`.\n\n### CSS Shadow Parts\n\nCSS Shadow Parts allow developers to style elements inside the Shadow DOM.  \nThe `StandardListItem` exposes the following CSS Shadow Parts:\n\n- title - Used to style the title of the list item\n- description - Used to style the description of the list item\n- additional-text - Used to style the additionalText of the list item\n- icon - Used to style the icon of the list item\n",D=`## Keyboard Handling
+
+### Basic Navigation
+
+The \`List\` provides advanced keyboard handling. When a list is focused the user can use the following keyboard shortcuts in order to perform a navigation:
+
+- \\[UP/DOWN\\] - Navigates up and down the items
+- \\[HOME\\] - Navigates to first item
+- \\[END\\] - Navigates to the last item
+
+The user can use the following keyboard shortcuts to perform actions (such as select, delete), when the \`mode\` property is in use:
+
+- \\[SPACE\\] - Select an item (if \`type\` is 'Active') when \`mode\` is selection
+- \\[DELETE\\] - Delete an item if \`mode\` property is \`Delete\`
+
+### Fast Navigation
+
+This component provides a build in fast navigation group which can be used via \`F6 / Shift + F6\` or \`Ctrl + Alt(Option) + Down / Ctrl + Alt(Option) + Up\`. In order to use this functionality, you need to import the following module: \`import "@ui5/webcomponents-base/dist/features/F6Navigation.js"\`
+`;function mo(e={}){const{wrapper:n}=Object.assign({},a(),e.components);return n?t.jsx(n,Object.assign({},e,{children:t.jsx(m,{})})):m();function m(){const o=Object.assign({h2:"h2"},a(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(h,{of:S}),`
+`,t.jsx(l,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(d,{of:p}),`
+`,t.jsx(i,{children:D}),`
+`,t.jsx("br",{}),`
+`,t.jsx(i,{children:u}),`
+`,t.jsx(o.h2,{id:"standardlistitem",children:"StandardListItem"}),`
+`,t.jsx(r,{of:j}),`
+`,t.jsx(i,{children:C}),`
+`,t.jsx(o.h2,{id:"customlistitem",children:"CustomListItem"}),`
+`,t.jsx(s,{of:g}),`
+`,t.jsx(r,{of:f}),`
+`,t.jsx(i,{children:L}),`
+`,t.jsx(o.h2,{id:"groupheaderlistitem",children:"GroupHeaderListItem"}),`
+`,t.jsx(s,{of:y}),`
+`,t.jsx(r,{of:x}),`
+`,t.jsx(i,{children:w}),`
+`,t.jsx(c,{})]})}}export{mo as default};
+//# sourceMappingURL=List-1889d832.js.map
---
+++ main/assets/Portals-dcfef8ca.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-5926aa06.js";import{M as a}from"./chunk-PCJTTTQV-99d9e69c.js";import"./DomRefTable.module-70c69e1c.js";import"./index-ebeaab24.js";import{T as s}from"./TableOfContent-3555cec9.js";import{F as p}from"./Footer-0c721c1f.js";import{u as r}from"./index-bda0bad7.js";import"./iframe-42d8f269.js";import"../sb-preview/runtime.js";import"./react-18-3d03c2cf.js";import"./mapValues-0a776dd9.js";import"./_baseForOwn-800a0312.js";import"./index-9c09ad76.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-593546d2.js";import"./_baseUniq-4c215180.js";import"./index-356e4a49.js";import"./react-jss.esm-022ab528.js";import"./index-08e2d528.js";import"./UI5Element-78be0f3d.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-d4224c1c.js";import"./utils-69f7a0e0.js";import"./useIsomorphicLayoutEffect-38a48652.js";import"./slot-76e48863.js";import"./Icon-70a9e5b9.js";import"./Icons-26e87c01.js";import"./decline-09ce3004.js";import"./i18n-defaults-80781f7e.js";import"./information-872f55da.js";import"./class-map-18f572ce.js";import"./Button-427cd4bb.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-99b8bbf4.js";import"./i18n-defaults-f002f496.js";import"./index-562e1433.js";import"./clsx.m-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-3e959ad1.js";import"./index-50232721.js";import"./Integer-f7f172c9.js";import"./index-4b8e5552.js";import"./Avatar-e1c9d8f0.js";import"./ResizeHandler-15c77712.js";import"./employee-1c1d2fc1.js";import"./index-1bb0d3b6.js";import"./index-256b9be6.js";import"./Link-04c4a519.js";import"./WrappingType-b81e595a.js";import"./index-ea625d40.js";import"./index-abb57bfe.js";import"./Label-263c93f2.js";import"./index-48fa4127.js";import"./index-7e9212aa.js";import"./Popover-a03c4c52.js";import"./PopupsCommon.css-1eb36bcf.js";import"./FocusableElements-53839075.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-54298215.js";import"./BrowserScrollbar.css-119d6915.js";function ye(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(i,{})})):i();function i(){const t=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",a:"a",pre:"pre"},r(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Working with Portals"}),`
+`,e.jsx(t.h1,{id:"working-with-portals",children:"Working with Portals"}),`
+`,e.jsx(t.p,{children:"This entry explains why portals are used in UI5 Web Components for React components and what you need to consider when using them."}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s,{headingSelector:"h2"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"why-portals",children:"Why Portals?"}),`
+`,e.jsxs(t.p,{children:["In most cases when a React component like the ",e.jsx(t.code,{children:"AnalyticalTable"})," implements a popover like a ",e.jsx(t.code,{children:"Dialog"})," or ",e.jsx(t.code,{children:"ResponsivePopover"}),", we use a ",e.jsx(t.a,{href:"https://react.dev/reference/react-dom/createPortal",target:"_blank",rel:"nofollow noopener noreferrer",children:"React Portal"})," to mount it outside the parents DOM hierarchy, to prevent unwanted side effects (wrong positioning, alignment, etc.)."]}),`
+`,e.jsx(t.p,{children:"For example:"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Dialog</Button>
+      {createPortal(<Dialog ref={dialogRef} />, document.body)}
+    </>
+  );
+};
+`})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"pitfalls",children:"Pitfalls"}),`
+`,e.jsxs(t.p,{children:[`In some cases you may want to add a class or attribute that is affecting all the components HTML elements. Since the popover is outside the DOM hierarchy of the most outer element of the component, the class/attribute/etc. will not be applied to it.
+Or maybe you just don't want the popover to be mounted on the `,e.jsx(t.code,{children:"body"}),` element (which is the default).
+For this we offer the `,e.jsx(t.code,{children:"portalContainer"})," prop. This prop is available on all components that implement a popover, and with its help you can define the container into which the popover is mounted."]}),`
+`,e.jsxs(t.p,{children:["For example, if you want to display the ",e.jsx(t.code,{children:"AnalyticalTable"})," in compact mode, while the rest of the app stays in cozy mode, then you could achieve that like this:"]}),`
+`,e.jsx(t.p,{children:"Define a container somewhere in your DOM tree, where the portal should be rendered into and add the respective attribute/class/etc. to it."}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-html",children:`<div id="modalContainer" class="ui5-content-density-compact" />
+`})}),`
+`,e.jsxs(t.p,{children:["Then, set the ",e.jsx(t.code,{children:"portalContainer"})," to the respective element."]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`<AnalyticalTable
+  className="ui5-content-density-compact"
+  portalContainer={document.getElementById('modalContainer')}
+  data={data}
+  columns={columns}
+/>
+`})}),`
+`,e.jsx("div",{style:{height:"var(--_ui5_bar_base_height)"}}),`
+`,e.jsx(p,{style:{position:"fixed",bottom:0,zIndex:2,maxHeight:"var(--_ui5_bar_base_height)"}})]})}}export{ye as default};
+//# sourceMappingURL=Portals-dcfef8ca.js.map
