53642745a6a0ef493a12ca12ef4dc698baf01929
Lukas742 <lukas.harbarth@sap.com>
2023-07-13 08:59:50 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@5168fc0ac7d8dbe6bb74295e273202b7abd1325a 🚀

---
+++ main/assets/Select.stories-6b289940.js
@@ -0,0 +1,12 @@
+import{j as t}from"./jsx-runtime-5926aa06.js";import{O as e}from"./index-249a5c4b.js";import{S as s}from"./index-f252e821.js";import{V as a}from"./ValueState-ab6838cc.js";const l={title:"Inputs / Select",component:s,argTypes:{children:{control:{disable:!0}},valueStateMessage:{control:{disable:!0}}},args:{valueState:a.None}},n={render:p=>t.jsxs(s,{...p,children:[t.jsx(e,{children:"Option 1"}),t.jsx(e,{children:"Option 2"}),t.jsx(e,{children:"Option 3"}),t.jsx(e,{children:"Option 4"}),t.jsx(e,{children:"Option 5"})]})};var o,r,i;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    return <Select {...args}>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+        <Option>Option 3</Option>
+        <Option>Option 4</Option>
+        <Option>Option 5</Option>
+      </Select>;
+  }
+}`,...(i=(r=n.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const c=["Default"],S=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:c,default:l},Symbol.toStringTag,{value:"Module"}));export{S as C,n as D};
+//# sourceMappingURL=Select.stories-6b289940.js.map
