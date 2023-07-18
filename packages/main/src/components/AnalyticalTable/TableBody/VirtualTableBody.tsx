9a69af20c41c88e9e65aa79710fed4bb3c4ae386
Marcus Notheis <marcus.notheis@sap.com>
2023-07-18 12:58:06 +0200

    migrate row virtualizer

--- packages/main/src/components/AnalyticalTable/TableBody/VirtualTableBody.tsx
+++ packages/main/src/components/AnalyticalTable/TableBody/VirtualTableBody.tsx
@@ -162,7 +162,7 @@ export const VirtualTableBody = (props: VirtualTableBodyProps) => {
           lastNonEmptyRow.current = row;
         }
         prepareRow(row);
-        const rowProps = row.getRowProps({ 'aria-rowindex': virtualRow.index });
+        const rowProps = row.getRowProps({ 'aria-rowindex': virtualRow.index, 'data-index': virtualRow.index });
         const isNavigatedCell = markNavigatedRow(row);
         const RowSubComponent = typeof renderRowSubComponent === 'function' ? renderRowSubComponent(row) : undefined;
 
@@ -186,6 +186,7 @@ export const VirtualTableBody = (props: VirtualTableBodyProps) => {
         return (
           // eslint-disable-next-line react/jsx-key
           <div
+            ref={rowVirtualizer.measureElement}
             {...rowProps}
             style={{
               ...(rowProps.style ?? {}),
@@ -215,9 +216,9 @@ export const VirtualTableBody = (props: VirtualTableBodyProps) => {
               const directionStyles = isRtl
                 ? {
                     transform: `translateX(-${virtualColumn.start}px)`,
-                    right: 0
+                    insertInlineStart: 0
                   }
-                : { transform: `translateX(${virtualColumn.start}px)`, left: 0 };
+                : { transform: `translateX(${virtualColumn.start}px)`, insertInlineStart: 0 };
               if (!cell) {
                 return null;
               }
