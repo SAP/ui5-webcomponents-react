e5dea98b3e803d528af78db87f5846d356d79e7b
Lukas742 <lukas.harbarth@sap.com>
2023-07-20 14:48:21 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@cbe5365df40dda1e6f8bce669b0d8e4481a29dff 🚀

---
+++ main/assets/MediaGallery-e5a53206.js
@@ -0,0 +1,28 @@
+import{j as t}from"./jsx-runtime-6b79a019.js";import{C as s,A as d}from"./DomRefTable.module-0deca827.js";import{D as l}from"./DocsHeader-6052bdae.js";import{F as h}from"./Footer-3e7afb62.js";import"./index-ccf6a75d.js";import{M as c,C as e,b as r}from"./chunk-PCJTTTQV-7486b0f8.js";import{S as u}from"./SubcomponentsSection-6138f2fc.js";import{C as x,D as p,a as f,I as j,M as g}from"./MediaGallery.stories-76787617.js";import{u as a}from"./index-f2208173.js";import"./react-jss.esm-0528916b.js";import"./inheritsLoose-93e09647.js";import"./index-212fcdfb.js";import"./iframe-3c17aa59.js";import"../sb-preview/runtime.js";import"./UI5Element-11982a12.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-03662f7f.js";import"./utils-f515de3e.js";import"./useIsomorphicLayoutEffect-0a189966.js";import"./slot-76e48863.js";import"./Icon-3498e12d.js";import"./Icons-234bf59e.js";import"./decline-c1cc2557.js";import"./i18n-defaults-837f5aed.js";import"./information-2644a02f.js";import"./class-map-341004db.js";import"./Button-3da2aede.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-6afa24d0.js";import"./i18n-defaults-1a83921e.js";import"./index-d3b71bde.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-2d3c4974.js";import"./index-dabbc199.js";import"./Integer-f7f172c9.js";import"./index-d4c08482.js";import"./Avatar-d7b74462.js";import"./ResizeHandler-9ebe913c.js";import"./employee-e34ffee2.js";import"./index-b3f1ebe8.js";import"./index-cd180eaf.js";import"./Link-6cd3dd1c.js";import"./WrappingType-b81e595a.js";import"./index-c4470453.js";import"./TableOfContent-6d956274.js";import"./index-a03453fb.js";import"./Label-f867a533.js";import"./index-8d7a8128.js";import"./index-10622f2d.js";import"./index-ae4783af.js";import"./Popover-1f09fd29.js";import"./PopupsCommon.css-fd974b5a.js";import"./FocusableElements-708d1a4e.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-9183c00a.js";import"./BrowserScrollbar.css-bcc05298.js";import"./react-18-74db5ca2.js";import"./mapValues-4f30c9d2.js";import"./_baseForOwn-1047660d.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-f4b610e7.js";import"./_baseUniq-a28bb1ec.js";import"./index-356e4a49.js";import"./parameters-bundle.css-f9dc8928.js";import"./index-d5118561.js";import"./ItemNavigation-a882fb90.js";import"./Carousel-01b8810d.js";import"./ScrollEnablement-dd3a6392.js";import"./animate-1a16ba20.js";import"./AnimationMode-b83806eb.js";import"./slim-arrow-left-496e4002.js";import"./slim-arrow-right-c0f59065.js";const y="**Since:** 0.21.0\n\nThe `MediaGalleryItem` web component represents the items displayed in the `MediaGallery` web component.\n\n**Note:** `MediaGalleryItem` is not supported when used outside of `MediaGallery`.\n\n### Keyboard Handling\n\nThe `MediaGallery` provides advanced keyboard handling. When focused, the user can use the following keyboard shortcuts in order to perform a navigation:\n\n- \\[SPACE/ENTER/RETURN\\] - Trigger `undefined` event\n",b=`## Keyboard Handling
+
+The \`MediaGallery\` provides advanced keyboard handling.  
+When the thumbnails menu is focused the following keyboard shortcuts allow the user to navigate through the thumbnail items:
+
+- \\[UP/DOWN\\] - Navigates up and down the items
+- \\[HOME\\] - Navigates to first item
+- \\[END\\] - Navigates to the last item
+- \\[SPACE/ENTER\\] - Select an item
+`;function _t(i={}){const{wrapper:n}=Object.assign({},a(),i.components);return n?t.jsx(n,Object.assign({},i,{children:t.jsx(m,{})})):m();function m(){const o=Object.assign({h2:"h2",h3:"h3"},a(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(c,{of:x}),`
+`,t.jsx(l,{since:"0.21.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(e,{of:p}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(r,{children:b}),`
+`,t.jsx(o.h2,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(o.h3,{id:"custom-thumbnail-image",children:"Custom Thumbnail Image"}),`
+`,t.jsx(e,{of:f}),`
+`,t.jsx(o.h3,{id:"interactive-display-area",children:"Interactive Display Area"}),`
+`,t.jsx(e,{of:j}),`
+`,t.jsx(r,{children:u}),`
+`,t.jsx(o.h2,{id:"mediagalleryitem",children:"MediaGalleryItem"}),`
+`,t.jsx(r,{children:y}),`
+`,t.jsx(d,{of:g}),`
+`,t.jsx(h,{})]})}}export{_t as default};
+//# sourceMappingURL=MediaGallery-e5a53206.js.map
---
+++ main/assets/useLongestYAxisLabel-ec1cae6f.js
@@ -0,0 +1,2 @@
+import{r as i}from"./index-ccf6a75d.js";import{g as l,d as n}from"./defaults-062d7b18.js";import{g as p}from"./useTooltipFormatter-29be6c97.js";const u=(r,t)=>i.useMemo(()=>{let a=0;const e=t[0];if(r instanceof Array&&e){const o=s=>t.map(m=>e.formatter(l(s,m.accessor,"")));a=Math.max(...r.map(o).flat().map(p)),a+=8}return a=Math.min(a,n),[a,{marginLeft:a,maxWidth:`calc(100% - ${a+10}px)`}]},[r,t]);export{u};
+//# sourceMappingURL=useLongestYAxisLabel-ec1cae6f.js.map
