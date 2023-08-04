fb88dbdf2b1d35984ad423bcb686ee2a308c6e09
MarcusNotheis <marcus.notheis@sap.com>
2023-08-04 14:22:25 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@f60604b7d7c754c0de5f64900352ef406f5e040e 🚀

---
+++ main/assets/DonutChart-b3fe7a91.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-d079401a.js";import{M as n,C as o}from"./chunk-S4VUQJ4A-2a91c9b3.js";import{C as a}from"./DomRefTable.module-f9bf86d9.js";import{D as h}from"./DocsHeader-fb327dc8.js";import{F as d}from"./Footer-5ee2cf95.js";import"./index-f1f2c4b1.js";import{C as l,D as e,W as x,a as c,b as j,L as f,c as g,d as u,H as C}from"./DonutChart.stories-4e7b6741.js";import{u as s}from"./index-59d6410c.js";import"./iframe-de1c4b67.js";import"../sb-preview/runtime.js";import"./index-92d824d3.js";import"./index-c74c9f7f.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./inheritsLoose-93e09647.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./react-jss.esm-c310038c.js";import"./index-c710aefd.js";import"./UI5Element-a4bd3d38.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7d7e5133.js";import"./utils-626dc1bf.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-42236945.js";import"./Icons-74c917eb.js";import"./decline-3cb804de.js";import"./i18n-defaults-2d2bf0b6.js";import"./information-dbbb1c9a.js";import"./class-map-0ab40ab9.js";import"./Button-02dfef34.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-6afa24d0.js";import"./i18n-defaults-d5d083dd.js";import"./index-47d0a85a.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-d9b40dee.js";import"./index-60dca775.js";import"./Integer-f7f172c9.js";import"./index-3d44dffe.js";import"./Avatar-6c09069b.js";import"./ResizeHandler-0af4416f.js";import"./employee-05c578de.js";import"./index-146ed284.js";import"./index-a1f75b7c.js";import"./Link-498d09c2.js";import"./WrappingType-b81e595a.js";import"./index-56b3d57c.js";import"./TableOfContent-15e6a806.js";import"./index-82cc736d.js";import"./Label-287c3220.js";import"./index-f0d458b5.js";import"./index-094a491a.js";import"./Popover-26ef4370.js";import"./PopupsCommon.css-f12c9479.js";import"./FocusableElements-66623361.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-132a30d8.js";import"./BrowserScrollbar.css-309143b9.js";import"./DemoProps-4c4a52a7.js";import"./PieChart-b82fd175.js";import"./index-99d317a4.js";import"./defaults-699125bd.js";import"./ChartContainer-f2d0b3a4.js";import"./index-d6dd836d.js";import"./i18n-defaults-456fcfbf.js";import"./I18nContext-dd74d568.js";import"./throttle-e28860fd.js";import"./isString-529da971.js";import"./staticProps-d8b65b3f.js";import"./useOnClickInternal-7ffac7fa.js";import"./Placeholder-2f403a27.js";import"./generateCategoricalChart-3f4ef5d8.js";import"./index-b580f7e8.js";import"./react-content-loader.es-27eb8e7e.js";import"./isPlainObject-d18d97c8.js";function Ut(r={}){const{wrapper:m}=Object.assign({},s(),r.components);return m?t.jsx(m,Object.assign({},r,{children:t.jsx(p,{})})):p();function p(){const i=Object.assign({h2:"h2",h3:"h3"},s(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:l}),`
+`,t.jsx(h,{}),`
+`,t.jsx(i.h2,{id:"example",children:"Example"}),`
+`,t.jsx(o,{of:e}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{of:e}),`
+`,t.jsx("br",{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(i.h2,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(i.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,t.jsx(o,{of:x}),`
+`,t.jsx(i.h3,{id:"with-padding-angle",children:"With Padding Angle"}),`
+`,t.jsx(o,{of:c}),`
+`,t.jsx(i.h3,{id:"with-custom-radius",children:"With Custom Radius"}),`
+`,t.jsx(o,{of:j}),`
+`,t.jsx(i.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,t.jsx(o,{of:f}),`
+`,t.jsx(i.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,t.jsx(o,{of:g}),`
+`,t.jsx(i.h3,{id:"with-highlighted-active-segment",children:"With highlighted active segment"}),`
+`,t.jsx(o,{of:u}),`
+`,t.jsx(i.h3,{id:"hide-labels",children:"Hide labels"}),`
+`,t.jsx(o,{of:C}),`
+`,t.jsx(d,{})]})}}export{Ut as default};
+//# sourceMappingURL=DonutChart-b3fe7a91.js.map
