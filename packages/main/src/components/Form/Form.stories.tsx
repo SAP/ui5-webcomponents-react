958e60d0337642ddf576e486860ffd97ea09a9a6
Lukas Harbarth <lukas.harbarth@sap.com>
2023-08-09 11:44:30 +0200

    docs(Form): add edit & display-only mode example

--- packages/main/src/components/Form/Form.stories.tsx
+++ packages/main/src/components/Form/Form.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { useReducer } from 'react';
 import {
+  Button,
   CheckBox,
   FormBackgroundDesign,
   FormGroup,
@@ -219,6 +221,112 @@ export const FormWithCustomComponents: Story = {
         </CustomComponent>
         <CustomComponent2 />
       </Form>
+    );
+  }
+};
+
+const StandardField = ({ editMode, value, inputType = InputType.None, onInput, ...rest }) => {
+  if (editMode) {
+    return <Input value={value} style={{ width: '100%' }} type={inputType} onInput={onInput} {...rest} />;
+  }
+  if (inputType === InputType.URL || inputType === InputType.Email) {
+    return (
+      <Link href={inputType === InputType.Email ? `mailto:${value}` : value} target="_blank" {...rest}>
+        {value}
+      </Link>
+    );
+  }
+  return <Text {...rest}>{value}</Text>;
+};
+
+const reducer = (state, { field, value }) => {
+  return { ...state, [field]: value };
+};
+
+export const DisplayEditMode: Story = {
+  name: 'Display & Edit mode',
+  args: { titleText: 'Supplier' },
+  render: (args) => {
+    const [editMode, toggleEditMode] = useReducer((prev) => !prev, false, undefined);
+    const [formState, dispatch] = useReducer(
+      reducer,
+      {
+        name: 'Red Point Stores',
+        street: 'Main St 1618',
+        zip: 31415,
+        city: 'Maintown',
+        country: 'Germany',
+        web: 'https://www.sap.com',
+        mail: 'john.smith@sap.com',
+        twitter: '@sap',
+        phone: '+49 1234 56789'
+      },
+      undefined
+    );
+    const { zip, city, name, street, country, web, mail, twitter, phone } = formState;
+
+    const handleInput = (e) => {
+      dispatch({ field: Object.keys(e.target.dataset)[0], value: e.target.value });
+    };
+
+    return (
+      <>
+        <Button onClick={toggleEditMode}>Toggle {editMode ? 'Display-Only Mode' : 'Edit Mode'}</Button>
+        <Form
+          {...args}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <FormGroup titleText="Address">
+            <FormItem label="Name">
+              <StandardField editMode={editMode} value={name} onInput={handleInput} data-name />
+            </FormItem>
+            <FormItem label="Street">
+              <StandardField editMode={editMode} value={street} onInput={handleInput} data-street />
+            </FormItem>
+            <FormItem label="ZIP Code / City">
+              {editMode ? (
+                <>
+                  <Input value={zip} type={InputType.Number} style={{ width: '30%' }} onInput={handleInput} data-zip />
+                  <Input value={city} style={{ width: '70%' }} onInput={handleInput} data-city />
+                </>
+              ) : (
+                <Text>{`${zip} ${city}`}</Text>
+              )}
+            </FormItem>
+            <FormItem label="Country">
+              <StandardField editMode={editMode} value={country} onInput={handleInput} data-country />
+            </FormItem>
+            <FormItem label="Web">
+              <StandardField editMode={editMode} value={web} inputType={InputType.URL} onInput={handleInput} data-web />
+            </FormItem>
+          </FormGroup>
+          <FormGroup titleText="Contact">
+            <FormItem label="Email">
+              <StandardField
+                editMode={editMode}
+                value={mail}
+                inputType={InputType.Email}
+                onInput={handleInput}
+                data-email
+              />
+            </FormItem>
+            <FormItem label="Twitter">
+              <StandardField editMode={editMode} value={twitter} onInput={handleInput} data-twitter />
+            </FormItem>
+            <FormItem label="Phone">
+              <StandardField
+                editMode={editMode}
+                value={phone}
+                inputType={InputType.Tel}
+                onInput={handleInput}
+                data-phone
+              />
+            </FormItem>
+          </FormGroup>
+        </Form>
+      </>
     );
   }
 };
