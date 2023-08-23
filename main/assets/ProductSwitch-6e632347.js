0b695d633ee1554f7cc7f9ae4f0b5dfd17ee168a
Lukas742 <lukas.harbarth@sap.com>
2023-08-23 14:23:45 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@9f9979c4c8609f85795cfa1f1df9e8b3eb6ace3b 🚀

---
+++ main/assets/ProductSwitch-6e632347.js
@@ -0,0 +1,21 @@
+import{j as t}from"./jsx-runtime-d079401a.js";import{C as s,b as c}from"./DomRefTable.module-d2537315.js";import{D as a}from"./DocsHeader-7505b502.js";import{F as d}from"./Footer-d744fc0c.js";import"./index-f1f2c4b1.js";import{M as h,C as u,b as i}from"./chunk-S4VUQJ4A-ddc15e16.js";import{S as f}from"./SubcomponentsSection-6138f2fc.js";import{C as l,D as m,P as x}from"./ProductSwitch.stories-584dbfca.js";import{u as p}from"./index-59d6410c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-9d463984.js";import"./iframe-5ff3bf70.js";import"../sb-preview/runtime.js";import"./UI5Element-a4bd3d38.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7d7e5133.js";import"./utils-626dc1bf.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-42236945.js";import"./Icons-74c917eb.js";import"./decline-3cb804de.js";import"./i18n-defaults-2d2bf0b6.js";import"./information-dbbb1c9a.js";import"./class-map-0ab40ab9.js";import"./Button-7043cfcf.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-6afa24d0.js";import"./i18n-defaults-d675a36d.js";import"./index-bb1f9706.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-d9b40dee.js";import"./index-139cb4c0.js";import"./Integer-f7f172c9.js";import"./index-3d4c5c26.js";import"./Avatar-8a894654.js";import"./ResizeHandler-0af4416f.js";import"./employee-05c578de.js";import"./index-466c5936.js";import"./index-95f7b871.js";import"./Link-307a9e73.js";import"./WrappingType-b81e595a.js";import"./index-43333f43.js";import"./TableOfContent-e86ec428.js";import"./index-ceda15f9.js";import"./Label-34e3bd87.js";import"./index-b42968db.js";import"./index-f911d1ec.js";import"./index-5124b0b6.js";import"./Popover-26ef4370.js";import"./PopupsCommon.css-f12c9479.js";import"./FocusableElements-66623361.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-132a30d8.js";import"./BrowserScrollbar.css-309143b9.js";import"./index-d203965f.js";import"./mapValues-e5c6f517.js";import"./_baseForOwn-24231fc1.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-00f63d78.js";import"./_baseUniq-edf521f4.js";import"./index-356e4a49.js";import"./home-e945f8a9.js";import"./parameters-bundle.css-f9dc8928.js";import"./ItemNavigation-0734b7c0.js";import"./i18n-defaults-2dcf78e8.js";const j="**Since:** 0.7.0\n\nThe `ProductSwitchItem` web component represents the items displayed in the `ProductSwitch` web component.\n\n**Note:** `ProductSwitchItem` is not supported when used outside of `ProductSwitch`.\n\n### Keyboard Handling\n\nThe `ProductSwitch` provides advanced keyboard handling. When focused, the user can use the following keyboard shortcuts in order to perform a navigation:\n\n- \\[SPACE/ENTER/RETURN\\] - Trigger `click` event\n",w=`## Keyboard Handling
+
+The \`ProductSwitch\` provides advanced keyboard handling. When focused, the user can use the following keyboard shortcuts in order to perform a navigation:
+
+- \\[TAB\\] - Move focus to the next interactive element after the \`ProductSwitch\`
+- \\[UP/DOWN\\] - Navigates up and down the items
+- \\[LEFT/RIGHT\\] - Navigates left and right the items
+`;function Ft(o={}){const{wrapper:e}=Object.assign({},p(),o.components);return e?t.jsx(e,Object.assign({},o,{children:t.jsx(n,{})})):n();function n(){const r=Object.assign({h2:"h2"},p(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(h,{of:l}),`
+`,t.jsx(a,{since:"0.7.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(u,{of:m}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:m}),`
+`,t.jsx(i,{children:w}),`
+`,t.jsx(i,{children:f}),`
+`,t.jsx(r.h2,{id:"productswitchitem",children:"ProductSwitchItem"}),`
+`,t.jsx(c,{of:x}),`
+`,t.jsx(i,{children:j}),`
+`,t.jsx(d,{})]})}}export{Ft as default};
+//# sourceMappingURL=ProductSwitch-6e632347.js.map
---
+++ main/assets/WithTooltip-YBG737T3-34bdea20.js
@@ -0,0 +1,2 @@
+import{W as u,W as x,a as P}from"./chunk-S4VUQJ4A-ddc15e16.js";import"./iframe-5ff3bf70.js";import"../sb-preview/runtime.js";import"./index-f1f2c4b1.js";import"./index-d203965f.js";import"./index-b42968db.js";import"./mapValues-e5c6f517.js";import"./_baseForOwn-24231fc1.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-00f63d78.js";import"./_baseUniq-edf521f4.js";import"./index-356e4a49.js";export{u as WithToolTipState,x as WithTooltip,P as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-YBG737T3-34bdea20.js.map
