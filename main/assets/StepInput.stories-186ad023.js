aba63b3d92f6e6b9c393e1cc618ae350126eda37
Lukas742 <lukas.harbarth@sap.com>
2023-08-23 05:56:57 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@98111a76ccb5de3cfd324405b25b5a9a34fee97a 🚀

---
+++ main/assets/StepInput.stories-186ad023.js
@@ -0,0 +1,26 @@
+import{M as d,C as c,e as l,b as h}from"./chunk-S4VUQJ4A-1a23b51c.js";import{S as r}from"./index-9e6d9fe5.js";import{V as s}from"./ValueState-ab6838cc.js";import{A as x}from"./DomRefTable.module-18b321e4.js";import{D as f}from"./DocsHeader-3709c719.js";import{F as j}from"./Footer-cbfb2dc5.js";import{j as t}from"./jsx-runtime-d079401a.js";import"./index-f1f2c4b1.js";import{u as a}from"./index-59d6410c.js";import"./iframe-1d8e178d.js";import"../sb-preview/runtime.js";import"./index-d203965f.js";import"./index-b42968db.js";import"./mapValues-e5c6f517.js";import"./_baseForOwn-24231fc1.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-00f63d78.js";import"./_baseUniq-edf521f4.js";import"./index-356e4a49.js";import"./UI5Element-a4bd3d38.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7d7e5133.js";import"./utils-626dc1bf.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-42236945.js";import"./Icons-74c917eb.js";import"./ValueState-2c5e5904.js";import"./AriaLabelHelper-43a261ec.js";import"./Float-99d99064.js";import"./Integer-f7f172c9.js";import"./i18n-defaults-d675a36d.js";import"./less-ec997304.js";import"./add-bbbd01bc.js";import"./i18n-defaults-2d2bf0b6.js";import"./Input-e218b89d.js";import"./ResizeHandler-0af4416f.js";import"./Device-6afa24d0.js";import"./decline-3cb804de.js";import"./Suggestions.css-8d6efd26.js";import"./information-dbbb1c9a.js";import"./Popover-26ef4370.js";import"./PopupsCommon.css-f12c9479.js";import"./FocusableElements-66623361.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-132a30d8.js";import"./class-map-0ab40ab9.js";import"./BrowserScrollbar.css-309143b9.js";import"./ResponsivePopoverCommon.css-870f7136.js";import"./ValueStateMessage.css-044f6113.js";import"./react-jss.esm-2e5f50f2.js";import"./index-30c7d21b.js";import"./Button-7043cfcf.js";import"./MarkedEvents-b83081e9.js";import"./index-bb1f9706.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-d9b40dee.js";import"./index-5d0c6867.js";import"./index-19857e10.js";import"./Avatar-8a894654.js";import"./employee-05c578de.js";import"./index-09740f27.js";import"./index-9ed47985.js";import"./Link-307a9e73.js";import"./WrappingType-b81e595a.js";import"./index-fb6e8b5e.js";import"./TableOfContent-0d0bedd7.js";import"./index-9822581b.js";import"./Label-34e3bd87.js";import"./index-63dadc62.js";import"./index-1cec78b0.js";const g=`## Usage
+
+The default step is 1 but the app developer can set a different one. App developers can set a maximum and minimum value for the \`StepInput\`. The increase/decrease button and the up/down keyboard navigation become disabled when the value reaches the max/min or a new value is entered from the input which is greater/less than the max/min.
+
+### When to use:
+
+- To adjust amounts, quantities, or other values quickly.
+- To adjust values for a specific step.
+
+### When not to use:
+
+- To enter a static number (for example, postal code, phone number, or ID). In this case, use the regular \`Input\` instead.
+- To display a value that rarely needs to be adjusted and does not pertain to a particular step. In this case, use the regular \`Input\` instead.
+- To enter dates and times. In this case, use date/time related components instead.
+`;function S(e={}){const{wrapper:i}=Object.assign({},a(),e.components);return i?t.jsx(i,{...e,children:t.jsx(n,{})}):n();function n(){const p=Object.assign({h2:"h2"},a(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(d,{title:"Inputs / StepInput",component:r,argTypes:{valueStateMessage:{control:{disable:!0}}},args:{valueState:s.None}}),`
+`,t.jsx(f,{since:"0.15.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(p.h2,{id:"example",children:"Example"}),`
+`,t.jsx(c,{children:t.jsx(l,{name:"Default",children:u=>t.jsx(r,{...u})})}),`
+`,t.jsx(p.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(x,{story:"Default"}),`
+`,t.jsx(h,{children:g}),`
+`,t.jsx(j,{})]})}}const m=e=>t.jsx(r,{...e});m.storyName="Default";m.parameters={storySource:{source:`args => {
+  return <StepInput {...args} />;
+}`}};const o={title:"Inputs / StepInput",component:r,args:{valueState:s.None},argTypes:{valueStateMessage:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:S};const Ht=["defaultStory"];export{Ht as __namedExportsOrder,o as default,m as defaultStory};
+//# sourceMappingURL=StepInput.stories-186ad023.js.map
