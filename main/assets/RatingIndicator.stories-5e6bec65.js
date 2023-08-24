5f7a501c6f3e40567835c7bb3130a3e499cf369a
Lukas742 <lukas.harbarth@sap.com>
2023-08-24 08:20:43 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@49ebe68667b7a9fa1e0d477803ff1d8e1f5aeb93 🚀

---
+++ main/assets/RatingIndicator.stories-5e6bec65.js
@@ -0,0 +1,31 @@
+import{M as h,C as c,e as d,b as u}from"./chunk-S4VUQJ4A-84f5c535.js";import{R as r}from"./index-24ed196d.js";import{A as l}from"./DomRefTable.module-556f000b.js";import{D as g}from"./DocsHeader-23de8f1f.js";import{F as f}from"./Footer-f35933b9.js";import{j as t}from"./jsx-runtime-d079401a.js";import"./index-f1f2c4b1.js";import{u as a}from"./index-59d6410c.js";import"./iframe-3b2630af.js";import"../sb-preview/runtime.js";import"./index-d203965f.js";import"./index-b42968db.js";import"./mapValues-e5c6f517.js";import"./_baseForOwn-24231fc1.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-00f63d78.js";import"./_baseUniq-edf521f4.js";import"./index-356e4a49.js";import"./withWebComponent-7d7e5133.js";import"./utils-626dc1bf.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./Icon-42236945.js";import"./UI5Element-a4bd3d38.js";import"./Icons-74c917eb.js";import"./AriaLabelHelper-43a261ec.js";import"./Integer-f7f172c9.js";import"./Float-99d99064.js";import"./i18n-defaults-d675a36d.js";import"./favorite-e77b94de.js";import"./unfavorite-e8b9860a.js";import"./react-jss.esm-2e5f50f2.js";import"./index-72758f0c.js";import"./slot-76e48863.js";import"./decline-3cb804de.js";import"./i18n-defaults-2d2bf0b6.js";import"./information-dbbb1c9a.js";import"./class-map-0ab40ab9.js";import"./Button-7043cfcf.js";import"./MarkedEvents-b83081e9.js";import"./Device-6afa24d0.js";import"./index-bb1f9706.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-d9b40dee.js";import"./index-24720d23.js";import"./index-1ce0bb97.js";import"./Avatar-8a894654.js";import"./ResizeHandler-0af4416f.js";import"./employee-05c578de.js";import"./index-30fb964e.js";import"./index-c6a7ad7e.js";import"./Link-307a9e73.js";import"./WrappingType-b81e595a.js";import"./index-43333f43.js";import"./TableOfContent-08fbc8de.js";import"./index-c1366ccd.js";import"./Label-34e3bd87.js";import"./index-4b38c9c4.js";import"./index-7f2027af.js";import"./Popover-26ef4370.js";import"./PopupsCommon.css-f12c9479.js";import"./FocusableElements-66623361.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-132a30d8.js";import"./BrowserScrollbar.css-309143b9.js";const x=`## Usage
+
+The recommended number of icons is between 5 and 7.
+
+## Responsive Behavior
+
+You can change the size of the Rating Indicator by changing its \`font-size\` CSS property.  
+Example: \`<RatingIndicator style="font-size: 3rem;"></RatingIndicator>\`
+
+## Keyboard Handling
+
+When the \`RatingIndicator\` is focused, the user can change the rating with the following keyboard shortcuts:
+
+- \\[RIGHT/UP\\] - Increases the value of the rating by one step. If the highest value is reached, does nothing
+- \\[LEFT/DOWN\\] - Decreases the value of the rating by one step. If the lowest value is reached, does nothing.
+- \\[HOME\\] - Sets the lowest value.
+- \\[END\\] - Sets the highest value.
+- \\[SPACE/ENTER/RETURN\\] - Increases the value of the rating by one step. If the highest value is reached, sets the rating to the lowest value.
+- Any number - Changes value to the corresponding number. If typed number is larger than the number of values, sets the highest value.
+`;function j(e={}){const{wrapper:n}=Object.assign({},a(),e.components);return n?t.jsx(n,{...e,children:t.jsx(i,{})}):i();function i(){const s=Object.assign({h2:"h2"},a(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(h,{title:"Inputs / RatingIndicator",component:r}),`
+`,t.jsx(g,{since:"0.10.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(s.h2,{id:"example",children:"Example"}),`
+`,t.jsx(c,{children:t.jsx(d,{name:"Default",children:p=>t.jsx(r,{...p})})}),`
+`,t.jsx(s.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{story:"Default"}),`
+`,t.jsx(u,{children:x}),`
+`,t.jsx(f,{})]})}}const m=e=>t.jsx(r,{...e});m.storyName="Default";m.parameters={storySource:{source:`args => {
+  return <RatingIndicator {...args} />;
+}`}};const o={title:"Inputs / RatingIndicator",component:r,tags:["stories-mdx"],includeStories:["defaultStory"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:j};const At=["defaultStory"];export{At as __namedExportsOrder,o as default,m as defaultStory};
+//# sourceMappingURL=RatingIndicator.stories-5e6bec65.js.map
---
+++ main/assets/VariantManagement-29ee9a0c.js
@@ -0,0 +1,320 @@
+import{j as e}from"./jsx-runtime-d079401a.js";import{C as m}from"./DomRefTable.module-556f000b.js";import{D as d}from"./DocsHeader-23de8f1f.js";import{F as c}from"./Footer-f35933b9.js";import{V as o}from"./VariantItem-9acb3417.js";import"./index-f1f2c4b1.js";import{M as p,C as a,b as u,f as h,A as v}from"./chunk-S4VUQJ4A-84f5c535.js";import{S as V}from"./SubcomponentsSection-6138f2fc.js";import{C as g,D as s,W as S,a as x}from"./VariantManagement.stories-6948d3c7.js";import{u as l}from"./index-59d6410c.js";import{M as C}from"./index-72758f0c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-bb1f9706.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-d9b40dee.js";import"./UI5Element-a4bd3d38.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./utils-626dc1bf.js";import"./index-24720d23.js";import"./iframe-3b2630af.js";import"../sb-preview/runtime.js";import"./withWebComponent-7d7e5133.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-42236945.js";import"./Icons-74c917eb.js";import"./Device-6afa24d0.js";import"./Integer-f7f172c9.js";import"./class-map-0ab40ab9.js";import"./i18n-defaults-d675a36d.js";import"./index-1ce0bb97.js";import"./Avatar-8a894654.js";import"./ResizeHandler-0af4416f.js";import"./employee-05c578de.js";import"./index-30fb964e.js";import"./AriaLabelHelper-43a261ec.js";import"./index-c6a7ad7e.js";import"./Link-307a9e73.js";import"./MarkedEvents-b83081e9.js";import"./WrappingType-b81e595a.js";import"./index-43333f43.js";import"./TableOfContent-08fbc8de.js";import"./index-c1366ccd.js";import"./Label-34e3bd87.js";import"./index-b42968db.js";import"./index-4b38c9c4.js";import"./Button-7043cfcf.js";import"./index-7f2027af.js";import"./Popover-26ef4370.js";import"./PopupsCommon.css-f12c9479.js";import"./FocusableElements-66623361.js";import"./isElementHidden-01c07146.js";import"./decline-3cb804de.js";import"./i18n-defaults-2d2bf0b6.js";import"./MediaRange-25b98f31.js";import"./style-map-132a30d8.js";import"./BrowserScrollbar.css-309143b9.js";import"./UnableToLoad-e6707f4e.js";import"./IllustratedMessage-bf3a8ed1.js";import"./Title-f7b3e876.js";import"./i18n-defaults-2dcf78e8.js";import"./parameters-bundle.css-f9dc8928.js";import"./navigation-down-arrow-43636bd7.js";import"./search-92a01fd0.js";import"./index-99d317a4.js";import"./i18n-defaults-771c9378.js";import"./I18nContext-dd74d568.js";import"./stopPropagation-da74d54e.js";import"./index-46eb1e98.js";import"./Bar-4e8debe5.js";import"./index-cc780a6d.js";import"./Dialog-4aad3648.js";import"./ValueState-2c5e5904.js";import"./information-dbbb1c9a.js";import"./ValueState-ab6838cc.js";import"./index-8394f338.js";import"./ItemNavigation-0734b7c0.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-1653a4a5.js";import"./CheckBox-b06ff672.js";import"./accept-aa607c05.js";import"./favorite-e77b94de.js";import"./unfavorite-e8b9860a.js";import"./index-1c92e148.js";import"./RadioButton-86fe32a8.js";import"./index-ba331884.js";import"./index-28308256.js";import"./index-924397d3.js";import"./Input-e218b89d.js";import"./Suggestions.css-8d6efd26.js";import"./ResponsivePopoverCommon.css-870f7136.js";import"./ValueStateMessage.css-044f6113.js";import"./useIsomorphicId-ed1d334d.js";import"./index-b1068b77.js";import"./index-783f0efb.js";import"./index-7afba4a9.js";import"./ResponsivePopover-5c74b7f5.js";import"./index-7e1d7b95.js";import"./ListItemBase-e006c78e.js";import"./index-80658d76.js";import"./StandardListItem-e017edb3.js";import"./ListItem-657b9c84.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-b5a9fe30.js";import"./index-d203965f.js";import"./mapValues-e5c6f517.js";import"./_baseForOwn-24231fc1.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-00f63d78.js";import"./_baseUniq-edf521f4.js";import"./index-356e4a49.js";import"./index-85661a5f.js";import"./DatePicker-25edcbf7.js";import"./Calendar-e467a0b6.js";import"./Gregorian-e62abc1d.js";import"./LocaleData-e8db12a1.js";import"./slim-arrow-left-cd18fc92.js";import"./index-585ef307.js";import"./slim-arrow-down-9459236f.js";import"./GroupHeaderListItem-ca0e4d22.js";import"./Tokenizer-9b870757.js";import"./ScrollEnablement-dd3a6392.js";import"./animate-1a16ba20.js";import"./Token-69b8ac1e.js";import"./ToggleButton-ffb4fffb.js";import"./ComboBoxItem-677d5363.js";import"./index-7ae66584.js";import"./index-fb965b4c.js";import"./InvisibleMessage-2b547741.js";import"./index-3d805298.js";import"./index-5a6ec3ad.js";import"./overflow-fb44e468.js";import"./index-d2c40bbf.js";import"./CssSizeVariables-17481929.js";import"./CustomVariables-b0f63fee.js";import"./debounce-afefed63.js";import"./index-7feac488.js";import"./index-ff678518.js";import"./group-2-fcaaa3e0.js";import"./index-3f218aa5.js";import"./SegmentedButton-dcbc68e2.js";import"./index-472dec8a.js";import"./slideUp-745debd3.js";import"./AnimationMode-392b388d.js";import"./index-7f0661dd.js";import"./index-df6cbc06.js";import"./Media-6b270b80.js";function dn(n={}){const{wrapper:i}=Object.assign({},l(),n.components);return i?e.jsx(i,Object.assign({},n,{children:e.jsx(r,{})})):r();function r(){const t=Object.assign({h2:"h2",h1:"h1",p:"p",code:"code",strong:"strong",h3:"h3",pre:"pre",a:"a"},l(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:g}),`
+`,e.jsx(d,{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(a,{of:s}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(m,{of:s}),`
+`,e.jsx(t.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(t.h2,{id:"variantmanagement-with-custom-input",children:"VariantManagement with custom Input"}),`
+`,e.jsxs(t.p,{children:['You can customize the inputs inside the configuration dialogs ("Save View" and "Manage Views") by leveraging the corresponding ',e.jsx(t.code,{children:"inputProps"})," prop (",e.jsx(t.code,{children:"saveViewInputProps"})," and ",e.jsx(t.code,{children:"manageViewsInputProps"}),") of the ",e.jsx(t.code,{children:"VariantItem"}),` component.
+These props accept props of the `,e.jsx(t.code,{children:"Input"})," component with which you are able to overwrite internal implementations. E.g. ",e.jsx(t.code,{children:"valueState"})," and ",e.jsx(t.code,{children:"valueState"})," message."]}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"})," To prevent internal logic from being executed on save click, you can mark the ",e.jsx(t.code,{children:"onInput"})," event of the corresponding ",e.jsx(t.code,{children:"Input"})," component as invalid by setting ",e.jsx(t.code,{children:"event.isInvalid = true"})," inside the respective handler."]}),`
+`,e.jsx(C,{hideCloseButton:!0,children:"It is possible to overwrite internally used props. Please use with caution!"}),`
+`,e.jsx(a,{of:S,sourceState:"none"}),`
+`,e.jsx(t.h3,{id:"code-example",children:"Code Example"}),`
+`,e.jsxs("details",{style:{marginBlockEnd:"2rem"},children:[e.jsx("summary",{children:"Show Code"}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`export const VariantManagementWithCustomValidation = () => {
+  const [valueStateSaveView, setValueStateSaveView] = useState(undefined);
+  const [customSaveViewVariantText, setCustomSaveViewVariantText] = useState(
+    'Only alphanumeric chars in Save View input'
+  );
+  const [valueStateManageViews, setValueStateManageViews] = useState(undefined);
+  const [customManageViewsVariantText, setCustomManageViewsVariantText] = useState('Max 12 chars');
+
+  const handleSaveViewInput = (e) => {
+    // only allow alphanumeric and space characters
+    if (!e.target.value.match(/^[a-z0-9\\s]+$/i)) {
+      // mark variant as invalid (prevent internal logic from being applied)
+      e.isInvalid = true;
+      setValueStateSaveView(ValueState.Error);
+    } else {
+      // mark variant as valid again
+      e.isInvalid = false;
+      setValueStateSaveView(undefined);
+    }
+  };
+  const handleSaveAs = (e) => {
+    setCustomSaveViewVariantText(e.detail.children);
+  };
+
+  const handleManageViewInput = (e) => {
+    // only 12 or fewer characters
+    if (e.target.value.length > 12) {
+      // mark variant as invalid (prevent internal logic from being applied)
+      e.isInvalid = true;
+      setValueStateManageViews(ValueState.Error);
+    } else {
+      // mark variant as valid again
+      e.isInvalid = false;
+      setValueStateManageViews(undefined);
+    }
+  };
+  const handleSaveManageViews = (e) => {
+    // if is custom manage view variant and is not in error state, set children to new value
+    const isCustomManageViewsItem = e.detail.updatedVariants.find((item) => item['data-custom-manage-views']);
+    if (!valueStateManageViews && isCustomManageViewsItem) {
+      setCustomManageViewsVariantText(isCustomManageViewsItem.children);
+    }
+  };
+  return (
+    <VariantManagement onSaveAs={handleSaveAs} onSaveManageViews={handleSaveManageViews}>
+      <VariantItem
+        data-custom-save-view
+        saveViewInputProps={{
+          valueState: valueStateSaveView,
+          valueStateMessage: valueStateSaveView ? (
+            <div>Only alphanumeric and space characters allowed!</div>
+          ) : undefined,
+          onInput: handleSaveViewInput
+        }}
+      >
+        {customSaveViewVariantText}
+      </VariantItem>
+      <VariantItem
+        data-custom-manage-views
+        selected
+        manageViewsInputProps={{
+          valueState: valueStateManageViews,
+          valueStateMessage: valueStateManageViews ? <div>No more than 12 characters allowed!</div> : undefined,
+          onInput: handleManageViewInput
+        }}
+      >
+        {customManageViewsVariantText}
+      </VariantItem>
+    </VariantManagement>
+  );
+};
+`})})]}),`
+`,e.jsx(t.h2,{id:"basic-variantmanagement-implementation",children:"Basic VariantManagement Implementation"}),`
+`,e.jsxs(t.p,{children:["This example shows how a basic implementation of the ",e.jsx(t.code,{children:"VariantManagement"})," inside a ",e.jsx(t.code,{children:"FilterBar"})," could look like. For a more in depth documentation you can take a look at the official ",e.jsx(t.a,{href:"https://experience.sap.com/fiori-design-web/variant-management/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Fiori for Web Design Guidelines"}),"."]}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"}),` All views are applied automatically, so the "Apply Automatically" checkboxes in both dialogs won't be visible.`]}),`
+`,e.jsx(a,{of:x,sourceState:"none"}),`
+`,e.jsx(t.h3,{id:"code-example-1",children:"Code Example"}),`
+`,e.jsxs("details",{style:{marginBlockEnd:"2rem"},children:[e.jsx("summary",{children:"Show Code"}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`function FilterBarWithVariantManagement() {
+  const [selectedVariant, setSelectedVariant] = useState('Standard');
+  const [defaultVariant, setDefaultVariant] = useState('Standard');
+  const [customVariants, setCustomVariants] = useState<VariantItemPropTypes[]>([]);
+  const [isDirty, setIsDirty] = useState(false);
+  const [checkIfDiry, setCheckIfDirty] = useState(false);
+
+  // this should be persisted (e.g. by the local storage of the browser or on the server)
+  const initialVariantValues = useRef({
+    Standard: {
+      selectedCountry: 'Indonesia',
+      date: '',
+      selectedCodes: {}
+    }
+  });
+
+  const filterReducer = (state, action) => {
+    const { payload, type } = action;
+    setCheckIfDirty(true);
+    switch (type) {
+      case 'selectedCountry':
+        return { ...state, selectedCountry: payload };
+      case 'date':
+        return { ...state, date: payload };
+      case 'selectedCodes':
+        return { ...state, selectedCodes: payload };
+      case 'changeVariant':
+        return payload;
+      default:
+        console.warn('Unknown action type!');
+        return state;
+    }
+  };
+
+  const [filters, dispatchFiltersChange] = useReducer(filterReducer, initialVariantValues.current.Standard);
+  const { selectedCountry, date, selectedCodes } = filters;
+
+  const handleSelectChange = (e) => {
+    const { selectedOption } = e.detail;
+    dispatchFiltersChange({ type: 'selectedCountry', payload: selectedOption.textContent });
+  };
+  const handleDateChange = (e) => {
+    dispatchFiltersChange({ type: 'date', payload: e.detail.value });
+  };
+  const handleSelectedCodesChange = (e) => {
+    dispatchFiltersChange({
+      type: 'selectedCodes',
+      payload: e.detail.items.reduce((acc, cur) => {
+        acc[cur.dataset.code] = true;
+        return acc;
+      }, {})
+    });
+  };
+
+  const handleSaveAs = (e) => {
+    const {
+      variantItem: _omit,
+      selected: _omit2,
+      readOnly: _omit3,
+      labelReadOnly: _omit4,
+      ...variantItemProps
+    } = e.detail;
+    if (variantItemProps.isDefault) {
+      setDefaultVariant(variantItemProps.children);
+    }
+    setCustomVariants((prev) => [...prev, { ...variantItemProps, author: 'Current User' }]);
+    initialVariantValues.current[variantItemProps.children] = filters;
+    setSelectedVariant(variantItemProps.children);
+    setIsDirty(false);
+  };
+
+  const handleSave = (e) => {
+    const { variantItem: _omit, selected: _omit2, ...variantItemProps } = e.detail;
+    initialVariantValues.current[variantItemProps.children] = filters;
+    setIsDirty(false);
+  };
+
+  const handleSaveManageViews = (e) => {
+    const { deletedVariants, updatedVariants } = e.detail;
+    deletedVariants.forEach((variant) => {
+      delete initialVariantValues.current[variant.children];
+      setCustomVariants((prev) => prev.filter((item) => item.children !== variant.children));
+    });
+    updatedVariants.forEach((variant) => {
+      const { variantItem: _omit, selected: _omit2, ...variantItemProps } = variant;
+      initialVariantValues.current[variant.children] = variantItemProps;
+      if (variant.isDefault) {
+        setDefaultVariant(variant.children);
+      }
+      if (variant.selected) {
+        setSelectedVariant(variant.children);
+      }
+      setCustomVariants((prev) => {
+        return prev.map((item) => {
+          if (item.children === variant.prevVariant.children) {
+            return variantItemProps;
+          }
+          return item;
+        });
+      });
+    });
+  };
+
+  const handleSelect = (e) => {
+    const privateSelectedVariant = e.detail.selectedVariant.children;
+    dispatchFiltersChange({
+      type: 'changeVariant',
+      payload: initialVariantValues.current[privateSelectedVariant]
+    });
+    setSelectedVariant(privateSelectedVariant);
+  };
+
+  useEffect(() => {
+    if (checkIfDiry) {
+      const hasChanged = Object.entries(initialVariantValues.current[selectedVariant]).some(([key, val]) => {
+        if (key === 'selectedCodes') {
+          const selectedCodesLength = Object.keys(filters.selectedCodes).length;
+          if (selectedCodesLength > 0 && Object.keys(val).length !== selectedCodesLength) {
+            return true;
+          }
+          return Object.entries(filters.selectedCodes).some(([code, bool]) => {
+            return val[code] !== bool;
+          });
+        }
+        return filters[key] !== val;
+      });
+      setCheckIfDirty(false);
+      setIsDirty(hasChanged);
+    }
+  }, [checkIfDiry, selectedVariant]);
+
+  return (
+    <>
+      <FilterBar
+        header={
+          <VariantManagement
+            hideApplyAutomatically
+            dirtyState={isDirty}
+            onSaveAs={handleSaveAs}
+            onSave={handleSave}
+            onSelect={handleSelect}
+            onSaveManageViews={handleSaveManageViews}
+          >
+            <VariantItem
+              selected={selectedVariant === 'Standard'}
+              global
+              isDefault={defaultVariant === 'Standard'}
+              author="SAP"
+              readOnly
+              labelReadOnly
+            >
+              Standard
+            </VariantItem>
+            {customVariants.map((variantItemProps) => {
+              return (
+                <VariantItem
+                  key={variantItemProps.children}
+                  {...variantItemProps}
+                  selected={selectedVariant === variantItemProps.children}
+                  isDefault={defaultVariant === variantItemProps.children}
+                >
+                  {variantItemProps.children}
+                </VariantItem>
+              );
+            })}
+          </VariantManagement>
+        }
+      >
+        <FilterGroupItem label="Countries">
+          <Select onChange={handleSelectChange}>
+            <Option selected={selectedCountry === 'Indonesia'}>Indonesia</Option>
+            <Option selected={selectedCountry === 'Costa Rica'}>Costa Rica</Option>
+            <Option selected={selectedCountry === 'Slovakia'}>Slovakia</Option>
+            <Option selected={selectedCountry === 'Iceland'}>Iceland</Option>
+            <Option selected={selectedCountry === 'Malta'}>Malta</Option>
+            <Option selected={selectedCountry === 'Guyana'}>Guyana</Option>
+            <Option selected={selectedCountry === 'Spain'}>Spain</Option>
+            <Option selected={selectedCountry === 'Austria'}>Austria</Option>
+          </Select>
+        </FilterGroupItem>
+        <FilterGroupItem label="Date">
+          <DatePicker value={date} onChange={handleDateChange} />
+        </FilterGroupItem>
+        <FilterGroupItem label="Company Code">
+          <MultiComboBox onSelectionChange={handleSelectedCodesChange}>
+            <MultiComboBoxItem text="001" selected={selectedCodes['001']} data-code="001" />
+            <MultiComboBoxItem text="002" selected={selectedCodes['002']} data-code="002" />
+            <MultiComboBoxItem text="003" selected={selectedCodes['003']} data-code="003" />
+            <MultiComboBoxItem text="004" selected={selectedCodes['004']} data-code="004" />
+            <MultiComboBoxItem text="005" selected={selectedCodes['005']} data-code="005" />
+          </MultiComboBox>
+        </FilterGroupItem>
+      </FilterBar>
+      <Form
+        style={{ marginBlockStart: '2rem' }}
+        columnsS={1}
+        columnsM={1}
+        columnsL={1}
+        columnsXL={1}
+        labelSpanM={2}
+        labelSpanL={2}
+        labelSpanXL={2}
+      >
+        <FormItem label="Current View">
+          <Text>{selectedVariant}</Text>
+        </FormItem>
+        <FormGroup titleText="Filters">
+          <FormItem label="Selected Country">
+            <Text>{selectedCountry}</Text>
+          </FormItem>
+          <FormItem label="Selected Date">
+            <Text>{date}</Text>
+          </FormItem>
+          <FormItem label="Selected Company Codes">
+            <Text>{Object.keys(selectedCodes).join(', ')}</Text>
+          </FormItem>
+        </FormGroup>
+      </Form>
+    </>
+  );
+}
+`})})]}),`
+`,e.jsx(u,{children:V}),`
+`,e.jsx(t.h2,{id:"variantitem",children:"VariantItem"}),`
+`,e.jsx(h,{of:o}),`
+`,e.jsx(v,{of:o}),`
+`,e.jsx(c,{})]})}}export{dn as default};
+//# sourceMappingURL=VariantManagement-29ee9a0c.js.map
