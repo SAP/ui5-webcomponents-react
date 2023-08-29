60566c9eb95b5b9046ac76d69c15a2c7dae40105
Lukas742 <lukas.harbarth@sap.com>
2023-08-29 05:45:23 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@06c7f2728fb17176bc4e950cb417bf08eb526deb 🚀

---
+++ main/assets/FilterBar-d8c2acfc.js
@@ -0,0 +1,121 @@
+import{j as t}from"./jsx-runtime-d079401a.js";import{C as s,A as l}from"./DomRefTable.module-c6673833.js";import{D as c}from"./DocsHeader-52e549a2.js";import{F as d}from"./Footer-b6412483.js";import{a as m}from"./index-5d9c9a41.js";import"./index-f1f2c4b1.js";import{M as x,C as r,b as u,f as h}from"./chunk-S4VUQJ4A-f421359e.js";import{S as g}from"./SubcomponentsSection-6138f2fc.js";import{C,D as p,W as b,I as j}from"./FilterBar.stories-ad40b118.js";import{u as a}from"./index-59d6410c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-ffff92aa.js";import"./iframe-1631fc3d.js";import"../sb-preview/runtime.js";import"./UI5Element-6dcea8dd.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7d7e5133.js";import"./utils-626dc1bf.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-b7f7d9e7.js";import"./Icons-e937042c.js";import"./decline-d1fe7fdd.js";import"./i18n-defaults-2d2bf0b6.js";import"./information-a40f0e71.js";import"./class-map-0ab40ab9.js";import"./Button-6bb9ed58.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-6afa24d0.js";import"./i18n-defaults-d675a36d.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-5ee8a8c4.js";import"./index-f493c92d.js";import"./Integer-f7f172c9.js";import"./index-ae687b3a.js";import"./Avatar-faa225d6.js";import"./ResizeHandler-e83234a4.js";import"./employee-f0b5ef8f.js";import"./index-df273129.js";import"./index-0d3a53ba.js";import"./Link-34530090.js";import"./WrappingType-b81e595a.js";import"./index-fb6e8b5e.js";import"./TableOfContent-f4a650a3.js";import"./index-b70bd0f1.js";import"./Label-c4db564a.js";import"./index-b42968db.js";import"./index-6d255f71.js";import"./index-1ca22b1f.js";import"./Popover-8ece70a3.js";import"./PopupsCommon.css-2b73575b.js";import"./FocusableElements-ae22402e.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-132a30d8.js";import"./BrowserScrollbar.css-22391551.js";import"./index-99d317a4.js";import"./i18n-defaults-a506bd94.js";import"./I18nContext-dd74d568.js";import"./index-91c5b6ac.js";import"./overflow-da55ec27.js";import"./stopPropagation-da74d54e.js";import"./index-7a01e6cf.js";import"./ToggleButton-2d52195e.js";import"./CssSizeVariables-596b7aba.js";import"./CustomVariables-b0f63fee.js";import"./useIsRTL-f58f04b0.js";import"./debounce-5c77766a.js";import"./index-d8d3f7dc.js";import"./index-ff678518.js";import"./group-2-2a84c13d.js";import"./search-560fb697.js";import"./index-d220d257.js";import"./Dialog-ea4c9a60.js";import"./ValueState-2c5e5904.js";import"./ValueState-ab6838cc.js";import"./index-cb09ba84.js";import"./Bar-86528487.js";import"./parameters-bundle.css-f9dc8928.js";import"./index-fc8f8559.js";import"./Title-e46bbbf5.js";import"./index-48d3d888.js";import"./InvisibleMessage-b8889f2b.js";import"./slim-arrow-down-fd4d5a7b.js";import"./ListItemBase-c655abbb.js";import"./ItemNavigation-f231b836.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-713a4f98.js";import"./ResponsivePopover-b745dfe5.js";import"./StandardListItem-17582cb4.js";import"./ListItem-43381874.js";import"./RadioButton-f48638b8.js";import"./CheckBox-6d778514.js";import"./accept-d5698387.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-1acb3967.js";import"./ResponsivePopoverCommon.css-32174dbe.js";import"./ValueStateMessage.css-ee317505.js";import"./index-a89ab3fd.js";import"./index-f4399c07.js";import"./SegmentedButton-6deaa090.js";import"./index-fec5b81f.js";import"./Input-ee14120b.js";import"./Suggestions.css-64d314d5.js";import"./index-662a390a.js";import"./index-78a5fd76.js";import"./slideUp-745debd3.js";import"./animate-1a16ba20.js";import"./AnimationMode-c7999104.js";import"./index-ebb11e50.js";import"./index-c52cd53c.js";import"./index-d203965f.js";import"./mapValues-e5c6f517.js";import"./_baseForOwn-24231fc1.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-00f63d78.js";import"./_baseUniq-edf521f4.js";import"./index-356e4a49.js";import"./index-a665df06.js";import"./index-d08223e6.js";import"./slim-arrow-up-331641e5.js";import"./Media-6b270b80.js";import"./GlobalStyleClasses-7209358d.js";import"./PageBackgroundDesign-7e89cd25.js";import"./VariantItem-5727ede4.js";import"./UnableToLoad-d38caa94.js";import"./IllustratedMessage-9b3a4d82.js";import"./i18n-defaults-2dcf78e8.js";import"./navigation-down-arrow-1994eedb.js";import"./favorite-cc7e741d.js";import"./unfavorite-35561551.js";import"./index-92e5ca8d.js";import"./index-310bf27d.js";import"./useIsomorphicId-ed1d334d.js";import"./index-52a76918.js";import"./index-9bf5b0e2.js";import"./index-7aa3f979.js";import"./index-75b64e46.js";import"./index-6ab43541.js";import"./Float-99d99064.js";import"./less-2ba877c6.js";import"./add-935b19d6.js";import"./index-7559cc08.js";import"./index-7309ea18.js";import"./Token-3537a188.js";import"./Tokenizer-414bea12.js";import"./ScrollEnablement-dd3a6392.js";import"./index-382d3c48.js";import"./index-8c9c60e5.js";import"./index-10b5ed40.js";import"./ComboBoxItem-7da53959.js";import"./GroupHeaderListItem-7829eb80.js";import"./index-3677c17f.js";import"./Calendar-80cdf4bd.js";import"./Gregorian-d2b85539.js";import"./LocaleData-4f1c322f.js";import"./slim-arrow-left-60149d1a.js";import"./DatePicker-9a7dd06e.js";import"./index-abf19077.js";import"./index-3b45950b.js";function yo(o={}){const{wrapper:i}=Object.assign({},a(),o.components);return i?t.jsx(i,Object.assign({},o,{children:t.jsx(n,{})})):n();function n(){const e=Object.assign({h2:"h2",h1:"h1",p:"p",h3:"h3",pre:"pre",code:"code"},a(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(x,{of:C}),`
+`,t.jsx(c,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(e.h2,{id:"filterbar-with-logic",children:"FilterBar with logic"}),`
+`,t.jsx(e.p,{children:"This is a basic example of how the FilterBar could be used inside an app."}),`
+`,t.jsx(r,{of:b}),`
+`,t.jsx(e.h3,{id:"code",children:"Code"}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show Code"}),t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const FilterBarComponent = (args) => {
+  const [age, setAge] = useState(37);
+  const [countries, setCountries] = useState({});
+  const [currency, setCurrency] = useState('USD');
+  const [date, setDate] = useState('');
+  const [dateRange, setDateRange] = useState('');
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+  const handleCountriesChange = (e) => {
+    setCountries(
+      e.detail.items.reduce((acc, cur) => {
+        return { ...acc, [cur.getAttribute('text').toLowerCase()]: true };
+      }, {})
+    );
+  };
+  const handleCurrencyChange = (e) => {
+    setCurrency(e.detail.selectedOption.textContent);
+  };
+  const handleDateChange = (e) => {
+    if (e.detail.valid) {
+      setDate(e.detail.value);
+    }
+  };
+  const handleDateRangeChange = (e) => {
+    if (e.detail.valid) {
+      setDateRange(e.detail.value);
+    }
+  };
+  return (
+    <>
+      <FilterBar>
+        <FilterGroupItem label="Age" active={!!age} required>
+          <StepInput value={age} onChange={handleAgeChange} required />
+        </FilterGroupItem>
+        <FilterGroupItem label="Countries" active={Object.keys(countries).length > 0}>
+          <MultiComboBox onSelectionChange={handleCountriesChange}>
+            <MultiComboBoxItem text="Argentina" selected={countries.argentina} />
+            <MultiComboBoxItem text="Bulgaria" selected={countries.bulgaria} />
+            <MultiComboBoxItem text="Finland" selected={countries.finland} />
+            <MultiComboBoxItem text="Germany" selected={countries.germany} />
+            <MultiComboBoxItem text="Ireland" selected={countries.ireland} />
+            <MultiComboBoxItem text="Ukraine" selected={countries.ukraine} />
+            <MultiComboBoxItem text="USA" selected={countries.usa} />
+          </MultiComboBox>
+        </FilterGroupItem>
+        <FilterGroupItem label="Currency" active={!!currency}>
+          <Select onChange={handleCurrencyChange}>
+            <Option additionalText="€" selected={currency === 'EUR'}>
+              EUR
+            </Option>
+            <Option additionalText="$" selected={currency === 'USD'}>
+              USD
+            </Option>
+            <Option additionalText="£" selected={currency === 'GBP'}>
+              GBP
+            </Option>
+            <Option additionalText="₺" selected={currency === 'TRY'}>
+              TRY
+            </Option>
+            <Option additionalText="¥" selected={currency === 'JPY'}>
+              JPY
+            </Option>
+          </Select>
+        </FilterGroupItem>
+        <FilterGroupItem label="Date" active={!!date}>
+          <DatePicker value={date} onChange={handleDateChange} style={{ minWidth: 'auto' }} on />
+        </FilterGroupItem>
+        <FilterGroupItem label="Date Range" active={!!dateRange} visibleInFilterBar={false}>
+          <DateRangePicker value={dateRange} onChange={handleDateRangeChange} style={{ minWidth: 'auto' }} on />
+        </FilterGroupItem>
+      </FilterBar>
+      <FlexBox direction={FlexBoxDirection.Column}>
+        <FlexBox>
+          <Label showColon>Age</Label>
+          <Text>{age}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Countries</Label>
+          <Text>{JSON.stringify(countries)}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Currency</Label>
+          <Text>{currency}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Date</Label>
+          <Text>{date}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Date Range</Label>
+          <Text>{dateRange}</Text>
+        </FlexBox>
+      </FlexBox>
+    </>
+  );
+};
+`})})]}),`
+`,t.jsx(e.h2,{id:"filterbar-in-a-dynamicpage",children:"FilterBar in a DynamicPage"}),`
+`,t.jsxs(e.p,{children:["When a FilterBar is used inside e.g. a DynamicPage, ",t.jsx(e.code,{children:"hideToolbar"})," should be set to ",t.jsx(e.code,{children:"true"}),", since the ",t.jsx(e.code,{children:"VariantManagement"}),` of
+the page is then responsible for the different views.`]}),`
+`,t.jsx(r,{of:j}),`
+`,t.jsx(u,{children:g}),`
+`,t.jsx(e.h2,{id:"filtergroupitem",children:"FilterGroupItem"}),`
+`,t.jsx(h,{of:m}),`
+`,t.jsx(l,{of:m}),`
+`,t.jsx(d,{})]})}}export{yo as default};
+//# sourceMappingURL=FilterBar-d8c2acfc.js.map
---
+++ main/assets/Tree-6124fbdd.js
@@ -0,0 +1,109 @@
+import{j as e}from"./jsx-runtime-d079401a.js";import{C as p,b as s}from"./DomRefTable.module-c6673833.js";import{D as d}from"./DocsHeader-52e549a2.js";import{F as h}from"./Footer-b6412483.js";import"./index-f1f2c4b1.js";import{M as c,C as a,b as o}from"./chunk-S4VUQJ4A-f421359e.js";import{D as u}from"./DomRefTable-2403f73a.js";import{S as T}from"./SubcomponentsSection-6138f2fc.js";import{C as x,D as m,L as y,T as f,a as g}from"./Tree.stories-41d51b8f.js";import{u as l}from"./index-59d6410c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-ffff92aa.js";import"./iframe-1631fc3d.js";import"../sb-preview/runtime.js";import"./UI5Element-6dcea8dd.js";import"./CustomElementsScopeUtils-137da8c8.js";import"./withWebComponent-7d7e5133.js";import"./utils-626dc1bf.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-b7f7d9e7.js";import"./Icons-e937042c.js";import"./decline-d1fe7fdd.js";import"./i18n-defaults-2d2bf0b6.js";import"./information-a40f0e71.js";import"./class-map-0ab40ab9.js";import"./Button-6bb9ed58.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./Device-6afa24d0.js";import"./i18n-defaults-d675a36d.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-5ee8a8c4.js";import"./index-f493c92d.js";import"./Integer-f7f172c9.js";import"./index-ae687b3a.js";import"./Avatar-faa225d6.js";import"./ResizeHandler-e83234a4.js";import"./employee-f0b5ef8f.js";import"./index-df273129.js";import"./index-0d3a53ba.js";import"./Link-34530090.js";import"./WrappingType-b81e595a.js";import"./index-fb6e8b5e.js";import"./TableOfContent-f4a650a3.js";import"./index-b70bd0f1.js";import"./Label-c4db564a.js";import"./index-b42968db.js";import"./index-6d255f71.js";import"./index-1ca22b1f.js";import"./Popover-8ece70a3.js";import"./PopupsCommon.css-2b73575b.js";import"./FocusableElements-ae22402e.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-132a30d8.js";import"./BrowserScrollbar.css-22391551.js";import"./index-d203965f.js";import"./mapValues-e5c6f517.js";import"./_baseForOwn-24231fc1.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-00f63d78.js";import"./_baseUniq-edf521f4.js";import"./index-356e4a49.js";import"./general-leave-request-3cb542ee.js";import"./index-c52cd53c.js";import"./BusyIndicator-713a4f98.js";import"./index-ebb11e50.js";import"./Tree-7d557193.js";import"./ValueState-2c5e5904.js";import"./ListItem-43381874.js";import"./ListItemBase-c655abbb.js";import"./ItemNavigation-f231b836.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./RadioButton-f48638b8.js";import"./CheckBox-6d778514.js";import"./accept-d5698387.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-1acb3967.js";import"./navigation-right-arrow-6a4c9285.js";import"./navigation-down-arrow-1994eedb.js";import"./ValueState-ab6838cc.js";const S=`**Since:** 0.10.0
+
+The \`TreeItem\` represents a node in a tree structure, shown as a \`List\`.  
+This is the item to use inside a \`Tree\`. You can represent an arbitrary tree structure by recursively nesting tree items.
+
+### CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`TreeItem\` exposes the following CSS Shadow Parts:
+
+- title - Used to style the title of the tree list item
+- additionalText - Used to style the additionalText of the tree list item
+- icon - Used to style the icon of the tree list item
+`,j=`**Since:** undefined
+
+The \`TreeItemCustom\` represents a node in a tree structure, shown as a \`List\`.  
+This is the item to use inside a \`Tree\`. You can represent an arbitrary tree structure by recursively nesting tree items. You can use this item to put any custom content inside the tree item.
+
+### CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`TreeItemCustom\` exposes the following CSS Shadow Parts:
+
+- title - Used to style the title of the tree list item
+- additionalText - Used to style the additionalText of the tree list item
+- icon - Used to style the icon of the tree list item
+`,C=`## Usage
+
+### When to use:
+
+- To display hierarchically structured items.
+- To select one or more items out of a set of hierarchically structured items.
+
+### When not to use:
+
+- To display items not hierarchically strcutured. In this case, use the List component.
+- To select one item from a very small number of non-hierarchical items. Select or ComboBox might be more appropriate.
+- The hierarchy turns out to have only two levels. In this case, use List with group items.
+
+## Keyboard Handling
+
+The \`Tree\` provides advanced keyboard handling. The user can use the following keyboard shortcuts in order to navigate trough the tree:
+
+- \\[UP/DOWN\\] - Navigates up and down the tree items that are currently visible.
+- \\[RIGHT\\] - Drills down the tree by expanding the tree nodes.
+- \\[LEFT\\] - Goes up the tree and collapses the tree nodes.
+
+The user can use the following keyboard shortcuts to perform selection, when the \`mode\` property is in use:
+
+- \\[SPACE\\] - Selects the currently focused item upon keyup.
+- \\[ENTER\\] - Selects the currently focused item upon keydown.
+`,b=[{name:"walk",visibility:"public",parameters:[{name:"callback",type:"function",optional:!1,description:"function to execute on each node of the tree with 3 arguments: the node, the level and the index"}],description:"Perform Depth-First-Search walk on the tree and run a callback on each node"}];function ot(r={}){const{wrapper:n}=Object.assign({},l(),r.components);return n?e.jsx(n,Object.assign({},r,{children:e.jsx(i,{})})):i();function i(){const t=Object.assign({h2:"h2",h1:"h1",h3:"h3",pre:"pre",code:"code"},l(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:x}),`
+`,e.jsx(d,{since:"0.10.0"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(p,{of:m}),`
+`,e.jsx(o,{children:C}),`
+`,e.jsx(u,{rows:b}),`
+`,e.jsx(t.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"lazy-load-treeitems",children:"Lazy Load TreeItems"}),`
+`,e.jsx(a,{of:y}),`
+`,e.jsx(t.h3,{id:"code",children:"Code"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`const LazyLoadingTree = () => {
+  const [lazyChildren, setLazyChildren] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleItemToggle = (e) => {
+    if (e.detail.item.dataset.id === 'lazychildren' && !lazyChildren) {
+      e.preventDefault();
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setLazyChildren(
+          <>
+            <TreeItem text="Tree 3.1" />
+            <TreeItem text="Tree 3.2" />
+          </>
+        );
+        e.detail.item.toggle();
+      }, 3000);
+    }
+  };
+  return (
+    <BusyIndicator active={loading} style={{ width: '100%' }}>
+      <Tree onItemToggle={handleItemToggle}>
+        <TreeItem text="Has pre-loaded children">
+          <TreeItem text="Tree 1.1" />
+          <TreeItem text="Tree 1.2" />
+        </TreeItem>
+        <TreeItem text="Has no children" />
+        <TreeItem text="Has children but not yet loaded" hasChildren icon={dlCloutIcon} data-id="lazychildren">
+          {lazyChildren}
+        </TreeItem>
+      </Tree>
+    </BusyIndicator>
+  );
+};
+`})}),`
+`,e.jsx(o,{children:T}),`
+`,e.jsx(t.h2,{id:"treeitem",children:"TreeItem"}),`
+`,e.jsx(s,{of:f}),`
+`,e.jsx(o,{children:S}),`
+`,e.jsx(t.h2,{id:"treeitemcustom",children:"TreeItemCustom"}),`
+`,e.jsx(s,{of:g}),`
+`,e.jsx(o,{children:j}),`
+`,e.jsx(h,{})]})}}export{ot as default};
+//# sourceMappingURL=Tree-6124fbdd.js.map
