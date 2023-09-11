df4647abcf0db52b0900af4e3e6ebe49cc86b7d9
MarcusNotheis <marcus.notheis@sap.com>
2023-09-11 06:36:05 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@6bbfc85120a9635feace25d357ae633d480a5791 🚀

---
+++ main/assets/IllustratedMessage-51216357.js
@@ -0,0 +1,32 @@
+import{j as t}from"./jsx-runtime-d079401a.js";import{C as m}from"./DomRefTable.module-dfec042a.js";import{D as l}from"./DocsHeader-bfba24a9.js";import{F as p}from"./Footer-3b12d18c.js";import"./index-f1f2c4b1.js";import{M as a,C as c,b as d}from"./chunk-S4VUQJ4A-ad48e2da.js";import{M as h,a as u}from"./index-9d0785e8.js";import{C as x,D as s}from"./IllustratedMessage.stories-51562de5.js";import{I as j}from"./index-5715591f.js";import{u as n}from"./index-59d6410c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-203b56de.js";import"./UI5Element-e590cd60.js";import"./CustomElementsScopeUtils-264b8a39.js";import"./utils-e9940b93.js";import"./index-1d41d8c1.js";import"./iframe-82ad3897.js";import"../sb-preview/runtime.js";import"./withWebComponent-b4c67a1a.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-b897d66f.js";import"./Icons-65b34360.js";import"./Integer-f7f172c9.js";import"./class-map-e1b423a4.js";import"./i18n-defaults-d5195a49.js";import"./index-0e47890e.js";import"./Avatar-60d18bb8.js";import"./ResizeHandler-34ecee99.js";import"./employee-78fe46d2.js";import"./alert-7494fabe.js";import"./index-69b5c8eb.js";import"./AriaLabelHelper-43a261ec.js";import"./index-daab9cdc.js";import"./Link-a373b451.js";import"./MarkedEvents-b83081e9.js";import"./WrappingType-b81e595a.js";import"./index-343af056.js";import"./TableOfContent-12586314.js";import"./index-7d7570d3.js";import"./Label-69062358.js";import"./index-c74c9f7f.js";import"./index-2502fcd6.js";import"./Button-4505b9f6.js";import"./index-6330ef1f.js";import"./Popover-742c9988.js";import"./PopupUtils-f407c5e4.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-97942203.js";import"./FocusableElements-d024bb16.js";import"./isElementHidden-01c07146.js";import"./decline-2f6198d2.js";import"./i18n-defaults-bdef1cce.js";import"./MediaRange-25b98f31.js";import"./style-map-ecc46a93.js";import"./BrowserScrollbar.css-4a87524a.js";import"./index-92d824d3.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./information-e97f915a.js";import"./IllustratedMessage-aab77b4a.js";import"./Title-0fb688d3.js";import"./i18n-defaults-2dcf78e8.js";import"./parameters-bundle.css-1b860e2a.js";const g=`## Structure
+
+The IllustratedMessage consists of the following elements, which are displayed below each other in the following order:
+
+- Illustration
+- Title
+- Subtitle
+- Actions
+
+## Usage
+
+\`IllustratedMessage\` is meant to be used inside container component, for example a \`Card\`, a \`Dialog\` or a \`Page\`
+`;function At(o={}){const{wrapper:i}=Object.assign({},n(),o.components);return i?t.jsx(i,Object.assign({},o,{children:t.jsx(r,{})})):r();function r(){const e=Object.assign({h2:"h2",code:"code",br:"br",p:"p",strong:"strong",pre:"pre"},n(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:x}),`
+`,t.jsx(l,{since:"0.18.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(c,{of:s,story:{height:"300px"},layout:"fullscreen"}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(m,{of:s}),`
+`,t.jsx(d,{children:g}),`
+`,t.jsx(e.h2,{id:"fully-customizable-title",children:"Fully customizable title"}),`
+`,t.jsx(h,{design:u.Warning,hideCloseButton:!0,children:t.jsxs(t.Fragment,{children:[" As the ",t.jsx(e.code,{children:"title"})," slot name collides with the standard HTML ",t.jsx(e.code,{children:"title"})," attribute, using the"," ",t.jsx(e.code,{children:"title"})," prop will not work. ",t.jsx(e.br,{})," You must pass the ",t.jsx(e.code,{children:"title"})," slot as part of the components ",t.jsx(e.code,{children:"children"}),"."]})}),`
+`,t.jsxs(e.p,{children:["In some cases you might need to fully customize the title element of the ",t.jsx(e.code,{children:"IllustratedMessage"}),`.
+You can achieve this by using the `,t.jsx(e.code,{children:"title"})," slot."]}),`
+`,t.jsxs(e.p,{children:[t.jsx(e.strong,{children:"Note:"})," Using this slot, the default title text of illustration and the value of ",t.jsx(e.code,{children:"titleText"})," property will be overwritten."]}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`<IllustratedMessage>
+  <h5 slot="title">This is my fully customized title</h5>
+</IllustratedMessage>
+`})}),`
+`,t.jsx(j,{children:t.jsx("h5",{slot:"title",children:"This is my fully customized title"})}),`
+`,t.jsx(p,{})]})}}export{At as default};
+//# sourceMappingURL=IllustratedMessage-51216357.js.map
