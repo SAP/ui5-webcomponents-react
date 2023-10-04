556239ba13d4888c3173e74daa0101e621bc03c9
MarcusNotheis <marcus.notheis@sap.com>
2023-10-04 08:55:13 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@4de5c6e5e919e28d7d5e6ad78068e2257ec7771e 🚀

---
+++ main/assets/ActionSheet-a341024d.js
@@ -0,0 +1,104 @@
+import{j as e}from"./jsx-runtime-d079401a.js";import{C as c}from"./DomRefTable.module-cb531aaf.js";import{D as p}from"./DocsHeader-83b87078.js";import{F as h}from"./Footer-492ce19d.js";import"./index-f1f2c4b1.js";import{M as a,C as m}from"./chunk-S4VUQJ4A-16eeda01.js";import{D as d}from"./DomRefTable-40040ccf.js";import{C as l,D as i}from"./ActionSheet.stories-dbbf0a4c.js";import{R as u}from"./ResponsivePopoverDomRef-b2da8c70.js";import{u as s}from"./index-59d6410c.js";import{M as x}from"./index-77cb4d81.js";import"./react-jss.esm-2e5f50f2.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-dff384db.js";import"./UI5Element-9ae3ac4a.js";import"./CustomElementsScopeUtils-a6667bf6.js";import"./utils-eb830979.js";import"./index-0437ccfb.js";import"./iframe-37fe1ecd.js";import"../sb-preview/runtime.js";import"./withWebComponent-70ea33bf.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-085ae741.js";import"./Icons-aa504494.js";import"./Integer-f7f172c9.js";import"./class-map-a35dc276.js";import"./i18n-defaults-edeeca68.js";import"./index-a6fa8444.js";import"./Avatar-54e46144.js";import"./ResizeHandler-a1ba39c5.js";import"./employee-bd01b92f.js";import"./alert-f95f2d2e.js";import"./index-d09a05f2.js";import"./AriaLabelHelper-43a261ec.js";import"./index-a495f196.js";import"./Link-27f41eeb.js";import"./MarkedEvents-b83081e9.js";import"./WrappingType-b81e595a.js";import"./index-12e0da73.js";import"./TableOfContent-71580458.js";import"./index-6a54ba85.js";import"./Label-2351773c.js";import"./index-c74c9f7f.js";import"./index-e4a7bbe8.js";import"./Button-bda40b74.js";import"./index-b1928530.js";import"./Popover-f88acbeb.js";import"./PopupUtils-ebc1cf8a.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-afd5e311.js";import"./FocusableElements-ea9d71fb.js";import"./isElementHidden-01c07146.js";import"./decline-06bf09c2.js";import"./i18n-defaults-bdef1cce.js";import"./MediaRange-25b98f31.js";import"./style-map-0570471d.js";import"./BrowserScrollbar.css-35e4e851.js";import"./index-92d824d3.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./accept-a3b0d940.js";import"./delete-e4b5bf05.js";import"./i18n-defaults-f9eebbcc.js";import"./I18nContext-dd74d568.js";import"./useIsRTL-fd85e99a.js";import"./CustomVariables-f4d4caba.js";import"./CssSizeVariables-596b7aba.js";import"./index-8748fa7a.js";import"./ResponsivePopover-3603f824.js";import"./Dialog-6ca0f060.js";import"./ValueState-2c5e5904.js";import"./information-31cb56bb.js";import"./Title-9e1fec85.js";function He(t={}){const{wrapper:o}=Object.assign({},s(),t.components);return o?e.jsx(o,Object.assign({},t,{children:e.jsx(r,{})})):r();function r(){const n=Object.assign({h2:"h2",p:"p",h4:"h4",code:"code",pre:"pre",strong:"strong",em:"em",a:"a"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{of:l}),`
+`,e.jsx(p,{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h2,{id:"example",children:"Example"}),`
+`,e.jsx(m,{of:i}),`
+`,e.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(c,{of:i}),`
+`,e.jsxs(n.p,{children:[e.jsx(d,{rows:u})," |"]}),`
+`,e.jsx(n.h2,{id:"opening-actionsheets",children:"Opening ActionSheets"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h4,{id:"since-0220",children:"since 0.22.0"}),`
+`,e.jsxs(n.p,{children:["We recommend opening and closing the ",e.jsx(n.code,{children:"ActionSheet"})," component in a declarative way using the ",e.jsx(n.code,{children:"open"})," and ",e.jsx(n.code,{children:"opener"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const MyComponentWithActionSheet = () => {
+  const [actionSheetIsOpen, setActionSheetIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        id={'openActionSheetBtn'}
+        onClick={() => {
+          setActionSheetIsOpen(true);
+        }}
+      >
+        Open Action Sheet
+      </Button>
+      <ActionSheet
+        opener={'openActionSheetBtn'}
+        open={actionSheetIsOpen}
+        onAfterClose={() => {
+          setActionSheetIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.p,{children:e.jsxs(n.strong,{children:["Opening an ",e.jsx(n.code,{children:"ActionSheet"})," by reference and not by ",e.jsx(n.code,{children:"id"})]})}),`
+`,e.jsxs(n.p,{children:["This web component exposes a way to pass a reference of an element instead of an ",e.jsx(n.code,{children:"id"})," to the ",e.jsx(n.code,{children:"opener"})," prop. Since this is not supported when passing the reference in a declarative way to a React ",e.jsx(n.code,{children:"prop"}),`, you have to attach the ref directly on the web component.
+You can do that by e.g. leveraging a React Ref, and then set the corresponding property there.`]}),`
+`,e.jsx(x,{hideCloseButton:!0,children:`Changing properties outside of the React rendering cycle can lead to unwanted behavior, please be extra cautious when
+  using this approach!`}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const ActionSheetComponent = () => {
+  const popoverRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const handleOpenerClick = (e) => {
+    if (popoverRef.current) {
+      popoverRef.current.opener = e.target;
+      setOpen((prev) => !prev);
+    }
+  };
+  return (
+    <>
+      <Button onClick={handleOpenerClick}>Opener</Button>
+      <ActionSheet ref={popoverRef} open={open}>
+        {/* Content */}
+      </ActionSheet>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.h4,{id:"before-0220",children:"before 0.22.0"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"ActionSheets"})," can only be opened by attaching a ",e.jsx(n.code,{children:"ref"})," to the component and then call the corresponding ",e.jsx(n.strong,{children:e.jsx(n.code,{children:"showAt"})})," method. The method receives the target element - ",e.jsxs(n.em,{children:["on which the ",e.jsx(n.code,{children:"ActionSheet"})," is to be opened"]})," - as parameter."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const ActionSheetComponent = () => {
+  const actionSheetRef = useRef(null);
+  const onButtonClick = (e) => {
+    actionSheetRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open ActionSheet</Button>
+      <ActionSheet ref={actionSheetRef}>
+        <Button icon="add">Accept</Button>
+        <Button>Reject</Button>
+        <Button>This is my super long text!</Button>
+      </ActionSheet>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.h2,{id:"using-actionsheets-inside-other-components",children:"Using ActionSheets inside other components"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"ActionSheets"}),` are often used within other components, when opened this could sometimes have unwanted side effects.
+In this case, we recommend using `,e.jsx(n.a,{href:"https://reactjs.org/docs/portals.html",target:"_blank",rel:"nofollow noopener noreferrer",children:"createPortal"})," to mount the ",e.jsx(n.code,{children:"ActionSheet"})," outside of the DOM hierarchy of the parent component."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-JSX",children:`const ActionSheetComponent = () => {
+  const actionSheetRef = useRef(null);
+  const onButtonClick = (e) => {
+    actionSheetRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open ActionSheet</Button>
+      {createPortal(
+        <ActionSheet ref={actionSheetRef}>
+          <Button icon="add">Accept</Button>
+          <Button>Reject</Button>
+          <Button>This is my super long text!</Button>
+        </ActionSheet>,
+        document.body
+      )}
+    </>
+  );
+};
+`})}),`
+`,e.jsx(h,{})]})}}export{He as default};
+//# sourceMappingURL=ActionSheet-a341024d.js.map
---
+++ main/assets/CustomComponentsSlots-b89e6981.js
@@ -0,0 +1,41 @@
+import{j as t}from"./jsx-runtime-d079401a.js";import{M as m}from"./chunk-S4VUQJ4A-16eeda01.js";import{B as i}from"./index-fd9598cd.js";import{B as p}from"./index-e4a7bbe8.js";import"./DomRefTable.module-cb531aaf.js";import"./index-f1f2c4b1.js";import{F as a}from"./Footer-492ce19d.js";import{u as s}from"./index-59d6410c.js";import"./iframe-37fe1ecd.js";import"../sb-preview/runtime.js";import"./index-92d824d3.js";import"./index-c74c9f7f.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./Bar-a5fb143c.js";import"./UI5Element-9ae3ac4a.js";import"./CustomElementsScopeUtils-a6667bf6.js";import"./withWebComponent-70ea33bf.js";import"./utils-eb830979.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./ResizeHandler-a1ba39c5.js";import"./class-map-a35dc276.js";import"./parameters-bundle.css-1bee123e.js";import"./Button-bda40b74.js";import"./Icon-085ae741.js";import"./Icons-aa504494.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./i18n-defaults-edeeca68.js";import"./react-jss.esm-2e5f50f2.js";import"./index-77cb4d81.js";import"./decline-06bf09c2.js";import"./i18n-defaults-bdef1cce.js";import"./information-31cb56bb.js";import"./alert-f95f2d2e.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-dff384db.js";import"./index-0437ccfb.js";import"./Integer-f7f172c9.js";import"./index-a6fa8444.js";import"./Avatar-54e46144.js";import"./employee-bd01b92f.js";import"./index-d09a05f2.js";import"./index-a495f196.js";import"./Link-27f41eeb.js";import"./WrappingType-b81e595a.js";import"./index-12e0da73.js";import"./index-6a54ba85.js";import"./Label-2351773c.js";import"./index-b1928530.js";import"./Popover-f88acbeb.js";import"./PopupUtils-ebc1cf8a.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-afd5e311.js";import"./FocusableElements-ea9d71fb.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-0570471d.js";import"./BrowserScrollbar.css-35e4e851.js";const c=()=>t.jsx("div",{children:"Start"}),d=()=>t.jsx(p,{children:"Close"}),l=()=>t.jsx(i,{startContent:t.jsx(c,{}),endContent:t.jsx(d,{}),children:t.jsx("div",{children:"I'm not a custom component"})}),h=n=>t.jsx("div",{slot:n.slot,children:"Start"}),u=n=>t.jsx(p,{slot:n.slot,children:"Close"}),x=()=>t.jsx(i,{startContent:t.jsx(h,{}),endContent:t.jsx(u,{}),children:t.jsx("div",{children:"I'm not a custom component"})});function Mt(n={}){const{wrapper:r}=Object.assign({},s(),n.components);return r?t.jsx(r,Object.assign({},n,{children:t.jsx(e,{})})):e();function e(){const o=Object.assign({h1:"h1",p:"p",code:"code",pre:"pre"},s(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Handling Slots"}),`
+`,t.jsx(o.h1,{id:"adding-custom-components-to-slots",children:"Adding custom components to slots"}),`
+`,t.jsxs(o.p,{children:["With our wrapper we provide an easy way to consume the ",t.jsx(o.code,{children:"slots"})," of the UI5 Web Components by attaching them to a ",t.jsx(o.code,{children:"prop"}),`.
+In most cases you don't have to take anything into account, and the props can be used in a typical React way. However, there is an exception with custom components:`]}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`const BarStart = () => {
+  return <div>Start</div>;
+};
+const BarEnd = () => {
+  return <Button>Close</Button>;
+};
+export const BarComponent = () => {
+  return (
+    <Bar startContent={<BarStart />} endContent={<BarEnd />}>
+      <div>I'm not a custom component</div>
+    </Bar>
+  );
+};
+`})}),`
+`,t.jsxs(o.p,{children:["The ",t.jsx(o.code,{children:"BarComponent"})," would compile to this:"]}),`
+`,t.jsx(l,{}),`
+`,t.jsxs(o.p,{children:[`As you can see, both custom components are not displayed in the right place.
+Our wrapper does add the `,t.jsx(o.code,{children:"slot"})," prop to the custom component, but the most outer HTML-Element/component (e.g. ",t.jsx(o.code,{children:"<div>Start</div>"}),") does not accept it and therefore cannot use it."]}),`
+`,t.jsxs(o.p,{children:["To fix this the ",t.jsx(o.code,{children:"slot"})," prop must also be passed to the outer element of the component:"]}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`const BarStart = (props) => {
+  return <div slot={props.slot}>Start</div>;
+};
+const BarEnd = (props) => {
+  return <Button slot={props.slot}>Close</Button>;
+};
+export const BarComponent = () => {
+  return (
+    <Bar startContent={<BarStart />} endContent={<BarEnd />}>
+      <div>I'm not a custom component</div>
+    </Bar>
+  );
+};
+`})}),`
+`,t.jsxs(o.p,{children:["Now the ",t.jsx(o.code,{children:"BarComponent"})," would compile to this:"]}),`
+`,t.jsx(x,{}),`
+`,t.jsx(a,{style:{position:"fixed",bottom:0,zIndex:2}})]})}}export{Mt as default};
+//# sourceMappingURL=CustomComponentsSlots-b89e6981.js.map
