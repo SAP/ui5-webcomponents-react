73e6ca3c545d56ac21c328ce181f1419ae96b26b
Lukas742 <lukas.harbarth@sap.com>
2023-10-06 09:54:55 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@122dec93be31c14a3ce3ebfa958834d36e8e345c 🚀

---
+++ main/assets/NotificationListGroupItem-19c87fb7.js
@@ -0,0 +1,26 @@
+import{j as t}from"./jsx-runtime-d079401a.js";import{C as s,b as a}from"./DomRefTable.module-d1a9aebd.js";import{D as c}from"./DocsHeader-06519ad8.js";import{F as l}from"./Footer-e87f6e62.js";import"./index-f1f2c4b1.js";import{M as d,C as f,b as r}from"./chunk-S4VUQJ4A-acc67901.js";import{S as h}from"./SubcomponentsSection-6138f2fc.js";import{a as x}from"./index-e52537f4.js";import{N as j}from"./NotificationActionDescription-7983d4e2.js";import{C as u,D as n}from"./NotificationListGroupItem.stories-243614ad.js";import{e as S}from"./utils-b1ef2149.js";import{u as e}from"./index-59d6410c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-bdca9b6b.js";import"./iframe-cd6f9814.js";import"../sb-preview/runtime.js";import"./UI5Element-9ae3ac4a.js";import"./CustomElementsScopeUtils-a6667bf6.js";import"./withWebComponent-70ea33bf.js";import"./utils-eb830979.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-085ae741.js";import"./Icons-aa504494.js";import"./decline-06bf09c2.js";import"./i18n-defaults-bdef1cce.js";import"./information-31cb56bb.js";import"./alert-f95f2d2e.js";import"./class-map-a35dc276.js";import"./Button-bda40b74.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./i18n-defaults-edeeca68.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-dff384db.js";import"./index-96ac7308.js";import"./Integer-f7f172c9.js";import"./index-94fd078d.js";import"./Avatar-54e46144.js";import"./ResizeHandler-a1ba39c5.js";import"./employee-bd01b92f.js";import"./index-a892d9e5.js";import"./index-2ebe0a2b.js";import"./Link-27f41eeb.js";import"./WrappingType-b81e595a.js";import"./index-93a893a2.js";import"./TableOfContent-b4452ea7.js";import"./index-984c2413.js";import"./Label-2351773c.js";import"./index-c74c9f7f.js";import"./index-35f5e80f.js";import"./index-3ae35b02.js";import"./Popover-f88acbeb.js";import"./PopupUtils-ebc1cf8a.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-afd5e311.js";import"./FocusableElements-ea9d71fb.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-0570471d.js";import"./BrowserScrollbar.css-35e4e851.js";import"./index-92d824d3.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./BusyIndicator-6d3cbaa3.js";import"./ListItemBase-7c3bbca9.js";import"./ItemNavigation-53cee471.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./overflow-00c3f55f.js";import"./parameters-bundle.css-1bee123e.js";import"./i18n-defaults-2dcf78e8.js";import"./navigation-right-arrow-8c9f2450.js";const C=`## Usage
+
+The component can be used in a standard \`List\`.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`NotificationListGroupItem\` exposes the following CSS Shadow Parts:
+
+- title-text - Used to style the titleText of the notification list group item
+`;function qt(o={}){const{wrapper:m}=Object.assign({},e(),o.components);return m?t.jsx(m,Object.assign({},o,{children:t.jsx(p,{})})):p();function p(){const i=Object.assign({h2:"h2"},e(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(d,{of:u}),`
+`,t.jsx(c,{since:"0.10.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(i.h2,{id:"example",children:"Example"}),`
+`,t.jsx(f,{of:n}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:n}),`
+`,t.jsx(r,{children:C}),`
+`,`
+`,`
+`,t.jsx(r,{children:h}),`
+`,t.jsx(i.h2,{id:"notificationaction",children:"NotificationAction"}),`
+`,t.jsx(a,{hideHTMLPropsNote:!0,exclude:S,of:x}),`
+`,t.jsx(r,{children:j}),`
+`,t.jsx(l,{})]})}}export{qt as default};
+//# sourceMappingURL=NotificationListGroupItem-19c87fb7.js.map
