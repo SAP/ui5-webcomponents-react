f323d6a68496c559735f60fe272c32a8861866d3
Lukas742 <lukas.harbarth@sap.com>
2023-10-09 13:35:23 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0594b4843429323db9f612959cda5352fa37d1f2 🚀

---
+++ main/assets/SelectDialog-fc8fe7e8.js
@@ -0,0 +1,57 @@
+import{j as o}from"./jsx-runtime-d079401a.js";import{C as s}from"./DomRefTable.module-62bb9ca3.js";import{D as c}from"./DocsHeader-1e8afcd2.js";import{F as a}from"./Footer-ae80212b.js";import"./index-f1f2c4b1.js";import{M as l,C as n}from"./chunk-S4VUQJ4A-7702cbce.js";import{D as d}from"./DomRefTable-692b172b.js";import{D as h}from"./DialogDomRef-ffa73df1.js";import{C as g,D as p,M as x}from"./SelectDialog.stories-96826f23.js";import{u as m}from"./index-59d6410c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-4128dc98.js";import"./iframe-be52c6f7.js";import"../sb-preview/runtime.js";import"./UI5Element-9ae3ac4a.js";import"./CustomElementsScopeUtils-a6667bf6.js";import"./withWebComponent-70ea33bf.js";import"./utils-eb830979.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-085ae741.js";import"./Icons-aa504494.js";import"./decline-06bf09c2.js";import"./i18n-defaults-bdef1cce.js";import"./information-31cb56bb.js";import"./alert-f95f2d2e.js";import"./class-map-a35dc276.js";import"./Button-bda40b74.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./i18n-defaults-edeeca68.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-dff384db.js";import"./index-9d4834b0.js";import"./Integer-f7f172c9.js";import"./index-e81d5db7.js";import"./Avatar-54e46144.js";import"./ResizeHandler-a1ba39c5.js";import"./employee-bd01b92f.js";import"./index-9feb3c05.js";import"./index-87505218.js";import"./Link-27f41eeb.js";import"./WrappingType-b81e595a.js";import"./index-12e0da73.js";import"./TableOfContent-39d674ee.js";import"./index-129729fe.js";import"./Label-2351773c.js";import"./index-c74c9f7f.js";import"./index-f751c1b1.js";import"./index-6d294c79.js";import"./Popover-f88acbeb.js";import"./PopupUtils-ebc1cf8a.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-afd5e311.js";import"./FocusableElements-ea9d71fb.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-0570471d.js";import"./BrowserScrollbar.css-35e4e851.js";import"./index-92d824d3.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./search-4e7efcf2.js";import"./CssSizeVariables-596b7aba.js";import"./index-99d317a4.js";import"./i18n-defaults-f9eebbcc.js";import"./I18nContext-dd74d568.js";import"./index-f81fc388.js";import"./overflow-00c3f55f.js";import"./stopPropagation-da74d54e.js";import"./index-35ae044c.js";import"./ToggleButton-10e80b89.js";import"./CustomVariables-f4d4caba.js";import"./useIsRTL-fd85e99a.js";import"./debounce-5c77766a.js";import"./index-bd72dce1.js";import"./ListItemBase-7c3bbca9.js";import"./ItemNavigation-53cee471.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-6d3cbaa3.js";import"./index-ba022fbb.js";import"./Dialog-6ca0f060.js";import"./ValueState-2c5e5904.js";import"./ValueState-ab6838cc.js";import"./index-43d348b0.js";import"./Title-9e1fec85.js";import"./index-7e7c4cd1.js";import"./Input-bfce6809.js";import"./Suggestions.css-07dccdad.js";import"./ResponsivePopoverCommon.css-f284520a.js";import"./ValueStateMessage.css-29f8f350.js";import"./index-60edd0b1.js";import"./index-cb944dbb.js";import"./StandardListItem-a534f611.js";import"./ListItem-8f7be23d.js";import"./RadioButton-0df5d1bd.js";import"./CheckBox-05effb75.js";import"./accept-a3b0d940.js";import"./HasPopup-47461347.js";import"./slim-arrow-right-562fbac6.js";function jt(i={}){const{wrapper:r}=Object.assign({},m(),i.components);return r?o.jsx(r,Object.assign({},i,{children:o.jsx(e,{})})):e();function e(){const t=Object.assign({h2:"h2",h4:"h4",p:"p",code:"code",pre:"pre",strong:"strong",h3:"h3"},m(),i.components);return o.jsxs(o.Fragment,{children:[o.jsx(l,{of:g}),`
+`,o.jsx(c,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(n,{of:p}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:p}),`
+`,o.jsx(d,{rows:h}),`
+`,o.jsx(t.h2,{id:"opening-selectdialogs",children:"Opening SelectDialogs"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h4,{id:"since-0220",children:"since 0.22.0"}),`
+`,o.jsxs(t.p,{children:["We recommend opening and closing the ",o.jsx(t.code,{children:"SelectDialog"})," component in a declarative way using the ",o.jsx(t.code,{children:"open"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const MyComponentWithSelectDialog = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+      >
+        Open SelectDialog
+      </Button>
+      <SelectDialog open={dialogIsOpen} headerText="Select Product">
+        <StandardListItem image="pathToImg" description="description">
+          Product
+        </StandardListItem>
+      </SelectDialog>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(t.h4,{id:"before-0220",children:"before 0.22.0"}),`
+`,o.jsxs(t.p,{children:[o.jsx(t.code,{children:"SelectDialog"}),"s can be opened by attaching a ",o.jsx(t.code,{children:"ref"})," to the component and then calling the corresponding ",o.jsx(t.strong,{children:o.jsx(t.code,{children:"show"})})," method."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const SelectDialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open SelectDialog</Button>
+      <SelectDialog ref={dialogRef} headerText="Select Product">
+        <StandardListItem image="pathToImg" description="description">
+          Product
+        </StandardListItem>
+      </SelectDialog>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h3,{id:"selectdialog-in-multiselect-mode-with-search",children:"SelectDialog in MultiSelect mode with search"}),`
+`,o.jsx(n,{of:x}),`
+`,o.jsx(a,{})]})}}export{jt as default};
+//# sourceMappingURL=SelectDialog-fc8fe7e8.js.map
