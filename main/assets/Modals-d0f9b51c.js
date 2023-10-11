8d1cce3ee2b94eb5e0c6f23de525a728babd0b38
Lukas742 <lukas.harbarth@sap.com>
2023-10-11 12:09:16 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@6e9f89b9e92281a7b0358ba30bc799cf115745e2 🚀

---
+++ main/assets/Modals-d0f9b51c.js
@@ -0,0 +1,126 @@
+import{j as e}from"./jsx-runtime-d079401a.js";import"./DomRefTable.module-80135917.js";import{D as h}from"./DocsHeader-444be332.js";import{F as l}from"./Footer-d17099b8.js";import{M as a}from"./index-26071b46.js";import"./index-f1f2c4b1.js";import{M as x,C as r,A as o}from"./chunk-S4VUQJ4A-1abd7803.js";import{C as p,D as j,P as m,R as u,M as w,a as g,T as b}from"./Modals.stories-1199063a.js";import{u as d}from"./index-59d6410c.js";import{P as n}from"./index-b2d93b00.js";import{D as f}from"./index-2a9a9f15.js";import{P as M}from"./index-40a85b1c.js";import{R as y}from"./index-8b17509e.js";import{M as P}from"./index-2dd46660.js";import{T as v}from"./index-7e3d4521.js";import"./react-jss.esm-2e5f50f2.js";import"./index-f7bf7588.js";import"./iframe-75f2c25f.js";import"../sb-preview/runtime.js";import"./UI5Element-9ae3ac4a.js";import"./CustomElementsScopeUtils-a6667bf6.js";import"./withWebComponent-70ea33bf.js";import"./utils-eb830979.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-085ae741.js";import"./Icons-aa504494.js";import"./decline-06bf09c2.js";import"./i18n-defaults-bdef1cce.js";import"./information-31cb56bb.js";import"./alert-f95f2d2e.js";import"./class-map-a35dc276.js";import"./Button-bda40b74.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./i18n-defaults-edeeca68.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-dff384db.js";import"./index-66bcba84.js";import"./Integer-f7f172c9.js";import"./index-275c1e24.js";import"./Avatar-54e46144.js";import"./ResizeHandler-a1ba39c5.js";import"./employee-bd01b92f.js";import"./index-5be3c915.js";import"./index-8b070acb.js";import"./Link-27f41eeb.js";import"./WrappingType-b81e595a.js";import"./index-7122209e.js";import"./TableOfContent-ce56997a.js";import"./index-aad88b27.js";import"./Label-2351773c.js";import"./index-c74c9f7f.js";import"./index-c38a5bef.js";import"./Popover-f88acbeb.js";import"./PopupUtils-ebc1cf8a.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-afd5e311.js";import"./FocusableElements-ea9d71fb.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-0570471d.js";import"./BrowserScrollbar.css-35e4e851.js";import"./index-99d317a4.js";import"./i18n-defaults-f9eebbcc.js";import"./I18nContext-dd74d568.js";import"./stopPropagation-da74d54e.js";import"./CssSizeVariables-596b7aba.js";import"./useIsomorphicId-ed1d334d.js";import"./index-688cb26d.js";import"./Title-9e1fec85.js";import"./index-0dafb1e2.js";import"./ValueState-ab6838cc.js";import"./index-92d824d3.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./ModalsContext-c6c22f4c.js";import"./index-c7babd6b.js";import"./Bar-a5fb143c.js";import"./parameters-bundle.css-1bee123e.js";import"./slideUp-745debd3.js";import"./animate-1a16ba20.js";import"./AnimationMode-5a8d55c8.js";import"./slim-arrow-right-562fbac6.js";import"./Dialog-6ca0f060.js";import"./ValueState-2c5e5904.js";import"./ResponsivePopover-3603f824.js";import"./ListItemBase-7c3bbca9.js";import"./ItemNavigation-53cee471.js";import"./TabbableElements-8e49b367.js";import"./debounce-9c2fb7dd.js";import"./BusyIndicator-6d3cbaa3.js";import"./StandardListItem-a534f611.js";import"./ListItem-8f7be23d.js";import"./RadioButton-0df5d1bd.js";import"./CheckBox-05effb75.js";import"./accept-a3b0d940.js";import"./HasPopup-47461347.js";function Ms(t={}){const{wrapper:i}=Object.assign({},d(),t.components);return i?e.jsx(i,Object.assign({},t,{children:e.jsx(c,{})})):c();function c(){const s=Object.assign({h2:"h2",p:"p",strong:"strong",code:"code",h4:"h4",pre:"pre",table:"table",thead:"thead",tr:"tr",th:"th",tbody:"tbody",td:"td",em:"em"},d(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(x,{of:p}),`
+`,e.jsx(h,{since:"0.22.2"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"usage-notes",children:"Usage Notes"}),`
+`,e.jsx(s.p,{children:e.jsxs(s.strong,{children:["In order to use these helpers, please make sure that your application is wrapped in the ",e.jsx(s.code,{children:"<ThemeProvider />"})," component."]})}),`
+`,e.jsx(s.p,{children:"We are offering those helpers methods both as hooks and static methods:"}),`
+`,e.jsx(s.p,{children:e.jsx(s.code,{children:"Modals.useShowXZY"})}),`
+`,e.jsxs(s.p,{children:[`Use this hook when you are in a React context where you are allowed to use hooks.
+Calling the hook returns a memoized function, which you can execute to show the popup by passing the props and an optional container.
+`,e.jsx(s.strong,{children:"This should always be the preferred option!"})]}),`
+`,e.jsx(s.p,{children:e.jsx(s.code,{children:"Modals.showXZY"})}),`
+`,e.jsxs(s.p,{children:["Use this static helper in case you are not in a React context (-> you can't use hooks), e.g. in a ",e.jsx(s.code,{children:"redux"}),` reducer.
+You can pass the props and an optional container directly.`]}),`
+`,e.jsx("br",{}),`
+`,e.jsx(s.h2,{id:"dialog",children:"Dialog"}),`
+`,e.jsx(r,{of:j}),`
+`,e.jsx(s.h4,{id:"usage",children:"Usage"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+// when you can use hooks
+const showDialog = Modals.useShowDialog();
+const { ref, close } = showDialog(props, container);
+
+// when you can't use hooks (e.g. inside a redux reducer)
+const { ref, close } = Modals.showDialog(props, container);
+`})}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Parameters"})}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Parameter"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"props"})}),e.jsxs(s.td,{children:["All supported ",e.jsx(s.code,{children:"Dialog"})," props (see table below). ",e.jsx(s.code,{children:"open"})," will always be set to ",e.jsx(s.code,{children:"true"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.em,{children:e.jsx(s.code,{children:"container"})})}),e.jsxs(s.td,{children:["Optional container where the ",e.jsx(s.code,{children:"Dialog"})," should be mounted. Defaults to ",e.jsx(s.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Return Value"})}),`
+`,e.jsxs(s.p,{children:["The ",e.jsx(s.code,{children:"showDialog"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Property"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"ref"})}),e.jsxs(s.td,{children:["React ",e.jsx(s.code,{children:"RefObject"})," which can be used to get interact with the ",e.jsx(s.code,{children:"Dialog"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"close()"})}),e.jsxs(s.td,{children:["Convenience Method for closing the ",e.jsx(s.code,{children:"Dialog"}),"."]})]})]})]}),`
+`,e.jsx(n,{header:e.jsx(s.h4,{className:"noMargin",children:"Dialog Props"}),collapsed:!0,children:e.jsx(o,{of:f,exclude:["open"]})}),`
+`,e.jsx(s.h2,{id:"popover",children:"Popover"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(s.h4,{id:"usage-1",children:"Usage"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+// when you can use hooks
+const showPopover = Modals.useShowPopover();
+const { ref, close } = showPopover(props, container);
+
+// when you can't use hooks (e.g. inside a redux reducer)
+const { ref, close } = Modals.showPopover(props, container);
+`})}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Parameters"})}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Parameter"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"props"})}),e.jsxs(s.td,{children:["All supported ",e.jsx(s.code,{children:"Popover"})," props (see table below).",e.jsx(s.code,{children:"open"})," will always be set to ",e.jsx(s.code,{children:"true"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.em,{children:e.jsx(s.code,{children:"container"})})}),e.jsxs(s.td,{children:["Optional container where the ",e.jsx(s.code,{children:"Popover"})," should be mounted. Defaults to ",e.jsx(s.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Return Value"})}),`
+`,e.jsxs(s.p,{children:["The ",e.jsx(s.code,{children:"showPopover"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Property"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"ref"})}),e.jsxs(s.td,{children:["React ",e.jsx(s.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(s.code,{children:"Popover"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"close()"})}),e.jsxs(s.td,{children:["Convenience Method for closing the ",e.jsx(s.code,{children:"Popover"}),"."]})]})]})]}),`
+`,e.jsx(n,{header:e.jsx(s.h4,{className:"noMargin",children:"Popover Props"}),collapsed:!0,children:e.jsx(o,{of:M,exclude:["open"]})}),`
+`,e.jsx(s.h2,{id:"responsivepopover",children:"ResponsivePopover"}),`
+`,e.jsx(r,{of:u}),`
+`,e.jsx(s.h4,{id:"usage-2",children:"Usage"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+// when you can use hooks
+const showResponsivePopover = Modals.useShowResponsivePopover();
+const { ref, close } = showResponsivePopover(props, container);
+
+// when you can't use hooks (e.g. inside a redux reducer)
+const { ref, close } = Modals.showResponsivePopover(props, container);
+`})}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Parameters"})}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Parameter"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"props"})}),e.jsxs(s.td,{children:["All supported ",e.jsx(s.code,{children:"ResponsivePopover"})," props (see table below). ",e.jsx(s.code,{children:"open"})," will always be set to ",e.jsx(s.code,{children:"true"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.em,{children:e.jsx(s.code,{children:"container"})})}),e.jsxs(s.td,{children:["Optional container where the ",e.jsx(s.code,{children:"ResponsivePopover"})," should be mounted. Defaults to ",e.jsx(s.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Return Value"})}),`
+`,e.jsxs(s.p,{children:["The ",e.jsx(s.code,{children:"showResponsivePopover"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Property"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"ref"})}),e.jsxs(s.td,{children:["React ",e.jsx(s.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(s.code,{children:"ResponsivePopover"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"close()"})}),e.jsxs(s.td,{children:["Convenience Method for closing the ",e.jsx(s.code,{children:"ResponsivePopover"}),"."]})]})]})]}),`
+`,e.jsx(n,{header:e.jsx(s.h4,{className:"noMargin",children:"ResponsivePopover Props"}),collapsed:!0,children:e.jsx(o,{of:y,exclude:["open"]})}),`
+`,e.jsx(s.h2,{id:"menu",children:"Menu"}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"since 1.8.0"})}),`
+`,e.jsx(r,{of:w}),`
+`,e.jsx(s.h4,{id:"usage-3",children:"Usage"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+// when you can use hooks
+const showMenu = Modals.useShowMenu();
+const { ref, close } = showMenu(props, container);
+
+// when you can't use hooks (e.g. inside a redux reducer)
+const { ref, close } = Modals.showMenu(props, container);
+`})}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Parameters"})}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Parameter"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"props"})}),e.jsxs(s.td,{children:["All supported ",e.jsx(s.code,{children:"Menu"})," props (see table below). ",e.jsx(s.code,{children:"open"})," will always be set to ",e.jsx(s.code,{children:"true"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.em,{children:e.jsx(s.code,{children:"container"})})}),e.jsxs(s.td,{children:["Optional container where the ",e.jsx(s.code,{children:"Menu"})," should be mounted. Defaults to ",e.jsx(s.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Return Value"})}),`
+`,e.jsxs(s.p,{children:["The ",e.jsx(s.code,{children:"Menu"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Property"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"ref"})}),e.jsxs(s.td,{children:["React ",e.jsx(s.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(s.code,{children:"Menu"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"close()"})}),e.jsxs(s.td,{children:["Convenience Method for closing the ",e.jsx(s.code,{children:"Menu"}),"."]})]})]})]}),`
+`,e.jsx(n,{header:e.jsx(s.h4,{className:"noMargin",children:"Menu Props"}),collapsed:!0,children:e.jsx(o,{of:P,exclude:["open"]})}),`
+`,e.jsx(s.h2,{id:"messagebox",children:"MessageBox"}),`
+`,e.jsx(r,{of:g}),`
+`,e.jsx(s.h4,{id:"usage-4",children:"Usage"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+// when you can use hooks
+const showMessageBox = Modals.useShowMessageBox();
+const { ref, close } = showMessageBox(props, container);
+
+// when you can't use hooks (e.g. inside a redux reducer)
+const { ref, close } = Modals.showMessageBox(props, container);
+`})}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Parameters"})}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Parameter"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"props"})}),e.jsxs(s.td,{children:["All supported ",e.jsx(s.code,{children:"MessageBox"})," props (see table below). ",e.jsx(s.code,{children:"open"})," will always be set to ",e.jsx(s.code,{children:"true"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.em,{children:e.jsx(s.code,{children:"container"})})}),e.jsxs(s.td,{children:["Optional container where the ",e.jsx(s.code,{children:"MessageBox"})," should be mounted. Defaults to ",e.jsx(s.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(s.p,{children:e.jsx(s.strong,{children:"Return Value"})}),`
+`,e.jsxs(s.p,{children:["The ",e.jsx(s.code,{children:"showMessageBox"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Property"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"ref"})}),e.jsxs(s.td,{children:["React ",e.jsx(s.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(s.code,{children:"MessageBox"}),"."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"close()"})}),e.jsxs(s.td,{children:["Convenience Method for closing the ",e.jsx(s.code,{children:"MessageBox"}),"."]})]})]})]}),`
+`,e.jsx(n,{header:e.jsx(s.h4,{className:"noMargin",children:"MessageBox Props"}),collapsed:!0,children:e.jsx(o,{of:a,exclude:["open"]})}),`
+`,e.jsx(s.h2,{id:"toast",children:"Toast"}),`
+`,e.jsx(r,{of:b}),`
+`,e.jsx(s.h4,{id:"usage-5",children:"Usage"}),`
+`,e.jsx(s.pre,{children:e.jsx(s.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+// when you can use hooks
+const showToast = Modals.useShowToast();
+const { ref } = showToast(props, container);
+
+// when you can't use hooks (e.g. inside a redux reducer)
+const { ref } = Modals.showToast(props, container);
+`})}),`
+`,e.jsx(s.p,{children:"**Parameters **"}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Parameter"}),e.jsx(s.th,{children:"Description"})]})}),e.jsxs(s.tbody,{children:[e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"props"})}),e.jsxs(s.td,{children:["All supported ",e.jsx(s.code,{children:"Toast"})," props(see table below)."]})]}),e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.em,{children:e.jsx(s.code,{children:"container"})})}),e.jsxs(s.td,{children:["Optional container where the ",e.jsx(s.code,{children:"Toast"})," should be mounted.Defaults to ",e.jsx(s.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(s.p,{children:"**Return Value **"}),`
+`,e.jsxs(s.p,{children:["The",e.jsx(s.code,{children:"showToast"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(s.table,{children:[e.jsx(s.thead,{children:e.jsxs(s.tr,{children:[e.jsx(s.th,{children:"Property"}),e.jsx(s.th,{children:"Description"})]})}),e.jsx(s.tbody,{children:e.jsxs(s.tr,{children:[e.jsx(s.td,{children:e.jsx(s.code,{children:"ref"})}),e.jsxs(s.td,{children:["React ",e.jsx(s.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(s.code,{children:"Toast"}),"."]})]})})]}),`
+`,e.jsx(n,{header:e.jsx(s.h4,{className:"noMargin",children:"Toast Props "}),collapsed:!0,children:e.jsx(o,{of:v})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(l,{})]})}}export{Ms as default};
+//# sourceMappingURL=Modals-d0f9b51c.js.map
