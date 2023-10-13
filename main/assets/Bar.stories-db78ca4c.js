29a590a119ed282adcf696eb9e1046e95f072456
MarcusNotheis <marcus.notheis@sap.com>
2023-10-13 07:34:20 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@660cbf9db3f716a8ab7b933a61e8a9f5fcc1f1ee 🚀

---
+++ main/assets/Bar.stories-db78ca4c.js
@@ -0,0 +1,51 @@
+import{M as B,C as m,e as p,b as D}from"./chunk-S4VUQJ4A-910e0ec1.js";import{B as n}from"./index-441a06d3.js";import{B as c}from"./Bar-a5fb143c.js";import{B as d}from"./index-7c4b0d91.js";import{I as h}from"./index-b8a702fc.js";import{L as u}from"./index-34c892ea.js";import{B as g}from"./Button-bda40b74.js";import{T as x}from"./index-0310a224.js";import"./nav-back-3669d2c8.js";import{A as F}from"./DomRefTable.module-c13c6200.js";import{G as b,D as k}from"./DocsHeader-6710e46c.js";import{F as v}from"./Footer-a5f9d6d5.js";import{j as t}from"./jsx-runtime-d079401a.js";import"./index-f1f2c4b1.js";import{u as l}from"./index-59d6410c.js";import"./iframe-4d092b24.js";import"../sb-preview/runtime.js";import"./index-92d824d3.js";import"./index-c74c9f7f.js";import"./mapValues-57c90777.js";import"./_baseForOwn-2252ef3e.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./_getPrototype-73159a4c.js";import"./_baseUniq-61c59340.js";import"./index-356e4a49.js";import"./withWebComponent-70ea33bf.js";import"./utils-eb830979.js";import"./CustomElementsScopeUtils-a6667bf6.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./UI5Element-9ae3ac4a.js";import"./slot-76e48863.js";import"./ResizeHandler-a1ba39c5.js";import"./class-map-a35dc276.js";import"./parameters-bundle.css-1bee123e.js";import"./Input-bfce6809.js";import"./Icon-085ae741.js";import"./Icons-aa504494.js";import"./ValueState-2c5e5904.js";import"./Integer-f7f172c9.js";import"./AriaLabelHelper-43a261ec.js";import"./getActiveElement-bcae01ed.js";import"./decline-06bf09c2.js";import"./i18n-defaults-bdef1cce.js";import"./Suggestions.css-07dccdad.js";import"./information-31cb56bb.js";import"./alert-f95f2d2e.js";import"./Popover-f88acbeb.js";import"./PopupUtils-ebc1cf8a.js";import"./PopupsCommon.css-afd5e311.js";import"./FocusableElements-ea9d71fb.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-0570471d.js";import"./BrowserScrollbar.css-35e4e851.js";import"./i18n-defaults-edeeca68.js";import"./ResponsivePopoverCommon.css-f284520a.js";import"./ValueStateMessage.css-29f8f350.js";import"./ValueState-ab6838cc.js";import"./Link-27f41eeb.js";import"./MarkedEvents-b83081e9.js";import"./WrappingType-b81e595a.js";import"./Title-9e1fec85.js";import"./react-jss.esm-2e5f50f2.js";import"./index-ad10be0b.js";import"./index-743fc8f1.js";import"./clsx-1229b3e0.js";import"./ThemingParameters-7e2e4e30.js";import"./addCustomCSSWithScoping-dff384db.js";import"./index-722c3992.js";import"./index-b1a8eaba.js";import"./Avatar-54e46144.js";import"./employee-bd01b92f.js";import"./index-fcb210af.js";import"./index-12e0da73.js";import"./TableOfContent-6b2061f3.js";import"./index-6413fc7a.js";import"./Label-2351773c.js";import"./index-1dfee9a7.js";const L=`## Usage
+
+With the use of the design property, you can set the style of the Bar to appear designed like a Header, Subheader, Footer and FloatingFooter.  
+**Note:** Do not place a Bar inside another Bar or inside any bar-like component. Doing so may cause unpredictable behavior.
+
+## Responsive Behavior
+
+The default slot will be centered in the available space between the startContent and the endContent areas, therefore it might not always be centered in the entire bar.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Bar\` exposes the following CSS Shadow Parts:
+
+- bar - Used to style the wrapper of the content of the component
+
+## Keyboard Handling
+
+### Fast Navigation
+
+This component provides a build in fast navigation group which can be used via \`F6 / Shift + F6\` or \`Ctrl + Alt(Option) + Down / Ctrl + Alt(Option) + Up\`. In order to use this functionality, you need to import the following module: \`import "@ui5/webcomponents-base/dist/features/F6Navigation.js"\`
+`,j=t.jsx("span",{children:"Start Content"}),f=t.jsx("span",{children:"Center Content"}),C=t.jsx("span",{children:"End Content"}),S=t.jsx("img",{src:"https://raw.githubusercontent.com/SAP/ui5-webcomponents-react/main/assets/Logo.png",alt:"logo",style:{marginLeft:"6px",width:"120px"}});function T(e={}){const{wrapper:s}=Object.assign({},l(),e.components);return s?t.jsx(s,{...e,children:t.jsx(a,{})}):a();function a(){const r=Object.assign({h2:"h2",h1:"h1"},l(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(B,{title:"Layouts & Floorplans / Bar",component:n,argTypes:{startContent:{control:{disable:!0}},children:{control:{disable:!0}},endContent:{control:{disable:!0}}},args:{design:c.Header,startContent:j,children:f,endContent:C}}),`
+`,t.jsx(k,{since:"0.13.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(m,{children:t.jsx(p,{name:"Default",children:i=>t.jsx(n,{...i})})}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(F,{story:"Default"}),`
+`,t.jsx(D,{children:L}),`
+`,t.jsx(r.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"bar-with-custom-elements",children:"Bar with custom elements"}),`
+`,t.jsx(m,{children:t.jsx(p,{name:"with custom elements",children:i=>t.jsx(n,{...i,startContent:t.jsxs(t.Fragment,{children:[t.jsx(d,{icon:"nav-back",design:g.Transparent}),S]}),endContent:t.jsxs(t.Fragment,{children:[t.jsx(h,{placeholder:"Search"}),t.jsx(u,{style:{marginLeft:"6px"},href:"https://github.com/SAP/ui5-webcomponents-react",target:"_blank",children:t.jsx(b,{})})]}),children:t.jsx(x,{children:"Bar"})})})}),`
+`,t.jsx(v,{})]})}}const w=e=>t.jsx(n,{...e});w.storyName="Default";w.parameters={storySource:{source:`args => {
+  return <Bar {...args} />;
+}`}};const y=e=>t.jsx(n,{...e,startContent:t.jsxs(t.Fragment,{children:[t.jsx(d,{icon:"nav-back",design:g.Transparent}),S]}),endContent:t.jsxs(t.Fragment,{children:[t.jsx(h,{placeholder:"Search"}),t.jsx(u,{style:{marginLeft:"6px"},href:"https://github.com/SAP/ui5-webcomponents-react",target:"_blank",children:t.jsx(b,{})})]}),children:t.jsx(x,{children:"Bar"})});y.storyName="with custom elements";y.parameters={storySource:{source:`args => {
+  return <Bar {...args} startContent={<>
+              <Button icon="nav-back" design={ButtonDesign.Transparent} />
+              {Img}
+            </>} endContent={<>
+              <Input placeholder="Search" />
+              <Link style={{
+      marginLeft: "6px"
+    }} href="https://github.com/SAP/ui5-webcomponents-react" target="_blank">
+                <GitHubLogo />
+              </Link>
+            </>}>
+          <Title>Bar</Title>
+        </Bar>;
+}`}};const o={title:"Layouts & Floorplans / Bar",component:n,args:{design:c.Header,startContent:j,children:f,endContent:C},argTypes:{startContent:{control:{disable:!0}},children:{control:{disable:!0}},endContent:{control:{disable:!0}}},tags:["stories-mdx"],includeStories:["defaultStory","withCustomElements"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:T};const ee=["defaultStory","withCustomElements"];export{ee as __namedExportsOrder,o as default,w as defaultStory,y as withCustomElements};
+//# sourceMappingURL=Bar.stories-db78ca4c.js.map
