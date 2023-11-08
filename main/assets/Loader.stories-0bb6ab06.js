c4dbf7c8b32084706ec75b027ca121b8064fa6cc
Lukas742 <lukas.harbarth@sap.com>
2023-11-08 14:01:37 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@470fec00e3e31dbf3a8e3d139d59ab912b069774 🚀

---
+++ main/assets/Loader.stories-0bb6ab06.js
@@ -0,0 +1,84 @@
+import{A as z}from"./DomRefTable.module-e4f1df42.js";import{D as E}from"./DocsHeader-08bcaca0.js";import{F as H}from"./Footer-bc30cdc4.js";import{j as t}from"./jsx-runtime-d079401a.js";import{F as T,b as g}from"./index-12e0da73.js";import{L as s,a as b}from"./index-5c33297b.js";import{T as j}from"./index-743fc8f1.js";import{r as n}from"./index-f1f2c4b1.js";import{M as I,C as u,e as f}from"./chunk-HLWAVYOI-3cfe9455.js";import{o as U}from"./UI5Element-d089e658.js";import{r as w}from"./Icons-d115de21.js";import{i as y}from"./i18n-defaults-bdef1cce.js";import{s as k}from"./spacing-097dbbf3.js";import{C as L}from"./index-8dc2d6a1.js";import{C as v}from"./index-38b43fe5.js";import{I as q}from"./index-edd1d15b.js";import{u as C}from"./index-59d6410c.js";import"./react-jss.esm-2e5f50f2.js";import"./index-15680150.js";import"./iframe-9479b6dc.js";import"../sb-preview/runtime.js";import"./withWebComponent-f146e44d.js";import"./utils-85c0effb.js";import"./CustomElementsScopeUtils-90546106.js";import"./useIsomorphicLayoutEffect-c49de97d.js";import"./slot-76e48863.js";import"./Icon-dc979b51.js";import"./decline-a017ea5c.js";import"./information-ff75d6c3.js";import"./alert-fa33aca2.js";import"./class-map-5b190712.js";import"./Button-f20bbbdf.js";import"./AriaLabelHelper-43a261ec.js";import"./MarkedEvents-b83081e9.js";import"./i18n-defaults-a1ecaff4.js";import"./addCustomCSSWithScoping-b7e0672b.js";import"./index-0e91e77a.js";import"./Avatar-d6a8bd34.js";import"./ResizeHandler-36d5a74c.js";import"./employee-0930782f.js";import"./index-18d4ae37.js";import"./Link-0dce4afc.js";import"./WrappingType-b81e595a.js";import"./TableOfContent-59e49883.js";import"./index-8146d2a5.js";import"./Label-025b0296.js";import"./index-c74c9f7f.js";import"./index-882cd3c7.js";import"./index-b694814b.js";import"./Popover-84fe14ea.js";import"./Integer-f7f172c9.js";import"./PopupUtils-caf3caee.js";import"./getActiveElement-bcae01ed.js";import"./PopupsCommon.css-070f9d59.js";import"./FocusableElements-320054f2.js";import"./isElementHidden-01c07146.js";import"./MediaRange-25b98f31.js";import"./style-map-2f7624b7.js";import"./BrowserScrollbar.css-e63eefff.js";import"./clsx-1229b3e0.js";import"./i18n-defaults-3e7d7856.js";import"./I18nContext-dd74d568.js";import"./ThemingParameters-7e2e4e30.js";import"./index-f82c8853.js";import"./mapValues-6da481dc.js";import"./_baseForOwn-62af832b.js";import"./_baseUniq-27dbdc21.js";import"./index-356e4a49.js";const B="activate",D="M21 408l249-249q5-5 11-5t11 5l45 45q12 11 0 23L89 475q-5 5-12 5-6 0-11-5l-45-45q-11-11 0-22zM140 64l21-58 21 58h59l-47 39 20 64-53-40-55 40 21-64-47-39h60zm196 0h59l21-58 21 58h59l-47 39 20 64-53-40-54 40 21-64zm0 224h59l21-58 21 58h59l-47 39 20 64-53-40-54 40 21-64zM55 419l22 23 181-181-22-23z",N=!1,P=y,$="SAP-icons-v4",O="@ui5/webcomponents-icons";w(B,{pathData:D,ltr:N,accData:P,collection:$,packageName:O});const R="activate",S="M416 192q-9 0-16-6t-9-15l-8-42-42-8q-9-2-15-9t-6-16 6-16 15-9l42-9 8-41q2-9 9-15t16-6 16 6 9 15l9 41 42 9q9 2 14.5 9t5.5 16-5.5 16-14.5 9l-42 8-9 42q-2 9-9 15t-16 6zm-288 0q-9 0-16-6t-9-15l-8-42-42-8q-9-2-15-9t-6-16 6-16 15-9l42-9 8-41q2-9 9-15t16-6 16 6 9 15l9 41 42 9q9 2 14.5 9t5.5 16-5.5 16-14.5 9l-42 8-9 42q-2 9-9 15t-16 6zm185 55q7 7 7 18t-7 18L102 494q-20 18-44 18-26 0-42-18T0 453q0-24 17-42l212-212q7-7 18-7 12 0 19 7zm-78 41l-11-11L54 448q-3 1-3 5 0 8 8 8 2 0 6-2zm181 192q-9 0-16-6t-9-15l-8-42-42-8q-9-2-15-9t-6-16 6-16 15-9l42-9 8-41q2-9 9-15t16-6 16 6 9 15l9 41 42 9q9 2 14.5 9t5.5 16-5.5 16-14.5 9l-42 8-9 42q-2 9-9 15t-16 6z",X=!1,Y=y,V="SAP-icons-v5",W="@ui5/webcomponents-icons";w(R,{pathData:S,ltr:X,accData:Y,collection:V,packageName:W});U();function G(r={}){const{wrapper:o}=Object.assign({},C(),r.components);return o?t.jsx(o,{...r,children:t.jsx(c,{})}):c();function c(){const e=Object.assign({h2:"h2",h1:"h1",pre:"pre",code:"code"},C(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(I,{title:"User Feedback / Loader",component:s,args:{type:b.Indeterminate,progress:"60%"}}),`
+`,t.jsx(E,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(u,{children:t.jsx(f,{name:"Default",children:i=>t.jsx(s,{...i})})}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(z,{story:"Default"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"card-with-loader",children:"Card with Loader"}),`
+`,t.jsx(u,{children:t.jsx(f,{name:"with Card",children:i=>{const a="This is a text that will be updated 5 seconds after the header is clicked. The text will be visible until the new text has been fetched. To let the user know that the text is updating, the Loader will be displayed.",[d,m]=n.useState(a),[l,x]=n.useState(!1),h=n.useRef(0),M=()=>{x(!0)};return n.useEffect(()=>{l&&setTimeout(()=>{h.current<10?m(_=>_===a?"Updated (⌐■_■)":a):m("You really must be bored ಠ_ಠ"),h.current++,x(!1)},5e3)},[l]),t.jsx(L,{header:t.jsx(v,{titleText:"Click the header to update the text below.",interactive:!0,avatar:t.jsx(q,{name:"activate"}),onClick:M}),style:{width:"400px"},children:t.jsxs(T,{direction:g.Column,children:[l&&t.jsx(s,{...i}),t.jsx(j,{style:k,children:d})]})})}})}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const LoaderComponent = () => {
+  const initialText =
+    'This is a text that will be updated 5 seconds after the header is clicked. The text will be visible until the new text has been fetched. To let the user know that the text is updating, the Loader will be displayed.';
+  const [text, setText] = useState(initialText);
+  const [loading, setLoading] = useState(false);
+  const onHeaderClick = () => {
+    setLoading(true);
+  };
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setText((prev) => (prev === initialText ? 'Updated (⌐■_■)' : initialText));
+        setLoading(false);
+      }, 5000);
+    }
+  }, [loading]);
+  return (
+    <Card
+      header={
+        <CardHeader
+          titleText="Click the header to update the text below."
+          interactive
+          avatar={<Icon name="activate" />}
+          onClick={onHeaderClick}
+        />
+      }
+      style={{ width: '400px' }}
+    >
+      <FlexBox direction={FlexBoxDirection.Column}>
+        {loading && <Loader />}
+        <Text style={spacing.sapUiContentPadding}>{text}</Text>
+      </FlexBox>
+    </Card>
+  );
+};
+`})}),`
+`,t.jsx(H,{})]})}}const F=r=>t.jsx(s,{...r});F.storyName="Default";F.parameters={storySource:{source:`args => {
+  return <Loader {...args} />;
+}`}};const A=r=>{const o="This is a text that will be updated 5 seconds after the header is clicked. The text will be visible until the new text has been fetched. To let the user know that the text is updating, the Loader will be displayed.",[c,e]=n.useState(o),[i,a]=n.useState(!1),d=n.useRef(0),m=()=>{a(!0)};return n.useEffect(()=>{i&&setTimeout(()=>{d.current<10?e(l=>l===o?"Updated (⌐■_■)":o):e("You really must be bored ಠ_ಠ"),d.current++,a(!1)},5e3)},[i]),t.jsx(L,{header:t.jsx(v,{titleText:"Click the header to update the text below.",interactive:!0,avatar:t.jsx(q,{name:"activate"}),onClick:m}),style:{width:"400px"},children:t.jsxs(T,{direction:g.Column,children:[i&&t.jsx(s,{...r}),t.jsx(j,{style:k,children:c})]})})};A.storyName="with Card";A.parameters={storySource:{source:`args => {
+  const initialText = "This is a text that will be updated 5 seconds after the header is clicked. The text will be visible until the new text has been fetched. To let the user know that the text is updating, the Loader will be displayed.";
+  const [text, setText] = useState(initialText);
+  const [loading, setLoading] = useState(false);
+  const counter = useRef(0);
+
+  const onHeaderClick = () => {
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        if (counter.current < 10) {
+          setText(prev => prev === initialText ? "Updated (\\u2310\\u25A0_\\u25A0)" : initialText);
+        } else {
+          setText("You really must be bored \\u0CA0_\\u0CA0");
+        }
+
+        counter.current++;
+        setLoading(false);
+      }, 5000);
+    }
+  }, [loading]);
+  return <Card header={<CardHeader titleText="Click the header to update the text below." interactive avatar={<Icon name="activate" />} onClick={onHeaderClick} />} style={{
+    width: "400px"
+  }}>
+          <FlexBox direction={FlexBoxDirection.Column}>
+            {loading && <Loader {...args} />}
+            <Text style={spacing.sapUiContentPadding}>{text}</Text>
+          </FlexBox>
+        </Card>;
+}`}};const p={title:"User Feedback / Loader",component:s,args:{type:b.Indeterminate,progress:"60%"},tags:["stories-mdx"],includeStories:["defaultStory","withCard"]};p.parameters=p.parameters||{};p.parameters.docs={...p.parameters.docs||{},page:G};const de=["defaultStory","withCard"];export{de as __namedExportsOrder,p as default,F as defaultStory,A as withCard};
+//# sourceMappingURL=Loader.stories-0bb6ab06.js.map
--- main/assets/preview-0a5bd4df.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-c3007204.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-6a27fa4c.js"),["./DocsRenderer-NNNQARDV-6a27fa4c.js","./chunk-HLWAVYOI-22baf3b3.js","./iframe-c3007204.js","./index-f1f2c4b1.js","./index-f82c8853.js","./index-c74c9f7f.js","./mapValues-6da481dc.js","./_baseForOwn-62af832b.js","./_baseUniq-27dbdc21.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-0a5bd4df.js.map
