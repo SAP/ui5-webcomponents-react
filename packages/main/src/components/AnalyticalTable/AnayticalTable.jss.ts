b1fff8049419d83516a6a7a8acad04b042b9b8f5
Lukas Harbarth <lukas.harbarth@sap.com>
2023-11-21 12:33:29 +0100

    Update AnayticalTable.jss.ts

--- packages/main/src/components/AnalyticalTable/AnayticalTable.jss.ts
+++ packages/main/src/components/AnalyticalTable/AnayticalTable.jss.ts
@@ -163,7 +163,7 @@ const styles = {
     '&$tr': {
       fontFamily: CustomThemingParameters.AnalyticalTableHeaderFontFamily,
       backgroundColor: `${ThemingParameters.sapList_TableGroupHeaderBackground}`,
-      borderBlockEnd: `1px solid ${ThemingParameters.sapList_TableGroupHeaderBorderColor}`,
+      borderColor: ThemingParameters.sapList_TableGroupHeaderBorderColor,
       color: ThemingParameters.sapList_TextColor,
       '& $tableCell': {
         borderInlineEnd: `1px solid transparent`
