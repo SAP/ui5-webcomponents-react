ff185dc799cf41643752c28619ac515f80297681
Lukas742 <lukas.harbarth@sap.com>
2023-11-22 12:45:41 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@7bdb8c7136707b3c28d2e2166319e265d9a04095 🚀

---
+++ main/assets/Dialog-ro39pwcy.js
@@ -0,0 +1,106 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{C as a}from"./DomRefTable.module-1teiQWGe.js";import{D as l}from"./DocsHeader-TZSTd_u-.js";import{F as m}from"./Footer-f0i2sv-0.js";import"./index-OjgoNOWw.js";import{M as c,C as r,b as d}from"./chunk-HLWAVYOI-psJj60ml.js";import{D as h}from"./DomRefTable-wmSENnNG.js";import{C as g,D as s,W as u}from"./Dialog.stories-UpLXnNi0.js";import{D as f}from"./DialogDomRef-r9IDeiJ2.js";import{u as p}from"./index-RwE9nQVW.js";import"./react-jss.esm-AmlvxLhk.js";import"./index--fFZoO1L.js";import"./iframe-S4JyMSMM.js";import"../sb-preview/runtime.js";import"./UI5Element-OBgzz0fv.js";import"./i18nBundle-Crssojm_.js";import"./VersionInfo-hBCHoyLi.js";import"./CustomElementsScopeUtils-kYmWas10.js";import"./withWebComponent-6ZcDnwha.js";import"./utils-5CTKRvkw.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-2m0rU9av.js";import"./Icons-zRUxeAje.js";import"./decline-P9scilyE.js";import"./i18n-defaults-n2A5pq_J.js";import"./information-eS-oBj3B.js";import"./alert-SMfrQE5J.js";import"./class-map-dxoND1Lf.js";import"./Button-UDaVhcBU.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-RBzcmDt8.js";import"./index-yaLaEhii.js";import"./clsx-c3LcQGjR.js";import"./i18n-defaults-PmTGElot.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./addCustomCSSWithScoping-cCePQQVr.js";import"./index-Vrh7cvSu.js";import"./Integer-kog98579.js";import"./index-RDyNiobK.js";import"./Avatar-LWFiEvrj.js";import"./ResizeHandler-jV9qqcaG.js";import"./employee-DNZboz-g.js";import"./index-JiwscGpR.js";import"./index-FFmUsnk4.js";import"./Link-Gs09ax_S.js";import"./WrappingType-avPrqc94.js";import"./index-FvozjNxD.js";import"./index-3PxZPhBC.js";import"./index-QYh6idnL.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-IMpNFjIe.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./TableOfContent-uZmr2UoK.js";import"./index-17AA29k7.js";import"./Label-bo0gz_aS.js";import"./index-070p_6Jf.js";import"./index-xCjRCXU5.js";import"./Popover-SxOPU5wi.js";import"./PopupUtils-7oRQuqE1.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-dlrgNDf3.js";import"./FocusableElements-vmTxt0Po.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-oXcF1--7.js";import"./BrowserScrollbar.css-gfEIeu7r.js";import"./index-GL4gBoyB.js";import"./mapValues-HzTlT2lF.js";import"./_baseForOwn-UZJwRxDU.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./settings-jXR25DaW.js";import"./CodeGen-hQaGWn9O.js";import"./index-G8Lp4ZoB.js";import"./Dialog-sWOJmdZ6.js";import"./ValueState-zUcANXoY.js";import"./ValueState-I_-hGNIb.js";import"./index-yB67DRqs.js";import"./Bar-oatY253I.js";import"./parameters-bundle.css-96HoEB0F.js";import"./index-nalkIn2j.js";import"./index-fRK1tgHc.js";import"./Title-nBPKLywE.js";import"./index-G6gUwUJF.js";import"./ListItemBase-OVhNd6zE.js";import"./ItemNavigation-_xIDlcJ6.js";import"./TabbableElements-aYIPY3zp.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-v8eJGF06.js";import"./index-mhyKN2oj.js";import"./StandardListItem-srLARCBi.js";import"./ListItem-sP6HCr5u.js";import"./RadioButton-b_fTB1Cx.js";import"./CheckBox-Hp_NHnWe.js";import"./accept-sNf1zbjr.js";import"./HasPopup-9BAMrqbD.js";import"./slim-arrow-right-LgQh9lXE.js";const x=`## Structure
+
+A \`Dialog\` consists of a header, content, and a footer for action buttons. The \`Dialog\` is usually displayed at the center of the screen. Its position can be changed by the user. To enable this, you need to set the property \`draggable\` accordingly.
+
+## Responsive Behavior
+
+The \`stretch\` property can be used to stretch the \`Dialog\` on full screen.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Dialog\` exposes the following CSS Shadow Parts:
+
+- header - Used to style the header of the component
+- content - Used to style the content of the component
+- footer - Used to style the footer of the component
+
+**Note:** When a \`Bar\` is used in the header or in the footer, you should remove the default dialog's paddings.  
+For more information see the sample "Bar in Header/Footer".
+`;function de(n={}){const{wrapper:t}=Object.assign({},p(),n.components);return t?o.jsx(t,Object.assign({},n,{children:o.jsx(i,{})})):i();function i(){const e=Object.assign({h2:"h2",h4:"h4",p:"p",code:"code",pre:"pre",strong:"strong",a:"a",h1:"h1"},p(),n.components);return o.jsxs(o.Fragment,{children:[o.jsx(c,{of:g}),`
+`,o.jsx(l,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"example",children:"Example"}),`
+`,o.jsx(r,{of:s}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{of:s}),`
+`,o.jsx(d,{children:x}),`
+`,o.jsx(h,{rows:f}),`
+`,o.jsx(e.h2,{id:"opening-dialogs",children:"Opening Dialogs"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h4,{id:"since-0220",children:"since 0.22.0"}),`
+`,o.jsxs(e.p,{children:["We recommend opening and closing the ",o.jsx(e.code,{children:"Dialog"})," component in a declarative way using the ",o.jsx(e.code,{children:"open"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const MyComponentWithDialog = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+      >
+        Open Dialog
+      </Button>
+      <Dialog open={dialogIsOpen} />
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h4,{id:"before-0220",children:"before 0.22.0"}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.code,{children:"Dialogs"})," can be opened by attaching a ",o.jsx(e.code,{children:"ref"})," to the component and then calling the corresponding ",o.jsx(e.strong,{children:o.jsx(e.code,{children:"show"})})," method."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Dialog</Button>
+      <Dialog ref={dialogRef}>Some Content</Dialog>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h2,{id:"using-dialogs-inside-other-components",children:"Using Dialogs inside other components"}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.code,{children:"Dialogs"}),` are often used within other components, when opened this could sometimes have unwanted side effects.
+In this case, we recommend using `,o.jsx(e.a,{href:"https://reactjs.org/docs/portals.html",target:"_blank",rel:"nofollow noopener noreferrer",children:"createPortal"})," to mount the ",o.jsx(e.code,{children:"Dialog"})," outside of the DOM hierarchy of the parent component."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Dialog</Button>
+      {createPortal(<Dialog ref={dialogRef} />, document.body)}
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h2,{id:"closing-dialogs",children:"Closing Dialogs"}),`
+`,o.jsxs(e.p,{children:["Closing ",o.jsx(e.code,{children:"Dialogs"}),` works in the same way as opening them.
+You can either set the `,o.jsx(e.code,{children:"open"})," prop to ",o.jsx(e.code,{children:"false"})," or attaching a ",o.jsx(e.code,{children:"ref"})," on which the corresponding ",o.jsx(e.code,{children:"close"})," method is called."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const handleOpen = (e) => {
+    dialogRef.current.show();
+  };
+  const handleClose = () => {
+    dialogRef.current.close();
+  };
+  return (
+    <>
+      <Button onClick={handleOpen}>Open Dialog</Button>
+      <Dialog ref={dialogRef}>
+        <Button onClick={handleClose}>Close Dialog</Button>
+      </Dialog>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(e.h2,{id:"dialog-with-content",children:"Dialog with content"}),`
+`,o.jsx(r,{of:u}),`
+`,o.jsx(m,{})]})}}export{de as default};
+//# sourceMappingURL=Dialog-ro39pwcy.js.map
