47370d9ce11c099abcf722a71b48d981fac2263b
Lukas742 <lukas.harbarth@sap.com>
2023-12-04 16:47:01 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@501b923683a62eb08f3ef1b04fee4d73ba115409 🚀

---
+++ main/assets/RatingIndicator.stories-cKHDeBeO.js
@@ -0,0 +1,30 @@
+import{M as h,C as c,f as d,b as u}from"./chunk-HLWAVYOI-E-hYVLTa.js";import{R as e}from"./index-CVGpjUOr.js";import{b as l}from"./DomRefTable.module-y-qtgfhA.js";import{D as g}from"./DocsHeader-kz9fGWGq.js";import{F as f}from"./Footer-mrYFs9PK.js";import{j as t}from"./jsx-runtime-DtaoT6pD.js";import"./index-OjgoNOWw.js";import{u as m}from"./index-RwE9nQVW.js";import"./iframe-t1eVxq3x.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./withWebComponent-I1wSIv4y.js";import"./utils-KU8RGjGn.js";import"./VersionInfo-LCCO4Ig5.js";import"./CustomElementsScopeUtils-M7hMRmEH.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./Icon-G6aW2FRW.js";import"./UI5Element-Jt3C0_FN.js";import"./i18nBundle-ZoJ37UJR.js";import"./Icons-Nr5kgAbF.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./Integer-kog98579.js";import"./Float-0fEWDOdp.js";import"./i18n-defaults-zEJ36VRV.js";import"./favorite-zTpmtbjY.js";import"./unfavorite-JdQ0HieG.js";import"./react-jss.esm-tmBtHfec.js";import"./index-r_VsGxII.js";import"./slot-HSrR9XJ-.js";import"./decline-4Ez_2Sev.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-bkjyqQzA.js";import"./alert-7Etxrsap.js";import"./class-map-CHDPODen.js";import"./Button-CV8gKi73.js";import"./MarkedEvents-aYv14vsS.js";import"./index-2_yHJzy3.js";import"./clsx-c3LcQGjR.js";import"./i18n-defaults-8YolVglE.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./addCustomCSSWithScoping-B37rLD-H.js";import"./index-dBVYy2Lw.js";import"./index-ymcZtJR2.js";import"./Avatar-VKhV6EwI.js";import"./ResizeHandler-mTFvwsyT.js";import"./employee-9rldWxaE.js";import"./index-Whljgpch.js";import"./index-PuiUN97y.js";import"./Link-ipgrhFOm.js";import"./WrappingType-avPrqc94.js";import"./index-XbOz6Cvl.js";import"./index-bWwzC2qz.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./TableOfContent-AMxyMkMf.js";import"./index-4ZV9NEq2.js";import"./Label-Xo_6xXB6.js";import"./index-7qCLIi66.js";import"./index-JZmr7vOT.js";import"./Popover-5ANP4Jn7.js";import"./PopupUtils-QCyiTOMI.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-NvRre6vc.js";import"./FocusableElements-sOHKyey_.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-xQ1SPtMO.js";import"./BrowserScrollbar.css-R_PDLCAQ.js";const x=`## Usage
+
+The recommended number of icons is between 5 and 7.
+
+## Responsive Behavior
+
+You can change the size of the Rating Indicator by changing its \`font-size\` CSS property.  
+Example: \`<RatingIndicator style="font-size: 3rem;"></RatingIndicator>\`
+
+## Keyboard Handling
+
+When the \`RatingIndicator\` is focused, the user can change the rating with the following keyboard shortcuts:
+
+- \\[RIGHT/UP\\] - Increases the value of the rating by one step. If the highest value is reached, does nothing
+- \\[LEFT/DOWN\\] - Decreases the value of the rating by one step. If the lowest value is reached, does nothing.
+- \\[HOME\\] - Sets the lowest value.
+- \\[END\\] - Sets the highest value.
+- \\[SPACE/ENTER/RETURN\\] - Increases the value of the rating by one step. If the highest value is reached, sets the rating to the lowest value.
+- Any number - Changes value to the corresponding number. If typed number is larger than the number of values, sets the highest value.
+`;function j(r={}){const{wrapper:i}=Object.assign({},m(),r.components);return i?t.jsx(i,{...r,children:t.jsx(n,{})}):n();function n(){const s=Object.assign({h2:"h2"},m(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(h,{title:"Inputs / RatingIndicator",component:e}),`
+`,t.jsx(g,{since:"0.10.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(s.h2,{id:"example",children:"Example"}),`
+`,t.jsx(c,{children:t.jsx(d,{name:"Default",children:a=>t.jsx(e,{...a})})}),`
+`,t.jsx(s.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{story:"Default"}),`
+`,t.jsx(u,{children:x}),`
+`,t.jsx(f,{})]})}}const p=r=>t.jsx(e,{...r});p.storyName="Default";p.parameters={storySource:{source:`args => {
+  return <RatingIndicator {...args} />;
+}`}};const o={title:"Inputs / RatingIndicator",component:e,tags:["stories-mdx"],includeStories:["defaultStory"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:j};const kt=["defaultStory"];export{kt as __namedExportsOrder,o as default,p as defaultStory};
