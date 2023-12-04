e16c28278164a6eeb4fd091a5af3a89b057a2259
Lukas Harbarth <lukas.harbarth@sap.com>
2023-12-04 17:08:22 +0100

    wip

--- packages/main/src/components/DynamicPage/DynamicPage.cy.tsx
+++ packages/main/src/components/DynamicPage/DynamicPage.cy.tsx
@@ -623,8 +623,9 @@ describe('DynamicPage', () => {
       cy.findByTestId('dp').scrollTo(0, 750, { duration: 300 });
       cy.get('[data-component-name="DynamicPageAnchorBarPinBtn"]').should('not.exist');
 
-      cy.findByTestId('dp').scrollTo(0, 0);
       cy.wait(600);
+      cy.findByTestId('dp').scrollTo(0, 0);
+
       // fallback scroll for CI
       // cy.findByTestId('dp').scrollTo(0, 300, { duration: 300 });
       // cy.findByTestId('dp').scrollTo(0, 0);
