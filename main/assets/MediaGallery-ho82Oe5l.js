2f30b32fa0c0918823def1797bbf8b2d21be7e23
Lukas742 <lukas.harbarth@sap.com>
2023-12-07 09:45:07 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@61a509c5bce2aafdb418f74c7d5bd8dac20d9418 🚀

---
+++ main/assets/MediaGallery-ho82Oe5l.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{C as s,A as d}from"./DomRefTable.module-tJDSli5L.js";import{D as l}from"./DocsHeader-Y1u2dA2J.js";import{F as h}from"./Footer-k68uF00p.js";import"./index-OjgoNOWw.js";import{M as c,C as r,b as e}from"./chunk-HLWAVYOI-mgslNHI9.js";import{S as u}from"./SubcomponentsSection-pzdPLcqE.js";import{C as x,D as p,a as f,I as j,M as g}from"./MediaGallery.stories-H7Z8rEL-.js";import{u as a}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-2NLNaBNo.js";import"./iframe-0edzOeLt.js";import"../sb-preview/runtime.js";import"./UI5Element-Jt3C0_FN.js";import"./i18nBundle-ZoJ37UJR.js";import"./VersionInfo-LCCO4Ig5.js";import"./CustomElementsScopeUtils-M7hMRmEH.js";import"./withWebComponent-I1wSIv4y.js";import"./utils-KU8RGjGn.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-G6aW2FRW.js";import"./Icons-Nr5kgAbF.js";import"./decline-4Ez_2Sev.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-bkjyqQzA.js";import"./alert-7Etxrsap.js";import"./class-map-CHDPODen.js";import"./Button-CV8gKi73.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-zEJ36VRV.js";import"./index-2_yHJzy3.js";import"./clsx-c3LcQGjR.js";import"./i18n-defaults-8YolVglE.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./addCustomCSSWithScoping-B37rLD-H.js";import"./index-_LaNBmU6.js";import"./Integer-kog98579.js";import"./index-bBdKD5Do.js";import"./Avatar-VKhV6EwI.js";import"./ResizeHandler-mTFvwsyT.js";import"./employee-9rldWxaE.js";import"./index-H2Flh767.js";import"./index-i5JIYlEv.js";import"./Link-ipgrhFOm.js";import"./WrappingType-avPrqc94.js";import"./index-XbOz6Cvl.js";import"./index-bWwzC2qz.js";import"./index-QYh6idnL.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./TableOfContent-hKyyiU4m.js";import"./index-YW3XEXM0.js";import"./Label-Xo_6xXB6.js";import"./index-FsDhqkuN.js";import"./index-jpfIm4hA.js";import"./Popover-5ANP4Jn7.js";import"./PopupUtils-QCyiTOMI.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-NvRre6vc.js";import"./FocusableElements-sOHKyey_.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-xQ1SPtMO.js";import"./BrowserScrollbar.css-R_PDLCAQ.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./parameters-bundle.css-HXU4vJcG.js";import"./index-3nfQ-O5L.js";import"./ItemNavigation--ejGb_nH.js";import"./Carousel-PT0xsSDx.js";import"./ScrollEnablement-JhIGDfd8.js";import"./animate-Pj1TncyK.js";import"./AnimationMode-YBLH6EH2.js";import"./slim-arrow-left-Vwx_rhBh.js";import"./slim-arrow-right-0wqMoc5g.js";const y="**Since:** 0.21.0\n\nThe `MediaGalleryItem` web component represents the items displayed in the `MediaGallery` web component.\n\n**Note:** `MediaGalleryItem` is not supported when used outside of `MediaGallery`.\n\n### Keyboard Handling\n\nThe `MediaGallery` provides advanced keyboard handling. When focused, the user can use the following keyboard shortcuts in order to perform a navigation:\n\n- \\[SPACE/ENTER/RETURN\\] - Trigger `undefined` event\n",b=`## Keyboard Handling
+
+The \`MediaGallery\` provides advanced keyboard handling.  
+When the thumbnails menu is focused the following keyboard shortcuts allow the user to navigate through the thumbnail items:
+
+- \\[UP/DOWN\\] - Navigates up and down the items
+- \\[HOME\\] - Navigates to first item
+- \\[END\\] - Navigates to the last item
+- \\[SPACE/ENTER\\] - Select an item
+`;function Yt(i={}){const{wrapper:m}=Object.assign({},a(),i.components);return m?t.jsx(m,Object.assign({},i,{children:t.jsx(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h3:"h3"},a(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(c,{of:x}),`
+`,t.jsx(l,{since:"0.21.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(e,{children:b}),`
+`,t.jsx(o.h2,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(o.h3,{id:"custom-thumbnail-image",children:"Custom Thumbnail Image"}),`
+`,t.jsx(r,{of:f}),`
+`,t.jsx(o.h3,{id:"interactive-display-area",children:"Interactive Display Area"}),`
+`,t.jsx(r,{of:j}),`
+`,t.jsx(e,{children:u}),`
+`,t.jsx(o.h2,{id:"mediagalleryitem",children:"MediaGalleryItem"}),`
+`,t.jsx(e,{children:y}),`
+`,t.jsx(d,{of:g}),`
+`,t.jsx(h,{})]})}}export{Yt as default};
