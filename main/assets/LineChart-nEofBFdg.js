f1a9bcb392d69dc26e073fc8b51c64537ee7869f
Lukas742 <lukas.harbarth@sap.com>
2023-12-07 12:01:29 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0bc9892428cebaebaf3edfb7b872c3e0e92bae8f 🚀

---
+++ main/assets/LineChart-nEofBFdg.js
@@ -0,0 +1,60 @@
+import{j as r}from"./jsx-runtime-DtaoT6pD.js";import{M as m,C as e}from"./chunk-HLWAVYOI-gEeIl2kH.js";import{C as a}from"./DomRefTable.module-653hBK0i.js";import{D as c}from"./DocsHeader-GN0hNo-U.js";import{F as d}from"./Footer-yGToR2Fe.js";import"./index-OjgoNOWw.js";import{C as h,D as s,W as l,a as x,b as j,c as f,L as u,d as C,e as g}from"./LineChart.stories-ixr_HyS5.js";import{u as p}from"./index-RwE9nQVW.js";import"./iframe-SPzYBTgm.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./react-jss.esm-tmBtHfec.js";import"./index-_ohraHpC.js";import"./UI5Element-Jt3C0_FN.js";import"./i18nBundle-ZoJ37UJR.js";import"./VersionInfo-LCCO4Ig5.js";import"./CustomElementsScopeUtils-M7hMRmEH.js";import"./withWebComponent-I1wSIv4y.js";import"./utils-KU8RGjGn.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-G6aW2FRW.js";import"./Icons-Nr5kgAbF.js";import"./decline-4Ez_2Sev.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-bkjyqQzA.js";import"./alert-7Etxrsap.js";import"./class-map-CHDPODen.js";import"./Button-CV8gKi73.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-zEJ36VRV.js";import"./index-2_yHJzy3.js";import"./clsx-c3LcQGjR.js";import"./i18n-defaults-8YolVglE.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-bWwzC2qz.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-B37rLD-H.js";import"./index-2t3485bg.js";import"./Integer-kog98579.js";import"./index-7c3ijTPY.js";import"./Avatar-VKhV6EwI.js";import"./ResizeHandler-mTFvwsyT.js";import"./employee-9rldWxaE.js";import"./index-1HoB1E4Y.js";import"./index-faNpjDi0.js";import"./Link-ipgrhFOm.js";import"./WrappingType-avPrqc94.js";import"./index-5ETO3hDj.js";import"./TableOfContent-aScKSEg_.js";import"./index-GmyP3gZ3.js";import"./Label-Xo_6xXB6.js";import"./index-FS46D7k5.js";import"./index-yF6uS0Va.js";import"./Popover-5ANP4Jn7.js";import"./PopupUtils-QCyiTOMI.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-NvRre6vc.js";import"./FocusableElements-sOHKyey_.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-xQ1SPtMO.js";import"./BrowserScrollbar.css-R_PDLCAQ.js";import"./DemoProps-gxklhiCN.js";import"./LineChart-WnVCvzAR.js";import"./index-Jm2lAFsC.js";import"./YAxisTicks-TyGB6lQJ.js";import"./ChartContainer-YHkXgOdx.js";import"./index-TTzQMjGh.js";import"./throttle-NPD2LhEd.js";import"./isString-yBrqhg_E.js";import"./debounce-F9irgL9Y.js";import"./useTooltipFormatter-dm0WVnn1.js";import"./defaults-6TEcj9ka.js";import"./ChartDataLabel-hNb_mpK4.js";import"./generateCategoricalChart-XOjaZWRK.js";import"./tiny-invariant-VyoA2Os1.js";import"./isPlainObject-hqM_z3iV.js";import"./staticProps-YAKXJgHU.js";import"./useLongestYAxisLabel-G7uw77Oq.js";import"./react-content-loader.es-MhslfaKA.js";import"./Line-td0AluFS.js";import"./useIsRTL-CC2qjreu.js";function so(i={}){const{wrapper:t}=Object.assign({},p(),i.components);return t?r.jsx(t,Object.assign({},i,{children:r.jsx(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a",pre:"pre",h4:"h4"},p(),i.components);return r.jsxs(r.Fragment,{children:[r.jsx(m,{of:h}),`
+`,r.jsx(c,{}),`
+`,r.jsx(o.h2,{id:"example",children:"Example"}),`
+`,r.jsx(e,{of:s}),`
+`,r.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,r.jsx(a,{of:s}),`
+`,r.jsx("br",{}),`
+`,r.jsx("br",{}),`
+`,r.jsx(o.h2,{id:"more-examples",children:"More Examples"}),`
+`,r.jsx(o.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,r.jsx(e,{of:l}),`
+`,r.jsx(o.h3,{id:"with-secondary-dimension",children:"With Secondary Dimension"}),`
+`,r.jsx(e,{of:x}),`
+`,r.jsx(o.h3,{id:"with-data-labels",children:"With Data Labels"}),`
+`,r.jsx(e,{of:j}),`
+`,r.jsx(o.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,r.jsx(e,{of:f}),`
+`,r.jsx(o.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,r.jsx(e,{of:u}),`
+`,r.jsx(o.h3,{id:"with-reference-line",children:"With Reference Line"}),`
+`,r.jsxs(o.p,{children:["You can set a reference line to any value by using the ",r.jsx(o.code,{children:"referenceLine"})," ",r.jsx(o.code,{children:"chartConfig"})," property. ",r.jsx(o.code,{children:"referenceLine"})," accepts all properties described ",r.jsx(o.a,{href:"https://recharts.org/en-US/api/ReferenceLine",target:"_blank",rel:"nofollow noopener noreferrer",children:"here"}),", but it's recommended to only use ",r.jsx(o.code,{children:"label"}),",",r.jsx(o.code,{children:"value"})," and ",r.jsx(o.code,{children:"color"})," to preserve the intended design."]}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-jsx",children:`<LineChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,r.jsx(e,{of:C}),`
+`,r.jsx(o.h3,{id:"with-linear-gradient",children:"With Linear Gradient"}),`
+`,r.jsx(e,{of:g}),`
+`,r.jsx(o.h4,{id:"code",children:"Code"}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-jsx",children:`export const LineChartWithLinearGradient = () => {
+  return (
+    <LineChart
+      dataset={bigDataSet}
+      dimensions={[{ accessor: 'name' }]}
+      measures={[
+        {
+          accessor: 'users',
+          width: 2,
+          color: 'url(#colorUsers)'
+        }
+      ]}
+    >
+      <defs>
+        <linearGradient id="colorUsers" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="0%" stopColor="red" />
+          <stop offset="100%" stopColor="green" />
+        </linearGradient>
+      </defs>
+    </LineChart>
+  );
+};
+`})}),`
+`,r.jsx(d,{})]})}}export{so as default};
