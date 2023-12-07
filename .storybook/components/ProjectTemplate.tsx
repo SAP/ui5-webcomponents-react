0bc9892428cebaebaf3edfb7b872c3e0e92bae8f
Lukas Harbarth <lukas.harbarth@sap.com>
2023-12-07 12:56:53 +0100

    docs(Storybook): add `ThemeProvider` to docs components (#5330)

--- .storybook/components/ProjectTemplate.tsx
+++ .storybook/components/ProjectTemplate.tsx
@@ -7,7 +7,8 @@ import {
   LinkDesign,
   MessageStrip,
   MessageStripDesign,
-  Text
+  Text,
+  ThemeProvider
 } from '@ui5/webcomponents-react';
 import { ReactNode } from 'react';
 import { addCustomCSSWithScoping } from '../../packages/main/src/internal/addCustomCSSWithScoping';
@@ -33,50 +34,52 @@ export function ProjectTemplate(props: ProjectTemplatePropTypes) {
   const { title, subtitle, logo, logoAttribution, isTypeScript, children, href, deprecationNotice } = props;
 
   return (
-    <Card
-      className={classes.card}
-      header={
-        <CardHeader
-          data-project-template=""
-          titleText={title}
-          subtitleText={subtitle}
-          action={
-            isTypeScript && (
-              <img
-                src={TsLogo}
-                height={24}
-                width={24}
-                alt={'TypeScript Logo'}
-                title={'TypeScript Logo. Original Source: https://www.typescriptlang.org/branding/'}
-              />
-            )
-          }
-          avatar={
-            <Avatar title={logoAttribution}>
-              <img src={logo} alt={`Logo of ${title}`} />
-            </Avatar>
-          }
-        />
-      }
-    >
-      <div className={classes.cardContent}>
-        {deprecationNotice && (
-          <MessageStrip
-            hideCloseButton
-            design={MessageStripDesign.Warning}
-            children={deprecationNotice}
-            className={classes.deprecationNotice}
+    <ThemeProvider>
+      <Card
+        className={classes.card}
+        header={
+          <CardHeader
+            data-project-template=""
+            titleText={title}
+            subtitleText={subtitle}
+            action={
+              isTypeScript && (
+                <img
+                  src={TsLogo}
+                  height={24}
+                  width={24}
+                  alt={'TypeScript Logo'}
+                  title={'TypeScript Logo. Original Source: https://www.typescriptlang.org/branding/'}
+                />
+              )
+            }
+            avatar={
+              <Avatar title={logoAttribution}>
+                <img src={logo} alt={`Logo of ${title}`} />
+              </Avatar>
+            }
           />
-        )}
+        }
+      >
+        <div className={classes.cardContent}>
+          {deprecationNotice && (
+            <MessageStrip
+              hideCloseButton
+              design={MessageStripDesign.Warning}
+              children={deprecationNotice}
+              className={classes.deprecationNotice}
+            />
+          )}
 
-        <Link design={LinkDesign.Emphasized} href={href}>
-          View Example
-        </Link>
-        <br />
-        <br />
-        <Text>What&apos;s included:</Text>
-        {children}
-      </div>
-    </Card>
+          <Link design={LinkDesign.Emphasized} href={href}>
+            View Example
+          </Link>
+          <br />
+          <br />
+          <Text>What&apos;s included:</Text>
+          {children}
+        </div>
+      </Card>
+    </ThemeProvider>
   );
 }
---
+++ packages/main/src/webComponents/BusyIndicator/BusyIndicator.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Text } from '../../components/Text';
+import { BusyIndicatorSize } from '../../enums';
+import { Select } from '../Select';
+import { BusyIndicator } from './index';
+
+const meta = {
+  title: 'User Feedback / BusyIndicator',
+  component: BusyIndicator,
+  argTypes: { children: { control: { disable: true } } },
+  args: {
+    size: BusyIndicatorSize.Medium,
+    delay: 1000,
+    active: true
+  }
+} satisfies Meta<typeof BusyIndicator>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const WithChildren: Story = {
+  render(args) {
+    return (
+      <>
+        <BusyIndicator {...args}>
+          <Select />
+        </BusyIndicator>
+        <hr />
+        <BusyIndicator {...args}>
+          <Text>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+            clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+            sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </Text>
+        </BusyIndicator>
+      </>
+    );
+  }
+};
