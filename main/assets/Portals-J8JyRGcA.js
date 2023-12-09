8740874b0e06e84d3e0e26863943fc731a666350
Lukas742 <lukas.harbarth@sap.com>
2023-12-09 12:21:33 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@5b8aa8995d1b4671248209aa2ba5b704449249f6 🚀

---
+++ main/assets/Portals-J8JyRGcA.js
@@ -0,0 +1,40 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{M as p}from"./chunk-HLWAVYOI-UvOOWukE.js";import"./DomRefTable.module-HAO7vDgW.js";import"./index-OjgoNOWw.js";import{T as a}from"./TableOfContent-_DNHtYzw.js";import{F as s}from"./Footer-aUUdDqRf.js";import{u as r}from"./index-RwE9nQVW.js";import"./iframe-KoIkOD7m.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./react-jss.esm-tmBtHfec.js";import"./index-N7AwT1xE.js";import"./UI5Element-Jt3C0_FN.js";import"./i18nBundle-ZoJ37UJR.js";import"./VersionInfo-LCCO4Ig5.js";import"./CustomElementsScopeUtils-M7hMRmEH.js";import"./withWebComponent-I1wSIv4y.js";import"./utils-KU8RGjGn.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-G6aW2FRW.js";import"./Icons-Nr5kgAbF.js";import"./decline-4Ez_2Sev.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-bkjyqQzA.js";import"./alert-7Etxrsap.js";import"./class-map-CHDPODen.js";import"./Button-CV8gKi73.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-zEJ36VRV.js";import"./index-2_yHJzy3.js";import"./clsx-c3LcQGjR.js";import"./i18n-defaults-8YolVglE.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-bWwzC2qz.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-B37rLD-H.js";import"./index-XQscmlD0.js";import"./Integer-kog98579.js";import"./index-rq4D2vOx.js";import"./Avatar-VKhV6EwI.js";import"./ResizeHandler-mTFvwsyT.js";import"./employee-9rldWxaE.js";import"./index-hVTsdAjN.js";import"./index-V-WJjz6V.js";import"./Link-ipgrhFOm.js";import"./WrappingType-avPrqc94.js";import"./index-AqaqoFfn.js";import"./index-rbQ8EChD.js";import"./Label-Xo_6xXB6.js";import"./index-R6HiNhiD.js";import"./index-hKk6h3mc.js";import"./Popover-5ANP4Jn7.js";import"./PopupUtils-QCyiTOMI.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-NvRre6vc.js";import"./FocusableElements-sOHKyey_.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-xQ1SPtMO.js";import"./BrowserScrollbar.css-R_PDLCAQ.js";function Ot(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(i,{})})):i();function i(){const e=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",a:"a",pre:"pre"},r(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(p,{title:"Working with Portals"}),`
+`,t.jsx(e.h1,{id:"working-with-portals",children:"Working with Portals"}),`
+`,t.jsx(e.p,{children:"This entry explains why portals are used in UI5 Web Components for React components and what you need to consider when using them."}),`
+`,t.jsx("br",{}),`
+`,t.jsx(a,{headingSelector:"h2"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"why-portals",children:"Why Portals?"}),`
+`,t.jsxs(e.p,{children:["In most cases when a React component like the ",t.jsx(e.code,{children:"AnalyticalTable"})," implements a popover like a ",t.jsx(e.code,{children:"Dialog"})," or ",t.jsx(e.code,{children:"ResponsivePopover"}),", we use a ",t.jsx(e.a,{href:"https://react.dev/reference/react-dom/createPortal",target:"_blank",rel:"nofollow noopener noreferrer",children:"React Portal"})," to mount it outside the parents DOM hierarchy, to prevent unwanted side effects (wrong positioning, alignment, etc.)."]}),`
+`,t.jsx(e.p,{children:"For example:"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const DialogComponent = () => {
+  const dialogRef = useRef(null);
+  const onButtonClick = () => {
+    dialogRef.current.show();
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Dialog</Button>
+      {createPortal(<Dialog ref={dialogRef} />, document.body)}
+    </>
+  );
+};
+`})}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"pitfalls",children:"Pitfalls"}),`
+`,t.jsxs(e.p,{children:[`In some cases you may want to add a class or attribute that is affecting all the components HTML elements. Since the popover is outside the DOM hierarchy of the most outer element of the component, the class/attribute/etc. will not be applied to it.
+Or maybe you just don't want the popover to be mounted on the `,t.jsx(e.code,{children:"body"}),` element (which is the default).
+For this we offer the `,t.jsx(e.code,{children:"portalContainer"})," prop. This prop is available on all components that implement a popover, and with its help you can define the container into which the popover is mounted."]}),`
+`,t.jsxs(e.p,{children:["For example, if you want to display the ",t.jsx(e.code,{children:"AnalyticalTable"})," in compact mode, while the rest of the app stays in cozy mode, then you could achieve that like this:"]}),`
+`,t.jsx(e.p,{children:"Define a container somewhere in your DOM tree, where the portal should be rendered into and add the respective attribute/class/etc. to it."}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-html",children:`<div id="modalContainer" class="ui5-content-density-compact" />
+`})}),`
+`,t.jsxs(e.p,{children:["Then, set the ",t.jsx(e.code,{children:"portalContainer"})," to the respective element."]}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`<AnalyticalTable
+  className="ui5-content-density-compact"
+  portalContainer={document.getElementById('modalContainer')}
+  data={data}
+  columns={columns}
+/>
+`})}),`
+`,t.jsx(s,{style:{position:"fixed",bottom:0,zIndex:2}})]})}}export{Ot as default};
---
+++ main/assets/UploadCollection-gcYmPN0U.js
@@ -0,0 +1,123 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{C as a,A as s}from"./DomRefTable.module-HAO7vDgW.js";import{D as d}from"./DocsHeader-B-BiOesR.js";import{F as c}from"./Footer-aUUdDqRf.js";import"./index-OjgoNOWw.js";import{M as h,C as r,b as p}from"./chunk-HLWAVYOI-UvOOWukE.js";import{S as x}from"./SubcomponentsSection-pzdPLcqE.js";import{C as f,D as l,S as u,U as C}from"./UploadCollection.stories-Mvspb2PK.js";import{u as m}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-N7AwT1xE.js";import"./iframe-KoIkOD7m.js";import"../sb-preview/runtime.js";import"./UI5Element-Jt3C0_FN.js";import"./i18nBundle-ZoJ37UJR.js";import"./VersionInfo-LCCO4Ig5.js";import"./CustomElementsScopeUtils-M7hMRmEH.js";import"./withWebComponent-I1wSIv4y.js";import"./utils-KU8RGjGn.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-G6aW2FRW.js";import"./Icons-Nr5kgAbF.js";import"./decline-4Ez_2Sev.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-bkjyqQzA.js";import"./alert-7Etxrsap.js";import"./class-map-CHDPODen.js";import"./Button-CV8gKi73.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-zEJ36VRV.js";import"./index-2_yHJzy3.js";import"./clsx-c3LcQGjR.js";import"./i18n-defaults-8YolVglE.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-bWwzC2qz.js";import"./index-QYh6idnL.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-B37rLD-H.js";import"./index-XQscmlD0.js";import"./Integer-kog98579.js";import"./index-rq4D2vOx.js";import"./Avatar-VKhV6EwI.js";import"./ResizeHandler-mTFvwsyT.js";import"./employee-9rldWxaE.js";import"./index-hVTsdAjN.js";import"./index-V-WJjz6V.js";import"./Link-ipgrhFOm.js";import"./WrappingType-avPrqc94.js";import"./index-AqaqoFfn.js";import"./TableOfContent-_DNHtYzw.js";import"./index-rbQ8EChD.js";import"./Label-Xo_6xXB6.js";import"./index-R6HiNhiD.js";import"./index-hKk6h3mc.js";import"./Popover-5ANP4Jn7.js";import"./PopupUtils-QCyiTOMI.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-NvRre6vc.js";import"./FocusableElements-sOHKyey_.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-xQ1SPtMO.js";import"./BrowserScrollbar.css-R_PDLCAQ.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./index-z2fin4RT.js";import"./index-K8_8ua4m.js";import"./Title-xXK6MFOu.js";import"./ValueState-zUcANXoY.js";import"./ListItem-EtFo1sxA.js";import"./ListItemBase-HHKKMj74.js";import"./ItemNavigation--ejGb_nH.js";import"./TabbableElements-aYIPY3zp.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-i2jpCQEr.js";import"./RadioButton-Tsz_-gI0.js";import"./CheckBox-B5lM5OVp.js";import"./accept-ldlQyaEx.js";import"./HasPopup-9BAMrqbD.js";import"./slim-arrow-right-0wqMoc5g.js";import"./Input-24IzCGJD.js";import"./Suggestions.css-EG0pM5Z2.js";import"./ResponsivePopoverCommon.css-wcLb9RY0.js";import"./ValueStateMessage.css-T_IO2uU8.js";import"./ProgressIndicator-SCZReA5a.js";import"./AnimationMode-YBLH6EH2.js";import"./i18n-defaults--AZqiDkQ.js";import"./parameters-bundle.css-HXU4vJcG.js";import"./IllustratedMessage-NmV87xua.js";import"./Tent-er8ugjRY.js";const j=`**Since:** 0.9.0
+
+A component to be used within the \`UploadCollection\`.
+`;function co(n={}){const{wrapper:t}=Object.assign({},m(),n.components);return t?e.jsx(t,Object.assign({},n,{children:e.jsx(i,{})})):i();function i(){const o=Object.assign({h2:"h2",p:"p",code:"code",strong:"strong",pre:"pre",h1:"h1",h3:"h3"},m(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(h,{of:f}),`
+`,e.jsx(d,{since:"0.9.0"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h2,{id:"example",children:"Example"}),`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(a,{of:l}),`
+`,e.jsx(o.h2,{id:"handling-drag-and-drop-of-files",children:"Handling drag and drop of files"}),`
+`,e.jsxs(o.p,{children:["Per default, files can be added by dragging them into the ",e.jsx(o.code,{children:"UploadCollection"}),` component.
+To use this functionality the `,e.jsx(o.code,{children:"onDrop"})," handler has to be manually adjusted.",e.jsx("br",{}),`
+`,e.jsx(o.strong,{children:"Note:"})," Use ",e.jsx(o.code,{children:"event.preventDefault()"})," in the ",e.jsx(o.code,{children:"onDrop"})," handler, otherwise some browsers will open some files in a different tab."]}),`
+`,e.jsxs(o.p,{children:["This example shows, how to dynamically render a ",e.jsx(o.code,{children:"UploadCollectionItem"})," by dropping files into the ",e.jsx(o.code,{children:"UploadCollection"}),":"]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`import React, { useState } from 'react';
+import { Icon, Text, UploadCollection, UploadCollectionItem } from '@ui5/webcomponents-react';
+
+export const UploadCollectionComponent = () => {
+  const [children, setChildren] = useState([]);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    files.forEach((file) => {
+      setChildren((prev) => [
+        ...prev,
+        <UploadCollectionItem file={file} fileName={file.name} thumbnail={<Icon name="document" />}>
+          <Text>Uploaded On: Right now</Text>
+        </UploadCollectionItem>
+      ]);
+    });
+  };
+  return <UploadCollection onDrop={handleDrop}>{children}</UploadCollection>;
+};
+`})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h2,{id:"simulate-upload",children:"Simulate Upload"}),`
+`,e.jsx(r,{of:u}),`
+`,e.jsx(o.h3,{id:"code",children:"Code"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`const SimulateUploadComponent = () => {
+  const [children, setChildren] = useState([
+    <UploadCollectionItem
+      file={null}
+      fileName={'file-name.txt'}
+      thumbnail={<img src="https://sap.github.io/ui5-webcomponents/assets/images/logo.png" />}
+    >
+      <Text>Uploaded by: Susanne Schmitt · Uploaded On: 2019-04-20</Text>
+    </UploadCollectionItem>
+  ]);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    files.forEach((file) => {
+      setChildren((prev) => [
+        ...prev,
+        <UploadCollectionItem file={file} fileName={file.name} thumbnail={<Icon name="document" />}>
+          <Text>{\`
+Last
+modified: \${file.lastModifiedDate} ·
+Size: \${(file.size / 1000).toFixed(2)}KB\`}</Text>
+        </UploadCollectionItem>
+      ]);
+    });
+  };
+  const simulateUpload = () => {
+    if (children.length > 0) {
+      Children.forEach(children, (child, index) => {
+        if (child.props.uploadState === UploadState.Ready) {
+          let progress = 0;
+          const recTimeout = () => {
+            setTimeout(
+              () => {
+                progress += Math.floor(Math.random() * 4) + 1;
+                setChildren((prev) => {
+                  const updatedChildren = [...prev];
+                  updatedChildren[index] = cloneElement(prev[index], {
+                    uploadState: UploadState.Uploading,
+                    progress: Math.min(progress, 100)
+                  });
+                  return updatedChildren;
+                });
+                if (progress < 100) {
+                  recTimeout();
+                } else {
+                  setChildren((prev) => {
+                    const updatedChildren = [...prev];
+                    updatedChildren[index] = cloneElement(prev[index], {
+                      uploadState: UploadState.Complete
+                    });
+                    return updatedChildren;
+                  });
+                }
+              },
+              Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+            );
+          };
+          recTimeout();
+        }
+      });
+    }
+  };
+  return (
+    <UploadCollection
+      onDrop={handleDrop}
+      header={
+        <FlexBox alignItems={FlexBoxAlignItems.Center}>
+          <Title>Start upload:</Title>
+          <Button onClick={simulateUpload}>Upload!</Button>
+        </FlexBox>
+      }
+    >
+      {children}
+    </UploadCollection>
+  );
+};
+`})}),`
+`,e.jsx(p,{children:x}),`
+`,e.jsx(o.h2,{id:"uploadcollectionitem",children:"UploadCollectionItem"}),`
+`,e.jsx(p,{children:j}),`
+`,e.jsx(s,{of:C}),`
+`,e.jsx(c,{})]})}}export{co as default};
