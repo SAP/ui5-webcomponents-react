d75a21c9f1f24af37c463e712fb4799ec5af4ee7
Lukas742 <lukas.harbarth@sap.com>
2023-12-20 12:42:27 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@1fd9f5ae919b00e0cf53c1ef76640237622abb3f 🚀

---
+++ main/assets/Button-dyIC_iuE.js
@@ -0,0 +1,28 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{M as p,C as m,b as a}from"./chunk-HLWAVYOI-gDttGN3m.js";import{C as l}from"./DomRefTable.module-YF5yLoWc.js";import{D as c}from"./DocsHeader-BxNzXZJf.js";import{F as d}from"./Footer-ZsSjQxcR.js";import"./index-OjgoNOWw.js";import{D as u}from"./DomRefTable-kxoWpxsk.js";import{C as h,D as n}from"./Button.stories-G0zfQ-Ou.js";import{u as s}from"./index-RwE9nQVW.js";import"./iframe-eO5ZJSn9.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./react-jss.esm-tmBtHfec.js";import"./index-kt8wc_gZ.js";import"./UI5Element-4kitL615.js";import"./i18nBundle-h5yKZf-0.js";import"./VersionInfo-yX_RFFR1.js";import"./CustomElementsScopeUtils-icazltFf.js";import"./withWebComponent-y_2h_qsG.js";import"./utils-jDGQm03y.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-ic8wGgAs.js";import"./Icons-cDXElGrC.js";import"./decline-tlNcqIac.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-txDdHyyj.js";import"./alert-sbIfV1of.js";import"./class-map-aNJl42Aw.js";import"./Button-dXtUjl7w.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-zEJ36VRV.js";import"./index-IPJJPG41.js";import"./clsx-c3LcQGjR.js";import"./i18n-defaults-EHbh3l3V.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-DWXxr99t.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-kI6DTjO7.js";import"./index-nDwo5N6E.js";import"./Integer-kog98579.js";import"./index-D-MP_7kc.js";import"./Avatar-DexMZin0.js";import"./ResizeHandler-_S6lmbp5.js";import"./employee-jQid-Nvn.js";import"./index-Hv-mV4uB.js";import"./index-RppeFWF1.js";import"./Link-yiZ7VLOd.js";import"./WrappingType-avPrqc94.js";import"./index-5ETO3hDj.js";import"./TableOfContent-EK_0cmrB.js";import"./index-3K7vbEfI.js";import"./Label-avDcfEez.js";import"./index-ozDH_TmN.js";import"./index-QG4pr-3F.js";import"./Popover-jBPmsweJ.js";import"./PopupUtils-9DzRw33t.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-Honr3Kcv.js";import"./FocusableElements-eRJsgpvJ.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-cRo_ADDP.js";import"./BrowserScrollbar.css-IWLYYIFQ.js";const f=`## Usage
+
+For the \`Button\` UI, you can define text, icon, or both. You can also specify whether the text or the icon is displayed first.
+
+You can choose from a set of predefined types that offer different styling to correspond to the triggered action.
+
+You can set the \`Button\` as enabled or disabled. An enabled \`Button\` can be pressed by clicking or tapping it. The button changes its style to provide visual feedback to the user that it is pressed or hovered over with the mouse cursor. A disabled \`Button\` appears inactive and cannot be pressed.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`Button\` exposes the following CSS Shadow Parts:
+
+- button - Used to style the native button element
+`,b=[{name:"accessibilityAttributes",visibility:"public",since:"1.2.0",type:"object",noattribute:"true",description:`An object of strings that defines several additional accessibility attribute values for customization depending on the use case.
+
+It supports the following fields:
+
+<ul> <li><code>expanded</code>: Indicates whether the button, or another grouping element it controls, is currently expanded or collapsed. Accepts the following string values: <ul> <li><code>true</code></li> <li><code>false</code></li> </ul> </li> <li><code>hasPopup</code>: Indicates the availability and type of interactive popup element, such as menu or dialog, that can be triggered by the button. Accepts the following string values: <ul> <li><code>Dialog</code></li> <li><code>Grid</code></li> <li><code>ListBox</code></li> <li><code>Menu</code></li> <li><code>Tree</code></li> </ul> </li> <li><code>controls</code>: Identifies the element (or elements) whose contents or presence are controlled by the button element. Accepts a string value.</li> </ul>`}];function Yt(o={}){const{wrapper:e}=Object.assign({},s(),o.components);return e?t.jsx(e,Object.assign({},o,{children:t.jsx(i,{})})):i();function i(){const r=Object.assign({h2:"h2"},s(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(p,{of:h}),`
+`,t.jsx(c,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(m,{of:n}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{of:n}),`
+`,t.jsx(a,{children:f}),`
+`,t.jsx(u,{rows:b}),`
+`,t.jsx(d,{})]})}}export{Yt as default};
--- main/assets/preview-63JAEAAg.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-W9oKbgv_.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-rrByeCmM.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-rrByeCmM.js","./chunk-HLWAVYOI-2Z6kHeeZ.js","./iframe-W9oKbgv_.js","./index-OjgoNOWw.js","./react-18-1WT2dSvK.js","./index-QYh6idnL.js","./_baseForOwn-UZJwRxDU.js","./mapValues-HzTlT2lF.js","./_baseUniq-JVyScZ-n.js","./index-PPLHz8o0.js","./index-UBYxvHMA.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
