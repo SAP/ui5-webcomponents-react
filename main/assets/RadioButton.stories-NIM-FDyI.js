e58e9a74dfd6d8ad0bc45194111c199472447dee
Lukas742 <lukas.harbarth@sap.com>
2023-12-20 14:50:31 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@7217f169182931092d77017ea4e23b98413b2657 🚀

---
+++ main/assets/RadioButton.stories-NIM-FDyI.js
@@ -0,0 +1,53 @@
+import{M as b,C as u,f as d,b as y}from"./chunk-HLWAVYOI-z6cdnfpC.js";import{R as e}from"./index-E-FcixRv.js";import{L as i}from"./index-lixWgTAI.js";import{F as n,b as m,c,a as j}from"./index-xeXwq5Td.js";import{V as r}from"./ValueState-I_-hGNIb.js";import{b as R}from"./DomRefTable.module-deYQm94B.js";import{D as v}from"./DocsHeader-f_kbb6Od.js";import{F as f}from"./Footer-D6okhxg2.js";import{j as t}from"./jsx-runtime-DtaoT6pD.js";import"./index-OjgoNOWw.js";import{u as x}from"./index-RwE9nQVW.js";import"./iframe-OUhN9IcB.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./RadioButton-9sU8Xbqd.js";import"./UI5Element-4kitL615.js";import"./i18nBundle-h5yKZf-0.js";import"./VersionInfo-yX_RFFR1.js";import"./CustomElementsScopeUtils-icazltFf.js";import"./withWebComponent-y_2h_qsG.js";import"./utils-jDGQm03y.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-ic8wGgAs.js";import"./Icons-cDXElGrC.js";import"./ValueState-zUcANXoY.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./Label-avDcfEez.js";import"./WrappingType-avPrqc94.js";import"./i18n-defaults-zEJ36VRV.js";import"./class-map-aNJl42Aw.js";import"./clsx-c3LcQGjR.js";import"./react-jss.esm-tmBtHfec.js";import"./index-N10PmBcC.js";import"./decline-tlNcqIac.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-txDdHyyj.js";import"./alert-sbIfV1of.js";import"./Button-dXtUjl7w.js";import"./MarkedEvents-aYv14vsS.js";import"./index-IPJJPG41.js";import"./i18n-defaults-EHbh3l3V.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-DWXxr99t.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-kI6DTjO7.js";import"./index-JsB2bbd6.js";import"./Integer-kog98579.js";import"./index-aNibr1Qe.js";import"./Avatar-DexMZin0.js";import"./ResizeHandler-_S6lmbp5.js";import"./employee-jQid-Nvn.js";import"./index-HYdRW1ex.js";import"./index-RKRxVJ0W.js";import"./Link-yiZ7VLOd.js";import"./TableOfContent-pezQnszU.js";import"./index-2hVy3VGA.js";import"./index-ocVpV3yp.js";import"./Popover-jBPmsweJ.js";import"./PopupUtils-9DzRw33t.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-Honr3Kcv.js";import"./FocusableElements-eRJsgpvJ.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-cRo_ADDP.js";import"./BrowserScrollbar.css-IWLYYIFQ.js";const G=`## Keyboard Handling
+
+Once the \`RadioButton\` is on focus, it might be selected by pressing the Space and Enter keys.  
+The Arrow Down/Arrow Up and Arrow Left/Arrow Right keys can be used to change selection between next/previous radio buttons in one group, while TAB and SHIFT + TAB can be used to enter or leave the radio button group.  
+**Note:** On entering radio button group, the focus goes to the currently selected radio button.
+`;function E(a={}){const{wrapper:l}=Object.assign({},x(),a.components);return l?t.jsx(l,{...a,children:t.jsx(p,{})}):p();function p(){const o=Object.assign({h2:"h2",h1:"h1",p:"p",code:"code"},x(),a.components);return t.jsxs(t.Fragment,{children:[t.jsx(b,{title:"Inputs / RadioButton",component:e,args:{valueState:r.None,text:"RadioButton"}}),`
+`,t.jsx(v,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(u,{children:t.jsx(d,{name:"Default",children:S=>t.jsx(e,{...S})})}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(R,{story:"Default"}),`
+`,t.jsx(y,{children:G}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"radiobutton-groups",children:"RadioButton groups"}),`
+`,t.jsxs(o.p,{children:["Group radio buttons by using the ",t.jsx(o.code,{children:"name"})," prop."]}),`
+`,t.jsx(u,{children:t.jsx(d,{name:"RadioButton groups",children:()=>t.jsxs(n,{style:{width:"600px"},justifyContent:m.SpaceBetween,children:[t.jsxs(n,{alignItems:c.Center,children:[t.jsx(i,{children:"Horizontal Group: "}),t.jsx(e,{name:"GroupA",text:"Option A"}),t.jsx(e,{name:"GroupA",text:"Option B"}),t.jsx(e,{name:"GroupA",text:"Option C"})]}),t.jsxs(n,{direction:j.Column,children:[t.jsx(i,{children:"Vertical Group: "}),t.jsx(e,{name:"GroupB",text:"Neutral",valueState:r.None}),t.jsx(e,{name:"GroupB",text:"Warning",valueState:r.Warning}),t.jsx(e,{name:"GroupB",text:"Critical",valueState:r.Error})]})]})})}),`
+`,t.jsx("br",{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"radiobutton-states",children:"RadioButton states"}),`
+`,t.jsx(u,{children:t.jsx(d,{name:"RadioButton states",children:()=>t.jsxs(t.Fragment,{children:[t.jsx(e,{text:"Error",valueState:r.Error}),t.jsx(e,{text:"Warning",valueState:r.Warning}),t.jsx(e,{text:"Disabled",disabled:!0,checked:!0}),t.jsx(e,{text:"Readonly",readonly:!0,checked:!0}),t.jsx(e,{text:"Error disabled",disabled:!0,valueState:r.Error,checked:!0}),t.jsx(e,{text:"Warning disabled ",disabled:!0,valueState:r.Warning,checked:!0}),t.jsx(e,{text:"Error readonly",readonly:!0,valueState:r.Error,checked:!0}),t.jsx(e,{text:"Warning readonly",readonly:!0,valueState:r.Warning,checked:!0})]})})}),`
+`,t.jsx(f,{})]})}}const h=a=>t.jsx(e,{...a});h.storyName="Default";h.parameters={storySource:{source:`args => {
+  return <RadioButton {...args} />;
+}`}};const B=()=>t.jsxs(n,{style:{width:"600px"},justifyContent:m.SpaceBetween,children:[t.jsxs(n,{alignItems:c.Center,children:[t.jsx(i,{children:"Horizontal Group: "}),t.jsx(e,{name:"GroupA",text:"Option A"}),t.jsx(e,{name:"GroupA",text:"Option B"}),t.jsx(e,{name:"GroupA",text:"Option C"})]}),t.jsxs(n,{direction:j.Column,children:[t.jsx(i,{children:"Vertical Group: "}),t.jsx(e,{name:"GroupB",text:"Neutral",valueState:r.None}),t.jsx(e,{name:"GroupB",text:"Warning",valueState:r.Warning}),t.jsx(e,{name:"GroupB",text:"Critical",valueState:r.Error})]})]});B.storyName="RadioButton groups";B.parameters={storySource:{source:`() => {
+  return <FlexBox style={{
+    width: "600px"
+  }} justifyContent={FlexBoxJustifyContent.SpaceBetween}>
+          <FlexBox alignItems={FlexBoxAlignItems.Center}>
+            <Label>Horizontal Group: </Label>
+            <RadioButton name="GroupA" text="Option A" />
+            <RadioButton name="GroupA" text="Option B" />
+            <RadioButton name="GroupA" text="Option C" />
+          </FlexBox>
+          <FlexBox direction={FlexBoxDirection.Column}>
+            <Label>Vertical Group: </Label>
+            <RadioButton name="GroupB" text="Neutral" valueState={ValueState.None} />
+            <RadioButton name="GroupB" text="Warning" valueState={ValueState.Warning} />
+            <RadioButton name="GroupB" text="Critical" valueState={ValueState.Error} />
+          </FlexBox>
+        </FlexBox>;
+}`}};const g=()=>t.jsxs(t.Fragment,{children:[t.jsx(e,{text:"Error",valueState:r.Error}),t.jsx(e,{text:"Warning",valueState:r.Warning}),t.jsx(e,{text:"Disabled",disabled:!0,checked:!0}),t.jsx(e,{text:"Readonly",readonly:!0,checked:!0}),t.jsx(e,{text:"Error disabled",disabled:!0,valueState:r.Error,checked:!0}),t.jsx(e,{text:"Warning disabled ",disabled:!0,valueState:r.Warning,checked:!0}),t.jsx(e,{text:"Error readonly",readonly:!0,valueState:r.Error,checked:!0}),t.jsx(e,{text:"Warning readonly",readonly:!0,valueState:r.Warning,checked:!0})]});g.storyName="RadioButton states";g.parameters={storySource:{source:`() => {
+  return <>
+          <RadioButton text="Error" valueState={ValueState.Error} />
+          <RadioButton text="Warning" valueState={ValueState.Warning} />
+          <RadioButton text="Disabled" disabled checked />
+          <RadioButton text="Readonly" readonly checked />
+          <RadioButton text="Error disabled" disabled valueState={ValueState.Error} checked />
+          <RadioButton text="Warning disabled " disabled valueState={ValueState.Warning} checked />
+          <RadioButton text="Error readonly" readonly valueState={ValueState.Error} checked />
+          <RadioButton text="Warning readonly" readonly valueState={ValueState.Warning} checked />
+        </>;
+}`}};const s={title:"Inputs / RadioButton",component:e,args:{valueState:r.None,text:"RadioButton"},tags:["stories-mdx"],includeStories:["defaultStory","radioButtonGroups","radioButtonStates"]};s.parameters=s.parameters||{};s.parameters.docs={...s.parameters.docs||{},page:E};const Qt=["defaultStory","radioButtonGroups","radioButtonStates"];export{Qt as __namedExportsOrder,s as default,h as defaultStory,B as radioButtonGroups,g as radioButtonStates};
