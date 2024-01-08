2ea65d3375c939d0611a76164d4b87ffff72385c
Lukas742 <lukas.harbarth@sap.com>
2024-01-08 14:45:22 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@d90701f1015b8fc9a8f263e2f86d7ce1f22e92b5 🚀

---
+++ main/assets/BulletChart-d-WjneAz.js
@@ -0,0 +1,44 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{C as s}from"./DomRefTable.module-g00We47Y.js";import{D as a}from"./DocsHeader-RTkASSCj.js";import{F as l}from"./Footer-Tw0c5FOI.js";import"./index-OjgoNOWw.js";import{M as h,C as o}from"./chunk-HLWAVYOI-QyPUWpD_.js";import{C as d,D as p,W as c,a as u,L as x}from"./BulletChart.stories-TZT8lGze.js";import{u as n}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-30sL2KcA.js";import"./iframe-ZYoNH1Ok.js";import"../sb-preview/runtime.js";import"./UI5Element-N46uHiF6.js";import"./i18nBundle-1uTNzT_G.js";import"./VersionInfo-doEI4kCH.js";import"./CustomElementsScopeUtils--FMJdKyV.js";import"./withWebComponent-m2csmhkT.js";import"./utils-7qe6Bov5.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-q2l1h21j.js";import"./Icons-JGtWxtgH.js";import"./decline-tUGLActy.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-rXvvQTyd.js";import"./alert-n6PTDijY.js";import"./class-map-lkLxloiQ.js";import"./Button-5EpXIGtS.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-Hnw7H5Tc.js";import"./index-O1qeqEwP.js";import"./clsx-Zbgk8kpT.js";import"./i18n-defaults-PpubpVn0.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-Ad7wliO4.js";import"./index-QYh6idnL.js";import"./ModalsContext-0q87fkkd.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-SeojittA.js";import"./index-lrZfQyXV.js";import"./Integer-kog98579.js";import"./index-DcIhxOnL.js";import"./Avatar-3tdvLPxl.js";import"./ResizeHandler-pjwwRwdX.js";import"./employee-1rhW5wth.js";import"./index-1lrvU4gV.js";import"./index-9UPDDvaG.js";import"./Link-GBCkwT5X.js";import"./WrappingType-avPrqc94.js";import"./index-ndaVN1fR.js";import"./TableOfContent-cvO5GOsL.js";import"./index-ChgULISF.js";import"./Label-lanRvJLA.js";import"./index-mDmCwUag.js";import"./index-oyT_lcn6.js";import"./Popover-QuSZvfFm.js";import"./PopupUtils-K8GVBVSe.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-T6pSPG1a.js";import"./FocusableElements-p1LB11Hl.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-2OzZTNdj.js";import"./BrowserScrollbar.css-WTCBaVgb.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./DemoProps-gxklhiCN.js";import"./index-Jm2lAFsC.js";import"./YAxisTicks-sQ9DhT0Z.js";import"./ChartContainer-4h6PjsUo.js";import"./index-EbeA8sPb.js";import"./throttle-NPD2LhEd.js";import"./isString-yBrqhg_E.js";import"./debounce-F9irgL9Y.js";import"./useTooltipFormatter-y1wqlRZ4.js";import"./defaults-a7U8OXSF.js";import"./ChartDataLabel-i4exvmGX.js";import"./generateCategoricalChart-SrN07Gw4.js";import"./tiny-invariant-VyoA2Os1.js";import"./isPlainObject-hqM_z3iV.js";import"./staticProps-YAKXJgHU.js";import"./useLongestYAxisLabel-i86RvERZ.js";import"./useOnClickInternal-MGInwGCL.js";import"./react-content-loader.es-MhslfaKA.js";import"./ComposedChart-KubGOJcq.js";import"./Line-7kmLwHLk.js";import"./Scatter-eD9PObgp.js";import"./useIsRTL-jHm4vr0Q.js";function mr(i={}){const{wrapper:m}=Object.assign({},n(),i.components);return m?t.jsx(m,Object.assign({},i,{children:t.jsx(e,{})})):e();function e(){const r=Object.assign({h2:"h2",p:"p",code:"code",pre:"pre",h3:"h3"},n(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(h,{of:d}),`
+`,t.jsx(a,{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(o,{of:p}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"usage-notes",children:"Usage Notes"}),`
+`,t.jsxs(r.p,{children:[`In order to display the "Bullet" lines in the chart, you have to configure which measure should be rendered as bullet line.
+You can do this via the `,t.jsx(r.code,{children:"measures"})," array:"]}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`<BulletChart
+  data={
+    [
+      /* data */
+    ]
+  }
+  dimensions={
+    [
+      /* dimensions */
+    ]
+  }
+  measures={[
+    {
+      accessor: 'actuals',
+      label: 'Actual Revenue',
+      type: 'primary' // this will be the primary bar
+    },
+    {
+      accessor: 'target',
+      label: 'Planned Target Revenue',
+      type: 'comparison' // this will be the bullet line
+    }
+  ]}
+/>
+`})}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(r.h3,{id:"with-custom-size",children:"With Custom Size"}),`
+`,t.jsx(o,{of:c}),`
+`,t.jsx(r.h3,{id:"with-highlighted-measures",children:"With Highlighted Measures"}),`
+`,t.jsx(o,{of:u}),`
+`,t.jsx(r.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,t.jsx(o,{of:x}),`
+`,t.jsx(l,{})]})}}export{mr as default};
