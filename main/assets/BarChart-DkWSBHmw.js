23e9c58cf4c329550da64d8125f3abeb1f67b8b6
Lukas742 <lukas.harbarth@sap.com>
2024-01-15 09:53:50 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@e33c227fc38ecf2f1b61b3749fe8f52f8f22302f 🚀

---
+++ main/assets/BarChart-DkWSBHmw.js
@@ -0,0 +1,36 @@
+import{j as r}from"./jsx-runtime-DtaoT6pD.js";import{C as s}from"./DomRefTable.module-FHpKjoTY.js";import{D as a}from"./DocsHeader-veex5_VZ.js";import{F as h}from"./Footer-qb7e8Gzm.js";import"./index-OjgoNOWw.js";import{M as c,C as i}from"./chunk-HLWAVYOI-BtHNkjJ1.js";import{C as d,D as m,W as l,a as x,b as j,c as f,L as u,d as g,e as C}from"./BarChart.stories-7ICmLQRe.js";import{u as p}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-ah7_UbJ1.js";import"./iframe-6UCVWvHy.js";import"../sb-preview/runtime.js";import"./UI5Element-kBA_KQxG.js";import"./i18nBundle-7ihWi2Lo.js";import"./VersionInfo-quO5DfnC.js";import"./CustomElementsScopeUtils-QKoQ990a.js";import"./withWebComponent-22JjwLkL.js";import"./utils-3rf7SAGj.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-r6t1Zphl.js";import"./Icons-jRef6OLh.js";import"./decline-XpWJwTfv.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-U_TSW4LN.js";import"./alert-kkNs8dOr.js";import"./class-map-2qwEQoLI.js";import"./Button-jlOsC8Op.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-QBShB6vc.js";import"./index-9hLZiafD.js";import"./clsx-Zbgk8kpT.js";import"./i18n-defaults-gX8Lho5I.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-YJVSj-ci.js";import"./index-QYh6idnL.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-Ha-6ONKz.js";import"./index-3lVx-hFJ.js";import"./Integer-kog98579.js";import"./index-7O3Za8GJ.js";import"./Avatar-kcq5ywk4.js";import"./ResizeHandler-3FzJmCyz.js";import"./employee-APVUdoZS.js";import"./index-wnWRGYNo.js";import"./index-F4qp8fbP.js";import"./Link-xIUn2Lee.js";import"./WrappingType-avPrqc94.js";import"./index-ndaVN1fR.js";import"./TableOfContent-1jgPr6bS.js";import"./index-G1WlbNMS.js";import"./Label-yPOoTXIn.js";import"./index-EOhVcGrD.js";import"./index-ZfJFoP52.js";import"./Popover-qTobexuh.js";import"./PopupUtils-FgdW7Gkh.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-fa8QSlgN.js";import"./FocusableElements-jbTazhpM.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-duavIDje.js";import"./BrowserScrollbar.css-aT2mYiFz.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./DemoProps-gxklhiCN.js";import"./index-Jm2lAFsC.js";import"./defaults-Kh7Z5Jts.js";import"./ChartContainer-n6XLmd07.js";import"./index-Ydt675Et.js";import"./throttle-NPD2LhEd.js";import"./isString-yBrqhg_E.js";import"./useCancelAnimationFallback-WLkDw4sh.js";import"./generateCategoricalChart-82phN1si.js";import"./tiny-invariant-VyoA2Os1.js";import"./isPlainObject-hqM_z3iV.js";import"./YAxisTicks-I7gl3Z57.js";import"./debounce-F9irgL9Y.js";import"./useTooltipFormatter--xxRUKkq.js";import"./ChartDataLabel-pS_uKGJE.js";import"./staticProps-YAKXJgHU.js";import"./useOnClickInternal-MGInwGCL.js";import"./Placeholder-L1vH04j7.js";import"./react-content-loader.es-MhslfaKA.js";import"./useIsRTL-ji9lVEBB.js";function mo(t={}){const{wrapper:e}=Object.assign({},p(),t.components);return e?r.jsx(e,Object.assign({},t,{children:r.jsx(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h3:"h3",p:"p",code:"code",a:"a",pre:"pre"},p(),t.components);return r.jsxs(r.Fragment,{children:[r.jsx(c,{of:d}),`
+`,r.jsx(a,{}),`
+`,r.jsx(o.h2,{id:"example",children:"Example"}),`
+`,r.jsx(i,{of:m}),`
+`,r.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,r.jsx(s,{of:m}),`
+`,r.jsx("br",{}),`
+`,r.jsx("br",{}),`
+`,r.jsx(o.h2,{id:"more-examples",children:"More Examples"}),`
+`,r.jsx(o.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,r.jsx(i,{of:l}),`
+`,r.jsx(o.h3,{id:"with-secondary-dimension",children:"With Secondary Dimension"}),`
+`,r.jsx(i,{of:x}),`
+`,r.jsx(o.h3,{id:"with-data-labels",children:"With Data Labels"}),`
+`,r.jsx(i,{of:j}),`
+`,r.jsx(o.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,r.jsx(i,{of:f}),`
+`,r.jsx(o.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,r.jsx(i,{of:u}),`
+`,r.jsx(o.h3,{id:"with-reference-line",children:"With Reference Line"}),`
+`,r.jsxs(o.p,{children:["You can set a reference line to any value by using the ",r.jsx(o.code,{children:"referenceLine"})," ",r.jsx(o.code,{children:"chartConfig"})," property. ",r.jsx(o.code,{children:"referenceLine"})," accepts all properties described ",r.jsx(o.a,{href:"https://recharts.org/en-US/api/ReferenceLine",target:"_blank",rel:"nofollow noopener noreferrer",children:"here"}),", but it's recommended to only use ",r.jsx(o.code,{children:"label"}),",",r.jsx(o.code,{children:"value"})," and ",r.jsx(o.code,{children:"color"})," to preserve the intended design."]}),`
+`,r.jsx(o.pre,{children:r.jsx(o.code,{className:"language-jsx",children:`<BarChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,r.jsx(i,{of:g}),`
+`,r.jsx(o.h3,{id:"with-highlighted-measure",children:"With Highlighted Measure"}),`
+`,r.jsx(i,{of:C}),`
+`,r.jsx(h,{})]})}}export{mo as default};
---
+++ main/assets/Menu-sKH2K6ye.js
@@ -0,0 +1,84 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{C as m,A as c}from"./DomRefTable.module-FHpKjoTY.js";import{D as d}from"./DocsHeader-veex5_VZ.js";import"./index-OjgoNOWw.js";import"./index-QYh6idnL.js";import{M as h,C as s,b as o}from"./chunk-HLWAVYOI-BtHNkjJ1.js";import{D as u}from"./DomRefTable-GeXx8oXC.js";import{C as l,D as p,W as x}from"./Menu.stories-b8WEErDE.js";import{M as f}from"./index-ah7_UbJ1.js";import{a as j}from"./index--JqAmE17.js";import{S as b}from"./SubcomponentsSection-pzdPLcqE.js";import{e as g}from"./utils-77gZ8405.js";import{u as a}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-9hLZiafD.js";import"./clsx-Zbgk8kpT.js";import"./i18n-defaults-gX8Lho5I.js";import"./i18nBundle-7ihWi2Lo.js";import"./VersionInfo-quO5DfnC.js";import"./I18nContext-VUOpdMoH.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./ThemingParameters-UghqSl-x.js";import"./index-YJVSj-ci.js";import"./UI5Element-kBA_KQxG.js";import"./CustomElementsScopeUtils-QKoQ990a.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-Ha-6ONKz.js";import"./utils-3rf7SAGj.js";import"./index-3lVx-hFJ.js";import"./iframe-6UCVWvHy.js";import"../sb-preview/runtime.js";import"./withWebComponent-22JjwLkL.js";import"./slot-HSrR9XJ-.js";import"./Icon-r6t1Zphl.js";import"./Icons-jRef6OLh.js";import"./Integer-kog98579.js";import"./class-map-2qwEQoLI.js";import"./i18n-defaults-QBShB6vc.js";import"./index-7O3Za8GJ.js";import"./Avatar-kcq5ywk4.js";import"./ResizeHandler-3FzJmCyz.js";import"./employee-APVUdoZS.js";import"./alert-kkNs8dOr.js";import"./index-wnWRGYNo.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./index-F4qp8fbP.js";import"./Link-xIUn2Lee.js";import"./MarkedEvents-aYv14vsS.js";import"./WrappingType-avPrqc94.js";import"./index-ndaVN1fR.js";import"./TableOfContent-1jgPr6bS.js";import"./index-G1WlbNMS.js";import"./Label-yPOoTXIn.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-UZJwRxDU.js";import"./mapValues-HzTlT2lF.js";import"./_baseUniq-JVyScZ-n.js";import"./index-PPLHz8o0.js";import"./index-UBYxvHMA.js";import"./index-EOhVcGrD.js";import"./Button-jlOsC8Op.js";import"./decline-XpWJwTfv.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-U_TSW4LN.js";import"./Popover-qTobexuh.js";import"./PopupUtils-FgdW7Gkh.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-fa8QSlgN.js";import"./FocusableElements-jbTazhpM.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-duavIDje.js";import"./BrowserScrollbar.css-aT2mYiFz.js";import"./slim-arrow-right-0oBZC6Yv.js";import"./ResponsivePopover-wHlXl3nI.js";import"./Dialog-Y9sM4F0U.js";import"./ValueState-zUcANXoY.js";import"./Title-dLDxQQ5A.js";import"./List-XV25F2MG.js";import"./ItemNavigation-1Qtl9Bqn.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-ic-NtdR-.js";import"./CheckBox-vOrD_Qxh.js";import"./accept-RviPAaVm.js";import"./RadioButton-ssFCKs8g.js";import"./StandardListItem-cf7NJ5rV.js";import"./ListItem-_0EIb7ra.js";import"./HasPopup-9BAMrqbD.js";const M="## Usage\n\n`Menu` contains `Menu-item` components. An arbitrary hierarchy structure can be represented by recursively nesting menu items.\n\n## Keyboard Handling\n\nThe `Menu` provides advanced keyboard handling. The user can use the following keyboard shortcuts in order to navigate trough the tree:\n\n- `Arrow Up` / `Arrow Down` - Navigates up and down the menu items that are currently visible.\n- `Arrow Right`, `Space` or `Enter` - Opens a sub-menu if there are menu items nested in the currently clicked menu item.\n- `Arrow Left` or `Escape` - Closes the currently opened sub-menu.\n\nNote: if the text ditrection is set to Right-to-left (RTL), `Arrow Right` and `Arrow Left` functionality is swapped.\n",w="**Since:** 0.23.0\n**Abstract UI5 Web Component** - Find out more about abstract components [here](https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base-faq--docs#what-are-abstract-ui5-web-components).\n\n`MenuItem` is the item to use inside a `Menu`. An arbitrary hierarchy structure can be represented by recursively nesting menu items.\n\n### Usage\n\n`MenuItem` is an abstract element, representing a node in a `Menu`. The menu itself is rendered as a list, and each `MenuItem` is represented by a list item (`StandardListItem`) in that list. Therefore, you should only use `MenuItem` directly in your apps. The `StandardListItem` list item is internal for the list, and not intended for public use.\n",y=[{name:"close",visibility:"public",description:"Closes the Menu."},{name:"showAt",visibility:"public",parameters:[{name:"opener",type:"HTMLElement",optional:!1,description:"the element that the popover is shown at"}],description:"Shows the Menu near the opener element."}];function an(t={}){const{wrapper:r}=Object.assign({},a(),t.components);return r?e.jsx(r,Object.assign({},t,{children:e.jsx(i,{})})):i();function i(){const n=Object.assign({h2:"h2",h4:"h4",p:"p",code:"code",pre:"pre",strong:"strong",em:"em",h3:"h3"},a(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(h,{of:l}),`
+`,e.jsx(d,{since:"0.23.0"}),`
+`,e.jsx(n.h2,{id:"example",children:"Example"}),`
+`,e.jsx(s,{of:p}),`
+`,e.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(m,{of:p}),`
+`,e.jsx(o,{children:M}),`
+`,e.jsx(u,{rows:y}),`
+`,e.jsx(n.h2,{id:"opening-menus",children:"Opening Menus"}),`
+`,e.jsx(n.h4,{id:"since-180",children:"since 1.8.0"}),`
+`,e.jsxs(n.p,{children:["We recommend opening and closing the ",e.jsx(n.code,{children:"Menu"})," component in a declarative way using the ",e.jsx(n.code,{children:"open"})," and ",e.jsx(n.code,{children:"opener"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const MyComponentWithMenu = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        id={'openMenuBtn'}
+        onClick={() => {
+          setMenuIsOpen(true);
+        }}
+      >
+        Open Action Sheet
+      </Button>
+      <Menu
+        opener={'openMenuBtn'}
+        open={menuIsOpen}
+        onAfterClose={() => {
+          setMenuIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.p,{children:e.jsxs(n.strong,{children:["Opening a ",e.jsx(n.code,{children:"Menu"})," by reference and not by ",e.jsx(n.code,{children:"id"})]})}),`
+`,e.jsxs(n.p,{children:["This web component exposes a way to pass a reference of an element instead of an ",e.jsx(n.code,{children:"id"})," to the ",e.jsx(n.code,{children:"opener"})," prop. Since this is not supported when passing the reference in a declarative way to a React ",e.jsx(n.code,{children:"prop"}),`, you have to attach the ref directly on the web component.
+You can do that by e.g. leveraging a React Ref, and then set the corresponding property there.`]}),`
+`,e.jsx(f,{hideCloseButton:!0,children:`Changing properties outside of the React rendering cycle can lead to unwanted behavior, please be extra cautious when
+using this approach!`}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const MenuComponent = () => {
+  const popoverRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const handleOpenerClick = (e) => {
+    if (popoverRef.current) {
+      popoverRef.current.opener = e.target;
+      setOpen((prev) => !prev);
+    }
+  };
+  return (
+    <>
+      <Button onClick={handleOpenerClick}>Opener</Button>
+      <Menu ref={popoverRef} open={open}>
+        {/* Content */}
+      </Menu>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.h4,{id:"before-180",children:"before 1.8.0"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"Menu"}),"s can be opened by attaching a ",e.jsx(n.code,{children:"ref"})," to the component and then call the corresponding ",e.jsx(n.strong,{children:e.jsx(n.code,{children:"showAt"})})," method. The method receives the target element - ",e.jsxs(n.em,{children:["on which the ",e.jsx(n.code,{children:"Menu"})," is to be opened"]})," - as parameter."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const MenuComponent = () => {
+  const popoverRef = useRef();
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Menu</Button>
+      <Menu ref={popoverRef}>
+        <MenuItem text="New File" icon="add-document" />
+        <MenuItem text="New Folder" icon="add-folder" disabled />
+      </Menu>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.h2,{id:"more-examples",children:"More examples"}),`
+`,e.jsx(n.h3,{id:"menu-with-submenu",children:"Menu with Submenu"}),`
+`,e.jsx(s,{of:x}),`
+`,e.jsx(o,{children:b}),`
+`,e.jsx(n.h2,{id:"menuitem",children:"MenuItem"}),`
+`,e.jsx(o,{children:w}),`
+`,e.jsx(c,{hideHTMLPropsNote:!0,exclude:g,of:j})]})}}export{an as default};
