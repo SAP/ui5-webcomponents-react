51ac7f7ee9c2e9146a68f3a32253f62ff3fd8817
Lukas Harbarth <lukas.harbarth@sap.com>
2024-01-19 13:30:20 +0100

    fix test in ci

--- packages/main/src/components/ObjectPage/ObjectPage.cy.tsx
+++ packages/main/src/components/ObjectPage/ObjectPage.cy.tsx
@@ -191,12 +191,11 @@ describe('ObjectPage', () => {
     cy.findByText('ObjectPageHeader').should('be.visible');
 
     cy.findByTestId('op').scrollTo(0, 500);
+    cy.wait(500);
     cy.findByTestId('btn').click();
-    cy.wait(100);
     cy.findByText('ObjectPageHeader').should('not.be.visible');
 
     cy.findByTestId('btn').click();
-    cy.wait(100);
     cy.findByText('ObjectPageHeader').should('be.visible');
     cy.get('@onPinSpy').should('have.callCount', 7);
   });
