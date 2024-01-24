43001a9f8eb3a6beef9f542cbd9e162f22b2500b
Marcus Notheis <marcus.notheis@sap.com>
2024-01-24 15:42:26 +0100

    consolidate all interfaces in types folder

--- packages/main/src/types/index.ts
+++ packages/main/src/types/index.ts
@@ -1,11 +1,17 @@
 import type { ReactElement, ReactNode, ReactPortal } from 'react';
 
+export type { CommonProps } from './CommonProps.js';
+export type { Ui5CustomEvent } from './Ui5CustomEvent.js';
+export type { Ui5DomRef } from './Ui5DomRef.js';
+
 type ReducedReactNode = Exclude<ReactNode, string | number | boolean | ReactPortal | Iterable<ReactNode>>;
-export type UI5WCSlotsNode =
+type InternalUI5WCSlotsNode =
   | ReducedReactNode
   | Iterable<ReducedReactNode>
   | false
   | ReactElement /* necessary for React v16 & v17 ReactNode type*/;
+
+export type UI5WCSlotsNode = InternalUI5WCSlotsNode | InternalUI5WCSlotsNode[];
 
 export type Nullable<T> = T | null;
 
