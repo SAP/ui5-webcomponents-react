6806209c45d750dd6ea183c2cf3c54aa5ccb76da
Lukas742 <lukas.harbarth@sap.com>
2024-01-29 12:21:54 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@d3f7655190a98df6b15fb9bb86ce54ee49c323dd 🚀

---
+++ main/assets/MultiComboBox-A81dfjHR.js
@@ -0,0 +1,52 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{C as c,b as r}from"./DomRefTable.module-i5j1_1vk.js";import{D as h}from"./DocsHeader-5f7fG-zQ.js";import{F as d}from"./Footer--3QYZY2g.js";import"./index-OjgoNOWw.js";import{M as u,C as p,b as e}from"./chunk-HLWAVYOI-0k14eHPc.js";import{D as b}from"./DomRefTable-RZyfszQn.js";import{S as x}from"./SubcomponentsSection-pzdPLcqE.js";import{C as f,D as m,G as w,M as j}from"./MultiComboBox.stories-x1e5-ITZ.js";import{T as C}from"./index-YOVrtduB.js";import{a as g}from"./index-i6vrUtJq.js";import{T as k}from"./TokenDescription-3SUXzTwH.js";import{e as a}from"./utils-77gZ8405.js";import{u as l}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-efTFHaBa.js";import"./iframe-UlQYZDU8.js";import"../sb-preview/runtime.js";import"./UI5Element-kBA_KQxG.js";import"./i18nBundle-7ihWi2Lo.js";import"./VersionInfo-quO5DfnC.js";import"./CustomElementsScopeUtils-QKoQ990a.js";import"./withWebComponent-22JjwLkL.js";import"./utils-3rf7SAGj.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-r6t1Zphl.js";import"./Icons-jRef6OLh.js";import"./decline-XpWJwTfv.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-U_TSW4LN.js";import"./alert-kkNs8dOr.js";import"./class-map-2qwEQoLI.js";import"./Button-jlOsC8Op.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-QBShB6vc.js";import"./index-9hLZiafD.js";import"./clsx-Zbgk8kpT.js";import"./i18n-defaults-gX8Lho5I.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-YJVSj-ci.js";import"./index-QYh6idnL.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-Ha-6ONKz.js";import"./index-NcMsv4pw.js";import"./Integer-kog98579.js";import"./index-DDxmsKWU.js";import"./Avatar-kcq5ywk4.js";import"./ResizeHandler-3FzJmCyz.js";import"./employee-APVUdoZS.js";import"./index-0gHyQJeu.js";import"./index-FaUI-TP-.js";import"./Link-xIUn2Lee.js";import"./WrappingType-avPrqc94.js";import"./index-ndaVN1fR.js";import"./TableOfContent-5pBxVJbd.js";import"./index-nYEBNQf8.js";import"./Label-yPOoTXIn.js";import"./index-Riw62-zd.js";import"./index-71prD94s.js";import"./Popover-qTobexuh.js";import"./PopupUtils-FgdW7Gkh.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-fa8QSlgN.js";import"./FocusableElements-jbTazhpM.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-duavIDje.js";import"./BrowserScrollbar.css-aT2mYiFz.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-z3VktcOy.js";import"./mapValues-Iyc0g4nJ.js";import"./_baseUniq-PeaQ4i79.js";import"./index-PPLHz8o0.js";import"./index-ru46B3pF.js";import"./ValueState-I_-hGNIb.js";import"./Token-n-3_k5Ms.js";import"./ItemNavigation-1Qtl9Bqn.js";import"./ScrollEnablement-IFtxymRs.js";import"./animate-Pj1TncyK.js";import"./ValueState-zUcANXoY.js";import"./ResponsivePopover-wHlXl3nI.js";import"./Dialog-Y9sM4F0U.js";import"./Title-dLDxQQ5A.js";import"./List-XV25F2MG.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-ic-NtdR-.js";import"./CheckBox-vOrD_Qxh.js";import"./accept-RviPAaVm.js";import"./RadioButton-ssFCKs8g.js";import"./StandardListItem-cf7NJ5rV.js";import"./ListItem-_0EIb7ra.js";import"./HasPopup-9BAMrqbD.js";import"./slim-arrow-right-0oBZC6Yv.js";import"./ResponsivePopoverCommon.css-h8Z4T2yV.js";import"./ValueStateMessage.css-fNiak5rA.js";import"./Suggestions.css-uuIiA8km.js";import"./slim-arrow-down-I_Bw-gbQ.js";import"./GroupHeaderListItem-Ybq9bAuu.js";import"./ToggleButton-wDGnl2Rb.js";import"./ComboBoxItem-gZpfoj0O.js";const M="**Since:** 0.25.0\n**Abstract UI5 Web Component** - Find out more about abstract components [here](https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base-faq--docs#what-are-abstract-ui5-web-components).\n\nThe `MultiComboBoxGroupItem` is type of suggestion item, that can be used to split the `MultiComboBox` suggestions into groups\n",y="**Abstract UI5 Web Component** - Find out more about abstract components [here](https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base-faq--docs#what-are-abstract-ui5-web-components).\n\nThe `MultiComboBoxItem` represents the item for a `MultiComboBox`\n",S=`## Structure
+
+The \`MultiComboBox\` consists of the following elements:
+
+- Tokenizer - a list of tokens with selected options.
+- Input field - displays the selected option/s as token/s. Users can type to filter the list.
+- Drop-down arrow - expands\\\\collapses the option list.
+- Option list - the list of available options.
+
+## Keyboard Handling
+
+The \`MultiComboBox\` provides advanced keyboard handling.
+
+### Picker
+
+If the \`MultiComboBox\` is focused, you can open or close the drop-down by pressing \`F4\`, \`ALT+UP\` or \`ALT+DOWN\` keys. Once the drop-down is opened, you can use the \`UP\` and \`DOWN\` arrow keys to navigate through the available options and select one by pressing the \`Space\` or \`Enter\` keys.
+
+### Tokens
+
+- Left/Right arrow keys - moves the focus selection form the currently focused token to the previous/next one (if available).
+- Delete - deletes the token and focuses the previous token.
+- Backspace - deletes the token and focus the next token.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`MultiComboBox\` exposes the following CSS Shadow Parts:
+
+- token-{index} - Used to style each token(where \`token-0\` corresponds to the first item)
+`,T=[{name:"open",readonly:"true",visibility:"public",type:"boolean",description:"Indicates whether the dropdown is open. True if the dropdown is open, false otherwise.",defaultValue:"false"}];function Dt(i={}){const{wrapper:n}=Object.assign({},l(),i.components);return n?o.jsx(n,Object.assign({},i,{children:o.jsx(s,{})})):s();function s(){const t=Object.assign({h2:"h2",h1:"h1"},l(),i.components);return o.jsxs(o.Fragment,{children:[o.jsx(u,{of:f}),`
+`,o.jsx(h,{since:"0.4.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(p,{of:m}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(c,{of:m}),`
+`,o.jsx(e,{children:S}),`
+`,o.jsx(b,{rows:T}),`
+`,o.jsx(t.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h2,{id:"multicombobox-with-grouping-of-items",children:"MultiComboBox with Grouping of Items"}),`
+`,o.jsx(p,{of:w}),`
+`,o.jsx(e,{children:x}),`
+`,o.jsx(t.h2,{id:"multicomboboxitem",children:"MultiComboBoxItem"}),`
+`,o.jsx(r,{hideHTMLPropsNote:!0,exclude:a,of:g}),`
+`,o.jsx(e,{children:y}),`
+`,o.jsx(t.h2,{id:"multicomboboxgroupitem",children:"MultiComboBoxGroupItem"}),`
+`,o.jsx(r,{hideHTMLPropsNote:!0,exclude:a,of:j}),`
+`,o.jsx(e,{children:M}),`
+`,o.jsx(t.h2,{id:"token",children:"Token"}),`
+`,o.jsx(r,{hideHTMLPropsNote:!0,of:C}),`
+`,o.jsx(e,{children:k}),`
+`,o.jsx(d,{})]})}}export{Dt as default};
---
+++ main/assets/NotificationListGroupItem-Bal17ckH.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{C as s,A as a}from"./DomRefTable.module-i5j1_1vk.js";import{D as c}from"./DocsHeader-5f7fG-zQ.js";import{F as l}from"./Footer--3QYZY2g.js";import"./index-OjgoNOWw.js";import{M as d,C as f,b as r}from"./chunk-HLWAVYOI-0k14eHPc.js";import{S as h}from"./SubcomponentsSection-pzdPLcqE.js";import{a as x}from"./index-0K13uNds.js";import{N as j}from"./NotificationActionDescription-Ggqdz6r0.js";import{C as u,D as n}from"./NotificationListGroupItem.stories-c_mm1oRT.js";import{e as S}from"./utils-77gZ8405.js";import{u as e}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-efTFHaBa.js";import"./iframe-UlQYZDU8.js";import"../sb-preview/runtime.js";import"./UI5Element-kBA_KQxG.js";import"./i18nBundle-7ihWi2Lo.js";import"./VersionInfo-quO5DfnC.js";import"./CustomElementsScopeUtils-QKoQ990a.js";import"./withWebComponent-22JjwLkL.js";import"./utils-3rf7SAGj.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-r6t1Zphl.js";import"./Icons-jRef6OLh.js";import"./decline-XpWJwTfv.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-U_TSW4LN.js";import"./alert-kkNs8dOr.js";import"./class-map-2qwEQoLI.js";import"./Button-jlOsC8Op.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-QBShB6vc.js";import"./index-9hLZiafD.js";import"./clsx-Zbgk8kpT.js";import"./i18n-defaults-gX8Lho5I.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-YJVSj-ci.js";import"./index-QYh6idnL.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-Ha-6ONKz.js";import"./index-NcMsv4pw.js";import"./Integer-kog98579.js";import"./index-DDxmsKWU.js";import"./Avatar-kcq5ywk4.js";import"./ResizeHandler-3FzJmCyz.js";import"./employee-APVUdoZS.js";import"./index-0gHyQJeu.js";import"./index-FaUI-TP-.js";import"./Link-xIUn2Lee.js";import"./WrappingType-avPrqc94.js";import"./index-ndaVN1fR.js";import"./TableOfContent-5pBxVJbd.js";import"./index-nYEBNQf8.js";import"./Label-yPOoTXIn.js";import"./index-Riw62-zd.js";import"./index-71prD94s.js";import"./Popover-qTobexuh.js";import"./PopupUtils-FgdW7Gkh.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-fa8QSlgN.js";import"./FocusableElements-jbTazhpM.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-duavIDje.js";import"./BrowserScrollbar.css-aT2mYiFz.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-z3VktcOy.js";import"./mapValues-Iyc0g4nJ.js";import"./_baseUniq-PeaQ4i79.js";import"./index-PPLHz8o0.js";import"./index-ru46B3pF.js";import"./BusyIndicator-ic-NtdR-.js";import"./List-XV25F2MG.js";import"./ItemNavigation-1Qtl9Bqn.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./CheckBox-vOrD_Qxh.js";import"./ValueState-zUcANXoY.js";import"./accept-RviPAaVm.js";import"./RadioButton-ssFCKs8g.js";import"./overflow-8zwaJ6yo.js";import"./parameters-bundle.css--e9CNYD5.js";import"./i18n-defaults-HK6lzlSV.js";import"./navigation-right-arrow-4HU0Da2E.js";const C=`## Usage
+
+The component can be used in a standard \`List\`.
+
+## CSS Shadow Parts
+
+<ui5-link target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/::part">CSS Shadow Parts</ui5-link> allow developers to style elements inside the Shadow DOM.  
+The \`NotificationListGroupItem\` exposes the following CSS Shadow Parts:
+
+- title-text - Used to style the titleText of the notification list group item
+`;function ro(o={}){const{wrapper:m}=Object.assign({},e(),o.components);return m?t.jsx(m,Object.assign({},o,{children:t.jsx(p,{})})):p();function p(){const i=Object.assign({h2:"h2"},e(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(d,{of:u}),`
+`,t.jsx(c,{since:"0.10.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(i.h2,{id:"example",children:"Example"}),`
+`,t.jsx(f,{of:n}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:n}),`
+`,t.jsx(r,{children:C}),`
+`,`
+`,`
+`,t.jsx(r,{children:h}),`
+`,t.jsx(i.h2,{id:"notificationaction",children:"NotificationAction"}),`
+`,t.jsx(a,{hideHTMLPropsNote:!0,exclude:S,of:x}),`
+`,t.jsx(r,{children:j}),`
+`,t.jsx(l,{})]})}}export{ro as default};
