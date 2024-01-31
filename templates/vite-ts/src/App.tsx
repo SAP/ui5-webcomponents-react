1efdf7a6cc03b7bc7b140c9caa398079f7c40512
Lukas Harbarth <lukas.harbarth@sap.com>
2024-01-31 15:06:47 +0100

    docs: add vite & next templates

---
+++ templates/vite-ts/src/App.tsx
@@ -0,0 +1,21 @@
+import { Avatar, ShellBar } from '@ui5/webcomponents-react';
+import employeeIcon from '@ui5/webcomponents-icons/dist/employee.js';
+function App() {
+  return (
+    <>
+      <ShellBar
+        logo={
+          <img
+            src="https://raw.githubusercontent.com/SAP/ui5-webcomponents-react/main/assets/ui5-logo.svg"
+            alt={'UI5 Web Components for React logo'}
+          />
+        }
+        primaryTitle="UI5 Web Components for React Template"
+        profile={<Avatar icon={employeeIcon} />}
+      />
+      {/* Add your code here */}
+    </>
+  );
+}
+
+export default App;
