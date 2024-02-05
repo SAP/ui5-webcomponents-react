1032c1b18bba1e875c17d992b5c504f9c10e35bd
Lukas742 <lukas.harbarth@sap.com>
2024-02-05 09:09:33 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c148f42a841c054b0c3d868a3f129692db61d1c1 🚀

---
+++ main/assets/DonutChart-sqwvt6ij.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{M as n,C as o}from"./chunk-HLWAVYOI-9MSxkR43.js";import{C as a}from"./DomRefTable.module-Fdj_Xub3.js";import{D as h}from"./DocsHeader-udIb4GDP.js";import{F as d}from"./Footer-3fEP2g9r.js";import"./index-OjgoNOWw.js";import{C as l,D as e,W as x,a as c,b as j,L as f,c as g,d as u,H as C}from"./DonutChart.stories-DxFt4UnS.js";import{u as s}from"./index-RwE9nQVW.js";import"./iframe-yQuqTiPK.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./_baseForOwn-z3VktcOy.js";import"./mapValues-Iyc0g4nJ.js";import"./_baseUniq-PeaQ4i79.js";import"./index-PPLHz8o0.js";import"./index-ru46B3pF.js";import"./react-jss.esm-tmBtHfec.js";import"./index-_cz7--qu.js";import"./UI5Element-kBA_KQxG.js";import"./i18nBundle-7ihWi2Lo.js";import"./VersionInfo-quO5DfnC.js";import"./CustomElementsScopeUtils-QKoQ990a.js";import"./withWebComponent-22JjwLkL.js";import"./utils-3rf7SAGj.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./Icon-r6t1Zphl.js";import"./Icons-jRef6OLh.js";import"./decline-XpWJwTfv.js";import"./i18n-defaults-CPLPRuO1.js";import"./information-U_TSW4LN.js";import"./alert-kkNs8dOr.js";import"./class-map-2qwEQoLI.js";import"./Button-jlOsC8Op.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./i18n-defaults-QBShB6vc.js";import"./index-9hLZiafD.js";import"./clsx-Zbgk8kpT.js";import"./i18n-defaults-gX8Lho5I.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-YJVSj-ci.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-loCRUewX.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-Ha-6ONKz.js";import"./index-CXCszcko.js";import"./Integer-kog98579.js";import"./index-PpcFv82q.js";import"./Avatar-kcq5ywk4.js";import"./ResizeHandler-3FzJmCyz.js";import"./employee-APVUdoZS.js";import"./index-3cbqvEIv.js";import"./index-ctmUXfL-.js";import"./Link-xIUn2Lee.js";import"./WrappingType-avPrqc94.js";import"./index-ow_R__0g.js";import"./TableOfContent-u_pvi27Y.js";import"./index-Y10o1Y_w.js";import"./Label-yPOoTXIn.js";import"./index-uC5wBc3d.js";import"./index-oEIC5zqG.js";import"./Popover-qTobexuh.js";import"./PopupUtils-FgdW7Gkh.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-fa8QSlgN.js";import"./FocusableElements-jbTazhpM.js";import"./isElementHidden-d3-Fo4V6.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-duavIDje.js";import"./BrowserScrollbar.css-aT2mYiFz.js";import"./DemoProps-gxklhiCN.js";import"./PieChart-h0BAQe1j.js";import"./index-ENBflQbU.js";import"./defaults-4bnHm4Do.js";import"./ChartContainer-tEXKFlKF.js";import"./index-Ydt675Et.js";import"./throttle-TVG53lN7.js";import"./isString-jZL_QQlC.js";import"./tiny-invariant-csaaSLPr.js";import"./staticProps-5PxpsJ6y.js";import"./useOnClickInternal-0NwCcJNk.js";import"./Placeholder-XQf10n1n.js";import"./generateCategoricalChart-axaHwnGE.js";import"./isPlainObject-ZewI_Ya6.js";import"./react-content-loader.es-MhslfaKA.js";function ii(r={}){const{wrapper:m}=Object.assign({},s(),r.components);return m?t.jsx(m,Object.assign({},r,{children:t.jsx(p,{})})):p();function p(){const i=Object.assign({h2:"h2",h3:"h3"},s(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:l}),`
+`,t.jsx(h,{}),`
+`,t.jsx(i.h2,{id:"example",children:"Example"}),`
+`,t.jsx(o,{of:e}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{of:e}),`
+`,t.jsx("br",{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(i.h2,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(i.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,t.jsx(o,{of:x}),`
+`,t.jsx(i.h3,{id:"with-padding-angle",children:"With Padding Angle"}),`
+`,t.jsx(o,{of:c}),`
+`,t.jsx(i.h3,{id:"with-custom-radius",children:"With Custom Radius"}),`
+`,t.jsx(o,{of:j}),`
+`,t.jsx(i.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,t.jsx(o,{of:f}),`
+`,t.jsx(i.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,t.jsx(o,{of:g}),`
+`,t.jsx(i.h3,{id:"with-highlighted-active-segment",children:"With highlighted active segment"}),`
+`,t.jsx(o,{of:u}),`
+`,t.jsx(i.h3,{id:"hide-labels",children:"Hide labels"}),`
+`,t.jsx(o,{of:C}),`
+`,t.jsx(d,{})]})}}export{ii as default};
---
+++ main/assets/NotificationListItem.stories-IQgru8MX.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{e as c}from"./employee-APVUdoZS.js";import{A as r}from"./index-PpcFv82q.js";import{L as t}from"./index-Y10o1Y_w.js";import{L as f}from"./index-oVj0KytQ.js";import{N as s,P as v,a as n}from"./index-p17MYdP6.js";import{A as a,a as b}from"./Avatar-kcq5ywk4.js";import{B as l}from"./Button-jlOsC8Op.js";import{W as L}from"./WrappingType-avPrqc94.js";const A={title:"Data Display / NotificationListItem",component:s,argTypes:{avatar:{control:{disable:!0}},children:{control:"text"},footnotes:{control:{disable:!0}},actions:{control:{disable:!0}}},args:{titleText:"New order (#2525) With a very long title - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc.",priority:v.None,children:"And with a very long description and long labels of the action buttons - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc.",style:{width:"600px"}}},i={args:{avatar:e.jsx(r,{size:a.XS,children:e.jsx("img",{src:"https://sap.github.io/ui5-webcomponents/assets/images/avatars/woman_avatar_1.png"})}),footnotes:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"Monique Legrand"}),e.jsx(t,{children:"2 Days"})]})},render:x=>e.jsx(f,{children:e.jsx(s,{...x})})},o={render:()=>e.jsxs(f,{children:[e.jsx(s,{showClose:!0,avatar:e.jsx(r,{size:a.XS,icon:c}),footnotes:e.jsx(t,{children:"3 Days"}),titleText:"New order (#2526) with multiple NotificationAction buttons and close button",actions:e.jsxs(e.Fragment,{children:[e.jsx(n,{design:l.Transparent,text:"Accept All Requested Information"}),e.jsx(n,{design:l.Transparent,text:"Reject All Requested Information"})]}),children:"Short description"}),e.jsx(s,{avatar:e.jsx(r,{size:a.XS,icon:c,colorScheme:b.Accent1}),footnotes:e.jsx(t,{children:"3 Days"}),titleText:"New order (#2527) with single NotificationAction and close button",showClose:!0,actions:e.jsx(n,{design:l.Transparent,text:"Accept All Requested Information"}),children:"Short description"}),e.jsxs(s,{wrappingType:L.Normal,titleText:"New high priority order (#2528) without close button",priority:v.High,avatar:e.jsx(r,{size:a.XS,children:e.jsx("img",{src:"https://sap.github.io/ui5-webcomponents/assets/images/avatars/woman_avatar_1.png"})}),footnotes:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"John Smith"}),e.jsx(t,{children:"1 hour"})]}),children:["And with a very long description that will not be ellipsed:",e.jsx("br",{}),"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc. ",e.jsx("br",{}),"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum lectus turpis at nunc."]})]})};var u,p,m;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    avatar: <Avatar size={AvatarSize.XS}>
+        <img src="https://sap.github.io/ui5-webcomponents/assets/images/avatars/woman_avatar_1.png" />
+      </Avatar>,
+    footnotes: <>
+        <Label>Monique Legrand</Label>
+        <Label>2 Days</Label>
+      </>
+  },
+  render: args => {
+    return <List>
+        <NotificationListItem {...args} />
+      </List>;
+  }
+}`,...(m=(p=i.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var d,h,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => {
+    return <List>
+        <NotificationListItem showClose avatar={<Avatar size={AvatarSize.XS} icon={employeeIcon} />} footnotes={<Label>3 Days</Label>} titleText="New order (#2526) with multiple NotificationAction buttons and close button" actions={<>
+              <NotificationAction design={ButtonDesign.Transparent} text="Accept All Requested Information" />
+              <NotificationAction design={ButtonDesign.Transparent} text="Reject All Requested Information" />
+            </>}>
+          Short description
+        </NotificationListItem>
+        <NotificationListItem avatar={<Avatar size={AvatarSize.XS} icon={employeeIcon} colorScheme={AvatarColorScheme.Accent1} />} footnotes={<Label>3 Days</Label>} titleText="New order (#2527) with single NotificationAction and close button" showClose actions={<NotificationAction design={ButtonDesign.Transparent} text="Accept All Requested Information" />}>
+          Short description
+        </NotificationListItem>
+        <NotificationListItem wrappingType={WrappingType.Normal} titleText="New high priority order (#2528) without close button" priority={Priority.High} avatar={<Avatar size={AvatarSize.XS}>
+              <img src="https://sap.github.io/ui5-webcomponents/assets/images/avatars/woman_avatar_1.png" />
+            </Avatar>} footnotes={<>
+              <Label>John Smith</Label>
+              <Label>1 hour</Label>
+            </>}>
+          And with a very long description that will not be ellipsed:
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra,
+          tellus odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum
+          lectus turpis at nunc. <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra,
+          tellus odio vehicula dolor, nec elementum lectus turpis at nunc. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Praesent feugiat, turpis vel scelerisque pharetra, tellus odio vehicula dolor, nec elementum
+          lectus turpis at nunc.
+        </NotificationListItem>
+      </List>;
+  }
+}`,...(g=(h=o.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const w=["Default","MultipleNotifications"],P=Object.freeze(Object.defineProperty({__proto__:null,Default:i,MultipleNotifications:o,__namedExportsOrder:w,default:A},Symbol.toStringTag,{value:"Module"}));export{P as C,i as D,o as M};
