1124fa1fd78dd926e9e81c84a06eb709d277cacc
Marcus Notheis <marcus.notheis@sap.com>
2024-02-06 11:29:04 +0100

    DateRangePicker

---
+++ packages/main/src/webComponents/DateRangePicker/DateRangePicker.stories.tsx
@@ -0,0 +1,27 @@
+import '@ui5/webcomponents-localization/dist/features/calendar/Gregorian.js';
+import '@ui5/webcomponents-localization/dist/features/calendar/Buddhist.js';
+import '@ui5/webcomponents-localization/dist/features/calendar/Islamic.js';
+import '@ui5/webcomponents-localization/dist/features/calendar/Japanese.js';
+import '@ui5/webcomponents-localization/dist/features/calendar/Persian.js';
+import type { Meta, StoryObj } from '@storybook/react';
+import { CalendarType } from '../../enums/CalendarType';
+import { ValueState } from '../../enums/ValueState';
+import { DateRangePicker } from './index.js';
+
+const meta = {
+  title: 'Inputs / DateRangePicker',
+  component: DateRangePicker,
+  argTypes: {
+    valueStateMessage: { control: { disable: true } }
+  },
+  args: {
+    primaryCalendarType: CalendarType.Gregorian,
+    valueState: ValueState.None
+  },
+  tags: ['package:@ui5/webcomponents']
+} satisfies Meta<typeof DateRangePicker>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
