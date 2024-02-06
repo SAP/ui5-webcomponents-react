92006490340e4a5a84efa2eb4b718c0dbf0608df
Marcus Notheis <marcus.notheis@sap.com>
2024-02-06 11:48:14 +0100

    allow resolving custom dom refs

--- .storybook/components/DomRefTable.tsx
+++ .storybook/components/DomRefTable.tsx
@@ -27,72 +27,88 @@ export function DomRefTable() {
   const docsContext = useContext(DocsContext);
   const storyTags: string[] = docsContext.attachedCSFFile?.meta?.tags;
   const packageAnnotation = storyTags?.find((tag) => tag.startsWith('package:'));
+  const cemModuleName = storyTags?.find((tag) => tag.startsWith('cem-module:'));
   const componentName = docsContext.componentStories().at(0).component.displayName;
 
-  const cem = packageAnnotation === 'package:@ui5/webcomponents' ? cemMain : cemFiori;
-  const componentMembers = cem.modules
-    .find((m) => m.path === `dist/${componentName}.js`)
-    ?.declarations.find((d) => d.customElement === true && d.name === componentName);
+  let cem: CEM.CustomElementManifest;
+  switch (packageAnnotation) {
+    case 'package:@ui5/webcomponents':
+      cem = cemMain;
+      break;
+    case 'package:@ui5/webcomponents-fiori':
+      cem = cemFiori;
+      break;
+  }
 
-  console.log('-> componentMembers', componentMembers);
+  let moduleName = cemModuleName ? cemModuleName.split(':')[1] : componentName;
+
+  console.log('-> moduleName', moduleName);
+
+  const componentMembers = cem?.modules
+    .find((m) => m.path === `dist/${moduleName}.js`)
+    ?.declarations.find((d) => d.customElement === true && d.name === moduleName);
 
   const rows: CEM.ClassMember[] = componentMembers?.members ?? [];
   const cssParts: CEM.CssPart[] = componentMembers?.cssParts ?? [];
 
   return (
     <>
-      <Heading>Attributes & Methods</Heading>
-      <p>
-        This component exposes public attributes and methods. You can use them directly on the instance of the
-        component, e.g. by using React Refs.
-      </p>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Parameters</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row) => {
-            return (
-              <tr key={row.name}>
-                <td>
-                  <b>
-                    <Name {...row} />
-                  </b>
-                </td>
-                <td>
-                  {!!row.parameters ? (
-                    row.parameters.map((parameter) => {
-                      return (
-                        <div key={parameter.name} className={classes.parameters}>
-                          <p className={classes.parameterName}>{parameter.name}</p>
-                          {parameter.description ? (
-                            <p
-                              className={classes.parameterDetails}
-                              dangerouslySetInnerHTML={{ __html: parameter.description }}
-                            />
-                          ) : null}
-                          {parameter.type ? (
-                            <p className={classes.parameterDetails}>
-                              <code>{parameter.type.text}</code>
-                            </p>
-                          ) : null}
-                        </div>
-                      );
-                    })
-                  ) : (
-                    <>&mdash;</>
-                  )}
-                </td>
-                <td dangerouslySetInnerHTML={{ __html: row.description }}></td>
+      {rows.length > 0 ? (
+        <>
+          <Heading>Attributes & Methods</Heading>
+          <p>
+            This component exposes public attributes and methods. You can use them directly on the instance of the
+            component, e.g. by using React Refs.
+          </p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Parameters</th>
+                <th>Description</th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {rows.map((row) => {
+                return (
+                  <tr key={row.name}>
+                    <td>
+                      <b>
+                        <Name {...row} />
+                      </b>
+                    </td>
+                    <td>
+                      {!!row.parameters ? (
+                        row.parameters.map((parameter) => {
+                          return (
+                            <div key={parameter.name} className={classes.parameters}>
+                              <p className={classes.parameterName}>{parameter.name}</p>
+                              {parameter.description ? (
+                                <p
+                                  className={classes.parameterDetails}
+                                  dangerouslySetInnerHTML={{ __html: parameter.description }}
+                                />
+                              ) : null}
+                              {parameter.type ? (
+                                <p className={classes.parameterDetails}>
+                                  <code>{parameter.type.text}</code>
+                                </p>
+                              ) : null}
+                            </div>
+                          );
+                        })
+                      ) : (
+                        <>&mdash;</>
+                      )}
+                    </td>
+                    <td dangerouslySetInnerHTML={{ __html: row.description }}></td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      ) : null}
 
       {cssParts.length > 0 ? (
         <>
