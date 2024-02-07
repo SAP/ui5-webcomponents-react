02ad643ae88016db1c08bb646e991062abacc6be
Marcus Notheis <marcus.notheis@sap.com>
2024-02-07 15:29:18 +0100

    MessageStrip

---
+++ packages/main/src/webComponents/MessageStrip/MessageStrip.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import employeeIcon from '@ui5/webcomponents-icons/dist/employee.js';
+import { MessageStripDesign } from '../../enums/MessageStripDesign';
+import { Icon } from '../Icon/index.js';
+import { MessageStrip } from './index.js';
+
+const meta = {
+  title: 'Data Display / MessageStrip',
+  component: MessageStrip,
+  argTypes: {
+    children: { control: 'text' },
+    icon: { control: { disable: true } }
+  },
+  args: {
+    design: MessageStripDesign.Information,
+    children: 'MessageStrip Text'
+  }
+} satisfies Meta<typeof MessageStrip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const WithIcon: Story = {
+  args: {
+    icon: <Icon name={employeeIcon} />
+  }
+};
