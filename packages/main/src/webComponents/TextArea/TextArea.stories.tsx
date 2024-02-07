b7be9fe84a4708641a3587e3744dfbe03f8527d9
Marcus Notheis <marcus.notheis@sap.com>
2024-02-07 14:51:38 +0100

    TextArea

---
+++ packages/main/src/webComponents/TextArea/TextArea.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ValueState } from '../../enums/index.js';
+import { TextArea } from './index.js';
+
+const meta = {
+  title: 'Inputs / TextArea',
+  component: TextArea,
+  argTypes: {
+    valueStateMessage: { control: { disable: true } }
+  },
+  args: {
+    valueState: ValueState.None
+  },
+  tags: ['package:@ui5/webcomponents']
+} satisfies Meta<typeof TextArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
