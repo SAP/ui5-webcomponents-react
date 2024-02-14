fdf55ee5d729d1e5c3b0324865ca2db971f079e2
MarcusNotheis <marcus.notheis@sap.com>
2024-02-14 18:14:15 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c9197ca4d751bfa533bec50b9951965cb26e0fcd 🚀

---
+++ main/assets/Menu-zCj2E8RJ.js
@@ -0,0 +1,83 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{A as c,F as a}from"./ProjectTemplate-wDJDq-6m.js";import{C as d}from"./ControlsWithNote-IJH2vfU5.js";import{D as h}from"./DocsHeader-O--qGlRN.js";import"./index-OjgoNOWw.js";import{M as l,b as i,g as u,e as x}from"./index-bSzUnYHZ.js";import{C as j,D as p,W as f}from"./Menu.stories-AoDmvomK.js";import{M as g}from"./index-usxe9APU.js";import{a as s}from"./index-no1x1yzE.js";import{S as M}from"./SubcomponentsSection-pzdPLcqE.js";import{e as b}from"./utils-Ht-CFy0t.js";import{u as m}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./chunk-HLWAVYOI-zOsni49Q.js";import"./iframe-GVjVMRfF.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./index-D-00O0DK.js";import"./clsx-Zbgk8kpT.js";import"./index-6vImOm68.js";import"./Label-Q5XT_dC3.js";import"./UI5Element-oLeGfnU4.js";import"./Language-aesXCmkP.js";import"./VersionInfo-AzzP9wS2.js";import"./CustomElementsScopeUtils-IpwVqUeG.js";import"./withWebComponent--kEkWNJH.js";import"./utils-rNFQc6Ui.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./i18nBundle-YvxCnun_.js";import"./WrappingType-avPrqc94.js";import"./i18n-defaults-HIa-lgkZ.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./index-_Ouv0TUj.js";import"./Button-MUpci_CC.js";import"./event-lM5HanI-.js";import"./slot-HSrR9XJ-.js";import"./Keys-ZEuNsqmZ.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./Icons-B0Lm9Xgp.js";import"./willShowContent-MSvI8sYo.js";import"./Icon-s-QlKO5-.js";import"./HasPopup-9BAMrqbD.js";import"./index-PA7J6JWY.js";import"./Link-15HXWd6H.js";import"./index-fRqWzYEb.js";import"./Popover-RS8IZQFL.js";import"./Integer-kog98579.js";import"./PopupUtils-7MJMGT2m.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-umKiUwKH.js";import"./FocusableElements-o5zpB6FE.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-pk5_zz-J.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5njKSr-C.js";import"./class-map-Y-YjCFqX.js";import"./BrowserScrollbar.css-nSPAj6kk.js";import"./index-YN01GDwL.js";import"./i18n-defaults-1NWI3C8K.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-z21dC6BM.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-HFpqBBhk.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-0Qf_GZqW.js";import"./index-ZCaw7ujr.js";import"./index-P7atT9R-.js";import"./Avatar-CYVVssoh.js";import"./employee-PrBJTXfy.js";import"./alert-xBUzpOmY.js";import"./index-Ca8_VPaX.js";import"./index-sYrYI8p0.js";import"./sys-help-2-KdHoR5uo.js";import"./information-HjlDBfcs.js";import"./i18n-defaults-iZnAAWr4.js";import"./copy-rq9qK5qL.js";import"./GitHub-Mark-JYujJlUX.js";import"./TableOfContent-Yu3qvuOL.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./decline-pTlhPVol.js";import"./MenuItem-CwlHd9GE.js";import"./slim-arrow-right-rX9hOP5a.js";import"./ResponsivePopover-Iu59tfC3.js";import"./Dialog-GLNfYF-f.js";import"./ValueState-zUcANXoY.js";import"./Title-9LEfLuR0.js";import"./List-dEKXwq8P.js";import"./ItemNavigation-N1_IBWr4.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-ilxF3CHS.js";import"./CheckBox-Fj0rtVJO.js";import"./accept-8p0TeSaU.js";import"./RadioButton-h9kUnzLj.js";import"./StandardListItem-hcFFyleH.js";import"./ListItem-qoQB8hA4.js";function Mn(o={}){const{wrapper:t}=Object.assign({},m(),o.components);return t?e.jsx(t,Object.assign({},o,{children:e.jsx(r,{})})):r();function r(){const n=Object.assign({h2:"h2",h4:"h4",p:"p",code:"code",pre:"pre",strong:"strong",em:"em",h3:"h3"},m(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(l,{of:j}),`
+`,e.jsx(h,{since:"0.23.0"}),`
+`,e.jsx(n.h2,{id:"example",children:"Example"}),`
+`,e.jsx(i,{of:p}),`
+`,e.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(d,{of:p}),`
+`,e.jsx(n.h2,{id:"opening-menus",children:"Opening Menus"}),`
+`,e.jsx(n.h4,{id:"since-180",children:"since 1.8.0"}),`
+`,e.jsxs(n.p,{children:["We recommend opening and closing the ",e.jsx(n.code,{children:"Menu"})," component in a declarative way using the ",e.jsx(n.code,{children:"open"})," and ",e.jsx(n.code,{children:"opener"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const MyComponentWithMenu = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        id={'openMenuBtn'}
+        onClick={() => {
+          setMenuIsOpen(true);
+        }}
+      >
+        Open Action Sheet
+      </Button>
+      <Menu
+        opener={'openMenuBtn'}
+        open={menuIsOpen}
+        onAfterClose={() => {
+          setMenuIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.p,{children:e.jsxs(n.strong,{children:["Opening a ",e.jsx(n.code,{children:"Menu"})," by reference and not by ",e.jsx(n.code,{children:"id"})]})}),`
+`,e.jsxs(n.p,{children:["This web component exposes a way to pass a reference of an element instead of an ",e.jsx(n.code,{children:"id"})," to the ",e.jsx(n.code,{children:"opener"})," prop. Since this is not supported when passing the reference in a declarative way to a React ",e.jsx(n.code,{children:"prop"}),`, you have to attach the ref directly on the web component.
+You can do that by e.g. leveraging a React Ref, and then set the corresponding property there.`]}),`
+`,e.jsx(g,{hideCloseButton:!0,children:`Changing properties outside of the React rendering cycle can lead to unwanted behavior, please be extra cautious when
+using this approach!`}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const MenuComponent = () => {
+  const popoverRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const handleOpenerClick = (e) => {
+    if (popoverRef.current) {
+      popoverRef.current.opener = e.target;
+      setOpen((prev) => !prev);
+    }
+  };
+  return (
+    <>
+      <Button onClick={handleOpenerClick}>Opener</Button>
+      <Menu ref={popoverRef} open={open}>
+        {/* Content */}
+      </Menu>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.h4,{id:"before-180",children:"before 1.8.0"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"Menu"}),"s can be opened by attaching a ",e.jsx(n.code,{children:"ref"})," to the component and then call the corresponding ",e.jsx(n.strong,{children:e.jsx(n.code,{children:"showAt"})})," method. The method receives the target element - ",e.jsxs(n.em,{children:["on which the ",e.jsx(n.code,{children:"Menu"})," is to be opened"]})," - as parameter."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`const MenuComponent = () => {
+  const popoverRef = useRef();
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Menu</Button>
+      <Menu ref={popoverRef}>
+        <MenuItem text="New File" icon="add-document" />
+        <MenuItem text="New Folder" icon="add-folder" disabled />
+      </Menu>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.h2,{id:"more-examples",children:"More examples"}),`
+`,e.jsx(n.h3,{id:"menu-with-submenu",children:"Menu with Submenu"}),`
+`,e.jsx(i,{of:f}),`
+`,e.jsx(u,{children:M}),`
+`,e.jsx(n.h2,{id:"menuitem",children:"MenuItem"}),`
+`,e.jsx(x,{of:s}),`
+`,e.jsx(c,{hideHTMLPropsNote:!0,exclude:b,of:s}),`
+`,e.jsx(a,{})]})}}export{Mn as default};
