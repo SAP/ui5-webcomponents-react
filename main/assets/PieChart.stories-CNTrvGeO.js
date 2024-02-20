07d74cb2ace65667898f161abe399791401689de
Lukas742 <lukas.harbarth@sap.com>
2024-02-20 07:30:55 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@b076205bd2cc250705ade9cd2fdff5f2e939bcb7 🚀

---
+++ main/assets/PieChart.stories-CNTrvGeO.js
@@ -0,0 +1,35 @@
+import{s as L,d as b}from"./DemoProps-gxklhiCN.js";import{P as W}from"./PieChart-6iiuwpsm.js";import"./jsx-runtime-DtaoT6pD.js";import"./index-OjgoNOWw.js";import"./index-ENBflQbU.js";import"./clsx-Zbgk8kpT.js";import"./react-jss.esm-tmBtHfec.js";import"./defaults-nNYkFjf4.js";import"./ChartContainer-lnuTKA0K.js";import"./_baseForOwn-a7AeJH13.js";import"./_baseUniq-_JZHYp0e.js";import"./index-4Xp3nZk1.js";import"./i18n-defaults-1NWI3C8K.js";import"./i18nBundle-YvxCnun_.js";import"./Language-aesXCmkP.js";import"./VersionInfo-AzzP9wS2.js";import"./I18nContext-VUOpdMoH.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./ThemingParameters-UghqSl-x.js";import"./throttle-SW6qI-oX.js";import"./isString-F2o_0UD_.js";import"./index-zW6q8pE7.js";import"./iframe-ABu_JyX2.js";import"../sb-preview/runtime.js";import"./Label-z6oc8zzv.js";import"./UI5Element-oLeGfnU4.js";import"./CustomElementsScopeUtils-IpwVqUeG.js";import"./withWebComponent--kEkWNJH.js";import"./utils-rNFQc6Ui.js";import"./WrappingType-avPrqc94.js";import"./i18n-defaults-CQMo-g9a.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./tiny-invariant-csaaSLPr.js";import"./staticProps-5PxpsJ6y.js";import"./useOnClickInternal-0NwCcJNk.js";import"./Placeholder-xjWqcq5f.js";import"./generateCategoricalChart-8EFcATv_.js";import"./isPlainObject-F1bmvOby.js";import"./mapValues-_kP3xGjM.js";import"./react-content-loader.es-MhslfaKA.js";const nr={title:"PieChart",component:W,args:{dimension:{accessor:"name"},measure:{accessor:"users"},dataset:L},argTypes:{dataset:{control:{disable:!0}}}},r={},e={args:{measure:{accessor:"users",colors:["#f00","green","var(--sapNegativeColor)"]}}},t={args:{dataset:[]}},a={args:{measure:{accessor:"users",formatter:s=>s>200?"over 200":"lower"},chartConfig:{activeSegment:1,showActiveSegmentDataLabel:!0}}},o={args:{measure:{accessor:"users",hideDataLabel:s=>{if(s.percent<.01)return!0}},dataset:b}};var m,i,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var c,n,u;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      colors: ['#f00', 'green', 'var(--sapNegativeColor)']
+    }
+  }
+}`,...(u=(n=e.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};var l,d,g;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    dataset: []
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var h,f,S;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      formatter: d => d > 200 ? 'over 200' : 'lower'
+    },
+    chartConfig: {
+      activeSegment: 1,
+      showActiveSegmentDataLabel: true
+    }
+  }
+}`,...(S=(f=a.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var C,v,D;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      hideDataLabel: chartConfig => {
+        if (chartConfig.percent < 0.01) {
+          return true;
+        }
+      }
+    },
+    dataset: simpleDataSetWithSmallValues
+  }
+}`,...(D=(v=o.parameters)==null?void 0:v.docs)==null?void 0:D.source}}};const ur=["Default","WithCustomColor","LoadingPlaceholder","WithFormatter","HideLabels"];export{r as Default,o as HideLabels,t as LoadingPlaceholder,e as WithCustomColor,a as WithFormatter,ur as __namedExportsOrder,nr as default};
---
+++ main/assets/Toolbar-4Aq8OQBx.js
@@ -0,0 +1,24 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{A as t,F as f}from"./ProjectTemplate-v1ZWrepV.js";import{C as b}from"./ControlsWithNote-wh__AxPi.js";import{D as d}from"./DocsHeader-HbSUfGB_.js";import"./index-OjgoNOWw.js";import{M as h,b as u,g as S,e as i}from"./index-GsWhnFbz.js";import{S as T}from"./SubcomponentsSection-pzdPLcqE.js";import{C,D as s,T as e,a,b as c,c as l,d as x}from"./Toolbar.stories-G0Gv6-Ml.js";import{u as j}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-3xtxuRzi.js";import"./iframe-ABu_JyX2.js";import"../sb-preview/runtime.js";import"./UI5Element-oLeGfnU4.js";import"./Language-aesXCmkP.js";import"./VersionInfo-AzzP9wS2.js";import"./CustomElementsScopeUtils-IpwVqUeG.js";import"./withWebComponent--kEkWNJH.js";import"./utils-rNFQc6Ui.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-YvxCnun_.js";import"./decline-KM5PgfOz.js";import"./Icons-B0Lm9Xgp.js";import"./i18n-defaults-SIMqfZL6.js";import"./information-phWr88nL.js";import"./alert-xBUzpOmY.js";import"./class-map-Y-YjCFqX.js";import"./Icon-s-QlKO5-.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./Button-ui6PL7pJ.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-CQMo-g9a.js";import"./chunk-HLWAVYOI-yBvGQsXM.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./index-iYv8HJTi.js";import"./clsx-Zbgk8kpT.js";import"./index-zW6q8pE7.js";import"./Label-z6oc8zzv.js";import"./WrappingType-avPrqc94.js";import"./index-3hGqQ4Kr.js";import"./index-l7tBPmVO.js";import"./Link-lWkth14S.js";import"./index-4J8eXNvo.js";import"./Popover-RS8IZQFL.js";import"./Integer-kog98579.js";import"./PopupUtils-7MJMGT2m.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-umKiUwKH.js";import"./FocusableElements-o5zpB6FE.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-pk5_zz-J.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5njKSr-C.js";import"./BrowserScrollbar.css-nSPAj6kk.js";import"./index-YN01GDwL.js";import"./i18n-defaults-1NWI3C8K.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-z21dC6BM.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-HFpqBBhk.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-0Qf_GZqW.js";import"./index-S6xZvc8Q.js";import"./index-H_AyESaY.js";import"./Avatar-sqKRaJp5.js";import"./employee-PrBJTXfy.js";import"./index-i5VeJrm3.js";import"./utils-lC8W8Ku1.js";import"./index-PXFPwSxd.js";import"./sys-help-2-KdHoR5uo.js";import"./copy-rq9qK5qL.js";import"./GitHub-Mark-JYujJlUX.js";import"./TableOfContent-JPfRz_zh.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./ValueState-zUcANXoY.js";import"./Select-XFj6VNOY.js";import"./InvisibleMessage-bPJU3ltv.js";import"./slim-arrow-down-5r4ZYImA.js";import"./List-Ufq1X9dH.js";import"./ItemNavigation-N1_IBWr4.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-85VEhaCU.js";import"./CheckBox--si6UWMa.js";import"./accept-8p0TeSaU.js";import"./RadioButton-j200UsKu.js";import"./ResponsivePopover-RZ1IBFAr.js";import"./Dialog-DwTNuVSu.js";import"./Title-9LEfLuR0.js";import"./StandardListItem-b0X249jU.js";import"./ListItem-TjhLOPd0.js";import"./slim-arrow-right-rX9hOP5a.js";import"./ResponsivePopoverCommon.css-Ss_OKayz.js";import"./ValueStateMessage.css-eVfym5uW.js";import"./overflow-oufvxX-A.js";function gr(p={}){const{wrapper:m}=Object.assign({},j(),p.components);return m?o.jsx(m,Object.assign({},p,{children:o.jsx(n,{})})):n();function n(){const r=Object.assign({h2:"h2"},j(),p.components);return o.jsxs(o.Fragment,{children:[o.jsx(h,{of:C}),`
+`,o.jsx(d,{since:"1.19.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(u,{of:s}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(b,{of:s}),`
+`,o.jsx(S,{children:T}),`
+`,o.jsx(r.h2,{id:"toolbarbutton",children:"ToolbarButton"}),`
+`,o.jsx(i,{of:e}),`
+`,o.jsx(t,{of:e}),`
+`,o.jsx(r.h2,{id:"toolbarselect",children:"ToolbarSelect"}),`
+`,o.jsx(i,{of:a}),`
+`,o.jsx(t,{of:a}),`
+`,o.jsx(r.h2,{id:"toolbarselectoption",children:"ToolbarSelectOption"}),`
+`,o.jsx(i,{of:c}),`
+`,o.jsx(t,{of:c}),`
+`,o.jsx(r.h2,{id:"toolbarseparatorv2",children:"ToolbarSeparatorV2"}),`
+`,o.jsx(i,{of:l}),`
+`,o.jsx(t,{of:l}),`
+`,o.jsx(r.h2,{id:"toolbarspacerv2",children:"ToolbarSpacerV2"}),`
+`,o.jsx(i,{of:x}),`
+`,o.jsx(t,{of:x}),`
+`,o.jsx(f,{})]})}}export{gr as default};
