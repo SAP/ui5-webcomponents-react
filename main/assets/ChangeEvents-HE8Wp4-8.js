6efb5bc6fd3d201b9659dbcc57c5816988e079a2
MarcusNotheis <marcus.notheis@sap.com>
2024-02-21 06:20:36 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@bb65f33107ea6aa7dc456d9d9c383d692e2b6d10 🚀

---
+++ main/assets/ChangeEvents-HE8Wp4-8.js
@@ -0,0 +1,79 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import"./chunk-HLWAVYOI-VLgotpgB.js";import{M as r}from"./index-P2eCl2xY.js";import{F as s}from"./ProjectTemplate-wJgidHxg.js";import"./index-OjgoNOWw.js";import"./index-QYh6idnL.js";import"./copy-rq9qK5qL.js";import{u as a}from"./index-RwE9nQVW.js";import"./iframe-1OxCliDu.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./react-jss.esm-tmBtHfec.js";import"./index-PsHndSuA.js";import"./UI5Element-oLeGfnU4.js";import"./Language-aesXCmkP.js";import"./VersionInfo-AzzP9wS2.js";import"./CustomElementsScopeUtils-IpwVqUeG.js";import"./withWebComponent--kEkWNJH.js";import"./utils-rNFQc6Ui.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-YvxCnun_.js";import"./decline-KM5PgfOz.js";import"./Icons-B0Lm9Xgp.js";import"./i18n-defaults-SIMqfZL6.js";import"./information-phWr88nL.js";import"./alert-xBUzpOmY.js";import"./class-map-Y-YjCFqX.js";import"./Icon-s-QlKO5-.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./Button-ui6PL7pJ.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-CQMo-g9a.js";import"./index-6OPsdmYb.js";import"./clsx-Zbgk8kpT.js";import"./index-aYIxj2v1.js";import"./Label-z6oc8zzv.js";import"./WrappingType-avPrqc94.js";import"./index-TTLG1FH3.js";import"./index-6iAwDACH.js";import"./Link-lWkth14S.js";import"./index-Q2ls3PqU.js";import"./Popover-RS8IZQFL.js";import"./Integer-kog98579.js";import"./PopupUtils-7MJMGT2m.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-umKiUwKH.js";import"./FocusableElements-o5zpB6FE.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-pk5_zz-J.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5njKSr-C.js";import"./BrowserScrollbar.css-nSPAj6kk.js";import"./index-YN01GDwL.js";import"./i18n-defaults-1NWI3C8K.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-z21dC6BM.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-HFpqBBhk.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-0Qf_GZqW.js";import"./index-jbs7kkS-.js";import"./index-wmvsajsx.js";import"./Avatar-sqKRaJp5.js";import"./employee-PrBJTXfy.js";import"./index-5uEzVf5i.js";function Et(n={}){const{wrapper:o}=Object.assign({},a(),n.components);return o?t.jsx(o,Object.assign({},n,{children:t.jsx(i,{})})):i();function i(){const e=Object.assign({h1:"h1",p:"p",code:"code",a:"a",strong:"strong",pre:"pre"},a(),n.components);return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"IDs via dataset"}),`
+`,t.jsx(e.h1,{id:"using-dataset-as-container-for-ids-and-other-values",children:"Using dataset as container for IDs and other values"}),`
+`,t.jsxs(e.p,{children:["Many components like the ",t.jsx(e.code,{children:"Select"}),", ",t.jsx(e.code,{children:"List"}),", ",t.jsx(e.code,{children:"Table"}),`, etc. are registering an event that handles the selection of child elements/components.
+In the handler of the event you receive the corresponding element(s), where you can e.g. access the `,t.jsx(e.code,{children:"textContent"})," to retrieve the selected text."]}),`
+`,t.jsxs(e.p,{children:[`In many cases the "text" is only there for users of the application, the actual value for e.g. the backend is often an id or a key.
+To also include these in the callback of the event, we recommend using `,t.jsx(e.a,{href:"https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset",target:"_blank",rel:"nofollow noopener noreferrer",children:"HTMLElement.dataset"}),`.
+With the help of the `,t.jsx(e.code,{children:"data-"})," API you can define any primitive value (like numbers, strings) as an attribute on an element. This data will be available on the change event as part of the dataset object of the selected element(s)."]}),`
+`,t.jsx(e.p,{children:"Here you can find a few examples:"}),`
+`,t.jsx(e.p,{children:t.jsx(e.strong,{children:"Select:"})}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const data = [
+  { id: 1, text: 'Option 1' },
+  { id: 2, text: 'Option 2' }
+];
+
+const onChange = (event) => {
+  // event.detail.selectedOption is a reference to the selected HTML Element
+  // dataset contains all attributes that were passed with the data- prefix.
+  console.log(event.detail.selectedOption.dataset.id);
+};
+
+<Select onChange={onChange}>
+  {data.map((item) => (
+    <Option key={item.id} data-id={item.id}>
+      {item.text}
+    </Option>
+  ))}
+</Select>;
+`})}),`
+`,t.jsx(e.p,{children:t.jsx(e.strong,{children:"List:"})}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const onSelectionChange = (event) => {
+  const { targetItem } = event.detail;
+  console.log(targetItem.dataset.custom);
+};
+
+<List mode={ListMode.MultiSelect} onSelectionChange={onSelectionChange}>
+  <StandardListItem additionalText="3" data-custom="custom value of list item 1">
+    List Item 1
+  </StandardListItem>
+  <StandardListItem additionalText="2" data-custom="custom value of list item 2">
+    List Item 2
+  </StandardListItem>
+  <StandardListItem additionalText="1" data-custom="custom value of list item 3">
+    List Item 3
+  </StandardListItem>
+</List>;
+`})}),`
+`,t.jsx(e.p,{children:t.jsx(e.strong,{children:"MultiInput with suggestion items:"})}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const onTokenDelete = (event) => {
+  const { token } = event.detail;
+  console.log(token.dataset.code);
+};
+const onSuggestionItemSelect = (event) => {
+  const { item } = event.detail;
+  console.log(item.dataset.code);
+};
+
+<MultiInput
+  onTokenDelete={onTokenDelete}
+  onSuggestionItemSelect={onSuggestionItemSelect}
+  showSuggestions
+  tokens={
+    <>
+      <Token data-code="ARG" text="Argentina" />
+      <Token data-code="BGR" text="Bulgaria" />
+      <Token data-code="FIN" text="Finland" />
+      <Token data-code="MEX" text="Mexico" />
+      <Token data-code="PHL" text="Philippines" />
+    </>
+  }
+>
+  <SuggestionItem data-code="GBR" icon="globe" text="United Kingdom" />
+  <SuggestionItem data-code="USA" icon="globe" text="United States" />
+  <SuggestionItem data-code="DEU" icon="globe" text="Germany" />
+  <SuggestionItem data-code="ISL" icon="globe" text="Iceland" />
+  <SuggestionItem data-code="MDA" icon="globe" text="Moldova" />
+  <SuggestionItem data-code="MAR" icon="globe" text="Morocco" />
+</MultiInput>;
+`})}),`
+`,t.jsx(s,{})]})}}export{Et as default};
