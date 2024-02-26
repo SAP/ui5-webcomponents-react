e08e72c11638baaf3684bdc54c8d5b7ba2a1cbb2
Marcus Notheis <marcus.notheis@sap.com>
2024-02-26 18:04:02 +0100

    refactor(ThemeProvider): migrate styles to css modules

--- packages/main/src/components/ThemeProvider/index.tsx
+++ packages/main/src/components/ThemeProvider/index.tsx
@@ -2,19 +2,23 @@
 
 import { getTheme } from '@ui5/webcomponents-base/dist/config/Theme.js';
 import { attachThemeLoaded, detachThemeLoaded } from '@ui5/webcomponents-base/dist/theming/ThemeLoaded.js';
-import { getStyleContext, ThemingParameters, useIsomorphicLayoutEffect } from '@ui5/webcomponents-react-base';
+import {
+  getStyleContext,
+  ThemingParameters,
+  useIsomorphicLayoutEffect,
+  useStylesheet
+} from '@ui5/webcomponents-react-base';
 import type { FC, ReactNode } from 'react';
 import React, { useMemo } from 'react';
-import { createUseStyles, ThemeProvider as ReactJssThemeProvider } from 'react-jss';
+import { ThemeProvider as ReactJssThemeProvider } from 'react-jss';
 import { I18nProvider } from '../../internal/I18nProvider.js';
 import { ModalsProvider } from '../Modals/ModalsProvider.js';
-import { GlobalStyleClassesStyles } from './GlobalStyleClasses.jss.js';
+import { styleData } from './ThemeProvider.css.js';
 
-// todo remove after jss types have been enhanced (https://github.com/cssinjs/jss/issues/1480)
-// @ts-expect-error: types currently don't allow an array of objects for the `extend` value, but the implementation does
-const useStyles = createUseStyles(GlobalStyleClassesStyles, {
-  name: 'ThemeProvider'
-});
+function ThemeProviderStyles() {
+  useStylesheet(styleData, ThemeProvider.displayName);
+  return null;
+}
 
 export interface ThemeProviderPropTypes {
   children: ReactNode;
@@ -35,7 +39,6 @@ export interface ThemeProviderPropTypes {
  */
 const ThemeProvider: FC<ThemeProviderPropTypes> = (props: ThemeProviderPropTypes) => {
   const { children, withoutModalsProvider, staticCssInjected } = props;
-  useStyles();
 
   useIsomorphicLayoutEffect(() => {
     document.documentElement.setAttribute('data-sap-theme', getTheme());
@@ -58,6 +61,7 @@ const ThemeProvider: FC<ThemeProviderPropTypes> = (props: ThemeProviderPropTypes
 
   return (
     <StyleContext.Provider value={styleContextValue}>
+      <ThemeProviderStyles />
       <ReactJssThemeProvider theme={ThemingParameters}>
         <I18nProvider>{withoutModalsProvider ? children : <ModalsProvider>{children}</ModalsProvider>}</I18nProvider>
       </ReactJssThemeProvider>
