129d2cd77a186cb24bdf1a2ab5032e94abad2cd2
Lukas742 <lukas.harbarth@sap.com>
2024-02-28 13:18:59 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0e34aad60de919219dbb1bdcae27f37dfb762dc6 🚀

---
+++ main/assets/Loader-gN8ivg4B.js
@@ -0,0 +1,49 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{F as a}from"./ProjectTemplate-dZzm2W_I.js";import{C as s}from"./ControlsWithNote-H47eHGCy.js";import{D as d}from"./DocsHeader-iEUo6zKm.js";import"./index-OjgoNOWw.js";import{M as l,b as n}from"./index-qD7WQwId.js";import{C as x,D as p,W as c}from"./Loader.stories-uCPWKVkh.js";import{u as m}from"./index-RwE9nQVW.js";import"./react-jss.esm-tmBtHfec.js";import"./index-iF5btqun.js";import"./iframe-za-lvryk.js";import"../sb-preview/runtime.js";import"./UI5Element-d7BKyERc.js";import"./Language-gwOe_M78.js";import"./VersionInfo-zmLW7G4m.js";import"./CustomElementsScopeUtils-StEZUlti.js";import"./withWebComponent-c-uI2wMj.js";import"./utils-_oNUceU3.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-kNlRgZSb.js";import"./decline-WQNFq9Fi.js";import"./Icons-Bde3sOsn.js";import"./i18n-defaults-SIMqfZL6.js";import"./information-2I5Ry-zV.js";import"./alert-NotaXBj3.js";import"./class-map-kWxrnrVL.js";import"./Icon-lAPI10Qw.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./Button-ywDxCu8p.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-fXrRGy4a.js";import"./chunk-HLWAVYOI-WXmjjQ4M.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./index-6f3-3TZK.js";import"./clsx-Zbgk8kpT.js";import"./index-tQsmQy9q.js";import"./Label-T23s9s-4.js";import"./WrappingType-avPrqc94.js";import"./index-WbJYnAkm.js";import"./index-KsbEQ3H9.js";import"./Link-YZ4obMbX.js";import"./index-faHD0WdU.js";import"./Popover-AngQKyti.js";import"./Integer-kog98579.js";import"./PopupUtils-cNe84Tf4.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-OX3LG9lc.js";import"./FocusableElements-wiyFyaM0.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-iTCl5diO.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5XXpitLE.js";import"./BrowserScrollbar.css-HwP8mBZS.js";import"./index-lwweRvrD.js";import"./i18n-defaults-nru-HZH8.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-9LQRDEyt.js";import"./StyleContext-9nyrn-qG.js";import"./ModalsContext-T9gUbaXC.js";import"./CssSizeVariables-HFpqBBhk.js";import"./CustomVariables-jcpx1a4M.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./addCustomCSSWithScoping-bUOmzhXF.js";import"./index-A8x1fqNf.js";import"./index-PKEA4E2_.js";import"./Avatar-W5DY45OX.js";import"./employee-638U4O_O.js";import"./index-TR38i7_W.js";import"./utils-xIcHYtB1.js";import"./index-5Sy_d2oC.js";import"./sys-help-2-pEGjeWPi.js";import"./copy-sn9oDqVN.js";import"./GitHub-Mark-JYujJlUX.js";import"./TableOfContent-dKjURGz0.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./spacing-ftLAC4UG.js";import"./index-xLYDZSFO.js";import"./index-bzkw3A5Q.js";function Yt(i={}){const{wrapper:r}=Object.assign({},m(),i.components);return r?t.jsx(r,Object.assign({},i,{children:t.jsx(e,{})})):e();function e(){const o=Object.assign({h2:"h2",h1:"h1",pre:"pre",code:"code"},m(),i.components);return t.jsxs(t.Fragment,{children:[t.jsx(l,{of:x}),`
+`,t.jsx(d,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(n,{of:p}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"card-with-loader",children:"Card with Loader"}),`
+`,t.jsx(n,{of:c}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`const LoaderComponent = () => {
+  const initialText =
+    'This is a text that will be updated 5 seconds after the header is clicked. The text will be visible until the new text has been fetched. To let the user know that the text is updating, the Loader will be displayed.';
+  const [text, setText] = useState(initialText);
+  const [loading, setLoading] = useState(false);
+  const onHeaderClick = () => {
+    setLoading(true);
+  };
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setText((prev) => (prev === initialText ? 'Updated (⌐■_■)' : initialText));
+        setLoading(false);
+      }, 5000);
+    }
+  }, [loading]);
+  return (
+    <Card
+      header={
+        <CardHeader
+          titleText="Click the header to update the text below."
+          interactive
+          avatar={<Icon name="activate" />}
+          onClick={onHeaderClick}
+        />
+      }
+      style={{ width: '400px' }}
+    >
+      <FlexBox direction={FlexBoxDirection.Column}>
+        {loading && <Loader />}
+        <Text style={spacing.sapUiContentPadding}>{text}</Text>
+      </FlexBox>
+    </Card>
+  );
+};
+`})}),`
+`,t.jsx(a,{})]})}}export{Yt as default};
--- main/assets/preview-Eyl8KTYx.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-JncxsQwv.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-PNYNzDf-.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-PNYNzDf-.js","./chunk-HLWAVYOI-cw4To3qg.js","./iframe-JncxsQwv.js","./index-OjgoNOWw.js","./react-18-1WT2dSvK.js","./index-QYh6idnL.js","./index-efu0DiPj.js","./_baseForOwn-a7AeJH13.js","./mapValues-_kP3xGjM.js","./_baseUniq-_JZHYp0e.js","./index-PPLHz8o0.js","./index-7I0hdIjK.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
