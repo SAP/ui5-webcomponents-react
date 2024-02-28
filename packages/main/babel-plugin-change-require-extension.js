81afc1518ea3d588b7ee02579c7699acbed5db9e
prasad-thakur <prasad.thakur@sap.com>
2024-02-28 13:45:24 -0800

    wip

---
+++ packages/main/babel-plugin-change-require-extension.js
@@ -0,0 +1,42 @@
+export default function (babel) {
+    const { types: t } = babel;
+  
+    return {
+      name: 'babel-plugin-change-require-extension',
+      visitor: {
+        CallExpression(path, state) {
+          if (
+            t.isIdentifier(path.node.callee, { name: 'require' }) &&
+            t.isStringLiteral(path.node.arguments[0]) &&
+            !path.node.arguments[0].value.includes('@') && 
+            path.node.arguments[0].value.endsWith('.js')
+          ) {
+            path.node.arguments[0].value = path.node.arguments[0].value.replace(/\.js$/, '.cjs');
+          }
+          if (
+            t.isIdentifier(path.node.callee, { name: 'require' }) &&
+            t.isStringLiteral(path.node.arguments[0]) &&
+            !path.node.arguments[0].value.includes('@') && 
+            path.node.arguments[0].value.includes('/dist')
+          ) {
+            path.node.arguments[0].value = path.node.arguments[0].value.replace('/dist', '/cjs');
+          }
+          if (
+            t.isIdentifier(path.node.callee, { name: 'require' }) &&
+            t.isStringLiteral(path.node.arguments[0]) &&
+            (
+              path.node.arguments[0].value.includes('@ui5/webcomponents/dist')
+              || path.node.arguments[0].value.includes('@ui5/webcomponents-fiori/dist')
+              || path.node.arguments[0].value.includes('@ui5/webcomponents-base/dist')
+              || path.node.arguments[0].value.includes('@ui5/webcomponents-icons/dist')
+              || path.node.arguments[0].value.includes('@ui5/webcomponents-theming/dist')
+            )
+          ) {
+            path.node.arguments[0].value = path.node.arguments[0].value.replace('/dist', '/cjs');
+            path.node.arguments[0].value = path.node.arguments[0].value.replace(/\.js$/, '.cjs');
+          }
+        },
+      },
+    };
+  }
+  
