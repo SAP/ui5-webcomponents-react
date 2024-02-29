acd2f4f0bf6be75dc34d87fbbcf43e7c8588c3d6
Lukas742 <lukas.harbarth@sap.com>
2024-02-29 08:05:20 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@65d7a0f95f4547c337ee5d4c995bf6414eff1675 🚀

---
+++ main/assets/TestSetup-kB8vanPj.js
@@ -0,0 +1,65 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import"./chunk-HLWAVYOI-ff2zELMN.js";import{M as s}from"./index-ARUMgr15.js";import{F as m}from"./ProjectTemplate-lowc3q6V.js";import"./index-OjgoNOWw.js";import"./index-QYh6idnL.js";import"./copy-PqQY0mDn.js";import{T as p}from"./TableOfContent-_KZAaQYp.js";import{u as i}from"./index-RwE9nQVW.js";import{M as a}from"./index-k_H4XbHc.js";import"./iframe-6VXrt-Ia.js";import"../sb-preview/runtime.js";import"./react-18-1WT2dSvK.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./react-jss.esm-fzYOEaou.js";import"./theming.esm-g_6oAtQL.js";import"./index-pmORB0Fb.js";import"./clsx-Zbgk8kpT.js";import"./index-AdeU9dSa.js";import"./Label-fARuFZ49.js";import"./UI5Element-ZYvPEp0D.js";import"./Language-JQ3LAQtE.js";import"./VersionInfo-zmLW7G4m.js";import"./CustomElementsScopeUtils-StEZUlti.js";import"./withWebComponent-c-uI2wMj.js";import"./utils-_oNUceU3.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./i18nBundle-1RfbKOQr.js";import"./WrappingType-avPrqc94.js";import"./i18n-defaults-5NJLMpyi.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./index-gRf168Go.js";import"./Button-6A8kFSGe.js";import"./event-lM5HanI-.js";import"./slot-HSrR9XJ-.js";import"./Keys-ZEuNsqmZ.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./Icons-cAVhCySg.js";import"./willShowContent-MSvI8sYo.js";import"./Icon-7RzQfNaI.js";import"./HasPopup-9BAMrqbD.js";import"./index-f6bluueW.js";import"./Link-HKW89dQ_.js";import"./index-ACRzvQy9.js";import"./Popover-UsQMNwiB.js";import"./Integer-kog98579.js";import"./PopupUtils-ihEiSFJi.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-ITzmnbbD.js";import"./FocusableElements-vlqyWKZU.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-YURVNHbB.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5XXpitLE.js";import"./class-map-kWxrnrVL.js";import"./BrowserScrollbar.css-za47fcqD.js";import"./index-SpQb4Cav.js";import"./i18n-defaults-8iJXv8tC.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-O-3hYuZs.js";import"./useStylesheet-2TpYMoe1.js";import"./ModalsContext-T9gUbaXC.js";import"./addCustomCSSWithScoping-tGNiKL_K.js";import"./index-Ef0vmbGd.js";import"./index-hASC3BP7.js";import"./Avatar-W7sCbj87.js";import"./employee-LTkoba5Z.js";import"./alert-dBb1Jp03.js";import"./index-H8zVtivq.js";import"./decline-U1O_cqCB.js";import"./i18n-defaults-SIMqfZL6.js";import"./information-QdD8C0Qh.js";function Ie(r={}){const{wrapper:n}=Object.assign({},i(),r.components);return n?e.jsx(n,Object.assign({},r,{children:e.jsx(t,{})})):t();function t(){const o=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2",pre:"pre",code:"code"},i(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Setup"}),`
+`,e.jsx(p,{headingSelector:"h2"}),`
+`,e.jsx(o.h1,{id:"cypress-test-setup",children:"Cypress Test Setup"}),`
+`,e.jsxs(o.p,{children:["UI5 Web Components for React is using ",e.jsx(o.a,{href:"https://www.cypress.io/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Cypress"})," as preferred testing framework. There you have the options of testing components only or create End-to-End tests for your application."]}),`
+`,e.jsx(o.h2,{id:"configuration",children:"Configuration"}),`
+`,e.jsxs(o.p,{children:["When launching Cypress the first time you're guided through the setup, which then will create a ",e.jsx(o.a,{href:"https://docs.cypress.io/guides/references/configuration",target:"_blank",rel:"nofollow noopener noreferrer",children:"configuration file"})," for you. You can use any configuration you like, but since we're heavily relying on web-components, we recommend traversing the shadow DOM per default:"]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-js",children:`includeShadowDom: true
+`})}),`
+`,e.jsxs(o.p,{children:[e.jsx(o.a,{href:"https://docs.cypress.io/guides/component-testing/react/overview",target:"_blank",rel:"nofollow noopener noreferrer",children:"Here"})," you can find the Cypress Quickstart tutorial for React."]}),`
+`,e.jsx(o.h2,{id:"themeprovider",children:"ThemeProvider"}),`
+`,e.jsxs(o.p,{children:["UI5 Web Components for React components have to be wrapped inside a ",e.jsx(o.code,{children:"ThemeProvider"}),", depending on your test setup (especially for component-testing) it might prove beneficial creating a custom ",e.jsx(o.code,{children:"mount"})," command, that includes the ",e.jsx(o.code,{children:"ThemeProvider"}),"."]}),`
+`,e.jsx(o.p,{children:"JavaScript:"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-js",children:`/**
+ * Cypress mount with ThemeProvider
+ */
+Cypress.Commands.add('mount', (component, options) => {
+  return mount(<ThemeProvider>{component}</ThemeProvider>, options);
+});
+`})}),`
+`,e.jsx(o.p,{children:"TypeScript declaration:"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-ts",children:`declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Cypress mount with ThemeProvider
+       */
+      mount: typeof mount;
+    }
+  }
+}
+`})}),`
+`,e.jsxs(o.p,{children:["You can define the command for example in the ",e.jsx(o.code,{children:"commands.ts/js"})," file:"]}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:"Example file"}),e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-tsx",children:`import { mount } from 'cypress/react18';
+import { ThemeProvider } from '@ui5/webcomponents-react';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Cypress mount with ThemeProvider
+       */
+      mount: typeof mount;
+    }
+  }
+}
+
+/**
+ * Cypress mount with ThemeProvider
+ */
+Cypress.Commands.add('mount', (component, options) => {
+  return mount(<ThemeProvider>{component}</ThemeProvider>, options);
+});
+`})})]}),`
+`,e.jsx(o.h2,{id:"cypress-commands-package-for-ui5-webcomponents",children:"Cypress Commands package for ui5-webcomponents"}),`
+`,e.jsx(a,{hideCloseButton:!0,children:`This package is a work-in-progress and can be extended by you! If you feel like a command or query is missing, please
+feel free to open a PR adding it to our library. Contributions are always welcome!`}),`
+`,e.jsx(o.p,{children:"Since testing web components is sometimes not as straightforward as one would like it to be, we offer a package with a set of commands and queries to make it easier to interact with them:"}),`
+`,e.jsx(o.p,{children:"Install:"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-sh",children:`npm install @ui5/webcomponents-cypress-commands
+`})}),`
+`,e.jsxs(o.p,{children:["After the installation you can import it in your entry file (e.g. ",e.jsx(o.code,{children:"component.ts"}),"):"]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-ts",children:`import '@ui5/webcomponents-cypress-commands';
+`})}),`
+`,e.jsx(o.p,{children:"Now you can use all commands and queries that are available in this package."}),`
+`,e.jsx(m,{})]})}}export{Ie as default};
---
+++ main/assets/UploadCollection-6UXLdVgc.js
@@ -0,0 +1,120 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{A as a,F as s}from"./ProjectTemplate-lowc3q6V.js";import{C as d}from"./ControlsWithNote-brStiLHF.js";import{D as c}from"./DocsHeader-YxSFBcaj.js";import"./index-OjgoNOWw.js";import{M as h,b as r,g as x,e as f}from"./index-ARUMgr15.js";import{S as u}from"./SubcomponentsSection-pzdPLcqE.js";import{C,D as p,S as j,U as l}from"./UploadCollection.stories-X4tCe6F9.js";import{u as m}from"./index-RwE9nQVW.js";import"./react-jss.esm-fzYOEaou.js";import"./theming.esm-g_6oAtQL.js";import"./index-k_H4XbHc.js";import"./iframe-6VXrt-Ia.js";import"../sb-preview/runtime.js";import"./UI5Element-ZYvPEp0D.js";import"./Language-JQ3LAQtE.js";import"./VersionInfo-zmLW7G4m.js";import"./CustomElementsScopeUtils-StEZUlti.js";import"./withWebComponent-c-uI2wMj.js";import"./utils-_oNUceU3.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-1RfbKOQr.js";import"./decline-U1O_cqCB.js";import"./Icons-cAVhCySg.js";import"./i18n-defaults-SIMqfZL6.js";import"./information-QdD8C0Qh.js";import"./alert-dBb1Jp03.js";import"./class-map-kWxrnrVL.js";import"./Icon-7RzQfNaI.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./Button-6A8kFSGe.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-5NJLMpyi.js";import"./chunk-HLWAVYOI-ff2zELMN.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./index-pmORB0Fb.js";import"./clsx-Zbgk8kpT.js";import"./index-AdeU9dSa.js";import"./Label-fARuFZ49.js";import"./WrappingType-avPrqc94.js";import"./index-gRf168Go.js";import"./index-f6bluueW.js";import"./Link-HKW89dQ_.js";import"./index-ACRzvQy9.js";import"./Popover-UsQMNwiB.js";import"./Integer-kog98579.js";import"./PopupUtils-ihEiSFJi.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-ITzmnbbD.js";import"./FocusableElements-vlqyWKZU.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-YURVNHbB.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5XXpitLE.js";import"./BrowserScrollbar.css-za47fcqD.js";import"./index-SpQb4Cav.js";import"./i18n-defaults-8iJXv8tC.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-O-3hYuZs.js";import"./useStylesheet-2TpYMoe1.js";import"./ModalsContext-T9gUbaXC.js";import"./addCustomCSSWithScoping-tGNiKL_K.js";import"./index-Ef0vmbGd.js";import"./index-hASC3BP7.js";import"./Avatar-W7sCbj87.js";import"./employee-LTkoba5Z.js";import"./index-H8zVtivq.js";import"./utils-gk7l7qqc.js";import"./index-_gCYH-Ub.js";import"./sys-help-2-8ufeK3Od.js";import"./copy-PqQY0mDn.js";import"./GitHub-Mark-JYujJlUX.js";import"./TableOfContent-_KZAaQYp.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./index-2d9v8_Tk.js";import"./index-OyOVNIPb.js";import"./Title-i5azmM6D.js";import"./ValueState-zUcANXoY.js";import"./ListItem-cRtEZCRd.js";import"./List-_i_LL5sC.js";import"./ItemNavigation-mTheLK6M.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-FNk7agZk.js";import"./CheckBox-5UXdlycg.js";import"./accept-qNBoGBkc.js";import"./RadioButton-7huYa7NF.js";import"./slim-arrow-right-TNC8KtYg.js";import"./Input-4H-tf53k.js";import"./ResponsivePopoverCommon.css-qbe5WpYD.js";import"./ValueStateMessage.css-tLIWhzsS.js";import"./ProgressIndicator-d_9Dns4Z.js";import"./AnimationMode-pHImiIVn.js";import"./i18n-defaults-6Oj3hLmZ.js";import"./parameters-bundle.css-jS5Rgmp1.js";import"./IllustratedMessage-OXKcQk0N.js";import"./Tent-6DmWTTCm.js";import"./sapIllus-Dialog-Tent-7d9NKC7h.js";import"./sapIllus-Scene-Tent-mV3-ehqy.js";import"./sapIllus-Spot-Tent-_0pCLAqf.js";function ve(t={}){const{wrapper:n}=Object.assign({},m(),t.components);return n?o.jsx(n,Object.assign({},t,{children:o.jsx(i,{})})):i();function i(){const e=Object.assign({h2:"h2",p:"p",code:"code",strong:"strong",pre:"pre",h1:"h1",h3:"h3"},m(),t.components);return o.jsxs(o.Fragment,{children:[o.jsx(h,{of:C}),`
+`,o.jsx(c,{since:"0.9.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"example",children:"Example"}),`
+`,o.jsx(r,{of:p}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(d,{of:p}),`
+`,o.jsx(e.h2,{id:"handling-drag-and-drop-of-files",children:"Handling drag and drop of files"}),`
+`,o.jsxs(e.p,{children:["Per default, files can be added by dragging them into the ",o.jsx(e.code,{children:"UploadCollection"}),` component.
+To use this functionality the `,o.jsx(e.code,{children:"onDrop"})," handler has to be manually adjusted.",o.jsx("br",{}),`
+`,o.jsx(e.strong,{children:"Note:"})," Use ",o.jsx(e.code,{children:"event.preventDefault()"})," in the ",o.jsx(e.code,{children:"onDrop"})," handler, otherwise some browsers will open some files in a different tab."]}),`
+`,o.jsxs(e.p,{children:["This example shows, how to dynamically render a ",o.jsx(e.code,{children:"UploadCollectionItem"})," by dropping files into the ",o.jsx(e.code,{children:"UploadCollection"}),":"]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`import React, { useState } from 'react';
+import { Icon, Text, UploadCollection, UploadCollectionItem } from '@ui5/webcomponents-react';
+
+export const UploadCollectionComponent = () => {
+  const [children, setChildren] = useState([]);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    files.forEach((file) => {
+      setChildren((prev) => [
+        ...prev,
+        <UploadCollectionItem file={file} fileName={file.name} thumbnail={<Icon name="document" />}>
+          <Text>Uploaded On: Right now</Text>
+        </UploadCollectionItem>
+      ]);
+    });
+  };
+  return <UploadCollection onDrop={handleDrop}>{children}</UploadCollection>;
+};
+`})}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"simulate-upload",children:"Simulate Upload"}),`
+`,o.jsx(r,{of:j}),`
+`,o.jsx(e.h3,{id:"code",children:"Code"}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const SimulateUploadComponent = () => {
+  const [children, setChildren] = useState([
+    <UploadCollectionItem
+      file={null}
+      fileName={'file-name.txt'}
+      thumbnail={<img src="https://sap.github.io/ui5-webcomponents/assets/images/logo.png" />}
+    >
+      <Text>Uploaded by: Susanne Schmitt · Uploaded On: 2019-04-20</Text>
+    </UploadCollectionItem>
+  ]);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    files.forEach((file) => {
+      setChildren((prev) => [
+        ...prev,
+        <UploadCollectionItem file={file} fileName={file.name} thumbnail={<Icon name="document" />}>
+          <Text>{\`
+Last
+modified: \${file.lastModifiedDate} ·
+Size: \${(file.size / 1000).toFixed(2)}KB\`}</Text>
+        </UploadCollectionItem>
+      ]);
+    });
+  };
+  const simulateUpload = () => {
+    if (children.length > 0) {
+      Children.forEach(children, (child, index) => {
+        if (child.props.uploadState === UploadState.Ready) {
+          let progress = 0;
+          const recTimeout = () => {
+            setTimeout(
+              () => {
+                progress += Math.floor(Math.random() * 4) + 1;
+                setChildren((prev) => {
+                  const updatedChildren = [...prev];
+                  updatedChildren[index] = cloneElement(prev[index], {
+                    uploadState: UploadState.Uploading,
+                    progress: Math.min(progress, 100)
+                  });
+                  return updatedChildren;
+                });
+                if (progress < 100) {
+                  recTimeout();
+                } else {
+                  setChildren((prev) => {
+                    const updatedChildren = [...prev];
+                    updatedChildren[index] = cloneElement(prev[index], {
+                      uploadState: UploadState.Complete
+                    });
+                    return updatedChildren;
+                  });
+                }
+              },
+              Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+            );
+          };
+          recTimeout();
+        }
+      });
+    }
+  };
+  return (
+    <UploadCollection
+      onDrop={handleDrop}
+      header={
+        <FlexBox alignItems={FlexBoxAlignItems.Center}>
+          <Title>Start upload:</Title>
+          <Button onClick={simulateUpload}>Upload!</Button>
+        </FlexBox>
+      }
+    >
+      {children}
+    </UploadCollection>
+  );
+};
+`})}),`
+`,o.jsx(x,{children:u}),`
+`,o.jsx(e.h2,{id:"uploadcollectionitem",children:"UploadCollectionItem"}),`
+`,o.jsx(f,{of:l}),`
+`,o.jsx(a,{of:l}),`
+`,o.jsx(s,{})]})}}export{ve as default};
