33908265ca143ad96417dcd77092e93e8704bceb
MarcusNotheis <marcus.notheis@sap.com>
2024-03-01 15:44:55 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@67354103b497ca396345b55ebe378d99603f3397 🚀

---
+++ main/assets/PluginOrderedMultiSort-yWen8tEm.js
@@ -0,0 +1,62 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{b as p,F as a}from"./ProjectTemplate-cLZqyAJQ.js";import{M as s,b as l}from"./index-m5EORB41.js";import{C as d}from"./ControlsWithNote-bLBUnRZc.js";import"./chunk-HLWAVYOI-zhAePvfm.js";import"./copy-PqQY0mDn.js";import"./index-OjgoNOWw.js";import{g as m}from"./AnalyticalTable.stories-8LLYhknZ.js";import{u as n}from"./index-RwE9nQVW.js";import"./react-jss.esm-fzYOEaou.js";import"./theming.esm-g_6oAtQL.js";import"./index-6lFVwiQo.js";import"./iframe-DdBiQAE1.js";import"../sb-preview/runtime.js";import"./UI5Element-ZYvPEp0D.js";import"./Language-JQ3LAQtE.js";import"./VersionInfo-zmLW7G4m.js";import"./CustomElementsScopeUtils-StEZUlti.js";import"./withWebComponent-c-uI2wMj.js";import"./utils-_oNUceU3.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-1RfbKOQr.js";import"./decline-U1O_cqCB.js";import"./Icons-cAVhCySg.js";import"./i18n-defaults-SIMqfZL6.js";import"./information-QdD8C0Qh.js";import"./alert-dBb1Jp03.js";import"./class-map-kWxrnrVL.js";import"./Icon-7RzQfNaI.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./Button-6A8kFSGe.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-5NJLMpyi.js";import"./index-Qwyc25sK.js";import"./clsx-Zbgk8kpT.js";import"./index-QYh6idnL.js";import"./index-fjcjUhA8.js";import"./Label-fARuFZ49.js";import"./WrappingType-avPrqc94.js";import"./index-gnnZ0jpA.js";import"./index-dYsGOQHc.js";import"./Link-HKW89dQ_.js";import"./index-lgNY92D2.js";import"./Popover-UsQMNwiB.js";import"./Integer-kog98579.js";import"./PopupUtils-ihEiSFJi.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-ITzmnbbD.js";import"./FocusableElements-vlqyWKZU.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-YURVNHbB.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5XXpitLE.js";import"./BrowserScrollbar.css-za47fcqD.js";import"./index-SpQb4Cav.js";import"./i18n-defaults-8iJXv8tC.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-O-3hYuZs.js";import"./useStylesheet-2TpYMoe1.js";import"./ModalsContext-T9gUbaXC.js";import"./addCustomCSSWithScoping-tGNiKL_K.js";import"./index-UBNLMrP1.js";import"./index-yIg4j6j-.js";import"./Avatar-W7sCbj87.js";import"./employee-LTkoba5Z.js";import"./index-jbalrbjJ.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./utils-1d5dUPRz.js";import"./index-f7v9y0hz.js";import"./sys-help-2-8ufeK3Od.js";import"./react-18-1WT2dSvK.js";import"./delete-0CDZ0T3C.js";import"./ListItem-cRtEZCRd.js";import"./List-_i_LL5sC.js";import"./ItemNavigation-mTheLK6M.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-FNk7agZk.js";import"./CheckBox-5UXdlycg.js";import"./ValueState-zUcANXoY.js";import"./accept-qNBoGBkc.js";import"./RadioButton-7huYa7NF.js";import"./slim-arrow-right-TNC8KtYg.js";import"./settings-m8XJdo3x.js";import"./useIsomorphicId-G0VPUEKO.js";import"./useIsRTL-ztCGrKk1.js";import"./directionChange-ei-C-idv.js";import"./index-ENBflQbU.js";import"./CssSizeVariables-eR3x6QxW.js";import"./CustomVariables-ISwRKzww.js";import"./IndicationColor-5SfIXGxx.js";import"./filter-13zOCLAE.js";import"./group-2-OzfVRjVL.js";import"./index-i4rrrRTV.js";import"./stopPropagation-dV8jBYUw.js";import"./index-uCaoerAx.js";import"./CustomListItem-d9Bn8tSh.js";import"./index-NYqH1F8T.js";import"./index-qvkGv2Mw.js";import"./StandardListItem-V7FBX-1f.js";import"./index-4mFKGE9C.js";import"./Input-4H-tf53k.js";import"./ResponsivePopoverCommon.css-qbe5WpYD.js";import"./ValueStateMessage.css-tLIWhzsS.js";import"./navigation-down-arrow-IO_Gduqc.js";import"./navigation-right-arrow-PvnKZmSZ.js";import"./useCurrentTheme-qvWmqmMp.js";import"./index-CD-FFeO3.js";import"./ValueState-I_-hGNIb.js";import"./index-1TPBJNP6.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./index-EkArHFSm.js";import"./Title-i5azmM6D.js";import"./debounce-F9irgL9Y.js";import"./index-PdjxQdkd.js";import"./Select-brdGSPu2.js";import"./InvisibleMessage-q86LnTqD.js";import"./slim-arrow-down-NPcLg6Gd.js";import"./ResponsivePopover-UG3Wasz2.js";import"./Dialog-Ykep6bLi.js";import"./index-EvC9EGyK.js";import"./index-CBop45Lq.js";import"./GroupHeaderListItem-DwRPCW-k.js";import"./Token-knvZcBmS.js";import"./ScrollEnablement-AuCpuuLP.js";import"./animate-Pj1TncyK.js";import"./sys-cancel-kNN_G3I8.js";import"./ToggleButton-vfZFJfKA.js";import"./ComboBoxItem-kDr1ieQD.js";import"./index-reQlVz10.js";function Br(o={}){const{wrapper:e}=Object.assign({},n(),o.components);return e?t.jsx(e,Object.assign({},o,{children:t.jsx(i,{})})):i();function i(){const r=Object.assign({h1:"h1",p:"p",code:"code",ul:"ul",li:"li",strong:"strong",h2:"h2",h3:"h3",pre:"pre"},n(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Data Display / AnalyticalTable / Plugin: useOrderedMultiSort"}),`
+`,t.jsx(r.h1,{id:"analyticaltable-plugin-useorderedmultisort",children:"AnalyticalTable Plugin: useOrderedMultiSort"}),`
+`,t.jsx(p,{moduleNames:["AnalyticalTableHooks"],packageName:"@ui5/webcomponents-react"}),`
+`,t.jsxs(r.p,{children:["With the ",t.jsx(r.code,{children:"useOrderedMultiSort"})," it's possible to define the sorting priority if multi-sort is enabled for the respective columns (",t.jsx(r.code,{children:"enableMultiSort: true"}),")."]}),`
+`,t.jsx(r.p,{children:"It receives one parameter that defines the sorting priority by the column id."}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.code,{children:"orderedIds: string[]"}),": Defines the sort priority when sorting by multiple columns, starting with the first column ID."]}),`
+`]}),`
+`,t.jsxs(r.p,{children:[t.jsx(r.strong,{children:"Note:"})," Column IDs that are not found in the array use the default priority, so the first sorted column has a higher priority than the next sorted column."]}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(l,{sourceState:"none",of:m}),`
+`,t.jsx(r.h3,{id:"current-parameter",children:"Current Parameter"}),`
+`,t.jsx(d,{of:m,hideHTMLPropsNote:!0,include:["orderedIds"]}),`
+`,t.jsx(r.h3,{id:"code",children:"Code"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`const columns = [
+  {
+    Header: 'Name',
+    accessor: 'name',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age',
+    accessor: 'age',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Name 2',
+    accessor: 'name2',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age 2',
+    accessor: 'age2',
+    enableMultiSort: true
+  }
+];
+const data = [
+  { name: 'Peter', age: 40, name2: 'Alissa', age2: 18 },
+  { name: 'Kristen', age: 40, name2: 'Randolph', age2: 21 },
+  { name: 'Peter', age: 30, name2: 'Rose', age2: 90 },
+  { name: 'Peter', age: 70, name2: 'Rose', age2: 22 },
+  { name: 'Kristen', age: 60, name2: 'Willis', age2: 80 },
+  { name: 'Kristen', age: 20, name2: 'Alissa', age2: 80 },
+  { name: 'Graham', age: 40, name2: 'Alissa', age2: 80 },
+  { name: 'Peter', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Willis', age2: 26 },
+  { name: 'Graham', age: 62, name2: 'Willis', age2: 26 }
+];
+const orderedIds = ['name', 'name2', 'age', 'age2'];
+const TableComponent = () => {
+  return (
+    <AnalyticalTable
+      columns={columns}
+      data={data}
+      sortable
+      tableHooks={[AnalyticalTableHooks.useOrderedMultiSort(orderedIds)]}
+    />
+  );
+};
+`})}),`
+`,t.jsx(a,{})]})}}export{Br as default};
---
+++ main/assets/Toast-O2yahkPv.js
@@ -0,0 +1,27 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{F as n}from"./ProjectTemplate-cLZqyAJQ.js";import{C as s}from"./ControlsWithNote-bLBUnRZc.js";import{D as a}from"./DocsHeader-emBtmyqf.js";import"./index-OjgoNOWw.js";import{M as c,b as h}from"./index-m5EORB41.js";import{C as d,D as p}from"./Toast.stories-k6sg-wIT.js";import{u as e}from"./index-RwE9nQVW.js";import"./react-jss.esm-fzYOEaou.js";import"./theming.esm-g_6oAtQL.js";import"./index-6lFVwiQo.js";import"./iframe-DdBiQAE1.js";import"../sb-preview/runtime.js";import"./UI5Element-ZYvPEp0D.js";import"./Language-JQ3LAQtE.js";import"./VersionInfo-zmLW7G4m.js";import"./CustomElementsScopeUtils-StEZUlti.js";import"./withWebComponent-c-uI2wMj.js";import"./utils-_oNUceU3.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-1RfbKOQr.js";import"./decline-U1O_cqCB.js";import"./Icons-cAVhCySg.js";import"./i18n-defaults-SIMqfZL6.js";import"./information-QdD8C0Qh.js";import"./alert-dBb1Jp03.js";import"./class-map-kWxrnrVL.js";import"./Icon-7RzQfNaI.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-mPCp9TQx.js";import"./Button-6A8kFSGe.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-5NJLMpyi.js";import"./chunk-HLWAVYOI-zhAePvfm.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./index-Qwyc25sK.js";import"./clsx-Zbgk8kpT.js";import"./index-fjcjUhA8.js";import"./Label-fARuFZ49.js";import"./WrappingType-avPrqc94.js";import"./index-gnnZ0jpA.js";import"./index-dYsGOQHc.js";import"./Link-HKW89dQ_.js";import"./index-lgNY92D2.js";import"./Popover-UsQMNwiB.js";import"./Integer-kog98579.js";import"./PopupUtils-ihEiSFJi.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-ITzmnbbD.js";import"./FocusableElements-vlqyWKZU.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-YURVNHbB.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-5XXpitLE.js";import"./BrowserScrollbar.css-za47fcqD.js";import"./index-SpQb4Cav.js";import"./i18n-defaults-8iJXv8tC.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-O-3hYuZs.js";import"./useStylesheet-2TpYMoe1.js";import"./ModalsContext-T9gUbaXC.js";import"./addCustomCSSWithScoping-tGNiKL_K.js";import"./index-UBNLMrP1.js";import"./index-yIg4j6j-.js";import"./Avatar-W7sCbj87.js";import"./employee-LTkoba5Z.js";import"./index-jbalrbjJ.js";import"./utils-1d5dUPRz.js";import"./index-f7v9y0hz.js";import"./sys-help-2-8ufeK3Od.js";import"./copy-PqQY0mDn.js";import"./GitHub-Mark-JYujJlUX.js";import"./TableOfContent-W42zgc2B.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./index-tiBSU-HN.js";function Jo(r={}){const{wrapper:i}=Object.assign({},e(),r.components);return i?o.jsx(i,Object.assign({},r,{children:o.jsx(m,{})})):m();function m(){const t=Object.assign({h2:"h2",p:"p",code:"code",strong:"strong",pre:"pre"},e(),r.components);return o.jsxs(o.Fragment,{children:[o.jsx(c,{of:d}),`
+`,o.jsx(a,{since:"0.9.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(h,{of:p}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:p}),`
+`,o.jsx(t.h2,{id:"show-a-toast",children:"Show a toast"}),`
+`,o.jsxs(t.p,{children:["The ",o.jsx(t.code,{children:"Toast"}),` component has an imperative API for getting displayed. It will not be displayed just because it is part of the DOM.
+In order to show the Toast, you have to get a reference to the `,o.jsx(t.code,{children:"Toast"})," DOM element and call the ",o.jsx(t.code,{children:"show"}),`-method.
+You can either access the DOM element by using a React `,o.jsx(t.code,{children:"ref"})," or work with IDs."]}),`
+`,o.jsx(t.p,{children:o.jsx(t.strong,{children:"Example"})}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`export const MyComponentWithAToast() {
+  const toast = useRef(null);
+
+  const showToast = () => {
+    toast.current.show();
+  };
+  return (
+    <ThemeProvider>
+      <Button onClick={showToast}>Show Toast</Button>
+      <Toast ref={toast}>This is my Toast!</Toast>
+    </ThemeProvider>
+  );
+}
+`})}),`
+`,o.jsx(n,{})]})}}export{Jo as default};
