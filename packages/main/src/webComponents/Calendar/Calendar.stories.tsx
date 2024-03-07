990ab96b29e1c35fc66f6f075eb29a551a8a0a1f
Marcus Notheis <marcus.notheis@sap.com>
2024-03-07 10:47:43 +0100

    create stories

--- packages/main/src/webComponents/Calendar/Calendar.stories.tsx
+++ packages/main/src/webComponents/Calendar/Calendar.stories.tsx
@@ -4,7 +4,11 @@ import '@ui5/webcomponents-localization/dist/features/calendar/Islamic.js';
 import '@ui5/webcomponents-localization/dist/features/calendar/Japanese.js';
 import '@ui5/webcomponents-localization/dist/features/calendar/Persian.js';
 import type { Meta, StoryObj } from '@storybook/react';
-import { CalendarType, CalendarSelectionMode } from '../../enums/index.js';
+import CalendarLegendItemType from '@ui5/webcomponents/dist/types/CalendarLegendItemType.js';
+import { CalendarSelectionMode, CalendarType } from '../../enums/index.js';
+import { CalendarLegend } from '../CalendarLegend/index.js';
+import { CalendarLegendItem } from '../CalendarLegendItem/index.js';
+import { SpecialCalendarDate } from '../SpecialCalendarDate/index.js';
 import { Calendar } from './index.js';
 
 const meta = {
@@ -24,3 +28,38 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+const currentDate = new Date();
+const firstDayOfTheMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+const yearMonthString = `${firstDayOfTheMonth.getFullYear()}-${(firstDayOfTheMonth.getMonth() + 1).toString().padStart(2, '0')}-`;
+const firstDateOfMonthString = `${yearMonthString}${firstDayOfTheMonth.getDate()}`;
+
+export const SpecialDate: Story = {
+  args: {
+    specialDates: <SpecialCalendarDate type={CalendarLegendItemType.Type01} value={firstDateOfMonthString} />
+  }
+};
+
+export const CalendarLegendStory: Story = {
+  name: 'CalenderLegend',
+  args: {
+    calendarLegend: (
+      <CalendarLegend>
+        <CalendarLegendItem text="Vacation" type={CalendarLegendItemType.Type05} />
+        <CalendarLegendItem text="School Vacation" type={CalendarLegendItemType.Type07} />
+        <CalendarLegendItem text="Wedding" type={CalendarLegendItemType.Type13} />
+      </CalendarLegend>
+    ),
+    specialDates: (
+      <>
+        <SpecialCalendarDate type={CalendarLegendItemType.Type05} value={`${yearMonthString}01`} />
+        <SpecialCalendarDate type={CalendarLegendItemType.Type05} value={`${yearMonthString}02`} />
+        <SpecialCalendarDate type={CalendarLegendItemType.Type05} value={`${yearMonthString}03`} />
+        <SpecialCalendarDate type={CalendarLegendItemType.Type07} value={`${yearMonthString}11`} />
+        <SpecialCalendarDate type={CalendarLegendItemType.Type07} value={`${yearMonthString}12`} />
+        <SpecialCalendarDate type={CalendarLegendItemType.Type07} value={`${yearMonthString}13`} />
+        <SpecialCalendarDate type={CalendarLegendItemType.Type13} value={`${yearMonthString}26`} />
+      </>
+    )
+  }
+};
