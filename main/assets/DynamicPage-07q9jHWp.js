f733fb7ded93b14933d0eaea421c30e2d21b4a81
MarcusNotheis <marcus.notheis@sap.com>
2024-03-13 07:02:44 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@8b95cae18700686cf439183ae024d4637acd8cb8 🚀

---
+++ main/assets/DynamicPage-07q9jHWp.js
@@ -0,0 +1,66 @@
+import{j as t}from"./jsx-runtime-DtaoT6pD.js";import{A as p,F as h}from"./ProjectTemplate-Tpxvq93e.js";import{C as l}from"./ControlsWithNote-l8P1gBvK.js";import{D as g}from"./DocsHeader-r5CKSX9D.js";import{D as m,a as s}from"./index-BtL6frit.js";import"./index-OjgoNOWw.js";import{M as x,b as i,g as j,e as a}from"./index-10ItULUv.js";import{C as f,D as c,A as u,W as y,a as b,b as w,S as v}from"./DynamicPage.stories-6LFeC1eN.js";import{S as D}from"./SubcomponentsSection-pzdPLcqE.js";import{u as d}from"./index-RwE9nQVW.js";import"./react-jss.esm-fzYOEaou.js";import"./theming.esm-g_6oAtQL.js";import"./index-lcns09hs.js";import"./iframe-RNL-KbD1.js";import"../sb-preview/runtime.js";import"./UI5Element-9EXj3atz.js";import"./Boot-vTaFuwdG.js";import"./VersionInfo-ldzxTIVv.js";import"./CustomElementsScopeUtils-orabJqjR.js";import"./withWebComponent-LTFbYRvs.js";import"./utils-d3BcYjeO.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-j8vDD4VY.js";import"./decline-j4MODHQ_.js";import"./Icons-Vbmmw7bO.js";import"./i18n-defaults-9hHjZPfp.js";import"./information-6141dtH3.js";import"./alert--0SE21sY.js";import"./class-map-gjOVarQw.js";import"./Icon-kyep1SqZ.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-EKzfhWK3.js";import"./Button-DcwyQdV9.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-X1FTyPSK.js";import"./chunk-HLWAVYOI-vtWku3qR.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./index-xrvtQWKa.js";import"./clsx-Zbgk8kpT.js";import"./index-Z4iIm-43.js";import"./Label-mY98SzPQ.js";import"./WrappingType-avPrqc94.js";import"./index-1H2OSsad.js";import"./index-DykoCDza.js";import"./Link-9XR0tYIs.js";import"./index-8OGCBeEU.js";import"./Popover-Yl5FRmf7.js";import"./Integer-kog98579.js";import"./PopupUtils-kUkkQSCb.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-xzQQME9v.js";import"./FocusableElements-xTkOlDEw.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-_qRzLmqT.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-tejh5TI3.js";import"./BrowserScrollbar.css-wlj1ktzG.js";import"./index-z42VFOj5.js";import"./i18n-defaults-HYsq20ry.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-EsRn4j4y.js";import"./useStylesheet-QHWPoMwQ.js";import"./ModalsContext-T9gUbaXC.js";import"./addCustomCSSWithScoping-ZfvsxM11.js";import"./index-ceAHma9O.js";import"./index-W-wqq8i9.js";import"./Avatar-tUVXUHcz.js";import"./employee-EyN-jrGw.js";import"./index-_tDyOIwc.js";import"./utils-3GTUBjzG.js";import"./index-z1ARxk4x.js";import"./sys-help-2-QhGERdVJ.js";import"./copy-hCLuyOUN.js";import"./GitHub-Mark-JYujJlUX.js";import"./TableOfContent-d1vtA3nw.js";import"./slim-arrow-down-7iBYeJev.js";import"./slim-arrow-up-doHQO4do.js";import"./index-ENBflQbU.js";import"./index-tZVLiD8j.js";import"./ToggleButton-RjjppzzK.js";import"./stopPropagation-dV8jBYUw.js";import"./index-iuC4fKy_.js";import"./overflow-8qX-vrr1.js";import"./CssSizeVariables-eR3x6QxW.js";import"./CustomVariables-ISwRKzww.js";import"./debounce-F9irgL9Y.js";import"./index-qKpkKig6.js";import"./Media-HHIdDivs.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./index-Y1W1WFaS.js";import"./ItemNavigation-F0c19DRy.js";import"./TabbableElements-L1VqloMt.js";import"./debounce-8VpZswfd.js";import"./BusyIndicator-x3up_4gM.js";import"./CheckBox-W7r2Z5M4.js";import"./ValueState-zUcANXoY.js";import"./accept-IjkrYMVs.js";import"./navigation-down-arrow-yYEv3cZI.js";import"./index-wpTQNQbX.js";import"./PageBackgroundDesign-CpulD27W.js";import"./GlobalStyleClasses-kWTEndz_.js";import"./index-y-brTMva.js";import"./index-4brqieoI.js";import"./ValueState-I_-hGNIb.js";import"./VariantItem-_44hLM7C.js";import"./UnableToLoad-BXY81OGG.js";import"./IllustratedMessage-Eojhei9a.js";import"./Title-yLgJni0m.js";import"./i18n-defaults-MpaKYGWX.js";import"./parameters-bundle.css--8JKw2Pt.js";import"./sapIllus-Dialog-UnableToLoad-RKCJaSIM.js";import"./sapIllus-Scene-UnableToLoad-mYVabPF8.js";import"./sapIllus-Spot-UnableToLoad-ZpsEKsf6.js";import"./search-cNHASOp2.js";import"./index-zOvfMqJv.js";import"./index-T0zKZqPw.js";import"./Dialog-Kb1Go7sM.js";import"./favorite-Bl0dpVXG.js";import"./unfavorite-VJn_W3zM.js";import"./index-AMT5Xdm2.js";import"./RadioButton-Rfuvm4gi.js";import"./index-vbvs2wzd.js";import"./index-U4UxTA8D.js";import"./Input-l-VGVNnc.js";import"./ResponsivePopoverCommon.css-jkbZ-5Gq.js";import"./ValueStateMessage.css-ulM_ieaz.js";import"./useIsomorphicId-G0VPUEKO.js";import"./index-V0yWyD53.js";import"./index-YLNkfp0y.js";import"./index-WC-RSWKv.js";import"./ResponsivePopover-NpDqijfo.js";import"./index-vZEJ-YM4.js";import"./List-8miUKCwk.js";import"./index-vz7F4ueF.js";import"./StandardListItem-HxyuKE_F.js";import"./ListItem-WDqK2DXW.js";import"./slim-arrow-right-w4AUPzoN.js";import"./index-SkevvjSA.js";import"./index-_o1dVOPP.js";import"./slideUp-EM1U0pkt.js";import"./animate-Pj1TncyK.js";import"./AnimationMode-rtntj-do.js";import"./index-pS7f5z-O.js";function ui(r={}){const{wrapper:e}=Object.assign({},d(),r.components);return e?t.jsx(e,Object.assign({},r,{children:t.jsx(n,{})})):n();function n(){const o=Object.assign({h2:"h2",h1:"h1",p:"p",strong:"strong",code:"code",ul:"ul",li:"li",h3:"h3",pre:"pre"},d(),r.components);return t.jsxs(t.Fragment,{children:[t.jsx(x,{of:f}),`
+`,t.jsx(g,{subComponents:["DynamicPageTitle","DynamicPageHeader"]}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(i,{of:c}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{of:c}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(o.h2,{id:"always-show-header-content",children:"Always show Header Content"}),`
+`,t.jsx(i,{of:u}),`
+`,t.jsx(o.h2,{id:"variant-management-as-title",children:"Variant Management as Title"}),`
+`,t.jsxs(o.p,{children:[t.jsx(o.strong,{children:"Note:"})," To prevent the ",t.jsx(o.code,{children:"click"})," event of the ",t.jsx(o.code,{children:"VariantManagement"})," component from bubbling up and collapsing/expanding the ",t.jsx(o.code,{children:"DynamicPageHeader"}),", please call ",t.jsx(o.code,{children:"event.stopPropagation()"})," inside the ",t.jsx(o.code,{children:"onClick"})," callback of the ",t.jsx(o.code,{children:"VariantManagement"}),"."]}),`
+`,t.jsx(i,{of:y}),`
+`,t.jsx(o.h2,{id:"with-floating-footer",children:"With floating footer"}),`
+`,t.jsx(i,{of:b}),`
+`,t.jsx(o.h2,{id:"dynamicpage-with-custom-actions-and-navigationactions-overflow-button",children:"DynamicPage with custom actions and navigationActions overflow button"}),`
+`,t.jsxs(o.p,{children:["The ",t.jsx(o.code,{children:"DynamicPageTitle"})," offers two props (",t.jsx(o.code,{children:"actionsToolbarProps"})," and ",t.jsx(o.code,{children:"navigationActionsToolbarProps"}),") to configure the props of the ",t.jsx(o.code,{children:"actions"})," and ",t.jsx(o.code,{children:"navigationActions"}),` toolbars.
+With these objects it's possible to e.g. configure the overflow button displayed within each toolbar by setting the `,t.jsx(o.code,{children:"overflowButton"}),` prop.
+`,t.jsx(o.code,{children:"navigationActionsToolbarProps"})," only has an effect, if the ",t.jsx(o.code,{children:"navigationActions"})," toolbar is used. (width < 1280px)"]}),`
+`,t.jsx(i,{of:w}),`
+`,t.jsx(o.h2,{id:"with-sticky-sub-headers",children:"With sticky sub-headers"}),`
+`,t.jsxs(o.p,{children:["To display sticky sub-headers that line-up with the standard header of the ",t.jsx(o.code,{children:"DynamicPage"})," you're able to render ",t.jsx(o.code,{children:"children"}),` as function.
+There you receive an object as parameter that offers the following properties:`]}),`
+`,t.jsxs(o.ul,{children:[`
+`,t.jsxs(o.li,{children:[t.jsx(o.code,{children:"stickyHeaderHeight"}),": The height of the ",t.jsx(o.code,{children:"DynamicPage"})," header sections which will stick to the top of the page."]}),`
+`]}),`
+`,t.jsx(i,{of:v,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"code",children:"Code"}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`<DynamicPage {...props}>
+  {({ stickyHeaderHeight }) => (
+    <>
+      <div
+        style={{
+          position: 'sticky',
+          width: '100%',
+          height: '4rem',
+          background: 'lightgreen',
+          top: \`\${stickyHeaderHeight}px\`
+        }}
+      >
+        Sticky Header
+      </div>
+      <div style={{ width: '100%', background: 'orange', height: '10rem' }}>Content</div>
+      <div
+        style={{
+          position: 'sticky',
+          width: '100%',
+          height: '8rem',
+          background: 'lightgreen',
+          top: \`calc(\${stickyHeaderHeight}px + 4rem)\`
+        }}
+      >
+        Sticky Header 2
+      </div>
+      <div style={{ background: 'lightblue', height: '2000px', width: '100%' }}>Content</div>
+    </>
+  )}
+</DynamicPage>
+`})}),`
+`,t.jsx(j,{children:D}),`
+`,t.jsx(o.h2,{id:"dynamicpagetitle",children:"DynamicPageTitle"}),`
+`,t.jsx(a,{of:m}),`
+`,t.jsx(p,{of:m}),`
+`,t.jsx(o.h2,{id:"dynamicpageheader",children:"DynamicPageHeader"}),`
+`,t.jsx(a,{of:s}),`
+`,t.jsx(p,{of:s}),`
+`,t.jsx(h,{})]})}}export{ui as default};
