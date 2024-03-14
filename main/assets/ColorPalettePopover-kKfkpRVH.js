eefc5ade88bd084f28f130e61eb4040ff1e8e678
Lukas742 <lukas.harbarth@sap.com>
2024-03-14 13:51:56 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@9e026063207d5264b75355609c90f630d59c4ed9 🚀

---
+++ main/assets/ColorPalettePopover-kKfkpRVH.js
@@ -0,0 +1,30 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{F as s}from"./ProjectTemplate-PwI0GDGR.js";import{C as a}from"./ControlsWithNote-oa1iLw2x.js";import{D as l}from"./DocsHeader-48VmbZjM.js";import"./index-OjgoNOWw.js";import{M as c,b as d,g as h,e as x,h as j}from"./index-4APXiYZI.js";import{C,D as p}from"./ColorPalettePopover.stories-DRZUle04.js";import{S as f}from"./SubcomponentsSection-pzdPLcqE.js";import{C as m}from"./index-dGTVzwB2.js";import{u as n}from"./index-RwE9nQVW.js";import"./react-jss.esm-fzYOEaou.js";import"./theming.esm-g_6oAtQL.js";import"./index-RnPy2Io-.js";import"./iframe-xdC5_25V.js";import"../sb-preview/runtime.js";import"./UI5Element-9EXj3atz.js";import"./Boot-vTaFuwdG.js";import"./VersionInfo-ldzxTIVv.js";import"./CustomElementsScopeUtils-orabJqjR.js";import"./withWebComponent-LTFbYRvs.js";import"./utils-d3BcYjeO.js";import"./useIsomorphicLayoutEffect-s31TYDr7.js";import"./slot-HSrR9XJ-.js";import"./event-lM5HanI-.js";import"./i18nBundle-j8vDD4VY.js";import"./decline-j4MODHQ_.js";import"./Icons-Vbmmw7bO.js";import"./i18n-defaults-9hHjZPfp.js";import"./information-6141dtH3.js";import"./alert--0SE21sY.js";import"./class-map-gjOVarQw.js";import"./Icon-kyep1SqZ.js";import"./Keys-ZEuNsqmZ.js";import"./parameters-bundle.css-EKzfhWK3.js";import"./Button-DcwyQdV9.js";import"./AriaLabelHelper-RbKlVwzD.js";import"./MarkedEvents-aYv14vsS.js";import"./willShowContent-MSvI8sYo.js";import"./HasPopup-9BAMrqbD.js";import"./i18n-defaults-X1FTyPSK.js";import"./chunk-HLWAVYOI-UcZ4rZXN.js";import"./react-18-1WT2dSvK.js";import"./index-QYh6idnL.js";import"./index-0grRi0wq.js";import"./clsx-Zbgk8kpT.js";import"./index-m4xNMekf.js";import"./Label-mY98SzPQ.js";import"./WrappingType-avPrqc94.js";import"./index-Rv02CbMm.js";import"./index-R15YUt03.js";import"./Link-9XR0tYIs.js";import"./index-5uXVWs2W.js";import"./Popover-Yl5FRmf7.js";import"./Integer-kog98579.js";import"./PopupUtils-kUkkQSCb.js";import"./getActiveElement-IIKAy3xo.js";import"./PopupsCommon.css-xzQQME9v.js";import"./FocusableElements-xTkOlDEw.js";import"./isElementHidden-d3-Fo4V6.js";import"./getEffectiveScrollbarStyle-Gz48PLeD.js";import"./ResizeHandler-_qRzLmqT.js";import"./MediaRange-sEMt71Ve.js";import"./style-map-tejh5TI3.js";import"./BrowserScrollbar.css-wlj1ktzG.js";import"./index-z42VFOj5.js";import"./i18n-defaults-HYsq20ry.js";import"./I18nContext-VUOpdMoH.js";import"./ThemingParameters-UghqSl-x.js";import"./index-EsRn4j4y.js";import"./useStylesheet-QHWPoMwQ.js";import"./ModalsContext-T9gUbaXC.js";import"./addCustomCSSWithScoping-ZfvsxM11.js";import"./index-nPQIt2RH.js";import"./index-cAKCeWZu.js";import"./Avatar-tUVXUHcz.js";import"./employee-EyN-jrGw.js";import"./index-EyFaivVw.js";import"./utils-WGXmcXPp.js";import"./index-yx6XmAo1.js";import"./sys-help-2-QhGERdVJ.js";import"./copy-hCLuyOUN.js";import"./GitHub-Mark-JYujJlUX.js";import"./TableOfContent-iGRTdAhB.js";import"./_baseForOwn-a7AeJH13.js";import"./mapValues-_kP3xGjM.js";import"./_baseUniq-_JZHYp0e.js";import"./index-PPLHz8o0.js";import"./index-7I0hdIjK.js";import"./CSSColor-cu9KZbQi.js";import"./ResponsivePopoverCommon.css-jkbZ-5Gq.js";import"./Title-yLgJni0m.js";import"./ResponsivePopover-NpDqijfo.js";import"./Dialog-Kb1Go7sM.js";import"./ValueState-zUcANXoY.js";import"./ItemNavigation-F0c19DRy.js";function mt(r={}){const{wrapper:e}=Object.assign({},n(),r.components);return e?o.jsx(e,Object.assign({},r,{children:o.jsx(i,{})})):i();function i(){const t=Object.assign({h2:"h2",p:"p",code:"code",strong:"strong",em:"em",pre:"pre"},n(),r.components);return o.jsxs(o.Fragment,{children:[o.jsx(c,{of:C}),`
+`,o.jsx(l,{since:"0.20.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(d,{of:p}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{of:p}),`
+`,o.jsx(t.h2,{id:"opening-colorpalettepopovers",children:"Opening ColorPalettePopovers"}),`
+`,o.jsxs(t.p,{children:[o.jsx(t.code,{children:"ColorPalettePopover"}),"s can only be opened by attaching a ",o.jsx(t.code,{children:"ref"})," to the component and then call the corresponding ",o.jsx(t.strong,{children:o.jsx(t.code,{children:"showAt"})})," method. The method receives the target element - ",o.jsxs(t.em,{children:["on which the ",o.jsx(t.code,{children:"ColorPalettePopover"})," is to be opened"]})," - as parameter."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const ColorPalettePopoverComponent = () => {
+  const popoverRef = useRef();
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open ColorPalettePopover</Button>
+      <ColorPalettePopover ref={popoverRef}>
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="darkblue" />
+      </ColorPalettePopover>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(h,{children:f}),`
+`,o.jsx(t.h2,{id:"colorpaletteitem",children:"ColorPaletteItem"}),`
+`,o.jsx(x,{of:m}),`
+`,o.jsx(j,{of:m}),`
+`,o.jsx(s,{})]})}}export{mt as default};
