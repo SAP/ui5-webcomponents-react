c405c7691f2a7c414c424a0c4077a44499c11aa5
Lukas742 <lukas.harbarth@sap.com>
2024-03-14 15:45:16 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@7f78023312345210a50f42313326c25eec1ea60d 🚀

---
+++ main/assets/index-7YamMebP.js
@@ -0,0 +1,21 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./Title-yLgJni0m.js","./UI5Element-9EXj3atz.js","./Boot-vTaFuwdG.js","./VersionInfo-ldzxTIVv.js","./CustomElementsScopeUtils-orabJqjR.js","./withWebComponent-LTFbYRvs.js","./utils-d3BcYjeO.js","./index-OjgoNOWw.js","./jsx-runtime-DtaoT6pD.js","./useIsomorphicLayoutEffect-s31TYDr7.js","./WrappingType-avPrqc94.js","./parameters-bundle.css-EKzfhWK3.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as n}from"./iframe-oM_A8Xae.js";import"./Title-yLgJni0m.js";import{w as o}from"./withWebComponent-LTFbYRvs.js";const e=o("ui5-title",["level","wrappingType"],[],[],[],()=>n(()=>import("./Title-yLgJni0m.js").then(t=>t.b),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url));e.displayName="Title";try{e.displayName="Title",e.__docgenInfo={description:`The \`Title\` component is used to display titles inside a page.
+It is a simple, large-sized text with explicit header/title semantics.
+
+
+
+__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/playground/)`,displayName:"Title",props:{children:{defaultValue:null,description:`Defines the text of the component.
+This component supports nesting a \`Link\` component inside.
+
+**Note:** Although this slot accepts HTML Elements, it is strongly recommended that you only use text in order to preserve the intended design.`,name:"children",required:!1,type:{name:"ReactNode | ReactNode[]"}},level:{defaultValue:{value:'"H2"'},description:'Defines the component level.\nAvailable options are: `"H6"` to `"H1"`.',name:"level",required:!1,type:{name:"enum",value:[{value:'"H1"'},{value:'"H2"'},{value:'"H3"'},{value:'"H4"'},{value:'"H5"'},{value:'"H6"'},{value:'"H1"'},{value:'"H2"'},{value:'"H3"'},{value:'"H4"'},{value:'"H5"'},{value:'"H6"'}]}},wrappingType:{defaultValue:{value:'"None"'},description:`Defines how the text of a component will be displayed when there is not enough space.
+
+**Note:** for option "Normal" the text will wrap and the words will not be broken based on hyphenation.`,name:"wrappingType",required:!1,type:{name:"enum",value:[{value:'"None"'},{value:'"None"'},{value:'"Normal"'},{value:'"Normal"'}]}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}export{e as T};
