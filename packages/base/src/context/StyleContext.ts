a01ce8a4ab1aff5892482317e457ed6a64b19ad6
Marcus Notheis <marcus.notheis@sap.com>
2024-03-17 08:50:41 +0100

    fix(useStylesheet): only remove styles when all components are unmounted
    fixes #5608

--- packages/base/src/context/StyleContext.ts
+++ packages/base/src/context/StyleContext.ts
@@ -4,8 +4,14 @@ import { createContext, useContext } from 'react';
 
 const SYMBOL = Symbol.for('@ui5/webcomponents-react/StyleContext');
 
-const StyleContext = createContext<{ staticCssInjected: boolean }>({
-  staticCssInjected: false
+interface StyleContextValue {
+  staticCssInjected: boolean;
+  componentsMap: Map<string, number>;
+}
+
+const StyleContext = createContext<StyleContextValue>({
+  staticCssInjected: false,
+  componentsMap: new Map<string, number>()
 });
 
 export function getStyleContext(): typeof StyleContext {
