addac5f21f5f7d932b254f82f3c51aecced21537
Lukas742 <lukas.harbarth@sap.com>
2024-03-26 08:39:33 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@f89f0b88fa76240d4bd388c79af9b337f99429af 🚀

---
+++ main/assets/Bar.stories-BBL1JJSx.js
@@ -0,0 +1,20 @@
+import{j as t}from"./jsx-runtime-Du8NFWEI.js";import{G as l}from"./GitHub-Mark-EHn_tOX6.js";import{n as u}from"./nav-back-C9apjA2a.js";import{B as m,a as d}from"./index-CkcTpsXX.js";import{B as g}from"./Button-D5VmNntQ.js";import{B as h}from"./index-BZHlLZeN.js";import{I as x}from"./index-Cyn66iQs.js";import{L as f}from"./index-BJY2JDrh.js";import{T as b}from"./index-BWxR7zDR.js";const j={title:"Layouts & Floorplans / Bar",component:m,argTypes:{startContent:{control:{disable:!0}},children:{control:{disable:!0}},endContent:{control:{disable:!0}}},args:{design:d.Header,startContent:t.jsx("span",{children:"Start Content"}),children:t.jsx("span",{children:"Center Content"}),endContent:t.jsx("span",{children:"End Content"})},tags:["package:@ui5/webcomponents-fiori"]},e={},n={render(p){return t.jsx(m,{...p,startContent:t.jsxs(t.Fragment,{children:[t.jsx(h,{icon:u,design:g.Transparent}),t.jsx("img",{src:"https://raw.githubusercontent.com/SAP/ui5-webcomponents-react/main/assets/Logo.png",alt:"logo",style:{marginLeft:"6px",width:"120px"}})]}),endContent:t.jsxs(t.Fragment,{children:[t.jsx(x,{placeholder:"Search"}),t.jsx(f,{style:{marginLeft:"6px"},href:"https://github.com/SAP/ui5-webcomponents-react",target:"_blank",children:t.jsx(l,{})})]}),children:t.jsx(b,{children:"Bar"})})}};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var a,i,c;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render(args) {
+    return <Bar {...args} startContent={<>
+            <Button icon={navBackIcon} design={ButtonDesign.Transparent} />
+            <img src="https://raw.githubusercontent.com/SAP/ui5-webcomponents-react/main/assets/Logo.png" alt="logo" style={{
+        marginLeft: '6px',
+        width: '120px'
+      }} />
+          </>} endContent={<>
+            <Input placeholder="Search" />
+            <Link style={{
+        marginLeft: '6px'
+      }} href="https://github.com/SAP/ui5-webcomponents-react" target="_blank">
+              <GitHubLogo />
+            </Link>
+          </>}>
+        <Title>Bar</Title>
+      </Bar>;
+  }
+}`,...(c=(i=n.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const B=["Default","WithCustomElements"],E=Object.freeze(Object.defineProperty({__proto__:null,Default:e,WithCustomElements:n,__namedExportsOrder:B,default:j},Symbol.toStringTag,{value:"Module"}));export{E as C,e as D,n as W};
---
+++ main/assets/preview-KtwDg0hr.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-K4EAMTCU-DWRaOM-n.js","./chunk-HLWAVYOI-BfTOysne.js","./iframe-Cq6LHcvt.js","./index-Dl6G-zuu.js","./react-18-Bamk9FSf.js","./index-D1_ZHIBm.js","./index-Cx6gsX_3.js","./inheritsLoose-D8L2VVdp.js","./_baseForOwn-DFjKyXZi.js","./mapValues-BvzalXkB.js","./_baseUniq-DRxb5hvO.js","./index-2V9fy1S2.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./iframe-Cq6LHcvt.js";import"../sb-preview/runtime.js";const{global:s}=__STORYBOOK_MODULE_GLOBAL__;var _=Object.entries(s.TAGS_OPTIONS??{}).reduce((e,r)=>{let[t,o]=r;return o.excludeFromDocsStories&&(e[t]=!0),e},{}),d={docs:{renderer:async()=>{let{DocsRenderer:e}=await a(()=>import("./DocsRenderer-K4EAMTCU-DWRaOM-n.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12]),import.meta.url);return new e},stories:{filter:e=>{var r;return(e.tags||[]).filter(t=>_[t]).length===0&&!((r=e.parameters.docs)!=null&&r.disable)}}}};export{d as parameters};
