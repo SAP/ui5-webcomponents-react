d8aa3c96ed36cbcc169e8eef4a70190625585558
Lukas Harbarth <lukas.harbarth@sap.com>
2024-04-03 12:25:46 +0200

    docs(Icon): fix component description in storybook (#5655)

--- packages/main/src/webComponents/Icon/Icon.stories.tsx
+++ packages/main/src/webComponents/Icon/Icon.stories.tsx
@@ -1,6 +1,17 @@
+import cem from '@sb/custom-element-manifests/main.json';
 import type { Meta, StoryObj } from '@storybook/react';
 import employeeIcon from '@ui5/webcomponents-icons/dist/employee.js';
 import { Icon } from './index.js';
+
+//todo: remove custom description once JSDoc transforming is fixed
+const description = cem.modules
+  .find((item) => item.path === 'dist/Icon.js')
+  .declarations.find((item) => item.kind === 'class').description;
+
+function iconSummaryFormatter(htmlDesc: string) {
+  const summary = htmlDesc.replace(/###\s?Overview\n*/, '').replace(/### ES6 Module Import\n*`.+`/, '');
+  return summary.replaceAll('ui5-icon', 'Icon');
+}
 
 const meta = {
   title: 'Data Display / Icon',
@@ -8,6 +19,15 @@ const meta = {
   argTypes: {},
   args: {
     name: employeeIcon
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          iconSummaryFormatter(description) +
+          '\n__Note:__ This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/playground/)'
+      }
+    }
   },
   tags: ['package:@ui5/webcomponents']
 } satisfies Meta<typeof Icon>;
