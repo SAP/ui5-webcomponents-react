db3377ebc0e3b6b90b636c953e32107071f1568c
Lukas742 <lukas.harbarth@sap.com>
2024-04-04 06:44:23 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@bb66f312a9c5156fe30459f20bc8ac2b25aac379 🚀

---
+++ main/assets/ColorPalettePopover-DNHoSyBz.js
@@ -0,0 +1,30 @@
+import{j as o}from"./jsx-runtime-Du8NFWEI.js";import{useMDXComponents as m}from"./index-_byTH_QA.js";import{F as n}from"./ProjectTemplate-DJdE26-Z.js";import{C as s}from"./ControlsWithNote-Sm_wlCFP.js";import{D as a}from"./DocsHeader-DDvSJf4B.js";import"./index-Dl6G-zuu.js";import{M as l,d as c,e as d,f as h,g as x}from"./index-DlvU7JkR.js";import{C as j,D as e}from"./ColorPalettePopover.stories-Dr6Oupf7.js";import{S as f}from"./SubcomponentsSection-BVy4lpQe.js";import{C as i}from"./index-DrEcrtC0.js";import"./react-jss.esm-BPPChJW9.js";import"./inheritsLoose-D8L2VVdp.js";import"./theming.esm-5daRW75o.js";import"./index-BEogAxus.js";import"./iframe-aoKFBcy-.js";import"../sb-preview/runtime.js";import"./UI5Element-TZfj5eGG.js";import"./Boot-CiW9Yqx0.js";import"./VersionInfo-BESRVDo6.js";import"./CustomElementsScopeUtils-Brq83ayG.js";import"./withWebComponent-WdPEUVG_.js";import"./utils-B7AYZOSV.js";import"./useIsomorphicLayoutEffect-gsbYElqV.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-C2WczoLt.js";import"./decline-D6EEqVrc.js";import"./Icons-Bz_R9SHk.js";import"./i18n-defaults-BdfZArTM.js";import"./information-DW1Al7d0.js";import"./alert-CXHMnQvw.js";import"./class-map-DdS6yb0u.js";import"./Icon-CItvJRAI.js";import"./Keys-BiUfzlGt.js";import"./parameters-bundle.css-B6zgRRib.js";import"./Button-D5VmNntQ.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-weVOdquz.js";import"./chunk-HLWAVYOI-D_lbESS3.js";import"./react-18-Bamk9FSf.js";import"./index-D1_ZHIBm.js";import"./index-loc45roa.js";import"./clsx-B-dksMZM.js";import"./index-DaRmuT3k.js";import"./Label-Cuq9lQQu.js";import"./WrappingType-CW8URInd.js";import"./index-CQE2WSJR.js";import"./index-YgL8IrBJ.js";import"./Link-ShECJtRl.js";import"./index-CqQR8_Yl.js";import"./Popover-BZGRcTIp.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-FBsPN5sA.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-o-m9Y-i7.js";import"./FocusableElements-DLk5IaaK.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DDqpi34j.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-CSZX4I-3.js";import"./BrowserScrollbar.css-ByCTonOa.js";import"./index-CYjHeYtF.js";import"./i18n-defaults-FgEXIKeH.js";import"./I18nContext-BcGksm2n.js";import"./ThemingParameters--AGRs54p.js";import"./index-DvBQI2Vb.js";import"./useStylesheet-Cs3TLZWO.js";import"./ModalsContext-qaxSwC3j.js";import"./addCustomCSSWithScoping-CohoEJjR.js";import"./index-DXpHqzG4.js";import"./index-CrQT923E.js";import"./Avatar-BCyNq7eW.js";import"./employee-BRu8U1FM.js";import"./index-CC_AR8Lc.js";import"./utils-C78OPIsa.js";import"./main-0kl1aPpa.js";import"./index-59fucLxF.js";import"./sys-help-2-DXsgVRAc.js";import"./copy-ChpJf3sv.js";import"./GitHub-Mark-EHn_tOX6.js";import"./TableOfContent-7iRTACMe.js";import"./_baseForOwn-DFjKyXZi.js";import"./mapValues-BvzalXkB.js";import"./_baseUniq-DRxb5hvO.js";import"./index-2V9fy1S2.js";import"./index-DrFu-skq.js";import"./CSSColor-CzaElTOH.js";import"./ResponsivePopoverCommon.css-C4BkpW0K.js";import"./Title-BQHA4R7T.js";import"./ResponsivePopover-BUXZzlJ3.js";import"./Dialog-CU_OHoNP.js";import"./ValueState-Bn-H2OaL.js";import"./ItemNavigation-BjKsRQtS.js";function p(r){const t={code:"code",em:"em",h2:"h2",p:"p",pre:"pre",strong:"strong",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(l,{of:j}),`
+`,o.jsx(a,{since:"0.20.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(c,{of:e}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:e}),`
+`,o.jsx(t.h2,{id:"opening-colorpalettepopovers",children:"Opening ColorPalettePopovers"}),`
+`,o.jsxs(t.p,{children:[o.jsx(t.code,{children:"ColorPalettePopover"}),"s can only be opened by attaching a ",o.jsx(t.code,{children:"ref"})," to the component and then call the corresponding ",o.jsx(t.strong,{children:o.jsx(t.code,{children:"showAt"})})," method. The method receives the target element - ",o.jsxs(t.em,{children:["on which the ",o.jsx(t.code,{children:"ColorPalettePopover"})," is to be opened"]})," - as parameter."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const ColorPalettePopoverComponent = () => {
+  const popoverRef = useRef();
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open ColorPalettePopover</Button>
+      <ColorPalettePopover ref={popoverRef}>
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="darkblue" />
+      </ColorPalettePopover>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(d,{children:f}),`
+`,o.jsx(t.h2,{id:"colorpaletteitem",children:"ColorPaletteItem"}),`
+`,o.jsx(h,{of:i}),`
+`,o.jsx(x,{of:i}),`
+`,o.jsx(n,{})]})}function nt(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(p,{...r})}):p(r)}export{nt as default};
---
+++ main/assets/Toast-CArZeS08.js
@@ -0,0 +1,27 @@
+import{j as o}from"./jsx-runtime-Du8NFWEI.js";import{useMDXComponents as p}from"./index-_byTH_QA.js";import{F as e}from"./ProjectTemplate-DJdE26-Z.js";import{C as n}from"./ControlsWithNote-Sm_wlCFP.js";import{D as s}from"./DocsHeader-DDvSJf4B.js";import"./index-Dl6G-zuu.js";import{M as a,d as c}from"./index-DlvU7JkR.js";import{C as h,D as i}from"./Toast.stories-Dq3eyGcx.js";import"./react-jss.esm-BPPChJW9.js";import"./inheritsLoose-D8L2VVdp.js";import"./theming.esm-5daRW75o.js";import"./index-BEogAxus.js";import"./iframe-aoKFBcy-.js";import"../sb-preview/runtime.js";import"./UI5Element-TZfj5eGG.js";import"./Boot-CiW9Yqx0.js";import"./VersionInfo-BESRVDo6.js";import"./CustomElementsScopeUtils-Brq83ayG.js";import"./withWebComponent-WdPEUVG_.js";import"./utils-B7AYZOSV.js";import"./useIsomorphicLayoutEffect-gsbYElqV.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-C2WczoLt.js";import"./decline-D6EEqVrc.js";import"./Icons-Bz_R9SHk.js";import"./i18n-defaults-BdfZArTM.js";import"./information-DW1Al7d0.js";import"./alert-CXHMnQvw.js";import"./class-map-DdS6yb0u.js";import"./Icon-CItvJRAI.js";import"./Keys-BiUfzlGt.js";import"./parameters-bundle.css-B6zgRRib.js";import"./Button-D5VmNntQ.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-weVOdquz.js";import"./chunk-HLWAVYOI-D_lbESS3.js";import"./react-18-Bamk9FSf.js";import"./index-D1_ZHIBm.js";import"./index-loc45roa.js";import"./clsx-B-dksMZM.js";import"./index-DaRmuT3k.js";import"./Label-Cuq9lQQu.js";import"./WrappingType-CW8URInd.js";import"./index-CQE2WSJR.js";import"./index-YgL8IrBJ.js";import"./Link-ShECJtRl.js";import"./index-CqQR8_Yl.js";import"./Popover-BZGRcTIp.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-FBsPN5sA.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-o-m9Y-i7.js";import"./FocusableElements-DLk5IaaK.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DDqpi34j.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-CSZX4I-3.js";import"./BrowserScrollbar.css-ByCTonOa.js";import"./index-CYjHeYtF.js";import"./i18n-defaults-FgEXIKeH.js";import"./I18nContext-BcGksm2n.js";import"./ThemingParameters--AGRs54p.js";import"./index-DvBQI2Vb.js";import"./useStylesheet-Cs3TLZWO.js";import"./ModalsContext-qaxSwC3j.js";import"./addCustomCSSWithScoping-CohoEJjR.js";import"./index-DXpHqzG4.js";import"./index-CrQT923E.js";import"./Avatar-BCyNq7eW.js";import"./employee-BRu8U1FM.js";import"./index-CC_AR8Lc.js";import"./utils-C78OPIsa.js";import"./main-0kl1aPpa.js";import"./index-59fucLxF.js";import"./sys-help-2-DXsgVRAc.js";import"./copy-ChpJf3sv.js";import"./GitHub-Mark-EHn_tOX6.js";import"./TableOfContent-7iRTACMe.js";import"./_baseForOwn-DFjKyXZi.js";import"./mapValues-BvzalXkB.js";import"./_baseUniq-DRxb5hvO.js";import"./index-2V9fy1S2.js";import"./index-DrFu-skq.js";import"./index-DW2-zqw-.js";function m(r){const t={code:"code",h2:"h2",p:"p",pre:"pre",strong:"strong",...p(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{of:h}),`
+`,o.jsx(s,{since:"0.9.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(c,{of:i}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(t.h2,{id:"show-a-toast",children:"Show a toast"}),`
+`,o.jsxs(t.p,{children:["The ",o.jsx(t.code,{children:"Toast"}),` component has an imperative API for getting displayed. It will not be displayed just because it is part of the DOM.
+In order to show the Toast, you have to get a reference to the `,o.jsx(t.code,{children:"Toast"})," DOM element and call the ",o.jsx(t.code,{children:"show"}),`-method.
+You can either access the DOM element by using a React `,o.jsx(t.code,{children:"ref"})," or work with IDs."]}),`
+`,o.jsx(t.p,{children:o.jsx(t.strong,{children:"Example"})}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`export const MyComponentWithAToast() {
+  const toast = useRef(null);
+
+  const showToast = () => {
+    toast.current.show();
+  };
+  return (
+    <ThemeProvider>
+      <Button onClick={showToast}>Show Toast</Button>
+      <Toast ref={toast}>This is my Toast!</Toast>
+    </ThemeProvider>
+  );
+}
+`})}),`
+`,o.jsx(e,{})]})}function Ko(r={}){const{wrapper:t}={...p(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(m,{...r})}):m(r)}export{Ko as default};
