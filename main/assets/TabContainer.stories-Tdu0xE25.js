30de4d05502d6baee7b190f3d6ad6da5a65de4aa
Lukas742 <lukas.harbarth@sap.com>
2024-04-08 10:08:54 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0b24c5013f40933d7955dc5921d5a530a2e4fe94 🚀

---
+++ main/assets/TabContainer.stories-Tdu0xE25.js
@@ -0,0 +1,66 @@
+import{j as e}from"./jsx-runtime-Du8NFWEI.js";import{r as u,k as O}from"./Boot-CiW9Yqx0.js";import{r as h}from"./Icons-Bz_R9SHk.js";import{a as W}from"./add-CE_F4grT.js";import{e as B}from"./employee-BRu8U1FM.js";import{s as G}from"./settings-DltrQ7V8.js";import{b as H,a as f,c as J,d as q,T as t}from"./index-DHonDfO1.js";import{_ as K}from"./iframe-Dw6-Txvb.js";import{U as Q,e as S}from"./UI5Element-TZfj5eGG.js";import{a as _,l as x,b as w,s as F,c as X,f as Y,w as Z}from"./withWebComponent-WdPEUVG_.js";import{o as y}from"./class-map-DdS6yb0u.js";import{s as v}from"./style-map-CSZX4I-3.js";import{s as M}from"./parameters-bundle.css-B6zgRRib.js";function ee(a,n,o){return _`<div id="${x(this._id)}" role="separator" class="${y(this.classes.root)}"></div>`}function te(a,n,o){return o?_`<${w("ui5-li-custom",n,o)} id="${x(this._id)}" role="separator" class="${y(this.classes.root)}" disabled style="${v(this._style)}"></${w("ui5-li-custom",n,o)}>`:_`<ui5-li-custom id="${x(this._id)}" role="separator" class="${y(this.classes.root)}" disabled style="${v(this._style)}"></ui5-li-custom>`}u("@ui5/webcomponents-theming","sap_horizon",async()=>F);u("@ui5/webcomponents","sap_horizon",async()=>M);const ae={packageName:"@ui5/webcomponents",fileName:"themes/TabSeparatorInStrip.css.ts",content:`.ui5-tc__separator{width:0;border-left:.0625rem solid var(--sapGroup_TitleBorderColor);margin:.5rem .25rem}
+`};u("@ui5/webcomponents-theming","sap_horizon",async()=>F);u("@ui5/webcomponents","sap_horizon",async()=>M);const ne={packageName:"@ui5/webcomponents",fileName:"themes/TabSeparatorInOverflow.css.ts",content:`.ui5-tc__separator{min-height:.25rem;border-bottom:.0625rem solid var(--sapGroup_TitleBorderColor);margin-inline-start:calc(var(--_ui5-v1-23-1-tab-indentation-level) * .5rem);margin-inline-end:.5rem}[ui5-list]>.ui5-tc__separator:first-child{min-height:.5rem}
+`};var oe=function(a,n,o,m){var p=arguments.length,r=p<3?n:m===null?m=Object.getOwnPropertyDescriptor(n,o):m,b;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")r=Reflect.decorate(a,n,o,m);else for(var g=a.length-1;g>=0;g--)(b=a[g])&&(r=(p<3?b(r):p>3?b(n,o,r):b(n,o))||r);return p>3&&r&&Object.defineProperty(n,o,r),r},T;let d=T=class extends Q{static get stripTemplate(){return ee}static get overflowTemplate(){return te}get classes(){return{root:{"ui5-tc__separator":!0}}}get isSeparator(){return!0}getTabInStripDomRef(){return this.getElementInStrip?this.getElementInStrip():null}get stableDomRef(){return this.getAttribute("stable-dom-ref")||`${this._id}-stable-dom-ref`}get stripPresentation(){return S(T.stripTemplate,this)}get overflowPresentation(){return S(T.overflowTemplate,this)}};d=T=oe([X({tag:"ui5-tab-separator",renderer:Y})],d);d.define();H.registerTabStyles(ae);H.registerStaticAreaTabStyles(ne);const re=d,se=Object.freeze(Object.defineProperty({__proto__:null,default:re},Symbol.toStringTag,{value:"Module"})),s=Z("ui5-tab-separator",[],[],[],[],()=>K(()=>Promise.resolve().then(()=>se),void 0,import.meta.url));s.displayName="TabSeparator";try{s.displayName="TabSeparator",s.__docgenInfo={description:"The `TabSeparator` represents a vertical line to separate tabs inside a `TabContainer`.\n\n__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/playground/)",displayName:"TabSeparator",props:{style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}try{s.displayName="TabSeparator",s.__docgenInfo={description:"The `TabSeparator` represents a vertical line to separate tabs inside a `TabContainer`.\n\n__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/playground/)",displayName:"TabSeparator",props:{style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}const ie="activities",E="M480 32q13 0 22.5 9t9.5 23v384q0 13-9.5 22.5T480 480H32q-14 0-23-9.5T0 448V64q0-14 9-23t23-9h448zm0 32H32v384h448V64zM117 252l-53-53 17-19 36 36 71-88 18 17zm0 164l-53-54 17-18 36 36 71-89 18 18zm283-96q16 0 16 16 0 6-4.5 11t-11.5 5H272q-6 0-11-5t-5-11q0-7 5-11.5t11-4.5h128zm0-160q16 0 16 16 0 6-4.5 11t-11.5 5H272q-6 0-11-5t-5-11q0-7 5-11.5t11-4.5h128z",ce=!0,le="SAP-icons-v4",me="@ui5/webcomponents-icons";h(ie,{pathData:E,ltr:ce,collection:le,packageName:me});const pe="activities",L="M181 71q7-7 17-7 11 0 18.5 7.5T224 90q0 10-8 18L101 217q-7 7-17 7-11 0-18-7L8 162q-8-8-8-19t7.5-18 18.5-7q10 0 17 7l41 38zm133 89q-11 0-18.5-7.5T288 134t7.5-18 18.5-7h172q11 0 18.5 7t7.5 18-7.5 18.5T486 160H314zM181 295q7-7 17-7 11 0 18.5 7.5T224 314q0 10-8 18L101 441q-7 7-17 7-11 0-18-7L8 386q-8-8-8-19t7.5-18 18.5-7q10 0 17 7l41 38zm305 38q11 0 18.5 7t7.5 18-7.5 18.5T486 384H314q-11 0-18.5-7.5T288 358t7.5-18 18.5-7h172z",be=!0,Te="SAP-icons-v5",de="@ui5/webcomponents-icons";h(pe,{pathData:L,ltr:be,collection:Te,packageName:de});O();const R="activities",ue="menu",A="M32 64q14 0 23 9.5T64 96q0 14-9 23t-23 9q-13 0-22.5-9T0 96q0-13 9.5-22.5T32 64zm0 160q14 0 23 9.5t9 22.5q0 14-9 23t-23 9q-13 0-22.5-9T0 256q0-13 9.5-22.5T32 224zm0 160q14 0 23 9.5t9 22.5q0 14-9 23t-23 9q-13 0-22.5-9T0 416q0-13 9.5-22.5T32 384zm128-255q-13 0-22.5-9.5T128 97q0-14 9.5-23t22.5-9h320q14 0 23 9t9 23q0 13-9 22.5t-23 9.5H160zm0 159q-13 0-22.5-9.5T128 256q0-14 9.5-23t22.5-9h320q14 0 23 9t9 23q0 13-9 22.5t-23 9.5H160zm0 159q-13 0-22.5-9.5T128 415q0-14 9.5-23t22.5-9h320q14 0 23 9t9 23q0 13-9 22.5t-23 9.5H160z",he=!1,fe="SAP-icons-v4",ge="@ui5/webcomponents-icons";h(ue,{pathData:A,ltr:he,collection:fe,packageName:ge});const _e="menu",V="M48 64q20 0 34 14t14 34-14 34-34 14-34-14-14-34 14-34 34-14zm438 64H186q-11 0-18.5-7.5T160 102t7.5-18 18.5-7h300q11 0 18.5 7t7.5 18-7.5 18.5T486 128zM48 208q20 0 34 14t14 34-14 34-34 14-34-14-14-34 14-34 34-14zm438 80H186q-11 0-18.5-7.5T160 262t7.5-18 18.5-7h300q11 0 18.5 7t7.5 18-7.5 18.5T486 288zM48 352q20 0 34 14t14 34-14 34-34 14-34-14-14-34 14-34 34-14zm438 83H186q-11 0-18.5-7t-7.5-18 7.5-18.5T186 384h300q11 0 18.5 7.5T512 410t-7.5 18-18.5 7z",xe=!1,ye="SAP-icons-v5",qe="@ui5/webcomponents-icons";h(_e,{pathData:V,ltr:xe,collection:ye,packageName:qe});O();const U="menu",Se={title:"Layouts & Floorplans / TabContainer",component:f,argTypes:{children:{control:{disable:!0}},overflowButton:{control:{disable:!0}}},args:{tabLayout:J.Standard,contentBackgroundDesign:q.Solid,headerBackgroundDesign:q.Solid},tags:["package:@ui5/webcomponents"]},i={render:a=>e.jsxs(f,{...a,children:[e.jsx(t,{text:"Tab One",icon:U,selected:!0,additionalText:"5",children:"Content Tab 1"}),e.jsx(t,{text:"Tab Two",icon:R,additionalText:"20",children:"Content Tab 2"}),e.jsx(t,{text:"Tab Three",icon:W,children:"Content Tab 3"}),e.jsx(t,{text:"Tab Four",icon:B,children:"Content Tab 4"}),e.jsx(t,{text:"Tab Five",icon:G,children:"Content Tab 5"})]})},c={name:"with TabSeparator",render:a=>e.jsxs(f,{...a,children:[e.jsx(t,{text:"Tab One",icon:U,selected:!0,additionalText:"5",children:"Content Tab 1"}),e.jsx(s,{}),e.jsx(t,{text:"Tab Two",icon:R,additionalText:"20",children:"Content Tab 2"})]})},l={name:"with nested tabs",render:a=>e.jsxs(f,{...a,children:[e.jsx(t,{text:"Tab One",selected:!0,subTabs:e.jsxs(e.Fragment,{children:[e.jsx(t,{text:"Tab 1.1"}),e.jsx(t,{text:"Tab 1.2"}),e.jsx(t,{text:"Tab 1.3"})]}),children:e.jsx("div",{style:{display:"none"}})}),e.jsx(s,{}),e.jsx(t,{text:"Tab Two"}),e.jsx(t,{text:"Tab Three",subTabs:e.jsxs(e.Fragment,{children:[e.jsx(t,{text:"Tab 3.1"}),e.jsx(t,{text:"Tab 3.2"}),e.jsx(t,{text:"Tab 3.3"})]})})]})};var C,$,j;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: args => {
+    return <TabContainer {...args}>
+        <Tab text="Tab One" icon={menuIcon} selected additionalText="5">
+          Content Tab 1
+        </Tab>
+        <Tab text="Tab Two" icon={activitiesIcon} additionalText="20">
+          Content Tab 2
+        </Tab>
+        <Tab text="Tab Three" icon={addIcon}>
+          Content Tab 3
+        </Tab>
+        <Tab text="Tab Four" icon={employeeIcon}>
+          Content Tab 4
+        </Tab>
+        <Tab text="Tab Five" icon={settingsIcon}>
+          Content Tab 5
+        </Tab>
+      </TabContainer>;
+  }
+}`,...(j=($=i.parameters)==null?void 0:$.docs)==null?void 0:j.source}}};var D,z,N;c.parameters={...c.parameters,docs:{...(D=c.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  name: 'with TabSeparator',
+  render: args => {
+    return <TabContainer {...args}>
+        <Tab text="Tab One" icon={menuIcon} selected additionalText="5">
+          Content Tab 1
+        </Tab>
+        <TabSeparator />
+        <Tab text="Tab Two" icon={activitiesIcon} additionalText="20">
+          Content Tab 2
+        </Tab>
+      </TabContainer>;
+  }
+}`,...(N=(z=c.parameters)==null?void 0:z.docs)==null?void 0:N.source}}};var I,P,k;l.parameters={...l.parameters,docs:{...(I=l.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  name: 'with nested tabs',
+  render: args => {
+    return <TabContainer {...args}>
+        <Tab text="Tab One" selected subTabs={<>
+              <Tab text="Tab 1.1" />
+              <Tab text="Tab 1.2" />
+              <Tab text="Tab 1.3" />
+            </>}>
+          <div style={{
+          display: 'none'
+        }} />
+        </Tab>
+        <TabSeparator />
+        <Tab text="Tab Two" />
+        <Tab text="Tab Three" subTabs={<>
+              <Tab text="Tab 3.1" />
+              <Tab text="Tab 3.2" />
+              <Tab text="Tab 3.3" />
+            </>} />
+      </TabContainer>;
+  }
+}`,...(k=(P=l.parameters)==null?void 0:P.docs)==null?void 0:k.source}}};const we=["Default","WithTabSeparator","WithNestedTabs"],Ee=Object.freeze(Object.defineProperty({__proto__:null,Default:i,WithNestedTabs:l,WithTabSeparator:c,__namedExportsOrder:we,default:Se},Symbol.toStringTag,{value:"Module"}));export{Ee as C,i as D,s as T,c as W,l as a};
