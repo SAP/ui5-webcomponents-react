303dff873be5f0cb51ed92b153337c082636a8cd
MarcusNotheis <marcus.notheis@sap.com>
2024-04-11 10:11:25 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@5724f4328dc78c45e1d5ded86fe169a2240153a4 🚀

---
+++ main/assets/Loader-B24k5Yks.js
@@ -0,0 +1,49 @@
+import{j as t}from"./jsx-runtime-Du8NFWEI.js";import{useMDXComponents as m}from"./index-_byTH_QA.js";import{F as p}from"./ProjectTemplate-DBF1NYEx.js";import{C as a}from"./ControlsWithNote-BT0Q8NQu.js";import{D as s}from"./DocsHeader-CUWgSrYe.js";import"./index-Dl6G-zuu.js";import{M as d,d as r}from"./index-0daxby7w.js";import{C as l,D as e,W as x}from"./Loader.stories-DPQYDP_d.js";import"./react-jss.esm-BPPChJW9.js";import"./inheritsLoose-D8L2VVdp.js";import"./theming.esm-5daRW75o.js";import"./index-1hqexyoD.js";import"./iframe-D2SKHQ1w.js";import"../sb-preview/runtime.js";import"./UI5Element-Lw3nTtTN.js";import"./Boot-B0Gt2ObV.js";import"./ManagedStyles-DX5mlBXq.js";import"./CustomElementsScopeUtils-CeSGtO6z.js";import"./withWebComponent-zM8JcmxY.js";import"./utils-syHHPmsO.js";import"./useIsomorphicLayoutEffect-gsbYElqV.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-Duvqe7kG.js";import"./decline-TOiDlE0p.js";import"./Icons-DfCrNAdQ.js";import"./i18n-defaults-CFK4Zk7G.js";import"./information-ooFgY1vE.js";import"./alert-D4nm4k8k.js";import"./class-map-mIG3ftuK.js";import"./Icon-Dbo8BIv6.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-DjgCrOcY.js";import"./Button-BFndOBlc.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-D1sujP0E.js";import"./chunk-HLWAVYOI-CIUj62O3.js";import"./react-18-Bamk9FSf.js";import"./index-D1_ZHIBm.js";import"./index-t2jtc2K4.js";import"./clsx-B-dksMZM.js";import"./index-CF62uxHK.js";import"./Label-D6k6fzJu.js";import"./WrappingType-CW8URInd.js";import"./index-BrR_n-VZ.js";import"./index-DAkycPzA.js";import"./Link-G_0GWmft.js";import"./index-6BqiAAh_.js";import"./Popover-B7UMWr5A.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-DmBeTAQh.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BmjVxBVQ.js";import"./FocusableElements-4YGFH9ta.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-OFvM0oAF.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-CweQHFZo.js";import"./BrowserScrollbar.css-B3MoNUKh.js";import"./index-D9yM4vYh.js";import"./i18n-defaults-Ck1tOeoE.js";import"./I18nContext-BcGksm2n.js";import"./ThemingParameters--AGRs54p.js";import"./index-BPVTr8FI.js";import"./useStylesheet-D5wp95d6.js";import"./ModalsContext-qaxSwC3j.js";import"./addCustomCSSWithScoping-de5_lUMj.js";import"./index-clvnWEwH.js";import"./index-VObdDDVS.js";import"./Avatar-CHcvLeex.js";import"./employee-C3y4a2zL.js";import"./index-BRYwhfJF.js";import"./utils-BBzp-JFy.js";import"./main-HWtSBowW.js";import"./index-5JJt2HP_.js";import"./sys-help-2-Dp_ZH8Yt.js";import"./copy-Dz4oTMJU.js";import"./GitHub-Mark-EHn_tOX6.js";import"./TableOfContent-CzwzJjf-.js";import"./_baseForOwn-DFjKyXZi.js";import"./mapValues-BvzalXkB.js";import"./_baseUniq-DRxb5hvO.js";import"./index-2V9fy1S2.js";import"./index-DrFu-skq.js";import"./spacing-Bgb-YkCp.js";import"./index-CD83aP3T.js";import"./index-BORzHB6p.js";function n(i){const o={code:"code",h1:"h1",h2:"h2",pre:"pre",...m(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(d,{of:l}),`
+`,t.jsx(s,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(r,{of:e}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{of:e}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"card-with-loader",children:"Card with Loader"}),`
+`,t.jsx(r,{of:x}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`const LoaderComponent = () => {
+  const initialText =
+    'This is a text that will be updated 5 seconds after the header is clicked. The text will be visible until the new text has been fetched. To let the user know that the text is updating, the Loader will be displayed.';
+  const [text, setText] = useState(initialText);
+  const [loading, setLoading] = useState(false);
+  const onHeaderClick = () => {
+    setLoading(true);
+  };
+  useEffect(() => {
+    if (loading) {
+      setTimeout(() => {
+        setText((prev) => (prev === initialText ? 'Updated (⌐■_■)' : initialText));
+        setLoading(false);
+      }, 5000);
+    }
+  }, [loading]);
+  return (
+    <Card
+      header={
+        <CardHeader
+          titleText="Click the header to update the text below."
+          interactive
+          avatar={<Icon name="activate" />}
+          onClick={onHeaderClick}
+        />
+      }
+      style={{ width: '400px' }}
+    >
+      <FlexBox direction={FlexBoxDirection.Column}>
+        {loading && <Loader />}
+        <Text style={spacing.sapUiContentPadding}>{text}</Text>
+      </FlexBox>
+    </Card>
+  );
+};
+`})}),`
+`,t.jsx(p,{})]})}function Vt(i={}){const{wrapper:o}={...m(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(n,{...i})}):n(i)}export{Vt as default};
---
+++ main/assets/Tree-Cexraaf3.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-Du8NFWEI.js";import{useMDXComponents as d}from"./index-_byTH_QA.js";import{A as o,F as l}from"./ProjectTemplate-DBF1NYEx.js";import{C as c}from"./ControlsWithNote-BT0Q8NQu.js";import{D as h}from"./DocsHeader-CUWgSrYe.js";import"./index-Dl6G-zuu.js";import{M as x,d as i,e as j,f as m}from"./index-0daxby7w.js";import{S as T}from"./SubcomponentsSection-BVy4lpQe.js";import{C as f,D as n,L as u,T as p,a as s}from"./Tree.stories-UiaIek2R.js";import"./react-jss.esm-BPPChJW9.js";import"./inheritsLoose-D8L2VVdp.js";import"./theming.esm-5daRW75o.js";import"./index-1hqexyoD.js";import"./iframe-D2SKHQ1w.js";import"../sb-preview/runtime.js";import"./UI5Element-Lw3nTtTN.js";import"./Boot-B0Gt2ObV.js";import"./ManagedStyles-DX5mlBXq.js";import"./CustomElementsScopeUtils-CeSGtO6z.js";import"./withWebComponent-zM8JcmxY.js";import"./utils-syHHPmsO.js";import"./useIsomorphicLayoutEffect-gsbYElqV.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-Duvqe7kG.js";import"./decline-TOiDlE0p.js";import"./Icons-DfCrNAdQ.js";import"./i18n-defaults-CFK4Zk7G.js";import"./information-ooFgY1vE.js";import"./alert-D4nm4k8k.js";import"./class-map-mIG3ftuK.js";import"./Icon-Dbo8BIv6.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-DjgCrOcY.js";import"./Button-BFndOBlc.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-D1sujP0E.js";import"./chunk-HLWAVYOI-CIUj62O3.js";import"./react-18-Bamk9FSf.js";import"./index-D1_ZHIBm.js";import"./index-t2jtc2K4.js";import"./clsx-B-dksMZM.js";import"./index-CF62uxHK.js";import"./Label-D6k6fzJu.js";import"./WrappingType-CW8URInd.js";import"./index-BrR_n-VZ.js";import"./index-DAkycPzA.js";import"./Link-G_0GWmft.js";import"./index-6BqiAAh_.js";import"./Popover-B7UMWr5A.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-DmBeTAQh.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BmjVxBVQ.js";import"./FocusableElements-4YGFH9ta.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-OFvM0oAF.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-CweQHFZo.js";import"./BrowserScrollbar.css-B3MoNUKh.js";import"./index-D9yM4vYh.js";import"./i18n-defaults-Ck1tOeoE.js";import"./I18nContext-BcGksm2n.js";import"./ThemingParameters--AGRs54p.js";import"./index-BPVTr8FI.js";import"./useStylesheet-D5wp95d6.js";import"./ModalsContext-qaxSwC3j.js";import"./addCustomCSSWithScoping-de5_lUMj.js";import"./index-clvnWEwH.js";import"./index-VObdDDVS.js";import"./Avatar-CHcvLeex.js";import"./employee-C3y4a2zL.js";import"./index-BRYwhfJF.js";import"./utils-BBzp-JFy.js";import"./main-HWtSBowW.js";import"./index-5JJt2HP_.js";import"./sys-help-2-Dp_ZH8Yt.js";import"./copy-Dz4oTMJU.js";import"./GitHub-Mark-EHn_tOX6.js";import"./TableOfContent-CzwzJjf-.js";import"./_baseForOwn-DFjKyXZi.js";import"./mapValues-BvzalXkB.js";import"./_baseUniq-DRxb5hvO.js";import"./index-2V9fy1S2.js";import"./index-DrFu-skq.js";import"./general-leave-request-CJPP3Rr_.js";import"./index-DMUQ7KMV.js";import"./BusyIndicator-C42nFNla.js";import"./index-CD83aP3T.js";import"./ValueState-Bn-H2OaL.js";import"./ListItem-nzbFALtl.js";import"./List-CgPFTYVO.js";import"./ItemNavigation-D6hdzFqV.js";import"./TabbableElements-Ds_LZU0v.js";import"./debounce-jW346lN3.js";import"./CheckBox-BbNurf_2.js";import"./accept-CVuU4iIn.js";import"./RadioButton-CEFXfY4T.js";import"./slim-arrow-right-CIeoJHfT.js";import"./navigation-right-arrow-B-ihvrfB.js";import"./navigation-down-arrow-ChfUg1-f.js";function a(e){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...d(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(x,{of:f}),`
+`,t.jsx(h,{since:"0.10.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(i,{of:n}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(c,{of:n}),`
+`,t.jsx(r.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"lazy-load-treeitems",children:"Lazy Load TreeItems"}),`
+`,t.jsx(i,{of:u}),`
+`,t.jsx(r.h3,{id:"code",children:"Code"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`const LazyLoadingTree = () => {
+  const [lazyChildren, setLazyChildren] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleItemToggle = (e) => {
+    if (e.detail.item.dataset.id === 'lazychildren' && !lazyChildren) {
+      e.preventDefault();
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setLazyChildren(
+          <>
+            <TreeItem text="Tree 3.1" />
+            <TreeItem text="Tree 3.2" />
+          </>
+        );
+        e.detail.item.toggle();
+      }, 3000);
+    }
+  };
+  return (
+    <BusyIndicator active={loading} style={{ width: '100%' }}>
+      <Tree onItemToggle={handleItemToggle}>
+        <TreeItem text="Has pre-loaded children">
+          <TreeItem text="Tree 1.1" />
+          <TreeItem text="Tree 1.2" />
+        </TreeItem>
+        <TreeItem text="Has no children" />
+        <TreeItem text="Has children but not yet loaded" hasChildren icon={dlCloutIcon} data-id="lazychildren">
+          {lazyChildren}
+        </TreeItem>
+      </Tree>
+    </BusyIndicator>
+  );
+};
+`})}),`
+`,t.jsx(j,{children:T}),`
+`,t.jsx(r.h2,{id:"treeitem",children:"TreeItem"}),`
+`,t.jsx(m,{of:p}),`
+`,t.jsx(o,{of:p}),`
+`,t.jsx(r.h2,{id:"treeitemcustom",children:"TreeItemCustom"}),`
+`,t.jsx(m,{of:s}),`
+`,t.jsx(o,{of:s}),`
+`,t.jsx(l,{})]})}function Tr(e={}){const{wrapper:r}={...d(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(a,{...e})}):a(e)}export{Tr as default};
