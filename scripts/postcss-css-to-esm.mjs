1ff24c942c08387e9dac9e0e8c215374f6deeddb
Marcus Notheis <marcus.notheis@sap.com>
2024-04-11 10:25:03 +0200

    fix regex

--- scripts/postcss-css-to-esm.mjs
+++ scripts/postcss-css-to-esm.mjs
@@ -8,9 +8,9 @@ function scopeVariables(cssText, packageJSON) {
   const escapeVersion = (version) => 'v' + version?.replaceAll(/[^0-9A-Za-z\-_]/g, '-');
   const versionStr = escapeVersion(packageJSON.version);
 
-  const expr = /(--_?ui5_)([^\,\:\)\s]+)/g;
+  const expr = /(--_?ui5)([-_])([^,:)\s]+)/g;
 
-  return cssText.replaceAll(expr, `$1-${versionStr}$2`);
+  return cssText.replaceAll(expr, `$1-${versionStr}$2$3`);
 }
 
 const getTSContent = (targetFile, packageName, css, exportTokens) => {
