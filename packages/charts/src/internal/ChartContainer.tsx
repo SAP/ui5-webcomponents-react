f3e293da547686787ba5d4f7145780b890198eca
Lukas Harbarth <lukas.harbarth@sap.com>
2024-04-16 17:42:52 +0200

    refactor(ChartContainer): replace react-jss with css modules

--- packages/charts/src/internal/ChartContainer.tsx
+++ packages/charts/src/internal/ChartContainer.tsx
@@ -1,10 +1,10 @@
 import { type CommonProps, Label, Loader } from '@ui5/webcomponents-react';
-import { ThemingParameters } from '@ui5/webcomponents-react-base';
+import { useStylesheet } from '@ui5/webcomponents-react-base';
 import { clsx } from 'clsx';
 import type { ComponentType, CSSProperties, ReactElement, ReactNode } from 'react';
 import React, { Component, forwardRef } from 'react';
-import { createUseStyles } from 'react-jss';
 import { ResponsiveContainer } from 'recharts';
+import { classNames, styleData } from './ChartContainer.module.css.js';
 
 export interface ContainerProps extends CommonProps {
   children: ReactElement;
@@ -29,26 +29,6 @@ const loaderStyles: CSSProperties = {
   right: 0
 };
 
-const chartContainerStyles = {
-  container: {
-    fontSize: ThemingParameters.sapFontSmallSize,
-    color: ThemingParameters.sapTextColor,
-    fontFamily: ThemingParameters.sapFontFamily,
-    width: '100%',
-    height: '400px',
-    position: 'relative'
-  },
-  '@global': {
-    '.has-click-handler': {
-      '& .recharts-pie-sector, .recharts-bar-rectangles, .recharts-active-dot, .recharts-area-dot': {
-        cursor: 'pointer'
-      }
-    }
-  }
-};
-
-const useStyles = createUseStyles(chartContainerStyles, { name: 'ChartContainer' });
-
 class ErrorBoundary extends Component<{ children: ReactNode }, { errorCount: number }> {
   state = {
     errorCount: 0
@@ -70,10 +50,11 @@ class ErrorBoundary extends Component<{ children: ReactNode }, { errorCount: num
 
 const ChartContainer = forwardRef<HTMLDivElement, ContainerProps>((props, ref) => {
   const { Placeholder, loading = false, dataset, className, slot, children, resizeDebounce, ...rest } = props;
-  const classes = useStyles();
+
+  useStylesheet(styleData, ChartContainer.displayName);
 
   return (
-    <div ref={ref} className={clsx(classes.container, className)} slot={slot} {...rest}>
+    <div ref={ref} className={clsx(classNames.container, className)} slot={slot} {...rest}>
       {dataset?.length > 0 ? (
         <>
           {loading && <Loader style={loaderStyles} />}
