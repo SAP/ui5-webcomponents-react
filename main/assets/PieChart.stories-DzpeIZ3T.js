bcf37420a7d6bd734e17750bf4a553ef6879eac4
Lukas742 <lukas.harbarth@sap.com>
2024-04-17 07:28:35 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@ede029ea299d29ac84cf9583f648637ec0aef4f7 🚀

---
+++ main/assets/PieChart.stories-DzpeIZ3T.js
@@ -0,0 +1,35 @@
+import{s as L,d as b}from"./DemoProps-BWWtsAFv.js";import{P as W}from"./PieChart-BPtiicJV.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./index-CBpU8qq5.js";import"./clsx-B-dksMZM.js";import"./defaults-DaoCc9Ah.js";import"./ChartContainer-BcVR7lN1.js";import"./_baseForOwn-DFjKyXZi.js";import"./_baseUniq-DRxb5hvO.js";import"./index-oF-FT8yh.js";import"./i18n-defaults-DkhM4WLz.js";import"./i18nBundle-Duvqe7kG.js";import"./Boot-B0Gt2ObV.js";import"./ManagedStyles-DX5mlBXq.js";import"./I18nContext-BcGksm2n.js";import"./useIsomorphicLayoutEffect-gsbYElqV.js";import"./useStylesheet-D5wp95d6.js";import"./throttle-ClB5v_5B.js";import"./isString-B_cEKuVX.js";import"./index-ClXwilk6.js";import"./iframe-Cgt4Dk_0.js";import"../sb-preview/runtime.js";import"./Label-D6k6fzJu.js";import"./UI5Element-Lw3nTtTN.js";import"./CustomElementsScopeUtils-CeSGtO6z.js";import"./withWebComponent-zM8JcmxY.js";import"./utils-syHHPmsO.js";import"./WrappingType-CW8URInd.js";import"./i18n-defaults-D1sujP0E.js";import"./parameters-bundle.css-DjgCrOcY.js";import"./tiny-invariant-CCoILDQG.js";import"./staticProps-CjaVosBc.js";import"./ThemingParameters--AGRs54p.js";import"./useOnClickInternal-lyLIrhui.js";import"./Placeholder-r5b-pWxa.js";import"./generateCategoricalChart-D6Ltpsk8.js";import"./isPlainObject-1lSDzB4t.js";import"./mapValues-BvzalXkB.js";import"./react-content-loader.es-CRTrDzHi.js";const nr={title:"PieChart",component:W,args:{dimension:{accessor:"name"},measure:{accessor:"users"},dataset:L},argTypes:{dataset:{control:{disable:!0}}}},r={},e={args:{measure:{accessor:"users",colors:["#f00","green","var(--sapNegativeColor)"]}}},t={args:{dataset:[]}},a={args:{measure:{accessor:"users",formatter:s=>s>200?"over 200":"lower"},chartConfig:{activeSegment:1,showActiveSegmentDataLabel:!0}}},o={args:{measure:{accessor:"users",hideDataLabel:s=>{if(s.percent<.01)return!0}},dataset:b}};var m,i,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var c,n,u;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      colors: ['#f00', 'green', 'var(--sapNegativeColor)']
+    }
+  }
+}`,...(u=(n=e.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};var l,d,g;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    dataset: []
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var h,f,S;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      formatter: d => d > 200 ? 'over 200' : 'lower'
+    },
+    chartConfig: {
+      activeSegment: 1,
+      showActiveSegmentDataLabel: true
+    }
+  }
+}`,...(S=(f=a.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var C,v,D;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      hideDataLabel: chartConfig => {
+        if (chartConfig.percent < 0.01) {
+          return true;
+        }
+      }
+    },
+    dataset: simpleDataSetWithSmallValues
+  }
+}`,...(D=(v=o.parameters)==null?void 0:v.docs)==null?void 0:D.source}}};const ur=["Default","WithCustomColor","LoadingPlaceholder","WithFormatter","HideLabels"];export{r as Default,o as HideLabels,t as LoadingPlaceholder,e as WithCustomColor,a as WithFormatter,ur as __namedExportsOrder,nr as default};
