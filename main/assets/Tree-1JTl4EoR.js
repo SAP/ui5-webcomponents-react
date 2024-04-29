9edef51b0f371a9db288a94ca1720f5f697b6b75
Lukas742 <lukas.harbarth@sap.com>
2024-04-29 12:07:20 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@368815ac686a5edaed221be668b1a2d01a603f8c 🚀

---
+++ main/assets/Tree-1JTl4EoR.js
@@ -0,0 +1,55 @@
+import{j as t}from"./ManagedStyles-0Bwp9t5R.js";import{useMDXComponents as d}from"./index-3-_tf5dF.js";import{A as e}from"./ArgTypesWithNote-eM5du4oo.js";import{C as l}from"./ControlsWithNote-DwLyHAv3.js";import{D as c}from"./DocsHeader-CsPcff1r.js";import{F as h}from"./ProjectTemplate-fVoe9LNg.js";import"./index-BP8_t0zE.js";import{S as x}from"./SubcomponentsSection-BVy4lpQe.js";import{M as j,C as i,d as T,D as m}from"./index-CHs2jRtN.js";import{C as f,D as n,L as u,T as p,a as s}from"./Tree.stories-DU6AD5fx.js";import"./index-DDQ_XT5-.js";import"./iframe-T8bF6CMc.js";import"../sb-preview/runtime.js";import"./UI5Element-BfPpwo_k.js";import"./Boot-BtdVcW0c.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-CjWOyBed.js";import"./withWebComponent-DdLYI_QY.js";import"./utils-VXdlHuBA.js";import"./useIsomorphicLayoutEffect-CnI1knHw.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-Dx46P1iz.js";import"./decline-Bq4uAF5d.js";import"./Icons-DgaaT5sV.js";import"./i18n-defaults-BdfZArTM.js";import"./information-CIM5_BNu.js";import"./alert-DaAYzDGJ.js";import"./class-map-DwUNpVO-.js";import"./Icon-CDo0XdyR.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-DjgCrOcY.js";import"./Button-C4siPQ_G.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-BFrvdQTW.js";import"./index-BxmsGmlx.js";import"./utils-DaHSXCuS.js";import"./main-HWtSBowW.js";import"./index-g_WJmeQy.js";import"./sys-help-2-BtjmRNex.js";import"./WrappingType-CW8URInd.js";import"./index-BTRGQOJT.js";import"./Popover-BRx0aMsU.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-BDyegTwa.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BGWtnzwQ.js";import"./FocusableElements-FQMFYvMw.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-BxP3RYN1.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-BhswnH6I.js";import"./BrowserScrollbar.css-BmT0sFnr.js";import"./index-BAus-ghq.js";import"./Link-Ca8TnKXf.js";import"./chunk-HLWAVYOI-DzdAMzW3.js";import"./client-Di-4O7We.js";import"./copy-FfYDXqvA.js";import"./index-lyPlTZJl.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DXQOMd9Q.js";import"./index-CCV2VZkT.js";import"./i18n-defaults-B3VmmZkh.js";import"./I18nContext-RJHUyvz-.js";import"./index-CIMqV4DH.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./GitHub-Mark-QeDVj3uD.js";import"./TableOfContent-ClJxKlFX.js";import"./index-D1XApIPR.js";import"./Label-B0QBRtjK.js";import"./index-Cb9UHz9Y.js";import"./addCustomCSSWithScoping-B4X1apx3.js";import"./index-DAdrcYy4.js";import"./index-DPpMYsrE.js";import"./Avatar-DDfgA5P5.js";import"./employee-Cmog-JUE.js";import"./index-eC-nXWVd.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";import"./general-leave-request-BvmiuPyq.js";import"./index-CBk8jVnv.js";import"./BusyIndicator-Bh506WVf.js";import"./index-HP4ot1Ag.js";import"./ValueState-Bn-H2OaL.js";import"./ListItem-tgwFX80A.js";import"./List-Dj8Q9o3Y.js";import"./ItemNavigation-B1rM-mPj.js";import"./TabbableElements-Ds_LZU0v.js";import"./debounce-jW346lN3.js";import"./CheckBox-76mbRj2v.js";import"./accept-kIYT9b9U.js";import"./RadioButton-DO7IWeaB.js";import"./slim-arrow-right-BFi0TIdF.js";import"./navigation-right-arrow-krn87B6M.js";import"./navigation-down-arrow-SleU11Rd.js";function a(o){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...d(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(j,{of:f}),`
+`,t.jsx(c,{since:"0.10.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(i,{of:n}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{of:n}),`
+`,t.jsx(r.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"lazy-load-treeitems",children:"Lazy Load TreeItems"}),`
+`,t.jsx(i,{of:u}),`
+`,t.jsx(r.h3,{id:"code",children:"Code"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`const LazyLoadingTree = () => {
+  const [lazyChildren, setLazyChildren] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleItemToggle = (e) => {
+    if (e.detail.item.dataset.id === 'lazychildren' && !lazyChildren) {
+      e.preventDefault();
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setLazyChildren(
+          <>
+            <TreeItem text="Tree 3.1" />
+            <TreeItem text="Tree 3.2" />
+          </>
+        );
+        e.detail.item.toggle();
+      }, 3000);
+    }
+  };
+  return (
+    <BusyIndicator active={loading} style={{ width: '100%' }}>
+      <Tree onItemToggle={handleItemToggle}>
+        <TreeItem text="Has pre-loaded children">
+          <TreeItem text="Tree 1.1" />
+          <TreeItem text="Tree 1.2" />
+        </TreeItem>
+        <TreeItem text="Has no children" />
+        <TreeItem text="Has children but not yet loaded" hasChildren icon={dlCloutIcon} data-id="lazychildren">
+          {lazyChildren}
+        </TreeItem>
+      </Tree>
+    </BusyIndicator>
+  );
+};
+`})}),`
+`,t.jsx(T,{children:x}),`
+`,t.jsx(r.h2,{id:"treeitem",children:"TreeItem"}),`
+`,t.jsx(m,{of:p}),`
+`,t.jsx(e,{of:p}),`
+`,t.jsx(r.h2,{id:"treeitemcustom",children:"TreeItemCustom"}),`
+`,t.jsx(m,{of:s}),`
+`,t.jsx(e,{of:s}),`
+`,t.jsx(h,{})]})}function jr(o={}){const{wrapper:r}={...d(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(a,{...o})}):a(o)}export{jr as default};
---
+++ main/assets/index-DgCH_bXl.js
@@ -0,0 +1,112 @@
+const __vite__fileDeps=["./Slider-D71KDT3E.js","./withWebComponent-DdLYI_QY.js","./utils-VXdlHuBA.js","./EventProvider-B3ZIXKWe.js","./index-BP8_t0zE.js","./CustomElementsScopeUtils-CjWOyBed.js","./ManagedStyles-0Bwp9t5R.js","./useIsomorphicLayoutEffect-CnI1knHw.js","./Float-BYyKuwHF.js","./Integer-Dh6YzFpK.js","./i18nBundle-Dx46P1iz.js","./Boot-BtdVcW0c.js","./Keys-BgUkNma0.js","./SliderBase-BQqCrVT1.js","./UI5Element-BfPpwo_k.js","./event-CegLCnR0.js","./ResizeHandler-BxP3RYN1.js","./Icons-DgaaT5sV.js","./parameters-bundle.css-DjgCrOcY.js","./Icon-CDo0XdyR.js","./class-map-DwUNpVO-.js","./style-map-BhswnH6I.js","./i18n-defaults-BFrvdQTW.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as n}from"./iframe-T8bF6CMc.js";import"./Slider-D71KDT3E.js";import{w as a}from"./withWebComponent-DdLYI_QY.js";const e=a("ui5-slider",["accessibleName","labelInterval","max","min","step","value"],["disabled","showTickmarks","showTooltip"],[],["change","input"],()=>n(()=>import("./Slider-D71KDT3E.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]),import.meta.url));e.displayName="Slider";try{e.displayName="Slider",e.__docgenInfo={description:`The Slider component represents a numerical range and a handle (grip).
+The purpose of the component is to enable visual selection of a value in
+a continuous numerical range by moving an adjustable handle.
+
+### Structure
+The most important properties of the Slider are:
+
+- min - The minimum value of the slider range.
+- max - The maximum value of the slider range.
+- value - The current value of the slider range.
+- step - Determines the increments in which the slider will move.
+- showTooltip - Determines if a tooltip should be displayed above the handle.
+- showTickmarks - Displays a visual divider between the step values.
+- labelInterval - Labels some or all of the tickmarks with their values.
+
+### Usage
+The most common use case is to select values on a continuous numerical scale (e.g. temperature, volume, etc. ).
+
+### Responsive Behavior
+The \`Slider\` component adjusts to the size of its parent container by recalculating and
+resizing the width of the control. You can move the slider handle in several different ways:
+
+- Drag and drop the handle to the desired value.
+- Click/tap on the range bar to move the handle to that location.
+
+### Keyboard Handling
+
+- \`Left or Down Arrow\` - Moves the handle one step to the left, effectively decreasing the component's value by \`step\` amount;
+- \`Right or Up Arrow\` - Moves the handle one step to the right, effectively increasing the component's value by \`step\` amount;
+- \`Left or Down Arrow + Ctrl/Cmd\` - Moves the handle to the left with step equal to 1/10th of the entire range, effectively decreasing the component's value by 1/10th of the range;
+- \`Right or Up Arrow + Ctrl/Cmd\` - Moves the handle to the right with step equal to 1/10th of the entire range, effectively increasing the component's value by 1/10th of the range;
+- \`Plus\` - Same as \`Right or Up Arrow\`;
+- \`Minus\` - Same as \`Left or Down Arrow\`;
+- \`Home\` - Moves the handle to the beginning of the range;
+- \`End\` - Moves the handle to the end of the range;
+- \`Page Up\` - Same as \`Right or Up + Ctrl/Cmd\`;
+- \`Page Down\` - Same as \`Left or Down + Ctrl/Cmd\`;
+- \`Escape\` - Resets the value property after interaction, to the position prior the component's focusing;
+
+
+
+__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)`,displayName:"Slider",props:{onChange:{defaultValue:null,description:"Fired when the value changes and the user has finished interacting with the slider.",name:"onChange",required:!1,type:{name:"(event: Ui5CustomEvent<SliderDomRef, never>) => void"}},onInput:{defaultValue:null,description:"Fired when the value changes due to user interaction that is not yet finished - during mouse/touch dragging.",name:"onInput",required:!1,type:{name:"(event: Ui5CustomEvent<SliderDomRef, never>) => void"}},accessibleName:{defaultValue:null,description:`Defines the accessible ARIA name of the component.
+
+**Note:** Available since [v1.4.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v1.4.0) of **@ui5/webcomponents**.`,name:"accessibleName",required:!1,type:{name:"string"}},disabled:{defaultValue:{value:"false"},description:"Defines whether the slider is in disabled state.",name:"disabled",required:!1,type:{name:"boolean"}},labelInterval:{defaultValue:{value:"0"},description:`Displays a label with a value on every N-th step.
+
+**Note:** The step and tickmarks properties must be enabled.
+Example - if the step value is set to 2 and the label interval is also specified to 2 - then every second
+tickmark will be labelled, which means every 4th value number.`,name:"labelInterval",required:!1,type:{name:"number"}},max:{defaultValue:{value:"100"},description:"Defines the maximum value of the slider.",name:"max",required:!1,type:{name:"number"}},min:{defaultValue:{value:"0"},description:"Defines the minimum value of the slider.",name:"min",required:!1,type:{name:"number"}},showTickmarks:{defaultValue:{value:"false"},description:`Enables tickmarks visualization for each step.
+
+**Note:** The step must be a positive number.`,name:"showTickmarks",required:!1,type:{name:"boolean"}},showTooltip:{defaultValue:{value:"false"},description:"Enables handle tooltip displaying the current value.",name:"showTooltip",required:!1,type:{name:"boolean"}},step:{defaultValue:{value:"1"},description:`Defines the size of the slider's selection intervals (e.g. min = 0, max = 10, step = 5 would result in possible selection of the values 0, 5, 10).
+
+**Note:** If set to 0 the slider handle movement is disabled. When negative number or value other than a number, the component fallbacks to its default value.`,name:"step",required:!1,type:{name:"number"}},value:{defaultValue:{value:"0"},description:"Current value of the slider",name:"value",required:!1,type:{name:"number"}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}try{e.displayName="Slider",e.__docgenInfo={description:`The Slider component represents a numerical range and a handle (grip).
+The purpose of the component is to enable visual selection of a value in
+a continuous numerical range by moving an adjustable handle.
+
+### Structure
+The most important properties of the Slider are:
+
+- min - The minimum value of the slider range.
+- max - The maximum value of the slider range.
+- value - The current value of the slider range.
+- step - Determines the increments in which the slider will move.
+- showTooltip - Determines if a tooltip should be displayed above the handle.
+- showTickmarks - Displays a visual divider between the step values.
+- labelInterval - Labels some or all of the tickmarks with their values.
+
+### Usage
+The most common use case is to select values on a continuous numerical scale (e.g. temperature, volume, etc. ).
+
+### Responsive Behavior
+The \`Slider\` component adjusts to the size of its parent container by recalculating and
+resizing the width of the control. You can move the slider handle in several different ways:
+
+- Drag and drop the handle to the desired value.
+- Click/tap on the range bar to move the handle to that location.
+
+### Keyboard Handling
+
+- \`Left or Down Arrow\` - Moves the handle one step to the left, effectively decreasing the component's value by \`step\` amount;
+- \`Right or Up Arrow\` - Moves the handle one step to the right, effectively increasing the component's value by \`step\` amount;
+- \`Left or Down Arrow + Ctrl/Cmd\` - Moves the handle to the left with step equal to 1/10th of the entire range, effectively decreasing the component's value by 1/10th of the range;
+- \`Right or Up Arrow + Ctrl/Cmd\` - Moves the handle to the right with step equal to 1/10th of the entire range, effectively increasing the component's value by 1/10th of the range;
+- \`Plus\` - Same as \`Right or Up Arrow\`;
+- \`Minus\` - Same as \`Left or Down Arrow\`;
+- \`Home\` - Moves the handle to the beginning of the range;
+- \`End\` - Moves the handle to the end of the range;
+- \`Page Up\` - Same as \`Right or Up + Ctrl/Cmd\`;
+- \`Page Down\` - Same as \`Left or Down + Ctrl/Cmd\`;
+- \`Escape\` - Resets the value property after interaction, to the position prior the component's focusing;
+
+
+
+__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)`,displayName:"Slider",props:{onChange:{defaultValue:null,description:"Fired when the value changes and the user has finished interacting with the slider.",name:"onChange",required:!1,type:{name:"(event: Ui5CustomEvent<SliderDomRef, never>) => void"}},onInput:{defaultValue:null,description:"Fired when the value changes due to user interaction that is not yet finished - during mouse/touch dragging.",name:"onInput",required:!1,type:{name:"(event: Ui5CustomEvent<SliderDomRef, never>) => void"}},accessibleName:{defaultValue:null,description:`Defines the accessible ARIA name of the component.
+
+**Note:** Available since [v1.4.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v1.4.0) of **@ui5/webcomponents**.`,name:"accessibleName",required:!1,type:{name:"string"}},disabled:{defaultValue:{value:"false"},description:"Defines whether the slider is in disabled state.",name:"disabled",required:!1,type:{name:"boolean"}},labelInterval:{defaultValue:{value:"0"},description:`Displays a label with a value on every N-th step.
+
+**Note:** The step and tickmarks properties must be enabled.
+Example - if the step value is set to 2 and the label interval is also specified to 2 - then every second
+tickmark will be labelled, which means every 4th value number.`,name:"labelInterval",required:!1,type:{name:"number"}},max:{defaultValue:{value:"100"},description:"Defines the maximum value of the slider.",name:"max",required:!1,type:{name:"number"}},min:{defaultValue:{value:"0"},description:"Defines the minimum value of the slider.",name:"min",required:!1,type:{name:"number"}},showTickmarks:{defaultValue:{value:"false"},description:`Enables tickmarks visualization for each step.
+
+**Note:** The step must be a positive number.`,name:"showTickmarks",required:!1,type:{name:"boolean"}},showTooltip:{defaultValue:{value:"false"},description:"Enables handle tooltip displaying the current value.",name:"showTooltip",required:!1,type:{name:"boolean"}},step:{defaultValue:{value:"1"},description:`Defines the size of the slider's selection intervals (e.g. min = 0, max = 10, step = 5 would result in possible selection of the values 0, 5, 10).
+
+**Note:** If set to 0 the slider handle movement is disabled. When negative number or value other than a number, the component fallbacks to its default value.`,name:"step",required:!1,type:{name:"number"}},value:{defaultValue:{value:"0"},description:"Current value of the slider",name:"value",required:!1,type:{name:"number"}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}export{e as S};
