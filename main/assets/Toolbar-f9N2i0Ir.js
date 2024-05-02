628a87619bd8f7ec025aefea93c1686710c2e87e
Lukas742 <lukas.harbarth@sap.com>
2024-05-02 12:15:32 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@169ed4947a50d36d92b4adcc1a95e0fcb621140e 🚀

---
+++ main/assets/Toolbar-f9N2i0Ir.js
@@ -0,0 +1,162 @@
+import{j as o}from"./ManagedStyles-0Bwp9t5R.js";import{useMDXComponents as u}from"./index-3-_tf5dF.js";import{A as r}from"./ArgTypesWithNote-Bjm7v3HX.js";import{C as s}from"./ControlsWithNote-BqyZ_8yz.js";import{D as x}from"./DocsHeader-BBy-xScb.js";import{F as f}from"./ProjectTemplate-B6oy3HnN.js";import{C as v,D as p,R as b,E as j,W as g,a as l,O as w,b as T,P as O,c as a,d}from"./Toolbar.stories-CKsTTCau.js";import{T as c}from"./index-BqF0Fut7.js";import{T as m}from"./index-Bahdaizp.js";import"./index-BP8_t0zE.js";import{M as B,C as n,d as y,D as i}from"./index-DMRhODnk.js";import{S as C}from"./SubcomponentsSection-BVy4lpQe.js";import{M as I,a as P}from"./index-B_Y2Rpox.js";import"./index-BxmsGmlx.js";import"./utils-DX8ZRm4r.js";import"./main-HWtSBowW.js";import"./index-DWKl6ZR7.js";import"./iframe-BoJ5Mwmu.js";import"../sb-preview/runtime.js";import"./UI5Element-BfPpwo_k.js";import"./Boot-BtdVcW0c.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-CjWOyBed.js";import"./withWebComponent-DdLYI_QY.js";import"./utils-VXdlHuBA.js";import"./useIsomorphicLayoutEffect-CnI1knHw.js";import"./event-CegLCnR0.js";import"./slot-Df15G--e.js";import"./i18nBundle-Dx46P1iz.js";import"./willShowContent-BOkh0bwj.js";import"./Icon-CDo0XdyR.js";import"./Icons-DgaaT5sV.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-DjgCrOcY.js";import"./sys-help-2-BtjmRNex.js";import"./information-CIM5_BNu.js";import"./i18n-defaults-BdfZArTM.js";import"./alert-DaAYzDGJ.js";import"./WrappingType-CW8URInd.js";import"./i18n-defaults-BFrvdQTW.js";import"./index-M3I09fmT.js";import"./Popover-BRx0aMsU.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-BDyegTwa.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BGWtnzwQ.js";import"./FocusableElements-FQMFYvMw.js";import"./isElementHidden-Bpu2V8BK.js";import"./AriaLabelHelper-CzOXVcye.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-BxP3RYN1.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-BhswnH6I.js";import"./class-map-DwUNpVO-.js";import"./BrowserScrollbar.css-BmT0sFnr.js";import"./index-B2sc1RBr.js";import"./Link-Ca8TnKXf.js";import"./MarkedEvents-BPv6Lv4o.js";import"./HasPopup-Cecjtg2t.js";import"./chunk-HLWAVYOI-YHQxgl0_.js";import"./client-Di-4O7We.js";import"./copy-FfYDXqvA.js";import"./index-C9cH8ayn.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DXQOMd9Q.js";import"./index-CCV2VZkT.js";import"./i18n-defaults-B3VmmZkh.js";import"./I18nContext-RJHUyvz-.js";import"./index-CIMqV4DH.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./GitHub-Mark-QeDVj3uD.js";import"./TableOfContent-DmMYex3r.js";import"./index-BXGloPyl.js";import"./Label-B0QBRtjK.js";import"./index-cioWVBbU.js";import"./Button-C4siPQ_G.js";import"./addCustomCSSWithScoping-B4X1apx3.js";import"./index-Cc8Dl3VA.js";import"./index-B-c96jEL.js";import"./Avatar-DDfgA5P5.js";import"./employee-Cmog-JUE.js";import"./index-C7E_EHlo.js";import"./ListItem-tgwFX80A.js";import"./decline-Bq4uAF5d.js";import"./List-Dj8Q9o3Y.js";import"./ItemNavigation-B1rM-mPj.js";import"./TabbableElements-Ds_LZU0v.js";import"./debounce-jW346lN3.js";import"./BusyIndicator-Bh506WVf.js";import"./CheckBox-76mbRj2v.js";import"./ValueState-Bn-H2OaL.js";import"./accept-kIYT9b9U.js";import"./RadioButton-DO7IWeaB.js";import"./slim-arrow-right-BFi0TIdF.js";import"./favorite-Cze-JfjV.js";import"./settings-DxzrX51-.js";import"./index-DLhANecA.js";import"./DatePicker-CKM72M2S.js";import"./Gregorian-BYHGO3JP.js";import"./LocaleData-BpMMAuFN.js";import"./Calendar-CSTi4z3t.js";import"./slim-arrow-left-0Lcd0cO8.js";import"./ResponsivePopover-BSbuHI1C.js";import"./Dialog-DhuoG6wg.js";import"./Title-ClrU0KnW.js";import"./Input-CFmcBLeR.js";import"./ResponsivePopoverCommon.css-BeRAsjqV.js";import"./ValueStateMessage.css-Oa7QXPMA.js";import"./index-D6qTxhDD.js";import"./index-DqK71nSC.js";import"./index-DFPB-A3z.js";import"./Menu-DZA447_E.js";import"./CustomListItem-CnzueQa6.js";import"./StandardListItem-TNULnp6L.js";import"./index-BCOQXbbc.js";import"./Select-5p4MbL2n.js";import"./InvisibleMessage-XNJHwCLI.js";import"./slim-arrow-down-Dzsb4afV.js";import"./index-8Q4Pc1Tv.js";import"./Slider-D71KDT3E.js";import"./Float-BYyKuwHF.js";import"./SliderBase-BQqCrVT1.js";import"./index-STa0CFqm.js";import"./less-BEX4kzFy.js";import"./index-DyKDPxzG.js";import"./ToggleButton-Bv2qHodr.js";import"./index-RSj_zc7P.js";import"./overflow-BK0AEynQ.js";import"./stopPropagation-BJWsenK5.js";import"./debounce-D7W5PopO.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";function h(t){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",...u(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(B,{of:v}),`
+`,o.jsx(x,{subComponents:["OverflowToolbarButton","OverflowToolbarToggleButton","ToolbarSpacer","ToolbarSeparator"]}),`
+`,o.jsx(I,{design:P.Warning,hideCloseButton:!0,children:o.jsxs(o.Fragment,{children:["This component may be replaced by the ",o.jsx(e.code,{children:"ui5-toolbar"})," web-component (currently available as"," ",o.jsx(e.code,{children:"ToolbarV2"}),") with our next major release. If you only need to pass components supported by"," ",o.jsx(e.code,{children:"ToolbarV2"})," then please consider using ",o.jsx(e.code,{children:"ToolbarV2"})," instead of this component."]})}),`
+`,o.jsx(e.h2,{id:"example",children:"Example"}),`
+`,o.jsx(n,{of:p}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:p}),`
+`,o.jsx(e.h2,{id:"prevent-event-bubbling-of-toolbar-items",children:"Prevent event bubbling of Toolbar items"}),`
+`,o.jsxs(e.p,{children:["Per default, if the ",o.jsx(e.code,{children:"active"}),' prop is "true" and an actionable element like a button is clicked, the ',o.jsx(e.code,{children:"onClick"})," event of the ",o.jsx(e.code,{children:"Toolbar"}),` is also fired.
+To prevent this you can add `,o.jsx(e.code,{children:"e.stopPropagation()"})," to your event handler."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`<Toolbar>
+  <Button
+    onClick={(e) => {
+      //use e.stopPropagation() to prevent event bubbling
+      e.stopPropagation();
+    }}
+  >
+    Toolbar Item
+  </Button>
+</Toolbar>
+`})}),`
+`,o.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(e.h2,{id:"align-elements-inside-the-toolbar",children:"Align elements inside the Toolbar"}),`
+`,o.jsxs(e.p,{children:["To align items inside the ",o.jsx(e.code,{children:"Toolbar"})," you can use the ",o.jsx(e.code,{children:"ToolbarSpacer"})," component.",o.jsx("br",{}),`
+This will cover the remaining horizontal space between the `,o.jsx(e.code,{children:"Toolbar"})," items and for example pushes one item to the edge of the ",o.jsx(e.code,{children:"Toolbar"}),"."]}),`
+`,o.jsx(e.h3,{id:"right-aligned-items",children:"Right aligned items"}),`
+`,o.jsx(n,{of:b}),`
+`,o.jsx(e.h3,{id:"evenly-aligned-items",children:"Evenly aligned items"}),`
+`,o.jsx(n,{of:j}),`
+`,o.jsx(e.h2,{id:"toolbar-separator",children:"Toolbar Separator"}),`
+`,o.jsxs(e.p,{children:["You can add a visual separator between the preceding and succeeding ",o.jsx(e.code,{children:"Toolbar"})," item with the use of the ",o.jsx(e.code,{children:"ToolbarSeparator"}),"."]}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.strong,{children:"Note:"})," In the overflow popover the ",o.jsx(e.code,{children:"ToolbarSeparator"})," is represented by a horizontal line, in the ",o.jsx(e.code,{children:"Toolbar"}),` by a vertical
+line.`]}),`
+`,o.jsx(n,{of:g}),`
+`,o.jsx(e.h2,{id:"toolbar-with-overflow-button",children:"Toolbar with overflow button"}),`
+`,o.jsx(e.p,{children:"If the horizontally available space isn't enough to fit all items in it, an overflow button is displayed."}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.strong,{children:"Note:"})," You can change the width of the toolbar by dragging the ",o.jsx(e.code,{children:"Slider"}),"."]}),`
+`,o.jsx(s,{of:l,hideHTMLPropsNote:!0,include:["numberOfAlwaysVisibleItems"]}),`
+`,o.jsx(n,{of:l}),`
+`,o.jsx(e.h3,{id:"overflowtoolbarbutton--overflowtoolbartogglebutton",children:"OverflowToolbarButton & OverflowToolbarToggleButton"}),`
+`,o.jsxs(e.p,{children:["With the ",o.jsx(e.code,{children:"OverflowToolbarButton"})," and ",o.jsx(e.code,{children:"OverflowToolbarToggleButton"})," it is possible to display the text of a ",o.jsx(e.code,{children:"Button"})," with an icon only in the overflow popover of the ",o.jsx(e.code,{children:"Toolbar"}),`.
+Both components accept the same `,o.jsx(e.code,{children:"props"})," as the standard ",o.jsx(e.code,{children:"Button"})," or ",o.jsx(e.code,{children:"ToggleButton"})," component, with the difference that ",o.jsx(e.code,{children:"icon"})," is now required and ",o.jsx(e.code,{children:"children"})," are only rendered inside the popover."]}),`
+`,o.jsx(n,{of:w}),`
+`,o.jsx(e.h3,{id:"toolbar-with-custom-overflow-button",children:"Toolbar with custom overflow button"}),`
+`,o.jsxs(e.p,{children:["By setting the ",o.jsx(e.code,{children:"overflowButton"})," prop, it's possible to change the overflow button, which is displayed if the ",o.jsx(e.code,{children:"Toolbar"}),` goes into overflow.
+Only buttons in icon only mode are supported and it's recommended to use the `,o.jsx(e.code,{children:"ToggleButton"})," in ",o.jsx(e.code,{children:'"Transparent"'}),` design to comply with UX guidelines.
+Please also note that when using a custom button, it is your responsibility to set the appropriate a11y attributes.`]}),`
+`,o.jsxs(e.p,{children:["It is furthermore possible to prevent the popover from opening by using ",o.jsx(e.code,{children:"event.preventDefault()"})," in the ",o.jsx(e.code,{children:"onClick"})," of the button. This is useful e.g. when implementing a custom overflow popover."]}),`
+`,o.jsx(n,{of:T}),`
+`,o.jsx(e.h2,{id:"close-overflow-popover-on-interaction",children:"Close overflow popover on interaction"}),`
+`,o.jsxs(e.p,{children:["The overflow popover can be closed programmatically by using the ",o.jsx(e.code,{children:"overflowPopoverRef"}),"."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const ToolbarComponent = () => {
+  const overflowPopoverRef = useRef(null);
+  const handlePopoverClose = () => {
+    if (overflowPopoverRef.current.isOpen()) {
+      overflowPopoverRef.current.close();
+    }
+  };
+  return (
+    <Toolbar overflowPopoverRef={overflowPopoverRef}>
+      <Text>Toolbar</Text>
+      <Button onClick={handlePopoverClose}>Button One</Button>
+      <Select onChange={handlePopoverClose}>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+      </Select>
+    </Toolbar>
+  );
+};
+`})}),`
+`,o.jsx(e.h2,{id:"open-popovers-by-id-inside-the-overflow-popover",children:"Open Popovers by ID inside the overflow popover"}),`
+`,o.jsxs(e.p,{children:["To prevent duplicate child ",o.jsx(e.code,{children:"id"}),"s in the DOM, all child ",o.jsx(e.code,{children:"id"}),"s get an ",o.jsx(e.code,{children:"-overflow"})," suffix. If an ID is used to open the popover (e.g. via the ",o.jsx(e.code,{children:"opener"}),` prop), you have to make sure to use the correct id.
+You can achieve that either by leveraging the `,o.jsx(e.code,{children:"onOverflowChange"})," event and retrieve the current ID there, or by updating it inside the ",o.jsx(e.code,{children:"click"})," handler of the respective opener component."]}),`
+`,o.jsx(n,{of:O,sourceState:"none"}),`
+`,o.jsxs("details",{children:[o.jsx("summary",{children:"Set opener ID via click handler"}),o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const ToolbarComponent = () => {
+  const [popoverIsOpen, setPopoverIsOpen] = useState(false);
+  const [opener, setOpener] = useState('openMenuBtn');
+  const handlePopoverOpenerClick = (e) => {
+    setOpener(e.target.id);
+    setPopoverIsOpen(true);
+  };
+  return (
+    <>
+      <Toolbar style={{ width: '400px' }}>
+        <Text>Toolbar</Text>
+        <Button>Button One</Button>
+        <Button>Button Two</Button>
+        <Button id="openMenuBtn" onClick={handlePopoverOpenerClick}>
+          Open Popover (Menu)
+        </Button>
+      </Toolbar>
+      <Menu
+        opener={opener}
+        open={popoverIsOpen}
+        onAfterClose={() => {
+          setPopoverIsOpen(false);
+        }}
+      >
+        <MenuItem text="New File" />
+        <MenuItem text="New Folder" />
+      </Menu>
+    </>
+  );
+};
+`})})]}),`
+`,o.jsxs("details",{children:[o.jsx("summary",{children:"Set opener ID via onOverflowChange handler"}),o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const ToolbarComponent = () => {
+  const [popoverIsOpen, setPopoverIsOpen] = useState(false);
+  const initialOpenerId = 'openMenuBtn';
+  const [opener, setOpener] = useState(initialOpenerId);
+  const handlePopoverOpenerClick = () => {
+    setPopoverIsOpen(true);
+  };
+  const handleOverflowChange = (e) => {
+    if (e.overflowElements?.length) {
+      const overflowId = [...e.overflowElements].find((item) => item.dataset.opener)?.id;
+      if (overflowId) {
+        setOpener(overflowId);
+      } else {
+        setOpener(initialOpenerId);
+      }
+    } else {
+      setOpener(initialOpenerId);
+    }
+  };
+  return (
+    <>
+      <Toolbar style={{ width: '400px' }} onOverflowChange={handleOverflowChange}>
+        <Text>Toolbar</Text>
+        <Button>Button One</Button>
+        <Button>Button Two</Button>
+        <Button id="openMenuBtn" onClick={handlePopoverOpenerClick} data-opener>
+          Open Popover (Menu)
+        </Button>
+      </Toolbar>
+      <Menu
+        opener={opener}
+        open={popoverIsOpen}
+        onAfterClose={() => {
+          setPopoverIsOpen(false);
+        }}
+      >
+        <MenuItem text="New File" />
+        <MenuItem text="New Folder" />
+      </Menu>
+    </>
+  );
+};
+`})})]}),`
+`,o.jsx(y,{children:C}),`
+`,o.jsx(e.h2,{id:"overflowtoolbarbutton",children:"OverflowToolbarButton"}),`
+`,o.jsx(i,{of:a}),`
+`,o.jsx(r,{of:a}),`
+`,o.jsx(e.h2,{id:"overflowtoolbartogglebutton",children:"OverflowToolbarToggleButton"}),`
+`,o.jsx(i,{of:d}),`
+`,o.jsx(r,{of:d}),`
+`,o.jsx(e.h2,{id:"toolbarspacer",children:"ToolbarSpacer"}),`
+`,o.jsx(i,{of:m}),`
+`,o.jsx(r,{of:m}),`
+`,o.jsx(e.h2,{id:"toolbarseparator",children:"ToolbarSeparator"}),`
+`,o.jsx(i,{of:c}),`
+`,o.jsx(r,{of:c}),`
+`,o.jsx(f,{})]})}function sn(t={}){const{wrapper:e}={...u(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(h,{...t})}):h(t)}export{sn as default};
