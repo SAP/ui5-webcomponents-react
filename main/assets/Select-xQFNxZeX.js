4786ca413f9978ba0a7549939c711488b1d7ab7b
Lukas742 <lukas.harbarth@sap.com>
2024-05-15 10:50:47 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@2f3263ba77cc55c88aec8dc4369791a4a12e8ce1 🚀

---
+++ main/assets/Select-xQFNxZeX.js
@@ -0,0 +1,44 @@
+import{j as t}from"./ManagedStyles-B3AuKgSD.js";import{useMDXComponents as l}from"./index-3-_tf5dF.js";import{M as h,C as p,d as x,D as e}from"./index-BvGlwDyC.js";import{A as r}from"./ArgTypesWithNote-bFSpWCHe.js";import{C as j}from"./ControlsWithNote-5MHu5Lpr.js";import{D as f}from"./DocsHeader-DNl5QhA2.js";import{F as u}from"./ProjectTemplate-Cm2HRT9S.js";import"./index-BP8_t0zE.js";import{S as M}from"./SubcomponentsSection-BVy4lpQe.js";import{C,D as m,S as n,W as g,a as s}from"./Select.stories-DrY2nhKV.js";import{e as a}from"./utils-gjCqeecI.js";import{O as c}from"./index-BO-SV89I.js";import"./iframe-BT3hoZe9.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";import"./index-BLnzAoTS.js";import"./UI5Element-BDMamLf2.js";import"./Boot-DOb84JFo.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-CyqSdClf.js";import"./withWebComponent-I_l1rtvY.js";import"./utils-BFMQrOqR.js";import"./useIsomorphicLayoutEffect-CnI1knHw.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-VOPn86Vc.js";import"./decline-DIsY4YnD.js";import"./Icons-BEU0Gkqb.js";import"./i18n-defaults-BdfZArTM.js";import"./information-BKTX2iJj.js";import"./alert-DA8J16Yf.js";import"./class-map-CdwQvt9Z.js";import"./Icon-1XL4agOH.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-CG8awnKl.js";import"./Button-D3khXG03.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-YvCKXLrH.js";import"./index-BY8ZImT8.js";import"./sys-help-2-DvC0Pbzk.js";import"./WrappingType-CW8URInd.js";import"./index-CvVS04iS.js";import"./Popover-CSBFzLfb.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-DrS11q-G.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-DprvSSUl.js";import"./FocusableElements-B5R4EUYo.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DS8KPGJE.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-DYE6pHfF.js";import"./BrowserScrollbar.css-jPLSLZ0L.js";import"./index-CJ5K_4Ib.js";import"./Link-CcLSMxnp.js";import"./chunk-HLWAVYOI-uMJSDC5E.js";import"./client-Di-4O7We.js";import"./copy-C1-wxiKi.js";import"./index-DOzijD-M.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-NUMS3nLe.js";import"./index-yohFIstM.js";import"./i18n-defaults-BbHxfVEE.js";import"./I18nContext-RJHUyvz-.js";import"./index-CGRQZsF8.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./GitHub-Mark-ZT-4UEle.js";import"./TableOfContent-C0qHNeOz.js";import"./index-6jVUFvtp.js";import"./Label-MHQDFgLT.js";import"./index-1aa0GhA_.js";import"./addCustomCSSWithScoping-D_wcte3H.js";import"./index-cutNLVLJ.js";import"./index-SY4jLlEK.js";import"./Avatar-BRbCD7Rv.js";import"./employee-Cmzn10U8.js";import"./index-k7jXtlNh.js";import"./index-C2Z9Ihri.js";import"./ValueState-Bn-H2OaL.js";import"./ValueStateMessage.css-DqCXH3g3.js";import"./ResponsivePopoverCommon.css-DNItkg5h.js";import"./ResponsivePopover-Ck9HZciy.js";import"./Dialog-D-Fgd5bZ.js";import"./Title-BHx3zMoY.js";import"./List-DApNkGsj.js";import"./ItemNavigation-x2lw30XO.js";import"./TabbableElements-Ds_LZU0v.js";import"./debounce-jW346lN3.js";import"./BusyIndicator-B54HAClx.js";import"./CheckBox-B9clmfX-.js";import"./accept-BVRA-9LX.js";import"./RadioButton-B75AaruZ.js";import"./CustomListItem-Dmlfuhvv.js";import"./ListItem-nCr5j4gi.js";import"./slim-arrow-right-CHJduem3.js";import"./index-C6pWWKB3.js";import"./Select-CJ-cP_oP.js";import"./InvisibleMessage-C_gE0lg3.js";import"./slim-arrow-down-BGit97Og.js";import"./StandardListItem-CEExVdeZ.js";import"./ValueState-gQDshf3o.js";import"./main-HWtSBowW.js";function d(i){const o={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...l(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(h,{of:C}),`
+`,t.jsx(f,{since:"0.4.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(p,{of:m}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(j,{of:m}),`
+`,t.jsx(o.h2,{id:"recipes",children:"Recipes"}),`
+`,t.jsxs(o.h3,{id:"get-the-id-of-the-selected-item-with-onchange",children:["Get the ID of the selected item with ",t.jsx(o.code,{children:"onChange"})]}),`
+`,t.jsxs(o.p,{children:["With the help of the HTML5 ",t.jsx(o.code,{children:"data-"}),` API you can define any primitive value (like numbers, strings)
+as an attribute on an element.
+This data will be available on the `,t.jsx(o.code,{children:"onChange"})," event as part of the ",t.jsx(o.code,{children:"dataset"})," object of the ",t.jsx(o.code,{children:"selectedOption"}),":"]}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`const data = [
+  { id: 1, text: 'Option 1' },
+  { id: 2, text: 'Option 2' }
+];
+
+const onChange = (event) => {
+  // event.detail.selectedOption is a reference to the selected HTML Element
+  // dataset contains all attributes that were passed with the data- prefix.
+  console.log(event.detail.selectedOption.dataset.id);
+};
+
+<Select onChange={onChange}>
+  {data.map((item) => (
+    <Option key={item.id} data-id={item.id}>
+      {item.text}
+    </Option>
+  ))}
+</Select>;
+`})}),`
+`,t.jsx(x,{children:M}),`
+`,t.jsx(o.h2,{id:"option",children:"Option"}),`
+`,t.jsx(e,{of:c}),`
+`,t.jsx(r,{hideHTMLPropsNote:!0,exclude:a,of:c}),`
+`,t.jsx(o.h2,{id:"selectmenu",children:"SelectMenu"}),`
+`,t.jsx(e,{of:n}),`
+`,t.jsx(r,{of:n}),`
+`,t.jsx(o.h3,{id:"example-select-with-custom-options",children:"Example: Select with custom Options"}),`
+`,t.jsx(p,{of:g}),`
+`,t.jsx(o.h2,{id:"selectmenuoption",children:"SelectMenuOption"}),`
+`,t.jsx(e,{of:s}),`
+`,t.jsx(r,{hideHTMLPropsNote:!0,exclude:a,of:s}),`
+`,t.jsx(u,{})]})}function To(i={}){const{wrapper:o}={...l(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(d,{...i})}):d(i)}export{To as default};
