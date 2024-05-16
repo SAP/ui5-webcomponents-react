55e2f3cd53dceba2f11b8e20649c283bbb6e9cab
Lukas742 <lukas.harbarth@sap.com>
2024-05-16 12:41:49 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@94714547eefc8841b1ec25071e7f2688bcaf4a53 🚀

---
+++ main/assets/Toast-BZtC9C0H.js
@@ -0,0 +1,27 @@
+import{j as o}from"./ManagedStyles-B3AuKgSD.js";import{useMDXComponents as p}from"./index-3-_tf5dF.js";import{M as e,C as n}from"./index-Dy6ieE6U.js";import{C as s}from"./ControlsWithNote-BQvsmGPB.js";import{D as a}from"./DocsHeader-DZqpQQNG.js";import{F as c}from"./ProjectTemplate-CUeyJi66.js";import"./index-BP8_t0zE.js";import{C as h,D as i}from"./Toast.stories-CowJ35Wx.js";import"./iframe-fmJfv8Tc.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";import"./utils-DwQMyTkL.js";import"./main-HWtSBowW.js";import"./index-Ct4Xc43r.js";import"./UI5Element-BDMamLf2.js";import"./Boot-DOb84JFo.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-CyqSdClf.js";import"./withWebComponent-B6oBGWld.js";import"./utils-BFMQrOqR.js";import"./useIsomorphicLayoutEffect-CnI1knHw.js";import"./event-CegLCnR0.js";import"./slot-Df15G--e.js";import"./i18nBundle-VOPn86Vc.js";import"./willShowContent-BOkh0bwj.js";import"./Icon-BO7pZwTn.js";import"./Icons-BEU0Gkqb.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-CG8awnKl.js";import"./sys-help-2-DvC0Pbzk.js";import"./information-BKTX2iJj.js";import"./i18n-defaults-BdfZArTM.js";import"./alert-DA8J16Yf.js";import"./WrappingType-CW8URInd.js";import"./i18n-defaults-YvCKXLrH.js";import"./index-D-qF2Oqz.js";import"./decline-DIsY4YnD.js";import"./class-map-k-XuEcbQ.js";import"./Button-BZkI_7BG.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./HasPopup-Cecjtg2t.js";import"./index-CKsnnYU_.js";import"./Popover-CIglqovv.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-DrS11q-G.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-CQ0KG_Or.js";import"./FocusableElements-B5R4EUYo.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DS8KPGJE.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-C_Zo2l27.js";import"./BrowserScrollbar.css-ljOou4G0.js";import"./index-udlbQXC1.js";import"./Link-VbEG4eoi.js";import"./chunk-HLWAVYOI-Cvq742z1.js";import"./client-Di-4O7We.js";import"./copy-C1-wxiKi.js";import"./index-B17o-zTF.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-NUMS3nLe.js";import"./index-xGquCu8c.js";import"./i18n-defaults-DlmaUOWU.js";import"./I18nContext-RJHUyvz-.js";import"./index-CGRQZsF8.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./GitHub-Mark-ZT-4UEle.js";import"./TableOfContent-Dz7J4o95.js";import"./index-CWCBzOP1.js";import"./Label-DpitxhM9.js";import"./index-DdsYX7VF.js";import"./addCustomCSSWithScoping-D_wcte3H.js";import"./index-B0cMYEJO.js";import"./index-BSFikd5r.js";import"./Avatar-BGro6qG4.js";import"./employee-Cmzn10U8.js";import"./index-xKHUuYpW.js";import"./index-C43pt7hM.js";function m(r){const t={code:"code",h2:"h2",p:"p",pre:"pre",strong:"strong",...p(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:h}),`
+`,o.jsx(a,{since:"0.9.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:i}),`
+`,o.jsx(t.h2,{id:"show-a-toast",children:"Show a toast"}),`
+`,o.jsxs(t.p,{children:["The ",o.jsx(t.code,{children:"Toast"}),` component has an imperative API for getting displayed. It will not be displayed just because it is part of the DOM.
+In order to show the Toast, you have to get a reference to the `,o.jsx(t.code,{children:"Toast"})," DOM element and call the ",o.jsx(t.code,{children:"show"}),`-method.
+You can either access the DOM element by using a React `,o.jsx(t.code,{children:"ref"})," or work with IDs."]}),`
+`,o.jsx(t.p,{children:o.jsx(t.strong,{children:"Example"})}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`export const MyComponentWithAToast() {
+  const toast = useRef(null);
+
+  const showToast = () => {
+    toast.current.show();
+  };
+  return (
+    <ThemeProvider>
+      <Button onClick={showToast}>Show Toast</Button>
+      <Toast ref={toast}>This is my Toast!</Toast>
+    </ThemeProvider>
+  );
+}
+`})}),`
+`,o.jsx(c,{})]})}function Go(r={}){const{wrapper:t}={...p(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(m,{...r})}):m(r)}export{Go as default};
