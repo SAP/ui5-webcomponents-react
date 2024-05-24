5e38076d69fbc0d317cd4915e951c6c5645640fa
MarcusNotheis <marcus.notheis@sap.com>
2024-05-24 07:07:47 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@10fbc645ecde3aed4c098f4515edbe5d297a0799 🚀

---
+++ main/assets/PluginOrderedMultiSort-fxmpDQdF.js
@@ -0,0 +1,62 @@
+import{j as t}from"./useIsomorphicLayoutEffect-wJjydpYF.js";import{useMDXComponents as m}from"./index-3-_tf5dF.js";import{I as n,F as p}from"./ProjectTemplate-DLRPxc5Y.js";import{M as a,C as s}from"./index-Cp3SgDex.js";import{C as l}from"./ControlsWithNote-r1IdeIXd.js";import"./chunk-HLWAVYOI-x3X248OV.js";import"./copy-3Ib5ZJqq.js";import"./index-BP8_t0zE.js";import{e}from"./AnalyticalTableHooks.stories-cUCUKfVz.js";import"./index-CVhCT1_x.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DShc4-AB.js";import"./index-BxmsGmlx.js";import"./index-CPv09YCc.js";import"./iframe-DCtDS0JT.js";import"../sb-preview/runtime.js";import"./Label-IIemeO1C.js";import"./UI5Element-DC8MZeOy.js";import"./Boot-DRGCpzpB.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-yz8rUzbr.js";import"./withWebComponent-CWrtEEjV.js";import"./utils-EaJipdFl.js";import"./i18nBundle-BdneKiEM.js";import"./WrappingType-CW8URInd.js";import"./i18n-defaults-DKt7VHOa.js";import"./parameters-bundle.css-CtgfEhuk.js";import"./index-ClX_3Fz2.js";import"./Button-BrKU6WlN.js";import"./event-CegLCnR0.js";import"./slot-Df15G--e.js";import"./Keys-BgUkNma0.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./Icons-CG6FlOJ_.js";import"./willShowContent-BOkh0bwj.js";import"./Icon-CmpPkOGs.js";import"./HasPopup-Cecjtg2t.js";import"./index-DIA_iDu8.js";import"./Link-Bsw9emR4.js";import"./index-BFZ01pPU.js";import"./Popover-D2WBCDl9.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-C_R40Sx-.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BVkg1U_d.js";import"./FocusableElements-CrwTZl4-.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DEdNEOKg.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-MmmenpP7.js";import"./class-map-B9wLsnvZ.js";import"./BrowserScrollbar.css-cD_o1v9b.js";import"./index-6h8N-xs_.js";import"./i18n-defaults-DzvGxznN.js";import"./I18nContext-RJHUyvz-.js";import"./index-CSI8GtFb.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./addCustomCSSWithScoping-Bzep2v3d.js";import"./index-CBS9CWCt.js";import"./index-CmDmY3xx.js";import"./Avatar-CzFTtzk7.js";import"./employee-DAykk7OJ.js";import"./alert-BiuXmsCV.js";import"./index-B0SZxiF2.js";import"./index-CVyVDDRe.js";import"./decline--LpgiSgn.js";import"./i18n-defaults-BdfZArTM.js";import"./information-CWnHh7YX.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";import"./utils-DcqjOuAe.js";import"./main-HWtSBowW.js";import"./index-BrRjR2mM.js";import"./sys-help-2-BljVJXa3.js";import"./client-Di-4O7We.js";import"./AnalyticalTable.stories-DIkmxtGM.js";import"./delete-Dq2K_TXb.js";import"./ListItem-jW45-bGs.js";import"./List-DhzEUdMj.js";import"./ItemNavigation-am3g3xGz.js";import"./TabbableElements-Ds_LZU0v.js";import"./debounce-jW346lN3.js";import"./BusyIndicator-CTBaiQfJ.js";import"./CheckBox-BZj6MKWp.js";import"./ValueState-Bn-H2OaL.js";import"./accept-5SoBdaHQ.js";import"./RadioButton-hFf0Ixfj.js";import"./slim-arrow-right-Cttx0bDU.js";import"./settings-Dumt4rF5.js";import"./useIsRTL-qFbKFr62.js";import"./directionChange-BWsY0llq.js";import"./index-CBpU8qq5.js";import"./filter-1Gicul7x.js";import"./group-2-C6rgSWIz.js";import"./index-BpAnzUz8.js";import"./stopPropagation-BJWsenK5.js";import"./index-CHrUd1Ds.js";import"./CustomListItem-CXLB9Ugk.js";import"./index-DF-gzkxB.js";import"./index-D6qeh8jB.js";import"./StandardListItem-Cdr6osPe.js";import"./index-BvX8caEa.js";import"./Input-B1woo9T2.js";import"./ResponsivePopoverCommon.css-BI8MPqTJ.js";import"./ValueStateMessage.css-Caexp31Y.js";import"./navigation-down-arrow-O0MuuhU_.js";import"./navigation-right-arrow-DWfstGf9.js";import"./useCurrentTheme-D0grEgR1.js";import"./index-B0re_845.js";import"./ValueState-gQDshf3o.js";import"./IndicationColor-LRWnbNUo.js";import"./index-Dgs7OEdX.js";import"./GlobalStyleClasses-CJhxSfAI.js";import"./index-DGDDetcb.js";import"./Title-IUvBxdmV.js";import"./debounce-D7W5PopO.js";import"./index-Bi7BjyGy.js";import"./Select-BpJfwAqM.js";import"./InvisibleMessage-C0vBJgzv.js";import"./slim-arrow-down-D2RZ_g3E.js";import"./ResponsivePopover-DzElSN_X.js";import"./Dialog-DHFoNWVc.js";import"./index-cdwqWwUx.js";import"./index-CX5-wplm.js";import"./GroupHeaderListItem-CRMa9PMh.js";import"./Token-C6-K42Iq.js";import"./ScrollEnablement-DJMV82B5.js";import"./animate-DFaKyidW.js";import"./sys-cancel-DGhNKhuf.js";import"./ToggleButton-7PdvAx8B.js";import"./ComboBoxItem-mpP0cs0o.js";import"./index-BKbx7WSq.js";function i(o){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Data Display / AnalyticalTable / Plugin Hooks / useOrderedMultiSort"}),`
+`,t.jsx(r.h1,{id:"analyticaltable-plugin-useorderedmultisort",children:"AnalyticalTable Plugin: useOrderedMultiSort"}),`
+`,t.jsx(n,{moduleNames:["AnalyticalTableHooks"],packageName:"@ui5/webcomponents-react"}),`
+`,t.jsxs(r.p,{children:["With the ",t.jsx(r.code,{children:"useOrderedMultiSort"})," it's possible to define the sorting priority if multi-sort is enabled for the respective columns (",t.jsx(r.code,{children:"enableMultiSort: true"}),")."]}),`
+`,t.jsx(r.p,{children:"It receives one parameter that defines the sorting priority by the column id."}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.code,{children:"orderedIds: string[]"}),": Defines the sort priority when sorting by multiple columns, starting with the first column ID."]}),`
+`]}),`
+`,t.jsxs(r.p,{children:[t.jsx(r.strong,{children:"Note:"})," Column IDs that are not found in the array use the default priority, so the first sorted column has a higher priority than the next sorted column."]}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(s,{sourceState:"none",of:e}),`
+`,t.jsx(r.h3,{id:"current-parameter",children:"Current Parameter"}),`
+`,t.jsx(l,{of:e,hideHTMLPropsNote:!0,include:["orderedIds"]}),`
+`,t.jsx(r.h3,{id:"code",children:"Code"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`const columns = [
+  {
+    Header: 'Name',
+    accessor: 'name',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age',
+    accessor: 'age',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Name 2',
+    accessor: 'name2',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age 2',
+    accessor: 'age2',
+    enableMultiSort: true
+  }
+];
+const data = [
+  { name: 'Peter', age: 40, name2: 'Alissa', age2: 18 },
+  { name: 'Kristen', age: 40, name2: 'Randolph', age2: 21 },
+  { name: 'Peter', age: 30, name2: 'Rose', age2: 90 },
+  { name: 'Peter', age: 70, name2: 'Rose', age2: 22 },
+  { name: 'Kristen', age: 60, name2: 'Willis', age2: 80 },
+  { name: 'Kristen', age: 20, name2: 'Alissa', age2: 80 },
+  { name: 'Graham', age: 40, name2: 'Alissa', age2: 80 },
+  { name: 'Peter', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Willis', age2: 26 },
+  { name: 'Graham', age: 62, name2: 'Willis', age2: 26 }
+];
+const orderedIds = ['name', 'name2', 'age', 'age2'];
+const TableComponent = () => {
+  return (
+    <AnalyticalTable
+      columns={columns}
+      data={data}
+      sortable
+      tableHooks={[AnalyticalTableHooks.useOrderedMultiSort(orderedIds)]}
+    />
+  );
+};
+`})}),`
+`,t.jsx(p,{})]})}function Lr(o={}){const{wrapper:r}={...m(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{Lr as default};
---
+++ main/assets/SplitterLayout-CD6h2mY7.js
@@ -0,0 +1,56 @@
+import{j as t}from"./useIsomorphicLayoutEffect-wJjydpYF.js";import{useMDXComponents as s}from"./index-3-_tf5dF.js";import{A as m}from"./ArgTypesWithNote-CCgGQGbc.js";import{C as l}from"./ControlsWithNote-r1IdeIXd.js";import{D as d}from"./DocsHeader-ISDlOYot.js";import{F as a}from"./ProjectTemplate-DLRPxc5Y.js";import{C as h,D as i,L as c,N as x,S as o}from"./SplitterLayout.stories-rj21gx1q.js";import"./index-BP8_t0zE.js";import{M as j,C as n,d as u,D as S}from"./index-Cp3SgDex.js";import{S as C}from"./SubcomponentsSection-BVy4lpQe.js";import"./index-CVyVDDRe.js";import"./iframe-DCtDS0JT.js";import"../sb-preview/runtime.js";import"./UI5Element-DC8MZeOy.js";import"./Boot-DRGCpzpB.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-yz8rUzbr.js";import"./withWebComponent-CWrtEEjV.js";import"./utils-EaJipdFl.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-BdneKiEM.js";import"./decline--LpgiSgn.js";import"./Icons-CG6FlOJ_.js";import"./i18n-defaults-BdfZArTM.js";import"./information-CWnHh7YX.js";import"./alert-BiuXmsCV.js";import"./class-map-B9wLsnvZ.js";import"./Icon-CmpPkOGs.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-CtgfEhuk.js";import"./Button-BrKU6WlN.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-DKt7VHOa.js";import"./index-BxmsGmlx.js";import"./utils-DcqjOuAe.js";import"./main-HWtSBowW.js";import"./index-BrRjR2mM.js";import"./sys-help-2-BljVJXa3.js";import"./WrappingType-CW8URInd.js";import"./index-BFZ01pPU.js";import"./Popover-D2WBCDl9.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-C_R40Sx-.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BVkg1U_d.js";import"./FocusableElements-CrwTZl4-.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DEdNEOKg.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-MmmenpP7.js";import"./BrowserScrollbar.css-cD_o1v9b.js";import"./index-DIA_iDu8.js";import"./Link-Bsw9emR4.js";import"./chunk-HLWAVYOI-x3X248OV.js";import"./client-Di-4O7We.js";import"./copy-3Ib5ZJqq.js";import"./index-CVhCT1_x.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DShc4-AB.js";import"./index-6h8N-xs_.js";import"./i18n-defaults-DzvGxznN.js";import"./I18nContext-RJHUyvz-.js";import"./index-CSI8GtFb.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./GitHub-Mark-BXZU7gws.js";import"./TableOfContent-h3LZCels.js";import"./index-CPv09YCc.js";import"./Label-IIemeO1C.js";import"./index-ClX_3Fz2.js";import"./addCustomCSSWithScoping-Bzep2v3d.js";import"./index-CBS9CWCt.js";import"./index-CmDmY3xx.js";import"./Avatar-CzFTtzk7.js";import"./employee-DAykk7OJ.js";import"./index-B0SZxiF2.js";import"./useIsRTL-qFbKFr62.js";import"./directionChange-BWsY0llq.js";import"./useCurrentTheme-D0grEgR1.js";import"./index-BpAnzUz8.js";import"./debounce-D7W5PopO.js";import"./index-ZSUAQy3m.js";import"./SegmentedButton-BFsE14wn.js";import"./ItemNavigation-am3g3xGz.js";import"./ToggleButton-7PdvAx8B.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";function p(r){const e={code:"code",h2:"h2",h3:"h3",pre:"pre",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...s(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(j,{of:h}),`
+`,t.jsx(d,{subComponents:["SplitterElement"]}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(n,{of:i}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{of:i}),`
+`,t.jsx(e.h3,{id:"splitterlayoutoptions",children:"SplitterLayoutOptions"}),`
+`,t.jsxs(e.table,{children:[t.jsx(e.thead,{children:t.jsxs(e.tr,{children:[t.jsx(e.th,{children:"Property"}),t.jsx(e.th,{children:"Type"}),t.jsx(e.th,{children:"Description"})]})}),t.jsxs(e.tbody,{children:[t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"resetOnSizeChange"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"boolean"})}),t.jsxs(e.td,{children:["Defines whether the ",t.jsx(e.code,{children:"SplitterLayout"})," should be reset when its size changes depending on the orientation."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"resetOnChildrenChange"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"boolean"})}),t.jsxs(e.td,{children:["Defines whether the ",t.jsx(e.code,{children:"SplitterLayout"})," should be reset when its ",t.jsx(e.code,{children:"children"})," change."]})]}),t.jsxs(e.tr,{children:[t.jsx(e.td,{children:t.jsx(e.code,{children:"resetOnCustomDepsChange"})}),t.jsx(e.td,{children:t.jsx(e.code,{children:"React.DependencyList"})}),t.jsxs(e.td,{children:["Defines a list of dependencies that trigger a reset of the ",t.jsx(e.code,{children:"SplitterLayout"})," when they are changed."]})]})]})]}),`
+`,t.jsx(e.h2,{id:"splitterlayout-with-non-resizable-areas--areas-with-min-width",children:"SplitterLayout with non-resizable areas & areas with min-width"}),`
+`,t.jsx(n,{of:c,sourceState:"none"}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show Code"}),t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-tsx",children:`function SplitterElementContent({ text, background = 'transparent' }) {
+  return (
+    <FlexBox style={{ height: '100%', width: '100%', background }} alignItems="Center" justifyContent="Center">
+      <Text>{text}</Text>
+    </FlexBox>
+  );
+}
+
+function SplitterLayoutComponent(props) {
+  return (
+    <SplitterLayout {...props}>
+      <SplitterElement size="10%">
+        <SplitterElementContent text="Content 1 - (size: 10%)" background="cadetblue" />
+      </SplitterElement>
+      <SplitterElement>
+        <SplitterElementContent text="Content 2" background="skyblue" />
+      </SplitterElement>
+      <SplitterElement minSize={200}>
+        <SplitterElementContent text="Content 3 (minSize: 200)" background="lightblue" />
+      </SplitterElement>
+      <SplitterElement>
+        <SplitterElementContent text="Content 4" background="paleturquoise" />
+      </SplitterElement>
+      <SplitterElement>
+        <SplitterElementContent text="Content 5" background="lightskyblue" />
+      </SplitterElement>
+      <SplitterElement resizable={false} size="150px">
+        <SplitterElementContent text="Content 6 (not resizable - size: 150px)" background="powderblue" />
+      </SplitterElement>
+      <SplitterElement>
+        <SplitterElementContent text="Content 7" background="aliceblue" />
+      </SplitterElement>
+      <SplitterElement>
+        <SplitterElementContent text="Content 8" background="lightsteelblue" />
+      </SplitterElement>
+    </SplitterLayout>
+  );
+}
+`})})]}),`
+`,t.jsx(e.h2,{id:"nested-splitterlayouts",children:"Nested SplitterLayouts"}),`
+`,t.jsx(n,{of:x}),`
+`,t.jsx(u,{children:C}),`
+`,t.jsx(e.h2,{id:"splitterelement",children:"SplitterElement"}),`
+`,t.jsx(S,{of:o}),`
+`,t.jsx(m,{of:o}),`
+`,t.jsx(a,{})]})}function se(r={}){const{wrapper:e}={...s(),...r.components};return e?t.jsx(e,{...r,children:t.jsx(p,{...r})}):p(r)}export{se as default};
