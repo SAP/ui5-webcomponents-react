6a2940668602b043d2afdeedf486d7d89860df06
MarcusNotheis <marcus.notheis@sap.com>
2024-05-28 06:18:30 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@8f2ca882e008f1ba8f96e3a29afc0da5c088cead 🚀

---
+++ main/assets/Form-DjVpnLTn.js
@@ -0,0 +1,228 @@
+import{j as n}from"./useIsomorphicLayoutEffect-wJjydpYF.js";import{useMDXComponents as u}from"./index-3-_tf5dF.js";import{M as l,C as o,d,D as r,A as m}from"./index-xTWMZvyD.js";import{F as i,a as p}from"./index-CDYkYSi4.js";import{C as c}from"./ControlsWithNote-rXDN3MoM.js";import{D as I}from"./DocsHeader-CY2bE8ee.js";import{F}from"./ProjectTemplate-CtBUC3q_.js";import"./index-BP8_t0zE.js";import{S as h}from"./SubcomponentsSection-BVy4lpQe.js";import{C as x,D as a,F as b,a as f,b as C}from"./Form.stories-CzPnIMuu.js";import"./iframe-BQcBeHpl.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DShc4-AB.js";import"./withWebComponent-CWrtEEjV.js";import"./utils-EaJipdFl.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-yz8rUzbr.js";import"./Media-BZGMoUh-.js";import"./MediaRange-CQ-a3KRn.js";import"./index-BVwS2KJm.js";import"./Title-IUvBxdmV.js";import"./UI5Element-DC8MZeOy.js";import"./Boot-DRGCpzpB.js";import"./WrappingType-CW8URInd.js";import"./parameters-bundle.css-CtgfEhuk.js";import"./useIsomorphicId-D4u2wl0X.js";import"./index-D_ia5Nk3.js";import"./Label-IIemeO1C.js";import"./i18nBundle-BdneKiEM.js";import"./i18n-defaults-DKt7VHOa.js";import"./utils-CRaa0H4j.js";import"./main-HWtSBowW.js";import"./index-BaujzPbe.js";import"./event-CegLCnR0.js";import"./slot-Df15G--e.js";import"./willShowContent-BOkh0bwj.js";import"./Icon-CmpPkOGs.js";import"./Icons-CG6FlOJ_.js";import"./Keys-BgUkNma0.js";import"./sys-help-2-BljVJXa3.js";import"./information-CWnHh7YX.js";import"./i18n-defaults-BdfZArTM.js";import"./alert-BiuXmsCV.js";import"./index-OBknn40-.js";import"./decline--LpgiSgn.js";import"./class-map-B9wLsnvZ.js";import"./Button-BrKU6WlN.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./HasPopup-Cecjtg2t.js";import"./index-D2vxCChD.js";import"./Popover-D2WBCDl9.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-C_R40Sx-.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BVkg1U_d.js";import"./FocusableElements-CrwTZl4-.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DEdNEOKg.js";import"./style-map-MmmenpP7.js";import"./BrowserScrollbar.css-cD_o1v9b.js";import"./index-E16DE_KT.js";import"./Link-Bsw9emR4.js";import"./chunk-HLWAVYOI-Bvig2Afn.js";import"./client-Di-4O7We.js";import"./copy-3Ib5ZJqq.js";import"./index-CVhCT1_x.js";import"./index-6h8N-xs_.js";import"./i18n-defaults-DzvGxznN.js";import"./I18nContext-RJHUyvz-.js";import"./index-CSI8GtFb.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./GitHub-Mark-BXZU7gws.js";import"./TableOfContent-BSmWWHeC.js";import"./index-DTpC8dA3.js";import"./addCustomCSSWithScoping-Bzep2v3d.js";import"./index-BriLVNcT.js";import"./index-DwZk-Wmd.js";import"./Avatar-CzFTtzk7.js";import"./employee-DAykk7OJ.js";import"./index-CsWO67mF.js";import"./index-C9Rzv41z.js";import"./ValueState-Bn-H2OaL.js";import"./ValueStateMessage.css-Caexp31Y.js";import"./index-DSdvn16C.js";import"./Input-B1woo9T2.js";import"./ResponsivePopoverCommon.css-BI8MPqTJ.js";import"./index-BsWy3nUY.js";import"./Select-BpJfwAqM.js";import"./InvisibleMessage-C0vBJgzv.js";import"./slim-arrow-down-D2RZ_g3E.js";import"./List-DhzEUdMj.js";import"./ItemNavigation-am3g3xGz.js";import"./TabbableElements-Ds_LZU0v.js";import"./debounce-jW346lN3.js";import"./BusyIndicator-CTBaiQfJ.js";import"./CheckBox-BZj6MKWp.js";import"./accept-5SoBdaHQ.js";import"./RadioButton-hFf0Ixfj.js";import"./ResponsivePopover-DzElSN_X.js";import"./Dialog-DHFoNWVc.js";import"./StandardListItem-Cdr6osPe.js";import"./ListItem-jW45-bGs.js";import"./slim-arrow-right-Cttx0bDU.js";import"./index-CzmHA2Ph.js";import"./index-B0wUxIkf.js";function s(t){const e={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",...u(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(l,{of:x}),`
+`,n.jsx(I,{since:"0.7.0",subComponents:["FormGroup","FormItem"]}),`
+`,n.jsxs(e.p,{children:[n.jsx(e.strong,{children:"Note:"}),' For more detailed examples of how the form can be used or integrated with other external libraries such as "React Hook Form", you can visit our ',n.jsx(e.a,{href:"https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base-form-handling--docs",rel:"nofollow",children:"knowledge base"}),"."]}),`
+`,n.jsx("br",{}),`
+`,n.jsx(e.h2,{id:"example",children:"Example"}),`
+`,n.jsx(o,{of:a}),`
+`,n.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,n.jsx(c,{of:a}),`
+`,n.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,n.jsx(e.h2,{id:"form-with-wrapped-formitems-and-formgroups",children:"Form with wrapped FormItems and FormGroups"}),`
+`,n.jsxs(e.p,{children:["This feature is supported ",n.jsx(e.strong,{children:"since v1.17.0"}),"."]}),`
+`,n.jsxs(e.p,{children:["It is possible to nest ",n.jsx(e.code,{children:"FormItem"}),"s and ",n.jsx(e.code,{children:"FormGroup"}),"s in custom components, the only thing you have to take care of is that no other elements (e.g. ",n.jsx(e.code,{children:"div"})," or ",n.jsx(e.code,{children:"span"}),") are mounted outside of FilterItems, as this will most likely result in breaking the layout of the ",n.jsx(e.code,{children:"Form"}),"."]}),`
+`,n.jsx(o,{of:b,sourceState:"none"}),`
+`,n.jsx(e.h3,{id:"code",children:"Code"}),`
+`,n.jsxs("details",{children:[n.jsx("summary",{children:"Show Code"}),n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-jsx",children:`const CustomComponent = ({ children }) => {
+  return <>{children}</>;
+};
+
+const CustomComponent2 = () => {
+  return (
+    <CustomComponent>
+      <FormGroup titleText="Group 1 inside custom component2">
+        <CustomComponent>
+          <FormItem label="FormItem 1 within group">
+            <Input />
+          </FormItem>
+          <FormItem label="FormItem 2 within group">
+            <Input />
+          </FormItem>
+        </CustomComponent>
+      </FormGroup>
+      <FormGroup titleText="Group 2 inside custom component2">
+        <FormItem label="FormItem 1 within group">
+          <Input />
+        </FormItem>
+        <FormItem label="FormItem 2 within group">
+          <Input />
+        </FormItem>
+      </FormGroup>
+    </CustomComponent>
+  );
+};
+
+const FormComponent = (props) => {
+  return (
+    <Form {...props}>
+      <FormItem label="Standalone FormItem">
+        <Input />
+      </FormItem>
+      <FormGroup titleText="Standalone FormGroup">
+        <FormItem label="Standalone FormItem within group">
+          <Input />
+        </FormItem>
+        <FormItem label="Standalone FormItem within group">
+          <Input />
+        </FormItem>
+      </FormGroup>
+      <FormItem label="Standalone FormItem">
+        <Input />
+      </FormItem>
+      <CustomComponent>
+        <FormItem label="FormItem within custom component">
+          <Input />
+        </FormItem>
+      </CustomComponent>
+      <FormGroup titleText="Standalone FormGroup with custom component">
+        <CustomComponent>
+          <FormItem label="FormItem 1 within custom component">
+            <Input />
+          </FormItem>
+          <FormItem label="FormItem 2 within custom component">
+            <Input />
+          </FormItem>
+        </CustomComponent>
+      </FormGroup>
+      <CustomComponent>
+        <FormGroup titleText="FormGroup within custom component">
+          <FormItem label="FormItem 1 within group">
+            <Input />
+          </FormItem>
+          <FormItem label="FormItem 2 within group">
+            <Input />
+          </FormItem>
+        </FormGroup>
+      </CustomComponent>
+      <CustomComponent>
+        <FormGroup titleText="Group 1 inside custom component">
+          <CustomComponent>
+            <FormItem label="FormItem 1 within group">
+              <Input />
+            </FormItem>
+            <FormItem label="FormItem 2 within group">
+              <Input />
+            </FormItem>
+          </CustomComponent>
+        </FormGroup>
+        <FormGroup titleText="Group 2 inside custom component">
+          <FormItem label="FormItem 1 within group">
+            <Input />
+          </FormItem>
+          <FormItem label="FormItem 2 within group">
+            <Input />
+          </FormItem>
+        </FormGroup>
+      </CustomComponent>
+      <CustomComponent2 />
+    </Form>
+  );
+};
+`})})]}),`
+`,n.jsx(e.h2,{id:"display-only-and-edit-mode",children:"Display-Only and Edit mode"}),`
+`,n.jsx(o,{of:f,sourceState:"none"}),`
+`,n.jsx(e.h3,{id:"code-1",children:"Code"}),`
+`,n.jsxs("details",{children:[n.jsx("summary",{children:"Show Code"}),n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-jsx",children:`const StandardField = ({ editMode, value, inputType = InputType.None, onInput, ...rest }) => {
+  if (editMode) {
+    return <Input value={value} style={{ width: '100%' }} type={inputType} onInput={onInput} {...rest} />;
+  }
+  if (inputType === InputType.URL || inputType === InputType.Email) {
+    return (
+      <Link href={inputType === InputType.Email ? \`mailto:\${value}\` : value} target="_blank" {...rest}>
+        {value}
+      </Link>
+    );
+  }
+  return <Text {...rest}>{value}</Text>;
+};
+
+const reducer = (state, { field, value }) => {
+  return { ...state, [field]: value };
+};
+
+const FormComponent = (props) => {
+  const [editMode, toggleEditMode] = useReducer((prev) => !prev, false, undefined);
+  const [formState, dispatch] = useReducer(
+    reducer,
+    {
+      name: 'Red Point Stores',
+      street: 'Main St 1618',
+      zip: 31415,
+      city: 'Maintown',
+      country: 'Germany',
+      web: 'https://www.sap.com',
+      mail: 'john.smith@sap.com',
+      twitter: '@sap',
+      phone: '+49 1234 56789'
+    },
+    undefined
+  );
+  const { zip, city, name, street, country, web, mail, twitter, phone } = formState;
+
+  const handleInput = (e) => {
+    dispatch({ field: Object.keys(e.target.dataset)[0], value: e.target.value });
+  };
+
+  return (
+    <>
+      <Button onClick={toggleEditMode}>Toggle {editMode ? 'Display-Only Mode' : 'Edit Mode'}</Button>
+      <Form
+        {...props}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <FormGroup titleText="Address">
+          <FormItem label="Name">
+            <StandardField editMode={editMode} value={name} onInput={handleInput} data-name />
+          </FormItem>
+          <FormItem label="Street">
+            <StandardField editMode={editMode} value={street} onInput={handleInput} data-street />
+          </FormItem>
+          <FormItem label="ZIP Code / City">
+            {editMode ? (
+              <>
+                <Input value={zip} type={InputType.Number} style={{ width: '30%' }} onInput={handleInput} data-zip />
+                <Input value={city} style={{ width: '70%' }} onInput={handleInput} data-city />
+              </>
+            ) : (
+              <Text>{\`\${zip} \${city}\`}</Text>
+            )}
+          </FormItem>
+          <FormItem label="Country">
+            <StandardField editMode={editMode} value={country} onInput={handleInput} data-country />
+          </FormItem>
+          <FormItem label="Web">
+            <StandardField editMode={editMode} value={web} inputType={InputType.URL} onInput={handleInput} data-web />
+          </FormItem>
+        </FormGroup>
+        <FormGroup titleText="Contact">
+          <FormItem label="Email">
+            <StandardField
+              editMode={editMode}
+              value={mail}
+              inputType={InputType.Email}
+              onInput={handleInput}
+              data-email
+            />
+          </FormItem>
+          <FormItem label="Twitter">
+            <StandardField editMode={editMode} value={twitter} onInput={handleInput} data-twitter />
+          </FormItem>
+          <FormItem label="Phone">
+            <StandardField
+              editMode={editMode}
+              value={phone}
+              inputType={InputType.Tel}
+              onInput={handleInput}
+              data-phone
+            />
+          </FormItem>
+        </FormGroup>
+      </Form>
+    </>
+  );
+};
+`})})]}),`
+`,n.jsx(e.h2,{id:"custom-screen-reader-announcements",children:"Custom Screen Reader Announcements"}),`
+`,n.jsx(e.p,{children:"In general we recommend using the default announcements of the Form, but if you need to adjust them anyway, then this example might help avoiding some of the typical pitfalls."}),`
+`,n.jsx(o,{of:C,sourceState:"none"}),`
+`,n.jsx(e.h3,{id:"code-2",children:"Code"}),`
+`,n.jsxs("details",{children:[n.jsx("summary",{children:"Show Code"}),n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-jsx",children:'function FormComponent() {\n  const uniqueId = useId();\n  return (\n    <Form\n      titleText="Not announced (because of `aria-label` of the `Form`)"\n      aria-label="Custom announcement of the form title via aria-label"\n    >\n      <FormGroup titleText="Default Group Announcement">\n        <FormItem label={<Label>Default announcement with custom Label</Label>}>\n          <Input />\n        </FormItem>\n      </FormGroup>\n      <FormGroup titleText="Not announced (because of `accessibleName` of the `Input`)">\n        <FormItem label={<Label>Not announced (because of `accessibleName` of the `Input`)</Label>}>\n          <Input accessibleName="Custom announcement via accessibleName prop" />\n        </FormItem>\n      </FormGroup>\n      <FormGroup titleText="Not announced (because of `accessibleNameRef` of the `Input`)">\n        <FormItem label={<Label>Not announced (because of `accessibleNameRef` of the `Input`)</Label>}>\n          <Input accessibleNameRef={`${uniqueId}-input1`} />\n          <span id={`${uniqueId}-input1`} className="pseudoInvisibleText">\n            Custom announcement via accessibleNameRef prop\n          </span>\n        </FormItem>\n      </FormGroup>\n      <FormGroup\n        titleText="Announced (because of `accessibleNameRef` of the `Input` and linking id)"\n        id={`${uniqueId}-group`}\n      >\n        <FormItem label={<Label>Not announced (because of `accessibleNameRef` of the `Input`)</Label>}>\n          <Input accessibleNameRef={`${uniqueId}-group ${uniqueId}-input2`} />\n          <span id={`${uniqueId}-input2`} className="pseudoInvisibleText">\n            Custom announcement via accessibleNameRef prop\n          </span>\n        </FormItem>\n      </FormGroup>\n    </Form>\n  );\n}\n'})})]}),`
+`,n.jsx(d,{children:h}),`
+`,n.jsx(e.h2,{id:"form-group",children:"Form Group"}),`
+`,n.jsx(r,{of:i}),`
+`,n.jsx(m,{of:i}),`
+`,n.jsx(e.h2,{id:"formitem",children:"FormItem"}),`
+`,n.jsx(r,{of:p}),`
+`,n.jsx(m,{of:p}),`
+`,n.jsx(F,{})]})}function Se(t={}){const{wrapper:e}={...u(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(s,{...t})}):s(t)}export{Se as default};
---
+++ main/assets/Toolbar-CWvUI6ee.js
@@ -0,0 +1,24 @@
+import{j as o}from"./useIsomorphicLayoutEffect-wJjydpYF.js";import{useMDXComponents as x}from"./index-3-_tf5dF.js";import{A as i}from"./ArgTypesWithNote-DtGkh69A.js";import{C as j}from"./ControlsWithNote-rXDN3MoM.js";import{D as f}from"./DocsHeader-CY2bE8ee.js";import{F as d}from"./ProjectTemplate-CtBUC3q_.js";import"./index-BP8_t0zE.js";import{S as h}from"./SubcomponentsSection-BVy4lpQe.js";import{M as b,C as u,d as S,D as p}from"./index-xTWMZvyD.js";import{C as T,D as m,T as n,a as e,b as s,c as a,d as l}from"./Toolbar.stories-CviMvNYI.js";import"./index-OBknn40-.js";import"./iframe-BQcBeHpl.js";import"../sb-preview/runtime.js";import"./UI5Element-DC8MZeOy.js";import"./Boot-DRGCpzpB.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-yz8rUzbr.js";import"./withWebComponent-CWrtEEjV.js";import"./utils-EaJipdFl.js";import"./slot-Df15G--e.js";import"./event-CegLCnR0.js";import"./i18nBundle-BdneKiEM.js";import"./decline--LpgiSgn.js";import"./Icons-CG6FlOJ_.js";import"./i18n-defaults-BdfZArTM.js";import"./information-CWnHh7YX.js";import"./alert-BiuXmsCV.js";import"./class-map-B9wLsnvZ.js";import"./Icon-CmpPkOGs.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-CtgfEhuk.js";import"./Button-BrKU6WlN.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./willShowContent-BOkh0bwj.js";import"./HasPopup-Cecjtg2t.js";import"./i18n-defaults-DKt7VHOa.js";import"./index-BxmsGmlx.js";import"./utils-CRaa0H4j.js";import"./main-HWtSBowW.js";import"./index-BaujzPbe.js";import"./sys-help-2-BljVJXa3.js";import"./WrappingType-CW8URInd.js";import"./index-D2vxCChD.js";import"./Popover-D2WBCDl9.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-C_R40Sx-.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BVkg1U_d.js";import"./FocusableElements-CrwTZl4-.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DEdNEOKg.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-MmmenpP7.js";import"./BrowserScrollbar.css-cD_o1v9b.js";import"./index-E16DE_KT.js";import"./Link-Bsw9emR4.js";import"./chunk-HLWAVYOI-Bvig2Afn.js";import"./client-Di-4O7We.js";import"./copy-3Ib5ZJqq.js";import"./index-CVhCT1_x.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DShc4-AB.js";import"./index-6h8N-xs_.js";import"./i18n-defaults-DzvGxznN.js";import"./I18nContext-RJHUyvz-.js";import"./index-CSI8GtFb.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./GitHub-Mark-BXZU7gws.js";import"./TableOfContent-BSmWWHeC.js";import"./index-D_ia5Nk3.js";import"./Label-IIemeO1C.js";import"./index-DTpC8dA3.js";import"./addCustomCSSWithScoping-Bzep2v3d.js";import"./index-BriLVNcT.js";import"./index-DwZk-Wmd.js";import"./Avatar-CzFTtzk7.js";import"./employee-DAykk7OJ.js";import"./index-CsWO67mF.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";import"./ValueState-Bn-H2OaL.js";import"./Select-BpJfwAqM.js";import"./InvisibleMessage-C0vBJgzv.js";import"./slim-arrow-down-D2RZ_g3E.js";import"./List-DhzEUdMj.js";import"./ItemNavigation-am3g3xGz.js";import"./TabbableElements-Ds_LZU0v.js";import"./debounce-jW346lN3.js";import"./BusyIndicator-CTBaiQfJ.js";import"./CheckBox-BZj6MKWp.js";import"./accept-5SoBdaHQ.js";import"./RadioButton-hFf0Ixfj.js";import"./ResponsivePopover-DzElSN_X.js";import"./Dialog-DHFoNWVc.js";import"./Title-IUvBxdmV.js";import"./StandardListItem-Cdr6osPe.js";import"./ListItem-jW45-bGs.js";import"./slim-arrow-right-Cttx0bDU.js";import"./ResponsivePopoverCommon.css-BI8MPqTJ.js";import"./ValueStateMessage.css-Caexp31Y.js";import"./overflow-CiF9u6y3.js";function c(t){const r={h2:"h2",...x(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(b,{of:T}),`
+`,o.jsx(f,{since:"1.19.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(u,{of:m}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(j,{of:m}),`
+`,o.jsx(S,{children:h}),`
+`,o.jsx(r.h2,{id:"toolbarbutton",children:"ToolbarButton"}),`
+`,o.jsx(p,{of:n}),`
+`,o.jsx(i,{of:n}),`
+`,o.jsx(r.h2,{id:"toolbarselect",children:"ToolbarSelect"}),`
+`,o.jsx(p,{of:e}),`
+`,o.jsx(i,{of:e}),`
+`,o.jsx(r.h2,{id:"toolbarselectoption",children:"ToolbarSelectOption"}),`
+`,o.jsx(p,{of:s}),`
+`,o.jsx(i,{of:s}),`
+`,o.jsx(r.h2,{id:"toolbarseparatorv2",children:"ToolbarSeparatorV2"}),`
+`,o.jsx(p,{of:a}),`
+`,o.jsx(i,{of:a}),`
+`,o.jsx(r.h2,{id:"toolbarspacerv2",children:"ToolbarSpacerV2"}),`
+`,o.jsx(p,{of:l}),`
+`,o.jsx(i,{of:l}),`
+`,o.jsx(d,{})]})}function Dr(t={}){const{wrapper:r}={...x(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(c,{...t})}):c(t)}export{Dr as default};
