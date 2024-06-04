d23d733c6caac5bb13d9a6e282f08be7774ced15
Marcus Notheis <marcus.notheis@sap.com>
2024-06-04 12:08:43 +0200

    Tree

--- packages/main/src/webComponents/Tree/index.tsx
+++ packages/main/src/webComponents/Tree/index.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import '@ui5/webcomponents/dist/Tree.js';
-import { withWebComponent } from '../../internal/withWebComponent.js';
-import type { CommonProps, Ui5CustomEvent, Ui5DomRef, UI5WCSlotsNode } from '../../types/index.js';
 import type {
   TreeItemClickEventDetail,
   TreeItemDeleteEventDetail,
@@ -12,8 +10,10 @@ import type {
   TreeSelectionChangeEventDetail,
   WalkCallback
 } from '@ui5/webcomponents/dist/Tree.js';
-import ListSelectionMode from '@ui5/webcomponents/dist/types/ListSelectionMode.js';
+import type ListSelectionMode from '@ui5/webcomponents/dist/types/ListSelectionMode.js';
 import type { ReactNode } from 'react';
+import { withWebComponent } from '../../internal/withWebComponent.js';
+import type { CommonProps, Ui5CustomEvent, Ui5DomRef, UI5WCSlotsNode } from '../../types/index.js';
 
 interface TreeAttributes {
   /**
