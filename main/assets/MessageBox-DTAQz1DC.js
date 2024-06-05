3d1104545c420b9e422af073dc13f9a9fcba9a48
MarcusNotheis <marcus.notheis@sap.com>
2024-06-05 12:16:13 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@8986ac0295327e166630da6ca8f541c107b7412a 🚀

---
+++ main/assets/MessageBox-DTAQz1DC.js
@@ -0,0 +1,88 @@
+import{j as o}from"./useIsomorphicLayoutEffect-VT0hzFUo.js";import{useMDXComponents as r}from"./index-3-_tf5dF.js";import{M as p,C as t}from"./index-B6nrNYOe.js";import{C as c}from"./ControlsWithNote-huUEJZoK.js";import{D as m}from"./DocsHeader-BD1GkvD4.js";import{F as a}from"./ProjectTemplate-CaUSXQZH.js";import"./index-BP8_t0zE.js";import{C as l,D as s,W as d}from"./MessageBox.stories-PYeKR6Nm.js";import"./iframe-DBY450V3.js";import"../sb-preview/runtime.js";import"./index-BxmsGmlx.js";import"./_baseForOwn-B3hW39GE.js";import"./mapValues-COmDofVS.js";import"./_baseUniq-BbzeZuBs.js";import"./index-m7PT-roW.js";import"./index-DrFu-skq.js";import"./utils-DZ9XrJRC.js";import"./main-HWtSBowW.js";import"./index-CiGqOtnK.js";import"./UI5Element-DwbFyBon.js";import"./Boot-BVnGU0ip.js";import"./EventProvider-B3ZIXKWe.js";import"./CustomElementsScopeUtils-B2x3tIsm.js";import"./withWebComponent-DvmwIdUK.js";import"./utils-neMnggoA.js";import"./event-CegLCnR0.js";import"./slot-Df15G--e.js";import"./i18nBundle-DbWkXQeQ.js";import"./willShowContent-BOkh0bwj.js";import"./Icon-_9Cp-e0V.js";import"./Icons-DyBGPyMo.js";import"./Keys-BgUkNma0.js";import"./parameters-bundle.css-kZpvVQc-.js";import"./sys-help-2-pwK9_awn.js";import"./information-5jLlktsT.js";import"./i18n-defaults-BdfZArTM.js";import"./alert-CJTybc-z.js";import"./WrappingType-CW8URInd.js";import"./i18n-defaults-CqU3c0Xc.js";import"./index-DAXiXxHH.js";import"./decline-CHA057wR.js";import"./class-map-CS8eahb3.js";import"./Button-BsmQPuS3.js";import"./AriaLabelHelper-CzOXVcye.js";import"./MarkedEvents-BPv6Lv4o.js";import"./HasPopup-Cecjtg2t.js";import"./index-DBWZ9gDv.js";import"./Popover-ComfN-sG.js";import"./Integer-Dh6YzFpK.js";import"./PopupUtils-CoIIx-oA.js";import"./getActiveElement-BL9Yqj8a.js";import"./PopupsCommon.css-BspShIhm.js";import"./FocusableElements-DEjr2b5c.js";import"./isElementHidden-Bpu2V8BK.js";import"./getEffectiveScrollbarStyle-DWzHjL-d.js";import"./ResizeHandler-DPru0u-P.js";import"./MediaRange-CQ-a3KRn.js";import"./style-map-RUCW1mQv.js";import"./BrowserScrollbar.css-BTaDXFx1.js";import"./index-CHqHHRdF.js";import"./Link-DIcSBYfZ.js";import"./chunk-HLWAVYOI-CoYialVj.js";import"./client-Di-4O7We.js";import"./copy-AJFuNtDX.js";import"./index-Dp6KfAFn.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DovReOaw.js";import"./index-CNIrU-iy.js";import"./i18n-defaults-DgugUEvt.js";import"./I18nContext-RJHUyvz-.js";import"./index-CL_5mZ89.js";import"./ThemingParameters-DOkJX3Ed.js";import"./ModalsContext-LNbsEfoS.js";import"./useIsomorphicId-D4u2wl0X.js";import"./GitHub-Mark-Dk-S7sqj.js";import"./TableOfContent-DKuhQ5dr.js";import"./index-DQs6Ypy4.js";import"./Label-8bLCWgMU.js";import"./index-CJep7e_E.js";import"./addCustomCSSWithScoping-VI82Stk0.js";import"./index-CXVGh6-l.js";import"./index-BoYH-i_e.js";import"./Avatar-Czn1NGIX.js";import"./employee-wCGkTOTe.js";import"./index-CRO125q2.js";import"./index-CEtbm8MU.js";import"./index-CBpU8qq5.js";import"./stopPropagation-BJWsenK5.js";import"./index-CcmymLAm.js";import"./Dialog-B4XCpv2T.js";import"./ValueState-Bn-H2OaL.js";import"./index-BUQixSk8.js";import"./Title-vmdMlxkV.js";import"./index-mQxkfkOS.js";import"./ValueState-gQDshf3o.js";function i(n){const e={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",strong:"strong",...r(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:l}),`
+`,o.jsx(m,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"example",children:"Example"}),`
+`,o.jsx(t,{of:s}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(c,{of:s}),`
+`,o.jsx(e.h2,{id:"opening-and-closing-messageboxes",children:"Opening and Closing MessageBoxes"}),`
+`,o.jsxs(e.p,{children:["Unlike ",o.jsx(e.code,{children:"Popovers"})," or ",o.jsx(e.code,{children:"Dialogs"}),", ",o.jsx(e.code,{children:"MessageBoxes"})," do not need to be provided with a ",o.jsx(e.code,{children:"ref"})," to be opened or closed. Instead, the ",o.jsx(e.code,{children:"open"})," prop can be used for this."]}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.strong,{children:"Note:"})," ",o.jsx(e.code,{children:"onClose"})," is either called by clicking on one of the action buttons in the footer or by pressing the ",o.jsx(e.code,{children:"ESC"})," key."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      <MessageBox open={open} onClose={handleClose}>
+        Content
+      </MessageBox>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h2,{id:"accessing-actions-in-the-onclose-callback",children:"Accessing actions in the onClose callback"}),`
+`,o.jsxs(e.p,{children:["To add some logic to the respective actions you can use ",o.jsx(e.code,{children:"event.detail.action"})," inside ",o.jsx(e.code,{children:"onClose"})," which contains the name of the clicked action."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  const handleClose = (event) => {
+    if (event.detail.action === MessageBoxActions.OK) {
+      // do something on "Ok" button click
+    } else if (event.detail.action === 'Custom Action') {
+      // do something on "Custom Action" click
+    } else {
+      // do something on "Cancel" or "Abort" button click
+    }
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      <MessageBox
+        open={open}
+        onClose={handleClose}
+        actions={[MessageBoxActions.OK, 'Custom Action', MessageBoxActions.Cancel, MessageBoxActions.Abort]}
+      >
+        Content
+      </MessageBox>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h2,{id:"using-messageboxes-inside-other-components",children:"Using MessageBoxes inside other components"}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.code,{children:"MessageBoxes"}),` are often used within other components, when opened this could sometimes have unwanted side effects.
+In this case, we recommend using `,o.jsx(e.a,{href:"https://reactjs.org/docs/portals.html",rel:"nofollow",children:"createPortal"})," to mount the ",o.jsx(e.code,{children:"MessageBox"})," outside of the DOM hierarchy of the parent component."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-JSX",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      {createPortal(
+        <MessageBox open={open} onClose={handleClose}>
+          Content
+        </MessageBox>,
+        document.body
+      )}
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"messagebox-with-custom-actions",children:"MessageBox with custom actions"}),`
+`,o.jsx(t,{of:d}),`
+`,o.jsx(a,{})]})}function ee(n={}){const{wrapper:e}={...r(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(i,{...n})}):i(n)}export{ee as default};
