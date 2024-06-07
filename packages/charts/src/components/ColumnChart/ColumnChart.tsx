0df559b156d1726268449b57da7856c05bf02003
Lukas Harbarth <lukas.harbarth@sap.com>
2024-06-07 12:06:45 +0200

    BulletChart: remove `defaultProps`, fix ts-errors

--- packages/charts/src/components/ColumnChart/ColumnChart.tsx
+++ packages/charts/src/components/ColumnChart/ColumnChart.tsx
@@ -233,7 +233,6 @@ const ColumnChart = forwardRef<HTMLDivElement, ColumnChartProps>((props, ref) =>
       resizeDebounce={chartConfig.resizeDebounce}
       {...propsWithoutOmitted}
     >
-      {/*@ts-expect-error: todo not yet compatible with React19*/}
       <ColumnChartLib
         syncId={syncId}
         onClick={onClickInternal}
