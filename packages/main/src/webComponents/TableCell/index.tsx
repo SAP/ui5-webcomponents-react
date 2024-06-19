c2ec9114ae1e4272161070eaea1266c60d5bf6cf
Marcus Notheis <marcus.notheis@sap.com>
2024-06-19 12:33:12 +0200

    update tablecell

--- packages/main/src/webComponents/TableCell/index.tsx
+++ packages/main/src/webComponents/TableCell/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import '@ui5/webcomponents/dist/TableCell.js';
 import type { ReactNode } from 'react';
 import { withWebComponent } from '../../internal/withWebComponent.js';
 import type { CommonProps, Ui5DomRef } from '../../types/index.js';
@@ -26,13 +25,8 @@ interface TableCellPropTypes extends TableCellAttributes, Omit<CommonProps, 'chi
  *
  * @since [2.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v2.0) of __@ui5/webcomponents__.
  */
-const TableCell = withWebComponent<TableCellPropTypes, TableCellDomRef>(
-  'ui5-table-cell',
-  [],
-  [],
-  [],
-  [],
-  () => import('@ui5/webcomponents/dist/TableCell.js')
+const TableCell = withWebComponent<TableCellPropTypes, TableCellDomRef>('ui5-table-cell', [], [], [], [], () =>
+  Promise.resolve()
 );
 
 TableCell.displayName = 'TableCell';
