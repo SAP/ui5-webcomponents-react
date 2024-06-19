20d8976954d151902585e5af7eb579b1de39b48b
Marcus Notheis <marcus.notheis@sap.com>
2024-06-19 15:05:39 +0200

    Table

--- packages/main/src/webComponents/Table/index.tsx
+++ packages/main/src/webComponents/Table/index.tsx
@@ -1,13 +1,8 @@
 'use client';
 
-import '@ui5/webcomponents-compat/dist/Table.js';
-import type {
-  TablePopinChangeEventDetail,
-  TableSelectionChangeEventDetail
-} from '@ui5/webcomponents-compat/dist/Table.js';
-import type { TableRowClickEventDetail } from '@ui5/webcomponents-compat/dist/TableRow.js';
-import type TableGrowingMode from '@ui5/webcomponents-compat/dist/types/TableGrowingMode.js';
-import type TableMode from '@ui5/webcomponents-compat/dist/types/TableMode.js';
+import '@ui5/webcomponents/dist/Table.js';
+import type { TableRowClickEventDetail } from '@ui5/webcomponents/dist/Table.js';
+import type TableOverflowMode from '@ui5/webcomponents/dist/types/TableOverflowMode.js';
 import type { ReactNode } from 'react';
 import { withWebComponent } from '../../internal/withWebComponent.js';
 import type { CommonProps, Ui5CustomEvent, Ui5DomRef, UI5WCSlotsNode } from '../../types/index.js';
@@ -15,234 +10,166 @@ import type { CommonProps, Ui5CustomEvent, Ui5DomRef, UI5WCSlotsNode } from '../
 interface TableAttributes {
   /**
    * Defines the accessible ARIA name of the component.
-   *
-   * **Note:** Available since [v1.3.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v1.3.0) of **@ui5/webcomponents**.
-   * @default undefined
    */
-  accessibleName?: string | undefined;
+  accessibleName?: string;
 
   /**
-   * Receives id(or many ids) of the elements that label the component.
-   *
-   * **Note:** Available since [v1.3.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v1.3.0) of **@ui5/webcomponents**.
+   * Identifies the element (or elements) that labels the component.
    */
   accessibleNameRef?: string;
 
   /**
-   * Defines if the table is in busy state.
+   * Defines if the loading indicator should be shown.
    *
-   * In this state the component's opacity is reduced
-   * and busy indicator is displayed at the bottom of the table.
+   * <b>Note:</b> When the component is loading, it is non-interactive.
    * @default false
    */
-  busy?: boolean;
+  loading?: boolean;
 
   /**
-   * Defines the delay in milliseconds, after which the busy indicator will show up for this component.
-   * @default 1000
-   */
-  busyDelay?: number;
-
-  /**
-   * Defines whether the table will have growing capability either by pressing a `More` button,
-   * or via user scroll. In both cases `load-more` event is fired.
-   *
-   * Available options:
-   *
-   * `Button` - Shows a `More` button at the bottom of the table, pressing of which triggers the `load-more` event.
-   *
-   * `Scroll` - The `load-more` event is triggered when the user scrolls to the bottom of the table;
-   *
-   * `None` (default) - The growing is off.
-   *
-   * **Restrictions:** `growing="Scroll"` is not supported for Internet Explorer,
-   * and the component will fallback to `growing="Button"`.
-   * @default "None"
-   */
-  growing?: TableGrowingMode | keyof typeof TableGrowingMode;
-
-  /**
-   * Defines the subtext that will be displayed under the `growingButtonText`.
-   *
-   * **Note:** This property takes effect if `growing` is set to `Button`.
-   */
-  growingButtonSubtext?: string;
-
-  /**
-   * Defines the text that will be displayed inside the growing button at the bottom of the table,
-   * meant for loading more rows upon press.
-   *
-   * **Note:** If not specified a built-in text will be displayed.
-   *
-   * **Note:** This property takes effect if `growing` is set to `Button`.
-   */
-  growingButtonText?: string;
-
-  /**
-   * Defines if the value of `noDataText` will be diplayed when there is no rows present in the table.
-   * @default false
-   */
-  hideNoData?: boolean;
-
-  /**
-   * Defines the mode of the component.
-   * @default "None"
-   */
-  mode?: TableMode | keyof typeof TableMode;
-
-  /**
-   * Defines the text that will be displayed when there is no data and `hideNoData` is not present.
+   * Defines the text to be displayed when there are no rows in the component.
    */
   noDataText?: string;
 
   /**
-   * Determines whether the column headers remain fixed at the top of the page during
-   * vertical scrolling as long as the Web Component is in the viewport.
+   * Defines the mode of the <code>ui5-table</code> overflow behavior.
    *
-   * **Restrictions:**
+   * Available options are:
    *
-   * - Browsers that do not support this feature:
+   * <code>Scroll</code> - Columns are shown as regular columns and horizontal scrolling is enabled.
    *
-   * - Internet Explorer
-   * - Microsoft Edge lower than version 41 (EdgeHTML 16)
-   * - Mozilla Firefox lower than version 59
-   *
-   * - Scrolling behavior:
-   *
-   * - If the Web Component is placed in layout containers that have the `overflow: hidden`
-   * or `overflow: auto` style definition, this can
-   * prevent the sticky elements of the Web Component from becoming fixed at the top of the viewport.
-   * @default false
+   * <code>Popin</code> - Columns are shown as pop-ins instead of regular columns.
+   * @default "Scroll"
    */
-  stickyColumnHeader?: boolean;
+  overflowMode?: TableOverflowMode | keyof typeof TableOverflowMode;
 }
 
 interface TableDomRef extends Required<TableAttributes>, Ui5DomRef {}
 
 interface TablePropTypes
   extends TableAttributes,
-    Omit<
-      CommonProps,
-      | keyof TableAttributes
-      | 'children'
-      | 'columns'
-      | 'onLoadMore'
-      | 'onPopinChange'
-      | 'onRowClick'
-      | 'onSelectionChange'
-    > {
+    Omit<CommonProps, keyof TableAttributes | 'children' | 'features' | 'headerRow' | 'nodata' | 'onRowClick'> {
   /**
-   * Defines the component rows.
+   * Defines the rows of the component.
    *
-   * **Note:** Use `TableRow` for the intended design.
+   * Note: Use <code>ui5-table-row</code> for the intended design.
    */
   children?: ReactNode | ReactNode[];
 
   /**
-   * Defines the configuration for the columns of the component.
+   * Defines the features of the component.
    *
-   * **Note:** Use `TableColumn` for the intended design.
-   *
-   * __Note:__ The content of the prop will be rendered into a [&lt;slot&gt;](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/slot) by assigning the respective [slot](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot) attribute (`slot="columns"`).
+   * __Note:__ The content of the prop will be rendered into a [&lt;slot&gt;](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/slot) by assigning the respective [slot](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot) attribute (`slot="features"`).
    * Since you can't change the DOM order of slots when declaring them within a prop, it might prove beneficial to manually mount them as part of the component's children, especially when facing problems with the reading order of screen readers.
    *
    * __Note:__ When passing a custom React component to this prop, you have to make sure your component reads the `slot` prop and appends it to the most outer element of your component.
    * Learn more about it [here](https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base-handling-slots--docs).
    */
-  columns?: UI5WCSlotsNode;
+  features?: UI5WCSlotsNode;
+
   /**
-   * Fired when the user presses the `More` button or scrolls to the table's end.
+   * Defines the header row of the component.
    *
-   * **Note:** The event will be fired if `growing` is set to `Button` or `Scroll`.
+   * Note: Use <code>ui5-table-header-row</code> for the intended design.
+   *
+   * __Note:__ The content of the prop will be rendered into a [&lt;slot&gt;](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/slot) by assigning the respective [slot](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot) attribute (`slot="headerRow"`).
+   * Since you can't change the DOM order of slots when declaring them within a prop, it might prove beneficial to manually mount them as part of the component's children, especially when facing problems with the reading order of screen readers.
+   *
+   * __Note:__ When passing a custom React component to this prop, you have to make sure your component reads the `slot` prop and appends it to the most outer element of your component.
+   * Learn more about it [here](https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base-handling-slots--docs).
    */
-  onLoadMore?: (event: Ui5CustomEvent<TableDomRef>) => void;
+  headerRow?: UI5WCSlotsNode;
 
   /**
-   * Fired when `TableColumn` is shown as a pop-in instead of hiding it.
+   * Defines the custom visualization if there is no data available.
+   *
+   * __Note:__ The content of the prop will be rendered into a [&lt;slot&gt;](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/slot) by assigning the respective [slot](https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot) attribute (`slot="nodata"`).
+   * Since you can't change the DOM order of slots when declaring them within a prop, it might prove beneficial to manually mount them as part of the component's children, especially when facing problems with the reading order of screen readers.
+   *
+   * __Note:__ When passing a custom React component to this prop, you have to make sure your component reads the `slot` prop and appends it to the most outer element of your component.
+   * Learn more about it [here](https://sap.github.io/ui5-webcomponents-react/?path=/docs/knowledge-base-handling-slots--docs).
    */
-  onPopinChange?: (event: Ui5CustomEvent<TableDomRef, TablePopinChangeEventDetail>) => void;
-
+  nodata?: UI5WCSlotsNode;
   /**
-   * Fired when a row in `Active` mode is clicked or `Enter` key is pressed.
+   * Fired when an interactive row is clicked.
    */
   onRowClick?: (event: Ui5CustomEvent<TableDomRef, TableRowClickEventDetail>) => void;
-
-  /**
-   * Fired when selection is changed by user interaction
-   * in `SingleSelect` and `MultiSelect` modes.
-   */
-  onSelectionChange?: (event: Ui5CustomEvent<TableDomRef, TableSelectionChangeEventDetail>) => void;
 }
 
 /**
- * The `Table` component provides a set of sophisticated and convenient functions for responsive table design.
- * It provides a comprehensive set of features for displaying and dealing with vast amounts of data.
+ * The `Table` component provides a set of sophisticated features for displaying and dealing with vast amounts of data in a responsive manner.
+ * To render the `Table`, you need to define the columns and rows. You can use the provided `TableHeaderRow` and `TableRow` components for this purpose.
  *
- * To render the `Table` properly, the order of the `columns` should match with the
- * order of the item `cells` in the `rows`.
+ * ### Features
  *
- * Desktop and tablet devices are supported.
- * On tablets, special consideration should be given to the number of visible columns
- * and rows due to the limited performance of some devices.
+ * The `Table` can be enhanced in its functionalities by applying different features.
+ * Features can be slotted into the `features` slot, to enable them in the component.
  *
- * ### Selection
- * To benefit from the selection mechanism of `Table` component, you can use the available selection modes:
- * `SingleSelect` and `MultiSelect`.
+ * The following features are currently available:
  *
- * In additition to the used mode, you can also specify the `TableRow` type choosing between
- * `Active` or `Inactive`.
- *
- * In `SingleSelect` mode, you can select both an `Active` and `Inactive` row via mouse or
- * by pressing the `Space` or `Enter` keys.
- *
- * In `MultiSelect` mode, you can select both an `Active` and `Inactive` row by pressing the
- * `Space` key when a row is on focus or via mouse click over the selection checkbox of the row.
- * In order to select all the available rows at once, you can use the selection checkbox presented in the table's header.
- *
- * **Note:** Currently, when a column is shown as a pop-in, the visual indication for selection is not presented over it.
+ * * [TableSelection](../TableSelection) - adds selection capabilities to the table
+ * * [TableGrowing](../TableGrowing) - provides growing capabilities to load more data
  *
  * ### Keyboard Handling
  *
- * #### Fast Navigation
  * This component provides a build in fast navigation group which can be used via [F6] / [Shift] + [F6] / [Ctrl] + [Alt/Option] / [Down] or [Ctrl] + [Alt/Option] + [Up].
  * In order to use this functionality, you need to import the following module:
  * `import "@ui5/webcomponents-base/dist/features/F6Navigation.js"`
  *
- * Furthermore, you can interact with `Table` via the following keys.
+ * Furthermore, you can interact with `Table` via the following keys:
  *
- * - [F7] - If focus is on an interactive control inside an item, moves focus to the corresponding item.
- * - [Ctrl]+[A] - Selects all items, if MultiSelect mode is enabled.
- * - [Home]/[End] - Focuses the first/last item.
- * - [Page Up]/[Page Down] - Moves focus up/down by page size (20 items by default).
- * - [Alt]+[Down]/[Up] - Switches focus between header, last focused item, and More button (if applies) in either direction.
- * - [Shift]+[Down]/[Up] - Selects the next/previous item in a MultiSelect table, if the current item is selected (Range selection). Otherwise, deselects them (Range deselection).
- * - [Shift]+[Home]/[End] - Range selection to the first/last item of the List.
- * - [Ctrl]+[Home]/[End] - Same behavior as HOME & END.
+ * If the focus is on a row, the following keyboard shortcuts are available:
+ * * <kbd>Down</kbd> - Navigates down
+ * * <kbd>Up</kbd> - Navigates up
+ * * <kbd>Right</kbd> - Selects the first cell of the row
+ * * <kbd>Space</kbd> - Toggles the selection of the row
+ * * <kbd>Ctrl/Cmd + A</kbd> - In multi selection mode, toggles the selection of all rows
+ * * <kbd>Home</kbd> - Navigates to the first row, if the focus is on the first row, navigates to the header row
+ * * <kbd>End</kbd> - Navigates to the last row, if the focus is on the last row, navigates to the growing button
+ * * <kbd>Page Up</kbd> - Navigates one page up, if the focus is on the first row, navigates to the header row
+ * * <kbd>Page Down</kbd> - Navigates one page down, if the focus is on the last row, navigates to the growing button
+ * * <kbd>F2</kbd> - Focuses the first tabbable element in the row
+ * * <kbd>F7</kbd> - If focus position is remembered, moves focus to the corresponding focus position row, otherwise to the first tabbable element within the row
+ * * <kbd>[Shift]Tab</kbd> - Move focus to the element in the tab chain outside the table
  *
  *
- * `import "@ui5/webcomponents/dist/TableColumn.js";` (`TableColumn`)
- * `import "@ui5/webcomponents/dist/TableRow.js";` (`TableRow`)
- * `import "@ui5/webcomponents/dist/TableGroupRow.js";` (`TableGroupRow`)
- * `import "@ui5/webcomponents/dist/TableCell.js";` (`TableCell`)
+ * If the focus is on a cell, the following keyboard shortcuts are available:
+ * * <kbd>Down</kbd> - Navigates down
+ * * <kbd>Up</kbd> - Navigates up
+ * * <kbd>Right</kbd> - Navigates right
+ * * <kbd>Left</kbd> - Navigates left, if the focus is on the first cell of the row, the focus is moved to the row.
+ * * <kbd>Home</kbd> - Navigates to the first cell of the current row, if the focus is on the first cell, navigates to the corresponding row
+ * * <kbd>End</kbd> - Navigates to the last cell of the current row, if the focus is on the last cell, navigates to the corresponding row
+ * * <kbd>Page Up</kbd> - Navigates one page up while keeping the focus in same column
+ * * <kbd>Page Down</kbd> - Navigates one page down while keeping the focus in same column
+ * * <kbd>F2</kbd> - Toggles the focus between the first tabbable cell content and the cell
+ * * <kbd>Enter</kbd> - Focuses the first tabbable cell content
+ * * <kbd>F7</kbd> - If the focus is on an interactive element inside a row, moves focus to the corresponding row and remembers the focus position of the element within the row
+ * * <kbd>[Shift]Tab</kbd> - Move focus to the element in the tab chain outside the table
+ *
+ *
+ * If the focus is on an interactive cell content, the following keyboard shortcuts are available:
+ * * <kbd>Down</kbd> - Move the focus to the interactive element in the same column of the previous row, unless the focused element prevents the default
+ * * <kbd>Up</kbd> - Move the focus to the interactive element in the same column of the next row, unless the focused element prevents the default
+ * * <kbd>[Shift]Tab</kbd> - Move the focus to the element in the tab chain
+ *
+ * \
+ * `import "@ui5/webcomponents/dist/TableRow.js";` (`TableRow`)\
+ * `import "@ui5/webcomponents/dist/TableCell.js";` (`TableCell`)\
+ * `import "@ui5/webcomponents/dist/TableHeaderRow.js";` (`TableHeaderRow`)\
+ * `import "@ui5/webcomponents/dist/TableHeaderCell.js";` (`TableHeaderCell`)
  *
  * __Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)
+ *
+ * @since [2.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v2.0) of __@ui5/webcomponents__.
  */
 const Table = withWebComponent<TablePropTypes, TableDomRef>(
   'ui5-table',
-  [
-    'accessibleName',
-    'accessibleNameRef',
-    'busyDelay',
-    'growing',
-    'growingButtonSubtext',
-    'growingButtonText',
-    'mode',
-    'noDataText'
-  ],
-  ['busy', 'hideNoData', 'stickyColumnHeader'],
-  ['columns'],
-  ['load-more', 'popin-change', 'row-click', 'selection-change'],
-  () => import('@ui5/webcomponents-compat/dist/Table.js')
+  ['accessibleName', 'accessibleNameRef', 'noDataText', 'overflowMode'],
+  ['loading'],
+  ['features', 'headerRow', 'nodata'],
+  ['row-click'],
+  () => import('@ui5/webcomponents/dist/Table.js')
 );
 
 Table.displayName = 'Table';
