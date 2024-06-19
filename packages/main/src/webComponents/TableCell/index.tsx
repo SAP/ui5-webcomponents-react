ac09d7742e34cb75c8be943e0be87a55fc5a9596
Marcus Notheis <marcus.notheis@sap.com>
2024-06-19 08:11:35 +0200

    TableCell

--- packages/main/src/webComponents/TableCell/index.tsx
+++ packages/main/src/webComponents/TableCell/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import '@ui5/webcomponents-compat/dist/TableCell.js';
+import '@ui5/webcomponents/dist/TableCell.js';
 import type { ReactNode } from 'react';
 import { withWebComponent } from '../../internal/withWebComponent.js';
 import type { CommonProps, Ui5DomRef } from '../../types/index.js';
@@ -11,15 +11,20 @@ interface TableCellDomRef extends Required<TableCellAttributes>, Ui5DomRef {}
 
 interface TableCellPropTypes extends TableCellAttributes, Omit<CommonProps, 'children'> {
   /**
-   * Specifies the content of the component.
+   * Defines the content of the component.
    */
   children?: ReactNode | ReactNode[];
 }
 
 /**
- * The `TableCell` component defines the structure of the data in a single `Table` cell.
+ * The `TableCell` represents a cell inside of a `Table`.
+ * It is tightly coupled to the `Table` and thus should only be used in the table component.
+ *
+ *
  *
  * __Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)
+ *
+ * @since [2.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v2.0) of __@ui5/webcomponents__.
  */
 const TableCell = withWebComponent<TableCellPropTypes, TableCellDomRef>(
   'ui5-table-cell',
@@ -27,7 +32,7 @@ const TableCell = withWebComponent<TableCellPropTypes, TableCellDomRef>(
   [],
   [],
   [],
-  () => import('@ui5/webcomponents-compat/dist/TableCell.js')
+  () => import('@ui5/webcomponents/dist/TableCell.js')
 );
 
 TableCell.displayName = 'TableCell';
