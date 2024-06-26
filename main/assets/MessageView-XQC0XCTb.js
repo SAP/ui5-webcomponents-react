d02e7acc5232c2ab9dcbe33633b851819e8059d9
Lukas742 <lukas.harbarth@sap.com>
2024-06-26 11:31:31 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@a30867ae33df291db40631bf585e4db3228dfec4 🚀

---
+++ main/assets/MessageView-XQC0XCTb.js
@@ -0,0 +1,72 @@
+import{j as e}from"./useIsomorphicLayoutEffect-CpdNMtv3.js";import{useMDXComponents as d}from"./index-44b7z5bc.js";import{A as r}from"./ArgTypesWithNote-DcZx0TjK.js";import{C as l}from"./ControlsWithNote-CNTvuR2c.js";import{D as h}from"./DocsHeader-CsrPnnjk.js";import{F as c}from"./ProjectTemplate-CCT_5O8i.js";import{C as x,D as n,M as g,W as j,a as s}from"./MessageView.stories-DQXyk_Qd.js";import{M as m}from"./index-Dsgqsfs7.js";import"./index-DEe9W-hO.js";import"./WrappingType-CW8URInd.js";import{M as u,C as i,d as f,D as a}from"./index-D38e1mtA.js";import{S as w}from"./SubcomponentsSection-BVy4lpQe.js";import"./index-Cf_xLf8r.js";import"./iframe-CtxApo3q.js";import"../sb-preview/runtime.js";import"./UI5Element-GBezswur.js";import"./Boot-CS6Wsrzl.js";import"./EventProvider-CVfkyUHo.js";import"./CustomElementsScopeUtils-CNP7-t5L.js";import"./withWebComponent-ystZv1Rq.js";import"./utils-DQyyc3O0.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BRH4h-L2.js";import"./decline-DXuOh8V-.js";import"./Icons-DkFvE6sF.js";import"./i18n-defaults-CXs2tJOZ.js";import"./information-7KDHAkBn.js";import"./alert-CC8GYBM5.js";import"./class-map-AxOwT-Dx.js";import"./Icon-DVdZH5Ds.js";import"./Device-RcFucVeJ.js";import"./Keys-B00bFMgT.js";import"./parameters-bundle.css-DQGkr9pp.js";import"./Button-BDXX69rx.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-BHzefwI9.js";import"./TagDesign-Dou_yO3g.js";import"./index-D6UH2S5y.js";import"./utils-DLSqN4cM.js";import"./main-gIccE3pA.js";import"./index-BfNION8S.js";import"./Tag-L9twqo1B.js";import"./sys-help-2-Bsa8Xj43.js";import"./index-C8bo7MNu.js";import"./Popover-D7imDdfT.js";import"./Integer-DgRYWdxf.js";import"./PopupsCommon.css-D0jBQZ8g.js";import"./FocusableElements-D2BM6TGM.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getEffectiveScrollbarStyle-BwuZEAK6.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DDwTWVjk.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BQrULRrw.js";import"./BrowserScrollbar.css-CyDwNnGM.js";import"./index-Cd9Fhl7C.js";import"./Link-C0ufom_G.js";import"./chunk-HLWAVYOI-CpaUtCuO.js";import"./client-BEgA9EFc.js";import"./copy-BlXZdPH6.js";import"./copy-CpBkh8z4.js";import"./index-CXVi7eMT.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-B1i3sL0q.js";import"./index-D455cKCd.js";import"./i18n-defaults-CnAElpi7.js";import"./I18nContext-qX06pm-v.js";import"./index-DucvDNQa.js";import"./ModalsContext-C4nEcU7T.js";import"./useIsomorphicId-CULvwDT0.js";import"./GitHub-Mark-W2HRRVtA.js";import"./TableOfContent-ffsdQ9yb.js";import"./index-Bi9C1mzi.js";import"./Label-B7GS_Fjb.js";import"./index-CNufUkyX.js";import"./addCustomCSSWithScoping-Cxt-Ii4P.js";import"./index-hb75QWE6.js";import"./index-Bq1EaMhz.js";import"./index-AdeAp6Or.js";import"./Avatar-BLWqVh2K.js";import"./employee-RhZDCU51.js";import"./List-k7C2Gn4s.js";import"./ItemNavigation-D-dZSClp.js";import"./isElementInView-CjfjvN8v.js";import"./debounce-TTkz9ISH.js";import"./BusyIndicator-B-2HBIhX.js";import"./ValueState-Bg0UWejw.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-BNgUtgiV.js";import"./CheckBox-D4hlQcIm.js";import"./accept-CvbfU8vl.js";import"./slim-arrow-right-pIbcHzBu.js";import"./index-DzFU4eEG.js";import"./index-B54VsbQ_.js";import"./ListItemCustom-BLOYcphR.js";import"./information-DsbOL1xQ.js";import"./Title-BnHhmA7_.js";import"./slim-arrow-left-CHr4Me90.js";import"./index-Bf_W5Jx7.js";import"./index-CcO4RJiC.js";import"./Dialog-CSIc5fqg.js";import"./index-DaB0Jwcz.js";import"./ResponsivePopover-D2fiRNXH.js";import"./index-Dxe-vf-o.js";import"./index-CAYsebbr.js";import"./index-DHBgECeF.js";import"./index-BqB17Jy_.js";import"./SegmentedButton-CuyURPJT.js";import"./GlobalStyleClasses-CJhxSfAI.js";import"./_baseForOwn-DVWQo5xw.js";import"./mapValues-DXlf5GIm.js";import"./_baseUniq-BqAhPu7p.js";import"./index-Ba91SQPT.js";import"./index-DrFu-skq.js";function p(o){const t={code:"code",em:"em",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...d(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(u,{of:x}),`
+`,e.jsx(h,{subComponents:["MessageItem","MessageViewButton"]}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(i,{of:n}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(l,{of:n}),`
+`,e.jsx(t.h2,{id:"methods",children:"Methods"}),`
+`,e.jsx(t.p,{children:"This component exposes public methods. You can invoke them directly on the instance of the component, e.g. by using React Refs."}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Name"}),e.jsx(t.th,{children:"Parameters"}),e.jsx(t.th,{children:"Description"})]})}),e.jsx(t.tbody,{children:e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"navigateBack"})})}),e.jsx(t.td,{children:"—"}),e.jsx(t.td,{children:"Navigates back to the list view if you are in the details view of a message"})]})})]}),`
+`,e.jsx(t.h2,{id:"usage-notes",children:"Usage Notes"}),`
+`,e.jsxs(t.p,{children:["The ",e.jsx(t.code,{children:"MessageView"})," can be used in several scenarios, but most likely in Dialogs or Popovers."]}),`
+`,e.jsxs(t.p,{children:["If used in a Popover it is recommended that the ",e.jsx(t.code,{children:"MessageViewButton"}),` is used as an opener.
+The `,e.jsx(t.code,{children:"type"})," of the button should always reflect the highest severity (Error -> Warning -> Success -> Information)."]}),`
+`,e.jsxs(t.p,{children:["If the ",e.jsx(t.code,{children:"MessageView"})," is used in a standalone way, you can set the prop ",e.jsx(t.code,{children:"showDetailsPageHeader"})," to ",e.jsx(t.code,{children:"true"}),`.
+This will add a bar to the details page where a back button is contained.`]}),`
+`,e.jsxs(t.p,{children:["When used in a ",e.jsx(t.code,{children:"Dialog"})," or a ",e.jsx(t.code,{children:"Popover"}),", we recommend not setting the ",e.jsx(t.code,{children:"showDetailsPageHeader"}),` prop but rather listen
+to the `,e.jsx(t.code,{children:"onItemSelect"})," event and add a back button to your Dialog or Popover header and use the ",e.jsx(t.code,{children:"navigateBack()"}),` method
+to get back to the list view.`]}),`
+`,e.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(t.h3,{id:"messageview-in-a-dialog",children:"MessageView in a Dialog"}),`
+`,e.jsx(i,{of:g}),`
+`,e.jsxs(t.p,{children:["Sample Code: ",e.jsx(t.em,{children:"(Button and Dialog opening are omitted here)"})]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`function MyComponent() {
+  const messageViewRef = useRef(null);
+  const [isOnDetailsPage, setIsOnDetailsPage] = useState(false);
+  return (
+    <Dialog
+      className="modal-without-padding"
+      header={
+        <Bar
+          startContent={
+            <FlexBox alignItems={FlexBoxAlignItems.Center}>
+              {isOnDetailsPage && (
+                <Button
+                  icon="slim-arrow-left"
+                  design={ButtonDesign.Transparent}
+                  onClick={() => {
+                    setIsOnDetailsPage(false);
+                    messageViewRef.current.navigateBack();
+                  }}
+                  style={{ marginInline: '0 0.5rem' }}
+                />
+              )}
+              <Title level={TitleLevel.H4}>Messages</Title>
+            </FlexBox>
+          }
+        />
+      }
+    >
+      <MessageView
+        ref={messageViewRef}
+        showDetailsPageHeader={false}
+        onItemSelect={() => {
+          setIsOnDetailsPage(true);
+        }}
+      >
+        {/* children */}
+      </MessageView>
+    </Dialog>
+  );
+}
+`})}),`
+`,e.jsx(t.h3,{id:"messageview-in-popover-with-messageviewbutton",children:"MessageView in Popover with MessageViewButton"}),`
+`,e.jsx(i,{of:j}),`
+`,e.jsx(f,{children:w}),`
+`,e.jsx(t.h2,{id:"messageitem",children:"MessageItem"}),`
+`,e.jsx(a,{of:s}),`
+`,e.jsx(r,{of:s}),`
+`,e.jsx(t.h2,{id:"messageviewbutton",children:"MessageViewButton"}),`
+`,e.jsx(a,{of:m}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(c,{})]})}function kt(o={}){const{wrapper:t}={...d(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(p,{...o})}):p(o)}export{kt as default};
---
+++ main/assets/ShellBar-YTK1b7Pi.js
@@ -0,0 +1,32 @@
+import{j as o}from"./useIsomorphicLayoutEffect-CpdNMtv3.js";import{useMDXComponents as e}from"./index-44b7z5bc.js";import{M as n,C as s,d as l,D as a,A as c}from"./index-D38e1mtA.js";import"./index-Cf_xLf8r.js";import{C as h}from"./ControlsWithNote-CNTvuR2c.js";import{D as d}from"./DocsHeader-CsrPnnjk.js";import{F as x}from"./ProjectTemplate-CCT_5O8i.js";import"./index-DEe9W-hO.js";import"./WrappingType-CW8URInd.js";import{S as j}from"./SubcomponentsSection-BVy4lpQe.js";import{C as f,D as i,S as p}from"./ShellBar.stories-BC1NJ-p6.js";import{e as u}from"./utils-DLSqN4cM.js";import"./iframe-CtxApo3q.js";import"../sb-preview/runtime.js";import"./index-D6UH2S5y.js";import"./_baseForOwn-DVWQo5xw.js";import"./mapValues-DXlf5GIm.js";import"./_baseUniq-BqAhPu7p.js";import"./index-Ba91SQPT.js";import"./index-DrFu-skq.js";import"./UI5Element-GBezswur.js";import"./Boot-CS6Wsrzl.js";import"./EventProvider-CVfkyUHo.js";import"./CustomElementsScopeUtils-CNP7-t5L.js";import"./withWebComponent-ystZv1Rq.js";import"./utils-DQyyc3O0.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BRH4h-L2.js";import"./decline-DXuOh8V-.js";import"./Icons-DkFvE6sF.js";import"./i18n-defaults-CXs2tJOZ.js";import"./information-7KDHAkBn.js";import"./alert-CC8GYBM5.js";import"./class-map-AxOwT-Dx.js";import"./Icon-DVdZH5Ds.js";import"./Device-RcFucVeJ.js";import"./Keys-B00bFMgT.js";import"./parameters-bundle.css-DQGkr9pp.js";import"./Button-BDXX69rx.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-BHzefwI9.js";import"./TagDesign-Dou_yO3g.js";import"./index-BfNION8S.js";import"./Tag-L9twqo1B.js";import"./sys-help-2-Bsa8Xj43.js";import"./index-C8bo7MNu.js";import"./Popover-D7imDdfT.js";import"./Integer-DgRYWdxf.js";import"./PopupsCommon.css-D0jBQZ8g.js";import"./FocusableElements-D2BM6TGM.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getEffectiveScrollbarStyle-BwuZEAK6.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DDwTWVjk.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BQrULRrw.js";import"./BrowserScrollbar.css-CyDwNnGM.js";import"./index-Cd9Fhl7C.js";import"./Link-C0ufom_G.js";import"./chunk-HLWAVYOI-CpaUtCuO.js";import"./client-BEgA9EFc.js";import"./copy-BlXZdPH6.js";import"./copy-CpBkh8z4.js";import"./index-CXVi7eMT.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-B1i3sL0q.js";import"./index-D455cKCd.js";import"./i18n-defaults-CnAElpi7.js";import"./I18nContext-qX06pm-v.js";import"./index-DucvDNQa.js";import"./ModalsContext-C4nEcU7T.js";import"./useIsomorphicId-CULvwDT0.js";import"./GitHub-Mark-W2HRRVtA.js";import"./TableOfContent-ffsdQ9yb.js";import"./index-Bi9C1mzi.js";import"./Label-B7GS_Fjb.js";import"./index-CNufUkyX.js";import"./addCustomCSSWithScoping-Cxt-Ii4P.js";import"./index-hb75QWE6.js";import"./index-Bq1EaMhz.js";import"./index-AdeAp6Or.js";import"./Avatar-BLWqVh2K.js";import"./employee-RhZDCU51.js";import"./Person-Bk5r7PMP.js";import"./add-B3M4vtFv.js";import"./add-DGWxuUsj.js";import"./search-MpCpidDP.js";import"./search-Dfj8V769.js";import"./index-Dhkmr7R7.js";import"./AriaHasPopup-BTjm9d28.js";import"./List-k7C2Gn4s.js";import"./ItemNavigation-D-dZSClp.js";import"./isElementInView-CjfjvN8v.js";import"./debounce-TTkz9ISH.js";import"./BusyIndicator-B-2HBIhX.js";import"./ValueState-Bg0UWejw.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-BNgUtgiV.js";import"./CheckBox-D4hlQcIm.js";import"./accept-CvbfU8vl.js";import"./slim-arrow-right-pIbcHzBu.js";import"./overflow-CjqN0q0M.js";import"./parameters-bundle.css-QMGUSIkp.js";import"./i18n-defaults-YaANGwN_.js";import"./index-ClZC3faf.js";import"./index-DVzjcltr.js";import"./Input-CNNAv05m.js";import"./ResponsivePopoverCommon.css-DngV6LBK.js";import"./ValueStateMessage.css-DblJ3QyE.js";import"./ResponsivePopover-D2fiRNXH.js";import"./Dialog-CSIc5fqg.js";import"./Title-BnHhmA7_.js";import"./index-DzFU4eEG.js";import"./main-gIccE3pA.js";function m(t){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...e(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:f}),`
+`,o.jsx(d,{since:"0.4.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(s,{of:i}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(h,{of:i}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h3,{id:"recipe-how-to-open-a-popover-on-shellbaritem-click",children:"Recipe: How to open a popover on ShellBarItem click?"}),`
+`,o.jsxs(r.p,{children:["To open a popover with the ",o.jsx(r.code,{children:"ShellBarItem"})," you can use the ",o.jsx(r.code,{children:"targetRef"})," property of the ",o.jsx(r.code,{children:"onClick"})," event."]}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-jsx",children:`const ShellBarComponent = () => {
+  const popoverRef = useRef(null);
+  const handleShellBarItemClick = (e) => {
+    popoverRef.current.showAt(e.detail.targetRef);
+  };
+  return (
+    <>
+      <ShellBar>
+        <ShellBarItem onClick={handleShellBarItemClick} icon="add" text="add" />
+      </ShellBar>
+      <Popover ref={popoverRef}>Hello there!</Popover>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(l,{children:j}),`
+`,o.jsx(r.h2,{id:"shellbaritem",children:"ShellBarItem"}),`
+`,o.jsx(a,{of:p}),`
+`,o.jsx(c,{hideHTMLPropsNote:!0,exclude:u,of:p}),`
+`,o.jsx(x,{})]})}function wr(t={}){const{wrapper:r}={...e(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{wr as default};
