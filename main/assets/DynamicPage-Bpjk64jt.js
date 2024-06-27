6d1f7f3d9daa7617ed2e8fa94f781bef7b800c0f
MarcusNotheis <marcus.notheis@sap.com>
2024-06-27 11:20:03 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@5bcccbbcd1cbe93e27eca6a4a111ad246dd97d63 🚀

---
+++ main/assets/DynamicPage-Bpjk64jt.js
@@ -0,0 +1,93 @@
+import{j as e}from"./useIsomorphicLayoutEffect-CpdNMtv3.js";import{useMDXComponents as m}from"./index-44b7z5bc.js";import{M as s,C as i}from"./index-B4TJEIgY.js";import"./index-LuLNju66.js";import{C as p}from"./ControlsWithNote-M0G9DxJI.js";import{D as a}from"./DocsHeader-DKGjphB5.js";import{F as d}from"./ProjectTemplate-CLtinoqp.js";import"./index-DEe9W-hO.js";import"./WrappingType-CW8URInd.js";import{C as h,D as r,S as c}from"./DynamicPage.stories-CePoGMzL.js";import"./iframe-C-zh5q8t.js";import"../sb-preview/runtime.js";import"./index-D6UH2S5y.js";import"./_baseForOwn-DVWQo5xw.js";import"./mapValues-DXlf5GIm.js";import"./_baseUniq-BqAhPu7p.js";import"./index-Ba91SQPT.js";import"./index-DrFu-skq.js";import"./UI5Element-GBezswur.js";import"./Boot-CS6Wsrzl.js";import"./EventProvider-CVfkyUHo.js";import"./CustomElementsScopeUtils-CNP7-t5L.js";import"./withWebComponent-ystZv1Rq.js";import"./utils-DQyyc3O0.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BRH4h-L2.js";import"./decline-DXuOh8V-.js";import"./Icons-DkFvE6sF.js";import"./i18n-defaults-CXs2tJOZ.js";import"./information-7KDHAkBn.js";import"./alert-CC8GYBM5.js";import"./class-map-AxOwT-Dx.js";import"./Icon-DVdZH5Ds.js";import"./Device-RcFucVeJ.js";import"./Keys-B00bFMgT.js";import"./parameters-bundle.css-DQGkr9pp.js";import"./Button-BDXX69rx.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-BHzefwI9.js";import"./TagDesign-Dou_yO3g.js";import"./utils-DDWs46MV.js";import"./main-gIccE3pA.js";import"./index-DKEtODBn.js";import"./Tag-L9twqo1B.js";import"./sys-help-2-Bsa8Xj43.js";import"./index-woEZMAlh.js";import"./Popover-D7imDdfT.js";import"./Integer-DgRYWdxf.js";import"./PopupsCommon.css-D0jBQZ8g.js";import"./FocusableElements-D2BM6TGM.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getEffectiveScrollbarStyle-BwuZEAK6.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DDwTWVjk.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BQrULRrw.js";import"./BrowserScrollbar.css-CyDwNnGM.js";import"./index-DexbhTjs.js";import"./Link-C0ufom_G.js";import"./chunk-HLWAVYOI-BYiw5dBp.js";import"./client-BEgA9EFc.js";import"./copy-BlXZdPH6.js";import"./copy-CpBkh8z4.js";import"./index-B6HtmUYb.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-B1i3sL0q.js";import"./index-D455cKCd.js";import"./i18n-defaults-CnAElpi7.js";import"./I18nContext-qX06pm-v.js";import"./index-DucvDNQa.js";import"./ModalsContext-C4nEcU7T.js";import"./useIsomorphicId-CULvwDT0.js";import"./GitHub-Mark-W2HRRVtA.js";import"./TableOfContent-DmgsRN25.js";import"./index-CPUVJ5Mz.js";import"./Label-B7GS_Fjb.js";import"./index-BJLKZy-L.js";import"./addCustomCSSWithScoping-Cxt-Ii4P.js";import"./index-B60cVvq6.js";import"./index-MKQtZNd-.js";import"./index-CmmNuBv4.js";import"./Avatar-BLWqVh2K.js";import"./employee-RhZDCU51.js";import"./index-C0oAdn8M.js";import"./RadioButton-BNgUtgiV.js";import"./ValueState-Bg0UWejw.js";import"./TabbableElements-BJJu_CDc.js";import"./BusyIndicator-B-2HBIhX.js";import"./CheckBox-D4hlQcIm.js";import"./accept-CvbfU8vl.js";import"./index-BdTIVXpf.js";import"./decline-BKJP2WGI.js";import"./ThemingParameters-DOkJX3Ed.js";import"./index-DAAh2_21.js";import"./information-DsbOL1xQ.js";import"./index-C1rNd0It.js";import"./index-CMxFvgmw.js";import"./InvisibleMessage-D8v2f_2q.js";import"./debounce-TTkz9ISH.js";import"./parameters-bundle.css-QMGUSIkp.js";import"./ToggleButton-DWmG7gW5.js";import"./slim-arrow-up-DgDpfWBx.js";import"./slim-arrow-down-CKJ7Ged_.js";import"./pushpin-on-DSP5eaav.js";import"./i18n-defaults-YaANGwN_.js";import"./ToolbarItemOverflowBehavior-B0qqzw6U.js";import"./index-BhGxsbhy.js";import"./overflow-CjqN0q0M.js";import"./AriaHasPopup-BTjm9d28.js";import"./index-DIdgRBRW.js";import"./Title-BnHhmA7_.js";import"./index-CUqEw4LA.js";import"./ItemNavigation-D-dZSClp.js";import"./ResponsivePopover-D2fiRNXH.js";import"./Dialog-CSIc5fqg.js";import"./List-k7C2Gn4s.js";import"./isElementInView-CjfjvN8v.js";import"./slim-arrow-right-pIbcHzBu.js";function n(o){const t={code:"code",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:h}),`
+`,e.jsx(a,{}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(i,{of:r}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(p,{of:r}),`
+`,e.jsx(t.h2,{id:"dynamicpage-with-sticky-subheaders",children:"DynamicPage with sticky subheaders"}),`
+`,e.jsxs(t.p,{children:[`The DynamicPage currently doesn't support sticky subheaders out of the box.
+This example shows how you could achieve this behavior by applying custom CSS and using a `,e.jsx(t.code,{children:"ResizeObserver"})," for measuring the header height."]}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"})," We only recommend applying custom CSS if the ",e.jsx(t.code,{children:"DynamicPage"})," is scrollable."]}),`
+`,e.jsx(i,{of:c,sourceState:"none"}),`
+`,e.jsx(t.p,{children:"These are the key points of this implementation:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["Using the ",e.jsx(t.code,{children:"part"})," pseudo-element selector to set the ",e.jsx(t.code,{children:"fit-content"})," part to use ",e.jsx(t.code,{children:"position: static;"}),"."]}),`
+`,e.jsxs(t.li,{children:["Adding a ",e.jsx(t.code,{children:"ResizeObserver"})," to measure the ",e.jsx(t.code,{children:"header"})," element inside the shadow root of the component."]}),`
+`,e.jsxs(t.li,{children:["Waiting for the custom element to be defined and for the ",e.jsx(t.code,{children:"shadowRoot"})," to be populated."]}),`
+`]}),`
+`,e.jsx(t.h3,{id:"code",children:"Code"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-css",children:`.dynamicPageStickyContent::part(fit-content) {
+  position: static;
+}
+`})}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`const useGetHeaderHeight = (dynamicPageRef: RefObject<DynamicPageDomRef & { shadowRoot: ShadowRoot }>) => {
+  const [headerHeight, setHeaderHeight] = useState<undefined | number>(undefined);
+  useEffect(() => {
+    const headerObserver = new ResizeObserver(([header]) => {
+      setHeaderHeight(header.contentRect.height);
+    });
+    if (dynamicPageRef.current) {
+      // wait for the custom element to be defined (adjust the tag-name if you're using the scoping feature)
+      void customElements.whenDefined('ui5-dynamic-page').then(() => {
+        const { shadowRoot } = dynamicPageRef.current;
+
+        // wait for the shadowRoot to be populated
+        const shadowRootObserver = new MutationObserver(() => {
+          const header = shadowRoot.querySelector('header');
+          if (header) {
+            shadowRootObserver.disconnect();
+            headerObserver.observe(header);
+          }
+        });
+
+        if (shadowRoot && shadowRoot.childElementCount) {
+          headerObserver.observe(shadowRoot.querySelector('header'));
+        } else {
+          shadowRootObserver.observe(shadowRoot, { childList: true });
+        }
+      });
+    }
+    return () => {
+      headerObserver.disconnect();
+    };
+  }, []);
+  return headerHeight;
+};
+
+function DynamicPageWithStickyContent(props) {
+  const dynamicPageRef = useRef<DynamicPageDomRef & { shadowRoot: ShadowRoot }>(null);
+  const headerHeight = useGetHeaderHeight(dynamicPageRef);
+
+  return (
+    <DynamicPage {...props} ref={dynamicPageRef} className="dynamicPageStickyContent">
+      <>
+        <div
+          style={{
+            position: 'sticky',
+            width: '100%',
+            height: '4rem',
+            background: 'lightgreen',
+            insetBlockStart: \`\${headerHeight}px\`
+          }}
+        >
+          Sticky Header
+        </div>
+        <div style={{ width: '100%', background: 'orange', height: '10rem' }}>Content</div>
+        <div
+          style={{
+            position: 'sticky',
+            width: '100%',
+            height: '8rem',
+            background: 'lightgreen',
+            insetBlockStart: \`calc(\${headerHeight}px + 4rem)\`
+          }}
+        >
+          Sticky Header 2
+        </div>
+        <div style={{ background: 'lightblue', height: '2000px', width: '100%' }}>Content</div>
+      </>
+    </DynamicPage>
+  );
+}
+`})}),`
+`,e.jsx(d,{})]})}function Dt(o={}){const{wrapper:t}={...m(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n(o)}export{Dt as default};
