605622997553ff52e2546bf2f6d20dc3a1c706b3
Lukas Harbarth <lukas.harbarth@sap.com>
2024-06-27 09:50:33 +0200

    Update Ui5CustomEvent.ts

--- packages/main/src/types/Ui5CustomEvent.ts
+++ packages/main/src/types/Ui5CustomEvent.ts
@@ -1,7 +1,3 @@
 export interface Ui5CustomEvent<EventTarget = HTMLElement, Detail = never> extends Omit<CustomEvent<Detail>, 'target'> {
   target: EventTarget;
 }
-
-export type EnrichedUi5CustomEvent<T, NewDetail> = T extends (event: infer E) => void
-  ? (event: E extends Ui5CustomEvent<infer ET, any> ? Ui5CustomEvent<ET, NewDetail> : never) => void
-  : never;
