a7881b0b0e218cf2286cd3f351340928cbdefb2b
Marcus Notheis <marcus.notheis@sap.com>
2024-07-01 16:33:16 +0200

    cleanup

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -1,6 +1,6 @@
-import { getI18nContext, I18nContext } from './context/I18nContext.js';
 import * as Device from './Device/index.js';
 import * as hooks from './hooks/index.js';
+import { I18nStore } from './stores/I18nStore.js';
 import { StyleStore } from './stores/StyleStore.js';
 import * as spacing from './styling/spacing.js';
 import { ThemingParameters } from './styling/ThemingParameters.js';
@@ -8,6 +8,5 @@ import { ThemingParameters } from './styling/ThemingParameters.js';
 export * from './styling/CssSizeVariables.js';
 export * from './utils/index.js';
 export * from './hooks/index.js';
-export * from './stores/I18nStore.js';
 
-export { getI18nContext, I18nContext, StyleStore, ThemingParameters, Device, hooks, spacing };
+export { I18nStore, StyleStore, ThemingParameters, Device, hooks, spacing };
