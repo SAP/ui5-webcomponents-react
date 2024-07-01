f1d089a23056e292efefd90eaefeb6f75d139198
Lukas742 <lukas.harbarth@sap.com>
2024-07-01 06:20:44 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0961da312bf83fd0e38a1f7505cc33cac8e3cac2 🚀

---
+++ main/assets/ActionSheet-DiMTRK2c.js
@@ -0,0 +1,59 @@
+import{j as e}from"./useIsomorphicLayoutEffect-CpdNMtv3.js";import{useMDXComponents as i}from"./index-44b7z5bc.js";import{M as p,C as m}from"./index-C7PzC4sj.js";import{M as s}from"./index-D4BBHF6O.js";import{C as c}from"./ControlsWithNote-BAMz9vYw.js";import{D as a}from"./DocsHeader-D-6n8DWF.js";import{F as h}from"./ProjectTemplate-C3742T7h.js";import"./index-DEe9W-hO.js";import"./WrappingType-CW8URInd.js";import{C as d,D as n}from"./ActionSheet.stories-C01UCNyL.js";import"./iframe-B5w2X3Ic.js";import"../sb-preview/runtime.js";import"./index-D6UH2S5y.js";import"./_baseForOwn-DVWQo5xw.js";import"./mapValues-DXlf5GIm.js";import"./_baseUniq-BqAhPu7p.js";import"./index-Ba91SQPT.js";import"./index-DrFu-skq.js";import"./UI5Element-GBezswur.js";import"./Boot-CS6Wsrzl.js";import"./EventProvider-CVfkyUHo.js";import"./CustomElementsScopeUtils-CNP7-t5L.js";import"./withWebComponent-ystZv1Rq.js";import"./utils-DQyyc3O0.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BRH4h-L2.js";import"./decline-DXuOh8V-.js";import"./Icons-DkFvE6sF.js";import"./i18n-defaults-CXs2tJOZ.js";import"./information-7KDHAkBn.js";import"./alert-CC8GYBM5.js";import"./class-map-AxOwT-Dx.js";import"./Icon-DVdZH5Ds.js";import"./Device-RcFucVeJ.js";import"./Keys-B00bFMgT.js";import"./parameters-bundle.css-DQGkr9pp.js";import"./Button-BDXX69rx.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-BHzefwI9.js";import"./TagDesign-Dou_yO3g.js";import"./utils-fA7plqoh.js";import"./main-gIccE3pA.js";import"./index-DuIROogU.js";import"./Tag-L9twqo1B.js";import"./sys-help-2-Bsa8Xj43.js";import"./index-DNL3eq44.js";import"./Popover-D7imDdfT.js";import"./Integer-DgRYWdxf.js";import"./PopupsCommon.css-D0jBQZ8g.js";import"./FocusableElements-D2BM6TGM.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getEffectiveScrollbarStyle-BwuZEAK6.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DDwTWVjk.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BQrULRrw.js";import"./BrowserScrollbar.css-CyDwNnGM.js";import"./index-Djx0ClgX.js";import"./Link-C0ufom_G.js";import"./chunk-HLWAVYOI-3OjWq_eS.js";import"./client-BEgA9EFc.js";import"./copy-BlXZdPH6.js";import"./copy-CpBkh8z4.js";import"./index-B6HtmUYb.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-B1i3sL0q.js";import"./index-D455cKCd.js";import"./i18n-defaults-CnAElpi7.js";import"./I18nContext-qX06pm-v.js";import"./index-DucvDNQa.js";import"./ModalsContext-C4nEcU7T.js";import"./useIsomorphicId-CULvwDT0.js";import"./GitHub-Mark-W2HRRVtA.js";import"./TableOfContent-CYkmyX5V.js";import"./index-aOpROyGe.js";import"./Label-B7GS_Fjb.js";import"./index-BInFhtFg.js";import"./addCustomCSSWithScoping-Cxt-Ii4P.js";import"./index-BeJT65Xk.js";import"./index-DQI5W9fP.js";import"./index-EpNWXkzT.js";import"./Avatar-BLWqVh2K.js";import"./employee-RhZDCU51.js";import"./accept-CvbfU8vl.js";import"./decline-BKJP2WGI.js";import"./delete-BUw4jorC.js";import"./index-DGU7M8wQ.js";import"./ResponsivePopover-D2fiRNXH.js";import"./Dialog-CSIc5fqg.js";import"./ValueState-Bg0UWejw.js";import"./Title-BnHhmA7_.js";function r(o){const t={code:"code",h2:"h2",p:"p",pre:"pre",strong:"strong",...i(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:d}),`
+`,e.jsx(a,{subComponents:["Button"]}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(m,{of:n}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(c,{of:n}),`
+`,e.jsx(t.h2,{id:"opening-actionsheets",children:"Opening ActionSheets"}),`
+`,e.jsx("br",{}),`
+`,e.jsxs(t.p,{children:["You can open and close the ",e.jsx(t.code,{children:"ActionSheet"})," component in a declarative way using the ",e.jsx(t.code,{children:"open"})," and ",e.jsx(t.code,{children:"opener"})," prop."]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`const MyComponentWithActionSheet = () => {
+  const [actionSheetIsOpen, setActionSheetIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        id={'openActionSheetBtn'}
+        onClick={() => {
+          setActionSheetIsOpen(true);
+        }}
+      >
+        Open Action Sheet
+      </Button>
+      <ActionSheet
+        opener={'openActionSheetBtn'}
+        open={actionSheetIsOpen}
+        onClose={() => {
+          setActionSheetIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,e.jsx(t.p,{children:e.jsxs(t.strong,{children:["Opening an ",e.jsx(t.code,{children:"ActionSheet"})," by reference and not by ",e.jsx(t.code,{children:"id"})]})}),`
+`,e.jsxs(t.p,{children:["The ",e.jsx(t.code,{children:"ActionSheet"})," exposes a way to pass a reference of an element instead of an ",e.jsx(t.code,{children:"id"})," to the ",e.jsx(t.code,{children:"opener"}),` prop.
+Since this is not supported when passing the reference in a declarative way to a React `,e.jsx(t.code,{children:"prop"}),`, you have to attach the ref directly on the web component.
+You can do that by e.g. leveraging a React Ref, and then set the corresponding property there.`]}),`
+`,e.jsx(s,{hideCloseButton:!0,children:`Changing properties outside of the React rendering cycle can lead to unwanted behavior, please be extra cautious when
+  using this approach!`}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`const ActionSheetComponent = () => {
+  const popoverRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const handleOpenerClick = (e) => {
+    if (popoverRef.current) {
+      popoverRef.current.opener = e.target;
+      setOpen((prev) => !prev);
+    }
+  };
+  return (
+    <>
+      <Button onClick={handleOpenerClick}>Opener</Button>
+      <ActionSheet ref={popoverRef} open={open}>
+        {/* Content */}
+      </ActionSheet>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(h,{})]})}function et(o={}){const{wrapper:t}={...i(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}export{et as default};
---
+++ main/assets/Wizard-DvVSKwwV.js
@@ -0,0 +1,126 @@
+import{j as e}from"./useIsomorphicLayoutEffect-CpdNMtv3.js";import{useMDXComponents as s}from"./index-44b7z5bc.js";import{A as p}from"./ArgTypesWithNote-C6DEvoxY.js";import{C as a}from"./ControlsWithNote-BAMz9vYw.js";import{D as m}from"./DocsHeader-D-6n8DWF.js";import{F as l}from"./ProjectTemplate-C3742T7h.js";import"./index-DEe9W-hO.js";import"./WrappingType-CW8URInd.js";import{M as u,C as d,d as c,D as h}from"./index-C7PzC4sj.js";import{C as g,D as n,W as r}from"./Wizard.stories-DP8Eatoo.js";import{S}from"./SubcomponentsSection-BVy4lpQe.js";import{e as f}from"./utils-fA7plqoh.js";import"./index-D4BBHF6O.js";import"./iframe-B5w2X3Ic.js";import"../sb-preview/runtime.js";import"./UI5Element-GBezswur.js";import"./Boot-CS6Wsrzl.js";import"./EventProvider-CVfkyUHo.js";import"./CustomElementsScopeUtils-CNP7-t5L.js";import"./withWebComponent-ystZv1Rq.js";import"./utils-DQyyc3O0.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BRH4h-L2.js";import"./decline-DXuOh8V-.js";import"./Icons-DkFvE6sF.js";import"./i18n-defaults-CXs2tJOZ.js";import"./information-7KDHAkBn.js";import"./alert-CC8GYBM5.js";import"./class-map-AxOwT-Dx.js";import"./Icon-DVdZH5Ds.js";import"./Device-RcFucVeJ.js";import"./Keys-B00bFMgT.js";import"./parameters-bundle.css-DQGkr9pp.js";import"./Button-BDXX69rx.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-BHzefwI9.js";import"./TagDesign-Dou_yO3g.js";import"./index-D6UH2S5y.js";import"./index-DuIROogU.js";import"./Tag-L9twqo1B.js";import"./sys-help-2-Bsa8Xj43.js";import"./index-DNL3eq44.js";import"./Popover-D7imDdfT.js";import"./Integer-DgRYWdxf.js";import"./PopupsCommon.css-D0jBQZ8g.js";import"./FocusableElements-D2BM6TGM.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getEffectiveScrollbarStyle-BwuZEAK6.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DDwTWVjk.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BQrULRrw.js";import"./BrowserScrollbar.css-CyDwNnGM.js";import"./index-Djx0ClgX.js";import"./Link-C0ufom_G.js";import"./chunk-HLWAVYOI-3OjWq_eS.js";import"./client-BEgA9EFc.js";import"./copy-BlXZdPH6.js";import"./copy-CpBkh8z4.js";import"./index-B6HtmUYb.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-B1i3sL0q.js";import"./index-D455cKCd.js";import"./i18n-defaults-CnAElpi7.js";import"./I18nContext-qX06pm-v.js";import"./index-DucvDNQa.js";import"./ModalsContext-C4nEcU7T.js";import"./useIsomorphicId-CULvwDT0.js";import"./GitHub-Mark-W2HRRVtA.js";import"./TableOfContent-CYkmyX5V.js";import"./index-aOpROyGe.js";import"./Label-B7GS_Fjb.js";import"./index-BInFhtFg.js";import"./addCustomCSSWithScoping-Cxt-Ii4P.js";import"./index-BeJT65Xk.js";import"./index-DQI5W9fP.js";import"./index-EpNWXkzT.js";import"./Avatar-BLWqVh2K.js";import"./employee-RhZDCU51.js";import"./_baseForOwn-DVWQo5xw.js";import"./mapValues-DXlf5GIm.js";import"./_baseUniq-BqAhPu7p.js";import"./index-Ba91SQPT.js";import"./index-DrFu-skq.js";import"./index-D2IUM0kM.js";import"./CheckBox-D4hlQcIm.js";import"./ValueState-Bg0UWejw.js";import"./accept-CvbfU8vl.js";import"./index-kftj9KCB.js";import"./Input-CNNAv05m.js";import"./ResponsivePopoverCommon.css-DngV6LBK.js";import"./ValueStateMessage.css-DblJ3QyE.js";import"./ResponsivePopover-D2fiRNXH.js";import"./Dialog-CSIc5fqg.js";import"./Title-BnHhmA7_.js";import"./index-yfQmneAV.js";import"./ItemNavigation-D-dZSClp.js";import"./Float-C8QKXwz5.js";import"./debounce-TTkz9ISH.js";import"./i18n-defaults-YaANGwN_.js";import"./parameters-bundle.css-QMGUSIkp.js";import"./main-gIccE3pA.js";function o(i){const t={code:"code",h2:"h2",pre:"pre",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(u,{of:g}),`
+`,e.jsx(m,{since:"0.12.0"}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(d,{of:n,sourceState:"none"}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:"Show Code"}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`const WizardComponent = () => {
+  const [selected, setSelected] = useState('1');
+  const [disabled, setDisabled] = useState<Record<string, boolean>>({ '2': true, '3': true, '4': true });
+  const [hidden, setHidden] = useState<Record<string, boolean>>({ step3Btn: true, finalizeBtn: true });
+  const goToStep2 = () => {
+    setDisabled((prev) => {
+      const { '2': _omit, ...rest } = prev;
+      return rest;
+    });
+    setSelected('2');
+  };
+  const goToStep3 = () => {
+    setDisabled((prev) => {
+      const { '3': _omit, ...rest } = prev;
+      return rest;
+    });
+    setSelected('3');
+  };
+  const handleStep2Completed = (e) => {
+    if (e.target.checked) {
+      setHidden((prev) => {
+        const { step3Btn: _omit, ...rest } = prev;
+        return rest;
+      });
+    }
+  };
+  const finalizeWizard = () => {
+    alert('Wizard is now completed!');
+  };
+  const handlePriceInput = (e) => {
+    if (e.target.value) {
+      setHidden((prev) => {
+        const { finalizeBtn: _omit, ...rest } = prev;
+        return rest;
+      });
+    }
+  };
+  const handleStepChange = (e) => {
+    setSelected(e.detail.step.dataset.step);
+    args.onStepChange(e);
+  };
+
+  return (
+    <Wizard {...args} onStepChange={handleStepChange}>
+      <WizardStep icon={productIcon} titleText="Product Type" selected={selected === '1'} data-step={'1'}>
+        <Title>1. Product Type</Title>
+        <MessageStrip design={MessageStripDesign.Information}>
+          The Wizard control is supposed to break down large tasks, into smaller steps, easier for the user to work
+          with.
+        </MessageStrip>
+        <Label wrappingType={WrappingType.None}>
+          Sed fermentum, mi et tristique ullamcorper, sapien sapien faucibus sem, quis pretium nibh lorem malesuada
+          diam. Nulla quis arcu aliquet, feugiat massa semper, volutpat diam. Nam vitae ante posuere, molestie neque sit
+          amet, dapibus velit. Maecenas eleifend tempor lorem. Mauris vitae elementum mi, sed eleifend ligula. Nulla
+          tempor vulputate dolor, nec dignissim quam convallis ut. Praesent vitae commodo felis, ut iaculis felis. Fusce
+          quis eleifend sapien, eget facilisis nibh. Suspendisse est velit, scelerisque ut commodo eget, dignissim quis
+          metus. Cras faucibus consequat gravida. Curabitur vitae quam felis. Phasellus ac leo eleifend, commodo tortor
+          et, varius quam. Aliquam erat volutpat
+        </Label>
+        <Button design={ButtonDesign.Emphasized} onClick={goToStep2}>
+          Step 2
+        </Button>
+      </WizardStep>
+      <WizardStep
+        icon={hintIcon}
+        titleText="Product Information"
+        disabled={disabled['2']}
+        selected={selected === '2'}
+        data-step={'2'}
+      >
+        <Title>2. Product Information</Title>
+        <Label wrappingType={WrappingType.None}>
+          Integer pellentesque leo sit amet dui vehicula, quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse
+          arcu metus, molestie a turpis a, molestie aliquet dui. Donec ppellentesque leo sit amet dui vehicula, quis
+          ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie aliquet
+          dui. Donec pulvinar, sapien corper eu, posuere malesuada nisl. Integer pellentesque leo sit amet dui vehicula,
+          quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie
+          aliquet dui. Donec pulvinar, sapien
+        </Label>
+        <CheckBox onChange={handleStep2Completed} text="Step Completed" />
+        <br />
+        {!hidden['step3Btn'] && (
+          <Button design={ButtonDesign.Emphasized} onClick={goToStep3}>
+            Step 3
+          </Button>
+        )}
+      </WizardStep>
+      <WizardStep
+        icon={leadIcon}
+        titleText="Pricing"
+        disabled={disabled['3']}
+        selected={selected === '3'}
+        data-step={'3'}
+      >
+        <Title>3. Pricing</Title>
+        <Label wrappingType={WrappingType.None}>
+          Integer pellentesque leo sit amet dui vehicula, quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse
+          arcu metus, molestie a turpis a, molestie aliquet dui. Donec ppellentesque leo sit amet dui vehicula, quis
+          ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie aliquet
+          dui. Donec pulvinar, sapien corper eu, posuere malesuada nisl. Integer pellentesque leo sit amet dui vehicula,
+          quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie
+          aliquet dui. Donec pulvinar, sapien
+        </Label>
+        <Input placeholder="Item Price" onInput={handlePriceInput} />
+        {Br}
+        {!hidden['finalizeBtn'] && (
+          <Button design={ButtonDesign.Emphasized} onClick={finalizeWizard}>
+            Finalize
+          </Button>
+        )}
+      </WizardStep>
+    </Wizard>
+  );
+};
+`})})]}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(a,{of:n}),`
+`,e.jsx(c,{children:S}),`
+`,e.jsx(t.h2,{id:"wizardstep",children:"WizardStep"}),`
+`,e.jsx(h,{of:r}),`
+`,e.jsx(p,{hideHTMLPropsNote:!0,exclude:f,of:r}),`
+`,e.jsx(l,{})]})}function St(i={}){const{wrapper:t}={...s(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(o,{...i})}):o(i)}export{St as default};
