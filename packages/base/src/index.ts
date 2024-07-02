815f7078377005deed7de07924e85adb7f04b0e2
Lukas Harbarth <lukas.harbarth@sap.com>
2024-07-02 17:34:31 +0200

    refactor(`spacing`): remove all `spacing` variables
    BREAKING CHANGE: All spacing variables have been removed. You can use common CSS classes as a substitute for most variables. For more information, please refer to our [Migration Guide](https://sap.github.io/ui5-webcomponents-react/main/?path=/docs/migration-guide--docs).

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -2,11 +2,10 @@ import { getI18nContext, I18nContext } from './context/I18nContext.js';
 import * as Device from './Device/index.js';
 import * as hooks from './hooks/index.js';
 import { StyleStore } from './stores/StyleStore.js';
-import * as spacing from './styling/spacing.js';
 import { ThemingParameters } from './styling/ThemingParameters.js';
 
 export * from './styling/CssSizeVariables.js';
 export * from './utils/index.js';
 export * from './hooks/index.js';
 
-export { getI18nContext, I18nContext, StyleStore, ThemingParameters, Device, hooks, spacing };
+export { getI18nContext, I18nContext, StyleStore, ThemingParameters, Device, hooks };
