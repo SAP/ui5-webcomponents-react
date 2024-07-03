b157c09805a7ed9206de0432471a72b2af9a3e63
MarcusNotheis <marcus.notheis@sap.com>
2024-07-03 10:15:24 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@671cfaade247894784863a27c98978a60a8646fd 🚀

---
+++ main/assets/MessageBox-B-DmGPPt.js
@@ -0,0 +1,88 @@
+import{j as o}from"./useIsomorphicLayoutEffect-CpdNMtv3.js";import{useMDXComponents as r}from"./index-44b7z5bc.js";import{M as p,C as t}from"./index-CkMGW5dy.js";import"./index-BnOnnAUQ.js";import{C as c}from"./ControlsWithNote-Br_n8DKQ.js";import{D as m}from"./DocsHeader-ui2S4w8x.js";import{F as a}from"./CommandsAndQueries-C-XG2k3I.js";import"./index-DEe9W-hO.js";import"./WrappingType-CW8URInd.js";import{C as l,D as s,W as d}from"./MessageBox.stories-hwLfXM5s.js";import"./iframe-C7_2vYGb.js";import"../sb-preview/runtime.js";import"./index-D6UH2S5y.js";import"./_baseForOwn-DVWQo5xw.js";import"./mapValues-DXlf5GIm.js";import"./_baseUniq-BqAhPu7p.js";import"./index-Ba91SQPT.js";import"./index-DrFu-skq.js";import"./UI5Element-B4CRQCUa.js";import"./CustomElementsScopeUtils-C6IqSY4u.js";import"./withWebComponent-DVfwWPVB.js";import"./utils-CKbRNFS-.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-NJJr-Wgh.js";import"./decline-BWlX1wee.js";import"./Icons-CwMCtZ-T.js";import"./i18n-defaults-CXs2tJOZ.js";import"./information-Dhbw5K85.js";import"./alert-7wwKJW96.js";import"./class-map-nHH1vjEv.js";import"./if-defined-BG38O0o4.js";import"./Icon-R7_7gBpk.js";import"./Device-RcFucVeJ.js";import"./Keys-B00bFMgT.js";import"./parameters-bundle.css-DQGkr9pp.js";import"./Button-C_Cuoo5O.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-vwvk3Exl.js";import"./TagDesign-Dou_yO3g.js";import"./utils-FziswtHn.js";import"./main-gIccE3pA.js";import"./index-9WZ_rGd4.js";import"./Tag-j81rNQk_.js";import"./sys-help-2-BIRNWtM-.js";import"./index-Blk6_UpZ.js";import"./Popover-CA74kJve.js";import"./Integer-DgRYWdxf.js";import"./PopupsCommon.css-C85aKd3d.js";import"./FocusableElements-bN8doqn4.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getEffectiveScrollbarStyle-BwuZEAK6.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-Bwj_klJ5.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-Chhl6hRu.js";import"./BrowserScrollbar.css-Dn2WHWng.js";import"./index-DDnW2Nv-.js";import"./Link-CjU86A1I.js";import"./chunk-HLWAVYOI-DcDlny4p.js";import"./client-BEgA9EFc.js";import"./copy-DRlh-Vu-.js";import"./copy-CgbAc49M.js";import"./index-Dj6pWtDJ.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BlIIJPI8.js";import"./index-B5idhwOL.js";import"./I18nStore-DrHVTNpv.js";import"./ModalsContext-C4nEcU7T.js";import"./useIsomorphicId-CULvwDT0.js";import"./GitHub-Mark-W2HRRVtA.js";import"./TableOfContent-BbLiwQ5q.js";import"./index-CwAmHk73.js";import"./Label-BA6opWOJ.js";import"./index-UwpWNXQ1.js";import"./index-TrDmqHyZ.js";import"./addCustomCSSWithScoping-DtQWRUC7.js";import"./index-DZvGQ-C8.js";import"./index-BzMpWh1I.js";import"./index-B4NGyBFZ.js";import"./Avatar-5-z1ijpg.js";import"./employee-CMmC12VS.js";import"./index-D4g-4_Dl.js";import"./Title-CBnhZj17.js";import"./ValueState-Bg0UWejw.js";import"./i18n-defaults-BQ1aH0X1.js";import"./index-Bt6M0qXN.js";import"./Dialog-rvxO7hW_.js";import"./index-DdnIjCB7.js";import"./index-_zz_z5qx.js";function i(n){const e={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",strong:"strong",...r(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:l}),`
+`,o.jsx(m,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"example",children:"Example"}),`
+`,o.jsx(t,{of:s}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(c,{of:s}),`
+`,o.jsx(e.h2,{id:"opening-and-closing-messageboxes",children:"Opening and Closing MessageBoxes"}),`
+`,o.jsxs(e.p,{children:["Unlike ",o.jsx(e.code,{children:"Popovers"})," or ",o.jsx(e.code,{children:"Dialogs"}),", ",o.jsx(e.code,{children:"MessageBoxes"})," do not need to be provided with a ",o.jsx(e.code,{children:"ref"})," to be opened or closed. Instead, the ",o.jsx(e.code,{children:"open"})," prop can be used for this."]}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.strong,{children:"Note:"})," ",o.jsx(e.code,{children:"onClose"})," is either called by clicking on one of the action buttons in the footer or by pressing the ",o.jsx(e.code,{children:"ESC"})," key."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      <MessageBox open={open} onClose={handleClose}>
+        Content
+      </MessageBox>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h2,{id:"accessing-actions-in-the-onclose-callback",children:"Accessing actions in the onClose callback"}),`
+`,o.jsxs(e.p,{children:["To add some logic to the respective actions you can use ",o.jsx(e.code,{children:"event.detail.action"})," inside ",o.jsx(e.code,{children:"onClose"})," which contains the name of the clicked action."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  const handleClose = (event) => {
+    if (event.detail.action === MessageBoxAction.OK) {
+      // do something on "Ok" button click
+    } else if (event.detail.action === 'Custom Action') {
+      // do something on "Custom Action" click
+    } else {
+      // do something on "Cancel" or "Abort" button click
+    }
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      <MessageBox
+        open={open}
+        onClose={handleClose}
+        actions={[MessageBoxAction.OK, 'Custom Action', MessageBoxAction.Cancel, MessageBoxAction.Abort]}
+      >
+        Content
+      </MessageBox>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h2,{id:"using-messageboxes-inside-other-components",children:"Using MessageBoxes inside other components"}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.code,{children:"MessageBoxes"}),` are often used within other components, when opened this could sometimes have unwanted side effects.
+In this case, we recommend using `,o.jsx(e.a,{href:"https://reactjs.org/docs/portals.html",rel:"nofollow",children:"createPortal"})," to mount the ",o.jsx(e.code,{children:"MessageBox"})," outside of the DOM hierarchy of the parent component."]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-JSX",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      {createPortal(
+        <MessageBox open={open} onClose={handleClose}>
+          Content
+        </MessageBox>,
+        document.body
+      )}
+    </>
+  );
+};
+`})}),`
+`,o.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"messagebox-with-custom-actions",children:"MessageBox with custom actions"}),`
+`,o.jsx(t,{of:d}),`
+`,o.jsx(a,{})]})}function $o(n={}){const{wrapper:e}={...r(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(i,{...n})}):i(n)}export{$o as default};
---
+++ main/assets/Popover-Doj_xy8M.js
@@ -0,0 +1,115 @@
+import{j as e}from"./useIsomorphicLayoutEffect-CpdNMtv3.js";import{useMDXComponents as i}from"./index-44b7z5bc.js";import{M as s,C as r}from"./index-CkMGW5dy.js";import{M as c}from"./index-BnOnnAUQ.js";import{C as m}from"./ControlsWithNote-Br_n8DKQ.js";import{D as h}from"./DocsHeader-ui2S4w8x.js";import{F as a}from"./CommandsAndQueries-C-XG2k3I.js";import"./index-DEe9W-hO.js";import"./WrappingType-CW8URInd.js";import{C as d,D as t,P as l}from"./Popover.stories-BPOOu5hk.js";import"./iframe-C7_2vYGb.js";import"../sb-preview/runtime.js";import"./index-D6UH2S5y.js";import"./_baseForOwn-DVWQo5xw.js";import"./mapValues-DXlf5GIm.js";import"./_baseUniq-BqAhPu7p.js";import"./index-Ba91SQPT.js";import"./index-DrFu-skq.js";import"./UI5Element-B4CRQCUa.js";import"./CustomElementsScopeUtils-C6IqSY4u.js";import"./withWebComponent-DVfwWPVB.js";import"./utils-CKbRNFS-.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-NJJr-Wgh.js";import"./decline-BWlX1wee.js";import"./Icons-CwMCtZ-T.js";import"./i18n-defaults-CXs2tJOZ.js";import"./information-Dhbw5K85.js";import"./alert-7wwKJW96.js";import"./class-map-nHH1vjEv.js";import"./if-defined-BG38O0o4.js";import"./Icon-R7_7gBpk.js";import"./Device-RcFucVeJ.js";import"./Keys-B00bFMgT.js";import"./parameters-bundle.css-DQGkr9pp.js";import"./Button-C_Cuoo5O.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-vwvk3Exl.js";import"./TagDesign-Dou_yO3g.js";import"./utils-FziswtHn.js";import"./main-gIccE3pA.js";import"./index-9WZ_rGd4.js";import"./Tag-j81rNQk_.js";import"./sys-help-2-BIRNWtM-.js";import"./index-Blk6_UpZ.js";import"./Popover-CA74kJve.js";import"./Integer-DgRYWdxf.js";import"./PopupsCommon.css-C85aKd3d.js";import"./FocusableElements-bN8doqn4.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getEffectiveScrollbarStyle-BwuZEAK6.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-Bwj_klJ5.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-Chhl6hRu.js";import"./BrowserScrollbar.css-Dn2WHWng.js";import"./index-DDnW2Nv-.js";import"./Link-CjU86A1I.js";import"./chunk-HLWAVYOI-DcDlny4p.js";import"./client-BEgA9EFc.js";import"./copy-DRlh-Vu-.js";import"./copy-CgbAc49M.js";import"./index-Dj6pWtDJ.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BlIIJPI8.js";import"./index-B5idhwOL.js";import"./I18nStore-DrHVTNpv.js";import"./ModalsContext-C4nEcU7T.js";import"./useIsomorphicId-CULvwDT0.js";import"./GitHub-Mark-W2HRRVtA.js";import"./TableOfContent-BbLiwQ5q.js";import"./index-CwAmHk73.js";import"./Label-BA6opWOJ.js";import"./index-UwpWNXQ1.js";import"./index-TrDmqHyZ.js";import"./addCustomCSSWithScoping-DtQWRUC7.js";import"./index-DZvGQ-C8.js";import"./index-BzMpWh1I.js";import"./index-B4NGyBFZ.js";import"./Avatar-5-z1ijpg.js";import"./employee-CMmC12VS.js";import"./settings-ChqA2vGi.js";import"./index-BQPhFIEo.js";import"./index-_zz_z5qx.js";import"./index-BgS0UjNC.js";import"./List-wOCQxiAM.js";import"./ItemNavigation-nDjVNi0g.js";import"./isElementInView-CjfjvN8v.js";import"./debounce-TTkz9ISH.js";import"./BusyIndicator-BzRbx7-c.js";import"./ValueState-Bg0UWejw.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-BAtc6syF.js";import"./CheckBox-CDByuKAI.js";import"./accept-B07L1jHF.js";import"./slim-arrow-right-CMQBLv6O.js";import"./index-BlARpbNd.js";import"./index-DdnIjCB7.js";import"./Title-CBnhZj17.js";function p(n){const o={a:"a",code:"code",em:"em",h1:"h1",h2:"h2",h4:"h4",p:"p",pre:"pre",strong:"strong",...i(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:d}),`
+`,e.jsx(h,{since:"0.9.0"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h2,{id:"example",children:"Example"}),`
+`,e.jsx(r,{of:t}),`
+`,e.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(m,{of:t}),`
+`,e.jsx(o.h2,{id:"opening-popovers",children:"Opening Popovers"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h4,{id:"since-0220",children:"since 0.22.0"}),`
+`,e.jsxs(o.p,{children:["We recommend opening and closing the ",e.jsx(o.code,{children:"Popover"})," component in a declarative way using the ",e.jsx(o.code,{children:"open"})," and ",e.jsx(o.code,{children:"opener"}),` prop.
+You can still use the imperative way which is outlined below.`]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`const MyComponentWithPopover = () => {
+  const [popoverIsOpen, setPopoverIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        id={'openPopoverBtn'}
+        onClick={() => {
+          setPopoverIsOpen(true);
+        }}
+      >
+        Open Popover
+      </Button>
+      <Popover
+        opener={'openPopoverBtn'}
+        open={popoverIsOpen}
+        onClose={() => {
+          setPopoverIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,e.jsx(o.p,{children:e.jsxs(o.strong,{children:["Opening a ",e.jsx(o.code,{children:"Popover"})," by reference and not by ",e.jsx(o.code,{children:"id"})]})}),`
+`,e.jsxs(o.p,{children:["This web component exposes a way to pass a reference of an element instead of an ",e.jsx(o.code,{children:"id"})," to the ",e.jsx(o.code,{children:"opener"})," prop. Since this is not supported when passing the reference in a declarative way to a React ",e.jsx(o.code,{children:"prop"}),`, you have to attach the ref directly on the web component.
+You can do that by e.g. leveraging a React Ref, and then set the corresponding property there.`]}),`
+`,e.jsx(c,{hideCloseButton:!0,children:`Changing properties outside of the React rendering cycle can lead to unwanted behavior, please be extra cautious when
+using this approach!`}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`const PopoverComponent = () => {
+  const popoverRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const handleOpenerClick = (e) => {
+    if (popoverRef.current) {
+      popoverRef.current.opener = e.target;
+      setOpen((prev) => !prev);
+    }
+  };
+  return (
+    <>
+      <Button onClick={handleOpenerClick}>Opener</Button>
+      <Popover ref={popoverRef} open={open}>
+        {/* Content */}
+      </Popover>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(o.h4,{id:"before-0220",children:"before 0.22.0"}),`
+`,e.jsxs(o.p,{children:[e.jsx(o.code,{children:"Popovers"})," can only be opened by attaching a ",e.jsx(o.code,{children:"ref"})," to the component and then calling the corresponding ",e.jsx(o.strong,{children:e.jsx(o.code,{children:"showAt"})})," method. The method receives the target element - ",e.jsxs(o.em,{children:["on which the ",e.jsx(o.code,{children:"Popover"})," is to be opened"]})," - as parameter."]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`const PopoverComponent = () => {
+  const popoverRef = useRef();
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Popover</Button>
+      <Popover ref={popoverRef}>Some Content</Popover>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(o.h2,{id:"using-popovers-inside-other-components",children:"Using Popovers inside other components"}),`
+`,e.jsxs(o.p,{children:[e.jsx(o.code,{children:"Popovers"}),` are often used within other components, when opened this could sometimes have unwanted side effects.
+In this case, we recommend using `,e.jsx(o.a,{href:"https://reactjs.org/docs/portals.html",rel:"nofollow",children:"createPortal"})," to mount the ",e.jsx(o.code,{children:"Popover"})," outside of the DOM hierarchy of the parent component."]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`const PopoverComponent = () => {
+  const popoverRef = useRef(null);
+  const onButtonClick = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Popover</Button>
+      {createPortal(<Popover ref={popoverRef} />, document.body)}
+    </>
+  );
+};
+`})}),`
+`,e.jsx(o.h2,{id:"closing-popovers",children:"Closing Popovers"}),`
+`,e.jsxs(o.p,{children:["Closing ",e.jsx(o.code,{children:"Popovers"}),` works in the same way as opening them.
+You can either set the `,e.jsx(o.code,{children:"open"})," prop to ",e.jsx(o.code,{children:"false"})," or attaching a ",e.jsx(o.code,{children:"ref"})," on which the corresponding ",e.jsx(o.code,{children:"close"})," method is called."]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`const PopoverComponent = () => {
+  const popoverRef = useRef(null);
+  const handleOpen = (e) => {
+    popoverRef.current.showAt(e.target);
+  };
+  const handleClose = () => {
+    popoverRef.current.close();
+  };
+  return (
+    <>
+      <Button onClick={handleOpen}>Open Popover</Button>
+      <Popover ref={popoverRef}>
+        <Button onClick={handleClose}>Close Popover</Button>
+      </Popover>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(o.h2,{id:"popover-with-content",children:"Popover with content"}),`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(a,{})]})}function mo(n={}){const{wrapper:o}={...i(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(p,{...n})}):p(n)}export{mo as default};
