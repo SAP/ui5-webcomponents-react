4da4e2674d310599b0141458271b83593bdaa4ba
Lukas742 <lukas.harbarth@sap.com>
2024-07-09 15:09:35 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c3c6f06dbfcbebf51abe0238e2eec7fe9dcb1d63 🚀

---
+++ main/assets/Select.stories-CM6pBU1m.js
@@ -0,0 +1,11 @@
+import{j as e}from"./useIsomorphicLayoutEffect-Cwmfti2R.js";import{o as a}from"./ValueState-Bg0UWejw.js";import{O as t}from"./index-BTOA177p.js";import{S as p}from"./index-DpFnupuK.js";const c={title:"Inputs / Select",component:p,argTypes:{children:{control:{disable:!0}},valueStateMessage:{control:{disable:!0}}},args:{valueState:a.None},tags:["package:@ui5/webcomponents"]},n={render:s=>e.jsxs(p,{...s,children:[e.jsx(t,{children:"Option 1"}),e.jsx(t,{children:"Option 2"}),e.jsx(t,{children:"Option 3"}),e.jsx(t,{children:"Option 4"}),e.jsx(t,{children:"Option 5"})]})};var o,r,i;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    return <Select {...args}>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+        <Option>Option 3</Option>
+        <Option>Option 4</Option>
+        <Option>Option 5</Option>
+      </Select>;
+  }
+}`,...(i=(r=n.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const l=["Default"],S=Object.freeze(Object.defineProperty({__proto__:null,Default:n,__namedExportsOrder:l,default:c},Symbol.toStringTag,{value:"Module"}));export{S as C,n as D};
