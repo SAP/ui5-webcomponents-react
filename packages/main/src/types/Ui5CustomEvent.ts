656ad5a47d3c35669455b3a8b03bc7bd422c31d2
Lukas Harbarth <lukas.harbarth@sap.com>
2024-08-06 14:03:28 +0200

    fix(UI5CustomEvent - TypeScript): correctly `currentTarget` type (#6167)
    Fixes #6136

--- packages/main/src/types/Ui5CustomEvent.ts
+++ packages/main/src/types/Ui5CustomEvent.ts
@@ -1,3 +1,5 @@
-export interface Ui5CustomEvent<EventTarget = HTMLElement, Detail = never> extends Omit<CustomEvent<Detail>, 'target'> {
+export interface Ui5CustomEvent<EventTarget = HTMLElement, Detail = never>
+  extends Omit<CustomEvent<Detail>, 'target' | 'currentTarget'> {
   target: EventTarget;
+  currentTarget: EventTarget | null;
 }
