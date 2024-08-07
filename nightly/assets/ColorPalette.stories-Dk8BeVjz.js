6867405a57872ee7a8826a187b046851ddfe0a46
Lukas742 <lukas.harbarth@sap.com>
2024-08-07 08:43:03 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@5f7d56b11b5864c22ca991b5be8ab7c36c9c573e 🚀

---
+++ nightly/assets/ColorPalette.stories-Dk8BeVjz.js
@@ -0,0 +1,45 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./index-BByVC2kA.js","./withWebComponent-CPW0iYAf.js","./utils-B4B9qFe5.js","./index-ClU-Tc1p.js","./useIsomorphicLayoutEffect-BDy32gDd.js","./slot-_4yKMUwC.js","./event-Dq0fpeHi.js","./i18nBundle-shm5Q6c6.js","./ItemNavigation-CIi6TzBH.js","./Keys-F_3Gvx0K.js","./getActiveElement-kltGt_DR.js","./class-map-CQfLTfub.js","./if-defined-BCPYWAh1.js","./Icon-BIxZr4TA.js","./parameters-bundle.css-DJWIHzS2.js","./Button-BsDFAdhY.js","./AriaLabelHelper-C5uDZewF.js","./MarkedEvents-CAur0wxK.js","./willShowContent-CZcfsNXp.js","./i18n-defaults-DRoh8QZU.js","./iframe-CnktcOiE.js","./style-map-Do0KrzH5.js"])))=>i.map(i=>d[i]);
+import{j as e}from"./useIsomorphicLayoutEffect-BDy32gDd.js";import{f as m,a as u}from"./utils-B4B9qFe5.js";import{f as c}from"./i18nBundle-shm5Q6c6.js";import d from"./Dialog-BhrO_1Jd.js";import{b as p}from"./Button-BsDFAdhY.js";import h from"./ColorPicker-DZIhQ-Qg.js";import{C as f,i as C,j as g}from"./i18n-defaults-DRoh8QZU.js";import{C as t}from"./index-BByVC2kA.js";import{_}from"./iframe-CnktcOiE.js";import{w as P}from"./withWebComponent-CPW0iYAf.js";const o=P("ui5-color-palette",[],[],[],["item-click"],()=>_(()=>import("./index-BByVC2kA.js").then(l=>l.b),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]),import.meta.url));o.displayName="ColorPalette";try{o.displayName="ColorPalette",o.__docgenInfo={description:`The \`ColorPalette\` provides the users with a range of predefined colors. The colors are fixed and do not change with the theme.
+
+### Usage
+
+The \`ColorPalette\` is meant for users that need to select a color from a predefined set.
+To define the colors, use the \`ColorPaletteItem\` component inside the default slot of the \`ColorPalette\`.
+
+
+
+__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)`,displayName:"ColorPalette",props:{children:{defaultValue:null,description:"Defines the `ColorPaletteItem` elements.",name:"children",required:!1,type:{name:"ReactNode | ReactNode[]"}},onItemClick:{defaultValue:null,description:"Fired when the user selects a color.",name:"onItemClick",required:!1,type:{name:"(event: Ui5CustomEvent<ColorPaletteDomRef, ColorPaletteItemClickEventDetail>) => void"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}try{o.displayName="ColorPalette",o.__docgenInfo={description:`The \`ColorPalette\` provides the users with a range of predefined colors. The colors are fixed and do not change with the theme.
+
+### Usage
+
+The \`ColorPalette\` is meant for users that need to select a color from a predefined set.
+To define the colors, use the \`ColorPaletteItem\` component inside the default slot of the \`ColorPalette\`.
+
+
+
+__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)`,displayName:"ColorPalette",props:{children:{defaultValue:null,description:"Defines the `ColorPaletteItem` elements.",name:"children",required:!1,type:{name:"ReactNode | ReactNode[]"}},onItemClick:{defaultValue:null,description:"Fired when the user selects a color.",name:"onItemClick",required:!1,type:{name:"(event: Ui5CustomEvent<ColorPaletteDomRef, ColorPaletteItemClickEventDetail>) => void"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}class n extends u{static get dependencies(){return[d,p,h]}static async define(){n.i18nBundle=await c("@ui5/webcomponents")}get colorPaletteDialogTitle(){return n.i18nBundle.getText(f)}get colorPaletteDialogOKButton(){return n.i18nBundle.getText(C)}get colorPaletteCancelButton(){return n.i18nBundle.getText(g)}}m("ColorPaletteMoreColors",n);const y={title:"Data Display / ColorPalette",component:o,argTypes:{children:{control:{disable:!0}}},args:{},tags:["package:@ui5/webcomponents"]},a={render:l=>e.jsxs(o,{...l,children:[e.jsx(t,{value:"darkblue"}),e.jsx(t,{value:"pink"}),e.jsx(t,{value:"#444444"}),e.jsx(t,{value:"rgb(0,200,0)"}),e.jsx(t,{value:"green"}),e.jsx(t,{value:"darkred"}),e.jsx(t,{value:"yellow"}),e.jsx(t,{value:"blue"}),e.jsx(t,{value:"cyan"}),e.jsx(t,{value:"orange"}),e.jsx(t,{value:"#5480e7"}),e.jsx(t,{value:"#ff6699"})]})};var r,s,i;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => {
+    return <ColorPalette {...args}>
+        <ColorPaletteItem value="darkblue" />
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="#444444" />
+        <ColorPaletteItem value="rgb(0,200,0)" />
+        <ColorPaletteItem value="green" />
+        <ColorPaletteItem value="darkred" />
+        <ColorPaletteItem value="yellow" />
+        <ColorPaletteItem value="blue" />
+        <ColorPaletteItem value="cyan" />
+        <ColorPaletteItem value="orange" />
+        <ColorPaletteItem value="#5480e7" />
+        <ColorPaletteItem value="#ff6699" />
+      </ColorPalette>;
+  }
+}`,...(i=(s=a.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const b=["Default"],S=Object.freeze(Object.defineProperty({__proto__:null,Default:a,__namedExportsOrder:b,default:y},Symbol.toStringTag,{value:"Module"}));export{S as C,a as D};
---
+++ nightly/assets/DonutChart.stories-Ds7uRUod.js
@@ -0,0 +1,55 @@
+import{s as T,d as F}from"./DemoProps-BWWtsAFv.js";import{j as I}from"./useIsomorphicLayoutEffect-BDy32gDd.js";import{r as V}from"./index-ClU-Tc1p.js";import{P as $}from"./PieChart-DKp_Vd6m.js";import"./index-C-Zfmu7Z.js";import"./clsx-B-dksMZM.js";import"./defaults-DzRBvhQX.js";import"./ChartContainer-kPB_H8X6.js";import"./_baseForOwn-CS3BKc8p.js";import"./_baseUniq-DGAWXpCf.js";import"./addCustomCSSWithScoping-BIehpVOD.js";import"./withWebComponent-CPW0iYAf.js";import"./utils-B4B9qFe5.js";import"./throttle-BhuRV_m0.js";import"./isString-t1AFzGBk.js";import"./useStylesheet-Dw38joME.js";import"./index-D-w4TPPw.js";import"./iframe-CnktcOiE.js";import"../sb-preview/runtime.js";import"./BusyIndicator-C9Q0CDgp.js";import"./i18nBundle-shm5Q6c6.js";import"./Keys-F_3Gvx0K.js";import"./Label-BcgafC0L.js";import"./i18n-defaults-DRoh8QZU.js";import"./if-defined-BCPYWAh1.js";import"./parameters-bundle.css-DJWIHzS2.js";import"./class-map-CQfLTfub.js";import"./index-Bkt4nOY5.js";import"./tiny-invariant-CCoILDQG.js";import"./staticProps-CzVU8Uee.js";import"./ThemingParameters-DOkJX3Ed.js";import"./useOnClickInternal-BlzHz4BI.js";import"./Placeholder-yHsr0K6w.js";import"./generateCategoricalChart-DoW9HLZq.js";import"./isPlainObject-ByEQ1glX.js";import"./mapValues-DmWmqx7o.js";import"./react-content-loader.es-BFHa96Im.js";const e=V.forwardRef((r,j)=>{const E={legendPosition:"bottom",paddingAngle:0,innerRadius:"50%",...r.chartConfig};return I.jsx($,{...r,ref:j,chartConfig:E})});e.displayName="DonutChart";try{e.displayName="DonutChart",e.__docgenInfo={description:"A `DonutChart` is a type of graph that displays data in a circular graph with a cut-out hole in the middle.\nThe pieces of the graph are proportional to the fraction of the whole in each category.\nA `DonutChart` is basically a `PieChart` with a hole.",displayName:"DonutChart",props:{}}}catch{}try{e.displayName="DonutChart",e.__docgenInfo={description:"A `DonutChart` is a type of graph that displays data in a circular graph with a cut-out hole in the middle.\nThe pieces of the graph are proportional to the fraction of the whole in each category.\nA `DonutChart` is basically a `PieChart` with a hole.",displayName:"DonutChart",props:{}}}catch{}const Wr={title:"DonutChart",component:e,argTypes:{dataset:{control:{disable:!0}},children:{control:{disable:!0}}},args:{dataset:T,dimension:{accessor:"name"},measure:{accessor:"users"}}},a={},t={args:{measure:{accessor:"users",colors:["#f00","green","var(--sapNegativeColor)","black","yellow","pink"]}}},o={args:{chartConfig:{paddingAngle:5}}},s={args:{chartConfig:{innerRadius:"20%",outerRadius:"90%"}}},i={args:{dataset:[]}},n={args:{dimension:{accessor:"name",formatter:r=>r.slice(0,3)},measure:{accessor:"users",formatter:r=>`${r/10}`}}},c={args:{chartConfig:{activeSegment:9,showActiveSegmentDataLabel:!0}}},m={args:{measure:{accessor:"users",hideDataLabel:r=>{if(r.percent<.01)return!0}},dataset:F}};var p,d,l;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(l=(d=a.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var u,h,g;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      colors: ['#f00', 'green', 'var(--sapNegativeColor)', 'black', 'yellow', 'pink']
+    }
+  }
+}`,...(g=(h=t.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var f,C,y;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    chartConfig: {
+      paddingAngle: 5
+    }
+  }
+}`,...(y=(C=o.parameters)==null?void 0:C.docs)==null?void 0:y.source}}};var D,S,_;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    chartConfig: {
+      innerRadius: '20%',
+      outerRadius: '90%'
+    }
+  }
+}`,...(_=(S=s.parameters)==null?void 0:S.docs)==null?void 0:_.source}}};var b,A,W;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    dataset: []
+  }
+}`,...(W=(A=i.parameters)==null?void 0:A.docs)==null?void 0:W.source}}};var w,v,P;n.parameters={...n.parameters,docs:{...(w=n.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    dimension: {
+      accessor: 'name',
+      formatter: el => el.slice(0, 3)
+    },
+    measure: {
+      accessor: 'users',
+      formatter: el => \`\${el / 10}\`
+    }
+  }
+}`,...(P=(v=n.parameters)==null?void 0:v.docs)==null?void 0:P.source}}};var R,L,N;c.parameters={...c.parameters,docs:{...(R=c.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    chartConfig: {
+      activeSegment: 9,
+      showActiveSegmentDataLabel: true
+    }
+  }
+}`,...(N=(L=c.parameters)==null?void 0:L.docs)==null?void 0:N.source}}};var x,k,H;m.parameters={...m.parameters,docs:{...(x=m.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      hideDataLabel: chartConfig => {
+        if (chartConfig.percent < 0.01) {
+          return true;
+        }
+      }
+    },
+    dataset: simpleDataSetWithSmallValues
+  }
+}`,...(H=(k=m.parameters)==null?void 0:k.docs)==null?void 0:H.source}}};const wr=["Default","WithCustomColor","WithPaddedAngle","WithCustomRadius","LoadingPlaceholder","WithFormatter","WithHighlightedActiveSegment","HideLabels"];export{a as Default,m as HideLabels,i as LoadingPlaceholder,t as WithCustomColor,s as WithCustomRadius,n as WithFormatter,c as WithHighlightedActiveSegment,o as WithPaddedAngle,wr as __namedExportsOrder,Wr as default};
