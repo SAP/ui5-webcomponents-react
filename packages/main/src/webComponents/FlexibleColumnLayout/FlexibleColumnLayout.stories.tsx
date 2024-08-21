2ad71da468dee0ab62e695f2c92d693f1268b96f
Lukas Harbarth <lukas.harbarth@sap.com>
2024-08-21 14:26:59 +0200

    chore: remove wcr `Toolbar` from main pkg (#6236)

--- packages/main/src/webComponents/FlexibleColumnLayout/FlexibleColumnLayout.stories.tsx
+++ packages/main/src/webComponents/FlexibleColumnLayout/FlexibleColumnLayout.stories.tsx
@@ -1,12 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import AvatarSize from '@ui5/webcomponents/dist/types/AvatarSize.js';
+import BarDesign from '@ui5/webcomponents/dist/types/BarDesign.js';
 import ButtonDesign from '@ui5/webcomponents/dist/types/ButtonDesign.js';
+import TitleLevel from '@ui5/webcomponents/dist/types/TitleLevel.js';
 import FCLLayout from '@ui5/webcomponents-fiori/dist/types/FCLLayout.js';
 import { useState } from 'react';
 import { FlexBox } from '../../components/FlexBox/index.js';
-import { Toolbar } from '../../components/Toolbar/index.js';
-import { ToolbarSpacer } from '../../components/ToolbarSpacer/index.js';
-import { FlexBoxDirection } from '../../enums/index.js';
+import { FlexBoxAlignItems, FlexBoxDirection } from '../../enums/index.js';
+import { Bar, Title } from '../../index.js';
 import { Text } from '../../webComponents/Text/index.js';
 import { Avatar } from '../Avatar/index.js';
 import { Button } from '../Button/index.js';
@@ -17,7 +18,6 @@ import { List } from '../List/index.js';
 import { ListItemStandard } from '../ListItemStandard/index.js';
 import { RatingIndicator } from '../RatingIndicator/index.js';
 import { ShellBar } from '../ShellBar/index.js';
-import { Title } from '../Title/index.js';
 import { FlexibleColumnLayout } from './index.js';
 
 const meta = {
@@ -71,240 +71,6 @@ export const Default: Story = {
 
 export const WithDifferentColumns: Story = {
   render() {
-    const movieData = [
-      {
-        movie: 'Shanghai',
-        genre: 'Crime|Thriller',
-        country: 'Russia'
-      },
-      {
-        movie: 'Punk Syndrome, The (Kovasikajuttu)',
-        genre: 'Documentary',
-        country: 'Poland'
-      },
-      {
-        movie: 'Corridors of Blood',
-        genre: 'Crime|Drama|Horror|Thriller',
-        country: 'Germany'
-      },
-      {
-        movie: 'Three Faces East',
-        genre: 'Drama|Mystery|War',
-        country: 'Russia'
-      },
-      {
-        movie: 'Grave Secrets (Silent Screams)',
-        genre: 'Horror',
-        country: 'Russia'
-      },
-      {
-        movie: 'Dead Outside, The',
-        genre: 'Horror|Mystery|Thriller',
-        country: 'Poland'
-      },
-      {
-        movie: 'Cosmos',
-        genre: 'Documentary',
-        country: 'Russia'
-      },
-      {
-        movie: 'UHF',
-        genre: 'Comedy',
-        country: 'Mexico'
-      },
-      {
-        movie: 'Superweib, Das',
-        genre: 'Comedy',
-        country: 'Poland'
-      },
-      {
-        movie: "It's Good to Be Alive",
-        genre: 'Drama',
-        country: 'Belarus'
-      },
-      {
-        movie: 'Last Action Hero',
-        genre: 'Action|Adventure|Comedy|Fantasy',
-        country: 'Russia'
-      },
-      {
-        movie: "Nibelungen: Kriemhild's Revenge, Die (Die Nibelungen: Kriemhilds Rache)",
-        genre: 'Adventure|Drama|Fantasy',
-        country: 'Russia'
-      },
-      {
-        movie: 'Roller Boogie',
-        genre: 'Drama',
-        country: 'Brazil'
-      },
-      {
-        movie: "Amore (L'Amore)",
-        genre: 'Drama',
-        country: 'Poland'
-      },
-      {
-        movie: 'Dust of Time, The',
-        genre: 'Drama',
-        country: 'Poland'
-      },
-      {
-        movie: 'Far Side of the Moon, The (Face cachée de la lune, La)',
-        genre: 'Drama',
-        country: 'Brazil'
-      },
-      {
-        movie: 'Take Care of Your Scarf, Tatiana (Pidä huivista kiinni, Tatjana)',
-        genre: 'Comedy',
-        country: 'Poland'
-      },
-      {
-        movie: 'Trail Beyond, The',
-        genre: 'Action|Adventure|Western',
-        country: 'Russia'
-      },
-      {
-        movie: "Mood Indigo (L'écume des jours)",
-        genre: 'Drama|Fantasy',
-        country: 'Russia'
-      },
-      {
-        movie: 'Alpha and Omega',
-        genre: 'Adventure|Animation|Children|Comedy|Romance',
-        country: 'France'
-      },
-      {
-        movie: 'Battleship Potemkin',
-        genre: 'Drama|War',
-        country: 'United Kingdom'
-      },
-      {
-        movie: 'Rites of May, The (Itim) ',
-        genre: 'Drama|Horror|Thriller',
-        country: 'Brazil'
-      },
-      {
-        movie: "Black Waters of Echo's Pond, The",
-        genre: 'Fantasy|Horror|Thriller',
-        country: 'France'
-      },
-      {
-        movie: 'Speed',
-        genre: 'Action|Romance|Thriller',
-        country: 'Brazil'
-      },
-      {
-        movie: 'Ladybird Ladybird',
-        genre: 'Drama',
-        country: 'Poland'
-      },
-      {
-        movie: 'War, Inc.',
-        genre: 'Comedy|Crime|Thriller',
-        country: 'Brazil'
-      },
-      {
-        movie: 'Company of Heroes',
-        genre: 'Action|War',
-        country: 'United States'
-      },
-      {
-        movie: 'Secret Glory, The',
-        genre: 'Documentary',
-        country: 'Poland'
-      },
-      {
-        movie: 'Three Musketeers, The',
-        genre: 'Action|Adventure|Romance',
-        country: 'France'
-      },
-      {
-        movie: 'Secrets of the Heart (Secretos del Corazón)',
-        genre: 'Drama',
-        country: 'Japan'
-      }
-    ];
-    const castData = [
-      {
-        name: 'Clywd Gimeno',
-        gender: 'Male'
-      },
-      {
-        name: 'Essie Gadson',
-        gender: 'Female'
-      },
-      {
-        name: 'Claresta Greger',
-        gender: 'Female'
-      },
-      {
-        name: 'Susanne Pinson',
-        gender: 'Female'
-      },
-      {
-        name: 'Gardener Cutford',
-        gender: 'Male'
-      },
-      {
-        name: 'Red Swyer-Sexey',
-        gender: 'Male'
-      },
-      {
-        name: 'Alyson Bachellier',
-        gender: 'Female'
-      },
-      {
-        name: 'Amata Penke',
-        gender: 'Female'
-      },
-      {
-        name: 'Ancell Aprahamian',
-        gender: 'Male'
-      },
-      {
-        name: 'Kimmy Sworne',
-        gender: 'Female'
-      },
-      {
-        name: 'Liam Umpleby',
-        gender: 'Male'
-      },
-      {
-        name: 'Caleb Trathen',
-        gender: 'Male'
-      },
-      {
-        name: 'Starlin Kibard',
-        gender: 'Female'
-      },
-      {
-        name: 'Keefe Brammall',
-        gender: 'Male'
-      },
-      {
-        name: 'Terrill Reeme',
-        gender: 'Male'
-      },
-      {
-        name: 'Gerianne Maciejak',
-        gender: 'Female'
-      },
-      {
-        name: 'Wilhelmine Cutmare',
-        gender: 'Female'
-      },
-      {
-        name: 'Rodie Breukelman',
-        gender: 'Female'
-      },
-      {
-        name: 'Eduard Berk',
-        gender: 'Male'
-      },
-      {
-        name: 'Phil Koppens',
-        gender: 'Female'
-      }
-    ];
     const [layout, setLayout] = useState(FCLLayout.OneColumn);
     const [selectedMovie, setSelectedMovie] = useState(movieData[0]);
     const [selectedCast, setSelectedCast] = useState(castData[0]);
@@ -327,7 +93,6 @@ export const WithDifferentColumns: Story = {
                 <img src="https://raw.githubusercontent.com/SAP/ui5-webcomponents-react/main/assets/Logo-Sticker.png" />
               }
               primaryTitle="Movie DB"
-              showCoPilot
             />
             <List headerText="Movies" onItemClick={onStartColumnClick}>
               {movieData.map((item) => (
@@ -340,20 +105,22 @@ export const WithDifferentColumns: Story = {
         }
         midColumn={
           <>
-            <Toolbar design="Solid">
-              <Title>{selectedMovie.movie}</Title>
-              <ToolbarSpacer />
-              <Button
-                icon="decline"
-                design={ButtonDesign.Transparent}
-                onClick={() => {
-                  setLayout(FCLLayout.OneColumn);
-                }}
-              />
-            </Toolbar>
-            <Toolbar style={{ height: '200px' }}>
-              <Avatar icon="video" size={AvatarSize.XL} style={{ marginLeft: '12px' }} />
-              <FlexBox direction={FlexBoxDirection.Column} style={{ marginLeft: '6px' }}>
+            <Bar
+              design={BarDesign.Header}
+              startContent={<Title>{selectedMovie.movie}</Title>}
+              endContent={
+                <Button
+                  icon="decline"
+                  design={ButtonDesign.Transparent}
+                  onClick={() => {
+                    setLayout(FCLLayout.OneColumn);
+                  }}
+                />
+              }
+            />
+            <FlexBox style={{ padding: '0.5rem 1rem' }} alignItems={FlexBoxAlignItems.Center}>
+              <Avatar icon="video" size={AvatarSize.XL} style={{ marginInline: '0.5rem' }} />
+              <FlexBox direction={FlexBoxDirection.Column}>
                 <FlexBox>
                   <Label>Movie:</Label>
                   <Text style={{ marginLeft: '2px' }}>{selectedMovie.movie}</Text>
@@ -367,8 +134,9 @@ export const WithDifferentColumns: Story = {
                   <Text style={{ marginLeft: '2px' }}>{selectedMovie.country}</Text>
                 </FlexBox>
               </FlexBox>
+              <span style={{ flexGrow: 1 }} />
               <RatingIndicator style={{ marginLeft: '100px' }} value={Math.floor(Math.random() * 5) + 1} />
-            </Toolbar>
+            </FlexBox>
             <List headerText="Cast" onItemClick={onMiddleColumnClick}>
               {castData.map((item) => (
                 <ListItemStandard description={item.gender} data-name={item.name} key={item.name}>
@@ -380,17 +148,19 @@ export const WithDifferentColumns: Story = {
         }
         endColumn={
           <>
-            <Toolbar design="Solid">
-              <Title>{selectedCast.name}</Title>
-              <ToolbarSpacer />
-              <Button
-                icon="decline"
-                design={ButtonDesign.Transparent}
-                onClick={() => {
-                  setLayout(FCLLayout.TwoColumnsMidExpanded);
-                }}
-              />
-            </Toolbar>
+            <Bar
+              design={BarDesign.Header}
+              startContent={<Title>{selectedCast.name}</Title>}
+              endContent={
+                <Button
+                  icon="decline"
+                  design={ButtonDesign.Transparent}
+                  onClick={() => {
+                    setLayout(FCLLayout.TwoColumnsMidExpanded);
+                  }}
+                />
+              }
+            />
             <Card
               header={
                 <CardHeader
@@ -426,3 +196,238 @@ export const WithDifferentColumns: Story = {
     );
   }
 };
+
+const movieData = [
+  {
+    movie: 'Shanghai',
+    genre: 'Crime|Thriller',
+    country: 'Russia'
+  },
+  {
+    movie: 'Punk Syndrome, The (Kovasikajuttu)',
+    genre: 'Documentary',
+    country: 'Poland'
+  },
+  {
+    movie: 'Corridors of Blood',
+    genre: 'Crime|Drama|Horror|Thriller',
+    country: 'Germany'
+  },
+  {
+    movie: 'Three Faces East',
+    genre: 'Drama|Mystery|War',
+    country: 'Russia'
+  },
+  {
+    movie: 'Grave Secrets (Silent Screams)',
+    genre: 'Horror',
+    country: 'Russia'
+  },
+  {
+    movie: 'Dead Outside, The',
+    genre: 'Horror|Mystery|Thriller',
+    country: 'Poland'
+  },
+  {
+    movie: 'Cosmos',
+    genre: 'Documentary',
+    country: 'Russia'
+  },
+  {
+    movie: 'UHF',
+    genre: 'Comedy',
+    country: 'Mexico'
+  },
+  {
+    movie: 'Superweib, Das',
+    genre: 'Comedy',
+    country: 'Poland'
+  },
+  {
+    movie: "It's Good to Be Alive",
+    genre: 'Drama',
+    country: 'Belarus'
+  },
+  {
+    movie: 'Last Action Hero',
+    genre: 'Action|Adventure|Comedy|Fantasy',
+    country: 'Russia'
+  },
+  {
+    movie: "Nibelungen: Kriemhild's Revenge, Die (Die Nibelungen: Kriemhilds Rache)",
+    genre: 'Adventure|Drama|Fantasy',
+    country: 'Russia'
+  },
+  {
+    movie: 'Roller Boogie',
+    genre: 'Drama',
+    country: 'Brazil'
+  },
+  {
+    movie: "Amore (L'Amore)",
+    genre: 'Drama',
+    country: 'Poland'
+  },
+  {
+    movie: 'Dust of Time, The',
+    genre: 'Drama',
+    country: 'Poland'
+  },
+  {
+    movie: 'Far Side of the Moon, The (Face cachée de la lune, La)',
+    genre: 'Drama',
+    country: 'Brazil'
+  },
+  {
+    movie: 'Take Care of Your Scarf, Tatiana (Pidä huivista kiinni, Tatjana)',
+    genre: 'Comedy',
+    country: 'Poland'
+  },
+  {
+    movie: 'Trail Beyond, The',
+    genre: 'Action|Adventure|Western',
+    country: 'Russia'
+  },
+  {
+    movie: "Mood Indigo (L'écume des jours)",
+    genre: 'Drama|Fantasy',
+    country: 'Russia'
+  },
+  {
+    movie: 'Alpha and Omega',
+    genre: 'Adventure|Animation|Children|Comedy|Romance',
+    country: 'France'
+  },
+  {
+    movie: 'Battleship Potemkin',
+    genre: 'Drama|War',
+    country: 'United Kingdom'
+  },
+  {
+    movie: 'Rites of May, The (Itim) ',
+    genre: 'Drama|Horror|Thriller',
+    country: 'Brazil'
+  },
+  {
+    movie: "Black Waters of Echo's Pond, The",
+    genre: 'Fantasy|Horror|Thriller',
+    country: 'France'
+  },
+  {
+    movie: 'Speed',
+    genre: 'Action|Romance|Thriller',
+    country: 'Brazil'
+  },
+  {
+    movie: 'Ladybird Ladybird',
+    genre: 'Drama',
+    country: 'Poland'
+  },
+  {
+    movie: 'War, Inc.',
+    genre: 'Comedy|Crime|Thriller',
+    country: 'Brazil'
+  },
+  {
+    movie: 'Company of Heroes',
+    genre: 'Action|War',
+    country: 'United States'
+  },
+  {
+    movie: 'Secret Glory, The',
+    genre: 'Documentary',
+    country: 'Poland'
+  },
+  {
+    movie: 'Three Musketeers, The',
+    genre: 'Action|Adventure|Romance',
+    country: 'France'
+  },
+  {
+    movie: 'Secrets of the Heart (Secretos del Corazón)',
+    genre: 'Drama',
+    country: 'Japan'
+  }
+];
+const castData = [
+  {
+    name: 'Clywd Gimeno',
+    gender: 'Male'
+  },
+  {
+    name: 'Essie Gadson',
+    gender: 'Female'
+  },
+  {
+    name: 'Claresta Greger',
+    gender: 'Female'
+  },
+  {
+    name: 'Susanne Pinson',
+    gender: 'Female'
+  },
+  {
+    name: 'Gardener Cutford',
+    gender: 'Male'
+  },
+  {
+    name: 'Red Swyer-Sexey',
+    gender: 'Male'
+  },
+  {
+    name: 'Alyson Bachellier',
+    gender: 'Female'
+  },
+  {
+    name: 'Amata Penke',
+    gender: 'Female'
+  },
+  {
+    name: 'Ancell Aprahamian',
+    gender: 'Male'
+  },
+  {
+    name: 'Kimmy Sworne',
+    gender: 'Female'
+  },
+  {
+    name: 'Liam Umpleby',
+    gender: 'Male'
+  },
+  {
+    name: 'Caleb Trathen',
+    gender: 'Male'
+  },
+  {
+    name: 'Starlin Kibard',
+    gender: 'Female'
+  },
+  {
+    name: 'Keefe Brammall',
+    gender: 'Male'
+  },
+  {
+    name: 'Terrill Reeme',
+    gender: 'Male'
+  },
+  {
+    name: 'Gerianne Maciejak',
+    gender: 'Female'
+  },
+  {
+    name: 'Wilhelmine Cutmare',
+    gender: 'Female'
+  },
+  {
+    name: 'Rodie Breukelman',
+    gender: 'Female'
+  },
+  {
+    name: 'Eduard Berk',
+    gender: 'Male'
+  },
+  {
+    name: 'Phil Koppens',
+    gender: 'Female'
+  }
+];
