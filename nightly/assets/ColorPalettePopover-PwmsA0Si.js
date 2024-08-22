5c173d257dcce679ace0761e9fcd1522bc2c396c
MarcusNotheis <marcus.notheis@sap.com>
2024-08-22 13:25:27 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@8184b4e98fea38ec1b1ba40a43fb31651bba8ac8 🚀

---
+++ nightly/assets/ColorPalettePopover-PwmsA0Si.js
@@ -0,0 +1,33 @@
+import{j as o}from"./useIsomorphicLayoutEffect-dLBi2t77.js";import{useMDXComponents as n}from"./index-CcnH5Kt0.js";import{ae as m,af as s,ag as a,ah as l,ai as c}from"./index-pKFJHhVg.js";import"./index-C11JCJl1.js";import{C as d}from"./ControlsWithNote-DyxqX0e-.js";import{D as x}from"./DocsHeader-B3i_-Txq.js";import{F as u}from"./CommandsAndQueries-DU1Yyi_C.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CE707q_n.js";import{C as h,D as e}from"./ColorPalettePopover.stories-iKu5UjkP.js";import{S as f}from"./SubcomponentsSection-BVy4lpQe.js";import{C as p}from"./index-B4E0NvU0.js";import"./iframe-Cdrg2OY6.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./_baseUniq-BBslp21L.js";import"./_overArg-Der2MB70.js";import"./_baseAssignValue-PZ39NPQP.js";import"./_getPrototype-DY0Wt4dR.js";import"./index-uexYa9xf.js";import"./index-DrFu-skq.js";import"./withWebComponent-_WQ2NUTk.js";import"./utils-DNCbgL6O.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-Cey6jF2F.js";import"./decline-BO1MJY4p.js";import"./Icon-F4CTdRe4.js";import"./Keys-F_3Gvx0K.js";import"./if-defined-B1auRMLy.js";import"./parameters-bundle.css-P4kwt3kq.js";import"./i18n-defaults-CdZCUmzZ.js";import"./information-T18VK69N.js";import"./alert-Cstw3ukw.js";import"./class-map-BevVk84l.js";import"./Button-B2giC9cI.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-QZuI92fE.js";import"./TagDesign-Dou_yO3g.js";import"./utils-CjrKvuRN.js";import"./main-BPTHlokf.js";import"./index-Plva8_hs.js";import"./Tag-CyMlqXkf.js";import"./sys-help-2-Dka1TfNv.js";import"./index-BVhWS7pb.js";import"./Popover-BSoFyw8J.js";import"./PopupsCommon.css-CgymFC1Z.js";import"./getEffectiveScrollbarStyle-D0HAoBVS.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-CdXdU0-i.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BtPMqoOr.js";import"./BrowserScrollbar.css-DMtZWp5G.js";import"./index-t275OLbF.js";import"./Link-D7mI2b7i.js";import"./chunk-HLWAVYOI-D_JkwjTc.js";import"./client-BZNFO7bv.js";import"./copy-ITQxovDS.js";import"./copy-Dq9dQZCS.js";import"./index-BOHkKvVj.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DKtOrKyD.js";import"./index-yNJmpRbB.js";import"./I18nStore-DtMIGu-N.js";import"./GitHub-Mark-07DkmEp8.js";import"./TableOfContent-CGtrISih.js";import"./index-BuxU1AhZ.js";import"./Label-COQgloJF.js";import"./index-8YDO7RSh.js";import"./index-wlwsPO1S.js";import"./addCustomCSSWithScoping-C8yl6nHi.js";import"./index-BDUIaE25.js";import"./BusyIndicator-C2GpyWwn.js";import"./index-Cgdng9Qk.js";import"./index-Ceas2fKm.js";import"./Avatar-DDjIX45r.js";import"./employee-BW41J3Tc.js";import"./ResponsivePopoverCommon.css-EgE7Pt_d.js";import"./ResponsivePopover-COAYQ03W.js";import"./Dialog-B6BucGWs.js";import"./ValueState-Bg0UWejw.js";import"./ItemNavigation-CQqeVVOg.js";function i(r){const t={code:"code",h2:"h2",p:"p",pre:"pre",...n(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(m,{of:h}),`
+`,o.jsx(x,{since:"0.20.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(s,{of:e}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(d,{of:e}),`
+`,o.jsx(t.h2,{id:"opening-colorpalettepopovers",children:"Opening ColorPalettePopovers"}),`
+`,o.jsxs(t.p,{children:["You can open and close the ",o.jsx(t.code,{children:"ColorPalettePopover"})," component in a declarative way using the ",o.jsx(t.code,{children:"open"})," and ",o.jsx(t.code,{children:"opener"})," prop."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const ColorPalettePopoverComponent = () => {
+  const [showPopover, setShowPopover] = useState(false);
+  const buttonRef = useRef(null);
+  const onButtonClick = (e) => {
+    setShowPopover((prev) => !prev);
+  };
+  return (
+    <>
+      <Button ref={buttonRef} onClick={onButtonClick}>
+        Open ColorPalettePopover
+      </Button>
+      <ColorPalettePopover open={showPopover} opener={buttonRef.current}>
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="darkblue" />
+      </ColorPalettePopover>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(a,{children:f}),`
+`,o.jsx(t.h2,{id:"colorpaletteitem",children:"ColorPaletteItem"}),`
+`,o.jsx(l,{of:p}),`
+`,o.jsx(c,{of:p}),`
+`,o.jsx(u,{})]})}function Vo(r={}){const{wrapper:t}={...n(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{Vo as default};
---
+++ nightly/assets/ComposedChart.stories-VegxaF7S.js
@@ -0,0 +1,130 @@
+import{c as I,s as K,b as M}from"./DemoProps-BWWtsAFv.js";import{C as Y}from"./index-BCyOkuf8.js";import"./useIsomorphicLayoutEffect-dLBi2t77.js";import"./index-RYns6xqu.js";import"./ThemingParameters-DOkJX3Ed.js";import"./index-C-Zfmu7Z.js";import"./defaults-VEnXigIo.js";import"./ChartContainer-3oQVEPSC.js";import"./_baseUniq-BBslp21L.js";import"./_overArg-Der2MB70.js";import"./addCustomCSSWithScoping-C8yl6nHi.js";import"./withWebComponent-_WQ2NUTk.js";import"./utils-DNCbgL6O.js";import"./clsx-B-dksMZM.js";import"./debounce-J-oijQ3B.js";import"./isString-DRmlqCDy.js";import"./useStylesheet-DKtOrKyD.js";import"./index--RBLSWvg.js";import"./iframe-Cdrg2OY6.js";import"../sb-preview/runtime.js";import"./BusyIndicator-C2GpyWwn.js";import"./i18nBundle-Cey6jF2F.js";import"./Keys-F_3Gvx0K.js";import"./Label-COQgloJF.js";import"./i18n-defaults-QZuI92fE.js";import"./if-defined-B1auRMLy.js";import"./parameters-bundle.css-P4kwt3kq.js";import"./class-map-BevVk84l.js";import"./index-BuxU1AhZ.js";import"./tiny-invariant-CCoILDQG.js";import"./YAxisTicks-BXx4rngH.js";import"./generateCategoricalChart-CsfZKq5N.js";import"./isPlainObject-DGiVfQEi.js";import"./_getPrototype-DY0Wt4dR.js";import"./_baseAssignValue-PZ39NPQP.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-_bIKPPW6.js";import"./ChartDataLabel-K3FQLHbG.js";import"./staticProps-jtk3W8EM.js";import"./useLongestYAxisLabel-DClIwwGo.js";import"./useOnClickInternal-CrvD_ZAc.js";import"./react-content-loader.es-DAsmkmNP.js";import"./ComposedChart-DshTlM_I.js";import"./Line-C0RizmZ5.js";import"./Scatter-CCpVSGWq.js";import"./useIsRTL-C9EAOKbF.js";import"./directionChange-ChfEYpsa.js";const ze={title:"ComposedChart",component:Y,argTypes:{dataset:{control:{disable:!0}},children:{control:{disable:!0}}},args:{dataset:I,dimensions:[{accessor:"name",formatter:e=>`${e} 2019`,interval:0}],measures:[{accessor:"sessions",label:"Active Sessions",type:"bar"},{accessor:"users",label:"Users",formatter:e=>e.toLocaleString(),type:"area"},{accessor:"volume",label:"Vol.",formatter:e=>`${e} sessions`,type:"line"}]}},s={},r={args:{dataset:K,dimensions:[{accessor:"name"}],measures:[{accessor:"users",color:"red",type:"bar"}]}},o={args:{measures:[{accessor:"users",type:"area"},{accessor:"sessions",type:"bar",opacity:.6},{accessor:"volume",type:"line"}],chartConfig:{zoomingTool:!0,secondYAxis:{dataKey:"sessions"}}}},a={args:{dimensions:[{accessor:"name",formatter:e=>e.slice(0,3)}],measures:[{accessor:"users",formatter:e=>`${e/10}`,type:"bar",label:"number of users",stackId:"A",width:30},{accessor:"sessions",type:"bar",stackId:"A",width:30},{accessor:"volume",type:"line",width:2}]}},t={args:{chartConfig:{zoomingTool:!0,referenceLine:{color:"red",label:"MAX",value:650}},dataset:M,measures:[{accessor:"users",type:"bar",width:2},{accessor:"sessions",type:"line"},{accessor:"volume",type:"line"}]}},n={args:{dimensions:[{accessor:"name",formatter:e=>`${e} 2019`,interval:0}],measures:[{accessor:"sessions",label:"Active Sessions",type:"bar",highlightColor:e=>{if(e>400)return"red"}},{accessor:"users",label:"Users",formatter:e=>e.toLocaleString(),type:"area"},{accessor:"volume",label:"Vol.",formatter:e=>`${e} sessions`,type:"line"}]}},i={args:{measures:[{accessor:"volume",type:"line"},{accessor:"sessions",type:"bar",opacity:.6,formatter:e=>"$"+e}],chartConfig:{zoomingTool:!0,secondYAxis:{dataKey:"sessions"}}}},c={args:{dataset:[]}};var m,p,l;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(l=(p=s.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var d,u,g;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    dataset: simpleDataSet,
+    dimensions: [{
+      accessor: 'name'
+    }],
+    measures: [{
+      accessor: 'users',
+      color: 'red',
+      type: 'bar'
+    }]
+  }
+}`,...(g=(u=r.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var y,h,b;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    measures: [{
+      accessor: 'users',
+      type: 'area'
+    }, {
+      accessor: 'sessions',
+      type: 'bar',
+      opacity: 0.6
+    }, {
+      accessor: 'volume',
+      type: 'line'
+    }],
+    chartConfig: {
+      zoomingTool: true,
+      secondYAxis: {
+        dataKey: 'sessions'
+      }
+    }
+  }
+}`,...(b=(h=o.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var f,v,S;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    dimensions: [{
+      accessor: 'name',
+      formatter: element => element.slice(0, 3)
+    }],
+    measures: [{
+      accessor: 'users',
+      formatter: element => \`\${element / 10}\`,
+      type: 'bar',
+      label: 'number of users',
+      stackId: 'A',
+      width: 30
+    }, {
+      accessor: 'sessions',
+      type: 'bar',
+      stackId: 'A',
+      width: 30
+    }, {
+      accessor: 'volume',
+      type: 'line',
+      width: 2
+    }]
+  }
+}`,...(S=(v=a.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var C,A,W;t.parameters={...t.parameters,docs:{...(C=t.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    chartConfig: {
+      zoomingTool: true,
+      referenceLine: {
+        color: 'red',
+        label: 'MAX',
+        value: 650
+      }
+    },
+    dataset: bigDataSet,
+    measures: [{
+      accessor: 'users',
+      type: 'bar',
+      width: 2
+    }, {
+      accessor: 'sessions',
+      type: 'line'
+    }, {
+      accessor: 'volume',
+      type: 'line'
+    }]
+  }
+}`,...(W=(A=t.parameters)==null?void 0:A.docs)==null?void 0:W.source}}};var L,$,x;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    dimensions: [{
+      accessor: 'name',
+      formatter: d => \`\${d} 2019\`,
+      interval: 0
+    }],
+    measures: [{
+      accessor: 'sessions',
+      label: 'Active Sessions',
+      type: 'bar',
+      highlightColor: value => {
+        if (value > 400) {
+          return 'red';
+        }
+      }
+    }, {
+      accessor: 'users',
+      label: 'Users',
+      formatter: val => val.toLocaleString(),
+      type: 'area'
+    }, {
+      accessor: 'volume',
+      label: 'Vol.',
+      formatter: val => \`\${val} sessions\`,
+      type: 'line'
+    }]
+  }
+}`,...(x=($=n.parameters)==null?void 0:$.docs)==null?void 0:x.source}}};var D,w,T;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    measures: [{
+      accessor: 'volume',
+      type: 'line'
+    }, {
+      accessor: 'sessions',
+      type: 'bar',
+      opacity: 0.6,
+      formatter: e => '$' + e
+    }],
+    chartConfig: {
+      zoomingTool: true,
+      secondYAxis: {
+        dataKey: 'sessions'
+      }
+    }
+  }
+}`,...(T=(w=i.parameters)==null?void 0:w.docs)==null?void 0:T.source}}};var z,k,F;c.parameters={...c.parameters,docs:{...(z=c.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    dataset: []
+  }
+}`,...(F=(k=c.parameters)==null?void 0:k.docs)==null?void 0:F.source}}};const ke=["Default","WithCustomColor","WithDataLabels","WithFormatter","WithReferenceLine","WithHighlightedMeasures","WithFormattedSecondaryAxis","LoadingPlaceholder"];export{s as Default,c as LoadingPlaceholder,r as WithCustomColor,o as WithDataLabels,i as WithFormattedSecondaryAxis,a as WithFormatter,n as WithHighlightedMeasures,t as WithReferenceLine,ke as __namedExportsOrder,ze as default};
