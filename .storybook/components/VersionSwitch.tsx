6fdc36d96b37f56ad89a8757ada8922a63026c8e
Marcus Notheis <marcus.notheis@sap.com>
2024-08-23 15:46:24 +0200

    docs: create version switch toolbar button (#6259)

---
+++ .storybook/components/VersionSwitch.tsx
@@ -0,0 +1,56 @@
+import { IconButton } from '@storybook/components';
+import { addons, types } from '@storybook/manager-api';
+import { ActionSheet, Button } from '@ui5/webcomponents-react';
+import * as React from 'react';
+
+const ADDON_ID = 'version-switch';
+const TOOL_ID = `${ADDON_ID}/toolbar`;
+
+addons.register(ADDON_ID, (api) => {
+  addons.add(TOOL_ID, {
+    type: types.TOOLEXTRA,
+    title: 'Version Switch',
+    render: () => {
+      const [open, setOpen] = React.useState(false);
+
+      const handleVersionSelect = (e) => {
+        const { slug } = e.target.dataset;
+        window.location.href = `https://sap.github.io/ui5-webcomponents-react/${slug}/`;
+      };
+      return (
+        <>
+          <IconButton
+            active
+            key={TOOL_ID}
+            id={TOOL_ID}
+            title="Version Switch"
+            style={{ order: -1 }}
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            Version
+          </IconButton>
+          <ActionSheet
+            placement="Bottom"
+            open={open}
+            opener={TOOL_ID}
+            onClose={() => {
+              setOpen(false);
+            }}
+          >
+            <Button onClick={handleVersionSelect} data-slug={'v2'}>
+              Version 2
+            </Button>
+            <Button onClick={handleVersionSelect} data-slug={'v1'}>
+              Version 1
+            </Button>
+            <Button onClick={handleVersionSelect} data-slug={'nightly'}>
+              🚧 Nightly
+            </Button>
+          </ActionSheet>
+        </>
+      );
+    }
+  });
+});
