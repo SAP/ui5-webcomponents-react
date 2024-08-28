391dd1384a666947d565c25c0d3c23a0c30ddcc9
Lukas Harbarth <lukas.harbarth@sap.com>
2024-08-28 16:38:51 +0200

    chore: update Next.js App Router and Vite examples to v2 (#6270)

--- examples/nextjs-app/app/loading.tsx
+++ examples/nextjs-app/app/loading.tsx
@@ -1,10 +1,12 @@
-import { BusyIndicator, BusyIndicatorSize } from '@ui5/webcomponents-react';
+import { BusyIndicator } from '@ui5/webcomponents-react';
 import classes from './loading.module.css';
+
+import BusyIndicatorSize from '@ui5/webcomponents/dist/types/BusyIndicatorSize.js';
 
 export default function HomeLoading() {
   return (
     <div className={classes.container}>
-      <BusyIndicator active size={BusyIndicatorSize.Large} delay={0} />
+      <BusyIndicator active size={BusyIndicatorSize.L} delay={0} />
     </div>
   );
 }
