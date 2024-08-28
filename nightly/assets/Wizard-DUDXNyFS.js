da95d548987ce1870f40b13b35381524a8ed464e
MarcusNotheis <marcus.notheis@sap.com>
2024-08-28 14:35:38 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@747aa12daee7642decb8cf75ed25f3193046e9fe 🚀

---
+++ nightly/assets/Wizard-DUDXNyFS.js
@@ -0,0 +1,126 @@
+import{j as e}from"./useIsomorphicLayoutEffect-dLBi2t77.js";import{useMDXComponents as s}from"./index-CcnH5Kt0.js";import{A as a}from"./ArgTypesWithNote-CBr6Z4UJ.js";import{C as p}from"./ControlsWithNote-BszXfNQw.js";import{D as m}from"./DocsHeader-CC0MoKiK.js";import{F as l}from"./CommandsAndQueries-DZquK75I.js";import"./index-D-ZoZZyj.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-B1kZn19T.js";import{ae as u,af as d,ag as c,ah as h}from"./index-CW5PVTeU.js";import{C as g,D as n,W as r}from"./Wizard.stories-Dlxx-4_2.js";import{S}from"./SubcomponentsSection-BVy4lpQe.js";import{e as f}from"./utils-CSgJODgt.js";import"./index-BY3GWTH_.js";import"./withWebComponent-BI8vCDsY.js";import"./utils-D0srJWpX.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BIWfHXaq.js";import"./decline-iyYkY03H.js";import"./Icon-DlPyRvVh.js";import"./Keys-F_3Gvx0K.js";import"./if-defined-Cg9d3U_H.js";import"./parameters-bundle.css-P4kwt3kq.js";import"./i18n-defaults-CdZCUmzZ.js";import"./information-Co3fKK0V.js";import"./alert-CZQwJ3K-.js";import"./class-map-CxhdjMwf.js";import"./Button-CKJP0O-P.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-D-rFLjnS.js";import"./TagDesign-Dou_yO3g.js";import"./index-sbqOYYIm.js";import"./index-BIFp705q.js";import"./Tag-BTK7fx1B.js";import"./sys-help-2-kZJAP_Ld.js";import"./index-bSr5Ece3.js";import"./Popover-DPDg_71u.js";import"./PopupsCommon.css-SPVvUYf3.js";import"./getEffectiveScrollbarStyle-D0XpELd9.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-B--7Aoo7.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BbnETz47.js";import"./BrowserScrollbar.css-D66iWUQm.js";import"./index-B0KcZA3m.js";import"./Link-Dcjrw7gC.js";import"./chunk-HLWAVYOI-BZzNluwU.js";import"./iframe-B-PoTMsm.js";import"../sb-preview/runtime.js";import"./client-BZNFO7bv.js";import"./copy-xOArzHwF.js";import"./copy-BN9qFed-.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-07DkmEp8.js";import"./TableOfContent-B7cg-o8h.js";import"./index-DNJvSfrs.js";import"./I18nStore-V3KeBWWJ.js";import"./useStylesheet-DKtOrKyD.js";import"./index-DXbGRngL.js";import"./index-CcQmYgD0.js";import"./Label-DiR341eU.js";import"./index-LwfcJDto.js";import"./index-B4SaSvdi.js";import"./addCustomCSSWithScoping-BEazDlzU.js";import"./index-BQWKa7gA.js";import"./BusyIndicator-BnoGb-co.js";import"./index-4u-IcZfU.js";import"./index-CscaDHiu.js";import"./Avatar-C_o4ynlU.js";import"./employee-83ogZf--.js";import"./_baseUniq-DGDOfdbw.js";import"./_getPrototype-B0L0C3AT.js";import"./index-CFFTH5x_.js";import"./index-DrFu-skq.js";import"./index-CzxmZodA.js";import"./CheckBox-DD1kSSLj.js";import"./ValueState-Bg0UWejw.js";import"./accept-DCgxdukd.js";import"./index-C8VC2DGs.js";import"./Input-Bx1dHr9x.js";import"./ResponsivePopoverCommon.css-BJt3jLXg.js";import"./ValueStateMessage.css-DXvbAMBo.js";import"./ResponsivePopover-DPyxoipx.js";import"./Dialog-CquOHL68.js";import"./index-Cp0eNK9G.js";import"./ItemNavigation-Dq-tkqkz.js";import"./debounce-TTkz9ISH.js";import"./i18n-defaults-BCdZ8lmg.js";import"./parameters-bundle.css-D9P5Os1N.js";import"./main-BPTHlokf.js";function o(i){const t={code:"code",h2:"h2",pre:"pre",...s(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(u,{of:g}),`
+`,e.jsx(m,{since:"0.12.0"}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(d,{of:n,sourceState:"none"}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:"Show Code"}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`const WizardComponent = () => {
+  const [selected, setSelected] = useState('1');
+  const [disabled, setDisabled] = useState<Record<string, boolean>>({ '2': true, '3': true, '4': true });
+  const [hidden, setHidden] = useState<Record<string, boolean>>({ step3Btn: true, finalizeBtn: true });
+  const goToStep2 = () => {
+    setDisabled((prev) => {
+      const { '2': _omit, ...rest } = prev;
+      return rest;
+    });
+    setSelected('2');
+  };
+  const goToStep3 = () => {
+    setDisabled((prev) => {
+      const { '3': _omit, ...rest } = prev;
+      return rest;
+    });
+    setSelected('3');
+  };
+  const handleStep2Completed = (e) => {
+    if (e.target.checked) {
+      setHidden((prev) => {
+        const { step3Btn: _omit, ...rest } = prev;
+        return rest;
+      });
+    }
+  };
+  const finalizeWizard = () => {
+    alert('Wizard is now completed!');
+  };
+  const handlePriceInput = (e) => {
+    if (e.target.value) {
+      setHidden((prev) => {
+        const { finalizeBtn: _omit, ...rest } = prev;
+        return rest;
+      });
+    }
+  };
+  const handleStepChange = (e) => {
+    setSelected(e.detail.step.dataset.step);
+    args.onStepChange(e);
+  };
+
+  return (
+    <Wizard {...args} onStepChange={handleStepChange}>
+      <WizardStep icon={productIcon} titleText="Product Type" selected={selected === '1'} data-step={'1'}>
+        <Title>1. Product Type</Title>
+        <MessageStrip design={MessageStripDesign.Information}>
+          The Wizard control is supposed to break down large tasks, into smaller steps, easier for the user to work
+          with.
+        </MessageStrip>
+        <Label wrappingType={WrappingType.None}>
+          Sed fermentum, mi et tristique ullamcorper, sapien sapien faucibus sem, quis pretium nibh lorem malesuada
+          diam. Nulla quis arcu aliquet, feugiat massa semper, volutpat diam. Nam vitae ante posuere, molestie neque sit
+          amet, dapibus velit. Maecenas eleifend tempor lorem. Mauris vitae elementum mi, sed eleifend ligula. Nulla
+          tempor vulputate dolor, nec dignissim quam convallis ut. Praesent vitae commodo felis, ut iaculis felis. Fusce
+          quis eleifend sapien, eget facilisis nibh. Suspendisse est velit, scelerisque ut commodo eget, dignissim quis
+          metus. Cras faucibus consequat gravida. Curabitur vitae quam felis. Phasellus ac leo eleifend, commodo tortor
+          et, varius quam. Aliquam erat volutpat
+        </Label>
+        <Button design={ButtonDesign.Emphasized} onClick={goToStep2}>
+          Step 2
+        </Button>
+      </WizardStep>
+      <WizardStep
+        icon={hintIcon}
+        titleText="Product Information"
+        disabled={disabled['2']}
+        selected={selected === '2'}
+        data-step={'2'}
+      >
+        <Title>2. Product Information</Title>
+        <Label wrappingType={WrappingType.None}>
+          Integer pellentesque leo sit amet dui vehicula, quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse
+          arcu metus, molestie a turpis a, molestie aliquet dui. Donec ppellentesque leo sit amet dui vehicula, quis
+          ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie aliquet
+          dui. Donec pulvinar, sapien corper eu, posuere malesuada nisl. Integer pellentesque leo sit amet dui vehicula,
+          quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie
+          aliquet dui. Donec pulvinar, sapien
+        </Label>
+        <CheckBox onChange={handleStep2Completed} text="Step Completed" />
+        <br />
+        {!hidden['step3Btn'] && (
+          <Button design={ButtonDesign.Emphasized} onClick={goToStep3}>
+            Step 3
+          </Button>
+        )}
+      </WizardStep>
+      <WizardStep
+        icon={leadIcon}
+        titleText="Pricing"
+        disabled={disabled['3']}
+        selected={selected === '3'}
+        data-step={'3'}
+      >
+        <Title>3. Pricing</Title>
+        <Label wrappingType={WrappingType.None}>
+          Integer pellentesque leo sit amet dui vehicula, quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse
+          arcu metus, molestie a turpis a, molestie aliquet dui. Donec ppellentesque leo sit amet dui vehicula, quis
+          ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie aliquet
+          dui. Donec pulvinar, sapien corper eu, posuere malesuada nisl. Integer pellentesque leo sit amet dui vehicula,
+          quis ullamcorper est pulvinar. Nam in libero sem. Suspendisse arcu metus, molestie a turpis a, molestie
+          aliquet dui. Donec pulvinar, sapien
+        </Label>
+        <Input placeholder="Item Price" onInput={handlePriceInput} />
+        {Br}
+        {!hidden['finalizeBtn'] && (
+          <Button design={ButtonDesign.Emphasized} onClick={finalizeWizard}>
+            Finalize
+          </Button>
+        )}
+      </WizardStep>
+    </Wizard>
+  );
+};
+`})})]}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(p,{of:n}),`
+`,e.jsx(c,{children:S}),`
+`,e.jsx(t.h2,{id:"wizardstep",children:"WizardStep"}),`
+`,e.jsx(h,{of:r}),`
+`,e.jsx(a,{hideHTMLPropsNote:!0,exclude:f,of:r}),`
+`,e.jsx(l,{})]})}function st(i={}){const{wrapper:t}={...s(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(o,{...i})}):o(i)}export{st as default};
---
+++ nightly/assets/index-D-ZoZZyj.js
@@ -0,0 +1,1 @@
+const i={version:"2.0.2",major:2,minor:0,patch:2,suffix:"",isNext:!1,buildTime:1724855635};i.version;export{i as V};
