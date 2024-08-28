638568dc62218378bd2e508cff563b0cf56edab3
Lukas742 <lukas.harbarth@sap.com>
2024-08-28 14:45:17 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@391dd1384a666947d565c25c0d3c23a0c30ddcc9 🚀

---
+++ nightly/assets/PieChart.stories-Cvq3QfiM.js
@@ -0,0 +1,35 @@
+import{s as L,d as b}from"./DemoProps-BWWtsAFv.js";import{P as W}from"./PieChart-D2-Ya7Az.js";import"./useIsomorphicLayoutEffect-dLBi2t77.js";import"./index-RYns6xqu.js";import"./index-C-Zfmu7Z.js";import"./clsx-B-dksMZM.js";import"./staticProps-C5MG_lYS.js";import"./ChartContainer-C1W1qYxY.js";import"./_getPrototype-B0L0C3AT.js";import"./_baseUniq-DGDOfdbw.js";import"./debounce-BszCn3T9.js";import"./tiny-invariant-CCoILDQG.js";import"./isPlainObject-CkURvjQU.js";import"./index-CtVrCvEM.js";import"./addCustomCSSWithScoping-BEazDlzU.js";import"./withWebComponent-BI8vCDsY.js";import"./utils-D0srJWpX.js";import"./useStylesheet-DKtOrKyD.js";import"./index-BBgZv15h.js";import"./BusyIndicator-BnoGb-co.js";import"./i18nBundle-BIWfHXaq.js";import"./Keys-F_3Gvx0K.js";import"./Label-DiR341eU.js";import"./i18n-defaults-D-rFLjnS.js";import"./if-defined-Cg9d3U_H.js";import"./parameters-bundle.css-P4kwt3kq.js";import"./class-map-CxhdjMwf.js";import"./index-CcQmYgD0.js";import"./ThemingParameters-DOkJX3Ed.js";import"./useOnClickInternal-CrvD_ZAc.js";import"./Placeholder-DtMB5xIx.js";import"./react-content-loader.es-BcmDwNEo.js";const ar={title:"PieChart",component:W,args:{dimension:{accessor:"name"},measure:{accessor:"users"},dataset:L},argTypes:{dataset:{control:{disable:!0}}}},r={},e={args:{measure:{accessor:"users",colors:["#f00","green","var(--sapNegativeColor)"]}}},a={args:{dataset:[]}},t={args:{measure:{accessor:"users",formatter:o=>o>200?"over 200":"lower"},chartConfig:{activeSegment:1,showActiveSegmentDataLabel:!0}}},s={args:{measure:{accessor:"users",hideDataLabel:o=>{if(o.percent<.01)return!0}},dataset:b}};var m,i,c;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(c=(i=r.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var n,p,u;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      colors: ['#f00', 'green', 'var(--sapNegativeColor)']
+    }
+  }
+}`,...(u=(p=e.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};var l,d,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    dataset: []
+  }
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var h,f,S;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      formatter: d => d > 200 ? 'over 200' : 'lower'
+    },
+    chartConfig: {
+      activeSegment: 1,
+      showActiveSegmentDataLabel: true
+    }
+  }
+}`,...(S=(f=t.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var C,v,D;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    measure: {
+      accessor: 'users',
+      hideDataLabel: chartConfig => {
+        if (chartConfig.percent < 0.01) {
+          return true;
+        }
+      }
+    },
+    dataset: simpleDataSetWithSmallValues
+  }
+}`,...(D=(v=s.parameters)==null?void 0:v.docs)==null?void 0:D.source}}};const tr=["Default","WithCustomColor","LoadingPlaceholder","WithFormatter","HideLabels"];export{r as Default,s as HideLabels,a as LoadingPlaceholder,e as WithCustomColor,t as WithFormatter,tr as __namedExportsOrder,ar as default};
---
+++ nightly/assets/ShellBar-BYaWjQkm.js
@@ -0,0 +1,32 @@
+import{j as o}from"./useIsomorphicLayoutEffect-dLBi2t77.js";import{useMDXComponents as e}from"./index-CcnH5Kt0.js";import{ae as n,af as s,ag as a,ah as l,ai as c}from"./index-CMTanEbs.js";import"./index-BY3GWTH_.js";import"./index-CtVrCvEM.js";import{C as h}from"./ControlsWithNote-DO6QAytf.js";import{D as d}from"./DocsHeader-Bc1UrM6o.js";import{F as x}from"./CommandsAndQueries-x5CljleU.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-B1kZn19T.js";import{S as j}from"./SubcomponentsSection-BVy4lpQe.js";import{C as f,D as i,S as p}from"./ShellBar.stories-BsPVDX_I.js";import{e as u}from"./utils-WrYKmBBb.js";import"./iframe-Bksmz00_.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./_baseUniq-DGDOfdbw.js";import"./_getPrototype-B0L0C3AT.js";import"./index-CFFTH5x_.js";import"./index-DrFu-skq.js";import"./withWebComponent-BI8vCDsY.js";import"./utils-D0srJWpX.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BIWfHXaq.js";import"./decline-iyYkY03H.js";import"./Icon-DlPyRvVh.js";import"./Keys-F_3Gvx0K.js";import"./if-defined-Cg9d3U_H.js";import"./parameters-bundle.css-P4kwt3kq.js";import"./i18n-defaults-CdZCUmzZ.js";import"./information-Co3fKK0V.js";import"./alert-CZQwJ3K-.js";import"./class-map-CxhdjMwf.js";import"./Button-CKJP0O-P.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./i18n-defaults-D-rFLjnS.js";import"./TagDesign-Dou_yO3g.js";import"./index-BIFp705q.js";import"./Tag-BTK7fx1B.js";import"./sys-help-2-kZJAP_Ld.js";import"./index-bSr5Ece3.js";import"./Popover-DPDg_71u.js";import"./PopupsCommon.css-SPVvUYf3.js";import"./getEffectiveScrollbarStyle-D0XpELd9.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-B--7Aoo7.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-BbnETz47.js";import"./BrowserScrollbar.css-D66iWUQm.js";import"./index-B0KcZA3m.js";import"./Link-Dcjrw7gC.js";import"./chunk-HLWAVYOI-CrV4L7UW.js";import"./client-BZNFO7bv.js";import"./copy-xOArzHwF.js";import"./copy-BN9qFed-.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-07DkmEp8.js";import"./TableOfContent-B7cg-o8h.js";import"./index-CjG8euSN.js";import"./I18nStore-V3KeBWWJ.js";import"./useStylesheet-DKtOrKyD.js";import"./index-DXbGRngL.js";import"./index-CcQmYgD0.js";import"./Label-DiR341eU.js";import"./index-LwfcJDto.js";import"./index-B4SaSvdi.js";import"./addCustomCSSWithScoping-BEazDlzU.js";import"./index-BQWKa7gA.js";import"./BusyIndicator-BnoGb-co.js";import"./index-4u-IcZfU.js";import"./index-CscaDHiu.js";import"./Avatar-C_o4ynlU.js";import"./employee-83ogZf--.js";import"./Person-Bk5r7PMP.js";import"./add-D2nshvca.js";import"./add-f-JMYqF-.js";import"./search-CakGVn85.js";import"./search-DPPKnuV7.js";import"./index-DLTK9AM8.js";import"./AriaHasPopup-BTjm9d28.js";import"./List-D3H2m_yy.js";import"./ItemNavigation-Dq-tkqkz.js";import"./isElementInView-CjfjvN8v.js";import"./debounce-TTkz9ISH.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-CW-Op2xu.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-DD1kSSLj.js";import"./accept-DCgxdukd.js";import"./slim-arrow-right-B8WdzONY.js";import"./overflow-CJctNs9H.js";import"./slim-arrow-down-DVepNBkc.js";import"./parameters-bundle.css-D9P5Os1N.js";import"./i18n-defaults-BCdZ8lmg.js";import"./index-CpfNU4IM.js";import"./index-C8VC2DGs.js";import"./Input-Bx1dHr9x.js";import"./ResponsivePopoverCommon.css-BJt3jLXg.js";import"./ValueStateMessage.css-DXvbAMBo.js";import"./ResponsivePopover-DPyxoipx.js";import"./Dialog-CquOHL68.js";import"./index-BZckdg6G.js";import"./main-BPTHlokf.js";function m(t){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...e(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:f}),`
+`,o.jsx(d,{since:"0.4.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(s,{of:i}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(h,{of:i}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h3,{id:"recipe-how-to-open-a-popover-on-shellbaritem-click",children:"Recipe: How to open a popover on ShellBarItem click?"}),`
+`,o.jsxs(r.p,{children:["To open a popover with the ",o.jsx(r.code,{children:"ShellBarItem"})," you can use the ",o.jsx(r.code,{children:"targetRef"})," property of the ",o.jsx(r.code,{children:"onClick"})," event."]}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-jsx",children:`const ShellBarComponent = () => {
+  const popoverRef = useRef(null);
+  const handleShellBarItemClick = (e) => {
+    popoverRef.current.showAt(e.detail.targetRef);
+  };
+  return (
+    <>
+      <ShellBar>
+        <ShellBarItem onClick={handleShellBarItemClick} icon="add" text="add" />
+      </ShellBar>
+      <Popover ref={popoverRef}>Hello there!</Popover>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(a,{children:j}),`
+`,o.jsx(r.h2,{id:"shellbaritem",children:"ShellBarItem"}),`
+`,o.jsx(l,{of:p}),`
+`,o.jsx(c,{hideHTMLPropsNote:!0,exclude:u,of:p}),`
+`,o.jsx(x,{})]})}function vr(t={}){const{wrapper:r}={...e(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(m,{...t})}):m(t)}export{vr as default};
