3c2f0c88c058c8e9ced3ed6adcb0e70517d7fc97
Lukas Harbarth <lukas.harbarth@sap.com>
2024-08-28 15:22:01 +0200

    docs(Templates & Examples): BarcodeScannerDialog unsupported note

--- .storybook/components/ProjectTemplate.tsx
+++ .storybook/components/ProjectTemplate.tsx
@@ -1,9 +1,19 @@
 import TsLogo from '@sb/images/logo-ts.png';
 import LinkDesign from '@ui5/webcomponents/dist/types/LinkDesign.js';
 import MessageStripDesign from '@ui5/webcomponents/dist/types/MessageStripDesign.js';
-import { Avatar, Card, CardHeader, Link, MessageStrip, Text, ThemeProvider } from '@ui5/webcomponents-react';
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  Label,
+  Link,
+  MessageStrip,
+  Popover,
+  Text,
+  ThemeProvider
+} from '@ui5/webcomponents-react';
 import { clsx } from 'clsx';
-import { ReactNode } from 'react';
+import { ReactNode, useRef, useState } from 'react';
 import { addCustomCSSWithScoping } from '../../packages/main/src/internal/addCustomCSSWithScoping';
 import classes from './ProjectTemplate.module.css';
 
@@ -18,6 +28,8 @@ interface ProjectTemplatePropTypes {
   deprecationNotice?: string;
   isTemplate?: boolean;
   stackBlitzHref?: string;
+  showUnsupportedMessage?: boolean;
+  showNote?: boolean;
 }
 
 addCustomCSSWithScoping(
@@ -36,67 +48,126 @@ export function ProjectTemplate(props: ProjectTemplatePropTypes) {
     href,
     deprecationNotice,
     isTemplate,
-    stackBlitzHref
+    stackBlitzHref,
+    showUnsupportedMessage,
+    showNote
   } = props;
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const linkRef = useRef(null);
 
   return (
     <ThemeProvider>
-      <Card
-        className={clsx(classes.card, isTemplate && classes.template)}
-        header={
-          <CardHeader
-            data-project-template=""
-            titleText={title}
-            subtitleText={subtitle}
-            action={
-              isTypeScript && (
-                <img
-                  src={TsLogo}
-                  height={24}
-                  width={24}
-                  alt={'TypeScript Logo'}
-                  title={'TypeScript Logo. Original Source: https://www.typescriptlang.org/branding/'}
-                />
-              )
-            }
-            avatar={
-              <Avatar title={logoAttribution}>
-                <img src={logo} alt={`Logo of ${title}`} />
-              </Avatar>
-            }
-          />
-        }
-      >
-        <div className={classes.cardContent}>
-          {deprecationNotice && (
-            <MessageStrip
-              hideCloseButton
-              design={MessageStripDesign.Critical}
-              children={deprecationNotice}
-              className={classes.deprecationNotice}
-            />
-          )}
-          <Link design={LinkDesign.Emphasized} href={href}>
-            View Example
-          </Link>
-          {stackBlitzHref && (
-            <>
-              |
-              <Link design={LinkDesign.Emphasized} href={stackBlitzHref}>
-                View in StackBlitz
+      <div className={clsx(classes.cardContainer, isTemplate && classes.template)}>
+        {showUnsupportedMessage && (
+          <>
+            <MessageStrip hideCloseButton design={MessageStripDesign.Critical} className={classes.unssupportedMessage}>
+              Currently not supported by V2.{' '}
+              <Link
+                ref={linkRef}
+                accessibleRole="Button"
+                onClick={() => {
+                  setPopoverOpen(true);
+                }}
+              >
+                Show More.
               </Link>
-            </>
-          )}
-          {!isTemplate && (
-            <>
-              <br />
-              <br />
-              <Text>What&apos;s included:</Text>
-              {children}
-            </>
-          )}
-        </div>
-      </Card>
+            </MessageStrip>
+            <Popover
+              className={classes.popover}
+              opener={linkRef.current}
+              open={popoverOpen}
+              onClose={() => {
+                setPopoverOpen(false);
+              }}
+            >
+              <Text>
+                Currently, the <code>BarcodeScannerDialog</code> web component isn't compatible with SSR environments.
+                For the Pages Router this bug crashes the whole build process, although the component isn't imported.
+                <br /> This is <b>not</b> the case for the App Router, there the build only crashes if the component is
+                used.
+                <br />
+                <br />
+                You can find the corresponding GitHub issue{' '}
+                <Link href="https://github.com/SAP/ui5-webcomponents/issues/9771" target="_blank">
+                  here
+                </Link>
+                .
+              </Text>
+            </Popover>
+          </>
+        )}
+        <Card
+          className={clsx(showUnsupportedMessage && classes.overlay)}
+          header={
+            <CardHeader
+              data-project-template=""
+              titleText={title}
+              subtitleText={subtitle}
+              action={
+                isTypeScript && (
+                  <img
+                    src={TsLogo}
+                    height={24}
+                    width={24}
+                    alt={'TypeScript Logo'}
+                    title={'TypeScript Logo. Original Source: https://www.typescriptlang.org/branding/'}
+                  />
+                )
+              }
+              avatar={
+                <Avatar title={logoAttribution}>
+                  <img src={logo} alt={`Logo of ${title}`} />
+                </Avatar>
+              }
+            />
+          }
+        >
+          <div className={classes.cardContent}>
+            {deprecationNotice && (
+              <MessageStrip
+                hideCloseButton
+                design={MessageStripDesign.Critical}
+                children={deprecationNotice}
+                className={classes.deprecationNotice}
+              />
+            )}
+            <Link design={LinkDesign.Emphasized} href={href}>
+              View Example
+            </Link>
+            {stackBlitzHref && (
+              <>
+                |
+                <Link design={LinkDesign.Emphasized} href={stackBlitzHref}>
+                  View in StackBlitz
+                </Link>
+              </>
+            )}
+            {showNote && (
+              <>
+                <br />
+                <span>
+                  <Label showColon>Note</Label>
+                  <Text>
+                    The <code>BarcodeScannerDialog</code> component is currently{' '}
+                    <Link href="https://github.com/SAP/ui5-webcomponents/issues/9771" target="_blank">
+                      not supported
+                    </Link>
+                    .
+                  </Text>
+                </span>
+              </>
+            )}
+            {!isTemplate && (
+              <>
+                <br />
+                <br />
+                <Text>What&apos;s included:</Text>
+                {children}
+              </>
+            )}
+          </div>
+        </Card>
+      </div>
     </ThemeProvider>
   );
 }
