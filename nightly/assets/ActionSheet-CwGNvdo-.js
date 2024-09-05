cfe937db92da3b60bdc44e050f25b01757f75351
Lukas742 <lukas.harbarth@sap.com>
2024-09-05 06:24:23 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@83de18d6c2b54400f0401ee3eae4d7aa999ece5c 🚀

---
+++ nightly/assets/ActionSheet-CwGNvdo-.js
@@ -0,0 +1,55 @@
+import{j as t}from"./useIsomorphicLayoutEffect-Dhma_bjk.js";import{useMDXComponents as i}from"./index-CcnH5Kt0.js";import{ae as p,af as m}from"./index-DWdzs56n.js";import"./index-BXDvD5Ag.js";import"./index-DRZ5YHb2.js";import{C as s}from"./ControlsWithNote-B5PplksY.js";import{D as c}from"./DocsHeader-Bhhh1Pbe.js";import{F as a}from"./CommandsAndQueries-JRvLx9US.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-sU8Eh0Pe.js";import{C as h,D as e}from"./ActionSheet.stories-BXPPcg6L.js";import"./iframe-DaWDcjaD.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./_baseUniq-DGDOfdbw.js";import"./_getPrototype-B0L0C3AT.js";import"./index-CFFTH5x_.js";import"./index-DrFu-skq.js";import"./withWebComponent-YgbOdVTg.js";import"./utils-C_Hfx9mc.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-CRmnyxU0.js";import"./decline-Di3Z0MBP.js";import"./Icon-DDA-V63S.js";import"./Keys-D1SxbTOd.js";import"./parameters-bundle.css-Bga-3Zi8.js";import"./i18n-defaults-CdZCUmzZ.js";import"./information-ExggBqKR.js";import"./alert-nKQG-ySE.js";import"./class-map-D12-v36h.js";import"./Button-CkiVa5Iu.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-BUwKP62u.js";import"./i18n-defaults-PZPXNaBc.js";import"./TagDesign-Dou_yO3g.js";import"./utils-DjmPOo0b.js";import"./main-C6oD7XT2.js";import"./index-Cdmm4y_7.js";import"./Tag-gh5rEq7q.js";import"./sys-help-2-Dg0HLnT_.js";import"./index-C4dnCzjA.js";import"./Popover-COhsAIU7.js";import"./PopupsCommon.css-U0xQpWGd.js";import"./FocusableElements-BostZfMq.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D085ltoF.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-HUTC8E5D.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Dz1nSNRn.js";import"./Link-V-qm0Bxn.js";import"./chunk-HLWAVYOI-DFtKNqhv.js";import"./client-BZNFO7bv.js";import"./copy-C5P79WCS.js";import"./copy-BaVgvVlS.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-CnYostSV.js";import"./TableOfContent-J0xBpY2P.js";import"./index-CWTT1j6R.js";import"./I18nStore-5-32Wrgv.js";import"./useStylesheet-DcbwKAeb.js";import"./index-3WD7if5i.js";import"./index-DYtJKmyC.js";import"./Label-CPv3jFl2.js";import"./index-gHGG7lEc.js";import"./index-DIFmT50V.js";import"./addCustomCSSWithScoping-kPHcp3gn.js";import"./index-GP3fnmag.js";import"./BusyIndicator-DuGIcogQ.js";import"./index-CA4K8WmR.js";import"./index-CTeNKttK.js";import"./Avatar-DOs7P2bF.js";import"./employee-uQoQn_HB.js";import"./accept-BfX4whS3.js";import"./decline-Dc9eM1ai.js";import"./delete-DSpOAgc6.js";import"./i18n-defaults-BYkHhcAX.js";import"./index-BNnxir6N.js";import"./ResponsivePopover-CMO3c_7J.js";import"./Dialog-_QHdeRl1.js";import"./ValueState-Bg0UWejw.js";function r(n){const o={code:"code",h2:"h2",p:"p",pre:"pre",strong:"strong",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(p,{of:h}),`
+`,t.jsx(c,{subComponents:["Button"]}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(m,{of:e}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:e}),`
+`,t.jsx(o.h2,{id:"opening-actionsheets",children:"Opening ActionSheets"}),`
+`,t.jsx("br",{}),`
+`,t.jsxs(o.p,{children:["You can open and close the ",t.jsx(o.code,{children:"ActionSheet"})," component in a declarative way using the ",t.jsx(o.code,{children:"open"})," and ",t.jsx(o.code,{children:"opener"})," prop."]}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-tsx",children:`const MyComponentWithActionSheet = () => {
+  const [actionSheetIsOpen, setActionSheetIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        id={'openActionSheetBtn'}
+        onClick={() => {
+          setActionSheetIsOpen(true);
+        }}
+      >
+        Open Action Sheet
+      </Button>
+      <ActionSheet
+        opener={'openActionSheetBtn'}
+        open={actionSheetIsOpen}
+        onClose={() => {
+          setActionSheetIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,t.jsx(o.p,{children:t.jsxs(o.strong,{children:["Opening an ",t.jsx(o.code,{children:"ActionSheet"})," by reference"]})}),`
+`,t.jsxs(o.p,{children:["The ",t.jsx(o.code,{children:"ActionSheet"})," exposes a way to pass a reference of an element instead of an ",t.jsx(o.code,{children:"id"})," to the ",t.jsx(o.code,{children:"opener"}),` prop.
+You can do that by e.g. leveraging a React Ref.`]}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`const ActionSheetComponent = () => {
+  const buttonRef = useRef(null);
+  const [open, setOpen] = useState(false);
+  const handleOpenerClick = (e) => {
+    setOpen((prev) => !prev);
+  };
+  return (
+    <>
+      <Button ref={buttonRef} onClick={handleOpenerClick}>
+        Opener
+      </Button>
+      <ActionSheet opener={buttonRef.current} open={open}>
+        {/* Content */}
+      </ActionSheet>
+    </>
+  );
+};
+`})}),`
+`,t.jsx(a,{})]})}function zt(n={}){const{wrapper:o}={...i(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{zt as default};
---
+++ nightly/assets/Panel.stories-DFRaL_b0.js
@@ -0,0 +1,43 @@
+import{j as e}from"./useIsomorphicLayoutEffect-Dhma_bjk.js";import{B as o}from"./Button-CkiVa5Iu.js";import{T as p}from"./Title-sU8Eh0Pe.js";import"./index-DRZ5YHb2.js";import{T as x}from"./index-DIFmT50V.js";import{B as n}from"./index-gHGG7lEc.js";import{L as h}from"./index-tDYxPhZP.js";import{L as t}from"./index-lgWVrTss.js";import{T as L}from"./index-6lmZJCz8.js";import{P as i}from"./index-BVrpHxZ5.js";import{F as S,c as j}from"./index-3WD7if5i.js";const v={title:"Layouts & Floorplans / Panel",component:i,argTypes:{children:{control:{disable:!0}},header:{control:{disable:!0}}},args:{headerText:"Panel"},tags:["package:@ui5/webcomponents"]},a={render(s){return e.jsxs(i,{...s,children:[e.jsx(L,{level:p.H3,children:"Basic Panel"}),e.jsx(x,{children:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."})]})}},r={args:{collapsed:!0,header:e.jsxs(S,{fitContainer:!0,style:{gap:"0.25rem"},alignItems:j.Center,children:[e.jsx(L,{level:p.H2,children:"Countries"}),e.jsx("span",{style:{flexGrow:1}}),e.jsx(n,{children:"Edit"}),e.jsx(n,{design:o.Negative,children:"Remove"}),e.jsx(n,{design:o.Emphasized,children:"Add"})]})},render(s){return e.jsx(i,{...s,children:e.jsxs(h,{children:[e.jsx(t,{description:"Language: Spanish",children:"Argentina"}),e.jsx(t,{description:"Language: Portuguese",children:"Portugal"}),e.jsx(t,{description:"Language: Spanish",children:"Mexico"}),e.jsx(t,{description:"Language: Portuguese",children:"Brazil"}),e.jsx(t,{description:"Language: German",children:"Germany"}),e.jsx(t,{description:"Language: English",children:"Australia"}),e.jsx(t,{description:"Language: German",children:"Austria"})]})})}};var d,m,l;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render(args) {
+    return <Panel {...args}>
+        <Title level={TitleLevel.H3}>Basic Panel</Title>
+        <Text>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+          clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </Text>
+      </Panel>;
+  }
+}`,...(l=(m=a.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var u,c,g;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    collapsed: true,
+    header: <FlexBox fitContainer style={{
+      gap: '0.25rem'
+    }} alignItems={FlexBoxAlignItems.Center}>
+        <Title level={TitleLevel.H2}>Countries</Title>
+        <span style={{
+        flexGrow: 1
+      }} />
+        <Button>Edit</Button>
+        <Button design={ButtonDesign.Negative}>Remove</Button>
+        <Button design={ButtonDesign.Emphasized}>Add</Button>
+      </FlexBox>
+  },
+  render(args) {
+    return <Panel {...args}>
+        <List>
+          <ListItemStandard description="Language: Spanish">Argentina</ListItemStandard>
+          <ListItemStandard description="Language: Portuguese">Portugal</ListItemStandard>
+          <ListItemStandard description="Language: Spanish">Mexico</ListItemStandard>
+          <ListItemStandard description="Language: Portuguese">Brazil</ListItemStandard>
+          <ListItemStandard description="Language: German">Germany</ListItemStandard>
+          <ListItemStandard description="Language: English">Australia</ListItemStandard>
+          <ListItemStandard description="Language: German">Austria</ListItemStandard>
+        </List>
+      </Panel>;
+  }
+}`,...(g=(c=r.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};const B=["Default","WithCustomHeader"],F=Object.freeze(Object.defineProperty({__proto__:null,Default:a,WithCustomHeader:r,__namedExportsOrder:B,default:v},Symbol.toStringTag,{value:"Module"}));export{F as C,a as D,r as W};
