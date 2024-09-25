b223ca591d20b5bd2a614ece60663322acf6fc7c
Lukas742 <lukas.harbarth@sap.com>
2024-09-25 12:31:15 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@a7f3284685fad96677a22ef4739f4ff6abbc71a2 🚀

---
+++ nightly/assets/ComposedChart.stories-D5cMcnsP.js
@@ -0,0 +1,130 @@
+import{c as I,s as K,b as M}from"./DemoProps-BWWtsAFv.js";import{C as Y}from"./index--pq5BlNs.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./ThemingParameters-DOkJX3Ed.js";import"./index-C-Zfmu7Z.js";import"./staticProps-DXQKtD7M.js";import"./ChartContainer-BpzzC2oX.js";import"./_baseUniq-18efwtne.js";import"./debounce-BNez58WF.js";import"./tiny-invariant-CCoILDQG.js";import"./index-rNTiGNI1.js";import"./index-BJHSH_GQ.js";import"./addCustomCSSWithScoping-nMrqEq1r.js";import"./withWebComponent-BS26LpJo.js";import"./utils-4GUSeNxV.js";import"./useIsomorphicLayoutEffect-BErBnqdY.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-CIcp3lSM.js";import"./index-ByWjnIRN.js";import"./BusyIndicator-DLv3RVXJ.js";import"./i18nBundle-BUZJrdsA.js";import"./Keys-D1SxbTOd.js";import"./Label-DeMESwab.js";import"./i18n-defaults-DyAOnJXW.js";import"./parameters-bundle.css-Bga-3Zi8.js";import"./class-map-CcXho7vk.js";import"./index-D5w6Jnaw.js";import"./YAxisTicks-zzc3AHLH.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-Cob5eF7d.js";import"./ChartDataLabel-Dl3EJL6v.js";import"./useLongestYAxisLabel-CImgcNix.js";import"./useOnClickInternal-CrvD_ZAc.js";import"./react-content-loader.es-BcmDwNEo.js";import"./ComposedChart-Bm-G-VcK.js";import"./Line-D4_PyxgQ.js";import"./Scatter-CchiXoui.js";import"./useIsRTL-DtzqLicj.js";import"./directionChange-DQGERUPd.js";const We={title:"ComposedChart",component:Y,argTypes:{dataset:{control:{disable:!0}},children:{control:{disable:!0}}},args:{dataset:I,dimensions:[{accessor:"name",formatter:e=>`${e} 2019`,interval:0}],measures:[{accessor:"sessions",label:"Active Sessions",type:"bar"},{accessor:"users",label:"Users",formatter:e=>e.toLocaleString(),type:"area"},{accessor:"volume",label:"Vol.",formatter:e=>`${e} sessions`,type:"line"}]}},s={},r={args:{dataset:K,dimensions:[{accessor:"name"}],measures:[{accessor:"users",color:"red",type:"bar"}]}},a={args:{measures:[{accessor:"users",type:"area"},{accessor:"sessions",type:"bar",opacity:.6},{accessor:"volume",type:"line"}],chartConfig:{zoomingTool:!0,secondYAxis:{dataKey:"sessions"}}}},o={args:{dimensions:[{accessor:"name",formatter:e=>e.slice(0,3)}],measures:[{accessor:"users",formatter:e=>`${e/10}`,type:"bar",label:"number of users",stackId:"A",width:30},{accessor:"sessions",type:"bar",stackId:"A",width:30},{accessor:"volume",type:"line",width:2}]}},t={args:{chartConfig:{zoomingTool:!0,referenceLine:{color:"red",label:"MAX",value:650}},dataset:M,measures:[{accessor:"users",type:"bar",width:2},{accessor:"sessions",type:"line"},{accessor:"volume",type:"line"}]}},n={args:{dimensions:[{accessor:"name",formatter:e=>`${e} 2019`,interval:0}],measures:[{accessor:"sessions",label:"Active Sessions",type:"bar",highlightColor:e=>{if(e>400)return"red"}},{accessor:"users",label:"Users",formatter:e=>e.toLocaleString(),type:"area"},{accessor:"volume",label:"Vol.",formatter:e=>`${e} sessions`,type:"line"}]}},c={args:{measures:[{accessor:"volume",type:"line"},{accessor:"sessions",type:"bar",opacity:.6,formatter:e=>"$"+e}],chartConfig:{zoomingTool:!0,secondYAxis:{dataKey:"sessions"}}}},i={args:{dataset:[]}};var m,p,l;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(l=(p=s.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var d,u,g;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    dataset: simpleDataSet,
+    dimensions: [{
+      accessor: 'name'
+    }],
+    measures: [{
+      accessor: 'users',
+      color: 'red',
+      type: 'bar'
+    }]
+  }
+}`,...(g=(u=r.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var y,h,b;a.parameters={...a.parameters,docs:{...(y=a.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    measures: [{
+      accessor: 'users',
+      type: 'area'
+    }, {
+      accessor: 'sessions',
+      type: 'bar',
+      opacity: 0.6
+    }, {
+      accessor: 'volume',
+      type: 'line'
+    }],
+    chartConfig: {
+      zoomingTool: true,
+      secondYAxis: {
+        dataKey: 'sessions'
+      }
+    }
+  }
+}`,...(b=(h=a.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var f,v,S;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    dimensions: [{
+      accessor: 'name',
+      formatter: element => element.slice(0, 3)
+    }],
+    measures: [{
+      accessor: 'users',
+      formatter: element => \`\${element / 10}\`,
+      type: 'bar',
+      label: 'number of users',
+      stackId: 'A',
+      width: 30
+    }, {
+      accessor: 'sessions',
+      type: 'bar',
+      stackId: 'A',
+      width: 30
+    }, {
+      accessor: 'volume',
+      type: 'line',
+      width: 2
+    }]
+  }
+}`,...(S=(v=o.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var C,A,W;t.parameters={...t.parameters,docs:{...(C=t.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    chartConfig: {
+      zoomingTool: true,
+      referenceLine: {
+        color: 'red',
+        label: 'MAX',
+        value: 650
+      }
+    },
+    dataset: bigDataSet,
+    measures: [{
+      accessor: 'users',
+      type: 'bar',
+      width: 2
+    }, {
+      accessor: 'sessions',
+      type: 'line'
+    }, {
+      accessor: 'volume',
+      type: 'line'
+    }]
+  }
+}`,...(W=(A=t.parameters)==null?void 0:A.docs)==null?void 0:W.source}}};var L,$,x;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    dimensions: [{
+      accessor: 'name',
+      formatter: d => \`\${d} 2019\`,
+      interval: 0
+    }],
+    measures: [{
+      accessor: 'sessions',
+      label: 'Active Sessions',
+      type: 'bar',
+      highlightColor: value => {
+        if (value > 400) {
+          return 'red';
+        }
+      }
+    }, {
+      accessor: 'users',
+      label: 'Users',
+      formatter: val => val.toLocaleString(),
+      type: 'area'
+    }, {
+      accessor: 'volume',
+      label: 'Vol.',
+      formatter: val => \`\${val} sessions\`,
+      type: 'line'
+    }]
+  }
+}`,...(x=($=n.parameters)==null?void 0:$.docs)==null?void 0:x.source}}};var D,w,T;c.parameters={...c.parameters,docs:{...(D=c.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    measures: [{
+      accessor: 'volume',
+      type: 'line'
+    }, {
+      accessor: 'sessions',
+      type: 'bar',
+      opacity: 0.6,
+      formatter: e => '$' + e
+    }],
+    chartConfig: {
+      zoomingTool: true,
+      secondYAxis: {
+        dataKey: 'sessions'
+      }
+    }
+  }
+}`,...(T=(w=c.parameters)==null?void 0:w.docs)==null?void 0:T.source}}};var z,k,F;i.parameters={...i.parameters,docs:{...(z=i.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    dataset: []
+  }
+}`,...(F=(k=i.parameters)==null?void 0:k.docs)==null?void 0:F.source}}};const Le=["Default","WithCustomColor","WithDataLabels","WithFormatter","WithReferenceLine","WithHighlightedMeasures","WithFormattedSecondaryAxis","LoadingPlaceholder"];export{s as Default,i as LoadingPlaceholder,r as WithCustomColor,a as WithDataLabels,c as WithFormattedSecondaryAxis,o as WithFormatter,n as WithHighlightedMeasures,t as WithReferenceLine,Le as __namedExportsOrder,We as default};
---
+++ nightly/assets/MessageView-BtsRb775.js
@@ -0,0 +1,74 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as d}from"./index-CcnH5Kt0.js";import{A as n}from"./ArgTypesWithNote-BaaJpZD2.js";import{C as l}from"./ControlsWithNote-CGKoAwgp.js";import{D as h}from"./DocsHeader-BHv0zP8D.js";import{F as c}from"./CommandsAndQueries-DGvT4Pl0.js";import"./index-BJHSH_GQ.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CAqRFLM1.js";import{ae as x,af as i,ag as g,ah as r}from"./index-uTeF-M7r.js";import{C as j,D as s,M as u,W as f,a}from"./MessageView.stories-DMZ-B6bq.js";import{S as w}from"./SubcomponentsSection-BVy4lpQe.js";import{M as m}from"./index-DcFW1Ai6.js";import"./index-D7Nv88Gg.js";import"./withWebComponent-BS26LpJo.js";import"./utils-4GUSeNxV.js";import"./useIsomorphicLayoutEffect-BErBnqdY.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18nBundle-BUZJrdsA.js";import"./decline-ChPQ5Vpc.js";import"./Icon-BwGmtu7y.js";import"./Keys-D1SxbTOd.js";import"./parameters-bundle.css-Bga-3Zi8.js";import"./i18n-defaults-CdZCUmzZ.js";import"./information-C_Bqjk9j.js";import"./alert-qaECYIyO.js";import"./class-map-CcXho7vk.js";import"./Button-BoqZ1Nk6.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-BJ6YdIbZ.js";import"./i18n-defaults-DyAOnJXW.js";import"./TagDesign-Dou_yO3g.js";import"./index-sbqOYYIm.js";import"./utils-BH9-sYqc.js";import"./main-C6oD7XT2.js";import"./index-CDPCSlUD.js";import"./Tag-C2WChDcP.js";import"./sys-help-2-KG0RzPfQ.js";import"./index-Je_4VAbv.js";import"./Popover-CC4eno7O.js";import"./PopupsCommon.css-D2k_TiiU.js";import"./FocusableElements-DE2dOMez.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-CmgjED1E.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-DYOeniYq.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-B8rq_Cwn.js";import"./Link-DWmmrhq6.js";import"./chunk-NUUEMKO5-DcR1rGE7.js";import"./iframe-BD7w2sQk.js";import"../sb-preview/runtime.js";import"./client-BzooOYO_.js";import"./copy-CLCnF9Ex.js";import"./copy-B8ijgEE_.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DGRwmqGf.js";import"./TableOfContent-bhM-b_eu.js";import"./index-iR8TRbP5.js";import"./I18nStore-ZvXQBe85.js";import"./useStylesheet-CIcp3lSM.js";import"./index-uL74TcGH.js";import"./index-D5w6Jnaw.js";import"./Label-DeMESwab.js";import"./index-DOubndiy.js";import"./index-CHNB8-uH.js";import"./addCustomCSSWithScoping-nMrqEq1r.js";import"./index-DYrmYIPb.js";import"./BusyIndicator-DLv3RVXJ.js";import"./index-Z34fJdHI.js";import"./index-BNw2TIlH.js";import"./Avatar-D0wBket6.js";import"./employee-BnxYOghN.js";import"./_baseUniq-18efwtne.js";import"./index-ar2LJKLv.js";import"./index-DrFu-skq.js";import"./ValueState-Bg0UWejw.js";import"./List-BLM_yTQc.js";import"./ItemNavigation-DY9WPMGj.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-roj2v4D7.js";import"./CheckBox-Bh-hd90L.js";import"./accept-DENHAmCe.js";import"./slim-arrow-right-BNXaNlko.js";import"./slim-arrow-right-Bsc-dN8i.js";import"./index-Cxu5ACDo.js";import"./index-AKajxJoQ.js";import"./ListItemCustom-D5JW9Qac.js";import"./information-hwpnW75w.js";import"./slim-arrow-left-Cz664I2-.js";import"./index-avjw3n6i.js";import"./index-4LcfG95T.js";import"./Dialog-zzL9k4HV.js";import"./index-ze66iUFu.js";import"./ResponsivePopover-DRXUZ-Yn.js";import"./index-B8vlftc9.js";import"./i18n-defaults-CQg0rmX6.js";import"./index-B_IGV7Gn.js";import"./index-CmE_Nt52.js";import"./index-CLoYl4Ag.js";import"./SegmentedButton-DcOmszBL.js";function p(o){const t={code:"code",em:"em",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...d(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(x,{of:j}),`
+`,e.jsx(h,{subComponents:["MessageItem","MessageViewButton"]}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(i,{of:s}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(l,{of:s}),`
+`,e.jsx(t.h2,{id:"methods",children:"Methods"}),`
+`,e.jsx(t.p,{children:"This component exposes public methods. You can invoke them directly on the instance of the component, e.g. by using React Refs."}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Name"}),e.jsx(t.th,{children:"Parameters"}),e.jsx(t.th,{children:"Description"})]})}),e.jsx(t.tbody,{children:e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"navigateBack"})})}),e.jsx(t.td,{children:"—"}),e.jsx(t.td,{children:"Navigates back to the list view if you are in the details view of a message"})]})})]}),`
+`,e.jsx(t.h2,{id:"usage-notes",children:"Usage Notes"}),`
+`,e.jsxs(t.p,{children:["The ",e.jsx(t.code,{children:"MessageView"})," can be used in several scenarios, but most likely in Dialogs or Popovers."]}),`
+`,e.jsxs(t.p,{children:["If used in a Popover it is recommended that the ",e.jsx(t.code,{children:"MessageViewButton"}),` is used as an opener.
+The `,e.jsx(t.code,{children:"type"})," of the button should always reflect the highest severity (Error -> Warning -> Success -> Information)."]}),`
+`,e.jsxs(t.p,{children:["If the ",e.jsx(t.code,{children:"MessageView"})," is used in a standalone way, you can set the prop ",e.jsx(t.code,{children:"showDetailsPageHeader"})," to ",e.jsx(t.code,{children:"true"}),`.
+This will add a bar to the details page where a back button is contained.`]}),`
+`,e.jsxs(t.p,{children:["When used in a ",e.jsx(t.code,{children:"Dialog"})," or a ",e.jsx(t.code,{children:"Popover"}),", we recommend not setting the ",e.jsx(t.code,{children:"showDetailsPageHeader"}),` prop but rather listen
+to the `,e.jsx(t.code,{children:"onItemSelect"})," event and add a back button to your Dialog or Popover header and use the ",e.jsx(t.code,{children:"navigateBack()"}),` method
+to get back to the list view.`]}),`
+`,e.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(t.h3,{id:"messageview-in-a-dialog",children:"MessageView in a Dialog"}),`
+`,e.jsx(i,{of:u}),`
+`,e.jsxs(t.p,{children:["Sample Code: ",e.jsx(t.em,{children:"(Button and Dialog opening are omitted here)"})]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`function MyComponent() {
+  const messageViewRef = useRef(null);
+  const [isOnDetailsPage, setIsOnDetailsPage] = useState(false);
+  return (
+    <Dialog
+      resizable
+      style={{ width: '500px' }}
+      className="modal-without-padding"
+      header={
+        <Bar
+          startContent={
+            <FlexBox alignItems={FlexBoxAlignItems.Center}>
+              {isOnDetailsPage && (
+                <Button
+                  icon="slim-arrow-left"
+                  design={ButtonDesign.Transparent}
+                  onClick={() => {
+                    setIsOnDetailsPage(false);
+                    messageViewRef.current.navigateBack();
+                  }}
+                  style={{ marginInline: '0 0.5rem' }}
+                />
+              )}
+              <Title level={TitleLevel.H4}>Messages</Title>
+            </FlexBox>
+          }
+        />
+      }
+    >
+      <MessageView
+        ref={messageViewRef}
+        showDetailsPageHeader={false}
+        onItemSelect={() => {
+          setIsOnDetailsPage(true);
+        }}
+      >
+        {/* children */}
+      </MessageView>
+    </Dialog>
+  );
+}
+`})}),`
+`,e.jsx(t.h3,{id:"messageview-in-popover-with-messageviewbutton",children:"MessageView in Popover with MessageViewButton"}),`
+`,e.jsx(i,{of:f}),`
+`,e.jsx(g,{children:w}),`
+`,e.jsx(t.h2,{id:"messageitem",children:"MessageItem"}),`
+`,e.jsx(r,{of:a}),`
+`,e.jsx(n,{of:a}),`
+`,e.jsx(t.h2,{id:"messageviewbutton",children:"MessageViewButton"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(c,{})]})}function Dt(o={}){const{wrapper:t}={...d(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(p,{...o})}):p(o)}export{Dt as default};
