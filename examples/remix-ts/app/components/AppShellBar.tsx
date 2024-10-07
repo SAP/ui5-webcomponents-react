e6913f5afdd6ea612d0b5630a4812f6f2448372a
Marcus Notheis <marcus.notheis@sap.com>
2024-10-07 09:03:43 +0200

    chore(remix-example): update for v2 (#6455)

--- examples/remix-ts/app/components/AppShellBar.tsx
+++ examples/remix-ts/app/components/AppShellBar.tsx
@@ -1,21 +1,21 @@
+import { useLocation, useNavigate } from '@remix-run/react';
+import { getTheme, setTheme } from '@ui5/webcomponents-base/dist/config/Theme.js';
 import navBackIcon from '@ui5/webcomponents-icons/dist/nav-back.js';
 import paletteIcon from '@ui5/webcomponents-icons/dist/palette.js';
 import {
   Button,
   List,
-  ListMode,
+  ListItemStandard,
   ListPropTypes,
   ResponsivePopover,
   ResponsivePopoverDomRef,
   ShellBar,
   ShellBarItem,
-  ShellBarItemPropTypes,
-  StandardListItem
+  ShellBarItemPropTypes
 } from '@ui5/webcomponents-react';
+import ListSelectionMode from '@ui5/webcomponents/dist/types/ListSelectionMode.js';
 import { useRef, useState } from 'react';
 import classes from './AppShellBar.module.css';
-import { getTheme, setTheme } from '@ui5/webcomponents-base/dist/config/Theme.js';
-import { useLocation, useNavigate } from '@remix-run/react';
 
 const THEMES = [
   { key: 'sap_horizon', value: 'Morning Horizon (Light)' },
@@ -31,7 +31,10 @@ export function AppShellBar() {
   const [currentTheme, setCurrentTheme] = useState(getTheme);
 
   const handleThemeSwitchItemClick: ShellBarItemPropTypes['onClick'] = (e) => {
-    popoverRef.current?.showAt(e.detail.targetRef);
+    if (popoverRef.current) {
+      popoverRef.current.opener = e.detail.targetRef;
+      popoverRef.current.open = true;
+    }
   };
   const handleThemeSwitch: ListPropTypes['onSelectionChange'] = (e) => {
     const { targetItem } = e.detail;
@@ -58,11 +61,11 @@ export function AppShellBar() {
         <ShellBarItem icon={paletteIcon} text="Change Theme" onClick={handleThemeSwitchItemClick} />
       </ShellBar>
       <ResponsivePopover ref={popoverRef} className={classes.popover}>
-        <List onSelectionChange={handleThemeSwitch} headerText="Change Theme" mode={ListMode.SingleSelect}>
+        <List onSelectionChange={handleThemeSwitch} headerText="Change Theme" selectionMode={ListSelectionMode.Single}>
           {THEMES.map((theme) => (
-            <StandardListItem key={theme.key} selected={currentTheme === theme.key} data-key={theme.key}>
+            <ListItemStandard key={theme.key} selected={currentTheme === theme.key} data-key={theme.key}>
               {theme.value}
-            </StandardListItem>
+            </ListItemStandard>
           ))}
         </List>
       </ResponsivePopover>
--- examples/remix-ts/app/components/TodoList.tsx
+++ examples/remix-ts/app/components/TodoList.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from '@remix-run/react';
-import { List, ListItemType, ListPropTypes, StandardListItem, ValueState } from '@ui5/webcomponents-react';
+import ValueState from '@ui5/webcomponents-base/dist/types/ValueState.js';
+import { List, ListItemStandard, ListPropTypes } from '@ui5/webcomponents-react';
+import ListItemType from '@ui5/webcomponents/dist/types/ListItemType.js';
 import { Todo } from '~/mockData/todos';
 
 interface TodoListProps {
@@ -16,15 +18,15 @@ export function TodoList({ items }: TodoListProps) {
     <List onItemClick={handleTodoClick}>
       {items.map((todo) => {
         return (
-          <StandardListItem
+          <ListItemStandard
             key={todo.id}
             data-id={todo.id}
             type={ListItemType.Navigation}
             additionalText={`${!todo.completed ? 'Not ' : ''}Completed`}
-            additionalTextState={todo.completed ? ValueState.Success : ValueState.None}
+            additionalTextState={todo.completed ? ValueState.Positive : ValueState.None}
           >
             {todo.title}
-          </StandardListItem>
+          </ListItemStandard>
         );
       })}
     </List>
