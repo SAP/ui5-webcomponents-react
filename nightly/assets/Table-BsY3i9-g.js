b3f6081cb9934ce14597007bc0ed901a518bcd00
Lukas742 <lukas.harbarth@sap.com>
2024-10-10 06:29:01 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@bc2284fc5cb9137301ad634f848cd7052cbd7cf1 🚀

---
+++ nightly/assets/Table-BsY3i9-g.js
@@ -0,0 +1,74 @@
+import{j as o}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as b}from"./index-CcnH5Kt0.js";import{ae as h,af as t,ag as j,ai as n,ah as i}from"./index-CsPUD8Rz.js";import"./index-BBbw6PDx.js";import"./index-yhe3vwlT.js";import{C as f}from"./ControlsWithNote-Bijg7fvM.js";import{D as T}from"./DocsHeader-C8AKhjSe.js";import{F as w}from"./CommandsAndQueries-CrMZi79f.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CbLKT2ZM.js";import{S as C}from"./SubcomponentsSection-BVy4lpQe.js";import{T as l,a,b as m,c as p}from"./index-ChK28c-t.js";import{C as g,D as s,G as u,T as d}from"./Table.stories-5xbOWcV7.js";import{T as x}from"./index-Cugp4bH6.js";import"./iframe-BMbXpKUH.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./_baseUniq-18efwtne.js";import"./index-ar2LJKLv.js";import"./index-DrFu-skq.js";import"./withWebComponent-m_Fq5lNf.js";import"./utils-DzvJSare.js";import"./useIsomorphicLayoutEffect-Dm78g2BF.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18n-DEVDpFvK.js";import"./decline-C1nyYXx8.js";import"./Icon-CqlVqIII.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-BOSi_lPd.js";import"./i18n-defaults-CzsryEvw.js";import"./information-BCemT9Q2.js";import"./alert-RTfmNTjg.js";import"./class-map-De2mf49U.js";import"./Button-CN-iJYg4.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-RVqvTAYx.js";import"./i18n-defaults-NO4RLLYJ.js";import"./TagDesign-Dou_yO3g.js";import"./utils-CNdAMEjG.js";import"./main-r7D-Vqmb.js";import"./index-CU0BOneF.js";import"./Tag-DfB_qukv.js";import"./sys-help-2-TCPa0dFt.js";import"./index-DRQZYbIB.js";import"./Popover-CSyvOuD4.js";import"./PopupsCommon.css-C0UbsxGP.js";import"./FocusableElements-3aC5Xvwv.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DIO-YqVA.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-TzMr6uTh.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Ci7P8uUK.js";import"./Link-BieF31O5.js";import"./chunk-NUUEMKO5-DwB1bQHI.js";import"./client-BzooOYO_.js";import"./copy-Bps3bwPx.js";import"./copy-9aGvoyGQ.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DGRwmqGf.js";import"./TableOfContent-DBMEQ9W3.js";import"./index-CMAb2G80.js";import"./I18nStore-C2pCP-g5.js";import"./useStylesheet-DPBS0l4j.js";import"./index-atmFgQVQ.js";import"./index-CHC2M3KR.js";import"./Label-9bI6U8uT.js";import"./index-CzucBW9v.js";import"./index-Nq8WqOTU.js";import"./addCustomCSSWithScoping-D6Q9hDjm.js";import"./index-BHJpfsCV.js";import"./BusyIndicator-DyFdFCo7.js";import"./index-PL__NMMH.js";import"./index-CnaX4kM6.js";import"./Avatar-ClyMV4d2.js";import"./employee-q-Be_VIK.js";import"./TabbableElements-BJJu_CDc.js";import"./CheckBox-BnlECbaq.js";import"./ValueState-Bg0UWejw.js";import"./accept-xt9cvT2l.js";import"./RadioButton-DWjfFT92.js";function c(r){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...b(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(h,{of:g}),`
+`,o.jsx(T,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"example",children:"Example"}),`
+`,o.jsx(t,{of:s}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(f,{of:s}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"growing-table",children:"Growing Table"}),`
+`,o.jsxs(e.p,{children:[o.jsx(e.code,{children:"Table"})," with ",o.jsx(e.code,{children:"growing={TableGrowingMode.Scroll}"}),"."]}),`
+`,o.jsx(t,{of:u}),`
+`,o.jsx(e.h3,{id:"code",children:"Code"}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const createRows = (indexOffset) => {
+  return new Array(25).fill('').map((_, index) => (
+    <TableRow key={\`\${index + indexOffset} - row\`}>
+      <TableCell>
+        <Label>{index + indexOffset}</Label>
+      </TableCell>
+      <TableCell>
+        <Label>Placeholder</Label>
+      </TableCell>
+    </TableRow>
+  ));
+};
+
+const GrowingTable = () => {
+  const [rows, setRows] = useState(createRows(1));
+  const onLoadMore = () => {
+    setRows((prev) => [...prev, ...createRows(prev.length + 1)]);
+  };
+  return (
+    <div style={{ height: '250px', overflow: 'auto' }}>
+      <Table
+        onLoadMore={onLoadMore}
+        growing={TableGrowingMode.Scroll}
+        headerRow={
+          <TableHeaderRow>
+            <TableHeaderCell>
+              <Label>Column 1</Label>
+            </TableHeaderCell>
+            <TableHeaderCell>
+              <Label>Column 2</Label>
+            </TableHeaderCell>
+          </>
+        }
+      >
+        {rows}
+      </Table>
+    </div>
+  );
+};
+`})}),`
+`,o.jsx(j,{children:C}),`
+`,o.jsx(e.h2,{id:"tableheaderrow",children:"TableHeaderRow"}),`
+`,o.jsx(n,{of:l}),`
+`,o.jsx(i,{of:l}),`
+`,o.jsx(e.h2,{id:"tableheadercell",children:"TableHeaderCell"}),`
+`,o.jsx(n,{of:a}),`
+`,o.jsx(i,{of:a}),`
+`,o.jsx(e.h2,{id:"tablerow",children:"TableRow"}),`
+`,o.jsx(n,{of:m}),`
+`,o.jsx(i,{of:m}),`
+`,o.jsx(e.h2,{id:"tablecell",children:"TableCell"}),`
+`,o.jsx(n,{of:p}),`
+`,o.jsx(i,{of:p}),`
+`,o.jsx(e.h2,{id:"tablegrowing",children:"TableGrowing"}),`
+`,o.jsx(n,{of:d}),`
+`,o.jsx(i,{of:d}),`
+`,o.jsx(e.h2,{id:"tableselection",children:"TableSelection"}),`
+`,o.jsx(n,{of:x}),`
+`,o.jsx(i,{of:x}),`
+`,o.jsx(w,{})]})}function ne(r={}){const{wrapper:e}={...b(),...r.components};return e?o.jsx(e,{...r,children:o.jsx(c,{...r})}):c(r)}export{ne as default};
