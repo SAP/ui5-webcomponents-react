e48145d0ed8c45f76a9539a60cd7aa4e156138bc
MarcusNotheis <marcus.notheis@sap.com>
2024-10-17 04:58:09 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c1c03958b0a342beda9235f4e44ebf488c4fd143 🚀

---
+++ nightly/assets/Menu-ByAnGi05.js
@@ -0,0 +1,40 @@
+import{j as o}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as m}from"./index-CcnH5Kt0.js";import{A as s}from"./ArgTypesWithNote-LDNosBxy.js";import{C as a}from"./ControlsWithNote-BVtliWez.js";import{D as c}from"./DocsHeader-CyHSJmGP.js";import{F as u}from"./CommandsAndQueries-BCONpDEG.js";import"./index-BoUOxFPi.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CbLKT2ZM.js";import{ae as d,af as i,ag as h,ai as x}from"./index-BXUX9yLt.js";import{C as l,D as n,W as f}from"./Menu.stories-3HZ7P3mq.js";import{M as e}from"./index-DyoIkfcu.js";import{S as j}from"./SubcomponentsSection-BVy4lpQe.js";import{e as M}from"./utils-P_68-zvt.js";import"./index-BBbw6PDx.js";import"./withWebComponent-m_Fq5lNf.js";import"./utils-DzvJSare.js";import"./useIsomorphicLayoutEffect-Dm78g2BF.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18n-DEVDpFvK.js";import"./decline-C1nyYXx8.js";import"./Icon-CqlVqIII.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-BOSi_lPd.js";import"./i18n-defaults-CzsryEvw.js";import"./information-BCemT9Q2.js";import"./alert-RTfmNTjg.js";import"./class-map-De2mf49U.js";import"./Button-CN-iJYg4.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-RVqvTAYx.js";import"./i18n-defaults-NO4RLLYJ.js";import"./TagDesign-Dou_yO3g.js";import"./index-sbqOYYIm.js";import"./index-CU0BOneF.js";import"./Tag-DfB_qukv.js";import"./sys-help-2-TCPa0dFt.js";import"./index-DRQZYbIB.js";import"./Popover-CSyvOuD4.js";import"./PopupsCommon.css-C0UbsxGP.js";import"./FocusableElements-3aC5Xvwv.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DIO-YqVA.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-TzMr6uTh.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Ci7P8uUK.js";import"./Link-BieF31O5.js";import"./chunk-NUUEMKO5-CAr5WPkq.js";import"./iframe-DFD2kKi-.js";import"../sb-preview/runtime.js";import"./client-BzooOYO_.js";import"./copy-Bps3bwPx.js";import"./copy-9aGvoyGQ.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DGRwmqGf.js";import"./TableOfContent-DB04WaQs.js";import"./index-CYZh8WAa.js";import"./I18nStore-C2pCP-g5.js";import"./useStylesheet-DPBS0l4j.js";import"./index-atmFgQVQ.js";import"./index-CHC2M3KR.js";import"./Label-9bI6U8uT.js";import"./index-CzucBW9v.js";import"./index-Nq8WqOTU.js";import"./addCustomCSSWithScoping-D6Q9hDjm.js";import"./index-BHJpfsCV.js";import"./BusyIndicator-DyFdFCo7.js";import"./index-PL__NMMH.js";import"./index-CnaX4kM6.js";import"./Avatar-ClyMV4d2.js";import"./employee-q-Be_VIK.js";import"./_baseUniq-18efwtne.js";import"./index-ar2LJKLv.js";import"./index-DrFu-skq.js";import"./Menu-Ca15yCD9.js";import"./slim-arrow-right-B3gzPr_L.js";import"./ResponsivePopover-DDlqQ8qM.js";import"./Dialog-Ct0qHFap.js";import"./ValueState-Bg0UWejw.js";import"./List-CC3-RGLN.js";import"./ItemNavigation-Ch4M95JE.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-DWjfFT92.js";import"./CheckBox-BnlECbaq.js";import"./accept-xt9cvT2l.js";import"./AriaHasPopup-BTjm9d28.js";import"./nav-back-N_IHc4ft.js";import"./ListItemCustom-BQVsV2O1.js";import"./index-DdwrN8_E.js";import"./main-r7D-Vqmb.js";function p(r){const t={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(d,{of:l}),`
+`,o.jsx(c,{since:"0.23.0"}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:n}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{of:n}),`
+`,o.jsx(t.h2,{id:"opening-menus",children:"Opening Menus"}),`
+`,o.jsxs(t.p,{children:["You can open and close the ",o.jsx(t.code,{children:"Menu"})," component in a declarative way using the ",o.jsx(t.code,{children:"open"})," and ",o.jsx(t.code,{children:"opener"})," prop."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const MyComponentWithMenu = () => {
+  const buttonRef = useRef(null);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        onClick={() => {
+          setMenuIsOpen(true);
+        }}
+      >
+        Open Action Sheet
+      </Button>
+      <Menu
+        opener={buttonRef.current}
+        open={menuIsOpen}
+        onClose={() => {
+          setMenuIsOpen(false);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More examples"}),`
+`,o.jsx(t.h3,{id:"menu-with-submenu",children:"Menu with Submenu"}),`
+`,o.jsx(i,{of:f}),`
+`,o.jsx(h,{children:j}),`
+`,o.jsx(t.h2,{id:"menuitem",children:"MenuItem"}),`
+`,o.jsx(x,{of:e}),`
+`,o.jsx(s,{hideHTMLPropsNote:!0,exclude:M,of:e}),`
+`,o.jsx(u,{})]})}function ut(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(p,{...r})}):p(r)}export{ut as default};
