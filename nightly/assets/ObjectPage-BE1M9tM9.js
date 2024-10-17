1367872f449d15132035bd4641503d01c5907a34
Lukas742 <lukas.harbarth@sap.com>
2024-10-17 07:03:32 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@76b925b4859843bee355840044a1f9acf7a0df9d 🚀

---
+++ nightly/assets/ObjectPage-BE1M9tM9.js
@@ -0,0 +1,108 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as m}from"./index-CcnH5Kt0.js";import{A as n}from"./ArgTypesWithNote-bWze0FeP.js";import{C as h,a as g}from"./ControlsWithNote-BY0mOIMn.js";import{D as j}from"./DocsHeader-DKMf_es1.js";import{F as b}from"./CommandsAndQueries-CAAT9wfe.js";import"./index-HYKJ__xI.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CbLKT2ZM.js";import{ae as x,af as i,ag as u,ai as r}from"./index-sA7-Ssdj.js";import{C as f,D as s,W as y,S as T,F as S,L as P,O as a,a as c,b as l,c as p}from"./ObjectPage.stories-BkicSkPl.js";import{S as O}from"./SubcomponentsSection-BVy4lpQe.js";import"./index-BBbw6PDx.js";import"./withWebComponent-m_Fq5lNf.js";import"./utils-DzvJSare.js";import"./useIsomorphicLayoutEffect-Dm78g2BF.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18n-DEVDpFvK.js";import"./decline-C1nyYXx8.js";import"./Icon-CqlVqIII.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-BOSi_lPd.js";import"./i18n-defaults-CzsryEvw.js";import"./information-BCemT9Q2.js";import"./alert-RTfmNTjg.js";import"./class-map-De2mf49U.js";import"./Button-CN-iJYg4.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-RVqvTAYx.js";import"./i18n-defaults-NO4RLLYJ.js";import"./TagDesign-Dou_yO3g.js";import"./index-sbqOYYIm.js";import"./utils-CivWR93S.js";import"./main-r7D-Vqmb.js";import"./index-CU0BOneF.js";import"./Tag-DfB_qukv.js";import"./sys-help-2-TCPa0dFt.js";import"./index-C4nETofk.js";import"./Popover-CSyvOuD4.js";import"./PopupsCommon.css-C0UbsxGP.js";import"./FocusableElements-3aC5Xvwv.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DIO-YqVA.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-TzMr6uTh.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Ci7P8uUK.js";import"./Link-BieF31O5.js";import"./chunk-NUUEMKO5-CaVPWOki.js";import"./iframe-B4C8rKYC.js";import"../sb-preview/runtime.js";import"./client-BzooOYO_.js";import"./copy-Bps3bwPx.js";import"./copy-9aGvoyGQ.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DGRwmqGf.js";import"./TableOfContent-DB04WaQs.js";import"./index-DqpHxG4L.js";import"./I18nStore-C2pCP-g5.js";import"./useStylesheet-DPBS0l4j.js";import"./index-O71mrAzx.js";import"./index-CHC2M3KR.js";import"./Label-9bI6U8uT.js";import"./index-DaJvuRnK.js";import"./index-Nq8WqOTU.js";import"./addCustomCSSWithScoping-D6Q9hDjm.js";import"./index-BHJpfsCV.js";import"./BusyIndicator-DyFdFCo7.js";import"./index-PL__NMMH.js";import"./index-CnaX4kM6.js";import"./Avatar-ClyMV4d2.js";import"./employee-q-Be_VIK.js";import"./_baseUniq-18efwtne.js";import"./index-ar2LJKLv.js";import"./index-DrFu-skq.js";import"./UnableToLoad-CXaTvxNu.js";import"./IllustratedMessage-DKnlOSdK.js";import"./i18n-defaults-CIg60XUX.js";import"./parameters-bundle.css-Cedhh-Gq.js";import"./sapIllus-Dialog-UnableToLoad-DajuCrc0.js";import"./sapIllus-Scene-UnableToLoad-C4g-RFbv.js";import"./sapIllus-Spot-UnableToLoad-BbL6RxOz.js";import"./Person-Bk5r7PMP.js";import"./BarDesign-BLoXVb05.js";import"./ValueState-Bg0UWejw.js";import"./decline-DpiE-Rsr.js";import"./full-screen-Bm1HsV4N.js";import"./general-leave-request-Cy0fA77W.js";import"./AvatarSize-BceVhWoP.js";import"./ThemingParameters-B358vlzA.js";import"./index-C-Zfmu7Z.js";import"./pushpin-on-JS66VSXq.js";import"./slim-arrow-down-D5w6EhIv.js";import"./slim-arrow-down-aLITA2lv.js";import"./slim-arrow-up-B9th-CnI.js";import"./i18n-defaults-D6MUeGa8.js";import"./index-eUs5skyq.js";import"./ToggleButton-ClpmQRth.js";import"./index-CBDX23_D.js";import"./List-CC3-RGLN.js";import"./ItemNavigation-Ch4M95JE.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-DWjfFT92.js";import"./CheckBox-BnlECbaq.js";import"./accept-xt9cvT2l.js";import"./slim-arrow-right-B3gzPr_L.js";import"./ListItemCustom-BQVsV2O1.js";import"./slideUp-By1UZlL6.js";import"./animate-DF-0g--S.js";import"./ResponsivePopover-DDlqQ8qM.js";import"./Dialog-Ct0qHFap.js";import"./ResponsivePopoverCommon.css-rwRonswg.js";import"./debounce-D7W5PopO.js";import"./stopPropagation-BJWsenK5.js";import"./Media-9wVSkz_1.js";import"./index-B2dRfIfs.js";import"./index-DDWA4u6b.js";import"./information-CFzXnWIL.js";import"./index-BVI8Mxx2.js";import"./index-Df-73hqJ.js";import"./index-DxWvf3st.js";import"./overflow-CKNL9Etw.js";import"./index-CuCW71Fb.js";import"./index-CouWfnuU.js";import"./AriaHasPopup-BTjm9d28.js";import"./index-CLyTzbWB.js";import"./index-Dnt2M25B.js";function d(o){const t={code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(x,{of:f}),`
+`,e.jsx(j,{subComponents:["ObjectPageTitle","ObjectPageHeader","ObjectPageSection","ObjectPageSubSection"]}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(i,{of:s}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(h,{of:s}),`
+`,e.jsx(t.h2,{id:"methods",children:"Methods"}),`
+`,e.jsx(t.p,{children:"This component exposes public methods. You can use them directly on the instance of the component, e.g. by using React Refs."}),`
+`,e.jsxs("table",{children:[e.jsx("thead",{children:e.jsxs("tr",{children:[e.jsx("th",{children:"Name"}),e.jsx("th",{children:"Parameters"}),e.jsx("th",{children:"Description"})]})}),e.jsx("tbody",{children:e.jsxs("tr",{children:[e.jsx("td",{children:e.jsxs(g,{children:[e.jsx("span",{style:{color:"rgb(64, 120, 242)"},children:"toggleHeaderArea"}),e.jsx("span",{style:{color:"rgb(56, 58, 66)"},children:"("}),e.jsx("span",{style:{color:"rgb(56, 58, 66)"},children:"snapped"}),e.jsx("span",{style:{color:"rgb(64, 120, 242)"},children:"?"}),e.jsx("span",{style:{color:"rgb(64, 120, 242)"},children:": "}),e.jsx("span",{style:{color:"rgb(80, 161, 79)"},children:"boolean"}),e.jsx("span",{style:{color:"rgb(56, 58, 66)"},children:")"}),e.jsx("span",{style:{color:"rgb(64, 120, 242)"},children:": "}),e.jsx("span",{style:{color:"rgb(80, 161, 79)"},children:"void"})]})}),e.jsx("td",{children:e.jsxs("div",{children:[e.jsx("p",{style:{fontWeight:"bold"},children:"snapped"}),e.jsx("p",{style:{paddingInline:"1rem"},children:e.jsxs(t.p,{children:["Defines if the header should be snapped or expanded. ",e.jsx("br",{}),` If the argument is not provided, the header is
+toggled.`]})})]})}),e.jsxs("td",{children:["Toggles the ",e.jsx(t.code,{children:"headerArea"})," of the ",e.jsx(t.code,{children:"ObjectPage"}),"."]})]})})]}),`
+`,e.jsx(t.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsxs(t.h2,{id:"objectpage-with-illustratedmessage-unabletoload-placeholder",children:["ObjectPage with IllustratedMessage (",e.jsx(t.code,{children:"UnableToLoad"}),") placeholder"]}),`
+`,e.jsx(i,{of:y}),`
+`,e.jsxs(t.h2,{id:"objectpagesection-with-hidden-titletext-and-custom-header",children:["ObjectPageSection with hidden ",e.jsx(t.code,{children:"titleText"})," and custom ",e.jsx(t.code,{children:"header"})]}),`
+`,e.jsx(i,{of:T}),`
+`,e.jsx(t.h2,{id:"tabbar-objectpage-with-fullscreen-section",children:"TabBar ObjectPage with fullscreen section"}),`
+`,e.jsxs(t.p,{children:["To render a single section in fullscreen mode, set its height to ",e.jsx(t.code,{children:"100%"}),"."]}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note:"})," This is only supported for sections in ",e.jsx(t.code,{children:"TabBar"})," mode! Using multiple sections with ",e.jsx(t.code,{children:"height: 100%;"})," on the same page will most probably break your layout."]}),`
+`,e.jsx(i,{of:S}),`
+`,e.jsx(t.h3,{id:"example-section",children:"Example section"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`<ObjectPageSection titleText="Section with Overflow" id="section3" style={{ height: '100%', overflow: 'auto' }}>
+  <div style={{ height: '300%', background: 'lightyellow' }} />
+</ObjectPageSection>
+`})}),`
+`,e.jsx(t.h2,{id:"legacy-toolbar-support",children:"Legacy Toolbar Support"}),`
+`,e.jsxs(t.p,{children:["The ObjectPage still supports the old (React-only) ",e.jsx(t.code,{children:"Toolbar"})," implementation. Please only use this toolbar if your app is dependent on some features the ",e.jsx(t.code,{children:"Toolbar"}),` web component is currently not offering.
+Also, when using the legacy `,e.jsx(t.code,{children:"Toolbar"})," there are some things to consider that work out of the box with the recommended Toolbar:"]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:["To correctly align the actions to the end, use a legacy ",e.jsx(t.code,{children:"ToolbarSpacer"})," as first child of the ",e.jsx(t.code,{children:"Toolbar"}),"."]}),`
+`,e.jsxs(t.li,{children:["Toggling the ",e.jsx(t.code,{children:"headerArea"})," by clicking on the empty space inside the legacy ",e.jsx(t.code,{children:"Toolbar"})," has to be implemented on app side now. Please make sure to add the ",e.jsx(t.code,{children:"data-in-object-page-title"})," prop to the toolbars, as otherwise the ",e.jsx(t.code,{children:"click"})," event isn't fired. You can see an example of how to achieve this behavior below."]}),`
+`]}),`
+`,e.jsx(i,{of:P,sourceState:"hidden"}),`
+`,e.jsx(t.h3,{id:"code",children:"Code"}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:"Show Code"}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`import { useRef } from 'react';
+import { Toolbar as LegacyToolbar, ToolbarSpacer as LegacyToolbarSpacer } from '@ui5/webcomponents-react-compat';
+import { Button, ButtonDesign, ObjectPage, ObjectPageSection, ObjectPageTitle } from '@ui5/webcomponents-react';
+import type { ObjectPageDomRef } from '@ui5/webcomponents-react';
+
+function ObjectPageWithLegacyToolbar(props) {
+  const objectPageRef = useRef<ObjectPageDomRef>(null);
+  const handleToolbarClick = (e) => {
+    if (e.target.dataset.componentName === 'ToolbarContent') {
+      objectPageRef.current.toggleHeaderArea();
+    }
+  };
+  return (
+    <ObjectPage
+      {...props}
+      ref={objectPageRef}
+      titleArea={
+        <ObjectPageTitle
+          header="Legacy Toolbar Support"
+          subHeader="actions and navigation actions are rendered inside the legacy Toolbar component"
+          actionsBar={
+            <LegacyToolbar
+              design="Transparent"
+              toolbarStyle="Clear"
+              onClick={handleToolbarClick}
+              data-in-object-page-title
+            >
+              <LegacyToolbarSpacer />
+              <Button design={ButtonDesign.Emphasized}>Primary Action</Button>
+              <Button design={ButtonDesign.Transparent}>Action</Button>
+            </LegacyToolbar>
+          }
+          navigationBar={
+            <LegacyToolbar
+              design="Transparent"
+              toolbarStyle="Clear"
+              onClick={handleToolbarClick}
+              data-in-object-page-title
+            >
+              <LegacyToolbarSpacer />
+              <Button icon={'full-screen'} design={ButtonDesign.Transparent} />
+              <Button icon={'exit-full-screen'} design={ButtonDesign.Transparent} />
+              <Button icon={'decline'} design={ButtonDesign.Transparent} />
+            </LegacyToolbar>
+          }
+        />
+      }
+    >
+      <ObjectPageSection titleText="Section 1" id="opSection1">
+        <div style={{ height: '300px', width: '100%', background: 'lightblue' }}>Section 1 Content</div>
+      </ObjectPageSection>
+      <ObjectPageSection titleText="Section 2" id="opSection2">
+        <div style={{ height: '200px', width: '100%', background: 'lightblue' }}>Section 2 Content</div>
+      </ObjectPageSection>
+      <ObjectPageSection titleText="Section 3" id="opSection3">
+        <div style={{ height: '500px', width: '100%', background: 'lightblue' }}>Section 3 Content</div>
+      </ObjectPageSection>
+    </ObjectPage>
+  );
+}
+`})})]}),`
+`,e.jsx(u,{children:O}),`
+`,e.jsx(t.h2,{id:"objectpagetitle",children:"ObjectPageTitle"}),`
+`,e.jsx(r,{of:a}),`
+`,e.jsx(n,{of:a}),`
+`,e.jsx(t.h2,{id:"objectpageheader",children:"ObjectPageHeader"}),`
+`,e.jsx(r,{of:c}),`
+`,e.jsx(n,{of:c}),`
+`,e.jsx(t.h2,{id:"objectpagesection",children:"ObjectPageSection"}),`
+`,e.jsx(r,{of:l}),`
+`,e.jsx(n,{of:l}),`
+`,e.jsx(t.h2,{id:"objectpagesubsection",children:"ObjectPageSubSection"}),`
+`,e.jsx(r,{of:p}),`
+`,e.jsx(n,{of:p}),`
+`,e.jsx(b,{})]})}function Zt(o={}){const{wrapper:t}={...m(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(d,{...o})}):d(o)}export{Zt as default};
