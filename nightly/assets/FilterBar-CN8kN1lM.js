8faec28c513cdbc5996bc1a430eea7476265592a
Lukas742 <lukas.harbarth@sap.com>
2024-10-23 08:21:57 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@d6a3bb29a5b29f8720c1540f40cb276abbc5fc3e 🚀

---
+++ nightly/assets/FilterBar-CN8kN1lM.js
@@ -0,0 +1,364 @@
+import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as l}from"./index-CcnH5Kt0.js";import{A as s}from"./ArgTypesWithNote-C377B6L_.js";import{C as p}from"./ControlsWithNote-AQduKH9C.js";import{D as d}from"./DocsHeader-B9T8Bfni.js";import{F as c}from"./CommandsAndQueries-CY9aDcpz.js";import"./index-B5TrSa_s.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CbLKT2ZM.js";import{S as m}from"./SubcomponentsSection-BVy4lpQe.js";import{ae as u,af as i,ag as h,ai as x}from"./index-CM_PJCgo.js";import{F as r}from"./index-28Hhlalf.js";import{C as g,D as o,W as F,a as y,I,b as f}from"./FilterBar.stories-D_GwEUrj.js";import"./index-BBbw6PDx.js";import"./withWebComponent-m_Fq5lNf.js";import"./utils-DzvJSare.js";import"./useIsomorphicLayoutEffect-Dm78g2BF.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18n-DEVDpFvK.js";import"./decline-C1nyYXx8.js";import"./Icon-CqlVqIII.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-BOSi_lPd.js";import"./i18n-defaults-CzsryEvw.js";import"./information-BCemT9Q2.js";import"./alert-RTfmNTjg.js";import"./class-map-De2mf49U.js";import"./Button-CN-iJYg4.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-RVqvTAYx.js";import"./i18n-defaults-NO4RLLYJ.js";import"./TagDesign-Dou_yO3g.js";import"./index-sbqOYYIm.js";import"./utils-DpR-C64f.js";import"./main-r7D-Vqmb.js";import"./index-CU0BOneF.js";import"./Tag-DfB_qukv.js";import"./sys-help-2-TCPa0dFt.js";import"./index-DRQZYbIB.js";import"./Popover-CSyvOuD4.js";import"./PopupsCommon.css-C0UbsxGP.js";import"./FocusableElements-3aC5Xvwv.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DIO-YqVA.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-TzMr6uTh.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Ci7P8uUK.js";import"./Link-BieF31O5.js";import"./chunk-NUUEMKO5-DPIcnx4u.js";import"./iframe-DNrSGgQ5.js";import"../sb-preview/runtime.js";import"./client-BzooOYO_.js";import"./copy-Bps3bwPx.js";import"./copy-9aGvoyGQ.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DGRwmqGf.js";import"./TableOfContent-TNI-4ed_.js";import"./index-DvgcAHKa.js";import"./I18nStore-C2pCP-g5.js";import"./useStylesheet-DPBS0l4j.js";import"./index-O71mrAzx.js";import"./index-CHC2M3KR.js";import"./Label-9bI6U8uT.js";import"./index-DaJvuRnK.js";import"./index-Nq8WqOTU.js";import"./addCustomCSSWithScoping-D6Q9hDjm.js";import"./index-BHJpfsCV.js";import"./BusyIndicator-DyFdFCo7.js";import"./index-PL__NMMH.js";import"./index-CnaX4kM6.js";import"./Avatar-ClyMV4d2.js";import"./employee-q-Be_VIK.js";import"./_baseUniq-18efwtne.js";import"./index-ar2LJKLv.js";import"./index-DrFu-skq.js";import"./Input-CAl2HCPe.js";import"./ValueState-Bg0UWejw.js";import"./ResponsivePopoverCommon.css-rwRonswg.js";import"./ValueStateMessage.css-B3Wv_LTI.js";import"./ResponsivePopover-DDlqQ8qM.js";import"./Dialog-Ct0qHFap.js";import"./search-C600p1Xv.js";import"./search-Ql3KPjZ_.js";import"./index-C-Zfmu7Z.js";import"./i18n-defaults-Bc5wpF8S.js";import"./BusyIndicatorSize-BpUhrkNo.js";import"./circle-task-2-CTqJ5C8B.js";import"./group-2-CND4RZwV.js";import"./navigation-down-arrow-CAjsqJn7.js";import"./index-ChK28c-t.js";import"./TabbableElements-BJJu_CDc.js";import"./CheckBox-BnlECbaq.js";import"./accept-xt9cvT2l.js";import"./RadioButton-DWjfFT92.js";import"./index-xhnrNB2p.js";import"./index-BVI8Mxx2.js";import"./BarDesign-BLoXVb05.js";import"./index-Cugp4bH6.js";import"./stopPropagation-BJWsenK5.js";import"./index-ktTT5ual.js";import"./index-DfvCrtx5.js";import"./index-Dnt2M25B.js";import"./index-CuCW71Fb.js";import"./index-TDDJW5p7.js";import"./Select-DH8wtn1s.js";import"./InvisibleMessage-dl6sy_nE.js";import"./slim-arrow-down-aLITA2lv.js";import"./List-CC3-RGLN.js";import"./ItemNavigation-Ch4M95JE.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./slim-arrow-right-B3gzPr_L.js";import"./index-_AHjTTrC.js";import"./Option-BUkmAaGC.js";import"./index-BPSOXVNI.js";import"./SegmentedButton-CUOk9z2p.js";import"./index-BRgLYu8F.js";import"./index-D6w54y8L.js";import"./slideUp-By1UZlL6.js";import"./animate-DF-0g--S.js";import"./AnimationMode-ByrpT_9j.js";import"./index-DhiYZ63g.js";import"./overflow-CKNL9Etw.js";import"./AriaHasPopup-BTjm9d28.js";import"./debounce-D7W5PopO.js";import"./VariantItem-B5saX3ll.js";import"./UnableToLoad-B_vWuo88.js";import"./IllustratedMessage-DjwUpZE9.js";import"./i18n-defaults-Bh0NvB0l.js";import"./parameters-bundle.css-Cedhh-Gq.js";import"./sapIllus-Dialog-UnableToLoad-DajuCrc0.js";import"./sapIllus-Scene-UnableToLoad-C4g-RFbv.js";import"./sapIllus-Spot-UnableToLoad-BbL6RxOz.js";import"./decline-DpiE-Rsr.js";import"./favorite-A2Ba1Sdt.js";import"./favorite-eYxWOGI2.js";import"./unfavorite-DOEca-H-.js";import"./ThemingParameters-B358vlzA.js";import"./index-DjXaUyO9.js";import"./index-Do-4Jpgt.js";import"./index-Da8dvIB0.js";import"./index-DExolldx.js";import"./index-2LbIC8Ja.js";import"./index-Z_6OmEHs.js";import"./index-CfqPWI_1.js";import"./index-CP7rH8wf.js";import"./Token-BnHQ1Rqv.js";import"./ScrollEnablement-CKZ-dDpP.js";import"./sys-cancel-Cg6yLywl.js";import"./index-BV6g4ujw.js";import"./index-BzgTfir-.js";import"./ComboBoxItem-DkX_qA7_.js";import"./SuggestionItem-BBOfYj8T.js";import"./index-D2i7KcHW.js";import"./Calendar-DWi8trlM.js";import"./Gregorian-D-GrQ71i.js";import"./slim-arrow-left-jcqFDD-U.js";import"./DatePicker-C2t4tW7D.js";import"./index-D3jEkHbY.js";import"./query-DOGTENGM.js";import"./ToggleButton-ClpmQRth.js";import"./slim-arrow-up-B9th-CnI.js";import"./pushpin-on-JS66VSXq.js";import"./index-CV3onXyd.js";import"./less-CWZucnBW.js";import"./add-pCl0yz1R.js";import"./index-Dx3ZZx_S.js";import"./index-0B5RhvSB.js";import"./index-h1cm79Qh.js";function a(t){const n={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...l(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(u,{of:g}),`
+`,e.jsx(d,{subComponents:["FilterGroupItem"]}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h2,{id:"example",children:"Example"}),`
+`,e.jsx(i,{of:o}),`
+`,e.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(p,{of:o}),`
+`,e.jsx("br",{}),`
+`,e.jsx(n.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(n.h2,{id:"filterbar-with-logic",children:"FilterBar with logic"}),`
+`,e.jsx(n.p,{children:"This is a basic example of how the FilterBar could be used inside an app."}),`
+`,e.jsx(i,{of:F}),`
+`,e.jsx(n.h3,{id:"code",children:"Code"}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:"Show Code"}),e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`const initialState = {
+  age: 37,
+  countries: {},
+  currency: 'USD',
+  date: '',
+  dateRange: '',
+  search: ''
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_AGE':
+      return { ...state, age: action.payload };
+    case 'SET_COUNTRIES':
+      return { ...state, countries: action.payload };
+    case 'SET_CURRENCY':
+      return { ...state, currency: action.payload };
+    case 'SET_DATE':
+      return { ...state, date: action.payload };
+    case 'SET_DATE_RANGE':
+      return { ...state, dateRange: action.payload };
+    case 'SET_SEARCH':
+      return { ...state, search: action.payload };
+    case 'DIALOG_RESTORE':
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+function FilterBarComponent(args) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [visibleFilters, setVisibleFilters] = useState(new Set(['age', 'countries']));
+  const { age, countries, currency, date, dateRange, search } = state;
+  const prevDialogOpenState = useRef(undefined);
+
+  const handleSearch = (e) => {
+    dispatch({ type: 'SET_SEARCH', payload: e.target.value });
+  };
+
+  const handleAgeChange = (e) => {
+    dispatch({ type: 'SET_AGE', payload: e.target.value });
+  };
+
+  const handleCountriesChange = (e) => {
+    const newCountries = e.detail.items.reduce((acc, cur) => {
+      return { ...acc, [cur.getAttribute('text').toLowerCase()]: true };
+    }, {});
+    dispatch({ type: 'SET_COUNTRIES', payload: newCountries });
+  };
+
+  const handleCurrencyChange = (e) => {
+    dispatch({
+      type: 'SET_CURRENCY',
+      payload: e.detail.selectedOption.textContent
+    });
+  };
+
+  const handleDateChange = (e) => {
+    if (e.detail.valid) {
+      dispatch({ type: 'SET_DATE', payload: e.detail.value });
+    }
+  };
+
+  const handleDateRangeChange = (e) => {
+    if (e.detail.valid) {
+      dispatch({ type: 'SET_DATE_RANGE', payload: e.detail.value });
+    }
+  };
+
+  const handleFiltersDialogOpen = () => {
+    prevDialogOpenState.current = state;
+  };
+
+  const handleRestore: FilterBarPropTypes['onRestore'] = (payload) => {
+    const prevDialogOpenVisibleFilters = payload.selectedFilterKeys;
+    dispatch({ type: 'DIALOG_RESTORE', payload: prevDialogOpenState.current });
+    setVisibleFilters(new Set(prevDialogOpenVisibleFilters));
+  };
+
+  const handleFiltersDialogSelectionChange: FilterBarPropTypes['onFiltersDialogSelectionChange'] = (payload) => {
+    const visibleFilterKeys = payload.selectedFilterKeys;
+    setVisibleFilters(new Set(visibleFilterKeys));
+  };
+
+  return (
+    <>
+      <FilterBar
+        showResetButton
+        search={<Input onInput={handleSearch} />}
+        onRestore={handleRestore}
+        onFiltersDialogOpen={handleFiltersDialogOpen}
+        onFiltersDialogSelectionChange={handleFiltersDialogSelectionChange}
+      >
+        <FilterGroupItem filterKey="age" label="Age" active={!!age} required>
+          <StepInput value={age} onChange={handleAgeChange} required />
+        </FilterGroupItem>
+        <FilterGroupItem
+          filterKey="countries"
+          label="Countries"
+          active={Object.keys(countries).length > 0}
+          hiddenInFilterBar={!visibleFilters.has('countries')}
+        >
+          <MultiComboBox onSelectionChange={handleCountriesChange}>
+            <MultiComboBoxItem text="Argentina" selected={countries.argentina} />
+            <MultiComboBoxItem text="Bulgaria" selected={countries.bulgaria} />
+            <MultiComboBoxItem text="Finland" selected={countries.finland} />
+            <MultiComboBoxItem text="Germany" selected={countries.germany} />
+            <MultiComboBoxItem text="Ireland" selected={countries.ireland} />
+            <MultiComboBoxItem text="Ukraine" selected={countries.ukraine} />
+            <MultiComboBoxItem text="USA" selected={countries.usa} />
+          </MultiComboBox>
+        </FilterGroupItem>
+        <FilterGroupItem
+          filterKey="currency"
+          label="Currency"
+          active={!!currency}
+          hiddenInFilterBar={!visibleFilters.has('currency')}
+        >
+          <Select onChange={handleCurrencyChange}>
+            <Option additionalText="€" selected={currency === 'EUR'}>
+              EUR
+            </Option>
+            <Option additionalText="$" selected={currency === 'USD'}>
+              USD
+            </Option>
+            <Option additionalText="£" selected={currency === 'GBP'}>
+              GBP
+            </Option>
+            <Option additionalText="₺" selected={currency === 'TRY'}>
+              TRY
+            </Option>
+            <Option additionalText="¥" selected={currency === 'JPY'}>
+              JPY
+            </Option>
+          </Select>
+        </FilterGroupItem>
+        <FilterGroupItem filterKey="date" label="Date" active={!!date} hiddenInFilterBar={!visibleFilters.has('date')}>
+          <DatePicker value={date} onChange={handleDateChange} style={{ minWidth: 'auto' }} />
+        </FilterGroupItem>
+        <FilterGroupItem
+          filterKey="date-range"
+          label="Date Range"
+          active={!!dateRange}
+          hiddenInFilterBar={!visibleFilters.has('date-range')}
+        >
+          <DateRangePicker value={dateRange} onChange={handleDateRangeChange} style={{ minWidth: 'auto' }} />
+        </FilterGroupItem>
+      </FilterBar>
+      <FlexBox direction={FlexBoxDirection.Column}>
+        <FlexBox>
+          <Label showColon>Search</Label>
+          <Text>{search}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Age</Label>
+          <Text>{age}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Countries</Label>
+          <Text>{JSON.stringify(countries)}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Currency</Label>
+          <Text>{currency}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Date</Label>
+          <Text>{date}</Text>
+        </FlexBox>
+        <FlexBox>
+          <Label showColon>Date Range</Label>
+          <Text>{dateRange}</Text>
+        </FlexBox>
+        <hr style={{ width: '100%' }} />
+        <FlexBox>
+          <Label showColon>Visible Filters</Label>
+          <Text>{Array.from(visibleFilters).join(', ')}</Text>
+        </FlexBox>
+      </FlexBox>
+    </>
+  );
+}
+`})})]}),`
+`,e.jsx(n.h2,{id:"filterbar-with-reordering",children:"FilterBar with Reordering"}),`
+`,e.jsxs(n.p,{children:["To enable the reordering feature set ",e.jsx(n.code,{children:"enableReordering"})," to ",e.jsx(n.code,{children:"true"}),"."]}),`
+`,e.jsx(i,{of:y}),`
+`,e.jsx(n.h3,{id:"code-1",children:"Code"}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:"Show Code"}),e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`function FilterBarWithReordering(props) {
+  const uniqueId = useId();
+  const [visibleChildrenByKey, setVisibleChildrenByKey] = useState<
+    Record<string, boolean>
+  >({
+    "0": true,
+    "1": true,
+    "2": true,
+    "3": true,
+    "5": true,
+  });
+  const [orderedFilterKeys, setOrderedFilterKeys] = useState([
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+  ]);
+
+  const toggleVisibility = (key) => {
+    setVisibleChildrenByKey((prev) => ({
+      ...prev,
+      [key]: !prev[key],
+    }));
+  };
+
+  const handleFiltersDialogSave: FilterBarPropTypes["onFiltersDialogSave"] = (
+    e,
+  ) => {
+    setOrderedFilterKeys(e.detail.reorderedFilterKeys);
+    setVisibleChildrenByKey(
+      e.detail.selectedFilterKeys.reduce((acc, cur) => {
+        acc[cur] = true;
+        return acc;
+      }, {}),
+    );
+  };
+
+  return (
+    <FilterBar
+      enableReordering
+      showResetButton
+      onFiltersDialogSave={handleFiltersDialogSave}
+    >
+      {orderedFilterKeys.map((filterKey) => {
+        const isHidden = !visibleChildrenByKey[filterKey];
+        switch (filterKey) {
+          case "0":
+            return (
+              <FilterGroupItem
+                filterKey="0"
+                key={\`\${uniqueId}-0\`}
+                label="StepInput"
+                required
+              >
+                <StepInput required />
+              </FilterGroupItem>
+            );
+          case "1":
+            return (
+              <FilterGroupItem
+                filterKey="1"
+                key={\`\${uniqueId}-1\`}
+                label="RatingIndicator"
+                hiddenInFilterBar={isHidden}
+              >
+                <RatingIndicator />
+              </FilterGroupItem>
+            );
+          case "2":
+            return (
+              <FilterGroupItem
+                filterKey="2"
+                key={\`\${uniqueId}-2\`}
+                label="MultiInput"
+                active
+                hiddenInFilterBar={isHidden}
+              >
+                <MultiInput
+                  tokens={
+                    <>
+                      <Token text="Argentina" selected />
+                      <Token text="Bulgaria" />
+                      <Token text="England" />
+                      <Token text="Finland" />
+                    </>
+                  }
+                />
+              </FilterGroupItem>
+            );
+          case "3":
+            return (
+              <FilterGroupItem
+                filterKey="3"
+                key={\`\${uniqueId}-3\`}
+                label="Input"
+                hiddenInFilterBar={isHidden}
+              >
+                <Input placeholder="Placeholder" />
+              </FilterGroupItem>
+            );
+          case "4":
+            return (
+              <FilterGroupItem
+                filterKey="4"
+                key={\`\${uniqueId}-4\`}
+                label="Switch"
+                hiddenInFilterBar={isHidden}
+              >
+                <Switch />
+              </FilterGroupItem>
+            );
+          case "5":
+            return (
+              <FilterGroupItem
+                filterKey="5"
+                key={\`\${uniqueId}-5\`}
+                label="SELECT w/ initial selected"
+                hiddenInFilterBar={isHidden}
+              >
+                <Select>
+                  <Option>Option 1</Option>
+                  <Option selected>Option 2</Option>
+                  <Option>Option 3</Option>
+                  <Option>Option 4</Option>
+                </Select>
+              </FilterGroupItem>
+            );
+          default:
+            return null;
+        }
+      })}
+    </FilterBar>
+  );
+}
+
+`})})]}),`
+`,e.jsx(n.h2,{id:"filterbar-in-a-dynamicpage",children:"FilterBar in a DynamicPage"}),`
+`,e.jsxs(n.p,{children:["When a FilterBar is used inside e.g. a DynamicPage, ",e.jsx(n.code,{children:"hideToolbar"})," should be set to ",e.jsx(n.code,{children:"true"}),", since the ",e.jsx(n.code,{children:"VariantManagement"}),` of
+the page is then responsible for the different views.`]}),`
+`,e.jsx(i,{of:I}),`
+`,e.jsx(n.h2,{id:"identify-the-origin-of-an-inputfilter-event",children:"Identify the origin of an input/filter event"}),`
+`,e.jsxs(n.p,{children:[`When filters in the FilterBar should only be updated directly within the FilterBar itself, or when a user presses 'OK' in the filters dialog,
+it's essential to determine the origin of the corresponding event (e.g., `,e.jsx(n.code,{children:"onInput"}),`).
+To do this, you can check the `,e.jsx(n.code,{children:"dataset"})," of the ",e.jsx(n.code,{children:"parentElement"})," of the ",e.jsx(n.code,{children:"currentTarget"}),`.
+Depending on where the user interacts with the input component, the `,e.jsx(n.code,{children:"dataset"})," of the parent will indicate either ",e.jsx(n.code,{children:'inFilterBar: "true"'})," or ",e.jsx(n.code,{children:'inFiltersDialog: "true"'}),"."]}),`
+`,e.jsx(n.h3,{id:"example-1",children:"Example:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`<FilterGroupItem filterKey="0" label="Input">
+  <StepInput onInput={handleInput} />
+</FilterGroupItem>
+`})}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`const handleInput = (event) => {
+  const firedInFilterBar = !!event.currentTarget.parentElement.dataset.inFilterBar;
+  const firedInFiltersDialog = !!event.currentTarget.parentElement.dataset.inFiltersDialog;
+};
+`})}),`
+`,e.jsx(i,{of:f,source:!0}),`
+`,e.jsx(h,{children:m}),`
+`,e.jsx(n.h2,{id:"filtergroupitem",children:"FilterGroupItem"}),`
+`,e.jsx(x,{of:r}),`
+`,e.jsx(s,{of:r,exclude:["data-index"]}),`
+`,e.jsx(c,{})]})}function kt(t={}){const{wrapper:n}={...l(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a(t)}export{kt as default};
