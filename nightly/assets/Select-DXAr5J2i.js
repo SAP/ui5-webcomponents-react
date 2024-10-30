536e924d4004b35377f29515a604edea1b21012b
Lukas742 <lukas.harbarth@sap.com>
2024-10-30 09:34:06 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0070b837ead4d288e074eab73c3420205ebe821b 🚀

---
+++ nightly/assets/Select-DXAr5J2i.js
@@ -0,0 +1,36 @@
+import{j as t}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as m}from"./index-CcnH5Kt0.js";import{ae as n,af as s,ag as a,ai as d}from"./index-CtLTDx8z.js";import{A as c}from"./ArgTypesWithNote-BMQG8PZT.js";import{C as h}from"./ControlsWithNote-FPBOF4D2.js";import{D as l}from"./DocsHeader-D6toV4v1.js";import{F as x}from"./CommandsAndQueries-H9ugICZV.js";import"./index-DyHsm_iG.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CbLKT2ZM.js";import{S as f}from"./SubcomponentsSection-BVy4lpQe.js";import{C as j,D as e}from"./Select.stories-RduWEkf_.js";import{e as u}from"./utils-NQ9KzPS_.js";import{O as r}from"./index-QNo5vCfP.js";import"./iframe-BfiPxQew.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./_baseUniq-18efwtne.js";import"./index-ar2LJKLv.js";import"./index-DrFu-skq.js";import"./index-DTeI0hvp.js";import"./withWebComponent-m_Fq5lNf.js";import"./utils-DzvJSare.js";import"./useIsomorphicLayoutEffect-Dm78g2BF.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18n-DEVDpFvK.js";import"./decline-C1nyYXx8.js";import"./Icon-CqlVqIII.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-BOSi_lPd.js";import"./i18n-defaults-CzsryEvw.js";import"./information-BCemT9Q2.js";import"./alert-RTfmNTjg.js";import"./class-map-De2mf49U.js";import"./Button-D-clZqqQ.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-RVqvTAYx.js";import"./i18n-defaults-BOjDFSS8.js";import"./TagDesign-Dou_yO3g.js";import"./index-D1wQpcsi.js";import"./Tag-C7VVIUe2.js";import"./sys-help-2-TCPa0dFt.js";import"./index-DRQZYbIB.js";import"./Popover-CSyvOuD4.js";import"./PopupsCommon.css-C0UbsxGP.js";import"./FocusableElements-3aC5Xvwv.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DIO-YqVA.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-TzMr6uTh.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Cg3b7YA_.js";import"./Link-CHy7h4h1.js";import"./chunk-NUUEMKO5-AHF1mLSV.js";import"./client-BzooOYO_.js";import"./copy-Bps3bwPx.js";import"./copy-9aGvoyGQ.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DGRwmqGf.js";import"./TableOfContent-TNI-4ed_.js";import"./index-3D56ZoSL.js";import"./I18nStore-C2pCP-g5.js";import"./useStylesheet-DPBS0l4j.js";import"./index-DwKqG2yd.js";import"./index-BoCz3Ejb.js";import"./Label-CILeKjmV.js";import"./index-rZwgHNl0.js";import"./index-DlCwds3V.js";import"./addCustomCSSWithScoping-D6Q9hDjm.js";import"./index-BKGkUclu.js";import"./BusyIndicator-DiGmt9EG.js";import"./index-DQ1nU61M.js";import"./index-BVLWJSIa.js";import"./Avatar-DQMXMvsK.js";import"./employee-q-Be_VIK.js";import"./ValueState-Bg0UWejw.js";import"./index-CUqiKvhi.js";import"./Select-CRw32Xq8.js";import"./InvisibleMessage-dl6sy_nE.js";import"./slim-arrow-down-aLITA2lv.js";import"./List-C2IRwgbK.js";import"./ItemNavigation-Ch4M95JE.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-CXJ7vYDf.js";import"./CheckBox-BuS9sHLW.js";import"./accept-xt9cvT2l.js";import"./slim-arrow-right-B3gzPr_L.js";import"./ResponsivePopover-DTfvWiQf.js";import"./Dialog-w7klcLgB.js";import"./ResponsivePopoverCommon.css-rwRonswg.js";import"./ValueStateMessage.css-B3Wv_LTI.js";import"./main-r7D-Vqmb.js";import"./Option-BnUIrSYT.js";function p(i){const o={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...m(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:j}),`
+`,t.jsx(l,{since:"0.4.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(s,{of:e}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(h,{of:e}),`
+`,t.jsx(o.h2,{id:"recipes",children:"Recipes"}),`
+`,t.jsxs(o.h3,{id:"get-the-id-of-the-selected-item-with-onchange",children:["Get the ID of the selected item with ",t.jsx(o.code,{children:"onChange"})]}),`
+`,t.jsxs(o.p,{children:["With the help of the HTML5 ",t.jsx(o.code,{children:"data-"}),` API you can define any primitive value (like numbers, strings)
+as an attribute on an element.
+This data will be available on the `,t.jsx(o.code,{children:"onChange"})," event as part of the ",t.jsx(o.code,{children:"dataset"})," object of the ",t.jsx(o.code,{children:"selectedOption"}),":"]}),`
+`,t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-jsx",children:`const data = [
+  { id: 1, text: 'Option 1' },
+  { id: 2, text: 'Option 2' }
+];
+
+const onChange = (event) => {
+  // event.detail.selectedOption is a reference to the selected HTML Element
+  // dataset contains all attributes that were passed with the data- prefix.
+  console.log(event.detail.selectedOption.dataset.id);
+};
+
+<Select onChange={onChange}>
+  {data.map((item) => (
+    <Option key={item.id} data-id={item.id}>
+      {item.text}
+    </Option>
+  ))}
+</Select>;
+`})}),`
+`,t.jsx(a,{children:f}),`
+`,t.jsx(o.h2,{id:"option",children:"Option"}),`
+`,t.jsx(d,{of:r}),`
+`,t.jsx(c,{hideHTMLPropsNote:!0,exclude:u,of:r}),`
+`,t.jsx(x,{})]})}function ho(i={}){const{wrapper:o}={...m(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(p,{...i})}):p(i)}export{ho as default};
