182a0d2df2882d8192c78e522fc7c1d72a1b88a3
Lukas742 <lukas.harbarth@sap.com>
2024-11-04 07:23:55 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@c9542a55b93a3af3e1e9f65a56ae6dff960ad8ce 🚀

---
+++ nightly/assets/Dialog-CZ4aeBzA.js
@@ -0,0 +1,26 @@
+import{j as o}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as m}from"./index-CcnH5Kt0.js";import{ae as n,af as e}from"./index-CE-qJBut.js";import"./index-DTeI0hvp.js";import"./index-Bmhwq4B1.js";import{C as s}from"./ControlsWithNote-B0-736hN.js";import{D as a}from"./DocsHeader-B35saVCk.js";import{F as c}from"./CommandsAndQueries-CD8vktzd.js";import"./index-RYns6xqu.js";import"./WrappingType-CW8URInd.js";import"./Title-CbLKT2ZM.js";import{C as l,D as i}from"./Dialog.stories-BqNJ7vpQ.js";import"./iframe-P5qo--jW.js";import"../sb-preview/runtime.js";import"./index-sbqOYYIm.js";import"./_baseUniq-18efwtne.js";import"./index-ar2LJKLv.js";import"./index-DrFu-skq.js";import"./withWebComponent-m_Fq5lNf.js";import"./utils-DzvJSare.js";import"./useIsomorphicLayoutEffect-Dm78g2BF.js";import"./slot-_4yKMUwC.js";import"./event-Dq0fpeHi.js";import"./i18n-DEVDpFvK.js";import"./decline-C1nyYXx8.js";import"./Icon-CqlVqIII.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-BOSi_lPd.js";import"./i18n-defaults-CzsryEvw.js";import"./information-BCemT9Q2.js";import"./alert-RTfmNTjg.js";import"./class-map-De2mf49U.js";import"./Button-D-clZqqQ.js";import"./AriaLabelHelper-C5uDZewF.js";import"./MarkedEvents-CAur0wxK.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-RVqvTAYx.js";import"./i18n-defaults-BOjDFSS8.js";import"./TagDesign-Dou_yO3g.js";import"./utils-CNbvRuLI.js";import"./main-r7D-Vqmb.js";import"./index-D1wQpcsi.js";import"./Tag-C7VVIUe2.js";import"./sys-help-2-TCPa0dFt.js";import"./index-DRQZYbIB.js";import"./Popover-CSyvOuD4.js";import"./PopupsCommon.css-C0UbsxGP.js";import"./FocusableElements-3aC5Xvwv.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DIO-YqVA.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-TzMr6uTh.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Cg3b7YA_.js";import"./Link-CHy7h4h1.js";import"./chunk-NUUEMKO5-kp4f3kD-.js";import"./client-BzooOYO_.js";import"./copy-Bps3bwPx.js";import"./copy-9aGvoyGQ.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DGRwmqGf.js";import"./TableOfContent-TNI-4ed_.js";import"./index-CrKBc3_a.js";import"./I18nStore-C2pCP-g5.js";import"./useStylesheet-DPBS0l4j.js";import"./index-DwKqG2yd.js";import"./index-BoCz3Ejb.js";import"./Label-CILeKjmV.js";import"./index-rZwgHNl0.js";import"./index-DlCwds3V.js";import"./addCustomCSSWithScoping-D6Q9hDjm.js";import"./index-BKGkUclu.js";import"./BusyIndicator-DiGmt9EG.js";import"./index-DQ1nU61M.js";import"./index-BVLWJSIa.js";import"./Avatar-DQMXMvsK.js";import"./employee-q-Be_VIK.js";import"./BarDesign-BLoXVb05.js";import"./index-y64W25Tf.js";import"./Dialog-w7klcLgB.js";import"./ValueState-Bg0UWejw.js";import"./index-CXba_MIT.js";import"./List-C2IRwgbK.js";import"./ItemNavigation-Ch4M95JE.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-CXJ7vYDf.js";import"./CheckBox-BuS9sHLW.js";import"./accept-xt9cvT2l.js";import"./slim-arrow-right-B3gzPr_L.js";import"./index-Hbi_TcMB.js";import"./index-CuCW71Fb.js";function p(r){const t={code:"code",h2:"h2",p:"p",pre:"pre",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(a,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(e,{of:i}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:i}),`
+`,o.jsx(t.h2,{id:"opening-dialogs",children:"Opening Dialogs"}),`
+`,o.jsxs(t.p,{children:["You can open and close the ",o.jsx(t.code,{children:"Dialog"})," component in a declarative way using the ",o.jsx(t.code,{children:"open"})," prop."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const MyComponentWithDialog = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+      >
+        Open Dialog
+      </Button>
+      <Dialog open={dialogIsOpen} />
+    </>
+  );
+};
+`})}),`
+`,o.jsx(c,{})]})}function ot(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(p,{...r})}):p(r)}export{ot as default};
