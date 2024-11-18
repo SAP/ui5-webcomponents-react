7298ed6f243d788fcc18662e020bcb6b9bd42152
Lukas742 <lukas.harbarth@sap.com>
2024-11-18 07:38:10 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@cdf81985b9c5897d56f15db9bfdc9d23a57d0dd2 🚀

---
+++ nightly/assets/ColorPalettePopover-CY62VN6h.js
@@ -0,0 +1,33 @@
+import{j as o}from"./jsx-runtime-D6fbYt3N.js";import{useMDXComponents as n}from"./index-DyCphG7h.js";import{ae as m,af as s,ag as a,ai as l,ah as c}from"./index-BrAFqeZR.js";import"./index-CCF_Nqfq.js";import"./index-CjA6MgKv.js";import{C as d}from"./ControlsWithNote-CRbjS3Cq.js";import{D as x}from"./DocsHeader-BEIc4QGi.js";import{F as u}from"./CommandsAndQueries-DzD_V9E9.js";import"./index-DysCNOs_.js";import"./WrappingType-CW8URInd.js";import"./Title-CfsRC4A5.js";import{C as h,D as e}from"./ColorPalettePopover.stories-CVYZ3eYO.js";import{S as f}from"./SubcomponentsSection-BVy4lpQe.js";import{C as p}from"./index-GFRUuIpp.js";import"./iframe-Bjn9L1T8.js";import"../sb-preview/runtime.js";import"./index-C4cLd1FO.js";import"./index-BcsOxL0D.js";import"./index-DrFu-skq.js";import"./withWebComponent-De1aKRdZ.js";import"./utils-D0KbkLTK.js";import"./useIsomorphicLayoutEffect-m1Y1SyRb.js";import"./slot-_4yKMUwC.js";import"./event-BX8i4Y_x.js";import"./i18n-DEVDpFvK.js";import"./decline-BQlFLB9d.js";import"./Icon-D5-_6Id1.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-R51uC6BG.js";import"./i18n-defaults-neJWcM66.js";import"./information-jop4MMmq.js";import"./alert-DvgF7qhQ.js";import"./class-map-Bnbjx3OQ.js";import"./Button-KJt5TdaB.js";import"./AriaLabelHelper-C5uDZewF.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-BtcFvs45.js";import"./i18n-defaults-CMHHS2wK.js";import"./TagDesign-Dou_yO3g.js";import"./utils-BicCUtSb.js";import"./main-Dcm2jhNK.js";import"./index-5MeDKPL8.js";import"./Tag-MITF22Wo.js";import"./sys-help-2-dTYFqJDl.js";import"./index-DU6jDF7S.js";import"./Popover-CjT0D3fA.js";import"./PopupsCommon.css-Bz-EF7PV.js";import"./FocusableElements-CRBMDNv2.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-BehhMu6Y.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-D6VK90q7.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-ARMOpZ_f.js";import"./Link-Dgxh_lXY.js";import"./chunk-NUUEMKO5-BmIXdVIa.js";import"./client-CqEfiX9M.js";import"./copy-DbIR3NTj.js";import"./copy-DL61w25d.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DLtQKbjK.js";import"./TableOfContent-DRym89KV.js";import"./index-KbA9y-Oj.js";import"./I18nStore-QcXEI0Jp.js";import"./useStylesheet-9EPR2ucC.js";import"./index-c-ui4WSs.js";import"./index-D2qI6nMR.js";import"./Label-VsV_UpQE.js";import"./index-BwHAVSFf.js";import"./index-ZkmNu-JV.js";import"./addCustomCSSWithScoping-BPWKraku.js";import"./index-CIk6P_fC.js";import"./BusyIndicator-DW1-q5Ls.js";import"./index-CAmAWMmr.js";import"./index-D8X14Ybw.js";import"./Avatar-Kl9ImEjS.js";import"./employee-BzTDAMVi.js";import"./ResponsivePopoverCommon.css-CJXC1J28.js";import"./ResponsivePopover-DwtnX3dS.js";import"./Dialog-DUHeHLP-.js";import"./ValueState-Bg0UWejw.js";import"./ItemNavigation-Dk_Ce4oG.js";function i(r){const t={code:"code",h2:"h2",p:"p",pre:"pre",...n(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(m,{of:h}),`
+`,o.jsx(x,{since:"0.20.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(s,{of:e}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(d,{of:e}),`
+`,o.jsx(t.h2,{id:"opening-colorpalettepopovers",children:"Opening ColorPalettePopovers"}),`
+`,o.jsxs(t.p,{children:["You can open and close the ",o.jsx(t.code,{children:"ColorPalettePopover"})," component in a declarative way using the ",o.jsx(t.code,{children:"open"})," and ",o.jsx(t.code,{children:"opener"})," prop."]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`const ColorPalettePopoverComponent = () => {
+  const [showPopover, setShowPopover] = useState(false);
+  const buttonRef = useRef(null);
+  const onButtonClick = (e) => {
+    setShowPopover((prev) => !prev);
+  };
+  return (
+    <>
+      <Button ref={buttonRef} onClick={onButtonClick}>
+        Open ColorPalettePopover
+      </Button>
+      <ColorPalettePopover open={showPopover} opener={buttonRef.current}>
+        <ColorPaletteItem value="pink" />
+        <ColorPaletteItem value="darkblue" />
+      </ColorPalettePopover>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(a,{children:f}),`
+`,o.jsx(t.h2,{id:"colorpaletteitem",children:"ColorPaletteItem"}),`
+`,o.jsx(l,{of:p}),`
+`,o.jsx(c,{of:p}),`
+`,o.jsx(u,{})]})}function Ko(r={}){const{wrapper:t}={...n(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(i,{...r})}):i(r)}export{Ko as default};
---
+++ nightly/assets/ResponsivePopover.stories-BSiE43dv.js
@@ -0,0 +1,15 @@
+import{j as e}from"./jsx-runtime-D6fbYt3N.js";import{i as v}from"./utils-BicCUtSb.js";import{a as d,P as c,b as m}from"./Popover-CjT0D3fA.js";import{r as P}from"./index-DysCNOs_.js";import{B as u}from"./index-BwHAVSFf.js";import{L as f}from"./index-CDo61ns5.js";import{L as r}from"./index-jBZ8CH2v.js";import{R as i}from"./index-9K_HSZx-.js";import"./withWebComponent-De1aKRdZ.js";import"./settings-C3-ONh9r.js";const g={title:"Modals & Popovers / ResponsivePopover",component:i,argTypes:{footer:{control:{disable:!0}},children:{control:{disable:!0}},header:{control:{disable:!0}}},args:{children:e.jsxs(f,{children:[e.jsx(r,{additionalText:"Fruits",children:"Apples"}),e.jsx(r,{additionalText:"Fruits",children:"Bananas"}),e.jsx(r,{additionalText:"Vegetables",children:"Potato"})]}),headerText:"ResponsivePopover Header",horizontalAlign:d.Center,placement:c.End,verticalAlign:m.Center,opener:"openPopoverBtn",className:"footerPartNoPadding",open:v},tags:["package:@ui5/webcomponents"]},o={render(s){const[l,n]=P.useState(s.open);return e.jsxs(e.Fragment,{children:[e.jsx(u,{id:"openResponsivePopoverBtn",onClick:()=>{n(!0)},children:"Open Popover"}),e.jsx(i,{...s,onClose:()=>{n(!1)},opener:"openResponsivePopoverBtn",open:l})]})}};var t,p,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render(args) {
+    const [responsivePopoverIsOpen, setResponsivePopoverIsOpen] = useState(args.open);
+    return <>
+        <Button id={'openResponsivePopoverBtn'} onClick={() => {
+        setResponsivePopoverIsOpen(true);
+      }}>
+          Open Popover
+        </Button>
+        <ResponsivePopover {...args} onClose={() => {
+        setResponsivePopoverIsOpen(false);
+      }} opener="openResponsivePopoverBtn" open={responsivePopoverIsOpen} />
+      </>;
+  }
+}`,...(a=(p=o.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};const x=["Default"],_=Object.freeze(Object.defineProperty({__proto__:null,Default:o,__namedExportsOrder:x,default:g},Symbol.toStringTag,{value:"Module"}));export{_ as C,o as D};
