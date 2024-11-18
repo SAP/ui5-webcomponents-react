fe389bcd439bbd6dd68c51bd6217872eaf7d259f
Dawid <perdekdawid@gmail.com>
2024-11-18 17:45:16 +0100

    fix(VariantManagement): fix dirty-state and `readOnly` behavior (#6628)
    Fixes #6622
    Fixes #6617
    
    **Thank you for your contribution!** 👏
    
    To get it merged faster, kindly review the checklist below:
    
    ## Pull Request Checklist
    - [x] Reviewed the [Contributing
    Guidelines](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md)
    + Especially the [How to
    Contribute](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md#contribute-code)
    section
    - [x] [Correct commit message
    style](https://github.com/SAP/ui5-webcomponents-react/blob/main/docs/Guidelines.md#commit-message-style)

--- packages/main/src/components/VariantManagement/index.tsx
+++ packages/main/src/components/VariantManagement/index.tsx
@@ -317,8 +317,7 @@ const VariantManagement = forwardRef<HTMLDivElement, VariantManagementPropTypes>
     }
   }, [safeChildrenWithFavorites]);
 
-  // todo: this applies if `readOnly` is set to `false` as well since the value is read via data-attribute and is therefore a string not a boolean
-  const showSaveBtn = dirtyState && !Object.prototype.hasOwnProperty.call(selectedVariant, 'readOnly');
+  const showSaveBtn = dirtyState && selectedVariant && !selectedVariant.readOnly;
 
   return (
     <div className={variantManagementClasses} style={style} {...rest} ref={ref}>
