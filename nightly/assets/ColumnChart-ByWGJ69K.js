5d49491ccd787fbc88377ce2370c5bd0c4f3a045
Lukas742 <lukas.harbarth@sap.com>
2024-11-25 11:41:55 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@6954d956fbf8413e1d4b8fa93795e1ea505a117b 🚀

---
+++ nightly/assets/ColumnChart-ByWGJ69K.js
@@ -0,0 +1,39 @@
+import{j as o}from"./jsx-runtime-D6fbYt3N.js";import{useMDXComponents as m}from"./index-DyCphG7h.js";import{ae as p,af as i}from"./index-URQJwgp2.js";import"./index-OrErW1tb.js";import"./index-CW0pURC6.js";import{C as s}from"./ControlsWithNote-D7hBpI1A.js";import{D as a}from"./DocsHeader-CobXS4J_.js";import{F as h}from"./CommandsAndQueries-BMRwZY9A.js";import"./index-DysCNOs_.js";import"./WrappingType-CW8URInd.js";import"./Title-DnczWaCy.js";import"./chunk-NUUEMKO5-YaTFvJlV.js";import{M as c}from"./TooltipConfig-DzydwY7_.js";import{M as d}from"./LegendConfig-I8rlx_1s.js";import{C as l,D as e,W as x,a as j,b as f,c as u,L as g,d as C,e as W,f as M,g as D}from"./ColumnChart.stories-DqilSXzo.js";import"./iframe-HfST3bJB.js";import"../sb-preview/runtime.js";import"./index-C4cLd1FO.js";import"./index-BcsOxL0D.js";import"./index-DrFu-skq.js";import"./withWebComponent-DpVKsRHi.js";import"./utils-BMpfTDbr.js";import"./useIsomorphicLayoutEffect-F-N6bu_1.js";import"./slot-_4yKMUwC.js";import"./event-BX8i4Y_x.js";import"./i18n-DEVDpFvK.js";import"./decline-Ch4ABsnB.js";import"./Icon-CrKv37md.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-R51uC6BG.js";import"./i18n-defaults-neJWcM66.js";import"./information-D_LjlJHK.js";import"./alert-CiqdS0Ti.js";import"./class-map-CAgcI9af.js";import"./Button-CzXV-qq-.js";import"./AriaLabelHelper-C5uDZewF.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-HqN2rB8b.js";import"./i18n-defaults-CMHHS2wK.js";import"./TagDesign-Dou_yO3g.js";import"./utils-CTN7AkKv.js";import"./main-Dcm2jhNK.js";import"./index-DA9k7rzT.js";import"./Tag-BwJ4pIZx.js";import"./sys-help-2-Dw9i_TLE.js";import"./index-x5zB9JtW.js";import"./Popover-CxadHYVC.js";import"./PopupsCommon.css-Bu1W85h-.js";import"./FocusableElements-DpmShrVF.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D2m47C26.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-Bigav4TG.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-cawFe0IV.js";import"./Link-CcG_uYXc.js";import"./copy-C_xK9_A_.js";import"./copy-BtTL6xAX.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DLtQKbjK.js";import"./TableOfContent-DRym89KV.js";import"./index-DG836lmi.js";import"./I18nStore-CXxqSOxg.js";import"./useStylesheet-BAIsnduv.js";import"./index-VxGFz2Tr.js";import"./index-kNlKigw5.js";import"./Label-D_ADNoJV.js";import"./index-DP-u0XOf.js";import"./index-CA4smrai.js";import"./addCustomCSSWithScoping-DYmOnLgW.js";import"./index-qKU3PnwJ.js";import"./BusyIndicator-B17L6t2M.js";import"./index-C6sMyakJ.js";import"./index-CHrZj7jK.js";import"./Avatar-CLqwzIQ4.js";import"./employee-CUDhI-YO.js";import"./client-CqEfiX9M.js";import"./DemoProps-BXtrSXe3.js";import"./ThemingParameters-B358vlzA.js";import"./index-C-Zfmu7Z.js";import"./staticProps-D7eetPqi.js";import"./ChartContainer-CQMaoV__.js";import"./tiny-invariant-CCoILDQG.js";import"./index-V2oewtx_.js";import"./useCancelAnimationFallback-CfJzvL7V.js";import"./YAxisTicks-D6Q6JR4n.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-DCImYZHH.js";import"./ChartDataLabel-CJKcpx-v.js";import"./useLongestYAxisLabel-jneqzPRo.js";import"./useOnClickInternal-D3NUJALO.js";import"./react-content-loader.es-DGUccsgf.js";import"./useIsRTL-A2zy57qw.js";import"./directionChange-BJrRAK48.js";function n(t){const r={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...m(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:l}),`
+`,o.jsx(a,{isChart:!0}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:e}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:e}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(r.h3,{id:"with-custom-color",children:"With custom color"}),`
+`,o.jsx(i,{of:x}),`
+`,o.jsx(r.h3,{id:"with-secondary-dimension",children:"With Secondary Dimension"}),`
+`,o.jsx(i,{of:j}),`
+`,o.jsx(r.h3,{id:"with-stacks",children:"With Stacks"}),`
+`,o.jsx(i,{of:f}),`
+`,o.jsx(r.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,o.jsx(i,{of:u}),`
+`,o.jsx(r.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(i,{of:g}),`
+`,o.jsx(r.h3,{id:"with-reference-line",children:"With Reference Line"}),`
+`,o.jsxs(r.p,{children:["You can set a reference line to any value by using the ",o.jsx(r.code,{children:"referenceLine"})," ",o.jsx(r.code,{children:"chartConfig"})," property. ",o.jsx(r.code,{children:"referenceLine"})," accepts all properties described ",o.jsx(r.a,{href:"https://recharts.org/en-US/api/ReferenceLine",rel:"nofollow",children:"here"}),", but it's recommended to only use ",o.jsx(r.code,{children:"label"}),",",o.jsx(r.code,{children:"value"})," and ",o.jsx(r.code,{children:"color"})," to preserve the intended design."]}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-jsx",children:`<ColumnChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,o.jsx(i,{of:C}),`
+`,o.jsx(r.h2,{id:"with-highlighted-measures",children:"With Highlighted Measures"}),`
+`,o.jsx(i,{of:W}),`
+`,o.jsxs(r.p,{children:[o.jsx(c,{of:M}),";"]}),`
+`,o.jsx(d,{of:D}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(h,{})]})}function dr(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(n,{...t})}):n(t)}export{dr as default};
