2debb2fca1b5f97ab21a53a9c6d51d0070248c6e
Lukas742 <lukas.harbarth@sap.com>
2024-11-25 11:16:26 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@58a845695619e9b56559e636203210a63d532752 🚀

---
+++ nightly/assets/CommonCSS-DCebrbgw.js
@@ -0,0 +1,33 @@
+import{j as n}from"./jsx-runtime-D6fbYt3N.js";import{useMDXComponents as s}from"./index-DyCphG7h.js";import{ae as e}from"./index-Dxw9-lRg.js";import{M as r}from"./index-OrErW1tb.js";import"./index-dbrrX5ch.js";import"./TagDesign-Dou_yO3g.js";import"./index-DysCNOs_.js";import"./index-C4cLd1FO.js";import"./chunk-NUUEMKO5-CBxgZXZC.js";import"./Button-CzXV-qq-.js";import"./withWebComponent-DpVKsRHi.js";import"./copy-BtTL6xAX.js";import{F as m}from"./CommandsAndQueries-CVMe8F--.js";import{T as p}from"./TableOfContent-DRym89KV.js";import"./WrappingType-CW8URInd.js";import"./Title-DnczWaCy.js";import{L as a}from"./index-cawFe0IV.js";import"./iframe-BGgQJLC1.js";import"../sb-preview/runtime.js";import"./index-BcsOxL0D.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-BX8i4Y_x.js";import"./i18n-DEVDpFvK.js";import"./decline-Ch4ABsnB.js";import"./Icon-CrKv37md.js";import"./useIsomorphicLayoutEffect-F-N6bu_1.js";import"./Keys-DoZifIQ_.js";import"./utils-BMpfTDbr.js";import"./parameters-bundle.css-R51uC6BG.js";import"./i18n-defaults-neJWcM66.js";import"./information-D_LjlJHK.js";import"./alert-CiqdS0Ti.js";import"./class-map-CAgcI9af.js";import"./i18n-defaults-CMHHS2wK.js";import"./client-CqEfiX9M.js";import"./AriaLabelHelper-C5uDZewF.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-HqN2rB8b.js";import"./Popover-CxadHYVC.js";import"./PopupsCommon.css-Bu1W85h-.js";import"./FocusableElements-DpmShrVF.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D2m47C26.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-Bigav4TG.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-RFezEdyG.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BAIsnduv.js";import"./index-kNlKigw5.js";import"./Label-D_ADNoJV.js";import"./index-DP-u0XOf.js";import"./index-x5zB9JtW.js";import"./index-CA4smrai.js";import"./Link-CcG_uYXc.js";import"./addCustomCSSWithScoping-DYmOnLgW.js";import"./index-qKU3PnwJ.js";import"./BusyIndicator-B17L6t2M.js";import"./index-C6sMyakJ.js";import"./index-CHrZj7jK.js";import"./Avatar-CLqwzIQ4.js";import"./employee-CUDhI-YO.js";import"./index-BjPcBTkz.js";import"./I18nStore-CXxqSOxg.js";function i(t){const o={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e,{title:"Common CSS"}),`
+`,n.jsx(o.h1,{id:"using-common-css-in-your-application",children:"Using Common CSS in your application"}),`
+`,n.jsx(p,{}),`
+`,n.jsxs(o.p,{children:["We recommend using the ",n.jsx(o.a,{href:"https://www.npmjs.com/package/@sap-ui/common-css",rel:"nofollow",children:"@sap-ui/common-css"})," package for applying common styling blocks based on SAP Fiori Design Guidelines."]}),`
+`,n.jsx(r,{hideCloseButton:!0,children:n.jsxs(n.Fragment,{children:["This documentation is only showing a few samples which we consider to be useful for applications using UI5 Web Components for React. You can find the full documentation for SAP UI Common CSS"," ",n.jsx(a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-introduction--docs",children:"here"}),"."]})}),`
+`,n.jsx("br",{}),`
+`,n.jsx("br",{}),`
+`,n.jsx(o.h2,{id:"content-paddings",children:"Content Paddings"}),`
+`,n.jsx(o.p,{children:n.jsx(o.a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-content-paddings--docs",rel:"nofollow",children:"Full Documentation"})}),`
+`,n.jsx(o.p,{children:"You can import the content paddings by adding the following imports to your project:"}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-ts",children:`import '@sap-ui/common-css/dist/sap-content-paddings.css';
+import '@sap-ui/common-css/dist/sap-container-type.css';
+`})}),`
+`,n.jsx(o.h3,{id:"responsive-content-paddings",children:"Responsive Content Paddings"}),`
+`,n.jsx(o.p,{children:"You can apply a dynamic content padding for your element by applying the following classes to your HTML:"}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-jsx",children:`<div className="sap-container-type-inline-size">
+  <div className="sap-content-paddings-container">I will have a responsive content padding</div>
+</div>
+`})}),`
+`,n.jsx(o.h2,{id:"margins--paddings",children:"Margins & Paddings"}),`
+`,n.jsx(o.p,{children:"Common CSS is offering classes that you can use to create some spacing between elements."}),`
+`,n.jsx(o.p,{children:n.jsx(o.strong,{children:"Full Documentation:"})}),`
+`,n.jsxs(o.ul,{children:[`
+`,n.jsx(o.li,{children:n.jsx(o.a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-margins--docs",rel:"nofollow",children:"Margins"})}),`
+`,n.jsx(o.li,{children:n.jsx(o.a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-padding--docs",rel:"nofollow",children:"Paddings"})}),`
+`]}),`
+`,n.jsx(o.pre,{children:n.jsx(o.code,{className:"language-jsx",children:`<div className="sap-padding-x-small">
+  <button className="sap-margin-begin-small">Start Button</button>
+  <button className="sap-margin-x-small">Mid Button</button>
+  <button className="sap-margin-end-small">End Button</button>
+</div>
+`})}),`
+`,n.jsx(m,{})]})}function Mn(t={}){const{wrapper:o}={...s(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(i,{...t})}):i(t)}export{Mn as default};
