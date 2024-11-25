ce2e5c3b9284e7231bb87914dcce1217b698adc6
Lukas742 <lukas.harbarth@sap.com>
2024-11-25 11:32:39 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@6ec7e060e23614b48b008fabf59f410e6e0446ed 🚀

---
+++ nightly/assets/Toast-CeCvTP24.js
@@ -0,0 +1,35 @@
+import{j as o}from"./jsx-runtime-D6fbYt3N.js";import{useMDXComponents as m}from"./index-DyCphG7h.js";import{ae as n,af as e}from"./index-CUwy_iwf.js";import"./index-OrErW1tb.js";import"./index-CcZIK_pp.js";import{C as s}from"./ControlsWithNote-DqH4Tv5j.js";import{D as a}from"./DocsHeader-D-kNoD55.js";import{F as c}from"./CommandsAndQueries-Cw7-rFhx.js";import"./index-DysCNOs_.js";import"./WrappingType-CW8URInd.js";import"./Title-DnczWaCy.js";import{C as h,D as i}from"./Toast.stories-Ddna5a8U.js";import"./iframe-B3tdtHXf.js";import"../sb-preview/runtime.js";import"./index-C4cLd1FO.js";import"./index-BcsOxL0D.js";import"./index-DrFu-skq.js";import"./withWebComponent-DpVKsRHi.js";import"./utils-BMpfTDbr.js";import"./useIsomorphicLayoutEffect-F-N6bu_1.js";import"./slot-_4yKMUwC.js";import"./event-BX8i4Y_x.js";import"./i18n-DEVDpFvK.js";import"./decline-Ch4ABsnB.js";import"./Icon-CrKv37md.js";import"./Keys-DoZifIQ_.js";import"./parameters-bundle.css-R51uC6BG.js";import"./i18n-defaults-neJWcM66.js";import"./information-D_LjlJHK.js";import"./alert-CiqdS0Ti.js";import"./class-map-CAgcI9af.js";import"./Button-CzXV-qq-.js";import"./AriaLabelHelper-C5uDZewF.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-HqN2rB8b.js";import"./i18n-defaults-CMHHS2wK.js";import"./TagDesign-Dou_yO3g.js";import"./utils-D2XxBjUD.js";import"./main-Dcm2jhNK.js";import"./index-DA9k7rzT.js";import"./Tag-BwJ4pIZx.js";import"./sys-help-2-Dw9i_TLE.js";import"./index-x5zB9JtW.js";import"./Popover-CxadHYVC.js";import"./PopupsCommon.css-Bu1W85h-.js";import"./FocusableElements-DpmShrVF.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D2m47C26.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-Bigav4TG.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-cawFe0IV.js";import"./Link-CcG_uYXc.js";import"./chunk-NUUEMKO5-N-uQsvCy.js";import"./client-CqEfiX9M.js";import"./copy-C_xK9_A_.js";import"./copy-BtTL6xAX.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-DLtQKbjK.js";import"./TableOfContent-DRym89KV.js";import"./index-DKiUnCEg.js";import"./I18nStore-CXxqSOxg.js";import"./useStylesheet-BAIsnduv.js";import"./index-7lca3pcM.js";import"./index-kNlKigw5.js";import"./Label-D_ADNoJV.js";import"./index-DP-u0XOf.js";import"./index-CA4smrai.js";import"./addCustomCSSWithScoping-DYmOnLgW.js";import"./index-qKU3PnwJ.js";import"./BusyIndicator-B17L6t2M.js";import"./index-C6sMyakJ.js";import"./index-CHrZj7jK.js";import"./Avatar-CLqwzIQ4.js";import"./employee-CUDhI-YO.js";import"./index-CvrYPgx2.js";function p(r){const t={code:"code",h2:"h2",p:"p",pre:"pre",strong:"strong",...m(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:h}),`
+`,o.jsx(a,{since:"0.9.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(e,{of:i}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:i}),`
+`,o.jsx(t.h2,{id:"show-a-toast",children:"Show a toast"}),`
+`,o.jsxs(t.p,{children:["You can open and close the ",o.jsx(t.code,{children:"Toaast"})," component in a declarative way using the ",o.jsx(t.code,{children:"open"})," prop."]}),`
+`,o.jsx(t.p,{children:o.jsx(t.strong,{children:"Example"})}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`export function MyComponentWithAToast() {
+  const [showToast, setShowToast] = useState(false);
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setShowToast(true);
+        }}
+      >
+        Show Toast
+      </Button>
+      <Toast
+        open={showToast}
+        onClose={() => {
+          setShowToast(false);
+        }}
+      >
+        This is my Toast!
+      </Toast>
+    </>
+  );
+}
+`})}),`
+`,o.jsx(c,{})]})}function Ho(r={}){const{wrapper:t}={...m(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(p,{...r})}):p(r)}export{Ho as default};
---
+++ nightly/assets/index-CcZIK_pp.js
@@ -0,0 +1,1 @@
+const i={version:"2.4.0",major:2,minor:4,patch:0,suffix:"",isNext:!1,buildTime:1732534253};i.version;export{i as V};
