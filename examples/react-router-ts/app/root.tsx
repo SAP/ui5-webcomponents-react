d4c2aa946481384b148fd7cf457acbc994429ddc
Marcus Notheis <marcus.notheis@sap.com>
2024-12-12 11:19:42 +0100

    chore(examples): migrate remix to react-router 7 (#6725)

---
+++ examples/react-router-ts/app/root.tsx
@@ -0,0 +1,36 @@
+import wcrStyles from '@ui5/webcomponents-react/styles.css?url';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
+import type { Route } from './+types/root';
+import { AppShell } from './components/AppShell';
+import appStyles from './globals.css?url';
+
+export const links: Route.LinksFunction = () => [
+  { rel: 'stylesheet', href: appStyles },
+  { rel: 'stylesheet', href: wcrStyles }
+];
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head suppressHydrationWarning={true}>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return (
+    <AppShell>
+      <Outlet />
+    </AppShell>
+  );
+}
--- examples/react-router-ts/app/routes/index.tsx
+++ examples/react-router-ts/app/routes/index.tsx
@@ -1,28 +1,29 @@
-import type { MetaFunction } from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
 import { Bar, Page, Title } from '@ui5/webcomponents-react';
+import type { MetaFunction } from 'react-router';
 import { TodoList } from '~/components/TodoList';
 import { Todo, todos } from '~/mockData/todos';
+import type { Route } from './+types/index';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
+  return [
+    { title: 'UI5 Web Components React - React Router Example' },
+    { name: 'description', content: 'Welcome to this example!' }
+  ];
 };
 
-export const loader = async () => {
+export async function loader() {
   const todoList = await new Promise<Todo[]>((resolve) => {
     setTimeout(() => {
       resolve(todos);
     }, 1500);
   });
 
-  return json({ data: { todos: todoList } });
-};
+  return { todos: todoList };
+}
 
-export default function Index() {
-  const {
-    data: { todos }
-  } = useLoaderData<typeof loader>();
+export default function Index({ loaderData }: Route.ComponentProps) {
+  const { todos } = loaderData;
+
   return (
     <Page header={<Bar style={{ paddingInline: 0 }} startContent={<Title>My Todos</Title>} />}>
       <TodoList items={todos} />
