fab4ae618122e538fa89e1ca160087755a5de763
Lukas742 <lukas.harbarth@sap.com>
2024-12-18 10:45:12 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@4d7c8e1de3059374d6eeb1e24429c4fe3a8f66fd 🚀

---
+++ nightly/assets/BulletChart-C0IbnqBZ.js
@@ -0,0 +1,48 @@
+import{j as o}from"./jsx-runtime-BjG_zV1W.js";import{useMDXComponents as p}from"./index-BxVt_j7t.js";import{ae as n,af as i}from"./index-CLZGOB6R.js";import"./index-C009Ii_l.js";import"./index-DLJU6rWM.js";import{C as s}from"./ControlsWithNote-DO3Ds2nq.js";import{D as a}from"./DocsHeader-DPbZsjT9.js";import{F as h}from"./CommandsAndQueries-BGNZD4F9.js";import"./index-B7gF9TUu.js";import"./WrappingType-CW8URInd.js";import"./Title-1rDkpSXf.js";import"./chunk-NUUEMKO5-CnxHGLB3.js";import{M as l}from"./TooltipConfig-CyJqCU8s.js";import{M as d}from"./LegendConfig-B4UbZUrl.js";import{C as c,D as m,W as x,a as u,L as j,b as f,c as g}from"./BulletChart.stories-BHSCP0pI.js";import"./iframe-D43Q3l11.js";import"../sb-preview/runtime.js";import"./index-CE_NGgsP.js";import"./index-DHaMx3uI.js";import"./index-DrFu-skq.js";import"./withWebComponent-CEvNJu7C.js";import"./utils-BPMdnTU-.js";import"./useIsomorphicLayoutEffect-BE7Iif4x.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-DEVDpFvK.js";import"./decline-D0Sq_J--.js";import"./Icon-Cf72TZgw.js";import"./Keys-CAuuEE_3.js";import"./parameters-bundle.css-DY1ryZJt.js";import"./i18n-defaults-neJWcM66.js";import"./information-Dy9R6tRZ.js";import"./alert-C1M-4Vvc.js";import"./class-map-CFjQlWtw.js";import"./Button-C6xYvRbu.js";import"./AccessibilityTextsHelper-Beeze-lh.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-XZ46U0Ql.js";import"./i18n-defaults-DteWAmDn.js";import"./TagDesign-Dou_yO3g.js";import"./utils-Ch8DzmyN.js";import"./main-BwlHOnYP.js";import"./index-EUzft9Z_.js";import"./Tag-xKaXZN0c.js";import"./sys-help-2-C1sMmkr1.js";import"./index-ClFOKzxg.js";import"./Popover-B0GOUfMQ.js";import"./PopupsCommon.css-BerD7w9b.js";import"./FocusableElements-DA0BaiCm.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-CJRte1-9.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-g8foa4rA.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-NbVlvM4j.js";import"./Link-2s4hefKa.js";import"./copy-BJtGExJZ.js";import"./copy-C7Fphd8b.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-D5efYGjp.js";import"./TableOfContent-DCqtbPc-.js";import"./index-ChqC1qFt.js";import"./I18nStore-DnWKAes0.js";import"./useStylesheet-DVp2Y5Uf.js";import"./index-vaKWw58t.js";import"./index-CUd5UQ0Z.js";import"./Label-S_BgfXUY.js";import"./index-Yz8_Wo9H.js";import"./index-D8L7QTA6.js";import"./Text-CTrlD69_.js";import"./addCustomCSSWithScoping-B_vnxDyr.js";import"./index-BBLdTHsH.js";import"./BusyIndicator-CTFEFSn6.js";import"./index-7rUfxUBv.js";import"./index-D6lFKeEV.js";import"./Avatar-DMTAeY6x.js";import"./employee-B31iqtd8.js";import"./client-DPlujwLe.js";import"./DemoProps-CYHKGZ3J.js";import"./ThemingParameters-B358vlzA.js";import"./index-C-Zfmu7Z.js";import"./YAxisTicks-D_Ota-sj.js";import"./ChartContainer-B2MiXhEK.js";import"./tiny-invariant-CCoILDQG.js";import"./index-Ca_8nO-p.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-DyufvGFW.js";import"./staticProps-BYOJSasi.js";import"./ChartDataLabel-C0-X8uIG.js";import"./useLongestYAxisLabel-ZeKbJFaC.js";import"./useOnClickInternal-DjdYT5ze.js";import"./react-content-loader.es-CXJNlgSt.js";import"./ComposedChart-BFENWiuD.js";import"./Line-UUZ_PtMM.js";import"./Scatter-D9JE6wV6.js";import"./useIsRTL-DV1DKYoi.js";import"./directionChange-d4UHEGHN.js";function e(r){const t={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...p(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:c}),`
+`,o.jsx(a,{isChart:!0}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:m}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:m}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"usage-notes",children:"Usage Notes"}),`
+`,o.jsxs(t.p,{children:[`In order to display the "Bullet" lines in the chart, you have to configure which measure should be rendered as bullet line.
+You can do this via the `,o.jsx(t.code,{children:"measures"})," array:"]}),`
+`,o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-jsx",children:`<BulletChart
+  data={
+    [
+      /* data */
+    ]
+  }
+  dimensions={
+    [
+      /* dimensions */
+    ]
+  }
+  measures={[
+    {
+      accessor: 'actuals',
+      label: 'Actual Revenue',
+      type: 'primary' // this will be the primary bar
+    },
+    {
+      accessor: 'target',
+      label: 'Planned Target Revenue',
+      type: 'comparison' // this will be the bullet line
+    }
+  ]}
+/>
+`})}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h3,{id:"with-custom-size",children:"With Custom Size"}),`
+`,o.jsx(i,{of:x}),`
+`,o.jsx(t.h3,{id:"with-highlighted-measures",children:"With Highlighted Measures"}),`
+`,o.jsx(i,{of:u}),`
+`,o.jsx(t.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(i,{of:j}),`
+`,o.jsx(l,{of:f}),`
+`,o.jsx(d,{of:g}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(h,{})]})}function lt(r={}){const{wrapper:t}={...p(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(e,{...r})}):e(r)}export{lt as default};
---
+++ nightly/assets/MessageView-G0NNqu6B.js
@@ -0,0 +1,74 @@
+import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{useMDXComponents as d}from"./index-BxVt_j7t.js";import{A as n}from"./ArgTypesWithNote-Cm022BGs.js";import{C as l}from"./ControlsWithNote-DO3Ds2nq.js";import{D as h}from"./DocsHeader-DPbZsjT9.js";import{F as c}from"./CommandsAndQueries-BGNZD4F9.js";import"./index-DLJU6rWM.js";import"./index-B7gF9TUu.js";import"./WrappingType-CW8URInd.js";import"./Title-1rDkpSXf.js";import{ae as x,af as i,ag as g,ai as r}from"./index-CLZGOB6R.js";import{C as j,D as s,M as u,W as f,a}from"./MessageView.stories-B3_XpuEm.js";import{S as w}from"./SubcomponentsSection-BVy4lpQe.js";import{M as m}from"./index-C05xU1MJ.js";import"./index-C009Ii_l.js";import"./withWebComponent-CEvNJu7C.js";import"./utils-BPMdnTU-.js";import"./useIsomorphicLayoutEffect-BE7Iif4x.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-DEVDpFvK.js";import"./decline-D0Sq_J--.js";import"./Icon-Cf72TZgw.js";import"./Keys-CAuuEE_3.js";import"./parameters-bundle.css-DY1ryZJt.js";import"./i18n-defaults-neJWcM66.js";import"./information-Dy9R6tRZ.js";import"./alert-C1M-4Vvc.js";import"./class-map-CFjQlWtw.js";import"./Button-C6xYvRbu.js";import"./AccessibilityTextsHelper-Beeze-lh.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-XZ46U0Ql.js";import"./i18n-defaults-DteWAmDn.js";import"./TagDesign-Dou_yO3g.js";import"./index-CE_NGgsP.js";import"./utils-Ch8DzmyN.js";import"./main-BwlHOnYP.js";import"./index-EUzft9Z_.js";import"./Tag-xKaXZN0c.js";import"./sys-help-2-C1sMmkr1.js";import"./index-ClFOKzxg.js";import"./Popover-B0GOUfMQ.js";import"./PopupsCommon.css-BerD7w9b.js";import"./FocusableElements-DA0BaiCm.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-0ACQ4G_A.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-CJRte1-9.js";import"./MediaRange-Bnzt26Xb.js";import"./style-map-g8foa4rA.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-NbVlvM4j.js";import"./Link-2s4hefKa.js";import"./chunk-NUUEMKO5-CnxHGLB3.js";import"./iframe-D43Q3l11.js";import"../sb-preview/runtime.js";import"./client-DPlujwLe.js";import"./copy-BJtGExJZ.js";import"./copy-C7Fphd8b.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-D5efYGjp.js";import"./TableOfContent-DCqtbPc-.js";import"./index-ChqC1qFt.js";import"./I18nStore-DnWKAes0.js";import"./useStylesheet-DVp2Y5Uf.js";import"./index-vaKWw58t.js";import"./index-CUd5UQ0Z.js";import"./Label-S_BgfXUY.js";import"./index-Yz8_Wo9H.js";import"./index-D8L7QTA6.js";import"./Text-CTrlD69_.js";import"./addCustomCSSWithScoping-B_vnxDyr.js";import"./index-BBLdTHsH.js";import"./BusyIndicator-CTFEFSn6.js";import"./index-7rUfxUBv.js";import"./index-D6lFKeEV.js";import"./Avatar-DMTAeY6x.js";import"./employee-B31iqtd8.js";import"./index-DHaMx3uI.js";import"./index-DrFu-skq.js";import"./ValueState-Bg0UWejw.js";import"./List-BeOyiQ4g.js";import"./ItemNavigation-DWbUprUI.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./TabbableElements-BJJu_CDc.js";import"./RadioButton-2y34RTmJ.js";import"./CheckBox-BN3CQ6El.js";import"./accept-W9Dxcudv.js";import"./slim-arrow-right-BHycGIJI.js";import"./slim-arrow-right-CC2qO1Ek.js";import"./index-Dz--vSRg.js";import"./index-De4UtJsw.js";import"./ListItemCustom-DkZQDz4H.js";import"./information-kEwumuig.js";import"./slim-arrow-left-MmYLYCKc.js";import"./index-D7NmXKZZ.js";import"./index-BMg1go_M.js";import"./Dialog-7o0SsYjd.js";import"./index-AyDRyCcB.js";import"./ResponsivePopover-JS6adhSh.js";import"./index-BIquPCJE.js";import"./i18n-defaults-lSsYHFh1.js";import"./index-CxIs-oDB.js";import"./index-FC1O9n0s.js";import"./index-uDj7SyZr.js";import"./SegmentedButton-DXn0Jqs7.js";function p(o){const t={code:"code",em:"em",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...d(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(x,{of:j}),`
+`,e.jsx(h,{subComponents:["MessageItem","MessageViewButton"]}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(i,{of:s}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(l,{of:s}),`
+`,e.jsx(t.h2,{id:"methods",children:"Methods"}),`
+`,e.jsx(t.p,{children:"This component exposes public methods. You can invoke them directly on the instance of the component, e.g. by using React Refs."}),`
+`,e.jsxs(t.table,{children:[e.jsx(t.thead,{children:e.jsxs(t.tr,{children:[e.jsx(t.th,{children:"Name"}),e.jsx(t.th,{children:"Parameters"}),e.jsx(t.th,{children:"Description"})]})}),e.jsx(t.tbody,{children:e.jsxs(t.tr,{children:[e.jsx(t.td,{children:e.jsx(t.strong,{children:e.jsx(t.code,{children:"navigateBack"})})}),e.jsx(t.td,{children:"—"}),e.jsx(t.td,{children:"Navigates back to the list view if you are in the details view of a message"})]})})]}),`
+`,e.jsx(t.h2,{id:"usage-notes",children:"Usage Notes"}),`
+`,e.jsxs(t.p,{children:["The ",e.jsx(t.code,{children:"MessageView"})," can be used in several scenarios, but most likely in Dialogs or Popovers."]}),`
+`,e.jsxs(t.p,{children:["If used in a Popover it is recommended that the ",e.jsx(t.code,{children:"MessageViewButton"}),` is used as an opener.
+The `,e.jsx(t.code,{children:"type"})," of the button should always reflect the highest severity (Error -> Warning -> Success -> Information)."]}),`
+`,e.jsxs(t.p,{children:["If the ",e.jsx(t.code,{children:"MessageView"})," is used in a standalone way, you can set the prop ",e.jsx(t.code,{children:"showDetailsPageHeader"})," to ",e.jsx(t.code,{children:"true"}),`.
+This will add a bar to the details page where a back button is contained.`]}),`
+`,e.jsxs(t.p,{children:["When used in a ",e.jsx(t.code,{children:"Dialog"})," or a ",e.jsx(t.code,{children:"Popover"}),", we recommend not setting the ",e.jsx(t.code,{children:"showDetailsPageHeader"}),` prop but rather listen
+to the `,e.jsx(t.code,{children:"onItemSelect"})," event and add a back button to your Dialog or Popover header and use the ",e.jsx(t.code,{children:"navigateBack()"}),` method
+to get back to the list view.`]}),`
+`,e.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(t.h3,{id:"messageview-in-a-dialog",children:"MessageView in a Dialog"}),`
+`,e.jsx(i,{of:u}),`
+`,e.jsxs(t.p,{children:["Sample Code: ",e.jsx(t.em,{children:"(Button and Dialog opening are omitted here)"})]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`function MyComponent() {
+  const messageViewRef = useRef(null);
+  const [isOnDetailsPage, setIsOnDetailsPage] = useState(false);
+  return (
+    <Dialog
+      resizable
+      style={{ width: '500px' }}
+      className="modal-without-padding"
+      header={
+        <Bar
+          startContent={
+            <FlexBox alignItems={FlexBoxAlignItems.Center}>
+              {isOnDetailsPage && (
+                <Button
+                  icon="slim-arrow-left"
+                  design={ButtonDesign.Transparent}
+                  onClick={() => {
+                    setIsOnDetailsPage(false);
+                    messageViewRef.current.navigateBack();
+                  }}
+                  style={{ marginInline: '0 0.5rem' }}
+                />
+              )}
+              <Title level={TitleLevel.H4}>Messages</Title>
+            </FlexBox>
+          }
+        />
+      }
+    >
+      <MessageView
+        ref={messageViewRef}
+        showDetailsPageHeader={false}
+        onItemSelect={() => {
+          setIsOnDetailsPage(true);
+        }}
+      >
+        {/* children */}
+      </MessageView>
+    </Dialog>
+  );
+}
+`})}),`
+`,e.jsx(t.h3,{id:"messageview-in-popover-with-messageviewbutton",children:"MessageView in Popover with MessageViewButton"}),`
+`,e.jsx(i,{of:f}),`
+`,e.jsx(g,{children:w}),`
+`,e.jsx(t.h2,{id:"messageitem",children:"MessageItem"}),`
+`,e.jsx(r,{of:a}),`
+`,e.jsx(n,{of:a}),`
+`,e.jsx(t.h2,{id:"messageviewbutton",children:"MessageViewButton"}),`
+`,e.jsx(r,{of:m}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(c,{})]})}function Mt(o={}){const{wrapper:t}={...d(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(p,{...o})}):p(o)}export{Mt as default};
