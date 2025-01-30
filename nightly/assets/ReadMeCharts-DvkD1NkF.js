60462ed74fbc76918c35557b2f0bff5ee9ba827c
MarcusNotheis <marcus.notheis@sap.com>
2025-01-30 17:17:18 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@9bf0e7c1a65b092c6a6e1bb16faf7f296d5da974 🚀

---
+++ nightly/assets/ReadMeCharts-DvkD1NkF.js
@@ -0,0 +1,38 @@
+import{j as t}from"./jsx-runtime-CLpGMVip.js";import{useMDXComponents as c}from"./index-ChplMHeC.js";import{M as u,a as l,i as f}from"./index-CJdbXfOE.js";import"./index-DuvBkkWn.js";import"./index-BcBMWyg5.js";import"./TagDesign-Dou_yO3g.js";import{r as e}from"./index-D23YZj_x.js";import"./index-DJ2M8xrO.js";import"./DocsRenderer-CFRXHY34-C5bSffB2.js";import"./Button-BYnClEc8.js";import"./withWebComponent-DIBLIPif.js";import"./copy-C92HBupN.js";import{F as h}from"./CommandsAndQueries-BR2NNqB2.js";import{T as d}from"./TableOfContent-QtRkCFei.js";import"./WrappingType-CW8URInd.js";import"./Title-Bnj9fnoW.js";import{C}from"./index-DojaeQYC.js";import{D}from"./BarChart.stories-3eOyz0us.js";import{D as b}from"./BulletChart.stories-9wygHFTm.js";import{D as y}from"./ColumnChart.stories-BoZamgCe.js";import{D as x}from"./ColumnChartWithTrend.stories-Fu-2leQx.js";import{D as v}from"./ComposedChart.stories-BhXBpcoi.js";import{D as g}from"./DonutChart.stories-CTyvDl1r.js";import{D as j}from"./LineChart.stories-B1HGROHi.js";import{D as w}from"./PieChart.stories-pH41YEiK.js";import{D as R}from"./RadarChart.stories-Z4irynhV.js";import{D as S}from"./RadialChart.stories-DNF3aKcy.js";import{D as $}from"./ScatterChart.stories-CT-UoFi9.js";import{T as E}from"./index-D_w2Lc5B.js";import"./iframe-DcPvjdJQ.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-DEVDpFvK.js";import"./Icon-DVlQjg5y.js";import"./useIsomorphicLayoutEffect-BuGJanM4.js";import"./Keys-Df3IBHp2.js";import"./parameters-bundle.css-C_3tvNHF.js";import"./decline-CkvCC5iV.js";import"./i18n-defaults-CUGG7UPM.js";import"./information-aOc0GfxQ.js";import"./information-B7lIx4H4.js";import"./sys-enter-2-Cn8hrStw.js";import"./sys-enter-2-DXus7nkZ.js";import"./i18n-defaults-Cyg2J0QB.js";import"./index-DkMQ0k7r.js";import"./client-lwzJIAjg.js";import"./AccessibilityTextsHelper-CSnJP9TL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-Cq5L8-91.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./utils-DKbukTmU.js";import"./Popover-DWH7u18I.js";import"./PopupsCommon.css-CLlsUzpB.js";import"./FocusableElements-Da8ni1pm.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-J9tSIEDW.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-Bn2o07mV.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Cetcbt3u.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BO3w5Xt1.js";import"./index-Dbb581IU.js";import"./Label-DMEL6fX-.js";import"./index-CwE3qlmk.js";import"./index-DoDMd5jQ.js";import"./Link-Cr8wHvq2.js";import"./index-CkeY3nPI.js";import"./index-Cs_Ztk0v.js";import"./Text-DZiuAp7s.js";import"./addCustomCSSWithScoping-Dkzu7Yq-.js";import"./index-B5N8tz-A.js";import"./index-D1TSxMr6.js";import"./BusyIndicator-CgjmT3Fu.js";import"./index-CKTWv80J.js";import"./index-DN8baC31.js";import"./AvatarSize-BceVhWoP.js";import"./employee-DObyMRaP.js";import"./index-DcguYNce.js";import"./I18nStore-DlsSjWbm.js";import"./Carousel-CxJU7FGI.js";import"./ScrollEnablement-Cg3XvxgD.js";import"./animate-JylhYHXj.js";import"./AnimationMode-BtMEB35m.js";import"./slim-arrow-left-DilwSCcv.js";import"./slim-arrow-right-CF6hjUVH.js";import"./DemoProps-CM7ve7wo.js";import"./ThemingParameters-le4wpk1U.js";import"./staticProps-CecrJNp0.js";import"./ChartContainer-Ddn5Ok0K.js";import"./tiny-invariant-CopsF_GD.js";import"./index-BJRYdxMG.js";import"./useCancelAnimationFallback-CvvvAovm.js";import"./YAxisTicks-CPwNOrkg.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-D2-9i9sI.js";import"./ChartDataLabel-KRjjjjxD.js";import"./useOnClickInternal-CeuYB1HK.js";import"./react-content-loader.es-CXkD-qGt.js";import"./useIsRTL-C6PLHWQa.js";import"./directionChange-D2aZW7Vx.js";import"./useLongestYAxisLabel-BhoppQ_h.js";import"./ComposedChart-BCCk-n8V.js";import"./Line-BcSu_mC_.js";import"./Scatter-BFKnWH0W.js";import"./index-CDpALAk1.js";import"./PieChart-VI8wj4tl.js";import"./Placeholder-Aw3rQLND.js";import"./LineChart-4f_HkUYC.js";const T=`# @ui5/webcomponents-react-charts
+
+Chart library built on top of [recharts](https://recharts.org/) for ui5-webcomponents-react.
+
+## Installation
+
+\`\`\`bash
+npm install @ui5/webcomponents-react-charts
+\`\`\`
+
+## Accessibility
+
+Charts only offer limited accessibility support with only basic built-in features, so it’s essential to ensure your implementation meets the accessibility standards of your application.
+
+## React 19 support
+
+To use this library with React 19 you have to override your \`react-is\` version to match your React version!
+
+## Documentation
+
+You can find an interactive documentation in our [Storybook](https://sap.github.io/ui5-webcomponents-react/).
+
+## Contribute
+
+Please check our [Contribution Guidelines](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md).
+
+## License
+
+Please see our [LICENSE](https://github.com/SAP/ui5-webcomponents-react/blob/main/LICENSE) for copyright and license information.
+Detailed information including third-party components and their licensing/copyright information is available via the [REUSE tool](https://api.reuse.software/info/github.com/SAP/ui5-webcomponents-react).
+
+<!-- Use the force -->
+`;function I(o){const{children:r}=o,i=e.useRef(null),m=e.useRef(0),n=e.useRef(null),p=()=>{n.current&&(clearInterval(n.current),n.current=null)};return e.useEffect(()=>{const a=i.current;return a&&r.length&&(n.current=setInterval(()=>{m.current++,a.navigateTo(m.current%r.length)},5e3)),()=>{p()}},[r]),t.jsx(C,{style:{height:"500px"},arrowsPlacement:"Navigation",cyclic:!0,ref:i,onNavigate:()=>{p()},...o})}function s(o){const r={div:"div",h2:"h2",...c(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(u,{title:"Charts / Docs"}),`
+`,t.jsx(d,{}),`
+`,t.jsx(l,{children:T.split("## Documentation")[0].trim()}),`
+`,t.jsx(r.h2,{id:"charts",children:"Charts"}),`
+`,t.jsx(I,{children:[{name:"BarChart",component:D},{name:"BulletChart",component:b},{name:"ColumnChart",component:y},{name:"ColumnChartWithTrend",component:x},{name:"ComposedChart",component:v},{name:"DonutChart",component:g},{name:"LineChart",component:j},{name:"PieChart",component:w},{name:"RadarChart",component:R},{name:"RadialChart",component:S},{name:"ScatterChart",component:$}].map(i=>t.jsxs(r.div,{style:{width:"100%"},children:[t.jsx(E,{children:i.name}),t.jsx(f,{of:i.component})]},i.name))}),`
+`,t.jsx(h,{})]})}function Uo(o={}){const{wrapper:r}={...c(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(s,{...o})}):s(o)}export{Uo as default};
---
+++ nightly/assets/ReadMeCompat-Q-amKGEE.js
@@ -0,0 +1,48 @@
+import{j as t}from"./jsx-runtime-CLpGMVip.js";import{useMDXComponents as n}from"./index-ChplMHeC.js";import{M as r,a as m}from"./index-CJdbXfOE.js";import"./index-DuvBkkWn.js";import"./index-BcBMWyg5.js";import"./TagDesign-Dou_yO3g.js";import"./index-D23YZj_x.js";import"./index-DJ2M8xrO.js";import"./DocsRenderer-CFRXHY34-C5bSffB2.js";import"./Button-BYnClEc8.js";import"./withWebComponent-DIBLIPif.js";import"./copy-C92HBupN.js";import{F as p}from"./CommandsAndQueries-BR2NNqB2.js";import{T as a}from"./TableOfContent-QtRkCFei.js";import"./WrappingType-CW8URInd.js";import"./Title-Bnj9fnoW.js";import"./iframe-DcPvjdJQ.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-DEVDpFvK.js";import"./Icon-DVlQjg5y.js";import"./useIsomorphicLayoutEffect-BuGJanM4.js";import"./Keys-Df3IBHp2.js";import"./parameters-bundle.css-C_3tvNHF.js";import"./decline-CkvCC5iV.js";import"./i18n-defaults-CUGG7UPM.js";import"./information-aOc0GfxQ.js";import"./information-B7lIx4H4.js";import"./sys-enter-2-Cn8hrStw.js";import"./sys-enter-2-DXus7nkZ.js";import"./i18n-defaults-Cyg2J0QB.js";import"./index-DkMQ0k7r.js";import"./client-lwzJIAjg.js";import"./AccessibilityTextsHelper-CSnJP9TL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-Cq5L8-91.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./utils-DKbukTmU.js";import"./Popover-DWH7u18I.js";import"./PopupsCommon.css-CLlsUzpB.js";import"./FocusableElements-Da8ni1pm.js";import"./isElementHidden-B4HqmI2E.js";import"./isElementClickable-J9tSIEDW.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-Bn2o07mV.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Cetcbt3u.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BO3w5Xt1.js";import"./index-Dbb581IU.js";import"./Label-DMEL6fX-.js";import"./index-CwE3qlmk.js";import"./index-DoDMd5jQ.js";import"./Link-Cr8wHvq2.js";import"./index-CkeY3nPI.js";import"./index-Cs_Ztk0v.js";import"./Text-DZiuAp7s.js";import"./addCustomCSSWithScoping-Dkzu7Yq-.js";import"./index-B5N8tz-A.js";import"./index-D1TSxMr6.js";import"./BusyIndicator-CgjmT3Fu.js";import"./index-CKTWv80J.js";import"./index-DN8baC31.js";import"./AvatarSize-BceVhWoP.js";import"./employee-DObyMRaP.js";import"./index-DcguYNce.js";import"./I18nStore-DlsSjWbm.js";const s=`# @ui5/webcomponents-react-compat
+
+The \`compat\` (compatibility) package provides components that were removed in the v2 release but are still available for backwards compatibility.
+
+Some of these components lack accessibility or design support, while others have been replaced by UI5 web components. In general, we recommend using components from this package only as a temporary solution.
+
+## Component Support
+
+Components in this package will not receive any new features. Only critical bugs, that are outside the component's inherent limitations, will be addressed. This package will be maintained until the next major release (which is not currently on our roadmap).
+
+## Installation
+
+\`\`\`bash
+npm install @ui5/webcomponents-react-compat
+\`\`\`
+
+## Importing Components
+
+As the v1 \`ui5-table\` (\`Table\`) component is part of this package and some custom element names are equal to the new v2 \`ui5-table\`, importing components from the root (\`import { Table } from "@ui5/webcomponents-react-compat"\`) is discouraged.
+The primary reason is that tree-shaking capabilities are limited in the default configurations of most bundlers in dev mode. As a result, custom elements for the v1 table will still be defined, even if only the \`Toolbar\` is imported. This can cause unexpected behavior if the v2 table is also used in the same app.
+
+**Please only import components from the file path!**
+
+E.g.:
+
+- \`import { Table } from '@ui5/webcomponents-react-compat/dist/components/Table/index.js';\`
+- \`import { Toolbar } from '@ui5/webcomponents-react-compat/dist/components/Toolbar/index.js';\`
+
+**It is not supported using the v1 \`Table\` and the v2 \`Table\` in the same application!**
+
+## Documentation
+
+You can find an interactive documentation in our [Storybook](https://sap.github.io/ui5-webcomponents-react/) (Legacy Components).
+
+## Contribute
+
+Please check our [Contribution Guidelines](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md).
+
+## License
+
+Please see our [LICENSE](https://github.com/SAP/ui5-webcomponents-react/blob/main/LICENSE) for copyright and license information.
+Detailed information including third-party components and their licensing/copyright information is available via the [REUSE tool](https://api.reuse.software/info/github.com/SAP/ui5-webcomponents-react).
+
+<!-- Use the force 2 -->
+`;function i(o){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Legacy Components / Docs"}),`
+`,t.jsx(a,{}),`
+`,t.jsx(m,{children:s.split("## Documentation")[0].trim()}),`
+`,t.jsx(p,{})]})}function It(o={}){const{wrapper:e}={...n(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(i,{...o})}):i()}export{It as default};
