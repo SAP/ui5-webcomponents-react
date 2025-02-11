1b179a4e31f5f6217d321b47d35a502cb2bb3529
Lukas Harbarth <lukas.harbarth@sap.com>
2025-02-11 11:42:36 +0100

    move `withWebComponent`, types & utils to base pkg

--- packages/main/src/types/Ui5CustomEvent.ts
+++ packages/main/src/types/Ui5CustomEvent.ts
@@ -1,5 +1,1 @@
-export interface Ui5CustomEvent<EventTarget = HTMLElement, Detail = never>
-  extends Omit<CustomEvent<Detail>, 'target' | 'currentTarget'> {
-  target: EventTarget;
-  currentTarget: EventTarget | null;
-}
+export type { Ui5CustomEvent } from '@ui5/webcomponents-react-base';
