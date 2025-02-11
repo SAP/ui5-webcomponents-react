08b3f1779398f4b75dbac6e48f158b5a6f19da09
Lukas Harbarth <lukas.harbarth@sap.com>
2025-02-11 12:23:21 +0100

    Update index.tsx

--- packages/main/src/webComponents/ExpandableText/index.tsx
+++ packages/main/src/webComponents/ExpandableText/index.tsx
@@ -5,6 +5,36 @@ import type ExpandableTextOverflowMode from '@ui5/webcomponents/dist/types/Expan
 import type TextEmptyIndicatorMode from '@ui5/webcomponents/dist/types/TextEmptyIndicatorMode.js';
 import { withWebComponent } from '@ui5/webcomponents-react-base';
 import type { CommonProps, Ui5DomRef } from '@ui5/webcomponents-react-base';
+import { addCustomCSSWithScoping } from '@ui5/webcomponents-react-base/dist/utils/addCustomCSSWithScoping.js';
+
+//todo: remove once ExpandableText supports setting white-space
+addCustomCSSWithScoping(
+  'ui5-expandable-text',
+  `
+:host([data-_render-whitespace]) [ui5-text]{
+white-space: pre-wrap;
+}`
+);
+
+interface DeprecatedExpandableTextProps {
+  /**
+   * Text of the component.
+   *
+   * @deprecated: Please use `text` instead.
+   */
+  children?: string;
+  /**
+   * Determines if the full text should be displayed inside a `ResponsivePopover` or in-place.
+   *
+   * @deprecated: Please use `overflowMode` instead.
+   */
+  showOverflowInPopover?: boolean;
+  //todo: deprecate once white-space can be applied w/o addCustomCSS
+  /**
+   * Defines how white-space inside <code>Text</code> is handled. If set to true, sequences of white space are preserved.
+   */
+  renderWhitespace?: boolean;
+}
 
 interface ExpandableTextAttributes {
   /**
@@ -34,7 +64,10 @@ interface ExpandableTextAttributes {
 
 interface ExpandableTextDomRef extends Required<ExpandableTextAttributes>, Ui5DomRef {}
 
-interface ExpandableTextPropTypes extends ExpandableTextAttributes, Omit<CommonProps, keyof ExpandableTextAttributes> {}
+interface ExpandableTextPropTypes
+  extends ExpandableTextAttributes,
+    Omit<CommonProps, keyof ExpandableTextAttributes | 'children'>,
+    DeprecatedExpandableTextProps {}
 
 /**
  * The `ExpandableText` component allows displaying a large body of text in a small space. It provides an "expand/collapse" functionality, which shows/hides potentially truncated text.
@@ -52,7 +85,8 @@ interface ExpandableTextPropTypes extends ExpandableTextAttributes, Omit<CommonP
  *
  * On phones, if the component is configured to display the full text in a popover, the popover will appear in full screen.
  *
- *
+ * __Note:__ For compatibility reasons, `children`, `showOverflowInPopover`, and `renderWhitespace` are added by the UI5 Web Components for React wrapper and are not part of the underlying web component (`ui5-expandable-text`).
+ * These props will be removed in the next major release (not yet planned); see their deprecation notices for alternatives.
  *
  * __Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)
  *
