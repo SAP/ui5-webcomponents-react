8500719fb4880af5bdb4d8f81ba1481b89f05fb7
Lukas Harbarth <lukas.harbarth@sap.com>
2025-02-11 17:24:23 +0100

    Update index.ts

--- packages/ai/src/index.ts
+++ packages/ai/src/index.ts
@@ -3,4 +3,5 @@ export { ButtonState } from './components/ButtonState/index.js';
 export { PromptInput } from './components/PromptInput/index.js';
 
 export type { ButtonDomRef, ButtonPropTypes } from './components/Button/index.js';
+export type { ButtonStateDomRef, ButtonStatePropTypes } from './components/ButtonState/index.js';
 export type { PromptInputDomRef, PromptInputPropTypes } from './components/PromptInput/index.js';
