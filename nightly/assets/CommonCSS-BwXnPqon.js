a20b833cee223878178296a0b7535353900e67fc
Lukas742 <lukas.harbarth@sap.com>
2025-03-13 16:15:53 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@fb681d1618ca9e28a0ea509716a130e46c055f5f 🚀

---
+++ nightly/assets/CommonCSS-BwXnPqon.js
@@ -0,0 +1,33 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-zdXs1Etw.js";import{M as r}from"./index-CER4y58q.js";import{M as e}from"./index-Ckh4UXjO.js";import"./index-N5iqyQnR.js";import"./TagDesign-Ga50IS6u.js";import"./index-C--kS4iy.js";import"./index-DJcrgQOn.js";import{F as m}from"./CommandsAndQueries-BqtyXCDA.js";import"./Button-CDUAIOHY.js";import"./jsx-runtime-TYk6ziax.js";import"./copy-BIm13QSs.js";import{T as p}from"./TableOfContent-Bzt3eeAv.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-BZr7m-U9.js";import{L as a}from"./index-DG9f8b92.js";import"./iframe-JhipV5QF.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-CJfyoCyo.js";import"./CustomElementsScope-BUTWMsdD.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./decline-Ctf1iLC2.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CaDfZmsc.js";import"./information-axph4kwY.js";import"./sys-enter-2-W20sQgrM.js";import"./sys-enter-2-Dj0rlhGw.js";import"./i18n-defaults-BXUwkpCs.js";import"./index-B-pEBbkX.js";import"./preview-D5cLJ17q.js";import"./DocsRenderer-CFRXHY34-D5fHW_-0.js";import"./react-18-DGA_MWY9.js";import"./Popover-CYWkC5M7.js";import"./PopupsCommon.css-C4odaWdv.js";import"./FocusableElements-B0h6XdH-.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-BhhEdsAZ.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-DueWit44.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-D-3zc5-n.js";import"./index-BxZpBR5v.js";import"./Label-CNIQ9f-B.js";import"./index-DmVWxwwF.js";import"./index-C4AHKFdV.js";import"./index-DA4Nf3tz.js";import"./Text-BE7aNcNC.js";import"./willShowContent-CZcfsNXp.js";import"./Link-DJmP58JE.js";import"./addCustomCSSWithScoping-_tNs8Q2d.js";import"./index-CkLIGxQ0.js";import"./BusyIndicator-DRlXpx-R.js";import"./index-PdcYfHMq.js";import"./index-BGMpZMYY.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DCPNHV1H.js";import"./index-CVAswlxT.js";import"./I18nStore-DdNtknPF.js";import"./Tooltips-DGo6fNOA.js";import"./EventProvider-ChtD9rRX.js";function i(t){const n={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...s(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(r,{title:"Common CSS"}),`
+`,o.jsx(n.h1,{id:"using-common-css-in-your-application",children:"Using Common CSS in your application"}),`
+`,o.jsx(p,{}),`
+`,o.jsxs(n.p,{children:["We recommend using the ",o.jsx(n.a,{href:"https://www.npmjs.com/package/@sap-ui/common-css",rel:"nofollow",children:"@sap-ui/common-css"})," package for applying common styling blocks based on SAP Fiori Design Guidelines."]}),`
+`,o.jsx(e,{hideCloseButton:!0,children:o.jsxs(o.Fragment,{children:["This documentation is only showing a few samples which we consider to be useful for applications using UI5 Web Components for React. You can find the full documentation for SAP UI Common CSS"," ",o.jsx(a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-introduction--docs",children:"here"}),"."]})}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(n.h2,{id:"content-paddings",children:"Content Paddings"}),`
+`,o.jsx(n.p,{children:o.jsx(n.a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-content-paddings--docs",rel:"nofollow",children:"Full Documentation"})}),`
+`,o.jsx(n.p,{children:"You can import the content paddings by adding the following imports to your project:"}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-ts",children:`import '@sap-ui/common-css/dist/sap-content-paddings.css';
+import '@sap-ui/common-css/dist/sap-container-type.css';
+`})}),`
+`,o.jsx(n.h3,{id:"responsive-content-paddings",children:"Responsive Content Paddings"}),`
+`,o.jsx(n.p,{children:"You can apply a dynamic content padding for your element by applying the following classes to your HTML:"}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`<div className="sap-container-type-inline-size">
+  <div className="sap-content-paddings-container">I will have a responsive content padding</div>
+</div>
+`})}),`
+`,o.jsx(n.h2,{id:"margins--paddings",children:"Margins & Paddings"}),`
+`,o.jsx(n.p,{children:"Common CSS is offering classes that you can use to create some spacing between elements."}),`
+`,o.jsx(n.p,{children:o.jsx(n.strong,{children:"Full Documentation:"})}),`
+`,o.jsxs(n.ul,{children:[`
+`,o.jsx(n.li,{children:o.jsx(n.a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-margins--docs",rel:"nofollow",children:"Margins"})}),`
+`,o.jsx(n.li,{children:o.jsx(n.a,{href:"https://sap.github.io/fundamental-styles/?path=/docs/common-css-padding--docs",rel:"nofollow",children:"Paddings"})}),`
+`]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`<div className="sap-padding-x-small">
+  <button className="sap-margin-begin-small">Start Button</button>
+  <button className="sap-margin-x-small">Mid Button</button>
+  <button className="sap-margin-end-small">End Button</button>
+</div>
+`})}),`
+`,o.jsx(m,{})]})}function No(t={}){const{wrapper:n}={...s(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(i,{...t})}):i(t)}export{No as default};
---
+++ nightly/assets/Public-Utils-CWOYfCs4.js
@@ -0,0 +1,70 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as p}from"./index-zdXs1Etw.js";import{M as S,S as s}from"./index-CER4y58q.js";import{M as C,a as y}from"./index-Ckh4UXjO.js";import"./index-N5iqyQnR.js";import"./TagDesign-Ga50IS6u.js";import{r as l}from"./index-C--kS4iy.js";import"./index-DJcrgQOn.js";import{F as D}from"./CommandsAndQueries-BqtyXCDA.js";import"./Button-CDUAIOHY.js";import{r as T,u as P}from"./jsx-runtime-TYk6ziax.js";import"./copy-BIm13QSs.js";import{T as k}from"./TableOfContent-Bzt3eeAv.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-BZr7m-U9.js";import{T as o}from"./ThemingParameters-le4wpk1U.js";import"./CustomElementsScope-BUTWMsdD.js";import{M as R}from"./utils-DaKUh0cv.js";import{T as M}from"./index-CVAswlxT.js";import{S as B}from"./index-CVh3NAtu.js";import{O as F}from"./index-CZitOBzd.js";import{P as d}from"./index-BzJwQsi7.js";import{F as E,a as I}from"./index-DueWit44.js";import{T as O}from"./index-DA4Nf3tz.js";import{L as V}from"./index-BxZpBR5v.js";import"./iframe-JhipV5QF.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-CJfyoCyo.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./decline-Ctf1iLC2.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CaDfZmsc.js";import"./information-axph4kwY.js";import"./sys-enter-2-W20sQgrM.js";import"./sys-enter-2-Dj0rlhGw.js";import"./i18n-defaults-BXUwkpCs.js";import"./index-B-pEBbkX.js";import"./preview-D5cLJ17q.js";import"./DocsRenderer-CFRXHY34-D5fHW_-0.js";import"./react-18-DGA_MWY9.js";import"./Popover-CYWkC5M7.js";import"./PopupsCommon.css-C4odaWdv.js";import"./FocusableElements-B0h6XdH-.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-BhhEdsAZ.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-DmVWxwwF.js";import"./index-DG9f8b92.js";import"./Link-DJmP58JE.js";import"./index-C4AHKFdV.js";import"./addCustomCSSWithScoping-_tNs8Q2d.js";import"./clsx-B-dksMZM.js";import"./index-CkLIGxQ0.js";import"./BusyIndicator-DRlXpx-R.js";import"./Label-CNIQ9f-B.js";import"./index-PdcYfHMq.js";import"./index-BGMpZMYY.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DCPNHV1H.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DGo6fNOA.js";import"./EventProvider-ChtD9rRX.js";import"./main-BZcY5MQX.js";import"./I18nStore-DdNtknPF.js";import"./useStylesheet-D-3zc5-n.js";import"./Select-CycR8LxN.js";import"./InvisibleMessage-CmyZGU0D.js";import"./ValueState-Bg0UWejw.js";import"./List-BzJKOKyM.js";import"./ItemNavigation-CAinmIbD.js";import"./handleDrop-DAKm39Zp.js";import"./DropIndicator-aa1VaaHv.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-CMdRBa5_.js";import"./TabbableElements-XzXYKyvZ.js";import"./ResponsivePopover-B1wBYLBj.js";import"./Dialog-BomKBOpU.js";import"./slim-arrow-down-BivbN2fg.js";import"./slim-arrow-down-Bt-iwN9_.js";import"./ResponsivePopoverCommon.css-Dyu-3t3O.js";import"./ValueStateMessage.css-hLRGYX_8.js";import"./Option-DZaw-r6a.js";import"./ListItemBaseTemplate-Bh5w9A5L.js";import"./ListItemAdditionalText.css-Dsf8eclZ.js";import"./Panel-BqyU6yVj.js";import"./slideUp-DNFkFHG8.js";import"./animate-JylhYHXj.js";import"./AnimationMode-4jBfm7r7.js";import"./slim-arrow-right-Cb1dHIzE.js";import"./Text-BE7aNcNC.js";const c={display:"grid",gridTemplateColumns:"repeat(3, minmax(0, 1fr))",rowGap:"1rem",columnGap:"1rem"},x=[],u=[],j=[],z=Object.entries(o).filter(([r,i])=>r.includes("Font")?r.includes("FontUrl")?!0:(x.push([r,i]),!1):r.includes("Color")||r.includes("Shadow")||r.includes("Background")||r.includes("Chart")?(u.push([r,i]),!1):r.includes("Space")||r.includes("Margin")||r.includes("Padding")||r.includes("Gap")||r.includes("Breakpoint")?(j.push([r,i]),!1):!0),L=r=>{const i={};return r.includes("Family")||r.includes("FontUrl")?i.fontFamily=r:r.includes("Size")?i.fontSize=r:r.includes("Weight")&&(i.fontWeight=r),i},H=r=>{const i={};return r.includes("Shadow")?i.boxShadow=r:i.backgroundColor=r,i},b=({value:r,varKey:i,theme:n,style:t={}})=>{const[a,g]=l.useReducer(v=>!v,!1),[f,w]=l.useState(getComputedStyle(document.documentElement).getPropertyValue(`--${i}`));return l.useEffect(()=>{setTimeout(()=>{w(getComputedStyle(document.documentElement).getPropertyValue(`--${i}`))},200)},[n]),e.jsxs(E,{direction:I.Column,style:{width:"85%"},children:[e.jsx(O,{title:"Click to show CSS Variable",style:{cursor:"pointer",...t},onClick:g,children:a?r:i}),e.jsx(V,{children:f})]},i)},A=r=>{const{varKey:i,value:n,theme:t}=r;return e.jsxs("div",{style:{display:"inline-flex",alignItems:"center",justifyContent:"space-between"},children:[e.jsx(b,{value:n,varKey:i,theme:t}),e.jsx("div",{style:{...H(n),borderRadius:"50%",width:"2rem",height:"2rem",minWidth:"2rem",border:`1px solid ${o.sapField_BorderColor}`}})]})},h=r=>{const{varKey:i,value:n,theme:t,style:a={}}=r;return e.jsx("div",{style:{display:"inline-flex",alignItems:"center",justifyContent:"space-between"},children:e.jsx(b,{value:n,varKey:i,style:a,them:t})})},G=()=>{const[r,i]=l.useState(T());return e.jsx(M,{children:e.jsxs("div",{style:{backgroundColor:o.sapBackgroundColor,padding:"1rem",borderRadius:o.sapElement_BorderCornerRadius,boxShadow:o.sapContent_Shadow0},children:[e.jsx(B,{onChange:n=>{const t=n.detail.selectedOption.dataset.value;i(t),P(t)},children:R.map(({title:n,value:t})=>e.jsx(F,{"data-value":t,selected:t===r,children:n},t))}),e.jsx("br",{}),e.jsx("br",{}),e.jsx(d,{headerText:"Colors & Shadows",collapsed:!0,children:e.jsx("div",{style:c,children:u.map(([n,t])=>e.jsx(A,{varKey:n,value:t,theme:r},n))})}),e.jsx(d,{headerText:"Fonts",collapsed:!0,children:e.jsx("div",{style:{...c,gridTemplateColumns:"repeat(1, minmax(0, 1fr))"},children:x.map(([n,t])=>e.jsx(h,{varKey:n,value:t,style:{...L(t)},theme:r},n))})}),e.jsx(d,{headerText:"Spacing",collapsed:!0,children:e.jsx("div",{style:c,children:j.map(([n,t])=>e.jsx(h,{varKey:n,value:t,theme:r},n))})}),e.jsx(d,{headerText:"Others",collapsed:!0,children:e.jsx("div",{style:c,children:z.map(([n,t])=>e.jsx(h,{varKey:n,value:t,theme:r},n))})})]})})};function m(r){const i={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",ul:"ul",...p(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(S,{title:"Public Utils"}),`
+`,e.jsx(i.h1,{id:"public-utils",children:"Public Utils"}),`
+`,e.jsxs(i.p,{children:["The ",e.jsx(i.code,{children:"@ui5/webcomponents-react-base"})," package is providing a couple of utils, which can be used in your application as well."]}),`
+`,e.jsx(C,{design:y.Critical,hideCloseButton:!0,children:`Please only use the utils mentioned in this document in your application. All other exports are intended for internal
+  use and are not subject to semantic versioning!`}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(k,{}),`
+`,e.jsx(i.h2,{id:"device",children:"Device"}),`
+`,e.jsx(s,{code:"import { Device } from '@ui5/webcomponents-react-base';"}),`
+`,e.jsxs(i.p,{children:["The ",e.jsx(i.code,{children:"Device"})," allows you to detect information about the environment where your app is running:"]}),`
+`,e.jsx(i.h3,{id:"general-device-information",children:"General Device Information"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.isIE()"})," - returns ",e.jsx(i.code,{children:"true"})," if running in Internet Explorer"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.isSafari()"})," - returns ",e.jsx(i.code,{children:"true"})," if running in Safari"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.isDesktop()"})," - returns ",e.jsx(i.code,{children:"true"})," if running on desktop devices"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.isTablet()"})," - returns ",e.jsx(i.code,{children:"true"})," if running on tablet devices"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.isPhone()"})," - returns ",e.jsx(i.code,{children:"true"})," if running on mobile phone devices"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.supportsTouch()"})," - returns ",e.jsx(i.code,{children:"true"})," if device supports touch"]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.getCurrentRange(width?: number)"})," ",e.jsx("br",{}),`
+Returns the current media range of the window, or the optionally passed width. `,e.jsx("br",{}),`
+Returns: `,e.jsx(i.code,{children:"{ from: number, to?: number, name: 'Phone' | 'Tablet' | 'Desktop' | 'LargeDesktop', unit: string }"})]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.getOrientation()"})," ",e.jsx("br",{}),`
+Returns the current orientation of the device, `,e.jsx(i.code,{children:"{ landscape: boolean, portrait: boolean }"}),"."]}),`
+`]}),`
+`,e.jsx(i.h3,{id:"event-listeners",children:"Event Listeners"}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.attachResizeHandler(callback)"})," ",e.jsx("br",{}),`
+Attach an event handler which will be invoked whenever the window size has changed.
+`,e.jsx(i.code,{children:"callback"})," is a function which will be called with ",e.jsx(i.code,{children:"{ height: number, width: number }"}),"."]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.detachResizeHandler(callback)"})," ",e.jsx("br",{}),`
+Detach a previously attached resize handler`]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.attachOrientationChangeHandler(callback)"})," ",e.jsx("br",{}),`
+Attach an event handler which will be invoked the orientation of the device has changed e.g., through rotation.
+`,e.jsx(i.code,{children:"callback"})," is a function which will be called with ",e.jsx(i.code,{children:"{ landscape: boolean, portrait: boolean }"}),"."]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.detachOrientationChangeHandler(callback)"})," ",e.jsx("br",{}),`
+Detach a previously attached orientation change handler`]}),`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.attachMediaHandler(callback)"})," ",e.jsx("br",{}),`
+Attach an event handler whenever the window size is matching another media query. `,e.jsx("br",{}),`
+The current window size will be matched against the standard SAP Responsive Breakpoints:`]}),`
+`]}),`
+`,e.jsxs(i.table,{children:[e.jsx(i.thead,{children:e.jsxs(i.tr,{children:[e.jsx(i.th,{children:"Media Query"}),e.jsx(i.th,{children:"Name"})]})}),e.jsxs(i.tbody,{children:[e.jsxs(i.tr,{children:[e.jsx(i.td,{children:e.jsx(i.code,{children:"(max-width:599px)"})}),e.jsx(i.td,{children:e.jsx(i.code,{children:"Phone"})})]}),e.jsxs(i.tr,{children:[e.jsx(i.td,{children:e.jsx(i.code,{children:"(min-width:600px) and (max-width:1023px)"})}),e.jsx(i.td,{children:e.jsx(i.code,{children:"Tablet"})})]}),e.jsxs(i.tr,{children:[e.jsx(i.td,{children:e.jsx(i.code,{children:"(min-width:1024px) and (max-width:1439px)"})}),e.jsx(i.td,{children:e.jsx(i.code,{children:"Desktop"})})]}),e.jsxs(i.tr,{children:[e.jsx(i.td,{children:e.jsx(i.code,{children:"(min-width:1440px)"})}),e.jsx(i.td,{children:e.jsx(i.code,{children:"LargeDesktop"})})]})]})]}),`
+`,e.jsxs(i.p,{children:[e.jsx(i.code,{children:"callback"}),` is a function which will be called with
+`,e.jsx(i.code,{children:"{ from: number, to?: number, name: 'Phone' | 'Tablet' | 'Desktop' | 'LargeDesktop', unit: string }"}),"."]}),`
+`,e.jsxs(i.ul,{children:[`
+`,e.jsxs(i.li,{children:[e.jsx(i.code,{children:"Device.detachMediaHandler(callback)"})," ",e.jsx("br",{}),`
+Detach a previously attached media handler`]}),`
+`]}),`
+`,e.jsx(i.h2,{id:"theming-parameters",children:"Theming Parameters"}),`
+`,e.jsx(s,{code:"import { ThemingParameters } from '@ui5/webcomponents-react-base';"}),`
+`,e.jsxs(i.p,{children:["By using the global ",e.jsx(i.code,{children:"--sap..."}),` CSS variables, you can define the look and feel of your application without the need to hard-code any
+colors. The `,e.jsx(i.code,{children:"ThemingParameters"})," is an JS object containing all available CSS variables mapped to their name."]}),`
+`,e.jsx(i.p,{children:"Example:"}),`
+`,e.jsx(i.pre,{children:e.jsx(i.code,{className:"language-jsx",children:`<div style={{ background: ThemingParameters.sapBackgroundColor }} />
+`})}),`
+`,e.jsx(i.p,{children:"Is equivalent to:"}),`
+`,e.jsx(i.pre,{children:e.jsx(i.code,{className:"language-jsx",children:`<div style={{ background: 'var(--sapBackgroundColor)' }} />
+`})}),`
+`,e.jsxs(i.p,{children:[e.jsx(i.strong,{children:"Note:"})," We don't recommend excessive use of inline-style!"]}),`
+`,e.jsx(G,{}),`
+`,e.jsx(i.h2,{id:"hooks",children:"Hooks"}),`
+`,e.jsx(i.h3,{id:"usei18nbundle",children:e.jsx(i.code,{children:"useI18nBundle"})}),`
+`,e.jsx(s,{code:"import { useI18nBundle } from '@ui5/webcomponents-react-base';"}),`
+`,e.jsxs(i.p,{children:["The ",e.jsx(i.code,{children:"useI18nBundle"}),` hook can be used for adding internationalization to your application. Learn more about it in our
+`,e.jsx(i.a,{href:"?path=/docs/knowledge-base-internationalization--docs",children:"Internationalization Guide"}),"."]}),`
+`,e.jsx(i.h3,{id:"useviewportrange",children:e.jsx(i.code,{children:"useViewportRange"})}),`
+`,e.jsx(s,{code:"import { useViewportRange } from '@ui5/webcomponents-react-base';"}),`
+`,e.jsxs(i.p,{children:["The ",e.jsx(i.code,{children:"useViewportRange"})," hook is a utility hook based on the ",e.jsx(i.code,{children:"Device.getCurrentRange()"})," and ",e.jsx(i.code,{children:"Device.attachMediaHandler"}),` API.
+It will always return a string with the name of the currently active range.`]}),`
+`,e.jsx(D,{})]})}function Ii(r={}){const{wrapper:i}={...p(),...r.components};return i?e.jsx(i,{...r,children:e.jsx(m,{...r})}):m(r)}export{Ii as default};
