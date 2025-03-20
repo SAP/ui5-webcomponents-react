cb2b4781e24951952a40c20981dd7ebccb7620f2
Lukas742 <lukas.harbarth@sap.com>
2025-03-20 09:10:05 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@45664cf7e442ea5aa07703fe7d19109595372c52 🚀

---
+++ nightly/assets/ReadMeAI-DhSbvUzV.js
@@ -0,0 +1,39 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-DpYXPI8c.js";import{M as e,a as m}from"./index-BQVsWipE.js";import"./index-BPgdWBHk.js";import"./index-C-T8u1Nb.js";import"./TagDesign-Ga50IS6u.js";import"./index-ChI72X44.js";import"./index-DnYn5RTv.js";import{F as p}from"./CommandsAndQueries-DDzADXOk.js";import"./Button-BhJpItN3.js";import"./jsx-runtime-DITkwERB.js";import"./copy-BjWKS8Rq.js";import{T as s}from"./TableOfContent-DfYxHRnX.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-DKHf5Ic-.js";import"./iframe-CKDaW90P.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-g4jJGwIi.js";import"./CustomElementsScope-CRlaEhjH.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./decline-Bk6V92qv.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-B1L_xjDU.js";import"./information-BQmhacxY.js";import"./sys-enter-2-3Y2FtBDI.js";import"./sys-enter-2-DnGfeGAh.js";import"./i18n-defaults-BXUwkpCs.js";import"./index-BS9IDQLN.js";import"./preview-CfaXNrRA.js";import"./DocsRenderer-CFRXHY34-C2fymdDQ.js";import"./react-18-B55prW7j.js";import"./Popover-C53QSKRD.js";import"./PopupsCommon.css-BG-751as.js";import"./FocusableElements-noNskoYV.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-Cex6U3jO.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-DqiEy2LY.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BYtqX5T7.js";import"./index-B_fY4UqP.js";import"./Label-Cw3xmdjW.js";import"./index-D5gqvCq0.js";import"./index-C5x9Zqi4.js";import"./Link-C_1Qeegk.js";import"./index-vVWMke_Z.js";import"./index-BZypq4CF.js";import"./Text-BBBHwYsI.js";import"./willShowContent-CZcfsNXp.js";import"./addCustomCSSWithScoping-bWjUKP8X.js";import"./index-CbnmVlWA.js";import"./BusyIndicator-DTHwLaG3.js";import"./index-c5_gOmO2.js";import"./index-II2KQk7A.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DyHFsBXc.js";import"./index-Dx_3Pfp7.js";import"./I18nStore-BBWtp4Wg.js";import"./Tooltips-CtkDPpba.js";import"./EventProvider-ChtD9rRX.js";const a=`# @ui5/webcomponents-ai-react
+
+React wrapper for the [@ui5/webcomponents-ai](https://sap.github.io/ui5-webcomponents/components/ai/) web components.
+
+## Installation
+
+To consume \`@ui5/webcomponents-ai-react\`, you need to install the npm modules and required peer dependencies:
+
+\`\`\`bash
+npm install @ui5/webcomponents-ai-react @ui5/webcomponents-ai
+\`\`\`
+
+## Provided assets
+
+| Assets           | Module                                 | Notes                                                  |
+| ---------------- | -------------------------------------- | ------------------------------------------------------ |
+| \`i18n\`, \`themes\` | \`@ui5/webcomponents-ai/dist/Assets.js\` | Theming parameters and translations for the components |
+
+## UI5 Web Components Documentation and Patterns
+
+- Components: https://sap.github.io/ui5-webcomponents/components/ai/
+- Patterns: https://sap.github.io/ui5-webcomponents/components/patterns/
+
+## Documentation
+
+You can find an interactive documentation in our [Storybook](https://sap.github.io/ui5-webcomponents-react/).
+
+## Contribute
+
+Please check our [Contribution Guidelines](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md).
+
+## License
+
+Please see our [LICENSE](https://github.com/SAP/ui5-webcomponents-react/blob/main/LICENSE) for copyright and license information.
+Detailed information including third-party components and their licensing/copyright information is available via the [REUSE tool](https://api.reuse.software/info/github.com/SAP/ui5-webcomponents-react).
+`;function i(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{title:"AI / Docs"}),`
+`,t.jsx(s,{}),`
+`,t.jsx(m,{children:a.split("## Documentation")[0].trim()}),`
+`,t.jsx(p,{})]})}function Et(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i()}export{Et as default};
