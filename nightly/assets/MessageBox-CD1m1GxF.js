e6c301903b223198033feac12f35add74f509dff
Lukas742 <lukas.harbarth@sap.com>
2025-03-20 06:58:43 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@9649fd51fe003f8c737226061d17129c34ae4544 🚀

---
+++ nightly/assets/MessageBox-CD1m1GxF.js
@@ -0,0 +1,67 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-DpYXPI8c.js";import{M as p,C as t}from"./index-KYy7niC1.js";import"./index-BPgdWBHk.js";import"./index-BzmY_dwP.js";import{C as m}from"./ControlsWithNote-CyVxRG7l.js";import{D as c}from"./DocsHeader-Cei8rexJ.js";import{F as a}from"./CommandsAndQueries-LT_2avZf.js";import"./index-ChI72X44.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-DKHf5Ic-.js";import{C as l,D as s,W as d}from"./MessageBox.stories-CprhSCAn.js";import"./iframe-CuA38XEz.js";import"./index-DnYn5RTv.js";import"./index-BS9IDQLN.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-DITkwERB.js";import"./CustomElementsScope-CRlaEhjH.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-BhJpItN3.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-g4jJGwIi.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-CtkDPpba.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-BXUwkpCs.js";import"./decline-Bk6V92qv.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-B1L_xjDU.js";import"./information-BQmhacxY.js";import"./sys-enter-2-3Y2FtBDI.js";import"./sys-enter-2-DnGfeGAh.js";import"./TagDesign-Ga50IS6u.js";import"./utils-UoIwKSWY.js";import"./main-BZcY5MQX.js";import"./index-bxj-dcWC.js";import"./Tag-DJG7ubt1.js";import"./sys-help-2-WHz-04ii.js";import"./index-C5x9Zqi4.js";import"./Link-C_1Qeegk.js";import"./index-vVWMke_Z.js";import"./Popover-C53QSKRD.js";import"./PopupsCommon.css-BG-751as.js";import"./FocusableElements-noNskoYV.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-Cex6U3jO.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-BcmnvuMK.js";import"./copy-BjWKS8Rq.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-DfYxHRnX.js";import"./index-Dbcnf8ax.js";import"./I18nStore-BBWtp4Wg.js";import"./useStylesheet-BYtqX5T7.js";import"./index-BZHnAAif.js";import"./index-B_fY4UqP.js";import"./Label-Cw3xmdjW.js";import"./index-D5gqvCq0.js";import"./index-BZypq4CF.js";import"./Text-BBBHwYsI.js";import"./preview-D5SVmnLr.js";import"./DocsRenderer-CFRXHY34-CcOkRQsF.js";import"./react-18-B55prW7j.js";import"./addCustomCSSWithScoping-bWjUKP8X.js";import"./index-CbnmVlWA.js";import"./BusyIndicator-DTHwLaG3.js";import"./index-c5_gOmO2.js";import"./index-II2KQk7A.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DyHFsBXc.js";import"./index-DkoTjb6F.js";import"./ValueState-Bg0UWejw.js";import"./i18n-defaults-d97UCXvF.js";import"./index--oYoU1rB.js";import"./Dialog-CxuAGvin.js";import"./index-B3tcg6aL.js";import"./index-DNec9Trg.js";function i(e){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",strong:"strong",...r(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:l}),`
+`,o.jsx(c,{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(n.h2,{id:"example",children:"Example"}),`
+`,o.jsx(t,{of:s}),`
+`,o.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(m,{of:s}),`
+`,o.jsx(n.h2,{id:"opening-and-closing-messageboxes",children:"Opening and Closing MessageBoxes"}),`
+`,o.jsxs(n.p,{children:["You can open and close the ",o.jsx(n.code,{children:"MessageBox"})," with the ",o.jsx(n.code,{children:"open"})," prop."]}),`
+`,o.jsxs(n.p,{children:[o.jsx(n.strong,{children:"Note:"})," ",o.jsx(n.code,{children:"onClose"})," is either called by clicking on one of the action buttons in the footer or by pressing the ",o.jsx(n.code,{children:"ESC"})," key."]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      <MessageBox open={open} onClose={handleClose}>
+        Content
+      </MessageBox>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(n.h2,{id:"accessing-actions-in-the-onclose-callback",children:"Accessing actions in the onClose callback"}),`
+`,o.jsxs(n.p,{children:["To add some logic to the respective actions you can use first parameter of the ",o.jsx(n.code,{children:"onClose"})," event handler which contains the name of the clicked action."]}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`const MessageBoxComponent = () => {
+  const [open, setOpen] = useState(false);
+  const onButtonClick = () => {
+    setOpen(true);
+  };
+  // in case the MessageBox was closed by pressing \`ESC\`, \`action\` will be undefined and \`escapedPressed\` will be true.
+  const handleClose = (action, escapedPressed) => {
+    if (escapedPressed) {
+      // escape button was pressed
+    } else if (action === MessageBoxAction.OK) {
+      // do something on "Ok" button click
+    } else if (action === 'Custom Action') {
+      // do something on "Custom Action" click
+    } else {
+      // do something on "Cancel" or "Abort" button click
+    }
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={onButtonClick}>Open Messagebox</Button>
+      <MessageBox
+        open={open}
+        onClose={handleClose}
+        actions={[MessageBoxAction.OK, 'Custom Action', MessageBoxAction.Cancel, MessageBoxAction.Abort]}
+      >
+        Content
+      </MessageBox>
+    </>
+  );
+};
+`})}),`
+`,o.jsx(n.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(n.h2,{id:"messagebox-with-custom-actions",children:"MessageBox with custom actions"}),`
+`,o.jsx(t,{of:d}),`
+`,o.jsx(a,{})]})}function Go(e={}){const{wrapper:n}={...r(),...e.components};return n?o.jsx(n,{...e,children:o.jsx(i,{...e})}):i(e)}export{Go as default};
---
+++ nightly/assets/ReadMeCharts-DTFiGn7F.js
@@ -0,0 +1,38 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as c}from"./index-DpYXPI8c.js";import{M as u,a as l,k as f}from"./index-KYy7niC1.js";import"./index-BPgdWBHk.js";import"./index-BzmY_dwP.js";import"./TagDesign-Ga50IS6u.js";import{r as e}from"./index-ChI72X44.js";import"./index-DnYn5RTv.js";import{F as h}from"./CommandsAndQueries-LT_2avZf.js";import"./Button-BhJpItN3.js";import"./jsx-runtime-DITkwERB.js";import"./copy-BjWKS8Rq.js";import{T as d}from"./TableOfContent-DfYxHRnX.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-DKHf5Ic-.js";import{C}from"./index-BYj75_Uq.js";import{D}from"./BarChart.stories-CIDMtVaI.js";import{D as b}from"./BulletChart.stories-Cz6-hvTl.js";import{D as y}from"./ColumnChart.stories-6MrI-ZTp.js";import{D as x}from"./ColumnChartWithTrend.stories-CPfr7C8c.js";import{D as v}from"./ComposedChart.stories-GX48WIEu.js";import{D as g}from"./DonutChart.stories-CaToN50A.js";import{D as j}from"./LineChart.stories-Czg_1xn1.js";import{D as w}from"./PieChart.stories-qHn_Miei.js";import{D as R}from"./RadarChart.stories-CZTGbqgW.js";import{D as S}from"./RadialChart.stories-UbFmrJLa.js";import{D as $}from"./ScatterChart.stories-CyzfkA0Q.js";import{T as E}from"./index-B3tcg6aL.js";import"./iframe-CuA38XEz.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-g4jJGwIi.js";import"./CustomElementsScope-CRlaEhjH.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./decline-Bk6V92qv.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-B1L_xjDU.js";import"./information-BQmhacxY.js";import"./sys-enter-2-3Y2FtBDI.js";import"./sys-enter-2-DnGfeGAh.js";import"./i18n-defaults-BXUwkpCs.js";import"./index-BS9IDQLN.js";import"./preview-D5SVmnLr.js";import"./DocsRenderer-CFRXHY34-CcOkRQsF.js";import"./react-18-B55prW7j.js";import"./Popover-C53QSKRD.js";import"./PopupsCommon.css-BG-751as.js";import"./FocusableElements-noNskoYV.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-Cex6U3jO.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-BZHnAAif.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BYtqX5T7.js";import"./index-B_fY4UqP.js";import"./Label-Cw3xmdjW.js";import"./index-D5gqvCq0.js";import"./index-C5x9Zqi4.js";import"./Link-C_1Qeegk.js";import"./index-vVWMke_Z.js";import"./index-BZypq4CF.js";import"./Text-BBBHwYsI.js";import"./willShowContent-CZcfsNXp.js";import"./addCustomCSSWithScoping-bWjUKP8X.js";import"./index-CbnmVlWA.js";import"./BusyIndicator-DTHwLaG3.js";import"./index-c5_gOmO2.js";import"./index-II2KQk7A.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DyHFsBXc.js";import"./index-Dbcnf8ax.js";import"./I18nStore-BBWtp4Wg.js";import"./Tooltips-CtkDPpba.js";import"./EventProvider-ChtD9rRX.js";import"./Carousel-D1UGJ2mY.js";import"./ScrollEnablement-CKJWdxrT.js";import"./animate-JylhYHXj.js";import"./AnimationMode-Dkv8WE90.js";import"./slim-arrow-left-CkIG4bGe.js";import"./slim-arrow-right-CeL61nQX.js";import"./DemoProps-BerORI7p.js";import"./ThemingParameters-le4wpk1U.js";import"./staticProps-DJswKWE9.js";import"./ChartContainer--WZNNQwt.js";import"./tiny-invariant-CopsF_GD.js";import"./index-czeG7uL_.js";import"./useCancelAnimationFallback-CCEl5wNw.js";import"./YAxisTicks-IMsBHL7J.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-DfwIrVvY.js";import"./ChartDataLabel-CVTwTc7v.js";import"./useOnClickInternal-DGGRDvA0.js";import"./react-content-loader.es-DXzYY0s3.js";import"./useIsRTL-Dfhfug7K.js";import"./directionChange-BzugKGnv.js";import"./useLongestYAxisLabel-XUj-BGBe.js";import"./ComposedChart-6RGSP8HS.js";import"./Line-DFTsAiXx.js";import"./Scatter-DxzeX5Av.js";import"./index-C_64zBuj.js";import"./PieChart-vJDK1XAJ.js";import"./Placeholder-CxR7YaMJ.js";import"./LineChart-DA6sL9gZ.js";const T=`# @ui5/webcomponents-react-charts
+
+Chart library built on top of [recharts](https://recharts.org/) for ui5-webcomponents-react.
+
+## Installation
+
+\`\`\`bash
+npm install @ui5/webcomponents-react-charts
+\`\`\`
+
+## Accessibility
+
+Charts only offer limited accessibility support with only basic built-in features, so it’s essential to ensure your implementation meets the accessibility standards of your application.
+
+## React 19 support
+
+To use this library with React 19 you have to override your \`react-is\` version to match your React version!
+
+## Documentation
+
+You can find an interactive documentation in our [Storybook](https://sap.github.io/ui5-webcomponents-react/).
+
+## Contribute
+
+Please check our [Contribution Guidelines](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md).
+
+## License
+
+Please see our [LICENSE](https://github.com/SAP/ui5-webcomponents-react/blob/main/LICENSE) for copyright and license information.
+Detailed information including third-party components and their licensing/copyright information is available via the [REUSE tool](https://api.reuse.software/info/github.com/SAP/ui5-webcomponents-react).
+
+<!-- Use the force -->
+`;function I(o){const{children:r}=o,i=e.useRef(null),m=e.useRef(0),n=e.useRef(null),p=()=>{n.current&&(clearInterval(n.current),n.current=null)};return e.useEffect(()=>{const a=i.current;return a&&r.length&&(n.current=setInterval(()=>{m.current++,a.navigateTo(m.current%r.length)},5e3)),()=>{p()}},[r]),t.jsx(C,{style:{height:"500px"},arrowsPlacement:"Navigation",cyclic:!0,ref:i,onNavigate:()=>{p()},...o})}function s(o){const r={div:"div",h2:"h2",...c(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(u,{title:"Charts / Docs"}),`
+`,t.jsx(d,{}),`
+`,t.jsx(l,{children:T.split("## Documentation")[0].trim()}),`
+`,t.jsx(r.h2,{id:"charts",children:"Charts"}),`
+`,t.jsx(I,{children:[{name:"BarChart",component:D},{name:"BulletChart",component:b},{name:"ColumnChart",component:y},{name:"ColumnChartWithTrend",component:x},{name:"ComposedChart",component:v},{name:"DonutChart",component:g},{name:"LineChart",component:j},{name:"PieChart",component:w},{name:"RadarChart",component:R},{name:"RadialChart",component:S},{name:"ScatterChart",component:$}].map(i=>t.jsxs(r.div,{style:{width:"100%"},children:[t.jsx(E,{children:i.name}),t.jsx(f,{of:i.component})]},i.name))}),`
+`,t.jsx(h,{})]})}function ko(o={}){const{wrapper:r}={...c(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(s,{...o})}):s(o)}export{ko as default};
