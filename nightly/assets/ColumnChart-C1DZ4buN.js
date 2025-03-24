9be98c2652804373b55eb02b926a176683674df8
Lukas742 <lukas.harbarth@sap.com>
2025-03-24 12:37:50 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@fcad122f42456659374ca79203e5fb951bd2aaa4 🚀

---
+++ nightly/assets/ColumnChart-C1DZ4buN.js
@@ -0,0 +1,40 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-Ddg-oX0k.js";import{M as p,C as i}from"./index-DIvyspH4.js";import"./index-CB2NJyj5.js";import"./index-DCIP_YK8.js";import{C as s}from"./ControlsWithNote-JbRMd1Bj.js";import{D as a}from"./DocsHeader-YIP2_Nay.js";import{F as h}from"./CommandsAndQueries-CKr9GaN2.js";import"./index-Bi_W_Jd4.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-Dw2clFwJ.js";import{M as c}from"./TooltipConfig-D3HcYE57.js";import{M as d}from"./LegendConfig-YXskoGTD.js";import{M as l}from"./NormalizedStackedChart-BeITmul6.js";import{C as x,D as e,W as j,a as f,b as u,c as C,d as g,L as W,e as M,f as D,g as L,h as b}from"./ColumnChart.stories-DL_mqF_O.js";import"./iframe-C77ER5El.js";import"./index-C1S0AQHK.js";import"./index-C2EHZYIs.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-ByLklLy6.js";import"./CustomElementsScope-BJCkQSHf.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-CVhFbCad.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-CGby3L9v.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-CFLTQoOV.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-BXUwkpCs.js";import"./decline-U8TXFXmv.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CmvYYEtw.js";import"./information-DWKEhq3n.js";import"./sys-enter-2-DfvUZ16A.js";import"./sys-enter-2-DLBOjKNS.js";import"./TagDesign-Ga50IS6u.js";import"./utils-7xFAZnSc.js";import"./main-BZcY5MQX.js";import"./index-BlfpT5Yv.js";import"./Tag-B3GqqYCx.js";import"./sys-help-2-Cui80jVv.js";import"./index-DZZKhZ35.js";import"./Link-7OPDvJp0.js";import"./index-V9E_9yBg.js";import"./Popover-BgaZr5c3.js";import"./PopupsCommon.css-D4-KdmWM.js";import"./FocusableElements-Bof-Jfi5.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D-iSdYn6.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C0TcgQvK.js";import"./copy-BPa3OsAo.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-rwZqpKzI.js";import"./index-CQDokWoO.js";import"./I18nStore-BuTcnDwi.js";import"./useStylesheet-jv87nE5t.js";import"./index-DkLUlqZo.js";import"./index-CtYzPxrt.js";import"./Label-42unHoL8.js";import"./index-q_xBifsn.js";import"./index-D1NZoOym.js";import"./Text-BYHe0rMB.js";import"./preview-D_q1XC3T.js";import"./DocsRenderer-CFRXHY34-CZCcsv2F.js";import"./react-18-B98GjGe7.js";import"./addCustomCSSWithScoping-CRaJDqw6.js";import"./index-sxpf8z2p.js";import"./BusyIndicator-cXtbXcMV.js";import"./index-D-QlfT-1.js";import"./index-BBw2fVoz.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DP6pBYCZ.js";import"./DemoProps-BerORI7p.js";import"./ThemingParameters-le4wpk1U.js";import"./staticProps-CxMmt0Og.js";import"./ChartContainer-DcE3q0G-.js";import"./tiny-invariant-CopsF_GD.js";import"./index-DpvT3d6u.js";import"./useCancelAnimationFallback-CwJt3omq.js";import"./YAxisTicks-CYwBiknk.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-mUjbESkV.js";import"./ChartDataLabel-CLW7sHXV.js";import"./useLongestYAxisLabel-Dr4-9LFd.js";import"./useOnClickInternal-DuGnStyB.js";import"./react-content-loader.es-O9W5CoSf.js";import"./useIsRTL-DJ6lFuxX.js";import"./directionChange-BzugKGnv.js";function n(t){const r={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...m(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:x}),`
+`,o.jsx(a,{isChart:!0}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:e}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:e}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(r.h3,{id:"with-custom-color",children:"With custom color"}),`
+`,o.jsx(i,{of:j}),`
+`,o.jsx(r.h3,{id:"with-secondary-dimension",children:"With Secondary Dimension"}),`
+`,o.jsx(i,{of:f}),`
+`,o.jsx(r.h3,{id:"with-stacks",children:"With Stacks"}),`
+`,o.jsx(i,{of:u}),`
+`,o.jsx(l,{of:C}),`
+`,o.jsx(r.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,o.jsx(i,{of:g}),`
+`,o.jsx(r.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(i,{of:W}),`
+`,o.jsx(r.h3,{id:"with-reference-line",children:"With Reference Line"}),`
+`,o.jsxs(r.p,{children:["You can set a reference line to any value by using the ",o.jsx(r.code,{children:"referenceLine"})," ",o.jsx(r.code,{children:"chartConfig"})," property. ",o.jsx(r.code,{children:"referenceLine"})," accepts all properties described ",o.jsx(r.a,{href:"https://recharts.org/en-US/api/ReferenceLine",rel:"nofollow",children:"here"}),", but it's recommended to only use ",o.jsx(r.code,{children:"label"}),",",o.jsx(r.code,{children:"value"})," and ",o.jsx(r.code,{children:"color"})," to preserve the intended design."]}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-jsx",children:`<ColumnChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,o.jsx(i,{of:M}),`
+`,o.jsx(r.h2,{id:"with-highlighted-measures",children:"With Highlighted Measures"}),`
+`,o.jsx(i,{of:D}),`
+`,o.jsxs(r.p,{children:[o.jsx(c,{of:L}),";"]}),`
+`,o.jsx(d,{of:b}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(h,{})]})}function fr(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(n,{...t})}):n(t)}export{fr as default};
---
+++ nightly/assets/NavigationLayout-DBR9Kb_u.js
@@ -0,0 +1,123 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as c}from"./index-Ddg-oX0k.js";import{A as e}from"./ArgTypesWithNote-BfIEuW53.js";import{C as u}from"./ControlsWithNote-JbRMd1Bj.js";import{D as l}from"./DocsHeader-YIP2_Nay.js";import{F as g}from"./CommandsAndQueries-CKr9GaN2.js";import"./index-DCIP_YK8.js";import"./index-Bi_W_Jd4.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-Dw2clFwJ.js";import{S as x}from"./SubcomponentsSection-BVy4lpQe.js";import{M as S,C as v,a as f,D as n}from"./index-DIvyspH4.js";import{C as h,D as r}from"./NavigationLayout.stories-L6Rcb7Rn.js";import{S as m,a,b as p,c as s}from"./chain-link-D6dozNyv.js";import"./index-CB2NJyj5.js";import"./jsx-runtime-ByLklLy6.js";import"./CustomElementsScope-BJCkQSHf.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-CVhFbCad.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-CGby3L9v.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-CFLTQoOV.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-BXUwkpCs.js";import"./decline-U8TXFXmv.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CmvYYEtw.js";import"./information-DWKEhq3n.js";import"./sys-enter-2-DfvUZ16A.js";import"./sys-enter-2-DLBOjKNS.js";import"./TagDesign-Ga50IS6u.js";import"./index-C1S0AQHK.js";import"./index-C2EHZYIs.js";import"./utils-7xFAZnSc.js";import"./main-BZcY5MQX.js";import"./index-BlfpT5Yv.js";import"./Tag-B3GqqYCx.js";import"./sys-help-2-Cui80jVv.js";import"./index-DZZKhZ35.js";import"./Link-7OPDvJp0.js";import"./index-V9E_9yBg.js";import"./Popover-BgaZr5c3.js";import"./PopupsCommon.css-D4-KdmWM.js";import"./FocusableElements-Bof-Jfi5.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D-iSdYn6.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C0TcgQvK.js";import"./copy-BPa3OsAo.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-rwZqpKzI.js";import"./index-CQDokWoO.js";import"./I18nStore-BuTcnDwi.js";import"./useStylesheet-jv87nE5t.js";import"./index-DkLUlqZo.js";import"./index-CtYzPxrt.js";import"./Label-42unHoL8.js";import"./index-q_xBifsn.js";import"./index-D1NZoOym.js";import"./Text-BYHe0rMB.js";import"./preview-D_q1XC3T.js";import"./iframe-C77ER5El.js";import"./DocsRenderer-CFRXHY34-CZCcsv2F.js";import"./react-18-B98GjGe7.js";import"./addCustomCSSWithScoping-CRaJDqw6.js";import"./index-sxpf8z2p.js";import"./BusyIndicator-cXtbXcMV.js";import"./index-D-QlfT-1.js";import"./index-BBw2fVoz.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DP6pBYCZ.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./menu-Bsb7cB7Z.js";import"./home-BedPnjLp.js";import"./calendar-BY2CI5uO.js";import"./background-i-DaLCI9.js";import"./action-settings-DWXhjhXM.js";import"./index-CXKR2Wp5.js";import"./ListItemStandard-Cp1eitWQ.js";import"./ListItemTemplate-uLmtgV1d.js";import"./edit-DnrxsCWC.js";import"./List-tC_sBijc.js";import"./ItemNavigation-DkXbeD0_.js";import"./handleDrop-DBhvwkYi.js";import"./DropIndicator-C61opNKl.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-DkPsliH9.js";import"./TabbableElements-XzXYKyvZ.js";import"./ListItemAdditionalText.css-BHHJ96Za.js";import"./slim-arrow-right-BMo7GX2U.js";import"./RadioButton-aUUR-yd5.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-BqnF_2VA.js";import"./accept-M2YmBe7V.js";import"./Menu-CrTH9lSC.js";import"./MenuItem-DJEqkQzf.js";import"./nav-back-BncPATr4.js";import"./ResponsivePopover-loEtlXj6.js";import"./Dialog-CRHAE-yP.js";import"./ListItemCustom-BYeiHpiP.js";import"./search-CCCfxPNW.js";import"./overflow-Ct7n396U.js";import"./overflow-v7NQYAq-.js";import"./slim-arrow-down-CV4QzmzW.js";import"./slim-arrow-down-BbpAkhdE.js";import"./parameters-bundle.css-COB6mtRm.js";import"./i18n-defaults-P_CK-S23.js";import"./index-BvzOGisr.js";import"./navigation-right-arrow-fsVOeJhW.js";import"./navigation-right-arrow-CuTWS88m.js";import"./navigation-down-arrow-DKuwDuav.js";function d(o){const i={code:"code",h2:"h2",pre:"pre",...c(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(S,{of:h}),`
+`,t.jsx(l,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(i.h2,{id:"example",children:"Example"}),`
+`,t.jsx(v,{of:r}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show static code"}),t.jsx(i.pre,{children:t.jsx(i.code,{className:"language-tsx",children:`function NavigationLayoutComponent(props) {
+  const [selectedContent, setSelectedContent] = useState('Home');
+  const [mode, setMode] = useState(props.mode);
+  const navigationLayoutRef = useRef<NavigationLayoutDomRef>(null);
+
+  const handleSelectionChange: SideNavigationPropTypes['onSelectionChange'] = (e) => {
+    setSelectedContent(e.detail.item.text);
+  };
+
+  useEffect(() => {
+    setMode(props.mode);
+  }, [props.mode]);
+
+  return (
+    <div style={{ position: 'relative', height: '800px' }}>
+      <NavigationLayout
+        {...props}
+        ref={navigationLayoutRef}
+        mode={mode}
+        header={
+          <ShellBar
+            startButton={
+              <Button
+                icon={menuIcon}
+                onClick={() => {
+                  if (navigationLayoutRef.current?.isSideCollapsed()) {
+                    setMode(NavigationLayoutMode.Expanded);
+                  } else {
+                    setMode(NavigationLayoutMode.Collapsed);
+                  }
+                }}
+              />
+            }
+            primaryTitle="UI5 Web Components for React"
+            secondaryTitle="The Best Run SAP"
+          />
+        }
+        sideContent={
+          <SideNavigation slot="sideContent" onSelectionChange={handleSelectionChange}>
+            <SideNavigationItem text="Home" icon="home" />
+            <SideNavigationGroup text="Group 1" expanded>
+              <SideNavigationItem text="Item 1" icon="locate-me" expanded>
+                <SideNavigationSubItem text="Sub Item 1" />
+                <SideNavigationSubItem text="Sub Item 2" />
+              </SideNavigationItem>
+              <SideNavigationItem text="Item 2" icon="calendar" expanded>
+                <SideNavigationSubItem text="Sub Item 3" />
+                <SideNavigationSubItem text="Sub Item 4" />
+              </SideNavigationItem>
+              <SideNavigationItem text="Item 3" icon="activity-assigned-to-goal" expanded>
+                <SideNavigationSubItem text="Sub Item 5" />
+                <SideNavigationSubItem text="Sub Item 6" />
+              </SideNavigationItem>
+            </SideNavigationGroup>
+            <SideNavigationGroup text="Group 2" expanded>
+              <SideNavigationItem text="Item 4" icon="history" />
+              <SideNavigationItem text="Item 5" icon="source-code" />
+              <SideNavigationItem text="Item 6" icon="background" />
+            </SideNavigationGroup>
+
+            <SideNavigationItem
+              slot="fixedItems"
+              text="Legal"
+              href="https://www.sap.com/about/legal/impressum.html"
+              target="_blank"
+              icon="compare"
+            />
+            <SideNavigationItem
+              slot="fixedItems"
+              text="Privacy"
+              href="https://www.sap.com/about/legal/privacy.html"
+              target="_blank"
+              icon="locked"
+            />
+            <SideNavigationItem
+              slot="fixedItems"
+              text="Terms of Use"
+              href="https://www.sap.com/terms-of-use"
+              target="_blank"
+              icon="document-text"
+            />
+          </SideNavigation>
+        }
+      >
+        <div style={{ padding: '1rem' }}>
+          <div>
+            <Title>{selectedContent}</Title>
+            <br />
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </Text>
+          </div>
+        </div>
+      </NavigationLayout>
+    </div>
+  );
+}
+`})})]}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(u,{of:r}),`
+`,t.jsx(f,{children:x}),`
+`,t.jsx(i.h2,{id:"sidenavigation",children:"SideNavigation"}),`
+`,t.jsx(n,{of:m}),`
+`,t.jsx(e,{of:m}),`
+`,t.jsx(i.h2,{id:"sidenavigationitem",children:"SideNavigationItem"}),`
+`,t.jsx(n,{of:a}),`
+`,t.jsx(e,{of:a}),`
+`,t.jsx(i.h2,{id:"sidenavigationsubitem",children:"SideNavigationSubItem"}),`
+`,t.jsx(n,{of:p}),`
+`,t.jsx(e,{of:p}),`
+`,t.jsx(i.h2,{id:"sidenavigationgroup",children:"SideNavigationGroup"}),`
+`,t.jsx(n,{of:s}),`
+`,t.jsx(e,{of:s}),`
+`,t.jsx(g,{})]})}function qi(o={}){const{wrapper:i}={...c(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(d,{...o})}):d(o)}export{qi as default};
