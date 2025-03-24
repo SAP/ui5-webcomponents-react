da8e24e79cb2201ffacfc4fe60fbd11c0d818ff3
Lukas742 <lukas.harbarth@sap.com>
2025-03-24 16:30:59 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@079058ad0f26224687df214b203793dc6beba5e4 🚀

---
+++ nightly/assets/Form-D2C0Q36e.js
@@ -0,0 +1,121 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-Ddg-oX0k.js";import{M as s,C as o,a as u,D as r,A as i}from"./index-CnmxbZWa.js";import"./index-CB2NJyj5.js";import"./index-BhsMEYFm.js";import{C as c}from"./ControlsWithNote-MjQyOxHo.js";import{D as h}from"./DocsHeader-DFeTEnfD.js";import{F as I}from"./CommandsAndQueries-Bs9Gf-58.js";import"./index-Bi_W_Jd4.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-Dw2clFwJ.js";import{S as y}from"./SubcomponentsSection-BVy4lpQe.js";import{a as p,b as m}from"./index-Dn5cdImL.js";import{C as x,D as a,a as F}from"./Form.stories-CBjJZnlP.js";import"./iframe-BzZvA8xW.js";import"./index-C1S0AQHK.js";import"./index-C2EHZYIs.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-ByLklLy6.js";import"./CustomElementsScope-BJCkQSHf.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-CVhFbCad.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-CGby3L9v.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-CFLTQoOV.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-BXUwkpCs.js";import"./decline-U8TXFXmv.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CmvYYEtw.js";import"./information-DWKEhq3n.js";import"./sys-enter-2-DfvUZ16A.js";import"./sys-enter-2-DLBOjKNS.js";import"./TagDesign-Ga50IS6u.js";import"./utils-XuGp6s-R.js";import"./main-BZcY5MQX.js";import"./index-BwuPsjtr.js";import"./Tag-B3GqqYCx.js";import"./sys-help-2-Cui80jVv.js";import"./index-V8njyB6i.js";import"./Link-7OPDvJp0.js";import"./index-V9E_9yBg.js";import"./Popover-BgaZr5c3.js";import"./PopupsCommon.css-D4-KdmWM.js";import"./FocusableElements-Bof-Jfi5.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D-iSdYn6.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C0TcgQvK.js";import"./copy-BPa3OsAo.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-rwZqpKzI.js";import"./index-CuFAsSWh.js";import"./I18nStore-BuTcnDwi.js";import"./useStylesheet-jv87nE5t.js";import"./index-DkLUlqZo.js";import"./index-CTl4EJPq.js";import"./Label-42unHoL8.js";import"./index-q_xBifsn.js";import"./index-D1NZoOym.js";import"./Text-BYHe0rMB.js";import"./preview-DIjd8oxH.js";import"./DocsRenderer-CFRXHY34-xakGwjG1.js";import"./react-18-B98GjGe7.js";import"./addCustomCSSWithScoping-CRaJDqw6.js";import"./index-sxpf8z2p.js";import"./BusyIndicator-cXtbXcMV.js";import"./index-D-QlfT-1.js";import"./index-BBw2fVoz.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DP6pBYCZ.js";import"./Input-B4__fPFB.js";import"./ValueState-Bg0UWejw.js";import"./ResponsivePopoverCommon.css-ulAhQagc.js";import"./ValueStateMessage.css-eVIV6tCL.js";import"./Suggestions.css-BGzgip6g.js";import"./index-DAErskpc.js";import"./index-CVLjlAMB.js";import"./index-DluCj3z_.js";import"./Select-ChHPdi79.js";import"./InvisibleMessage-B88SiF25.js";import"./List-tC_sBijc.js";import"./ItemNavigation-DkXbeD0_.js";import"./handleDrop-DBhvwkYi.js";import"./DropIndicator-C61opNKl.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-DkPsliH9.js";import"./TabbableElements-XzXYKyvZ.js";import"./ResponsivePopover-loEtlXj6.js";import"./Dialog-CRHAE-yP.js";import"./slim-arrow-down-CV4QzmzW.js";import"./slim-arrow-down-BbpAkhdE.js";import"./index-DyuSALh-.js";import"./Option-BxQZlMo4.js";import"./ListItemBaseTemplate-KnOYKXBU.js";import"./ListItemAdditionalText.css-BHHJ96Za.js";import"./index-S39O0-HA.js";import"./CheckBox-BqnF_2VA.js";import"./accept-M2YmBe7V.js";function d(n){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...l(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:x}),`
+`,t.jsx(h,{subComponents:["FormGroup","FormItem"]}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(o,{of:a}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(c,{of:a}),`
+`,t.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(e.h2,{id:"display-only-and-edit-mode",children:"Display-Only and Edit mode"}),`
+`,t.jsx(o,{of:F,sourceState:"none"}),`
+`,t.jsx(e.h3,{id:"code",children:"Code"}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show Code"}),t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const StandardField = ({ editMode, value, inputType = InputType.Text, onInput, ...rest }) => {
+  if (editMode) {
+    return <Input value={value} style={{ width: '100%' }} type={inputType} onInput={onInput} {...rest} />;
+  }
+  if (inputType === InputType.URL || inputType === InputType.Email) {
+    return (
+      <Link href={inputType === InputType.Email ? \`mailto:\${value}\` : value} target="_blank" {...rest}>
+        {value}
+      </Link>
+    );
+  }
+  return <Text {...rest}>{value}</Text>;
+};
+
+const reducer = (state, { field, value }) => {
+  return { ...state, [field]: value };
+};
+
+const FormComponent = (props) => {
+  const [editMode, toggleEditMode] = useReducer((prev) => !prev, false, undefined);
+  const [formState, dispatch] = useReducer(
+    reducer,
+    {
+      name: 'Red Point Stores',
+      street: 'Main St 1618',
+      zip: 31415,
+      city: 'Maintown',
+      country: 'Germany',
+      web: 'https://www.sap.com',
+      mail: 'john.smith@sap.com',
+      twitter: '@sap',
+      phone: '+49 1234 56789'
+    },
+    undefined
+  );
+  const { zip, city, name, street, country, web, mail, twitter, phone } = formState;
+
+  const handleInput = (e) => {
+    dispatch({ field: Object.keys(e.target.dataset)[0], value: e.target.value });
+  };
+
+  return (
+    <>
+      <Button onClick={toggleEditMode}>Toggle {editMode ? 'Display-Only Mode' : 'Edit Mode'}</Button>
+      <Form
+        {...props}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <FormGroup titleText="Address">
+          <FormItem label="Name">
+            <StandardField editMode={editMode} value={name} onInput={handleInput} data-name />
+          </FormItem>
+          <FormItem label="Street">
+            <StandardField editMode={editMode} value={street} onInput={handleInput} data-street />
+          </FormItem>
+          <FormItem label="ZIP Code / City">
+            {editMode ? (
+              <>
+                <Input value={zip} type={InputType.Number} style={{ width: '30%' }} onInput={handleInput} data-zip />
+                <Input value={city} style={{ width: '70%' }} onInput={handleInput} data-city />
+              </>
+            ) : (
+              <Text>{\`\${zip} \${city}\`}</Text>
+            )}
+          </FormItem>
+          <FormItem label="Country">
+            <StandardField editMode={editMode} value={country} onInput={handleInput} data-country />
+          </FormItem>
+          <FormItem label="Web">
+            <StandardField editMode={editMode} value={web} inputType={InputType.URL} onInput={handleInput} data-web />
+          </FormItem>
+        </FormGroup>
+        <FormGroup titleText="Contact">
+          <FormItem label="Email">
+            <StandardField
+              editMode={editMode}
+              value={mail}
+              inputType={InputType.Email}
+              onInput={handleInput}
+              data-email
+            />
+          </FormItem>
+          <FormItem label="Twitter">
+            <StandardField editMode={editMode} value={twitter} onInput={handleInput} data-twitter />
+          </FormItem>
+          <FormItem label="Phone">
+            <StandardField
+              editMode={editMode}
+              value={phone}
+              inputType={InputType.Tel}
+              onInput={handleInput}
+              data-phone
+            />
+          </FormItem>
+        </FormGroup>
+      </Form>
+    </>
+  );
+};
+`})})]}),`
+`,t.jsx(u,{children:y}),`
+`,t.jsx(e.h2,{id:"form-group",children:"Form Group"}),`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(i,{of:p}),`
+`,t.jsx(e.h2,{id:"formitem",children:"FormItem"}),`
+`,t.jsx(r,{of:m}),`
+`,t.jsx(i,{of:m}),`
+`,t.jsx(I,{})]})}function je(n={}){const{wrapper:e}={...l(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(d,{...n})}):d(n)}export{je as default};
---
+++ nightly/assets/ShellBar-Do4_98-T.js
@@ -0,0 +1,53 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as b}from"./index-Ddg-oX0k.js";import{M as y,C as _,a as w,D as c,A as h}from"./index-CnmxbZWa.js";import"./index-CB2NJyj5.js";import"./index-BhsMEYFm.js";import{C as j}from"./ControlsWithNote-MjQyOxHo.js";import{D as g}from"./DocsHeader-DFeTEnfD.js";import{F as C}from"./CommandsAndQueries-Bs9Gf-58.js";import"./index-Bi_W_Jd4.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-Dw2clFwJ.js";import{S as B}from"./SubcomponentsSection-BVy4lpQe.js";import{C as v,D as d,S as u}from"./ShellBar.stories-CGYYPO_R.js";import{s as N,m as D,A as P,w as T}from"./jsx-runtime-ByLklLy6.js";import{e as f}from"./utils-XuGp6s-R.js";import"./iframe-BzZvA8xW.js";import"./index-C1S0AQHK.js";import"./index-C2EHZYIs.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-CVhFbCad.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-CGby3L9v.js";import"./CustomElementsScope-BJCkQSHf.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-CFLTQoOV.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-BXUwkpCs.js";import"./decline-U8TXFXmv.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CmvYYEtw.js";import"./information-DWKEhq3n.js";import"./sys-enter-2-DfvUZ16A.js";import"./sys-enter-2-DLBOjKNS.js";import"./TagDesign-Ga50IS6u.js";import"./index-BwuPsjtr.js";import"./Tag-B3GqqYCx.js";import"./sys-help-2-Cui80jVv.js";import"./index-V8njyB6i.js";import"./Link-7OPDvJp0.js";import"./index-V9E_9yBg.js";import"./Popover-BgaZr5c3.js";import"./PopupsCommon.css-D4-KdmWM.js";import"./FocusableElements-Bof-Jfi5.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-D-iSdYn6.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C0TcgQvK.js";import"./copy-BPa3OsAo.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-rwZqpKzI.js";import"./index-CuFAsSWh.js";import"./I18nStore-BuTcnDwi.js";import"./useStylesheet-jv87nE5t.js";import"./index-DkLUlqZo.js";import"./index-CTl4EJPq.js";import"./Label-42unHoL8.js";import"./index-q_xBifsn.js";import"./index-D1NZoOym.js";import"./Text-BYHe0rMB.js";import"./preview-DIjd8oxH.js";import"./DocsRenderer-CFRXHY34-xakGwjG1.js";import"./react-18-B98GjGe7.js";import"./addCustomCSSWithScoping-CRaJDqw6.js";import"./index-sxpf8z2p.js";import"./BusyIndicator-cXtbXcMV.js";import"./index-D-QlfT-1.js";import"./index-BBw2fVoz.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DP6pBYCZ.js";import"./Person-Bk5r7PMP.js";import"./add-Bc8A5U9e.js";import"./search-CCCfxPNW.js";import"./index-CXKR2Wp5.js";import"./ListItemStandard-Cp1eitWQ.js";import"./ListItemTemplate-uLmtgV1d.js";import"./edit-DnrxsCWC.js";import"./List-tC_sBijc.js";import"./ItemNavigation-DkXbeD0_.js";import"./handleDrop-DBhvwkYi.js";import"./DropIndicator-C61opNKl.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-DkPsliH9.js";import"./TabbableElements-XzXYKyvZ.js";import"./ListItemAdditionalText.css-BHHJ96Za.js";import"./slim-arrow-right-BMo7GX2U.js";import"./RadioButton-aUUR-yd5.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-BqnF_2VA.js";import"./accept-M2YmBe7V.js";import"./Menu-CrTH9lSC.js";import"./MenuItem-DJEqkQzf.js";import"./nav-back-BncPATr4.js";import"./ResponsivePopover-loEtlXj6.js";import"./Dialog-CRHAE-yP.js";import"./ListItemCustom-BYeiHpiP.js";import"./overflow-Ct7n396U.js";import"./overflow-v7NQYAq-.js";import"./slim-arrow-down-CV4QzmzW.js";import"./slim-arrow-down-BbpAkhdE.js";import"./parameters-bundle.css-COB6mtRm.js";import"./i18n-defaults-P_CK-S23.js";import"./index-CVLjlAMB.js";import"./Input-B4__fPFB.js";import"./ResponsivePopoverCommon.css-ulAhQagc.js";import"./ValueStateMessage.css-eVIV6tCL.js";import"./Suggestions.css-BGzgip6g.js";import"./index-Blw57xH1.js";import"./index-BVnFB9i2.js";import"./EventProvider-ChtD9rRX.js";import"./main-BZcY5MQX.js";var x=function(t,o,i,p){var m=arguments.length,r=m<3?o:p===null?p=Object.getOwnPropertyDescriptor(o,i):p,a;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")r=Reflect.decorate(t,o,i,p);else for(var l=t.length-1;l>=0;l--)(a=t[l])&&(r=(m<3?a(r):m>3?a(o,i,r):a(o,i))||r);return m>3&&r&&Object.defineProperty(o,i,r),r};let s=class extends P{constructor(){super(...arguments),this.visible=!1}};x([N({type:Boolean})],s.prototype,"visible",void 0);s=x([D({tag:"ui5-shellbar-spacer"})],s);s.define();const n=T("ui5-shellbar-spacer",[],[],[],[]);n.displayName="ShellBarSpacer";try{n.displayName="ShellBarSpacer",n.__docgenInfo={description:"The `ShellBarSpacer` is an element, used for visual separation between the two content parts of the `ShellBar`.\n**Note:** The `ShellBarSpacer` component is in an experimental state and is a subject to change.\n\n__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)",displayName:"ShellBarSpacer",props:{style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}try{n.displayName="ShellBarSpacer",n.__docgenInfo={description:"The `ShellBarSpacer` is an element, used for visual separation between the two content parts of the `ShellBar`.\n**Note:** The `ShellBarSpacer` component is in an experimental state and is a subject to change.\n\n__Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)",displayName:"ShellBarSpacer",props:{style:{defaultValue:null,description:`Element style which will be appended to the most outer element of a component.
+Use this prop carefully, some css properties might break the component.`,name:"style",required:!1,type:{name:"CSSProperties"}},className:{defaultValue:null,description:`CSS Class Name which will be appended to the most outer element of a component.
+Use this prop carefully, overwriting CSS rules might break the component.`,name:"className",required:!1,type:{name:"string"}},waitForDefine:{defaultValue:null,description:`Defines whether the component should wait for the underlying custom element of the web component to be defined. This can be useful, for example, for using instance methods when mounting the component.
+
+__Note:__ This adds a rendering cycle to your component.`,name:"waitForDefine",required:!1,type:{name:"boolean"}}}}}catch{}function S(t){const o={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...b(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(y,{of:v}),`
+`,e.jsx(g,{since:"0.4.0"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h2,{id:"example",children:"Example"}),`
+`,e.jsx(_,{of:d}),`
+`,e.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(j,{of:d}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h1,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(o.h2,{id:"open-a-popover-on-shellbaritem-click",children:"Open a Popover on ShellBarItem click"}),`
+`,e.jsxs(o.p,{children:["To open a popover with the ",e.jsx(o.code,{children:"ShellBarItem"})," you can use the ",e.jsx(o.code,{children:"detail.targetRef"})," property of the ",e.jsx(o.code,{children:"onClick"})," event."]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-tsx",children:`const ShellBarComponent = () => {
+  const popoverRef = useRef<PopoverDomRef>(null);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const handleShellBarItemClick: ShellBarItemPropTypes['onClick'] = (e) => {
+    popoverRef.current!.opener = e.detail.targetRef;
+    setPopoverOpen(true);
+  };
+  return (
+    <>
+      <ShellBar>
+        <ShellBarItem onClick={handleShellBarItemClick} icon="add" text="add" />
+      </ShellBar>
+      <Popover
+        ref={popoverRef}
+        open={popoverOpen}
+        onClose={() => {
+          setPopoverOpen(false);
+        }}
+      >
+        Hello there!
+      </Popover>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(w,{children:B}),`
+`,e.jsx(o.h2,{id:"shellbaritem",children:"ShellBarItem"}),`
+`,e.jsx(c,{of:u}),`
+`,e.jsx(h,{hideHTMLPropsNote:!0,exclude:f,of:u}),`
+`,e.jsx(o.h2,{id:"shellbarspacer",children:"ShellBarSpacer"}),`
+`,e.jsx(c,{of:n}),`
+`,e.jsx(h,{hideHTMLPropsNote:!0,exclude:f,of:n}),`
+`,e.jsx(C,{})]})}function Xo(t={}){const{wrapper:o}={...b(),...t.components};return o?e.jsx(o,{...t,children:e.jsx(S,{...t})}):S(t)}export{Xo as default};
