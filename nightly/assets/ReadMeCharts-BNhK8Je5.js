4813b0afbe6d4a2cf6df111f121b8154eba9eead
Lukas742 <lukas.harbarth@sap.com>
2025-04-07 08:52:25 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@43fcb35783abd7d97380fc0924546447809a574c 🚀

---
+++ nightly/assets/ReadMeCharts-BNhK8Je5.js
@@ -0,0 +1,38 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as c}from"./index-B5Ip8tK9.js";import{M as u,a as l,k as f}from"./index-mCNKEE1Z.js";import"./index-BgnWomVF.js";import"./index-BALFoiIZ.js";import"./TagDesign-Ga50IS6u.js";import{r as e}from"./index-U0ga5oGA.js";import"./index-B8m469-U.js";import{F as h}from"./CommandsAndQueries-v3u3L6Ll.js";import"./Button-BlFaFbRM.js";import"./jsx-runtime-BhvuzFo7.js";import"./copy-bdWnQ-6t.js";import{T as d}from"./TableOfContent-lIiVb8Ys.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-MRVfrrA9.js";import{C}from"./index-CijvN4XH.js";import{D}from"./BarChart.stories-DrK6gzPm.js";import{D as b}from"./BulletChart.stories-D4gmJjbL.js";import{D as y}from"./ColumnChart.stories-wN4r8f1j.js";import{D as x}from"./ColumnChartWithTrend.stories-DUU1MFVF.js";import{D as v}from"./ComposedChart.stories-DcPJ3YMd.js";import{D as g}from"./DonutChart.stories-Bqya_WLH.js";import{D as j}from"./LineChart.stories-Dpz8yFem.js";import{D as w}from"./PieChart.stories-KQZbf3iP.js";import{D as R}from"./RadarChart.stories-3HMfco_q.js";import{D as S}from"./RadialChart.stories-CplJ7hEI.js";import{D as $}from"./ScatterChart.stories-CNQ7tsTu.js";import{T as E}from"./index-dnBkf-nF.js";import"./iframe-C8dv7yYl.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-C0UcZWk2.js";import"./CustomElementsScope-Bim9ocLE.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./decline-DYSKo6LW.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CnxOfLPM.js";import"./information-DbtMcSD6.js";import"./sys-enter-2-CTMCSnyQ.js";import"./sys-enter-2-Ciletl9c.js";import"./i18n-defaults-BXUwkpCs.js";import"./index-BVR6w1bU.js";import"./preview-DdOR66Kv.js";import"./DocsRenderer-CFRXHY34-CMwimztQ.js";import"./react-18-SRpBHcoT.js";import"./Popover-CrUg6BDL.js";import"./PopupsCommon.css-CkrnA4gF.js";import"./FocusableElements-DCoyCYrA.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-B16Uz3cm.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-DoR-amcW.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-BbE0XC0n.js";import"./index-noTXb15B.js";import"./Label-Iv8Am_pm.js";import"./index-n4YNqY3D.js";import"./index-iA4rRkZi.js";import"./Link-BveVQiXd.js";import"./index-DgC-Wkyw.js";import"./index-j5LXKHzi.js";import"./Text-fxG8xjHY.js";import"./willShowContent-CZcfsNXp.js";import"./addCustomCSSWithScoping-Cgfw5Re6.js";import"./index-ThksJ1V-.js";import"./BusyIndicator-DQ6Oocfo.js";import"./index-CnihKeIw.js";import"./index-DDwnn4pC.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-ba84kQhl.js";import"./index-DBp2-dmi.js";import"./I18nStore-BFRjgGfw.js";import"./Tooltips-v50zrNfc.js";import"./EventProvider-ChtD9rRX.js";import"./Carousel-DGM4Ezpe.js";import"./ScrollEnablement-ngnhni4d.js";import"./animate-JylhYHXj.js";import"./AnimationMode-BEKJ_YE6.js";import"./slim-arrow-left-D3GgWkh7.js";import"./slim-arrow-right-tjC5xpR0.js";import"./DemoProps-BerORI7p.js";import"./ThemingParameters-le4wpk1U.js";import"./staticProps-wY361Fad.js";import"./ChartContainer-CznzXIJK.js";import"./tiny-invariant-CopsF_GD.js";import"./index-Is81ktUE.js";import"./useCancelAnimationFallback-D5uNBTnn.js";import"./YAxisTicks-5Sp6tZtG.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-B0fQi-by.js";import"./ChartDataLabel-CVMeMunV.js";import"./useOnClickInternal-CIocwmIU.js";import"./react-content-loader.es-C9Vmu4FD.js";import"./useIsRTL-DnnYNU6Y.js";import"./directionChange-BzugKGnv.js";import"./useLongestYAxisLabel-BVdm48ao.js";import"./ComposedChart-DsK83Ly6.js";import"./Line-DiYDjurG.js";import"./Scatter-CwvB7nV2.js";import"./index-CfFKIjfv.js";import"./PieChart-Cy44-a0L.js";import"./Placeholder-CgY8nFJl.js";import"./LineChart-BrAV6AtP.js";const T=`# @ui5/webcomponents-react-charts
+
+Chart library built on top of [recharts](https://recharts.org/) for ui5-webcomponents-react.
+
+## Installation
+
+\`\`\`bash
+npm install @ui5/webcomponents-react-charts
+\`\`\`
+
+## Accessibility
+
+Charts only offer limited accessibility support with only basic built-in features, so it’s essential to ensure your implementation meets the accessibility standards of your application.
+
+## React 19 support
+
+To use this library with React 19 you have to override your \`react-is\` version to match your React version!
+
+## Documentation
+
+You can find an interactive documentation in our [Storybook](https://sap.github.io/ui5-webcomponents-react/).
+
+## Contribute
+
+Please check our [Contribution Guidelines](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md).
+
+## License
+
+Please see our [LICENSE](https://github.com/SAP/ui5-webcomponents-react/blob/main/LICENSE) for copyright and license information.
+Detailed information including third-party components and their licensing/copyright information is available via the [REUSE tool](https://api.reuse.software/info/github.com/SAP/ui5-webcomponents-react).
+
+<!-- Use the force -->
+`;function I(o){const{children:r}=o,i=e.useRef(null),m=e.useRef(0),n=e.useRef(null),p=()=>{n.current&&(clearInterval(n.current),n.current=null)};return e.useEffect(()=>{const a=i.current;return a&&r.length&&(n.current=setInterval(()=>{m.current++,a.navigateTo(m.current%r.length)},5e3)),()=>{p()}},[r]),t.jsx(C,{style:{height:"500px"},arrowsPlacement:"Navigation",cyclic:!0,ref:i,onNavigate:()=>{p()},...o})}function s(o){const r={div:"div",h2:"h2",...c(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(u,{title:"Charts / Docs"}),`
+`,t.jsx(d,{}),`
+`,t.jsx(l,{children:T.split("## Documentation")[0].trim()}),`
+`,t.jsx(r.h2,{id:"charts",children:"Charts"}),`
+`,t.jsx(I,{children:[{name:"BarChart",component:D},{name:"BulletChart",component:b},{name:"ColumnChart",component:y},{name:"ColumnChartWithTrend",component:x},{name:"ComposedChart",component:v},{name:"DonutChart",component:g},{name:"LineChart",component:j},{name:"PieChart",component:w},{name:"RadarChart",component:R},{name:"RadialChart",component:S},{name:"ScatterChart",component:$}].map(i=>t.jsxs(r.div,{style:{width:"100%"},children:[t.jsx(E,{children:i.name}),t.jsx(f,{of:i.component})]},i.name))}),`
+`,t.jsx(h,{})]})}function ko(o={}){const{wrapper:r}={...c(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(s,{...o})}):s(o)}export{ko as default};
---
+++ nightly/assets/Toolbar-2_jbp3gK.js
@@ -0,0 +1,59 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as h}from"./index-B5Ip8tK9.js";import{A as e}from"./ArgTypesWithNote-cFMVoZlK.js";import{C as x}from"./ControlsWithNote-BS427cto.js";import{D as f}from"./DocsHeader-eGQssjwR.js";import{F as j}from"./CommandsAndQueries-v3u3L6Ll.js";import"./index-BALFoiIZ.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-MRVfrrA9.js";import{S as b}from"./SubcomponentsSection-BVy4lpQe.js";import{M as u,C as i,a as v,D as p}from"./index-mCNKEE1Z.js";import{C as T,D as n,O as S,T as m,a as s,b as a,c}from"./Toolbar.stories-BeFvZ1fa.js";import{T as l}from"./index-Bc8fAqIj.js";import"./index-BgnWomVF.js";import"./jsx-runtime-BhvuzFo7.js";import"./CustomElementsScope-Bim9ocLE.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-BlFaFbRM.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-C0UcZWk2.js";import"./parameters-bundle.css-DHLNbl9v.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-v50zrNfc.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-BXUwkpCs.js";import"./decline-DYSKo6LW.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-CnxOfLPM.js";import"./information-DbtMcSD6.js";import"./sys-enter-2-CTMCSnyQ.js";import"./sys-enter-2-Ciletl9c.js";import"./TagDesign-Ga50IS6u.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./utils-FphjGn1v.js";import"./main-BZcY5MQX.js";import"./index-DaoYA3yQ.js";import"./Tag-BTr-mxkm.js";import"./sys-help-2-COnLU0C4.js";import"./index-iA4rRkZi.js";import"./Link-BveVQiXd.js";import"./index-DgC-Wkyw.js";import"./Popover-CrUg6BDL.js";import"./PopupsCommon.css-CkrnA4gF.js";import"./FocusableElements-DCoyCYrA.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-B16Uz3cm.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-D-zU-_A-.js";import"./copy-bdWnQ-6t.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-lIiVb8Ys.js";import"./index-DBp2-dmi.js";import"./I18nStore-BFRjgGfw.js";import"./useStylesheet-BbE0XC0n.js";import"./index-DoR-amcW.js";import"./index-noTXb15B.js";import"./Label-Iv8Am_pm.js";import"./index-n4YNqY3D.js";import"./index-j5LXKHzi.js";import"./Text-fxG8xjHY.js";import"./preview-DdOR66Kv.js";import"./iframe-C8dv7yYl.js";import"./DocsRenderer-CFRXHY34-CMwimztQ.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-Cgfw5Re6.js";import"./index-ThksJ1V-.js";import"./BusyIndicator-DQ6Oocfo.js";import"./index-CnihKeIw.js";import"./index-DDwnn4pC.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-ba84kQhl.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./ToolbarButton-X8VRR4MW.js";import"./overflow-BBK20QiU.js";import"./overflow-BeRy0u9L.js";import"./Select-OCY17SLj.js";import"./InvisibleMessage-D4KNpaEn.js";import"./ValueState-Bg0UWejw.js";import"./List-D0S5TnB8.js";import"./ItemNavigation-DjLK4Lar.js";import"./handleDrop-B9DcvvxB.js";import"./DropIndicator-Cr_UsxJu.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-BH9fbE2k.js";import"./TabbableElements-XzXYKyvZ.js";import"./ResponsivePopover-9R-SZhSc.js";import"./Dialog-Bnmn5Ec8.js";import"./slim-arrow-down-BJXO-3cd.js";import"./slim-arrow-down-vf7luc8v.js";import"./ResponsivePopoverCommon.css-DIPw_9ev.js";import"./ValueStateMessage.css-By9uw0uX.js";import"./Option-Mdzjt7XX.js";import"./ListItemBaseTemplate-BsNS-2sJ.js";import"./ListItemAdditionalText.css-BcsepVCA.js";function d(t){const r={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...h(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(u,{of:T}),`
+`,o.jsx(f,{since:"1.19.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:n}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(x,{of:n}),`
+`,o.jsx(r.h2,{id:"opening-popovers-via-toolbarbutton",children:"Opening Popovers via ToolbarButton"}),`
+`,o.jsxs(r.p,{children:["Since the ",o.jsx(r.code,{children:"ToolbarButton"})," is an ",o.jsx(r.a,{href:"?path=/docs/knowledge-base-faq--docs#what-are-abstract-ui5-web-components",children:"abstract UI5 web component"}),", the opener of the ",o.jsx(r.code,{children:"Popover"}),` needs the DOM reference of the actual element to position the popover correctly.
+Starting with v2.5.0 of `,o.jsx(r.code,{children:"@ui5/webcomponents(-react)"}),", the ",o.jsx(r.code,{children:"detail"})," property of the ",o.jsx(r.code,{children:"ToolbarButton"}),"'s click handler now includes a ",o.jsx(r.code,{children:"targetRef"})," property, which can be used as the opener."]}),`
+`,o.jsx(i,{of:S}),`
+`,o.jsx(r.h3,{id:"example-code",children:"Example Code"}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-tsx",children:`function ToolbarWithPopover() {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const popoverRef = useRef<PopoverDomRef>(null);
+  return (
+    <>
+      <Toolbar>
+        <ToolbarButton
+          onClick={(e) => {
+            const { targetRef } = e.detail;
+            if (popoverRef.current) {
+              popoverRef.current.opener = targetRef;
+              setPopoverOpen(true);
+            }
+          }}
+          text="Open Popover"
+        />
+      </Toolbar>
+      <Popover
+        open={popoverOpen}
+        ref={popoverRef}
+        onClose={() => {
+          setPopoverOpen(false);
+        }}
+      >
+        Content
+      </Popover>
+    </>
+  );
+}
+`})}),`
+`,o.jsx(v,{children:b}),`
+`,o.jsx(r.h2,{id:"toolbarbutton",children:"ToolbarButton"}),`
+`,o.jsx(p,{of:l}),`
+`,o.jsx(e,{of:l}),`
+`,o.jsx(r.h2,{id:"toolbarselect",children:"ToolbarSelect"}),`
+`,o.jsx(p,{of:m}),`
+`,o.jsx(e,{of:m}),`
+`,o.jsx(r.h2,{id:"toolbarselectoption",children:"ToolbarSelectOption"}),`
+`,o.jsx(p,{of:s}),`
+`,o.jsx(e,{of:s}),`
+`,o.jsx(r.h2,{id:"toolbarseparator",children:"ToolbarSeparator"}),`
+`,o.jsx(p,{of:a}),`
+`,o.jsx(e,{of:a}),`
+`,o.jsx(r.h2,{id:"toolbarspacer",children:"ToolbarSpacer"}),`
+`,o.jsx(p,{of:c}),`
+`,o.jsx(e,{of:c}),`
+`,o.jsx(j,{})]})}function Tr(t={}){const{wrapper:r}={...h(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(d,{...t})}):d(t)}export{Tr as default};
