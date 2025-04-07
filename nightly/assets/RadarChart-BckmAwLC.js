b503912ec6bcbf96e97ddaebc2f3df567c064154
Lukas742 <lukas.harbarth@sap.com>
2025-04-07 14:43:24 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0c55ff01292187c25ea9a2ad8117c15627166ef8 🚀

---
+++ nightly/assets/RadarChart-BckmAwLC.js
@@ -0,0 +1,22 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as n}from"./index-B5Ip8tK9.js";import{M as s,C as i}from"./index-BMuSDG18.js";import"./index-DlRg2OHl.js";import"./index-DAuqKUvF.js";import{C as e}from"./ControlsWithNote-B1EvaK32.js";import{D as a}from"./DocsHeader-DSR4zHy9.js";import{F as l}from"./CommandsAndQueries-43r2TnUR.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-C1L07-tE.js";import{M as h}from"./TooltipConfig-CtxHMouB.js";import{C as x,D as m,W as d,a as j,P as c,L as f,b as C,c as u}from"./RadarChart.stories-BSEqO0kf.js";import{M as D}from"./LegendConfig-DnebsppJ.js";import"./iframe-e5xmDyFA.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-BvNWHM6A.js";import"./CustomElementsScope-lRvT7iCm.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-CRJQYaat.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-Cx68SyXj.js";import"./parameters-bundle.css-BLEjIEH9.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-BSX3YpE0.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-DaBEYLpV.js";import"./decline-B3prtgTL.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-C2QpdGW_.js";import"./information-DpRbDwJs.js";import"./sys-enter-2-BNMrwyXi.js";import"./sys-enter-2-38y2ESjX.js";import"./alert-oRgA-QL-.js";import"./TagDesign-Ga50IS6u.js";import"./utils-CF5zuRW_.js";import"./main-DXdDxF5b.js";import"./index-hGdgpmpy.js";import"./Tag-Clr1V4cc.js";import"./sys-help-2-DtkETXXa.js";import"./index-C_lI6Qk2.js";import"./Link-DDBI2Aee.js";import"./index-CkquY_70.js";import"./Popover-B5iz04rB.js";import"./PopupsCommon.css-Cwh_2soo.js";import"./FocusableElements-B2W9U87f.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-ClsBPvBy.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-CihNYBWc.js";import"./copy-gHwufZZP.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-lIiVb8Ys.js";import"./index-DooFWhc5.js";import"./I18nStore-C3xHSFao.js";import"./useStylesheet-N7C2bzZq.js";import"./index-KM_ngbs2.js";import"./index-CBrYwcJf.js";import"./Label-zmLCH7da.js";import"./index-DlD3csZn.js";import"./index-CqWP2qFq.js";import"./Text-BmPWIBNd.js";import"./preview-DdDskDMv.js";import"./DocsRenderer-CFRXHY34-DxHmPy-n.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-CzgbjJA9.js";import"./index-DjS4b8wL.js";import"./BusyIndicator-aPNGxP00.js";import"./index-DEF9m15S.js";import"./index-BLlAsUsV.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-Bi9EDez9.js";import"./DemoProps-BerORI7p.js";import"./ThemingParameters-le4wpk1U.js";import"./ChartDataLabel-C0bjOC7u.js";import"./useTooltipFormatter-CBxkXes0.js";import"./staticProps-9MoPexX1.js";import"./ChartContainer-CBOwCFTO.js";import"./tiny-invariant-CopsF_GD.js";import"./index-CyR-fTxW.js";import"./Placeholder-Cyl5gr0q.js";import"./react-content-loader.es-C9Vmu4FD.js";function p(r){const t={h2:"h2",h3:"h3",...n(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:x}),`
+`,o.jsx(a,{isChart:!0}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:m}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(e,{of:m}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,o.jsx(i,{of:d}),`
+`,o.jsx(t.h3,{id:"with-data-labels",children:"With Data Labels"}),`
+`,o.jsx(i,{of:j}),`
+`,o.jsx(t.h3,{id:"polygon",children:"Polygon"}),`
+`,o.jsx(i,{of:c}),`
+`,o.jsx(t.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(i,{of:f}),`
+`,o.jsx(h,{of:C}),`
+`,o.jsx(D,{of:u}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(l,{})]})}function pt(r={}){const{wrapper:t}={...n(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(p,{...r})}):p(r)}export{pt as default};
---
+++ nightly/assets/Styling-BABc888U.js
@@ -0,0 +1,105 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as p}from"./index-B5Ip8tK9.js";import{M as u}from"./index-BMuSDG18.js";import{M as s,a as i}from"./index-DlRg2OHl.js";import"./index-DAuqKUvF.js";import"./TagDesign-Ga50IS6u.js";import{r as g}from"./index-U0ga5oGA.js";import"./index-B8m469-U.js";import{F as x}from"./CommandsAndQueries-43r2TnUR.js";import"./Button-CRJQYaat.js";import"./jsx-runtime-BvNWHM6A.js";import"./copy-gHwufZZP.js";import{T as j}from"./TableOfContent-lIiVb8Ys.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-C1L07-tE.js";import{e as y,r as S}from"./entry-preview-DKbN34Hu.js";import"./entry-preview-docs-DL00MZyZ.js";import{C as f}from"./Card.stories-DHhIVGm-.js";import{T as a}from"./ThemingParameters-le4wpk1U.js";import"./CustomElementsScope-lRvT7iCm.js";import{T as b}from"./index-DooFWhc5.js";import"./iframe-e5xmDyFA.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-Cx68SyXj.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-BLEjIEH9.js";import"./decline-B3prtgTL.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-C2QpdGW_.js";import"./information-DpRbDwJs.js";import"./sys-enter-2-BNMrwyXi.js";import"./sys-enter-2-38y2ESjX.js";import"./alert-oRgA-QL-.js";import"./i18n-defaults-DaBEYLpV.js";import"./index-BVR6w1bU.js";import"./preview-DdDskDMv.js";import"./DocsRenderer-CFRXHY34-DxHmPy-n.js";import"./react-18-SRpBHcoT.js";import"./Popover-B5iz04rB.js";import"./PopupsCommon.css-Cwh_2soo.js";import"./FocusableElements-B2W9U87f.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-ClsBPvBy.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-KM_ngbs2.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-N7C2bzZq.js";import"./index-CBrYwcJf.js";import"./Label-zmLCH7da.js";import"./index-DlD3csZn.js";import"./index-C_lI6Qk2.js";import"./Link-DDBI2Aee.js";import"./index-CkquY_70.js";import"./index-CqWP2qFq.js";import"./Text-BmPWIBNd.js";import"./willShowContent-CZcfsNXp.js";import"./addCustomCSSWithScoping-CzgbjJA9.js";import"./index-DjS4b8wL.js";import"./BusyIndicator-aPNGxP00.js";import"./index-DEF9m15S.js";import"./index-BLlAsUsV.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-Bi9EDez9.js";import"./Tooltips-BSX3YpE0.js";import"./EventProvider-ChtD9rRX.js";import"./chunk-XP5HYGXS-BpfKkqn7.js";import"./person-placeholder-BjOgjDro.js";import"./person-placeholder-BAYZd3sI.js";import"./LineChart-B8iSNrH_.js";import"./YAxisTicks-B7--5FQS.js";import"./ChartContainer-CBOwCFTO.js";import"./tiny-invariant-CopsF_GD.js";import"./index-CyR-fTxW.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-CBxkXes0.js";import"./staticProps-9MoPexX1.js";import"./ChartDataLabel-C0bjOC7u.js";import"./useLongestYAxisLabel-BN0VrZLu.js";import"./react-content-loader.es-C9Vmu4FD.js";import"./Line-CdxkG_S7.js";import"./useIsRTL-Do5V6Yn-.js";import"./directionChange-BzugKGnv.js";import"./index-DbikbtzH.js";import"./i18n-defaults-D9IRU8Fs.js";import"./I18nStore-C3xHSFao.js";import"./utils-BHG0mHDm.js";import"./index-HjnsKsTs.js";import"./index-C3KTbAMe.js";import"./List-DNKPGvi7.js";import"./ItemNavigation-hLCo6z-b.js";import"./handleDrop-jOBP6cTL.js";import"./DropIndicator-DIccGubx.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-Bxepv6fz.js";import"./TabbableElements-XzXYKyvZ.js";import"./index-DWCm18TY.js";import"./ListItemStandard-B1ZxRCO0.js";import"./ListItemTemplate-BMD5oK3c.js";import"./edit-BIVXU7lG.js";import"./ListItemAdditionalText.css-C2eJrzN4.js";import"./slim-arrow-right-GFkGzBgQ.js";import"./RadioButton-CjkzEnZH.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-BHk8tqcV.js";import"./accept-BewKwmCE.js";const{global:w}=__STORYBOOK_MODULE_GLOBAL__,{setDefaultProjectAnnotations:Dt,setProjectAnnotations:Rt,composeStory:C,composeStories:v}=__STORYBOOK_MODULE_PREVIEW_API__;var{window:l}=w;l&&(l.STORYBOOK_ENV="react");var T={...y,renderToCanvas:async(o,t)=>{if(o.storyContext.testingLibraryRender==null)return S(o,t);let{storyContext:{context:n,unboundStoryFn:r,testingLibraryRender:d}}=o,{unmount:h}=d(g.createElement(r,{...n}),{container:n.canvasElement});return h}};function P(o,t,n,r){return C(o,t,n,globalThis.globalProjectAnnotations??T,r)}function _(o,t){return v(o,t,P)}var m;typeof module<"u"&&((m=module==null?void 0:module.hot)==null||m.decline());const{Default:E}=_(f),L=()=>e.jsx("div",{children:e.jsx(E,{className:"card"})}),I=()=>e.jsx("div",{className:"containerCustomElement",children:e.jsx("span",{style:{color:a.sapNegativeColor,fontSize:a.sapFontLargeSize},children:"My Text"})});function c(o){const t={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong","ui5-link":"ui5-link",ul:"ul",...p(),...o.components},n=t["ui5-link"];return e.jsxs(e.Fragment,{children:[e.jsx(u,{title:"Styling"}),`
+`,e.jsx(t.h1,{id:"styling",children:"Styling"}),`
+`,e.jsx(j,{}),`
+`,e.jsx(t.h2,{id:"styling-ui5-web-components-for-react-components",children:"Styling UI5 Web Components for React components"}),`
+`,e.jsxs(t.p,{children:["You can change the appearance of the Web Components by using ",e.jsx(t.a,{href:"https://www.w3schools.com/Css/css3_variables.asp",rel:"nofollow",children:"CSS Variables"}),`.
+Per default, we are injecting the Horizon theme parameters as CSS Variables into the `,e.jsx(t.code,{children:"<head>"}),`.
+For example, if you want to change the color of all texts that use the `,e.jsx(t.code,{children:"--sapTextColor"})," variable, you can create an additional ",e.jsx(t.code,{children:"style"})," tag with the following content:"]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-html",children:`<style>
+  * {
+    --sapTextColor: limegreen;
+  }
+</style>
+`})}),`
+`,e.jsx(s,{hideCloseButton:!0,design:i.Critical,children:"Changing the value of a CSS Variable will affect theming capabilities, as it will be changed for all themes!"}),`
+`,e.jsxs(t.p,{children:["As a consequence, all HTML Elements in the subtree where this style was applied are now displaying their texts in ",e.jsx(t.code,{children:"limegreen"})," instead of ",e.jsx(t.code,{children:"#32363a"}),` which would be the default value for Fiori 3.
+You can change CSS Variables on any level - in the head, or on every single element by using either CSS classes or element style.`]}),`
+`,e.jsxs(t.p,{children:["A full list of all supported CSS Variables can be found ",e.jsx(t.a,{href:"https://github.com/SAP/ui5-webcomponents-react/blob/main/packages/base/src/styling/ThemingParameters.ts",rel:"nofollow",children:"here"}),`
+or in the `,e.jsx(t.a,{href:"https://github.com/SAP/theming-base-content/tree/master/content/Base/baseLib",rel:"nofollow",children:"theming-base-content"})," repo."]}),`
+`,e.jsx(t.h2,{id:"scrollbars",children:"Scrollbars"}),`
+`,e.jsxs(t.p,{children:["Rendering our ",e.jsx(t.code,{children:"ThemeProvider"}),` will apply the Fiori styles to all scrollbars on the page.
+If you want to opt-out of this behavior, you can add the `,e.jsx(t.code,{children:".ui5-content-native-scrollbars"})," class to the respective element to prevent the scrollbar styling from being applied."]}),`
+`,e.jsx(s,{design:i.Information,hideCloseButton:!0,children:"Due to the limited configuration options for scrollbars, you must apply this class to each scroll-container element individually."}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`<ObjectPage className="ui5-content-native-scrollbars">{children}</ObjectPage>
+`})}),`
+`,e.jsx(t.h2,{id:"style-your-own-components",children:"Style your own components"}),`
+`,e.jsxs(t.p,{children:[`It's quite likely you have to create some custom components when you are building an app.
+In order to reuse our central styling approach, you can import the `,e.jsx(t.code,{children:"ThemingParameters"})," that contain the various CSS variables used in our theming, or use the CSS variables directly."]}),`
+`,e.jsx(s,{design:i.Information,hideCloseButton:!0,children:e.jsxs(e.Fragment,{children:["You can find all ",e.jsx(t.code,{children:"ThemingParameters"})," ",e.jsx(n,{href:"./?path=/docs/knowledge-base-public-utils--docs#theming-parameters",children:"here"}),". If you don't want to use CSS-in-JS library, you can also just use the corresponding CSS variable."]})}),`
+`,e.jsx(t.p,{children:"You can then create a custom component by following this recipe:"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`import { ThemingParameters } from '@ui5/webcomponents-react-base';
+import './MyCustomElement.css';
+
+export const MyCustomElement = () => {
+  return (
+    <div className="containerCustomElement">
+      <span style={{ color: ThemingParameters.sapNegativeColor, fontSize: ThemingParameters.sapFontLargeSize }}>
+        My Text
+      </span>
+    </div>
+  );
+};
+`})}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-css",children:`.containerCustomElement {
+  background-color: var(--sapBackgroundColor);
+  font-family: var(--sapFontFamily);
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+`})}),`
+`,e.jsx(t.p,{children:"This would then be the result:"}),`
+`,e.jsx(b,{children:e.jsx(I,{})}),`
+`,e.jsxs(t.h2,{id:"applying-styling-via-classname-or-style",children:["Applying styling via ",e.jsx(t.code,{children:"className"})," or ",e.jsx(t.code,{children:"style"})]}),`
+`,e.jsxs(t.p,{children:["Almost all components allow setting ",e.jsx(t.code,{children:"className"})," or ",e.jsx(t.code,{children:"style"})," for custom styling. For standard elements like ",e.jsx(t.code,{children:"div"}),", ",e.jsx(t.code,{children:"span"}),", etc., you can easily override internal CSS properties and values, as our styles are encapsulated in a ",e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/@layer",rel:"nofollow",children:"CSS layer"}),`.
+For web components, this does `,e.jsx(t.strong,{children:"not"}),` mean that styles are inherited by shadow root elements per default.
+Only `,e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/inheritance#inherited_properties",rel:"nofollow",children:"inherited CSS properties"}),` that are not set inside the shadow root or internally passed properties will change the styles of the internal elements.
+Another special case are abstract components like the `,e.jsx(t.code,{children:"SuggestionItem"}),". The ",e.jsx(t.code,{children:"ui5-suggestion-item"})," element is mainly there to pass props to the actual component inside the shadow root and is therefore not stylable."]}),`
+`,e.jsx(t.h2,{id:"explicitly-import-css-bundles",children:"Explicitly import CSS bundles"}),`
+`,e.jsxs(t.p,{children:["By default, the ",e.jsx(t.code,{children:"ThemeProvider"})," injects the CSS of the components during runtime. If you want to import the CSS bundles explicitly, you can set the ",e.jsx(t.code,{children:"staticCssInjected"})," prop to ",e.jsx(t.code,{children:"true"}),`.
+This approach provides you with control over the order in which CSS files are injected, making it easier to override styles.`]}),`
+`,e.jsxs(t.p,{children:["Please note that the static CSS is not wrapped inside a ",e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/@layer",rel:"nofollow",children:"CSS layer"}),". You can use the ",e.jsxs(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/@import",rel:"nofollow",children:[e.jsx(t.code,{children:"@import"})," CSS at-rule"]})," to achieve that."]}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"main"})," package import: ",e.jsx(t.code,{children:"import '@ui5/webcomponents-react/styles.css'"})]}),`
+`,e.jsxs(t.li,{children:[e.jsx(t.strong,{children:"charts"})," package import: ",e.jsx(t.code,{children:"import '@ui5/webcomponents-react-charts/styles.css'"})]}),`
+`]}),`
+`,e.jsx(t.h2,{id:"css-shadow-parts",children:"CSS Shadow Parts"}),`
+`,e.jsxs(t.p,{children:["When using web components like the ",e.jsx(t.a,{href:"?path=/docs/data-display-card--docs#css-shadow-parts",children:"Card"}),`, you will sometimes see a "CSS Shadow Parts" section.
+One of the main advantages of the shadow root is encapsulation of parts of the DOM and styles, but since it could still be necessary for some elements to be adjusted even though they're inside the shadow root, the ui5-webcomponents offer a way to customize CSS of selected elements by attaching the `,e.jsx(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes#part",rel:"nofollow",children:"part"}),` attribute.
+With the help of this attribute it's possible styling the respective element, by using the `,e.jsxs(t.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/::part",rel:"nofollow",children:[e.jsx(t.code,{children:"::part"})," pseudo-element"]}),"."]}),`
+`,e.jsx(t.h3,{id:"example",children:"Example"}),`
+`,e.jsx(L,{}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsxs("details",{children:[" ",e.jsx("summary",{children:"Show Code"}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-css",children:`.card::part(root) {
+  background-color: lightgreen;
+}
+.card::part(content) {
+  transform: rotate(180deg);
+}
+`})}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`function MyComponent() {
+  return (
+    <Card
+      header={
+        <CardHeader
+          additionalText="3 of 5"
+          avatar={<Icon name="person-placeholder" />}
+          subtitleText="Direct Reports"
+          titleText="TeamSpace"
+        />
+      }
+      className="card"
+    >
+      <List>
+        <ListItemStandard description="Software Architect">Richard Wilson</ListItemStandard>
+        <ListItemStandard description="Visual Designer">Elena Petrova</ListItemStandard>
+        <ListItemStandard description="Quality Specialist">John Miller</ListItemStandard>
+      </List>
+    </Card>
+  );
+}
+`})})]}),`
+`,e.jsx(t.h2,{id:"common-css",children:"Common CSS"}),`
+`,e.jsxs(t.p,{children:["For applying common styling blocks based on SAP Fiori Design Guidelines, we recommend using the ",e.jsx(t.a,{href:"https://www.npmjs.com/package/@sap-ui/common-css",rel:"nofollow",children:"@sap-ui/common-css"})," package. You can find out more about this ",e.jsx(t.a,{href:"?path=/docs/knowledge-base-common-css--docs",children:"here"}),"."]}),`
+`,e.jsx(x,{})]})}function Wt(o={}){const{wrapper:t}={...p(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(c,{...o})}):c(o)}export{Wt as default};
