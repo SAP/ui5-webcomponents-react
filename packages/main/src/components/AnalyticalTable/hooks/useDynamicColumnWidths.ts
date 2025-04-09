a09e2e651e01839d240d953034e5e9ce7116ccae
Lukas Harbarth <lukas.harbarth@sap.com>
2025-04-09 17:18:54 +0200

    fix(AnalyticalTable): improve `"Default"` `scaleWidthMode` calculation

--- packages/main/src/components/AnalyticalTable/hooks/useDynamicColumnWidths.ts
+++ packages/main/src/components/AnalyticalTable/hooks/useDynamicColumnWidths.ts
@@ -3,6 +3,12 @@ import { AnalyticalTableScaleWidthMode } from '../../../enums/index.js';
 import { DEFAULT_COLUMN_WIDTH } from '../defaults/Column/index.js';
 import type { AnalyticalTableColumnDefinition, ReactTableHooks, TableInstance } from '../types/index.js';
 
+interface IColumnMeta {
+  contentPxAvg: number;
+  headerPx: number;
+  headerDefinesWidth?: boolean;
+}
+
 const ROW_SAMPLE_SIZE = 20;
 const MAX_WIDTH = 700;
 export const CELL_PADDING_PX = 18; /* padding left and right 0.5rem each (16px) + borders (1px) + buffer (1px) */
@@ -39,47 +45,153 @@ function stringToPx(dataPoint, id, isHeader = false) {
   return 0;
 }
 
-const columnsDeps = (
-  deps,
-  { instance: { state, webComponentsReactProperties, visibleColumns, data, rows, columns } }
-) => {
-  const isLoadingPlaceholder = !data?.length && webComponentsReactProperties.loading;
-  const hasRows = rows?.length > 0;
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const colsEqual = useMemo(() => {
-    return visibleColumns
-      ?.filter(
-        (col) =>
-          col.id !== '__ui5wcr__internal_selection_column' &&
-          col.id !== '__ui5wcr__internal_highlight_column' &&
-          col.id !== '__ui5wcr__internal_navigation_column'
-      )
-      .every((visCol) => {
-        const id = visCol.id ?? visCol.accessor;
-        return columns.some((item) => {
-          return item.accessor === id || item.id === id;
-        });
-      });
-  }, [visibleColumns, columns]);
+function calculateDefaultColumnWidths(tableWidth: number, columns: AnalyticalTableColumnDefinition[]) {
+  // Columns w/ external width property
+  const fixed = [];
+  // Columns w/o external width property
+  const dynamic = [];
+  let fixedTotal = 0;
 
-  return [
-    ...deps,
-    hasRows,
-    colsEqual,
-    visibleColumns?.length,
-    !state.tableColResized && state.tableClientWidth,
-    state.hiddenColumns.length,
-    webComponentsReactProperties.scaleWidthMode,
-    isLoadingPlaceholder
-  ];
-};
-interface IColumnMeta {
-  contentPxAvg: number;
-  headerPx: number;
-  headerDefinesWidth?: boolean;
+  // Separate fixed and dynamic columns
+  for (const col of columns) {
+    const minSize = col.minWidth ?? 0;
+    const maxSize = col.maxWidth ?? Infinity;
+
+    // External `width` defined
+    if (col.width !== undefined) {
+      let width = col.width;
+      if (width < minSize) {
+        width = minSize;
+      }
+      if (width > maxSize) {
+        width = maxSize;
+      }
+      fixedTotal += width;
+      fixed.push({ col, width });
+    } else {
+      dynamic.push({ col, width: 0 });
+    }
+  }
+
+  // Determine remaining width for dynamic columns
+  const remaining = tableWidth - fixedTotal;
+
+  // Calc total min-width required by dynamic columns
+  let totalFlexibleMin = 0;
+  for (const { col } of dynamic) {
+    totalFlexibleMin += col.minWidth ?? 0;
+  }
+
+  if (remaining < totalFlexibleMin) {
+    // Not enough space - assign each dynamic column its `minSize`
+    for (const dc of dynamic) {
+      dc.width = dc.col.minWidth ?? 0;
+    }
+  } else if (dynamic.length) {
+    // Grant same space for each dynamic column
+    const initialShare = remaining / dynamic.length;
+    for (const dc of dynamic) {
+      const minSize = dc.col.minWidth ?? 0;
+      const maxSize = dc.col.maxWidth ?? Infinity;
+      let width = initialShare;
+      if (width < minSize) {
+        width = minSize;
+      }
+      if (width > maxSize) {
+        width = maxSize;
+      }
+      dc.width = width;
+    }
+
+    // Calc assigned width and remaining space
+    let assigned = 0;
+    for (const { width } of dynamic) {
+      assigned += width ?? 0;
+    }
+
+    /**
+     * - negative: table overflows
+     * - positive: table has white-space between last column and borderInlineEnd
+     */
+    let remainingSpace = remaining - assigned;
+
+    // Grow or shrink columns that are still dynamic
+
+    // Grow columns
+    while (remainingSpace > 0) {
+      let expandableCount = 0;
+      for (const { col, width } of dynamic) {
+        if (width < (col.maxWidth ?? Infinity)) {
+          expandableCount++;
+        }
+      }
+      if (expandableCount === 0) {
+        break;
+      }
+      const extra = remainingSpace / expandableCount;
+      let used = 0;
+      for (const dc of dynamic) {
+        const maxWidth = dc.col.maxWidth ?? Infinity;
+        if (dc.width < maxWidth) {
+          const potential = dc.width + extra;
+          if (potential > maxWidth) {
+            used += maxWidth - dc.width;
+            dc.width = maxWidth;
+          } else {
+            dc.width = potential;
+            used += extra;
+          }
+        }
+      }
+      remainingSpace -= used;
+      if (used === 0) {
+        break;
+      }
+    }
+
+    // Shrink columns
+    while (remainingSpace < 0) {
+      let shrinkableCount = 0;
+      for (const { col, width } of dynamic) {
+        const min = col.minWidth ?? 0;
+        if (width > min) {
+          shrinkableCount++;
+        }
+      }
+      if (shrinkableCount === 0) {
+        break;
+      }
+      const reduction = Math.abs(remainingSpace) / shrinkableCount;
+      let used = 0;
+      for (const dc of dynamic) {
+        const min = dc.col.minWidth ?? 0;
+        if (dc.width > min) {
+          const potential = dc.width - reduction;
+          if (potential < min) {
+            used += dc.width - min;
+            dc.width = min;
+          } else {
+            dc.width = potential;
+            used += reduction;
+          }
+        }
+      }
+      remainingSpace += used;
+      if (used === 0) {
+        break;
+      }
+    }
+  }
+
+  const result = {};
+  for (const { col, width } of [...fixed, ...dynamic]) {
+    const key = col.id ?? col.accessor;
+    result[key] = width;
+  }
+  return result;
 }
 
-const smartColumns = (columns: AnalyticalTableColumnDefinition[], instance, hiddenColumns) => {
+const calculateSmartColumns = (columns: AnalyticalTableColumnDefinition[], instance, hiddenColumns) => {
   const { rows, state, webComponentsReactProperties } = instance;
   const rowSample = rows.slice(0, ROW_SAMPLE_SIZE);
   const { tableClientWidth: totalWidth } = state;
@@ -206,6 +318,41 @@ const smartColumns = (columns: AnalyticalTableColumnDefinition[], instance, hidd
   });
 };
 
+const columnsDeps = (
+  deps,
+  { instance: { state, webComponentsReactProperties, visibleColumns, data, rows, columns } }
+) => {
+  const isLoadingPlaceholder = !data?.length && webComponentsReactProperties.loading;
+  const hasRows = rows?.length > 0;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const colsEqual = useMemo(() => {
+    return visibleColumns
+      ?.filter(
+        (col) =>
+          col.id !== '__ui5wcr__internal_selection_column' &&
+          col.id !== '__ui5wcr__internal_highlight_column' &&
+          col.id !== '__ui5wcr__internal_navigation_column'
+      )
+      .every((visCol) => {
+        const id = visCol.id ?? visCol.accessor;
+        return columns.some((item) => {
+          return item.accessor === id || item.id === id;
+        });
+      });
+  }, [visibleColumns, columns]);
+
+  return [
+    ...deps,
+    hasRows,
+    colsEqual,
+    visibleColumns?.length,
+    !state.tableColResized && state.tableClientWidth,
+    state.hiddenColumns.length,
+    webComponentsReactProperties.scaleWidthMode,
+    isLoadingPlaceholder
+  ];
+};
+
 const columns = (columns: TableInstance['columns'], { instance }: { instance: TableInstance }) => {
   if (!instance.state || !instance.rows) {
     return columns;
@@ -233,91 +380,21 @@ const columns = (columns: TableInstance['columns'], { instance }: { instance: Ta
     })
     .filter(Boolean) as TableInstance['columns'];
   if (scaleWidthMode === AnalyticalTableScaleWidthMode.Smart) {
-    return smartColumns(columns, instance, hiddenColumns);
+    return calculateSmartColumns(columns, instance, hiddenColumns);
   }
-
-  const calculateDefaultTableWidth = () => {
-    const columnsWithWidthProperties = visibleColumns
-      .filter((column) => column.width ?? column.minWidth ?? column.maxWidth ?? false)
-      .map((column) => ({
-        accessor: column.id ?? column.accessor,
-        minWidth: column.minWidth,
-        width: column.width,
-        maxWidth: column.maxWidth
-      }));
-    let availableWidth = totalWidth;
-    let defaultColumnsCount = visibleColumns.length;
-
-    const columnsWithFixedWidth = columnsWithWidthProperties
-      .map((column) => {
-        const { width, minWidth, maxWidth, accessor } = column;
-        if (width) {
-          // necessary because of default minWidth
-          const acceptedWidth =
-            accessor !== '__ui5wcr__internal_highlight_column' &&
-            accessor !== '__ui5wcr__internal_selection_column' &&
-            accessor !== '__ui5wcr__internal_navigation_column' &&
-            width < 60
-              ? 60
-              : width;
-
-          availableWidth -= acceptedWidth;
-          defaultColumnsCount--;
-          return acceptedWidth;
-        }
-        const columnsWithMaxWidth = columnsWithWidthProperties.filter((item) => item.maxWidth);
-        const aggregatedColumnsMaxWidth = columnsWithMaxWidth.reduce((acc, cur) => acc + cur.maxWidth, 0);
-        const aggregatedColumnsMinWidth = columnsWithWidthProperties
-          .filter((item) => item.minWidth && !item.maxWidth)
-          .reduce((acc, cur) => acc + cur.minWidth, 0);
-
-        if (minWidth > availableWidth / defaultColumnsCount) {
-          // don't apply minWidth if enough space is available because of maxWidth properties
-          if (
-            availableWidth - aggregatedColumnsMaxWidth >
-            aggregatedColumnsMinWidth + (visibleColumns.length - columnsWithWidthProperties.length) * 60
-          ) {
-            // apply minWidth only if it's larger than the calculated available width
-            if (minWidth > (availableWidth - aggregatedColumnsMaxWidth) / columnsWithMaxWidth.length) {
-              availableWidth -= minWidth;
-              defaultColumnsCount--;
-              return minWidth;
-            }
-            return false;
-          }
-          availableWidth -= minWidth;
-          defaultColumnsCount--;
-          return minWidth;
-        }
-        if (maxWidth < availableWidth / defaultColumnsCount) {
-          availableWidth -= maxWidth;
-          defaultColumnsCount--;
-          return maxWidth;
-        }
-        return false;
-      })
-      .filter(Boolean) as number[];
-
-    const fixedWidth = columnsWithFixedWidth.reduce((acc, val) => acc + val, 0);
-    // check if columns are visible and table has width
-    if (visibleColumns.length > 0 && totalWidth > 0) {
-      // set fixedWidth as defaultWidth if all visible columns have fixed value
-      if (visibleColumns.length === columnsWithFixedWidth.length) {
-        return fixedWidth / visibleColumns.length;
-      }
-      // spread default columns
-      if (totalWidth >= fixedWidth + defaultColumnsCount * DEFAULT_COLUMN_WIDTH) {
-        return (totalWidth - fixedWidth) / defaultColumnsCount;
-      }
-    }
-    return DEFAULT_COLUMN_WIDTH;
-  };
 
   const hasData = instance.data.length > 0;
 
   if (scaleWidthMode === AnalyticalTableScaleWidthMode.Default || (!hasData && loading)) {
-    const defaultWidth = calculateDefaultTableWidth();
-    return columns.map((column) => ({ ...column, width: column.width ?? defaultWidth }));
+    const calculatedWidths = calculateDefaultColumnWidths(totalWidth, visibleColumns);
+    return columns.map((column) => {
+      const calculatedWidth = calculatedWidths[column.id] || calculatedWidths[column.accessor];
+      if (typeof calculatedWidth !== 'number') {
+        console.warn('Could not determine column width!');
+        return column;
+      }
+      return { ...column, width: calculatedWidth };
+    });
   }
 
   // AnalyticalTableScaleWidthMode.Grow
