d91ea2a95c50f4fb0d169ab64c6b6e56cbfcc6b8
Lukas742 <lukas.harbarth@sap.com>
2025-04-15 14:55:13 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@1d4c4feaaae88dfb5da1b42158932856fd1dac69 🚀

---
+++ nightly/assets/Button-BhaJMoI4.js
@@ -0,0 +1,104 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-B5Ip8tK9.js";import{S as p}from"./SubcomponentsSection-BVy4lpQe.js";import{M as s,C as a,a as u,D as c}from"./index-B_RHyumS.js";import{A as l}from"./ArgTypesWithNote-CNK8aqWd.js";import{C as x}from"./ControlsWithNote-C56loArH.js";import{D as d}from"./DocsHeader-D_5VhL_t.js";import{F as f}from"./CommandsAndQueries-DFsl54yX.js";import"./index-Dcu26oOx.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-zAoAJako.js";import{C as M,D as o,B as r}from"./Button.stories-CcgtDAkU.js";import{e as I}from"./utils-DsNTp64j.js";import"./iframe-BTn4y0nK.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./index-Ultsg-G0.js";import"./jsx-runtime-BCizSm9J.js";import"./CustomElementsScope-lRvT7iCm.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-C3KIEZ_B.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-D8gJIGEW.js";import"./parameters-bundle.css-BLEjIEH9.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DBnVRLcV.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B8cpOtV2.js";import"./decline-CO_yryFo.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-BiJmvry2.js";import"./information-BA_ST16a.js";import"./sys-enter-2-L9fob0bg.js";import"./sys-enter-2-C09nDPBO.js";import"./alert-BjJeIaiS.js";import"./TagDesign-Ga50IS6u.js";import"./index-3KvboCIt.js";import"./Tag-JkllhvLP.js";import"./sys-help-2-bRrqGaJr.js";import"./index-Cin_GHPy.js";import"./Link-DU2LYizk.js";import"./index-DogrUJhP.js";import"./Popover-ZaMaqrll.js";import"./PopupsCommon.css-DkWsY6As.js";import"./FocusableElements-BcTTx3-_.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DMSxvinm.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-D5y8ERWb.js";import"./copy-BJ2RS-I5.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-BGrOnDbB.js";import"./I18nStore-BqSUVB5y.js";import"./useStylesheet-N7C2bzZq.js";import"./index-hbDzTvue.js";import"./index-BT6O4HVG.js";import"./Label-efMMm_Yw.js";import"./index-DoqJBpXZ.js";import"./index-BCr_l6cl.js";import"./Text-ahyk1WrN.js";import"./preview-rFZEIRXq.js";import"./DocsRenderer-CFRXHY34-B6NElpfl.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-BBYkOVIV.js";import"./index-CWu8VubW.js";import"./BusyIndicator-CXYqK0WZ.js";import"./index-GxH6uhPh.js";import"./index-D-WnL20f.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-CIU74CR3.js";import"./ai-DUb86FXK.js";import"./navigation-down-arrow-w7X5vYJb.js";import"./stop-C-TY6Egz.js";import"./query-DOGTENGM.js";import"./SplitButton-MBgsmLon.js";import"./slim-arrow-down-BDYFrML6.js";import"./slim-arrow-down-CKUZ-1eY.js";import"./parameters-bundle.css-CrrhGu77.js";import"./index-oXuKD_5j.js";import"./MenuItem-DzgNRDKp.js";import"./nav-back-CgWDXmhS.js";import"./ItemNavigation-CI8VBZuZ.js";import"./ListItemTemplate-CcqalvRg.js";import"./edit-BwonER89.js";import"./List-COtqWMJI.js";import"./handleDrop-Bmmjrojp.js";import"./DropIndicator-DDSuuoEU.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-BvHqtelz.js";import"./TabbableElements-XzXYKyvZ.js";import"./ListItemAdditionalText.css-VpRBIBul.js";import"./slim-arrow-right-q6_LTn1J.js";import"./RadioButton-8YHj4Kz3.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-BRq3_G1m.js";import"./accept-Bmwffvy6.js";import"./ResponsivePopover-BAaf0na0.js";import"./Dialog-CrFT51rq.js";import"./index-BUHSN-nc.js";import"./Menu-BIugMdyd.js";import"./ListItemCustom-4RY6Qin6.js";import"./index-W9vpPI6U.js";import"./main-DXdDxF5b.js";function i(n){const e={code:"code",h2:"h2",pre:"pre",...m(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:M}),`
+`,t.jsx(d,{experimental:!0}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(a,{of:o}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show static code"}),t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-tsx",children:`function AIButton(props) {
+  const generationIdRef = useRef<NodeJS.Timeout | null>(null);
+  const buttonRef = useRef<ButtonDomRef>(null);
+  const [buttonState, setButtonState] = useState("generate");
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const startGeneration = () => {
+    generationIdRef.current = setTimeout(() => {
+      setButtonState("revise");
+    }, 3000);
+  };
+
+  const handleClick: ButtonPropTypes["onClick"] = (e) => {
+    const btn = e.target;
+
+    setMenuOpen(false);
+    switch (btn.state) {
+      case "generate":
+        setButtonState("generating");
+        startGeneration();
+        break;
+      case "generating":
+        if (generationIdRef.current) {
+          clearTimeout(generationIdRef.current);
+          generationIdRef.current = null;
+        }
+        setButtonState("generate");
+        break;
+      case "revise":
+        setMenuOpen(true);
+        break;
+    }
+  };
+
+  const handleMenuItemClick: MenuPropTypes["onItemClick"] = (e) => {
+    if (e.detail.text === "Regenerate") {
+      setButtonState("generating");
+      startGeneration();
+    }
+  };
+
+  return (
+    <>
+      <Button
+        {...props}
+        onClick={handleClick}
+        state={buttonState}
+        ref={buttonRef}
+      >
+        <ButtonState name="generate" text="Generate" icon={aiIcon} />
+        <ButtonState name="generating" text="Stop Generating" icon={stopIcon} />
+        <ButtonState
+          name="revise"
+          text="Revise"
+          icon={aiIcon}
+          endIcon={navDownIcon}
+        />
+      </Button>
+      {menuOpen && (
+        <Menu
+          opener={buttonRef.current}
+          open
+          onItemClick={handleMenuItemClick}
+          onClose={() => {
+            setMenuOpen(false);
+          }}
+        >
+          <MenuItem text="Regenerate" />
+          <MenuSeparator />
+          <MenuItem text="Fix Spelling & Grammar" />
+          <MenuItem text="Change Tone">
+            <MenuItem text="Option 1" />
+            <MenuItem text="Option 2" />
+            <MenuItem text="Option 3" />
+          </MenuItem>
+          <MenuItem text="Adjust Length">
+            <MenuItem text="Shorten text" />
+            <MenuItem text="Lengthen text" />
+          </MenuItem>
+          <MenuItem text="Bulleted List" />
+          <MenuItem text="Translate">
+            <MenuItem text="English" />
+            <MenuItem text="German" />
+            <MenuItem text="Spanish" />
+          </MenuItem>
+        </Menu>
+      )}
+    </>
+  );
+}
+`})})]}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(x,{of:o}),`
+`,t.jsx(u,{children:p}),`
+`,t.jsx(e.h2,{id:"buttonstate",children:"ButtonState"}),`
+`,t.jsx(c,{of:r}),`
+`,t.jsx(l,{of:r,hideHTMLPropsNote:!0,exclude:I}),`
+`,t.jsx(f,{}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{})})]})}function be(n={}){const{wrapper:e}={...m(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(i,{...n})}):i(n)}export{be as default};
