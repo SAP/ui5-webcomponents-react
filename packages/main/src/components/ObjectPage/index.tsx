e77f0b7f678471cdc5b400eec772fbc034bd0f01
Lukas Harbarth <lukas.harbarth@sap.com>
2025-04-15 08:50:06 +0200

    fix(ObjectPage): correct spacing when `image` is not present

--- packages/main/src/components/ObjectPage/index.tsx
+++ packages/main/src/components/ObjectPage/index.tsx
@@ -617,7 +617,7 @@ const ObjectPage = forwardRef<ObjectPageDomRef, ObjectPagePropTypes>((props, ref
             'data-not-clickable': !!preserveHeaderStateOnClick,
             'data-header-content-visible': headerArea && headerCollapsed !== true,
             _snappedAvatar:
-              !headerArea || (titleArea && image && headerCollapsed === true) ? (
+              (!headerArea && image) || (image && headerCollapsed === true) ? (
                 <CollapsedAvatar image={image} imageShapeCircle={imageShapeCircle} />
               ) : null
           })}
