5f966d0893067a044336c416cf78c713fba3ede3
Lukas742 <lukas.harbarth@sap.com>
2025-05-05 06:21:42 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@7c69489a0b5c690e02477be7cecad0aaeb89d693 🚀

---
+++ nightly/assets/PluginOrderedMultiSort-CR2Cp1e-.js
@@ -0,0 +1,62 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-B5Ip8tK9.js";import{I as n,F as p}from"./CommandsAndQueries-B8Wy9Icw.js";import{M as a,C as s}from"./index-DA6h6TiF.js";import"./index-Ultsg-G0.js";import"./index-lGGczWRA.js";import{C as l}from"./ControlsWithNote-BdH-Rl3M.js";import"./Button-C3KIEZ_B.js";import"./jsx-runtime-BCizSm9J.js";import"./copy-BJ2RS-I5.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-zAoAJako.js";import{e}from"./AnalyticalTableHooks.stories-Bz10SSpx.js";import"./preview-BX_GW9ew.js";import"./iframe-ca_icXud.js";import"./DocsRenderer-CFRXHY34-De5Puzc-.js";import"./react-18-SRpBHcoT.js";import"./index-BVR6w1bU.js";import"./Popover-ZaMaqrll.js";import"./slot-_4yKMUwC.js";import"./PopupsCommon.css-DkWsY6As.js";import"./event-strict-DgQLNDEV.js";import"./FocusableElements-BcTTx3-_.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./CustomElementsScope-lRvT7iCm.js";import"./Keys-B3oBAe0t.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DMSxvinm.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./parameters-bundle.css-BLEjIEH9.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-B8m469-U.js";import"./index-Dtw3V25N.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-N7C2bzZq.js";import"./index-BT6O4HVG.js";import"./Label-efMMm_Yw.js";import"./i18n-CjR4HGlq.js";import"./i18n-defaults-B8cpOtV2.js";import"./index-DoqJBpXZ.js";import"./index-Cin_GHPy.js";import"./Link-DU2LYizk.js";import"./Icon-D8gJIGEW.js";import"./index-DogrUJhP.js";import"./index-BCr_l6cl.js";import"./Text-ahyk1WrN.js";import"./willShowContent-CZcfsNXp.js";import"./addCustomCSSWithScoping-BBYkOVIV.js";import"./index-CWu8VubW.js";import"./BusyIndicator-CXYqK0WZ.js";import"./index-GxH6uhPh.js";import"./index-D-WnL20f.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-CIU74CR3.js";import"./index-DOF6V_xK.js";import"./I18nStore-BqSUVB5y.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./decline-CO_yryFo.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-BiJmvry2.js";import"./information-BA_ST16a.js";import"./sys-enter-2-L9fob0bg.js";import"./sys-enter-2-C09nDPBO.js";import"./alert-BjJeIaiS.js";import"./TagDesign-Ga50IS6u.js";import"./utils-BIVnZhr0.js";import"./main-DXdDxF5b.js";import"./index-3KvboCIt.js";import"./Tag-JkllhvLP.js";import"./sys-help-2-bRrqGaJr.js";import"./Tooltips-DBnVRLcV.js";import"./EventProvider-ChtD9rRX.js";import"./Friends500-CRbR9XAn.js";import"./AnalyticalTable.stories-DqkugLub.js";import"./delete-VBZHUjs5.js";import"./edit-BwonER89.js";import"./settings-BLpaNyt-.js";import"./index-mr09dELX.js";import"./ValueState-Bg0UWejw.js";import"./slim-arrow-down-CKUZ-1eY.js";import"./ListBoxItemGroupTemplate-C5f5tyiM.js";import"./ListItemGroup-BvHqtelz.js";import"./DropIndicator-DDSuuoEU.js";import"./TabbableElements-XzXYKyvZ.js";import"./ComboBoxItemGroup-UA-bKctZ.js";import"./ListItemBaseTemplate-BaXET7RV.js";import"./Token-x7Ew4Ebm.js";import"./ItemNavigation-CI8VBZuZ.js";import"./ScrollEnablement-CGuDCzzT.js";import"./animate-JylhYHXj.js";import"./List-COtqWMJI.js";import"./handleDrop-Bmmjrojp.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemStandard-DCkSmcmr.js";import"./ListItemTemplate-CcqalvRg.js";import"./ListItemAdditionalText.css-VpRBIBul.js";import"./slim-arrow-right-q6_LTn1J.js";import"./RadioButton-8YHj4Kz3.js";import"./CheckBox-BRq3_G1m.js";import"./accept-Bmwffvy6.js";import"./ResponsivePopover-BAaf0na0.js";import"./Dialog-CrFT51rq.js";import"./ResponsivePopoverCommon.css-GWs4D-OS.js";import"./Suggestions.css-BfKccsl2.js";import"./ToggleButton-B9SDC_8m.js";import"./Input-Dq2g75jQ.js";import"./ValueStateMessage.css-BMlIHNzi.js";import"./SuggestionItem-C8qFKawP.js";import"./index-Cw0tR2Ff.js";import"./Option-2JLejXaS.js";import"./index-Cs_rboP9.js";import"./Select-B4r_2S9H.js";import"./InvisibleMessage-C93gGji-.js";import"./slim-arrow-down-BDYFrML6.js";import"./i18n-defaults-fwhIyFUy.js";import"./useIsRTL-Do5V6Yn-.js";import"./directionChange-BzugKGnv.js";import"./index-D_N4EdlB.js";import"./IconDesign-DXd8PPVF.js";import"./filter-D8yhAw3Z.js";import"./group-2-nvfv5XDd.js";import"./sort-descending-Cij5RtVd.js";import"./ThemingParameters-BB3UHRT7.js";import"./index-DZIy6DqY.js";import"./stopPropagation-BJWsenK5.js";import"./index-DK37Epwc.js";import"./utils-BHG0mHDm.js";import"./index-BlpWI8MS.js";import"./index-D7fH2Alh.js";import"./ListItemCustom-4RY6Qin6.js";import"./index-hsck0mqN.js";import"./navigation-down-arrow-w7X5vYJb.js";import"./navigation-right-arrow-5oXObhj2.js";import"./navigation-right-arrow-XWO6S813.js";import"./useCurrentTheme-cn65QFku.js";import"./IndicationColor-DVw-fSM_.js";import"./index-zKXzvywe.js";import"./debounce-D7W5PopO.js";import"./index-SttPHCbI.js";import"./index-C5zp3NuJ.js";function i(o){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...m(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Data Display / AnalyticalTable / Plugin Hooks / useOrderedMultiSort"}),`
+`,t.jsx(r.h1,{id:"analyticaltable-plugin-useorderedmultisort",children:"AnalyticalTable Plugin: useOrderedMultiSort"}),`
+`,t.jsx(n,{moduleNames:["AnalyticalTableHooks"],packageName:"@ui5/webcomponents-react"}),`
+`,t.jsxs(r.p,{children:["With the ",t.jsx(r.code,{children:"useOrderedMultiSort"})," it's possible to define the sorting priority if multi-sort is enabled for the respective columns (",t.jsx(r.code,{children:"enableMultiSort: true"}),")."]}),`
+`,t.jsx(r.p,{children:"It receives one parameter that defines the sorting priority by the column id."}),`
+`,t.jsxs(r.ul,{children:[`
+`,t.jsxs(r.li,{children:[t.jsx(r.code,{children:"orderedIds: string[]"}),": Defines the sort priority when sorting by multiple columns, starting with the first column ID."]}),`
+`]}),`
+`,t.jsxs(r.p,{children:[t.jsx(r.strong,{children:"Note:"})," Column IDs that are not found in the array use the default priority, so the first sorted column has a higher priority than the next sorted column."]}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(s,{sourceState:"none",of:e}),`
+`,t.jsx(r.h3,{id:"current-parameter",children:"Current Parameter"}),`
+`,t.jsx(l,{of:e,hideHTMLPropsNote:!0,include:["orderedIds"]}),`
+`,t.jsx(r.h3,{id:"code",children:"Code"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`const columns = [
+  {
+    Header: 'Name',
+    accessor: 'name',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age',
+    accessor: 'age',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Name 2',
+    accessor: 'name2',
+    enableMultiSort: true
+  },
+  {
+    Header: 'Age 2',
+    accessor: 'age2',
+    enableMultiSort: true
+  }
+];
+const data = [
+  { name: 'Peter', age: 40, name2: 'Alissa', age2: 18 },
+  { name: 'Kristen', age: 40, name2: 'Randolph', age2: 21 },
+  { name: 'Peter', age: 30, name2: 'Rose', age2: 90 },
+  { name: 'Peter', age: 70, name2: 'Rose', age2: 22 },
+  { name: 'Kristen', age: 60, name2: 'Willis', age2: 80 },
+  { name: 'Kristen', age: 20, name2: 'Alissa', age2: 80 },
+  { name: 'Graham', age: 40, name2: 'Alissa', age2: 80 },
+  { name: 'Peter', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Rose', age2: 26 },
+  { name: 'Graham', age: 65, name2: 'Willis', age2: 26 },
+  { name: 'Graham', age: 62, name2: 'Willis', age2: 26 }
+];
+const orderedIds = ['name', 'name2', 'age', 'age2'];
+const TableComponent = () => {
+  return (
+    <AnalyticalTable
+      columns={columns}
+      data={data}
+      sortable
+      tableHooks={[AnalyticalTableHooks.useOrderedMultiSort(orderedIds)]}
+    />
+  );
+};
+`})}),`
+`,t.jsx(p,{})]})}function Qr(o={}){const{wrapper:r}={...m(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{Qr as default};
--- nightly/assets/index-CRRRDip3.js
+++
@@ -1,1 +0,0 @@
-const i={version:"2.9.2",major:2,minor:9,patch:2,suffix:"",isNext:!1,buildTime:1746079054};i.version;export{i as V};
