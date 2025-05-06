afc07d9059c6bb186eb5b65644fb110cf1b2dc4c
Lukas742 <lukas.harbarth@sap.com>
2025-05-06 07:59:00 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@cbb3a14a42758891f12bf09dea2aed0c95c2a5c7 🚀

---
+++ nightly/assets/NavigationLayout-BESQsW4F.js
@@ -0,0 +1,123 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as c}from"./index-B5Ip8tK9.js";import{A as e}from"./ArgTypesWithNote-DqTYSCMO.js";import{C as u}from"./ControlsWithNote-J6NWzxIw.js";import{D as l}from"./DocsHeader-B7lT20pR.js";import{F as g}from"./CommandsAndQueries-WnVjarr3.js";import"./index-DJZ2Iw3D.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-zAoAJako.js";import{S as x}from"./SubcomponentsSection-BVy4lpQe.js";import{M as S,C as v,a as f,D as n}from"./index-CPUYVYC9.js";import{C as h,D as r}from"./NavigationLayout.stories-eaddKp1n.js";import{S as m,a,b as p,c as s}from"./chain-link-DSBSVorU.js";import"./index-Ultsg-G0.js";import"./jsx-runtime-BCizSm9J.js";import"./CustomElementsScope-lRvT7iCm.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-C3KIEZ_B.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-D8gJIGEW.js";import"./parameters-bundle.css-BLEjIEH9.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DBnVRLcV.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B8cpOtV2.js";import"./decline-CO_yryFo.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-BiJmvry2.js";import"./information-BA_ST16a.js";import"./sys-enter-2-L9fob0bg.js";import"./sys-enter-2-C09nDPBO.js";import"./alert-BjJeIaiS.js";import"./TagDesign-Ga50IS6u.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./utils-CsoSS6Je.js";import"./main-DXdDxF5b.js";import"./index-3KvboCIt.js";import"./Tag-JkllhvLP.js";import"./sys-help-2-bRrqGaJr.js";import"./index-Cin_GHPy.js";import"./Link-DU2LYizk.js";import"./index-DogrUJhP.js";import"./Popover-ZaMaqrll.js";import"./PopupsCommon.css-DkWsY6As.js";import"./FocusableElements-BcTTx3-_.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DMSxvinm.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-D5y8ERWb.js";import"./copy-BJ2RS-I5.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-MM-2q_iP.js";import"./I18nStore-BqSUVB5y.js";import"./useStylesheet-N7C2bzZq.js";import"./index-KM_ngbs2.js";import"./index-BT6O4HVG.js";import"./Label-efMMm_Yw.js";import"./index-DoqJBpXZ.js";import"./index-BCr_l6cl.js";import"./Text-ahyk1WrN.js";import"./preview-DE8qqRls.js";import"./iframe-CUSB7HcV.js";import"./DocsRenderer-CFRXHY34-I_O2nZaB.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-BBYkOVIV.js";import"./index-CWu8VubW.js";import"./BusyIndicator-CXYqK0WZ.js";import"./index-GxH6uhPh.js";import"./index-D-WnL20f.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-CIU74CR3.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./menu-GDdAV5G8.js";import"./home-TeeHX4kK.js";import"./calendar-DKUaHwWk.js";import"./background-D_td2KOQ.js";import"./action-settings-CIeRlDc7.js";import"./index-UVJPYVaA.js";import"./ListItemStandard-DCkSmcmr.js";import"./ListItemTemplate-CcqalvRg.js";import"./edit-BwonER89.js";import"./List-COtqWMJI.js";import"./ItemNavigation-CI8VBZuZ.js";import"./handleDrop-Bmmjrojp.js";import"./DropIndicator-DDSuuoEU.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-BvHqtelz.js";import"./TabbableElements-XzXYKyvZ.js";import"./ListItemAdditionalText.css-VpRBIBul.js";import"./slim-arrow-right-q6_LTn1J.js";import"./RadioButton-8YHj4Kz3.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-BRq3_G1m.js";import"./accept-Bmwffvy6.js";import"./Menu-BIugMdyd.js";import"./MenuItem-DzgNRDKp.js";import"./nav-back-CgWDXmhS.js";import"./ResponsivePopover-BAaf0na0.js";import"./Dialog-CrFT51rq.js";import"./ListItemCustom-4RY6Qin6.js";import"./search-BdVRvW0m.js";import"./overflow-V6Gdxnl2.js";import"./overflow-DdGXGAjV.js";import"./slim-arrow-down-BDYFrML6.js";import"./slim-arrow-down-CKUZ-1eY.js";import"./parameters-bundle.css-Bs_zJ0h6.js";import"./i18n-defaults-C268ZfY_.js";import"./index-SttPHCbI.js";import"./navigation-right-arrow-5oXObhj2.js";import"./navigation-right-arrow-XWO6S813.js";import"./navigation-down-arrow-w7X5vYJb.js";function d(o){const i={code:"code",h2:"h2",pre:"pre",...c(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(S,{of:h}),`
+`,t.jsx(l,{}),`
+`,t.jsx("br",{}),`
+`,t.jsx(i.h2,{id:"example",children:"Example"}),`
+`,t.jsx(v,{of:r}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show static code"}),t.jsx(i.pre,{children:t.jsx(i.code,{className:"language-tsx",children:`function NavigationLayoutComponent(props) {
+  const [selectedContent, setSelectedContent] = useState('Home');
+  const [mode, setMode] = useState(props.mode);
+  const navigationLayoutRef = useRef<NavigationLayoutDomRef>(null);
+
+  const handleSelectionChange: SideNavigationPropTypes['onSelectionChange'] = (e) => {
+    setSelectedContent(e.detail.item.text);
+  };
+
+  useEffect(() => {
+    setMode(props.mode);
+  }, [props.mode]);
+
+  return (
+    <div style={{ position: 'relative', height: '800px' }}>
+      <NavigationLayout
+        {...props}
+        ref={navigationLayoutRef}
+        mode={mode}
+        header={
+          <ShellBar
+            startButton={
+              <Button
+                icon={menuIcon}
+                onClick={() => {
+                  if (navigationLayoutRef.current?.isSideCollapsed()) {
+                    setMode(NavigationLayoutMode.Expanded);
+                  } else {
+                    setMode(NavigationLayoutMode.Collapsed);
+                  }
+                }}
+              />
+            }
+            primaryTitle="UI5 Web Components for React"
+            secondaryTitle="The Best Run SAP"
+          />
+        }
+        sideContent={
+          <SideNavigation slot="sideContent" onSelectionChange={handleSelectionChange}>
+            <SideNavigationItem text="Home" icon="home" />
+            <SideNavigationGroup text="Group 1" expanded>
+              <SideNavigationItem text="Item 1" icon="locate-me" expanded>
+                <SideNavigationSubItem text="Sub Item 1" />
+                <SideNavigationSubItem text="Sub Item 2" />
+              </SideNavigationItem>
+              <SideNavigationItem text="Item 2" icon="calendar" expanded>
+                <SideNavigationSubItem text="Sub Item 3" />
+                <SideNavigationSubItem text="Sub Item 4" />
+              </SideNavigationItem>
+              <SideNavigationItem text="Item 3" icon="activity-assigned-to-goal" expanded>
+                <SideNavigationSubItem text="Sub Item 5" />
+                <SideNavigationSubItem text="Sub Item 6" />
+              </SideNavigationItem>
+            </SideNavigationGroup>
+            <SideNavigationGroup text="Group 2" expanded>
+              <SideNavigationItem text="Item 4" icon="history" />
+              <SideNavigationItem text="Item 5" icon="source-code" />
+              <SideNavigationItem text="Item 6" icon="background" />
+            </SideNavigationGroup>
+
+            <SideNavigationItem
+              slot="fixedItems"
+              text="Legal"
+              href="https://www.sap.com/about/legal/impressum.html"
+              target="_blank"
+              icon="compare"
+            />
+            <SideNavigationItem
+              slot="fixedItems"
+              text="Privacy"
+              href="https://www.sap.com/about/legal/privacy.html"
+              target="_blank"
+              icon="locked"
+            />
+            <SideNavigationItem
+              slot="fixedItems"
+              text="Terms of Use"
+              href="https://www.sap.com/terms-of-use"
+              target="_blank"
+              icon="document-text"
+            />
+          </SideNavigation>
+        }
+      >
+        <div style={{ padding: '1rem' }}>
+          <div>
+            <Title>{selectedContent}</Title>
+            <br />
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </Text>
+          </div>
+        </div>
+      </NavigationLayout>
+    </div>
+  );
+}
+`})})]}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(u,{of:r}),`
+`,t.jsx(f,{children:x}),`
+`,t.jsx(i.h2,{id:"sidenavigation",children:"SideNavigation"}),`
+`,t.jsx(n,{of:m}),`
+`,t.jsx(e,{of:m}),`
+`,t.jsx(i.h2,{id:"sidenavigationitem",children:"SideNavigationItem"}),`
+`,t.jsx(n,{of:a}),`
+`,t.jsx(e,{of:a}),`
+`,t.jsx(i.h2,{id:"sidenavigationsubitem",children:"SideNavigationSubItem"}),`
+`,t.jsx(n,{of:p}),`
+`,t.jsx(e,{of:p}),`
+`,t.jsx(i.h2,{id:"sidenavigationgroup",children:"SideNavigationGroup"}),`
+`,t.jsx(n,{of:s}),`
+`,t.jsx(e,{of:s}),`
+`,t.jsx(g,{})]})}function Ei(o={}){const{wrapper:i}={...c(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(d,{...o})}):d(o)}export{Ei as default};
---
+++ nightly/assets/ReadMeAI-DiOF1y9N.js
@@ -0,0 +1,39 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-B5Ip8tK9.js";import{M as e,a as m}from"./index-CPUYVYC9.js";import"./index-Ultsg-G0.js";import"./index-DJZ2Iw3D.js";import"./TagDesign-Ga50IS6u.js";import"./index-U0ga5oGA.js";import"./index-B8m469-U.js";import{F as p}from"./CommandsAndQueries-WnVjarr3.js";import"./Button-C3KIEZ_B.js";import"./jsx-runtime-BCizSm9J.js";import"./copy-BJ2RS-I5.js";import{T as s}from"./TableOfContent-BG9P-NK7.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-zAoAJako.js";import"./iframe-CUSB7HcV.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-D8gJIGEW.js";import"./CustomElementsScope-lRvT7iCm.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-BLEjIEH9.js";import"./decline-CO_yryFo.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-BiJmvry2.js";import"./information-BA_ST16a.js";import"./sys-enter-2-L9fob0bg.js";import"./sys-enter-2-C09nDPBO.js";import"./alert-BjJeIaiS.js";import"./i18n-defaults-B8cpOtV2.js";import"./index-BVR6w1bU.js";import"./preview-DE8qqRls.js";import"./DocsRenderer-CFRXHY34-I_O2nZaB.js";import"./react-18-SRpBHcoT.js";import"./Popover-ZaMaqrll.js";import"./PopupsCommon.css-DkWsY6As.js";import"./FocusableElements-BcTTx3-_.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DMSxvinm.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-KM_ngbs2.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-N7C2bzZq.js";import"./index-BT6O4HVG.js";import"./Label-efMMm_Yw.js";import"./index-DoqJBpXZ.js";import"./index-Cin_GHPy.js";import"./Link-DU2LYizk.js";import"./index-DogrUJhP.js";import"./index-BCr_l6cl.js";import"./Text-ahyk1WrN.js";import"./willShowContent-CZcfsNXp.js";import"./addCustomCSSWithScoping-BBYkOVIV.js";import"./index-CWu8VubW.js";import"./BusyIndicator-CXYqK0WZ.js";import"./index-GxH6uhPh.js";import"./index-D-WnL20f.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-CIU74CR3.js";import"./index-MM-2q_iP.js";import"./I18nStore-BqSUVB5y.js";import"./Tooltips-DBnVRLcV.js";import"./EventProvider-ChtD9rRX.js";const a=`# @ui5/webcomponents-ai-react
+
+React wrapper for the [@ui5/webcomponents-ai](https://sap.github.io/ui5-webcomponents/components/ai/) web components.
+
+## Installation
+
+To consume \`@ui5/webcomponents-ai-react\`, you need to install the npm modules and required peer dependencies:
+
+\`\`\`bash
+npm install @ui5/webcomponents-ai-react @ui5/webcomponents-ai
+\`\`\`
+
+## Provided assets
+
+| Assets           | Module                                 | Notes                                                  |
+| ---------------- | -------------------------------------- | ------------------------------------------------------ |
+| \`i18n\`, \`themes\` | \`@ui5/webcomponents-ai/dist/Assets.js\` | Theming parameters and translations for the components |
+
+## UI5 Web Components Documentation and Patterns
+
+- Components: https://sap.github.io/ui5-webcomponents/components/ai/
+- Patterns: https://sap.github.io/ui5-webcomponents/components/patterns/
+
+## Documentation
+
+You can find an interactive documentation in our [Storybook](https://sap.github.io/ui5-webcomponents-react/).
+
+## Contribute
+
+Please check our [Contribution Guidelines](https://github.com/SAP/ui5-webcomponents-react/blob/main/CONTRIBUTING.md).
+
+## License
+
+Please see our [LICENSE](https://github.com/SAP/ui5-webcomponents-react/blob/main/LICENSE) for copyright and license information.
+Detailed information including third-party components and their licensing/copyright information is available via the [REUSE tool](https://api.reuse.software/info/github.com/SAP/ui5-webcomponents-react).
+`;function n(o){return t.jsxs(t.Fragment,{children:[t.jsx(e,{title:"AI / Docs"}),`
+`,t.jsx(s,{}),`
+`,t.jsx(m,{children:a.split("## Documentation")[0].trim()}),`
+`,t.jsx(p,{})]})}function It(o={}){const{wrapper:i}={...r(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(n,{...o})}):n()}export{It as default};
