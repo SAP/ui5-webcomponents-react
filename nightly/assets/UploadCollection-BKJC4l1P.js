a7ac8e3fcb20697514026a893858c98e6df949a2
Lukas742 <lukas.harbarth@sap.com>
2025-05-06 15:24:44 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@5b75245a7667e82c31baff0892dd0ceef0fa1763 🚀

---
+++ nightly/assets/UploadCollection-BKJC4l1P.js
@@ -0,0 +1,127 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-B5Ip8tK9.js";import{A as m}from"./ArgTypesWithNote-CYDi5T9l.js";import{C as a}from"./ControlsWithNote-CF6NhBkX.js";import{D as d}from"./DocsHeader-D-0fWmtF.js";import{F as s}from"./CommandsAndQueries-NCJOqs8W.js";import"./index-BvBDF_5X.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-zAoAJako.js";import{S as c}from"./SubcomponentsSection-BVy4lpQe.js";import{M as h,C as n,a as f,D as x}from"./index-B4Czy4rc.js";import{C as u,D as i,S as C,U as r}from"./UploadCollection.stories-nknZ2FPh.js";import"./index-Ultsg-G0.js";import"./jsx-runtime-BCizSm9J.js";import"./CustomElementsScope-lRvT7iCm.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-C3KIEZ_B.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-D8gJIGEW.js";import"./parameters-bundle.css-BLEjIEH9.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DBnVRLcV.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B8cpOtV2.js";import"./decline-CO_yryFo.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-BiJmvry2.js";import"./information-BA_ST16a.js";import"./sys-enter-2-L9fob0bg.js";import"./sys-enter-2-C09nDPBO.js";import"./alert-BjJeIaiS.js";import"./TagDesign-Ga50IS6u.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./utils-CWtRxDRY.js";import"./main-DXdDxF5b.js";import"./index-sa6HOp3c.js";import"./Tag-JkllhvLP.js";import"./sys-help-2-bRrqGaJr.js";import"./index-BQnCm__z.js";import"./Link-DU2LYizk.js";import"./index-DogrUJhP.js";import"./Popover-ZaMaqrll.js";import"./PopupsCommon.css-DkWsY6As.js";import"./FocusableElements-BcTTx3-_.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DMSxvinm.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-D5y8ERWb.js";import"./copy-BJ2RS-I5.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-C20XHsFQ.js";import"./I18nStore-BqSUVB5y.js";import"./useStylesheet-N7C2bzZq.js";import"./index-CRT1e2Cp.js";import"./index-Cvp8w3St.js";import"./Label-efMMm_Yw.js";import"./index-DoqJBpXZ.js";import"./index-BCr_l6cl.js";import"./Text-ahyk1WrN.js";import"./preview-DA1GaxG9.js";import"./iframe-DEQIVPnd.js";import"./DocsRenderer-CFRXHY34-C8fjKU_Z.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-BBYkOVIV.js";import"./index-CWu8VubW.js";import"./BusyIndicator-CXYqK0WZ.js";import"./index-GxH6uhPh.js";import"./index-D-WnL20f.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-CIU74CR3.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./List-COtqWMJI.js";import"./ItemNavigation-CI8VBZuZ.js";import"./handleDrop-Bmmjrojp.js";import"./DropIndicator-DDSuuoEU.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-BvHqtelz.js";import"./TabbableElements-XzXYKyvZ.js";import"./index-DZIy6DqY.js";import"./index-Bb2qNNsG.js";import"./ValueState-Bg0UWejw.js";import"./ListItemTemplate-CcqalvRg.js";import"./edit-BwonER89.js";import"./ListItemAdditionalText.css-VpRBIBul.js";import"./slim-arrow-right-q6_LTn1J.js";import"./RadioButton-8YHj4Kz3.js";import"./CheckBox-BRq3_G1m.js";import"./accept-Bmwffvy6.js";import"./stop-C-TY6Egz.js";import"./i18n-defaults-Dwe5xu0P.js";import"./Input-T21OFmzM.js";import"./ResponsivePopoverCommon.css-GWs4D-OS.js";import"./ValueStateMessage.css-BMlIHNzi.js";import"./Suggestions.css-BfKccsl2.js";import"./ProgressIndicator-C87gmk0Z.js";import"./AnimationMode-DHtcdHol.js";import"./parameters-bundle.css-Bs_zJ0h6.js";import"./Tent-BkCeZCIS.js";import"./IllustratedMessage-CF10ZEVU.js";import"./sapIllus-Dialog-Tent-C6CWEzSF.js";import"./sapIllus-Scene-Tent-2yKp567E.js";import"./sapIllus-Spot-Tent-B36x4esc.js";import"./sapIllus-Dot-Tent-D2csHWSQ.js";import"./ListItemCustom-4RY6Qin6.js";function p(t){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",strong:"strong",...l(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(h,{of:u}),`
+`,o.jsx(d,{since:"0.9.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"example",children:"Example"}),`
+`,o.jsx(n,{of:i}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{of:i}),`
+`,o.jsx(e.h2,{id:"handling-drag-and-drop-of-files",children:"Handling drag and drop of files"}),`
+`,o.jsxs(e.p,{children:["Per default, files can be added by dragging them into the ",o.jsx(e.code,{children:"UploadCollection"}),` component.
+To use this functionality the `,o.jsx(e.code,{children:"onDrop"})," handler has to be manually adjusted.",o.jsx("br",{}),`
+`,o.jsx(e.strong,{children:"Note:"})," Use ",o.jsx(e.code,{children:"event.preventDefault()"})," in the ",o.jsx(e.code,{children:"onDrop"})," handler, otherwise some browsers will open some files in a different tab."]}),`
+`,o.jsxs(e.p,{children:["This example shows, how to dynamically render a ",o.jsx(e.code,{children:"UploadCollectionItem"})," by dropping files into the ",o.jsx(e.code,{children:"UploadCollection"}),":"]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`import { useState } from 'react';
+import { Icon, Text, UploadCollection, UploadCollectionItem } from '@ui5/webcomponents-react';
+
+export const UploadCollectionComponent = () => {
+  const [children, setChildren] = useState([]);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    files.forEach((file) => {
+      setChildren((prev) => [
+        ...prev,
+        <UploadCollectionItem file={file} fileName={file.name} thumbnail={<Icon name="document" />}>
+          <Text>Uploaded On: Right now</Text>
+        </UploadCollectionItem>
+      ]);
+    });
+  };
+  return <UploadCollection onDrop={handleDrop}>{children}</UploadCollection>;
+};
+`})}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(e.h2,{id:"simulate-upload",children:"Simulate Upload"}),`
+`,o.jsx(n,{of:C}),`
+`,o.jsx(e.h3,{id:"code",children:"Code"}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-jsx",children:`const SimulateUploadComponent = () => {
+  const [children, setChildren] = useState([
+    <UploadCollectionItem
+      key={'0'}
+      file={null}
+      fileName={'file-name.txt'}
+      uploadState={UploadState.Ready}
+      thumbnail={
+        <img src="https://raw.githubusercontent.com/SAP/ui5-webcomponents/main/docs/images/UI5_logo_water.png" />
+      }
+    >
+      <Text>Uploaded by: Susanne Schmitt · Uploaded On: 2019-04-20</Text>
+    </UploadCollectionItem>
+  ]);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    files.forEach((file) => {
+      setChildren((prev) => [
+        ...prev,
+        <UploadCollectionItem
+          key={file.name}
+          file={file}
+          fileName={file.name}
+          uploadState={UploadState.Ready}
+          thumbnail={<Icon name={documentIcon} />}
+        >
+          <Text>{\`Last modified: \${file.lastModifiedDate} · Size: \${(file.size / 1000).toFixed(2)}KB\`}</Text>
+        </UploadCollectionItem>
+      ]);
+    });
+  };
+  const simulateUpload = () => {
+    if (children.length > 0) {
+      Children.forEach(children, (child, index) => {
+        if (child.props.uploadState === UploadState.Ready) {
+          let progress = 0;
+          const recTimeout = () => {
+            setTimeout(
+              () => {
+                progress += Math.floor(Math.random() * 4) + 1;
+                setChildren((prev) => {
+                  const updatedChildren = [...prev];
+                  updatedChildren[index] = cloneElement(prev[index], {
+                    uploadState: UploadState.Uploading,
+                    progress: Math.min(progress, 100)
+                  });
+                  return updatedChildren;
+                });
+                if (progress < 100) {
+                  recTimeout();
+                } else {
+                  setChildren((prev) => {
+                    const updatedChildren = [...prev];
+                    updatedChildren[index] = cloneElement(prev[index], {
+                      uploadState: UploadState.Complete
+                    });
+                    return updatedChildren;
+                  });
+                }
+              },
+              Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+            );
+          };
+          recTimeout();
+        }
+      });
+    }
+  };
+  return (
+    <UploadCollection
+      onDrop={handleDrop}
+      header={
+        <FlexBox alignItems={FlexBoxAlignItems.Center}>
+          <Title>Start upload:</Title>
+          <Button onClick={simulateUpload}>Upload!</Button>
+        </FlexBox>
+      }
+    >
+      {children}
+    </UploadCollection>
+  );
+};
+`})}),`
+`,o.jsx(f,{children:c}),`
+`,o.jsx(e.h2,{id:"uploadcollectionitem",children:"UploadCollectionItem"}),`
+`,o.jsx(x,{of:r}),`
+`,o.jsx(m,{of:r}),`
+`,o.jsx(s,{})]})}function ye(t={}){const{wrapper:e}={...l(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(p,{...t})}):p(t)}export{ye as default};
