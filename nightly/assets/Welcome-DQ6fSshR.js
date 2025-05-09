7dfb6c8f519917462dc0d32037b68eea0b040653
Lukas742 <lukas.harbarth@sap.com>
2025-05-09 15:02:56 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@7243b5f2b1bd856cd6bf16a7313bf12f0647e27f 🚀

---
+++ nightly/assets/Welcome-DQ6fSshR.js
@@ -0,0 +1,136 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as f}from"./index-B5Ip8tK9.js";import{M as w}from"./index-BE1ha5Vr.js";import{M as l}from"./index-aiOo9W_a.js";import"./index-BFvZhBmE.js";import"./TagDesign-Ga50IS6u.js";import{r as u}from"./index-U0ga5oGA.js";import"./index-B8m469-U.js";import{F as y}from"./CommandsAndQueries-CtVd5Qd9.js";import"./Button-DcNvBpEn.js";import"./jsx-runtime-CkEx_Gfk.js";import"./copy-DmqZtkbO.js";import{T as v}from"./TableOfContent-BG9P-NK7.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-xNG2nZLh.js";import{i as k}from"./slim-arrow-down-P4GuIPIC.js";import{i as C}from"./slim-arrow-right-BP05c3XT.js";import{T as i}from"./ThemingParameters-BB3UHRT7.js";import"./CustomElementsScope-CjZbIYTD.js";import{B as T}from"./index-dNG4P2z4.js";import{L as P}from"./index-D9S9JAEE.js";import"./iframe-CaUN4hfq.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Icon-BexaehQh.js";import"./Keys-B3oBAe0t.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./decline-zF6-_htN.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-a8ApNaQO.js";import"./information-DmA-zxas.js";import"./sys-enter-2-DU2G_wE6.js";import"./sys-enter-2-CbaoLfaf.js";import"./alert-CeIMTDH_.js";import"./i18n-defaults-B158X0Y5.js";import"./index-BVR6w1bU.js";import"./preview-Drcufdhz.js";import"./DocsRenderer-CFRXHY34-CVFOjlUS.js";import"./react-18-SRpBHcoT.js";import"./Popover-eNK7pDla.js";import"./PopupsCommon.css-DXmEAn4P.js";import"./FocusableElements-DDPXQQfz.js";import"./isElementHidden-B4HqmI2E.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DS2o_9u5.js";import"./MediaRange-Bnzt26Xb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./index-Dmm22w00.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DUe1LpVj.js";import"./index-Bjofktik.js";import"./Label-BrFTKjrK.js";import"./index-D_lPDdUa.js";import"./index-BxLcq94m.js";import"./Text-CNWDrMdn.js";import"./willShowContent-CZcfsNXp.js";import"./Link-CBPPqDB9.js";import"./addCustomCSSWithScoping-B73Gg2hE.js";import"./index-CD5x5g4B.js";import"./BusyIndicator-vKEUjaWy.js";import"./index-8fyqcNlw.js";import"./index-DOaWR8S5.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-BtfkId0O.js";import"./index-DldzXScy.js";import"./I18nStore-tbyxgrjv.js";import"./Tooltips-DOGhZSxR.js";import"./EventProvider-ChtD9rRX.js";import"./slim-arrow-down-CpKeKmlD.js";const b={"0.8.0":"0.4.0","0.9.0":"0.4.0","0.11.0":"0.4.0","0.12.0":"0.4.0","1.0.0-rc.1":"0.4.0","1.0.0-rc.2":"0.4.0","1.0.0-rc.5":"0.7.0","1.0.0-rc.6":"0.9.0","1.0.0-rc.7":"0.9.0","1.0.0-rc.8":"0.10.0","1.0.0-rc.9":"0.11.0","1.0.0-rc.10":"0.12.0","1.0.0-rc.11":"0.13.0","1.0.0-rc.12":"0.14.0","1.0.0-rc.13":"0.15.0","1.0.0-rc.14":"0.17.0","1.0.0-rc.15":"0.18.0","1.0.0-rc.16":"0.20.0","1.0.0":"0.20.0","1.1.0":"0.21.0","1.2.0":"0.22.0","1.3.0":"0.23.0","1.4.0":"0.25.0","1.5.0":"0.26.0","1.6.0":"0.27.0","1.7.1":"1.0.1","1.8.0":"1.1.0","1.9.0":"1.3.0","1.9.3":"1.7.0","1.10.3":"1.8.0","1.11.0":"1.10.0","1.12.0":"1.13.0","1.13.0":"1.15.0","1.14.0":"1.16.0","1.15.0":"1.17.0","1.16.0":"1.18.0","1.17.0":"1.19.0","1.18.0":"1.20.0","1.19.0":"1.22.0","1.20.0":"1.23.0","1.21.0":"1.24.0","1.22.0":"1.25.0","1.23.1":"1.26.0","1.24.0":"1.27.0","2.1.2":"2.0.0","2.2.0":"2.1.0","2.3.0":"2.2.0","2.4.0":"2.4.0","2.5.0":"2.5.0","2.6.0":"2.6.2","2.7.0":"2.7.0","2.8.0":"2.8.0","2.9.0":"2.9.0","2.10.0":"2.10.0"},x=Object.values(b);function S(){const[o,n]=u.useState(!1);return e.jsxs(e.Fragment,{children:[e.jsxs(l,{hideCloseButton:!0,children:["Starting with version ",e.jsx("code",{children:"2.4.0"})," of ",e.jsx("code",{children:"@ui5/webcomponents-react"}),", all packages now align their"," ",e.jsx("b",{children:"minor"})," version with ",e.jsx("code",{children:"@ui5/webcomponents"}),"!"]}),e.jsx("p",{children:e.jsxs("b",{children:["In order to allow patching releases of UI5 Web Components by yourself, ",e.jsx("code",{children:"@ui5/webcomponents"})," and  ",e.jsx("code",{children:"@ui5/webcomponents-fiori"})," are peer dependencies of ",e.jsx("code",{children:"@ui5/webcomponents-react"}),". The following table gives an overview of the required peer dependencies per version:"]})}),e.jsxs("table",{children:[e.jsx("thead",{children:e.jsxs("tr",{children:[e.jsxs("th",{children:[e.jsx("code",{children:"@ui5/webcomponents-react"})," version"]}),e.jsxs("th",{children:[e.jsx("code",{children:"@ui5/webcomponents"})," & ",e.jsx("code",{children:"@ui5/webcomponents-fiori"})," version"]})]})}),e.jsx("tbody",{children:Object.entries(b).map(([p,t])=>{if(t.startsWith("0"))return null;const g=x.findIndex(r=>r===t),d=x[g+1],s=parseInt(t.split(".")[0]),h=parseInt(t.split(".")[1]),m=s===1&&h===27,c=[m?t:`~${t}`];if(d){const r=parseInt(d.split(".")[1]);for(let a=h+1;a<r;a++)c.push(`~${s}.${a}.0`)}return e.jsxs(u.Fragment,{children:[t==="1.0.1"&&e.jsx("tr",{children:e.jsxs("td",{colSpan:2,children:[e.jsx(T,{"data-ui5-compact-size":!0,icon:o?k:C,design:"Transparent",onClick:()=>{n(r=>!r)}}),e.jsx("span",{style:{fontSize:i.sapFontLargeSize,fontFamily:i.sapFontBoldFamily,marginInlineStart:"0.5rem"},children:"Version 1"})]})}),s===1&&o&&e.jsxs("tr",{children:[e.jsxs("td",{children:[m?"^":"",c.join(", ")]}),e.jsxs("td",{children:["~",p]})]}),t==="2.0.0"&&e.jsx("tr",{children:e.jsx("td",{colSpan:2,children:e.jsx("span",{style:{fontSize:i.sapFontLargeSize,fontFamily:i.sapFontBoldFamily,marginInlineEnd:"0.5rem"},children:"Version 2"})})}),s===2&&e.jsxs("tr",{children:[e.jsx("td",{children:c.join(", ")}),e.jsxs("td",{children:["~",p]})]})]},t)})})]})]})}function j(o){const n={a:"a",code:"code",h1:"h1",h2:"h2",h3:"h3",h4:"h4",img:"img",li:"li",p:"p",pre:"pre",strong:"strong",ul:"ul",...f(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(w,{title:"Getting Started"}),`
+`,e.jsx(n.h1,{id:"ui5-web-components-for-react",children:"UI5 Web Components for React"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"https://github.com/SAP/ui5-webcomponents-react/actions?query=workflow:%22build%22",rel:"nofollow",children:e.jsx(n.img,{src:"https://github.com/SAP/ui5-webcomponents-react/workflows/build/badge.svg",alt:""})}),`
+`,e.jsx(n.a,{href:"https://coveralls.io/github/SAP/ui5-web",rel:"nofollow",children:e.jsx(n.img,{src:"https://coveralls.io/repos/github/SAP/ui5-webcomponents-react/badge.svg",alt:""})}),`
+`,e.jsx(n.a,{href:"https://ui5-slack-invite.cfapps.eu10.hana.ondemand.com/",rel:"nofollow",children:e.jsx(n.img,{src:"https://badgen.net/badge/slack/Join%20OpenUI5%20Slack%20workspace/blue?icon=slack",alt:""})}),`
+`,e.jsx(n.a,{href:"https://openui5.slack.com/archives/CSQEJ2J04",rel:"nofollow",children:e.jsx(n.img,{src:"https://badgen.net/badge/slack/webcomponents-react/orange?icon=slack",alt:""})}),`
+`,e.jsx(n.a,{href:"https://github.com/prettier/prettier",rel:"nofollow",children:e.jsx(n.img,{src:"https://badgen.net/badge/code%20style/prettier?color=pink",alt:""})}),`
+`,e.jsx(n.a,{href:"https://lernajs.io/",rel:"nofollow",children:e.jsx(n.img,{src:"https://badgen.net/badge/maintained%20with/lerna?color=purple",alt:""})}),`
+`,e.jsx(n.a,{href:"https://github.com/SAP/ui5-webcomponents-react/stargazers",rel:"nofollow",children:e.jsx(n.img,{src:"https://badgen.net/github/stars/SAP/ui5-webcomponents-react?icon=github&color=black",alt:""})})]}),`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://github.com/SAP/ui5-webcomponents-react",rel:"nofollow",children:"Open Github Project"})}),`
+`,e.jsx(n.p,{children:`UI5 Web Components for React is a Fiori compliant React library built on top of the UI5 Web Components.
+With the help of UI5 Web Components for React, you can use UI5 Web Components as if they were native React components.
+In addition to that, UI5 Web Components for React is providing complex components and layouts on top of the UI5 Web Components.`}),`
+`,e.jsx(v,{}),`
+`,e.jsx(n.h2,{id:"requirements",children:"Requirements"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://www.npmjs.com/package/react",rel:"nofollow",children:"React"})," and ",e.jsx(n.a,{href:"https://www.npmjs.com/package/react-dom",rel:"nofollow",children:"React-DOM"})," (",e.jsx(n.strong,{children:"18.0.0 or higher"}),")"]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://nodejs.org/",rel:"nofollow",children:"Node.js"})," (",e.jsx(n.strong,{children:"LTS version"}),")"]}),`
+`,e.jsxs(n.li,{children:["If you're using ",e.jsx(n.a,{href:"https://www.typescriptlang.org/",rel:"nofollow",children:"TypeScript"})," we recommend version ",e.jsx(n.strong,{children:"4.7"})," or later."]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"ongoing-support-for-version-1x",children:"Ongoing Support for Version 1.x"}),`
+`,e.jsxs(n.p,{children:["We will continue to support version 1.x until ",e.jsx(n.strong,{children:"end of June 2025"}),", focusing on bug fixes to ensure continuity for our existing users."]}),`
+`,e.jsx(n.p,{children:e.jsx(n.a,{href:"https://sap.github.io/ui5-webcomponents-react/v1/?path=/docs/getting-started--docs",rel:"nofollow",children:"V1 Release Documentation"})}),`
+`,e.jsx(n.h2,{id:"getting-started",children:"Getting Started"}),`
+`,e.jsx(n.h3,{id:"tutorial",children:"Tutorial"}),`
+`,e.jsxs(n.p,{children:["You are new to UI5 Web Components for React and don't know where to start?",e.jsx("br",{}),`
+Then take a look at our `,e.jsx(n.a,{href:"https://developers.sap.com/mission.react-spa.html",rel:"nofollow",children:"Tutorial Mission"}),` at “SAP Developers”!
+There you get a first glimpse at how easy it is to create an Application with UI5 Web Components for React.`,e.jsx("br",{}),`
+In about an hour you will create a business dashboard from scratch and get familiar with some React basics in case you don't know them already.`]}),`
+`,e.jsx(n.h3,{id:"examples--templates",children:"Examples & Templates"}),`
+`,e.jsxs(n.p,{children:["You can find a curated list of project templates and examples on our ",e.jsx(n.a,{href:"?path=/docs/project-templates-examples--docs",children:"Project Templates & Examples page"}),"."]}),`
+`,e.jsxs(n.h3,{id:"add-ui5webcomponents-react-to-an-existing-app",children:["Add ",e.jsx(n.code,{children:"@ui5/webcomponents-react"})," to an existing app"]}),`
+`,e.jsxs(n.p,{children:["First of all, you need to add the ",e.jsx(n.code,{children:"@ui5/webcomponents-react"})," dependency to your project. Please also keep in mind installing the required peer dependencies:"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-sh",children:`npm install @ui5/webcomponents @ui5/webcomponents-react @ui5/webcomponents-fiori
+`})}),`
+`,e.jsx(S,{}),`
+`,e.jsx(l,{hideCloseButton:!0,children:e.jsxs(e.Fragment,{children:["Please note that if a ",e.jsx(n.code,{children:"@ui5/webcomponents"})," or ",e.jsx(n.code,{children:"@ui5/webcomponents-fiori"})," version doesn't start with a patch-version of 0 (e.g. ",e.jsx(n.code,{children:"~1.10.3"}),"), only the specified version is supported and the previous patch-versions will most likely not work with ",e.jsx(n.code,{children:"@ui5/webcomponents-react"}),"."]})}),`
+`,e.jsxs(n.p,{children:["In order to use ",e.jsx(n.code,{children:"@ui5/webcomponents-react"})," you have to wrap your application's root component into the ",e.jsx(n.code,{children:"ThemeProvider"}),".",e.jsx("br",{}),`
+You will find this component most likely in `,e.jsx(n.code,{children:"src/index.js"}),":"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { ThemeProvider } from '@ui5/webcomponents-react';
+...
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
+`})}),`
+`,e.jsxs(n.p,{children:["Then you are ready to use ",e.jsx(n.code,{children:"@ui5/webcomponents-react"})," and you can import the desired component(s) in your app:",e.jsx("br",{}),`
+For example, to use the `,e.jsx(n.code,{children:"Button"})," component you need to import it:"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { Button } from '@ui5/webcomponents-react'; // loads ui5-button wrapped in a ui5-webcomponents-react component
+`})}),`
+`,e.jsx(n.p,{children:"Then, you can use the Button in your app:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`<Button onClick={() => alert('Hello World!')}>Hello world!</Button>
+`})}),`
+`,e.jsx(n.h3,{id:"theming",children:"Theming"}),`
+`,e.jsxs(n.p,{children:["UI5 Web Components and UI5 Web Components for React are both coming with the ",e.jsx(n.code,{children:"sap_fiori_3"})," a.k.a. ",e.jsx(n.code,{children:"Quartz"})," and ",e.jsx(n.code,{children:"sap_horizon"})," Theme families built in."]}),`
+`,e.jsx(l,{hideCloseButton:!0,children:e.jsxs(e.Fragment,{children:["UI5 Web Components for React uses the theme configuration of UI5 Web Components. Please also have a look at their"," ",e.jsx(P,{href:"https://sap.github.io/ui5-webcomponents/docs/advanced/configuration/#theme",children:"documentation"}),"."]})}),`
+`,e.jsx(n.p,{children:"In case you want to change your applications' theme, you have to import a couple of modules:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-js",children:`import { setTheme } from '@ui5/webcomponents-base/dist/config/Theme';
+import '@ui5/webcomponents-react/dist/Assets';
+`})}),`
+`,e.jsxs(n.p,{children:["You can now change the Theme by calling ",e.jsx(n.code,{children:"setTheme"})," with a string parameter of the new theme.",e.jsx("br",{}),`
+Available Themes:`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"sap_horizon"})," (default)"]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:"sap_horizon_dark"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:"sap_horizon_hcb"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:"sap_horizon_hcw"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:"sap_fiori_3"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:"sap_fiori_3_dark"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:"sap_fiori_3_hcb"})}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsx(n.p,{children:e.jsx(n.code,{children:"sap_fiori_3_hcw"})}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h3,{id:"configure-compactcozy-setting",children:"Configure Compact/Cozy setting"}),`
+`,e.jsxs(n.p,{children:["UI5 Web Components supports ",e.jsx(n.code,{children:"Compact"})," and ",e.jsx(n.code,{children:"Cozy"})," mode. It is set to ",e.jsx(n.code,{children:"Cozy"})," by default. To enable ",e.jsx(n.code,{children:"Compact"}),", provide the css class ",e.jsx(n.code,{children:"ui5-content-density-compact"})," to any of your HTML elements and it apply compact size to all of its children."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-html",children:`<body class="ui5-content-density-compact">
+  ...
+</body>
+`})}),`
+`,e.jsx(n.h3,{id:"typescript",children:"TypeScript"}),`
+`,e.jsxs(n.p,{children:["UI5 Web Components for React supports ",e.jsx(n.a,{href:"https://www.typescriptlang.org/",rel:"nofollow",children:"TypeScript"}),", therefore we also provide type interfaces for event parameters, public methods of ",e.jsx(n.code,{children:"ui5-webcomponents"}),", and more."]}),`
+`,e.jsxs(n.p,{children:["You can find all available interfaces of the main package ",e.jsx(n.a,{href:"https://github.com/SAP/ui5-webcomponents-react/tree/main/packages/main/src/interfaces",rel:"nofollow",children:"here"})," and for charts ",e.jsx(n.a,{href:"https://github.com/SAP/ui5-webcomponents-react/tree/main/packages/charts/src/interfaces",rel:"nofollow",children:"here"}),"."]}),`
+`,e.jsx(n.h4,{id:"example",children:"Example"}),`
+`,e.jsx(n.p,{children:"Small app with a popover opened by clicking a button including type declarations:"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { useState, useRef } from 'react';
+import type { ButtonPropTypes, PopoverDomRef, PopoverPropTypes } from '@ui5/webcomponents-react';
+import { ThemeProvider, Button, Popover } from '@ui5/webcomponents-react';
+
+export default function App() {
+  const [open, setOpen] = useState<PopoverPropTypes['open']>(false);
+  const popoverRef = useRef<PopoverDomRef>(null);
+
+  const handleClick: ButtonPropTypes['onClick'] = (e) => {
+    setOpen(true);
+  };
+  const handleAfterClose: PopoverPropTypes['onClose'] = (e) => {
+    setOpen(false);
+  };
+
+  return (
+    <ThemeProvider>
+      <Button id="opener" onClick={handleClick}>
+        Open Popover
+      </Button>
+      <Popover ref={popoverRef} opener="opener" open={open} onClose={handleAfterClose}>
+        Content
+      </Popover>
+    </ThemeProvider>
+  );
+}
+`})}),`
+`,e.jsx(n.h3,{id:"feature-registration",children:"Feature Registration"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"@ui5/webcomponents"})," offers a variety of ",e.jsx(n.a,{href:"https://sap.github.io/ui5-webcomponents/docs/advanced/using-features/",rel:"nofollow",children:"feature (side-effect) imports"})," that are available with ",e.jsx(n.code,{children:"@ui5/webcomponents-react"}),` as well.
+To use them you have to make sure they are imported before any other imports!`]}),`
+`,e.jsx(n.h3,{id:"support",children:"Support"}),`
+`,e.jsxs(n.p,{children:["Feel free to open issues or ask us directly in the ",e.jsx(n.a,{href:"https://openui5.slack.com/archives/CSQEJ2J04",rel:"nofollow",children:e.jsx(n.code,{children:"#webcomponents-react"})}),` channel in the
+`,e.jsx(n.a,{href:"https://ui5-slack-invite.cfapps.eu10.hana.ondemand.com/",rel:"nofollow",children:"OpenUI5 Community Slack"}),".",e.jsx("br",{}),`
+Please note that you have to join this slack workspace via `,e.jsx(n.a,{href:"https://ui5-slack-invite.cfapps.eu10.hana.ondemand.com/",rel:"nofollow",children:"this link"})," if you are not part of it already."]}),`
+`,e.jsx(y,{})]})}function Qe(o={}){const{wrapper:n}={...f(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(j,{...o})}):j(o)}export{Qe as default};
