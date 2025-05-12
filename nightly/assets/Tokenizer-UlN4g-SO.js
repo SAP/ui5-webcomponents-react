6b3613b091472f598ab8c9a59f1cdcaf92b3cf06
Lukas742 <lukas.harbarth@sap.com>
2025-05-12 06:17:20 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@74ebb1900ce28afee66a8a268d0af18a289a1ba5 🚀

---
+++ nightly/assets/Tokenizer-UlN4g-SO.js
@@ -0,0 +1,65 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-B5Ip8tK9.js";import{A as s}from"./ArgTypesWithNote-D3EusEqh.js";import{C as a}from"./ControlsWithNote-Cf8ENyxV.js";import{D as c}from"./DocsHeader-C8-OKxdy.js";import{F as l}from"./CommandsAndQueries-qyVmuAXi.js";import"./index-ipRiiO2B.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-xNG2nZLh.js";import{M as d,C as n,a as x,D as u}from"./index-bhqQDHbq.js";import{C as h,D as r,W as k}from"./Tokenizer.stories-Ckyr_QJa.js";import{T as i}from"./index-DGqpcsY8.js";import{S as j}from"./SubcomponentsSection-BVy4lpQe.js";import"./index-aiOo9W_a.js";import"./jsx-runtime-CkEx_Gfk.js";import"./CustomElementsScope-CjZbIYTD.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-DcNvBpEn.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-BexaehQh.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DOGhZSxR.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B158X0Y5.js";import"./decline-zF6-_htN.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-a8ApNaQO.js";import"./information-DmA-zxas.js";import"./sys-enter-2-DU2G_wE6.js";import"./sys-enter-2-CbaoLfaf.js";import"./alert-CeIMTDH_.js";import"./TagDesign-Ga50IS6u.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./utils-B1FUGKw1.js";import"./main-DnREbZ5I.js";import"./index-CqFOiAdv.js";import"./Tag-DUc97KOd.js";import"./sys-help-2-i21-KsxS.js";import"./index-DmKfE7Xu.js";import"./Link-CBPPqDB9.js";import"./index-D_lPDdUa.js";import"./Popover-eNK7pDla.js";import"./PopupsCommon.css-DXmEAn4P.js";import"./FocusableElements-DDPXQQfz.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DS2o_9u5.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C8Wa9dhW.js";import"./copy-DmqZtkbO.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-C3tO4Ons.js";import"./I18nStore-tbyxgrjv.js";import"./useStylesheet-DUe1LpVj.js";import"./index-CvFb7rar.js";import"./index-DotsZLBN.js";import"./Label-BrFTKjrK.js";import"./index-dNG4P2z4.js";import"./index-BxLcq94m.js";import"./Text-CNWDrMdn.js";import"./preview-qmIG6d7s.js";import"./iframe-CTG73Y80.js";import"./DocsRenderer-CFRXHY34-D1pSMuWp.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-B73Gg2hE.js";import"./index-CD5x5g4B.js";import"./BusyIndicator-vKEUjaWy.js";import"./index-8fyqcNlw.js";import"./index-DOaWR8S5.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-BtfkId0O.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./Token-DGT2HhJ2.js";import"./ItemNavigation-B1w6o4UP.js";import"./ScrollEnablement-CfTOvVgV.js";import"./animate-JylhYHXj.js";import"./List-DiCPSBKg.js";import"./handleDrop-BdclM_pf.js";import"./DropIndicator-Pzk2J86G.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-B5I1cQuH.js";import"./TabbableElements-XzXYKyvZ.js";import"./ListItemStandard-DJnb6h3Q.js";import"./ListItemTemplate-HwqjeDC8.js";import"./edit-RWsE3aRn.js";import"./ListItemAdditionalText.css-DC55uWF7.js";import"./slim-arrow-right-BP05c3XT.js";import"./RadioButton-CVDuHlIL.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-BFEdJvwI.js";import"./accept-W9f43P7H.js";import"./ResponsivePopover-DdHf6Gx6.js";import"./Dialog-DYMZoMI2.js";import"./ResponsivePopoverCommon.css-BHUgyS0n.js";import"./Suggestions.css-bckqdeoL.js";function p(e){const t={code:"code",h2:"h2",pre:"pre",...m(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(d,{of:h}),`
+`,o.jsx(c,{subComponents:["Token"],experimental:!0}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(n,{of:r}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{of:r}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More examples"}),`
+`,o.jsx(t.h2,{id:"tokenizer-with-selection--delete-logic",children:"Tokenizer with selection & delete logic"}),`
+`,o.jsx(n,{of:k}),`
+`,o.jsxs("details",{children:[o.jsx("summary",{children:"Show Code"}),o.jsx(t.pre,{children:o.jsx(t.code,{className:"language-tsx",children:`function TokenizerComponent(props) {
+  const [countries, setCountries] = useState([
+    'Andorra',
+    'Bulgaria',
+    'Canada',
+    'Denmark',
+    'Estonia',
+    'Fiji',
+    'Ghana',
+    'India',
+    'Japan',
+    'Kenya',
+    'Luxembourg',
+    'Mexico',
+    'Nepal',
+    'Qatar',
+    'Uganda'
+  ]);
+  const [selectedCountries, setSelectedCountries] = useState<string[]>([]);
+  const handleTokenDelete: TokenizerPropTypes['onTokenDelete'] = (e) => {
+    const { tokens } = e.detail;
+    if (tokens) {
+      const tokensToDelete = tokens.map((token) => token.text);
+      setCountries((prev) => prev.filter((country) => !tokensToDelete.includes(country)));
+      setSelectedCountries([]);
+    }
+  };
+  const handleSelectionChange: TokenizerPropTypes['onSelectionChange'] = (e) => {
+    const { tokens } = e.detail;
+    if (tokens) {
+      const selectedTokens = tokens.map((token) => token.text);
+      setSelectedCountries(selectedTokens);
+    }
+  };
+  return (
+    <>
+      <Tokenizer {...props} onTokenDelete={handleTokenDelete} onSelectionChange={handleSelectionChange}>
+        {countries.map((country) => (
+          <Token key={country} text={country} />
+        ))}
+      </Tokenizer>
+      <br />
+      <div style={{ display: 'flex', gap: '0.2rem' }}>
+        <Label showColon>Selected countries</Label>
+        <Text>{selectedCountries.join(', ')}</Text>
+      </div>
+    </>
+  );
+}
+`})})]}),`
+`,o.jsx(x,{children:j}),`
+`,o.jsx(t.h2,{id:"token",children:"Token"}),`
+`,o.jsx(u,{of:i}),`
+`,o.jsx(s,{of:i}),`
+`,o.jsx(l,{})]})}function Ct(e={}){const{wrapper:t}={...m(),...e.components};return t?o.jsx(t,{...e,children:o.jsx(p,{...e})}):p(e)}export{Ct as default};
