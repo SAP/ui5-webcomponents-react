915f315a2aa728f65d9749a632e78c54bb55d390
MarcusNotheis <marcus.notheis@sap.com>
2025-05-13 13:01:25 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@267ed9596c2f9d26c0d7bfd7c33ba86a2f008cb4 🚀

---
+++ nightly/assets/PieChart-DoBQYpRS.js
@@ -0,0 +1,22 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as n}from"./index-B5Ip8tK9.js";import{M as s,C as i}from"./index-Df9Cqovj.js";import"./index-CV9953r4.js";import"./index-7kixX6G6.js";import{C as e}from"./ControlsWithNote-Ba5bi_UX.js";import{D as a}from"./DocsHeader-DmCtrlDe.js";import{F as h}from"./CommandsAndQueries-Cw7A7NDo.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-xNG2nZLh.js";import{M as x}from"./TooltipConfig-BV7IrIyc.js";import{C as l,D as m,W as d,L as j,a as f,H as c,b as C,c as u}from"./PieChart.stories-DV0DQM67.js";import{M}from"./LegendConfig-D5VPB2tw.js";import"./iframe-CAqVT8bX.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-CkEx_Gfk.js";import"./CustomElementsScope-CjZbIYTD.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-DcNvBpEn.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-BexaehQh.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DOGhZSxR.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B158X0Y5.js";import"./decline-zF6-_htN.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-a8ApNaQO.js";import"./information-DmA-zxas.js";import"./sys-enter-2-DU2G_wE6.js";import"./sys-enter-2-CbaoLfaf.js";import"./alert-CeIMTDH_.js";import"./TagDesign-Ga50IS6u.js";import"./utils-Dn8Ud13X.js";import"./main-DnREbZ5I.js";import"./index-B_D8PZjO.js";import"./Tag-DUc97KOd.js";import"./sys-help-2-i21-KsxS.js";import"./index-D9S9JAEE.js";import"./Link-CBPPqDB9.js";import"./index-PlOiYqrR.js";import"./Popover-eNK7pDla.js";import"./PopupsCommon.css-DXmEAn4P.js";import"./FocusableElements-DDPXQQfz.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DS2o_9u5.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C8Wa9dhW.js";import"./copy-DmqZtkbO.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-DIAjmy42.js";import"./I18nStore-tbyxgrjv.js";import"./useStylesheet-DUe1LpVj.js";import"./index-Cj6ai3XZ.js";import"./index-Bjofktik.js";import"./Label-BrFTKjrK.js";import"./index-dNG4P2z4.js";import"./index-BxLcq94m.js";import"./Text-CNWDrMdn.js";import"./preview-C3yESBk-.js";import"./DocsRenderer-CFRXHY34-BnrWvW-W.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-B73Gg2hE.js";import"./index-CD5x5g4B.js";import"./BusyIndicator-vKEUjaWy.js";import"./index-8fyqcNlw.js";import"./index-DOaWR8S5.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-BtfkId0O.js";import"./DemoProps-BerORI7p.js";import"./PieChart-4r3IX2E4.js";import"./staticProps-BDmBA1JB.js";import"./ChartContainer-BUarOKaG.js";import"./tiny-invariant-CopsF_GD.js";import"./index-D49NKxue.js";import"./ThemingParameters-BB3UHRT7.js";import"./useOnClickInternal-Btb9191g.js";import"./Placeholder-BTdEi6Ks.js";import"./react-content-loader.es-C9Vmu4FD.js";function p(r){const t={h2:"h2",h3:"h3",...n(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:l}),`
+`,o.jsx(a,{isChart:!0}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:m}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(e,{of:m}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,o.jsx(i,{of:d}),`
+`,o.jsx(t.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(i,{of:j}),`
+`,o.jsx(t.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,o.jsx(i,{of:f}),`
+`,o.jsx(t.h3,{id:"hide-labels",children:"Hide labels"}),`
+`,o.jsx(i,{of:c}),`
+`,o.jsx(x,{of:C}),`
+`,o.jsx(M,{of:u}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(h,{})]})}function pt(r={}){const{wrapper:t}={...n(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(p,{...r})}):p(r)}export{pt as default};
