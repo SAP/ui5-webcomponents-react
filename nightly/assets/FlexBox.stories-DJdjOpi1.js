1aad3ab78b5ea2adc84232b4b6ddd757f1793157
Lukas742 <lukas.harbarth@sap.com>
2025-05-14 07:51:21 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@6217fec6034e15b32a85cb8e525f61f6bfa505f6 🚀

---
+++ nightly/assets/FlexBox.stories-DJdjOpi1.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{e as m}from"./employee-rWykNOj7.js";import"./index-BvO6PusU.js";import{b as i,a as p,c,d as x,F as s}from"./index-Cj6ai3XZ.js";import{T as I}from"./index-JIX6lJE2.js";import{I as d}from"./index-DtQjvaZb.js";import{L as o}from"./index-Bjofktik.js";import{I as u}from"./index-CQSfprc1.js";const f={title:"Layouts & Floorplans / FlexBox",component:s,argTypes:{as:{control:{disable:!0}}},args:{justifyContent:x.Start,alignItems:c.Stretch,direction:p.Row,wrap:i.NoWrap}},t={render(l){return e.jsxs(s,{...l,children:[e.jsx(I,{children:"Item 1"}),e.jsx(d,{name:m,title:"Item 2"}),e.jsx(o,{children:"Item 3"}),e.jsx(u,{placeholder:"Item 4"}),e.jsx(o,{children:"Item 5"}),e.jsx(o,{children:"Item 6"})]})}};var r,a,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render(args) {
+    return <FlexBox {...args}>
+        <Title>Item 1</Title>
+        <Icon name={employeeIcon} title="Item 2" />
+        <Label>Item 3</Label>
+        <Input placeholder="Item 4" />
+        <Label>Item 5</Label>
+        <Label>Item 6</Label>
+      </FlexBox>;
+  }
+}`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const b=["Default"],T=Object.freeze(Object.defineProperty({__proto__:null,Default:t,__namedExportsOrder:b,default:f},Symbol.toStringTag,{value:"Module"}));export{T as C,t as D};
