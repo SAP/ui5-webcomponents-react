869d4e08f574fafde9b397f0ca17c2b24ca1dfec
Lukas742 <lukas.harbarth@sap.com>
2025-05-15 15:41:05 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@d36a749bae9939293d52e329a9f2857218b35958 🚀

---
+++ nightly/assets/ColumnChart-CLKx1mVH.js
@@ -0,0 +1,40 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-B5Ip8tK9.js";import{M as p,C as i}from"./index-CIqXG3DN.js";import"./index-aiOo9W_a.js";import"./index-CA0ZeyDT.js";import{C as s}from"./ControlsWithNote-1StwDLIl.js";import{D as a}from"./DocsHeader-fxFuXfUT.js";import{F as h}from"./CommandsAndQueries-gklJ_u_0.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-xNG2nZLh.js";import{M as c}from"./TooltipConfig-CUymxgXf.js";import{M as d}from"./LegendConfig-DxDMuUUR.js";import{M as l}from"./NormalizedStackedChart-B1K8Timw.js";import{C as x,D as e,W as j,a as f,b as u,c as C,d as g,L as W,e as M,f as D,g as L,h as b}from"./ColumnChart.stories-BUSn1ef6.js";import"./iframe-CTG5RYNs.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-CkEx_Gfk.js";import"./CustomElementsScope-CjZbIYTD.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-DcNvBpEn.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-BexaehQh.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DOGhZSxR.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B158X0Y5.js";import"./decline-zF6-_htN.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-a8ApNaQO.js";import"./information-DmA-zxas.js";import"./sys-enter-2-DU2G_wE6.js";import"./sys-enter-2-CbaoLfaf.js";import"./alert-CeIMTDH_.js";import"./TagDesign-Ga50IS6u.js";import"./utils-DrvXC6M7.js";import"./main-DnREbZ5I.js";import"./index-DVwdyzd9.js";import"./Tag-DUc97KOd.js";import"./sys-help-2-i21-KsxS.js";import"./index-D9S9JAEE.js";import"./Link-CBPPqDB9.js";import"./index-D_lPDdUa.js";import"./Popover-eNK7pDla.js";import"./PopupsCommon.css-DXmEAn4P.js";import"./FocusableElements-DDPXQQfz.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DS2o_9u5.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C8Wa9dhW.js";import"./copy-DmqZtkbO.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-B2Lr3OgG.js";import"./I18nStore-tbyxgrjv.js";import"./useStylesheet-DUe1LpVj.js";import"./index-Ds9f0mzO.js";import"./index-Bjofktik.js";import"./Label-BrFTKjrK.js";import"./index-dNG4P2z4.js";import"./index-BxLcq94m.js";import"./Text-CNWDrMdn.js";import"./preview-DDCMMdq5.js";import"./DocsRenderer-CFRXHY34-HGS6ds9R.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-B73Gg2hE.js";import"./index-CD5x5g4B.js";import"./BusyIndicator-vKEUjaWy.js";import"./index-8fyqcNlw.js";import"./index-DOaWR8S5.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-BtfkId0O.js";import"./DemoProps-BerORI7p.js";import"./ThemingParameters-BB3UHRT7.js";import"./staticProps-BbLHqO5C.js";import"./ChartContainer-_q_L4NdQ.js";import"./tiny-invariant-CopsF_GD.js";import"./index-D49NKxue.js";import"./useCancelAnimationFallback-BjKYhsIB.js";import"./YAxisTicks-Cu5bPvWv.js";import"./debounce-D7W5PopO.js";import"./useTooltipFormatter-CaqLaKxJ.js";import"./ChartDataLabel-BX-BJS1f.js";import"./useLongestYAxisLabel-POfJkzv5.js";import"./useOnClickInternal-Btb9191g.js";import"./react-content-loader.es-C9Vmu4FD.js";import"./useIsRTL-BwLW7KlC.js";import"./directionChange-BzugKGnv.js";function n(t){const r={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...m(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:x}),`
+`,o.jsx(a,{isChart:!0}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:e}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(s,{of:e}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(r.h3,{id:"with-custom-color",children:"With custom color"}),`
+`,o.jsx(i,{of:j}),`
+`,o.jsx(r.h3,{id:"with-secondary-dimension",children:"With Secondary Dimension"}),`
+`,o.jsx(i,{of:f}),`
+`,o.jsx(r.h3,{id:"with-stacks",children:"With Stacks"}),`
+`,o.jsx(i,{of:u}),`
+`,o.jsx(l,{of:C}),`
+`,o.jsx(r.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,o.jsx(i,{of:g}),`
+`,o.jsx(r.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(i,{of:W}),`
+`,o.jsx(r.h3,{id:"with-reference-line",children:"With Reference Line"}),`
+`,o.jsxs(r.p,{children:["You can set a reference line to any value by using the ",o.jsx(r.code,{children:"referenceLine"})," ",o.jsx(r.code,{children:"chartConfig"})," property. ",o.jsx(r.code,{children:"referenceLine"})," accepts all properties described ",o.jsx(r.a,{href:"https://recharts.org/en-US/api/ReferenceLine",rel:"nofollow",children:"here"}),", but it's recommended to only use ",o.jsx(r.code,{children:"label"}),",",o.jsx(r.code,{children:"value"})," and ",o.jsx(r.code,{children:"color"})," to preserve the intended design."]}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-jsx",children:`<ColumnChart
+  {...props}
+  chartConfig={{
+    referenceLine: {
+      color: 'red',
+      label: 'MAX',
+      value: 650
+    }
+  }}
+/>
+`})}),`
+`,o.jsx(i,{of:M}),`
+`,o.jsx(r.h2,{id:"with-highlighted-measures",children:"With Highlighted Measures"}),`
+`,o.jsx(i,{of:D}),`
+`,o.jsxs(r.p,{children:[o.jsx(c,{of:L}),";"]}),`
+`,o.jsx(d,{of:b}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(h,{})]})}function ur(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(n,{...t})}):n(t)}export{ur as default};
---
+++ nightly/assets/Modals-BnfIGrR1.js
@@ -0,0 +1,97 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as d}from"./index-B5Ip8tK9.js";import{M as c,C as o,A as n}from"./index-CIqXG3DN.js";import"./index-aiOo9W_a.js";import"./index-CA0ZeyDT.js";import"./TagDesign-Ga50IS6u.js";import"./index-U0ga5oGA.js";import"./index-B8m469-U.js";import{D as l}from"./DocsHeader-fxFuXfUT.js";import{F as h}from"./CommandsAndQueries-gklJ_u_0.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-xNG2nZLh.js";import{a as x}from"./index-eOWEK8_G.js";import{C as j,D as p,P as a,R as m,M as u,a as g,T as b}from"./Modals.stories-DNkUd_rD.js";import{M as f}from"./index-hkEAFh6P.js";import{P as i}from"./index-CHeCojj8.js";import{D as w}from"./index-CeuB081l.js";import{P as M}from"./index-D_lPDdUa.js";import{R as P}from"./index-BiHE_w2X.js";import{M as v}from"./index-Doqm0zq5.js";import{T as y}from"./index-haAT3RjO.js";import"./iframe-CTG5RYNs.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-CkEx_Gfk.js";import"./CustomElementsScope-CjZbIYTD.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-DcNvBpEn.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-BexaehQh.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DOGhZSxR.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B158X0Y5.js";import"./decline-zF6-_htN.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-a8ApNaQO.js";import"./information-DmA-zxas.js";import"./sys-enter-2-DU2G_wE6.js";import"./sys-enter-2-CbaoLfaf.js";import"./alert-CeIMTDH_.js";import"./index-BVR6w1bU.js";import"./copy-C8Wa9dhW.js";import"./copy-DmqZtkbO.js";import"./clsx-B-dksMZM.js";import"./utils-DrvXC6M7.js";import"./main-DnREbZ5I.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-B2Lr3OgG.js";import"./I18nStore-tbyxgrjv.js";import"./useStylesheet-DUe1LpVj.js";import"./index-Ds9f0mzO.js";import"./index-Bjofktik.js";import"./Label-BrFTKjrK.js";import"./index-D9S9JAEE.js";import"./Link-CBPPqDB9.js";import"./index-dNG4P2z4.js";import"./index-BxLcq94m.js";import"./Text-CNWDrMdn.js";import"./preview-DDCMMdq5.js";import"./DocsRenderer-CFRXHY34-HGS6ds9R.js";import"./react-18-SRpBHcoT.js";import"./Popover-eNK7pDla.js";import"./PopupsCommon.css-DXmEAn4P.js";import"./FocusableElements-DDPXQQfz.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DS2o_9u5.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./addCustomCSSWithScoping-B73Gg2hE.js";import"./index-CD5x5g4B.js";import"./BusyIndicator-vKEUjaWy.js";import"./index-8fyqcNlw.js";import"./index-DOaWR8S5.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-BtfkId0O.js";import"./ValueState-Bg0UWejw.js";import"./sys-help-2-i21-KsxS.js";import"./i18n-defaults-BfrFKt2A.js";import"./index-JIX6lJE2.js";import"./index-DtQjvaZb.js";import"./index-p6jpgleG.js";import"./MenuItem-BlqhXiFh.js";import"./nav-back-BNINg4v4.js";import"./ItemNavigation-B1w6o4UP.js";import"./ListItemTemplate-HwqjeDC8.js";import"./edit-RWsE3aRn.js";import"./List-DiCPSBKg.js";import"./handleDrop-BdclM_pf.js";import"./DropIndicator-Pzk2J86G.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-B5I1cQuH.js";import"./TabbableElements-XzXYKyvZ.js";import"./ListItemAdditionalText.css-DC55uWF7.js";import"./slim-arrow-right-BP05c3XT.js";import"./RadioButton-CVDuHlIL.js";import"./CheckBox-BFEdJvwI.js";import"./accept-W9f43P7H.js";import"./ResponsivePopover-DdHf6Gx6.js";import"./Dialog-DYMZoMI2.js";import"./Panel-D8CXC8dN.js";import"./slideUp-DNFkFHG8.js";import"./animate-JylhYHXj.js";import"./AnimationMode-DCkw60w9.js";import"./Menu-qX8_xrCV.js";import"./ListItemCustom-ZuNo6bnp.js";function t(s){const r={code:"code",em:"em",h2:"h2",h4:"h4",p:"p",pre:"pre",strong:"strong",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...d(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(f,{}),`
+`,e.jsx(c,{of:j}),`
+`,e.jsx(l,{since:"0.22.2"}),`
+`,e.jsx(r.h2,{id:"general-usage-information",children:"General Usage Information"}),`
+`,e.jsxs(r.p,{children:["Only one ",e.jsx(r.code,{children:"Modals"})," component (",e.jsx(r.code,{children:"<Modals />"}),") should be rendered for each application, otherwise multiple popovers or dialogs are displayed."]}),`
+`,e.jsxs(r.p,{children:["Example for mounting the ",e.jsx(r.code,{children:"Modals"})," component:"]}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-jsx",children:`import { Modals, ThemeProvider } from '@ui5/webcomponents-react';
+...
+const root = createRoot(document.getElementById("root"));
+root.render(
+        <ThemeProvider>
+          <Modals />
+          <App />
+        </ThemeProvider>
+);
+`})}),`
+`,e.jsx(r.h2,{id:"dialog",children:"Dialog"}),`
+`,e.jsx(o,{of:p}),`
+`,e.jsx(r.h4,{id:"usage",children:"Usage"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+const { ref, close } = Modals.showDialog(props, container);
+`})}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Parameters"})}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Parameter"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"props"})}),e.jsxs(r.td,{children:["All supported ",e.jsx(r.code,{children:"Dialog"})," props (see table below). ",e.jsx(r.code,{children:"open"})," will always be set to ",e.jsx(r.code,{children:"true"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.em,{children:e.jsx(r.code,{children:"container"})})}),e.jsxs(r.td,{children:["Optional container where the ",e.jsx(r.code,{children:"Dialog"})," should be mounted. Defaults to ",e.jsx(r.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Return Value"})}),`
+`,e.jsxs(r.p,{children:["The ",e.jsx(r.code,{children:"showDialog"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Property"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"ref"})}),e.jsxs(r.td,{children:["React ",e.jsx(r.code,{children:"RefObject"})," which can be used to get interact with the ",e.jsx(r.code,{children:"Dialog"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"close()"})}),e.jsxs(r.td,{children:["Convenience Method for closing the ",e.jsx(r.code,{children:"Dialog"}),"."]})]})]})]}),`
+`,e.jsx(i,{header:e.jsx(r.h4,{className:"noMargin",children:"Dialog Props"}),collapsed:!0,children:e.jsx(n,{of:w,exclude:["open"]})}),`
+`,e.jsx(r.h2,{id:"popover",children:"Popover"}),`
+`,e.jsx(o,{of:a}),`
+`,e.jsx(r.h4,{id:"usage-1",children:"Usage"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+const { ref, close } = Modals.showPopover(props, container);
+`})}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Parameters"})}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Parameter"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"props"})}),e.jsxs(r.td,{children:["All supported ",e.jsx(r.code,{children:"Popover"})," props (see table below).",e.jsx(r.code,{children:"open"})," will always be set to ",e.jsx(r.code,{children:"true"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.em,{children:e.jsx(r.code,{children:"container"})})}),e.jsxs(r.td,{children:["Optional container where the ",e.jsx(r.code,{children:"Popover"})," should be mounted. Defaults to ",e.jsx(r.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Return Value"})}),`
+`,e.jsxs(r.p,{children:["The ",e.jsx(r.code,{children:"showPopover"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Property"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"ref"})}),e.jsxs(r.td,{children:["React ",e.jsx(r.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(r.code,{children:"Popover"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"close()"})}),e.jsxs(r.td,{children:["Convenience Method for closing the ",e.jsx(r.code,{children:"Popover"}),"."]})]})]})]}),`
+`,e.jsx(i,{header:e.jsx(r.h4,{className:"noMargin",children:"Popover Props"}),collapsed:!0,children:e.jsx(n,{of:M,exclude:["open"]})}),`
+`,e.jsx(r.h2,{id:"responsivepopover",children:"ResponsivePopover"}),`
+`,e.jsx(o,{of:m}),`
+`,e.jsx(r.h4,{id:"usage-2",children:"Usage"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+const { ref, close } = Modals.showResponsivePopover(props, container);
+`})}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Parameters"})}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Parameter"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"props"})}),e.jsxs(r.td,{children:["All supported ",e.jsx(r.code,{children:"ResponsivePopover"})," props (see table below). ",e.jsx(r.code,{children:"open"})," will always be set to ",e.jsx(r.code,{children:"true"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.em,{children:e.jsx(r.code,{children:"container"})})}),e.jsxs(r.td,{children:["Optional container where the ",e.jsx(r.code,{children:"ResponsivePopover"})," should be mounted. Defaults to ",e.jsx(r.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Return Value"})}),`
+`,e.jsxs(r.p,{children:["The ",e.jsx(r.code,{children:"showResponsivePopover"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Property"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"ref"})}),e.jsxs(r.td,{children:["React ",e.jsx(r.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(r.code,{children:"ResponsivePopover"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"close()"})}),e.jsxs(r.td,{children:["Convenience Method for closing the ",e.jsx(r.code,{children:"ResponsivePopover"}),"."]})]})]})]}),`
+`,e.jsx(i,{header:e.jsx(r.h4,{className:"noMargin",children:"ResponsivePopover Props"}),collapsed:!0,children:e.jsx(n,{of:P,exclude:["open"]})}),`
+`,e.jsx(r.h2,{id:"menu",children:"Menu"}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"since 1.8.0"})}),`
+`,e.jsx(o,{of:u}),`
+`,e.jsx(r.h4,{id:"usage-3",children:"Usage"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+const { ref, close } = Modals.showMenu(props, container);
+`})}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Parameters"})}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Parameter"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"props"})}),e.jsxs(r.td,{children:["All supported ",e.jsx(r.code,{children:"Menu"})," props (see table below). ",e.jsx(r.code,{children:"open"})," will always be set to ",e.jsx(r.code,{children:"true"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.em,{children:e.jsx(r.code,{children:"container"})})}),e.jsxs(r.td,{children:["Optional container where the ",e.jsx(r.code,{children:"Menu"})," should be mounted. Defaults to ",e.jsx(r.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Return Value"})}),`
+`,e.jsxs(r.p,{children:["The ",e.jsx(r.code,{children:"Menu"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Property"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"ref"})}),e.jsxs(r.td,{children:["React ",e.jsx(r.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(r.code,{children:"Menu"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"close()"})}),e.jsxs(r.td,{children:["Convenience Method for closing the ",e.jsx(r.code,{children:"Menu"}),"."]})]})]})]}),`
+`,e.jsx(i,{header:e.jsx(r.h4,{className:"noMargin",children:"Menu Props"}),collapsed:!0,children:e.jsx(n,{of:v,exclude:["open"]})}),`
+`,e.jsx(r.h2,{id:"messagebox",children:"MessageBox"}),`
+`,e.jsx(o,{of:g}),`
+`,e.jsx(r.h4,{id:"usage-4",children:"Usage"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+const { ref, close } = Modals.showMessageBox(props, container);
+`})}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Parameters"})}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Parameter"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"props"})}),e.jsxs(r.td,{children:["All supported ",e.jsx(r.code,{children:"MessageBox"})," props (see table below). ",e.jsx(r.code,{children:"open"})," will always be set to ",e.jsx(r.code,{children:"true"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.em,{children:e.jsx(r.code,{children:"container"})})}),e.jsxs(r.td,{children:["Optional container where the ",e.jsx(r.code,{children:"MessageBox"})," should be mounted. Defaults to ",e.jsx(r.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Return Value"})}),`
+`,e.jsxs(r.p,{children:["The ",e.jsx(r.code,{children:"showMessageBox"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Property"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"ref"})}),e.jsxs(r.td,{children:["React ",e.jsx(r.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(r.code,{children:"MessageBox"}),"."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"close()"})}),e.jsxs(r.td,{children:["Convenience Method for closing the ",e.jsx(r.code,{children:"MessageBox"}),"."]})]})]})]}),`
+`,e.jsx(i,{header:e.jsx(r.h4,{className:"noMargin",children:"MessageBox Props"}),collapsed:!0,children:e.jsx(n,{of:x,exclude:["open"]})}),`
+`,e.jsx(r.h2,{id:"toast",children:"Toast"}),`
+`,e.jsx(o,{of:b}),`
+`,e.jsx(r.h4,{id:"usage-5",children:"Usage"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-typescript",children:`import { Modals } from '@ui5/webcomponents-react';
+
+const { ref } = Modals.showToast(props, container);
+`})}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Parameters"})}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Parameter"}),e.jsx(r.th,{children:"Description"})]})}),e.jsxs(r.tbody,{children:[e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"props"})}),e.jsxs(r.td,{children:["All supported ",e.jsx(r.code,{children:"Toast"})," props(see table below)."]})]}),e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.em,{children:e.jsx(r.code,{children:"container"})})}),e.jsxs(r.td,{children:["Optional container where the ",e.jsx(r.code,{children:"Toast"})," should be mounted.Defaults to ",e.jsx(r.code,{children:"document.body"}),"."]})]})]})]}),`
+`,e.jsx(r.p,{children:e.jsx(r.strong,{children:"Return Value"})}),`
+`,e.jsxs(r.p,{children:["The",e.jsx(r.code,{children:"showToast"})," method returns an object with the following properties:"]}),`
+`,e.jsxs(r.table,{children:[e.jsx(r.thead,{children:e.jsxs(r.tr,{children:[e.jsx(r.th,{children:"Property"}),e.jsx(r.th,{children:"Description"})]})}),e.jsx(r.tbody,{children:e.jsxs(r.tr,{children:[e.jsx(r.td,{children:e.jsx(r.code,{children:"ref"})}),e.jsxs(r.td,{children:["React ",e.jsx(r.code,{children:"RefObject"})," which can be used to interact with the ",e.jsx(r.code,{children:"Toast"}),"."]})]})})]}),`
+`,e.jsx(i,{header:e.jsx(r.h4,{className:"noMargin",children:"Toast Props "}),collapsed:!0,children:e.jsx(n,{of:y})}),`
+`,e.jsx("br",{}),`
+`,e.jsx(h,{})]})}function Br(s={}){const{wrapper:r}={...d(),...s.components};return r?e.jsx(r,{...s,children:e.jsx(t,{...s})}):t(s)}export{Br as default};
