b4b923806bed0da92218444b80134f20129b885b
Lukas Harbarth <lukas.harbarth@sap.com>
2025-05-16 11:56:59 +0200

    docs(AnalyticalTable): improve `width` & `minWidth` column property description

--- packages/main/src/components/AnalyticalTable/types/index.ts
+++ packages/main/src/components/AnalyticalTable/types/index.ts
@@ -418,15 +418,19 @@ export interface AnalyticalTableColumnDefinition {
    */
   cellLabel?: (param?: CellLabelParam) => string;
   /**
-   * Cell width, if not set the table will distribute all columns without a width evenly.
+   * Defines the column width. If not set the table will distribute all columns without a width evenly.
+   *
+   * __Note:__ Undercutting the `minWidth` value is not supported!
    */
   width?: number;
   /**
-   * Minimum width of the column, e.g. used for resizing.
+   * Minimum width of the column.
+   *
+   * @default: 60
    */
   minWidth?: number;
   /**
-   * Maximum with of the column, e.g. used for resizing.
+   * Maximum with of the column.
    */
   maxWidth?: number;
   /**
