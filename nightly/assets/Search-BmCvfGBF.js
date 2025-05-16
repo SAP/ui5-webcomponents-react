b47fca02fe501c83ff4e56f66f643ca46dd37021
Lukas742 <lukas.harbarth@sap.com>
2025-05-16 06:13:17 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@741ff5cbc34b471a063d265160bbd1cf3624737f 🚀

---
+++ nightly/assets/Search-BmCvfGBF.js
@@ -0,0 +1,47 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as m}from"./index-B5Ip8tK9.js";import{M as n,C as p}from"./index-C8j1a1wf.js";import"./index-CV9953r4.js";import"./index-BEtTPGE0.js";import{C as s}from"./ControlsWithNote-Dlihrsue.js";import{D as a}from"./DocsHeader-D1z4P3Uo.js";import{F as c}from"./CommandsAndQueries-Dc5LslcP.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-xNG2nZLh.js";import{C as h,D as e,W as l}from"./Search.stories-DvvgS6QR.js";import"./iframe-BUIjnSfL.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-CkEx_Gfk.js";import"./CustomElementsScope-CjZbIYTD.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-DcNvBpEn.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-BexaehQh.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DOGhZSxR.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B158X0Y5.js";import"./decline-zF6-_htN.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-a8ApNaQO.js";import"./information-DmA-zxas.js";import"./sys-enter-2-DU2G_wE6.js";import"./sys-enter-2-CbaoLfaf.js";import"./alert-CeIMTDH_.js";import"./TagDesign-Ga50IS6u.js";import"./utils-DLp8nVmU.js";import"./main-DnREbZ5I.js";import"./index-B_D8PZjO.js";import"./Tag-DUc97KOd.js";import"./sys-help-2-i21-KsxS.js";import"./index-D9S9JAEE.js";import"./Link-CBPPqDB9.js";import"./index-D_lPDdUa.js";import"./Popover-eNK7pDla.js";import"./PopupsCommon.css-DXmEAn4P.js";import"./FocusableElements-DDPXQQfz.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DS2o_9u5.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C8Wa9dhW.js";import"./copy-DmqZtkbO.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-CX5fbFNZ.js";import"./I18nStore-tbyxgrjv.js";import"./useStylesheet-DUe1LpVj.js";import"./index-Cj6ai3XZ.js";import"./index-Bjofktik.js";import"./Label-BrFTKjrK.js";import"./index-dNG4P2z4.js";import"./index-BxLcq94m.js";import"./Text-CNWDrMdn.js";import"./preview-DLSX0WQj.js";import"./DocsRenderer-CFRXHY34-CQQuXYXR.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-B73Gg2hE.js";import"./index-CD5x5g4B.js";import"./BusyIndicator-vKEUjaWy.js";import"./index-8fyqcNlw.js";import"./index-DOaWR8S5.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-BtfkId0O.js";import"./ListItemGroup-B5I1cQuH.js";import"./DropIndicator-Pzk2J86G.js";import"./TabbableElements-XzXYKyvZ.js";import"./parameters-bundle.css-VMdlhyu5.js";import"./generateHighlightedMarkup-DyKtynnz.js";import"./Input-CphtcWNd.js";import"./ValueState-Bg0UWejw.js";import"./ResponsivePopoverCommon.css-BHUgyS0n.js";import"./ValueStateMessage.css-C_geo_MX.js";import"./Suggestions.css-bckqdeoL.js";import"./Search-DHA3Lvyh.js";import"./Option-BrrbxGpX.js";import"./ListItemBaseTemplate-BGUdMTZ8.js";import"./ListItemAdditionalText.css-DC55uWF7.js";import"./Select-CZ_Vru5I.js";import"./InvisibleMessage-BAUrg6H4.js";import"./List-DiCPSBKg.js";import"./ItemNavigation-B1w6o4UP.js";import"./handleDrop-BdclM_pf.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ResponsivePopover-DdHf6Gx6.js";import"./Dialog-DYMZoMI2.js";import"./slim-arrow-down-P4GuIPIC.js";import"./slim-arrow-down-CpKeKmlD.js";import"./search-B6fpuQYT.js";import"./SuggestionItem-OjDxZh-1.js";import"./i18n-defaults-BpYbS4V6.js";function i(r){const o={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...m(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:h}),`
+`,t.jsx(a,{experimental:!0}),`
+`,t.jsx("br",{}),`
+`,t.jsx(o.h2,{id:"example",children:"Example"}),`
+`,t.jsx(p,{of:e}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{of:e}),`
+`,t.jsx(o.h2,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(o.h3,{id:"search-with-scope-and-searchitems",children:"Search with Scope and SearchItems"}),`
+`,t.jsxs(o.p,{children:["A ",t.jsx(o.code,{children:"Search"})," component with enabled scope, items and filtering by scope."]}),`
+`,t.jsx(p,{of:l}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show static code"}),t.jsx(o.pre,{children:t.jsx(o.code,{className:"language-tsx",children:`const scopeData = [
+  { name: 'Laptop', scope: 'products' },
+  { name: 'Leave Requests', scope: 'apps' },
+  { name: 'Log work', scope: 'apps' },
+  { name: 'Manage Products', scope: 'apps' },
+  { name: 'Mobile Phones', scope: 'products' },
+  { name: 'Tablet', scope: 'products' }
+];
+
+function SearchComponent(props) {
+  const [filterData, setFilterData] = useState(scopeData);
+
+  const handleScopeChange: SearchPropTypes['onScopeChange'] = (e) => {
+    props.onScopeChange(e);
+    const { scope } = e.detail;
+    if (scope.text === 'All') {
+      setFilterData(scopeData);
+    } else {
+      setFilterData(
+        scopeData.filter((item) => {
+          return item.scope === scope.text.toLowerCase();
+        })
+      );
+    }
+  };
+
+  return (
+    <Search {...props} onScopeChange={handleScopeChange}>
+      {filterData.map((item) => {
+        return <SearchItem key={item.name} text={item.name} scopeName={item.scope} />;
+      })}
+    </Search>
+  );
+}
+`})})]}),`
+`,t.jsx(c,{})]})}function xo(r={}){const{wrapper:o}={...m(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(i,{...r})}):i(r)}export{xo as default};
---
+++ nightly/assets/Toolbar-CA1WGm9M.js
@@ -0,0 +1,59 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as h}from"./index-B5Ip8tK9.js";import{A as e}from"./ArgTypesWithNote-7rsXGO4D.js";import{C as x}from"./ControlsWithNote-Dlihrsue.js";import{D as f}from"./DocsHeader-D1z4P3Uo.js";import{F as j}from"./CommandsAndQueries-Dc5LslcP.js";import"./index-BEtTPGE0.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-xNG2nZLh.js";import{S as b}from"./SubcomponentsSection-BVy4lpQe.js";import{M as u,C as i,a as v,D as p}from"./index-C8j1a1wf.js";import{C as T,D as n,O as S,T as m,a as s,b as a,c}from"./Toolbar.stories-BCAvJgUP.js";import{T as l}from"./index-CuMgiZae.js";import"./index-CV9953r4.js";import"./jsx-runtime-CkEx_Gfk.js";import"./CustomElementsScope-CjZbIYTD.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-DcNvBpEn.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-BexaehQh.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DOGhZSxR.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B158X0Y5.js";import"./decline-zF6-_htN.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-a8ApNaQO.js";import"./information-DmA-zxas.js";import"./sys-enter-2-DU2G_wE6.js";import"./sys-enter-2-CbaoLfaf.js";import"./alert-CeIMTDH_.js";import"./TagDesign-Ga50IS6u.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./utils-DLp8nVmU.js";import"./main-DnREbZ5I.js";import"./index-B_D8PZjO.js";import"./Tag-DUc97KOd.js";import"./sys-help-2-i21-KsxS.js";import"./index-D9S9JAEE.js";import"./Link-CBPPqDB9.js";import"./index-D_lPDdUa.js";import"./Popover-eNK7pDla.js";import"./PopupsCommon.css-DXmEAn4P.js";import"./FocusableElements-DDPXQQfz.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DS2o_9u5.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-C8Wa9dhW.js";import"./copy-DmqZtkbO.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-CX5fbFNZ.js";import"./I18nStore-tbyxgrjv.js";import"./useStylesheet-DUe1LpVj.js";import"./index-Cj6ai3XZ.js";import"./index-Bjofktik.js";import"./Label-BrFTKjrK.js";import"./index-dNG4P2z4.js";import"./index-BxLcq94m.js";import"./Text-CNWDrMdn.js";import"./preview-DLSX0WQj.js";import"./iframe-BUIjnSfL.js";import"./DocsRenderer-CFRXHY34-CQQuXYXR.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-B73Gg2hE.js";import"./index-CD5x5g4B.js";import"./BusyIndicator-vKEUjaWy.js";import"./index-8fyqcNlw.js";import"./index-DOaWR8S5.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-BtfkId0O.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./ToolbarButton-EPSeOMC_.js";import"./overflow-BsuFHrJn.js";import"./overflow-DO5jD8Lt.js";import"./Select-CZ_Vru5I.js";import"./InvisibleMessage-BAUrg6H4.js";import"./ValueState-Bg0UWejw.js";import"./List-DiCPSBKg.js";import"./ItemNavigation-B1w6o4UP.js";import"./handleDrop-BdclM_pf.js";import"./DropIndicator-Pzk2J86G.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-B5I1cQuH.js";import"./TabbableElements-XzXYKyvZ.js";import"./ResponsivePopover-DdHf6Gx6.js";import"./Dialog-DYMZoMI2.js";import"./slim-arrow-down-P4GuIPIC.js";import"./slim-arrow-down-CpKeKmlD.js";import"./ResponsivePopoverCommon.css-BHUgyS0n.js";import"./ValueStateMessage.css-C_geo_MX.js";import"./Option-BrrbxGpX.js";import"./ListItemBaseTemplate-BGUdMTZ8.js";import"./ListItemAdditionalText.css-DC55uWF7.js";function d(t){const r={a:"a",code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...h(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(u,{of:T}),`
+`,o.jsx(f,{since:"1.19.0"}),`
+`,o.jsx("br",{}),`
+`,o.jsx(r.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:n}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(x,{of:n}),`
+`,o.jsx(r.h2,{id:"opening-popovers-via-toolbarbutton",children:"Opening Popovers via ToolbarButton"}),`
+`,o.jsxs(r.p,{children:["Since the ",o.jsx(r.code,{children:"ToolbarButton"})," is an ",o.jsx(r.a,{href:"?path=/docs/knowledge-base-faq--docs#what-are-abstract-ui5-web-components",children:"abstract UI5 web component"}),", the opener of the ",o.jsx(r.code,{children:"Popover"}),` needs the DOM reference of the actual element to position the popover correctly.
+Starting with v2.5.0 of `,o.jsx(r.code,{children:"@ui5/webcomponents(-react)"}),", the ",o.jsx(r.code,{children:"detail"})," property of the ",o.jsx(r.code,{children:"ToolbarButton"}),"'s click handler now includes a ",o.jsx(r.code,{children:"targetRef"})," property, which can be used as the opener."]}),`
+`,o.jsx(i,{of:S}),`
+`,o.jsx(r.h3,{id:"example-code",children:"Example Code"}),`
+`,o.jsx(r.pre,{children:o.jsx(r.code,{className:"language-tsx",children:`function ToolbarWithPopover() {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const popoverRef = useRef<PopoverDomRef>(null);
+  return (
+    <>
+      <Toolbar>
+        <ToolbarButton
+          onClick={(e) => {
+            const { targetRef } = e.detail;
+            if (popoverRef.current) {
+              popoverRef.current.opener = targetRef;
+              setPopoverOpen(true);
+            }
+          }}
+          text="Open Popover"
+        />
+      </Toolbar>
+      <Popover
+        open={popoverOpen}
+        ref={popoverRef}
+        onClose={() => {
+          setPopoverOpen(false);
+        }}
+      >
+        Content
+      </Popover>
+    </>
+  );
+}
+`})}),`
+`,o.jsx(v,{children:b}),`
+`,o.jsx(r.h2,{id:"toolbarbutton",children:"ToolbarButton"}),`
+`,o.jsx(p,{of:l}),`
+`,o.jsx(e,{of:l}),`
+`,o.jsx(r.h2,{id:"toolbarselect",children:"ToolbarSelect"}),`
+`,o.jsx(p,{of:m}),`
+`,o.jsx(e,{of:m}),`
+`,o.jsx(r.h2,{id:"toolbarselectoption",children:"ToolbarSelectOption"}),`
+`,o.jsx(p,{of:s}),`
+`,o.jsx(e,{of:s}),`
+`,o.jsx(r.h2,{id:"toolbarseparator",children:"ToolbarSeparator"}),`
+`,o.jsx(p,{of:a}),`
+`,o.jsx(e,{of:a}),`
+`,o.jsx(r.h2,{id:"toolbarspacer",children:"ToolbarSpacer"}),`
+`,o.jsx(p,{of:c}),`
+`,o.jsx(e,{of:c}),`
+`,o.jsx(j,{})]})}function Sr(t={}){const{wrapper:r}={...h(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(d,{...t})}):d(t)}export{Sr as default};
