7551b2bbd4d436aad613f9c668dd9b0d32244a78
Lukas742 <lukas.harbarth@sap.com>
2025-05-22 13:26:11 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@a8a688d098328a85b6c54b3b06c80161670d3098 🚀

---
+++ nightly/assets/Form-DEcJPVG1.js
@@ -0,0 +1,121 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-B5Ip8tK9.js";import{M as s,C as o,a as u,D as r,A as i}from"./index-B5ffn4Gr.js";import"./index-CIMhrZAh.js";import"./index-B7Fr7u5p.js";import{C as c}from"./ControlsWithNote-Bzj_kfA2.js";import{D as h}from"./DocsHeader-D4CmuFjx.js";import{F as I}from"./CommandsAndQueries-DO6R18Bo.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-Bi6E7_f2.js";import{S as y}from"./SubcomponentsSection-BVy4lpQe.js";import{a as p,b as m}from"./index-CIh5T3fl.js";import{C as x,D as a,a as F}from"./Form.stories-DMdJOTUS.js";import"./iframe-4IIzKaC1.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-gx_Zcjo3.js";import"./CustomElementsScope-CjZbIYTD.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-BhunxXU0.js";import"./Keys-B3oBAe0t.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-CZw5gRks.js";import"./parameters-bundle.css-Dkq9X0yL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-8xifowfb.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-B158X0Y5.js";import"./decline-BwbUHmi7.js";import"./i18n-defaults-BJtDw6Mf.js";import"./information-Beb07qW6.js";import"./information-DPOcXyRn.js";import"./sys-enter-2-SJ-ADG9s.js";import"./sys-enter-2-3iNjTkh9.js";import"./alert-OoqNCaQj.js";import"./TagDesign-Ga50IS6u.js";import"./utils-nuAEn-QT.js";import"./main-DnREbZ5I.js";import"./index-Bpis0SRi.js";import"./Tag-CEzSQIl5.js";import"./sys-help-2-RmNMPhxT.js";import"./index-DY8ctAYr.js";import"./Link-CCYqxCbK.js";import"./index-DMDy6m5O.js";import"./Popover-inHmHWag.js";import"./PopupsCommon.css-C6QW1NU3.js";import"./FocusableElements-B0ox-u2n.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DeuUg-iv.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-C9nZ6DfS.js";import"./copy-CbwaFUHa.js";import"./copy-1xAMALOS.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-0bR6QLY4.js";import"./I18nStore-XmjZR7rr.js";import"./useStylesheet-DUe1LpVj.js";import"./index-Cj6ai3XZ.js";import"./index-DNdqOyXb.js";import"./Label-Xvb1prxJ.js";import"./index-BWd6G_fU.js";import"./index-_2xpPfjR.js";import"./Text-CkerREnW.js";import"./preview-dA2HxX6P.js";import"./DocsRenderer-CFRXHY34-BLZ_yyXk.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-PCpHaHtG.js";import"./index-D9fhKjZm.js";import"./BusyIndicator-DjtkhCPk.js";import"./index-DkwX2oO3.js";import"./index-DWxMKmlL.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-qIXMnRwj.js";import"./Input-DI4l6fPz.js";import"./ValueState-Bg0UWejw.js";import"./ResponsivePopoverCommon.css-ngelvl8I.js";import"./ValueStateMessage.css-E0DjtdJ4.js";import"./Suggestions.css-CNnloLYM.js";import"./index-JWu-BKOf.js";import"./index-BlOu-dU6.js";import"./index-CgACGHI8.js";import"./Select-C61xPb0K.js";import"./InvisibleMessage-B5xNggWT.js";import"./List-qZDTvK7t.js";import"./ItemNavigation-DmP37oe7.js";import"./handleDrop-AMQxvv7R.js";import"./DropIndicator-yg2DufZG.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-CsRDqRTe.js";import"./TabbableElements-XzXYKyvZ.js";import"./ResponsivePopover-Blp0O4E4.js";import"./Dialog-BSquvsbt.js";import"./slim-arrow-down-DQ2NAFnC.js";import"./slim-arrow-down-DtYGSLu3.js";import"./index-D4N6r4Rb.js";import"./Option-Dr-HXByj.js";import"./ListItemBaseTemplate-BF_oW5Ft.js";import"./ListItemAdditionalText.css-DI-wOckb.js";import"./index-rH-i952H.js";import"./CheckBox-WFu_Bb_l.js";import"./accept-tKQJV64V.js";function d(n){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...l(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:x}),`
+`,t.jsx(h,{subComponents:["FormGroup","FormItem"]}),`
+`,t.jsx("br",{}),`
+`,t.jsx(e.h2,{id:"example",children:"Example"}),`
+`,t.jsx(o,{of:a}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(c,{of:a}),`
+`,t.jsx(e.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx(e.h2,{id:"display-only-and-edit-mode",children:"Display-Only and Edit mode"}),`
+`,t.jsx(o,{of:F,sourceState:"none"}),`
+`,t.jsx(e.h3,{id:"code",children:"Code"}),`
+`,t.jsxs("details",{children:[t.jsx("summary",{children:"Show Code"}),t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-jsx",children:`const StandardField = ({ editMode, value, inputType = InputType.Text, onInput, ...rest }) => {
+  if (editMode) {
+    return <Input value={value} style={{ width: '100%' }} type={inputType} onInput={onInput} {...rest} />;
+  }
+  if (inputType === InputType.URL || inputType === InputType.Email) {
+    return (
+      <Link href={inputType === InputType.Email ? \`mailto:\${value}\` : value} target="_blank" {...rest}>
+        {value}
+      </Link>
+    );
+  }
+  return <Text {...rest}>{value}</Text>;
+};
+
+const reducer = (state, { field, value }) => {
+  return { ...state, [field]: value };
+};
+
+const FormComponent = (props) => {
+  const [editMode, toggleEditMode] = useReducer((prev) => !prev, false, undefined);
+  const [formState, dispatch] = useReducer(
+    reducer,
+    {
+      name: 'Red Point Stores',
+      street: 'Main St 1618',
+      zip: 31415,
+      city: 'Maintown',
+      country: 'Germany',
+      web: 'https://www.sap.com',
+      mail: 'john.smith@sap.com',
+      twitter: '@sap',
+      phone: '+49 1234 56789'
+    },
+    undefined
+  );
+  const { zip, city, name, street, country, web, mail, twitter, phone } = formState;
+
+  const handleInput = (e) => {
+    dispatch({ field: Object.keys(e.target.dataset)[0], value: e.target.value });
+  };
+
+  return (
+    <>
+      <Button onClick={toggleEditMode}>Toggle {editMode ? 'Display-Only Mode' : 'Edit Mode'}</Button>
+      <Form
+        {...props}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <FormGroup titleText="Address">
+          <FormItem label="Name">
+            <StandardField editMode={editMode} value={name} onInput={handleInput} data-name />
+          </FormItem>
+          <FormItem label="Street">
+            <StandardField editMode={editMode} value={street} onInput={handleInput} data-street />
+          </FormItem>
+          <FormItem label="ZIP Code / City">
+            {editMode ? (
+              <>
+                <Input value={zip} type={InputType.Number} style={{ width: '30%' }} onInput={handleInput} data-zip />
+                <Input value={city} style={{ width: '70%' }} onInput={handleInput} data-city />
+              </>
+            ) : (
+              <Text>{\`\${zip} \${city}\`}</Text>
+            )}
+          </FormItem>
+          <FormItem label="Country">
+            <StandardField editMode={editMode} value={country} onInput={handleInput} data-country />
+          </FormItem>
+          <FormItem label="Web">
+            <StandardField editMode={editMode} value={web} inputType={InputType.URL} onInput={handleInput} data-web />
+          </FormItem>
+        </FormGroup>
+        <FormGroup titleText="Contact">
+          <FormItem label="Email">
+            <StandardField
+              editMode={editMode}
+              value={mail}
+              inputType={InputType.Email}
+              onInput={handleInput}
+              data-email
+            />
+          </FormItem>
+          <FormItem label="Twitter">
+            <StandardField editMode={editMode} value={twitter} onInput={handleInput} data-twitter />
+          </FormItem>
+          <FormItem label="Phone">
+            <StandardField
+              editMode={editMode}
+              value={phone}
+              inputType={InputType.Tel}
+              onInput={handleInput}
+              data-phone
+            />
+          </FormItem>
+        </FormGroup>
+      </Form>
+    </>
+  );
+};
+`})})]}),`
+`,t.jsx(u,{children:y}),`
+`,t.jsx(e.h2,{id:"form-group",children:"Form Group"}),`
+`,t.jsx(r,{of:p}),`
+`,t.jsx(i,{of:p}),`
+`,t.jsx(e.h2,{id:"formitem",children:"FormItem"}),`
+`,t.jsx(r,{of:m}),`
+`,t.jsx(i,{of:m}),`
+`,t.jsx(I,{})]})}function Te(n={}){const{wrapper:e}={...l(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(d,{...n})}):d(n)}export{Te as default};
---
+++ nightly/assets/index-B7Fr7u5p.js
@@ -0,0 +1,1 @@
+const i={version:"2.10.0",major:2,minor:10,patch:0,suffix:"",isNext:!1,buildTime:1747920265};i.version;export{i as V};
