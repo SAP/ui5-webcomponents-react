a1b1fbca980ee7ece13d4484c57f1b4dfc1ab658
Lukas742 <lukas.harbarth@sap.com>
2025-06-20 06:11:25 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@19036700d66812c3abab78946ba9d4cee824a093 🚀

---
+++ nightly/assets/Tree-CVmgB-Js.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as d}from"./index-B5Ip8tK9.js";import{A as o}from"./ArgTypesWithNote-CS0ubuHH.js";import{C as l}from"./ControlsWithNote-BKsh0eC1.js";import{D as c}from"./DocsHeader-2Ksld7FY.js";import{F as h}from"./CommandsAndQueries-BdHrZ-Gp.js";import"./index-CZLqEiAj.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-JqJU4F4n.js";import{S as x}from"./SubcomponentsSection-BVy4lpQe.js";import{M as j,C as i,a as T,D as m}from"./index-Cl01kn3x.js";import{C as f,D as n,L as u,T as p,a as s}from"./Tree.stories-CW_1TqId.js";import"./index-8msv1YOk.js";import"./jsx-runtime-CTdjW1dy.js";import"./CustomElementsScope-BI7irdrP.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-CddyUUJt.js";import"./Keys-BxH5KEHJ.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-Cq4WdPfD.js";import"./parameters-bundle.css-CKhNLE8L.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-CywStIph.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-Bwpyempw.js";import"./decline-CzIZ7Vnl.js";import"./i18n-defaults-CmVK7_Fz.js";import"./information-C85sdt-6.js";import"./information-CovZJhop.js";import"./sys-enter-2-LJy9JDcm.js";import"./sys-enter-2-CUlYw7x6.js";import"./alert-DKjZxuBr.js";import"./Tag-BGp0HQg1.js";import"./sys-help-2-CdqLSqXL.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./utils-DT78QYod.js";import"./main-DirjBNUD.js";import"./index-DjYHlBL_.js";import"./index-xR1I-Wi5.js";import"./Link-BvcafW2a.js";import"./index-GYOhI-mh.js";import"./Popover-D3TB1XeJ.js";import"./PopupsCommon.css-3rSjbG44.js";import"./FocusableElements-Cvm20E5b.js";import"./isElementHidden-B4HqmI2E.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-B38a0cLW.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-zkSO3ShF.js";import"./copy-iFL-9i7I.js";import"./copy-DVi57tol.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-DlxXdoKk.js";import"./I18nStore-C5X80oUZ.js";import"./useStylesheet-4DYJndCZ.js";import"./index-D71WPoKU.js";import"./index-BNsc4RDp.js";import"./Label-CxXPup1E.js";import"./index-CK7zNLqv.js";import"./index-OzB7lNJQ.js";import"./Text-DZG3aMF7.js";import"./preview-CxTsH5xX.js";import"./iframe-_IVPTs7k.js";import"./DocsRenderer-CFRXHY34-Ci7H-lZF.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-B6DvbxXy.js";import"./index-DPnISxA_.js";import"./BusyIndicator-BKaUugOY.js";import"./index-CnTKSd9L.js";import"./index-Dj0HSimA.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-1lbVTklO.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./List-B_8iuIil.js";import"./ItemNavigation-DNb9B6_M.js";import"./TableUtils-DqLEf2oU.js";import"./DropIndicator-CFcDZwdF.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-BpIMRvvP.js";import"./TabbableElements-XzXYKyvZ.js";import"./general-leave-request-CaNW-MHV.js";import"./index-CLcOKllD.js";import"./index-BiF-sMal.js";import"./ListItemTemplate-CXAphkt3.js";import"./edit-C3yWC7Dh.js";import"./ListItemAdditionalText.css-Bs0NBuuA.js";import"./slim-arrow-right-ypoHwspP.js";import"./RadioButton-DU941qqD.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-B3wYHGWl.js";import"./accept-COVgYUg4.js";import"./navigation-right-arrow-BnNPWmHK.js";import"./navigation-down-arrow-BtIi_0ft.js";import"./navigation-right-arrow-Ce4d-8YI.js";function a(e){const r={code:"code",h1:"h1",h2:"h2",h3:"h3",pre:"pre",...d(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(j,{of:f}),`
+`,t.jsx(c,{since:"0.10.0"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"example",children:"Example"}),`
+`,t.jsx(i,{of:n}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{of:n}),`
+`,t.jsx(r.h1,{id:"more-examples",children:"More Examples"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"lazy-load-treeitems",children:"Lazy Load TreeItems"}),`
+`,t.jsx(i,{of:u}),`
+`,t.jsx(r.h3,{id:"code",children:"Code"}),`
+`,t.jsx(r.pre,{children:t.jsx(r.code,{className:"language-jsx",children:`const LazyLoadingTree = () => {
+  const [lazyChildren, setLazyChildren] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleItemToggle = (e) => {
+    if (e.detail.item.dataset.id === 'lazychildren' && !lazyChildren) {
+      e.preventDefault();
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setLazyChildren(
+          <>
+            <TreeItem text="Tree 3.1" />
+            <TreeItem text="Tree 3.2" />
+          </>
+        );
+        e.detail.item.toggle();
+      }, 3000);
+    }
+  };
+  return (
+    <BusyIndicator active={loading} style={{ width: '100%' }}>
+      <Tree onItemToggle={handleItemToggle}>
+        <TreeItem text="Has pre-loaded children">
+          <TreeItem text="Tree 1.1" />
+          <TreeItem text="Tree 1.2" />
+        </TreeItem>
+        <TreeItem text="Has no children" />
+        <TreeItem text="Has children but not yet loaded" hasChildren icon={dlCloutIcon} data-id="lazychildren">
+          {lazyChildren}
+        </TreeItem>
+      </Tree>
+    </BusyIndicator>
+  );
+};
+`})}),`
+`,t.jsx(T,{children:x}),`
+`,t.jsx(r.h2,{id:"treeitem",children:"TreeItem"}),`
+`,t.jsx(m,{of:p}),`
+`,t.jsx(o,{of:p}),`
+`,t.jsx(r.h2,{id:"treeitemcustom",children:"TreeItemCustom"}),`
+`,t.jsx(m,{of:s}),`
+`,t.jsx(o,{of:s}),`
+`,t.jsx(h,{})]})}function xr(e={}){const{wrapper:r}={...d(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(a,{...e})}):a(e)}export{xr as default};
---
+++ nightly/assets/index-CZLqEiAj.js
@@ -0,0 +1,1 @@
+const i={version:"2.11.1",major:2,minor:11,patch:1,suffix:"",isNext:!1,buildTime:1750399785};i.version;export{i as V};
