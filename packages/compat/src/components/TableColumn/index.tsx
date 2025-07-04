8d59de2fca8665f55b5ef78657d24ddf8c1c6053
Lukas Harbarth <lukas.harbarth@sap.com>
2025-07-04 13:27:29 +0200

    run wrapper script (compat)

--- packages/compat/src/components/TableColumn/index.tsx
+++ packages/compat/src/components/TableColumn/index.tsx
@@ -61,6 +61,7 @@ interface TableColumnPropTypes
  * when rendering the `Table` component.
  *
  * __Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)
+ * @deprecated Deprecated as of version 2.12.0, use `@ui5/webcomponents/dist/Table.js` instead.
  */
 const TableColumn = withWebComponent<TableColumnPropTypes, TableColumnDomRef>(
   'ui5-table-column',
--- packages/compat/src/components/TableGroupRow/index.tsx
+++ packages/compat/src/components/TableGroupRow/index.tsx
@@ -24,6 +24,7 @@ interface TableGroupRowPropTypes extends TableGroupRowAttributes, Omit<CommonPro
  * __Note__: This is a UI5 Web Component! [Repository](https://github.com/SAP/ui5-webcomponents) | [Documentation](https://sap.github.io/ui5-webcomponents/)
  *
  * @since [2.0.0](https://github.com/SAP/ui5-webcomponents/releases/tag/v2.0.0) of __@ui5/webcomponents-compat__.
+ * @deprecated Deprecated as of version 2.12.0, use `@ui5/webcomponents/dist/Table.js` instead.
  */
 const TableGroupRow = withWebComponent<TableGroupRowPropTypes, TableGroupRowDomRef>(
   'ui5-table-group-row',
