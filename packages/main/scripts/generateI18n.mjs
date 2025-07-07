ea8e7a0a6d0889b8d5ea00100037ea22c3a7cf2e
Lukas Harbarth <lukas.harbarth@sap.com>
2025-07-07 11:30:34 +0200

    feat(Assets): add `Assets-fetch.js` (#7345)

--- packages/main/scripts/generateI18n.mjs
+++ packages/main/scripts/generateI18n.mjs
@@ -55,16 +55,24 @@ spawnSync('npx', ['prettier', '--write', path.resolve(SRC_I18N_PROPERTIES, 'i18n
   stdio: [0, 1, 2],
 });
 
-// generate Assets.js
+// generate Assets.js and Assets-fetch.js
 const jsonImports = await readdir(TARGET_I18N_JSON_IMPORTS);
 
 const assets = [`import '@ui5/webcomponents/dist/Assets.js';`, `import '@ui5/webcomponents-fiori/dist/Assets.js';`];
+const assetsFetch = [
+  `import '@ui5/webcomponents/dist/Assets-fetch.js';`,
+  `import '@ui5/webcomponents-fiori/dist/Assets-fetch.js';`,
+];
+const assetsNode = [
+  `import '@ui5/webcomponents/dist/Assets-node.js';`,
+  `import '@ui5/webcomponents-fiori/dist/Assets-node.js';`,
+];
 
 for (const file of jsonImports) {
   if (file.includes('-fetch')) {
-    //todo: add to Assets-fetch.js
+    assetsFetch.push(`import './json-imports/${file}';`);
   } else if (file.includes('-node')) {
-    //todo: add to Assets-node.js
+    assetsNode.push(`import './json-imports/${file}';`);
   } else {
     assets.push(`import './json-imports/${file}';`);
   }
@@ -73,4 +81,14 @@ for (const file of jsonImports) {
 await writeFile(
   path.resolve(DIST_DIR, 'Assets.js'),
   await prettier.format(assets.join('\n'), { ...prettierConfig, parser: 'babel' }),
+);
+
+await writeFile(
+  path.resolve(DIST_DIR, 'Assets-fetch.js'),
+  await prettier.format(assetsFetch.join('\n'), { ...prettierConfig, parser: 'babel' }),
+);
+
+await writeFile(
+  path.resolve(DIST_DIR, 'Assets-node.js'),
+  await prettier.format(assetsNode.join('\n'), { ...prettierConfig, parser: 'babel' }),
 );
