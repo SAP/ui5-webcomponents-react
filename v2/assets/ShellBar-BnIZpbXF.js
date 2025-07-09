facc268ddc5dbe92fd78aa3d3fd55b90a4740cb1
Lukas742 <Lukas742@users.noreply.github.com>
2025-07-09 10:07:41 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@dbb6574b7183cd375d444fbcce92ce2010063512 🚀

---
+++ v2/assets/ShellBar-BnIZpbXF.js
@@ -0,0 +1,70 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as x}from"./index-B5Ip8tK9.js";import{M as f,C as o,a as p,D as i,A as n}from"./index-CG1eezO7.js";import"./index-Bh0L8GxH.js";import"./index-DejPHfb6.js";import{C as j}from"./ControlsWithNote-DNF5MTq1.js";import{D as u}from"./DocsHeader-BkSi4rdm.js";import{F as S}from"./CommandsAndQueries-CRq5j1Wk.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-l18RNg85.js";import{S as v}from"./SubcomponentsSection-BVy4lpQe.js";import{S as m,a as s,b as a}from"./sys-help-XHMmZ49g.js";import{C as B,D as l,A as g,E as b,T as P,P as C,N as y,S as M,a as c}from"./ShellBar.stories-CKUOoihG.js";import{e as d}from"./utils-CLzsc1hI.js";import"./iframe-DtSFpSgq.js";import"./index-B8m469-U.js";import"./index-BVR6w1bU.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-2NO35c9p.js";import"./CustomElementsScope-BZQ-yqK5.js";import"./EventProvider-ChtD9rRX.js";import"./slot-_4yKMUwC.js";import"./event-strict-DgQLNDEV.js";import"./i18n-CjR4HGlq.js";import"./Button-DsiN4KEf.js";import"./Keys-CLf_QmYW.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-Dy5MqRQN.js";import"./parameters-bundle.css-D0uTR4GL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DqK_Zwt1.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-Wx6G1HAa.js";import"./decline-Cf8Y7eZn.js";import"./i18n-defaults-CmVK7_Fz.js";import"./information-CPeQ-J71.js";import"./information-DVtVmQQs.js";import"./sys-enter-2-DM7CihIf.js";import"./sys-enter-2-D_EhgDRM.js";import"./alert-B-6XnuHB.js";import"./Tag-B8Y5CAT_.js";import"./sys-help-2-CQSD8GDX.js";import"./index-WWlNSEWj.js";import"./index-CpZlRrqk.js";import"./Link-CIigJXcZ.js";import"./index-39R8lAwY.js";import"./Popover-DHgn1IwJ.js";import"./PopupsCommon.css-1ftV5PB8.js";import"./FocusableElements--dusX2Bm.js";import"./isElementHidden-gS67o0pl.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DGDOSiJK.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-zkSO3ShF.js";import"./copy--eUL3OtC.js";import"./copy-C9kEgeiD.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./index-BbU1ZdK1.js";import"./I18nStore-Bb3vbAIQ.js";import"./useStylesheet-DhspXHhi.js";import"./index-C1uP44Ty.js";import"./index-49bJeJce.js";import"./Label-DvK4qyHf.js";import"./index-C2cs6waD.js";import"./index-Z1IFI4-r.js";import"./Text-CWzAOMGb.js";import"./preview-Bu3YvvQj.js";import"./DocsRenderer-CFRXHY34-BwXwSrh2.js";import"./react-18-SRpBHcoT.js";import"./addCustomCSSWithScoping-BPwSVJcY.js";import"./index-B-i0fidp.js";import"./BusyIndicator-kBFPsFJK.js";import"./index-B61k-92l.js";import"./index-6SfSTYbY.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DTc-n_gr.js";import"./Search-ZrsOFRtN.js";import"./Option-Cf6aXVvg.js";import"./ListItemGroup-CfwLEKW4.js";import"./DropIndicator-BAXclq7I.js";import"./TabbableElements-CqFMEI5U.js";import"./ListItemBaseTemplate-yE5a7oOf.js";import"./ListItemAdditionalText.css-Bw4YnmYD.js";import"./Select-DKio74ug.js";import"./InvisibleMessage-CtO9enL8.js";import"./ValueState-Bg0UWejw.js";import"./List-2melzpex.js";import"./ItemNavigation-D8f2eHwJ.js";import"./TableUtils-BdnmFIk7.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ResponsivePopover-DbareGdm.js";import"./Dialog-TYaONLew.js";import"./slim-arrow-down-ClE0dvFp.js";import"./slim-arrow-down-BsPWSXBU.js";import"./ResponsivePopoverCommon.css-D8oCtj4w.js";import"./ValueStateMessage.css-26HjZ_sA.js";import"./search-CD189Cpa.js";import"./Input-CqW83Wug.js";import"./Suggestions.css-LA4jUZtL.js";import"./SuggestionItem-_veE5DGW.js";import"./parameters-bundle.css-BcDcM98N.js";import"./i18n-defaults-dyGKyNAh.js";import"./Person-Bk5r7PMP.js";import"./nav-back-h8PAIATe.js";import"./index-1MLnxj3P.js";import"./accept-CcEwUC-x.js";import"./less-BFMA4_R1.js";import"./index-DzYDEEMp.js";import"./ListItemStandard-BI1nQu-4.js";import"./ListItemTemplate-BQJ_nEnI.js";import"./edit-BOpKjwiH.js";import"./slim-arrow-right-Db6husuF.js";import"./RadioButton-k0vTSgaJ.js";import"./CheckBox-Cwy3o2MP.js";import"./ButtonBadge-DSyuaVHg.js";import"./Menu-D51jUwfh.js";import"./MenuItem-Dzrec60C.js";import"./ListItemCustom-DM2MF6XX.js";import"./overflow-BKSW4She.js";import"./overflow-D6GMRaeX.js";import"./main-Dn6sLQP9.js";const k=`## Design-Compliant Examples
+
+These examples demonstrate the implementation of design specifications. They primarily illustrate design concepts and mostly do not provide additional functionality. You can reference them to select the appropriate design patterns.
+`;function h(t){const r={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...x(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(f,{of:B}),`
+`,e.jsx(u,{since:"0.4.0"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(r.h2,{id:"example",children:"Example"}),`
+`,e.jsx(o,{of:l}),`
+`,e.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(j,{of:l}),`
+`,e.jsx(p,{children:k}),`
+`,e.jsx(r.h3,{id:"all-features",children:"All Features"}),`
+`,e.jsx(r.p,{children:"A comprehensive ShellBar demonstrating all available features including notifications, search, content items, and profile."}),`
+`,e.jsx(o,{of:g}),`
+`,e.jsx(r.h3,{id:"embedded-back-navigation",children:"Embedded Back Navigation"}),`
+`,e.jsx(r.p,{children:"ShellBar with embedded back navigation button in the start area."}),`
+`,e.jsx(o,{of:b}),`
+`,e.jsx(r.h3,{id:"trial-example",children:"Trial Example"}),`
+`,e.jsx(r.p,{children:"ShellBar configured for trial environments with trial tags and remaining days indicator."}),`
+`,e.jsx(o,{of:P}),`
+`,e.jsx(r.h3,{id:"multiple-productive-instances",children:"Multiple Productive Instances"}),`
+`,e.jsx(r.p,{children:"ShellBar setup for multiple productive system instances with region indicators."}),`
+`,e.jsx(o,{of:C}),`
+`,e.jsx(r.h3,{id:"multiple-non-productive-instances",children:"Multiple Non-Productive Instances"}),`
+`,e.jsx(r.p,{children:"ShellBar setup for multiple non-productive system instances with system and region tags."}),`
+`,e.jsx(o,{of:y}),`
+`,e.jsx(r.h2,{id:"more-examples",children:"More examples"}),`
+`,e.jsx(r.h3,{id:"open-a-popover-on-shellbaritem-click",children:"Open a Popover on ShellBarItem click"}),`
+`,e.jsxs(r.p,{children:["To open a popover with the ",e.jsx(r.code,{children:"ShellBarItem"})," you can use the ",e.jsx(r.code,{children:"detail.targetRef"})," property of the ",e.jsx(r.code,{children:"onClick"})," event."]}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{className:"language-tsx",children:`const ShellBarComponent = () => {
+  const popoverRef = useRef<PopoverDomRef>(null);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const handleShellBarItemClick: ShellBarItemPropTypes['onClick'] = (e) => {
+    popoverRef.current!.opener = e.detail.targetRef;
+    setPopoverOpen(true);
+  };
+  return (
+    <>
+      <ShellBar>
+        <ShellBarItem onClick={handleShellBarItemClick} icon="add" text="add" />
+      </ShellBar>
+      <Popover
+        ref={popoverRef}
+        open={popoverOpen}
+        onClose={() => {
+          setPopoverOpen(false);
+        }}
+      >
+        Hello there!
+      </Popover>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(r.h3,{id:"shellbar-with-shellbarbranding-slot",children:"ShellBar with ShellBarBranding slot"}),`
+`,e.jsx(o,{of:M}),`
+`,e.jsx(p,{children:v}),`
+`,e.jsx(r.h2,{id:"shellbaritem",children:"ShellBarItem"}),`
+`,e.jsx(i,{of:m}),`
+`,e.jsx(n,{hideHTMLPropsNote:!0,exclude:d,of:m}),`
+`,e.jsx(r.h2,{id:"shellbarspacer",children:"ShellBarSpacer"}),`
+`,e.jsx(i,{of:s}),`
+`,e.jsx(n,{hideHTMLPropsNote:!0,exclude:d,of:s}),`
+`,e.jsx(r.h2,{id:"shellbarsearch-experimental",children:"ShellBarSearch (experimental)"}),`
+`,e.jsx(i,{of:a}),`
+`,e.jsx(n,{hideHTMLPropsNote:!0,of:a}),`
+`,e.jsx(r.h2,{id:"shellbarbranding-experimental",children:"ShellBarBranding (experimental)"}),`
+`,e.jsx(i,{of:c}),`
+`,e.jsx(n,{hideHTMLPropsNote:!0,of:c}),`
+`,e.jsx(S,{})]})}function Gr(t={}){const{wrapper:r}={...x(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(h,{...t})}):h(t)}export{Gr as default};
--- v2/assets/index-OU7vbiqi.js
+++
@@ -1,1 +0,0 @@
-const i={version:"2.12.0",major:2,minor:12,patch:0,suffix:"",isNext:!1,buildTime:1751896942};i.version;export{i as V};
