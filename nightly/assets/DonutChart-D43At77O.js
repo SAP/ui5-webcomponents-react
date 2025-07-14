071086cff5e64f4b71142041405aba4f13b211db
Lukas742 <lukas.harbarth@sap.com>
2025-07-14 12:18:08 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@0b5a16368166d7c064d062e4acc5606f369d9a9f 🚀

---
+++ nightly/assets/DonutChart-D43At77O.js
@@ -0,0 +1,28 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as e}from"./index-B5Ip8tK9.js";import{F as s}from"./CommandsAndQueries-DT9Ja2VL.js";import{M as n,C as i}from"./index-CiuGqvd0.js";import"./index-BsLeVBVw.js";import"./index-Bgve6bvD.js";import{C as a}from"./ControlsWithNote-DJI1O4Do.js";import{D as h}from"./DocsHeader-DGxgCEXy.js";import"./index-U0ga5oGA.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-l18RNg85.js";import{M as d}from"./TooltipConfig-DwrmURi1.js";import{C as l,D as m,W as x,a as j,b as c,L as f,c as C,d as u,H as g,e as W,f as M}from"./DonutChart.stories-Cgukp1PU.js";import{M as D}from"./LegendConfig-CjfYzCsg.js";import"./preview-BlK6oQjd.js";import"./iframe-Bv8etj6L.js";import"./DocsRenderer-CFRXHY34-CBrhq3lM.js";import"./react-18-SRpBHcoT.js";import"./index-BVR6w1bU.js";import"./Button-DsiN4KEf.js";import"./jsx-runtime-2NO35c9p.js";import"./CustomElementsScope-BZQ-yqK5.js";import"./EventProvider-ChtD9rRX.js";import"./event-strict-DgQLNDEV.js";import"./slot-_4yKMUwC.js";import"./i18n-CjR4HGlq.js";import"./Keys-CLf_QmYW.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Icon-Dy5MqRQN.js";import"./parameters-bundle.css-D0uTR4GL.js";import"./willShowContent-CZcfsNXp.js";import"./Tooltips-DqK_Zwt1.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./i18n-defaults-Wx6G1HAa.js";import"./Popover-DHgn1IwJ.js";import"./PopupsCommon.css-1ftV5PB8.js";import"./FocusableElements--dusX2Bm.js";import"./isElementHidden-gS67o0pl.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-DGDOSiJK.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-zkSO3ShF.js";import"./index-B8m469-U.js";import"./index-B_tnU4S0.js";import"./clsx-B-dksMZM.js";import"./useStylesheet-DhspXHhi.js";import"./index-49bJeJce.js";import"./Label-DvK4qyHf.js";import"./index-C2cs6waD.js";import"./index-28AZPOp1.js";import"./Link-CIigJXcZ.js";import"./index-39R8lAwY.js";import"./index-Z1IFI4-r.js";import"./Text-CWzAOMGb.js";import"./addCustomCSSWithScoping-BPwSVJcY.js";import"./index-B-i0fidp.js";import"./BusyIndicator-kBFPsFJK.js";import"./index-B61k-92l.js";import"./index-6SfSTYbY.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-DTc-n_gr.js";import"./index-Cv8KzCSa.js";import"./I18nStore-Bb3vbAIQ.js";import"./index-Bmfoy9ob.js";import"./Tag-B8Y5CAT_.js";import"./sys-help-2-CQSD8GDX.js";import"./sys-enter-2-D_EhgDRM.js";import"./i18n-defaults-CmVK7_Fz.js";import"./information-DVtVmQQs.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./decline-Cf8Y7eZn.js";import"./information-CPeQ-J71.js";import"./sys-enter-2-DM7CihIf.js";import"./alert-B-6XnuHB.js";import"./utils-DIZaq3vH.js";import"./main-Dn6sLQP9.js";import"./copy--eUL3OtC.js";import"./copy-C9kEgeiD.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-BG9P-NK7.js";import"./DemoProps-BerORI7p.js";import"./PieChart-CrVYNdoT.js";import"./staticProps-wDw56Y-I.js";import"./ChartContainer-BQKPG3gg.js";import"./tiny-invariant-CopsF_GD.js";import"./index-1pNkI20x.js";import"./ThemingParameters-pyhX3s39.js";import"./useOnClickInternal-CrJVn7-y.js";import"./Placeholder-BvRogK9t.js";import"./react-content-loader.es-C9Vmu4FD.js";function p(r){const t={h2:"h2",h3:"h3",...e(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(h,{isChart:!0}),`
+`,o.jsx(t.h2,{id:"example",children:"Example"}),`
+`,o.jsx(i,{of:m}),`
+`,o.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{of:m}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,o.jsx(t.h3,{id:"with-custom-color",children:"With Custom Color"}),`
+`,o.jsx(i,{of:x}),`
+`,o.jsx(t.h3,{id:"with-padding-angle",children:"With Padding Angle"}),`
+`,o.jsx(i,{of:j}),`
+`,o.jsx(t.h3,{id:"with-custom-radius",children:"With Custom Radius"}),`
+`,o.jsx(i,{of:c}),`
+`,o.jsx(t.h3,{id:"loading-placeholder",children:"Loading Placeholder"}),`
+`,o.jsx(i,{of:f}),`
+`,o.jsx(t.h3,{id:"with-formatter",children:"With Formatter"}),`
+`,o.jsx(i,{of:C}),`
+`,o.jsx(t.h3,{id:"with-highlighted-active-segment",children:"With highlighted active segment"}),`
+`,o.jsx(i,{of:u}),`
+`,o.jsx(t.h3,{id:"hide-labels",children:"Hide labels"}),`
+`,o.jsx(i,{of:g}),`
+`,o.jsx(d,{of:W}),`
+`,o.jsx(D,{of:M}),`
+`,o.jsx("br",{}),`
+`,o.jsx("br",{}),`
+`,o.jsx(s,{})]})}function st(r={}){const{wrapper:t}={...e(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(p,{...r})}):p(r)}export{st as default};
