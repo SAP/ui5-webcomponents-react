bb4ac358e7ab5c1189808f31fa6373bb90731a5c
Lukas742 <lukas.harbarth@sap.com>
2025-08-04 14:16:20 +0000

    Deploying to gh-pages from @ SAP/ui5-webcomponents-react@776f8d7c67067f08e2cc18f9090756e36eff2a36 🚀

---
+++ nightly/assets/List.stories-FMD06g4W.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{L as T,a as j,b as S}from"./List-DPa82ePk.js";import{T as C}from"./ThemingParameters-pyhX3s39.js";import"./CustomElementsScope-CraI7z9b.js";import"./index-CEr2UAyE.js";import{F as f,d as y}from"./index-C5VKFbIU.js";import{T as n}from"./index-g22nm93k.js";import{L as m}from"./index-CJvudsVS.js";import{L as d}from"./index-BYsVQxlJ.js";import{L as e}from"./index-f4KjJaat.js";import{P as G}from"./index-C-0yLdrm.js";import{L as i}from"./index-BGNjzVxN.js";const w={title:"Data Display / List",component:i,argTypes:{children:{control:{disable:!0}},header:{control:{disable:!0}}},args:{headerText:"List with ListItemStandard",selectionMode:S.None,separators:j.All,growing:T.None},tags:["package:@ui5/webcomponents"]},r={render:s=>t.jsxs(i,{...s,children:[t.jsx(e,{additionalText:"3",text:"List Item 1"}),t.jsx(e,{additionalText:"2",text:"List Item 2"}),t.jsx(e,{additionalText:"1",text:"List Item 3"})]})},o={name:"ListItemCustom",args:{headerText:"List with a ListItemCustom"},render:s=>t.jsxs(i,{...s,children:[t.jsx(m,{children:t.jsxs(f,{justifyContent:y.SpaceAround,style:{width:"100%"},children:[t.jsx(n,{style:{fontWeight:"bold"},children:"FULLY"}),t.jsx(n,{style:{color:"red"},children:"CUSTOMIZABLE"}),t.jsx(n,{style:{color:"white",backgroundColor:C.sapButton_Emphasized_Background},children:"CHILDREN"})]})}),t.jsx(m,{children:t.jsx(G,{value:50})})]})},a={name:"ListItemGroup",args:{headerText:"List with a ListItemGroup"},render:s=>t.jsxs(i,{...s,children:[t.jsxs(d,{headerText:"ListItemGroup 1",children:[t.jsx(e,{text:"List Item"}),t.jsx(e,{text:"List Item"})]}),t.jsx(d,{headerText:"ListItemGroup 2",children:t.jsx(e,{text:"List Item"})})]})};var L,x,u;r.parameters={...r.parameters,docs:{...(L=r.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: args => {
+    return <List {...args}>
+        <ListItemStandard additionalText="3" text="List Item 1" />
+        <ListItemStandard additionalText="2" text="List Item 2" />
+        <ListItemStandard additionalText="1" text="List Item 3" />
+      </List>;
+  }
+}`,...(u=(x=r.parameters)==null?void 0:x.docs)==null?void 0:u.source}}};var l,p,c;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: 'ListItemCustom',
+  args: {
+    headerText: 'List with a ListItemCustom'
+  },
+  render: args => {
+    return <List {...args}>
+        <ListItemCustom>
+          <FlexBox justifyContent={FlexBoxJustifyContent.SpaceAround} style={{
+          width: '100%'
+        }}>
+            <Text style={{
+            fontWeight: 'bold'
+          }}>FULLY</Text>
+            <Text style={{
+            color: 'red'
+          }}>CUSTOMIZABLE</Text>
+            <Text style={{
+            color: 'white',
+            backgroundColor: ThemingParameters.sapButton_Emphasized_Background
+          }}>
+              CHILDREN
+            </Text>
+          </FlexBox>
+        </ListItemCustom>
+        <ListItemCustom>
+          <ProgressIndicator value={50} />
+        </ListItemCustom>
+      </List>;
+  }
+}`,...(c=(p=o.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var I,h,g;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  name: 'ListItemGroup',
+  args: {
+    headerText: 'List with a ListItemGroup'
+  },
+  render: args => {
+    return <List {...args}>
+        <ListItemGroup headerText="ListItemGroup 1">
+          <ListItemStandard text="List Item" />
+          <ListItemStandard text="List Item" />
+        </ListItemGroup>
+        <ListItemGroup headerText="ListItemGroup 2">
+          <ListItemStandard text="List Item" />
+        </ListItemGroup>
+      </List>;
+  }
+}`,...(g=(h=a.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const b=["Default","ListItemCustomStory","ListItemGroupStory"],v=Object.freeze(Object.defineProperty({__proto__:null,Default:r,ListItemCustomStory:o,ListItemGroupStory:a,__namedExportsOrder:b,default:w},Symbol.toStringTag,{value:"Module"}));export{v as C,r as D,o as L,a};
---
+++ nightly/assets/SelectDialog-CGoxguT5.js
@@ -0,0 +1,139 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as s}from"./index-Duvwo7dg.js";import{M as c,C as o}from"./index-Bb1Mufgq.js";import"./Tag-Ck-rQ93o.js";import"./index-BWmLiuWl.js";import"./index-CEr2UAyE.js";import"./index-7Z4Dhk6u.js";import{M as p}from"./index-DOj1f8Lg.js";import{C as l}from"./ControlsWithNote-CJ0J82Xo.js";import{D as m}from"./DocsHeader-OizU8y1f.js";import{F as a}from"./CommandsAndQueries-CX-9m4HR.js";import"./WrappingType-BBpR8qCJ.js";import"./Title-BTb5jHlz.js";import{C as d,D as r,M as h}from"./SelectDialog.stories-xhnVqogG.js";import"./iframe-B2MkOCiQ.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./jsx-runtime-xa789Fx-.js";import"./CustomElementsScope-CraI7z9b.js";import"./EventProvider-ChtD9rRX.js";import"./event-strict-DgQLNDEV.js";import"./slot-_4yKMUwC.js";import"./i18n-CjR4HGlq.js";import"./willShowContent-CZcfsNXp.js";import"./sys-help-2-QU1zRg7_.js";import"./Icon-BC8KE6RK.js";import"./Keys-CLf_QmYW.js";import"./parameters-bundle.css-D0uTR4GL.js";import"./sys-enter-2-CUYL3AE0.js";import"./i18n-defaults-CmVK7_Fz.js";import"./information-l0VFevGn.js";import"./i18n-defaults-Wx6G1HAa.js";import"./index-BX0qWsV0.js";import"./Button-BAohpRXl.js";import"./AccessibilityTextsHelper-4hgGqLyb.js";import"./Tooltips-CwjopbrF.js";import"./toLowercaseEnumValue-C_8o5td_.js";import"./decline-BJJ7TY3L.js";import"./information-jHKcowyE.js";import"./sys-enter-2-vblLkH1D.js";import"./alert-CtWydQwI.js";import"./utils-C8oRburP.js";import"./main-Dn6sLQP9.js";import"./index-LDgp1SER.js";import"./index-BqqAHTof.js";import"./Link-DYEiCybJ.js";import"./index-BKXgMucs.js";import"./Popover-DrPdnlD_.js";import"./PopupsCommon.css-D_uQUn1t.js";import"./FocusableElements-CinXTr6J.js";import"./isElementHidden-gS67o0pl.js";import"./getActiveElement-kltGt_DR.js";import"./ResizeHandler-OXK24k1p.js";import"./MediaRange-Bnzt26Xb.js";import"./getEffectiveScrollbarStyle-zkSO3ShF.js";import"./copy-C0jPBuQX.js";import"./copy-Ce-W84Ka.js";import"./clsx-B-dksMZM.js";import"./GitHub-Mark-k522kM_X.js";import"./TableOfContent-ClPXL3dS.js";import"./index-DZOOa-3Z.js";import"./I18nStore-Bx61vi8M.js";import"./useStylesheet-Dh6tKsMV.js";import"./index-C5VKFbIU.js";import"./index-DspB395S.js";import"./Label-DvI9nTT8.js";import"./index-BTImLLsO.js";import"./index-g22nm93k.js";import"./Text-BhvqvgTY.js";import"./preview-ZGbtI96M.js";import"./DocsRenderer-CFRXHY34-BCaALCKc.js";import"./client-2ALsBH8W.js";import"./addCustomCSSWithScoping-Br5D2MMD.js";import"./index-CB6Dlc1E.js";import"./BusyIndicator-CP9fjQ10.js";import"./index-BUVDlgt4.js";import"./index-5xYsTlk-.js";import"./AvatarSize-BtKTMFJ5.js";import"./employee-JpmbdEoa.js";import"./List-DPa82ePk.js";import"./ItemNavigation-CN_qUVl5.js";import"./TableUtils-Dw6-SQcm.js";import"./DropIndicator-IZIrFUYd.js";import"./getNormalizedTarget-C4mxORXI.js";import"./debounce-TTkz9ISH.js";import"./ListItemGroup-C5yw13O_.js";import"./TabbableElements-CqFMEI5U.js";import"./index-f4KjJaat.js";import"./ListItemStandard-AMqcHtoK.js";import"./ListItemTemplate-C_ypHU-i.js";import"./edit-CLNeHyx4.js";import"./ListItemAdditionalText.css-LfmHRrff.js";import"./slim-arrow-right-Cg5m15Ca.js";import"./RadioButton-cNkNy5nm.js";import"./ValueState-Bg0UWejw.js";import"./CheckBox-BDt864Yc.js";import"./accept-AveWdi_K.js";import"./Input-J1QehVse.js";import"./ResponsivePopoverCommon.css-CN-Nsjj0.js";import"./ValueStateMessage.css-CX2Qi1Mq.js";import"./Suggestions.css-DGJ1L_cV.js";import"./search-mET5TEas.js";import"./i18n-defaults-STWvP3WT.js";import"./index-BGNjzVxN.js";import"./index-Q2ZmhNoh.js";import"./Dialog-DV9znwaF.js";import"./index-DvktgRJV.js";import"./index-C7JawZaJ.js";import"./index-BUASso1G.js";function i(n){const t={code:"code",h2:"h2",h3:"h3",p:"p",pre:"pre",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(c,{of:d}),`
+`,e.jsx(m,{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.h2,{id:"example",children:"Example"}),`
+`,e.jsx(o,{of:r}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(l,{of:r}),`
+`,e.jsx(t.h2,{id:"opening-selectdialogs",children:"Opening SelectDialogs"}),`
+`,e.jsxs(t.p,{children:["You can open and close the ",e.jsx(t.code,{children:"SelectDialog"})," component in a declarative way using the ",e.jsx(t.code,{children:"open"})," and ",e.jsx(t.code,{children:"opener"})," prop."]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`const MyComponentWithSelectDialog = () => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setDialogIsOpen(true);
+        }}
+      >
+        Open SelectDialog
+      </Button>
+      <SelectDialog open={dialogIsOpen} headerText="Select Product">
+        <ListItemStandard image="pathToImg" description="description" text="Product" />
+      </SelectDialog>
+    </>
+  );
+};
+`})}),`
+`,e.jsx(t.h2,{id:"more-examples",children:"More Examples"}),`
+`,e.jsx(t.h3,{id:"selectdialog-in-multiselect-mode-with-search",children:"SelectDialog in MultiSelect mode with search"}),`
+`,e.jsx(p,{hideCloseButton:!0,children:e.jsxs(e.Fragment,{children:["This example remembers its selection by setting ",e.jsx(t.code,{children:"rememberSelections"})," to ",e.jsx(t.code,{children:"true"}),"."]})}),`
+`,e.jsx(o,{of:h,sourceState:"none"}),`
+`,e.jsxs("details",{children:[e.jsx("summary",{children:"Show Code"}),e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-tsx",children:`const MultiSelectDialog = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  // predefined selection
+  const selectedProducts = { 'HT-102': true, 'HT-203': true, 'HT-1038': true };
+  // number of selected items
+  const [selectedItems, setSelectedItems] = useState<Record<string, boolean>>(selectedProducts);
+  const selectedItemsBeforeOpen = useRef(selectedItems);
+  const [searchVal, setSearchVal] = useState();
+  const [products, setProducts] = useState(Object.keys(selectedProducts));
+
+  const handleBeforeOpen = () => {
+    selectedItemsBeforeOpen.current = selectedItems;
+  };
+  const handleOpen = () => {
+    setDialogOpen(true);
+  };
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
+  // search
+  const handleSearch = (e) => {
+    setSearchVal(e.detail.value);
+  };
+  // reset input value of search field
+  const handleSearchReset = () => {
+    setSearchVal(undefined);
+  };
+  // select/unselect
+  const handleItemClick = (e) => {
+    const itemDescription = e.detail.targetItem.dataset.description;
+    setSelectedItems((prev) => {
+      if (prev[itemDescription]) {
+        const { [itemDescription]: _omit, ...rest } = prev;
+        return rest;
+      } else {
+        return { ...prev, [itemDescription]: true };
+      }
+    });
+  };
+  // clear selection
+  const handleClear = () => {
+    setSelectedItems({});
+  };
+  // confirm selection
+  const handleConfirm = () => {
+    setProducts(Object.keys(selectedItems));
+  };
+  // cancel selection
+  const handleCancel = () => {
+    setSelectedItems(selectedItemsBeforeOpen.current);
+  };
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Open Dialog</Button>
+      <SelectDialog
+        open={dialogOpen}
+        mode={ListSelectionMode.Multiple}
+        numberOfSelectedItems={Object.keys(selectedItems).length}
+        listProps={{ onSelectionChange: handleItemClick }}
+        showClearButton
+        rememberSelections
+        onClear={handleClear}
+        onConfirm={handleConfirm}
+        onClose={handleClose}
+        onSearchInput={handleSearch}
+        onSearch={handleSearch}
+        onSearchReset={handleSearchReset}
+        onBeforeOpen={handleBeforeOpen}
+        onCancel={handleCancel}
+      >
+        {new Array(40)
+          .fill('')
+          .map((_, index) => {
+            const currentProduct = listItems[index % 4];
+            const description = \`\${currentProduct.description}\${index}\`;
+            const lowerCaseSearchVal = searchVal?.toLowerCase();
+            if (
+              searchVal &&
+              !description.toLowerCase().includes(lowerCaseSearchVal) &&
+              !currentProduct.text.toLowerCase().includes(lowerCaseSearchVal)
+            ) {
+              return null;
+            }
+            return (
+              <ListItemStandard
+                image={currentProduct.img}
+                description={\`\${currentProduct.description}\${index}\`}
+                data-description={\`\${currentProduct.description}\${index}\`}
+                key={\`\${currentProduct.text}\${index}\`}
+                selected={selectedItems[description]}
+                text={currentProduct.text}
+              />
+            );
+          })
+          .filter(Boolean)}
+      </SelectDialog>
+      <FlexBox>
+        <Label>Selected: </Label>
+        <Text>{products.join(', ')}</Text>
+      </FlexBox>
+    </>
+  );
+};
+`})})]}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(a,{})]})}function xt(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{xt as default};
