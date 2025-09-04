9124977aa98b2e5d8661a5fa3da4b0b92c226d9d
Lukas Harbarth <lukas.harbarth@sap.com>
2025-09-04 13:29:49 +0200

    add Search subcomponents and example

--- packages/main/src/webComponents/Search/Search.stories.tsx
+++ packages/main/src/webComponents/Search/Search.stories.tsx
@@ -1,6 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import globeIcon from '@ui5/webcomponents-icons/dist/globe.js';
+import historyIcon from '@ui5/webcomponents-icons/dist/history.js';
+import searchIcon from '@ui5/webcomponents-icons/dist/search.js';
 import { useState } from 'react';
 import { SearchItem } from '../SearchItem/index.js';
+import { SearchItemGroup } from '../SearchItemGroup/index.js';
+import { SearchItemShowMore } from '../SearchItemShowMore/index.js';
 import { SearchScope } from '../SearchScope/index.js';
 import { Search } from './index.js';
 import type { SearchPropTypes } from './index.js';
@@ -66,6 +71,54 @@ export const WithScopeAndItems: Story = {
         {filterData.map((item) => {
           return <SearchItem key={item.name} text={item.name} scopeName={item.scope} />;
         })}
+      </Search>
+    );
+  },
+};
+
+const allItems = [
+  { text: 'Search Item 1', icon: historyIcon },
+  { text: 'Search Item 2', icon: searchIcon },
+  { text: 'Search Item 3', icon: historyIcon },
+  { text: 'Search Item 4', icon: historyIcon },
+  { text: 'Search Item 5', icon: searchIcon },
+  { text: 'Search Item 6', icon: globeIcon },
+];
+const visibleItems = allItems.slice(0, 3);
+const hiddenItems = allItems.slice(3);
+
+export const ShowMoreItem: Story = {
+  args: {
+    showClearIcon: true,
+    placeholder: 'Placeholder',
+  },
+  render(args) {
+    const [showHiddenItems, setShowHiddenItems] = useState(false);
+
+    return (
+      <Search {...args}>
+        <SearchItemGroup headerText="Group Header 1">
+          {visibleItems.map((item) => {
+            return <SearchItem key={item.text} text={item.text} icon={item.icon} />;
+          })}
+          {!showHiddenItems && (
+            <SearchItemShowMore
+              itemsToShowCount={hiddenItems.length}
+              onClick={() => {
+                setShowHiddenItems(true);
+              }}
+            />
+          )}
+          {showHiddenItems &&
+            hiddenItems.map((item) => {
+              return <SearchItem key={item.text} text={item.text} icon={item.icon} />;
+            })}
+        </SearchItemGroup>
+        <SearchItemGroup headerText="Group Header 2">
+          <SearchItem text="Search Item 1" icon={historyIcon} />
+          <SearchItem text="Search Item 2" icon={historyIcon} />
+          <SearchItem text="Search Item 3" icon={globeIcon} />
+        </SearchItemGroup>
       </Search>
     );
   },
